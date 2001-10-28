59ac5a936ca7eab31c8422b934f1872e61612f6a
Nick Kurshev <nickols_k@mail.ru>
2001-10-28 10:28:08 +0000

    vo_vesa: more rgb2rgb support
    Originally committed as revision 2506 to svn://svn.mplayerhq.hu/mplayer/trunk/postproc

--- postproc/rgb2rgb.c
+++ postproc/rgb2rgb.c
@@ -2,6 +2,8 @@
 #include "../config.h"
 #include "rgb2rgb.h"
 
+/* TODO: MMX optimization */
+
 void rgb24to32(uint8_t *src,uint8_t *dst,uint32_t src_size)
 {
   uint32_t *dest = (uint32_t *)dst;
@@ -14,5 +16,20 @@ void rgb24to32(uint8_t *src,uint8_t *dst,uint32_t src_size)
     rgb0 = *(uint32_t *)s;
     *dest++ = rgb0 & 0xFFFFFFUL;
     s += 3;
+  }
+}
+
+void rgb32to24(uint8_t *src,uint8_t *dst,uint32_t src_size)
+{
+  uint8_t *dest = dst;
+  uint8_t *s = src;
+  uint8_t *end;
+  end = s + src_size;
+  while(s < end)
+  {
+    *dest++ = *s++;
+    *dest++ = *s++;
+    *dest++ = *s++;
+    s++;
   }
 }
--- postproc/rgb2rgb.h
+++ postproc/rgb2rgb.h
@@ -1,6 +1,6 @@
 /* 
  *
- *  rgb2rgb.h, Software RGB to RGB coverter
+ *  rgb2rgb.h, Software RGB to RGB converter
  *
  */
 
@@ -8,5 +8,6 @@
 #define RGB2RGB_INCLUDED
 
 extern void rgb24to32(uint8_t *src,uint8_t *dst,uint32_t src_size);
+extern void rgb32to24(uint8_t *src,uint8_t *dst,uint32_t src_size);
 
 #endif
