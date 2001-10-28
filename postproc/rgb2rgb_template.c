fde33ab52ee76bc07a58a1b70f7b18bd61e67cb2
Nick Kurshev <nickols_k@mail.ru>
2001-10-28 11:00:40 +0000

    Prev ver could work only on x86
    Originally committed as revision 2509 to svn://svn.mplayerhq.hu/mplayer/trunk/postproc

--- postproc/rgb2rgb_template.c
+++ postproc/rgb2rgb_template.c
@@ -7,16 +7,16 @@
 
 void rgb24to32(uint8_t *src,uint8_t *dst,uint32_t src_size)
 {
-  uint32_t *dest = (uint32_t *)dst;
+  uint8_t *dest = dst;
   uint8_t *s = src;
   uint8_t *end;
   end = s + src_size;
   while(s < end)
   {
-    uint32_t rgb0;
-    rgb0 = *(uint32_t *)s;
-    *dest++ = rgb0 & 0xFFFFFFUL;
-    s += 3;
+    *dest++ = *s++;
+    *dest++ = *s++;
+    *dest++ = *s++;
+    *dest++ = 0;
   }
 }
 
