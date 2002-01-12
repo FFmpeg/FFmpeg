eede215681e27f4a32d6327ef79781ed2e0e9c61
Arpi <arpi@thot.banki.hu>
2002-01-12 14:36:40 +0000

    rv10 sig11 fix by Michael Niedermayer <michaelni@gmx.at>
    Originally committed as revision 258 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/mpegvideo.c
+++ libavcodec/mpegvideo.c
@@ -413,7 +413,7 @@ void MPV_frame_end(MpegEncContext *s)
 {
     /* draw edge for correct motion prediction if outside */
     if (s->pict_type != B_TYPE) {
-      if(s->avctx->codec->id!=CODEC_ID_MPEG4){
+      if(s->avctx==NULL || s->avctx->codec->id!=CODEC_ID_MPEG4){
         draw_edges(s->current_picture[0], s->linesize, s->mb_width*16, s->mb_height*16, EDGE_WIDTH);
         draw_edges(s->current_picture[1], s->linesize/2, s->mb_width*8, s->mb_height*8, EDGE_WIDTH/2);
         draw_edges(s->current_picture[2], s->linesize/2, s->mb_width*8, s->mb_height*8, EDGE_WIDTH/2);
