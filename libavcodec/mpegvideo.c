2a052693dd5d81397345aecc34898f6c61adc513
Michael Niedermayer <michaelni@gmx.at>
2002-06-02 13:31:48 +0000

    divx5 bug workaround
    Originally committed as revision 647 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/mpegvideo.c
+++ libavcodec/mpegvideo.c
@@ -599,7 +599,7 @@ void MPV_frame_end(MpegEncContext *s)
 
     /* draw edge for correct motion prediction if outside */
     if (s->pict_type != B_TYPE && !s->intra_only) {
-      if(s->avctx==NULL || s->avctx->codec->id!=CODEC_ID_MPEG4 || s->divx_version==500){
+      if(s->avctx==NULL || s->avctx->codec->id!=CODEC_ID_MPEG4 || s->divx_version>=500){
         draw_edges(s->current_picture[0], s->linesize, s->mb_width*16, s->mb_height*16, EDGE_WIDTH);
         draw_edges(s->current_picture[1], s->linesize/2, s->mb_width*8, s->mb_height*8, EDGE_WIDTH/2);
         draw_edges(s->current_picture[2], s->linesize/2, s->mb_width*8, s->mb_height*8, EDGE_WIDTH/2);
