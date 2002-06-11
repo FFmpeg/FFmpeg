d714edf190413ff34eee7938c51dc9f04a428aa3
Fabrice Bellard <fabrice@bellard.org>
2002-06-11 13:23:09 +0000

    optimized ticker_tick() if num is big - please test it (should reduce CPU usage - this code is temporary before new PTS handling scheme)
    Originally committed as revision 681 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libav/tick.h
+++ libav/tick.h
@@ -22,10 +22,20 @@ static inline int ticker_tick(Ticker *tick, int num)
     int n = num * tick->div;
 
     tick->value += num * tick->mod;
+#if 1
+    if (tick->value > 0) {
+        n += (tick->value / tick->inrate);
+        tick->value = tick->value % tick->inrate;
+        if (tick->value > 0) {
+            tick->value -= tick->inrate;
+            n++;
+        }
+    }
+#else
     while (tick->value > 0) {
         tick->value -= tick->inrate;
         n++;
     }
-
+#endif
     return n;
 }
