f197d19a21d3ae185f55d62efba1b272459e835b
Zdenek Kabelac <kabi@informatics.muni.cz>
2002-07-10 14:34:06 +0000

    * clean compiler warning
    Originally committed as revision 733 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/mjpeg.c
+++ libavcodec/mjpeg.c
@@ -372,10 +372,10 @@ static void jpeg_put_comments(MpegEncContext *s)
     flush_put_bits(p);
     ptr = pbBufPtr(p);
     put_bits(p, 16, 0); /* patched later */
-#define VERSION "FFmpeg" LIBAVCODEC_VERSION "b" LIBAVCODEC_BUILD_STR
-    put_string(p, VERSION);
-    size = strlen(VERSION)+3;
-#undef VERSION
+#define MJPEG_VERSION "FFmpeg" LIBAVCODEC_VERSION "b" LIBAVCODEC_BUILD_STR
+    put_string(p, MJPEG_VERSION);
+    size = strlen(MJPEG_VERSION)+3;
+#undef MJPEG_VERSION
     ptr[0] = size >> 8;
     ptr[1] = size;
 }
