ec7e0bf0fd738afcf5022fc13a3964cb28ece12c
Zdenek Kabelac <kabi@informatics.muni.cz>
2002-11-11 09:43:43 +0000

    * using DSPContext - so each codec could use its local (sub)set of CPU extension * I think IDCT needs some better generic solution
    Originally committed as revision 1195 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/i386/simple_idct_mmx.c
+++ libavcodec/i386/simple_idct_mmx.c
@@ -1301,10 +1301,10 @@ void ff_simple_idct_mmx(int16_t *block)
 void ff_simple_idct_put_mmx(UINT8 *dest, int line_size, DCTELEM *block)
 {
     idct(block);
-    put_pixels_clamped(block, dest, line_size);
+    put_pixels_clamped_mmx(block, dest, line_size);
 }
 void ff_simple_idct_add_mmx(UINT8 *dest, int line_size, DCTELEM *block)
 {
     idct(block);
-    add_pixels_clamped(block, dest, line_size);
+    add_pixels_clamped_mmx(block, dest, line_size);
 }
