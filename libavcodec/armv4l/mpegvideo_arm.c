594026277250fecf0ba34c9e1498921936c6bfed
Zdenek Kabelac <kabi@informatics.muni.cz>
2002-11-19 10:08:31 +0000

    * oops fixed bad initialization of ff vals.   - put FF_LIBMPEG2_IDCT_PERM into CVS - so it will work for now
    Originally committed as revision 1227 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/armv4l/mpegvideo_arm.c
+++ libavcodec/armv4l/mpegvideo_arm.c
@@ -44,12 +44,12 @@ void MPV_common_init_armv4l(MpegEncContext *s)
     int i;
     const int idct_algo= s->avctx->idct_algo;
 
-    ff_put_pixels_clamped = s->avctx->dsp.put_pixels_clamped;
-    ff_add_pixels_clamped = s->avctx->dsp.put_pixels_clamped;
+    ff_put_pixels_clamped = s->dsp.put_pixels_clamped;
+    ff_add_pixels_clamped = s->dsp.put_pixels_clamped;
 
     if(idct_algo==FF_IDCT_AUTO || idct_algo==FF_IDCT_ARM){
         s->idct_put= arm_idct_put;
         s->idct_add= arm_idct_add;
-        s->idct_permutation_type= FF_NO_IDCT_PERM;
+        s->idct_permutation_type= FF_LIBMPEG2_IDCT_PERM;/* FF_NO_IDCT_PERM */
     }
 }
