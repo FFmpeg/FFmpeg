bb285683646d181c889798bdf26b83e5d03a5212
Zdenek Kabelac <kabi@informatics.muni.cz>
2002-11-20 07:32:18 +0000

    * cut&paste fix
    Originally committed as revision 1249 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/armv4l/mpegvideo_arm.c
+++ libavcodec/armv4l/mpegvideo_arm.c
@@ -45,7 +45,7 @@ void MPV_common_init_armv4l(MpegEncContext *s)
     const int idct_algo= s->avctx->idct_algo;
 
     ff_put_pixels_clamped = s->dsp.put_pixels_clamped;
-    ff_add_pixels_clamped = s->dsp.put_pixels_clamped;
+    ff_add_pixels_clamped = s->dsp.add_pixels_clamped;
 
     if(idct_algo==FF_IDCT_AUTO || idct_algo==FF_IDCT_ARM){
         s->idct_put= arm_idct_put;
