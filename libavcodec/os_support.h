5beaa01b4a88607e534984b6d1bd12e527cc1085
Alex Beregszaszi <alex@rtfs.hu>
2003-03-14 11:21:19 +0000

    10l
    Originally committed as revision 1683 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/os_support.h
+++ libavcodec/os_support.h
@@ -25,7 +25,7 @@
 #if defined(CONFIG_OS2)
 #include <stdlib.h>
 static inline int usleep(unsigned int t) { return _sleep2(t / 1000); }
-static inline float floatf(float f) { return floor(f); }
+static inline float floorf(float f) { return floor(f); }
 static inline int strcasecmp(const char* s1, const char* s2) { return stricmp(s1,s2); }
 #endif
 
