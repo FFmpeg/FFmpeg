d746f9281f5de2e4e9a8be6b848f1f77d6f4202b
Philip Gladstone <philipjsg@users.sourceforge.net>
2003-04-12 03:04:55 +0000

    Add a file serving test as well
    Originally committed as revision 1763 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- tests/server-regression.sh
+++ tests/server-regression.sh
@@ -1,21 +1,34 @@
 #!/bin/bash
 
-FILES=`perl -n -e 'print \$1, " " if /<stream\\s+(\\S+)>/i' test.conf`
+# Make sure that the data directory exists
+mkdir -p data
+
+cp test.conf data/test.conf
+perl -e 'chomp($wd = `pwd`); print map { s!data/!!; "<Stream $_>\nFile $wd/data/$_\n</Stream>\n\n" } @ARGV' data/a* >> data/test.conf
+perl -e 'chomp($wd = `pwd`); print map { s!data/!!; "<Stream $_.asf>\nFile $wd/data/$_\n</Stream>\n\n" } @ARGV' data/a* >> data/test.conf
+
+FILES=`perl -n -e 'print \$1, " " if /<stream\\s+(\\S+)>/i' data/test.conf`
 
 rm -f /tmp/feed.ffm
-../ffserver -d -f test.conf 2> /dev/null &
+../ffserver -d -f data/test.conf 2> /dev/null &
 FFSERVER_PID=$!
-sleep 1
+echo "Waiting for feeds to startup..."
+sleep 2
 (
-    cd data
-    rm -f $FILES;
+    cd data || exit $?
+    rm -f ff-*;
+    WGET_OPTIONS="--user-agent=NSPlayer -q --proxy=off -e verbose=off -e debug=off -e server_response=off"
     for file in $FILES; do
-        wget --user-agent=NSPlayer -q --proxy=off -e verbose=off -e debug=off -e server_response=off \
-          --output-document=- http://localhost:9999/$file?date=19700101T000000Z | head --bytes=100000 > $file &
+        if [ `expr match $file "a-*"` -ne 0 ]; then
+            wget $WGET_OPTIONS --output-document=- http://localhost:9999/$file > ff-$file &
+        else
+            wget $WGET_OPTIONS --output-document=- http://localhost:9999/$file?date=19700101T000000Z | head --bytes=100000 > ff-$file &
+        fi
+        MDFILES="$MDFILES ff-$file"
     done    
     wait
     # the status page is always different
-    md5sum $FILES | grep -v html > ffserver.regression
+    md5sum $MDFILES | grep -v html > ffserver.regression
 )
 kill $FFSERVER_PID
 wait > /dev/null 2>&1
