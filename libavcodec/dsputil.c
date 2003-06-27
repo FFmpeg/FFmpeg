89ebf4e85ef14dbfb469b8ca9264a337208d5aca
Mike Melanson <mike@multimedia.cx>
2003-06-27 15:03:58 +0000

    Fix third pixel motion compensation
    Originally committed as revision 1990 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/dsputil.c
+++ libavcodec/dsputil.c
@@ -999,7 +999,7 @@ static inline void put_tpel_pixels_mc12_c(uint8_t *dst, const uint8_t *src, int 
     int i,j;
     for (i=0; i < height; i++) {
       for (j=0; j < width; j++) {
-	dst[j] = (2731*(3*src[j] + 4*src[j+1] + 2*src[j+stride] + 3*src[j+stride+1] + 6)) >> 15;
+	dst[j] = (2731*(3*src[j] + 2*src[j+1] + 4*src[j+stride] + 3*src[j+stride+1] + 6)) >> 15;
       }
       src += stride;
       dst += stride;
@@ -1021,7 +1021,7 @@ static inline void put_tpel_pixels_mc21_c(uint8_t *dst, const uint8_t *src, int 
     int i,j;
     for (i=0; i < height; i++) {
       for (j=0; j < width; j++) {
-	dst[j] = (2731*(3*src[j] + 2*src[j+1] + 4*src[j+stride] + 3*src[j+stride+1] + 6)) >> 15;
+	dst[j] = (2731*(3*src[j] + 4*src[j+1] + 2*src[j+stride] + 3*src[j+stride+1] + 6)) >> 15;
       }
       src += stride;
       dst += stride;
@@ -1096,7 +1096,7 @@ static inline void avg_tpel_pixels_mc12_c(uint8_t *dst, const uint8_t *src, int 
     int i,j;
     for (i=0; i < height; i++) {
       for (j=0; j < width; j++) {
-	dst[j] = (dst[j] + ((2731*(3*src[j] + 4*src[j+1] + 2*src[j+stride] + 3*src[j+stride+1] + 6)) >> 15) + 1) >> 1;
+	dst[j] = (dst[j] + ((2731*(3*src[j] + 2*src[j+1] + 4*src[j+stride] + 3*src[j+stride+1] + 6)) >> 15) + 1) >> 1;
       }
       src += stride;
       dst += stride;
@@ -1118,7 +1118,7 @@ static inline void avg_tpel_pixels_mc21_c(uint8_t *dst, const uint8_t *src, int 
     int i,j;
     for (i=0; i < height; i++) {
       for (j=0; j < width; j++) {
-	dst[j] = (dst[j] + ((2731*(3*src[j] + 2*src[j+1] + 4*src[j+stride] + 3*src[j+stride+1] + 6)) >> 15) + 1) >> 1;
+	dst[j] = (dst[j] + ((2731*(3*src[j] + 4*src[j+1] + 2*src[j+stride] + 3*src[j+stride+1] + 6)) >> 15) + 1) >> 1;
       }
       src += stride;
       dst += stride;
