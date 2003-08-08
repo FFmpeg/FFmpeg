32f38cb4ccbbaad721ce34755bef2c6f028b3947
Fabrice Bellard <fabrice@bellard.org>
2003-08-08 17:54:05 +0000

    exports mpegps_demux
    Originally committed as revision 2112 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/mpeg.c
+++ libavformat/mpeg.c
@@ -690,7 +690,7 @@ static AVOutputFormat mpeg2vob_mux = {
     mpeg_mux_end,
 };
 
-static AVInputFormat mpegps_demux = {
+AVInputFormat mpegps_demux = {
     "mpeg",
     "MPEG PS format",
     sizeof(MpegDemuxContext),
