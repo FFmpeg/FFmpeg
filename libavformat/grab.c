72a476c1e1f50246af0bfb906a92bb2a38b8ebab
Michael Niedermayer <michaelni@gmx.at>
2003-09-09 19:14:05 +0000

    kernel header bug workaround by (Ronald Bultje <rbultje at ronald dot bitfreak dot net>)
    Originally committed as revision 2241 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/grab.c
+++ libavformat/grab.c
@@ -17,12 +17,13 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 #include "avformat.h"
-#include <linux/videodev.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/ioctl.h>
 #include <sys/mman.h>
 #include <sys/time.h>
+#define _LINUX_TIME_H 1
+#include <linux/videodev.h>
 #include <time.h>
 
 typedef struct {
