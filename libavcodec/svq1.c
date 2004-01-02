2ad5d5a8c834c53ca11402d6cdc52329487e8a2c
Mike Melanson <mike@multimedia.cx>
2004-01-02 02:31:53 +0000

    off-by-1 error in the never-before-tested embedded string code
    Originally committed as revision 2649 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/svq1.c
+++ libavcodec/svq1.c
@@ -579,7 +579,7 @@ static void svq1_parse_string (GetBitContext *bitbuf, uint8_t *out) {
 
   seed = string_table[out[0]];
 
-  for (i=1; i < out[0]; i++) {
+  for (i=1; i <= out[0]; i++) {
     out[i] = get_bits (bitbuf, 8) ^ seed;
     seed   = string_table[out[i] ^ seed];
   }
