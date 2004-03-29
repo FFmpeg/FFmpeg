58c2182d728507bd69496b172dced477fee9c4ca
Michael Niedermayer <michaelni@gmx.at>
2004-03-29 00:29:27 +0000

    fixing alignment, 2nd try
    Originally committed as revision 2940 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/h263.c
+++ libavcodec/h263.c
@@ -2996,8 +2996,8 @@ void ff_mpeg4_init_partitions(MpegEncContext *s)
     uint8_t *start= pbBufPtr(&s->pb);
     uint8_t *end= s->pb.buf_end;
     int size= end - start;
-    int pb_size = (size/3)&(~1);
-    int tex_size= size - 2*pb_size;
+    int pb_size = (((int)start + size/3)&(~3)) - (int)start;
+    int tex_size= (size - 2*pb_size)&(~3);
     
     set_put_bits_buffer_size(&s->pb, pb_size);
     init_put_bits(&s->tex_pb, start + pb_size           , tex_size);
