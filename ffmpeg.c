b2e3c52814bdbced4ac3a3e3187dee8d72441106
Michael Niedermayer <michaelni@gmx.at>
2004-04-09 14:50:00 +0000

    require the user to explicitly specifiy --enable-gpl before enabling of gpl parts becomes possible print correct license if --enable-gpl is specified
    Originally committed as revision 2985 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- ffmpeg.c
+++ ffmpeg.c
@@ -3488,6 +3488,21 @@ static void show_license(void)
 {
     show_banner();
     printf(
+#ifdef CONFIG_GPL
+    "This program is free software; you can redistribute it and/or modify\n"   
+    "it under the terms of the GNU General Public License as published by\n"
+    "the Free Software Foundation; either version 2 of the License, or\n"
+    "(at your option) any later version.\n"
+    "\n"
+    "This program is distributed in the hope that it will be useful,\n"
+    "but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
+    "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n"
+    "GNU General Public License for more details.\n"
+    "\n"
+    "You should have received a copy of the GNU General Public License\n"
+    "along with this program; if not, write to the Free Software\n"
+    "Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA\n"
+#else
     "This library is free software; you can redistribute it and/or\n"
     "modify it under the terms of the GNU Lesser General Public\n"
     "License as published by the Free Software Foundation; either\n"
@@ -3501,6 +3516,7 @@ static void show_license(void)
     "You should have received a copy of the GNU Lesser General Public\n"
     "License along with this library; if not, write to the Free Software\n"
     "Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA\n"
+#endif
     );
     exit(1);
 }
