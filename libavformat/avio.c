31290c2b9bdf64c7aad83a76d1643c4ccd374492
Michael Niedermayer <michaelni@gmx.at>
2004-04-11 02:07:31 +0000

    avoid seeking to the end, as it confuses some crappy code
    Originally committed as revision 2996 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/avio.c
+++ libavformat/avio.c
@@ -147,7 +147,7 @@ offset_t url_filesize(URLContext *h)
     offset_t pos, size;
     
     pos = url_seek(h, 0, SEEK_CUR);
-    size = url_seek(h, 0, SEEK_END);
+    size = url_seek(h, -1, SEEK_END)+1;
     url_seek(h, pos, SEEK_SET);
     return size;
 }
