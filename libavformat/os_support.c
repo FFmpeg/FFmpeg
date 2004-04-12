17c22f574b2817d9782ff3404a1b528221e3728b
Steven M. Schultz <sms@2bsd.com>
2004-04-12 22:54:48 +0000

    libavformat/os_support.c HAVE_LOCALTIME_R patch by ("Steven M. Schultz" <sms at 2BSD dot COM>)
    Originally committed as revision 3009 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/os_support.c
+++ libavformat/os_support.c
@@ -16,6 +16,7 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
+#include "config.h"
 #include "avformat.h"
 #ifdef CONFIG_WIN32
 #include <sys/types.h>
