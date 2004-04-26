d890d4aed1c38c99a07c4f10e45b6369499f00fc
Alex Beregszaszi <alex@rtfs.hu>
2004-04-26 08:51:56 +0000

    disable hadamard for gcc below 3.3 - better fix awaited
    Originally committed as revision 3075 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/ppc/dsputil_ppc.c
+++ libavcodec/ppc/dsputil_ppc.c
@@ -279,8 +279,10 @@ void dsputil_init_ppc(DSPContext* c, AVCodecContext *avctx)
         
 	c->gmc1 = gmc1_altivec;
 
+#if (__GNUC__ * 100 + __GNUC_MINOR__ >= 330)
 	c->hadamard8_diff[0] = hadamard8_diff16_altivec;
 	c->hadamard8_diff[1] = hadamard8_diff8x8_altivec;
+#endif
 
 #ifdef CONFIG_ENCODERS
 	if (avctx->dct_algo == FF_DCT_AUTO ||
