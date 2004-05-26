12013f67c4559c94e6be616ae3d66a5af8db2395
Michael Niedermayer <michaelni@gmx.at>
2004-05-26 20:04:49 +0000

    10l fixes by ("Debabrata Banerjee" <davatar at comcast dot net>)
    Originally committed as revision 3161 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/pthread.c
+++ libavcodec/pthread.c
@@ -87,7 +87,7 @@ void avcodec_thread_free(AVCodecContext *avctx)
     
     pthread_mutex_lock(&c->current_job_lock);
     c->done = 1;
-    pthread_cond_signal(&c->current_job_cond);
+    pthread_cond_broadcast(&c->current_job_cond);
     pthread_mutex_unlock(&c->current_job_lock);
 
     for (i=0; i<avctx->thread_count; i++)
@@ -97,7 +97,7 @@ void avcodec_thread_free(AVCodecContext *avctx)
     pthread_cond_destroy(&c->current_job_cond);
     pthread_cond_destroy(&c->last_job_cond);
     av_free(c->workers);
-    av_freep(c);
+    av_free(c);
 }
 
 int avcodec_thread_execute(AVCodecContext *avctx, action_t* func, void **arg, int *ret, int job_count) 
