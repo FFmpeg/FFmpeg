61b3b639e7f2b482719d543a20efde1e6035c6d7
Michael Niedermayer <michaelni@gmx.at>
2004-07-06 12:27:36 +0000

    dc_precission=11 fix
    Originally committed as revision 3291 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/i386/mpegvideo_mmx_template.c
+++ libavcodec/i386/mpegvideo_mmx_template.c
@@ -58,7 +58,7 @@ static int RENAME(dct_quantize)(MpegEncContext *s,
         if (!s->h263_aic) {
 #if 1
         asm volatile (
-        	"imul %%ecx		\n\t"
+        	"mul %%ecx		\n\t"
         	: "=d" (level), "=a"(dummy)
         	: "a" ((block[0]>>2) + q), "c" (inverse[q<<1])
         );
