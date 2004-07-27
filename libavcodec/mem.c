c07a22fb9fc5cfa24b5b80f83e8f08b17937ca97
Michael Niedermayer <michaelni@gmx.at>
2004-07-27 11:09:41 +0000

    realloc(NULL) fix
    Originally committed as revision 3351 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/mem.c
+++ libavcodec/mem.c
@@ -95,7 +95,9 @@ void *av_realloc(void *ptr, unsigned int size)
 {
 #ifdef MEMALIGN_HACK
     //FIXME this isnt aligned correctly though it probably isnt needed
-    int diff= ptr ? ((char*)ptr)[-1] : 0;
+    int diff;
+    if(!ptr) return av_malloc(size);
+    diff= ((char*)ptr)[-1];
     return realloc(ptr - diff, size + diff) + diff;
 #else
     return realloc(ptr, size);
