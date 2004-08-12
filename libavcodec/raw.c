6d24231e504f71a76a8fabe87c8d7cfa826da75a
Michael Niedermayer <michaelni@gmx.at>
2004-08-12 23:36:48 +0000

    give the user a chance to override codec_tag
    Originally committed as revision 3382 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/raw.c
+++ libavcodec/raw.c
@@ -151,7 +151,8 @@ static int raw_init_encoder(AVCodecContext *avctx)
     avctx->coded_frame = (AVFrame *)avctx->priv_data;
     avctx->coded_frame->pict_type = FF_I_TYPE;
     avctx->coded_frame->key_frame = 1;
-    avctx->codec_tag = findFourCC(avctx->pix_fmt);
+    if(!avctx->codec_tag)
+        avctx->codec_tag = findFourCC(avctx->pix_fmt);
     return 0;
 }
 
