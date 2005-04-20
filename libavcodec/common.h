ee572c5482d9aacbf2cfaf3a6b717c32ba1b5ac5
Michael Niedermayer <michaelni@gmx.at>
2005-04-20 10:05:15 +0000

    remove duplicate clip to 8bit function
    Originally committed as revision 4143 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/common.h
+++ libavcodec/common.h
@@ -501,8 +501,6 @@ tend= rdtsc();\
 #define STOP_TIMER(id) {}
 #endif
 
-#define CLAMP_TO_8BIT(d) ((d > 0xff) ? 0xff : (d < 0) ? 0 : d)
-
 /* avoid usage of various functions */
 #define malloc please_use_av_malloc
 #define free please_use_av_free
