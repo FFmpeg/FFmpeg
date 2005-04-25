3ca4b65479f587d5d79080699d16cdd5c9ec7e0a
Michael Niedermayer <michaelni@gmx.at>
2005-04-25 15:13:39 +0000

    os2.diff by (Paul Smedley // paul smedley info)
    Originally committed as revision 4160 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- ffplay.c
+++ ffplay.c
@@ -28,6 +28,23 @@
 #undef main /* We don't want SDL to override our main() */
 #endif
 
+#ifdef CONFIG_OS2
+#define INCL_DOS
+ #include <os2.h>
+ #include <stdio.h>
+ 
+ void MorphToPM()
+ {
+   PPIB pib;
+   PTIB tib;
+ 
+   DosGetInfoBlocks(&tib, &pib);
+ 
+   // Change flag from VIO to PM:
+   if (pib->pib_ultype==2) pib->pib_ultype = 3;
+ }
+#endif
+
 #if defined(__linux__)
 #define HAVE_X11
 #endif
@@ -1886,6 +1903,14 @@ int main(int argc, char **argv)
     
     /* register all codecs, demux and protocols */
     av_register_all();
+
+    #ifdef CONFIG_OS2
+      MorphToPM(); // Morph the VIO application to a PM one to be able to use Win* functions
+ 
+      // Make stdout and stderr unbuffered
+      setbuf( stdout, NULL );
+      setbuf( stderr, NULL );
+    #endif
 
     parse_options(argc, argv, options);
 
