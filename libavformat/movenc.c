0c1e0bab6640e922b1ca17a2b2f94a978005b4e7
Erik Slagter <erik@slagter.name>
2005-06-19 23:45:54 +0000

    This trivial patch adds "avc1" as fourcc for h264 in mp4, which makes it a little bit more standard, although it still seems to lack some metadata. Anyway, recent mplayer plays these streams without problems.
    It also has two globally scoped global structs changed to "static".
    patch by (Erik Slagter <erik slagter name)
    
    Originally committed as revision 4386 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/movenc.c
+++ libavformat/movenc.c
@@ -259,7 +259,7 @@ static int mov_write_wave_tag(ByteIOContext *pb, MOVTrack* track)
     return updateSize (pb, pos);
 }
 
-const CodecTag codec_movaudio_tags[] = {
+static const CodecTag codec_movaudio_tags[] = {
     { CODEC_ID_PCM_MULAW, MKTAG('u', 'l', 'a', 'w') },
     { CODEC_ID_PCM_ALAW, MKTAG('a', 'l', 'a', 'w') },
     { CODEC_ID_ADPCM_IMA_QT, MKTAG('i', 'm', 'a', '4') },
@@ -491,11 +491,12 @@ static int mov_write_esds_tag(ByteIOContext *pb, MOVTrack* track) // Basic
     return updateSize (pb, pos);
 }
 
-const CodecTag codec_movvideo_tags[] = {
+static const CodecTag codec_movvideo_tags[] = {
     { CODEC_ID_SVQ1, MKTAG('S', 'V', 'Q', '1') },
     { CODEC_ID_SVQ3, MKTAG('S', 'V', 'Q', '3') },
     { CODEC_ID_MPEG4, MKTAG('m', 'p', '4', 'v') },
     { CODEC_ID_H263, MKTAG('s', '2', '6', '3') },
+    { CODEC_ID_H264, MKTAG('a', 'v', 'c', '1') },
     { CODEC_ID_DVVIDEO, MKTAG('d', 'v', 'c', ' ') },
     { 0, 0 },
 };
