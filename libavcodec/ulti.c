282152b13342b698e91c861b42b253b55017d583
Alex Beregszaszi <alex@rtfs.hu>
2005-07-02 19:10:30 +0000

    1l
    Originally committed as revision 4414 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/ulti.c
+++ libavcodec/ulti.c
@@ -1,5 +1,5 @@
 /*
- *
+ * IBM Ultimotion Video Decoder
  * Copyright (C) 2004 Konstantin Shishkov
  *
  * This library is free software; you can redistribute it and/or
@@ -15,8 +15,6 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
- * IBM Ultimotion Video Decoder
  *
  */
 
