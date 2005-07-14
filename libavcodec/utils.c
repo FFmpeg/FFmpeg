fec9ccb7e6fdc6844b1e2d1bb95436b36f47a412
Michael Niedermayer <michaelni@gmx.at>
2005-07-14 15:30:39 +0000

    check for CODEC_CAP_DELAY in audio decoders too
    Originally committed as revision 4439 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/utils.c
+++ libavcodec/utils.c
@@ -642,9 +642,12 @@ int avcodec_decode_audio(AVCodecContext *avctx, int16_t *samples,
     int ret;
 
     *frame_size_ptr= 0;
-    ret = avctx->codec->decode(avctx, samples, frame_size_ptr, 
-                               buf, buf_size);
-    avctx->frame_number++;
+    if((avctx->codec->capabilities & CODEC_CAP_DELAY) || buf_size){
+        ret = avctx->codec->decode(avctx, samples, frame_size_ptr, 
+                                buf, buf_size);
+        avctx->frame_number++;
+    }else
+        ret= 0;
     return ret;
 }
 
