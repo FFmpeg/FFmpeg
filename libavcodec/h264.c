1fd97d8c1aba061d03caa3d02ec31ee2ef5b6073
Loren Merritt <lorenm@u.washington.edu>
2005-11-03 23:26:51 +0000

    tidier cabac_intra4x4_pred_mode
    Originally committed as revision 4673 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/h264.c
+++ libavcodec/h264.c
@@ -5291,12 +5291,10 @@ static int decode_cabac_mb_intra4x4_pred_mode( H264Context *h, int pred_mode ) {
     if( get_cabac( &h->cabac, &h->cabac_state[68] ) )
         return pred_mode;
 
-    if( get_cabac( &h->cabac, &h->cabac_state[69] ) )
-        mode += 1;
-    if( get_cabac( &h->cabac, &h->cabac_state[69] ) )
-        mode += 2;
-    if( get_cabac( &h->cabac, &h->cabac_state[69] ) )
-        mode += 4;
+    mode += 1 * get_cabac( &h->cabac, &h->cabac_state[69] );
+    mode += 2 * get_cabac( &h->cabac, &h->cabac_state[69] );
+    mode += 4 * get_cabac( &h->cabac, &h->cabac_state[69] );
+
     if( mode >= pred_mode )
         return mode + 1;
     else
