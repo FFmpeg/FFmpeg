765c34406074b56c85966b4813383014ce8b7d0a
Fredrik Orderud <fredrik.orderud@idi.ntnu.no>
2005-12-12 00:48:42 +0000

    Define EMULATE_INTTYPES for all windows-systems, except Cygwin and MinGW to simplify usage on Windows. patch by Fredrik Orderud fredrik - . - orderud - @ - idi - . - ntnu - . - no
    Originally committed as revision 4736 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavutil/common.h
+++ libavutil/common.h
@@ -10,6 +10,10 @@
 #    define CONFIG_WIN32
 #endif
 
+#if defined(WIN32) && !defined(__MINGW32__) && !defined(__CYGWIN__) && !defined(EMULATE_INTTYPES)
+#    define EMULATE_INTTYPES
+#endif
+
 #ifndef M_PI
 #define M_PI    3.14159265358979323846
 #endif
