38cf2a723c65d81a0183379e7dae438930fbcff9
Michael Niedermayer <michaelni@gmx.at>
2006-03-02 19:50:21 +0000

    timebase fix
    Originally committed as revision 5099 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/asf.c
+++ libavformat/asf.c
@@ -802,7 +802,7 @@ static int64_t asf_read_pts(AVFormatContext *s, int stream_index, int64_t *ppos,
             return AV_NOPTS_VALUE;
         }
 
-        pts= pkt->pts * 1000 / AV_TIME_BASE;
+        pts= pkt->pts;
 
         av_free_packet(pkt);
         if(pkt->flags&PKT_FLAG_KEY){
