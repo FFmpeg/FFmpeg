b5c85991f62e84ff412ba187fe3b0490e8ecf5ac
Michael Niedermayer <michaelni@gmx.at>
2006-03-03 20:36:08 +0000

    fix segfault with BLUR8.AVI
    Originally committed as revision 5104 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/utils.c
+++ libavcodec/utils.c
@@ -844,9 +844,6 @@ int avcodec_open(AVCodecContext *avctx, AVCodec *codec)
     if(avctx->codec)
         goto end;
 
-    avctx->codec = codec;
-    avctx->codec_id = codec->id;
-    avctx->frame_number = 0;
     if (codec->priv_data_size > 0) {
         avctx->priv_data = av_mallocz(codec->priv_data_size);
         if (!avctx->priv_data)
@@ -865,9 +862,13 @@ int avcodec_open(AVCodecContext *avctx, AVCodec *codec)
         goto end;
     }
 
+    avctx->codec = codec;
+    avctx->codec_id = codec->id;
+    avctx->frame_number = 0;
     ret = avctx->codec->init(avctx);
     if (ret < 0) {
         av_freep(&avctx->priv_data);
+        avctx->codec= NULL;
         goto end;
     }
     ret=0;
