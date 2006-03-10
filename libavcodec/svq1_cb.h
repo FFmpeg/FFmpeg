5d9db9c4a9bdfa2dfcbe292670a96fbffb81e44a
Michael Niedermayer <michaelni@gmx.at>
2006-03-10 20:58:42 +0000

    out of array read fix
    Originally committed as revision 5141 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/svq1_cb.h
+++ libavcodec/svq1_cb.h
@@ -764,9 +764,10 @@ static const int8_t svq1_inter_codebook_8x8[6144] = {
 };
 
 /* list of codebooks for inter-coded vectors */
-static const int8_t* const svq1_inter_codebooks[4] = {
+static const int8_t* const svq1_inter_codebooks[6] = {
     svq1_inter_codebook_4x2, svq1_inter_codebook_4x4,
-    svq1_inter_codebook_8x4, svq1_inter_codebook_8x8
+    svq1_inter_codebook_8x4, svq1_inter_codebook_8x8,
+    NULL, NULL,
 };
 
 static const int8_t svq1_inter_codebook_sum[4][16*6] = {
@@ -1538,9 +1539,10 @@ static const int8_t svq1_intra_codebook_8x8[6144] = {
 };
 
 /* list of codebooks for intra-coded vectors */
-static const int8_t* const svq1_intra_codebooks[4] = {
+static const int8_t* const svq1_intra_codebooks[6] = {
     svq1_intra_codebook_4x2, svq1_intra_codebook_4x4,
-    svq1_intra_codebook_8x4, svq1_intra_codebook_8x8
+    svq1_intra_codebook_8x4, svq1_intra_codebook_8x8,
+    NULL, NULL,
 };
 
 static const int8_t svq1_intra_codebook_sum[4][16*6] = {
