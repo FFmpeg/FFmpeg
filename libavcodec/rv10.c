37921ffde81284f4cc8b69d99d4f39f593c65542
Michael Niedermayer <michaelni@gmx.at>
2006-04-14 20:45:08 +0000

    fix "concealing 0 DC, 0 AC, 0 MV errors" bug
    Originally committed as revision 5290 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/rv10.c
+++ libavcodec/rv10.c
@@ -639,9 +639,9 @@ static int rv10_decode_packet(AVCodecContext *avctx,
         if(s->mb_y==0) s->first_slice_line=1;
     }else{
         s->first_slice_line=1;
-        s->resync_mb_x= s->mb_x;
-        s->resync_mb_y= s->mb_y;
     }
+    s->resync_mb_x= s->mb_x;
+    s->resync_mb_y= s->mb_y;
     if(s->h263_aic){
         s->y_dc_scale_table=
         s->c_dc_scale_table= ff_aic_dc_scale_table;
