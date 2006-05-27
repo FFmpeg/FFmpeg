ac2750ec796bed25e7f995a1ff2850d382269a8a
Michael Niedermayer <michaelni@gmx.at>
2006-05-27 20:56:14 +0000

    suggest solution for 2 fixmies
    Originally committed as revision 5415 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/rtp.c
+++ libavformat/rtp.c
@@ -532,6 +532,7 @@ static int rtp_write_header(AVFormatContext *s1)
         payload_type = RTP_PT_PRIVATE; /* private payload type */
     s->payload_type = payload_type;
 
+// following 2 FIXMies could be set based on the current time, theres normaly no info leak, as rtp will likely be transmitted immedeatly
     s->base_timestamp = 0; /* FIXME: was random(), what should this be? */
     s->timestamp = s->base_timestamp;
     s->ssrc = 0; /* FIXME: was random(), what should this be? */
