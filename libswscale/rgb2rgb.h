2df19ea53ad77de79ab16cea99cd98359c2e300a
Luca Abeni <lucabe72@email.it>
2006-07-01 14:39:14 +0000

    Remove unneeded code
    Originally committed as revision 18880 to svn://svn.mplayerhq.hu/mplayer/trunk/libswscale

--- libswscale/rgb2rgb.h
+++ libswscale/rgb2rgb.h
@@ -124,21 +124,6 @@ extern void (*yvu9_to_yuy2)(const uint8_t *src1, const uint8_t *src2, const uint
 			long srcStride1, long srcStride2,
 			long srcStride3, long dstStride);
 	
-
-#define MODE_RGB  0x1
-#define MODE_BGR  0x2
-
-static void yuv2rgb(uint8_t * image, uint8_t * py,
-			      uint8_t * pu, uint8_t * pv,
-			      unsigned h_size, unsigned v_size,
-			      int rgb_stride, int y_stride, int uv_stride){
-printf("broken, this should use the swscaler\n");
-}
-
-static void yuv2rgb_init (unsigned bpp, int mode){
-printf("broken, this should use the swscaler\n");
-}
-
 void sws_rgb2rgb_init(int flags);
 
 #endif
