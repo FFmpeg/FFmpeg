93d6aeb0a452f7557fd85bc0af977e3f7eba84dd
Ivo van Poorten <ivop@euronet.nl>
2006-07-02 19:01:09 +0000

    add missing declaration for av_md5_sum
    Originally committed as revision 5583 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavutil/md5.h
+++ libavutil/md5.h
@@ -8,6 +8,7 @@ struct AVMD5;
 void av_md5_init(struct AVMD5 *ctx);
 void av_md5_update(struct AVMD5 *ctx, const uint8_t *src, const int len);
 void av_md5_final(struct AVMD5 *ctx, uint8_t *dst);
+void av_md5_sum(uint8_t *dst, const uint8_t *src, const int len);
 
 #endif /* MD5_H */
 
