d26a9ea0df8c08faf1ed37041f4bc252db30f047
Michael Niedermayer <michaelni@gmx.at>
2006-07-19 23:21:20 +0000

    remove 166 useless underscores, and make this file valid c code as a result
    Originally committed as revision 5798 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/vp3dsp.c
+++ libavcodec/vp3dsp.c
@@ -42,8 +42,8 @@ static always_inline void idct(uint8_t *dst, int stride, int16_t *input, int typ
     int16_t *ip = input;
     uint8_t *cm = cropTbl + MAX_NEG_CROP;
 
-    int A_, B_, C_, D_, _Ad, _Bd, _Cd, _Dd, E_, F_, G_, H_;
-    int _Ed, _Gd, _Add, _Bdd, _Fd, _Hd;
+    int A, B, C, D, Ad, Bd, Cd, Dd, E, F, G, H;
+    int Ed, Gd, Add, Bdd, Fd, Hd;
 
     int i;
 
@@ -51,44 +51,44 @@ static always_inline void idct(uint8_t *dst, int stride, int16_t *input, int typ
     for (i = 0; i < 8; i++) {
         /* Check for non-zero values */
         if ( ip[0] | ip[1] | ip[2] | ip[3] | ip[4] | ip[5] | ip[6] | ip[7] ) {
-            A_ = M(xC1S7, ip[1]) + M(xC7S1, ip[7]);
-            B_ = M(xC7S1, ip[1]) - M(xC1S7, ip[7]);
-            C_ = M(xC3S5, ip[3]) + M(xC5S3, ip[5]);
-            D_ = M(xC3S5, ip[5]) - M(xC5S3, ip[3]);
+            A = M(xC1S7, ip[1]) + M(xC7S1, ip[7]);
+            B = M(xC7S1, ip[1]) - M(xC1S7, ip[7]);
+            C = M(xC3S5, ip[3]) + M(xC5S3, ip[5]);
+            D = M(xC3S5, ip[5]) - M(xC5S3, ip[3]);
 
-            _Ad = M(xC4S4, (A_ - C_));
-            _Bd = M(xC4S4, (B_ - D_));
+            Ad = M(xC4S4, (A - C));
+            Bd = M(xC4S4, (B - D));
 
-            _Cd = A_ + C_;
-            _Dd = B_ + D_;
+            Cd = A + C;
+            Dd = B + D;
 
-            E_ = M(xC4S4, (ip[0] + ip[4]));
-            F_ = M(xC4S4, (ip[0] - ip[4]));
+            E = M(xC4S4, (ip[0] + ip[4]));
+            F = M(xC4S4, (ip[0] - ip[4]));
 
-            G_ = M(xC2S6, ip[2]) + M(xC6S2, ip[6]);
-            H_ = M(xC6S2, ip[2]) - M(xC2S6, ip[6]);
+            G = M(xC2S6, ip[2]) + M(xC6S2, ip[6]);
+            H = M(xC6S2, ip[2]) - M(xC2S6, ip[6]);
 
-            _Ed = E_ - G_;
-            _Gd = E_ + G_;
+            Ed = E - G;
+            Gd = E + G;
 
-            _Add = F_ + _Ad;
-            _Bdd = _Bd - H_;
+            Add = F + Ad;
+            Bdd = Bd - H;
 
-            _Fd = F_ - _Ad;
-            _Hd = _Bd + H_;
+            Fd = F - Ad;
+            Hd = Bd + H;
 
             /*  Final sequence of operations over-write original inputs. */
-            ip[0] = _Gd + _Cd ;
-            ip[7] = _Gd - _Cd ;
+            ip[0] = Gd + Cd ;
+            ip[7] = Gd - Cd ;
 
-            ip[1] = _Add + _Hd;
-            ip[2] = _Add - _Hd;
+            ip[1] = Add + Hd;
+            ip[2] = Add - Hd;
 
-            ip[3] = _Ed + _Dd ;
-            ip[4] = _Ed - _Dd ;
+            ip[3] = Ed + Dd ;
+            ip[4] = Ed - Dd ;
 
-            ip[5] = _Fd + _Bdd;
-            ip[6] = _Fd - _Bdd;
+            ip[5] = Fd + Bdd;
+            ip[6] = Fd - Bdd;
         }
 
         ip += 8;            /* next row */
@@ -101,80 +101,80 @@ static always_inline void idct(uint8_t *dst, int stride, int16_t *input, int typ
         if ( ip[1 * 8] | ip[2 * 8] | ip[3 * 8] |
              ip[4 * 8] | ip[5 * 8] | ip[6 * 8] | ip[7 * 8] ) {
 
-            A_ = M(xC1S7, ip[1*8]) + M(xC7S1, ip[7*8]);
-            B_ = M(xC7S1, ip[1*8]) - M(xC1S7, ip[7*8]);
-            C_ = M(xC3S5, ip[3*8]) + M(xC5S3, ip[5*8]);
-            D_ = M(xC3S5, ip[5*8]) - M(xC5S3, ip[3*8]);
+            A = M(xC1S7, ip[1*8]) + M(xC7S1, ip[7*8]);
+            B = M(xC7S1, ip[1*8]) - M(xC1S7, ip[7*8]);
+            C = M(xC3S5, ip[3*8]) + M(xC5S3, ip[5*8]);
+            D = M(xC3S5, ip[5*8]) - M(xC5S3, ip[3*8]);
 
-            _Ad = M(xC4S4, (A_ - C_));
-            _Bd = M(xC4S4, (B_ - D_));
+            Ad = M(xC4S4, (A - C));
+            Bd = M(xC4S4, (B - D));
 
-            _Cd = A_ + C_;
-            _Dd = B_ + D_;
+            Cd = A + C;
+            Dd = B + D;
 
-            E_ = M(xC4S4, (ip[0*8] + ip[4*8]));
-            F_ = M(xC4S4, (ip[0*8] - ip[4*8]));
+            E = M(xC4S4, (ip[0*8] + ip[4*8]));
+            F = M(xC4S4, (ip[0*8] - ip[4*8]));
 
-            G_ = M(xC2S6, ip[2*8]) + M(xC6S2, ip[6*8]);
-            H_ = M(xC6S2, ip[2*8]) - M(xC2S6, ip[6*8]);
+            G = M(xC2S6, ip[2*8]) + M(xC6S2, ip[6*8]);
+            H = M(xC6S2, ip[2*8]) - M(xC2S6, ip[6*8]);
 
-            _Ed = E_ - G_;
-            _Gd = E_ + G_;
+            Ed = E - G;
+            Gd = E + G;
 
-            _Add = F_ + _Ad;
-            _Bdd = _Bd - H_;
+            Add = F + Ad;
+            Bdd = Bd - H;
 
-            _Fd = F_ - _Ad;
-            _Hd = _Bd + H_;
+            Fd = F - Ad;
+            Hd = Bd + H;
 
             if(type==1){  //HACK
-                _Gd += 16*128;
-                _Add+= 16*128;
-                _Ed += 16*128;
-                _Fd += 16*128;
+                Gd += 16*128;
+                Add+= 16*128;
+                Ed += 16*128;
+                Fd += 16*128;
             }
-            _Gd += IdctAdjustBeforeShift;
-            _Add += IdctAdjustBeforeShift;
-            _Ed += IdctAdjustBeforeShift;
-            _Fd += IdctAdjustBeforeShift;
+            Gd += IdctAdjustBeforeShift;
+            Add += IdctAdjustBeforeShift;
+            Ed += IdctAdjustBeforeShift;
+            Fd += IdctAdjustBeforeShift;
 
             /* Final sequence of operations over-write original inputs. */
             if(type==0){
-                ip[0*8] = (_Gd + _Cd )  >> 4;
-                ip[7*8] = (_Gd - _Cd )  >> 4;
+                ip[0*8] = (Gd + Cd )  >> 4;
+                ip[7*8] = (Gd - Cd )  >> 4;
 
-                ip[1*8] = (_Add + _Hd ) >> 4;
-                ip[2*8] = (_Add - _Hd ) >> 4;
+                ip[1*8] = (Add + Hd ) >> 4;
+                ip[2*8] = (Add - Hd ) >> 4;
 
-                ip[3*8] = (_Ed + _Dd )  >> 4;
-                ip[4*8] = (_Ed - _Dd )  >> 4;
+                ip[3*8] = (Ed + Dd )  >> 4;
+                ip[4*8] = (Ed - Dd )  >> 4;
 
-                ip[5*8] = (_Fd + _Bdd ) >> 4;
-                ip[6*8] = (_Fd - _Bdd ) >> 4;
+                ip[5*8] = (Fd + Bdd ) >> 4;
+                ip[6*8] = (Fd - Bdd ) >> 4;
             }else if(type==1){
-                dst[0*stride] = cm[(_Gd + _Cd )  >> 4];
-                dst[7*stride] = cm[(_Gd - _Cd )  >> 4];
+                dst[0*stride] = cm[(Gd + Cd )  >> 4];
+                dst[7*stride] = cm[(Gd - Cd )  >> 4];
 
-                dst[1*stride] = cm[(_Add + _Hd ) >> 4];
-                dst[2*stride] = cm[(_Add - _Hd ) >> 4];
+                dst[1*stride] = cm[(Add + Hd ) >> 4];
+                dst[2*stride] = cm[(Add - Hd ) >> 4];
 
-                dst[3*stride] = cm[(_Ed + _Dd )  >> 4];
-                dst[4*stride] = cm[(_Ed - _Dd )  >> 4];
+                dst[3*stride] = cm[(Ed + Dd )  >> 4];
+                dst[4*stride] = cm[(Ed - Dd )  >> 4];
 
-                dst[5*stride] = cm[(_Fd + _Bdd ) >> 4];
-                dst[6*stride] = cm[(_Fd - _Bdd ) >> 4];
+                dst[5*stride] = cm[(Fd + Bdd ) >> 4];
+                dst[6*stride] = cm[(Fd - Bdd ) >> 4];
             }else{
-                dst[0*stride] = cm[dst[0*stride] + ((_Gd + _Cd )  >> 4)];
-                dst[7*stride] = cm[dst[7*stride] + ((_Gd - _Cd )  >> 4)];
+                dst[0*stride] = cm[dst[0*stride] + ((Gd + Cd )  >> 4)];
+                dst[7*stride] = cm[dst[7*stride] + ((Gd - Cd )  >> 4)];
 
-                dst[1*stride] = cm[dst[1*stride] + ((_Add + _Hd ) >> 4)];
-                dst[2*stride] = cm[dst[2*stride] + ((_Add - _Hd ) >> 4)];
+                dst[1*stride] = cm[dst[1*stride] + ((Add + Hd ) >> 4)];
+                dst[2*stride] = cm[dst[2*stride] + ((Add - Hd ) >> 4)];
 
-                dst[3*stride] = cm[dst[3*stride] + ((_Ed + _Dd )  >> 4)];
-                dst[4*stride] = cm[dst[4*stride] + ((_Ed - _Dd )  >> 4)];
+                dst[3*stride] = cm[dst[3*stride] + ((Ed + Dd )  >> 4)];
+                dst[4*stride] = cm[dst[4*stride] + ((Ed - Dd )  >> 4)];
 
-                dst[5*stride] = cm[dst[5*stride] + ((_Fd + _Bdd ) >> 4)];
-                dst[6*stride] = cm[dst[6*stride] + ((_Fd - _Bdd ) >> 4)];
+                dst[5*stride] = cm[dst[5*stride] + ((Fd + Bdd ) >> 4)];
+                dst[6*stride] = cm[dst[6*stride] + ((Fd - Bdd ) >> 4)];
             }
 
         } else {
