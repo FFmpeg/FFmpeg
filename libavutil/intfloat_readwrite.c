1ce83a36bf10717ca5590cb4d96a9fa73a7563bf
Baptiste Coudurier <baptiste.coudurier@gmail.com>
2006-08-11 21:39:09 +0000

    use C99 standard constant, thanks to Foxy Shadis
    Originally committed as revision 5986 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavutil/intfloat_readwrite.c
+++ libavutil/intfloat_readwrite.c
@@ -27,7 +27,7 @@
 #include "intfloat_readwrite.h"
 
 double av_int2dbl(int64_t v){
-    if(v+v > 0xFFELLU<<52)
+    if(v+v > 0xFFEULL<<52)
         return 0.0/0.0;
     return ldexp(((v&((1LL<<52)-1)) + (1LL<<52)) * (v>>63|1), (v>>52&0x7FF)-1075);
 }
