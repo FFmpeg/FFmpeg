7228914047b378698fe554fd55ac210922c1bb6d
Baptiste Coudurier <baptiste.coudurier@gmail.com>
2006-08-11 09:27:40 +0000

    set elst start time to first pts
    Originally committed as revision 5981 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/movenc.c
+++ libavformat/movenc.c
@@ -926,10 +926,7 @@ static int mov_write_edts_tag(ByteIOContext *pb, MOVTrack *track)
 
     put_be32(pb, av_rescale_rnd(track->trackDuration, globalTimescale, track->timescale, AV_ROUND_UP)); /* duration   ... doesn't seem to effect psp */
 
-    if (track->hasBframes)
-        put_be32(pb, track->sampleDuration); /* first pts is 1 */
-    else
-        put_be32(pb, 0);
+    put_be32(pb, track->cluster[0].cts); /* first pts is cts since dts is 0 */
     put_be32(pb, 0x00010000);
     return 0x24;
 }
