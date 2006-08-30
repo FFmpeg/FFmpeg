d1a7ae1a377774b387b534609befd3f4dc09e3bb
Michael Niedermayer <michaelni@gmx.at>
2006-08-30 10:50:20 +0000

    make the C code of the swscaler which i wrote LGPL
    Originally committed as revision 19600 to svn://svn.mplayerhq.hu/mplayer/trunk/libswscale

--- libswscale/swscale.h
+++ libswscale/swscale.h
@@ -14,6 +14,9 @@
     You should have received a copy of the GNU General Public License
     along with this program; if not, write to the Free Software
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+    the C code (not assembly, mmx, ...) of the swscaler which has been written
+    by Michael Niedermayer can be used under the LGPL license too
 */
 
 #ifndef SWSCALE_H
