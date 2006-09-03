74945176736e1d347a5ed8cd2f0d8374d3d9bfb9
Diego Biurrun <diego@biurrun.de>
2006-09-03 14:13:32 +0000

    Add license from zlib.h instead of referring to it.
    Originally committed as revision 6155 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavutil/adler32.c
+++ libavutil/adler32.c
@@ -1,6 +1,24 @@
 /* adler32.c -- compute the Adler-32 checksum of a data stream
+ * This is a modified version based on adler32.c from the zlib library.
+ *
  * Copyright (C) 1995 Mark Adler
- * For conditions of distribution and use, see copyright notice in zlib.h
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty.  In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ *
  */
 
 #include "common.h"
