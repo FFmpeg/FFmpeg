d67898427b82f1fa92b32256d76ba7222d92c7ba
Diego Biurrun <diego@biurrun.de>
2006-09-04 09:37:28 +0000

    Exchange informal GPL notice by official license header.
    Originally committed as revision 6165 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/liba52/resample_mmx.c
+++ libavcodec/liba52/resample_mmx.c
@@ -1,5 +1,24 @@
-
-// MMX optimizations from Michael Niedermayer (michaelni@gmx.at) (under GPL)
+/*
+ * resample_mmx.c
+ * Copyright (C) 2003 Michael Niedermayer <michaelni@gmx.at>
+ *
+ * This file is part of a52dec, a free ATSC A-52 stream decoder.
+ * See http://liba52.sourceforge.net/ for updates.
+ *
+ * a52dec is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * a52dec is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
 
 /* optimization TODO / NOTES
     movntq is slightly faster (0.5% with the current test.c benchmark)
