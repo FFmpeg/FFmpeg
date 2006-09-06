4fadc2b4f1670fbd2a909e9ea03ab17ed92093e6
Diego Biurrun <diego@biurrun.de>
2006-09-06 10:24:19 +0000

    Add official GPL header to make license explicit as discussed on ffmpeg-devel.
    Originally committed as revision 19709 to svn://svn.mplayerhq.hu/mplayer/trunk/libswscale

--- libswscale/rgb2rgb_template.c
+++ libswscale/rgb2rgb_template.c
@@ -5,8 +5,22 @@
  *               Software YUV to YUV convertor
  *               Software YUV to RGB convertor
  *  Written by Nick Kurshev.
- *  palette & yuv & runtime cpu stuff by Michael (michaelni@gmx.at) (under GPL)
+ *  palette & YUV & runtime CPU stuff by Michael (michaelni@gmx.at)
  *  lot of big-endian byteorder fixes by Alex Beregszaszi
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
 #include <stddef.h>
