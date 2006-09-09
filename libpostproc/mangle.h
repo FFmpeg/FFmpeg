380b2da75a6b4ed721a10bfb5bac78281d109fbb
Diego Biurrun <diego@biurrun.de>
2006-09-09 21:09:37 +0000

    Replace informal GPL notice by official header.
    Originally committed as revision 6215 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libpostproc/mangle.h
+++ libpostproc/mangle.h
@@ -1,8 +1,23 @@
-/* mangle.h - This file has some CPP macros to deal with different symbol
+/*
+ * mangle.h - This file has some CPP macros to deal with different symbol
  * mangling across binary formats.
+ *
  * (c)2002 by Felix Buenemann <atmosfear at users.sourceforge.net>
- * File licensed under the GPL, see http://www.fsf.org/ for more info.
- */
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+*/
 
 #ifndef __MANGLE_H
 #define __MANGLE_H
