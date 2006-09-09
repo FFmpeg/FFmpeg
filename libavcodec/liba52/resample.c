9a8e6f87080bba32a8801ff7b690d8032df75004
Diego Biurrun <diego@biurrun.de>
2006-09-09 22:04:43 +0000

    Add proper GPL headers.
    Originally committed as revision 6216 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/liba52/resample.c
+++ libavcodec/liba52/resample.c
@@ -1,3 +1,23 @@
+/*
+ * copyright (C) 2001 Arpad Gereoffy
+ *
+ * This file is part of a52dec, a free ATSC A-52 stream decoder.
+ * See http://liba52.sourceforge.net/ for updates.
+ *
+ * a52dec is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * a52dec is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
 
 // a52_resample_init should find the requested converter (from type flags ->
 // given number of channels) and set up some function pointers...
--- libavcodec/liba52/resample_c.c
+++ libavcodec/liba52/resample_c.c
@@ -1,4 +1,24 @@
-// this code is based on a52dec/libao/audio_out_oss.c
+/*
+ * this code is based on a52dec/libao/audio_out_oss.c
+ * copyright (C) 2001 Arpad Gereoffy
+ *
+ * This file is part of a52dec, a free ATSC A-52 stream decoder.
+ * See http://liba52.sourceforge.net/ for updates.
+ *
+ * a52dec is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * a52dec is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
 
 static inline int16_t convert (int32_t i)
 {
