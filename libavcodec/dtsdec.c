bca519e0f6e51f93a8fd44d5813357b8da4f580f
Diego Biurrun <diego@biurrun.de>
2006-10-09 07:31:27 +0000

    Fix GPL/LGPL license version mismatch.
    Originally committed as revision 6597 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/dtsdec.c
+++ libavcodec/dtsdec.c
@@ -6,7 +6,7 @@
  *
  * FFmpeg is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2.1 of the License, or
+ * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
  * FFmpeg is distributed in the hope that it will be useful,
