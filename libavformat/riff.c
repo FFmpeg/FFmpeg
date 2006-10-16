39051d5d6e28ae89449fbc5661ae05bac4112251
Diego Biurrun <diego@biurrun.de>
2006-10-16 11:18:26 +0000

    Replace FLAC FourCC by TwoCC.
    Originally committed as revision 6708 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/riff.c
+++ libavformat/riff.c
@@ -193,9 +193,9 @@ const CodecTag codec_wav_tags[] = {
     { CODEC_ID_ADPCM_CT, 0x200 },
     { CODEC_ID_ADPCM_SWF, ('S'<<8)+'F' },
     { CODEC_ID_TRUESPEECH, 0x22 },
+    { CODEC_ID_FLAC, 0xF1AC },
 
     /* FIXME: All of the IDs below are not 16 bit and thus illegal. */
-    { CODEC_ID_FLAC, MKTAG('f', 'L', 'a', 'C') },
     { CODEC_ID_TTA, MKTAG('T', 'T', 'A', '1') },
     { CODEC_ID_WAVPACK, MKTAG('W', 'V', 'P', 'K') },
     { CODEC_ID_SHORTEN, MKTAG('s', 'h', 'r', 'n') },
