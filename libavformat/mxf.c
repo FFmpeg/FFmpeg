4d6ac1a47b1c866bcee6cb48332f435d193264cf
Baptiste Coudurier <baptiste.coudurier@gmail.com>
2006-10-17 11:30:22 +0000

    skip packet if no stream index is found, fix proxy_pal030926.mxf
    Originally committed as revision 6721 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/mxf.c
+++ libavformat/mxf.c
@@ -265,6 +265,7 @@ static int mxf_read_packet(AVFormatContext *s, AVPacket *pkt)
             int index = mxf_get_stream_index(s, &klv);
             if (index < 0) {
                 av_log(s, AV_LOG_ERROR, "error getting stream index\n");
+                url_fskip(&s->pb, klv.length);
                 return -1;
             }
             /* check for 8 channels AES3 element */
