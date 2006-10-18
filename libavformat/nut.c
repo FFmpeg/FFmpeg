3d1eed611b28f94550f88f6650db6065f8ea09ff
Diego Biurrun <diego@biurrun.de>
2006-10-18 13:15:27 +0000

    Fix license header error notice by Steve LHomme.
    Originally committed as revision 6730 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/nut.c
+++ libavformat/nut.c
@@ -15,7 +15,7 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public
+ * You should have received a copy of the GNU Lesser General Public
  * License along with FFmpeg; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  *
