9eeec55d77f4a9706596255969816df84dfd39d4
Guillaume Poirier <gpoirier@mplayerhq.hu>
2006-10-26 22:45:19 +0000

    move licence header fixes
    Originally committed as revision 6800 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/rtp_internal.h
+++ libavformat/rtp_internal.h
@@ -2,7 +2,9 @@
  * RTP definitions
  * Copyright (c) 2006 Ryan Martell <rdm4@martellventures.com>
  *
- * This library is free software; you can redistribute it and/or
+ * This file is part of FFmpeg.
+ *
+ * FFmpeg is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
