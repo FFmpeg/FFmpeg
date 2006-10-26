cbced386c23863265ec18af0e3a02321530f4f5b
Guillaume Poirier <gpoirier@mplayerhq.hu>
2006-10-26 22:40:18 +0000

    Fix licence header (aka <pedantic mode='on' /> )
    Originally committed as revision 6799 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/rtp_internal.h
+++ libavformat/rtp_internal.h
@@ -5,9 +5,9 @@
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
+ * version 2.1 of the License, or (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful,
+ * FFmpeg is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
