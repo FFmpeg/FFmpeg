550b2843468b6089c7e8f6600ef747b015664be8
Guillaume Poirier <gpoirier@mplayerhq.hu>
2006-10-27 06:06:58 +0000

    Last fix of licence header
    Originally committed as revision 6801 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/rtp_internal.h
+++ libavformat/rtp_internal.h
@@ -15,7 +15,7 @@
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
+ * License along with FFmpeg; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
