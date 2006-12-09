46f9dcc3ee03c6698313e85cbdc9b40e5d23d1af
Reimar Döffinger <Reimar.Doeffinger@gmx.de>
2006-12-09 13:30:28 +0000

    Fix 16 bit cscd samples, 16 bit raw means RGB555 on Windows, and the original cscd codec is for windows.
    Originally committed as revision 7266 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/cscd.c
+++ libavcodec/cscd.c
@@ -220,7 +220,7 @@ static int decode_init(AVCodecContext *avctx) {
     }
     avctx->has_b_frames = 0;
     switch (avctx->bits_per_sample) {
-        case 16: avctx->pix_fmt = PIX_FMT_RGB565; break;
+        case 16: avctx->pix_fmt = PIX_FMT_RGB555; break;
         case 24: avctx->pix_fmt = PIX_FMT_BGR24; break;
         case 32: avctx->pix_fmt = PIX_FMT_RGBA32; break;
         default:
