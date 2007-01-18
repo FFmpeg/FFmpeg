763750242bdd1ff4e2be3d6654cb8fb748295d5e
Aurelien Jacobs <aurel@gnuage.org>
2007-01-18 23:23:41 +0000

    cosmetics: indentation
    Originally committed as revision 7583 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/tcp.c
+++ libavformat/tcp.c
@@ -23,7 +23,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-# include <arpa/inet.h>
+#include <arpa/inet.h>
 #include <netdb.h>
 #include <sys/time.h>
 #include <fcntl.h>
--- libavformat/udp.c
+++ libavformat/udp.c
@@ -23,7 +23,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-# include <arpa/inet.h>
+#include <arpa/inet.h>
 #include <netdb.h>
 
 #ifndef IPV6_ADD_MEMBERSHIP
