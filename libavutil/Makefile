558b86a5d03690b359f5a84ba1cd3222055e55fb
Luca Barbato <lu_zero@gentoo.org>
2007-02-28 03:40:23 +0000

    Reverting stray commit part II, r8156 had the base64 export patch mixed with the nutdec patch
    Originally committed as revision 8158 to svn://svn.ffmpeg.org/ffmpeg/trunk

---
+++ libavutil/Makefile
@@ -0,0 +1,27 @@
+include ../config.mak
+
+OBJS= mathematics.o \
+      rational.o \
+      intfloat_readwrite.o \
+      crc.o \
+      md5.o \
+      lls.o \
+      adler32.o \
+      log.o \
+      mem.o \
+      fifo.o \
+      tree.o \
+      lzo.o \
+      random.o \
+      aes.o \
+      base64.o \
+
+HEADERS = avutil.h common.h mathematics.h integer.h rational.h \
+          intfloat_readwrite.h md5.h adler32.h log.h fifo.h lzo.h \
+          random.h
+
+NAME=avutil
+LIBVERSION=$(LAVUVERSION)
+LIBMAJOR=$(LAVUMAJOR)
+
+include ../common.mak
---
+++ libavutil/base64.h
@@ -0,0 +1,33 @@
+/*
+ * Base64.c
+ * Copyright (c) 2006 Ryan Martell. (rdm4@martellventures.com)
+ *
+ * This file is part of FFmpeg.
+ *
+ * FFmpeg is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * FFmpeg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FFmpeg; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+/**
+ * decodes base64
+ * param order as strncpy()
+ */
+int av_base64_decode(uint8_t * out, const char *in, int out_length);
+
+/**
+ * encodes base64
+ * @param src data, not a string
+ */
+char *av_base64_encode(uint8_t * src, int len);
+
