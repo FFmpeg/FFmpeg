cb50328dbb4d29efd393b1ff8ba082b3e3d73cef
Michael Niedermayer <michaelni@gmx.at>
2007-03-05 21:08:03 +0000

    free opt_names at the end of main() based on a patch by Limin Wang
    Originally committed as revision 8268 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- ffmpeg.c
+++ ffmpeg.c
@@ -3810,6 +3810,7 @@ int main(int argc, char **argv)
 
     av_free(intra_matrix);
     av_free(inter_matrix);
+    av_free(opt_names);
 
 #ifdef CONFIG_POWERPC_PERF
     extern void powerpc_display_perf_report(void);
