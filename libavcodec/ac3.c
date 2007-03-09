96ae7f11e7c8a2e379f9c79451926e217dd65653
Justin Ruggles <justin.ruggles@gmail.com>
2007-03-09 13:54:44 +0000

    Create ac3.c which will be used for AC-3 common code.
    Originally committed as revision 8305 to svn://svn.ffmpeg.org/ffmpeg/trunk

---
+++ libavcodec/ac3.c
@@ -0,0 +1,197 @@
+/*
+ * Common code between AC3 encoder and decoder
+ * Copyright (c) 2000 Fabrice Bellard.
+ *
+ * This file is part of FFmpeg.
+ *
+ * FFmpeg is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * FFmpeg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FFmpeg; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+/**
+ * @file ac3.c
+ * Common code between AC3 encoder and decoder.
+ */
+
+#include "avcodec.h"
+#include "ac3.h"
+#include "ac3tab.h"
+
+static inline int calc_lowcomp1(int a, int b0, int b1, int c)
+{
+    if ((b0 + 256) == b1) {
+        a = c;
+    } else if (b0 > b1) {
+        a = FFMAX(a - 64, 0);
+    }
+    return a;
+}
+
+static inline int calc_lowcomp(int a, int b0, int b1, int bin)
+{
+    if (bin < 7) {
+        return calc_lowcomp1(a, b0, b1, 384);
+    } else if (bin < 20) {
+        return calc_lowcomp1(a, b0, b1, 320);
+    } else {
+        return FFMAX(a - 128, 0);
+    }
+}
+
+/* AC3 bit allocation. The algorithm is the one described in the AC3
+   spec. */
+void ac3_parametric_bit_allocation(AC3BitAllocParameters *s, uint8_t *bap,
+                                   int8_t *exp, int start, int end,
+                                   int snroffset, int fgain, int is_lfe,
+                                   int deltbae,int deltnseg,
+                                   uint8_t *deltoffst, uint8_t *deltlen, uint8_t *deltba)
+{
+    int bin,i,j,k,end1,v,bndstrt,bndend,lowcomp,begin;
+    int fastleak,slowleak,address,tmp;
+    int16_t psd[256]; /* scaled exponents */
+    int16_t bndpsd[50]; /* interpolated exponents */
+    int16_t excite[50]; /* excitation */
+    int16_t mask[50];   /* masking value */
+
+    /* exponent mapping to PSD */
+    for(bin=start;bin<end;bin++) {
+        psd[bin]=(3072 - (exp[bin] << 7));
+    }
+
+    /* PSD integration */
+    j=start;
+    k=masktab[start];
+    do {
+        v=psd[j];
+        j++;
+        end1 = FFMIN(bndtab[k+1], end);
+        for(i=j;i<end1;i++) {
+            /* logadd */
+            int adr = FFMIN(FFABS(v - psd[j]) >> 1, 255);
+            v = FFMAX(v, psd[j]) + latab[adr];
+            j++;
+        }
+        bndpsd[k]=v;
+        k++;
+    } while (end > bndtab[k]);
+
+    /* excitation function */
+    bndstrt = masktab[start];
+    bndend = masktab[end-1] + 1;
+
+    if (bndstrt == 0) {
+        lowcomp = 0;
+        lowcomp = calc_lowcomp1(lowcomp, bndpsd[0], bndpsd[1], 384);
+        excite[0] = bndpsd[0] - fgain - lowcomp;
+        lowcomp = calc_lowcomp1(lowcomp, bndpsd[1], bndpsd[2], 384);
+        excite[1] = bndpsd[1] - fgain - lowcomp;
+        begin = 7;
+        for (bin = 2; bin < 7; bin++) {
+            if (!(is_lfe && bin == 6))
+                lowcomp = calc_lowcomp1(lowcomp, bndpsd[bin], bndpsd[bin+1], 384);
+            fastleak = bndpsd[bin] - fgain;
+            slowleak = bndpsd[bin] - s->sgain;
+            excite[bin] = fastleak - lowcomp;
+            if (!(is_lfe && bin == 6)) {
+                if (bndpsd[bin] <= bndpsd[bin+1]) {
+                    begin = bin + 1;
+                    break;
+                }
+            }
+        }
+
+        end1=bndend;
+        if (end1 > 22) end1=22;
+
+        for (bin = begin; bin < end1; bin++) {
+            if (!(is_lfe && bin == 6))
+                lowcomp = calc_lowcomp(lowcomp, bndpsd[bin], bndpsd[bin+1], bin);
+
+            fastleak = FFMAX(fastleak - s->fdecay, bndpsd[bin] - fgain);
+            slowleak = FFMAX(slowleak - s->sdecay, bndpsd[bin] - s->sgain);
+            excite[bin] = FFMAX(fastleak - lowcomp, slowleak);
+        }
+        begin = 22;
+    } else {
+        /* coupling channel */
+        begin = bndstrt;
+
+        fastleak = (s->cplfleak << 8) + 768;
+        slowleak = (s->cplsleak << 8) + 768;
+    }
+
+    for (bin = begin; bin < bndend; bin++) {
+        fastleak = FFMAX(fastleak - s->fdecay, bndpsd[bin] - fgain);
+        slowleak = FFMAX(slowleak - s->sdecay, bndpsd[bin] - s->sgain);
+        excite[bin] = FFMAX(fastleak, slowleak);
+    }
+
+    /* compute masking curve */
+
+    for (bin = bndstrt; bin < bndend; bin++) {
+        tmp = s->dbknee - bndpsd[bin];
+        if (tmp > 0) {
+            excite[bin] += tmp >> 2;
+        }
+        mask[bin] = FFMAX(hth[bin >> s->halfratecod][s->fscod], excite[bin]);
+    }
+
+    /* delta bit allocation */
+
+    if (deltbae == 0 || deltbae == 1) {
+        int band, seg, delta;
+        band = 0;
+        for (seg = 0; seg < deltnseg; seg++) {
+            band += deltoffst[seg];
+            if (deltba[seg] >= 4) {
+                delta = (deltba[seg] - 3) << 7;
+            } else {
+                delta = (deltba[seg] - 4) << 7;
+            }
+            for (k = 0; k < deltlen[seg]; k++) {
+                mask[band] += delta;
+                band++;
+            }
+        }
+    }
+
+    /* compute bit allocation */
+
+    i = start;
+    j = masktab[start];
+    do {
+        v = (FFMAX(mask[j] - snroffset - s->floor, 0) & 0x1FE0) + s->floor;
+        end1 = FFMIN(bndtab[j] + bndsz[j], end);
+        for (k = i; k < end1; k++) {
+            address = av_clip((psd[i] - v) >> 5, 0, 63);
+            bap[i] = baptab[address];
+            i++;
+        }
+    } while (end > bndtab[j++]);
+}
+
+void ac3_common_init(void)
+{
+    int i, j, k, l, v;
+    /* compute bndtab and masktab from bandsz */
+    k = 0;
+    l = 0;
+    for(i=0;i<50;i++) {
+        bndtab[i] = l;
+        v = bndsz[i];
+        for(j=0;j<v;j++) masktab[k++]=i;
+        l += v;
+    }
+    bndtab[50] = l;
+}
