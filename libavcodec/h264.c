1153be3be15f0eccbb5055839185c5bf2425bd79
Baptiste Coudurier <baptiste.coudurier@gmail.com>
2007-03-14 11:12:02 +0000

    h264 complete frames parsing support
    Originally committed as revision 8398 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/h264.c
+++ libavcodec/h264.c
@@ -8048,6 +8048,9 @@ static int h264_parse(AVCodecParserContext *s,
     ParseContext *pc = &h->s.parse_context;
     int next;
 
+    if(s->flags & PARSER_FLAG_COMPLETE_FRAMES){
+        next= buf_size;
+    }else{
     next= find_frame_end(h, buf, buf_size);
 
     if (ff_combine_frame(pc, next, (uint8_t **)&buf, &buf_size) < 0) {
@@ -8058,6 +8061,7 @@ static int h264_parse(AVCodecParserContext *s,
 
     if(next<0){
         find_frame_end(h, &pc->buffer[pc->last_index + next], -next); //update state
+    }
     }
 
     *poutbuf = (uint8_t *)buf;
