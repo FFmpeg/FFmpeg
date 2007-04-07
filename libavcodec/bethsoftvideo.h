fee9e80550b5e52304a83fdc63ca5ddaf0f0509a
Michael Niedermayer <michaelni@gmx.at>
2007-04-07 23:31:30 +0000

    align vertically
    Originally committed as revision 8672 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/bethsoftvideo.h
+++ libavcodec/bethsoftvideo.h
@@ -1,8 +1,8 @@
 enum BethsoftVidBlockType
 {
-    PALETTE_BLOCK = 0x02,
-    FIRST_AUDIO_BLOCK = 0x7c,
-    AUDIO_BLOCK = 0x7d,
+    PALETTE_BLOCK       = 0x02,
+    FIRST_AUDIO_BLOCK   = 0x7c,
+    AUDIO_BLOCK         = 0x7d,
     VIDEO_I_FRAME       = 0x03,
     VIDEO_P_FRAME       = 0x01,
     VIDEO_YOFF_P_FRAME  = 0x04,
