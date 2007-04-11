ea7081737990e2b1aef8b2de9b6143698460991c
Ivo van Poorten <ivop@euronet.nl>
2007-04-11 22:17:58 +0000

    cosmetics after last commit; remove superfluous braces
    Originally committed as revision 22979 to svn://svn.mplayerhq.hu/mplayer/trunk/libswscale

--- libswscale/rgb2rgb.c
+++ libswscale/rgb2rgb.c
@@ -215,17 +215,15 @@ static uint64_t __attribute__((aligned(8))) dither8[2]={
 
 void sws_rgb2rgb_init(int flags){
 #if (defined(HAVE_MMX2) || defined(HAVE_3DNOW) || defined(HAVE_MMX))  && defined(CONFIG_GPL)
-	if(flags & SWS_CPU_CAPS_MMX2){
+	if(flags & SWS_CPU_CAPS_MMX2)
 		rgb2rgb_init_MMX2();
-	}else if(flags & SWS_CPU_CAPS_3DNOW){
+	else if(flags & SWS_CPU_CAPS_3DNOW)
 		rgb2rgb_init_3DNOW();
-	}else if(flags & SWS_CPU_CAPS_MMX){
+	else if(flags & SWS_CPU_CAPS_MMX)
 		rgb2rgb_init_MMX();
-	}else
+	else
 #endif /* defined(HAVE_MMX2) || defined(HAVE_3DNOW) || defined(HAVE_MMX) */
-	{
 		rgb2rgb_init_C();
-	}
 }
 
 /**
