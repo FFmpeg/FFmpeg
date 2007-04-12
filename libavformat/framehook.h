4303c9bf62a3c417a6298a7a7ab76f5ea4cab0e0
Diego Biurrun <diego@biurrun.de>
2007-04-12 11:28:34 +0000

    spelling
    Originally committed as revision 8722 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/framehook.h
+++ libavformat/framehook.h
@@ -22,7 +22,7 @@
 #ifndef _FRAMEHOOK_H
 #define _FRAMEHOOK_H
 
-#warning VHOOK is deprecated please help porting libmpcodecs or a better filter system to ffmpeg instead of wasting your time writing new fiters for this crappy one
+#warning VHOOK is deprecated. Please help porting libmpcodecs or a better filter system to FFmpeg instead of wasting your time writing new filters for this crappy one.
 
 /*
  * Prototypes for interface to .so that implement a video processing hook
