859d95baa16a38d8b56e1c949effea870ee30d3b
Michael Niedermayer <michaelni@gmx.at>
2007-04-14 17:50:39 +0000

    fix overhead and bitrate statistics
    Originally committed as revision 8732 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- ffmpeg.c
+++ ffmpeg.c
@@ -915,7 +915,9 @@ static void print_report(AVFormatContext **output_files,
 
     oc = output_files[0];
 
-    total_size = url_ftell(&oc->pb);
+    total_size = url_fsize(&oc->pb);
+    if(total_size<0) // FIXME improve url_fsize() so it works with non seekable output too
+        total_size= url_ftell(&oc->pb);
 
     buf[0] = '\0';
     ti1 = 1e10;
