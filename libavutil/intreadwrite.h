85b1a7222d45c6fbd019996cbe713c60fad0cb4e
Diego Biurrun <diego@biurrun.de>
2007-04-24 07:16:47 +0000

    cosmetics: Reorder endianness macros by bit depth, alignment prettyprinting.
    Originally committed as revision 8796 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavutil/intreadwrite.h
+++ libavutil/intreadwrite.h
@@ -44,52 +44,53 @@ struct unaligned_16 { uint16_t l; } __attribute__((packed));
 #endif /* !__GNUC__ */
 
 /* endian macros */
-#define AV_RB8(x)  (((uint8_t*)(x))[0])
+#define AV_RB8(x)     (((uint8_t*)(x))[0])
 #define AV_WB8(p, d)  { ((uint8_t*)(p))[0] = (d); }
 
-#define AV_RB16(x) ((((uint8_t*)(x))[0] << 8) | ((uint8_t*)(x))[1])
+#define AV_RL8(x)     AV_RB8(x)
+#define AV_WL8(p, d)  AV_WB8(p, d)
+
+#define AV_RB16(x)  ((((uint8_t*)(x))[0] << 8) | ((uint8_t*)(x))[1])
 #define AV_WB16(p, d) { \
                     ((uint8_t*)(p))[1] = (d); \
                     ((uint8_t*)(p))[0] = (d)>>8; }
 
-#define AV_RB24(x) ((((uint8_t*)(x))[0] << 16) | \
-                    (((uint8_t*)(x))[1] << 8) | \
-                     ((uint8_t*)(x))[2])
+#define AV_RL16(x)  ((((uint8_t*)(x))[1] << 8) | \
+                      ((uint8_t*)(x))[0])
+#define AV_WL16(p, d) { \
+                    ((uint8_t*)(p))[0] = (d); \
+                    ((uint8_t*)(p))[1] = (d)>>8; }
+
+#define AV_RB24(x)  ((((uint8_t*)(x))[0] << 16) | \
+                     (((uint8_t*)(x))[1] <<  8) | \
+                      ((uint8_t*)(x))[2])
 #define AV_WB24(p, d) { \
                     ((uint8_t*)(p))[2] = (d); \
                     ((uint8_t*)(p))[1] = (d)>>8; \
                     ((uint8_t*)(p))[0] = (d)>>16; }
 
-#define AV_RB32(x) ((((uint8_t*)(x))[0] << 24) | \
-                   (((uint8_t*)(x))[1] << 16) | \
-                   (((uint8_t*)(x))[2] << 8) | \
-                    ((uint8_t*)(x))[3])
+#define AV_RL24(x)  ((((uint8_t*)(x))[2] << 16) | \
+                     (((uint8_t*)(x))[1] <<  8) | \
+                      ((uint8_t*)(x))[0])
+#define AV_WL24(p, d) { \
+                    ((uint8_t*)(p))[0] = (d); \
+                    ((uint8_t*)(p))[1] = (d)>>8; \
+                    ((uint8_t*)(p))[2] = (d)>>16; }
+
+#define AV_RB32(x)  ((((uint8_t*)(x))[0] << 24) | \
+                     (((uint8_t*)(x))[1] << 16) | \
+                     (((uint8_t*)(x))[2] <<  8) | \
+                      ((uint8_t*)(x))[3])
 #define AV_WB32(p, d) { \
                     ((uint8_t*)(p))[3] = (d); \
                     ((uint8_t*)(p))[2] = (d)>>8; \
                     ((uint8_t*)(p))[1] = (d)>>16; \
                     ((uint8_t*)(p))[0] = (d)>>24; }
 
-#define AV_RL8(x)  AV_RB8(x)
-#define AV_WL8(p, d)  AV_WB8(p, d)
-
-#define AV_RL16(x) ((((uint8_t*)(x))[1] << 8) | ((uint8_t*)(x))[0])
-#define AV_WL16(p, d) { \
-                    ((uint8_t*)(p))[0] = (d); \
-                    ((uint8_t*)(p))[1] = (d)>>8; }
-
-#define AV_RL24(x) ((((uint8_t*)(x))[2] << 16) | \
-                    (((uint8_t*)(x))[1] << 8) | \
-                     ((uint8_t*)(x))[0])
-#define AV_WL24(p, d) { \
-                    ((uint8_t*)(p))[0] = (d); \
-                    ((uint8_t*)(p))[1] = (d)>>8; \
-                    ((uint8_t*)(p))[2] = (d)>>16; }
-
 #define AV_RL32(x) ((((uint8_t*)(x))[3] << 24) | \
-                   (((uint8_t*)(x))[2] << 16) | \
-                   (((uint8_t*)(x))[1] << 8) | \
-                    ((uint8_t*)(x))[0])
+                    (((uint8_t*)(x))[2] << 16) | \
+                    (((uint8_t*)(x))[1] <<  8) | \
+                     ((uint8_t*)(x))[0])
 #define AV_WL32(p, d) { \
                     ((uint8_t*)(p))[0] = (d); \
                     ((uint8_t*)(p))[1] = (d)>>8; \
