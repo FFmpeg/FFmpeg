70fa2486a151200725489bb89c4f919c0a790420
David Conrad <lessen42@gmail.com>
2007-04-25 23:22:05 +0000

    Support for seeking in TTA files.
    Originally committed as revision 8820 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/tta.c
+++ libavformat/tta.c
@@ -120,6 +120,20 @@ static int tta_read_packet(AVFormatContext *s, AVPacket *pkt)
     return av_get_packet(&s->pb, pkt, size);
 }
 
+static int tta_read_seek(AVFormatContext *s, int stream_index, int64_t timestamp, int flags)
+{
+    TTAContext *c = s->priv_data;
+    AVStream *st = s->streams[stream_index];
+    int index = av_index_search_timestamp(st, timestamp, flags);
+    if (index < 0)
+        return -1;
+
+    c->currentframe = index;
+    url_fseek(&s->pb, st->index_entries[index].pos, SEEK_SET);
+
+    return 0;
+}
+
 AVInputFormat tta_demuxer = {
     "tta",
     "true-audio",
@@ -128,5 +142,6 @@ AVInputFormat tta_demuxer = {
     tta_read_header,
     tta_read_packet,
     NULL,
+    tta_read_seek,
     .extensions = "tta",
 };
