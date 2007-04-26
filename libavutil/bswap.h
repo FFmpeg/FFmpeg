853d15c2f886343297a6e31ad66a5c8e718bcc01
Ramiro Polla <ramiro@lisha.ufsc.br>
2007-04-26 08:15:39 +0000

    make 2 functions "return x;" to simplify next patch patch by Ramiro Polla ramiro lisha ufsc br
    Originally committed as revision 8822 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavutil/bswap.h
+++ libavutil/bswap.h
@@ -111,7 +111,8 @@ static inline uint64_t bswap_64(uint64_t x)
 #else
 
 static av_always_inline uint16_t bswap_16(uint16_t x){
-    return (x>>8) | (x<<8);
+    x= (x>>8) | (x<<8);
+    return x;
 }
 
 #ifdef ARCH_ARM
@@ -140,7 +141,8 @@ static av_always_inline uint32_t bswap_32(uint32_t x){
 #else
 static av_always_inline uint32_t bswap_32(uint32_t x){
     x= ((x<<8)&0xFF00FF00) | ((x>>8)&0x00FF00FF);
-    return (x>>16) | (x<<16);
+    x= (x>>16) | (x<<16);
+    return x;
 }
 #endif
 
