bf5d7db5a09acb1f4b0dc26b22900074166c9540
Ramiro Polla <ramiro@lisha.ufsc.br>
2007-04-26 08:20:21 +0000

    reorder bswap functions into bit-depth, special-casing inside the functions. patch by Ramiro Polla ramiro lisha ufsc br
    Originally committed as revision 8823 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavutil/bswap.h
+++ libavutil/bswap.h
@@ -36,17 +36,23 @@
 #  define LEGACY_REGS "=q"
 #endif
 
-#if defined(ARCH_X86)
 static av_always_inline uint16_t bswap_16(uint16_t x)
 {
+#if defined(ARCH_X86)
   __asm("rorw $8, %0"   :
         LEGACY_REGS (x) :
         "0" (x));
+#elif defined(ARCH_SH4)
+        __asm__("swap.b %0,%0":"=r"(x):"0"(x));
+#else
+    x= (x>>8) | (x<<8);
+#endif
     return x;
 }
 
 static av_always_inline uint32_t bswap_32(uint32_t x)
 {
+#if defined(ARCH_X86)
 #if __CPU__ != 386
  __asm("bswap   %0":
       "=r" (x)     :
@@ -57,66 +63,13 @@ static av_always_inline uint32_t bswap_32(uint32_t x)
       LEGACY_REGS (x)                :
 #endif
       "0" (x));
-  return x;
-}
-
-static inline uint64_t bswap_64(uint64_t x)
-{
-#ifdef ARCH_X86_64
-  __asm("bswap  %0":
-        "=r" (x)   :
-        "0" (x));
-  return x;
-#else
-    union {
-        uint64_t ll;
-        struct {
-           uint32_t l,h;
-        } l;
-    } r;
-    r.l.l = bswap_32 (x);
-    r.l.h = bswap_32 (x>>32);
-    return r.ll;
-#endif
-}
-
 #elif defined(ARCH_SH4)
-
-static av_always_inline uint16_t bswap_16(uint16_t x) {
-        __asm__("swap.b %0,%0":"=r"(x):"0"(x));
-        return x;
-}
-
-static av_always_inline uint32_t bswap_32(uint32_t x) {
         __asm__(
         "swap.b %0,%0\n"
         "swap.w %0,%0\n"
         "swap.b %0,%0\n"
         :"=r"(x):"0"(x));
-        return x;
-}
-
-static inline uint64_t bswap_64(uint64_t x)
-{
-    union {
-        uint64_t ll;
-        struct {
-           uint32_t l,h;
-        } l;
-    } r;
-    r.l.l = bswap_32 (x);
-    r.l.h = bswap_32 (x>>32);
-    return r.ll;
-}
-#else
-
-static av_always_inline uint16_t bswap_16(uint16_t x){
-    x= (x>>8) | (x<<8);
-    return x;
-}
-
-#ifdef ARCH_ARM
-static av_always_inline uint32_t bswap_32(uint32_t x){
+#elif defined(ARCH_ARM)
     uint32_t t;
     __asm__ (
       "eor %1, %0, %0, ror #16 \n\t"
@@ -124,27 +77,19 @@ static av_always_inline uint32_t bswap_32(uint32_t x){
       "mov %0, %0, ror #8      \n\t"
       "eor %0, %0, %1, lsr #8  \n\t"
       : "+r"(x), "+r"(t));
-    return x;
-}
-
 #elif defined(ARCH_BFIN)
-static av_always_inline uint32_t bswap_32(uint32_t x){
     unsigned tmp;
     asm("%1 = %0 >> 8 (V);\n\t"
         "%0 = %0 << 8 (V);\n\t"
         "%0 = %0 | %1;\n\t"
         "%0 = PACK(%0.L, %0.H);\n\t"
         : "+d"(x), "=&d"(tmp));
-    return x;
-}
-
 #else
-static av_always_inline uint32_t bswap_32(uint32_t x){
     x= ((x<<8)&0xFF00FF00) | ((x>>8)&0x00FF00FF);
     x= (x>>16) | (x<<16);
+#endif
     return x;
 }
-#endif
 
 static inline uint64_t bswap_64(uint64_t x)
 {
@@ -152,6 +97,11 @@ static inline uint64_t bswap_64(uint64_t x)
     x= ((x<< 8)&0xFF00FF00FF00FF00ULL) | ((x>> 8)&0x00FF00FF00FF00FFULL);
     x= ((x<<16)&0xFFFF0000FFFF0000ULL) | ((x>>16)&0x0000FFFF0000FFFFULL);
     return (x>>32) | (x<<32);
+#elif defined(ARCH_X86_64)
+  __asm("bswap  %0":
+        "=r" (x)   :
+        "0" (x));
+  return x;
 #else
     union {
         uint64_t ll;
@@ -163,7 +113,6 @@ static inline uint64_t bswap_64(uint64_t x)
     return r.ll;
 #endif
 }
-#endif  /* defined(ARCH_X86) */
 
 #endif  /* !HAVE_BYTESWAP_H */
 
