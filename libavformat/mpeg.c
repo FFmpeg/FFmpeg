7af636d44ae549cf6848da9f5656f88b7303eb90
Wolfram Gloger <wmglo@dent.med.uni-muenchen.de>
2007-04-30 09:46:11 +0000

    Fix loop condition so it can be run through more than once. patch by Wolfram Gloger, wmglo dent.med.uni-muenchen de reference: [Ffmpeg-devel] buffer underflow messages from the output_example.c
    Originally committed as revision 8860 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/mpeg.c
+++ libavformat/mpeg.c
@@ -1028,9 +1028,10 @@ static int remove_decoded_packets(AVFormatContext *ctx, int64_t scr){
     for(i=0; i<ctx->nb_streams; i++){
         AVStream *st = ctx->streams[i];
         StreamInfo *stream = st->priv_data;
-        PacketDesc *pkt_desc= stream->predecode_packet;
+        PacketDesc *pkt_desc;
 
-        while(pkt_desc && scr > pkt_desc->dts){ //FIXME > vs >=
+        while((pkt_desc= stream->predecode_packet)
+              && scr > pkt_desc->dts){ //FIXME > vs >=
             if(stream->buffer_index < pkt_desc->size ||
                stream->predecode_packet == stream->premux_packet){
                 av_log(ctx, AV_LOG_ERROR,
