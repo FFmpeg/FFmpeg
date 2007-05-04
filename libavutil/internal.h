fd735e4b77a2e973b890c328805ee029dfe4b318
Michael Niedermayer <michaelni@gmx.at>
2007-05-04 21:22:43 +0000

    simplify
    Originally committed as revision 8890 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavutil/internal.h
+++ libavutil/internal.h
@@ -190,14 +190,13 @@ static inline int ff_sqrt(int a)
 {
     int ret=0;
     int s;
-    int ret_sq=0;
 
     if(a<128) return ff_sqrt_tab[a];
 
     for(s=15; s>=0; s--){
-        int b= ret_sq + (1<<(s*2)) + (ret<<s)*2;
+        int b= (1<<(s*2)) + (ret<<s)*2;
         if(b<=a){
-            ret_sq=b;
+            a-=b;
             ret+= 1<<s;
         }
     }
