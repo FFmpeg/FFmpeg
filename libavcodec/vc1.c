2a2072fe1e0dcbce68a33ac688c04c00963e56e7
Kostya Shishkov <kostya.shishkov@gmail.com>
2007-05-09 11:30:30 +0000

    Use correct dequantizer value
    Originally committed as revision 8950 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/vc1.c
+++ libavcodec/vc1.c
@@ -2962,7 +2962,7 @@ static int vc1_decode_p_block(VC1Context *v, DCTELEM block[64], int n, int mquan
         if(ttblk == TT_8X4_TOP || ttblk == TT_8X4_BOTTOM) ttblk = TT_8X4;
         if(ttblk == TT_4X8_RIGHT || ttblk == TT_4X8_LEFT) ttblk = TT_4X8;
     }
-    scale = 2 * mquant + v->halfpq;
+    scale = 2 * mquant + ((v->pq == mquant) ? v->halfpq : 0);
 
     // convert transforms like 8X4_TOP to generic TT and SUBBLKPAT
     if(ttblk == TT_8X4_TOP || ttblk == TT_8X4_BOTTOM) {
