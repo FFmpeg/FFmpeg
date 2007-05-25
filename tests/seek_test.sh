6d593d43567b55cccc4aa5e89150d63b1539f910
Diego Biurrun <diego@biurrun.de>
2007-05-25 13:27:08 +0000

    'grep -q' is a GNU extension.
    Originally committed as revision 9128 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- tests/seek_test.sh
+++ tests/seek_test.sh
@@ -11,7 +11,7 @@ rm -f $logfile
 for i in $list ; do
     echo ---------------- >>$logfile
     echo $i >>$logfile
-    echo $i | grep -vq 'b-libav[01][0-9][.]' &&
+    echo $i | grep -v 'b-libav[01][0-9][.]' 2> /dev/null &&
     ./seek_test $i >> $logfile
 done
 
