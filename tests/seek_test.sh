dc75f7eeb499b32ec47e35986ef6692fc6123027
Diego Biurrun <diego@biurrun.de>
2007-05-25 13:28:34 +0000

    whitespace cosmetics
    Originally committed as revision 9129 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- tests/seek_test.sh
+++ tests/seek_test.sh
@@ -9,8 +9,8 @@ reffile="$1"
 list=`ls data/a-* data/b-* | sort`
 rm -f $logfile
 for i in $list ; do
-    echo ---------------- >>$logfile
-    echo $i >>$logfile
+    echo ---------------- >> $logfile
+    echo $i >> $logfile
     echo $i | grep -v 'b-libav[01][0-9][.]' 2> /dev/null &&
     ./seek_test $i >> $logfile
 done
