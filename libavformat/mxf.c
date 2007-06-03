6dc55543284994403740012693c9a32648dddea3
Baptiste Coudurier <baptiste.coudurier@gmail.com>
2007-06-03 17:19:15 +0000

    no need to seek on specific key anymore, since klv_read_packet will sync, tested on encrypted files
    Originally committed as revision 9187 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/mxf.c
+++ libavformat/mxf.c
@@ -1017,7 +1017,6 @@ static int mxf_probe(AVProbeData *p) {
 /* XXX: use MXF Index */
 static int mxf_read_seek(AVFormatContext *s, int stream_index, int64_t sample_time, int flags)
 {
-    MXFContext *mxf = s->priv_data;
     AVStream *st = s->streams[stream_index];
     int64_t seconds;
 
@@ -1027,11 +1026,6 @@ static int mxf_read_seek(AVFormatContext *s, int stream_index, int64_t sample_ti
         sample_time = 0;
     seconds = av_rescale(sample_time, st->time_base.num, st->time_base.den);
     url_fseek(&s->pb, (s->bit_rate * seconds) >> 3, SEEK_SET);
-    if (!mxf_read_sync(&s->pb, mxf->sync_key, 12))
-        return -1;
-
-    /* found KLV key */
-    url_fseek(&s->pb, -12, SEEK_CUR);
     av_update_cur_dts(s, st, sample_time);
     return 0;
 }
