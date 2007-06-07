d07026cd16c96eccff4f74a8f76036c2dd2a3c97
Ronald S. Bultje <rsbultje@gmail.com>
2007-06-07 16:15:20 +0000

    CPU feature definitions should always be available, not just when the CPU feature is enabled. taken from a patch by Ronald S. Bultje, rbultje ronald.bitfreak net Date: Thu, 07 Jun 2007 08:57:46 -0400 Subject: Re: [FFmpeg-devel] [PATCH] Coremake support - ffmpeg_nommx.patch (1/1)  - ffmpeg-nommx.patch (1/1) - ff-cputest-noavconfig.patch (1/1)
    Originally committed as revision 9246 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/avcodec.h
+++ libavcodec/avcodec.h
@@ -1305,17 +1305,13 @@ typedef struct AVCodecContext {
     unsigned dsp_mask;
 #define FF_MM_FORCE    0x80000000 /* Force usage of selected flags (OR) */
     /* lower 16 bits - CPU features */
-#ifdef HAVE_MMX
 #define FF_MM_MMX      0x0001 /* standard MMX */
 #define FF_MM_3DNOW    0x0004 /* AMD 3DNOW */
 #define FF_MM_MMXEXT   0x0002 /* SSE integer functions or AMD MMX ext */
 #define FF_MM_SSE      0x0008 /* SSE functions */
 #define FF_MM_SSE2     0x0010 /* PIV SSE2 functions */
 #define FF_MM_3DNOWEXT 0x0020 /* AMD 3DNowExt */
-#endif /* HAVE_MMX */
-#ifdef HAVE_IWMMXT
 #define FF_MM_IWMMXT   0x0100 /* XScale IWMMXT */
-#endif /* HAVE_IWMMXT */
 
     /**
      * bits per sample/pixel from the demuxer (needed for huffyuv).
