c64bab0805f35ef11ea4b689c24a7ff64da4bffa
Diego Biurrun <diego@biurrun.de>
2007-06-07 14:13:59 +0000

    Fix linking: emms_c is a macro, #include the correct header file.
    Originally committed as revision 9238 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/grab.c
+++ libavformat/grab.c
@@ -19,6 +19,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 #include "avformat.h"
+#include "dsputil.h"
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/ioctl.h>
