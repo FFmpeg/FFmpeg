e336139f0c609c662d129a0a8a9acf91c1083e3d
Måns Rullgård <mans@mansr.com>
2007-06-16 15:15:16 +0000

    correct type for h263_parse()
    Originally committed as revision 9337 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/h263_parser.c
+++ libavcodec/h263_parser.c
@@ -63,7 +63,7 @@ int ff_h263_find_frame_end(ParseContext *pc, const uint8_t *buf, int buf_size){
 
 static int h263_parse(AVCodecParserContext *s,
                            AVCodecContext *avctx,
-                           uint8_t **poutbuf, int *poutbuf_size,
+                           const uint8_t **poutbuf, int *poutbuf_size,
                            const uint8_t *buf, int buf_size)
 {
     ParseContext *pc = s->priv_data;
