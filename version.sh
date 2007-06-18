7a68f7fa22861be70e983db01dad9960a762031a
Måns Rullgård <mans@mansr.com>
2007-06-18 19:43:11 +0000

    kill error messages from failed svn revision extraction commands
    Originally committed as revision 9363 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- version.sh
+++ version.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
 svn_revision=`cd "$1" && LC_ALL=C svn info 2> /dev/null | grep Revision | cut -d' ' -f2`
-test $svn_revision || svn_revision=`cd "$1" && grep revision .svn/entries | \
-                                    cut -d '"' -f2 2> /dev/null`
-test $svn_revision || svn_revision=`cd "$1" && sed -n '/^dir$/{n;p;q}' < .svn/entries`
+test $svn_revision || svn_revision=`cd "$1" && grep revision .svn/entries 2>/dev/null | cut -d '"' -f2`
+test $svn_revision || svn_revision=`cd "$1" && sed -n -e '/^dir$/{n;p;q}' .svn/entries 2>/dev/null`
 test $svn_revision || svn_revision=UNKNOWN
 
 NEW_REVISION="#define FFMPEG_VERSION \"SVN-r$svn_revision\""
