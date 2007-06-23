26301cb80611652efbdbf80b77f9de73b488421d
Måns Rullgård <mans@mansr.com>
2007-06-23 00:21:06 +0000

    simplify pstrcpy()
    Originally committed as revision 9391 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/cutils.c
+++ libavformat/cutils.c
@@ -75,19 +75,12 @@ int stristart(const char *str, const char *val, const char **ptr)
  */
 void pstrcpy(char *buf, int buf_size, const char *str)
 {
-    int c;
-    char *q = buf;
-
     if (buf_size <= 0)
         return;
 
-    for(;;) {
-        c = *str++;
-        if (c == 0 || q >= buf + buf_size - 1)
-            break;
-        *q++ = c;
-    }
-    *q = '\0';
+    while (buf_size-- > 1 && *str)
+        *buf++ = *str++;
+    *buf = 0;
 }
 
 /* strcat and truncate. */
