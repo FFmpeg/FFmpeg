3565e10b44cc978b75a5179191bc4a3c946a2e4a
Luca Barbato <lu_zero@gentoo.org>
2007-06-24 09:18:20 +0000

    bump micro version: new string functions
    Originally committed as revision 9405 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavutil/avutil.h
+++ libavutil/avutil.h
@@ -30,8 +30,8 @@
 #define AV_STRINGIFY(s)         AV_TOSTRING(s)
 #define AV_TOSTRING(s) #s
 
-#define LIBAVUTIL_VERSION_INT   ((49<<16)+(4<<8)+0)
-#define LIBAVUTIL_VERSION       49.4.0
+#define LIBAVUTIL_VERSION_INT   ((49<<16)+(4<<8)+1)
+#define LIBAVUTIL_VERSION       49.4.1
 #define LIBAVUTIL_BUILD         LIBAVUTIL_VERSION_INT
 
 #define LIBAVUTIL_IDENT         "Lavu" AV_STRINGIFY(LIBAVUTIL_VERSION)
