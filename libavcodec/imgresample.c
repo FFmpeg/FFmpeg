5b43487d23204001070efd44430134b5ea8e0087
Benoit Fouet <benoit.fouet@free.fr>
2007-06-26 18:00:50 +0000

    test context before accessing it
    Originally committed as revision 9436 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/imgresample.c
+++ libavcodec/imgresample.c
@@ -643,6 +643,7 @@ struct SwsContext *sws_getContext(int srcW, int srcH, int srcFormat,
     struct SwsContext *ctx;
 
     ctx = av_malloc(sizeof(struct SwsContext));
+    if (ctx)
     ctx->av_class = av_mallocz(sizeof(AVClass));
     if (!ctx || !ctx->av_class) {
         av_log(NULL, AV_LOG_ERROR, "Cannot allocate a resampling context!\n");
