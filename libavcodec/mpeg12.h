b66af278ace09bfb20bcfc9912beaa7ca64885d7
Aurelien Jacobs <aurel@gnuage.org>
2007-07-05 12:59:38 +0000

    forgot to svn add mpeg12.h in r9477
    Originally committed as revision 9487 to svn://svn.ffmpeg.org/ffmpeg/trunk

---
+++ libavcodec/mpeg12.h
@@ -0,0 +1,31 @@
+/*
+ * MPEG1/2 common code
+ * Copyright (c) 2007 Aurelien Jacobs <aurel@gnuage.org>
+ *
+ * This file is part of FFmpeg.
+ *
+ * FFmpeg is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * FFmpeg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FFmpeg; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#ifndef AVCODEC_MPEG12_H
+#define AVCODEC_MPEG12_H
+
+#include "mpegvideo.h"
+
+extern uint8_t ff_mpeg12_static_rl_table_store[2][2][2*MAX_RUN + MAX_LEVEL + 3];
+
+void ff_mpeg12_common_init(MpegEncContext *s);
+
+#endif /* AVCODEC_MPEG12_H */
