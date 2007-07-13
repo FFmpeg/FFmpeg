66be5b4536f9edebbbfe8146d44767e4f93d15e4
Ramiro Polla <ramiro.polla@gmail.com>
2007-07-13 16:04:00 +0000

    Check for getrusage
    Originally committed as revision 9627 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- ffmpeg.c
+++ ffmpeg.c
@@ -3113,10 +3113,12 @@ static int64_t getutime(void)
 #else
 static int64_t getutime(void)
 {
+#ifdef HAVE_GETRUSAGE
     struct rusage rusage;
 
     getrusage(RUSAGE_SELF, &rusage);
     return (rusage.ru_utime.tv_sec * 1000000LL) + rusage.ru_utime.tv_usec;
+#endif
 }
 #endif
 
