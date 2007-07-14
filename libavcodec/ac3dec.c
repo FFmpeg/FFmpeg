9d109601944c9b4d50bf4fb40f5ea8c983c10779
Justin Ruggles <justin.ruggles@gmail.com>
2007-07-14 22:17:23 +0000

    10l to me. I accidentally removed a word.
    Originally committed as revision 9677 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/ac3dec.c
+++ libavcodec/ac3dec.c
@@ -14,7 +14,7 @@
  * This file is part of FFmpeg.
  *
  * FFmpeg is free software; you can redistribute it and/or
- * modify it the terms of the GNU General Public
+ * modify it under the terms of the GNU General Public
  * License as published by the Free Software Foundation; either
  * version 2 of the License, or (at your option) any later version.
  *
