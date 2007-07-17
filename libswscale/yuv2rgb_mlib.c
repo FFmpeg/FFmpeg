325751ef328d4cb9da304d91a98cc0c6e0d1cce4
Diego Biurrun <diego@biurrun.de>
2007-07-17 09:35:21 +0000

    This was rewritten by Michael, switch copyrights to him and license to LGPL.
    Originally committed as revision 23796 to svn://svn.mplayerhq.hu/mplayer/trunk/libswscale

--- libswscale/yuv2rgb_mlib.c
+++ libswscale/yuv2rgb_mlib.c
@@ -1,24 +1,22 @@
 /*
  * yuv2rgb_mlib.c, Software YUV to RGB converter using mediaLib
+ * Copyright (C) 2003 Michael Niedermayer <michaelni@gmx.at>
  *
- *  Copyright (C) 2000, Håkan Hjort <d95hjort@dtek.chalmers.se>
- *  All Rights Reserved.
+ * This file is part of FFmpeg.
  *
- *  This file is part of mpeg2dec, a free MPEG-2 video decoder
+ * FFmpeg is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
  *
- *  mpeg2dec is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2, or (at your option)
- *  any later version.
+ * FFmpeg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
  *
- *  mpeg2dec is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with mpeg2dec; if not, write to the Free Software
- *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FFmpeg; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
 #include <mlib_types.h>
