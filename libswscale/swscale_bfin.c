7889f52890f7562de96a5b5ccf208832bb05f128
Marc Hoffman <mmhoffm@gmail.com>
2007-07-20 00:11:38 +0000

    integrating blackfin optimized yuyvtoyv12 converter
    Originally committed as revision 23833 to svn://svn.mplayerhq.hu/mplayer/trunk/libswscale

--- libswscale/swscale_bfin.c
+++ libswscale/swscale_bfin.c
@@ -39,6 +39,10 @@ extern int ff_bfin_uyvytoyv12 (const uint8_t *src, uint8_t *ydst, uint8_t *udst,
                                long width, long height,
                                long lumStride, long chromStride, long srcStride) __attribute__ ((l1_text));
 
+extern int ff_bfin_yuyvtoyv12 (const uint8_t *src, uint8_t *ydst, uint8_t *udst, uint8_t *vdst,
+                               long width, long height,
+                               long lumStride, long chromStride, long srcStride) __attribute__ ((l1_text));
+
 static int uyvytoyv12_unscaled (SwsContext *c, uint8_t* src[], int srcStride[], int srcSliceY,
                                 int srcSliceH, uint8_t* dst[], int dstStride[])
 {
@@ -53,6 +57,20 @@ static int uyvytoyv12_unscaled (SwsContext *c, uint8_t* src[], int srcStride[], 
     return srcSliceH;
 }
 
+static int yuyvtoyv12_unscaled (SwsContext *c, uint8_t* src[], int srcStride[], int srcSliceY,
+                                int srcSliceH, uint8_t* dst[], int dstStride[])
+{
+    uint8_t *dsty = dst[0] + dstStride[0]*srcSliceY;
+    uint8_t *dstu = dst[1] + dstStride[1]*srcSliceY/2;
+    uint8_t *dstv = dst[2] + dstStride[2]*srcSliceY/2;
+    uint8_t *ip   = src[0] + srcStride[0]*srcSliceY;
+    int w         = dstStride[0];
+
+    ff_bfin_yuyvtoyv12 (ip, dsty, dstu, dstv, w, srcSliceH, dstStride[0], dstStride[1], srcStride[0]);
+
+    return srcSliceH;
+}
+
 
 void ff_bfin_get_unscaled_swscale (SwsContext *c)
 {
@@ -62,5 +80,10 @@ void ff_bfin_get_unscaled_swscale (SwsContext *c)
             if (c->srcFormat == PIX_FMT_UYVY422) {
                 av_log (NULL, AV_LOG_VERBOSE, "selecting Blackfin optimized uyvytoyv12_unscaled\n");
                 c->swScale = uyvytoyv12_unscaled;
+            }
+        if (c->dstFormat == PIX_FMT_YUV420P)
+            if (c->srcFormat == PIX_FMT_YUYV422) {
+                av_log (NULL, AV_LOG_VERBOSE, "selecting Blackfin optimized yuyvtoyv12_unscaled\n");
+                c->swScale = yuyvtoyv12_unscaled;
             }
 }
