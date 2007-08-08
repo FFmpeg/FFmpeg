33a4ecbe20a5fd841f12bad76b0915874c2f0e56
Alex Beregszaszi <alex@rtfs.hu>
2007-08-08 18:56:27 +0000

    cosmetic: indentation
    Originally committed as revision 9986 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- ffserver.c
+++ ffserver.c
@@ -343,10 +343,10 @@ static void update_datarate(DataRateData *drd, int64_t count)
         drd->time1 = drd->time2 = cur_time;
         drd->count1 = drd->count2 = count;
     } else if (cur_time - drd->time2 > 5000) {
-            drd->time1 = drd->time2;
-            drd->count1 = drd->count2;
-            drd->time2 = cur_time;
-            drd->count2 = count;
+        drd->time1 = drd->time2;
+        drd->count1 = drd->count2;
+        drd->time2 = cur_time;
+        drd->count2 = count;
     }
 }
 
