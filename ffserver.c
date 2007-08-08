eeffbdea973f10a9ab5cb2f26e57e295235a2af9
Alex Beregszaszi <alex@rtfs.hu>
2007-08-08 18:55:49 +0000

    cosmetic: remove extra parenthesis
    Originally committed as revision 9985 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- ffserver.c
+++ ffserver.c
@@ -342,13 +342,11 @@ static void update_datarate(DataRateData *drd, int64_t count)
     if (!drd->time1 && !drd->count1) {
         drd->time1 = drd->time2 = cur_time;
         drd->count1 = drd->count2 = count;
-    } else {
-        if (cur_time - drd->time2 > 5000) {
+    } else if (cur_time - drd->time2 > 5000) {
             drd->time1 = drd->time2;
             drd->count1 = drd->count2;
             drd->time2 = cur_time;
             drd->count2 = count;
-        }
     }
 }
 
