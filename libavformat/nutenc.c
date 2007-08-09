6e6cf116d8b00ce10b54679d36dfcd928629b3bb
Michael Niedermayer <michaelni@gmx.at>
2007-08-09 20:55:09 +0000

    100l
    Originally committed as revision 10030 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/nutenc.c
+++ libavformat/nutenc.c
@@ -277,7 +277,7 @@ static int write_streamheader(NUTContext *nut, ByteIOContext *bc, AVCodecContext
     }else
         return -1;
 
-    put_v(bc, (nut->stream[i].time_base - nut->time_base)/sizeof(AVRational));
+    put_v(bc, nut->stream[i].time_base - nut->time_base);
     put_v(bc, nut->stream[i].msb_pts_shift);
     put_v(bc, nut->stream[i].max_pts_distance);
     put_v(bc, codec->has_b_frames);
