9ac34d947ab26e113a48b10b6f66fc4bd7f1c107
Michael Niedermayer <michaelni@gmx.at>
2007-08-13 01:05:19 +0000

    fix motion vectors for old version
    Originally committed as revision 10096 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/4xm.c
+++ libavcodec/4xm.c
@@ -248,7 +248,10 @@ static void init_mv(FourXContext *f){
     int i;
 
     for(i=0; i<256; i++){
-        f->mv[i] = mv[i][0] + mv[i][1]*f->current_picture.linesize[0]/2;
+        if(f->version)
+            f->mv[i] = mv[i][0]   + mv[i][1]  *f->current_picture.linesize[0]/2;
+        else
+            f->mv[i] = (i&15) - 8 + ((i>>4)-8)*f->current_picture.linesize[0]/2;
     }
 }
 
