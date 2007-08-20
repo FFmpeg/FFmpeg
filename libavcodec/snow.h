f29bd6fa714482f92de4115c7fd7b81058b153ff
Michael Niedermayer <michaelni@gmx.at>
2007-08-20 22:26:49 +0000

    more stupid double subtractions
    Originally committed as revision 10155 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/snow.h
+++ libavcodec/snow.h
@@ -165,11 +165,11 @@ static av_always_inline void snow_horizontal_compose_lift_lead_out(int i, DWTELE
 
 static av_always_inline void snow_horizontal_compose_liftS_lead_out(int i, DWTELEM * dst, DWTELEM * src, DWTELEM * ref, int width, int w){
         for(; i<w; i++){
-            dst[i] = src[i] - (((-(ref[i] + ref[(i+1)])+W_BO) - 4 * src[i]) >> W_BS);
+            dst[i] = src[i] + ((ref[i] + ref[(i+1)]+W_BO-1 + 4 * src[i]) >> W_BS);
         }
 
         if(width&1){
-            dst[w] = src[w] - (((-2 * ref[w] + W_BO) - 4 * src[w]) >> W_BS);
+            dst[w] = src[w] + ((2 * ref[w] + W_BO-1 + 4 * src[w]) >> W_BS);
         }
 }
 
