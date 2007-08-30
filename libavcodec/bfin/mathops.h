543a90d14ae9c4e96ce33491f2d262bfaca6dc21
Marc Hoffman <mmhoffm@gmail.com>
2007-08-30 15:15:48 +0000

    Blackfin 32bit multiplication primitives MULH MUL16
    Originally committed as revision 10262 to svn://svn.ffmpeg.org/ffmpeg/trunk

---
+++ libavcodec/bfin/mathops.h
@@ -0,0 +1,52 @@
+/*
+ * mathops.h
+ *
+ * Copyright (C) 2007 Marc Hoffman <mmhoffm@gmail.com>
+ *
+ * This file is part of FFmpeg.
+ *
+ * FFmpeg is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * FFmpeg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FFmpeg; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+#ifndef MATHOPS_BFIN
+#define MATHOPS_BFIN
+
+#ifdef CONFIG_MPEGAUDIO_HP
+#define MULH(X,Y) ({ int xxo;                           \
+    asm (                                               \
+        "a1 = %2.L * %1.L (FU);\n\t"                    \
+        "a1 = a1 >> 16;\n\t"                            \
+        "a1 += %2.H * %1.L (IS,M);\n\t"                 \
+        "a0 = %1.H * %2.H, a1+= %1.H * %2.L (IS,M);\n\t"\
+        "a1 = a1 >>> 16;\n\t"                           \
+        "%0 = (a0 += a1);\n\t"                          \
+        : "=d" (xxo) : "d" (X), "d" (Y)); xxo; })
+#else
+#define MULH(X,Y) ({ int xxo;                           \
+    asm (                                               \
+        "a1 = %2.H * %1.L (IS,M);\n\t"                  \
+        "a0 = %1.H * %2.H, a1+= %1.H * %2.L (IS,M);\n\t"\
+        "a1 = a1 >>> 16;\n\t"                           \
+        "%0 = (a0 += a1);\n\t"                          \
+        : "=d" (xxo) : "d" (X), "d" (Y)); xxo; })
+#endif
+
+/* signed 16x16 -> 32 multiply */
+#define MUL16(a, b) ({ int xxo;                         \
+    asm (                                               \
+       "%0 = %1.l*%2.l (is);\n\t"                       \
+       : "=W" (xxo) : "d" (a), "d" (b));                \
+    xxo; })
+
+#endif
