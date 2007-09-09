fd065d0593747eb38112bf02e02b23cd5eb6cba2
Reimar Döffinger <Reimar.Doeffinger@gmx.de>
2007-09-09 09:16:47 +0000

    Make sure rtjpeg quality is a valid value
    Originally committed as revision 10444 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/nuv.c
+++ libavcodec/nuv.c
@@ -97,6 +97,7 @@ static int get_quant(AVCodecContext *avctx, NuvContext *c,
  */
 static void get_quant_quality(NuvContext *c, int quality) {
     int i;
+    quality = FFMAX(quality, 1);
     for (i = 0; i < 64; i++) {
         c->lq[i] = (fallback_lquant[i] << 7) / quality;
         c->cq[i] = (fallback_cquant[i] << 7) / quality;
