9990460353251d43a95aa564ed9c0ffd8e253a0e
Aurelien Jacobs <aurel@gnuage.org>
2007-09-25 19:30:03 +0000

    use the VP6A codec
    Originally committed as revision 10578 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/flvdec.c
+++ libavformat/flvdec.c
@@ -62,6 +62,9 @@ static int flv_set_video_codec(AVFormatContext *s, AVStream *vstream, int flv_co
         case FLV_CODECID_H263  : vcodec->codec_id = CODEC_ID_FLV1   ; break;
         case FLV_CODECID_SCREEN: vcodec->codec_id = CODEC_ID_FLASHSV; break;
         case FLV_CODECID_VP6   : vcodec->codec_id = CODEC_ID_VP6F   ;
+        case FLV_CODECID_VP6A  :
+            if(flv_codecid == FLV_CODECID_VP6A)
+                vcodec->codec_id = CODEC_ID_VP6A;
             if(vcodec->extradata_size != 1) {
                 vcodec->extradata_size = 1;
                 vcodec->extradata = av_malloc(1);
