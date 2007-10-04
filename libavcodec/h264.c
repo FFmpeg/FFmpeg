5a7b254c75e7577ce5a8d4036e6fb7708180869a
Jeff Downs <heydowns@borg.com>
2007-10-04 06:37:04 +0000

    Re-indent unreference_pic.
    patch by Jeff Downs, heydowns a borg d com
    original thread:
    Subject: [FFmpeg-devel] [PATCH] Implement PAFF in H.264
    Date: 18/09/07 20:30
    
    Originally committed as revision 10660 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/h264.c
+++ libavcodec/h264.c
@@ -3109,15 +3109,15 @@ static inline int unreference_pic(H264Context *h, Picture *pic, int refmask){
     if (pic->reference &= refmask) {
         return 0;
     } else {
-    if(pic == h->delayed_output_pic)
-        pic->reference=DELAYED_PIC_REF;
-    else{
-        for(i = 0; h->delayed_pic[i]; i++)
-            if(pic == h->delayed_pic[i]){
-                pic->reference=DELAYED_PIC_REF;
-                break;
-            }
-    }
+        if(pic == h->delayed_output_pic)
+            pic->reference=DELAYED_PIC_REF;
+        else{
+            for(i = 0; h->delayed_pic[i]; i++)
+                if(pic == h->delayed_pic[i]){
+                    pic->reference=DELAYED_PIC_REF;
+                    break;
+                }
+        }
         return 1;
     }
 }
