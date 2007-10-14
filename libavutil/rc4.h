c1d5a67c560abe4f84fbdf4e5428f0ad4b57386f
Diego Biurrun <diego@biurrun.de>
2007-10-14 09:32:06 +0000

    Add standard license header.
    Originally committed as revision 10731 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavutil/rc4.h
+++ libavutil/rc4.h
@@ -1,1 +1,21 @@
+/*
+ * RC4 encryption/decryption/pseudo-random number generator
+ *
+ * This file is part of FFmpeg.
+ *
+ * FFmpeg is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * FFmpeg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FFmpeg; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
 void ff_rc4_enc(const uint8_t *key, int keylen, uint8_t *data, int datalen);
