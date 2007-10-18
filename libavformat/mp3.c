f97931c1223d27b68300aeca0f8ab699da2a5fa7
Thorsten Jordan <tjordan@ macrosystem.de>
2007-10-18 15:02:34 +0000

    set start_time to 0 to enable seeking in mp3-files
    patch by Thorsten Jordan, tjordan macrosystem de
    
    Originally committed as revision 10780 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/mp3.c
+++ libavformat/mp3.c
@@ -438,6 +438,7 @@ static int mp3_read_header(AVFormatContext *s,
     st->codec->codec_type = CODEC_TYPE_AUDIO;
     st->codec->codec_id = CODEC_ID_MP3;
     st->need_parsing = AVSTREAM_PARSE_FULL;
+    st->start_time = 0;
 
     /* try to get the TAG */
     if (!url_is_streamed(&s->pb)) {
