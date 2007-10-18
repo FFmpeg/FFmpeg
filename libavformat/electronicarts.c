a2c9473d96bd4dc05d04b4f801fe0c3e59fab63a
Aurelien Jacobs <aurel@gnuage.org>
2007-10-18 23:44:05 +0000

    cosmetics: indentation
    Originally committed as revision 10805 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/electronicarts.c
+++ libavformat/electronicarts.c
@@ -258,23 +258,22 @@ static int ea_read_header(AVFormatContext *s,
     }
 
     if (ea->audio_codec) {
-    /* initialize the audio decoder stream */
-    st = av_new_stream(s, 0);
-    if (!st)
-        return AVERROR(ENOMEM);
-    av_set_pts_info(st, 33, 1, ea->sample_rate);
-    st->codec->codec_type = CODEC_TYPE_AUDIO;
-    st->codec->codec_id = ea->audio_codec;
-    st->codec->codec_tag = 0;  /* no tag */
-    st->codec->channels = ea->num_channels;
-    st->codec->sample_rate = ea->sample_rate;
-    st->codec->bits_per_sample = ea->bytes * 8;
-    st->codec->bit_rate = st->codec->channels * st->codec->sample_rate *
-        st->codec->bits_per_sample / 4;
-    st->codec->block_align = st->codec->channels * st->codec->bits_per_sample;
-
-    ea->audio_stream_index = st->index;
-    ea->audio_frame_counter = 0;
+        /* initialize the audio decoder stream */
+        st = av_new_stream(s, 0);
+        if (!st)
+            return AVERROR(ENOMEM);
+        av_set_pts_info(st, 33, 1, ea->sample_rate);
+        st->codec->codec_type = CODEC_TYPE_AUDIO;
+        st->codec->codec_id = ea->audio_codec;
+        st->codec->codec_tag = 0;  /* no tag */
+        st->codec->channels = ea->num_channels;
+        st->codec->sample_rate = ea->sample_rate;
+        st->codec->bits_per_sample = ea->bytes * 8;
+        st->codec->bit_rate = st->codec->channels * st->codec->sample_rate *
+            st->codec->bits_per_sample / 4;
+        st->codec->block_align = st->codec->channels*st->codec->bits_per_sample;
+        ea->audio_stream_index = st->index;
+        ea->audio_frame_counter = 0;
     }
 
     return 1;
