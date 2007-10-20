123103caa225219f80efdfc5be3aef2bd4ea2531
Aurelien Jacobs <aurel@gnuage.org>
2007-10-20 00:05:04 +0000

    cosmetics to make version.sh more VCS agnostic
    Originally committed as revision 10816 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- version.sh
+++ version.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
-svn_revision=`cd "$1" && LC_ALL=C svn info 2> /dev/null | grep Revision | cut -d' ' -f2`
-test $svn_revision || svn_revision=`cd "$1" && grep revision .svn/entries 2>/dev/null | cut -d '"' -f2`
-test $svn_revision || svn_revision=`cd "$1" && sed -n -e '/^dir$/{n;p;q}' .svn/entries 2>/dev/null`
-test $svn_revision || svn_revision=UNKNOWN
+# check for SVN revision number
+revision=`cd "$1" && LC_ALL=C svn info 2> /dev/null | grep Revision | cut -d' ' -f2`
+test $revision || revision=`cd "$1" && grep revision .svn/entries 2>/dev/null | cut -d '"' -f2`
+test $revision || revision=`cd "$1" && sed -n -e '/^dir$/{n;p;q}' .svn/entries 2>/dev/null`
+test $revision && revision=SVN-r$revision
 
-NEW_REVISION="#define FFMPEG_VERSION \"SVN-r$svn_revision\""
+# no version number found
+test $revision || revision=UNKNOWN
+
+NEW_REVISION="#define FFMPEG_VERSION \"$revision\""
 OLD_REVISION=`cat version.h 2> /dev/null`
 
 # Update version.h only on revision changes to avoid spurious rebuilds
