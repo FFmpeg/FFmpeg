90f03441654f85a1402a65c3dcaa3f634a24c27e
Loren Merritt <lorenm@u.washington.edu>
2007-10-27 13:39:19 +0000

    remove a branch from an inner loop. 1% faster flac encoding.
    Originally committed as revision 10865 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/flacenc.c
+++ libavcodec/flacenc.c
@@ -474,16 +474,15 @@ static uint32_t calc_optimal_rice_params(RiceContext *rc, int porder,
     uint32_t all_bits;
 
     part = (1 << porder);
-    all_bits = 0;
+    all_bits = 4 * part;
 
     cnt = (n >> porder) - pred_order;
     for(i=0; i<part; i++) {
-        if(i == 1) cnt = (n >> porder);
         k = find_optimal_param(sums[i], cnt);
         rc->params[i] = k;
         all_bits += rice_encode_count(sums[i], cnt, k);
+        cnt = n >> porder;
     }
-    all_bits += (4 * part);
 
     rc->porder = porder;
 
