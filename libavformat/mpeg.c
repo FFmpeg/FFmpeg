73039e0fb4e5fbd13602c866ec98e097a777f1f5
Aurelien Jacobs <aurel@gnuage.org>
2007-11-08 20:53:06 +0000

    fix potential buffer over-read
    Originally committed as revision 10966 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/mpeg.c
+++ libavformat/mpeg.c
@@ -121,7 +121,7 @@ static int mpegps_read_header(AVFormatContext *s,
     s->ctx_flags |= AVFMTCTX_NOHEADER;
 
     get_buffer(&s->pb, buffer, sizeof(buffer));
-    if ((p=memchr(buffer, 'S', sizeof(buffer))))
+    if ((p=memchr(buffer, 'S', sizeof(buffer)-5)))
         if (!memcmp(p, "Sofdec", 6))
             m->sofdec = 1;
     url_fseek(&s->pb, -(offset_t)sizeof(buffer), SEEK_CUR);
