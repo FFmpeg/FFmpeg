5fba300d02f693de3c741e07740a851b2b3a94c7
Michael Niedermayer <michaelni@gmx.at>
2007-11-13 23:43:18 +0000

    rename fps(frame per second) to tb(time base) to prevent confusion with the average fps and hopefully stop these "wrong framerate" bug reports
    Originally committed as revision 11007 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/utils.c
+++ libavformat/utils.c
@@ -2525,11 +2525,11 @@ static void dump_stream_format(AVFormatContext *ic, int i, int index, int is_out
     av_log(NULL, AV_LOG_INFO, ": %s", buf);
     if(st->codec->codec_type == CODEC_TYPE_VIDEO){
         if(st->r_frame_rate.den && st->r_frame_rate.num)
-            av_log(NULL, AV_LOG_INFO, ", %5.2f fps(r)", av_q2d(st->r_frame_rate));
+            av_log(NULL, AV_LOG_INFO, ", %5.2f tb(r)", av_q2d(st->r_frame_rate));
 /*      else if(st->time_base.den && st->time_base.num)
-            av_log(NULL, AV_LOG_INFO, ", %5.2f fps(m)", 1/av_q2d(st->time_base));*/
+            av_log(NULL, AV_LOG_INFO, ", %5.2f tb(m)", 1/av_q2d(st->time_base));*/
         else
-            av_log(NULL, AV_LOG_INFO, ", %5.2f fps(c)", 1/av_q2d(st->codec->time_base));
+            av_log(NULL, AV_LOG_INFO, ", %5.2f tb(c)", 1/av_q2d(st->codec->time_base));
     }
     av_log(NULL, AV_LOG_INFO, "\n");
 }
