7f0cd6a5293ff7608b3725f4dcc6ad141030cc4f
Luca Abeni <lucabe72@email.it>
2007-11-19 07:39:55 +0000

    Remove perror() usage from libavutil
    Originally committed as revision 11057 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavutil/internal.h
+++ libavutil/internal.h
@@ -263,7 +263,7 @@ if((y)<(x)){\
 {\
     p= av_mallocz(size);\
     if(p==NULL && (size)!=0){\
-        perror("malloc");\
+        av_log(NULL, AV_LOG_ERROR, "Cannot allocate memory.");\
         goto fail;\
     }\
 }
