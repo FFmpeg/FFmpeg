c4035e5d3f3a28ec9a46dcb2a9760ccf10ba4b7d
Luca Abeni <lucabe72@email.it>
2007-11-23 08:07:23 +0000

    Fix the creation of alldevices.c (create it without any rename). This also requires some changes to configure
    Originally committed as revision 11080 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavdevice/alldevices.c
+++ libavdevice/alldevices.c
@@ -20,13 +20,13 @@
 #include "avformat.h"
 #include "avdevice.h"
 
-#define REGISTER_OUTDEV(X,x) { \
+#define REGISTER_MUXER(X,x) { \
           extern AVOutputFormat x##_muxer; \
           if(ENABLE_##X##_MUXER)   av_register_output_format(&x##_muxer); }
-#define REGISTER_INDEV(X,x) { \
+#define REGISTER_DEMUXER(X,x) { \
           extern AVInputFormat x##_demuxer; \
           if(ENABLE_##X##_DEMUXER) av_register_input_format(&x##_demuxer); }
-#define REGISTER_INOUTDEV(X,x)  REGISTER_INDEV(X,x); REGISTER_OUTDEV(X,x)
+#define REGISTER_MUXDEMUX(X,x)  REGISTER_MUXER(X,x); REGISTER_DEMUXER(X,x)
 
 void avdevice_register_all(void)
 {
@@ -37,14 +37,14 @@ void avdevice_register_all(void)
     inited = 1;
 
     /* devices */
-    REGISTER_INOUTDEV (AUDIO_BEOS, audio_beos);
-    REGISTER_INDEV    (BKTR, bktr);
-    REGISTER_INDEV    (DV1394, dv1394);
-    REGISTER_INOUTDEV (OSS, oss);
-    REGISTER_INDEV    (V4L2, v4l2);
-    REGISTER_INDEV    (V4L, v4l);
-    REGISTER_INDEV    (X11_GRAB_DEVICE, x11_grab_device);
+    REGISTER_MUXDEMUX (AUDIO_BEOS, audio_beos);
+    REGISTER_DEMUXER  (BKTR, bktr);
+    REGISTER_DEMUXER  (DV1394, dv1394);
+    REGISTER_MUXDEMUX (OSS, oss);
+    REGISTER_DEMUXER  (V4L2, v4l2);
+    REGISTER_DEMUXER  (V4L, v4l);
+    REGISTER_DEMUXER  (X11_GRAB_DEVICE, x11_grab_device);
 
     /* external libraries */
-    REGISTER_INDEV    (LIBDC1394, libdc1394);
+    REGISTER_DEMUXER  (LIBDC1394, libdc1394);
 }
