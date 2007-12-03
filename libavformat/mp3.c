2db971135ab498a1258e2a00d76e004ede53ffa2
Michael Niedermayer <michaelni@gmx.at>
2007-12-03 09:26:44 +0000

    return a slightly larger score if we find more than 500 valid mp3 frames in a row
    Originally committed as revision 11148 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/mp3.c
+++ libavformat/mp3.c
@@ -420,6 +420,7 @@ static int mp3_read_probe(AVProbeData *p)
             first_frames= frames;
     }
     if   (first_frames>=3) return AVPROBE_SCORE_MAX/2+1;
+    else if(max_frames>500)return AVPROBE_SCORE_MAX/2;
     else if(max_frames>=3) return AVPROBE_SCORE_MAX/4;
     else if(max_frames>=1) return 1;
     else                   return 0;
