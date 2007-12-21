00ef4f58e0071501da4db43f98c46bf61aab9821
Diego Biurrun <diego@biurrun.de>
2007-12-21 11:51:39 +0000

    Fix Doxygen function parameter name.
    Originally committed as revision 11295 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/avio.h
+++ libavformat/avio.h
@@ -104,7 +104,7 @@ int av_url_read_pause(URLContext *h, int pause);
  *        units from the beginning of the presentation.
  *        If a stream_index >= 0 is used and the protocol does not support
  *        seeking based on component streams, the call will fail with ENOTSUP.
- * @param time_stamp timestamp timestamp in AVStream.time_base units
+ * @param timestamp timestamp in AVStream.time_base units
  *        or if there is no stream specified then in AV_TIME_BASE units.
  * @param flags Optional combination of AVSEEK_FLAG_BACKWARD, AVSEEK_FLAG_BYTE
  *        and AVSEEK_FLAG_ANY. The protocol may silently ignore
