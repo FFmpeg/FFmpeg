3c808d8cc4c9dfbb24274205dcc3183ba95a3a5f
Ivo van Poorten <ivop@euronet.nl>
2007-12-28 13:09:57 +0000

    Sort properly
    Originally committed as revision 11343 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/allcodecs.c
+++ libavcodec/allcodecs.c
@@ -137,9 +137,9 @@ void avcodec_register_all(void)
     REGISTER_DECODER (SMC, smc);
     REGISTER_ENCDEC  (SNOW, snow);
     REGISTER_DECODER (SP5X, sp5x);
+    REGISTER_DECODER (SUNRAST, sunrast);
     REGISTER_ENCDEC  (SVQ1, svq1);
     REGISTER_DECODER (SVQ3, svq3);
-    REGISTER_DECODER (SUNRAST, sunrast);
     REGISTER_ENCDEC  (TARGA, targa);
     REGISTER_DECODER (THEORA, theora);
     REGISTER_DECODER (THP, thp);
