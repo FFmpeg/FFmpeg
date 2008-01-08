f34b221bd2bffc18e9d383b3e159a03b411818e9
Justin Ruggles <justin.ruggles@gmail.com>
2008-01-08 23:44:48 +0000

    used defined name for testing error resilience level
    Originally committed as revision 11478 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/ac3dec.c
+++ libavcodec/ac3dec.c
@@ -1133,7 +1133,7 @@ static int ac3_decode_frame(AVCodecContext * avctx, void *data, int *data_size, 
     }
 
     /* check for crc mismatch */
-    if(avctx->error_resilience > 0) {
+    if(avctx->error_resilience >= FF_ER_CAREFUL) {
         if(av_crc(av_crc_get_table(AV_CRC_16_ANSI), 0, &buf[2], s->frame_size-2)) {
             av_log(avctx, AV_LOG_ERROR, "frame CRC mismatch\n");
             return -1;
