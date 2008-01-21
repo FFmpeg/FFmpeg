a4b375c61475f46d9c17a33330fa251892267914
Diego Biurrun <diego@biurrun.de>
2008-01-21 00:28:29 +0000

    Remove unnecessary forward declaration.
    Originally committed as revision 11583 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/ac3enc.c
+++ libavcodec/ac3enc.c
@@ -73,8 +73,6 @@ static int16_t xsin1[128];
 /* new exponents are sent if their Norm 1 exceed this number */
 #define EXP_DIFF_THRESHOLD 1000
 
-static void fft_init(int ln);
-
 static inline int16_t fix15(float a)
 {
     int v;
