896b438915c656b877901a8514e70bfcd1915390
Michael Niedermayer <michaelni@gmx.at>
2008-02-01 16:21:22 +0000

    const
    Originally committed as revision 11794 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/xl.c
+++ libavcodec/xl.c
@@ -40,7 +40,7 @@ static const int xl_table[32] = {
 
 static int decode_frame(AVCodecContext *avctx,
                         void *data, int *data_size,
-                        uint8_t *buf, int buf_size)
+                        const uint8_t *buf, int buf_size)
 {
     VideoXLContext * const a = avctx->priv_data;
     AVFrame * const p= (AVFrame*)&a->pic;
