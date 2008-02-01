1dc5527934596c2499668405cf729e563b5925c4
Michael Niedermayer <michaelni@gmx.at>
2008-02-01 14:00:31 +0000

    const
    Originally committed as revision 11736 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/pngdec.c
+++ libavcodec/pngdec.c
@@ -31,9 +31,9 @@
 //#define DEBUG
 
 typedef struct PNGDecContext {
-    uint8_t *bytestream;
-    uint8_t *bytestream_start;
-    uint8_t *bytestream_end;
+    const uint8_t *bytestream;
+    const uint8_t *bytestream_start;
+    const uint8_t *bytestream_end;
     AVFrame picture;
 
     int state;
@@ -312,7 +312,7 @@ static int png_decode_idat(PNGDecContext *s, int length)
 
 static int decode_frame(AVCodecContext *avctx,
                         void *data, int *data_size,
-                        uint8_t *buf, int buf_size)
+                        const uint8_t *buf, int buf_size)
 {
     PNGDecContext * const s = avctx->priv_data;
     AVFrame *picture = data;
