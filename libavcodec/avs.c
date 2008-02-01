ebc0790441fa50a5e21190f3d6e66d9a51f7ee60
Michael Niedermayer <michaelni@gmx.at>
2008-02-01 03:48:55 +0000

    const
    Originally committed as revision 11716 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/avs.c
+++ libavcodec/avs.c
@@ -44,12 +44,13 @@ typedef enum {
 
 static int
 avs_decode_frame(AVCodecContext * avctx,
-                 void *data, int *data_size, uint8_t * buf, int buf_size)
+                 void *data, int *data_size, const uint8_t * buf, int buf_size)
 {
     avs_context_t *const avs = avctx->priv_data;
     AVFrame *picture = data;
     AVFrame *const p = (AVFrame *) & avs->picture;
-    uint8_t *table, *vect, *out;
+    const uint8_t *table, *vect;
+    uint8_t *out;
     int i, j, x, y, stride, vect_w = 3, vect_h = 3;
     int sub_type;
     avs_block_type_t type;
