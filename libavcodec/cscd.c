48670d4740f97eec85431f5b9585b16368f70889
Michael Niedermayer <michaelni@gmx.at>
2008-02-01 14:50:36 +0000

    const
    Originally committed as revision 11753 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/cscd.c
+++ libavcodec/cscd.c
@@ -135,7 +135,7 @@ static void add_frame_32(AVFrame *f, uint8_t *src,
 #endif
 
 static int decode_frame(AVCodecContext *avctx, void *data, int *data_size,
-                        uint8_t *buf, int buf_size) {
+                        const uint8_t *buf, int buf_size) {
     CamStudioContext *c = avctx->priv_data;
     AVFrame *picture = data;
 
