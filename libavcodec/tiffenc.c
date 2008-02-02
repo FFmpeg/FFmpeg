6be4042c528df11c9b1d6c96464042ba048963a2
Reimar Döffinger <Reimar.Doeffinger@gmx.de>
2008-02-02 21:38:25 +0000

    Fix warning: libavcodec/tiffenc.c:136: warning: pointer type mismatch in conditional expression
    Originally committed as revision 11817 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/tiffenc.c
+++ libavcodec/tiffenc.c
@@ -133,7 +133,7 @@ static void add_entry1(TiffEncoderContext * s,
                        enum TiffTags tag, enum TiffTypes type, int val){
     uint16_t w = val;
     uint32_t dw= val;
-    add_entry(s, tag, type, 1, type == TIFF_SHORT ? &w : &dw);
+    add_entry(s, tag, type, 1, type == TIFF_SHORT ? (void *)&w : (void *)&dw);
 }
 
 /**
