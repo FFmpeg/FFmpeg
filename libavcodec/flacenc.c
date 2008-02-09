fa4ed8c10af49ca22ead46ee6fa4e51c8327a7ff
Michael Niedermayer <michaelni@gmx.at>
2008-02-09 03:42:48 +0000

    optimize apply_welch_window()
    Originally committed as revision 11881 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/flacenc.c
+++ libavcodec/flacenc.c
@@ -595,11 +595,14 @@ static void apply_welch_window(const int32_t *data, int len, double *w_data)
 
     n2 = (len >> 1);
     c = 2.0 / (len - 1.0);
+
+    w_data+=n2;
+      data+=n2;
     for(i=0; i<n2; i++) {
-        w = c - i - 1.0;
+        w = c - n2 + i;
         w = 1.0 - (w * w);
-        w_data[i] = data[i] * w;
-        w_data[len-1-i] = data[len-1-i] * w;
+        w_data[-i-1] = data[-i-1] * w;
+        w_data[+i  ] = data[+i  ] * w;
     }
 }
 
