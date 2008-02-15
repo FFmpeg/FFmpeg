e4152452d57552b78a1170278ffff2e8bcbf63b0
Vitor Sessak <vitor1001@gmail.com>
2008-02-15 22:15:06 +0000

    Missed this file in my conditional compilation patch Commited in SoC by Vitor Sessak on 2008-02-12 20:40:32
    Originally committed as revision 12118 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavfilter/avfilter.h
+++ libavfilter/avfilter.h
@@ -491,6 +491,9 @@ void avfilter_draw_slice(AVFilterLink *link, int y, int h);
 /** Initialize the filter system.  Registers all builtin filters */
 void avfilter_init(void);
 
+/** Initialize the filter system.  Registers all builtin filters */
+void avfilter_register_all(void)
+
 /** Uninitialize the filter system.  Unregisters all filters */
 void avfilter_uninit(void);
 
