ef712a3b28c5212d7f5df6401535622ac9927426
Vitor Sessak <vitor1001@gmail.com>
2008-02-15 21:37:39 +0000

    Add a filter to overlay one video on top of another, picture-in-picture style.
    Commited in SoC by Bobby Bingham on 2007-08-13 16:53:19
    
    Originally committed as revision 12003 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavfilter/avfilter.c
+++ libavfilter/avfilter.c
@@ -220,6 +220,7 @@ void avfilter_init(void)
     avfilter_register(&vf_crop);
     avfilter_register(&vf_fps);
     avfilter_register(&vf_graph);
+    avfilter_register(&vf_overlay);
     avfilter_register(&vf_passthrough);
     avfilter_register(&vf_rgb2bgr);
     avfilter_register(&vf_slicify);
