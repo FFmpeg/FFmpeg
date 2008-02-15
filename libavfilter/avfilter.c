49d915cbf50cf75c972267e35631c5cf114312e4
Vitor Sessak <vitor1001@gmail.com>
2008-02-15 21:37:36 +0000

    Add a filter to modify the framerate. This demonstrates differing number of input and output frames.
    Commited in SoC by Bobby Bingham on 2007-08-11 17:50:36
    
    Originally committed as revision 12002 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavfilter/avfilter.c
+++ libavfilter/avfilter.c
@@ -218,6 +218,7 @@ void avfilter_init(void)
     avfilter_register(&vsrc_dummy);
     avfilter_register(&vsrc_ppm);
     avfilter_register(&vf_crop);
+    avfilter_register(&vf_fps);
     avfilter_register(&vf_graph);
     avfilter_register(&vf_passthrough);
     avfilter_register(&vf_rgb2bgr);
