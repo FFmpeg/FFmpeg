31304587da246672a240216d95298bb0ba9f9c64
Loren Merritt <lorenm@u.washington.edu>
2008-02-21 07:55:42 +0000

    indent
    Originally committed as revision 12166 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/dsputil.c
+++ libavcodec/dsputil.c
@@ -3304,16 +3304,16 @@ static void diff_bytes_c(uint8_t *dst, uint8_t *src1, uint8_t *src2, int w){
     long i;
 #ifndef HAVE_FAST_UNALIGNED
     if((long)src2 & (sizeof(long)-1)){
-    for(i=0; i+7<w; i+=8){
-        dst[i+0] = src1[i+0]-src2[i+0];
-        dst[i+1] = src1[i+1]-src2[i+1];
-        dst[i+2] = src1[i+2]-src2[i+2];
-        dst[i+3] = src1[i+3]-src2[i+3];
-        dst[i+4] = src1[i+4]-src2[i+4];
-        dst[i+5] = src1[i+5]-src2[i+5];
-        dst[i+6] = src1[i+6]-src2[i+6];
-        dst[i+7] = src1[i+7]-src2[i+7];
-    }
+        for(i=0; i+7<w; i+=8){
+            dst[i+0] = src1[i+0]-src2[i+0];
+            dst[i+1] = src1[i+1]-src2[i+1];
+            dst[i+2] = src1[i+2]-src2[i+2];
+            dst[i+3] = src1[i+3]-src2[i+3];
+            dst[i+4] = src1[i+4]-src2[i+4];
+            dst[i+5] = src1[i+5]-src2[i+5];
+            dst[i+6] = src1[i+6]-src2[i+6];
+            dst[i+7] = src1[i+7]-src2[i+7];
+        }
     }else
 #endif
     for(i=0; i<=w-sizeof(long); i+=sizeof(long)){
