4649fa90529c27520f2e9b7990dd11b47adc4465
Baptiste Coudurier <baptiste.coudurier@gmail.com>
2008-02-25 12:59:42 +0000

    fix mace duration
    Originally committed as revision 12209 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/aiff.c
+++ libavformat/aiff.c
@@ -130,6 +130,10 @@ static unsigned int get_aiff_header(ByteIOContext *pb, AVCodecContext *codec,
             codec->block_align = 34*codec->channels;
             codec->frame_size = 64;
             break;
+        case CODEC_ID_MACE3:
+        case CODEC_ID_MACE6:
+            codec->frame_size = 6;
+            break;
         default:
             break;
         }
