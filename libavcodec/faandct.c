5b0d04d308318082cfa4ca458583db6b06c036ab
Michael Niedermayer <michaelni@gmx.at>
2008-02-25 14:42:12 +0000

    Allow use of my AAN DCT under BSD, AMD has agreed to write and contribute SSE(float) AAN DCT and IDCT to ffmpeg (no C++, no crap).
    Originally committed as revision 12212 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/faandct.c
+++ libavcodec/faandct.c
@@ -19,6 +19,9 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with FFmpeg; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ * The AAN DCT in this file except ff_faandct248() can also be used under the
+ * BSD license.
  */
 
 /**
