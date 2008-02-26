3635791a9925992c2a2a2cd1bf81176b7773595c
Michael Niedermayer <michaelni@gmx.at>
2008-02-26 23:49:41 +0000

    include dependencies ...
    Originally committed as revision 12256 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/faanidct.h
+++ libavcodec/faanidct.h
@@ -22,6 +22,9 @@
 #ifndef FFMPEG_FAANIDCT_H
 #define FFMPEG_FAANIDCT_H
 
+#include <stdint.h>
+#include "dsputil.h"
+
 void ff_faanidct(DCTELEM block[64]);
 void ff_faanidct_add(uint8_t *dest, int line_size, DCTELEM block[64]);
 void ff_faanidct_put(uint8_t *dest, int line_size, DCTELEM block[64]);
