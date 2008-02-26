b85976ba647a0addea2c34c0bed070bbb289c32d
Michael Niedermayer <michaelni@gmx.at>
2008-02-26 23:51:59 +0000

    better include
    Originally committed as revision 12257 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/faanidct.c
+++ libavcodec/faanidct.c
@@ -18,7 +18,7 @@
  * License along with FFmpeg; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
-#include "mpegvideo.h"
+#include "faanidct.h"
 
 /* To allow switching to double. */
 #define FLOAT float
