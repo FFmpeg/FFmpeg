1ba3bae800d41f239109ae82dc06617962657f48
Kostya Shishkov <kostya.shishkov@gmail.com>
2008-03-09 14:15:40 +0000

    Correctly handle empty frames
    Originally committed as revision 12394 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/zmbv.c
+++ libavcodec/zmbv.c
@@ -504,6 +504,7 @@ static int decode_frame(AVCodecContext *avctx, void *data, int *data_size, const
     } else {
         c->pic.key_frame = 0;
         c->pic.pict_type = FF_P_TYPE;
+        if(c->decomp_len)
         c->decode_xor(c);
     }
 
