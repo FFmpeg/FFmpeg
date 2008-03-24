362b3bf757ba2f836a9ab6c0c7ff79164b3e0c10
Diego Biurrun <diego@biurrun.de>
2008-03-24 10:50:16 +0000

    Remove unneeded sys/mman.h #include.
    Originally committed as revision 12567 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavdevice/audio.c
+++ libavdevice/audio.c
@@ -31,9 +31,6 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/ioctl.h>
-#ifdef HAVE_SYS_MMAN_H
-#include <sys/mman.h>
-#endif
 #include <sys/time.h>
 
 #define AUDIO_BLOCK_SIZE 4096
--- libavdevice/x11grab.c
+++ libavdevice/x11grab.c
@@ -40,9 +40,6 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/ioctl.h>
-#ifdef HAVE_SYS_MMAN_H
-#include <sys/mman.h>
-#endif
 #include <sys/time.h>
 #define _LINUX_TIME_H 1
 #include <time.h>
