d42927893b4239805e8590071045b6d3c7547b7a
Vitor Sessak <vitor1001@gmail.com>
2008-04-04 20:08:07 +0000

    Header inclusion redux Commited in SoC by Vitor Sessak on 2008-02-10 08:46:50
    Originally committed as revision 12720 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavfilter/avfiltergraph.c
+++ libavfilter/avfiltergraph.c
@@ -19,10 +19,6 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
-#include <string.h>
-#include <stddef.h>
-
-#include "avstring.h"
 #include "avfilter.h"
 #include "avfiltergraph.h"
 
