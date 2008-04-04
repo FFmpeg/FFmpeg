e1a43a414677258b1e01d3f9a8f40bfc74be4223
Vitor Sessak <vitor1001@gmail.com>
2008-04-04 20:09:38 +0000

    Remove unused check Commited in SoC by Vitor Sessak on 2008-04-03 16:38:51
    Originally committed as revision 12750 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavfilter/avfiltergraph.c
+++ libavfilter/avfiltergraph.c
@@ -44,9 +44,6 @@ AVFilterContext *avfilter_graph_get_filter(AVFilterGraph *graph, char *name)
 {
     int i;
 
-    if(!name)
-        return NULL;
-
     for(i = 0; i < graph->filter_count; i ++)
         if(graph->filters[i]->name && !strcmp(name, graph->filters[i]->name))
             return graph->filters[i];
