54d7fcc1207ed37356f06e3a31a4e6bdaa096958
Vitor Sessak <vitor1001@gmail.com>
2008-04-04 20:07:37 +0000

    initialize filter graphs completely even if there is no list of filters given to initially load.
    Commited in SoC by Bobby Bingham on 2007-12-20 16:50:45
    
    Originally committed as revision 12709 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavfilter/avfiltergraph.c
+++ libavfilter/avfiltergraph.c
@@ -546,13 +546,13 @@ static int init(AVFilterContext *ctx, const char *args, void *opaque)
 {
     GraphContext *gctx = ctx->priv;
 
-    if(!args)
-        return 0;
-
     if(!(gctx->link_filter = avfilter_open(&vf_graph_dummy, NULL)))
         return -1;
     if(avfilter_init_filter(gctx->link_filter, NULL, ctx))
         goto fail;
+
+    if(!args)
+        return 0;
 
     return graph_load_chain_from_string(ctx, args, NULL, NULL);
 
