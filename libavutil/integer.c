4764fdc987b696adeab569012bb71414410cb9b3
Michael Niedermayer <michaelni@gmx.at>
2008-04-05 10:52:27 +0000

    simplify
    Originally committed as revision 12755 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavutil/integer.c
+++ libavutil/integer.c
@@ -96,10 +96,10 @@ AVInteger av_shr_i(AVInteger a, int s){
     int i;
 
     for(i=0; i<AV_INTEGER_SIZE; i++){
-        int index= i + (s>>4);
+        unsigned int index= i + (s>>4);
         unsigned int v=0;
-        if(index+1<AV_INTEGER_SIZE && index+1>=0) v = a.v[index+1]<<16;
-        if(index  <AV_INTEGER_SIZE && index  >=0) v+= a.v[index  ];
+        if(index+1<AV_INTEGER_SIZE) v = a.v[index+1]<<16;
+        if(index  <AV_INTEGER_SIZE) v+= a.v[index  ];
         out.v[i]= v >> (s&15);
     }
     return out;
