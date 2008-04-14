26d95a1ecf0b841bad3cbdb18bf1654fef2ec270
Michael Niedermayer <michaelni@gmx.at>
2008-04-14 12:10:53 +0000

    revert 	r12809 	Log: 	10l 	Setting AVFMTCTX_NOHEADER if streams<2 so phantom streams are found. 	fixes WELCOMETOBELGRADE.flv As it unexpectly breaks seek regression tests.
    Originally committed as revision 12811 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/flvdec.c
+++ libavformat/flvdec.c
@@ -256,9 +256,6 @@ static int flv_read_header(AVFormatContext *s,
         flags = FLV_HEADER_FLAG_HASVIDEO | FLV_HEADER_FLAG_HASAUDIO;
         av_log(s, AV_LOG_WARNING, "Broken FLV file, which says no streams present, this might fail\n");
     }
-    if((flags & (FLV_HEADER_FLAG_HASVIDEO|FLV_HEADER_FLAG_HASAUDIO))
-             != (FLV_HEADER_FLAG_HASVIDEO|FLV_HEADER_FLAG_HASAUDIO))
-        s->ctx_flags |= AVFMTCTX_NOHEADER;
 
     if(flags & FLV_HEADER_FLAG_HASVIDEO){
         if(!create_stream(s, 0))
