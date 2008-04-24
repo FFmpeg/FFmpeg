76f34857ce056235556bcfff259b4bcf41035b02
Baptiste Coudurier <baptiste.coudurier@gmail.com>
2008-04-24 17:16:13 +0000

    qcelp fourcc
    Originally committed as revision 12940 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/isom.c
+++ libavformat/isom.c
@@ -161,6 +161,10 @@ const AVCodecTag codec_movaudio_tags[] = {
 
     { CODEC_ID_GSM,  MKTAG('a', 'g', 's', 'm') },
     { CODEC_ID_ALAC, MKTAG('a', 'l', 'a', 'c') }, /* Apple Lossless */
+
+    { CODEC_ID_QCELP, MKTAG('Q','c','l','p') },
+    { CODEC_ID_QCELP, MKTAG('s','q','c','p') }, /* ISO Media fourcc */
+
     { CODEC_ID_QDM2, MKTAG('Q', 'D', 'M', '2') }, /* QDM2 */
 
     { CODEC_ID_DVAUDIO, MKTAG('v', 'd', 'v', 'a') },
