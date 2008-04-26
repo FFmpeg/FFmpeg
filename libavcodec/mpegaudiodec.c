41b8800f8861a6d7eac3a37ea39f5476abd73e92
Baptiste Coudurier <baptiste.coudurier@gmail.com>
2008-04-26 14:36:34 +0000

    merge init with declaration
    Originally committed as revision 12998 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/mpegaudiodec.c
+++ libavcodec/mpegaudiodec.c
@@ -2566,14 +2566,12 @@ static int decode_frame_mp3on4(AVCodecContext * avctx,
 {
     MP3On4DecodeContext *s = avctx->priv_data;
     MPADecodeContext *m;
-    int len, fsize, out_size = 0;
+    int fsize, len = buf_size, out_size = 0;
     uint32_t header;
     OUT_INT *out_samples = data;
     OUT_INT decoded_buf[MPA_FRAME_SIZE * MPA_MAX_CHANNELS];
     OUT_INT *outptr, *bp;
     int fr, j, n;
-
-    len = buf_size;
 
     *data_size = 0;
     // Discard too short frames
