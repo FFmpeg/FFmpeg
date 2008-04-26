8726882993df25c713ee51c2674492366ff84cfe
Vladimir Voroshilov <voroshil@gmail.com>
2008-04-26 11:52:33 +0000

    Fixed-point LSP and LPC decoding routines for ACELP-based codecs
    Originally committed as revision 12978 to svn://svn.ffmpeg.org/ffmpeg/trunk

---
+++ libavcodec/lsp.c
@@ -0,0 +1,120 @@
+/*
+ * LSP routines for ACELP-based codecs
+ *
+ * Copyright (c) 2008 Vladimir Voroshilov
+ *
+ * This file is part of FFmpeg.
+ *
+ * FFmpeg is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * FFmpeg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FFmpeg; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#include <inttypes.h>
+
+#include "avcodec.h"
+#define FRAC_BITS 14
+#include "mathops.h"
+#include "lsp.h"
+#include "acelp_math.h"
+
+void ff_acelp_reorder_lsf(int16_t* lsfq, int lsfq_min_distance, int lsfq_min, int lsfq_max)
+{
+    int i, j;
+
+    /* sort lsfq in ascending order. float bubble agorithm,
+       O(n) if data already sorted, O(n^2) - otherwise */
+    for(i=0; i<9; i++)
+        for(j=i; j>=0 && lsfq[j] > lsfq[j+1]; j--)
+            FFSWAP(int16_t, lsfq[j], lsfq[j+1]);
+
+    for(i=0;i<10; i++)
+    {
+        lsfq[i] = FFMAX(lsfq[i], lsfq_min);
+        lsfq_min = lsfq[i] + lsfq_min_distance;
+    }
+    lsfq[9] = FFMIN(lsfq[9], lsfq_max);//Is warning required ?
+}
+
+void ff_acelp_lsf2lsp(int16_t *lsp, const int16_t *lsf)
+{
+    int i;
+
+    /* Convert LSF to LSP, lsp=cos(lsf) */
+    for(i=0;i<10; i++)
+        // 20861 = 2.0 / PI in (0.15)
+        lsp[i] = ff_cos(lsf[i] * 20861 >> 15); // divide by PI and (0,13) -> (0,14)
+}
+
+/**
+ * \brief decodes polynomial coefficients from LSP
+ * \param f [out] decoded polynomial coefficients (-0x20000000 <= (3.22) <= 0x1fffffff)
+ * \param lsp LSP coefficients (-0x8000 <= (0.15) <= 0x7fff)
+ */
+static void lsp2poly(int* f, const int16_t* lsp)
+{
+    int i, j;
+
+    f[0] = 0x400000;          // 1.0 in (3.22)
+    f[1] = -lsp[0] << 8;      // *2 and (0.15) -> (3.22)
+
+    for(i=2; i<=5; i++)
+    {
+        f[i] = f[i-2];
+        for(j=i; j>1; j--)
+            f[j] -= MULL(f[j-1], lsp[2*i-2]) - f[j-2]; // (3.22) * (0.15) * 2 -> (3.22)
+
+        f[1] -= lsp[2*i-2] << 8;
+    }
+}
+
+void ff_acelp_lsp2lpc(int16_t* lp, const int16_t* lsp)
+{
+    int i;
+    int f1[6]; // (3.22)
+    int f2[6]; // (3.22)
+
+    lsp2poly(f1, lsp  );
+    lsp2poly(f2, lsp+1);
+
+    /* 3.2.6 of G.729, Equations 25 and  26*/
+    lp[0] = 4096;
+    for(i=1; i<6; i++)
+    {
+        int ff1 = f1[i] + f1[i-1]; // (3.22)
+        int ff2 = f2[i] - f2[i-1]; // (3.22)
+
+        ff1 += 1 << 10; // for rounding
+        lp[i]    = (ff1 + ff2) >> 11; // divide by 2 and (3.22) -> (3.12)
+        lp[11-i] = (ff1 - ff2) >> 11; // divide by 2 and (3.22) -> (3.12)
+    }
+}
+
+void ff_acelp_lp_decode(int16_t* lp_1st, int16_t* lp_2nd, const int16_t* lsp_2nd, const int16_t* lsp_prev)
+{
+    int16_t lsp_1st[10]; // (0.15)
+    int i;
+
+    /* LSP values for first subframe (3.2.5 of G.729, Equation 24)*/
+    for(i=0;i<10;i++)
+#ifdef G729_BITEXACT
+        lsp_1st[i] = (lsp_2nd[i] >> 1) + (lsp_prev[i] >> 1);
+#else
+        lsp_1st[i] = (lsp_2nd[i] + lsp_prev[i]) >> 1;
+#endif
+
+    ff_acelp_lsp2lpc(lp_1st, lsp_1st);
+
+    /* LSP values for second subframe (3.2.5 of G.729)*/
+    ff_acelp_lsp2lpc(lp_2nd, lsp_2nd);
+}
---
+++ libavcodec/lsp.h
@@ -0,0 +1,63 @@
+/*
+ * LSP computing for ACELP-based codecs
+ *
+ * Copyright (c) 2008 Vladimir Voroshilov
+ *
+ * This file is part of FFmpeg.
+ *
+ * FFmpeg is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * FFmpeg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FFmpeg; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#ifndef FFMPEG_ACELP_LPC_H
+#define FFMPEG_ACELP_LPC_H
+/**
+  (I.F) means fixed-point value with F fractional and I integer bits
+*/
+
+/**
+ * \brief keep distance between LSFs.
+ * \param lsfq [in/out] LSF to check and adjust
+ * \param lsfq_min_distance minimum distance between LSFs
+ * \param lsfq_min minimum allowed LSF value
+ * \param lsfq_max maximum allowed LSF value
+ */
+void ff_acelp_reorder_lsf(int16_t* lsfq, int lsfq_min_distance, int lsqf_min, int lsfq_max);
+
+/**
+ * \brief Convert LSF to LSP
+ * \param lsp [out] LSP coefficients (-0x8000 <= (0.15) < 0x8000)
+ * \param lsf normalized LSF coefficients (0 <= (2.13) < 0x2000 * PI)
+ *
+ * \remark It is safe to pass the same array in lsf and lsp parameters
+ */
+void ff_acelp_lsf2lsp(int16_t *lsp, const int16_t *lsf);
+
+/**
+ * \brief LSP to LP conversion (3.2.6 of G.729)
+ * \param lp [out] decoded LP coefficients (-0x8000 <= (3.12) < 0x8000)
+ * \param lsp LSP coefficients (-0x8000 <= (0.15) < 0x8000)
+ */
+void ff_acelp_lsp2lpc(int16_t* lp, const int16_t* lsp);
+
+/**
+ * \brief Interpolate LSP for the first subframe and convert LSP -> LP for both subframes (3.2.5 and 3.2.6 of G.729)
+ * \param lp_1st [out] decoded LP coefficients for 1st subframe (-0x8000 <= (3.12) < 0x8000)
+ * \param lp_2nd [out] decoded LP coefficients for second subframe (-0x8000 <= (3.12) < 0x8000)
+ * \param lsp_2nd LSP coefficients of the second subframe (-0x8000 <= (0.15) < 0x8000)
+ * \param lsp_prev past LSP coefficients (-0x8000 <= (0.15) < 0x8000)
+ */
+void ff_acelp_lp_decode(int16_t* lp_1st, int16_t* lp_2nd, const int16_t* lsp_2nd, const int16_t* lsp_prev);
+
+#endif // FFMPEG_ACELP_LPC_H
