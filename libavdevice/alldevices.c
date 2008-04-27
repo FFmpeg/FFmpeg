1f9449234e503d00d4189549e41a4b6eed4075f8
Diego Biurrun <diego@biurrun.de>
2008-04-27 11:52:35 +0000

    Remove unused avdevice.h header file; add config.h which is used.
    Originally committed as revision 13006 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavdevice/alldevices.c
+++ libavdevice/alldevices.c
@@ -18,7 +18,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 #include "avformat.h"
-#include "avdevice.h"
+#include "config.h"
 
 #define REGISTER_MUXER(X,x) { \
           extern AVOutputFormat x##_muxer; \
