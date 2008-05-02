512b37bb284fc97e68680781c4b1423ec6234d0a
Justin Ruggles <justin.ruggles@gmail.com>
2008-05-02 21:35:32 +0000

    move call to init_get_bits inside conditional
    Originally committed as revision 13037 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/flac.c
+++ libavcodec/flac.c
@@ -104,11 +104,11 @@ static av_cold int flac_decode_init(AVCodecContext * avctx)
 
     if (avctx->extradata_size > 4) {
         /* initialize based on the demuxer-supplied streamdata header */
-        init_get_bits(&s->gb, avctx->extradata, avctx->extradata_size*8);
         if (avctx->extradata_size == FLAC_STREAMINFO_SIZE) {
             ff_flac_parse_streaminfo(avctx, (FLACStreaminfo *)s, avctx->extradata);
             allocate_buffers(s);
         } else {
+            init_get_bits(&s->gb, avctx->extradata, avctx->extradata_size*8);
             metadata_parse(s);
         }
     }
