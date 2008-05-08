953f54f1f4796ec866745fbc9802975d432dd6fe
Diego Biurrun <diego@biurrun.de>
2008-05-08 23:11:04 +0000

    #include required stdint.h header directly.
    Originally committed as revision 13084 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- tests/seek_test.c
+++ tests/seek_test.c
@@ -18,6 +18,8 @@
  * License along with FFmpeg; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
+
+#include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
 
