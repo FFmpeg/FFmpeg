3f3865d30bf220a8224126ee05c97f659619448d
Vladimir Voroshilov <voroshil@gmail.com>
2008-05-11 03:18:18 +0000

    Replace hardcoded LP filter order with parameter. This will allow to reuse the code in the SIPR decoder.
    Originally committed as revision 13108 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/lsp.c
+++ libavcodec/lsp.c
@@ -28,30 +28,30 @@
 #include "lsp.h"
 #include "acelp_math.h"
 
-void ff_acelp_reorder_lsf(int16_t* lsfq, int lsfq_min_distance, int lsfq_min, int lsfq_max)
+void ff_acelp_reorder_lsf(int16_t* lsfq, int lsfq_min_distance, int lsfq_min, int lsfq_max, int lp_order)
 {
     int i, j;
 
     /* sort lsfq in ascending order. float bubble agorithm,
        O(n) if data already sorted, O(n^2) - otherwise */
-    for(i=0; i<9; i++)
+    for(i=0; i<lp_order-1; i++)
         for(j=i; j>=0 && lsfq[j] > lsfq[j+1]; j--)
             FFSWAP(int16_t, lsfq[j], lsfq[j+1]);
 
-    for(i=0;i<10; i++)
+    for(i=0; i<lp_order; i++)
     {
         lsfq[i] = FFMAX(lsfq[i], lsfq_min);
         lsfq_min = lsfq[i] + lsfq_min_distance;
     }
-    lsfq[9] = FFMIN(lsfq[9], lsfq_max);//Is warning required ?
+    lsfq[lp_order-1] = FFMIN(lsfq[lp_order-1], lsfq_max);//Is warning required ?
 }
 
-void ff_acelp_lsf2lsp(int16_t *lsp, const int16_t *lsf)
+void ff_acelp_lsf2lsp(int16_t *lsp, const int16_t *lsf, int lp_order)
 {
     int i;
 
     /* Convert LSF to LSP, lsp=cos(lsf) */
-    for(i=0;i<10; i++)
+    for(i=0; i<lp_order; i++)
         // 20861 = 2.0 / PI in (0.15)
         lsp[i] = ff_cos(lsf[i] * 20861 >> 15); // divide by PI and (0,13) -> (0,14)
 }
@@ -61,14 +61,14 @@ void ff_acelp_lsf2lsp(int16_t *lsp, const int16_t *lsf)
  * \param f [out] decoded polynomial coefficients (-0x20000000 <= (3.22) <= 0x1fffffff)
  * \param lsp LSP coefficients (-0x8000 <= (0.15) <= 0x7fff)
  */
-static void lsp2poly(int* f, const int16_t* lsp)
+static void lsp2poly(int* f, const int16_t* lsp, int lp_half_order)
 {
     int i, j;
 
     f[0] = 0x400000;          // 1.0 in (3.22)
     f[1] = -lsp[0] << 8;      // *2 and (0.15) -> (3.22)
 
-    for(i=2; i<=5; i++)
+    for(i=2; i<=lp_half_order; i++)
     {
         f[i] = f[i-2];
         for(j=i; j>1; j--)
@@ -78,43 +78,43 @@ static void lsp2poly(int* f, const int16_t* lsp)
     }
 }
 
-void ff_acelp_lsp2lpc(int16_t* lp, const int16_t* lsp)
+void ff_acelp_lsp2lpc(int16_t* lp, const int16_t* lsp, int lp_half_order)
 {
     int i;
-    int f1[6]; // (3.22)
-    int f2[6]; // (3.22)
+    int f1[lp_half_order+1]; // (3.22)
+    int f2[lp_half_order+1]; // (3.22)
 
-    lsp2poly(f1, lsp  );
-    lsp2poly(f2, lsp+1);
+    lsp2poly(f1, lsp  , lp_half_order);
+    lsp2poly(f2, lsp+1, lp_half_order);
 
     /* 3.2.6 of G.729, Equations 25 and  26*/
     lp[0] = 4096;
-    for(i=1; i<6; i++)
+    for(i=1; i<lp_half_order+1; i++)
     {
         int ff1 = f1[i] + f1[i-1]; // (3.22)
         int ff2 = f2[i] - f2[i-1]; // (3.22)
 
         ff1 += 1 << 10; // for rounding
         lp[i]    = (ff1 + ff2) >> 11; // divide by 2 and (3.22) -> (3.12)
-        lp[11-i] = (ff1 - ff2) >> 11; // divide by 2 and (3.22) -> (3.12)
+        lp[lp_order + 1 - i] = (ff1 - ff2) >> 11; // divide by 2 and (3.22) -> (3.12)
     }
 }
 
-void ff_acelp_lp_decode(int16_t* lp_1st, int16_t* lp_2nd, const int16_t* lsp_2nd, const int16_t* lsp_prev)
+void ff_acelp_lp_decode(int16_t* lp_1st, int16_t* lp_2nd, const int16_t* lsp_2nd, const int16_t* lsp_prev, int lp_order)
 {
-    int16_t lsp_1st[10]; // (0.15)
+    int16_t lsp_1st[lp_order]; // (0.15)
     int i;
 
     /* LSP values for first subframe (3.2.5 of G.729, Equation 24)*/
-    for(i=0;i<10;i++)
+    for(i=0; i<lp_order; i++)
 #ifdef G729_BITEXACT
         lsp_1st[i] = (lsp_2nd[i] >> 1) + (lsp_prev[i] >> 1);
 #else
         lsp_1st[i] = (lsp_2nd[i] + lsp_prev[i]) >> 1;
 #endif
 
-    ff_acelp_lsp2lpc(lp_1st, lsp_1st);
+    ff_acelp_lsp2lpc(lp_1st, lsp_1st, lp_order >> 1);
 
     /* LSP values for second subframe (3.2.5 of G.729)*/
-    ff_acelp_lsp2lpc(lp_2nd, lsp_2nd);
+    ff_acelp_lsp2lpc(lp_2nd, lsp_2nd, lp_order >> 1);
 }
