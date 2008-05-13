54d2d4f698bb04464b061273d7c45d9362343b29
Stefano Sabatini <stefano.sabatini-lala@poste.it>
2008-05-13 15:36:10 +0000

    Make 'ffmpeg -formats' print codec long names. patch by Stefano Sabatini, stefano.sabatini-lala poste it
    Originally committed as revision 13142 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- ffmpeg.c
+++ ffmpeg.c
@@ -3466,14 +3466,15 @@ static void opt_show_formats(void)
             break;
         }
         printf(
-            " %s%s%s%s%s%s %s",
+            " %s%s%s%s%s%s %-15s %s",
             decode ? "D": (/*p2->decoder ? "d":*/" "),
             encode ? "E":" ",
             type_str,
             cap & CODEC_CAP_DRAW_HORIZ_BAND ? "S":" ",
             cap & CODEC_CAP_DR1 ? "D":" ",
             cap & CODEC_CAP_TRUNCATED ? "T":" ",
-            p2->name);
+            p2->name,
+            p2->long_name ? p2->long_name : "");
        /* if(p2->decoder && decode==0)
             printf(" use %s for decoding", p2->decoder->name);*/
         printf("\n");
