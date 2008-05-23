42720ee4d04ee934f4b0780e296141245cf7a3ad
Michael Niedermayer <michaelni@gmx.at>
2008-05-23 13:03:52 +0000

    indent
    Originally committed as revision 13258 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/utils.c
+++ libavformat/utils.c
@@ -2251,7 +2251,7 @@ int ff_new_chapter(AVFormatContext *s, int id, int64_t start, int64_t end, const
     }
     if(chapter->title)
         av_free(chapter->title);
-        chapter->title = av_strdup(title);
+    chapter->title = av_strdup(title);
     chapter->id    = id;
     chapter->start = start;
     chapter->end = end;
