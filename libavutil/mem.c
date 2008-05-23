19757f6169a43ddd11fa0c01c0762844858a6aea
Michael Niedermayer <michaelni@gmx.at>
2008-05-23 12:37:52 +0000

    indent
    Originally committed as revision 13251 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavutil/mem.c
+++ libavutil/mem.c
@@ -141,10 +141,10 @@ char *av_strdup(const char *s)
 {
     char *ptr= NULL;
     if(s){
-    int len = strlen(s) + 1;
-    ptr = av_malloc(len);
-    if (ptr)
-        memcpy(ptr, s, len);
+        int len = strlen(s) + 1;
+        ptr = av_malloc(len);
+        if (ptr)
+            memcpy(ptr, s, len);
     }
     return ptr;
 }
