12849837d4fbd82afb78358fe73c99882f1ade8f
Vitor Sessak <vitor1001@gmail.com>
2008-05-24 20:41:57 +0000

    10l: fix previous commit Commited in SoC by Vitor Sessak on 2008-05-24 13:06:05
    Originally committed as revision 13348 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavfilter/graphparser.c
+++ libavfilter/graphparser.c
@@ -165,9 +165,10 @@ static AVFilterContext *parse_filter(const char **buf, AVFilterGraph *graph,
     char *opts = NULL;
     char *name = consume_string(buf);
 
-    if(**buf == '=')
+    if(**buf == '=') {
         (*buf)++;
         opts = consume_string(buf);
+    }
 
     return create_filter(graph, index, name, opts, log_ctx);
 }
