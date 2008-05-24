ba3fed2fc22fb1c80b807444a5c74eebd3dabe55
Vitor Sessak <vitor1001@gmail.com>
2008-05-24 20:41:54 +0000

    Simplify Commited in SoC by Vitor Sessak on 2008-05-24 13:05:30
    Originally committed as revision 13347 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavfilter/graphparser.c
+++ libavfilter/graphparser.c
@@ -162,15 +162,12 @@ static AVFilterContext *create_filter(AVFilterGraph *ctx, int index,
 static AVFilterContext *parse_filter(const char **buf, AVFilterGraph *graph,
                                      int index, AVClass *log_ctx)
 {
-    char *opts;
+    char *opts = NULL;
     char *name = consume_string(buf);
 
-    if(**buf == '=') {
+    if(**buf == '=')
         (*buf)++;
         opts = consume_string(buf);
-    } else {
-        opts = NULL;
-    }
 
     return create_filter(graph, index, name, opts, log_ctx);
 }
