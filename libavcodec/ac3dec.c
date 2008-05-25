cf03a8173a412962e5b370c6ee52e3ddddcde143
Justin Ruggles <justin.ruggles@gmail.com>
2008-05-25 18:54:17 +0000

    set audio parameters if frame header is ok, even if crc fails
    Originally committed as revision 13399 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/ac3dec.c
+++ libavcodec/ac3dec.c
@@ -1181,15 +1181,15 @@ static int ac3_decode_frame(AVCodecContext * avctx, void *data, int *data_size,
     }
 
     /* check for crc mismatch */
-    if(avctx->error_resilience >= FF_ER_CAREFUL) {
+    if(!err && avctx->error_resilience >= FF_ER_CAREFUL) {
         if(av_crc(av_crc_get_table(AV_CRC_16_ANSI), 0, &buf[2], s->frame_size-2)) {
             av_log(avctx, AV_LOG_ERROR, "frame CRC mismatch\n");
             err = 1;
         }
     }
 
-    /* if frame is ok, set audio parameters */
-    if (!err) {
+    /* if frame header is ok, set audio parameters */
+    if (err >= 0) {
         avctx->sample_rate = s->sample_rate;
         avctx->bit_rate = s->bit_rate;
 
