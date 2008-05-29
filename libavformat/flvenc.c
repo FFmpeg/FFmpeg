c2acba13a450869ff749ad2c5077f3902810d204
Baptiste Coudurier <baptiste.coudurier@gmail.com>
2008-05-29 00:35:23 +0000

    check if extradata comes from mp4 and assume bitsteam is already formated, should fix streamcopy
    Originally committed as revision 13516 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/flvenc.c
+++ libavformat/flvenc.c
@@ -336,7 +336,9 @@ static int flv_write_packet(AVFormatContext *s, AVPacket *pkt)
         put_byte(pb, FLV_TAG_TYPE_AUDIO);
     }
 
-    if (enc->codec_id == CODEC_ID_H264) {
+    if (enc->codec_id == CODEC_ID_H264 &&
+        /* check if extradata looks like mp4 formated */
+        enc->extradata_size > 0 && *(uint8_t*)enc->extradata != 1) {
         if (ff_avc_parse_nal_units(pkt->data, &pkt->data, &pkt->size) < 0)
             return -1;
         assert(pkt->size);
