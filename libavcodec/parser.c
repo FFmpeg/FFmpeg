ebdd73778c56d07caa57c236d11ee3df3ee1004c
Michael Niedermayer <michaelni@gmx.at>
2008-06-02 13:14:01 +0000

    cosmetic
    Originally committed as revision 13624 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/parser.c
+++ libavcodec/parser.c
@@ -78,11 +78,12 @@ AVCodecParserContext *av_parser_init(int codec_id)
 
 void ff_fetch_timestamp(AVCodecParserContext *s, int off, int remove){
     int i;
+
     s->dts= s->pts= AV_NOPTS_VALUE;
     s->offset= 0;
     for(i = 0; i < AV_PARSER_PTS_NB; i++) {
         if (   s->next_frame_offset + off >= s->cur_frame_offset[i]
-            &&(s->     frame_offset <  s->cur_frame_offset[i] || !s->frame_offset)
+            &&(s->     frame_offset       <  s->cur_frame_offset[i] || !s->frame_offset)
             && s->next_frame_offset + off <  s->cur_frame_end[i]){
             s->dts= s->cur_frame_dts[i];
             s->pts= s->cur_frame_pts[i];
