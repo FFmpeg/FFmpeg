6dfafe109d89f74d485f54aef5387e59d86abf1d
Baptiste Coudurier <baptiste.coudurier@gmail.com>
2008-06-11 23:45:00 +0000

    set log debug when avformat debug is set
    Originally committed as revision 13750 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- ffmpeg.c
+++ ffmpeg.c
@@ -2214,7 +2214,7 @@ static int opt_default(const char *opt, const char *arg){
     opt_names= av_realloc(opt_names, sizeof(void*)*(opt_name_count+1));
     opt_names[opt_name_count++]= o->name;
 
-    if(avctx_opts[0]->debug)
+    if(avctx_opts[0]->debug || avformat_opts->debug)
         av_log_set_level(AV_LOG_DEBUG);
     return 0;
 }
