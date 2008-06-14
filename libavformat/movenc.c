6ba9ed9557d82ffa98ea95955e6a94fe6fe3ddd9
Baptiste Coudurier <baptiste.coudurier@gmail.com>
2008-06-14 21:12:51 +0000

    invert mode_mov condition
    Originally committed as revision 13773 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/movenc.c
+++ libavformat/movenc.c
@@ -1407,13 +1407,14 @@ static int mov_write_ftyp_tag(ByteIOContext *pb, AVFormatContext *s)
 
     put_be32(pb, 0x200);
 
-    if(mov->mode != MODE_MOV){
+    if(mov->mode == MODE_MOV)
+        put_tag(pb, "qt  ");
+    else{
         put_tag(pb, "isom");
         put_tag(pb, "iso2");
         if(has_h264)
             put_tag(pb, "avc1");
-    }else
-        put_tag(pb, "qt  ");
+    }
 
     if (mov->mode == MODE_3GP)
         put_tag(pb, has_h264 ? "3gp6":"3gp4");
