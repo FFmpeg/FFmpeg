7d7a9e7f37eaf30b45af7afc5ea7a0de05a5b3d7
Michael Niedermayer <michaelni@gmx.at>
2008-06-16 23:31:23 +0000

    simplify
    Originally committed as revision 13788 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/g726.c
+++ libavcodec/g726.c
@@ -52,8 +52,8 @@ static inline int16_t mult(Float11* f1, Float11* f2)
         int res, exp;
 
         exp = f1->exp + f2->exp;
-        res = (((f1->mant * f2->mant) + 0x30) >> 4) << 7;
-        res = exp > 26 ? res << (exp - 26) : res >> (26 - exp);
+        res = (((f1->mant * f2->mant) + 0x30) >> 4);
+        res = exp > 19 ? res << (exp - 19) : res >> (19 - exp);
         return (f1->sign ^ f2->sign) ? -res : res;
 }
 
