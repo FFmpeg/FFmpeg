796d200a77a90e82897f1deac5513b9d903e8c21
Michael Niedermayer <michaelni@gmx.at>
2008-06-24 23:56:16 +0000

    Prevent duration calculation code from generating random durations for vorbis in ogg.
    Originally committed as revision 13958 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/utils.c
+++ libavformat/utils.c
@@ -541,6 +541,9 @@ static int get_audio_frame_size(AVCodecContext *enc, int size)
     if (enc->frame_size <= 1) {
         int bits_per_sample = av_get_bits_per_sample(enc->codec_id);
 
+        if(enc->codec_id == CODEC_ID_VORBIS)
+            return -1;
+
         if (bits_per_sample) {
             if (enc->channels == 0)
                 return -1;
