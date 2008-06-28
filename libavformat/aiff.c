c7adbedb08729d22d4a093f320014703084d3c33
Baptiste Coudurier <baptiste.coudurier@gmail.com>
2008-06-28 04:53:01 +0000

    cosmetics, align vertically
    Originally committed as revision 14017 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/aiff.c
+++ libavformat/aiff.c
@@ -25,19 +25,19 @@
 #include "riff.h"
 
 static const AVCodecTag codec_aiff_tags[] = {
-    { CODEC_ID_PCM_S16BE, MKTAG('N','O','N','E') },
-    { CODEC_ID_PCM_S8, MKTAG('N','O','N','E') },
-    { CODEC_ID_PCM_S24BE, MKTAG('N','O','N','E') },
-    { CODEC_ID_PCM_S32BE, MKTAG('N','O','N','E') },
-    { CODEC_ID_PCM_ALAW, MKTAG('a','l','a','w') },
-    { CODEC_ID_PCM_MULAW, MKTAG('u','l','a','w') },
-    { CODEC_ID_MACE3, MKTAG('M','A','C','3') },
-    { CODEC_ID_MACE6, MKTAG('M','A','C','6') },
-    { CODEC_ID_GSM, MKTAG('G','S','M',' ') },
-    { CODEC_ID_ADPCM_G726, MKTAG('G','7','2','6') },
-    { CODEC_ID_PCM_S16LE, MKTAG('s','o','w','t') },
+    { CODEC_ID_PCM_S16BE,    MKTAG('N','O','N','E') },
+    { CODEC_ID_PCM_S8,       MKTAG('N','O','N','E') },
+    { CODEC_ID_PCM_S24BE,    MKTAG('N','O','N','E') },
+    { CODEC_ID_PCM_S32BE,    MKTAG('N','O','N','E') },
+    { CODEC_ID_PCM_ALAW,     MKTAG('a','l','a','w') },
+    { CODEC_ID_PCM_MULAW,    MKTAG('u','l','a','w') },
+    { CODEC_ID_MACE3,        MKTAG('M','A','C','3') },
+    { CODEC_ID_MACE6,        MKTAG('M','A','C','6') },
+    { CODEC_ID_GSM,          MKTAG('G','S','M',' ') },
+    { CODEC_ID_ADPCM_G726,   MKTAG('G','7','2','6') },
+    { CODEC_ID_PCM_S16LE,    MKTAG('s','o','w','t') },
     { CODEC_ID_ADPCM_IMA_QT, MKTAG('i','m','a','4') },
-    { CODEC_ID_QDM2, MKTAG('Q','D','M','2') },
+    { CODEC_ID_QDM2,         MKTAG('Q','D','M','2') },
     { 0, 0 },
 };
 
