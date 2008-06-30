0abf0c5398c5784dfb660d4cc1e039a235a7daa4
Benoit Fouet <benoit.fouet@free.fr>
2008-06-30 11:16:47 +0000

    Simplify mm_support() for ARM (by Måns).
    Originally committed as revision 14033 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/armv4l/dsputil_arm.c
+++ libavcodec/armv4l/dsputil_arm.c
@@ -205,11 +205,7 @@ static void simple_idct_ipp_add(uint8_t *dest, int line_size, DCTELEM *block)
 
 int mm_support(void)
 {
-    int result = 0;
-#ifdef HAVE_IWMMXT
-    result |= MM_IWMMXT;
-#endif /* result */
-    return result;
+    return ENABLE_IWMMXT * MM_IWMMXT;
 }
 
 void dsputil_init_armv4l(DSPContext* c, AVCodecContext *avctx)
