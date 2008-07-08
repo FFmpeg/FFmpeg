265fccccde4ece85feea6fd5b355c58fbb550391
Stefano Sabatini <stefano.sabatini-lala@poste.it>
2008-07-08 07:05:57 +0000

    Print libavformat version in outgoing SDPs.
    Originally committed as revision 14106 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/sdp.c
+++ libavformat/sdp.c
@@ -59,7 +59,7 @@ static void sdp_write_header(char *buff, int size, struct sdp_session_level *s)
                             "o=- %d %d IN IPV4 %s\r\n"
                             "t=%d %d\r\n"
                             "s=%s\r\n"
-                            "a=tool:libavformat\r\n",
+                            "a=tool:libavformat " AV_STRINGIFY(LIBAVFORMAT_VERSION) "\r\n",
                             s->sdp_version,
                             s->id, s->version, s->src_addr,
                             s->start_time, s->end_time,
