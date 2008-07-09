304ba23a0b640e5f590295a024c49ffcbf888b08
Stefano Sabatini <stefano.sabatini-lala@poste.it>
2008-07-09 18:45:33 +0000

    Print the program_name rather than always "FFmpeg" when showing the commandline tools license.
    Originally committed as revision 14141 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- cmdutils.c
+++ cmdutils.c
@@ -230,40 +230,43 @@ void show_license(void)
 {
 #ifdef CONFIG_NONFREE
     printf(
-    "This version of FFmpeg has nonfree parts compiled in.\n"
-    "Therefore it is not legally redistributable.\n"
+    "This version of %s has nonfree parts compiled in.\n"
+    "Therefore it is not legally redistributable.\n",
+    program_name
     );
 #elif CONFIG_GPL
     printf(
-    "FFmpeg is free software; you can redistribute it and/or modify\n"
+    "%s is free software; you can redistribute it and/or modify\n"
     "it under the terms of the GNU General Public License as published by\n"
     "the Free Software Foundation; either version 2 of the License, or\n"
     "(at your option) any later version.\n"
     "\n"
-    "FFmpeg is distributed in the hope that it will be useful,\n"
+    "%s is distributed in the hope that it will be useful,\n"
     "but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
     "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n"
     "GNU General Public License for more details.\n"
     "\n"
     "You should have received a copy of the GNU General Public License\n"
-    "along with FFmpeg; if not, write to the Free Software\n"
-    "Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA\n"
+    "along with %s; if not, write to the Free Software\n"
+    "Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA\n",
+    program_name, program_name, program_name
     );
 #else
     printf(
-    "FFmpeg is free software; you can redistribute it and/or\n"
+    "%s is free software; you can redistribute it and/or\n"
     "modify it under the terms of the GNU Lesser General Public\n"
     "License as published by the Free Software Foundation; either\n"
     "version 2.1 of the License, or (at your option) any later version.\n"
     "\n"
-    "FFmpeg is distributed in the hope that it will be useful,\n"
+    "%s is distributed in the hope that it will be useful,\n"
     "but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
     "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU\n"
     "Lesser General Public License for more details.\n"
     "\n"
     "You should have received a copy of the GNU Lesser General Public\n"
-    "License along with FFmpeg; if not, write to the Free Software\n"
-    "Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA\n"
+    "License along with %s; if not, write to the Free Software\n"
+    "Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA\n",
+    program_name, program_name, program_name
     );
 #endif
 }
