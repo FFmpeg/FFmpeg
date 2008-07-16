7a9dba3cd6b8dc300f6b4bbdda9369939820710d
Michael Niedermayer <michaelni@gmx.at>
2008-07-16 17:48:47 +0000

    SVQ3 uses the JPEG variant of YUV 4:2:0. Fixes issue256
    Originally committed as revision 14257 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/h264.c
+++ libavcodec/h264.c
@@ -2200,6 +2200,10 @@ static av_cold int decode_init(AVCodecContext *avctx){
 //    s->decode_mb= ff_h263_decode_mb;
     s->quarter_sample = 1;
     s->low_delay= 1;
+
+    if(avctx->codec_id == CODEC_ID_SVQ3)
+        avctx->pix_fmt= PIX_FMT_YUVJ420P;
+    else
     avctx->pix_fmt= PIX_FMT_YUV420P;
 
     decode_init_vlc();
