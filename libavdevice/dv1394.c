29a717aef24af559321f74c7457e8bd72c2df8cf
Diego Biurrun <diego@biurrun.de>
2008-08-14 15:50:31 +0000

    Use '#include <poll.h>' instead of '#include <sys/poll.h>'. It is the standard location as defined by the Open Group.
    Originally committed as revision 14761 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavdevice/dv1394.c
+++ libavdevice/dv1394.c
@@ -23,9 +23,9 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <errno.h>
+#include <poll.h>
 #include <sys/ioctl.h>
 #include <sys/mman.h>
-#include <sys/poll.h>
 #include <sys/time.h>
 #include <time.h>
 #include <strings.h>
