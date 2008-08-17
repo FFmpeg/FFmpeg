4869f47eca587fd178e78ba37efa22919b96a474
Michael Niedermayer <michaelni@gmx.at>
2008-08-17 15:53:12 +0000

    Add multiple inclusion guards for consistency and to avoid them being forgotten in case they ever become neccessary.
    Originally committed as revision 14810 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavutil/pca.h
+++ libavutil/pca.h
@@ -23,7 +23,12 @@
  * Principal component analysis
  */
 
+#ifndef FFMPEG_PCA_H
+#define FFMPEG_PCA_H
+
 struct PCA *ff_pca_init(int n);
 void ff_pca_free(struct PCA *pca);
 void ff_pca_add(struct PCA *pca, double *v);
 int ff_pca(struct PCA *pca, double *eigenvector, double *eigenvalue);
+
+#endif /* FFMPEG_PCA_H */
