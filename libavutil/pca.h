7a0d00d49e587767e158f3af8f3d0d31d26faff7
Michael Niedermayer <michaelni@gmx.at>
2008-08-17 15:28:12 +0000

    Principal component analysis (will be cleaned up in next commits)
    Originally committed as revision 14802 to svn://svn.ffmpeg.org/ffmpeg/trunk

---
+++ libavutil/pca.h
@@ -0,0 +1,31 @@
+/*
+ * Principal component analysis
+ * Copyright (c) 2004 Michael Niedermayer <michaelni@gmx.at>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+/**
+ * @file pca.h
+ * Principal component analysis
+ */
+
+typedef struct PCA{
+    int count;
+    int n;
+    double *covariance;
+    double *mean;
+}PCA;
