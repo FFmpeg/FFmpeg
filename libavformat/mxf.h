c69968b4fe6403103c62665f64a84abbe5d1ef2b
Zhentan Feng <spyfeng@gmail.com>
2008-08-18 17:54:18 +0000

    Factorize common code out of the mxf demuxer. Patch by zhentan feng.
    Originally committed as revision 14826 to svn://svn.ffmpeg.org/ffmpeg/trunk

---
+++ libavformat/mxf.h
@@ -0,0 +1,70 @@
+/*
+ * MXF
+ * Copyright (c) 2006 SmartJog S.A., Baptiste Coudurier <baptiste dot coudurier at smartjog dot com>.
+ *
+ * This file is part of FFmpeg.
+ *
+ * FFmpeg is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * FFmpeg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FFmpeg; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+#ifndef FFMPEG_MXF_H
+#define FFMPEG_MXF_H
+
+#include "avformat.h"
+#include "libavcodec/bytestream.h"
+
+typedef uint8_t UID[16];
+
+enum MXFMetadataSetType {
+    AnyType,
+    MaterialPackage,
+    SourcePackage,
+    SourceClip,
+    TimecodeComponent,
+    Sequence,
+    MultipleDescriptor,
+    Descriptor,
+    Track,
+    CryptoContext,
+    Preface,
+    Identification,
+    ContentStorage,
+    SubDescriptor,
+};
+
+typedef struct {
+    UID key;
+    offset_t offset;
+    uint64_t length;
+} KLVPacket;
+
+typedef struct {
+    UID uid;
+    unsigned matching_len;
+    enum CodecID id;
+} MXFCodecUL;
+
+typedef struct {
+    UID uid;
+    enum CodecType type;
+} MXFDataDefinitionUL;
+
+extern const MXFDataDefinitionUL ff_mxf_data_definition_uls[];
+extern const MXFCodecUL ff_mxf_codec_uls[];
+extern const MXFCodecUL ff_mxf_essence_container_uls[];
+
+#define PRINT_KEY(pc, s, x) dprintf(pc, "%s %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X\n", s, \
+                             (x)[0], (x)[1], (x)[2], (x)[3], (x)[4], (x)[5], (x)[6], (x)[7], (x)[8], (x)[9], (x)[10], (x)[11], (x)[12], (x)[13], (x)[14], (x)[15])
+
+#endif /* FFMPEG_MXF_H */
