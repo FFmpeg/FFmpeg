6214266362a7881b5fe3c2e57a4e4c1762a832eb
elupus <elupus@ecce.se>
2008-08-27 20:08:20 +0200

    cosmetic reindent of mpegts_get_pcr

--- libavformat/mpegts.c
+++ libavformat/mpegts.c
@@ -1719,18 +1719,18 @@ static int64_t mpegts_get_pcr(AVFormatContext *s, int stream_index,
     uint8_t buf[TS_PACKET_SIZE];
     int pcr_l, pcr_pid = ((PESContext*)s->streams[stream_index]->priv_data)->pcr_pid;
     pos = ((*ppos  + ts->raw_packet_size - 1 - ts->pos47) / ts->raw_packet_size) * ts->raw_packet_size + ts->pos47;
-        while(pos < pos_limit) {
-            if (avio_seek(s->pb, pos, SEEK_SET) < 0)
-                return AV_NOPTS_VALUE;
-            if (avio_read(s->pb, buf, TS_PACKET_SIZE) != TS_PACKET_SIZE)
-                return AV_NOPTS_VALUE;
-            if ((pcr_pid < 0 || (AV_RB16(buf + 1) & 0x1fff) == pcr_pid) &&
-                parse_pcr(&timestamp, &pcr_l, buf) == 0) {
-                *ppos = pos;
-                return timestamp;
-            }
-            pos += ts->raw_packet_size;
+    while(pos < pos_limit) {
+        if (avio_seek(s->pb, pos, SEEK_SET) < 0)
+            return AV_NOPTS_VALUE;
+        if (avio_read(s->pb, buf, TS_PACKET_SIZE) != TS_PACKET_SIZE)
+            return AV_NOPTS_VALUE;
+        if ((pcr_pid < 0 || (AV_RB16(buf + 1) & 0x1fff) == pcr_pid) &&
+            parse_pcr(&timestamp, &pcr_l, buf) == 0) {
+            *ppos = pos;
+            return timestamp;
         }
+        pos += ts->raw_packet_size;
+    }
 
     return AV_NOPTS_VALUE;
 }
