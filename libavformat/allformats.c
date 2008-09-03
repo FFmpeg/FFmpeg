5df643c5dfcb830a9cb1bfc81d221b7b2f01294a
Vladimir Voroshilov <voroshil@gmail.com>
2008-09-03 15:18:40 +0700

    G.729 .bit file demuxer

--- libavformat/allformats.c
+++ libavformat/allformats.c
@@ -73,6 +73,7 @@ void av_register_all(void)
     REGISTER_DEMUXER  (BFI, bfi);
     REGISTER_DEMUXER  (BINTEXT, bintext);
     REGISTER_DEMUXER  (BINK, bink);
+    REGISTER_MUXDEMUX (BIT, bit);
     REGISTER_DEMUXER  (C93, c93);
     REGISTER_MUXDEMUX (CAF, caf);
     REGISTER_MUXDEMUX (CAVSVIDEO, cavsvideo);
