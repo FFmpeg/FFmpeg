a0528ed7dea2bcef78f4a413df8e7ac8ca9bf4bc
Diego Biurrun <diego@biurrun.de>
2008-09-06 16:09:57 +0000

    license header consistency cosmetics
    Originally committed as revision 15225 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/h264enc.c
+++ libavcodec/h264enc.c
@@ -1,6 +1,8 @@
 /*
  * H.264 encoder
  *
+ * This file is part of FFmpeg.
+ *
  * FFmpeg is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
