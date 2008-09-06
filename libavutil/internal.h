6123abad2e852952567c648faae6c6056cc5cac7
Stefano Sabatini <stefano.sabatini-lala@poste.it>
2008-09-06 07:26:05 +0000

    Remove wrong and unnecessary condition. VHOOK filters are not special so they are not supposed to use the printf, fprintf, perror and puts functions but av_log instead.
    Originally committed as revision 15221 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavutil/internal.h
+++ libavutil/internal.h
@@ -242,7 +242,7 @@ if((y)<(x)){\
 #define strcat strcat_is_forbidden_due_to_security_issues_use_av_strlcat
 #undef  exit
 #define exit exit_is_forbidden
-#if !(defined(LIBAVFORMAT_BUILD) || defined(FFMPEG_FRAMEHOOK_H))
+#ifndef LIBAVFORMAT_BUILD
 #undef  printf
 #define printf please_use_av_log
 #undef  fprintf
