3943bd09018a0648ebc4ca6f8df753360a0aa1e3
Diego Biurrun <diego@biurrun.de>
2008-09-07 21:02:28 +0000

    Relicense from LGPL 2.1 to LGPL 2.1 or later with the authors' permission.
    Originally committed as revision 15258 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/qtrleenc.c
+++ libavcodec/qtrleenc.c
@@ -9,7 +9,8 @@
  *
  * FFmpeg is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
- * License, version 2.1, as published by the Free Software Foundation
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
  *
  * FFmpeg is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
