03ea32f8504dac6193a56a6430445ab67e43ef0a
Aurelien Jacobs <aurel@gnuage.org>
2008-09-08 23:47:50 +0000

    remove one more stuff under #if LIBAVCODEC_VERSION_INT which was forgotten in r15263
    Originally committed as revision 15278 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/eval.h
+++ libavcodec/eval.h
@@ -28,16 +28,6 @@
 #ifndef AVCODEC_EVAL_H
 #define AVCODEC_EVAL_H
 
-#if LIBAVCODEC_VERSION_INT < ((52<<16)+(0<<8)+0)
-/**
- * @deprecated Use ff_eval2 instead
- */
-double ff_eval(char *s, double *const_value, const char **const_name,
-               double (**func1)(void *, double), const char **func1_name,
-               double (**func2)(void *, double, double), char **func2_name,
-               void *opaque);
-#endif
-
 /**
  * Parses and evaluates an expression.
  * Note, this is significantly slower than ff_parse_eval()
