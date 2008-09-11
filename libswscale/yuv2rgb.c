85a51e6bd52d70aba110862fd5b4cfe6160ce1cd
Michael Niedermayer <michaelni@gmx.at>
2008-09-11 00:09:01 +0000

    Fix chroma yuv->rgb tables for jpeg style yuv, this was missed as it only affects the C code while mmx uses different tables.
    Originally committed as revision 27576 to svn://svn.mplayerhq.hu/mplayer/trunk/libswscale

--- libswscale/yuv2rgb.c
+++ libswscale/yuv2rgb.c
@@ -880,10 +880,10 @@ int yuv2rgb_c_init_tables (SwsContext *c, const int inv_table[4], int fullRange,
     }
 
     for (i = 0; i < 256; i++) {
-        c->table_rV[i] = (uint8_t *)table_r + entry_size * div_round (crv * (i-128), 76309);
-        c->table_gU[i] = (uint8_t *)table_g + entry_size * div_round (cgu * (i-128), 76309);
-        c->table_gV[i] = entry_size * div_round (cgv * (i-128), 76309);
-        c->table_bU[i] = (uint8_t *)table_b + entry_size * div_round (cbu * (i-128), 76309);
+        c->table_rV[i] = (uint8_t *)table_r + entry_size * div_round (crv * (i-128), cy);
+        c->table_gU[i] = (uint8_t *)table_g + entry_size * div_round (cgu * (i-128), cy);
+        c->table_gV[i] = entry_size * div_round (cgv * (i-128), cy);
+        c->table_bU[i] = (uint8_t *)table_b + entry_size * div_round (cbu * (i-128), cy);
     }
 
     av_free(c->yuvTable);
