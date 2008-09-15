631e8afb8c59a0759e57ade1c8a910bb46e0fd6d
Michael Niedermayer <michaelni@gmx.at>
2008-09-15 20:30:22 +0000

    Correct the threshold of get_ur_golomb_jpegls() at which the optimized case is used. Fix issue245.
    Originally committed as revision 15334 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/golomb.h
+++ libavcodec/golomb.h
@@ -255,7 +255,7 @@ static inline int get_ur_golomb_jpegls(GetBitContext *gb, int k, int limit, int 
 
     log= av_log2(buf);
 
-    if(log > 31-11){
+    if(log - k >= 32-MIN_CACHE_BITS){
         buf >>= log - k;
         buf += (30-log)<<k;
         LAST_SKIP_BITS(re, gb, 32 + k - log);
