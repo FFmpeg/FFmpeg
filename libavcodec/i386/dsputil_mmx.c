8cfd78ce8ffa59d6286a09664f8e65fe591ba23f
David Conrad <lessen42@gmail.com>
2008-09-17 19:49:31 +0000

    Ensure MMX/SSE2 VP3 IDCT selection isn't disabled when only Theora is enabled
    Originally committed as revision 15350 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/i386/dsputil_mmx.c
+++ libavcodec/i386/dsputil_mmx.c
@@ -2527,7 +2527,7 @@ void dsputil_init_mmx(DSPContext* c, AVCodecContext *avctx)
                 }
                 c->idct_permutation_type= FF_LIBMPEG2_IDCT_PERM;
 #endif
-            }else if((ENABLE_VP3_DECODER || ENABLE_VP5_DECODER || ENABLE_VP6_DECODER) &&
+            }else if((ENABLE_VP3_DECODER || ENABLE_VP5_DECODER || ENABLE_VP6_DECODER || ENABLE_THEORA_DECODER) &&
                      idct_algo==FF_IDCT_VP3){
                 if(mm_flags & MM_SSE2){
                     c->idct_put= ff_vp3_idct_put_sse2;
