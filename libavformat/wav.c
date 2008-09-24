f2d65a6c8a0dba80a11236d17a499093788b12d3
Michael Niedermayer <michaelni@gmx.at>
2008-09-24 17:46:23 +0000

    Avoid segfault due to not set AVCodec.
    Originally committed as revision 15400 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/wav.c
+++ libavformat/wav.c
@@ -45,7 +45,7 @@ static int wav_write_header(AVFormatContext *s)
     fmt = start_tag(pb, "fmt ");
     if (put_wav_header(pb, s->streams[0]->codec) < 0) {
         av_log(s, AV_LOG_ERROR, "%s codec not supported in WAVE format\n",
-               s->streams[0]->codec->codec->name);
+               s->streams[0]->codec->codec ? s->streams[0]->codec->codec->name : "NONE");
         av_free(wav);
         return -1;
     }
