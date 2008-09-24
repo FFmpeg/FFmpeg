e774c41cab765f5d12ecfb31e5fa30df41230de0
Kostya Shishkov <kostya.shishkov@gmail.com>
2008-09-24 12:45:28 +0000

    Correct wrong lower limit and condition used in APE decoder
    Originally committed as revision 15396 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/apedec.c
+++ libavcodec/apedec.c
@@ -358,11 +358,10 @@ static inline int range_get_symbol(APEContext * ctx,
 
 static inline void update_rice(APERice *rice, int x)
 {
+    int lim = rice->k ? (1 << (rice->k + 4)) : 0;
     rice->ksum += ((x + 1) / 2) - ((rice->ksum + 16) >> 5);
 
-    if (rice->k == 0)
-        rice->k = 1;
-    else if (rice->ksum < (1 << (rice->k + 4)))
+    if (rice->ksum < lim)
         rice->k--;
     else if (rice->ksum >= (1 << (rice->k + 5)))
         rice->k++;
