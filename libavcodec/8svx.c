6dbfb7de82552062bb892e9f775f83a3e7072af4
Reimar Döffinger <Reimar.Doeffinger@gmx.de>
2008-10-02 15:51:44 +0000

    Use "static const" instead of "const static"
    Originally committed as revision 15514 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/8svx.c
+++ libavcodec/8svx.c
@@ -35,9 +35,9 @@ typedef struct EightSvxContext {
     int16_t *table;
 } EightSvxContext;
 
-const static int16_t fibonacci[16]   = { -34<<8, -21<<8, -13<<8,  -8<<8, -5<<8, -3<<8, -2<<8, -1<<8,
+static const int16_t fibonacci[16]   = { -34<<8, -21<<8, -13<<8,  -8<<8, -5<<8, -3<<8, -2<<8, -1<<8,
                                           0, 1<<8, 2<<8, 3<<8, 5<<8, 8<<8, 13<<8, 21<<8 };
-const static int16_t exponential[16] = { -128<<8, -64<<8, -32<<8, -16<<8, -8<<8, -4<<8, -2<<8, -1<<8,
+static const int16_t exponential[16] = { -128<<8, -64<<8, -32<<8, -16<<8, -8<<8, -4<<8, -2<<8, -1<<8,
                                           0, 1<<8, 2<<8, 4<<8, 8<<8, 16<<8, 32<<8, 64<<8 };
 
 /** decode a frame */
