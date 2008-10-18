bf9067cf3eba497d450b46d075dde93a90675539
Michael Niedermayer <michaelni@gmx.at>
2008-10-18 10:40:31 +0000

    Assume mono if no other information for raw. Should fix issue687
    Originally committed as revision 15631 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/raw.c
+++ libavformat/raw.c
@@ -113,7 +113,8 @@ static int raw_read_header(AVFormatContext *s, AVFormatParameters *ap)
         switch(st->codec->codec_type) {
         case CODEC_TYPE_AUDIO:
             st->codec->sample_rate = ap->sample_rate;
-            st->codec->channels = ap->channels;
+            if(ap->channels) st->codec->channels = ap->channels;
+            else             st->codec->channels = 1;
             av_set_pts_info(st, 64, 1, st->codec->sample_rate);
             break;
         case CODEC_TYPE_VIDEO:
