ccfe8ae310b54814d22dde1ed289e6891cac243d
Kostya Shishkov <kostya.shishkov@gmail.com>
2008-10-20 05:59:50 +0000

    34l: free RV3/4 context variables at the end
    Originally committed as revision 15652 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/rv34.c
+++ libavcodec/rv34.c
@@ -1329,6 +1329,9 @@ av_cold int ff_rv34_decode_end(AVCodecContext *avctx)
     av_freep(&r->intra_types_hist);
     r->intra_types = NULL;
     av_freep(&r->mb_type);
+    av_freep(&r->cbp_luma);
+    av_freep(&r->cbp_chroma);
+    av_freep(&r->deblock_coefs);
 
     return 0;
 }
