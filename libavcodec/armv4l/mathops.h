96dea17a1d9a2caffc3923b4721ff88ac2e72144
Måns Rullgård <mans@mansr.com>
2008-11-06 01:33:17 +0000

    ARM: mathops.h whitespace cosmetics
    Originally committed as revision 15779 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/armv4l/mathops.h
+++ libavcodec/armv4l/mathops.h
@@ -55,7 +55,7 @@ static inline av_const int64_t MUL64(int a, int b)
 {
     union { uint64_t x; unsigned hl[2]; } x;
     __asm__ ("smull %0, %1, %2, %3"
-         : "=r"(x.hl[0]), "=r"(x.hl[1]) : "r"(a), "r"(b));
+             : "=r"(x.hl[0]), "=r"(x.hl[1]) : "r"(a), "r"(b));
     return x.x;
 }
 #define MUL64 MUL64
@@ -64,7 +64,7 @@ static inline av_const int64_t MAC64(int64_t d, int a, int b)
 {
     union { uint64_t x; unsigned hl[2]; } x = { d };
     __asm__ ("smlal %0, %1, %2, %3"
-         : "+r"(x.hl[0]), "+r"(x.hl[1]) : "r"(a), "r"(b));
+             : "+r"(x.hl[0]), "+r"(x.hl[1]) : "r"(a), "r"(b));
     return x.x;
 }
 #define MAC64(d, a, b) ((d) = MAC64(d, a, b))
