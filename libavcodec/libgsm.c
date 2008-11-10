916ff02261f79e759d996c76670958276276bf2a
Martin Storsjö <martin@martin.st>
2008-11-10 20:02:00 +0000

    Fix memory leak in libgsm wrapper.
    Patch by Martin Storsjö, martin at martin dot st
    
    Originally committed as revision 15798 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/libgsm.c
+++ libavcodec/libgsm.c
@@ -89,6 +89,7 @@ static av_cold int libgsm_init(AVCodecContext *avctx) {
 }
 
 static av_cold int libgsm_close(AVCodecContext *avctx) {
+    av_freep(&avctx->coded_frame);
     gsm_destroy(avctx->priv_data);
     avctx->priv_data = NULL;
     return 0;
