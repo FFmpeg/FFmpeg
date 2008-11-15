108cd247934f640f3719ebddcb5fd6be36d4f529
Ronald S. Bultje <rsbultje@gmail.com>
2008-11-15 20:43:57 +0000

    Make function comment a bit more linguistically correct.
    Originally committed as revision 15835 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/rdt.h
+++ libavformat/rdt.h
@@ -73,7 +73,7 @@ void ff_rdt_subscribe_rule2(RDTDemuxContext *s, char *cmd, int size,
  * @param buf input buffer
  * @param len length of input buffer
  * @param set_id will be set to the set ID this packet belongs to
- * @param seq_no will be set to the sequence number this packet belongs to
+ * @param seq_no will be set to the sequence number of the packet
  * @param stream_id will be set to the stream ID this packet belongs to
  * @param is_keyframe will be whether this packet belongs to a keyframe
  * @param timestamp will be set to the timestamp of the packet
