bad406374038d9edef171a3ff4cfb54464edbc10
Diego Biurrun <diego@biurrun.de>
2008-12-03 16:47:07 +0000

    Move declaration of av_set_program_name and av_program_add_stream_index to a more sensible place.
    Originally committed as revision 15991 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/internal.h
+++ libavformat/internal.h
@@ -22,7 +22,11 @@
 #define AVFORMAT_INTERNAL_H
 
 #include <stdint.h>
+#include "avformat.h"
 
 char *ff_data_to_hex(char *buf, const uint8_t *src, int size);
+
+void av_set_program_name(AVProgram *program, char *provider_name, char *name);
+void av_program_add_stream_index(AVFormatContext *ac, int progid, unsigned int idx);
 
 #endif /* AVFORMAT_INTERNAL_H */
