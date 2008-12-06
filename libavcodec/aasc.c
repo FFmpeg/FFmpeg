56da1fd7d111a81237451baf9b531288b1ad5987
Kostya Shishkov <kostya.shishkov@gmail.com>
2008-12-06 08:57:31 +0000

    AASC can contain raw data in addition to MS-RLE
    Originally committed as revision 16011 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/aasc.c
+++ libavcodec/aasc.c
@@ -62,6 +62,7 @@ static int aasc_decode_frame(AVCodecContext *avctx,
                               const uint8_t *buf, int buf_size)
 {
     AascContext *s = avctx->priv_data;
+    int compr, i, stride;
 
     s->frame.reference = 1;
     s->frame.buffer_hints = FF_BUFFER_HINTS_VALID | FF_BUFFER_HINTS_PRESERVE | FF_BUFFER_HINTS_REUSABLE;
@@ -70,7 +71,24 @@ static int aasc_decode_frame(AVCodecContext *avctx,
         return -1;
     }
 
-    ff_msrle_decode(avctx, &s->frame, 8, buf, buf_size);
+    compr = AV_RL32(buf);
+    buf += 4;
+    buf_size -= 4;
+    switch(compr){
+    case 0:
+        stride = (avctx->width * 3 + 3) & ~3;
+        for(i = avctx->height - 1; i >= 0; i--){
+            memcpy(s->frame.data[0] + i*s->frame.linesize[0], buf, avctx->width*3);
+            buf += stride;
+        }
+        break;
+    case 1:
+        ff_msrle_decode(avctx, &s->frame, 8, buf - 4, buf_size + 4);
+        break;
+    default:
+        av_log(avctx, AV_LOG_ERROR, "Unknown compression type %d\n", compr);
+        return -1;
+    }
 
     *data_size = sizeof(AVFrame);
     *(AVFrame*)data = s->frame;
