00d9d0f5e2429dcf6870b36d0cc24acd9640c6f9
Baptiste Coudurier <baptiste.coudurier@gmail.com>
2008-12-11 01:16:01 +0000

    cosmetics
    Originally committed as revision 16050 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/dnxhdenc.c
+++ libavcodec/dnxhdenc.c
@@ -389,14 +389,10 @@ static av_always_inline void dnxhd_get_pixels_4x8(DCTELEM *restrict block, const
 {
     int i;
     for (i = 0; i < 4; i++) {
-        block[0] = pixels[0];
-        block[1] = pixels[1];
-        block[2] = pixels[2];
-        block[3] = pixels[3];
-        block[4] = pixels[4];
-        block[5] = pixels[5];
-        block[6] = pixels[6];
-        block[7] = pixels[7];
+        block[0] = pixels[0]; block[1] = pixels[1];
+        block[2] = pixels[2]; block[3] = pixels[3];
+        block[4] = pixels[4]; block[5] = pixels[5];
+        block[6] = pixels[6]; block[7] = pixels[7];
         pixels += line_size;
         block += 8;
     }
