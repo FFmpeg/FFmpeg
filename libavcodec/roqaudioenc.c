259037d0997969c339bce52e57a6f54b269c1fb2
Vitor Sessak <vitor1001@gmail.com>
2008-12-11 19:27:53 +0000

    Rename ROQDPCMContext_t to ROQDPCMContext to avoid _t reserved prefix.
    Originally committed as revision 16059 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/roqaudioenc.c
+++ libavcodec/roqaudioenc.c
@@ -35,7 +35,7 @@ static unsigned char dpcmValues[MAX_DPCM];
 typedef struct
 {
     short lastSample[2];
-} ROQDPCMContext_t;
+} ROQDPCMContext;
 
 static av_cold void roq_dpcm_table_init(void)
 {
@@ -51,7 +51,7 @@ static av_cold void roq_dpcm_table_init(void)
 
 static int roq_dpcm_encode_init(AVCodecContext *avctx)
 {
-    ROQDPCMContext_t *context = avctx->priv_data;
+    ROQDPCMContext *context = avctx->priv_data;
 
     if (avctx->channels > 2) {
         av_log(avctx, AV_LOG_ERROR, "Audio must be mono or stereo\n");
@@ -123,7 +123,7 @@ static int roq_dpcm_encode_frame(AVCodecContext *avctx,
     short *in;
     unsigned char *out;
 
-    ROQDPCMContext_t *context = avctx->priv_data;
+    ROQDPCMContext *context = avctx->priv_data;
 
     stereo = (avctx->channels == 2);
 
@@ -169,7 +169,7 @@ AVCodec roq_dpcm_encoder = {
     "roq_dpcm",
     CODEC_TYPE_AUDIO,
     CODEC_ID_ROQ_DPCM,
-    sizeof(ROQDPCMContext_t),
+    sizeof(ROQDPCMContext),
     roq_dpcm_encode_init,
     roq_dpcm_encode_frame,
     roq_dpcm_encode_close,
