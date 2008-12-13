d5574b283995f0744394ade1741cf8da07b45113
Luca Abeni <lucabe72@email.it>
2008-12-13 23:03:34 +0000

    Remove unused types
    Originally committed as revision 16113 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/rtp.h
+++ libavformat/rtp.h
@@ -90,29 +90,4 @@ typedef struct rtp_payload_data_s
     int cur_au_index;
 } rtp_payload_data_t;
 
-#if 0
-typedef enum {
-  RTCP_SR   = 200,
-  RTCP_RR   = 201,
-  RTCP_SDES = 202,
-  RTCP_BYE  = 203,
-  RTCP_APP  = 204
-} rtcp_type_t;
-
-typedef enum {
-  RTCP_SDES_END    =  0,
-  RTCP_SDES_CNAME  =  1,
-  RTCP_SDES_NAME   =  2,
-  RTCP_SDES_EMAIL  =  3,
-  RTCP_SDES_PHONE  =  4,
-  RTCP_SDES_LOC    =  5,
-  RTCP_SDES_TOOL   =  6,
-  RTCP_SDES_NOTE   =  7,
-  RTCP_SDES_PRIV   =  8,
-  RTCP_SDES_IMG    =  9,
-  RTCP_SDES_DOOR   = 10,
-  RTCP_SDES_SOURCE = 11
-} rtcp_sdes_type_t;
-#endif
-
 #endif /* AVFORMAT_RTP_H */
