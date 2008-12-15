6915cf38542828488b1129e6ce6207d5e5699798
Diego Biurrun <diego@biurrun.de>
2008-12-15 22:14:58 +0000

    whitespace cosmetics
    Originally committed as revision 16152 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/xvmc_render.h
+++ libavcodec/xvmc_render.h
@@ -38,37 +38,37 @@
 //                     1337    IDCT MCo
 #define MP_XVMC_RENDER_MAGIC 0x1DC711C0
 
-typedef   struct{
-//these are not changed by the decoder!
-  int  magic;
+typedef   struct  {
+    //these are not changed by the decoder!
+    int  magic;
 
-  short * data_blocks;
-  XvMCMacroBlock * mv_blocks;
-  int total_number_of_mv_blocks;
-  int total_number_of_data_blocks;
-  int mc_type;//XVMC_MPEG1/2/4,XVMC_H263 without XVMC_IDCT
-  int idct;//Do we use IDCT acceleration?
-  int chroma_format;//420, 422, 444
-  int unsigned_intra;//+-128 for intra pictures after clipping
-  XvMCSurface* p_surface;//pointer to rendered surface, never changed
+    short * data_blocks;
+    XvMCMacroBlock * mv_blocks;
+    int total_number_of_mv_blocks;
+    int total_number_of_data_blocks;
+    int mc_type; //XVMC_MPEG1/2/4,XVMC_H263 without XVMC_IDCT
+    int idct; //Do we use IDCT acceleration?
+    int chroma_format; //420, 422, 444
+    int unsigned_intra; //+-128 for intra pictures after clipping
+    XvMCSurface* p_surface; //pointer to rendered surface, never changed
 
-//these are changed by the decoder
-//used by the XvMCRenderSurface function
-  XvMCSurface* p_past_surface;//pointer to the past surface
-  XvMCSurface* p_future_surface;//pointer to the future prediction surface
+    //these are changed by the decoder
+    //used by the XvMCRenderSurface function
+    XvMCSurface* p_past_surface; //pointer to the past surface
+    XvMCSurface* p_future_surface; //pointer to the future prediction surface
 
-  unsigned int picture_structure;//top/bottom fields or frame!
-  unsigned int flags;//XVMC_SECOND_FIELD - 1st or 2nd field in the sequence
-  unsigned int display_flags; //1,2 or 1+2 fields for XvMCPutSurface
+    unsigned int picture_structure; //top/bottom fields or frame!
+    unsigned int flags; //XVMC_SECOND_FIELD - 1st or 2nd field in the sequence
+    unsigned int display_flags; //1,2 or 1+2 fields for XvMCPutSurface
 
-//these are for internal communication
-  int state;//0 - free, 1 - waiting to display, 2 - waiting for prediction
-  int start_mv_blocks_num;//offset in the array for the current slice, updated by vo
-  int filled_mv_blocks_num;//processed mv block in this slice, changed by decoder
+    //these are for internal communication
+    int state; //0 - free, 1 - waiting to display, 2 - waiting for prediction
+    int start_mv_blocks_num; //offset in the array for the current slice, updated by vo
+    int filled_mv_blocks_num; //processed mv block in this slice, changed by decoder
 
-  int next_free_data_block_num;//used in add_mv_block, pointer to next free block
-//extensions
-  void * p_osd_target_surface_render;//pointer to the surface where subpicture is rendered
+    int next_free_data_block_num; //used in add_mv_block, pointer to next free block
+    //extensions
+    void * p_osd_target_surface_render; //pointer to the surface where subpicture is rendered
 
 } xvmc_render_state_t;
 
