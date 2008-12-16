7d2cc552ed8d6423e4b39a40ac754c9ca9f5e3c3
Benoit Fouet <benoit.fouet@free.fr>
2008-12-16 07:56:33 +0000

    Fix warning "format not a string literal and no format arguments".
    Originally committed as revision 16164 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/libamr.c
+++ libavcodec/libamr.c
@@ -81,9 +81,9 @@
 #include <amrnb/interf_enc.h>
 #endif
 
-static const char *nb_bitrate_unsupported =
+static const char nb_bitrate_unsupported[] =
     "bitrate not supported: use one of 4.75k, 5.15k, 5.9k, 6.7k, 7.4k, 7.95k, 10.2k or 12.2k\n";
-static const char *wb_bitrate_unsupported =
+static const char wb_bitrate_unsupported[] =
     "bitrate not supported: use one of 6.6k, 8.85k, 12.65k, 14.25k, 15.85k, 18.25k, 19.85k, 23.05k, or 23.85k\n";
 
 /* Common code for fixed and float version*/
