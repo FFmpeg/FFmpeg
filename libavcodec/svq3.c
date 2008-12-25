ddaf298cee875364dc65f9b87c085a7ff04bea1f
Diego Biurrun <diego@biurrun.de>
2008-12-25 15:49:02 +0000

    Fix sample URL.
    Originally committed as revision 16309 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/svq3.c
+++ libavcodec/svq3.c
@@ -37,7 +37,7 @@
  *
  * You will know you have these parameters passed correctly when the decoder
  * correctly decodes this file:
- *  ftp://ftp.mplayerhq.hu/MPlayer/samples/V-codecs/SVQ3/Vertical400kbit.sorenson3.mov
+ *  http://samples.mplayerhq.hu/V-codecs/SVQ3/Vertical400kbit.sorenson3.mov
  */
 
 #ifdef CONFIG_ZLIB
