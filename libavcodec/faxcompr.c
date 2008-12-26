d68542f019c89e7938297a18da282e3a892718aa
Michael Niedermayer <michaelni@gmx.at>
2008-12-26 14:57:42 +0000

    Ensure that an invalid run terminates the line.
    Originally committed as revision 16331 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/faxcompr.c
+++ libavcodec/faxcompr.c
@@ -136,7 +136,7 @@ static int decode_group3_1d_line(AVCodecContext *avctx, GetBitContext *gb,
             if(pix_left <= 0){
                 if(!pix_left)
                     break;
-                runs[-1] = 0;
+                runs[-1] += pix_left;
                 av_log(avctx, AV_LOG_ERROR, "Run went out of bounds\n");
                 return -1;
             }
