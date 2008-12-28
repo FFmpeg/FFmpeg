5d88c2647def2557637077e9284cfe9806954502
Ronald S. Bultje <rsbultje@gmail.com>
2008-12-28 05:55:09 +0000

    Change audio_pkt_cnt from an array into a single field, since only a single member of the array is ever used (compare to RMDemuxContext->audio_pkt_cnt). See "[PATCH] oops I broke rdt.c" mailinglist thread.
    Originally committed as revision 16367 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/rdt.c
+++ libavformat/rdt.c
@@ -85,7 +85,7 @@ struct PayloadContext {
     uint8_t *mlti_data;
     unsigned int mlti_data_size;
     char buffer[RTP_MAX_PACKET_LENGTH + FF_INPUT_BUFFER_PADDING_SIZE];
-    int audio_pkt_cnt[MAX_STREAMS]; /**< remaining audio packets in rmdec */
+    int audio_pkt_cnt; /**< remaining audio packets in rmdec */
 };
 
 void
@@ -307,8 +307,8 @@ rdt_parse_packet (PayloadContext *rdt, AVStream *st,
         pos = url_ftell(&pb);
         if (res < 0)
             return res;
-        rdt->audio_pkt_cnt[st->id] = res;
-        if (rdt->audio_pkt_cnt[st->id] > 0 &&
+        rdt->audio_pkt_cnt = res;
+        if (rdt->audio_pkt_cnt > 0 &&
             st->codec->codec_id == CODEC_ID_AAC) {
             memcpy (rdt->buffer, buf + pos, len - pos);
             rdt->rmctx->pb = av_alloc_put_byte (rdt->buffer, len - pos, 0,
@@ -316,14 +316,14 @@ rdt_parse_packet (PayloadContext *rdt, AVStream *st,
         }
     } else {
         ff_rm_retrieve_cache (rdt->rmctx, rdt->rmctx->pb, st, rdt->rmst[0], pkt);
-        if (rdt->audio_pkt_cnt[st->id] == 0 &&
+        if (rdt->audio_pkt_cnt == 0 &&
             st->codec->codec_id == CODEC_ID_AAC)
             av_freep(&rdt->rmctx->pb);
     }
     pkt->stream_index = st->index;
     pkt->pts = *timestamp;
 
-    return rdt->audio_pkt_cnt[st->id] > 0;
+    return rdt->audio_pkt_cnt > 0;
 }
 
 int
