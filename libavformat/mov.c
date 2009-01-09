fa50a0276f4e03828e1250f6524b61ef48ca9d15
Baptiste Coudurier <baptiste.coudurier@gmail.com>
2009-01-09 22:13:37 +0000

    only set le if value is 1, fix boom5.mov, issue #770
    Originally committed as revision 16507 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/mov.c
+++ libavformat/mov.c
@@ -575,7 +575,8 @@ static int mov_read_enda(MOVContext *c, ByteIOContext *pb, MOVAtom atom)
     AVStream *st = c->fc->streams[c->fc->nb_streams-1];
     int little_endian = get_be16(pb);
 
-    if (little_endian) {
+    dprintf(c->fc, "enda %d\n", little_endian);
+    if (little_endian == 1) {
         switch (st->codec->codec_id) {
         case CODEC_ID_PCM_S24BE:
             st->codec->codec_id = CODEC_ID_PCM_S24LE;
