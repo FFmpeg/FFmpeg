69b6d53bebbdb25f424b87123eef60de24693cf2
Aurelien Jacobs <aurel@gnuage.org>
2009-02-02 21:32:11 +0000

    cosmetics: whitespace
    Originally committed as revision 16951 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/matroskadec.c
+++ libavformat/matroskadec.c
@@ -871,8 +871,7 @@ static int matroska_decode_buffer(uint8_t** buf, int* buf_size,
     case MATROSKA_TRACK_ENCODING_COMP_LZO:
         do {
             olen = pkt_size *= 3;
-            pkt_data = av_realloc(pkt_data,
-                                  pkt_size+AV_LZO_OUTPUT_PADDING);
+            pkt_data = av_realloc(pkt_data, pkt_size+AV_LZO_OUTPUT_PADDING);
             result = av_lzo1x_decode(pkt_data, &olen, data, &isize);
         } while (result==AV_LZO_OUTPUT_FULL && pkt_size<10000000);
         if (result)
