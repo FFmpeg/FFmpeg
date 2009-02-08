353f87b8d44f84b0e7b242788c4447e7fbd06361
Baptiste Coudurier <baptiste.coudurier@gmail.com>
2009-02-08 06:35:21 +0000

    fix typo in h264dsp_mmx (no effect currently as the function is not used), approved by Dark Shikari on IRC
    Originally committed as revision 17046 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/x86/h264dsp_mmx.c
+++ libavcodec/x86/h264dsp_mmx.c
@@ -535,7 +535,7 @@ static void ff_h264_idct_add16intra_sse2(uint8_t *dst, const int *block_offset, 
 
 static void ff_h264_idct_add8_sse2(uint8_t **dest, const int *block_offset, DCTELEM *block, int stride, const uint8_t nnzc[6*8]){
     int i;
-    for(i=16; i<16+8; i++){
+    for(i=16; i<16+8; i+=2){
         if(nnzc[ scan8[i+0] ]|nnzc[ scan8[i+1] ])
             ff_x264_add8x4_idct_sse2 (dest[(i&4)>>2] + block_offset[i], block + i*16, stride);
         else if(block[i*16]|block[i*16+16])
