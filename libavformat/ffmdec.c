2dc91884652aa586951026dc2d518b9b6957577a
Baptiste Coudurier <baptiste.coudurier@gmail.com>
2009-02-12 02:17:16 +0000

    return error if read failed
    Originally committed as revision 17173 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/ffmdec.c
+++ libavformat/ffmdec.c
@@ -30,7 +30,8 @@ int64_t ffm_read_write_index(int fd)
     uint8_t buf[8];
 
     lseek(fd, 8, SEEK_SET);
-    read(fd, buf, 8);
+    if (read(fd, buf, 8) != 8)
+        return AVERROR(EIO);
     return AV_RB64(buf);
 }
 
