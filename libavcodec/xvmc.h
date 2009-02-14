6785e509d45a7ccb15fd9485b93932e077c7aaee
Ivan Kalvachev <ikalvachev@gmail.com>
2009-02-14 17:56:51 +0000

    doxygen style comments
    Originally committed as revision 17251 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/xvmc.h
+++ libavcodec/xvmc.h
@@ -25,10 +25,15 @@
 
 #include "avcodec.h"
 
-#define AV_XVMC_STATE_DISPLAY_PENDING          1  // the surface should be shown, the video driver manipulates this
-#define AV_XVMC_STATE_PREDICTION               2  // the surface is needed for prediction, the codec manipulates this
-#define AV_XVMC_STATE_OSD_SOURCE               4  // this surface is needed for subpicture rendering
-#define AV_XVMC_RENDER_MAGIC          0x1DC711C0  // 1337 IDCT MCo
+/**the surface should be shown, the video driver manipulates this*/
+#define AV_XVMC_STATE_DISPLAY_PENDING 1
+/**the surface is needed for prediction, the codec manipulates this*/
+#define AV_XVMC_STATE_PREDICTION      2
+/**this surface is needed for subpicture rendering*/
+#define AV_XVMC_STATE_OSD_SOURCE      4
+/**<magic value to ensure that regular pixel routines haven't corrupted the struct*/
+//                              1337    IDCT MCo
+#define AV_XVMC_RENDER_MAGIC          0x1DC711C0
 
 #if LIBAVCODEC_VERSION_MAJOR < 53
 #define MP_XVMC_STATE_DISPLAY_PENDING AV_XVMC_STATE_DISPLAY_PENDING
@@ -38,36 +43,44 @@
 #endif
 
 struct xvmc_render_state {
-    //these are not changed by the decoder!
-    int             magic;                        ///< used as check for memory corruption by regular pixel routines
+/**  these are not changed by the decoder! */
+//@{
+    int             magic;              ///< used as check for memory corruption by regular pixel routines
 
     short*          data_blocks;
     XvMCMacroBlock* mv_blocks;
     int             total_number_of_mv_blocks;
     int             total_number_of_data_blocks;
-    int             mc_type;                      //   XVMC_MPEG1/2/4,XVMC_H263 without XVMC_IDCT
-    int             idct;                         ///< indicate that IDCT acceleration level is used
-    int             chroma_format;                ///< XVMC_CHROMA_FORMAT_420/422/444
-    int             unsigned_intra;               //   +-128 for intra pictures after clipping
-    XvMCSurface*    p_surface;                    //   pointer to rendered surface, never changed
+    int             mc_type;            ///<XVMC_MPEG1/2/4,XVMC_H263 without XVMC_IDCT
+    int             idct;               ///< indicate that IDCT acceleration level is used
+    int             chroma_format;      ///< XVMC_CHROMA_FORMAT_420/422/444
+    int             unsigned_intra;     ///<+-128 for intra pictures after clipping
+    XvMCSurface*    p_surface;          ///<pointer to rendered surface, never changed
+//}@
 
-    //these are changed by the decoder
-    //used by the XvMCRenderSurface function
-    XvMCSurface*    p_past_surface;               // pointer to the past surface
-    XvMCSurface*    p_future_surface;             // pointer to the future prediction surface
+/** these are changed by the decoder
+    //used by the XvMCRenderSurface function */
+//@{
+    XvMCSurface* p_past_surface;    ///<pointer to the past surface
+    XvMCSurface* p_future_surface;  ///<pointer to the future prediction surface
 
-    unsigned int    picture_structure;            // top/bottom fields or frame!
-    unsigned int    flags;                        // XVMC_SECOND_FIELD - 1st or 2nd field in the sequence
-    unsigned int    display_flags;                // 1,2 or 1+2 fields for XvMCPutSurface
+    unsigned int picture_structure; ///<top/bottom fields or frame!
+    unsigned int flags;             ///<XVMC_SECOND_FIELD - 1st or 2nd field in the sequence
+    unsigned int display_flags;     ///<1,2 or 1+2 fields for XvMCPutSurface
+//}@
 
-    //these are for internal communication
-    int             state;                        // 0 - free, 1 - waiting to display, 2 - waiting for prediction
-    int             start_mv_blocks_num;          // offset in the array for the current slice, updated by vo
-    int             filled_mv_blocks_num;         // processed mv block in this slice, changed by decoder
+/** these are for internal communication */
+//@{
+    int state;                      ///<0 - free, 1 - waiting to display, 2 - waiting for prediction
+    int start_mv_blocks_num;        ///<offset in the array for the current slice, updated by vo
+    int filled_mv_blocks_num;       ///<processed mv block in this slice, changed by decoder
 
-    int             next_free_data_block_num;     // used in add_mv_block, pointer to next free block
-    //extensions
-    void*           p_osd_target_surface_render;  // pointer to the surface where subpicture is rendered
+    int next_free_data_block_num;   ///<used in add_mv_block, pointer to next free block
+//}@
+/**extensions*/
+//@{
+    void * p_osd_target_surface_render; ///<pointer to the surface where subpicture is rendered
+//}@
 
 };
 
