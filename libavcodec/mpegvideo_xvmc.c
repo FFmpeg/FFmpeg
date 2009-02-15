967d2ae616a1fc6ffc2c00810e09c232dfdb7911
Diego Biurrun <diego@biurrun.de>
2009-02-15 01:24:09 +0000

    Remove redundant assert(render) in ff_xvmc_init_block().  The immediately following if condition checks render and does an assert if it fails.
    Originally committed as revision 17304 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/mpegvideo_xvmc.c
+++ libavcodec/mpegvideo_xvmc.c
@@ -38,7 +38,6 @@
 void ff_xvmc_init_block(MpegEncContext *s)
 {
     struct xvmc_render_state *render = (struct xvmc_render_state*)s->current_picture.data[2];
-    assert(render);
     if (!render || render->magic != AV_XVMC_RENDER_MAGIC) {
         assert(0);
         return; // make sure that this is a render packet
