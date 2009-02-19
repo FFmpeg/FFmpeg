528374980e1109f220a7a43f296fddb80eb63d0d
Alex Converse <alex.converse@gmail.com>
2009-02-19 23:09:24 +0000

    Add Divio MPEG-4 FOURCC (M4CC)
    Originally committed as revision 17465 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/riff.c
+++ libavformat/riff.c
@@ -73,6 +73,7 @@ const AVCodecTag codec_bmp_tags[] = {
     { CODEC_ID_MPEG4,        MKTAG('I', 'N', 'M', 'C') },
     { CODEC_ID_MPEG4,        MKTAG('E', 'P', 'H', 'V') }, /* Ephv MPEG-4 */
     { CODEC_ID_MPEG4,        MKTAG('E', 'M', '4', 'A') },
+    { CODEC_ID_MPEG4,        MKTAG('M', '4', 'C', 'C') }, /* Divio MPEG-4 */
     { CODEC_ID_MSMPEG4V3,    MKTAG('D', 'I', 'V', '3') }, /* default signature when using MSMPEG4 */
     { CODEC_ID_MSMPEG4V3,    MKTAG('M', 'P', '4', '3') },
     { CODEC_ID_MSMPEG4V3,    MKTAG('M', 'P', 'G', '3') },
