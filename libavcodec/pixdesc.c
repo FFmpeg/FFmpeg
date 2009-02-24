9b61d838f643f780b2597442a63e6c5b98ee8821
Michael Niedermayer <michaelni@gmx.at>
2009-02-24 01:08:29 +0000

    Pixel Format descriptor, try #1. (yes its intentionally unused, this is intended as a basis for further  work, and to prevent repeation of the whole discussion dying and the  results being forgotten)
    Originally committed as revision 17548 to svn://svn.ffmpeg.org/ffmpeg/trunk

---
+++ libavcodec/pixdesc.c
@@ -0,0 +1,187 @@
+/*
+ * Pixel Format descriptor
+ * Copyright (c) 2009 Michael Niedermayer <michaelni@gmx.at>
+ *
+ * This file is part of FFmpeg.
+ *
+ * FFmpeg is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * FFmpeg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FFmpeg; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#include "libavutil/pixfmt.h"
+#include "pixdesc.h"
+
+static const AVPixFmtDescriptor pix_fmt_desc[PIX_FMT_NB] = {
+    [PIX_FMT_YUV422P] = {
+        .nb_channels  = 3,
+        .log2_chroma_w= 1,
+        .log2_chroma_h= 0,
+        .comp = {
+            {0,0,1,0,7},
+            {1,0,1,0,7},
+            {2,0,1,0,7},
+        },
+    },
+    [PIX_FMT_YUV420P] = {
+        .nb_channels  = 3,
+        .log2_chroma_w= 1,
+        .log2_chroma_h= 1,
+        .comp = {
+            {0,0,1,0,7},
+            {1,0,1,0,7},
+            {2,0,1,0,7},
+        },
+    },
+    [PIX_FMT_YUV410P] = {
+        .nb_channels  = 3,
+        .log2_chroma_w= 2,
+        .log2_chroma_h= 2,
+        .comp = {
+            {0,0,1,0,7},
+            {1,0,1,0,7},
+            {2,0,1,0,7},
+        },
+    },
+    [PIX_FMT_NV12] = {
+        .nb_channels  = 3,
+        .log2_chroma_w= 1,
+        .log2_chroma_h= 1,
+        .comp = {
+            {0,0,1,0,7},
+            {1,1,1,0,7},
+            {1,1,2,0,7},
+        },
+    },
+    [PIX_FMT_YUYV422] = {
+        .nb_channels  = 3,
+        .log2_chroma_w= 1,
+        .log2_chroma_h= 0,
+        .comp = {
+            {0,1,1,0,7},
+            {0,3,2,0,7},
+            {0,3,4,0,7},
+        },
+    },
+    [PIX_FMT_UYVY422] = {
+        .nb_channels  = 3,
+        .log2_chroma_w= 1,
+        .log2_chroma_h= 0,
+        .comp = {
+            {0,1,2,0,7},
+            {0,3,1,0,7},
+            {0,3,3,0,7},
+        },
+    },
+    [PIX_FMT_GRAY16LE] = {
+        .nb_channels  = 1,
+        .log2_chroma_w= 0,
+        .log2_chroma_h= 0,
+        .comp = {
+            {0,1,1,0,15},
+        },
+    },
+    [PIX_FMT_GRAY16BE] = {
+        .nb_channels  = 1,
+        .log2_chroma_w= 0,
+        .log2_chroma_h= 0,
+        .comp = {
+            {0,1,1,0,15},
+        },
+        .flags = PIX_FMT_BE,
+    },
+    [PIX_FMT_RGB24] = {
+        .nb_channels  = 3,
+        .log2_chroma_w= 0,
+        .log2_chroma_h= 0,
+        .comp = {
+            {0,2,1,0,7},
+            {0,2,2,0,7},
+            {0,2,3,0,7},
+        },
+    },
+    [PIX_FMT_RGBA] = {
+        .nb_channels  = 4,
+        .log2_chroma_w= 0,
+        .log2_chroma_h= 0,
+        .comp = {
+            {0,3,1,0,7},
+            {0,3,2,0,7},
+            {0,3,3,0,7},
+            {0,3,4,0,7},
+        },
+    },
+    [PIX_FMT_RGB48LE] = {
+        .nb_channels  = 3,
+        .log2_chroma_w= 0,
+        .log2_chroma_h= 0,
+        .comp = {
+            {0,5,1,0,15},
+            {0,5,3,0,15},
+            {0,5,5,0,15},
+        },
+    },
+    [PIX_FMT_RGB48BE] = {
+        .nb_channels  = 3,
+        .log2_chroma_w= 0,
+        .log2_chroma_h= 0,
+        .comp = {
+            {0,5,1,0,15},
+            {0,5,3,0,15},
+            {0,5,5,0,15},
+        },
+        .flags = PIX_FMT_BE,
+    },
+//FIXME chaneg pix fmt defines so that we have a LE & BE instead of a Native endian
+#if 0
+    [PIX_FMT_RGB565_LE] = {
+        .nb_channels  = 3,
+        .log2_chroma_w= 0,
+        .log2_chroma_h= 0,
+        .comp = {
+            {0,1,1,0,4},
+            {0,1,1,5,5},
+            {0,1,2,3,4},
+        },
+    },
+    [PIX_FMT_RGB565_BE] = {
+        .nb_channels  = 3,
+        .log2_chroma_w= 0,
+        .log2_chroma_h= 0,
+        .comp = {
+            {0,1,1, 0,4},
+            {0,1,1, 5,5},
+            {0,1,0, 3,4},
+        },
+        .flags = PIX_FMT_BE,
+    },
+#endif
+    [PIX_FMT_MONOBLACK] = {
+        .nb_channels  = 1,
+        .log2_chroma_w= 0,
+        .log2_chroma_h= 0,
+        .comp = {
+            {0,0,1,7,0},
+        },
+        .flags = PIX_FMT_BITSTREAM,
+    },
+    [PIX_FMT_PAL8] = {
+        .nb_channels  = 1,
+        .log2_chroma_w= 0,
+        .log2_chroma_h= 0,
+        .comp = {
+            {0,0,1,0,7},
+        },
+        .flags = PIX_FMT_PAL,
+    },
+};
---
+++ libavcodec/pixdesc.h
@@ -0,0 +1,91 @@
+/*
+ * Pixel Format descriptor
+ * Copyright (c) 2009 Michael Niedermayer <michaelni@gmx.at>
+ *
+ * This file is part of FFmpeg.
+ *
+ * FFmpeg is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * FFmpeg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FFmpeg; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#include <inttypes.h>
+
+#include "libavutil/intreadwrite.h"
+
+typedef struct AVComponentDescriptor{
+    uint16_t plane        :2;            ///< which of the 4 planes contains the component
+    uint16_t step_minus1  :3;            ///< number of bytes between 2 horizontally consecutive pixels minus 1
+    uint16_t offset_plus1 :3;            ///< number of bytes before the component of the first pixel plus 1
+    uint16_t shift        :3;            ///< number of lsb that must be shifted away to get the value
+    uint16_t depth_minus1 :4;            ///< number of bits in the component minus 1
+}AVComponentDescriptor;
+
+typedef struct AVPixFmtDescriptor{
+    uint8_t nb_channels;        ///< The number of components each pixel has, (1-4)
+
+    /**
+     * Amount to shift the luma width right to find the chroma width, this is for
+     * example 1 for YV12
+     * chroma_width = -((-luma_width )>>log2_chroma_w)
+     * note above is needed to ensure rounding up
+     */
+    uint8_t log2_chroma_w;      ///< chroma_width = -((-luma_width )>>log2_chroma_w)
+
+    /**
+     * Amount to shift the luma height right to find the chroma height, this is for
+     * example 1 for YV12
+     * chroma_height= -((-luma_height)>>log2_chroma_h)
+     * note above is needed to ensure rounding up
+     */
+    uint8_t log2_chroma_h;
+    uint8_t flags;
+    AVComponentDescriptor comp[4]; ///< parameters that describes how pixels are packed
+}AVPixFmtDescriptor;
+
+#define PIX_FMT_BE        1 ///< Big endian
+#define PIX_FMT_PAL       2 ///< Pixel format has a palette i data[1], values are indexes in this palette
+#define PIX_FMT_BITSTREAM 4 ///< All values of a component are bitwise packed end to end
+
+
+static inline void read_line(uint16_t *dst, const uint8_t *data[4], const int linesize[4], AVPixFmtDescriptor *desc, int x, int y, int c, int w)
+{
+    AVComponentDescriptor comp= desc->comp[c];
+    int plane= comp.plane;
+    int depth= comp.depth_minus1+1;
+    int mask = (1<<depth)-1;
+    int shift= comp.shift;
+    int step = comp.step_minus1+1;
+    int flags= desc->flags;
+    const uint8_t *p= data[plane]+y*linesize[plane] + x * step + comp.offset_plus1 - 1;
+
+    //FIXME initial x in case of PIX_FMT_BITSTREAM is wrong
+
+    while(w--){
+        int val;
+        if(flags & PIX_FMT_BE) val= AV_RB16(p);
+        else                   val= AV_RL16(p);
+        val = (val>>shift) & mask;
+        if(flags & PIX_FMT_PAL)
+            val= data[1][4*val + c];
+        if(flags & PIX_FMT_BITSTREAM){
+            shift-=depth;
+            while(shift<0){
+                shift+=8;
+                p++;
+            }
+        }else
+            p+= step;
+        *dst++= val;
+    }
+}
