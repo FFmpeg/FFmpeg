7f123e7f8ad2cfd4ab7e2fbd83e17d0a8b436785
Michael Niedermayer <michaelni@gmx.at>
2009-02-24 13:35:54 +0000

    Add CODEC_ID_H264 to tb_unreliable(), it belongs there for the same reason as mpeg2. (telecine amongth others)
    Originally committed as revision 17551 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/utils.c
+++ libavformat/utils.c
@@ -1976,7 +1976,9 @@ static int tb_unreliable(AVCodecContext *c){
        || c->time_base.den <    5L*c->time_base.num
 /*       || c->codec_tag == AV_RL32("DIVX")
        || c->codec_tag == AV_RL32("XVID")*/
-       || c->codec_id == CODEC_ID_MPEG2VIDEO)
+       || c->codec_id == CODEC_ID_MPEG2VIDEO
+       || c->codec_id == CODEC_ID_H264
+       )
         return 1;
     return 0;
 }
