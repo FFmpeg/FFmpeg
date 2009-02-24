cd3356ff73bb9ed2ee0190a1d240adcb6d5e9864
Stefan Gehrer <stefan.gehrer@gmx.de>
2009-02-24 15:56:53 +0000

    check for a stuffing byte at the end of a slice
    Originally committed as revision 17559 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/cavsdec.c
+++ libavcodec/cavsdec.c
@@ -439,6 +439,9 @@ static inline int check_for_slice(AVSContext *h) {
     if(h->mbx)
         return 0;
     align = (-get_bits_count(gb)) & 7;
+    /* check for stuffing byte */
+    if(!align && (show_bits(gb,8) == 0x80))
+        get_bits(gb,8);
     if((show_bits_long(gb,24+align) & 0xFFFFFF) == 0x000001) {
         skip_bits_long(gb,24+align);
         h->stc = get_bits(gb,8);
