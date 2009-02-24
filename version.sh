d33a14e4f55605a067c259372e8c88a7344b0828
Diego Biurrun <diego@biurrun.de>
2009-02-24 22:54:58 +0000

    cosmetics: Use $() instead of ``, the former can be nested more easily.
    Originally committed as revision 17576 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- version.sh
+++ version.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 # check for SVN revision number
-revision=`cd "$1" && LC_ALL=C svn info 2> /dev/null | grep Revision | cut -d' ' -f2`
-test $revision || revision=`cd "$1" && grep revision .svn/entries 2>/dev/null | cut -d '"' -f2`
-test $revision || revision=`cd "$1" && sed -n -e '/^dir$/{n;p;q}' .svn/entries 2>/dev/null`
+revision=$(cd "$1" && LC_ALL=C svn info 2> /dev/null | grep Revision | cut -d' ' -f2)
+test $revision || revision=$(cd "$1" && grep revision .svn/entries 2>/dev/null | cut -d '"' -f2)
+test $revision || revision=$(cd "$1" && sed -n -e '/^dir$/{n;p;q}' .svn/entries 2>/dev/null)
 test $revision && revision=SVN-r$revision
 
 # check for git short hash
 if ! test $revision; then
-    revision=`cd "$1" && git log -1 --pretty=format:%h`
+    revision=$(cd "$1" && git log -1 --pretty=format:%h)
     test $revision && revision=git-$revision
 fi
 
@@ -18,7 +18,7 @@ test $revision || revision=UNKNOWN
 test -n "$3" && revision=$revision-$3
 
 NEW_REVISION="#define FFMPEG_VERSION \"$revision\""
-OLD_REVISION=`cat version.h 2> /dev/null`
+OLD_REVISION=$(cat version.h 2> /dev/null)
 
 # Update version.h only on revision changes to avoid spurious rebuilds
 if test "$NEW_REVISION" != "$OLD_REVISION"; then
