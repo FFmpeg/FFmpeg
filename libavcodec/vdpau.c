f90b55166e6a6d06d06fa1ba63f94f3d292cfd81
NVIDIA Corporation <>
2009-02-26 00:29:01 +0000

    The field rangered in VDPAU's VdpPictureInfoVC1 now also contains rangeredfrm (VC-1 7.1.13) in bit 1.
    Patch by NVIDIA
    
    Originally committed as revision 17595 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/vdpau.c
+++ libavcodec/vdpau.c
@@ -270,7 +270,7 @@ void ff_vdpau_vc1_decode_picture(MpegEncContext *s, const uint8_t *buf,
     /* Specific to simple/main profile only */
     render->info.vc1.multires           = v->multires;
     render->info.vc1.syncmarker         = v->s.resync_marker;
-    render->info.vc1.rangered           = v->rangered;
+    render->info.vc1.rangered           = v->rangered | (v->rangeredfrm << 1);
     render->info.vc1.maxbframes         = v->s.max_b_frames;
 
     render->info.vc1.deblockEnable      = v->postprocflag & 1;
