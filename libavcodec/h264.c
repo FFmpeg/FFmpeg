19df37a8a6a442f93518f891ad5d474e27f11b13
Michael Niedermayer <michaelni@gmx.at>
2009-02-27 14:01:32 +0000

    Fix SVQ3 regression since ticks_per_frame=2.
    Originally committed as revision 17640 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/h264.c
+++ libavcodec/h264.c
@@ -2201,7 +2201,8 @@ static av_cold int decode_init(AVCodecContext *avctx){
     h->sei_dpb_output_delay = 0;
     h->sei_cpb_removal_delay = -1;
     h->sei_buffering_period_present = 0;
-    avctx->ticks_per_frame = 2;
+    if(avctx->codec_id == CODEC_ID_H264)
+        avctx->ticks_per_frame = 2;
     return 0;
 }
 
