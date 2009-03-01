d819b41ba18efdf468bfc7dc52928b534576ce13
Reimar Döffinger <Reimar.Doeffinger@gmx.de>
2009-03-01 09:14:20 +0000

    Remove unused variable in mpeg_mc_decode_init
    Originally committed as revision 17676 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/mpeg12.c
+++ libavcodec/mpeg12.c
@@ -2508,8 +2508,6 @@ AVCodec mpegvideo_decoder = {
 
 #if CONFIG_MPEG_XVMC_DECODER
 static av_cold int mpeg_mc_decode_init(AVCodecContext *avctx){
-    Mpeg1Context *s;
-
     if( avctx->thread_count > 1)
         return -1;
     if( !(avctx->slice_flags & SLICE_FLAG_CODED_ORDER) )
@@ -2518,7 +2516,6 @@ static av_cold int mpeg_mc_decode_init(AVCodecContext *avctx){
         dprintf(avctx, "mpeg12.c: XvMC decoder will work better if SLICE_FLAG_ALLOW_FIELD is set\n");
     }
     mpeg_decode_init(avctx);
-    s = avctx->priv_data;
 
     avctx->pix_fmt = PIX_FMT_XVMC_MPEG2_IDCT;
     avctx->xvmc_acceleration = 2;//2 - the blocks are packed!
