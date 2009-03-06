7d87e2ceacc551dc1510282824c97ea457fd655d
Alex Converse <alex.converse@gmail.com>
2009-03-06 22:37:21 +0000

    Re-indent after last commit.
    Originally committed as revision 17861 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/aac.c
+++ libavcodec/aac.c
@@ -1598,13 +1598,13 @@ static int parse_adts_frame_header(AACContext * ac, GetBitContext * gb) {
         ac->m4ac.sample_rate     = hdr_info.sample_rate;
         ac->m4ac.sampling_index  = hdr_info.sampling_index;
         ac->m4ac.object_type     = hdr_info.object_type;
-    if (hdr_info.num_aac_frames == 1) {
-        if (!hdr_info.crc_absent)
-            skip_bits(gb, 16);
-    } else {
-        ff_log_missing_feature(ac->avccontext, "More than one AAC RDB per ADTS frame is", 0);
-        return -1;
-    }
+        if (hdr_info.num_aac_frames == 1) {
+            if (!hdr_info.crc_absent)
+                skip_bits(gb, 16);
+        } else {
+            ff_log_missing_feature(ac->avccontext, "More than one AAC RDB per ADTS frame is", 0);
+            return -1;
+        }
     }
     return size;
 }
