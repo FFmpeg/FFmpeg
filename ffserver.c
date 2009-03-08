49ceb58bf687f676e0176567bb881c30d32803bc
Michael Niedermayer <michaelni@gmx.at>
2009-03-08 14:33:30 +0000

    Bump major version of libavutil due to FIFO fixes. The current API/ABI should not be considered stable yet, further ABI/API breakage is possible without major bumps.
    Originally committed as revision 17871 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- ffserver.c
+++ ffserver.c
@@ -354,7 +354,7 @@ static void http_av_log(void *ptr, int level, const char *fmt, va_list vargs)
 {
     static int print_prefix = 1;
     AVClass *avc = ptr ? *(AVClass**)ptr : NULL;
-    if (level > av_log_level)
+    if (level > av_log_get_level())
         return;
     if (print_prefix && avc)
         http_log("[%s @ %p]", avc->item_name(ptr), ptr);
