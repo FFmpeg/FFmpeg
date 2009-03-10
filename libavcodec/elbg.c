e40ee25c4943af8d3fbf0bf47109473d5a2ba8d1
Vitor Sessak <vitor1001@gmail.com>
2009-03-10 17:38:16 +0000

    Fix bug when elbg->utility_inc[elbg->numCB-1] == 1
    Originally committed as revision 17926 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/elbg.c
+++ libavcodec/elbg.c
@@ -105,7 +105,7 @@ static int get_high_utility_cell(elbg_data *elbg)
 {
     int i=0;
     /* Using linear search, do binary if it ever turns to be speed critical */
-    int r = av_lfg_get(elbg->rand_state)%(elbg->utility_inc[elbg->numCB-1]-1) + 1;
+    int r = av_lfg_get(elbg->rand_state)%elbg->utility_inc[elbg->numCB-1] + 1;
     while (elbg->utility_inc[i] < r)
         i++;
 
