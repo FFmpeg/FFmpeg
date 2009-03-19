8e3d8a82e6eb8ef37daecddf651fe6cdadaab7e8
Michael Niedermayer <michaelni@gmx.at>
2009-03-19 14:49:47 +0000

    typedef int x86_reg on non x86
    Originally committed as revision 18043 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavutil/x86_cpu.h
+++ libavutil/x86_cpu.h
@@ -62,6 +62,8 @@ typedef int32_t x86_reg;
 #    define REGc    ecx
 #    define REGd    edx
 #    define REGSP   esp
+#else
+typedef int x86_reg;
 #endif
 
 #define HAVE_7REGS (ARCH_X86_64 || (HAVE_EBX_AVAILABLE && HAVE_EBP_AVAILABLE))
