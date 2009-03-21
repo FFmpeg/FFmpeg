b4800b8b7dfba22117d8edd02164b00c83ae3753
Baptiste Coudurier <baptiste.coudurier@gmail.com>
2009-03-21 00:50:51 +0000

    protect realloc overflow
    Originally committed as revision 18088 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/mxfdec.c
+++ libavformat/mxfdec.c
@@ -364,6 +364,8 @@ static int mxf_read_primer_pack(MXFContext *mxf)
 
 static int mxf_add_metadata_set(MXFContext *mxf, void *metadata_set)
 {
+    if (mxf->metadata_sets_count+1 >= UINT_MAX / sizeof(*mxf->metadata_sets))
+        return AVERROR(ENOMEM);
     mxf->metadata_sets = av_realloc(mxf->metadata_sets, (mxf->metadata_sets_count + 1) * sizeof(*mxf->metadata_sets));
     if (!mxf->metadata_sets)
         return -1;
