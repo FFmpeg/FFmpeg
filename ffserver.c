85fe4ae0d0fbae4e57fc5a060c1e99fa11f42d67
Baptiste Coudurier <baptiste.coudurier@gmail.com>
2009-03-21 09:58:07 +0000

    do not call av_find_stream_info if stream is ffm, should fix stalling
    Originally committed as revision 18104 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- ffserver.c
+++ ffserver.c
@@ -2002,7 +2002,7 @@ static int open_input_stream(HTTPContext *c, const char *info)
     }
     s->flags |= AVFMT_FLAG_GENPTS;
     c->fmt_in = s;
-    if (av_find_stream_info(c->fmt_in) < 0) {
+    if (strcmp(s->iformat->name, "ffm") && av_find_stream_info(c->fmt_in) < 0) {
         http_log("Could not find stream info '%s'\n", input_filename);
         av_close_input_file(s);
         return -1;
