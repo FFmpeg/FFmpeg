168fffdf0194989d4c8a01d89b8ef2fbdd90e287
Benoit Fouet <benoit.fouet@free.fr>
2009-03-31 14:00:46 +0000

    Fix warnings in tree.c test code.
    Originally committed as revision 18267 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavutil/tree.c
+++ libavutil/tree.c
@@ -167,19 +167,20 @@ static void print(AVTreeNode *t, int depth){
     int i;
     for(i=0; i<depth*4; i++) av_log(NULL, AV_LOG_ERROR, " ");
     if(t){
-        av_log(NULL, AV_LOG_ERROR, "Node %p %2d %4d\n", t, t->state, t->elem);
+        av_log(NULL, AV_LOG_ERROR, "Node %p %2d %p\n", t, t->state, t->elem);
         print(t->child[0], depth+1);
         print(t->child[1], depth+1);
     }else
         av_log(NULL, AV_LOG_ERROR, "NULL\n");
 }
 
-static int cmp(const void *a, const void *b){
-    return a-b;
+static int cmp(void *a, const void *b){
+    return (uint8_t*)a-(const uint8_t*)b;
 }
 
 int main(void){
-    int i,k;
+    int i;
+    void *k;
     AVTreeNode *root= NULL, *node=NULL;
     AVLFG prn;
 
