bcbd9a149e85d21270f28e4fef1239f28885c57d
Stefano Sabatini <stefano.sabatini-lala@poste.it>
2009-03-31 18:52:50 +0000

    Cosmetics: reorder the pixel format descriptors to make them match the same order in pixfmt.h.
    Originally committed as revision 18281 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/pixdesc.c
+++ libavcodec/pixdesc.c
@@ -23,22 +23,44 @@
 #include "pixdesc.h"
 
 const AVPixFmtDescriptor av_pix_fmt_descriptors[PIX_FMT_NB] = {
-    [PIX_FMT_YUV422P] = {
-        .name = "yuv422p",
+    [PIX_FMT_YUV420P] = {
+        .name = "yuv420p",
         .nb_channels  = 3,
         .log2_chroma_w= 1,
-        .log2_chroma_h= 0,
+        .log2_chroma_h= 1,
         .comp = {
             {0,0,1,0,7},        /* Y */
             {1,0,1,0,7},        /* U */
             {2,0,1,0,7},        /* V */
         },
     },
-    [PIX_FMT_YUV420P] = {
-        .name = "yuv420p",
+    [PIX_FMT_YUYV422] = {
+        .name = "yuyv422",
         .nb_channels  = 3,
         .log2_chroma_w= 1,
-        .log2_chroma_h= 1,
+        .log2_chroma_h= 0,
+        .comp = {
+            {0,1,1,0,7},        /* Y */
+            {0,3,2,0,7},        /* U */
+            {0,3,4,0,7},        /* V */
+        },
+    },
+    [PIX_FMT_RGB24] = {
+        .name = "rgb24",
+        .nb_channels  = 3,
+        .log2_chroma_w= 0,
+        .log2_chroma_h= 0,
+        .comp = {
+            {0,2,1,0,7},        /* R */
+            {0,2,2,0,7},        /* G */
+            {0,2,3,0,7},        /* B */
+        },
+    },
+    [PIX_FMT_YUV422P] = {
+        .name = "yuv422p",
+        .nb_channels  = 3,
+        .log2_chroma_w= 1,
+        .log2_chroma_h= 0,
         .comp = {
             {0,0,1,0,7},        /* Y */
             {1,0,1,0,7},        /* U */
@@ -55,127 +77,6 @@ const AVPixFmtDescriptor av_pix_fmt_descriptors[PIX_FMT_NB] = {
             {1,0,1,0,7},        /* U */
             {2,0,1,0,7},        /* V */
         },
-    },
-    [PIX_FMT_NV12] = {
-        .name = "nv12",
-        .nb_channels  = 3,
-        .log2_chroma_w= 1,
-        .log2_chroma_h= 1,
-        .comp = {
-            {0,0,1,0,7},        /* Y */
-            {1,1,1,0,7},        /* U */
-            {1,1,2,0,7},        /* V */
-        },
-    },
-    [PIX_FMT_YUYV422] = {
-        .name = "yuyv422",
-        .nb_channels  = 3,
-        .log2_chroma_w= 1,
-        .log2_chroma_h= 0,
-        .comp = {
-            {0,1,1,0,7},        /* Y */
-            {0,3,2,0,7},        /* U */
-            {0,3,4,0,7},        /* V */
-        },
-    },
-    [PIX_FMT_UYVY422] = {
-        .name = "uyvy422",
-        .nb_channels  = 3,
-        .log2_chroma_w= 1,
-        .log2_chroma_h= 0,
-        .comp = {
-            {0,1,2,0,7},        /* Y */
-            {0,3,1,0,7},        /* U */
-            {0,3,3,0,7},        /* V */
-        },
-    },
-    [PIX_FMT_GRAY16LE] = {
-        .name = "gray16le",
-        .nb_channels  = 1,
-        .log2_chroma_w= 0,
-        .log2_chroma_h= 0,
-        .comp = {
-            {0,1,1,0,15},       /* Y */
-        },
-    },
-    [PIX_FMT_GRAY16BE] = {
-        .name = "gray16be",
-        .nb_channels  = 1,
-        .log2_chroma_w= 0,
-        .log2_chroma_h= 0,
-        .comp = {
-            {0,1,1,0,15},       /* Y */
-        },
-        .flags = PIX_FMT_BE,
-    },
-    [PIX_FMT_RGB24] = {
-        .name = "rgb24",
-        .nb_channels  = 3,
-        .log2_chroma_w= 0,
-        .log2_chroma_h= 0,
-        .comp = {
-            {0,2,1,0,7},        /* R */
-            {0,2,2,0,7},        /* G */
-            {0,2,3,0,7},        /* B */
-        },
-    },
-    [PIX_FMT_RGBA] = {
-        .name = "rgba",
-        .nb_channels  = 4,
-        .log2_chroma_w= 0,
-        .log2_chroma_h= 0,
-        .comp = {
-            {0,3,1,0,7},        /* R */
-            {0,3,2,0,7},        /* G */
-            {0,3,3,0,7},        /* B */
-            {0,3,4,0,7},        /* A */
-        },
-    },
-    [PIX_FMT_RGB48LE] = {
-        .name = "rgb48le",
-        .nb_channels  = 3,
-        .log2_chroma_w= 0,
-        .log2_chroma_h= 0,
-        .comp = {
-            {0,5,1,0,15},       /* R */
-            {0,5,3,0,15},       /* G */
-            {0,5,5,0,15},       /* B */
-        },
-    },
-    [PIX_FMT_RGB48BE] = {
-        .name = "rgb48be",
-        .nb_channels  = 3,
-        .log2_chroma_w= 0,
-        .log2_chroma_h= 0,
-        .comp = {
-            {0,5,1,0,15},       /* R */
-            {0,5,3,0,15},       /* G */
-            {0,5,5,0,15},       /* B */
-        },
-        .flags = PIX_FMT_BE,
-    },
-    [PIX_FMT_RGB565LE] = {
-        .name = "rgb565le",
-        .nb_channels  = 3,
-        .log2_chroma_w= 0,
-        .log2_chroma_h= 0,
-        .comp = {
-            {0,1,2,3,4},        /* R */
-            {0,1,1,5,5},        /* G */
-            {0,1,1,0,4},        /* B */
-        },
-    },
-    [PIX_FMT_RGB565BE] = {
-        .name = "rgb565be",
-        .nb_channels  = 3,
-        .log2_chroma_w= 0,
-        .log2_chroma_h= 0,
-        .comp = {
-            {0,1,0,3,4},        /* R */
-            {0,1,1,5,5},        /* G */
-            {0,1,1,0,4},        /* B */
-        },
-        .flags = PIX_FMT_BE,
     },
     [PIX_FMT_MONOBLACK] = {
         .name = "monoblack",
@@ -196,5 +97,104 @@ const AVPixFmtDescriptor av_pix_fmt_descriptors[PIX_FMT_NB] = {
             {0,0,1,0,7},
         },
         .flags = PIX_FMT_PAL,
+    },
+    [PIX_FMT_UYVY422] = {
+        .name = "uyvy422",
+        .nb_channels  = 3,
+        .log2_chroma_w= 1,
+        .log2_chroma_h= 0,
+        .comp = {
+            {0,1,2,0,7},        /* Y */
+            {0,3,1,0,7},        /* U */
+            {0,3,3,0,7},        /* V */
+        },
+    },
+    [PIX_FMT_NV12] = {
+        .name = "nv12",
+        .nb_channels  = 3,
+        .log2_chroma_w= 1,
+        .log2_chroma_h= 1,
+        .comp = {
+            {0,0,1,0,7},        /* Y */
+            {1,1,1,0,7},        /* U */
+            {1,1,2,0,7},        /* V */
+        },
+    },
+    [PIX_FMT_RGBA] = {
+        .name = "rgba",
+        .nb_channels  = 4,
+        .log2_chroma_w= 0,
+        .log2_chroma_h= 0,
+        .comp = {
+            {0,3,1,0,7},        /* R */
+            {0,3,2,0,7},        /* G */
+            {0,3,3,0,7},        /* B */
+            {0,3,4,0,7},        /* A */
+        },
+    },
+    [PIX_FMT_GRAY16BE] = {
+        .name = "gray16be",
+        .nb_channels  = 1,
+        .log2_chroma_w= 0,
+        .log2_chroma_h= 0,
+        .comp = {
+            {0,1,1,0,15},       /* Y */
+        },
+        .flags = PIX_FMT_BE,
+    },
+    [PIX_FMT_GRAY16LE] = {
+        .name = "gray16le",
+        .nb_channels  = 1,
+        .log2_chroma_w= 0,
+        .log2_chroma_h= 0,
+        .comp = {
+            {0,1,1,0,15},       /* Y */
+        },
+    },
+    [PIX_FMT_RGB48BE] = {
+        .name = "rgb48be",
+        .nb_channels  = 3,
+        .log2_chroma_w= 0,
+        .log2_chroma_h= 0,
+        .comp = {
+            {0,5,1,0,15},       /* R */
+            {0,5,3,0,15},       /* G */
+            {0,5,5,0,15},       /* B */
+        },
+        .flags = PIX_FMT_BE,
+    },
+    [PIX_FMT_RGB48LE] = {
+        .name = "rgb48le",
+        .nb_channels  = 3,
+        .log2_chroma_w= 0,
+        .log2_chroma_h= 0,
+        .comp = {
+            {0,5,1,0,15},       /* R */
+            {0,5,3,0,15},       /* G */
+            {0,5,5,0,15},       /* B */
+        },
+    },
+    [PIX_FMT_RGB565BE] = {
+        .name = "rgb565be",
+        .nb_channels  = 3,
+        .log2_chroma_w= 0,
+        .log2_chroma_h= 0,
+        .comp = {
+            {0,1,0,3,4},        /* R */
+            {0,1,1,5,5},        /* G */
+            {0,1,1,0,4},        /* B */
+        },
+        .flags = PIX_FMT_BE,
+    },
+    [PIX_FMT_RGB565LE] = {
+        .name = "rgb565le",
+        .nb_channels  = 3,
+        .log2_chroma_w= 0,
+        .log2_chroma_h= 0,
+        .comp = {
+            {0,1,2,3,4},        /* R */
+            {0,1,1,5,5},        /* G */
+            {0,1,1,0,4},        /* B */
+        },
     },
 };
