f0ed92df60e47c9bcc68f001006ec11ccd87cd58
Robert Swain <robert.swain@gmail.com>
2009-04-07 19:29:00 +0000

    Fix second amr_nb_decode_frame() to also use the new AVPacket argument
    Originally committed as revision 18354 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/libamr.c
+++ libavcodec/libamr.c
@@ -443,8 +443,10 @@ static av_cold int amr_nb_encode_close(AVCodecContext * avctx)
 
 static int amr_nb_decode_frame(AVCodecContext * avctx,
             void *data, int *data_size,
-            const uint8_t * buf, int buf_size)
+            AVPacket *avpkt)
 {
+    const uint8_t *buf = avpkt->data;
+    int buf_size = avpkt->size;
     AMRContext *s = avctx->priv_data;
     const uint8_t*amrData=buf;
     static const uint8_t block_size[16]={ 12, 13, 15, 17, 19, 20, 26, 31, 5, 0, 0, 0, 0, 0, 0, 0 };
