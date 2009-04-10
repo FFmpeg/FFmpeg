0dccbba02c9c8ae4391876c4a015c08bfb1e5697
Thilo Borgmann <thilo.borgmann@googlemail.com>
2009-04-10 12:18:04 +0000

    Add 'PNG1' to PNG fourcc codes.
    Patch by Thilo Borgmann thilo DOT borgmann _AT_ gXXglemail.cXm.
    
    Originally committed as revision 18415 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/riff.c
+++ libavformat/riff.c
@@ -198,6 +198,7 @@ const AVCodecTag codec_bmp_tags[] = {
     { CODEC_ID_VMNC,         MKTAG('V', 'M', 'n', 'c') },
     { CODEC_ID_TARGA,        MKTAG('t', 'g', 'a', ' ') },
     { CODEC_ID_PNG,          MKTAG('M', 'P', 'N', 'G') },
+    { CODEC_ID_PNG,          MKTAG('P', 'N', 'G', '1') },
     { CODEC_ID_CLJR,         MKTAG('c', 'l', 'j', 'r') },
     { CODEC_ID_DIRAC,        MKTAG('d', 'r', 'a', 'c') },
     { CODEC_ID_RPZA,         MKTAG('a', 'z', 'p', 'r') },
