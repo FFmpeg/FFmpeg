5338bec6972bac8ee2ff4f3100632b9f3f08d3a8
Michael Niedermayer <michaelni@gmx.at>
2009-04-11 13:40:39 +0000

    Get rid of INIT_VLC_USE_STATIC in asv1.
    Originally committed as revision 18434 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/asv1.c
+++ libavcodec/asv1.c
@@ -119,21 +119,21 @@ static av_cold void init_vlcs(ASV1Context *a){
     if (!done) {
         done = 1;
 
-        init_vlc(&ccp_vlc, VLC_BITS, 17,
+        INIT_VLC_STATIC(&ccp_vlc, VLC_BITS, 17,
                  &ccp_tab[0][1], 2, 1,
-                 &ccp_tab[0][0], 2, 1, INIT_VLC_USE_STATIC);
-        init_vlc(&dc_ccp_vlc, VLC_BITS, 8,
+                 &ccp_tab[0][0], 2, 1, 64);
+        INIT_VLC_STATIC(&dc_ccp_vlc, VLC_BITS, 8,
                  &dc_ccp_tab[0][1], 2, 1,
-                 &dc_ccp_tab[0][0], 2, 1, INIT_VLC_USE_STATIC);
-        init_vlc(&ac_ccp_vlc, VLC_BITS, 16,
+                 &dc_ccp_tab[0][0], 2, 1, 64);
+        INIT_VLC_STATIC(&ac_ccp_vlc, VLC_BITS, 16,
                  &ac_ccp_tab[0][1], 2, 1,
-                 &ac_ccp_tab[0][0], 2, 1, INIT_VLC_USE_STATIC);
-        init_vlc(&level_vlc,  VLC_BITS, 7,
+                 &ac_ccp_tab[0][0], 2, 1, 64);
+        INIT_VLC_STATIC(&level_vlc,  VLC_BITS, 7,
                  &level_tab[0][1], 2, 1,
-                 &level_tab[0][0], 2, 1, INIT_VLC_USE_STATIC);
-        init_vlc(&asv2_level_vlc, ASV2_LEVEL_VLC_BITS, 63,
+                 &level_tab[0][0], 2, 1, 64);
+        INIT_VLC_STATIC(&asv2_level_vlc, ASV2_LEVEL_VLC_BITS, 63,
                  &asv2_level_tab[0][1], 2, 1,
-                 &asv2_level_tab[0][0], 2, 1, INIT_VLC_USE_STATIC);
+                 &asv2_level_tab[0][0], 2, 1, 1024);
     }
 }
 
