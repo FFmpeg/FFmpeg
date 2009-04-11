2a0241ae12c7187a42dd039dc438844a63589634
Michael Niedermayer <michaelni@gmx.at>
2009-04-11 13:43:09 +0000

    Get rid of INIT_VLC_USE_STATIC in 4xm.
    Originally committed as revision 18435 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/4xm.c
+++ libavcodec/4xm.c
@@ -237,12 +237,15 @@ static void idct(DCTELEM block[64]){
 }
 
 static av_cold void init_vlcs(FourXContext *f){
+    static VLC_TYPE table[8][32][2];
     int i;
 
     for(i=0; i<8; i++){
+        block_type_vlc[0][i].table= table[i];
+        block_type_vlc[0][i].table_allocated= 32;
         init_vlc(&block_type_vlc[0][i], BLOCK_TYPE_VLC_BITS, 7,
                  &block_type_tab[0][i][0][1], 2, 1,
-                 &block_type_tab[0][i][0][0], 2, 1, INIT_VLC_USE_STATIC);
+                 &block_type_tab[0][i][0][0], 2, 1, INIT_VLC_USE_NEW_STATIC);
     }
 }
 
