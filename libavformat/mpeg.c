e8c93839148a168aedc978388f14c3599dd072f8
Michael Niedermayer <michaelni@gmx.at>
2009-04-17 18:08:39 +0000

    Remove 2 ++ from check_pes() that are useless, found by CSA.
    Originally committed as revision 18577 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/mpeg.c
+++ libavformat/mpeg.c
@@ -42,10 +42,8 @@ static int check_pes(uint8_t *p, uint8_t *end){
     if((*p&0xC0) == 0x40) p+=2;
     if((*p&0xF0) == 0x20){
         pes1= p[0]&p[2]&p[4]&1;
-        p+=5;
     }else if((*p&0xF0) == 0x30){
         pes1= p[0]&p[2]&p[4]&p[5]&p[7]&p[9]&1;
-        p+=10;
     }else
         pes1 = *p == 0x0F;
 
