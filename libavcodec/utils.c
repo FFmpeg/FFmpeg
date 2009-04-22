445f0a8b666a34e6402f6ae96c6804c8bc024baa
Michael Niedermayer <michaelni@gmx.at>
2009-04-22 01:54:05 +0000

    Update safety check as the maximum pixel size is no longer 4. New max size is 16bit * 4 samples (RGBA).
    Originally committed as revision 18655 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/utils.c
+++ libavcodec/utils.c
@@ -200,7 +200,7 @@ void avcodec_align_dimensions(AVCodecContext *s, int *width, int *height){
 }
 
 int avcodec_check_dimensions(void *av_log_ctx, unsigned int w, unsigned int h){
-    if((int)w>0 && (int)h>0 && (w+128)*(uint64_t)(h+128) < INT_MAX/4)
+    if((int)w>0 && (int)h>0 && (w+128)*(uint64_t)(h+128) < INT_MAX/8)
         return 0;
 
     av_log(av_log_ctx, AV_LOG_ERROR, "picture size invalid (%ux%u)\n", w, h);
