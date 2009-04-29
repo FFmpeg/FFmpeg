a068a7f1f0cbf0962424dfade42a1e9a5bf7f5bb
Diego Biurrun <diego@biurrun.de>
2009-04-29 15:43:57 +0000

    Drop unused libamr-nb fixed-point glue code.
    Originally committed as revision 18716 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/libamr.c
+++ libavcodec/libamr.c
@@ -37,10 +37,6 @@
  * The float version (default) can be downloaded from:
  * http://www.3gpp.org/ftp/Specs/archive/26_series/26.104/26104-610.zip
  *
- * \subsection Fixed-point
- * The fixed-point (TS26.073) can be downloaded from:
- * http://www.3gpp.org/ftp/Specs/archive/26_series/26.073/26073-600.zip
- *
  * \subsection Specification
  * The specification for AMR-NB can be found in TS 26.071
  * (http://www.3gpp.org/ftp/Specs/html-info/26071.htm) and some other
@@ -52,10 +48,6 @@
  * The reference code can be downloaded from:
  * http://www.3gpp.org/ftp/Specs/archive/26_series/26.204/26204-600.zip
  *
- * \subsection Fixed-point
- * If someone wants to use the fixed point version it can be downloaded from:
- * http://www.3gpp.org/ftp/Specs/archive/26_series/26.173/26173-571.zip.
- *
  * \subsection Specification
  * The specification for AMR-WB can be found in TS 26.171
  * (http://www.3gpp.org/ftp/Specs/html-info/26171.htm) and some other
@@ -65,21 +57,8 @@
 
 #include "avcodec.h"
 
-#if CONFIG_LIBAMR_NB_FIXED
-
-#define MMS_IO
-
-#include "amr/sp_dec.h"
-#include "amr/d_homing.h"
-#include "amr/typedef.h"
-#include "amr/sp_enc.h"
-#include "amr/sid_sync.h"
-#include "amr/e_homing.h"
-
-#else
 #include <amrnb/interf_dec.h>
 #include <amrnb/interf_enc.h>
-#endif
 
 static const char nb_bitrate_unsupported[] =
     "bitrate not supported: use one of 4.75k, 5.15k, 5.9k, 6.7k, 7.4k, 7.95k, 10.2k or 12.2k\n";
@@ -137,225 +116,7 @@ static void amr_decode_fix_avctx(AVCodecContext * avctx)
     avctx->sample_fmt = SAMPLE_FMT_S16;
 }
 
-#if CONFIG_LIBAMR_NB_FIXED
-/* fixed point version*/
-/* frame size in serial bitstream file (frame type + serial stream + flags) */
-#define SERIAL_FRAMESIZE (1+MAX_SERIAL_SIZE+5)
-
-typedef struct AMRContext {
-    int frameCount;
-    Speech_Decode_FrameState *speech_decoder_state;
-    enum RXFrameType rx_type;
-    enum Mode mode;
-    Word16 reset_flag;
-    Word16 reset_flag_old;
-
-    int enc_bitrate;
-    Speech_Encode_FrameState *enstate;
-    sid_syncState *sidstate;
-    enum TXFrameType tx_frametype;
-} AMRContext;
-
-static av_cold int amr_nb_decode_init(AVCodecContext * avctx)
-{
-    AMRContext *s = avctx->priv_data;
-
-    s->frameCount=0;
-    s->speech_decoder_state=NULL;
-    s->rx_type = (enum RXFrameType)0;
-    s->mode= (enum Mode)0;
-    s->reset_flag=0;
-    s->reset_flag_old=1;
-
-    if(Speech_Decode_Frame_init(&s->speech_decoder_state, "Decoder"))
-    {
-        av_log(avctx, AV_LOG_ERROR, "Speech_Decode_Frame_init error\n");
-        return -1;
-    }
-
-    amr_decode_fix_avctx(avctx);
-
-    if(avctx->channels > 1)
-    {
-        av_log(avctx, AV_LOG_ERROR, "amr_nb: multichannel decoding not supported\n");
-        return -1;
-    }
-
-    return 0;
-}
-
-static av_cold int amr_nb_encode_init(AVCodecContext * avctx)
-{
-    AMRContext *s = avctx->priv_data;
-
-    s->frameCount=0;
-    s->speech_decoder_state=NULL;
-    s->rx_type = (enum RXFrameType)0;
-    s->mode= (enum Mode)0;
-    s->reset_flag=0;
-    s->reset_flag_old=1;
-
-    if(avctx->sample_rate!=8000)
-    {
-        av_log(avctx, AV_LOG_ERROR, "Only 8000Hz sample rate supported\n");
-        return -1;
-    }
-
-    if(avctx->channels!=1)
-    {
-        av_log(avctx, AV_LOG_ERROR, "Only mono supported\n");
-        return -1;
-    }
-
-    avctx->frame_size=160;
-    avctx->coded_frame= avcodec_alloc_frame();
-
-    if(Speech_Encode_Frame_init(&s->enstate, 0, "encoder") || sid_sync_init (&s->sidstate))
-    {
-        av_log(avctx, AV_LOG_ERROR, "Speech_Encode_Frame_init error\n");
-        return -1;
-    }
-
-    if((s->enc_bitrate=getBitrateMode(avctx->bit_rate))<0)
-    {
-        av_log(avctx, AV_LOG_ERROR, nb_bitrate_unsupported);
-        return -1;
-    }
-
-    return 0;
-}
-
-static av_cold int amr_nb_encode_close(AVCodecContext * avctx)
-{
-    AMRContext *s = avctx->priv_data;
-
-    Speech_Encode_Frame_exit(&s->enstate);
-    sid_sync_exit (&s->sidstate);
-    av_freep(&avctx->coded_frame);
-    return 0;
-}
-
-static av_cold int amr_nb_decode_close(AVCodecContext * avctx)
-{
-    AMRContext *s = avctx->priv_data;
-
-    Speech_Decode_Frame_exit(&s->speech_decoder_state);
-    return 0;
-}
-
-static int amr_nb_decode_frame(AVCodecContext * avctx,
-            void *data, int *data_size,
-            AVPacket *avpkt)
-{
-    const uint8_t *buf = avpkt->data;
-    int buf_size = avpkt->size;
-    AMRContext *s = avctx->priv_data;
-    const uint8_t*amrData=buf;
-    int offset=0;
-    UWord8 toc, q, ft;
-    Word16 serial[SERIAL_FRAMESIZE];   /* coded bits */
-    Word16 *synth;
-    UWord8 *packed_bits;
-    static Word16 packed_size[16] = {12, 13, 15, 17, 19, 20, 26, 31, 5, 0, 0, 0, 0, 0, 0, 0};
-    int i;
-
-    //printf("amr_decode_frame data_size=%i buf=0x%X buf_size=%d frameCount=%d!!\n",*data_size,buf,buf_size,s->frameCount);
-
-    synth=data;
-
-    toc=amrData[offset];
-    /* read rest of the frame based on ToC byte */
-    q  = (toc >> 2) & 0x01;
-    ft = (toc >> 3) & 0x0F;
-
-    //printf("offset=%d, packet_size=%d amrData= 0x%X %X %X %X\n",offset,packed_size[ft],amrData[offset],amrData[offset+1],amrData[offset+2],amrData[offset+3]);
-
-    offset++;
-
-    packed_bits=amrData+offset;
-
-    offset+=packed_size[ft];
-
-    //Unsort and unpack bits
-    s->rx_type = UnpackBits(q, ft, packed_bits, &s->mode, &serial[1]);
-
-    //We have a new frame
-    s->frameCount++;
-
-    if (s->rx_type == RX_NO_DATA)
-    {
-        s->mode = s->speech_decoder_state->prev_mode;
-    }
-    else {
-        s->speech_decoder_state->prev_mode = s->mode;
-    }
-
-    /* if homed: check if this frame is another homing frame */
-    if (s->reset_flag_old == 1)
-    {
-        /* only check until end of first subframe */
-        s->reset_flag = decoder_homing_frame_test_first(&serial[1], s->mode);
-    }
-    /* produce encoder homing frame if homed & input=decoder homing frame */
-    if ((s->reset_flag != 0) && (s->reset_flag_old != 0))
-    {
-        for (i = 0; i < L_FRAME; i++)
-        {
-            synth[i] = EHF_MASK;
-        }
-    }
-    else
-    {
-        /* decode frame */
-        Speech_Decode_Frame(s->speech_decoder_state, s->mode, &serial[1], s->rx_type, synth);
-    }
-
-    //Each AMR-frame results in 160 16-bit samples
-    *data_size=160*2;
-
-    /* if not homed: check whether current frame is a homing frame */
-    if (s->reset_flag_old == 0)
-    {
-        /* check whole frame */
-        s->reset_flag = decoder_homing_frame_test(&serial[1], s->mode);
-    }
-    /* reset decoder if current frame is a homing frame */
-    if (s->reset_flag != 0)
-    {
-        Speech_Decode_Frame_reset(s->speech_decoder_state);
-    }
-    s->reset_flag_old = s->reset_flag;
-
-    return offset;
-}
-
-
-static int amr_nb_encode_frame(AVCodecContext *avctx,
-                            unsigned char *frame/*out*/, int buf_size, void *data/*in*/)
-{
-    short serial_data[250] = {0};
-    AMRContext *s = avctx->priv_data;
-    int written;
-
-    s->reset_flag = encoder_homing_frame_test(data);
-
-    Speech_Encode_Frame(s->enstate, s->enc_bitrate, data, &serial_data[1], &s->mode);
-
-    /* add frame type and mode */
-    sid_sync (s->sidstate, s->mode, &s->tx_frametype);
-
-    written = PackBits(s->mode, s->enc_bitrate, s->tx_frametype, &serial_data[1], frame);
-
-    if (s->reset_flag != 0)
-    {
-        Speech_Encode_Frame_reset(s->enstate);
-        sid_sync_reset(s->sidstate);
-    }
-    return written;
-}
-
-
-#elif CONFIG_LIBAMR_NB /* Float point version*/
+#if CONFIG_LIBAMR_NB
 
 typedef struct AMRContext {
     int frameCount;
@@ -496,7 +257,7 @@ static int amr_nb_encode_frame(AVCodecContext *avctx,
 
 #endif
 
-#if CONFIG_LIBAMR_NB || CONFIG_LIBAMR_NB_FIXED
+#if CONFIG_LIBAMR_NB
 
 AVCodec libamr_nb_decoder =
 {
