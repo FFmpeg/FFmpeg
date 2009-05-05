9507d91150a98d4c1d6acf4dc08475bf1852eb18
Michael Niedermayer <michaelni@gmx.at>
2009-05-05 01:34:16 +0000

    Change VOFW for x86 to 5120, it allows larger images to be scaled and was not slower. Other archs are not changed as the larger VOFW was slower on PPC.
    Originally committed as revision 29256 to svn://svn.mplayerhq.hu/mplayer/trunk/libswscale

--- libswscale/swscale_internal.h
+++ libswscale/swscale_internal.h
@@ -33,7 +33,12 @@
 
 #define MAX_FILTER_SIZE 256
 
-#define VOFW 2048
+#if ARCH_X86
+#define VOFW 5120
+#else
+#define VOFW 2048 // faster on PPC and not tested on others
+#endif
+
 #define VOF  (VOFW*2)
 
 #ifdef WORDS_BIGENDIAN
