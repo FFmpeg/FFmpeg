89cb5cd25d2169db0f32b8542567ee62a37e2492
Daniel Verkamp <daniel@drv.nu>
2009-05-06 21:51:58 +0000

    Add CGA and palette data for 8088flex TMV decoder.
    Patch by Daniel Verkamp  daniel _AT_ drv DOT nu.
    
    Originally committed as revision 18764 to svn://svn.ffmpeg.org/ffmpeg/trunk

---
+++ libavcodec/cga_data.h
@@ -0,0 +1,27 @@
+/*
+ * CGA ROM data
+ *
+ * This file is part of FFmpeg.
+ *
+ * FFmpeg is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * FFmpeg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FFmpeg; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#ifndef AVCODEC_CGA_DATA_H
+#define AVCODEC_CGA_DATA_H
+
+extern const uint8_t ff_cga_font[2048];
+extern const uint32_t ff_cga_palette[16];
+
+#endif
