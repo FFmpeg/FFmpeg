b3867c93d10b42dae1aeb1e8fa6059c5b87c28a5
Kostya Shishkov <kostya.shishkov@gmail.com>
2009-05-16 06:39:08 +0000

    Fix off-by-one error in MS RLE decoder which may result into writing past picture buffer.
    Originally committed as revision 18852 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/msrledec.c
+++ libavcodec/msrledec.c
@@ -134,7 +134,7 @@ static int msrle_decode_8_16_24_32(AVCodecContext *avctx, AVPicture *pic, int de
 {
     uint8_t *output, *output_end;
     const uint8_t* src = data;
-    int p1, p2, line=avctx->height, pos=0, i;
+    int p1, p2, line=avctx->height - 1, pos=0, i;
     uint16_t av_uninit(pix16);
     uint32_t av_uninit(pix32);
 
