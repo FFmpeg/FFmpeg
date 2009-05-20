e20a4f53a091ae74e6c9e9a280eefe5c59c7ebe4
Måns Rullgård <mans@mansr.com>
2009-05-20 18:49:55 +0000

    mathops: sort arch-specific #include lines
    Originally committed as revision 18886 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/mathops.h
+++ libavcodec/mathops.h
@@ -24,14 +24,14 @@
 
 #include "libavutil/common.h"
 
-#if   ARCH_X86
-#   include "x86/mathops.h"
-#elif ARCH_ARM
+#if   ARCH_ARM
 #   include "arm/mathops.h"
-#elif ARCH_PPC
-#   include "ppc/mathops.h"
 #elif ARCH_BFIN
 #   include "bfin/mathops.h"
+#elif ARCH_PPC
+#   include "ppc/mathops.h"
+#elif ARCH_X86
+#   include "x86/mathops.h"
 #endif
 
 /* generic implementation */
