64d882155dbedd2e2e7dd7d5ab9d6285db0323f4
Baptiste Coudurier <baptiste.coudurier@gmail.com>
2009-05-27 00:48:39 +0000

    10l, forgot ';', fix compilation
    Originally committed as revision 18962 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/mpegts.c
+++ libavformat/mpegts.c
@@ -893,7 +893,7 @@ static void mpegts_push_data(MpegTSFilter *filter,
         case MPEGTS_PESHEADER_FILL:
             len = pes->pes_header_size - pes->data_index;
             if (len < 0)
-                return
+                return;
             if (len > buf_size)
                 len = buf_size;
             memcpy(pes->header + pes->data_index, p, len);
