7103a77b4dbf4de31d909a52e2638cb01bf4d3fc
Baptiste Coudurier <baptiste.coudurier@gmail.com>
2009-05-31 04:10:15 +0000

    set codec_tag to stream type, so user can deal with it
    Originally committed as revision 19013 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/mpegts.c
+++ libavformat/mpegts.c
@@ -557,6 +557,8 @@ static AVStream *new_pes_av_stream(PESContext *pes, uint32_t prog_reg_desc, uint
     dprintf(pes->stream, "stream_type=%x pid=%x prog_reg_desc=%.4s\n",
             pes->stream_type, pes->pid, (char*)&prog_reg_desc);
 
+    st->codec->codec_tag = pes->stream_type;
+
     mpegts_find_stream_type(st, pes->stream_type, ISO_types);
     if (prog_reg_desc == AV_RL32("HDMV") &&
         st->codec->codec_id == CODEC_ID_PROBE)
