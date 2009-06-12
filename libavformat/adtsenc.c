70f99f8dd229daa33f6c251528cf02947e4e6419
Alex Converse <alex.converse@gmail.com>
2009-06-12 19:49:46 +0000

    Do not include libavcodec/internal.h in the ADTS muxer.
    Originally committed as revision 19173 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/adtsenc.c
+++ libavformat/adtsenc.c
@@ -22,7 +22,6 @@
 
 #include "libavcodec/get_bits.h"
 #include "libavcodec/put_bits.h"
-#include "libavcodec/internal.h"
 #include "libavcodec/mpeg4audio.h"
 #include "avformat.h"
 
@@ -64,7 +63,7 @@ static int decode_extradata(AVFormatContext *s, ADTSContext *adts, uint8_t *buf,
         return -1;
     }
     if (get_bits(&gb, 1)) {
-        ff_log_missing_feature(s, "Signaled SBR or PS", 0);
+        av_log(s, AV_LOG_ERROR, "Signaled SBR or PS is not supported\n");
         return -1;
     }
     if (!adts->channel_conf) {
