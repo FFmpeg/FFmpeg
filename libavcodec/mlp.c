7cc8d616aab68ba5534e5a74214786ad08aac5ce
Ramiro Polla <ramiro@lisha.ufsc.br>
2009-06-15 14:38:30 +0000

    Fix unaligned accesses by doing bytewise access until aligned, then continuing in 32-bit quantities. Fixes crash observed on sparc during FATE mlp test. Patch by Ramiro.
    Originally committed as revision 19200 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/mlp.c
+++ libavcodec/mlp.c
@@ -96,6 +96,8 @@ uint8_t ff_mlp_calculate_parity(const uint8_t *buf, unsigned int buf_size)
     uint32_t scratch = 0;
     const uint8_t *buf_end = buf + buf_size;
 
+    for (; ((intptr_t) buf & 3) && buf < buf_end; buf++)
+        scratch ^= *buf;
     for (; buf < buf_end - 3; buf += 4)
         scratch ^= *((const uint32_t*)buf);
 
