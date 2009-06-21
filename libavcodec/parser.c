b27758bc02c7111d32c1e0d36d7cbdb4ce5d0400
Baptiste Coudurier <baptiste.coudurier@gmail.com>
2009-06-21 03:50:26 +0000

    Fetch timestamp for the first frame of field only if frame_offset and next_frame_offset are not set, because second field has next frame offset set but not frame_offset, otherwise this wrongly fetch timestamp for the second field.
    Originally committed as revision 19244 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/parser.c
+++ libavcodec/parser.c
@@ -89,7 +89,8 @@ void ff_fetch_timestamp(AVCodecParserContext *s, int off, int remove){
     s->offset= 0;
     for(i = 0; i < AV_PARSER_PTS_NB; i++) {
         if (   s->cur_offset + off >= s->cur_frame_offset[i]
-            &&(s->     frame_offset       <  s->cur_frame_offset[i] || !s->frame_offset)
+            && (s->frame_offset < s->cur_frame_offset[i] ||
+              (!s->frame_offset && !s->next_frame_offset)) // first field/frame
             //check is disabled  becausue mpeg-ts doesnt send complete PES packets
             && /*s->next_frame_offset + off <*/  s->cur_frame_end[i]){
             s->dts= s->cur_frame_dts[i];
