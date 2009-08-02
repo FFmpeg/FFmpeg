4546bf4116b97d98b6b8380c7bc85a209dfe707c
Baptiste Coudurier <baptiste.coudurier@gmail.com>
2009-08-02 08:46:11 +0000

    fix jpeg parser when PARSER_FLAG_COMPLETE_FRAMES is set
    Originally committed as revision 19558 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/mjpeg_parser.c
+++ libavcodec/mjpeg_parser.c
@@ -78,12 +78,16 @@ static int jpeg_parse(AVCodecParserContext *s,
     ParseContext *pc = s->priv_data;
     int next;
 
+    if(s->flags & PARSER_FLAG_COMPLETE_FRAMES){
+        next= buf_size;
+    }else{
     next= find_frame_end(pc, buf, buf_size);
 
     if (ff_combine_frame(pc, next, &buf, &buf_size) < 0) {
         *poutbuf = NULL;
         *poutbuf_size = 0;
         return buf_size;
+    }
     }
 
     *poutbuf = buf;
