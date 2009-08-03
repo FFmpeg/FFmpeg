d5652f38fbfbf09fcd4bcb5370899fb7cff4fd49
Colin McQuillan <m.niloc@googlemail.com>
2009-08-03 17:15:01 +0000

    Correct the sign of the arithmetic in ff_celp_lp_zero_synthesis_filterf()
    Patch by Colin McQuillan ( m.niloc googlemail com )
    
    Originally committed as revision 19574 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/celp_filters.c
+++ libavcodec/celp_filters.c
@@ -110,6 +110,6 @@ void ff_celp_lp_zero_synthesis_filterf(float *out,
     for (n = 0; n < buffer_length; n++) {
         out[n] = in[n];
         for (i = 1; i < filter_length; i++)
-            out[n] -= filter_coeffs[i-1] * in[n-i];
+            out[n] += filter_coeffs[i-1] * in[n-i];
     }
 }
