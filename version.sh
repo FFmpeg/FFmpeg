7f6d9b30890161d40b4bf8917f5cff7517029423
Måns Rullgård <mans@mansr.com>
2009-08-16 15:51:47 +0000

    version.sh: use standard sed syntax
    Originally committed as revision 19655 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- version.sh
+++ version.sh
@@ -4,7 +4,10 @@
 revision=$(cat snapshot_version 2> /dev/null)
 test $revision || revision=$(cd "$1" && LC_ALL=C svn info 2> /dev/null | grep Revision | cut -d' ' -f2)
 test $revision || revision=$(cd "$1" && grep revision .svn/entries 2>/dev/null | cut -d '"' -f2)
-test $revision || revision=$(cd "$1" && sed -n -e '/^dir$/{n;p;q}' .svn/entries 2>/dev/null)
+test $revision || revision=$(cd "$1" && sed -n -e '/^dir$/{n
+p
+q
+}' .svn/entries 2>/dev/null)
 test $revision && revision=SVN-r$revision
 
 # check for git short hash
