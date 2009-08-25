6aa333471c95587dffd83580b4e58c52245dbc79
Baptiste Coudurier <baptiste.coudurier@gmail.com>
2009-08-25 19:59:38 +0000

    return AVERROR(ENOMEM) if stream could not be allocated
    Originally committed as revision 19702 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/r3d.c
+++ libavformat/r3d.c
@@ -55,7 +55,7 @@ static int r3d_read_red1(AVFormatContext *s)
     int tmp, tmp2;
 
     if (!st)
-        return -1;
+        return AVERROR(ENOMEM);
     st->codec->codec_type = CODEC_TYPE_VIDEO;
     st->codec->codec_id = CODEC_ID_JPEG2000;
 
