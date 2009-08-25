a13e687034093c751da6fd19be0f88e695e1375a
Måns Rullgård <mans@mansr.com>
2009-08-25 23:28:33 +0000

    mdec: remove unused members from struct MDECContext
    Originally committed as revision 19705 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/mdec.c
+++ libavcodec/mdec.c
@@ -45,8 +45,6 @@ typedef struct MDECContext{
     int mb_height;
     int mb_x, mb_y;
     DECLARE_ALIGNED_16(DCTELEM, block[6][64]);
-    DECLARE_ALIGNED_8(uint16_t, intra_matrix[64]);
-    DECLARE_ALIGNED_8(int, q_intra_matrix[64]);
     uint8_t *bitstream_buffer;
     unsigned int bitstream_buffer_size;
     int block_last_index[6];
