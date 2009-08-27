8fc9376c590d2998820bb27b4465636a3f3257a8
Loren Merritt <lorenm@u.washington.edu>
2009-08-27 13:41:29 +0000

    indent
    Originally committed as revision 19727 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/vorbis.c
+++ libavcodec/vorbis.c
@@ -176,20 +176,20 @@ static void render_line(int x0, int y0, int x1, int y1, float * buf) {
     if(ady*2<=adx) { // optimized common case
         render_line_unrolled(x0, y0, x1, sy, ady, adx, buf);
     } else {
-    int base = dy / adx;
-    int x = x0;
-    int y = y0;
-    int err = -adx;
-    ady -= FFABS(base) * adx;
-    while (++x < x1) {
-        y += base;
-        err += ady;
-        if (err >= 0) {
-            err -= adx;
-            y += sy;
+        int base = dy / adx;
+        int x = x0;
+        int y = y0;
+        int err = -adx;
+        ady -= FFABS(base) * adx;
+        while (++x < x1) {
+            y += base;
+            err += ady;
+            if (err >= 0) {
+                err -= adx;
+                y += sy;
+            }
+            buf[x] = ff_vorbis_floor1_inverse_db_table[y];
         }
-        buf[x] = ff_vorbis_floor1_inverse_db_table[y];
-    }
     }
 }
 
