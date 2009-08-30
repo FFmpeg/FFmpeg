c6dbbc4576dd024b97919b8ee0d51a11cf9dbdaa
Thilo Borgmann <thilo.borgmann@googlemail.com>
2009-08-30 15:50:03 +0000

    Add function to compute ceil(log2(x)). Patch by Thilo Borgmann <thilo.borgmann _at_ googlemail.com>
    Originally committed as revision 19747 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavutil/common.h
+++ libavutil/common.h
@@ -225,6 +225,15 @@ static inline av_const float av_clipf(float a, float amin, float amax)
     else               return a;
 }
 
+/** Computes ceil(log2(x)).
+ * @param x value used to compute ceil(log2(x))
+ * @return computed ceiling of log2(x)
+ */
+static inline av_const int av_ceil_log2(int x)
+{
+    return av_log2((x - 1) << 1);
+}
+
 #define MKTAG(a,b,c,d) (a | (b << 8) | (c << 16) | (d << 24))
 #define MKBETAG(a,b,c,d) (d | (c << 8) | (b << 16) | (a << 24))
 
