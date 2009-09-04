315fa33f29266b44ba3738a2017ab84accb79178
Ramiro Polla <ramiro.polla@gmail.com>
2009-09-04 16:32:06 +0000

    Indent.
    Originally committed as revision 19757 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavdevice/vfwcap.c
+++ libavdevice/vfwcap.c
@@ -295,15 +295,15 @@ static int vfw_read_header(AVFormatContext *s, AVFormatParameters *ap)
     bi->bmiHeader.biHeight = height;
 
     if (0) {
-    /* For testing yet unsupported compressions
-     * Copy these values from user-supplied verbose information */
-    bi->bmiHeader.biWidth       = 320;
-    bi->bmiHeader.biHeight      = 240;
-    bi->bmiHeader.biPlanes      = 1;
-    bi->bmiHeader.biBitCount    = 12;
-    bi->bmiHeader.biCompression = MKTAG('I','4','2','0');
-    bi->bmiHeader.biSizeImage   = 115200;
-    dump_bih(s, &bi->bmiHeader);
+        /* For testing yet unsupported compressions
+         * Copy these values from user-supplied verbose information */
+        bi->bmiHeader.biWidth       = 320;
+        bi->bmiHeader.biHeight      = 240;
+        bi->bmiHeader.biPlanes      = 1;
+        bi->bmiHeader.biBitCount    = 12;
+        bi->bmiHeader.biCompression = MKTAG('I','4','2','0');
+        bi->bmiHeader.biSizeImage   = 115200;
+        dump_bih(s, &bi->bmiHeader);
     }
 
     ret = SendMessage(ctx->hwnd, WM_CAP_SET_VIDEOFORMAT, bisize, (LPARAM) bi);
