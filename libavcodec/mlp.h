a55247e76168e5e67d45e5fedec9cb9c62069673
Reimar Döffinger <Reimar.Doeffinger@gmx.de>
2009-09-26 16:04:35 +0000

    Specify maximum sample rate of MLP by defining the factor relative to 48000 instead of directly. This makes clear that the code assumes the maximum sample rate to be a multiple of 48000 and also removes the division from the MAX_BLOCKSIZE macros, which causes an issue with the Solaris assembler where "/" is a comment marker unless the --divide option is used.
    Originally committed as revision 20026 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/mlp.h
+++ libavcodec/mlp.h
@@ -47,13 +47,15 @@
  */
 #define MAX_SUBSTREAMS      3
 
+/** which multiple of 48000 the maximum sample rate is */
+#define MAX_RATEFACTOR      4
 /** maximum sample frequency seen in files */
-#define MAX_SAMPLERATE      192000
+#define MAX_SAMPLERATE      (MAX_RATEFACTOR * 48000)
 
 /** maximum number of audio samples within one access unit */
-#define MAX_BLOCKSIZE       (40 * (MAX_SAMPLERATE / 48000))
+#define MAX_BLOCKSIZE       (40 * MAX_RATEFACTOR)
 /** next power of two greater than MAX_BLOCKSIZE */
-#define MAX_BLOCKSIZE_POW2  (64 * (MAX_SAMPLERATE / 48000))
+#define MAX_BLOCKSIZE_POW2  (64 * MAX_RATEFACTOR)
 
 /** number of allowed filters */
 #define NUM_FILTERS         2
