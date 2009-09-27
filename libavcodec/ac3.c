3538a2e47a96ddccc42f06d4b53ecc04b434b9a7
Justin Ruggles <justin.ruggles@gmail.com>
2009-09-27 06:01:32 +0000

    Remove unneeded variable.
    Originally committed as revision 20047 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/ac3.c
+++ libavcodec/ac3.c
@@ -216,7 +216,7 @@ void ff_ac3_bit_alloc_calc_bap(int16_t *mask, int16_t *psd, int start, int end,
                                int snr_offset, int floor,
                                const uint8_t *bap_tab, uint8_t *bap)
 {
-    int i, j, k, end1, v, address;
+    int i, j, end1, v, address;
 
     /* special case, if snr offset is -960, set all bap's to zero */
     if (snr_offset == -960) {
@@ -229,10 +229,9 @@ void ff_ac3_bit_alloc_calc_bap(int16_t *mask, int16_t *psd, int start, int end,
     do {
         v = (FFMAX(mask[j] - snr_offset - floor, 0) & 0x1FE0) + floor;
         end1 = FFMIN(band_start_tab[j] + ff_ac3_critical_band_size_tab[j], end);
-        for (k = i; k < end1; k++) {
+        for (; i < end1; i++) {
             address = av_clip((psd[i] - v) >> 5, 0, 63);
             bap[i] = bap_tab[address];
-            i++;
         }
     } while (end > band_start_tab[j++]);
 }
