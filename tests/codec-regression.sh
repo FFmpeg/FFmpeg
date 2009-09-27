7f5c14210b3184d6c4330bf63af9512808c37379
Stefano Sabatini <stefano.sabatini-lala@poste.it>
2009-09-27 10:29:24 +0000

    Remove duplicated / stray code which slipped in with r20057.
    Originally committed as revision 20060 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- tests/codec-regression.sh
+++ tests/codec-regression.sh
@@ -672,22 +672,6 @@ do_lavfi() {
     fi
 }
 
-# do_lavfi "crop" "crop=100:100:-1:-1"
-# do_lavfi "crop_scale" "crop=100:100,scale=200:-1"
-# do_lavfi "scale" "scale=200:200"
-
-# libavfilter testing
-
-do_lavfi() {
-    test_name=$1
-    eval test=\$do_$test_name
-    vfilters=$2
-
-    if [ -n "$test" ] ; then
-        do_video_encoding ${test_name}.avi "-qscale 10" "-vcodec huffyuv -vfilters $vfilters"
-    fi
-}
-
 # example tests:
 # do_lavfi "crop" "crop=100:100:-1:-1"
 # do_lavfi "crop_scale" "crop=100:100,scale=200:-1"
