681c180dc441bf9fea1bb363c75c5d443f11a53a
Reimar Döffinger <Reimar.Doeffinger@gmx.de>
2009-10-01 15:54:55 +0000

    Make avcodec_open set codec_id and codec_type if they haven't been set. This fixes the API breakage introduced by the check that avctx codec id and type match the opened codec and should make (almost?) all applications work again.
    Originally committed as revision 20125 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/utils.c
+++ libavcodec/utils.c
@@ -480,6 +480,11 @@ int attribute_align_arg avcodec_open(AVCodecContext *avctx, AVCodec *codec)
     }
 
     avctx->codec = codec;
+    if ((avctx->codec_type == CODEC_TYPE_UNKNOWN || avctx->codec_type == codec->type) &&
+        avctx->codec_id == CODEC_ID_NONE) {
+        avctx->codec_type = codec->type;
+        avctx->codec_id   = codec->id;
+    }
     if(avctx->codec_id != codec->id || avctx->codec_type != codec->type){
         av_log(avctx, AV_LOG_ERROR, "codec type or id mismatches\n");
         goto free_and_end;
