52ae1e869be9ec1d13dea45277bf58a37a37f467
Ramiro Polla <ramiro.polla@gmail.com>
2009-10-16 16:12:04 +0000

    mlp: Indent.
    Originally committed as revision 20255 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/mlpdec.c
+++ libavcodec/mlpdec.c
@@ -156,15 +156,15 @@ static VLC huff_vlc[3];
 static av_cold void init_static(void)
 {
     if (!huff_vlc[0].bits) {
-    INIT_VLC_STATIC(&huff_vlc[0], VLC_BITS, 18,
-                &ff_mlp_huffman_tables[0][0][1], 2, 1,
-                &ff_mlp_huffman_tables[0][0][0], 2, 1, 512);
-    INIT_VLC_STATIC(&huff_vlc[1], VLC_BITS, 16,
-                &ff_mlp_huffman_tables[1][0][1], 2, 1,
-                &ff_mlp_huffman_tables[1][0][0], 2, 1, 512);
-    INIT_VLC_STATIC(&huff_vlc[2], VLC_BITS, 15,
-                &ff_mlp_huffman_tables[2][0][1], 2, 1,
-                &ff_mlp_huffman_tables[2][0][0], 2, 1, 512);
+        INIT_VLC_STATIC(&huff_vlc[0], VLC_BITS, 18,
+                    &ff_mlp_huffman_tables[0][0][1], 2, 1,
+                    &ff_mlp_huffman_tables[0][0][0], 2, 1, 512);
+        INIT_VLC_STATIC(&huff_vlc[1], VLC_BITS, 16,
+                    &ff_mlp_huffman_tables[1][0][1], 2, 1,
+                    &ff_mlp_huffman_tables[1][0][0], 2, 1, 512);
+        INIT_VLC_STATIC(&huff_vlc[2], VLC_BITS, 15,
+                    &ff_mlp_huffman_tables[2][0][1], 2, 1,
+                    &ff_mlp_huffman_tables[2][0][0], 2, 1, 512);
     }
 
     ff_mlp_init_crc();
