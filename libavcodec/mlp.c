33815fa106853d031e633dd9df50d628da5592b2
Reimar Döffinger <Reimar.Doeffinger@gmx.de>
2009-10-16 16:03:26 +0000

    mlp: Use smaller CRC tables if CONFIG_SMALL. Patch by Reimar.
    Originally committed as revision 20253 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/mlp.c
+++ libavcodec/mlp.c
@@ -42,9 +42,14 @@ const uint8_t ff_mlp_huffman_tables[3][18][2] = {
 };
 
 static int crc_init = 0;
-static AVCRC crc_63[1024];
-static AVCRC crc_1D[1024];
-static AVCRC crc_2D[1024];
+#if CONFIG_SMALL
+#define CRC_TABLE_SIZE 257
+#else
+#define CRC_TABLE_SIZE 1024
+#endif
+static AVCRC crc_63[CRC_TABLE_SIZE];
+static AVCRC crc_1D[CRC_TABLE_SIZE];
+static AVCRC crc_2D[CRC_TABLE_SIZE];
 
 av_cold void ff_mlp_init_crc(void)
 {
