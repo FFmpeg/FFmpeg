a0992374ee727b700d059d3c83b41498b32a43a6
Jai Menon <jmenon86@gmail.com>
2009-10-16 16:50:15 +0000

    alacenc : Move some code around for clarity.
    Originally committed as revision 20256 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/alacenc.c
+++ libavcodec/alacenc.c
@@ -132,7 +132,16 @@ static void calc_predictor_params(AlacEncodeContext *s, int ch)
     int shift[MAX_LPC_ORDER];
     int opt_order;
 
-    if (s->avctx->compression_level > 1) {
+    if (s->avctx->compression_level == 1) {
+        s->lpc[ch].lpc_order = 6;
+        s->lpc[ch].lpc_quant = 6;
+        s->lpc[ch].lpc_coeff[0] =  160;
+        s->lpc[ch].lpc_coeff[1] = -190;
+        s->lpc[ch].lpc_coeff[2] =  170;
+        s->lpc[ch].lpc_coeff[3] = -130;
+        s->lpc[ch].lpc_coeff[4] =   80;
+        s->lpc[ch].lpc_coeff[5] =  -25;
+    } else {
         opt_order = ff_lpc_calc_coefs(&s->dspctx, s->sample_buf[ch],
                                       s->avctx->frame_size,
                                       s->min_prediction_order,
@@ -143,15 +152,6 @@ static void calc_predictor_params(AlacEncodeContext *s, int ch)
         s->lpc[ch].lpc_order = opt_order;
         s->lpc[ch].lpc_quant = shift[opt_order-1];
         memcpy(s->lpc[ch].lpc_coeff, coefs[opt_order-1], opt_order*sizeof(int));
-    } else {
-        s->lpc[ch].lpc_order = 6;
-        s->lpc[ch].lpc_quant = 6;
-        s->lpc[ch].lpc_coeff[0] =  160;
-        s->lpc[ch].lpc_coeff[1] = -190;
-        s->lpc[ch].lpc_coeff[2] =  170;
-        s->lpc[ch].lpc_coeff[3] = -130;
-        s->lpc[ch].lpc_coeff[4] =   80;
-        s->lpc[ch].lpc_coeff[5] =  -25;
     }
 }
 
