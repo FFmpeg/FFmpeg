2bd0758efaca4aba6d914d1e7de1c40d521b4217
Anton Khirnov <wyskas@gmail.com>
2009-10-18 12:17:01 +0000

    matroskaenc: Add support for writing chapters. patch by Anton Khirnov  wyskas _at_ gmail _dot_ com
    Originally committed as revision 20277 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/matroskaenc.c
+++ libavformat/matroskaenc.c
@@ -619,6 +619,50 @@ static int mkv_write_tracks(AVFormatContext *s)
     return 0;
 }
 
+static int mkv_write_chapters(AVFormatContext *s)
+{
+    MatroskaMuxContext *mkv = s->priv_data;
+    ByteIOContext *pb = s->pb;
+    ebml_master chapters, editionentry;
+    AVRational scale = {1, 1E9};
+    int i, ret;
+
+    if (!s->nb_chapters)
+        return 0;
+
+    ret = mkv_add_seekhead_entry(mkv->main_seekhead, MATROSKA_ID_CHAPTERS, url_ftell(pb));
+    if (ret < 0) return ret;
+
+    chapters     = start_ebml_master(pb, MATROSKA_ID_CHAPTERS    , 0);
+    editionentry = start_ebml_master(pb, MATROSKA_ID_EDITIONENTRY, 0);
+    put_ebml_uint(pb, MATROSKA_ID_EDITIONFLAGDEFAULT, 1);
+    put_ebml_uint(pb, MATROSKA_ID_EDITIONFLAGHIDDEN , 0);
+    for (i = 0; i < s->nb_chapters; i++) {
+        ebml_master chapteratom, chapterdisplay;
+        AVChapter *c     = s->chapters[i];
+        AVMetadataTag *t = NULL;
+
+        chapteratom = start_ebml_master(pb, MATROSKA_ID_CHAPTERATOM, 0);
+        put_ebml_uint(pb, MATROSKA_ID_CHAPTERUID, c->id);
+        put_ebml_uint(pb, MATROSKA_ID_CHAPTERTIMESTART,
+                      av_rescale_q(c->start, c->time_base, scale));
+        put_ebml_uint(pb, MATROSKA_ID_CHAPTERTIMEEND,
+                      av_rescale_q(c->end,   c->time_base, scale));
+        put_ebml_uint(pb, MATROSKA_ID_CHAPTERFLAGHIDDEN , 0);
+        put_ebml_uint(pb, MATROSKA_ID_CHAPTERFLAGENABLED, 1);
+        if ((t = av_metadata_get(c->metadata, "title", NULL, 0))) {
+            chapterdisplay = start_ebml_master(pb, MATROSKA_ID_CHAPTERDISPLAY, 0);
+            put_ebml_string(pb, MATROSKA_ID_CHAPSTRING, t->value);
+            put_ebml_string(pb, MATROSKA_ID_CHAPLANG  , "und");
+            end_ebml_master(pb, chapterdisplay);
+        }
+        end_ebml_master(pb, chapteratom);
+    }
+    end_ebml_master(pb, editionentry);
+    end_ebml_master(pb, chapters);
+    return 0;
+}
+
 static int mkv_write_header(AVFormatContext *s)
 {
     MatroskaMuxContext *mkv = s->priv_data;
@@ -676,6 +720,9 @@ static int mkv_write_header(AVFormatContext *s)
     end_ebml_master(pb, segment_info);
 
     ret = mkv_write_tracks(s);
+    if (ret < 0) return ret;
+
+    ret = mkv_write_chapters(s);
     if (ret < 0) return ret;
 
     ret = mkv_add_seekhead_entry(mkv->cluster_seekhead, MATROSKA_ID_CLUSTER, url_ftell(pb));
