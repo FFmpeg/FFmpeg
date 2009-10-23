f94e56cf82ec2851639d0c5b0880cfa202e66c86
Stefano Sabatini <stefano.sabatini-lala@poste.it>
2009-10-23 22:49:05 +0000

    Split codec-regression.sh, put all the code which can be shared by other regression test scripts in the regression-funcs.sh file.
    Originally committed as revision 20359 to svn://svn.ffmpeg.org/ffmpeg/trunk

---
+++ tests/regression-funcs.sh
@@ -0,0 +1,126 @@
+#!/bin/sh
+#
+# common regression functions for ffmpeg
+#
+#
+
+test="${1#regtest-}"
+test_ref=$2
+raw_src_dir=$3
+outfile_prefix=$4
+target_exec=$5
+target_path=$6
+
+datadir="./tests/data"
+target_datadir="${target_path}/${datadir}"
+
+this="$test.$test_ref"
+logfile="$datadir/$this.regression"
+outfile="$datadir/${outfile_prefix}-"
+
+# various files
+ffmpeg="$target_exec ${target_path}/ffmpeg_g"
+tiny_psnr="tests/tiny_psnr"
+benchfile="$datadir/$this.bench"
+bench="$datadir/$this.bench.tmp"
+bench2="$datadir/$this.bench2.tmp"
+raw_src="${target_path}/$raw_src_dir/%02d.pgm"
+raw_dst="$datadir/$this.out.yuv"
+raw_ref="$datadir/$test_ref.ref.yuv"
+pcm_src="$target_datadir/asynth1.sw"
+pcm_dst="$datadir/$this.out.wav"
+pcm_ref="$datadir/$test_ref.ref.wav"
+crcfile="$datadir/$this.crc"
+target_crcfile="$target_datadir/$this.crc"
+
+if [ X"`echo | md5sum 2> /dev/null`" != X ]; then
+    do_md5sum() { md5sum -b $1; }
+elif [ X"`echo | md5 2> /dev/null`" != X ]; then
+    do_md5sum() { md5 $1 | sed 's#MD5 (\(.*\)) = \(.*\)#\2 *\1#'; }
+elif [ -x /sbin/md5 ]; then
+    do_md5sum() { /sbin/md5 -r $1 | sed 's# \**\./# *./#'; }
+else
+    do_md5sum() { echo No md5sum program found; }
+fi
+
+FFMPEG_OPTS="-v 0 -y -flags +bitexact -dct fastint -idct simple -sws_flags +accurate_rnd+bitexact"
+
+do_ffmpeg()
+{
+    f="$1"
+    shift
+    set -- $* ${target_path}/$f
+    echo $ffmpeg $FFMPEG_OPTS $*
+    $ffmpeg $FFMPEG_OPTS -benchmark $* > $bench
+    do_md5sum $f >> $logfile
+    if [ $f = $raw_dst ] ; then
+        $tiny_psnr $f $raw_ref >> $logfile
+    elif [ $f = $pcm_dst ] ; then
+        $tiny_psnr $f $pcm_ref 2 >> $logfile
+    else
+        wc -c $f >> $logfile
+    fi
+    expr "`cat $bench`" : '.*utime=\(.*s\)' > $bench2
+    echo `cat $bench2` $f >> $benchfile
+}
+
+do_ffmpeg_nomd5()
+{
+    f="$1"
+    shift
+    set -- $* ${target_path}/$f
+    echo $ffmpeg $FFMPEG_OPTS $*
+    $ffmpeg $FFMPEG_OPTS -benchmark $* > $bench
+    if [ $f = $raw_dst ] ; then
+        $tiny_psnr $f $raw_ref >> $logfile
+    elif [ $f = $pcm_dst ] ; then
+        $tiny_psnr $f $pcm_ref 2 >> $logfile
+    else
+        wc -c $f >> $logfile
+    fi
+    expr "`cat $bench`" : '.*utime=\(.*s\)' > $bench2
+    echo `cat $bench2` $f >> $benchfile
+}
+
+do_ffmpeg_crc()
+{
+    f="$1"
+    shift
+    echo $ffmpeg $FFMPEG_OPTS $* -f crc "$target_crcfile"
+    $ffmpeg $FFMPEG_OPTS $* -f crc "$target_crcfile"
+    echo "$f `cat $crcfile`" >> $logfile
+    rm -f "$crcfile"
+}
+
+do_ffmpeg_nocheck()
+{
+    f="$1"
+    shift
+    echo $ffmpeg $FFMPEG_OPTS $*
+    $ffmpeg $FFMPEG_OPTS -benchmark $* > $bench
+    expr "`cat $bench`" : '.*utime=\(.*s\)' > $bench2
+    echo `cat $bench2` $f >> $benchfile
+}
+
+do_video_decoding()
+{
+    do_ffmpeg $raw_dst $1 -i $target_path/$file -f rawvideo $2
+    rm -f $raw_dst
+}
+
+do_video_encoding()
+{
+    file=${outfile}$1
+    do_ffmpeg $file $2 -f image2 -vcodec pgmyuv -i $raw_src $3
+}
+
+do_audio_encoding()
+{
+    file=${outfile}$1
+    do_ffmpeg $file -ab 128k -ac 2 -f s16le -i $pcm_src $3
+}
+
+do_audio_decoding()
+{
+    do_ffmpeg $pcm_dst -i $target_path/$file -sample_fmt s16 -f wav
+}
