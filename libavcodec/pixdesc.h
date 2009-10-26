6b27353cfdadeea5913c4dbdbc322289a47a7e2c
Stefano Sabatini <stefano.sabatini-lala@poste.it>
2009-10-26 19:34:46 +0000

    Add a PIX_FMT_HWACCEL flag to the AVPixFmtDescriptor.flags.
    It is used to mark the pixel format used for hardware acceleration
    support.
    
    Originally committed as revision 20374 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/pixdesc.h
+++ libavcodec/pixdesc.h
@@ -79,6 +79,7 @@ typedef struct AVPixFmtDescriptor{
 #define PIX_FMT_BE        1 ///< big-endian
 #define PIX_FMT_PAL       2 ///< Pixel format has a palette in data[1], values are indexes in this palette.
 #define PIX_FMT_BITSTREAM 4 ///< All values of a component are bit-wise packed end to end.
+#define PIX_FMT_HWACCEL   8 ///< Pixel format is an HW accelerated format.
 
 /**
  * The array of all the pixel format descriptors.
