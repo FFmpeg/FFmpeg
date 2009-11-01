dee34af4d6eec7cfb2f96e81d6b7f5719a04a866
Justin Ruggles <justin.ruggles@gmail.com>
2009-11-01 15:08:49 +0000

    Set the sample format for Smacker audio in the decoder rather than in the demuxer.
    Originally committed as revision 20429 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/smacker.c
+++ libavformat/smacker.c
@@ -183,7 +183,6 @@ static int smacker_read_header(AVFormatContext *s, AVFormatParameters *ap)
             ast[i]->codec->bits_per_coded_sample = (smk->rates[i] & SMK_AUD_16BITS) ? 16 : 8;
             if(ast[i]->codec->bits_per_coded_sample == 16 && ast[i]->codec->codec_id == CODEC_ID_PCM_U8)
                 ast[i]->codec->codec_id = CODEC_ID_PCM_S16LE;
-            ast[i]->codec->sample_fmt = ast[i]->codec->bits_per_coded_sample == 8 ? SAMPLE_FMT_U8 : SAMPLE_FMT_S16;
             av_set_pts_info(ast[i], 64, 1, ast[i]->codec->sample_rate
                     * ast[i]->codec->channels * ast[i]->codec->bits_per_coded_sample / 8);
         }
