187bc061e4186b8e19a051d7b046210ccbdf8de9
Justin Ruggles <justin.ruggles@gmail.com>
2009-11-08 03:06:05 +0000

    ac3dec: revert r20089. The change was just wrong. 10l for me.
    Originally committed as revision 20473 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/ac3dec.c
+++ libavcodec/ac3dec.c
@@ -607,11 +607,10 @@ static void do_rematrixing(AC3DecodeContext *s)
 
     end = FFMIN(s->end_freq[1], s->end_freq[2]);
 
-    i = ff_ac3_rematrix_band_tab[0];
     for(bnd=0; bnd<s->num_rematrixing_bands; bnd++) {
         if(s->rematrixing_flags[bnd]) {
             bndend = FFMIN(end, ff_ac3_rematrix_band_tab[bnd+1]);
-            for(; i<bndend; i++) {
+            for(i=ff_ac3_rematrix_band_tab[bnd]; i<bndend; i++) {
                 int tmp0 = s->fixed_coeffs[1][i];
                 s->fixed_coeffs[1][i] += s->fixed_coeffs[2][i];
                 s->fixed_coeffs[2][i]  = tmp0 - s->fixed_coeffs[2][i];
