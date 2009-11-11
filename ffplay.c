ba11257e48ca547d747d3b0640a8022daa4f66ac
Diego Biurrun <diego@biurrun.de>
2009-11-11 23:46:11 +0000

    Add #include for required header config.h.
    Originally committed as revision 20518 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- ffplay.c
+++ ffplay.c
@@ -19,6 +19,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
+#include "config.h"
 #include <math.h>
 #include <limits.h>
 #include "libavutil/avstring.h"
