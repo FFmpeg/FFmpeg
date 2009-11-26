797229a6a2a189d6f58bf6bbd40c87f88ac1304c
Stefano Sabatini <stefano.sabatini-lala@poste.it>
2009-11-26 21:04:33 +0000

    Make avcodec_get_pix_fmt_name() use av_pix_fmt_descriptors rather than pix_fmt_info.
    Originally committed as revision 20625 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/imgconvert.c
+++ libavcodec/imgconvert.c
@@ -486,7 +486,7 @@ const char *avcodec_get_pix_fmt_name(enum PixelFormat pix_fmt)
     if (pix_fmt < 0 || pix_fmt >= PIX_FMT_NB)
         return NULL;
     else
-        return pix_fmt_info[pix_fmt].name;
+        return av_pix_fmt_descriptors[pix_fmt].name;
 }
 
 static enum PixelFormat avcodec_get_pix_fmt_internal(const char *name)
