e137934fae1bd9f963f4f69881ae2b0c13143983
Diego Biurrun <diego@biurrun.de>
2009-11-26 09:59:10 +0000

    cosmetics: K&R style, prettyprinting
    Originally committed as revision 20619 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/mpegaudio_tablegen.h
+++ libavcodec/mpegaudio_tablegen.h
@@ -34,7 +34,7 @@
 #define mpegaudio_tableinit()
 #include "libavcodec/mpegaudio_tables.h"
 #else
-static int8_t  table_4_3_exp[TABLE_4_3_SIZE];
+static int8_t   table_4_3_exp[TABLE_4_3_SIZE];
 static uint32_t table_4_3_value[TABLE_4_3_SIZE];
 static uint32_t exp_table[512];
 static uint32_t expval_table[512][16];
@@ -42,25 +42,25 @@ static uint32_t expval_table[512][16];
 static void mpegaudio_tableinit(void)
 {
     int i, value, exponent;
-    for(i=1;i<TABLE_4_3_SIZE;i++) {
-        double value = i/4;
+    for (i = 1; i < TABLE_4_3_SIZE; i++) {
+        double value = i / 4;
         double f, fm;
         int e, m;
-        f = value * cbrtf(value) * pow(2, (i&3)*0.25);
+        f  = value * cbrtf(value) * pow(2, (i & 3) * 0.25);
         fm = frexp(f, &e);
-        m = (uint32_t)(fm*(1LL<<31) + 0.5);
-        e+= FRAC_BITS - 31 + 5 - 100;
+        m  = (uint32_t)(fm * (1LL << 31) + 0.5);
+        e += FRAC_BITS - 31 + 5 - 100;
 
         /* normalized to FRAC_BITS */
-        table_4_3_value[i] = m;
-        table_4_3_exp[i] = -e;
+        table_4_3_value[i] =  m;
+        table_4_3_exp[i]   = -e;
     }
-    for(exponent=0; exponent<512; exponent++) {
-        for(value=0; value<16; value++) {
-            double f= (double)value * cbrtf(value) * pow(2, (exponent-400)*0.25 + FRAC_BITS + 5);
-            expval_table[exponent][value]= llrint(f);
+    for (exponent = 0; exponent < 512; exponent++) {
+        for (value = 0; value < 16; value++) {
+            double f = (double)value * cbrtf(value) * pow(2, (exponent - 400) * 0.25 + FRAC_BITS + 5);
+            expval_table[exponent][value] = llrint(f);
         }
-        exp_table[exponent]= expval_table[exponent][1];
+        exp_table[exponent] = expval_table[exponent][1];
     }
 }
 #endif /* CONFIG_HARDCODED_TABLES */
