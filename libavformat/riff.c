4b86926b95e08f8a6ae46182d6d8eff5ef1bfda0
Carl Eugen Hoyos <cehoyos@rainbow.studorg.tuwien.ac.at>
2009-12-08 23:47:45 +0000

    Support AVI1 and AVI2 in avi as MJPEG. Fixes issue 1602.
    Originally committed as revision 20781 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/riff.c
+++ libavformat/riff.c
@@ -134,6 +134,8 @@ const AVCodecTag ff_codec_bmp_tags[] = {
     { CODEC_ID_MJPEG,        MKTAG('C', 'J', 'P', 'G') }, /* Creative Webcam JPEG */
     { CODEC_ID_MJPEG,        MKTAG('I', 'J', 'L', 'V') }, /* Intel JPEG Library Video Codec */
     { CODEC_ID_MJPEG,        MKTAG('M', 'V', 'J', 'P') }, /* Midvid JPEG Video Codec */
+    { CODEC_ID_MJPEG,        MKTAG('A', 'V', 'I', '1') },
+    { CODEC_ID_MJPEG,        MKTAG('A', 'V', 'I', '2') },
     { CODEC_ID_HUFFYUV,      MKTAG('H', 'F', 'Y', 'U') },
     { CODEC_ID_FFVHUFF,      MKTAG('F', 'F', 'V', 'H') },
     { CODEC_ID_CYUV,         MKTAG('C', 'Y', 'U', 'V') },
