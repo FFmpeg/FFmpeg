47a81dad922c8c434087de4901306530a99dbaea
Michael Niedermayer <michaelni@gmx.at>
2009-12-11 21:50:08 +0000

    indent
    Originally committed as revision 20802 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/avpacket.c
+++ libavcodec/avpacket.c
@@ -49,9 +49,9 @@ int av_new_packet(AVPacket *pkt, int size)
 {
     uint8_t *data= NULL;
     if((unsigned)size < (unsigned)size + FF_INPUT_BUFFER_PADDING_SIZE)
-    data = av_malloc(size + FF_INPUT_BUFFER_PADDING_SIZE);
+        data = av_malloc(size + FF_INPUT_BUFFER_PADDING_SIZE);
     if (data){
-    memset(data + size, 0, FF_INPUT_BUFFER_PADDING_SIZE);
+        memset(data + size, 0, FF_INPUT_BUFFER_PADDING_SIZE);
     }else
         size=0;
 
