1f15747fc9650e6ee338c1e037f475eadd29c0be
Carl Eugen Hoyos <cehoyos@rainbow.studorg.tuwien.ac.at>
2009-12-14 10:52:32 +0000

    Use enum PixelFormat as type for input_pixfmt to avoid one icc warning.
    Originally committed as revision 20859 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavdevice/x11grab.c
+++ libavdevice/x11grab.c
@@ -85,7 +85,7 @@ x11grab_read_header(AVFormatContext *s1, AVFormatParameters *ap)
     struct x11_grab *x11grab = s1->priv_data;
     Display *dpy;
     AVStream *st = NULL;
-    int input_pixfmt;
+    enum PixelFormat input_pixfmt;
     XImage *image;
     int x_off = 0;
     int y_off = 0;
