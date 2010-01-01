7a78bc85aaf34ef9039c9a324f9756bdf9566a46
Stefano Sabatini <stefano.sabatini-lala@poste.it>
2010-01-01 15:07:35 +0000

    Update this_year value.
    Originally committed as revision 20995 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- cmdutils.c
+++ cmdutils.c
@@ -50,7 +50,7 @@ AVCodecContext *avcodec_opts[CODEC_TYPE_NB];
 AVFormatContext *avformat_opts;
 struct SwsContext *sws_opts;
 
-const int this_year = 2009;
+const int this_year = 2010;
 
 double parse_number_or_die(const char *context, const char *numstr, int type, double min, double max)
 {
