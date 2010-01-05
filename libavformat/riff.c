418a287cae2d3b1ff96ead02564e86c8cd0b91ee
Ben Littler <tempn@twmi.rr.com>
2010-01-05 20:14:50 +0000

    add another mjpeg fourcc to riff.c list
    Originally committed as revision 21032 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/riff.c
+++ libavformat/riff.c
@@ -139,6 +139,7 @@ const AVCodecTag ff_codec_bmp_tags[] = {
     { CODEC_ID_MJPEG,        MKTAG('A', 'V', 'I', '1') },
     { CODEC_ID_MJPEG,        MKTAG('A', 'V', 'I', '2') },
     { CODEC_ID_MJPEG,        MKTAG('M', 'T', 'S', 'J') },
+    { CODEC_ID_MJPEG,        MKTAG('Z', 'J', 'P', 'G') }, /* Paradigm Matrix M-JPEG Codec */
     { CODEC_ID_HUFFYUV,      MKTAG('H', 'F', 'Y', 'U') },
     { CODEC_ID_FFVHUFF,      MKTAG('F', 'F', 'V', 'H') },
     { CODEC_ID_CYUV,         MKTAG('C', 'Y', 'U', 'V') },
