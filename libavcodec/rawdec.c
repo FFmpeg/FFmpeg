6698d13998f983a5d592c5df525d67882c9c186f
Carl Eugen Hoyos <cehoyos@rainbow.studorg.tuwien.ac.at>
2010-01-05 01:14:38 +0000

    Cosmetics: Fix indentation and comment after r21027.
    Originally committed as revision 21028 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/rawdec.c
+++ libavcodec/rawdec.c
@@ -115,7 +115,7 @@ static int raw_decode(AVCodecContext *avctx,
     frame->interlaced_frame = avctx->coded_frame->interlaced_frame;
     frame->top_field_first = avctx->coded_frame->top_field_first;
 
-    //4bpp raw in avi and mov (yes this is ugly ...)
+    //2bpp and 4bpp raw in avi and mov (yes this is ugly ...)
     if((avctx->bits_per_coded_sample == 4 || avctx->bits_per_coded_sample == 2) &&
        avctx->pix_fmt==PIX_FMT_PAL8 &&
        (!avctx->codec_tag || avctx->codec_tag == MKTAG('r','a','w',' '))){
@@ -123,10 +123,10 @@ static int raw_decode(AVCodecContext *avctx,
         uint8_t *dst = context->buffer + 256*4;
         buf_size = context->length - 256*4;
         if (avctx->bits_per_coded_sample == 4){
-        for(i=0; 2*i+1 < buf_size; i++){
-            dst[2*i+0]= buf[i]>>4;
-            dst[2*i+1]= buf[i]&15;
-        }
+            for(i=0; 2*i+1 < buf_size; i++){
+                dst[2*i+0]= buf[i]>>4;
+                dst[2*i+1]= buf[i]&15;
+            }
         } else
             for(i=0; 4*i+3 < buf_size; i++){
                 dst[4*i+0]= buf[i]>>6;
