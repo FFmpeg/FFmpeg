2efcf292750a1bc99ae65912ee6987995314c809
Michael Niedermayer <michaelni@gmx.at>
2010-01-06 22:40:52 +0000

    Stop the approximation in bessel() once it does no longer improve. This is faster.
    Originally committed as revision 21038 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/resample2.c
+++ libavcodec/resample2.c
@@ -76,11 +76,13 @@ typedef struct AVResampleContext{
  */
 static double bessel(double x){
     double v=1;
+    double lastv=0;
     double t=1;
     int i;
 
     x= x*x/4;
-    for(i=1; i<50; i++){
+    for(i=1; v != lastv; i++){
+        lastv=v;
         t *= x/(i*i);
         v += t;
     }
