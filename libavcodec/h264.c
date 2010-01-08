4ad04da2142e6a38aebee0960152488f6820f440
Andreas Öman <andreas@lonelycoder.com>
2010-01-08 15:50:26 +0000

    Log unknown NAL code from correct context
    Originally committed as revision 21090 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/h264.c
+++ libavcodec/h264.c
@@ -7656,7 +7656,7 @@ static int decode_nal_units(H264Context *h, const uint8_t *buf, int buf_size){
         case NAL_AUXILIARY_SLICE:
             break;
         default:
-            av_log(avctx, AV_LOG_DEBUG, "Unknown NAL code: %d (%d bits)\n", h->nal_unit_type, bit_length);
+            av_log(avctx, AV_LOG_DEBUG, "Unknown NAL code: %d (%d bits)\n", hx->nal_unit_type, bit_length);
         }
 
         if(context_count == h->max_contexts) {
