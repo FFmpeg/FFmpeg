15ccfff12cc5a834ba13a0b45cc44163734b5cb4
Ramiro Polla <ramiro.polla@gmail.com>
2010-01-16 18:29:52 +0000

    Read "Last Changed Rev" field instead of "Revision" field to avoid being affected by changes in other directories (such as branches).
    Originally committed as revision 21252 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- version.sh
+++ version.sh
@@ -2,8 +2,8 @@
 
 # check for SVN revision number
 revision=$(cd "$1" && cat snapshot_version 2> /dev/null)
-test $revision || revision=$(cd "$1" && LC_ALL=C svn info 2> /dev/null | grep Revision | cut -d' ' -f2)
-test $revision || revision=$(cd "$1" && grep revision .svn/entries 2>/dev/null | cut -d '"' -f2)
+test $revision || revision=$(cd "$1" && LC_ALL=C svn info 2> /dev/null | grep "Last Changed Rev" | cut -d' ' -f4)
+test $revision || revision=$(cd "$1" && grep committed-rev .svn/entries 2>/dev/null | head -n 1 | cut -d '"' -f2)
 test $revision || revision=$(cd "$1" && sed -n -e '/^dir$/{n
 p
 q
