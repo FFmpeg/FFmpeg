b5eab66e9fe6c93056e414b0b4a70a34948843e1
Stefano Sabatini <stefano.sabatini-lala@poste.it>
2010-01-16 10:10:14 +0000

    Make avfilter_default_start_frame() request a buffer with the same size of the output link of the destination filter, rather than of the size of the input link. In particular this fixes a crash in the rotate filter.
    Originally committed as revision 21238 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavfilter/defaults.c
+++ libavfilter/defaults.c
@@ -72,7 +72,7 @@ void avfilter_default_start_frame(AVFilterLink *link, AVFilterPicRef *picref)
         out = link->dst->outputs[0];
 
     if(out) {
-        out->outpic      = avfilter_get_video_buffer(out, AV_PERM_WRITE, link->w, link->h);
+        out->outpic      = avfilter_get_video_buffer(out, AV_PERM_WRITE, out->w, out->h);
         out->outpic->pts = picref->pts;
         avfilter_start_frame(out, avfilter_ref_pic(out->outpic, ~0));
     }
