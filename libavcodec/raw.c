a4be782cbde337ced47b04e2e6f79ba99e79ae1d
ami_stuff <ami_stuff@o2.pl>
2010-01-16 00:32:09 +0000

    Support 2Vuy in mov, fixes issue 1690.
    Patch by ami_stuff, ami_stuff o2 pl
    
    Originally committed as revision 21230 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/raw.c
+++ libavcodec/raw.c
@@ -63,6 +63,7 @@ const PixelFormatTag ff_raw_pixelFormatTags[] = {
 
     /* quicktime */
     { PIX_FMT_UYVY422, MKTAG('2', 'v', 'u', 'y') },
+    { PIX_FMT_UYVY422, MKTAG('2', 'V', 'u', 'y') },
     { PIX_FMT_UYVY422, MKTAG('A', 'V', 'U', 'I') }, /* FIXME merge both fields */
     { PIX_FMT_YUYV422, MKTAG('y', 'u', 'v', '2') },
     { PIX_FMT_YUYV422, MKTAG('y', 'u', 'v', 's') },
