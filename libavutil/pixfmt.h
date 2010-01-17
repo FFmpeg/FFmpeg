3f37ab8e4162db146b8959625b3c90b7b732ea60
Reimar Döffinger <Reimar.Doeffinger@gmx.de>
2010-01-17 14:17:03 +0000

    Make sure we do not export incorrect _NE pixformat descriptions by either including config.h if available or not defining them if it isn't.
    Originally committed as revision 21262 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavutil/pixfmt.h
+++ libavutil/pixfmt.h
@@ -127,6 +127,8 @@ enum PixelFormat {
     PIX_FMT_NB,        ///< number of pixel formats, DO NOT USE THIS if you want to link with shared libav* because the number of formats might differ between versions
 };
 
+#ifdef HAVE_AV_CONFIG_H
+#include "config.h"
 #if HAVE_BIGENDIAN
 #   define PIX_FMT_NE(be, le) PIX_FMT_##be
 #else
@@ -148,5 +150,6 @@ enum PixelFormat {
 #define PIX_FMT_YUV420P16 PIX_FMT_NE(YUV420P16BE, YUV420P16LE)
 #define PIX_FMT_YUV422P16 PIX_FMT_NE(YUV422P16BE, YUV422P16LE)
 #define PIX_FMT_YUV444P16 PIX_FMT_NE(YUV444P16BE, YUV444P16LE)
+#endif
 
 #endif /* AVUTIL_PIXFMT_H */
