ade5b91ddade9abaf9eb1063559985341ee9ca2d
Stefano Sabatini <stefano.sabatini-lala@poste.it>
2010-01-17 15:57:51 +0000

    Use av_match_ext() rather than the deprecated match_ext(), fix warnings.
    Originally committed as revision 21263 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/movenc.c
+++ libavformat/movenc.c
@@ -647,7 +647,7 @@ static int ipod_get_codec_tag(AVFormatContext *s, MOVTrack *track)
          tag == MKTAG('t','e','x','t'))))
         tag = ff_codec_get_tag(codec_ipod_tags, track->enc->codec_id);
 
-    if (!match_ext(s->filename, "m4a") && !match_ext(s->filename, "m4v"))
+    if (!av_match_ext(s->filename, "m4a") && !av_match_ext(s->filename, "m4v"))
         av_log(s, AV_LOG_WARNING, "Warning, extension is not .m4a nor .m4v "
                "Quicktime/Ipod might not play the file\n");
 
