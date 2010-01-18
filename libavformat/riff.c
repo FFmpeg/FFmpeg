5bc3fc2187e6186cfeb3cac193ab99b3045e9f58
Ben Littler <tempn@twmi.rr.com>
2010-01-18 03:57:01 +0000

    add uldx fourcc to mpeg4 in riff.c
    Originally committed as revision 21279 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/riff.c
+++ libavformat/riff.c
@@ -78,6 +78,7 @@ const AVCodecTag ff_codec_bmp_tags[] = {
     { CODEC_ID_MPEG4,        MKTAG('M', '4', 'C', 'C') }, /* Divio MPEG-4 */
     { CODEC_ID_MPEG4,        MKTAG('S', 'N', '4', '0') },
     { CODEC_ID_MPEG4,        MKTAG('V', 'S', 'P', 'X') },
+    { CODEC_ID_MPEG4,        MKTAG('U', 'L', 'D', 'X') },
     { CODEC_ID_MSMPEG4V3,    MKTAG('D', 'I', 'V', '3') }, /* default signature when using MSMPEG4 */
     { CODEC_ID_MSMPEG4V3,    MKTAG('M', 'P', '4', '3') },
     { CODEC_ID_MSMPEG4V3,    MKTAG('M', 'P', 'G', '3') },
