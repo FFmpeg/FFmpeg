d5206d3dd636264f5750eb8888ebccbadc34e33b
Zuxy Meng <zuxy.meng@gmail.com>
2010-01-19 05:21:17 +0000

    User friendly warning message that gives out names of source and target formats
    Originally committed as revision 30370 to svn://svn.mplayerhq.hu/mplayer/trunk/libswscale

--- libswscale/yuv2rgb.c
+++ libswscale/yuv2rgb.c
@@ -530,7 +530,7 @@ SwsFunc ff_yuv2rgb_get_func_ptr(SwsContext *c)
     if (t)
         return t;
 
-    av_log(c, AV_LOG_WARNING, "No accelerated colorspace conversion found.\n");
+    av_log(c, AV_LOG_WARNING, "No accelerated colorspace conversion found from %s to %s.\n", sws_format_name(c->srcFormat), sws_format_name(c->dstFormat));
 
     switch (c->dstFormat) {
     case PIX_FMT_RGB48BE:
