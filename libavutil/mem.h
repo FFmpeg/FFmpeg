8a24e98d506f0f44ec58e06291fa0fce703fb6a8
Måns Rullgård <mans@mansr.com>
2010-01-21 12:59:22 +0000

    Make DECLARE_ALIGNED macros work with external array specifiers
    The macro implementation might need the name of the variable being
    declared for compiler-specific syntax.  Moving array specifiers outside
    the macro invocation allows this to work.
    
    Originally committed as revision 21363 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavutil/mem.h
+++ libavutil/mem.h
@@ -29,11 +29,11 @@
 #include "common.h"
 
 #if defined(__ICC) || defined(__SUNPRO_C)
-    #define DECLARE_ALIGNED(n,t,v)      t v __attribute__ ((aligned (n)))
+    #define DECLARE_ALIGNED(n,t,v)      t __attribute__ ((aligned (n))) v
     #define DECLARE_ASM_CONST(n,t,v)    const t __attribute__ ((aligned (n))) v
 #elif defined(__GNUC__)
-    #define DECLARE_ALIGNED(n,t,v)      t v __attribute__ ((aligned (n)))
-    #define DECLARE_ASM_CONST(n,t,v)    static const t v attribute_used __attribute__ ((aligned (n)))
+    #define DECLARE_ALIGNED(n,t,v)      t __attribute__ ((aligned (n))) v
+    #define DECLARE_ASM_CONST(n,t,v)    static const t attribute_used __attribute__ ((aligned (n))) v
 #elif defined(_MSC_VER)
     #define DECLARE_ALIGNED(n,t,v)      __declspec(align(n)) t v
     #define DECLARE_ASM_CONST(n,t,v)    __declspec(align(n)) static const t v
