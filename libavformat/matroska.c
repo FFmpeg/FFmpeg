39c6b3f54c6affb2db6bd021185b874dd05496d6
Carl Eugen Hoyos <cehoyos@rainbow.studorg.tuwien.ac.at>
2010-01-27 22:39:18 +0000

    Support demuxing Blu-ray subtitles in matroska files.
    Patch sent to mailing list by Taylor Ralph, taylor D ralph A gmail
    
    Originally committed as revision 21491 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/matroska.c
+++ libavformat/matroska.c
@@ -58,6 +58,7 @@ const CodecTags ff_mkv_codec_tags[]={
     {"S_ASS"            , CODEC_ID_SSA},
     {"S_SSA"            , CODEC_ID_SSA},
     {"S_VOBSUB"         , CODEC_ID_DVD_SUBTITLE},
+    {"S_HDMV/PGS"       , CODEC_ID_HDMV_PGS_SUBTITLE},
 
     {"V_DIRAC"          , CODEC_ID_DIRAC},
     {"V_MJPEG"          , CODEC_ID_MJPEG},
