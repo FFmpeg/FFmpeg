2cf0d46d4c3e4590086ec29860772ea54ccd98e6
Michael Niedermayer <michaelni@gmx.at>
2010-01-28 11:12:46 +0000

    Restructure check_mv() ~20 cpu cycles faster loopfilter
    Originally committed as revision 21505 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/h264_loopfilter.c
+++ libavcodec/h264_loopfilter.c
@@ -439,14 +439,13 @@ static int check_mv(H264Context *h, long b_idx, long bn_idx, int mvy_limit){
 
     if(v){
         if(h->ref_cache[0][b_idx] != h->ref_cache[1][bn_idx] |
+           h->ref_cache[1][b_idx] != h->ref_cache[0][bn_idx])
+            return 1;
+        return
            h->mv_cache[0][b_idx][0] - h->mv_cache[1][bn_idx][0] + 3 >= 7U |
-           FFABS( h->mv_cache[0][b_idx][1] - h->mv_cache[1][bn_idx][1] ) >= mvy_limit)
-            return 1;
-        if(h->ref_cache[1][b_idx] != h->ref_cache[0][bn_idx] |
+           FFABS( h->mv_cache[0][b_idx][1] - h->mv_cache[1][bn_idx][1] ) >= mvy_limit |
            h->mv_cache[1][b_idx][0] - h->mv_cache[0][bn_idx][0] + 3 >= 7U |
-           FFABS( h->mv_cache[1][b_idx][1] - h->mv_cache[0][bn_idx][1] ) >= mvy_limit)
-            return 1;
-        return 0;
+           FFABS( h->mv_cache[1][b_idx][1] - h->mv_cache[0][bn_idx][1] ) >= mvy_limit;
     }
     }
 
