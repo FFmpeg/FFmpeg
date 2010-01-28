5364db2893eddb27e5b93436c20ba52b1c406511
Michael Niedermayer <michaelni@gmx.at>
2010-01-28 11:18:06 +0000

    indent
    Originally committed as revision 21506 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/h264_loopfilter.c
+++ libavcodec/h264_loopfilter.c
@@ -433,20 +433,20 @@ static int check_mv(H264Context *h, long b_idx, long bn_idx, int mvy_limit){
 
     if(h->list_count==2){
         if(!v)
-        v = h->ref_cache[1][b_idx] != h->ref_cache[1][bn_idx] |
-            h->mv_cache[1][b_idx][0] - h->mv_cache[1][bn_idx][0] + 3 >= 7U |
-            FFABS( h->mv_cache[1][b_idx][1] - h->mv_cache[1][bn_idx][1] ) >= mvy_limit;
+            v = h->ref_cache[1][b_idx] != h->ref_cache[1][bn_idx] |
+                h->mv_cache[1][b_idx][0] - h->mv_cache[1][bn_idx][0] + 3 >= 7U |
+                FFABS( h->mv_cache[1][b_idx][1] - h->mv_cache[1][bn_idx][1] ) >= mvy_limit;
 
-    if(v){
-        if(h->ref_cache[0][b_idx] != h->ref_cache[1][bn_idx] |
-           h->ref_cache[1][b_idx] != h->ref_cache[0][bn_idx])
-            return 1;
-        return
-           h->mv_cache[0][b_idx][0] - h->mv_cache[1][bn_idx][0] + 3 >= 7U |
-           FFABS( h->mv_cache[0][b_idx][1] - h->mv_cache[1][bn_idx][1] ) >= mvy_limit |
-           h->mv_cache[1][b_idx][0] - h->mv_cache[0][bn_idx][0] + 3 >= 7U |
-           FFABS( h->mv_cache[1][b_idx][1] - h->mv_cache[0][bn_idx][1] ) >= mvy_limit;
-    }
+        if(v){
+            if(h->ref_cache[0][b_idx] != h->ref_cache[1][bn_idx] |
+               h->ref_cache[1][b_idx] != h->ref_cache[0][bn_idx])
+                return 1;
+            return
+                h->mv_cache[0][b_idx][0] - h->mv_cache[1][bn_idx][0] + 3 >= 7U |
+                FFABS( h->mv_cache[0][b_idx][1] - h->mv_cache[1][bn_idx][1] ) >= mvy_limit |
+                h->mv_cache[1][b_idx][0] - h->mv_cache[0][bn_idx][0] + 3 >= 7U |
+                FFABS( h->mv_cache[1][b_idx][1] - h->mv_cache[0][bn_idx][1] ) >= mvy_limit;
+        }
     }
 
     return v;
