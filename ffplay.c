967030eb24074919f0363e53823cf54d89d12dda
Michael Niedermayer <michaelni@gmx.at>
2010-02-01 12:27:26 +0000

    Reset frame_last_pts on flush (and thus also at start) fixes issue558 and probably others.
    Originally committed as revision 21590 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- ffplay.c
+++ ffplay.c
@@ -1362,6 +1362,8 @@ static int video_thread(void *arg)
 
             is->last_dts_for_fault_detection=
             is->last_pts_for_fault_detection= INT64_MIN;
+            is->frame_last_pts= AV_NOPTS_VALUE;
+
             continue;
         }
 
