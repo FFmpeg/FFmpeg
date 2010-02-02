3890dd3a5c0e375ee7ea23be39b363aa2f361c89
Michael Niedermayer <michaelni@gmx.at>
2010-02-02 16:01:22 +0000

    Fix seek flags
    Originally committed as revision 21603 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- ffplay.c
+++ ffplay.c
@@ -985,6 +985,7 @@ static void stream_seek(VideoState *is, int64_t pos, int64_t rel, int seek_by_by
     if (!is->seek_req) {
         is->seek_pos = pos;
         is->seek_rel = rel;
+        is->seek_flags &= ~AVSEEK_FLAG_BYTE;
         if (seek_by_bytes)
             is->seek_flags |= AVSEEK_FLAG_BYTE;
         is->seek_req = 1;
