b7e0f88f2b3bb36a7a0ad1417e90ea6409b7d685
Daniel Verkamp <daniel@drv.nu>
2010-02-10 20:26:47 +0000

    Fix demuxing of wav files with broken data header
    Originally committed as revision 21746 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/wav.c
+++ libavformat/wav.c
@@ -227,6 +227,9 @@ static int wav_read_header(AVFormatContext *s,
         size = data_size;
     if (size < 0)
         return -1;
+    if (!size) {
+        wav->data_end = INT64_MAX;
+    } else
     wav->data_end= url_ftell(pb) + size;
     return 0;
 }
