4689ac41e9d3bb3627bbbd1b95264a385baab73e
Kostya Shishkov <kostya.shishkov@gmail.com>
2010-02-19 22:23:55 +0000

    16l trocadero: don't forget to free frame data buffer in APE decoder
    Originally committed as revision 21910 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/apedec.c
+++ libavcodec/apedec.c
@@ -211,6 +211,7 @@ static av_cold int ape_decode_close(AVCodecContext * avctx)
     for (i = 0; i < APE_FILTER_LEVELS; i++)
         av_freep(&s->filterbuf[i]);
 
+    av_freep(&s->data);
     return 0;
 }
 
