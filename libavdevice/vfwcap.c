d7a4961e5353ff983bd7b53acf56ab8076dfaeba
Nash Tsai <nash.tsai@gmail.com>
2010-02-23 16:49:16 +0000

    vfwcap: support MJPG compressed streams. Patch by Nash Tsai <nash dot tsai at gmail dot com>
    Originally committed as revision 22004 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavdevice/vfwcap.c
+++ libavdevice/vfwcap.c
@@ -74,6 +74,9 @@ static enum CodecID vfw_codecid(DWORD biCompression)
     switch(biCompression) {
     case MKTAG('d', 'v', 's', 'd'):
         return CODEC_ID_DVVIDEO;
+    case MKTAG('M', 'J', 'P', 'G'):
+    case MKTAG('m', 'j', 'p', 'g'):
+        return CODEC_ID_MJPEG;
     }
     return CODEC_ID_NONE;
 }
