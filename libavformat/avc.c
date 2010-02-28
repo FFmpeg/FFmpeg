dda8fc6a097ec9e8e381003f90b84eba68af4614
Carl Eugen Hoyos <cehoyos@rainbow.studorg.tuwien.ac.at>
2010-02-28 02:04:34 +0000

    Include avc.h from avc.c: It contains several prototypes.
    Originally committed as revision 22098 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/avc.c
+++ libavformat/avc.c
@@ -22,6 +22,7 @@
 #include "libavutil/intreadwrite.h"
 #include "avformat.h"
 #include "avio.h"
+#include "avc.h"
 
 static const uint8_t *ff_avc_find_startcode_internal(const uint8_t *p, const uint8_t *end)
 {
