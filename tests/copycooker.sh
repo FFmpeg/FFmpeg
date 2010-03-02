88b83cac2436ed5afdaa42a46a1bf8f8915505b8
Michael Niedermayer <michaelni@gmx.at>
2010-03-02 16:57:20 +0000

    Codec copy test.
    Originally committed as revision 22148 to svn://svn.ffmpeg.org/ffmpeg/trunk

---
+++ tests/copycooker.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+LC_ALL=C
+export LC_ALL
+
+datadir="tests/data"
+
+logfile="$datadir/copy.regression"
+reffile="$1"
+
+list=$(grep -oh ' ./tests/data/.*' tests/ref/*/* "$reffile")
+rm -f $logfile
+for i in $list ; do
+    echo ---------------- >> $logfile
+    echo $i >> $logfile
+    ./ffmpeg_g -flags +bitexact -i $i -acodec copy -vcodec copy -y first.nut
+    ./ffmpeg_g -flags +bitexact -i first.nut -acodec copy -vcodec copy -y second.nut
+    cmp first.nut second.nut >> $logfile
+    md5sum first.nut >> $logfile
+done
+
+if diff -u -w "$reffile" "$logfile" ; then
+    echo
+    echo copy regression test: success
+    exit 0
+else
+    echo
+    echo copy regression test: error
+    exit 1
+fi
