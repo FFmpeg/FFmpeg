11ae0c52b93652328d356d9316789b830e785175
Gwenole Beauchesne <gbeauchesne@splitted-desktop.com>
2010-03-04 09:14:19 +0000

    Cope with rev 22183: Reorder indexes in weight tables.
    Originally committed as revision 22202 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/vaapi_h264.c
+++ libavcodec/vaapi_h264.c
@@ -198,16 +198,16 @@ static void fill_vaapi_plain_pred_weight_table(H264Context   *h,
         /* VA API also wants the inferred (default) values, not
            only what is available in the bitstream (7.4.3.2). */
         if (h->luma_weight_flag[list]) {
-            luma_weight[i] = h->luma_weight[list][i][0];
-            luma_offset[i] = h->luma_weight[list][i][1];
+            luma_weight[i] = h->luma_weight[i][list][0];
+            luma_offset[i] = h->luma_weight[i][list][1];
         } else {
             luma_weight[i] = 1 << h->luma_log2_weight_denom;
             luma_offset[i] = 0;
         }
         for (j = 0; j < 2; j++) {
             if (h->chroma_weight_flag[list]) {
-                chroma_weight[i][j] = h->chroma_weight[list][i][j][0];
-                chroma_offset[i][j] = h->chroma_weight[list][i][j][1];
+                chroma_weight[i][j] = h->chroma_weight[i][list][j][0];
+                chroma_offset[i][j] = h->chroma_weight[i][list][j][1];
             } else {
                 chroma_weight[i][j] = 1 << h->chroma_log2_weight_denom;
                 chroma_offset[i][j] = 0;
