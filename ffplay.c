ff358eca17b9182a0ce45bd9b7b5a52f55aaa52e
Stefano Sabatini <stefano.sabatini-lala@poste.it>
2010-03-06 19:19:13 +0000

    Reindent after the last commit.
    Originally committed as revision 22248 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- ffplay.c
+++ ffplay.c
@@ -1442,8 +1442,8 @@ static int output_picture2(VideoState *is, AVFrame *src_frame, double pts1, int6
     is->video_clock += frame_delay;
 
 #if defined(DEBUG_SYNC) && 0
-        printf("frame_type=%c clock=%0.3f pts=%0.3f\n",
-               av_get_pict_type_char(src_frame->pict_type), pts, pts1);
+    printf("frame_type=%c clock=%0.3f pts=%0.3f\n",
+           av_get_pict_type_char(src_frame->pict_type), pts, pts1);
 #endif
     return queue_picture(is, src_frame, pts, pos);
 }
