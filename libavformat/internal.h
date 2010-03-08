f19341e17a0ece29613cc583daaee6ec58aea9c5
Måns Rullgård <mans@mansr.com>
2010-03-08 03:46:37 +0000

    Revert "Move the probe loop from av_open_input_file() into its own method"
    This reverts r22296.  This change made some files to fail to open.
    The patch submitter has promised to investigate next week.
    
    Originally committed as revision 22315 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/internal.h
+++ libavformat/internal.h
@@ -37,24 +37,4 @@ void ff_interleave_add_packet(AVFormatContext *s, AVPacket *pkt,
 
 void av_read_frame_flush(AVFormatContext *s);
 
-/**
- * Probes a bytestream to determine the input format. Each time a
- * probe returns with a score that is too low, the probe buffer size
- * is increased and another attempt is made. When the maximum probe
- * size is reached, the input format with the highest score is
- * returned.
- *
- * @param pb the bytestream to probe, it may be closed and opened again
- * @param fmt the input format is put here
- * @param filename the filename of the stream
- * @param logctx the log context
- * @param offset the offset within the bytestream to probe from
- * @param max_probe_size the maximum probe buffer size (zero for default)
- * @return 0 if OK, a negative value corresponding to an AVERROR(xxx)
- * code otherwise
- */
-int ff_probe_input_buffer(ByteIOContext **pb, AVInputFormat **fmt,
-                          const char *filename, void *logctx,
-                          unsigned int offset, unsigned int max_probe_size);
-
 #endif /* AVFORMAT_INTERNAL_H */
