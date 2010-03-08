9169174fd6101490f391bc5879ded1efd7880db6
Baptiste Coudurier <baptiste.coudurier@gmail.com>
2010-03-08 20:57:42 +0000

    10l typo, fix ts total bit rate computation
    Originally committed as revision 22343 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/mpegtsenc.c
+++ libavformat/mpegtsenc.c
@@ -476,8 +476,8 @@ static int mpegts_write_header(AVFormatContext *s)
 
     total_bit_rate +=
         total_bit_rate * 4 / (TS_PACKET_SIZE-4)        + /* TS header size */
-        1000 * 8 * sdt_size     / PAT_RETRANS_TIME     + /* SDT size */
-        1000 * 8 * pat_pmt_size / SDT_RETRANS_TIME     + /* PAT+PMT size */
+        1000 * 8 * sdt_size     / SDT_RETRANS_TIME     + /* SDT size */
+        1000 * 8 * pat_pmt_size / PAT_RETRANS_TIME     + /* PAT+PMT size */
         1000 * 8 * 8            / PCR_RETRANS_TIME;      /* PCR size */
 
     if (s->mux_rate)
