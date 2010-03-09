1307463d5259a80fdbb43f50434eb9ba37c50d30
Baptiste Coudurier <baptiste.coudurier@gmail.com>
2010-03-09 00:37:13 +0000

    Fix interlaced vc-3 decoding, issue #1753
    Originally committed as revision 22362 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/dnxhddec.c
+++ libavcodec/dnxhddec.c
@@ -126,12 +126,17 @@ static int dnxhd_decode_header(DNXHDContext *ctx, const uint8_t *buf, int buf_si
     ctx->mb_width = ctx->width>>4;
     ctx->mb_height = buf[0x16d];
 
-    if (ctx->mb_height > 68) {
-        av_log(ctx->avctx, AV_LOG_ERROR, "mb height too big\n");
+    dprintf(ctx->avctx, "mb width %d, mb height %d\n", ctx->mb_width, ctx->mb_height);
+
+    if ((ctx->height+15)>>4 == ctx->mb_height && ctx->picture.interlaced_frame)
+        ctx->height <<= 1;
+
+    if (ctx->mb_height > 68 ||
+        (ctx->mb_height<<ctx->picture.interlaced_frame) > (ctx->height+15)>>4) {
+        av_log(ctx->avctx, AV_LOG_ERROR, "mb height too big: %d\n", ctx->mb_height);
         return -1;
     }
 
-    dprintf(ctx->avctx, "mb width %d, mb height %d\n", ctx->mb_width, ctx->mb_height);
     for (i = 0; i < ctx->mb_height; i++) {
         ctx->mb_scan_index[i] = AV_RB32(buf + 0x170 + (i<<2));
         dprintf(ctx->avctx, "mb scan index %d\n", ctx->mb_scan_index[i]);
@@ -291,6 +296,13 @@ static int dnxhd_decode_frame(AVCodecContext *avctx, void *data, int *data_size,
  decode_coding_unit:
     if (dnxhd_decode_header(ctx, buf, buf_size, first_field) < 0)
         return -1;
+
+    if ((avctx->width || avctx->height) &&
+        (ctx->width != avctx->width || ctx->height != avctx->height)) {
+        av_log(avctx, AV_LOG_WARNING, "frame size changed: %dx%d -> %dx%d\n",
+               avctx->width, avctx->height, ctx->width, ctx->height);
+        first_field = 1;
+    }
 
     avctx->pix_fmt = PIX_FMT_YUV422P;
     if (avcodec_check_dimensions(avctx, ctx->width, ctx->height))
