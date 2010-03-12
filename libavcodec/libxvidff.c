2325bdad7b67b1c8539ef6beebb99d3247f08669
Aurelien Jacobs <aurel@gnuage.org>
2010-03-12 23:24:54 +0000

    libxvid is producing standard mpeg4 so it don't need its own codec id
    Originally committed as revision 22489 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/libxvidff.c
+++ libavcodec/libxvidff.c
@@ -770,7 +770,7 @@ int xvid_ff_2pass(void *ref, int cmd, void *p1, void *p2) {
 AVCodec libxvid_encoder = {
     "libxvid",
     CODEC_TYPE_VIDEO,
-    CODEC_ID_XVID,
+    CODEC_ID_MPEG4,
     sizeof(struct xvid_context),
     xvid_encode_init,
     xvid_encode_frame,
