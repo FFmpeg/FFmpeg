e88c9dac07f71c981cba5fa6bfa2d0c977aa7c5a
Bobby Bingham <uhmmmm@gmail.com>
2010-03-17 04:37:43 +0000

    Cosmetics: vertical align
    Originally committed as revision 22578 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavfilter/allfilters.c
+++ libavfilter/allfilters.c
@@ -34,17 +34,17 @@ void avfilter_register_all(void)
         return;
     initialized = 1;
 
-    REGISTER_FILTER (ASPECT, aspect, vf);
-    REGISTER_FILTER (CROP, crop, vf);
-    REGISTER_FILTER (FORMAT, format, vf);
-    REGISTER_FILTER (NOFORMAT, noformat, vf);
-    REGISTER_FILTER (NULL, null, vf);
+    REGISTER_FILTER (ASPECT,      aspect,      vf);
+    REGISTER_FILTER (CROP,        crop,        vf);
+    REGISTER_FILTER (FORMAT,      format,      vf);
+    REGISTER_FILTER (NOFORMAT,    noformat,    vf);
+    REGISTER_FILTER (NULL,        null,        vf);
     REGISTER_FILTER (PIXELASPECT, pixelaspect, vf);
-    REGISTER_FILTER (SCALE, scale, vf);
-    REGISTER_FILTER (SLICIFY, slicify, vf);
-    REGISTER_FILTER (VFLIP, vflip, vf);
+    REGISTER_FILTER (SCALE,       scale,       vf);
+    REGISTER_FILTER (SLICIFY,     slicify,     vf);
+    REGISTER_FILTER (VFLIP,       vflip,       vf);
 
-    REGISTER_FILTER (NULLSRC, nullsrc, vsrc);
+    REGISTER_FILTER (NULLSRC,     nullsrc,     vsrc);
 
-    REGISTER_FILTER (NULLSINK, nullsink, vsink);
+    REGISTER_FILTER (NULLSINK,    nullsink,    vsink);
 }
