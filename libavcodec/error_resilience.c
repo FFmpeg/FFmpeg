3546fa8d879d2eda2afd51afec1569f4e9782e2b
Aurelien Jacobs <aurel@gnuage.org>
2010-03-20 23:18:07 +0000

    fix compilation with --disable-everything --enable-decoder=mpeg2video
    Originally committed as revision 22612 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/error_resilience.c
+++ libavcodec/error_resilience.c
@@ -43,7 +43,7 @@ static void decode_mb(MpegEncContext *s, int ref){
     s->dest[1] = s->current_picture.data[1] + (s->mb_y * (16>>s->chroma_y_shift) * s->uvlinesize) + s->mb_x * (16>>s->chroma_x_shift);
     s->dest[2] = s->current_picture.data[2] + (s->mb_y * (16>>s->chroma_y_shift) * s->uvlinesize) + s->mb_x * (16>>s->chroma_x_shift);
 
-    if(s->codec_id == CODEC_ID_H264){
+    if(CONFIG_H264_DECODER && s->codec_id == CODEC_ID_H264){
         H264Context *h= (void*)s;
         h->mb_xy= s->mb_x + s->mb_y*s->mb_stride;
         memset(h->non_zero_count_cache, 0, sizeof(h->non_zero_count_cache));
