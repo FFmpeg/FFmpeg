ef768b0c326cc9a6208b5021e648edb33188e503
Michael Niedermayer <michaelni@gmx.at>
2010-03-22 21:12:31 +0000

    Disallow VLC coding with more than 8 bits as there are several bugs in that code that could lead to broken files. AC coding is unaffected.
    Originally committed as revision 22638 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/ffv1.c
+++ libavcodec/ffv1.c
@@ -673,6 +673,10 @@ static av_cold int encode_init(AVCodecContext *avctx)
             av_log(avctx, AV_LOG_ERROR, "bits_per_raw_sample invalid\n");
             return -1;
         }
+        if(!s->ac){
+            av_log(avctx, AV_LOG_ERROR, "bits_per_raw_sample of more than 8 needs -coder 1 currently\n");
+            return -1;
+        }
         s->version= 1;
     case PIX_FMT_YUV444P:
     case PIX_FMT_YUV422P:
