3bfb30b9e31dbc741501e7511b02cab1963e83c7
Vitor Sessak <vitor1001@gmail.com>
2010-03-24 22:44:06 +0000

    Fix warnings in NUT demuxer:
    libavformat/nutdec.c: In function ‘read_seek’:
    libavformat/nutdec.c:862: warning: passing argument 4 of ‘av_tree_find’ from incompatible pointer type
    ./libavutil/tree.h:44: note: expected ‘void **’ but argument is of type ‘struct Syncpoint **’
    libavformat/nutdec.c:871: warning: passing argument 4 of ‘av_tree_find’ from incompatible pointer type
    ./libavutil/tree.h:44: note: expected ‘void **’ but argument is of type ‘struct Syncpoint **’
    
    Originally committed as revision 22662 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/nutdec.c
+++ libavformat/nutdec.c
@@ -859,7 +859,7 @@ static int read_seek(AVFormatContext *s, int stream_index, int64_t pts, int flag
         pos2= st->index_entries[index].pos;
         ts  = st->index_entries[index].timestamp;
     }else{
-        av_tree_find(nut->syncpoints, &dummy, ff_nut_sp_pts_cmp, next_node);
+        av_tree_find(nut->syncpoints, &dummy, ff_nut_sp_pts_cmp, (void **) next_node);
         av_log(s, AV_LOG_DEBUG, "%"PRIu64"-%"PRIu64" %"PRId64"-%"PRId64"\n", next_node[0]->pos, next_node[1]->pos,
                                                     next_node[0]->ts , next_node[1]->ts);
         pos= av_gen_search(s, -1, dummy.ts, next_node[0]->pos, next_node[1]->pos, next_node[1]->pos,
@@ -868,7 +868,7 @@ static int read_seek(AVFormatContext *s, int stream_index, int64_t pts, int flag
         if(!(flags & AVSEEK_FLAG_BACKWARD)){
             dummy.pos= pos+16;
             next_node[1]= &nopts_sp;
-            av_tree_find(nut->syncpoints, &dummy, ff_nut_sp_pos_cmp, next_node);
+            av_tree_find(nut->syncpoints, &dummy, ff_nut_sp_pos_cmp, (void **) next_node);
             pos2= av_gen_search(s, -2, dummy.pos, next_node[0]->pos     , next_node[1]->pos, next_node[1]->pos,
                                                 next_node[0]->back_ptr, next_node[1]->back_ptr, flags, &ts, nut_read_timestamp);
             if(pos2>=0)
