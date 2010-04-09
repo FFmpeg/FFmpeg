b9f60bd8fdc3b1de67d0ee061b24ea0dcd135bd0
Alex Converse <alex.converse@gmail.com>
2010-04-09 23:29:47 +0000

    Add M_SQRT2 to the constants in mathematics.h.
    Originally committed as revision 22825 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavutil/mathematics.h
+++ libavutil/mathematics.h
@@ -44,6 +44,9 @@
 #ifndef M_SQRT1_2
 #define M_SQRT1_2      0.70710678118654752440  /* 1/sqrt(2) */
 #endif
+#ifndef M_SQRT2
+#define M_SQRT2        1.41421356237309504880  /* sqrt(2) */
+#endif
 #ifndef NAN
 #define NAN            (0.0/0.0)
 #endif
