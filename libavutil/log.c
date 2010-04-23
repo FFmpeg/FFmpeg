841073ce5ecaceb4c74410887095632d7cc5ef6b
Michael Niedermayer <michaelni@gmx.at>
2010-04-23 07:33:02 +0000

    Reenable ANSI colors, use method from VLC as suggested by ramiro. Please tell us asap if this breaks for your platform & terminal.
    Originally committed as revision 22951 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavutil/log.c
+++ libavutil/log.c
@@ -33,18 +33,18 @@ static
 #endif
 int av_log_level = AV_LOG_INFO;
 
-#if !HAVE_ISATTY
+#if (!HAVE_ISATTY) || defined(WIN32)
 #define isatty(s) 0
 #endif
 
 #undef fprintf
 static void colored_fputs(int color, const char *str){
     if(isatty(2)){
-//        fprintf(stderr, "\033[%dm\033[3%dm", color>>4, color&15);
+        fprintf(stderr, "\033[%dm\033[3%dm", color>>4, color&15);
     }
     fputs(str, stderr);
     if(isatty(2)){
-  //      fprintf(stderr, "\033[0m");
+        fprintf(stderr, "\033[0m");
     }
 }
 
