FFmpeg/FFmpeg

commit 8ca618b8fd348648fd644f8bd6f731342b2ea157

Support VP6F in Matroska.
Patch by Christian Schmidt, schmidt digadd de 


git-svn-id: svn://svn.mplayerhq.hu/ffmpeg/trunk@22941 9553f0bf-9b14-0410-a0b8-cfaf0461ba5b

libavformat/riff.c
@@ -180,6 +180,7 @@ const AVCodecTag ff_codec_bmp_tags[] = {
     { CODEC_ID_VP6,          MKTAG('V', 'P', '6', '0') },
     { CODEC_ID_VP6,          MKTAG('V', 'P', '6', '1') },
     { CODEC_ID_VP6,          MKTAG('V', 'P', '6', '2') },
+    { CODEC_ID_VP6F,         MKTAG('F', 'L', 'V', '4') },
     { CODEC_ID_ASV1,         MKTAG('A', 'S', 'V', '1') },
     { CODEC_ID_ASV2,         MKTAG('A', 'S', 'V', '2') },
     { CODEC_ID_VCR1,         MKTAG('V', 'C', 'R', '1') },

commit 337104ac1fe3ded85e5d43e5717a7d934e3da6c4

Fix FourCC when remuxing VP6F, regression introduced in r22941.
Patch by Anssi Hannula, anssi D hannula A iki fi

Originally committed as revision 22985 to svn://svn.ffmpeg.org/ffmpeg/trunk

libavformat/riff.c
@@ -180,6 +180,7 @@ const AVCodecTag ff_codec_bmp_tags[] = {
     { CODEC_ID_VP6,          MKTAG('V', 'P', '6', '0') },
     { CODEC_ID_VP6,          MKTAG('V', 'P', '6', '1') },
     { CODEC_ID_VP6,          MKTAG('V', 'P', '6', '2') },
+    { CODEC_ID_VP6F,         MKTAG('V', 'P', '6', 'F') },
     { CODEC_ID_VP6F,         MKTAG('F', 'L', 'V', '4') },
     { CODEC_ID_ASV1,         MKTAG('A', 'S', 'V', '1') },
     { CODEC_ID_ASV2,         MKTAG('A', 'S', 'V', '2') },
@@ -205,7 +206,6 @@ const AVCodecTag ff_codec_bmp_tags[] = {
     { CODEC_ID_4XM,          MKTAG('4', 'X', 'M', 'V') },
     { CODEC_ID_FLV1,         MKTAG('F', 'L', 'V', '1') },
     { CODEC_ID_FLASHSV,      MKTAG('F', 'S', 'V', '1') },
-    { CODEC_ID_VP6F,         MKTAG('V', 'P', '6', 'F') },
     { CODEC_ID_SVQ1,         MKTAG('s', 'v', 'q', '1') },
     { CODEC_ID_TSCC,         MKTAG('t', 's', 'c', 'c') },
     { CODEC_ID_ULTI,         MKTAG('U', 'L', 'T', 'I') },