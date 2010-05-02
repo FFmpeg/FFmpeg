1302ccc1a771d6e713a28fbbf70fc6cd6ae93b07
Ronald S. Bultje <rsbultje@gmail.com>
2010-05-02 21:52:25 +0000

    Another buffer overflow, fixes issue1758.
    Originally committed as revision 23011 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/wmavoice.c
+++ libavcodec/wmavoice.c
@@ -697,7 +697,7 @@ static void wiener_denoise(WMAVoiceContext *s, int fcb_type,
         ff_rdft_calc(&s->rdft, coeffs);
         synth_pf[0] *= coeffs[0];
         synth_pf[1] *= coeffs[1];
-        for (n = 1; n < 128; n++) {
+        for (n = 1; n < 64; n++) {
             float v1 = synth_pf[n * 2], v2 = synth_pf[n * 2 + 1];
             synth_pf[n * 2]     = v1 * coeffs[n * 2] - v2 * coeffs[n * 2 + 1];
             synth_pf[n * 2 + 1] = v2 * coeffs[n * 2] + v1 * coeffs[n * 2 + 1];
