9e94bd3e8048db97abbf6a5ed3882cc63d517e6f
Stefano Sabatini <stefano.sabatini-lala@poste.it>
2010-05-05 21:45:00 +0000

    Reindent after the last commit.
    Originally committed as revision 23034 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- cmdutils.c
+++ cmdutils.c
@@ -294,9 +294,9 @@ void print_error(const char *filename, int err)
     char errbuf[128];
     const char *errbuf_ptr = errbuf;
 
-        if (av_strerror(err, errbuf, sizeof(errbuf)) < 0)
-            errbuf_ptr = strerror(AVUNERROR(err));
-        fprintf(stderr, "%s: %s\n", filename, errbuf_ptr);
+    if (av_strerror(err, errbuf, sizeof(errbuf)) < 0)
+        errbuf_ptr = strerror(AVUNERROR(err));
+    fprintf(stderr, "%s: %s\n", filename, errbuf_ptr);
 }
 
 #define PRINT_LIB_VERSION(outstream,libname,LIBNAME,indent)             \
