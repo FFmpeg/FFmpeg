6d810162823c2c76088df267c5c3e07e07b1437a
Sebastian Vater <cdgs.basty@googlemail.com>
2010-05-13 19:18:54 +0000

    Reindent after r23124. Patch by Sebastian Vater <cdgs basty googlemail com>.
    Originally committed as revision 23128 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/iff.c
+++ libavcodec/iff.c
@@ -131,9 +131,9 @@ int ff_cmap_read_palette(AVCodecContext *avctx, uint32_t *pal)
     // If extradata is smaller than actually needed, fill the remaining with black.
     count = FFMIN(avctx->extradata_size / 3, count);
     if (count) {
-    for (i=0; i < count; i++) {
-        pal[i] = 0xFF000000 | AV_RB24( avctx->extradata + i*3 );
-    }
+        for (i=0; i < count; i++) {
+            pal[i] = 0xFF000000 | AV_RB24( avctx->extradata + i*3 );
+        }
     } else { // Create gray-scale color palette for bps < 8
         count = 1 << avctx->bits_per_coded_sample;
 
