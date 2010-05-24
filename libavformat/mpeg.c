612dc0238aa2583905aba35601a0160d0f841dd8
Janne Grunau <janne-ffmpeg@jannau.net>
2010-05-24 12:32:13 +0000

    Skip pes payload during probing to avoid start code emulation.
    Patch by Janne Grunau, janne-ffmpeg jannau net
    
    Originally committed as revision 23286 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/mpeg.c
+++ libavformat/mpeg.c
@@ -60,16 +60,20 @@ static int mpegps_probe(AVProbeData *p)
     for(i=0; i<p->buf_size; i++){
         code = (code<<8) + p->buf[i];
         if ((code & 0xffffff00) == 0x100) {
+            int len= p->buf[i+1] << 8 | p->buf[i+2];
             int pes= check_pes(p->buf+i, p->buf+p->buf_size);
 
             if(code == SYSTEM_HEADER_START_CODE) sys++;
-            else if(code == PRIVATE_STREAM_1)    priv1++;
             else if(code == PACK_START_CODE)     pspack++;
             else if((code & 0xf0) == VIDEO_ID &&  pes) vid++;
-            else if((code & 0xe0) == AUDIO_ID &&  pes) audio++;
+            // skip pes payload to avoid start code emulation for private
+            // and audio streams
+            else if((code & 0xe0) == AUDIO_ID &&  pes) {audio++; i+=len;}
+            else if(code == PRIVATE_STREAM_1  &&  pes) {priv1++; i+=len;}
 
             else if((code & 0xf0) == VIDEO_ID && !pes) invalid++;
             else if((code & 0xe0) == AUDIO_ID && !pes) invalid++;
+            else if(code == PRIVATE_STREAM_1  && !pes) invalid++;
         }
     }
 
