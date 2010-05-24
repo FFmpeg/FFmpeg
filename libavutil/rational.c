31fdd641945dc5cdcfde9ee92329e22c0279981e
David Conrad <lessen42@gmail.com>
2010-05-24 00:44:02 +0000

    Convert NaN to 0/0 in av_d2q
    This fixes aspect ratio calculation for encoding from files with 0/0 stored,
    common with ogg/theora
    
    Originally committed as revision 23280 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavutil/rational.c
+++ libavutil/rational.c
@@ -98,6 +98,8 @@ AVRational av_d2q(double d, int max){
 #define LOG2  0.69314718055994530941723212145817656807550013436025
     int exponent= FFMAX( (int)(log(fabs(d) + 1e-20)/LOG2), 0);
     int64_t den= 1LL << (61 - exponent);
+    if (isnan(d))
+        return (AVRational){0,0};
     av_reduce(&a.num, &a.den, (int64_t)(d * den + 0.5), den, max);
 
     return a;
