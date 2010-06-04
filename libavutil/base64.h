31878fcf430cf0f0eb39741c65ccee02b22ef785
Howard Chu <hyc@highlandsun.com>
2010-06-04 01:15:07 +0000

    Add AV_BASE64_SIZE() macro
    Originally committed as revision 23461 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavutil/base64.h
+++ libavutil/base64.h
@@ -46,4 +46,9 @@ int av_base64_decode(uint8_t *out, const char *in, int out_size);
  */
 char *av_base64_encode(char *out, int out_size, const uint8_t *in, int in_size);
 
+/**
+ * Calculate the output size needed to base64-encode x bytes.
+ */
+#define AV_BASE64_SIZE(x)  (((x)+2) / 3 * 4 + 1)
+
 #endif /* AVUTIL_BASE64_H */
