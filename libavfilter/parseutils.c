5ba949fe904e7beed2d601ba3664f8da247be703
Stefano Sabatini <stefano.sabatini-lala@poste.it>
2010-06-13 09:27:09 +0000

    Add missing check to av_get_token().
    Originally committed as revision 23594 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavfilter/parseutils.c
+++ libavfilter/parseutils.c
@@ -34,6 +34,7 @@ char *av_get_token(const char **buf, const char *term)
     char *out = av_malloc(strlen(*buf) + 1);
     char *ret= out, *end= out;
     const char *p = *buf;
+    if (!out) return NULL;
     p += strspn(p, WHITESPACES);
 
     while(*p && !strspn(p, term)) {
