701935968d63b7f5b18828f6fd29c78e5b159752
Michael Niedermayer <michaelni@gmx.at>
2010-06-17 10:13:11 +0000

    add #include so make checkheaders passes
    Originally committed as revision 23637 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavfilter/vsrc_buffer.h
+++ libavfilter/vsrc_buffer.h
@@ -19,6 +19,8 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
+#include "avfilter.h"
+
 int av_vsrc_buffer_add_frame(AVFilterContext *buffer_filter, AVFrame *frame,
                              int64_t pts, AVRational pixel_aspect);
 
