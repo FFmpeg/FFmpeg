c47f567c701d49f26b514a7156ccc594b18c788b
Josh Allmann <joshua.allmann@gmail.com>
2010-06-25 08:03:52 +0000

    rtpdec_mpeg4: Rename PayloadContext to be consistently 'data'
    Patch by Josh Allmann, joshua dot allmann at gmail
    
    Originally committed as revision 23773 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/rtpdec_mpeg4.c
+++ libavformat/rtpdec_mpeg4.c
@@ -150,7 +150,7 @@ static int parse_fmtp_config(AVCodecContext * codec, char *value)
     return 0;
 }
 
-static int rtp_parse_mp4_au(PayloadContext *infos, const uint8_t *buf)
+static int rtp_parse_mp4_au(PayloadContext *data, const uint8_t *buf)
 {
     int au_headers_length, au_header_size, i;
     GetBitContext getbitcontext;
@@ -162,36 +162,36 @@ static int rtp_parse_mp4_au(PayloadContext *infos, const uint8_t *buf)
     if (au_headers_length > RTP_MAX_PACKET_LENGTH)
       return -1;
 
-    infos->au_headers_length_bytes = (au_headers_length + 7) / 8;
+    data->au_headers_length_bytes = (au_headers_length + 7) / 8;
 
     /* skip AU headers length section (2 bytes) */
     buf += 2;
 
-    init_get_bits(&getbitcontext, buf, infos->au_headers_length_bytes * 8);
+    init_get_bits(&getbitcontext, buf, data->au_headers_length_bytes * 8);
 
     /* XXX: Wrong if optionnal additional sections are present (cts, dts etc...) */
-    au_header_size = infos->sizelength + infos->indexlength;
+    au_header_size = data->sizelength + data->indexlength;
     if (au_header_size <= 0 || (au_headers_length % au_header_size != 0))
         return -1;
 
-    infos->nb_au_headers = au_headers_length / au_header_size;
-    if (!infos->au_headers || infos->au_headers_allocated < infos->nb_au_headers) {
-        av_free(infos->au_headers);
-        infos->au_headers = av_malloc(sizeof(struct AUHeaders) * infos->nb_au_headers);
-        infos->au_headers_allocated = infos->nb_au_headers;
+    data->nb_au_headers = au_headers_length / au_header_size;
+    if (!data->au_headers || data->au_headers_allocated < data->nb_au_headers) {
+        av_free(data->au_headers);
+        data->au_headers = av_malloc(sizeof(struct AUHeaders) * data->nb_au_headers);
+        data->au_headers_allocated = data->nb_au_headers;
     }
 
     /* XXX: We handle multiple AU Section as only one (need to fix this for interleaving)
        In my test, the FAAD decoder does not behave correctly when sending each AU one by one
        but does when sending the whole as one big packet...  */
-    infos->au_headers[0].size = 0;
-    infos->au_headers[0].index = 0;
-    for (i = 0; i < infos->nb_au_headers; ++i) {
-        infos->au_headers[0].size += get_bits_long(&getbitcontext, infos->sizelength);
-        infos->au_headers[0].index = get_bits_long(&getbitcontext, infos->indexlength);
+    data->au_headers[0].size = 0;
+    data->au_headers[0].index = 0;
+    for (i = 0; i < data->nb_au_headers; ++i) {
+        data->au_headers[0].size += get_bits_long(&getbitcontext, data->sizelength);
+        data->au_headers[0].index = get_bits_long(&getbitcontext, data->indexlength);
     }
 
-    infos->nb_au_headers = 1;
+    data->nb_au_headers = 1;
 
     return 0;
 }
@@ -199,29 +199,29 @@ static int rtp_parse_mp4_au(PayloadContext *infos, const uint8_t *buf)
 
 /* Follows RFC 3640 */
 static int aac_parse_packet(AVFormatContext *ctx,
-                            PayloadContext *infos,
+                            PayloadContext *data,
                             AVStream *st,
                             AVPacket *pkt,
                             uint32_t *timestamp,
                             const uint8_t *buf, int len, int flags)
 {
-    if (rtp_parse_mp4_au(infos, buf))
+    if (rtp_parse_mp4_au(data, buf))
         return -1;
 
-    buf += infos->au_headers_length_bytes + 2;
-    len -= infos->au_headers_length_bytes + 2;
+    buf += data->au_headers_length_bytes + 2;
+    len -= data->au_headers_length_bytes + 2;
 
     /* XXX: Fixme we only handle the case where rtp_parse_mp4_au define
                     one au_header */
-    av_new_packet(pkt, infos->au_headers[0].size);
-    memcpy(pkt->data, buf, infos->au_headers[0].size);
+    av_new_packet(pkt, data->au_headers[0].size);
+    memcpy(pkt->data, buf, data->au_headers[0].size);
 
     pkt->stream_index = st->index;
     return 0;
 }
 
 static int parse_sdp_line(AVFormatContext *s, int st_index,
-                          PayloadContext *rtp_payload_data, const char *line)
+                          PayloadContext *data, const char *line)
 {
     const char *p;
     char value[4096], attr[25];
@@ -250,10 +250,10 @@ static int parse_sdp_line(AVFormatContext *s, int st_index,
                 for (i = 0; attr_names[i].str; ++i) {
                     if (!strcasecmp(attr, attr_names[i].str)) {
                         if (attr_names[i].type == ATTR_NAME_TYPE_INT) {
-                            *(int *)((char *)rtp_payload_data +
+                            *(int *)((char *)data+
                                 attr_names[i].offset) = atoi(value);
                         } else if (attr_names[i].type == ATTR_NAME_TYPE_STR)
-                            *(char **)((char *)rtp_payload_data +
+                            *(char **)((char *)data+
                                 attr_names[i].offset) = av_strdup(value);
                     }
                 }
