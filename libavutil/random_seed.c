b7fa5c5abb39f819fbb42253711bc13e5cac0e81
Måns Rullgård <mans@mansr.com>
2010-07-01 11:40:50 +0000

    random_seed: allow to block on /dev/random
    If both /dev/random and /dev/urandom failed to return data, an
    uninitialised value might be returned.  Since most systems have a
    non-blocking /dev/urandom or have /dev/random with similar properties,
    the chance of blocking is minimal, and the alternative of returning
    non-random data is worse.
    
    Originally committed as revision 23930 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavutil/random_seed.c
+++ libavutil/random_seed.c
@@ -31,9 +31,6 @@ static int read_random(uint32_t *dst, const char *file)
 
     if (fd == -1)
         return -1;
-#if HAVE_FCNTL && defined(O_NONBLOCK)
-    if (fcntl(fd, F_SETFL, fcntl(fd, F_GETFL) | O_NONBLOCK) != -1)
-#endif
         err = read(fd, dst, sizeof(*dst));
     close(fd);
 
