b8ab52e702e9536e054169f30ee05855a8f0ac93
Diego Biurrun <diego@biurrun.de>
2010-07-02 11:41:13 +0000

    Fix Doxygen function parameter documentation syntax.
    Originally committed as revision 23978 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/id3v2.h
+++ libavformat/id3v2.h
@@ -35,15 +35,15 @@
 
 /**
  * Detect ID3v2 Header.
- * @buf   must be ID3v2_HEADER_SIZE byte long
- * @magic magic bytes to identify the header, machine byte order.
+ * @param buf   must be ID3v2_HEADER_SIZE byte long
+ * @param magic magic bytes to identify the header, machine byte order.
  * If in doubt, use ID3v2_DEFAULT_MAGIC.
  */
 int ff_id3v2_match(const uint8_t *buf, const char *magic);
 
 /**
  * Get the length of an ID3v2 tag.
- * @buf must be ID3v2_HEADER_SIZE bytes long and point to the start of an
+ * @param buf must be ID3v2_HEADER_SIZE bytes long and point to the start of an
  * already detected ID3v2 tag
  */
 int ff_id3v2_tag_len(const uint8_t *buf);
