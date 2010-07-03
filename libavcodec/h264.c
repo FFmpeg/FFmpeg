bb943bb8fc55170760b151a2b4299e6e564231e1
Baptiste Coudurier <baptiste.coudurier@gmail.com>
2010-07-03 04:40:12 +0000

    Give context to av_log
    Originally committed as revision 24018 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/h264.c
+++ libavcodec/h264.c
@@ -1902,7 +1902,7 @@ static int decode_slice_header(H264Context *h, H264Context *h0){
     if(h0->current_slice == 0){
         while(h->frame_num !=  h->prev_frame_num &&
               h->frame_num != (h->prev_frame_num+1)%(1<<h->sps.log2_max_frame_num)){
-            av_log(NULL, AV_LOG_DEBUG, "Frame num gap %d %d\n", h->frame_num, h->prev_frame_num);
+            av_log(h->s.avctx, AV_LOG_DEBUG, "Frame num gap %d %d\n", h->frame_num, h->prev_frame_num);
             if (ff_h264_frame_start(h) < 0)
                 return -1;
             h->prev_frame_num++;
