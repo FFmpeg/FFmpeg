a46b84d1204d3cd2de14f08de29afee08f8f86d0
Måns Rullgård <mans@mansr.com>
2010-07-04 18:33:47 +0000

    PPC: convert Altivec FFT to pure assembler
    On PPC a leaf function has a 288-byte red zone below the stack pointer,
    sparing these functions the chore of setting up a full stack frame.
    
    When a function call is disguised within an inline asm block, the
    compiler might not adjust the stack pointer as required before a
    function call, resulting in the red zone being clobbered.
    
    Moving the entire function to pure asm avoids this problem and also
    results in somewhat better code.
    
    Originally committed as revision 24044 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/ppc/fft_altivec.c
+++ libavcodec/ppc/fft_altivec.c
@@ -22,7 +22,6 @@
 #include "libavcodec/fft.h"
 #include "util_altivec.h"
 #include "types_altivec.h"
-#include "regs.h"
 
 /**
  * Do a complex FFT with the parameters defined in ff_fft_init(). The
@@ -33,51 +32,10 @@
  * It also assumes all FFTComplex are 8 bytes-aligned pair of float
  */
 
-// Pointers to functions. Not using function pointer syntax, because
-// that involves an extra level of indirection on some PPC ABIs.
-extern void *ff_fft_dispatch_altivec[2][15];
+void ff_fft_calc_altivec(FFTContext *s, FFTComplex *z);
+void ff_fft_calc_interleave_altivec(FFTContext *s, FFTComplex *z);
 
 #if HAVE_GNU_AS
-static av_always_inline void fft_dispatch(FFTContext *s, FFTComplex *z, int do_swizzle)
-{
-    register vec_f  v14 __asm__("v14") = {0,0,0,0};
-    register vec_f  v15 __asm__("v15") = *(const vec_f*)ff_cos_16;
-    register vec_f  v16 __asm__("v16") = {0, 0.38268343, M_SQRT1_2, 0.92387953};
-    register vec_f  v17 __asm__("v17") = {-M_SQRT1_2, M_SQRT1_2, M_SQRT1_2,-M_SQRT1_2};
-    register vec_f  v18 __asm__("v18") = { M_SQRT1_2, M_SQRT1_2, M_SQRT1_2, M_SQRT1_2};
-    register vec_u8 v19 __asm__("v19") = vcprm(s0,3,2,1);
-    register vec_u8 v20 __asm__("v20") = vcprm(0,1,s2,s1);
-    register vec_u8 v21 __asm__("v21") = vcprm(2,3,s0,s3);
-    register vec_u8 v22 __asm__("v22") = vcprm(2,s3,3,s2);
-    register vec_u8 v23 __asm__("v23") = vcprm(0,1,s0,s1);
-    register vec_u8 v24 __asm__("v24") = vcprm(2,3,s2,s3);
-    register vec_u8 v25 __asm__("v25") = vcprm(2,3,0,1);
-    register vec_u8 v26 __asm__("v26") = vcprm(1,2,s3,s0);
-    register vec_u8 v27 __asm__("v27") = vcprm(0,3,s2,s1);
-    register vec_u8 v28 __asm__("v28") = vcprm(0,2,s1,s3);
-    register vec_u8 v29 __asm__("v29") = vcprm(1,3,s0,s2);
-    register FFTSample *const*cos_tabs __asm__("r12") = ff_cos_tabs;
-    register FFTComplex *zarg __asm__("r3") = z;
-    __asm__(
-        "mtctr %0               \n"
-        "li   "r(9)", 16        \n"
-        "subi "r(1)","r(1) ",%1 \n"
-        "bctrl                  \n"
-        "addi "r(1)","r(1) ",%1 \n"
-        ::"r"(ff_fft_dispatch_altivec[do_swizzle][s->nbits-2]), "i"(12*sizeof(void*)),
-          "r"(zarg), "r"(cos_tabs),
-          "v"(v14),"v"(v15),"v"(v16),"v"(v17),"v"(v18),"v"(v19),"v"(v20),"v"(v21),
-          "v"(v22),"v"(v23),"v"(v24),"v"(v25),"v"(v26),"v"(v27),"v"(v28),"v"(v29)
-        : "lr","ctr","r0","r4","r5","r6","r7","r8","r9","r10","r11",
-          "v0","v1","v2","v3","v4","v5","v6","v7","v8","v9","v10","v11","v12","v13"
-    );
-}
-
-static void ff_fft_calc_altivec(FFTContext *s, FFTComplex *z)
-{
-    fft_dispatch(s, z, 1);
-}
-
 static void ff_imdct_half_altivec(FFTContext *s, FFTSample *output, const FFTSample *input)
 {
     int j, k;
@@ -132,7 +90,7 @@ static void ff_imdct_half_altivec(FFTContext *s, FFTSample *output, const FFTSam
         k--;
     } while(k >= 0);
 
-    fft_dispatch(s, (FFTComplex*)output, 0);
+    ff_fft_calc_altivec(s, (FFTComplex*)output);
 
     /* post rotation + reordering */
     j = -n32;
@@ -182,7 +140,7 @@ static void ff_imdct_calc_altivec(FFTContext *s, FFTSample *output, const FFTSam
 av_cold void ff_fft_init_altivec(FFTContext *s)
 {
 #if HAVE_GNU_AS
-    s->fft_calc = ff_fft_calc_altivec;
+    s->fft_calc   = ff_fft_calc_interleave_altivec;
     s->imdct_calc = ff_imdct_calc_altivec;
     s->imdct_half = ff_imdct_half_altivec;
 #endif
