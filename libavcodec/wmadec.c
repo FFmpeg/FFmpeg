c7310b7b2bb65bd31786840ecd0fab459bd3a6bd
Eli Friedman <eli.friedman@gmail.com>
2010-07-07 10:24:34 +0000

    Silence warning "new qualifiers in middle of multi-level non-const cast are unsafe".
    Patch by Eli Friedman, eli d friedman a gmail
    
    Originally committed as revision 24080 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/wmadec.c
+++ libavcodec/wmadec.c
@@ -804,10 +804,10 @@ static int wma_decode_frame(WMACodecContext *s, int16_t *samples)
                     s->frame_len * sizeof(float));
         }
     } else {
-        float *output[MAX_CHANNELS];
+        const float *output[MAX_CHANNELS];
         for (ch = 0; ch < MAX_CHANNELS; ch++)
             output[ch] = s->frame_out[ch];
-        s->dsp.float_to_int16_interleave(samples, (const float **)output, n, incr);
+        s->dsp.float_to_int16_interleave(samples, output, n, incr);
         for(ch = 0; ch < incr; ch++) {
             /* prepare for next block */
             memmove(&s->frame_out[ch][0], &s->frame_out[ch][n], n * sizeof(float));
