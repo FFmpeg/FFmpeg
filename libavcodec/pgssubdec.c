849ff0c4d169d098a34306f9ea05bdc6f74a3b3f
Reimar Döffinger <Reimar.Doeffinger@gmx.de>
2010-07-11 07:48:04 +0000

    Set pix_fmt to the correct value for the format the PGS decoder actually uses.
    Originally committed as revision 24187 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/pgssubdec.c
+++ libavcodec/pgssubdec.c
@@ -64,7 +64,7 @@ typedef struct PGSSubContext {
 
 static av_cold int init_decoder(AVCodecContext *avctx)
 {
-    avctx->pix_fmt     = PIX_FMT_RGB32;
+    avctx->pix_fmt = PIX_FMT_PAL8;
 
     return 0;
 }
