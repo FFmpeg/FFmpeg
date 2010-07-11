8b834ac5d9f32bff432eb84cd4b78eae01a7abe7
Reimar Döffinger <Reimar.Doeffinger@gmx.de>
2010-07-11 07:53:39 +0000

    100l, change avsubtitle_free to the actually tested and working version.
    Originally committed as revision 24188 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/utils.c
+++ libavcodec/utils.c
@@ -695,16 +695,16 @@ void avsubtitle_free(AVSubtitle *sub)
 
     for (i = 0; i < sub->num_rects; i++)
     {
-        av_freep(sub->rects[i]->pict.data[0]);
-        av_freep(sub->rects[i]->pict.data[1]);
-        av_freep(sub->rects[i]->pict.data[2]);
-        av_freep(sub->rects[i]->pict.data[3]);
-        av_freep(sub->rects[i]->text);
-        av_freep(sub->rects[i]->ass);
-        av_freep(sub->rects[i]);
+        av_freep(&sub->rects[i]->pict.data[0]);
+        av_freep(&sub->rects[i]->pict.data[1]);
+        av_freep(&sub->rects[i]->pict.data[2]);
+        av_freep(&sub->rects[i]->pict.data[3]);
+        av_freep(&sub->rects[i]->text);
+        av_freep(&sub->rects[i]->ass);
+        av_freep(&sub->rects[i]);
     }
 
-    av_freep(sub->rects);
+    av_freep(&sub->rects);
 
     memset(sub, 0, sizeof(AVSubtitle));
 }
