2356a7834bd235f111fdd76b2612f9155b47a2a8
Ronald S. Bultje <rsbultje@gmail.com>
2010-07-16 19:42:32 +0000

    Remove duplicate define.
    Originally committed as revision 24270 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/x86/vp8dsp.asm
+++ libavcodec/x86/vp8dsp.asm
@@ -1374,7 +1374,7 @@ SIMPLE_LOOPFILTER sse2,   v, 3
 SIMPLE_LOOPFILTER sse2,   h, 6
 
 ;-----------------------------------------------------------------------------
-; void vp8_h/v_loop_filter<size>_inner_<opt>(uint8_t *dst, int stride,
+; void vp8_h/v_loop_filter<size>_inner_<opt>(uint8_t *dst, [uint8_t *v,] int stride,
 ;                                            int flimE, int flimI, int hev_thr);
 ;-----------------------------------------------------------------------------
 
@@ -1382,7 +1382,6 @@ SIMPLE_LOOPFILTER sse2,   h, 6
 cglobal vp8_%2_loop_filter16_inner_%1, 5, %3, %4
 %define dst_reg     r0
 %define mstride_reg r1
-%define stride_reg  r2
 %define E_reg       r2
 %define I_reg       r3
 %define hev_thr_reg r4
