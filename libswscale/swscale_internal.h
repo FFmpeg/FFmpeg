709c0181fdd53637dfc6f6c0618143a3cde3a072
Ramiro Polla <ramiro.polla@gmail.com>
2010-07-20 01:45:25 +0000

    Restore vertical alignment in some defines
    Originally committed as revision 31758 to svn://svn.mplayerhq.hu/mplayer/trunk/libswscale

--- libswscale/swscale_internal.h
+++ libswscale/swscale_internal.h
@@ -343,12 +343,12 @@ const char *sws_format_name(enum PixelFormat format);
         || (x)==PIX_FMT_GRAY16LE    \
         || (x)==PIX_FMT_RGB48BE     \
         || (x)==PIX_FMT_RGB48LE     \
-        || (x)==PIX_FMT_YUV420P16LE   \
-        || (x)==PIX_FMT_YUV422P16LE   \
-        || (x)==PIX_FMT_YUV444P16LE   \
-        || (x)==PIX_FMT_YUV420P16BE   \
-        || (x)==PIX_FMT_YUV422P16BE   \
-        || (x)==PIX_FMT_YUV444P16BE   \
+        || (x)==PIX_FMT_YUV420P16LE \
+        || (x)==PIX_FMT_YUV422P16LE \
+        || (x)==PIX_FMT_YUV444P16LE \
+        || (x)==PIX_FMT_YUV420P16BE \
+        || (x)==PIX_FMT_YUV422P16BE \
+        || (x)==PIX_FMT_YUV444P16BE \
     )
 #define isBE(x) ((x)&1)
 #define isPlanar8YUV(x) (           \
@@ -364,12 +364,12 @@ const char *sws_format_name(enum PixelFormat format);
     )
 #define isPlanarYUV(x)  (           \
         isPlanar8YUV(x)             \
-        || (x)==PIX_FMT_YUV420P16LE   \
-        || (x)==PIX_FMT_YUV422P16LE   \
-        || (x)==PIX_FMT_YUV444P16LE   \
-        || (x)==PIX_FMT_YUV420P16BE   \
-        || (x)==PIX_FMT_YUV422P16BE   \
-        || (x)==PIX_FMT_YUV444P16BE   \
+        || (x)==PIX_FMT_YUV420P16LE \
+        || (x)==PIX_FMT_YUV422P16LE \
+        || (x)==PIX_FMT_YUV444P16LE \
+        || (x)==PIX_FMT_YUV420P16BE \
+        || (x)==PIX_FMT_YUV422P16BE \
+        || (x)==PIX_FMT_YUV444P16BE \
     )
 #define isYUV(x)        (           \
            (x)==PIX_FMT_UYVY422     \
