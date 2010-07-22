99ac59ca69039cd28701c46e7e896f2d05bb5528
Stefano Sabatini <stefano.sabatini-lala@poste.it>
2010-07-22 09:55:57 +0000

    Make link_filter() propagate the generated error code.
    Originally committed as revision 24419 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavfilter/graphparser.c
+++ libavfilter/graphparser.c
@@ -34,11 +34,12 @@ static int link_filter(AVFilterContext *src, int srcpad,
                        AVFilterContext *dst, int dstpad,
                        AVClass *log_ctx)
 {
-    if (avfilter_link(src, srcpad, dst, dstpad)) {
+    int ret;
+    if ((ret = avfilter_link(src, srcpad, dst, dstpad))) {
         av_log(log_ctx, AV_LOG_ERROR,
                "cannot create the link %s:%d -> %s:%d\n",
                src->filter->name, srcpad, dst->filter->name, dstpad);
-        return -1;
+        return ret;
     }
 
     return 0;
