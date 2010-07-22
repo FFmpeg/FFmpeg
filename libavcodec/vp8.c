867215336debce9fe38f728ad5c74776a1965785
Jason Garrett-Glaser <darkshikari@gmail.com>
2010-07-22 23:04:51 +0000

    Shut up an uninitialized variable GCC warning in VP8.
    Originally committed as revision 24445 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/vp8.c
+++ libavcodec/vp8.c
@@ -1402,7 +1402,7 @@ static int vp8_decode_frame(AVCodecContext *avctx, void *data, int *data_size,
     VP8Context *s = avctx->priv_data;
     int ret, mb_x, mb_y, i, y, referenced;
     enum AVDiscard skip_thresh;
-    AVFrame *curframe;
+    AVFrame *curframe = NULL;
 
     if ((ret = decode_frame_header(s, avpkt->data, avpkt->size)) < 0)
         return ret;
