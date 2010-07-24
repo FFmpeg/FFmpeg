6cce7cabdb2cd49f7182c8f74aac1e981d781c43
Reimar Döffinger <Reimar.Doeffinger@gmx.de>
2010-07-24 14:43:55 +0000

    Fix compilation, forgot to add const also to the definition of ff_yuv2packedX_altivec
    Originally committed as revision 31782 to svn://svn.mplayerhq.hu/mplayer/trunk/libswscale

--- libswscale/ppc/yuv2rgb_altivec.c
+++ libswscale/ppc/yuv2rgb_altivec.c
@@ -778,8 +778,8 @@ void ff_yuv2rgb_init_tables_altivec(SwsContext *c, const int inv_table[4], int b
 
 void
 ff_yuv2packedX_altivec(SwsContext *c,
-                       const int16_t *lumFilter, int16_t **lumSrc, int lumFilterSize,
-                       const int16_t *chrFilter, int16_t **chrSrc, int chrFilterSize,
+                       const int16_t *lumFilter, const int16_t **lumSrc, int lumFilterSize,
+                       const int16_t *chrFilter, const int16_t **chrSrc, int chrFilterSize,
                      uint8_t *dest, int dstW, int dstY)
 {
     int i,j;
