be7110cdedbc27a24be9440177c6466813937578
Stefano Sabatini <stefano.sabatini-lala@poste.it>
2010-08-21 15:18:52 +0000

    Cosmetics: if( -> if (.
    Originally committed as revision 24859 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcore/imgutils.c
+++ libavcore/imgutils.c
@@ -114,7 +114,7 @@ int av_check_image_size(unsigned int w, unsigned int h, int log_offset, void *lo
 {
     ImgUtils imgutils = { &imgutils_class, log_offset, log_ctx };
 
-    if((int)w>0 && (int)h>0 && (w+128)*(uint64_t)(h+128) < INT_MAX/8)
+    if ((int)w>0 && (int)h>0 && (w+128)*(uint64_t)(h+128) < INT_MAX/8)
         return 0;
 
     av_log(&imgutils, AV_LOG_ERROR, "Picture size %ux%u is invalid\n", w, h);
