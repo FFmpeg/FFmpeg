901694f1607802202ec7ba594369413d8ae1e06e
Tobias Bindhammer <tobias.bindhammer@uni-ulm.de>
2010-08-23 11:47:50 +0000

    Corresponding muxer for the a64 codec
    Originally committed as revision 24875 to svn://svn.ffmpeg.org/ffmpeg/trunk

---
+++ libavformat/a64.c
@@ -0,0 +1,72 @@
+/*
+ * a64 muxer
+ * Copyright (c) 2009 Tobias Bindhammer
+ *
+ * This file is part of FFmpeg.
+ *
+ * FFmpeg is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * FFmpeg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FFmpeg; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#include "libavcodec/avcodec.h"
+#include "libavcodec/a64enc.h"
+#include "libavcodec/bytestream.h"
+#include "avformat.h"
+
+static int a64_write_header(struct AVFormatContext *s)
+{
+    AVCodecContext *avctx=s->streams[0]->codec;
+    uint8_t header[5] = {
+        0x00, //load
+        0x40, //address
+        0x00, //mode
+        0x00, //charset_lifetime (multi only)
+        0x00  //fps in 50/fps;
+    };
+    switch (avctx->codec->id) {
+    case CODEC_ID_A64_MULTI:
+        header[2] = 0x00;
+        header[3] = 4;
+        header[4] = 2;
+        break;
+    case CODEC_ID_A64_MULTI5:
+        header[2] = 0x01;
+        header[3] = 4;
+        header[4] = 3;
+        break;
+    default:
+        return -1;
+        break;
+    }
+    put_buffer(s->pb, header, 2);
+    return 0;
+}
+
+static int a64_write_packet(struct AVFormatContext *s, AVPacket *pkt)
+{
+    put_buffer(s->pb, pkt->data, pkt->size);
+    put_flush_packet(s->pb);
+    return 0;
+}
+
+AVOutputFormat a64_muxer = {
+    .name = "a64",
+    .long_name = NULL_IF_CONFIG_SMALL("a64 - video for Commodore 64"),
+    .mime_type = NULL,
+    .extensions = "a64, A64",
+    .priv_data_size = sizeof (A64Context),
+    .video_codec = CODEC_ID_A64_MULTI,
+    a64_write_header,
+    a64_write_packet,
+};
