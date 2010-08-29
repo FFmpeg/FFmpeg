9243ed3f91f2dc9cb9b851740bfe65836462c81f
Aurelien Jacobs <aurel@gnuage.org>
2010-08-29 21:28:51 +0000

    move h264 demuxer to its own file
    Originally committed as revision 24978 to svn://svn.ffmpeg.org/ffmpeg/trunk

---
+++ libavformat/h264dec.c
@@ -0,0 +1,80 @@
+/*
+ * RAW H.264 video demuxer
+ * Copyright (c) 2008 Michael Niedermayer <michaelni@gmx.at>
+ *
+ * This file is part of FFmpeg.
+ *
+ * FFmpeg is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * FFmpeg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FFmpeg; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#include "avformat.h"
+#include "raw.h"
+
+static int h264_probe(AVProbeData *p)
+{
+    uint32_t code= -1;
+    int sps=0, pps=0, idr=0, res=0, sli=0;
+    int i;
+
+    for(i=0; i<p->buf_size; i++){
+        code = (code<<8) + p->buf[i];
+        if ((code & 0xffffff00) == 0x100) {
+            int ref_idc= (code>>5)&3;
+            int type   = code & 0x1F;
+            static const int8_t ref_zero[32]={
+                2, 0, 0, 0, 0,-1, 1,-1,
+               -1, 1, 1, 1, 1,-1, 2, 2,
+                2, 2, 2, 0, 2, 2, 2, 2,
+                2, 2, 2, 2, 2, 2, 2, 2
+            };
+
+            if(code & 0x80) //forbidden bit
+                return 0;
+
+            if(ref_zero[type] == 1 && ref_idc)
+                return 0;
+            if(ref_zero[type] ==-1 && !ref_idc)
+                return 0;
+            if(ref_zero[type] == 2)
+                res++;
+
+            switch(type){
+            case     1:   sli++; break;
+            case     5:   idr++; break;
+            case     7:
+                if(p->buf[i+2]&0x0F)
+                    return 0;
+                sps++;
+                break;
+            case     8:   pps++; break;
+            }
+        }
+    }
+    if(sps && pps && (idr||sli>3) && res<(sps+pps+idr))
+        return AVPROBE_SCORE_MAX/2+1; // +1 for .mpg
+    return 0;
+}
+
+AVInputFormat h264_demuxer = {
+    "h264",
+    NULL_IF_CONFIG_SMALL("raw H.264 video format"),
+    0,
+    h264_probe,
+    ff_raw_video_read_header,
+    ff_raw_read_partial_packet,
+    .flags= AVFMT_GENERIC_INDEX,
+    .extensions = "h26l,h264,264", //FIXME remove after writing mpeg4_probe
+    .value = CODEC_ID_H264,
+};
--- libavformat/raw.c
+++ libavformat/raw.c
@@ -241,53 +241,6 @@ int ff_raw_video_read_header(AVFormatContext *s,
 }
 #endif
 
-#if CONFIG_H264_DEMUXER
-static int h264_probe(AVProbeData *p)
-{
-    uint32_t code= -1;
-    int sps=0, pps=0, idr=0, res=0, sli=0;
-    int i;
-
-    for(i=0; i<p->buf_size; i++){
-        code = (code<<8) + p->buf[i];
-        if ((code & 0xffffff00) == 0x100) {
-            int ref_idc= (code>>5)&3;
-            int type   = code & 0x1F;
-            static const int8_t ref_zero[32]={
-                2, 0, 0, 0, 0,-1, 1,-1,
-               -1, 1, 1, 1, 1,-1, 2, 2,
-                2, 2, 2, 0, 2, 2, 2, 2,
-                2, 2, 2, 2, 2, 2, 2, 2
-            };
-
-            if(code & 0x80) //forbidden bit
-                return 0;
-
-            if(ref_zero[type] == 1 && ref_idc)
-                return 0;
-            if(ref_zero[type] ==-1 && !ref_idc)
-                return 0;
-            if(ref_zero[type] == 2)
-                res++;
-
-            switch(type){
-            case     1:   sli++; break;
-            case     5:   idr++; break;
-            case     7:
-                if(p->buf[i+2]&0x0F)
-                    return 0;
-                sps++;
-                break;
-            case     8:   pps++; break;
-            }
-        }
-    }
-    if(sps && pps && (idr||sli>3) && res<(sps+pps+idr))
-        return AVPROBE_SCORE_MAX/2+1; // +1 for .mpg
-    return 0;
-}
-#endif
-
 #if CONFIG_H263_DEMUXER
 static int h263_probe(AVProbeData *p)
 {
@@ -659,20 +612,6 @@ AVOutputFormat h263_muxer = {
     NULL,
     ff_raw_write_packet,
     .flags= AVFMT_NOTIMESTAMPS,
-};
-#endif
-
-#if CONFIG_H264_DEMUXER
-AVInputFormat h264_demuxer = {
-    "h264",
-    NULL_IF_CONFIG_SMALL("raw H.264 video format"),
-    0,
-    h264_probe,
-    ff_raw_video_read_header,
-    ff_raw_read_partial_packet,
-    .flags= AVFMT_GENERIC_INDEX,
-    .extensions = "h26l,h264,264", //FIXME remove after writing mpeg4_probe
-    .value = CODEC_ID_H264,
 };
 #endif
 
