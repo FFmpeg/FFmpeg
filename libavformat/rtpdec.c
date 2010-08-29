682d28a965c859a8826c1b52c3437a6a0c4ab59f
Josh Allmann <joshua.allmann@gmail.com>
2010-08-29 10:20:18 +0000

    Reindent
    Patch by Josh Allmann, joshua dot allmann at gmail
    
    Originally committed as revision 24964 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/rtpdec.c
+++ libavformat/rtpdec.c
@@ -84,10 +84,10 @@ static int rtcp_parse_packet(RTPDemuxContext *s, const unsigned char *buf, int l
             }
             payload_len = (AV_RB16(buf + 2) + 1) * 4;
 
-    s->last_rtcp_ntp_time = AV_RB64(buf + 8);
-    if (s->first_rtcp_ntp_time == AV_NOPTS_VALUE)
-        s->first_rtcp_ntp_time = s->last_rtcp_ntp_time;
-    s->last_rtcp_timestamp = AV_RB32(buf + 16);
+            s->last_rtcp_ntp_time = AV_RB64(buf + 8);
+            if (s->first_rtcp_ntp_time == AV_NOPTS_VALUE)
+                s->first_rtcp_ntp_time = s->last_rtcp_ntp_time;
+            s->last_rtcp_timestamp = AV_RB32(buf + 16);
 
             buf += payload_len;
             len -= payload_len;
