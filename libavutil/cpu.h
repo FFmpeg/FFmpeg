d593b4ed023be2394201514509a68712732bf5bb
Stefano Sabatini <stefano.sabatini-lala@poste.it>
2010-09-06 00:04:01 +0000

    Fix typo.
    Originally committed as revision 25046 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavutil/cpu.h
+++ libavutil/cpu.h
@@ -34,7 +34,7 @@
 #define AV_CPU_FLAG_SSE2SLOW 0x40000000 ///< SSE2 supported, but usually not faster
 #define AV_CPU_FLAG_3DNOWEXT     0x0020 ///< AMD 3DNowExt
 #define AV_CPU_FLAG_SSE3         0x0040 ///< Prescott SSE3 functions
-#define AV_CPU_FLAG_SSE3SLOW 0x20000000 ///< SSE3 supported, but usually not faster t
+#define AV_CPU_FLAG_SSE3SLOW 0x20000000 ///< SSE3 supported, but usually not faster
 #define AV_CPU_FLAG_SSSE3        0x0080 ///< Conroe SSSE3 functions
 #define AV_CPU_FLAG_SSE4         0x0100 ///< Penryn SSE4.1 functions
 #define AV_CPU_FLAG_SSE42        0x0200 ///< Nehalem SSE4.2 functions
