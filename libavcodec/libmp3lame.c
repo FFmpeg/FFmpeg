eb6fb058f20745171d7866408c3299ca0ea1c996
Carl Eugen Hoyos <cehoyos@rainbow.studorg.tuwien.ac.at>
2010-09-16 08:30:01 +0000

    Remove useless comment.
    Originally committed as revision 25129 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/libmp3lame.c
+++ libavcodec/libmp3lame.c
@@ -61,7 +61,6 @@ static av_cold int MP3lame_encode_init(AVCodecContext *avctx)
     if(avctx->flags & CODEC_FLAG_QSCALE) {
         lame_set_brate(s->gfp, 0);
         lame_set_VBR(s->gfp, vbr_default);
-        /* requires lame 3.98 or later */
         lame_set_VBR_quality(s->gfp, avctx->global_quality/(float)FF_QP2LAMBDA);
     }
     lame_set_bWriteVbrTag(s->gfp,0);
