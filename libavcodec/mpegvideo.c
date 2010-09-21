392c99007fa2a9d7659a883eff9816440ba70250
Ivan Kalvachev <ikalvachev@gmail.com>
2010-09-21 22:44:27 +0000

    The debug text output of macroblocks can indicate MB_TYPE_INTERLACED, but it used to do it only for h264 codec. Allow it for other codecs, as mpeg2 and mpeg4 also set this flag.
    Originally committed as revision 25156 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/mpegvideo.c
+++ libavcodec/mpegvideo.c
@@ -1262,7 +1262,7 @@ void ff_print_debug_info(MpegEncContext *s, AVFrame *pict){
                         av_log(s->avctx, AV_LOG_DEBUG, "?");
 
 
-                    if(IS_INTERLACED(mb_type) && s->codec_id == CODEC_ID_H264)
+                    if(IS_INTERLACED(mb_type))
                         av_log(s->avctx, AV_LOG_DEBUG, "=");
                     else
                         av_log(s->avctx, AV_LOG_DEBUG, " ");
