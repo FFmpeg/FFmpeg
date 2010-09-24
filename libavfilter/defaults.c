120119225a5363f89822addb472085631d2157bc
Stefano Sabatini <stefano.sabatini-lala@poste.it>
2010-09-24 20:01:38 +0000

    Add missing NULL checks, fix crash.
    Originally committed as revision 25181 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavfilter/defaults.c
+++ libavfilter/defaults.c
@@ -282,8 +282,8 @@ void avfilter_set_common_formats(AVFilterContext *ctx, AVFilterFormats *formats)
 
 int avfilter_default_query_formats(AVFilterContext *ctx)
 {
-    enum AVMediaType type = ctx->inputs [0] ? ctx->inputs [0]->type :
-                            ctx->outputs[0] ? ctx->outputs[0]->type :
+    enum AVMediaType type = ctx->inputs  && ctx->inputs [0] ? ctx->inputs [0]->type :
+                            ctx->outputs && ctx->outputs[0] ? ctx->outputs[0]->type :
                             AVMEDIA_TYPE_VIDEO;
 
     avfilter_set_common_formats(ctx, avfilter_all_formats(type));
