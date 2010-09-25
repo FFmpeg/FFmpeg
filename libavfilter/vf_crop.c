921bf9f1ff53305bf24b3ef1cc2ac8da05d75c9e
Stefano Sabatini <stefano.sabatini-lala@poste.it>
2010-09-25 13:42:17 +0000

    Fix memleak introduced in:
      r25185 | stefano | 2010-09-25 03:18:43 +0200 (Sat, 25 Sep 2010) | 1 line
    
      Make the crop filters accept parametric expressions.
    
    Originally committed as revision 25193 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavfilter/vf_crop.c
+++ libavfilter/vf_crop.c
@@ -305,6 +305,7 @@ static void end_frame(AVFilterLink *link)
     CropContext *crop = link->dst->priv;
 
     crop->var_values[N] += 1.0;
+    avfilter_unref_buffer(link->cur_buf);
     avfilter_end_frame(link->dst->outputs[0]);
 }
 
