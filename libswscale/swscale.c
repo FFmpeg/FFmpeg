bd8154d0b337e7a9f4dd1a728f4409fbe37c8fea
Baptiste Coudurier <baptiste.coudurier@gmail.com>
2010-09-27 23:47:54 +0000

    100l fix if condition
    Originally committed as revision 32395 to svn://svn.mplayerhq.hu/mplayer/trunk/libswscale

--- libswscale/swscale.c
+++ libswscale/swscale.c
@@ -1941,7 +1941,7 @@ int sws_scale(SwsContext *c, const uint8_t* const src[], const int srcStride[], 
                 r= (i>>3    )*255;
                 g= ((i>>1)&3)*85;
                 b= (i&1     )*255;
-            } else if(c->srcFormat == PIX_FMT_GRAY8 || PIX_FMT_Y400A) {
+            } else if(c->srcFormat == PIX_FMT_GRAY8 || c->srcFormat == PIX_FMT_Y400A) {
                 r = g = b = i;
             } else {
                 assert(c->srcFormat == PIX_FMT_BGR4_BYTE);
