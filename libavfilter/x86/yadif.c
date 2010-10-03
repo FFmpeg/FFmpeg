5ef42826d2e17fe041c0f2744868cf0e5deb7d12
İsmail Dönmez <ismail@namtrac.org>
2010-10-03 20:01:36 +0000

    yadif: Explicit wordlength for compare. Fixes compile with clang.
    Patch by İsmail Dönmez: ismail at namtrac dot org
    
    Originally committed as revision 25328 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavfilter/x86/yadif.c
+++ libavfilter/x86/yadif.c
@@ -156,7 +156,7 @@ void ff_yadif_filter_line_mmx(uint8_t *dst,
 \
             /* if(p->mode<2) ... */\
             "movq    %[tmp3], %%mm6 \n\t" /* diff */\
-            "cmp       $2, %[mode] \n\t"\
+            "cmpl      $2, %[mode] \n\t"\
             "jge       1f \n\t"\
             LOAD4("(%["prev2"],%[mrefs],2)", %%mm2) /* prev2[x-2*refs] */\
             LOAD4("(%["next2"],%[mrefs],2)", %%mm4) /* next2[x-2*refs] */\
