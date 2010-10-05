93ee7f9c41c4083c49cd031b6325dd92e0b21b84
Tobias Bindhammer <tobias.bindhammer@uni-ulm.de>
2010-10-05 12:14:20 +0000

    Add check for height when cropping.
    Originally committed as revision 25345 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/a64multienc.c
+++ libavcodec/a64multienc.c
@@ -49,11 +49,11 @@ static void to_meta_with_crop(AVCodecContext *avctx, AVFrame *p, int *dest)
     int width  = FFMIN(avctx->width ,C64XRES);
     uint8_t *src = p->data[0];
 
-    for (blocky = 0; blocky < height; blocky += 8) {
+    for (blocky = 0; blocky < C64YRES; blocky += 8) {
         for (blockx = 0; blockx < C64XRES; blockx += 8) {
-            for (y = blocky; y < blocky+8 && y < height; y++) {
+            for (y = blocky; y < blocky+8 && y < C64YRES; y++) {
                 for (x = blockx; x < blockx+8 && x < C64XRES; x += 2) {
-                    if(x < width) {
+                    if(x < width && y < height) {
                         /* build average over 2 pixels */
                         luma = (src[(x + 0 + y * p->linesize[0])] +
                                 src[(x + 1 + y * p->linesize[0])]) / 2;
