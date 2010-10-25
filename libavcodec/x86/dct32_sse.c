c6a908be586fdba6148cb716b4beee48c006a61b
Ramiro Polla <ramiro.polla@gmail.com>
2010-10-25 20:29:29 +0000

    dct32: mark xmm registers in clobber list in ff_dct32_float_sse()
    Originally committed as revision 25569 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/x86/dct32_sse.c
+++ libavcodec/x86/dct32_sse.c
@@ -289,6 +289,8 @@ void ff_dct32_float_sse(FFTSample *out, const FFTSample *in)
         :"+&r"(tmp1)
         :"r"(out), "r"(b1), "r"(smask), "r"(in)
         :"memory"
+         XMM_CLOBBERS(, "%xmm0", "%xmm1", "%xmm2", "%xmm3",
+                        "%xmm4", "%xmm5", "%xmm6", "%xmm7")
         );
 }
 
