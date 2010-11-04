37e9cc4ec760b997ec075a9b2afe340ad2a8059c
Ronald S. Bultje <rsbultje@gmail.com>
2010-11-04 13:53:16 +0000

    Remove two instances of AVERROR(ff_neterrno()), because ff_neterrno() itself already does AVERROR().
    Originally committed as revision 25671 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/tcp.c
+++ libavformat/tcp.c
@@ -155,7 +155,7 @@ static int tcp_read(URLContext *h, uint8_t *buf, int size)
             if (len < 0) {
                 if (ff_neterrno() != FF_NETERROR(EINTR) &&
                     ff_neterrno() != FF_NETERROR(EAGAIN))
-                    return AVERROR(ff_neterrno());
+                    return ff_neterrno();
             } else return len;
         } else if (ret < 0) {
             if (ff_neterrno() == FF_NETERROR(EINTR))
@@ -187,7 +187,7 @@ static int tcp_write(URLContext *h, const uint8_t *buf, int size)
             if (len < 0) {
                 if (ff_neterrno() != FF_NETERROR(EINTR) &&
                     ff_neterrno() != FF_NETERROR(EAGAIN))
-                    return AVERROR(ff_neterrno());
+                    return ff_neterrno();
                 continue;
             }
             size -= len;
