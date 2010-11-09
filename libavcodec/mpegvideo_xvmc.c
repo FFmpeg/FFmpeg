fe655f6723ff091c9a536f537ec35da9e7177bcd
Nicolas Kaiser <nikai@nikai.net>
2010-11-09 23:58:41 +0000

    Simplify: Remove impossible condition.
    Patch by Nicolas Kaiser, nikai nikai net
    
    Originally committed as revision 25714 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/mpegvideo_xvmc.c
+++ libavcodec/mpegvideo_xvmc.c
@@ -301,7 +301,7 @@ void ff_xvmc_decode_mb(MpegEncContext *s)
     for (i = 0; i < blocks_per_mb; i++) {
         if (s->block_last_index[i] >= 0) {
             // I do not have unsigned_intra MOCO to test, hope it is OK.
-            if (s->mb_intra && (render->idct || (!render->idct && !render->unsigned_intra)))
+            if (s->mb_intra && (render->idct || !render->unsigned_intra))
                 *s->pblocks[i][0] -= 1 << 10;
             if (!render->idct) {
                 s->dsp.idct(*s->pblocks[i]);
