42396c2e67bec46c5842ace9c237469cde196b4e
Peter Ross <pross@xvid.org>
2011-01-10 12:51:45 +0000

    electronicarts: only apply audio sanity checks when audio stream is present
    Originally committed as revision 26301 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavformat/electronicarts.c
+++ libavformat/electronicarts.c
@@ -422,17 +422,18 @@ static int ea_read_header(AVFormatContext *s,
         st->codec->height = ea->height;
     }
 
-    if (ea->num_channels <= 0) {
-        av_log(s, AV_LOG_WARNING, "Unsupported number of channels: %d\n", ea->num_channels);
-        ea->audio_codec = 0;
-    }
-
-    if (ea->sample_rate <= 0) {
-        av_log(s, AV_LOG_ERROR, "Unsupported sample rate: %d\n", ea->sample_rate);
-        ea->audio_codec = 0;
-    }
-
     if (ea->audio_codec) {
+        if (ea->num_channels <= 0) {
+            av_log(s, AV_LOG_WARNING, "Unsupported number of channels: %d\n", ea->num_channels);
+            ea->audio_codec = 0;
+            return 1;
+        }
+        if (ea->sample_rate <= 0) {
+            av_log(s, AV_LOG_ERROR, "Unsupported sample rate: %d\n", ea->sample_rate);
+            ea->audio_codec = 0;
+            return 1;
+        }
+
         /* initialize the audio decoder stream */
         st = av_new_stream(s, 0);
         if (!st)
