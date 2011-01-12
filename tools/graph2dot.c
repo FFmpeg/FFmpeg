b382d1e3e49143c7e9ebab3986dd84b64073de66
Stefano Sabatini <stefano.sabatini-lala@poste.it>
2011-01-12 18:25:03 +0000

    Print link audio format name in tools/graph2dot.
    Originally committed as revision 26322 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- tools/graph2dot.c
+++ tools/graph2dot.c
@@ -68,8 +68,9 @@ static void print_digraph(FILE *outfile, AVFilterGraph *graph)
 
                 fprintf(outfile, "\"%s\" -> \"%s\"", filter_ctx_label, dst_filter_ctx_label);
                 fprintf(outfile, " [ label= \"fmt:%s w:%d h:%d tb:%d/%d\" ];\n",
-                        av_pix_fmt_descriptors[link->format].name, link->w, link->h,
-                        link->time_base.num, link->time_base.den);
+                        link->type == AVMEDIA_TYPE_VIDEO ? av_pix_fmt_descriptors[link->format].name :
+                        link->type == AVMEDIA_TYPE_AUDIO ? av_get_sample_fmt_name(link->format) : "unknown",
+                        link->w, link->h, link->time_base.num, link->time_base.den);
             }
         }
     }
