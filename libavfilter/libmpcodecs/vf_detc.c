45c889a3adf6df9e6a9bf5a2e7c960d866e2ccb4
Michael Niedermayer <michaelni@gmx.at>
2011-01-14 04:52:19 +0100

    Add #define _BSD_SOURCE where mplayer is not C99.

--- libavfilter/libmpcodecs/vf_detc.c
+++ libavfilter/libmpcodecs/vf_detc.c
@@ -16,6 +16,7 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
+#define _BSD_SOURCE //strdup
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
--- libavfilter/libmpcodecs/vf_palette.c
+++ libavfilter/libmpcodecs/vf_palette.c
@@ -16,6 +16,7 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
+#define _BSD_SOURCE //strcasecmp
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
