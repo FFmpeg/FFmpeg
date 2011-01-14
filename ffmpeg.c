fa34a3626cf1aa19acbe9f8c6fcddb6c3eb8eaa1
Stefano Sabatini <stefano.sabatini-lala@poste.it>
2011-01-14 15:50:55 +0100

    Make ffmpeg warns the user when the selected sample format is ignored.
    Signed-off-by: Mans Rullgard <mans@mansr.com>

--- ffmpeg.c
+++ ffmpeg.c
@@ -600,8 +600,14 @@ static void choose_sample_fmt(AVStream *st, AVCodec *codec)
             if(*p == st->codec->sample_fmt)
                 break;
         }
-        if(*p == -1)
+        if (*p == -1) {
+            av_log(NULL, AV_LOG_WARNING,
+                   "Incompatible sample format '%s' for codec '%s', auto-selecting format '%s'\n",
+                   av_get_sample_fmt_name(st->codec->sample_fmt),
+                   codec->name,
+                   av_get_sample_fmt_name(codec->sample_fmts[0]));
             st->codec->sample_fmt = codec->sample_fmts[0];
+        }
     }
 }
 
