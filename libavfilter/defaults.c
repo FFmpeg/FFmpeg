ef28c7b3a4f4513f7ea7c2f118eb806490d79546
Stefano Sabatini <stefano.sabatini-lala@poste.it>
2011-01-15 18:48:37 +0100

    lavfi: use av_samples_alloc() in avfilter_default_get_audio_buffer()

--- libavfilter/defaults.c
+++ libavfilter/defaults.c
@@ -86,8 +86,7 @@ AVFilterBufferRef *avfilter_default_get_audio_buffer(AVFilterLink *link, int per
 {
     AVFilterBuffer *samples = av_mallocz(sizeof(AVFilterBuffer));
     AVFilterBufferRef *ref = NULL;
-    int i, sample_size, chans_nb, bufsize, per_channel_size, step_size = 0;
-    char *buf;
+    int nb_channels = av_get_channel_layout_nb_channels(channel_layout);
 
     if (!samples || !(ref = av_mallocz(sizeof(AVFilterBufferRef))))
         goto fail;
@@ -109,40 +108,11 @@ AVFilterBufferRef *avfilter_default_get_audio_buffer(AVFilterLink *link, int per
     samples->refcount   = 1;
     samples->free       = ff_avfilter_default_free_buffer;
 
-    sample_size = av_get_bits_per_sample_fmt(sample_fmt) >>3;
-    chans_nb = av_get_channel_layout_nb_channels(channel_layout);
-
-    per_channel_size = nb_samples * sample_size;
-
-    /* Set the number of bytes to traverse to reach next sample of a particular channel:
-     * For planar, this is simply the sample size.
-     * For packed, this is the number of samples * sample_size.
-     */
-    for (i = 0; i < chans_nb; i++)
-        samples->linesize[i] = planar > 0 ? per_channel_size : sample_size;
-    memset(&samples->linesize[chans_nb], 0, (8-chans_nb) * sizeof(samples->linesize[0]));
-
     /* Calculate total buffer size, round to multiple of 16 to be SIMD friendly */
-    bufsize = (nb_samples * chans_nb * sample_size + 15)&~15;
-    buf = av_malloc(bufsize);
-    if (!buf)
+    if (av_samples_alloc(samples->data, samples->linesize,
+                         nb_channels, nb_samples, sample_fmt,
+                         planar, 16) < 0)
         goto fail;
-
-    /* For planar, set the start point of each channel's data within the buffer
-     * For packed, set the start point of the entire buffer only
-     */
-    samples->data[0] = buf;
-    if (buf && planar) {
-        for (i = 1; i < chans_nb; i++) {
-            step_size += per_channel_size;
-            samples->data[i] = buf + step_size;
-        }
-    } else {
-        for (i = 1; i < chans_nb; i++)
-            samples->data[i] = buf;
-    }
-
-    memset(&samples->data[chans_nb], 0, (8-chans_nb) * sizeof(samples->data[0]));
 
     memcpy(ref->data,     samples->data,     sizeof(ref->data));
     memcpy(ref->linesize, samples->linesize, sizeof(ref->linesize));
