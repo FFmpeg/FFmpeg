013291501fac5162e93bbcc9783e5e25d9cd2ab3
Michael Niedermayer <michaelni@gmx.at>
2011-01-16 22:08:34 +0000

    Allocate last picture for 4xm pfrm if its not allocated. Fixes segfault and issue2527
    Originally committed as revision 26397 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavcodec/4xm.c
+++ libavcodec/4xm.c
@@ -791,6 +791,14 @@ static int decode_frame(AVCodecContext *avctx,
         if(decode_i_frame(f, buf, frame_size) < 0)
             return -1;
     }else if(frame_4cc == AV_RL32("pfrm") || frame_4cc == AV_RL32("pfr2")){
+        if(!f->last_picture.data[0]){
+            f->last_picture.reference= 1;
+            if(avctx->get_buffer(avctx, &f->last_picture) < 0){
+                av_log(avctx, AV_LOG_ERROR, "get_buffer() failed\n");
+                return -1;
+            }
+        }
+
         p->pict_type= FF_P_TYPE;
         if(decode_p_frame(f, buf, frame_size) < 0)
             return -1;
