84cac86e7e82e287c8ea98d8f43d655ab553c07b
Ronald S. Bultje <rsbultje@gmail.com>
2011-01-16 03:20:03 +0000

    Fix fate breakage in r26382.
    Originally committed as revision 26383 to svn://svn.ffmpeg.org/ffmpeg/trunk

--- libavfilter/defaults.c
+++ libavfilter/defaults.c
@@ -209,7 +209,6 @@ int avfilter_default_config_output_link(AVFilterLink *link)
         if (link->type == AVMEDIA_TYPE_VIDEO) {
             link->w = link->src->inputs[0]->w;
             link->h = link->src->inputs[0]->h;
-            link->sample_aspect_ratio = link->src->inputs[0]->sample_aspect_ratio;
             link->time_base = link->src->inputs[0]->time_base;
         } else if (link->type == AVMEDIA_TYPE_AUDIO) {
             link->channel_layout = link->src->inputs[0]->channel_layout;
