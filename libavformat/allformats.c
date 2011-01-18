54bc6403620286f595becd01842947395020e2f4
Reimar Döffinger <Reimar.Doeffinger@gmx.de>
2011-01-18 07:59:49 +0100

    Missing parts of IVF muxer support.

--- libavformat/allformats.c
+++ libavformat/allformats.c
@@ -109,7 +109,7 @@ void av_register_all(void)
     REGISTER_MUXER    (IPOD, ipod);
     REGISTER_DEMUXER  (ISS, iss);
     REGISTER_DEMUXER  (IV8, iv8);
-    REGISTER_DEMUXER  (IVF, ivf);
+    REGISTER_MUXDEMUX (IVF, ivf);
     REGISTER_DEMUXER  (LMLM4, lmlm4);
     REGISTER_DEMUXER  (LXF, lxf);
     REGISTER_MUXDEMUX (M4V, m4v);
