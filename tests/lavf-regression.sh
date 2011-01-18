024f63709d8c42db3c3b4e6068768b7a99fc5025
Justin Ruggles <justin.ruggles@gmail.com>
2011-01-18 17:58:49 +0000

    Add regression test for stereo s16le in voc.
    Signed-off-by: Mans Rullgard <mans@mansr.com>
    (cherry picked from commit a4f5af13fb00d7f55946470bb0f52e1dbf5f3c6a)

--- tests/lavf-regression.sh
+++ tests/lavf-regression.sh
@@ -40,7 +40,7 @@ do_image_formats()
 do_audio_only()
 {
     file=${outfile}lavf.$1
-    do_ffmpeg $file -t 1 -qscale 10 -f s16le -i $pcm_src
+    do_ffmpeg $file -t 1 -qscale 10 $2 -f s16le -i $pcm_src $3
     do_ffmpeg_crc $file -i $target_path/$file
 }
 
@@ -204,6 +204,10 @@ fi
 
 if [ -n "$do_voc" ] ; then
 do_audio_only voc
+fi
+
+if [ -n "$do_voc_s16" ] ; then
+do_audio_only s16.voc "-ac 2" "-acodec pcm_s16le"
 fi
 
 if [ -n "$do_ogg" ] ; then
