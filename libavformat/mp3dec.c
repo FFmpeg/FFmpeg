204a18151c7c6deb65fcdce5273f23813df3dc83
Clément Bœsch <ubitux@gmail.com>
2011-01-21 23:55:31 +0100

    Move ID3v1 skip from decoder to demuxer
    Signed-off-by: Janne Grunau <janne-ffmpeg@jannau.net>
    (cherry picked from commit 045b80e52df7ba9c97084c946b4929000d5f5bc7)

--- libavformat/mp3dec.c
+++ libavformat/mp3dec.c
@@ -174,6 +174,11 @@ static int mp3_read_packet(AVFormatContext *s, AVPacket *pkt)
     if (ret <= 0) {
         return AVERROR(EIO);
     }
+
+    if (ret > ID3v1_TAG_SIZE &&
+        memcmp(&pkt->data[ret - ID3v1_TAG_SIZE], "TAG", 3) == 0)
+        ret -= ID3v1_TAG_SIZE;
+
     /* note: we need to modify the packet size here to handle the last
        packet */
     pkt->size = ret;
