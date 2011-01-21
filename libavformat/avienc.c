e910a77b0d203870253eaa8ec43527560221268d
Anton Khirnov <anton@khirnov.net>
2011-01-21 19:18:07 +0000

    avio: add av_put_str and deprecate put_strz in favor of it
    Signed-off-by: Mans Rullgard <mans@mansr.com>
    (cherry picked from commit 4efd5cf34b7a04f87805aa0f09913d1d122d300c)

--- libavformat/avienc.c
+++ libavformat/avienc.c
@@ -109,7 +109,7 @@ static void avi_write_info_tag(ByteIOContext *pb, const char *tag, const char *s
         len++;
         put_tag(pb, tag);
         put_le32(pb, len);
-        put_strz(pb, str);
+        avio_put_str(pb, str);
         if (len & 1)
             put_byte(pb, 0);
     }
