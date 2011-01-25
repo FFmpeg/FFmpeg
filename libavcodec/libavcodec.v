ac28ce5fac2f77891d70c9e49cda65ddb51818b2
Diego Elio Pettenò <flameeyes@gmail.com>
2011-01-25 21:40:12 +0000

    Hide the now-prefixed decoders, encoders, parsers, bsf, hwaccel objects.
    This significantly reduces the size of the symbol table in the generated ELF
    shared object (as well as the other linked tables).
    
    Signed-off-by: Mans Rullgard <mans@mansr.com>

--- libavcodec/libavcodec.v
+++ libavcodec/libavcodec.v
@@ -1,3 +1,9 @@
 LIBAVCODEC_$MAJOR {
         global: *;
+        local:
+                ff_*_bsf;
+                ff_*_decoder;
+                ff_*_encoder;
+                ff_*_hwaccel;
+                ff_*_parser;
 };
