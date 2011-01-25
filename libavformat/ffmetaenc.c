9de202a633e643a33f50ddba9dae00cc664c6854
Jai Menon <jai@retroficial.org>
2011-01-24 22:55:43 -0600

    ffmetaenc : Use more correct format specifiers.
    Additionally, this fixes a warning when building with clang.

--- libavformat/ffmetaenc.c
+++ libavformat/ffmetaenc.c
@@ -72,8 +72,8 @@ static int write_trailer(AVFormatContext *s)
         put_tag(s->pb, ID_CHAPTER);
         put_byte(s->pb, '\n');
         url_fprintf(s->pb, "TIMEBASE=%d/%d\n", ch->time_base.num, ch->time_base.den);
-        url_fprintf(s->pb, "START=%lld\n", ch->start);
-        url_fprintf(s->pb, "END=%lld\n",   ch->end);
+        url_fprintf(s->pb, "START=%"PRId64"\n", ch->start);
+        url_fprintf(s->pb, "END=%"PRId64"\n",   ch->end);
         write_tags(s->pb, ch->metadata);
     }
 
