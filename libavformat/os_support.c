13904b552b48e72bc472f98144ab54d0535ce99e
Mans Rullgard <mans@mansr.com>
2011-01-28 17:16:15 +0000

    os_support: make poll() fallbacks conditional on CONFIG_NETWORK
    poll() is only used by networking code, so the fallback should
    only be built if networking is enabled.  Also remove CONFIG_FFSERVER
    condition from the declarations.
    
    This should fix building on systems without poll(), broken
    by a8475bbdb64e638bd8161df9647876fd23f8a29a.
    
    Signed-off-by: Mans Rullgard <mans@mansr.com>
    (cherry picked from commit 362d8f7d9e6a3bca2ce89f767d153d3ab8bf5773)

--- libavformat/os_support.c
+++ libavformat/os_support.c
@@ -234,7 +234,6 @@ int ff_socket_nonblock(int socket, int enable)
       return fcntl(socket, F_SETFL, fcntl(socket, F_GETFL) & ~O_NONBLOCK);
 #endif
 }
-#endif /* CONFIG_NETWORK */
 
 #if !HAVE_POLL_H
 int poll(struct pollfd *fds, nfds_t numfds, int timeout)
@@ -304,3 +303,4 @@ int poll(struct pollfd *fds, nfds_t numfds, int timeout)
     return rc;
 }
 #endif /* HAVE_POLL_H */
+#endif /* CONFIG_NETWORK */
