e86e858111501650bb9ce8e39282e20c57bac913
Gianluigi Tiesi <mplayer@netfarm.it>
2011-02-01 04:12:26 +0000

    dca: avoid C99 declaration in for() expression
    Signed-off-by: Mans Rullgard <mans@mansr.com>

--- libavcodec/dca.c
+++ libavcodec/dca.c
@@ -1354,7 +1354,9 @@ static int dca_exss_mask2count(int mask)
  */
 static void dca_exss_skip_mix_coeffs(GetBitContext *gb, int channels, int out_ch)
 {
-    for (int i = 0; i < channels; i++) {
+    int i;
+
+    for (i = 0; i < channels; i++) {
         int mix_map_mask = get_bits(gb, out_ch);
         int num_coeffs = av_popcount(mix_map_mask);
         skip_bits_long(gb, num_coeffs * 6);
