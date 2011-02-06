e11afd71a37316e2ea4e765dcb7a6a72867d9bc8
Sascha Sommer <saschasommer@freenet.de>
2011-02-06 13:54:03 +0100

    pass QDMC extradata to the decoder
    Makes playing QDMC files in MPlayer work when using the libavformat demuxer.
    Problem was that the extradata was not passed from demuxer to decoder.
    
    Signed-off-by: Ronald S. Bultje <rsbultje@gmail.com>
    (cherry picked from commit ed19fafd486d6ab0d6e1eba80ceaa482a740c556)

--- libavcodec/avcodec.h
+++ libavcodec/avcodec.h
@@ -389,6 +389,7 @@ enum CodecID {
     CODEC_ID_BINKAUDIO_RDFT,
     CODEC_ID_BINKAUDIO_DCT,
     CODEC_ID_AAC_LATM,
+    CODEC_ID_QDMC,
 
     /* subtitle codecs */
     CODEC_ID_DVD_SUBTITLE= 0x17000,
