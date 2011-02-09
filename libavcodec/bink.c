adb1ad0d8005dfcbae863bccdda8fdd26bb63f30
Peter Ross <pross@xvid.org>
2011-02-09 23:05:19 +1100

    bink: reindent after last commit
    Signed-off-by: Janne Grunau <janne-ffmpeg@jannau.net>

--- libavcodec/bink.c
+++ libavcodec/bink.c
@@ -1163,13 +1163,13 @@ static int decode_frame(AVCodecContext *avctx, void *data, int *data_size, AVPac
     int bits_count = pkt->size << 3;
 
     if (c->version > 'b') {
-    if(c->pic.data[0])
-        avctx->release_buffer(avctx, &c->pic);
+        if(c->pic.data[0])
+            avctx->release_buffer(avctx, &c->pic);
 
-    if(avctx->get_buffer(avctx, &c->pic) < 0){
-        av_log(avctx, AV_LOG_ERROR, "get_buffer() failed\n");
-        return -1;
-    }
+        if(avctx->get_buffer(avctx, &c->pic) < 0){
+            av_log(avctx, AV_LOG_ERROR, "get_buffer() failed\n");
+            return -1;
+        }
     } else {
         if(avctx->reget_buffer(avctx, &c->pic) < 0){
             av_log(avctx, AV_LOG_ERROR, "reget_buffer() failed\n");
