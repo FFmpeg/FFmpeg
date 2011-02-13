2034ba08ec8d7d3eb66d83622a817f6bd2361c4f
Michael Niedermayer <michaelni@gmx.at>
2011-02-13 17:20:46 +0100

    Detect null gop headers. Fix issue2592. / mpeg4-bad-gvop.raw

--- libavcodec/mpeg4videodec.c
+++ libavcodec/mpeg4videodec.c
@@ -1495,6 +1495,11 @@ end:
 static int mpeg4_decode_gop_header(MpegEncContext * s, GetBitContext *gb){
     int hours, minutes, seconds;
 
+    if(!show_bits(gb, 18)){
+        av_log(s->avctx, AV_LOG_WARNING, "GOP header invalid\n");
+        return -1;
+    }
+
     hours= get_bits(gb, 5);
     minutes= get_bits(gb, 6);
     skip_bits1(gb);
