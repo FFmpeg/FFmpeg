938b62538a719785bce0f1d49d3065937b29e3aa
Stefano Sabatini <stefano.sabatini-lala@poste.it>
2011-02-14 10:59:25 +0100

    Document audio_resample_close().
    Signed-off-by: Janne Grunau <janne-ffmpeg@jannau.net>

--- libavcodec/avcodec.h
+++ libavcodec/avcodec.h
@@ -3247,6 +3247,13 @@ ReSampleContext *av_audio_resample_init(int output_channels, int input_channels,
                                         int linear, double cutoff);
 
 int audio_resample(ReSampleContext *s, short *output, short *input, int nb_samples);
+
+/**
+ * Free resample context.
+ *
+ * @param s a non-NULL pointer to a resample context previously
+ *          created with av_audio_resample_init()
+ */
 void audio_resample_close(ReSampleContext *s);
 
 
