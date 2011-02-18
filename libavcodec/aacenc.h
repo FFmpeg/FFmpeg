5e06b02005265d8c862e7c6ee3c6f1d0a5bd8c4d
Young Han Lee <cpumaker@gmail.com>
2011-02-18 09:33:11 +0900

    aacenc: remove the data arrays
    Signed-off-by: Ronald S. Bultje <rsbultje@gmail.com>
    (cherry picked from commit 2790d7a9ffbd51f33e5367a31ace5c44c30401a1)

--- libavcodec/aacenc.h
+++ libavcodec/aacenc.h
@@ -52,8 +52,7 @@ typedef struct AACEncContext {
     FFTContext mdct1024;                         ///< long (1024 samples) frame transform context
     FFTContext mdct128;                          ///< short (128 samples) frame transform context
     DSPContext  dsp;
-    DECLARE_ALIGNED(16, FFTSample, output)[2048]; ///< temporary buffer for MDCT input coefficients
-    int16_t* samples;                            ///< saved preprocessed input
+    int16_t *samples;                            ///< saved preprocessed input
 
     int samplerate_index;                        ///< MPEG-4 samplerate index
 
@@ -64,8 +63,8 @@ typedef struct AACEncContext {
     int cur_channel;
     int last_frame;
     float lambda;
+    float *scoefs;                               ///< scaled coefficients
     DECLARE_ALIGNED(16, int,   qcoefs)[96];      ///< quantized coefficients
-    DECLARE_ALIGNED(16, float, scoefs)[1024];    ///< scaled coefficients
 } AACEncContext;
 
 #endif /* AVCODEC_AACENC_H */
