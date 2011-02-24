49e617f9565b6528fe707bae7ea4b62b10c771a5
Mans Rullgard <mans@mansr.com>
2011-02-24 21:25:53 +0000

    yadif: add parens around macro parameters
    This fixes compilation with preprocessors which do not add whitespace
    around replaced tokens, resulting in invalid expressions like 1--1.
    
    Signed-off-by: Mans Rullgard <mans@mansr.com>

--- libavfilter/vf_yadif.c
+++ libavfilter/vf_yadif.c
@@ -73,12 +73,12 @@ static void filter_line_c(uint8_t *dst,
                           + FFABS(cur[mrefs+1] - cur[prefs+1]) - 1;
 
 #define CHECK(j)\
-    {   int score = FFABS(cur[mrefs-1+j] - cur[prefs-1-j])\
-                  + FFABS(cur[mrefs  +j] - cur[prefs  -j])\
-                  + FFABS(cur[mrefs+1+j] - cur[prefs+1-j]);\
+    {   int score = FFABS(cur[mrefs-1+(j)] - cur[prefs-1-(j)])\
+                  + FFABS(cur[mrefs  +(j)] - cur[prefs  -(j)])\
+                  + FFABS(cur[mrefs+1+(j)] - cur[prefs+1-(j)]);\
         if (score < spatial_score) {\
             spatial_score= score;\
-            spatial_pred= (cur[mrefs  +j] + cur[prefs  -j])>>1;\
+            spatial_pred= (cur[mrefs  +(j)] + cur[prefs  -(j)])>>1;\
 
         CHECK(-1) CHECK(-2) }} }}
         CHECK( 1) CHECK( 2) }} }}
