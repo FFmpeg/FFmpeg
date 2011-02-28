65ad2c618ffa83dd589f7d02d2ea9853aa477849
Baptiste Coudurier <baptiste.coudurier@gmail.com>
2011-02-27 16:29:21 -0800

    In mov muxer, fix adpcm mono muxing, patch by Alex Converse

--- libavformat/movenc.c
+++ libavformat/movenc.c
@@ -91,6 +91,7 @@ static int mov_write_stsz_tag(AVIOContext *pb, MOVTrack *track)
     }
     if (equalChunks) {
         int sSize = track->cluster[0].size/track->cluster[0].entries;
+        sSize = FFMAX(1, sSize); // adpcm mono case could make sSize == 0
         avio_wb32(pb, sSize); // sample size
         avio_wb32(pb, entries); // sample count
     }
