3bd624b4ee355328382295af411f5343b1329f43
Michael Niedermayer <michaelni@gmx.at>
2011-03-10 13:21:52 +0100

    Fix url_feof() for file appendings. Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/aviobuf.c
+++ libavformat/aviobuf.c
@@ -269,6 +269,10 @@ int url_feof(AVIOContext *s)
 {
     if(!s)
         return 0;
+    if(s->eof_reached){
+        s->eof_reached=0;
+        fill_buffer(s);
+    }
     return s->eof_reached;
 }
 
