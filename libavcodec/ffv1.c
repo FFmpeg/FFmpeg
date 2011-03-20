FFmpeg/FFmpeg

commit cbabccc3674241bbe466befc3acba261c3f5cbda

ffv1: remove VLAs
Originally committed as revision 23802 to svn://svn.ffmpeg.org/ffmpeg/trunk

libavcodec/ffv1.c
@@ -237,6 +237,7 @@ typedef struct FFV1Context{
     uint8_t state_transition[256];
     int run_index;
     int colorspace;
+    int_fast16_t *sample_buffer;
 
     DSPContext dsp;
 }FFV1Context;
@@ -509,14 +510,14 @@ static inline int encode_line(FFV1Context *s, int w, int_fast16_t *sample[2], in
 static void encode_plane(FFV1Context *s, uint8_t *src, int w, int h, int stride, int plane_index){
     int x,y,i;
     const int ring_size= s->avctx->context_model ? 3 : 2;
-    int_fast16_t sample_buffer[ring_size][w+6], *sample[ring_size];
+    int_fast16_t *sample[3];
     s->run_index=0;
 
-    memset(sample_buffer, 0, sizeof(sample_buffer));
+    memset(s->sample_buffer, 0, ring_size*(w+6)*sizeof(*s->sample_buffer));
 
     for(y=0; y<h; y++){
         for(i=0; i<ring_size; i++)
-            sample[i]= sample_buffer[(h+i-y)%ring_size]+3;
+            sample[i]= s->sample_buffer + (w+6)*((h+i-y)%ring_size) + 3;
 
         sample[0][-1]= sample[1][0  ];
         sample[1][ w]= sample[1][w-1];
@@ -539,15 +540,15 @@ static void encode_plane(FFV1Context *s, uint8_t *src, int w, int h, int stride,
 static void encode_rgb_frame(FFV1Context *s, uint32_t *src, int w, int h, int stride){
     int x, y, p, i;
     const int ring_size= s->avctx->context_model ? 3 : 2;
-    int_fast16_t sample_buffer[3][ring_size][w+6], *sample[3][ring_size];
+    int_fast16_t *sample[3][3];
     s->run_index=0;
 
-    memset(sample_buffer, 0, sizeof(sample_buffer));
+    memset(s->sample_buffer, 0, ring_size*3*(w+6)*sizeof(*s->sample_buffer));
 
     for(y=0; y<h; y++){
         for(i=0; i<ring_size; i++)
             for(p=0; p<3; p++)
-                sample[p][i]= sample_buffer[p][(h+i-y)%ring_size]+3;
+                sample[p][i]= s->sample_buffer + p*ring_size*(w+6) + ((h+i-y)%ring_size)*(w+6) + 3;
 
         for(x=0; x<w; x++){
             int v= src[x + stride*y];
@@ -630,6 +631,10 @@ static av_cold int common_init(AVCodecContext *avctx){
     s->height= avctx->height;
 
     assert(s->width && s->height);
+
+    s->sample_buffer = av_malloc(6 * (s->width+6) * sizeof(*s->sample_buffer));
+    if (!s->sample_buffer)
+        return AVERROR(ENOMEM);
 
     return 0;
 }
@@ -823,6 +828,8 @@ static av_cold int common_end(AVCodecContext *avctx){
         av_freep(&p->vlc_state);
     }
 
+    av_freep(&s->sample_buffer);
+
     return 0;
 }
 
@@ -885,14 +892,13 @@ static av_always_inline void decode_line(FFV1Context *s, int w, int_fast16_t *sa
 
 static void decode_plane(FFV1Context *s, uint8_t *src, int w, int h, int stride, int plane_index){
     int x, y;
-    int_fast16_t sample_buffer[2][w+6];
     int_fast16_t *sample[2];
-    sample[0]=sample_buffer[0]+3;
-    sample[1]=sample_buffer[1]+3;
+    sample[0]=s->sample_buffer    +3;
+    sample[1]=s->sample_buffer+w+6+3;
 
     s->run_index=0;
 
-    memset(sample_buffer, 0, sizeof(sample_buffer));
+    memset(s->sample_buffer, 0, 2*(w+6)*sizeof(*s->sample_buffer));
 
     for(y=0; y<h; y++){
         int_fast16_t *temp= sample[0]; //FIXME try a normal buffer
@@ -921,16 +927,15 @@ static void decode_plane(FFV1Context *s, uint8_t *src, int w, int h, int stride,
 
 static void decode_rgb_frame(FFV1Context *s, uint32_t *src, int w, int h, int stride){
     int x, y, p;
-    int_fast16_t sample_buffer[3][2][w+6];
     int_fast16_t *sample[3][2];
     for(x=0; x<3; x++){
-        sample[x][0] = sample_buffer[x][0]+3;
-        sample[x][1] = sample_buffer[x][1]+3;
+        sample[x][0] = s->sample_buffer +  x*2   *(w+6) + 3;
+        sample[x][1] = s->sample_buffer + (x*2+1)*(w+6) + 3;
     }
 
     s->run_index=0;
 
-    memset(sample_buffer, 0, sizeof(sample_buffer));
+    memset(s->sample_buffer, 0, 6*(w+6)*sizeof(*s->sample_buffer));
 
     for(y=0; y<h; y++){
         for(p=0; p<3; p++){

commit 027f60f32b758aa8e7c08685729084b1a12d81e9

ffv1: allocate correct size for sample buffer
This fixes a typo in the size calculation for the sample buffer
introduced in cbabccc367424.

Signed-off-by: Mans Rullgard <mans@mansr.com>

libavcodec/ffv1.c
@@ -792,7 +792,7 @@ static av_cold int init_slice_contexts(FFV1Context *f){
         fs->slice_x     = sxs;
         fs->slice_y     = sys;
 
-        fs->sample_buffer = av_malloc(6 * (fs->width+6) * sizeof(*fs->sample_buffer));
+        fs->sample_buffer = av_malloc(9 * (fs->width+6) * sizeof(*fs->sample_buffer));
         if (!fs->sample_buffer)
             return AVERROR(ENOMEM);
     }