2ee85c067129e5c456dc2eb1807140dc2e434d9a
Stefano Sabatini <stefano.sabatini-lala@poste.it>
2011-03-26 15:26:45 +0100

    ffmpeg: warns the user when the selected pixel format is ignored

--- ffmpeg.c
+++ ffmpeg.c
@@ -593,8 +593,14 @@ static void choose_pixel_fmt(AVStream *st, AVCodec *codec)
             if(*p == st->codec->pix_fmt)
                 break;
         }
-        if(*p == -1)
+        if (*p == -1) {
+            av_log(NULL, AV_LOG_WARNING,
+                   "Incompatible pixel format '%s' for codec '%s', auto-selecting format '%s'\n",
+                   av_pix_fmt_descriptors[st->codec->pix_fmt].name,
+                   codec->name,
+                   av_pix_fmt_descriptors[codec->pix_fmts[0]].name);
             st->codec->pix_fmt = codec->pix_fmts[0];
+        }
     }
 }
 
