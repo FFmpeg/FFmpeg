7f6e05cdfd1242a6774e89283b6e2cefde191590
Mans Rullgard <mans@mansr.com>
2011-03-31 01:28:08 +0100

    ac3enc: simplify sym_quant()
    These expressions are equivalent since levels is always odd, and
    overflow is impossible due to the constraints set by the assert().
    
    Signed-off-by: Mans Rullgard <mans@mansr.com>

--- libavcodec/ac3enc.c
+++ libavcodec/ac3enc.c
@@ -1192,7 +1192,7 @@ static int compute_bit_allocation(AC3EncodeContext *s)
  */
 static inline int sym_quant(int c, int e, int levels)
 {
-    int v = ((((levels * c) >> (24 - e)) + 1) >> 1) + (levels >> 1);
+    int v = (((levels * c) >> (24 - e)) + levels) >> 1;
     av_assert2(v >= 0 && v < levels);
     return v;
 }
