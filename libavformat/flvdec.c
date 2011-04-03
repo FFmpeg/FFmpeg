e3c007bb43d991836e2ebf941d14a04903ece654
Justin Ruggles <justin.ruggles@gmail.com>
2011-04-03 15:15:36 -0400

    flvdec: fix segfault in amf_parse_object() due to NULL key
    fixes Issue 2674

--- libavformat/flvdec.c
+++ libavformat/flvdec.c
@@ -211,7 +211,7 @@ static int amf_parse_object(AVFormatContext *s, AVStream *astream, AVStream *vst
         case AMF_DATA_TYPE_OBJECT: {
             unsigned int keylen;
 
-            if (!strcmp(KEYFRAMES_TAG, key) && depth == 1)
+            if (key && !strcmp(KEYFRAMES_TAG, key) && depth == 1)
                 if (parse_keyframes_index(s, ioc, vstream, max_pos) < 0)
                     return -1;
 
