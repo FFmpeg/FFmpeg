e601ff19b465a0ef75515ecb6366e2c925a791de
Stefano Sabatini <stefano.sabatini-lala@poste.it>
2011-04-03 17:46:43 +0200

    vsrc_buffer.h: add multiple inclusion guards
    Signed-off-by: Stefano Sabatini <stefano.sabatini-lala@poste.it>

--- libavfilter/vsrc_buffer.h
+++ libavfilter/vsrc_buffer.h
@@ -19,6 +19,9 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
+#ifndef AVFILTER_VSRC_BUFFER_H
+#define AVFILTER_VSRC_BUFFER_H
+
 #include "libavcodec/avcodec.h" /* AVFrame */
 #include "avfilter.h"
 
@@ -29,3 +32,5 @@ int av_vsrc_buffer_add_frame2(AVFilterContext *buffer_filter, AVFrame *frame,
                               int64_t pts, AVRational pixel_aspect, int width,
                               int height, enum PixelFormat  pix_fmt,
                               const char *sws_param);
+
+#endif /* AVFILTER_VSRC_BUFFER_H */
