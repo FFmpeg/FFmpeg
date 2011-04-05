2ed05f657f622a1c84f97a841a488e876f734201
Carl Eugen Hoyos <cehoyos@ag.or.at>
2011-04-05 16:12:41 +0200

    Add support for pcm_f32* to alsa.

--- libavdevice/alsa-audio-common.c
+++ libavdevice/alsa-audio-common.c
@@ -36,6 +36,8 @@
 static av_cold snd_pcm_format_t codec_id_to_pcm_format(int codec_id)
 {
     switch(codec_id) {
+        case CODEC_ID_PCM_F32LE: return SND_PCM_FORMAT_FLOAT_LE;
+        case CODEC_ID_PCM_F32BE: return SND_PCM_FORMAT_FLOAT_BE;
         case CODEC_ID_PCM_S32LE: return SND_PCM_FORMAT_S32_LE;
         case CODEC_ID_PCM_S32BE: return SND_PCM_FORMAT_S32_BE;
         case CODEC_ID_PCM_S16LE: return SND_PCM_FORMAT_S16_LE;
