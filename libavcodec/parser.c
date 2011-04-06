FFmpeg/FFmpeg

commit 9073ca6c6ebb9e3e9b60d968b7aa16cc8b6d558d

parser: mark av_parser_parse() for removal on next major bump
Fixes build with the bump, since the prototype is already marked for
removal.

libavcodec/parser.c
@@ -105,6 +105,7 @@ void ff_fetch_timestamp(AVCodecParserContext *s, int off, int remove){
     }
 }
 
+#if LIBAVCODEC_VERSION_MINOR < 53
 /**
  *
  * @param buf           input
@@ -139,6 +140,7 @@ int av_parser_parse(AVCodecParserContext *s,
 {
     return av_parser_parse2(s, avctx, poutbuf, poutbuf_size, buf, buf_size, pts, dts, AV_NOPTS_VALUE);
 }
+#endif
 
 int av_parser_parse2(AVCodecParserContext *s,
                      AVCodecContext *avctx,

commit 1f6265e011f6e56562b2f58c182bc0261062b3c4

Fix parser: mark av_parser_parse() for removal on next major bump
Fix the breakage introduced with 9073ca6c6ebb9e3e9b60d968b7aa16cc8b6d558d

libavcodec/parser.c
@@ -105,7 +105,7 @@ void ff_fetch_timestamp(AVCodecParserContext *s, int off, int remove){
     }
 }
 
-#if LIBAVCODEC_VERSION_MINOR < 53
+#if LIBAVCODEC_VERSION_MAJOR < 53
 /**
  *
  * @param buf           input