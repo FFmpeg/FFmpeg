acb62e998f4d72bd53cd6837cdcb2ba6f59ab3f1
Carl Eugen Hoyos <cehoyos@ag.or.at>
2011-04-14 01:04:18 +0200

    alsa: support unsigned variants of already supported signed formats.
    (cherry picked from commit 2359aeb52d2325ed6c28d4f7579e0999963bcec1)
    
    Signed-off-by: Reinhard Tartler <siretart@tauware.de>

--- libavdevice/alsa-audio-common.c
+++ libavdevice/alsa-audio-common.c
@@ -42,11 +42,18 @@ static av_cold snd_pcm_format_t codec_id_to_pcm_format(int codec_id)
         case CODEC_ID_PCM_F32BE: return SND_PCM_FORMAT_FLOAT_BE;
         case CODEC_ID_PCM_S32LE: return SND_PCM_FORMAT_S32_LE;
         case CODEC_ID_PCM_S32BE: return SND_PCM_FORMAT_S32_BE;
+        case CODEC_ID_PCM_U32LE: return SND_PCM_FORMAT_U32_LE;
+        case CODEC_ID_PCM_U32BE: return SND_PCM_FORMAT_U32_BE;
         case CODEC_ID_PCM_S24LE: return SND_PCM_FORMAT_S24_3LE;
         case CODEC_ID_PCM_S24BE: return SND_PCM_FORMAT_S24_3BE;
+        case CODEC_ID_PCM_U24LE: return SND_PCM_FORMAT_U24_3LE;
+        case CODEC_ID_PCM_U24BE: return SND_PCM_FORMAT_U24_3BE;
         case CODEC_ID_PCM_S16LE: return SND_PCM_FORMAT_S16_LE;
         case CODEC_ID_PCM_S16BE: return SND_PCM_FORMAT_S16_BE;
+        case CODEC_ID_PCM_U16LE: return SND_PCM_FORMAT_U16_LE;
+        case CODEC_ID_PCM_U16BE: return SND_PCM_FORMAT_U16_BE;
         case CODEC_ID_PCM_S8:    return SND_PCM_FORMAT_S8;
+        case CODEC_ID_PCM_U8:    return SND_PCM_FORMAT_U8;
         case CODEC_ID_PCM_MULAW: return SND_PCM_FORMAT_MU_LAW;
         case CODEC_ID_PCM_ALAW:  return SND_PCM_FORMAT_A_LAW;
         default:                 return SND_PCM_FORMAT_UNKNOWN;
