e2049edb4e6663c658d888ae0cf3632607333e12
Carl Eugen Hoyos <cehoyos@ag.or.at>
2011-04-14 01:14:19 +0200

    Add forgotten line to silence a warning for int32.
    AV_CH_LAYOUT_QUAD is the only (multi-) channel layout that has the
    same channel ordering for FFmpeg and alsa.

--- libavdevice/alsa-audio-common.c
+++ libavdevice/alsa-audio-common.c
@@ -141,6 +141,7 @@ static av_cold ff_reorder_func find_reorder_func(int codec_id,
             NULL :
     codec_id == CODEC_ID_PCM_U32LE || codec_id == CODEC_ID_PCM_U32BE ||
     codec_id == CODEC_ID_PCM_S32LE || codec_id == CODEC_ID_PCM_S32BE ?
+        layout == AV_CH_LAYOUT_QUAD ? REORDER_DUMMY :
         layout == AV_CH_LAYOUT_5POINT0_BACK || layout == AV_CH_LAYOUT_5POINT0 ?
             out ? alsa_reorder_int32_out_50 : NULL :
         layout == AV_CH_LAYOUT_5POINT1_BACK || layout == AV_CH_LAYOUT_5POINT1 ?
