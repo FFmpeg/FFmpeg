3372ec327b09abe492399d662c903ed77b359d45
Stefano Sabatini <stefano.sabatini-lala@poste.it>
2011-04-16 13:41:53 +0200

    cmdutils: add OPT_INT check in parse_number_or_die()
    Check that the value passed for an OPT_INT option is an int, fail
    otherwise.
    
    Signed-off-by: Anton Khirnov <anton@khirnov.net>

--- cmdutils.c
+++ cmdutils.c
@@ -107,6 +107,8 @@ double parse_number_or_die(const char *context, const char *numstr, int type, do
         error= "The value for %s was %s which is not within %f - %f\n";
     else if(type == OPT_INT64 && (int64_t)d != d)
         error= "Expected int64 for %s but found %s\n";
+    else if (type == OPT_INT && (int)d != d)
+        error= "Expected int for %s but found %s\n";
     else
         return d;
     fprintf(stderr, error, context, numstr, min, max);
