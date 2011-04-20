63de9e7d80ac3609fe378c5cadd5e899185846a7
Diego Biurrun <diego@biurrun.de>
2011-04-20 17:18:27 +0200

    Remove a version check in av_log made unnecessary by the big bump.

--- libavutil/log.c
+++ libavutil/log.c
@@ -91,7 +91,7 @@ void av_log_default_callback(void* ptr, int level, const char* fmt, va_list vl)
     line[0]=0;
 #undef fprintf
     if(print_prefix && avc) {
-        if(avc->version >= (50<<16 | 15<<8 | 3) && avc->parent_log_context_offset){
+        if (avc->parent_log_context_offset) {
             AVClass** parent= *(AVClass***)(((uint8_t*)ptr) + avc->parent_log_context_offset);
             if(parent && *parent){
                 snprintf(line, sizeof(line), "[%s @ %p] ", (*parent)->item_name(parent), parent);
