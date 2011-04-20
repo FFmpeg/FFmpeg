095290f9a4230698f64c56b5ee9d89c046f6dd5b
Anton Khirnov <anton@khirnov.net>
2011-04-20 17:09:45 +0200

    lavc: provide the opt.h header until the next bump
    AVOptions were moved to libavutil only recently.

---
+++ libavcodec/opt.h
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * This header is provided for compatibility only and will be removed
+ * on next major bump
+ */
+
+#ifndef AVCODEC_OPT_H
+#define AVCODEC_OPT_H
+
+#include "libavcodec/version.h"
+
+#if FF_API_OPT_H
+#include "libavutil/opt.h"
+#endif
+
+#endif
