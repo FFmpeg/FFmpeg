7d62475656af60a061b5186b96a519e4aeac892a
Michael Niedermayer <michaelni@gmx.at>
2011-04-26 03:06:51 +0200

    mp3enc: remove unneeded ifdef Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/mp3enc.c
+++ libavformat/mp3enc.c
@@ -412,9 +412,7 @@ static int mp3_write_packet(AVFormatContext *s, AVPacket *pkt)
     else {
         MP3Context  *mp3 = s->priv_data;
         MPADecodeHeader c;
-#ifdef FILTER_VBR_HEADERS
         int base;
-#endif
 
         ff_mpegaudio_decode_header(&c, AV_RB32(pkt->data));
 
