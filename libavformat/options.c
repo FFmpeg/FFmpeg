29e3489602aeb72dbd8ceebfcfa7025e8a57acaf
Anton Khirnov <anton@khirnov.net>
2011-04-29 11:42:05 +0200

    lavf: remove duplicate assignment in avformat_alloc_context.
    AVClass is already initialized in  avformat_get_context_defaults.

--- libavformat/options.c
+++ libavformat/options.c
@@ -86,6 +86,5 @@ AVFormatContext *avformat_alloc_context(void)
     ic = av_malloc(sizeof(AVFormatContext));
     if (!ic) return ic;
     avformat_get_context_defaults(ic);
-    ic->av_class = &av_format_context_class;
     return ic;
 }
