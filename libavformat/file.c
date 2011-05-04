8799541a0a7bd84c04a2fac73bf819aaf53fc302
Diego Biurrun <diego@biurrun.de>
2011-05-04 19:20:03 +0200

    Fix standalone compilation of pipe protocol.
    file_check() is not only used by the file protocol, adjust #ifdef accordingly.

--- libavformat/file.c
+++ libavformat/file.c
@@ -51,6 +51,19 @@ static int file_get_handle(URLContext *h)
     return (intptr_t) h->priv_data;
 }
 
+static int file_check(URLContext *h, int mask)
+{
+    struct stat st;
+    int ret = stat(h->filename, &st);
+    if (ret < 0)
+        return AVERROR(errno);
+
+    ret |= st.st_mode&S_IRUSR ? mask&AVIO_FLAG_READ  : 0;
+    ret |= st.st_mode&S_IWUSR ? mask&AVIO_FLAG_WRITE : 0;
+
+    return ret;
+}
+
 #if CONFIG_FILE_PROTOCOL
 
 static int file_open(URLContext *h, const char *filename, int flags)
@@ -93,19 +106,6 @@ static int file_close(URLContext *h)
 {
     int fd = (intptr_t) h->priv_data;
     return close(fd);
-}
-
-static int file_check(URLContext *h, int mask)
-{
-    struct stat st;
-    int ret = stat(h->filename, &st);
-    if (ret < 0)
-        return AVERROR(errno);
-
-    ret |= st.st_mode&S_IRUSR ? mask&AVIO_FLAG_READ  : 0;
-    ret |= st.st_mode&S_IWUSR ? mask&AVIO_FLAG_WRITE : 0;
-
-    return ret;
 }
 
 URLProtocol ff_file_protocol = {
