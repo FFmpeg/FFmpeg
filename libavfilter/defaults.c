21d84246d522d8f1b2435bb64089ca0098379680
Michael Niedermayer <michaelni@gmx.at>
2011-05-07 00:01:33 +0200

    avfilter: Fix ticket82 / fix initial buffer values. Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavfilter/defaults.c
+++ libavfilter/defaults.c
@@ -60,7 +60,7 @@ AVFilterBufferRef *avfilter_default_get_video_buffer(AVFilterLink *link, int per
     }
 
     // +2 is needed for swscaler, +16 to be SIMD-friendly
-    if (av_image_alloc(data, linesize, w, h, link->format, 16) < 0)
+    if ((i=av_image_alloc(data, linesize, w, h, link->format, 16)) < 0)
         return NULL;
 
     picref = avfilter_get_video_buffer_ref_from_arrays(data, linesize,
@@ -69,6 +69,8 @@ AVFilterBufferRef *avfilter_default_get_video_buffer(AVFilterLink *link, int per
         av_free(data[0]);
         return NULL;
     }
+    memset(data[0], 128, i);
+
     picref->buf->priv= link;
     picref->buf->free= NULL;
 
