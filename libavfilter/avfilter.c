fa3eddc0110cb237cd57c3e82cafdbcbf798b3a1
Michael Niedermayer <michaelni@gmx.at>
2011-05-06 23:37:43 +0200

    avfilter_unref_buffer: favor av_freep() Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavfilter/avfilter.c
+++ libavfilter/avfilter.c
@@ -75,8 +75,8 @@ void avfilter_unref_buffer(AVFilterBufferRef *ref)
         return;
     if (!(--ref->buf->refcount))
         ref->buf->free(ref->buf);
-    av_free(ref->video);
-    av_free(ref->audio);
+    av_freep(&ref->video);
+    av_freep(&ref->audio);
     av_free(ref);
 }
 
