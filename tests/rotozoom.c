771339ca206468636a64a6041852068be2da3dd2
Diego Biurrun <diego@biurrun.de>
2011-05-09 11:16:55 +0200

    rotozoom: Return an error value instead of calling exit().

--- tests/rotozoom.c
+++ tests/rotozoom.c
@@ -279,7 +279,7 @@ int main(int argc, char **argv)
     if (argc != 3) {
         printf("usage: %s directory/ image.pnm\n"
                "generate a test video stream\n", argv[0]);
-        exit(1);
+        return 1;
     }
 
     w = DEFAULT_WIDTH;
