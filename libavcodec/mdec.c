32ac63ee10ca5daa149344a75d736c1b98177392
Ronald S. Bultje <rsbultje@gmail.com>
2011-05-10 11:29:08 -0400

    mdec.c: fix overread.

--- libavcodec/mdec.c
+++ libavcodec/mdec.c
@@ -125,7 +125,8 @@ static inline int decode_mb(MDECContext *a, DCTELEM block[6][64]){
     a->dsp.clear_blocks(block[0]);
 
     for(i=0; i<6; i++){
-        if( mdec_decode_block_intra(a, block[ block_index[i] ], block_index[i]) < 0)
+        if( mdec_decode_block_intra(a, block[ block_index[i] ], block_index[i]) < 0 ||
+            get_bits_left(&a->gb) < 0)
             return -1;
     }
     return 0;
