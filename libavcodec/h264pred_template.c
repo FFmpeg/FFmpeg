c77b858c4cafe74cd663bdd1bc9d96a487b20e14
Ronald S. Bultje <rsbultje@gmail.com>
2011-05-11 17:22:41 +0200

    h264pred: fix one more aliasing violation.

--- libavcodec/h264pred_template.c
+++ libavcodec/h264pred_template.c
@@ -695,10 +695,12 @@ static void FUNCC(pred8x8l_horizontal)(uint8_t *p_src, int has_topleft, int has_
 {
     pixel *src = (pixel*)p_src;
     int stride = p_stride>>(sizeof(pixel)-1);
+    pixel4 a;
 
     PREDICT_8x8_LOAD_LEFT;
-#define ROW(y) ((pixel4*)(src+y*stride))[0] =\
-               ((pixel4*)(src+y*stride))[1] = PIXEL_SPLAT_X4(l##y)
+#define ROW(y) a = PIXEL_SPLAT_X4(l##y); \
+               AV_WN4PA(src+y*stride, a); \
+               AV_WN4PA(src+y*stride+4, a);
     ROW(0); ROW(1); ROW(2); ROW(3); ROW(4); ROW(5); ROW(6); ROW(7);
 #undef ROW
 }
