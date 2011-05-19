9f2405661d5bcc9416f4b3339f1139997467e1f5
Diego Biurrun <diego@biurrun.de>
2011-05-19 19:22:41 +0200

    Collapse tableprint.c into tableprint.h.
    tableprint.c serves little purpose on its own and removing it allows building
    the table generator programs with the normal HOSTPROGS Makefile rules.

--- libavcodec/tableprint.h
+++ libavcodec/tableprint.h
@@ -23,8 +23,9 @@
 #ifndef AVCODEC_TABLEPRINT_H
 #define AVCODEC_TABLEPRINT_H
 
-#include <stdint.h>
+#include <inttypes.h>
 #include <stdio.h>
+
 #include "libavutil/common.h"
 
 #define WRITE_1D_FUNC_ARGV(type, linebrk, fmtstr, ...)\
@@ -70,9 +71,6 @@ void write_uint32_t_2d_array(const void *, int, int);
 void write_float_2d_array   (const void *, int, int);
 /** \} */ // end of printfuncs group
 
-/** Write a standard file header */
-void write_fileheader(void);
-
 #define WRITE_ARRAY(prefix, type, name)                 \
     do {                                                \
         const size_t array_size = FF_ARRAY_ELEMS(name); \
@@ -91,5 +89,23 @@ void write_fileheader(void);
         write_##type##_2d_array(name, array_size1, array_size2);        \
         printf("};\n");                                                 \
     } while(0)
+
+
+WRITE_1D_FUNC(int8_t,   "%3"PRIi8, 15)
+WRITE_1D_FUNC(uint8_t,  "0x%02"PRIx8, 15)
+WRITE_1D_FUNC(uint16_t, "0x%08"PRIx16, 7)
+WRITE_1D_FUNC(uint32_t, "0x%08"PRIx32, 7)
+WRITE_1D_FUNC(float,    "%.18e", 3)
+
+WRITE_2D_FUNC(int8_t)
+WRITE_2D_FUNC(uint8_t)
+WRITE_2D_FUNC(uint32_t)
+WRITE_2D_FUNC(float)
+
+static inline void write_fileheader(void)
+{
+    printf("/* This file was automatically generated. */\n");
+    printf("#include <stdint.h>\n");
+}
 
 #endif /* AVCODEC_TABLEPRINT_H */
