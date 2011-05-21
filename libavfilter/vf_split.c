a38a00eddf200e18c6cf3ba090beaa3b224bd114
Bobby Bingham <uhmmmm@gmail.com>
2011-05-21 16:46:11 +0200

    libavfilter: vf_split from soc.
    Commited by michael, for detailed authorship see soc repo

---
+++ libavfilter/vf_split.c
@@ -0,0 +1,63 @@
+/*
+ * Video splitter
+ * copyright (c) 2007 Bobby Bingham
+ *
+ * This file is part of FFmpeg.
+ *
+ * FFmpeg is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * FFmpeg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FFmpeg; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#include "avfilter.h"
+
+static void start_frame(AVFilterLink *link, AVFilterBufferRef *picref)
+{
+    avfilter_start_frame(link->dst->outputs[0],
+                         avfilter_ref_buffer(picref, ~AV_PERM_WRITE));
+    avfilter_start_frame(link->dst->outputs[1],
+                         avfilter_ref_buffer(picref, ~AV_PERM_WRITE));
+}
+
+static void end_frame(AVFilterLink *link)
+{
+    avfilter_end_frame(link->dst->outputs[0]);
+    avfilter_end_frame(link->dst->outputs[1]);
+
+    avfilter_unref_buffer(link->cur_buf);
+}
+
+static void draw_slice(AVFilterLink *link, int y, int h, int slice_dir)
+{
+    avfilter_draw_slice(link->dst->outputs[0], y, h, slice_dir);
+    avfilter_draw_slice(link->dst->outputs[1], y, h, slice_dir);
+}
+
+AVFilter avfilter_vf_split =
+{
+    .name      = "split",
+
+    .inputs    = (AVFilterPad[]) {{ .name            = "default",
+                                    .type            = AVMEDIA_TYPE_VIDEO,
+                                    .get_video_buffer= avfilter_null_get_video_buffer,
+                                    .start_frame     = start_frame,
+                                    .draw_slice      = draw_slice,
+                                    .end_frame       = end_frame, },
+                                  { .name = NULL}},
+    .outputs   = (AVFilterPad[]) {{ .name            = "default",
+                                    .type            = AVMEDIA_TYPE_VIDEO, },
+                                  { .name            = "default2",
+                                    .type            = AVMEDIA_TYPE_VIDEO, },
+                                  { .name = NULL}},
+};
+
