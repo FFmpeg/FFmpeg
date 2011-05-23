1bc81bf981552ec542e14af194cd05d7c74a83e1
Michael Niedermayer <michaelni@gmx.at>
2011-05-24 01:31:15 +0200

    avcodec_find_decoder: prefer non experimental decoders.
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/utils.c
+++ libavcodec/utils.c
@@ -903,14 +903,18 @@ AVCodec *avcodec_find_encoder_by_name(const char *name)
 
 AVCodec *avcodec_find_decoder(enum CodecID id)
 {
-    AVCodec *p;
+    AVCodec *p, *experimental=NULL;
     p = first_avcodec;
     while (p) {
-        if (p->decode != NULL && p->id == id)
-            return p;
+        if (p->decode != NULL && p->id == id) {
+            if (p->capabilities & CODEC_CAP_EXPERIMENTAL && !experimental) {
+                experimental = p;
+            } else
+                return p;
+        }
         p = p->next;
     }
-    return NULL;
+    return experimental;
 }
 
 AVCodec *avcodec_find_decoder_by_name(const char *name)
