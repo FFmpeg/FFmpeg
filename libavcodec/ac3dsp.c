dc0ad40de2b0d6995eb842e56b22f9096bd539ff
Justin Ruggles <justin.ruggles@gmail.com>
2011-05-28 14:40:16 -0400

    ac3dsp: fix loop condition in ac3_update_bap_counts_c()

--- libavcodec/ac3dsp.c
+++ libavcodec/ac3dsp.c
@@ -131,7 +131,7 @@ static void ac3_bit_alloc_calc_bap_c(int16_t *mask, int16_t *psd,
 static void ac3_update_bap_counts_c(uint16_t mant_cnt[16], uint8_t *bap,
                                     int len)
 {
-    while (len-- >= 0)
+    while (len-- > 0)
         mant_cnt[bap[len]]++;
 }
 
