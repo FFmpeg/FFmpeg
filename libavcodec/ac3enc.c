90da52f01f8b6c22af22a002eb226989b1cf7ef8
Mans Rullgard <mans@mansr.com>
2011-05-28 20:27:09 +0100

    ac3enc: fix LOCAL_ALIGNED usage in count_mantissa_bits()
    Signed-off-by: Mans Rullgard <mans@mansr.com>

--- libavcodec/ac3enc.c
+++ libavcodec/ac3enc.c
@@ -1519,7 +1519,7 @@ static void count_mantissa_bits_update_ch(AC3EncodeContext *s, int ch,
 static int count_mantissa_bits(AC3EncodeContext *s)
 {
     int ch, max_end_freq;
-    LOCAL_ALIGNED_16(uint16_t, mant_cnt,[AC3_MAX_BLOCKS][16]);
+    LOCAL_ALIGNED_16(uint16_t, mant_cnt, [AC3_MAX_BLOCKS], [16]);
 
     count_mantissa_bits_init(mant_cnt);
 
