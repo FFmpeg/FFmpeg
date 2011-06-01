3c194f390fe7e18a6e2e51eb4f29495eb230585e
Reimar Döffinger <Reimar.Doeffinger@gmx.de>
2011-06-01 22:15:36 +0200

    Replace "vector const" by "const vector" otherwise gcc 4.6.0 fails.
    Given that this compiles fine with the Apple compiler that is probably
    a gcc bug, but "const vector" is nicer anyway.

--- libswscale/ppc/yuv2rgb_altivec.c
+++ libswscale/ppc/yuv2rgb_altivec.c
@@ -298,7 +298,7 @@ static int altivec_##name (SwsContext *c,                               \
     vector signed short R1,G1,B1;                                       \
     vector unsigned char R,G,B;                                         \
                                                                         \
-    vector const unsigned char *y1ivP, *y2ivP, *uivP, *vivP;            \
+    const vector unsigned char *y1ivP, *y2ivP, *uivP, *vivP;            \
     vector unsigned char align_perm;                                    \
                                                                         \
     vector signed short                                                 \
@@ -335,10 +335,10 @@ static int altivec_##name (SwsContext *c,                               \
                                                                         \
         for (j=0;j<w/16;j++) {                                          \
                                                                         \
-            y1ivP = (vector const unsigned char *)y1i;                  \
-            y2ivP = (vector const unsigned char *)y2i;                  \
-            uivP  = (vector const unsigned char *)ui;                   \
-            vivP  = (vector const unsigned char *)vi;                   \
+            y1ivP = (const vector unsigned char *)y1i;                  \
+            y2ivP = (const vector unsigned char *)y2i;                  \
+            uivP  = (const vector unsigned char *)ui;                   \
+            vivP  = (const vector unsigned char *)vi;                   \
                                                                         \
             align_perm = vec_lvsl (0, y1i);                             \
             y0 = (vector unsigned char)                                 \
