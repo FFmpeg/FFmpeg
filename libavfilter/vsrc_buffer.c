9e66b64c360568a91faa84d0cda96ab93c467502
Stefano Sabatini <stefano.sabatini-lala@poste.it>
2011-06-02 16:09:24 +0200

    vsrc_buffer: propagate error code in av_vsrc_buffer_add_frame()
    Propagate av_vsrc_buffer_add_video_buffer_ref() error code rather than
    return 0.

--- libavfilter/vsrc_buffer.c
+++ libavfilter/vsrc_buffer.c
@@ -111,15 +111,16 @@ int av_vsrc_buffer_add_video_buffer_ref(AVFilterContext *buffer_filter, AVFilter
 
 int av_vsrc_buffer_add_frame(AVFilterContext *buffer_src, const AVFrame *frame)
 {
+    int ret;
     AVFilterBufferRef *picref =
         avfilter_get_video_buffer_ref_from_frame(frame, AV_PERM_WRITE);
     if (!picref)
         return AVERROR(ENOMEM);
-    av_vsrc_buffer_add_video_buffer_ref(buffer_src, picref);
+    ret = av_vsrc_buffer_add_video_buffer_ref(buffer_src, picref);
     picref->buf->data[0] = NULL;
     avfilter_unref_buffer(picref);
 
-    return 0;
+    return ret;
 }
 #endif
 
