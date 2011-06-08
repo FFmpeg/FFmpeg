e5f4e249422834f727bcd432b73af971277f1371
Mina Nagy Zaki <mnzaki@gmail.com>
2011-06-08 19:24:25 +0300

    lavfi: avfilter_merge_formats: handle case where inputs are same
    This fixes a double-free crash if lists are the same due to the two
    merge_ref() calls at the end of the (useless) merging that happens.
    
    Signed-off-by: Anton Khirnov <anton@khirnov.net>
    (cherry picked from commit 11b6a82412bcd372adf694a26d83b07d337e1325)
    
    Conflicts:
    
    	libavfilter/formats.c
    
    Signed-off-by: Reinhard Tartler <siretart@tauware.de>

--- libavfilter/formats.c
+++ libavfilter/formats.c
@@ -45,6 +45,9 @@ AVFilterFormats *avfilter_merge_formats(AVFilterFormats *a, AVFilterFormats *b)
     AVFilterFormats *ret;
     unsigned i, j, k = 0, m_count;
 
+    if (a == b)
+        return a;
+
     ret = av_mallocz(sizeof(AVFilterFormats));
 
     /* merge list of formats */
