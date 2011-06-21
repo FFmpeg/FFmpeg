684f4abfac6e007d3481e06e1c0777b826e3ba42
Justin Ruggles <justin.ruggles@gmail.com>
2011-06-21 16:49:16 -0400

    ac3enc: remove unneeded local variable in asym_quant()

--- libavcodec/ac3enc.c
+++ libavcodec/ac3enc.c
@@ -1123,14 +1123,14 @@ static inline int sym_quant(int c, int e, int levels)
  */
 static inline int asym_quant(int c, int e, int qbits)
 {
-    int m, v;
+    int m;
 
-    v = (((c << e) >> (24 - qbits)) + 1) >> 1;
+    c = (((c << e) >> (24 - qbits)) + 1) >> 1;
     m = (1 << (qbits-1));
-    if (v >= m)
-        v = m - 1;
-    av_assert2(v >= -m);
-    return v;
+    if (c >= m)
+        c = m - 1;
+    av_assert2(c >= -m);
+    return c;
 }
 
 
