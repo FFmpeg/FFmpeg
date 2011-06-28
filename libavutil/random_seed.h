94e59cb84b0fbc016333c22685cdc4afeabbdf08
Michael Niedermayer <michaelni@gmx.at>
2011-06-28 04:23:20 +0200

    random_seed: more elaborate doxy.
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavutil/random_seed.h
+++ libavutil/random_seed.h
@@ -25,6 +25,10 @@
 
 /**
  * Get a seed to use in conjunction with random functions.
+ * This function tries to provide a good seed at a best effort bases.
+ * Its possible to call this function multiple times if more bits are needed.
+ * It can be quite slow, which is why it should only be used as seed for a faster
+ * PRNG. The quality of the seed depends on the platform.
  */
 uint32_t av_get_random_seed(void);
 
