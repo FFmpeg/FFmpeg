da55ee6ccc05efdd9006bb8c31db9012a3326857
Jason Garrett-Glaser <jason@x264.com>
2011-06-28 18:22:23 -0700

    Add new yuv444 pixfmts to avcodec_align_dimensions2
    Fixes draw_edges crashes with high-bit-depth 4:4:4 decoding.

--- libavcodec/utils.c
+++ libavcodec/utils.c
@@ -141,6 +141,10 @@ void avcodec_align_dimensions2(AVCodecContext *s, int *width, int *height, int l
     case PIX_FMT_YUV420P10BE:
     case PIX_FMT_YUV422P10LE:
     case PIX_FMT_YUV422P10BE:
+    case PIX_FMT_YUV444P9LE:
+    case PIX_FMT_YUV444P9BE:
+    case PIX_FMT_YUV444P10LE:
+    case PIX_FMT_YUV444P10BE:
         w_align= 16; //FIXME check for non mpeg style codecs and use less alignment
         h_align= 16;
         if(s->codec_id == CODEC_ID_MPEG2VIDEO || s->codec_id == CODEC_ID_MJPEG || s->codec_id == CODEC_ID_AMV || s->codec_id == CODEC_ID_THP || s->codec_id == CODEC_ID_H264)
