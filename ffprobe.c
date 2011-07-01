46edd3a01b68f7a1ca833510bef39f599a5055c9
Stefano Sabatini <stefano.sabatini-lala@poste.it>
2011-07-01 12:52:46 +0200

    ffprobe: fix opt_pretty() signature
    This is required after the OPT_FUNC2 removal.

--- ffprobe.c
+++ ffprobe.c
@@ -364,12 +364,13 @@ static int opt_help(const char *opt, const char *arg)
     return 0;
 }
 
-static void opt_pretty(void)
+static int opt_pretty(const char *opt, const char *arg)
 {
     show_value_unit              = 1;
     use_value_prefix             = 1;
     use_byte_value_binary_prefix = 1;
     use_value_sexagesimal_format = 1;
+    return 0;
 }
 
 static const OptionDef options[] = {
