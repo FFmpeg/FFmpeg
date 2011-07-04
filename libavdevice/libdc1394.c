d3f751e6030545c5e1f312c5a83c1e6ed5794f18
Diego Biurrun <diego@biurrun.de>
2011-07-05 00:42:31 +0200

    Add some missing mathematics.h #includes for av_rescale().

--- libavdevice/libdc1394.c
+++ libavdevice/libdc1394.c
@@ -23,6 +23,7 @@
 #include "config.h"
 #include "libavformat/avformat.h"
 #include "libavutil/log.h"
+#include "libavutil/mathematics.h"
 #include "libavutil/opt.h"
 #include "libavutil/parseutils.h"
 #include "libavutil/pixdesc.h"
