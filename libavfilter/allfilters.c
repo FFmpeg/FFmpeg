6b61920ab76dc6d85ef462909951923935dd643f
Stefano Sabatini <stefano.sabatini-lala@poste.it>
2011-07-09 18:13:10 +0200

    lavfi: port boxblur filter from libmpcodecs
    With the following additions:
    * support to gray format
    * support to yuva420p format
    * parametric luma/chroma/alpha radius
    * consistency check on the radius values, avoid crashes with invalid values

--- libavfilter/allfilters.c
+++ libavfilter/allfilters.c
@@ -41,6 +41,7 @@ void avfilter_register_all(void)
     REGISTER_FILTER (ANULLSINK,   anullsink,   asink);
 
     REGISTER_FILTER (BLACKFRAME,  blackframe,  vf);
+    REGISTER_FILTER (BOXBLUR,     boxblur,     vf);
     REGISTER_FILTER (COPY,        copy,        vf);
     REGISTER_FILTER (CROP,        crop,        vf);
     REGISTER_FILTER (CROPDETECT,  cropdetect,  vf);
