ffe3856de7a5f18aba36b56a314aee48d7b7ac78
Stefano Sabatini <stefano.sabatini-lala@poste.it>
2011-07-09 15:39:16 +0200

    examples/encoding.c: remove reference to unexisting file

--- doc/examples/encoding.c
+++ doc/examples/encoding.c
@@ -22,7 +22,6 @@
  * @file
  * libavcodec API use example.
  *
- * @example libavcodec/api-example.c
  * Note that this library only handles codecs (mpeg, mpeg4, etc...),
  * not file formats (avi, vob, etc...). See library 'libavformat' for the
  * format handling
