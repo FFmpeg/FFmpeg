daca33659d12fd85062bc1798f4eb7ffeb83cc3c
Martin Storsjö <martin@martin.st>
2011-07-14 23:01:29 +0300

    ffmpeg: Fix the check for experimental codecs
    The variable 'codec' was used uninitialized here.
    
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- ffmpeg.c
+++ ffmpeg.c
@@ -522,7 +522,7 @@ static void assert_codec_experimental(AVCodecContext *c, int encoder)
         av_log(NULL, AV_LOG_ERROR, "%s '%s' is experimental and might produce bad "
                 "results.\nAdd '-strict experimental' if you want to use it.\n",
                 codec_string, c->codec->name);
-        codec = encoder ? avcodec_find_encoder(codec->id) : avcodec_find_decoder(codec->id);
+        codec = encoder ? avcodec_find_encoder(c->codec->id) : avcodec_find_decoder(c->codec->id);
         if (!(codec->capabilities & CODEC_CAP_EXPERIMENTAL))
             av_log(NULL, AV_LOG_ERROR, "Or use the non experimental %s '%s'.\n",
                    codec_string, codec->name);
