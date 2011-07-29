18cb40f074ec7e79b20bf9b8c8f19c55784a071c
Dustin Brody <libav@parsoma.net>
2011-07-29 03:25:34 -0400

    lavc: Deprecate unused FF_ER_VERY_AGGRESSIVE
    Signed-off-by: Ronald S. Bultje <rsbultje@gmail.com>

--- libavcodec/avcodec.h
+++ libavcodec/avcodec.h
@@ -1445,8 +1445,12 @@ typedef struct AVCodecContext {
 #define FF_ER_CAREFUL         1
 #define FF_ER_COMPLIANT       2
 #define FF_ER_AGGRESSIVE      3
+#if FF_API_VERY_AGGRESSIVE
 #define FF_ER_VERY_AGGRESSIVE 4
 #define FF_ER_EXPLODE         5
+#else
+#define FF_ER_EXPLODE         4
+#endif /* FF_API_VERY_AGGRESSIVE */
 
     /**
      * Called at the beginning of each frame to get a buffer for it.
