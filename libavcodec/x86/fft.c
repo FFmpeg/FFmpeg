18b131de0473a3110c63966cd7c6cd2ab118d401
Vitor Sessak <vitor1001@gmail.com>
2011-07-30 18:39:25 +0200

    dct32: Add SSE2 ASM optimizations
    Signed-off-by: Ronald S. Bultje <rsbultje@gmail.com>

--- libavcodec/x86/fft.c
+++ libavcodec/x86/fft.c
@@ -60,6 +60,8 @@ av_cold void ff_dct_init_mmx(DCTContext *s)
     int has_vectors = av_get_cpu_flags();
     if (has_vectors & AV_CPU_FLAG_AVX && HAVE_AVX)
         s->dct32 = ff_dct32_float_avx;
+    else if (has_vectors & AV_CPU_FLAG_SSE2 && HAVE_SSE)
+        s->dct32 = ff_dct32_float_sse2;
     else if (has_vectors & AV_CPU_FLAG_SSE && HAVE_SSE)
         s->dct32 = ff_dct32_float_sse;
 #endif
