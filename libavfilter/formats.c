a7196795613f2cd416cf2c51c767a1125e27b057
Stefano Sabatini <stefano.sabatini-lala@poste.it>
2011-08-16 01:59:50 +0200

    lavfi: complain and exit for invalid named values in ff_parse_packing_format()

--- libavfilter/formats.c
+++ libavfilter/formats.c
@@ -283,8 +283,11 @@ int ff_parse_packing_format(int *ret, const char *arg, void *log_ctx)
     char *tail;
     int planar = strtol(arg, &tail, 10);
     if (*tail) {
-        planar = (strcmp(arg, "packed") != 0);
-    } else if (planar != 0 && planar != 1) {
+        planar = !strcmp(arg, "packed") ? 0:
+                 !strcmp(arg, "planar") ? 1: -1;
+    }
+
+    if (planar != 0 && planar != 1) {
         av_log(log_ctx, AV_LOG_ERROR, "Invalid packing format '%s'\n", arg);
         return AVERROR(EINVAL);
     }
