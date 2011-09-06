c0323b9c9bcebe029df0b19a19a6f81deef94b3a
Michael Niedermayer <michaelni@gmx.at>
2011-09-06 22:16:26 +0200

    avfilter: dont crash on filters with NULL instance names.
    Found-by: Rolf Siegrist
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavfilter/avfiltergraph.c
+++ libavfilter/avfiltergraph.c
@@ -272,7 +272,7 @@ int avfilter_graph_send_command(AVFilterGraph *graph, const char *target, const 
 
     for (i = 0; i < graph->filter_count; i++) {
         AVFilterContext *filter = graph->filters[i];
-        if(!strcmp(target, "all") || !strcmp(target, filter->name) || !strcmp(target, filter->filter->name)){
+        if(!strcmp(target, "all") || (filter->name && !strcmp(target, filter->name)) || !strcmp(target, filter->filter->name)){
             r = avfilter_process_command(filter, cmd, arg, res, res_len, flags);
             if(r != AVERROR(ENOSYS)) {
                 if((flags & AVFILTER_CMD_FLAG_ONE) || r<0)
