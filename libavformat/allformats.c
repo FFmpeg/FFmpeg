0ca36b4de76e10578e23199c2932682c0f510e31
Kieran Kunhya <kieran@kunhya.com>
2011-09-06 20:05:27 +0100

    Add LATM muxer
    Signed-off-by: Janne Grunau <janne-libav@jannau.net>

--- libavformat/allformats.c
+++ libavformat/allformats.c
@@ -113,6 +113,7 @@ void av_register_all(void)
     REGISTER_DEMUXER  (IV8, iv8);
     REGISTER_MUXDEMUX (IVF, ivf);
     REGISTER_DEMUXER  (JV, jv);
+    REGISTER_MUXER    (LATM, latm);
     REGISTER_DEMUXER  (LMLM4, lmlm4);
     REGISTER_DEMUXER  (LXF, lxf);
     REGISTER_MUXDEMUX (M4V, m4v);
