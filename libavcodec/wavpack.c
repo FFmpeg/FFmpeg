aee461277a54736511fdcb6298f0e7f9d90e0672
Laurent Aimar <fenrir@videolan.org>
2011-09-07 23:12:32 +0200

    Fixed invalid access in wavpack decoder on corrupted extra bits sub-blocks.
    Signed-off-by: Martin Storsjö <martin@martin.st>
    (cherry picked from commit beefafda639dd53fc59c21d8a7cf8334da9a1062)
    
    Signed-off-by: Anton Khirnov <anton@khirnov.net>

--- libavcodec/wavpack.c
+++ libavcodec/wavpack.c
@@ -385,7 +385,7 @@ static inline int wv_get_value_integer(WavpackFrameContext *s, uint32_t *crc, in
     if(s->extra_bits){
         S <<= s->extra_bits;
 
-        if(s->got_extra_bits){
+        if(s->got_extra_bits && get_bits_left(&s->gb_extra_bits) >= s->extra_bits){
             S |= get_bits(&s->gb_extra_bits, s->extra_bits);
             *crc = *crc * 9 + (S&0xffff) * 3 + ((unsigned)S>>16);
         }
