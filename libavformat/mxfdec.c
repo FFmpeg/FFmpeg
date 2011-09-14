a3f4fc813d479fb1f80aef180c17d6cffb4339ef
Joakim Plate <elupus@ecce.se>
2011-09-14 19:26:48 +0200

    [mxfdec] Check return value of avio_seek and avoid modifying state if it fails

--- libavformat/mxfdec.c
+++ libavformat/mxfdec.c
@@ -1119,7 +1119,8 @@ static int mxf_read_seek(AVFormatContext *s, int stream_index, int64_t sample_ti
     if (sample_time < 0)
         sample_time = 0;
     seconds = av_rescale(sample_time, st->time_base.num, st->time_base.den);
-    avio_seek(s->pb, (s->bit_rate * seconds) >> 3, SEEK_SET);
+    if (avio_seek(s->pb, (s->bit_rate * seconds) >> 3, SEEK_SET) < 0)
+        return -1;
     av_update_cur_dts(s, st, sample_time);
     return 0;
 }
