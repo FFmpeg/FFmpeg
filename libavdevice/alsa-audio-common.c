4bc447a3c5ef4e69b54cc20857335e8dd5bca60c
Carl Eugen Hoyos <cehoyos@ag.or.at>
2011-09-18 22:22:45 +0200

    Fix compilation with --disable-everything --enable-outdev=alsa.
    Bug found and analysed by skierpage.

--- libavdevice/alsa-audio-common.c
+++ libavdevice/alsa-audio-common.c
@@ -319,7 +319,8 @@ av_cold int ff_alsa_close(AVFormatContext *s1)
     AlsaData *s = s1->priv_data;
 
     av_freep(&s->reorder_buf);
-    ff_timefilter_destroy(s->timefilter);
+    if (CONFIG_ALSA_INDEV)
+        ff_timefilter_destroy(s->timefilter);
     snd_pcm_close(s->h);
     return 0;
 }
