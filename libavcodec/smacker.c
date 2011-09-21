ef7a4df4584db51a97edea3b1140661faf4ddf56
Justin Ruggles <justin.ruggles@gmail.com>
2011-09-21 11:37:51 -0400

    smacker: validate number of channels
    (cherry picked from commit e190e453bd1e4d4b409ed3556b3a50d1087c15d7)
    
    Signed-off-by: Anton Khirnov <anton@khirnov.net>

--- libavcodec/smacker.c
+++ libavcodec/smacker.c
@@ -559,6 +559,10 @@ static av_cold int decode_end(AVCodecContext *avctx)
 
 static av_cold int smka_decode_init(AVCodecContext *avctx)
 {
+    if (avctx->channels < 1 || avctx->channels > 2) {
+        av_log(avctx, AV_LOG_ERROR, "invalid number of channels\n");
+        return AVERROR(EINVAL);
+    }
     avctx->channel_layout = (avctx->channels==2) ? AV_CH_LAYOUT_STEREO : AV_CH_LAYOUT_MONO;
     avctx->sample_fmt = avctx->bits_per_coded_sample == 8 ? AV_SAMPLE_FMT_U8 : AV_SAMPLE_FMT_S16;
     return 0;
