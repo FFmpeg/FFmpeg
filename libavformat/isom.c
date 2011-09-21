e71322cb009ad7b042e93734aeffddd1d8fc4370
Carl Eugen Hoyos <cehoyos@ag.or.at>
2011-09-21 23:33:02 +0200

    Support Sorenson Spark in f4v.
    Such files are produced by the Flash Media Server,
    see samples/f4v/H263_NM_f.mp4

--- libavformat/isom.c
+++ libavformat/isom.c
@@ -216,6 +216,7 @@ const AVCodecTag codec_movvideo_tags[] = {
     { CODEC_ID_PRORES, MKTAG('a', 'p', '4', 'h') }, /* Apple ProRes 4444 */
 
     { CODEC_ID_MSMPEG4V3, MKTAG('3', 'I', 'V', 'D') }, /* 3ivx DivX Doctor */
+    { CODEC_ID_FLV1,      MKTAG('H', '2', '6', '3') }, /* Flash Media Server */
 
     { CODEC_ID_NONE, 0 },
 };
