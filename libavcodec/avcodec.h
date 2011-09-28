dc38625ebce7af8e218ce7f3c553d1b570a349ed
Hendrik Leppkes <h.leppkes@gmail.com>
2011-09-28 21:29:21 +0200

    Add SMPTE240M transfer characteristics flag. (cherry picked from commit 4266191919c31a1921b497ec831d3b1c1fa28df8)
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/avcodec.h
+++ libavcodec/avcodec.h
@@ -504,6 +504,7 @@ enum AVColorTransferCharacteristic{
     AVCOL_TRC_UNSPECIFIED=2,
     AVCOL_TRC_GAMMA22    =4, ///< also ITU-R BT470M / ITU-R BT1700 625 PAL & SECAM
     AVCOL_TRC_GAMMA28    =5, ///< also ITU-R BT470BG
+    AVCOL_TRC_SMPTE240M  =7,
     AVCOL_TRC_NB           , ///< Not part of ABI
 };
 
