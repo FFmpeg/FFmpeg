e89f58810d0d508552089495781e2a70e95edb99
Michael Niedermayer <michaelni@gmx.at>
2011-09-28 15:11:19 +0200

    libx264: remove check_default_settings()
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/libx264.c
+++ libavcodec/libx264.c
@@ -209,31 +209,6 @@ static av_cold int X264_close(AVCodecContext *avctx)
     return 0;
 }
 
-/**
- * Detect default settings and use default profile to avoid libx264 failure.
- */
-static void check_default_settings(AVCodecContext *avctx)
-{
-    X264Context *x4 = avctx->priv_data;
-
-    int score = 0;
-    score += x4->params.analyse.i_me_range == 0;
-    score += x4->params.rc.i_qp_step == 3;
-    score += x4->params.i_keyint_max == 12;
-    score += x4->params.rc.i_qp_min == 2;
-    score += x4->params.rc.i_qp_max == 31;
-    score += x4->params.rc.f_qcompress == 0.5;
-    score += fabs(x4->params.rc.f_ip_factor - 1.25) < 0.01;
-    score += fabs(x4->params.rc.f_pb_factor - 1.25) < 0.01;
-    score += x4->params.analyse.inter == 0 && x4->params.analyse.i_subpel_refine == 8;
-    if (score >= 5) {
-        av_log(avctx, AV_LOG_ERROR, "Default settings detected, using medium profile\n");
-        x4->preset = av_strdup("medium");
-        if (avctx->bit_rate == 200*1000)
-            avctx->crf = 23;
-    }
-}
-
 #define OPT_STR(opt, param)                                                   \
     do {                                                                      \
         int ret;                                                              \
