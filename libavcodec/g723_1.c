6e505547f148235b5e2733bf42a4615ae67d4cb4
Vitor Sessak <vitor1001@gmail.com>
2011-09-29 21:28:18 +0200

    g723.1: cleanup square_root()
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/g723_1.c
+++ libavcodec/g723_1.c
@@ -196,17 +196,7 @@ static int unpack_bitstream(G723_1_Context *p, const uint8_t *buf,
  */
 static int16_t square_root(int val)
 {
-    int16_t res = 0;
-    int16_t exp = 0x4000;
-    int i;
-
-    for (i = 0; i < 14; i ++) {
-        int res_exp = res + exp;
-        if (val >= res_exp * res_exp << 1)
-            res += exp;
-        exp >>= 1;
-    }
-    return res;
+    return (ff_sqrt(val << 1) >> 1) & (~1);
 }
 
 /**
