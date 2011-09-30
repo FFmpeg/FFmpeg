e3e962835fbcc0c5220382539c84afd700d6a369
Ramiro Polla <ramiro.polla@gmail.com>
2011-09-30 17:50:00 -0300

    dshow: save opened device reference so it may be properly closed
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavdevice/dshow.c
+++ libavdevice/dshow.c
@@ -561,11 +561,13 @@ static int
 dshow_list_device_options(AVFormatContext *avctx, ICreateDevEnum *devenum,
                           enum dshowDeviceType devtype)
 {
+    struct dshow_ctx *ctx = avctx->priv_data;
     IBaseFilter *device_filter = NULL;
     int r;
 
     if ((r = dshow_cycle_devices(avctx, devenum, devtype, &device_filter)) < 0)
         return r;
+    ctx->device_filter[devtype] = device_filter;
     if ((r = dshow_cycle_pins(avctx, devtype, device_filter, NULL)) < 0)
         return r;
 
