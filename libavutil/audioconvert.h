60aaf044f3fdbeb1cf702b144cb0597ce4988020
Marton Balint <cus@passwd.hu>
2011-10-02 16:28:36 +0200

    audioconvert: add av_get_default_channel_layout public function

--- libavutil/audioconvert.h
+++ libavutil/audioconvert.h
@@ -92,4 +92,9 @@ void av_get_channel_layout_string(char *buf, int buf_size, int nb_channels, int6
  */
 int av_get_channel_layout_nb_channels(int64_t channel_layout);
 
+/**
+ * Return default channel layout for a given number of channels.
+ */
+int av_get_default_channel_layout(int nb_channels);
+
 #endif /* AVUTIL_AUDIOCONVERT_H */
