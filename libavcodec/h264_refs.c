f7da257a897684415c23a472b068febade7c2aca
Michael Niedermayer <michaelni@gmx.at>
2011-10-05 03:15:39 +0200

    h264: remove assert() on ref_frame_count.
    This assert can fail if a SPS with a smaller value becomes active.
    Fixes Ticket532
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/h264_refs.c
+++ libavcodec/h264_refs.c
@@ -479,10 +479,9 @@ static void print_long_term(H264Context *h) {
 
 void ff_generate_sliding_window_mmcos(H264Context *h) {
     MpegEncContext * const s = &h->s;
-    av_assert0(h->long_ref_count + h->short_ref_count <= h->sps.ref_frame_count);
 
     h->mmco_index= 0;
-    if(h->short_ref_count && h->long_ref_count + h->short_ref_count == h->sps.ref_frame_count &&
+    if(h->short_ref_count && h->long_ref_count + h->short_ref_count >= h->sps.ref_frame_count &&
             !(FIELD_PICTURE && !s->first_field && s->current_picture_ptr->f.reference)) {
         h->mmco[0].opcode= MMCO_SHORT2UNUSED;
         h->mmco[0].short_pic_num= h->short_ref[ h->short_ref_count - 1 ]->frame_num;
