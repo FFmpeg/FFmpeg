db27bef747f88944d5c419c435a7124603f9115b
Michael Niedermayer <michaelni@gmx.at>
2011-10-08 19:58:27 +0200

    dct-test: document the number of bits we use for testing.
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/dct-test.c
+++ libavcodec/dct-test.c
@@ -511,10 +511,11 @@ static void idct248_error(const char *name,
 
 static void help(void)
 {
-    printf("dct-test [-i] [<test-number>]\n"
+    printf("dct-test [-i] [<test-number>] [<bits>]\n"
            "test-number 0 -> test with random matrixes\n"
            "            1 -> test with random sparse matrixes\n"
            "            2 -> do 3. test from mpeg4 std\n"
+           "bits        Number of time domain bits to use, 8 is default\n"
            "-i          test IDCT implementations\n"
            "-4          test IDCT248 implementations\n"
            "-t          speed test\n");
