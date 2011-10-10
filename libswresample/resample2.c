ade3ec8457f76a127e8f495ee73a2dee6cb80002
Clément Bœsch <clement.boesch@smartjog.com>
2011-10-10 15:54:09 +0200

    swresample: fix another broken indent chunk.
    Also use a consistent style inside that block.

--- libswresample/resample2.c
+++ libswresample/resample2.c
@@ -203,25 +203,24 @@ AVResampleContext *swr_resample_init(AVResampleContext *c, int out_rate, int in_
     double factor= FFMIN(out_rate * cutoff / in_rate, 1.0);
     int phase_count= 1<<phase_shift;
 
-    if(!c || c->phase_shift!=phase_shift || c->linear!=linear || c->factor != factor
-         || c->filter_length!=FFMAX((int)ceil(filter_size/factor), 1)){
-    c= av_mallocz(sizeof(AVResampleContext));
-    if (!c)
-        return NULL;
+    if (!c || c->phase_shift != phase_shift || c->linear!=linear || c->factor != factor
+           || c->filter_length != FFMAX((int)ceil(filter_size/factor), 1)) {
+        c = av_mallocz(sizeof(AVResampleContext));
+        if (!c)
+            return NULL;
 
-    c->phase_shift= phase_shift;
-    c->phase_mask= phase_count-1;
-    c->linear= linear;
-    c->factor= factor;
-
-    c->filter_length= FFMAX((int)ceil(filter_size/factor), 1);
-    c->filter_bank= av_mallocz(c->filter_length*(phase_count+1)*sizeof(FELEM));
-    if (!c->filter_bank)
-        goto error;
-    if (build_filter(c->filter_bank, factor, c->filter_length, phase_count, 1<<FILTER_SHIFT, WINDOW_TYPE))
-        goto error;
-    memcpy(&c->filter_bank[c->filter_length*phase_count+1], c->filter_bank, (c->filter_length-1)*sizeof(FELEM));
-    c->filter_bank[c->filter_length*phase_count]= c->filter_bank[c->filter_length - 1];
+        c->phase_shift   = phase_shift;
+        c->phase_mask    = phase_count - 1;
+        c->linear        = linear;
+        c->factor        = factor;
+        c->filter_length = FFMAX((int)ceil(filter_size/factor), 1);
+        c->filter_bank   = av_mallocz(c->filter_length*(phase_count+1)*sizeof(FELEM));
+        if (!c->filter_bank)
+            goto error;
+        if (build_filter(c->filter_bank, factor, c->filter_length, phase_count, 1<<FILTER_SHIFT, WINDOW_TYPE))
+            goto error;
+        memcpy(&c->filter_bank[c->filter_length*phase_count+1], c->filter_bank, (c->filter_length-1)*sizeof(FELEM));
+        c->filter_bank[c->filter_length*phase_count]= c->filter_bank[c->filter_length - 1];
     }
 
     c->compensation_distance= 0;
