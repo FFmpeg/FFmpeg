347363995251a3aec65defadd45f1e053a24cc76
Clément Bœsch <ubitux@gmail.com>
2011-10-11 14:24:20 +0200

    ffmpeg: fix two stream related comments.

--- ffmpeg.c
+++ ffmpeg.c
@@ -187,10 +187,10 @@ typedef struct InputStream {
 typedef struct InputFile {
     AVFormatContext *ctx;
     int eof_reached;      /* true if eof reached */
-    int ist_index;        /* index of first stream in ist_table */
+    int ist_index;        /* index of first stream in input_streams */
     int buffer_size;      /* current total buffer size */
     int64_t ts_offset;
-    int nb_streams;       /* number of stream that avconv is aware of; may be different
+    int nb_streams;       /* number of stream that ffmpeg is aware of; may be different
                              from ctx.nb_streams if new streams appear during av_read_frame() */
     int rate_emu;
 } InputFile;
