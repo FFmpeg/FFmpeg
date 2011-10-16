02b651a7e39844edd573a9d057ec87a0160bef8d
Michael Niedermayer <michaelni@gmx.at>
2011-10-16 16:54:27 +0200

    cache: Fallback to using the cache size for AVSEEK_SIZE if the underlaying protocol has nothing better.
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/cache.c
+++ libavformat/cache.c
@@ -95,7 +95,14 @@ static int64_t cache_seek(URLContext *h, int64_t pos, int whence)
     Context *c= h->priv_data;
 
     if (whence == AVSEEK_SIZE) {
-        return ffurl_seek(c->inner, pos, whence);
+        pos= ffurl_seek(c->inner, pos, whence);
+        if(pos <= 0){
+            pos= ffurl_seek(c->inner, -1, SEEK_END);
+            ffurl_seek(c->inner, c->end, SEEK_SET);
+            if(pos <= 0)
+                return c->end;
+        }
+        return pos;
     }
 
     pos= lseek(c->fd, pos, whence);
