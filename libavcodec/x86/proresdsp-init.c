2cb7c8166920aa8dbb969de9befd0981b5a8f2ad
Diego Biurrun <diego@biurrun.de>
2011-10-20 15:59:22 +0200

    x86: Fix linking of ProRes DSP ASM with YASM disabled.

--- libavcodec/x86/proresdsp-init.c
+++ libavcodec/x86/proresdsp-init.c
@@ -31,7 +31,7 @@ void ff_prores_idct_put_10_avx (uint16_t *dst, int linesize,
 
 void ff_proresdsp_x86_init(ProresDSPContext *dsp)
 {
-#if ARCH_X86_64
+#if ARCH_X86_64 && HAVE_YASM
     int flags = av_get_cpu_flags();
 
     if (flags & AV_CPU_FLAG_SSE2) {
@@ -49,6 +49,6 @@ void ff_proresdsp_x86_init(ProresDSPContext *dsp)
         dsp->idct_permutation_type = FF_TRANSPOSE_IDCT_PERM;
         dsp->idct_put = ff_prores_idct_put_10_avx;
     }
-#endif
-#endif
+#endif /* HAVE_AVX */
+#endif /* ARCH_X86_64 && HAVE_YASM */
 }
