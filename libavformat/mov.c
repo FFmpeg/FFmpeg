9e8dff90efa3faae5ac07fa5fae7e154367e24fd
Carl Eugen Hoyos <cehoyos@ag.or.at>
2011-10-23 00:54:56 +0200

    Revert "mov: do not misreport empty stts"
    Fixes ticket #578.
    
    This reverts commit 30c3d976f12665d5d13971172aab062a97cb1bce.

--- libavformat/mov.c
+++ libavformat/mov.c
@@ -1563,8 +1563,8 @@ static int mov_read_stts(MOVContext *c, AVIOContext *pb, MOVAtom atom)
     av_dlog(c->fc, "track[%i].stts.entries = %i\n",
             c->fc->nb_streams-1, entries);
 
-    if (!entries || entries >= UINT_MAX / sizeof(*sc->stts_data))
-        return AVERROR(EINVAL);
+    if (entries >= UINT_MAX / sizeof(*sc->stts_data))
+        return -1;
 
     sc->stts_data = av_malloc(entries * sizeof(*sc->stts_data));
     if (!sc->stts_data)
