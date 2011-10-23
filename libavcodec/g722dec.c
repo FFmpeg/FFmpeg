4e41973794c5fc5c3a045b00051e0c089774cf9b
Justin Ruggles <justin.ruggles@gmail.com>
2011-10-23 13:02:04 -0400

    g722dec: cosmetics: reindent/linewrap

--- libavcodec/g722dec.c
+++ libavcodec/g722dec.c
@@ -105,24 +105,22 @@ static int g722_decode_frame(AVCodecContext *avctx, void *data,
 
         ff_g722_update_low_predictor(&c->band[0], ilow >> (2 - skip));
 
-            dhigh = c->band[1].scale_factor *
-                    ff_g722_high_inv_quant[ihigh] >> 10;
-            rhigh = av_clip(dhigh + c->band[1].s_predictor, -16384, 16383);
+        dhigh = c->band[1].scale_factor * ff_g722_high_inv_quant[ihigh] >> 10;
+        rhigh = av_clip(dhigh + c->band[1].s_predictor, -16384, 16383);
 
-            ff_g722_update_high_predictor(&c->band[1], dhigh, ihigh);
+        ff_g722_update_high_predictor(&c->band[1], dhigh, ihigh);
 
-            c->prev_samples[c->prev_samples_pos++] = rlow + rhigh;
-            c->prev_samples[c->prev_samples_pos++] = rlow - rhigh;
-            ff_g722_apply_qmf(c->prev_samples + c->prev_samples_pos - 24,
-                              &xout1, &xout2);
-            out_buf[out_len++] = av_clip_int16(xout1 >> 12);
-            out_buf[out_len++] = av_clip_int16(xout2 >> 12);
-            if (c->prev_samples_pos >= PREV_SAMPLES_BUF_SIZE) {
-                memmove(c->prev_samples,
-                        c->prev_samples + c->prev_samples_pos - 22,
-                        22 * sizeof(c->prev_samples[0]));
-                c->prev_samples_pos = 22;
-            }
+        c->prev_samples[c->prev_samples_pos++] = rlow + rhigh;
+        c->prev_samples[c->prev_samples_pos++] = rlow - rhigh;
+        ff_g722_apply_qmf(c->prev_samples + c->prev_samples_pos - 24,
+                          &xout1, &xout2);
+        out_buf[out_len++] = av_clip_int16(xout1 >> 12);
+        out_buf[out_len++] = av_clip_int16(xout2 >> 12);
+        if (c->prev_samples_pos >= PREV_SAMPLES_BUF_SIZE) {
+            memmove(c->prev_samples, c->prev_samples + c->prev_samples_pos - 22,
+                    22 * sizeof(c->prev_samples[0]));
+            c->prev_samples_pos = 22;
+        }
     }
     *data_size = out_len << 1;
     return avpkt->size;
