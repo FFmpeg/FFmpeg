a39b5e8b323785695fb0e3c0f30bd9e24287db87
Michael Niedermayer <michaelni@gmx.at>
2011-10-27 14:34:45 +0200

    resample2: fix potential overflow
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/resample2.c
+++ libavcodec/resample2.c
@@ -248,10 +248,9 @@ int av_resample(AVResampleContext *c, short *dst, short *src, int *consumed, int
             dst[dst_index] = src[index2>>32];
             index2 += incr;
         }
-        frac += dst_index * dst_incr_frac;
         index += dst_index * dst_incr;
-        index += frac / c->src_incr;
-        frac %= c->src_incr;
+        index += (frac + dst_index * (int64_t)dst_incr_frac) / c->src_incr;
+        frac   = (frac + dst_index * (int64_t)dst_incr_frac) % c->src_incr;
   }else{
     for(dst_index=0; dst_index < dst_size; dst_index++){
         FELEM *filter= c->filter_bank + c->filter_length*(index & c->phase_mask);
