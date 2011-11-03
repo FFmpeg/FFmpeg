7c5dfc174be59afa671a98ec85fe208c528ab64f
Alex Converse <alex.converse@gmail.com>
2011-11-03 15:56:37 -0700

    mpeg12enc: Don't set up run-level info for level 0.
    run: The number of zero coefficients preceding a non-zero coefficient,
    in the scan order. The absolute value of the non-zero coefficient is
    called "level".
    
    The run-level code makes illegal reads when trying to set up tables for
    nonsense level 0.

--- libavcodec/mpeg12enc.c
+++ libavcodec/mpeg12enc.c
@@ -74,6 +74,8 @@ static void init_uni_ac_vlc(RLTable *rl, uint8_t *uni_ac_vlc_len){
     for(i=0; i<128; i++){
         int level= i-64;
         int run;
+        if (!level)
+            continue;
         for(run=0; run<64; run++){
             int len, bits, code;
 
