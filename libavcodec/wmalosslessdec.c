79b16e780d14a80becf666973784caaff2ca5aae
Mashiat Sarker Shakkhar <shahriman_ams@yahoo.com>
2011-11-05 02:32:56 +0600

    Add .recent syntax element to cdlms struct

--- libavcodec/wmalosslessdec.c
+++ libavcodec/wmalosslessdec.c
@@ -250,7 +250,10 @@ typedef struct WmallDecodeCtx {
 	int16_t coefs[256];
     int lms_prevvalues[512];    // FIXME: see above
     int16_t lms_updates[512];   // and here too
-    } cdlms[2][9];
+    int recent;
+    } cdlms[2][9];              /* XXX: Here, 2 is the max. no. of channels allowed,
+                                        9 is the maximum no. of filters per channel.
+                                        Question is, why 2 if WMALL_MAX_CHANNELS == 8 */
 
 
     int cdlms_ttl[2];
