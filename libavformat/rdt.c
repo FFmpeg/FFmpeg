ed307e2659f2db81a434afece905383fdceb9b6e
Martin Storsjö <martin@martin.st>
2011-11-06 23:51:19 +0200

    rdt: Check the return value of avformat_open
    If it failed, return NULL. This avoids trying to use an
    half-initialized RDTDemuxContext.
    
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- libavformat/rdt.c
+++ libavformat/rdt.c
@@ -524,7 +524,11 @@ rdt_new_context (void)
 {
     PayloadContext *rdt = av_mallocz(sizeof(PayloadContext));
 
-    avformat_open_input(&rdt->rmctx, "", &ff_rdt_demuxer, NULL);
+    int ret = avformat_open_input(&rdt->rmctx, "", &ff_rdt_demuxer, NULL);
+    if (ret < 0) {
+        av_free(rdt);
+        return NULL;
+    }
 
     return rdt;
 }
