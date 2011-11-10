9ef5d707e6c12ec5164e6612d547f845da9cbafc
Mashiat Sarker Shakkhar <shahriman_ams@yahoo.com>
2011-11-10 13:42:10 +0600

    Implement use_high_update_speed() and use_normal_update_speed() (cherry picked from commit 9ebc93fa4623791eca9efeee4de65b23dc015de1)
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/wmalosslessdec.c
+++ libavcodec/wmalosslessdec.c
@@ -829,6 +829,38 @@ static void lms_update(WmallDecodeCtx *s, int ich, int ilms, int32_t input, int3
     }
 }
 
+static void use_high_update_speed(WmallDecodeCtx *s, int ich)
+{
+    int ilms, recent, icoef;
+    s->update_speed[ich] = 16;
+    for (ilms = s->cdlms_ttl[ich]; ilms >= 0; ilms--) {
+        recent = s->cdlms[ich][ilms].recent;
+        if (s->bV3RTM) {
+            for (icoef = 0; icoef < s->cdlms[ich][ilms].order; icoef++)
+                s->cdlms[ich][ilms].lms_updates[icoef + recent] *= 2;
+        } else {
+            for (icoef = 0; icoef < s->cdlms[ich][ilms].order; icoef++)
+                s->cdlms[ich][ilms].lms_updates[icoef] *= 2;
+        }
+    }
+}
+
+static void use_normal_update_speed(WmallDecodeCtx *s, int ich)
+{
+    int ilms, recent, icoef;
+    s->update_speed[ich] = 8;
+    for (ilms = s->cdlms_ttl[ich]; ilms >= 0; ilms--) {
+        recent = s->cdlms[ich][ilms].recent;
+        if (s->bV3RTM) {
+            for (icoef = 0; icoef < s->cdlms[ich][ilms].order; icoef++)
+                s->cdlms[ich][ilms].lms_updates[icoef + recent] /= 2;
+        } else {
+            for (icoef = 0; icoef < s->cdlms[ich][ilms].order; icoef++)
+                s->cdlms[ich][ilms].lms_updates[icoef] /= 2;
+        }
+    }
+}
+
 /**
  *@brief Decode a single subframe (block).
  *@param s codec context
