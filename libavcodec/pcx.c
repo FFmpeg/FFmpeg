0b7aec5fa48288e0963d1bf531f7d0f44a482ada
Carl Eugen Hoyos <cehoyos@ag.or.at>
2011-11-11 18:38:16 +0100

    Make PAL8 pcx images opaque.

--- libavcodec/pcx.c
+++ libavcodec/pcx.c
@@ -71,7 +71,7 @@ static void pcx_palette(const uint8_t **src, uint32_t *dst, unsigned int pallen)
     unsigned int i;
 
     for (i=0; i<pallen; i++)
-        *dst++ = bytestream_get_be24(src);
+        *dst++ = 0xFF000000 | bytestream_get_be24(src);
     if (pallen < 256)
         memset(dst, 0, (256 - pallen) * sizeof(*dst));
 }
