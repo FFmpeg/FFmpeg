9780c33eca3e03f146970e25196b8363e9ef4790
Carl Eugen Hoyos <cehoyos@ag.or.at>
2011-11-12 20:49:53 +0100

    Increase Psygnosis YOP palette dynamics.

--- libavcodec/yop.c
+++ libavcodec/yop.c
@@ -221,7 +221,8 @@ static int yop_decode_frame(AVCodecContext *avctx, void *data, int *data_size,
         palette[i + firstcolor] = (s->srcptr[0] << 18) |
                                   (s->srcptr[1] << 10) |
                                   (s->srcptr[2] << 2);
-        palette[i + firstcolor] |= 0xFF << 24;
+        palette[i + firstcolor] |= 0xFF << 24 |
+                                   (palette[i + firstcolor] >> 6) & 0x30303;
     }
 
     s->frame.palette_has_changed = 1;
