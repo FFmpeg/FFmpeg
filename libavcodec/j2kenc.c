8a25d0a86099a2ee5e3ff6b358bc2c0672960af7
Dominique Leuenberger <dominique@leuenberger.net>
2011-11-12 20:35:33 +0100

    Trivial: j2kenc: fix compiler warning
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/j2kenc.c
+++ libavcodec/j2kenc.c
@@ -869,7 +869,8 @@ static int encode_tile(J2kEncoderContext *s, J2kTile *tile, int tileno)
                                 int *ptr = t1.data[y-yy0];
                                 for (x = xx0; x < xx1; x++){
                                     *ptr = (comp->data[(comp->coord[0][1] - comp->coord[0][0]) * y + x]);
-                                    *ptr++ = (int64_t)*ptr * (int64_t)(8192 * 8192 / band->stepsize) >> 13 - NMSEDEC_FRACBITS;
+                                    *ptr = (int64_t)*ptr * (int64_t)(8192 * 8192 / band->stepsize) >> 13 - NMSEDEC_FRACBITS;
+                                    *ptr++;
                                 }
                             }
                         }
