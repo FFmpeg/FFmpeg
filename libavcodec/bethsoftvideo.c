f98722443f3171327fcc10b523112cec1db1b1cb
Carl Eugen Hoyos <cehoyos@ag.or.at>
2011-11-12 19:34:07 +0100

    Set Bethesda VID palette opaque.

--- libavcodec/bethsoftvideo.c
+++ libavcodec/bethsoftvideo.c
@@ -52,7 +52,7 @@ static void set_palette(AVFrame * frame, const uint8_t * palette_buffer)
     uint32_t * palette = (uint32_t *)frame->data[1];
     int a;
     for(a = 0; a < 256; a++){
-        palette[a] = AV_RB24(&palette_buffer[a * 3]) * 4;
+        palette[a] = 0xFF << 24 | AV_RB24(&palette_buffer[a * 3]) * 4;
     }
     frame->palette_has_changed = 1;
 }
