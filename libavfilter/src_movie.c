2b1a4c5b3411c3030a5bdbd70d80bc606be570f7
Stefano Sabatini <stefasab@gmail.com>
2011-11-13 23:51:06 +0100

    amovie: avoid crash in amovie_request_frame() if no samplesref is available
    The crash occurrs when a frame is successfully decoded, but no decoded
    data is immediately available (typically happens with ogg/vorbis).

--- libavfilter/src_movie.c
+++ libavfilter/src_movie.c
@@ -446,8 +446,10 @@ static int amovie_request_frame(AVFilterLink *outlink)
 
     if (movie->is_done)
         return AVERROR_EOF;
-    if ((ret = amovie_get_samples(outlink)) < 0)
-        return ret;
+    do {
+        if ((ret = amovie_get_samples(outlink)) < 0)
+            return ret;
+    } while (!movie->samplesref);
 
     avfilter_filter_samples(outlink, avfilter_ref_buffer(movie->samplesref, ~0));
     avfilter_unref_buffer(movie->samplesref);
