7ea396381743c300f8450baa3054b62b1f3e68d0
Michael Niedermayer <michaelni@gmx.at>
2011-11-15 04:15:34 +0100

    mpegts: Completely ignore H222 spec and consider reserved adaption  fields to be perfectly fine mpeg TS. Fixes Ticket580
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/mpegts.c
+++ libavformat/mpegts.c
@@ -399,7 +399,7 @@ static int analyze(const uint8_t *buf, int size, int packet_size, int *index){
     memset(stat, 0, packet_size*sizeof(int));
 
     for(x=i=0; i<size-3; i++){
-        if(buf[i] == 0x47 && !(buf[i+1] & 0x80) && (buf[i+3] & 0x30)){
+        if(buf[i] == 0x47 && !(buf[i+1] & 0x80) && buf[i+3] != 0x47){
             stat[x]++;
             if(stat[x] > best_score){
                 best_score= stat[x];
