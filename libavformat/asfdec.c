b88eb87630c3384517437a1ee640bca9fe92852f
John Stebbins <stebbins@jetheaddev.com>
2011-11-15 03:56:37 +0100

    asf: use packet dts as approximation of pts
    Having a somehow off seeking is better than having none at all.
    
    Signed-off-by: Luca Barbato <lu_zero@gentoo.org>

--- libavformat/asfdec.c
+++ libavformat/asfdec.c
@@ -1173,7 +1173,7 @@ static int64_t asf_read_pts(AVFormatContext *s, int stream_index, int64_t *ppos,
             return AV_NOPTS_VALUE;
         }
 
-        pts= pkt->pts;
+        pts = pkt->dts;
 
         av_free_packet(pkt);
         if(pkt->flags&AV_PKT_FLAG_KEY){
