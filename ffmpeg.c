b329fc346e046a2fb8bcc0acd66bf5726e8e284d
Michael Niedermayer <michaelni@gmx.at>
2011-11-23 23:20:20 +0100

    ffmpeg: fix indention
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- ffmpeg.c
+++ ffmpeg.c
@@ -1871,14 +1871,14 @@ static int transcode_video(InputStream *ist, AVPacket *pkt, int *got_output, int
 
 #if CONFIG_AVFILTER
     for(i=0;i<nb_output_streams;i++) {
-            OutputStream *ost = ost = &output_streams[i];
-            if(check_output_constraints(ist, ost)){
-                if (!decoded_frame->sample_aspect_ratio.num)
-                    decoded_frame->sample_aspect_ratio = ist->st->sample_aspect_ratio;
-                decoded_frame->pts = ist->pts;
+        OutputStream *ost = ost = &output_streams[i];
+        if(check_output_constraints(ist, ost)){
+            if (!decoded_frame->sample_aspect_ratio.num)
+                decoded_frame->sample_aspect_ratio = ist->st->sample_aspect_ratio;
+            decoded_frame->pts = ist->pts;
 
-                av_vsrc_buffer_add_frame(ost->input_video_filter, decoded_frame, AV_VSRC_BUF_FLAG_OVERWRITE);
-            }
+            av_vsrc_buffer_add_frame(ost->input_video_filter, decoded_frame, AV_VSRC_BUF_FLAG_OVERWRITE);
+        }
     }
 #endif
 
