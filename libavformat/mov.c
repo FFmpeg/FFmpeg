028a2375e25d66d1f927f8a0b531eaaf6642cf5c
Alex Converse <alex.converse@gmail.com>
2011-11-26 19:41:54 -0800

    mov: Make format string match variable type.

--- libavformat/mov.c
+++ libavformat/mov.c
@@ -89,7 +89,7 @@ static int mov_metadata_int8_bypass_padding(MOVContext *c, AVIOContext *pb,
     avio_r8(pb);
     avio_r8(pb);
 
-    snprintf(buf, sizeof(buf), "%hu", avio_r8(pb));
+    snprintf(buf, sizeof(buf), "%d", avio_r8(pb));
     av_dict_set(&c->fc->metadata, key, buf, 0);
 
     return 0;
@@ -100,7 +100,7 @@ static int mov_metadata_int8_no_padding(MOVContext *c, AVIOContext *pb,
 {
     char buf[16];
 
-    snprintf(buf, sizeof(buf), "%hu", avio_r8(pb));
+    snprintf(buf, sizeof(buf), "%d", avio_r8(pb));
     av_dict_set(&c->fc->metadata, key, buf, 0);
 
     return 0;
