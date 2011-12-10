874340096120a219cdc456baa2de56bfa243d331
Michael Niedermayer <michaelni@gmx.at>
2011-12-10 23:03:49 +0100

    h264: reset prev_poc_msb to the same value everywhere.
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/h264.c
+++ libavcodec/h264.c
@@ -2373,7 +2373,7 @@ static void idr(H264Context *h){
     ff_h264_remove_all_refs(h);
     h->prev_frame_num= -1;
     h->prev_frame_num_offset= 0;
-    h->prev_poc_msb=
+    h->prev_poc_msb= 1<<16;
     h->prev_poc_lsb= 0;
     for (i = 0; i < MAX_DELAYED_PIC_COUNT; i++)
         h->last_pocs[i] = INT_MIN;
