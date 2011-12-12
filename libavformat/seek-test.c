7881dad2b287117a789693942c2b002cc3ecc75b
Michael Niedermayer <michaelni@gmx.at>
2011-12-12 02:03:38 +0100

    seek-test: use standard format specifiers in snprintf()
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>
    (cherry picked from commit cd2f7ed0007f4803b6bd845366b2398abb32c355)
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/seek-test.c
+++ libavformat/seek-test.c
@@ -53,7 +53,7 @@ static void ts_str(char buffer[60], int64_t ts, AVRational base)
         return;
     }
     ts= av_rescale_q(ts, base, (AVRational){1, 1000000});
-    snprintf(buffer, 60, "%c%Ld.%06Ld", ts<0 ? '-' : ' ', FFABS(ts)/1000000, FFABS(ts)%1000000);
+    snprintf(buffer, 60, "%c%"PRId64".%06"PRId64"", ts<0 ? '-' : ' ', FFABS(ts)/1000000, FFABS(ts)%1000000);
 }
 
 int main(int argc, char **argv)
