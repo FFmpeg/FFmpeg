16b771b1f47daa9fec84477040984d83a92ba4a6
Michael Niedermayer <michaelni@gmx.at>
2011-12-13 21:01:45 +0100

    cleanup: for(int
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- ffplay.c
+++ ffplay.c
@@ -2773,10 +2773,11 @@ static void stream_cycle_channel(VideoState *is, int codec_type)
 
 static void toggle_full_screen(VideoState *is)
 {
+    int i;
     is_full_screen = !is_full_screen;
 #if defined(__APPLE__) && SDL_VERSION_ATLEAST(1, 2, 14)
     /* OS X needs to reallocate the SDL overlays */
-    for (int i = 0; i < VIDEO_PICTURE_QUEUE_SIZE; i++) {
+    for (i = 0; i < VIDEO_PICTURE_QUEUE_SIZE; i++) {
         is->pictq[i].reallocate = 1;
     }
 #endif
