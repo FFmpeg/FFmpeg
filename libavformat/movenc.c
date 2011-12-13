b92ef485c712fe8af2c32ddd28e4470abcb7e5ff
Michael Niedermayer <michaelni@gmx.at>
2011-12-13 20:01:49 +0100

    Fix all GNU %Ld
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>
    (cherry picked from commit 9aeaf629038c572073e836277cd1fa981465dd6c)
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/movenc.c
+++ libavformat/movenc.c
@@ -2562,7 +2562,7 @@ static int update_first_fragment(AVFormatContext *s)
     if(mov->reserved_moov_size){
         int64_t size=  mov->reserved_moov_size - (avio_tell(pb) - mov->reserved_moov_pos);
         if(size < 8){
-            av_log(s, AV_LOG_ERROR, "reserved_moov_size is too small, needed %Ld additional\n", 8-size);
+            av_log(s, AV_LOG_ERROR, "reserved_moov_size is too small, needed %"PRId64" additional\n", 8-size);
             return -1;
         }
         avio_wb32(pb, size);
