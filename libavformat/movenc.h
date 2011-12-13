a6c4c0f2740bc8e65aa68b890fedae42ca7a83cf
Michael Niedermayer <michaelni@gmx.at>
2011-12-13 03:27:19 +0100

    mp4enc: Avoid storing sizes in trun when they all match.
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/movenc.h
+++ libavformat/movenc.h
@@ -94,6 +94,7 @@ typedef struct MOVIndex {
     uint32_t    tref_tag;
     int         tref_id; ///< trackID of the referenced track
     uint32_t    trex_flags;
+    int         trex_size;
 
     int         hint_track;   ///< the track that hints this track, -1 if no hint track is set
     int         src_track;    ///< the track that this hint track describes
