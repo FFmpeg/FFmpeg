207d9eab5a47c54173a3a3cbe990aa746fe6e39a
Michael Niedermayer <michaelni@gmx.at>
2011-12-16 15:44:29 +0100

    txd/westwood: remove demuxer specific overallocate solutions as the new generic code handles it fine.
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/westwood.c
+++ libavformat/westwood.c
@@ -320,14 +320,8 @@ static int wsvqa_read_packet(AVFormatContext *s,
     int skip_byte;
 
     while (avio_read(pb, preamble, VQA_PREAMBLE_SIZE) == VQA_PREAMBLE_SIZE) {
-        int64_t filesize= avio_size(s->pb);
         chunk_type = AV_RB32(&preamble[0]);
         chunk_size = AV_RB32(&preamble[4]);
-
-        if(filesize>=0 && chunk_size > filesize){
-            av_log(s, AV_LOG_ERROR, "Chunk with size %d truncated\n", chunk_size);
-            chunk_size= filesize;
-        }
 
         skip_byte = chunk_size & 0x01;
 
