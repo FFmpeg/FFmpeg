f0e90e00354f5705e132406a06d3360e2c00565d
Michael Niedermayer <michaelni@gmx.at>
2011-12-17 06:25:12 +0100

    lavc: make 100 the base for the micro version number. This way ffmpeg can be distinguished from the fork by a user application or a encoded file by a decoder. The highest value micro had, in the past, that i could find, was 6 thus 100 should be safe.
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/utils.c
+++ libavcodec/utils.c
@@ -1415,6 +1415,7 @@ unsigned avcodec_version( void )
     av_assert0(CODEC_ID_ADPCM_G722==69660);
     av_assert0(CODEC_ID_BMV_AUDIO==86071);
     av_assert0(CODEC_ID_SRT==94216);
+    av_assert0(LIBAVCODEC_VERSION_MICRO >= 100);
 
   return LIBAVCODEC_VERSION_INT;
 }
