b0aaf304a778f0a49d42c032aa83efa62c3291c4
Nicolas George <nicolas.george@normalesup.org>
2011-12-20 19:08:04 +0100

    Replace PATCHWELCOME by relevant error codes.
    This supersedes commit 154c0a8.
    The bug was also reported by Rafaël Carré.

--- libavformat/sbgdec.c
+++ libavformat/sbgdec.c
@@ -1467,7 +1467,7 @@ static int sbg_read_seek2(AVFormatContext *avf, int stream_index,
                           int64_t min_ts, int64_t ts, int64_t max_ts, int flags)
 {
     if (flags || stream_index > 0)
-        return AVERROR_PATCHWELCOME;
+        return AVERROR(EINVAL);
     if (stream_index < 0)
         ts = av_rescale_q(ts, AV_TIME_BASE_Q, avf->streams[0]->time_base);
     avf->streams[0]->cur_dts = ts;
