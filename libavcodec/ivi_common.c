f27930cd9a2d4970b182024a42a9f5103c942f21
Michael Niedermayer <michaelni@gmx.at>
2011-12-20 22:06:35 +0100

    indeo5: Fix null pointer dereference Fixes Ticket803 Bug found by: Oana Stratulat
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/ivi_common.c
+++ libavcodec/ivi_common.c
@@ -611,6 +611,9 @@ void ff_ivi_output_plane(IVIPlaneDesc *plane, uint8_t *dst, int dst_pitch)
     const int16_t   *src  = plane->bands[0].buf;
     uint32_t        pitch = plane->bands[0].pitch;
 
+    if(!src)
+        return;
+
     for (y = 0; y < plane->height; y++) {
         for (x = 0; x < plane->width; x++)
             dst[x] = av_clip_uint8(src[x] + 128);
