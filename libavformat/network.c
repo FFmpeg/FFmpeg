139cef8e29bd14de66e29e37722fb09ab2b7e9c5
Sergey Radionov <RSATom@gmail.com>
2011-12-21 09:19:10 +0700

    network: properly declare WSADATA in windows
    Fixed "ISO C90 forbids mixed declarations and code" in ff_network_init
    
    Signed-off-by: Luca Barbato <lu_zero@gentoo.org>

--- libavformat/network.c
+++ libavformat/network.c
@@ -123,13 +123,16 @@ int ff_network_inited_globally;
 
 int ff_network_init(void)
 {
+#if HAVE_WINSOCK2_H
+    WSADATA wsaData;
+#endif
+
     if (!ff_network_inited_globally)
         av_log(NULL, AV_LOG_WARNING, "Using network protocols without global "
                                      "network initialization. Please use "
                                      "avformat_network_init(), this will "
                                      "become mandatory later.\n");
 #if HAVE_WINSOCK2_H
-    WSADATA wsaData;
     if (WSAStartup(MAKEWORD(1,1), &wsaData))
         return 0;
 #endif
