196c9e5c8c1706e81217e372ea6a3a101486af9e
Anton Khirnov <anton@khirnov.net>
2011-12-21 20:47:52 +0100

    lavfi: always build vsrc_buffer.
    It's a part of public API.

--- libavfilter/allfilters.c
+++ libavfilter/allfilters.c
@@ -80,7 +80,6 @@ void avfilter_register_all(void)
     REGISTER_FILTER (VFLIP,       vflip,       vf);
     REGISTER_FILTER (YADIF,       yadif,       vf);
 
-    REGISTER_FILTER (BUFFER,      buffer,      vsrc);
     REGISTER_FILTER (COLOR,       color,       vsrc);
     REGISTER_FILTER (FREI0R,      frei0r_src,  vsrc);
     REGISTER_FILTER (MOVIE,       movie,       vsrc);
@@ -89,4 +88,10 @@ void avfilter_register_all(void)
     REGISTER_FILTER (TESTSRC,     testsrc,     vsrc);
 
     REGISTER_FILTER (NULLSINK,    nullsink,    vsink);
+
+    /* vsrc_buffer is a part of public API => registered unconditionally */
+    {
+        extern avfilter_vsrc_buffer;
+        avfilter_register(&avfilter_vsrc_buffer);
+    }
 }
