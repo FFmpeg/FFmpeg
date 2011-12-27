549b53e8cbc45d20a52a90a181d40d86c751585d
Stefano Sabatini <stefasab@gmail.com>
2011-12-27 15:12:29 +0100

    lavfi: remove redundant definition of avfilter_copy_frame_props() in avfilter.h
    Rationale: avfilter_copy_frame_props() was already defined in
    libavfilter/avcodec.h, and keeping the lavc/lavfi API glue localized in a
    specific file should ease maintainance and help the ones which use
    libavfilter without depending on libavcodec.

--- libavfilter/avfilter.h
+++ libavfilter/avfilter.h
@@ -31,7 +31,7 @@
 
 #define LIBAVFILTER_VERSION_MAJOR  2
 #define LIBAVFILTER_VERSION_MINOR 55
-#define LIBAVFILTER_VERSION_MICRO 100
+#define LIBAVFILTER_VERSION_MICRO 101
 
 #define LIBAVFILTER_VERSION_INT AV_VERSION_INT(LIBAVFILTER_VERSION_MAJOR, \
                                                LIBAVFILTER_VERSION_MINOR, \
@@ -953,13 +953,5 @@ static inline void avfilter_insert_outpad(AVFilterContext *f, unsigned index,
     avfilter_insert_pad(index, &f->output_count, offsetof(AVFilterLink, srcpad),
                         &f->output_pads, &f->outputs, p);
 }
-
-/**
- * Copy the frame properties of src to dst, without copying the actual
- * image data.
- *
- * @return 0 on success, a negative number on error.
- */
-int avfilter_copy_frame_props(AVFilterBufferRef *dst, const AVFrame *src);
 
 #endif /* AVFILTER_AVFILTER_H */
