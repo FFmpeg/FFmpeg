9d9316ae34ea2a5e481ac15f31d42c87a8ef4380
Michael Niedermayer <michaelni@gmx.at>
2011-12-31 20:25:02 +0100

    h264: Fix seeking in 00017.MTS Fixes Ticket852
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/h264_refs.c
+++ libavcodec/h264_refs.c
@@ -653,7 +653,7 @@ int ff_h264_execute_ref_pic_marking(H264Context *h, MMCO *mmco, int mmco_count){
     print_short_term(h);
     print_long_term(h);
 
-    if(err >= 0 && h->long_ref_count==0 && h->short_ref_count<=2 && h->pps.ref_count[0]<=1 && s->current_picture_ptr->f.pict_type == AV_PICTURE_TYPE_I){
+    if(err >= 0 && h->long_ref_count==0 && h->short_ref_count<=2 && h->pps.ref_count[0]<=1 + (s->picture_structure != PICT_FRAME) && s->current_picture_ptr->f.pict_type == AV_PICTURE_TYPE_I){
         h->sync |= 1;
         s->current_picture_ptr->sync |= h->sync;
     }
