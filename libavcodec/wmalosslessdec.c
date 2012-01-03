6429269096902d872db977abaede1a78fa107d7f
Michael Niedermayer <michaelni@gmx.at>
2012-01-03 23:52:28 +0100

    wmall: remove ;;
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/wmalosslessdec.c
+++ libavcodec/wmalosslessdec.c
@@ -1425,7 +1425,7 @@ static int decode_packet(AVCodecContext *avctx,
         s->samples_16_end = (int16_t *) ((int8_t*)data + *data_size);
     } else {
         s->samples_32     = (int *) data;
-        s->samples_32_end = (int *) ((int8_t*)data + *data_size);;
+        s->samples_32_end = (int *) ((int8_t*)data + *data_size);
     }
     *data_size = 0;
 
