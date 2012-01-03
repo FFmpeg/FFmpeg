001567c767132aec24471529a057ac7d74de70a1
Carl Eugen Hoyos <cehoyos@ag.or.at>
2012-01-03 23:05:54 +0100

    Set bits_per_coded_sample when encoding Y41P.
    This allows mplayer -vc bt411 to decode files
    encoded with FFmpeg.
    
    Reviewed-by: Paul B Mahol

--- libavcodec/y41penc.c
+++ libavcodec/y41penc.c
@@ -30,6 +30,7 @@ static av_cold int y41p_encode_init(AVCodecContext *avctx)
     }
 
     avctx->coded_frame = avcodec_alloc_frame();
+    avctx->bits_per_coded_sample = 12;
 
     if (!avctx->coded_frame) {
         av_log(avctx, AV_LOG_ERROR, "Could not allocate frame.\n");
