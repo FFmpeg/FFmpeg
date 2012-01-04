68f121269694d61c137f046ea8f51ce717e57d7c
Michael Niedermayer <michaelni@gmx.at>
2012-01-05 00:51:12 +0100

    Revert "rmdec: Avoid allocating huge packets"
    This reverts commit 66f71f3b5e2e7b6e5049bd9831c371e16aff8a53.
    
    This causes regressions with RDT.
    (cherry picked from commit c4eec85a1fa768025f88261995af08f1dba9685d)
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/rmdec.c
+++ libavformat/rmdec.c
@@ -24,7 +24,6 @@
 #include "libavutil/dict.h"
 #include "avformat.h"
 #include "internal.h"
-#include "avio_internal.h"
 #include "riff.h"
 #include "rm.h"
 
@@ -613,7 +612,6 @@ static int rm_assemble_video_frame(AVFormatContext *s, AVIOContext *pb,
     }
     if(type != 1){  // not whole frame
         len2 = get_num(pb, &len);
-        len2 = ffio_limit(pb, len2);
         pos  = get_num(pb, &len);
         pic_num = avio_r8(pb); len--;
     }
