febf75f3d2e4335652e6e40ef3427ec019d45aa8
ami_stuff <ami_stuff@o2.pl>
2012-01-06 01:25:54 +0100

    pam: Reject 64bpp rgba.
    Fixes ticket #883

--- libavcodec/pnm.c
+++ libavcodec/pnm.c
@@ -125,7 +125,12 @@ int ff_pnm_decode_header(AVCodecContext *avctx, PNMContext * const s)
                 avctx->pix_fmt = PIX_FMT_RGB48BE;
             }
         } else if (depth == 4) {
-            avctx->pix_fmt = PIX_FMT_RGB32;
+            if (maxval < 256) {
+                avctx->pix_fmt = PIX_FMT_RGB32;
+            } else {
+                av_log(avctx, AV_LOG_ERROR, "Unsupported bit depth\n");
+                return -1;
+            }
         } else {
             return -1;
         }
