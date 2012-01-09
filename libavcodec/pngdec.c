e20a4881ffdb31915a82e9c301b7db8531167852
Paul B Mahol <onemda@gmail.com>
2012-01-09 22:35:19 +0100

    pngdec: show verbose message for unsupported files

--- libavcodec/pngdec.c
+++ libavcodec/pngdec.c
@@ -502,6 +502,9 @@ static int decode_frame(AVCodecContext *avctx,
                            s->color_type == PNG_COLOR_TYPE_GRAY_ALPHA) {
                     avctx->pix_fmt = PIX_FMT_GRAY8A;
                 } else {
+                    av_log(avctx, AV_LOG_ERROR, "unsupported bit depth %d "
+                                                "and color type %d\n",
+                                                 s->bit_depth, s->color_type);
                     goto fail;
                 }
                 if(p->data[0])
