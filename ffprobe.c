0cc88646877aea8fc2adb2244f6019707df2082a
Stefano Sabatini <stefasab@gmail.com>
2012-01-10 22:11:02 +0100

    ffprobe: use av_toupper() in upcase_string()

--- ffprobe.c
+++ ffprobe.c
@@ -423,11 +423,7 @@ static inline char *upcase_string(char *dst, size_t dst_size, const char *src)
 {
     int i;
     for (i = 0; src[i] && i < dst_size-1; i++)
-        if (src[i] >= 'a' && src[i] <= 'z') {
-            dst[i] = src[i]-32;
-        } else {
-            dst[i] = src[i];
-        }
+        dst[i] = av_toupper(src[i]);
     dst[i] = 0;
     return dst;
 }
