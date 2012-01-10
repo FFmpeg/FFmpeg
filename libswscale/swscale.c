5ab6f0fe5abf8c5fccede33f7b68c9f3a3864e3a
Michael Niedermayer <michaelni@gmx.at>
2012-01-10 17:24:14 +0100

    Revert "swscale: fix integer overflows in RGB pixel writing."
    This reverts commit 77d88b872d8cbb42738ede2d4fc098c16f204236.
    
    The revert fixes actual overflows and a segfault as the variables
    are signed and can be negative.
    
    Conflicts:
    
    	libswscale/swscale.c
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libswscale/swscale.c
+++ libswscale/swscale.c
@@ -990,7 +990,7 @@ YUV2PACKED16WRAPPER(yuv2, rgb48, bgr48le, PIX_FMT_BGR48LE)
  * correct RGB values into the destination buffer.
  */
 static av_always_inline void
-yuv2rgb_write(uint8_t *_dest, int i, unsigned Y1, unsigned Y2,
+yuv2rgb_write(uint8_t *_dest, int i, int Y1, int Y2,
               unsigned A1, unsigned A2,
               const void *_r, const void *_g, const void *_b, int y,
               enum PixelFormat target, int hasAlpha)
