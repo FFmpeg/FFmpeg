5387f9917f1e5a053824f9ba68545b74a2fc225b
Michael Niedermayer <michaelni@gmx.at>
2012-01-11 18:09:56 +0100

    cabac: Try to disable problematic ASM for gcc-llvm 4.2.1
    This should fix compilation with gcc-llvm (see darwin fate box)
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/x86/cabac.h
+++ libavcodec/x86/cabac.h
@@ -82,7 +82,8 @@
         "1:                                                             \n\t"
 
 
-#if HAVE_7REGS && !defined(BROKEN_RELOCATIONS) && !(defined(__i386) && defined(__clang__) && (__clang_major__<2 || (__clang_major__==2 && __clang_minor__<10)))
+#if HAVE_7REGS && !defined(BROKEN_RELOCATIONS) && !(defined(__i386) && defined(__clang__) && (__clang_major__<2 || (__clang_major__==2 && __clang_minor__<10)))\
+                                               && !(defined(__i386) && !defined(__clang__) && defined(__llvm__) && __GNUC__==4 && __GNUC_MINOR__==2 && __GNUC_PATCHLEVEL__<=1)
 #define get_cabac_inline get_cabac_inline_x86
 static av_always_inline int get_cabac_inline_x86(CABACContext *c,
                                                  uint8_t *const state)
