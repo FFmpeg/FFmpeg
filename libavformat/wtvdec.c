742b660eb77db42ecb97fe7f467899efe53ccebf
Reimar Döffinger <Reimar.Doeffinger@gmx.de>
2012-01-12 00:50:48 +0100

    wtvdec: fix a few memleaks as show by valgrind with the FATE test.
    Signed-off-by: Reimar Döffinger <Reimar.Doeffinger@gmx.de>

--- libavformat/wtvdec.c
+++ libavformat/wtvdec.c
@@ -287,6 +287,8 @@ static void wtvfile_close(AVIOContext *pb)
 {
     WtvFile *wf = pb->opaque;
     av_free(wf->sectors);
+    av_freep(&pb->opaque);
+    av_freep(&pb->buffer);
     av_free(pb);
 }
 
