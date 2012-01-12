7efc6f293289923bc1a6d0905f492a468562f638
Stefano Sabatini <stefasab@gmail.com>
2012-01-12 23:56:58 +0100

    ffmpeg: clarify error message in case of bitstream filter opening failure

--- ffmpeg.c
+++ ffmpeg.c
@@ -988,7 +988,7 @@ static void write_frame(AVFormatContext *s, AVPacket *pkt, OutputStream *ost)
             av_free_packet(pkt);
             new_pkt.destruct = av_destruct_packet;
         } else if (a < 0) {
-            av_log(NULL, AV_LOG_ERROR, "%s failed for stream %d, codec %s",
+            av_log(NULL, AV_LOG_ERROR, "Failed to open bitstream filter %s for stream %d with codec %s",
                    bsfc->filter->name, pkt->stream_index,
                    avctx->codec ? avctx->codec->name : "copy");
             print_error("", a);
