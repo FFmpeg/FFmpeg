60168f9319ab959758ce75698e1855408a76fd28
Jean First <jeanfirst@gmail.com>
2012-01-14 14:10:19 +0100

    movenc: add FIXME to add alis and rsrc atoms
    Signed-off-by: Jean First <jeanfirst@gmail.com>
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/movenc.c
+++ libavformat/movenc.c
@@ -1036,6 +1036,7 @@ static int mov_write_dref_tag(AVIOContext *pb)
     avio_wb32(pb, 1); /* entry count */
 
     avio_wb32(pb, 0xc); /* size */
+    //FIXME add the alis and rsrc atom
     ffio_wfourcc(pb, "url ");
     avio_wb32(pb, 1); /* version & flags */
 
