eef4b70450b5ed57a5c57a719d31b8ca14532bc9
Stefano Sabatini <stefasab@gmail.com>
2012-01-18 19:28:31 +0100

    ffprobe: reindent after previous commit

--- ffprobe.c
+++ ffprobe.c
@@ -107,19 +107,19 @@ static char *value_string(char *buf, int buf_size, struct unit_value uv)
         int l;
 
         if (use_value_prefix) {
-        long long int index;
+            long long int index;
 
-        if (uv.unit == unit_byte_str && use_byte_value_binary_prefix) {
-            index = (long long int) (log(vald)/log(2)) / 10;
-            index = av_clip(index, 0, FF_ARRAY_ELEMS(binary_unit_prefixes) - 1);
-            vald /= pow(2, index * 10);
-            prefix_string = binary_unit_prefixes[index];
-        } else {
-            index = (long long int) (log10(vald)) / 3;
-            index = av_clip(index, 0, FF_ARRAY_ELEMS(decimal_unit_prefixes) - 1);
-            vald /= pow(10, index * 3);
-            prefix_string = decimal_unit_prefixes[index];
-        }
+            if (uv.unit == unit_byte_str && use_byte_value_binary_prefix) {
+                index = (long long int) (log(vald)/log(2)) / 10;
+                index = av_clip(index, 0, FF_ARRAY_ELEMS(binary_unit_prefixes) - 1);
+                vald /= pow(2, index * 10);
+                prefix_string = binary_unit_prefixes[index];
+            } else {
+                index = (long long int) (log10(vald)) / 3;
+                index = av_clip(index, 0, FF_ARRAY_ELEMS(decimal_unit_prefixes) - 1);
+                vald /= pow(10, index * 3);
+                prefix_string = decimal_unit_prefixes[index];
+            }
         }
 
         if (show_float || (use_value_prefix && vald != (long long int)vald))
