ef9623f670f57014fab0651cc2132baac7c8e981
ami_stuff <ami_stuff@o2.pl>
2012-01-19 14:16:41 +0100

    Add "S263" FourCC
    fixes ticket #923
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/riff.c
+++ libavformat/riff.c
@@ -240,6 +240,7 @@ const AVCodecTag ff_codec_bmp_tags[] = {
     { CODEC_ID_SNOW,         MKTAG('S', 'N', 'O', 'W') },
     { CODEC_ID_4XM,          MKTAG('4', 'X', 'M', 'V') },
     { CODEC_ID_FLV1,         MKTAG('F', 'L', 'V', '1') },
+    { CODEC_ID_FLV1,         MKTAG('S', '2', '6', '3') },
     { CODEC_ID_FLASHSV,      MKTAG('F', 'S', 'V', '1') },
     { CODEC_ID_SVQ1,         MKTAG('s', 'v', 'q', '1') },
     { CODEC_ID_TSCC,         MKTAG('t', 's', 'c', 'c') },
