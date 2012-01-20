c3d5e290ca868f72c53740fdc66133a474c12c7e
Felipe Contreras <felipe.contreras@gmail.com>
2012-01-20 16:01:07 +0000

    ARM: fix build with FFT enabled and MDCT disabled
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>
    Signed-off-by: Mans Rullgard <mans@mansr.com>

--- libavcodec/arm/fft_init_arm.c
+++ libavcodec/arm/fft_init_arm.c
@@ -42,10 +42,12 @@ av_cold void ff_fft_init_arm(FFTContext *s)
     if (HAVE_NEON) {
         s->fft_permute  = ff_fft_permute_neon;
         s->fft_calc     = ff_fft_calc_neon;
+#if CONFIG_MDCT
         s->imdct_calc   = ff_imdct_calc_neon;
         s->imdct_half   = ff_imdct_half_neon;
         s->mdct_calc    = ff_mdct_calc_neon;
         s->mdct_permutation = FF_MDCT_PERM_INTERLEAVE;
+#endif
     }
 }
 
