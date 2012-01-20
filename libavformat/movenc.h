5d95112d926760046bc0b07aeda5cfc766ba54a6
Martin Storsjö <martin@martin.st>
2012-01-20 20:48:19 +0200

    movenc: Reorder entries in the MOVIentry struct, for tigheter packing
    Originally, sizeof(struct MOVIentry) was 48, after the reordering,
    it is 40 in my build configuration.
    
    When writing really long mov/mp4 files, this can make a difference
    - this saves a bit over 2 MB of memory per hour of video (down to
    10.3 MB per hour from 12.3 MB per hour initially) for a video with
    75 packets per second - 25 fps + 50 audio packets (which is the
    case for AMR audio).
    
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- libavformat/movenc.h
+++ libavformat/movenc.h
@@ -40,12 +40,12 @@
 #define MODE_IPOD 0x20
 
 typedef struct MOVIentry {
-    unsigned int size;
     uint64_t     pos;
+    int64_t      dts;
+    unsigned int size;
     unsigned int samplesInChunk;
     unsigned int entries;
     int          cts;
-    int64_t      dts;
 #define MOV_SYNC_SAMPLE         0x0001
 #define MOV_PARTIAL_SYNC_SAMPLE 0x0002
     uint32_t     flags;
