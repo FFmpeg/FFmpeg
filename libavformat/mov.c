6813450209bab97c30e8b25a018cdc4c936b224a
Paul B Mahol <onemda@gmail.com>
2012-01-23 18:49:25 +0000

    lavf: replace remaining use of deprecated get_strz()
    Signed-off-by: Paul B Mahol <onemda@gmail.com>
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/mov.c
+++ libavformat/mov.c
@@ -2619,7 +2619,7 @@ static void mov_read_chapters(AVFormatContext *s)
                 if (len == 1 || len == 2)
                     title[len] = 0;
                 else
-                    get_strz(sc->pb, title + 2, len - 1);
+                    avio_get_str(sc->pb, INT_MAX, title + 2, len - 1);
             }
         }
 
