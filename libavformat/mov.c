FFmpeg/FFmpeg

commit 550f7c43ece1af27604407647d10e74b52e6fedf

Fixes wrong duration in .mov file. Issue 599
Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

libavformat/mov.c
@@ -807,6 +807,7 @@ static int mov_read_mvhd(MOVContext *c, AVIOContext *pb, MOVAtom atom)
     avio_rb32(pb); /* current time */
     avio_rb32(pb); /* next track ID */
 
+    c->fc->duration = av_rescale(c->duration, AV_TIME_BASE, c->time_scale);
     return 0;
 }
 

commit 8847561f93c8e44095324a2c16ba78adf1a7511a

mov: Fix seeking regression in fragemnted movs.
Regression introduced in 550f7c43ece1af27604407647d10e74b52e6fedf

Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

libavformat/mov.c
@@ -2254,6 +2254,9 @@ static int mov_read_trex(MOVContext *c, AVIOContext *pb, MOVAtom atom)
     trex = av_realloc(c->trex_data, (c->trex_count+1)*sizeof(*c->trex_data));
     if (!trex)
         return AVERROR(ENOMEM);
+
+    c->fc->duration = AV_NOPTS_VALUE; // the duration from mvhd is not representing the whole file when fragments are used.
+
     c->trex_data = trex;
     trex = &c->trex_data[c->trex_count++];
     avio_r8(pb); /* version */