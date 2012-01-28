762311ff94e78d6f6cdd344882123210c1270757
Paul B Mahol <onemda@gmail.com>
2012-01-28 03:12:29 +0000

    swscale: fix conversion into yuva444p from non-alpha formats
    Signed-off-by: Paul B Mahol <onemda@gmail.com>
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libswscale/swscale.c
+++ libswscale/swscale.c
@@ -2804,7 +2804,7 @@ static int swScale(SwsContext *c, const uint8_t* src[],
         }
     }
 
-    if ((dstFormat == PIX_FMT_YUVA420P) && !alpPixBuf)
+    if (isPlanarYUV(dstFormat) && isALPHA(dstFormat) && !alpPixBuf)
         fillPlane(dst[3], dstStride[3], dstW, dstY-lastDstY, lastDstY, 255);
 
 #if HAVE_MMX2
