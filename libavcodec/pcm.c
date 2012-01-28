1fa24d08b2279e0a770c056178ea2bc0754cdeae
Paul B Mahol <onemda@gmail.com>
2012-01-28 20:06:36 +0000

    pcm: remove redundant assignment
    Signed-off-by: Paul B Mahol <onemda@gmail.com>
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/pcm.c
+++ libavcodec/pcm.c
@@ -49,7 +49,6 @@ static av_cold int pcm_encode_init(AVCodecContext *avctx)
     avctx->bits_per_coded_sample = av_get_bits_per_sample(avctx->codec->id);
     avctx->block_align = avctx->channels * avctx->bits_per_coded_sample/8;
     avctx->coded_frame= avcodec_alloc_frame();
-    avctx->coded_frame->key_frame= 1;
     if (!avctx->coded_frame)
         return AVERROR(ENOMEM);
 
