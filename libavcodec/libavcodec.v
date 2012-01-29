afa4069e3ba373ed958f5fd392c184b084287b46
Anton Khirnov <anton@khirnov.net>
2012-01-29 12:22:49 +0100

    lavc/lavf: remove unnecessary symbols from the symbol version script.

--- libavcodec/libavcodec.v
+++ libavcodec/libavcodec.v
@@ -2,7 +2,5 @@ LIBAVCODEC_$MAJOR {
         global: av*;
                 audio_resample;
                 audio_resample_close;
-                #deprecated, remove after next bump
-                img_get_alpha_info;
         local:  *;
 };
--- libavformat/libavformat.v
+++ libavformat/libavformat.v
@@ -1,29 +1,4 @@
 LIBAVFORMAT_$MAJOR {
         global: av*;
-                #FIXME those are for avserver
-                ff_inet_aton;
-                ff_socket_nonblock;
-                ffm_set_write_index;
-                ffm_read_write_index;
-                ffm_write_write_index;
-                ff_rtsp_parse_line;
-                ff_rtp_get_local_rtp_port;
-                ff_rtp_get_local_rtcp_port;
-                ffio_open_dyn_packet_buf;
-                url_open;
-                url_close;
-                url_write;
-                url_get_max_packet_size;
-                #those are deprecated, remove on next bump
-                find_info_tag;
-                parse_date;
-                dump_format;
-                url_*;
-                get_*;
-                put_*;
-                udp_set_remote_url;
-                udp_get_local_port;
-                init_checksum;
-                init_put_byte;
         local: *;
 };
