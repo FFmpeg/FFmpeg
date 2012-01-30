97da38c99bf5ad619ea15f2f1b4c050f0f44d11f
Carl Eugen Hoyos <cehoyos@ag.or.at>
2012-01-31 00:13:24 +0100

    Allow decoding of 64bit png images.
    Fixes a part of ticket #639.

--- libavcodec/pngdec.c
+++ libavcodec/pngdec.c
@@ -485,6 +485,9 @@ static int decode_frame(AVCodecContext *avctx,
                 } else if (s->bit_depth == 16 &&
                            s->color_type == PNG_COLOR_TYPE_RGB) {
                     avctx->pix_fmt = PIX_FMT_RGB48BE;
+                } else if (s->bit_depth == 16 &&
+                           s->color_type == PNG_COLOR_TYPE_RGB_ALPHA) {
+                    avctx->pix_fmt = PIX_FMT_RGBA64BE;
                 } else if (s->color_type == PNG_COLOR_TYPE_PALETTE) {
                     avctx->pix_fmt = PIX_FMT_PAL8;
                 } else if (s->bit_depth == 1) {
