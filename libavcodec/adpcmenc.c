ddf70db6d71fdab15bf8626aea4301b85891c2bd
Justin Ruggles <justin.ruggles@gmail.com>
2012-01-30 12:57:34 -0500

    adpcmenc: Do not set coded_frame->key_frame.
    It is already set in avcodec_alloc_frame().

--- libavcodec/adpcmenc.c
+++ libavcodec/adpcmenc.c
@@ -136,7 +136,6 @@ static av_cold int adpcm_encode_init(AVCodecContext *avctx)
     }
 
     avctx->coded_frame = avcodec_alloc_frame();
-    avctx->coded_frame->key_frame= 1;
 
     return 0;
 error:
