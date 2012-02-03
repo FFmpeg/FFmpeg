97aa092997cab8f8f337548a7c85b7fde09b4cdf
Joakim Plate <elupus@ecce.se>
2012-02-03 19:13:40 +0000

    mpc8: Check return value of avio_seek and avoid modifying state if it fails
    Signed-off-by: Paul B Mahol <onemda@gmail.com>
    Signed-off-by: Justin Ruggles <justin.ruggles@gmail.com>

--- libavformat/mpc8.c
+++ libavformat/mpc8.c
@@ -275,7 +275,8 @@ static int mpc8_read_seek(AVFormatContext *s, int stream_index, int64_t timestam
     int index = av_index_search_timestamp(st, timestamp, flags);
 
     if(index < 0) return -1;
-    avio_seek(s->pb, st->index_entries[index].pos, SEEK_SET);
+    if (avio_seek(s->pb, st->index_entries[index].pos, SEEK_SET) < 0)
+        return -1;
     c->frame = st->index_entries[index].timestamp;
     return 0;
 }
