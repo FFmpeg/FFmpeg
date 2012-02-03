3371e89640eb519edf0ca8695f370867e55040d0
Joakim Plate <elupus@ecce.se>
2012-02-03 19:13:34 +0000

    bink: Check return value of avio_seek and avoid modifying state if it fails
    Signed-off-by: Paul B Mahol <onemda@gmail.com>
    Signed-off-by: Justin Ruggles <justin.ruggles@gmail.com>

--- libavformat/bink.c
+++ libavformat/bink.c
@@ -257,7 +257,9 @@ static int read_seek(AVFormatContext *s, int stream_index, int64_t timestamp, in
         return -1;
 
     /* seek to the first frame */
-    avio_seek(s->pb, vst->index_entries[0].pos, SEEK_SET);
+    if (avio_seek(s->pb, vst->index_entries[0].pos, SEEK_SET) < 0)
+        return -1;
+
     bink->video_pts = 0;
     memset(bink->audio_pts, 0, sizeof(bink->audio_pts));
     bink->current_track = -1;
