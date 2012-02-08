7665da46ce454b9ce619241375639947d58e9c43
Michael Niedermayer <michaelni@gmx.at>
2012-02-08 16:15:47 +0100

    ffmpeg: update ist->pts in transcode_audio with the audio decoders output.
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- ffmpeg.c
+++ ffmpeg.c
@@ -1970,7 +1970,7 @@ static int transcode_audio(InputStream *ist, AVPacket *pkt, int *got_output)
     /* if the decoder provides a pts, use it instead of the last packet pts.
        the decoder could be delaying output by a packet or more. */
     if (decoded_frame->pts != AV_NOPTS_VALUE)
-        ist->next_pts = decoded_frame->pts;
+        ist->pts = ist->next_pts = decoded_frame->pts;
 
     /* increment next_pts to use for the case where the input stream does not
        have timestamps or there are multiple frames in the packet */
