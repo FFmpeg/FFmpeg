7f215e3e725ddd45e6fc1e012aadcc5fd83ee1b5
Benjamin Larsson <benjamin@southpole.se>
2012-02-16 20:09:01 +0600

    Use fixed-length array in revert_mclms()

--- libavcodec/wmalosslessdec.c
+++ libavcodec/wmalosslessdec.c
@@ -885,7 +885,7 @@ static void mclms_predict(WmallDecodeCtx *s, int icoef, int *pred)
 
 static void revert_mclms(WmallDecodeCtx *s, int tile_size)
 {
-    int icoef, pred[s->num_channels];
+    int icoef, pred[WMALL_MAX_CHANNELS] = {0};
     for (icoef = 0; icoef < tile_size; icoef++) {
         mclms_predict(s, icoef, pred);
         mclms_update(s, icoef, pred);
