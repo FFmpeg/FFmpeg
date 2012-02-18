a520a9946f25fe36000558cf8bb512109ea40353
Mashiat Sarker Shakkhar <shahriman_ams@yahoo.com>
2012-02-18 21:00:03 +0600

    Perform inter-channel decorr. only if both channels are coded

--- libavcodec/wmalosslessdec.c
+++ libavcodec/wmalosslessdec.c
@@ -1025,7 +1025,7 @@ static void revert_inter_ch_decorr(WmallDecodeCtx *s, int tile_size)
     int icoef;
     if (s->num_channels != 2)
         return;
-    else {
+    else if (s->is_channel_coded[0] && s->is_channel_coded[1]) {
         for (icoef = 0; icoef < tile_size; icoef++) {
             s->channel_residues[0][icoef] -= s->channel_residues[1][icoef] >> 1;
             s->channel_residues[1][icoef] += s->channel_residues[0][icoef];
