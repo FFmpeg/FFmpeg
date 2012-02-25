4edb34f6cc43a64ea6582ebaf95a7e9e56cbb9af
Martin Storsjö <martin@martin.st>
2012-02-26 01:35:13 +0200

    segment: Pass the interrupt callback on to the chained AVFormatContext, too
    This might not be needed at the moment, but it's good practice to
    pass it to all chained AVFormatContexts, if it would happen to be
    used there at a later point.
    
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- libavformat/segment.c
+++ libavformat/segment.c
@@ -144,6 +144,7 @@ static int seg_write_header(AVFormatContext *s)
         goto fail;
     }
 
+    oc->interrupt_callback = s->interrupt_callback;
     seg->avf = oc;
 
     oc->streams = s->streams;
