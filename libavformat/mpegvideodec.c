174678ff5bb74e481c25377c6ae509755bb8e8a2
Michael Niedermayer <michaelni@gmx.at>
2012-03-09 22:25:32 +0100

    mpegvideo_probe: Fix misdetection of mpeg4video files.
    (issue1210)
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/mpegvideodec.c
+++ libavformat/mpegvideodec.c
@@ -34,7 +34,7 @@
 static int mpegvideo_probe(AVProbeData *p)
 {
     uint32_t code= -1;
-    int pic=0, seq=0, slice=0, pspack=0, vpes=0, apes=0;
+    int pic=0, seq=0, slice=0, pspack=0, vpes=0, apes=0, res=0;
     int i;
 
     for(i=0; i<p->buf_size; i++){
@@ -44,13 +44,15 @@ static int mpegvideo_probe(AVProbeData *p)
             case     SEQ_START_CODE:   seq++; break;
             case PICTURE_START_CODE:   pic++; break;
             case    PACK_START_CODE: pspack++; break;
+            case              0x1b6:
+                                        res++; break;
             }
             if (code >= SLICE_START_CODE && code <= 0x1af) slice++;
             if     ((code & 0x1f0) == VIDEO_ID)   vpes++;
             else if((code & 0x1e0) == AUDIO_ID)   apes++;
         }
     }
-    if(seq && seq*9<=pic*10 && pic*9<=slice*10 && !pspack && !apes)
+    if(seq && seq*9<=pic*10 && pic*9<=slice*10 && !pspack && !apes && !res)
         if(vpes) return AVPROBE_SCORE_MAX/8;
         else     return pic>1 ? AVPROBE_SCORE_MAX/2+1 : AVPROBE_SCORE_MAX/4; // +1 for .mpg
     return 0;
