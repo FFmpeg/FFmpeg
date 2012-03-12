2367b2a5025309bd97a7897ff479915b468362d4
Michael Niedermayer <michaelni@gmx.at>
2012-03-12 06:04:37 +0100

    libavformat.v: add ffio_set_buf_size() for ffserver.
    its inevitable with the current API
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/libavformat.v
+++ libavformat/libavformat.v
@@ -10,6 +10,7 @@ LIBAVFORMAT_$MAJOR {
                 ff_rtp_get_local_rtp_port;
                 ff_rtp_get_local_rtcp_port;
                 ffio_open_dyn_packet_buf;
+                ffio_set_buf_size;
                 ffurl_close;
                 ffurl_open;
                 ffurl_read_complete;
