a41340f8e85a13ec0b5a8a6bb93d09342adac047
Paul B Mahol <onemda@gmail.com>
2012-03-18 18:04:44 +0000

    pgmyuvenc: abort encoding if width is not even
    Signed-off-by: Paul B Mahol <onemda@gmail.com>
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/pnmenc.c
+++ libavcodec/pnmenc.c
@@ -71,6 +71,10 @@ static int pnm_encode_frame(AVCodecContext *avctx, AVPacket *pkt,
         n  = avctx->width * 6;
         break;
     case PIX_FMT_YUV420P:
+        if (avctx->width & 1) {
+            av_log(avctx, AV_LOG_ERROR, "pgmyuv needs even width\n");
+            return AVERROR(EINVAL);
+        }
         c  = '5';
         n  = avctx->width;
         h1 = (h * 3) / 2;
