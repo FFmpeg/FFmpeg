30f2d97afe14ea7c067b22c4728ebe26eef6cc65
Derek Buitenhuis <derek.buitenhuis@gmail.com>
2012-03-19 18:06:48 -0400

    zerocodec: Fix license
    ISC doesn't contain this line, so remove it to
    prevent confusion.
    
    Signed-off-by: Derek Buitenhuis <derek.buitenhuis@gmail.com>
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/zerocodec.c
+++ libavcodec/zerocodec.c
@@ -3,8 +3,6 @@
  *
  * Copyright (c) 2012, Derek Buitenhuis
  *
- * This file is part of FFMpeg.
- *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
  * copyright notice and this permission notice appear in all copies.
