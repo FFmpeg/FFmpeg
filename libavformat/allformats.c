62c9ae11a7f9716848de0c26df855d6b8071eb16
Martin Storsjö <martin@martin.st>
2012-03-19 23:12:54 +0200

    Add a smooth streaming segmenter muxer
    This muxer splits the output from the ismv muxer into individual
    files, in realtime.
    
    The same can also be done by the standalone tool ismindex, but this
    muxer is needed for doing it in realtime (especially for live
    streams that need extra handling for updating the lookahead fields
    in the fragment headers).
    
    Using this muxer, one can deliver live smooth streaming from a
    normal static file web server. (Using ismindex, one can deliver
    premade smooth streaming files from a static file web server,
    or prepare files for serving with IIS.)
    
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- libavformat/allformats.c
+++ libavformat/allformats.c
@@ -207,6 +207,7 @@ void av_register_all(void)
     REGISTER_DEMUXER  (SIFF, siff);
     REGISTER_DEMUXER  (SMACKER, smacker);
     REGISTER_MUXDEMUX (SMJPEG, smjpeg);
+    REGISTER_MUXER    (SMOOTHSTREAMING, smoothstreaming);
     REGISTER_DEMUXER  (SOL, sol);
     REGISTER_MUXDEMUX (SOX, sox);
     REGISTER_MUXDEMUX (SPDIF, spdif);
--- libavformat/os_support.h
+++ libavformat/os_support.h
@@ -38,6 +38,13 @@
 #  define fstat(f,s) _fstati64((f), (s))
 #endif /* defined(__MINGW32__) && !defined(__MINGW32CE__) */
 
+#ifdef _WIN32
+#include <direct.h>
+#define mkdir(a, b) _mkdir(a)
+#else
+#include <sys/stat.h>
+#endif
+
 static inline int is_dos_path(const char *path)
 {
 #if HAVE_DOS_PATHS
