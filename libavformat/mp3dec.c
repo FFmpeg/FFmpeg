2b07f572af65aa411a24e87348671fad7cd10fe9
Michael Niedermayer <michaelni@gmx.at>
2012-03-19 20:41:34 +0100

    mp3_probe: consider id3 tags to be low scoring mp3.
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/mp3dec.c
+++ libavformat/mp3dec.c
@@ -66,6 +66,8 @@ static int mp3_read_probe(AVProbeData *p)
     if   (first_frames>=4) return AVPROBE_SCORE_MAX/2+1;
     else if(max_frames>200)return AVPROBE_SCORE_MAX/2;
     else if(max_frames>=4) return AVPROBE_SCORE_MAX/4;
+    else if(ff_id3v2_match(buf0, ID3v2_DEFAULT_MAGIC) && 2*ff_id3v2_tag_len(buf0) >= p->buf_size)
+                           return AVPROBE_SCORE_MAX/8;
     else if(max_frames>=1) return 1;
     else                   return 0;
 //mpegps_mp3_unrecognized_format.mpg has max_frames=3
