9407cf137f70b07473de974078464ec8733c3e1f
Michael Niedermayer <michaelni@gmx.at>
2012-03-22 15:00:48 +0100

    libvorbis: switch to ff_alloc_packet2().
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/libvorbis.c
+++ libavcodec/libvorbis.c
@@ -353,8 +353,7 @@ static int oggvorbis_encode_frame(AVCodecContext *avctx, AVPacket *avpkt,
 
     av_fifo_generic_read(s->pkt_fifo, &op, sizeof(ogg_packet), NULL);
 
-    if ((ret = ff_alloc_packet(avpkt, op.bytes))) {
-        av_log(avctx, AV_LOG_ERROR, "Error getting output packet\n");
+    if ((ret = ff_alloc_packet2(avctx, avpkt, op.bytes))) {
         return ret;
     }
     av_fifo_generic_read(s->pkt_fifo, avpkt->data, op.bytes, NULL);
