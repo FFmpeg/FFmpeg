f8a67cb81fccc2f321c205a312de8e61da802756
Michael Niedermayer <michaelni@gmx.at>
2012-03-22 04:25:06 +0100

    jpeglsenc: switch to ff_alloc_packet2()
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/jpeglsenc.c
+++ libavcodec/jpeglsenc.c
@@ -250,9 +250,8 @@ static int encode_picture_ls(AVCodecContext *avctx, AVPacket *pkt,
     else
         comps = 3;
 
-    if ((ret = ff_alloc_packet(pkt, avctx->width*avctx->height*comps*4 +
+    if ((ret = ff_alloc_packet2(avctx, pkt, avctx->width*avctx->height*comps*4 +
                                     FF_MIN_BUFFER_SIZE)) < 0) {
-        av_log(avctx, AV_LOG_ERROR, "Error getting output packet.\n");
         return ret;
     }
 
