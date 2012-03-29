fe5ed69dc7f6acf6928c27d614f2be3fc89ed176
Christophe Gisquet <christophe.gisquet@gmail.com>
2012-03-29 22:37:41 +0200

    rv34dsp x86: implement MMX2 inverse transform
    141 cycles down to 51.
    
    Signed-off-by: Ronald S. Bultje <rsbultje@gmail.com>

--- libavcodec/x86/rv34dsp_init.c
+++ libavcodec/x86/rv34dsp_init.c
@@ -28,6 +28,7 @@ void ff_rv34_idct_dc_mmx2(DCTELEM *block);
 void ff_rv34_idct_dc_noround_mmx2(DCTELEM *block);
 void ff_rv34_idct_dc_add_mmx(uint8_t *dst, ptrdiff_t stride, int dc);
 void ff_rv34_idct_dc_add_sse4(uint8_t *dst, ptrdiff_t stride, int dc);
+void ff_rv34_idct_add_mmx2(uint8_t *dst, ptrdiff_t stride, DCTELEM *block);
 
 av_cold void ff_rv34dsp_init_x86(RV34DSPContext* c, DSPContext *dsp)
 {
@@ -38,6 +39,7 @@ av_cold void ff_rv34dsp_init_x86(RV34DSPContext* c, DSPContext *dsp)
         c->rv34_idct_dc_add = ff_rv34_idct_dc_add_mmx;
     if (mm_flags & AV_CPU_FLAG_MMX2) {
         c->rv34_inv_transform_dc = ff_rv34_idct_dc_noround_mmx2;
+        c->rv34_idct_add         = ff_rv34_idct_add_mmx2;
     }
     if (mm_flags & AV_CPU_FLAG_SSE4)
         c->rv34_idct_dc_add = ff_rv34_idct_dc_add_sse4;
