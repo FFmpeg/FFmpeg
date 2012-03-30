FFmpeg/FFmpeg

commit 85469f1c9eb1e678f9d7135dd6bcc1df68072e29

libspeexdec: do not set AVCodecContext.frame_size
It is not necessary

libavcodec/libspeexdec.c
@@ -54,9 +54,6 @@ static av_cold int libspeex_decode_init(AVCodecContext *avctx)
     if (s->header) {
         avctx->sample_rate = s->header->rate;
         avctx->channels    = s->header->nb_channels;
-        avctx->frame_size  = s->frame_size = s->header->frame_size;
-        if (s->header->frames_per_packet)
-            avctx->frame_size *= s->header->frames_per_packet;
 
         mode = speex_lib_get_mode(s->header->mode);
         if (!mode) {

commit 67aec401d86c3b7c55ce6f97ea753dcb1ca04c95

libspeexdec: set frame size in libspeex_decode_init()
This fixes speex decoding, which was broken in 85469f1c.

libavcodec/libspeexdec.c
@@ -54,6 +54,7 @@ static av_cold int libspeex_decode_init(AVCodecContext *avctx)
     if (s->header) {
         avctx->sample_rate = s->header->rate;
         avctx->channels    = s->header->nb_channels;
+        s->frame_size      = s->header->frame_size;
 
         mode = speex_lib_get_mode(s->header->mode);
         if (!mode) {