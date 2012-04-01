ef0ee7f657e66d91162d2b4fad882ece9fbb264e
Diego Biurrun <diego@biurrun.de>
2012-04-01 10:34:10 +0200

    swscale: K&R formatting cosmetics (part II)
    Also adjust some comments including wording and typo fixes.

--- libswscale/swscale_internal.h
+++ libswscale/swscale_internal.h
@@ -614,8 +614,8 @@ const char *sws_format_name(enum PixelFormat format);
     (isRGBinInt(x)              ||     \
      isBGRinInt(x))
 
-#define isALPHA(x)                                            \
-    (av_pix_fmt_descriptors[x].nb_components == 2          || \
+#define isALPHA(x)                                             \
+    (av_pix_fmt_descriptors[x].nb_components == 2          ||  \
      av_pix_fmt_descriptors[x].nb_components == 4)
 
 #define isPacked(x)                                            \
@@ -623,7 +623,7 @@ const char *sws_format_name(enum PixelFormat format);
       !(av_pix_fmt_descriptors[x].flags & PIX_FMT_PLANAR)) ||  \
      (x) == PIX_FMT_PAL8)
 
-#define isPlanar(x) \
+#define isPlanar(x)                                            \
     (av_pix_fmt_descriptors[x].nb_components >= 2          &&  \
      (av_pix_fmt_descriptors[x].flags & PIX_FMT_PLANAR))
 
@@ -635,7 +635,7 @@ const char *sws_format_name(enum PixelFormat format);
     ((av_pix_fmt_descriptors[x].flags                        & \
      (PIX_FMT_PLANAR | PIX_FMT_RGB)) == (PIX_FMT_PLANAR | PIX_FMT_RGB))
 
-#define usePal(x) ((av_pix_fmt_descriptors[x].flags & PIX_FMT_PAL) ||       \
+#define usePal(x) ((av_pix_fmt_descriptors[x].flags & PIX_FMT_PAL)       || \
                    (av_pix_fmt_descriptors[x].flags & PIX_FMT_PSEUDOPAL) || \
                    (x) == PIX_FMT_Y400A)
 
