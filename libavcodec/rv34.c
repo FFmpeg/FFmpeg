2d0b4bc4cf92dd961dbafbff3f1f4a9e08a9565b
Michael Niedermayer <michaelni@gmx.at>
2012-04-07 16:49:17 +0200

    rv34: Fix check_slice_end() handling of 0 bits left case
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/rv34.c
+++ libavcodec/rv34.c
@@ -1337,7 +1337,7 @@ static int check_slice_end(RV34DecContext *r, MpegEncContext *s)
     if(r->s.mb_skip_run > 1)
         return 0;
     bits = get_bits_left(&s->gb);
-    if(bits < 0 || (bits < 8 && !show_bits(&s->gb, bits)))
+    if(bits <= 0 || (bits < 8 && !show_bits(&s->gb, bits)))
         return 1;
     return 0;
 }
