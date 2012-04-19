2a70d8304dd2f8e97b9b0d2321ffbf6da69093f1
Michael Niedermayer <michaelni@gmx.at>
2012-04-19 21:43:15 +0200

    jvdec: Make sure there is enough data for the id string.
    Previously too little data could lead to a false detection.
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/jvdec.c
+++ libavformat/jvdec.c
@@ -52,8 +52,8 @@ typedef struct {
 
 static int read_probe(AVProbeData *pd)
 {
-    if (pd->buf[0] == 'J' && pd->buf[1] == 'V' &&
-        !memcmp(pd->buf + 4, MAGIC, FFMIN(strlen(MAGIC), pd->buf_size - 4)))
+    if (pd->buf[0] == 'J' && pd->buf[1] == 'V' && strlen(MAGIC) <= pd->buf_size - 4 &&
+        !memcmp(pd->buf + 4, MAGIC, strlen(MAGIC)))
         return AVPROBE_SCORE_MAX;
     return 0;
 }
