e52b96187b5451d44ec9bbbb4b6151aebac8b559
Roland Scheidegger <rscheidegger_lists@hispeed.ch>
2012-04-27 22:12:18 +0200

    h264: (trivial) make ff_h264_lps_state static
    not used outside the cabac test functions (which probably means it's
    a bad test if it doesn't use the same tables as the real functions?)
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/cabac.c
+++ libavcodec/cabac.c
@@ -52,8 +52,8 @@ static const uint8_t lps_range[64][4]= {
 
 uint8_t ff_h264_mlps_state[4*64];
 uint8_t ff_h264_lps_range[4*2*64];
-uint8_t ff_h264_lps_state[2*64];
-static uint8_t h264_mps_state[2 * 64];
+static uint8_t h264_lps_state[2*64];
+static uint8_t h264_mps_state[2*64];
 
 static const uint8_t mps_state[64]= {
   1, 2, 3, 4, 5, 6, 7, 8,
@@ -147,14 +147,14 @@ void ff_init_cabac_states(CABACContext *c){
         h264_mps_state[2 * i + 1] = 2 * mps_state[i] + 1;
 
         if( i ){
-            ff_h264_lps_state[2*i+0]=
+            h264_lps_state[2*i+0]=
             ff_h264_mlps_state[128-2*i-1]= 2*lps_state[i]+0;
-            ff_h264_lps_state[2*i+1]=
+            h264_lps_state[2*i+1]=
             ff_h264_mlps_state[128-2*i-2]= 2*lps_state[i]+1;
         }else{
-            ff_h264_lps_state[2*i+0]=
+            h264_lps_state[2*i+0]=
             ff_h264_mlps_state[128-2*i-1]= 1;
-            ff_h264_lps_state[2*i+1]=
+            h264_lps_state[2*i+1]=
             ff_h264_mlps_state[128-2*i-2]= 0;
         }
     }
@@ -201,7 +201,7 @@ static void put_cabac(CABACContext *c, uint8_t * const state, int bit){
     }else{
         c->low += c->range - RangeLPS;
         c->range = RangeLPS;
-        *state= ff_h264_lps_state[*state];
+        *state= h264_lps_state[*state];
     }
 
     renorm_cabac_encoder(c);
