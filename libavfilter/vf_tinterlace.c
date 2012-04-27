18c6bd098baba1ace8fea946e4bc0c60098f49d4
Stefano Sabatini <stefasab@gmail.com>
2012-04-27 22:21:22 +0200

    lavfi/tinterlace: remove unnecessary NULL check in start_frame()

--- libavfilter/vf_tinterlace.c
+++ libavfilter/vf_tinterlace.c
@@ -165,8 +165,7 @@ static void start_frame(AVFilterLink *inlink, AVFilterBufferRef *picref)
     AVFilterContext *ctx = inlink->dst;
     TInterlaceContext *tinterlace = ctx->priv;
 
-    if (tinterlace->cur)
-        avfilter_unref_buffer(tinterlace->cur);
+    avfilter_unref_buffer(tinterlace->cur);
     tinterlace->cur  = tinterlace->next;
     tinterlace->next = picref;
 }
