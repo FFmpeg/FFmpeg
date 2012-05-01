8501bed08ce44e30a8bf926fe6700225d65d7b24
Jakub Stachowski <qbast@go2.pl>
2012-05-02 00:33:17 +0200

    Don't restrict reverse decorrelation to both coded channels. It is also used for mono data.
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/wmalosslessdec.c
+++ libavcodec/wmalosslessdec.c
@@ -793,7 +793,7 @@ static void revert_inter_ch_decorr(WmallDecodeCtx *s, int tile_size)
 {
     if (s->num_channels != 2)
         return;
-    else if (s->is_channel_coded[0] && s->is_channel_coded[1]) {
+    else {
         int icoef;
         for (icoef = 0; icoef < tile_size; icoef++) {
             s->channel_residues[0][icoef] -= s->channel_residues[1][icoef] >> 1;
