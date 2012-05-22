c7448c182a701b4c6efc52e0224bcbecc1aa6c3b
Justin Ruggles <justin.ruggles@gmail.com>
2012-05-21 21:27:59 -0400

    lavfi: add audio mix filter

--- libavfilter/allfilters.c
+++ libavfilter/allfilters.c
@@ -35,6 +35,7 @@ void avfilter_register_all(void)
     initialized = 1;
 
     REGISTER_FILTER (AFORMAT,     aformat,     af);
+    REGISTER_FILTER (AMIX,        amix,        af);
     REGISTER_FILTER (ANULL,       anull,       af);
     REGISTER_FILTER (ASPLIT,      asplit,      af);
     REGISTER_FILTER (ASYNCTS,     asyncts,     af);
