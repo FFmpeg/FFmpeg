70762508ec5919474edb92a5b1f266fd06640f9c
Stefano Sabatini <stefasab@gmail.com>
2012-05-26 01:38:03 +0200

    lavc: Prettify printing of codec tags containing non alphanumeric characters
    Make av_get_codec_tag_string() show codec tag string characters in a more
    intelligible ways. For example the ascii char "@" is used as a number, so
    should be displayed like "[64]" rather than as a printable character.
    
    Apart alphanumeric chars, only the characters ' ' and '.' are used
    literally in codec tags, all the other characters represent numbers.
    
    This also avoids relying on locale-dependent character class functions.
    
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- libavcodec/utils.c
+++ libavcodec/utils.c
@@ -1562,9 +1562,14 @@ size_t av_get_codec_tag_string(char *buf, size_t buf_size, unsigned int codec_ta
 {
     int i, len, ret = 0;
 
+#define TAG_PRINT(x)                                              \
+    (((x) >= '0' && (x) <= '9') ||                                \
+     ((x) >= 'a' && (x) <= 'z') || ((x) >= 'A' && (x) <= 'Z') ||  \
+     ((x) == '.' || (x) == ' '))
+
     for (i = 0; i < 4; i++) {
         len = snprintf(buf, buf_size,
-                       isprint(codec_tag & 0xFF) ? "%c" : "[%d]", codec_tag & 0xFF);
+                       TAG_PRINT(codec_tag & 0xFF) ? "%c" : "[%d]", codec_tag & 0xFF);
         buf        += len;
         buf_size    = buf_size > len ? buf_size - len : 0;
         ret        += len;
