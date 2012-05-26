8a45e0c35496ff5fa75f05d610d255e3cde7ee48
Michael Niedermayer <michaelni@gmx.at>
2012-05-26 19:54:27 +0200

    lswsutils: fix "ncompatible pointer type" warning
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavfilter/lswsutils.c
+++ libavfilter/lswsutils.c
@@ -42,7 +42,7 @@ int ff_scale_image(uint8_t *dst_data[4], int dst_linesize[4],
     if ((ret = av_image_alloc(dst_data, dst_linesize, dst_w, dst_h, dst_pix_fmt, 16)) < 0)
         goto end;
     ret = 0;
-    sws_scale(sws_ctx, src_data, src_linesize, 0, src_h, dst_data, dst_linesize);
+    sws_scale(sws_ctx, (const uint8_t * const*)src_data, src_linesize, 0, src_h, dst_data, dst_linesize);
 
 end:
     sws_freeContext(sws_ctx);
