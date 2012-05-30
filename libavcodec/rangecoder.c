0dfcd7e7e37e70347285dae9a0ad8ec3ef0f93d2
Michael Niedermayer <michaelni@gmx.at>
2012-05-31 00:48:29 +0200

    range coder test code: decoding errors are av_log_error not debug.
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/rangecoder.c
+++ libavcodec/rangecoder.c
@@ -148,7 +148,7 @@ STOP_TIMER("put_rac")
     for(i=0; i<SIZE; i++){
 START_TIMER
         if( (r[i]&1) != get_rac(&c, state) )
-            av_log(NULL, AV_LOG_DEBUG, "rac failure at %d\n", i);
+            av_log(NULL, AV_LOG_ERROR, "rac failure at %d\n", i);
 STOP_TIMER("get_rac")
     }
 
