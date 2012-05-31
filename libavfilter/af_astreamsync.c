2f2d47ab632138e841102f87bf903cbd875450a1
Nicolas George <nicolas.george@normalesup.org>
2012-05-31 21:15:00 +0200

    af_astreamsync: set samples rate in query_formats.

--- libavfilter/af_astreamsync.c
+++ libavfilter/af_astreamsync.c
@@ -80,13 +80,16 @@ static av_cold int init(AVFilterContext *ctx, const char *args0, void *opaque)
 static int query_formats(AVFilterContext *ctx)
 {
     int i;
-    AVFilterFormats *formats;
+    AVFilterFormats *formats, *rates;
     AVFilterChannelLayouts *layouts;
 
     for (i = 0; i < 2; i++) {
         formats = ctx->inputs[i]->in_formats;
         avfilter_formats_ref(formats, &ctx->inputs[i]->out_formats);
         avfilter_formats_ref(formats, &ctx->outputs[i]->in_formats);
+        rates = ff_all_samplerates();
+        avfilter_formats_ref(rates, &ctx->inputs[i]->out_samplerates);
+        avfilter_formats_ref(rates, &ctx->outputs[i]->in_samplerates);
         layouts = ctx->inputs[i]->in_channel_layouts;
         ff_channel_layouts_ref(layouts, &ctx->inputs[i]->out_channel_layouts);
         ff_channel_layouts_ref(layouts, &ctx->outputs[i]->in_channel_layouts);
