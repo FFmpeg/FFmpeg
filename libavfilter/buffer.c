0da9bce5a317a8a5ce4f8ed72c460ff929bf5c8e
Stefano Sabatini <stefasab@gmail.com>
2012-06-01 11:05:44 +0200

    lavfi: move avfilter_copy_frame_props() definition from buffer.c to avcodec.c
    The new location is more suited, as it is where the lavfi/lavc glue is
    defined.

--- libavfilter/buffer.c
+++ libavfilter/buffer.c
@@ -163,33 +163,6 @@ void avfilter_unref_bufferp(AVFilterBufferRef **ref)
     *ref = NULL;
 }
 
-int avfilter_copy_frame_props(AVFilterBufferRef *dst, const AVFrame *src)
-{
-    dst->pts    = src->pts;
-    dst->pos    = av_frame_get_pkt_pos(src);
-    dst->format = src->format;
-
-    switch (dst->type) {
-    case AVMEDIA_TYPE_VIDEO:
-        dst->video->w                   = src->width;
-        dst->video->h                   = src->height;
-        dst->video->sample_aspect_ratio = src->sample_aspect_ratio;
-        dst->video->interlaced          = src->interlaced_frame;
-        dst->video->top_field_first     = src->top_field_first;
-        dst->video->key_frame           = src->key_frame;
-        dst->video->pict_type           = src->pict_type;
-        break;
-    case AVMEDIA_TYPE_AUDIO:
-        dst->audio->sample_rate         = src->sample_rate;
-        dst->audio->channel_layout      = src->channel_layout;
-        break;
-    default:
-        return AVERROR(EINVAL);
-    }
-
-    return 0;
-}
-
 int avfilter_copy_buf_props(AVFrame *dst, const AVFilterBufferRef *src)
 {
     int planes, nb_channels;
