503f2feb143b58bf9b7850ef00ec0bb02d509f78
Alex Converse <alex.converse@gmail.com>
2012-06-04 17:35:51 -0700

    avprobe: Identify codec probe failures rather than calling them unsupported codecs.

--- avprobe.c
+++ avprobe.c
@@ -733,7 +733,10 @@ static int open_input_file(AVFormatContext **fmt_ctx_ptr, const char *filename)
         AVStream *stream = fmt_ctx->streams[i];
         AVCodec *codec;
 
-        if (!(codec = avcodec_find_decoder(stream->codec->codec_id))) {
+        if (stream->codec->codec_id == CODEC_ID_PROBE) {
+            fprintf(stderr, "Failed to probe codec for input stream %d\n",
+                    stream->index);
+        } else if (!(codec = avcodec_find_decoder(stream->codec->codec_id))) {
             fprintf(stderr,
                     "Unsupported codec with id %d for input stream %d\n",
                     stream->codec->codec_id, stream->index);
