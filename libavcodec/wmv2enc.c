eeba7c874b2c6895ea5a74a45e1fa6a987d8613b
Michael Niedermayer <michaelni@gmx.at>
2012-06-06 21:18:47 +0200

    wmv2enc: use av_assert
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/wmv2enc.c
+++ libavcodec/wmv2enc.c
@@ -85,10 +85,10 @@ int ff_wmv2_encode_picture_header(MpegEncContext * s, int picture_number)
     w->abt_type=0;
     w->j_type=0;
 
-    assert(s->flipflop_rounding);
+    av_assert0(s->flipflop_rounding);
 
     if (s->pict_type == AV_PICTURE_TYPE_I) {
-        assert(s->no_rounding==1);
+        av_assert0(s->no_rounding==1);
         if(w->j_type_bit) put_bits(&s->pb, 1, w->j_type);
 
         if(w->per_mb_rl_bit) put_bits(&s->pb, 1, s->per_mb_rl_table);
