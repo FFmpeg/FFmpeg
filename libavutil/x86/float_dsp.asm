f0313e90227571ae71d7f566c03d34de87ae0f91
Michael Niedermayer <michaelni@gmx.at>
2012-06-08 23:35:35 +0200

    x86/float_dsp.asm: restore author attribution
    The attribution was removed by libav while moving the code to libavutil
    
    The original code is from
    commit eb4825b5d43bb6ecfae4d64688f9e2d2ac075263
    Author: Loren Merritt <lorenm@u.washington.edu>
    Date:   Thu Aug 10 19:06:25 2006 +0000
    
        sse and 3dnow implementations of float->int conversion and mdct windowing.
        15% faster vorbis.
    
    and
    
    commit 069720565ce0f2cc94fa2474f30d155b2755e350
    Author: Loren Merritt <lorenm@u.washington.edu>
    Date:   Fri Aug 11 18:19:37 2006 +0000
    
        vorbis simd tweaks
    
    Reviewed-by: Paul B Mahol <onemda@gmail.com>
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavutil/x86/float_dsp.asm
+++ libavutil/x86/float_dsp.asm
@@ -1,20 +1,22 @@
 ;*****************************************************************************
 ;* x86-optimized Float DSP functions
 ;*
-;* This file is part of Libav.
+;* Copyright 2006 Loren Merritt
 ;*
-;* Libav is free software; you can redistribute it and/or
+;* This file is part of FFmpeg.
+;*
+;* FFmpeg is free software; you can redistribute it and/or
 ;* modify it under the terms of the GNU Lesser General Public
 ;* License as published by the Free Software Foundation; either
 ;* version 2.1 of the License, or (at your option) any later version.
 ;*
-;* Libav is distributed in the hope that it will be useful,
+;* FFmpeg is distributed in the hope that it will be useful,
 ;* but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 ;* Lesser General Public License for more details.
 ;*
 ;* You should have received a copy of the GNU Lesser General Public
-;* License along with Libav; if not, write to the Free Software
+;* License along with FFmpeg; if not, write to the Free Software
 ;* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 ;******************************************************************************
 
