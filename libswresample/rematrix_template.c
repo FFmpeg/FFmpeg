e016e3c9d7ce1493b952bdd8232f7e03077469a3
Michael Niedermayer <michaelni@gmx.at>
2012-06-08 22:12:00 +0200

    swr: add mix8to2
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libswresample/rematrix_template.c
+++ libswresample/rematrix_template.c
@@ -45,12 +45,29 @@ static void RENAME(mix6to2)(SAMPLE **out, const SAMPLE **in, COEFF *coeffp, int 
     }
 }
 
+static void RENAME(mix8to2)(SAMPLE **out, const SAMPLE **in, COEFF *coeffp, int len){
+    int i;
+
+    for(i=0; i<len; i++) {
+        INTER t = in[2][i]*coeffp[0*8+2] + in[3][i]*coeffp[0*8+3];
+        out[0][i] = R(t + in[0][i]*coeffp[0*8+0] + in[4][i]*coeffp[0*8+4] + in[6][i]*coeffp[0*8+6]);
+        out[1][i] = R(t + in[1][i]*coeffp[1*8+1] + in[5][i]*coeffp[1*8+5] + in[7][i]*coeffp[1*8+7]);
+    }
+}
+
 static mix_any_func_type *RENAME(get_mix_any_func)(SwrContext *s){
     if(   s->out_ch_layout == AV_CH_LAYOUT_STEREO && (s->in_ch_layout == AV_CH_LAYOUT_5POINT1 || s->in_ch_layout == AV_CH_LAYOUT_5POINT1_BACK)
        && s->matrix[0][2] == s->matrix[1][2] && s->matrix[0][3] == s->matrix[1][3]
        && !s->matrix[0][1] && !s->matrix[0][5] && !s->matrix[1][0] && !s->matrix[1][4]
     )
         return RENAME(mix6to2);
+
+    if(   s->out_ch_layout == AV_CH_LAYOUT_STEREO && s->in_ch_layout == AV_CH_LAYOUT_7POINT1
+       && s->matrix[0][2] == s->matrix[1][2] && s->matrix[0][3] == s->matrix[1][3]
+       && !s->matrix[0][1] && !s->matrix[0][5] && !s->matrix[1][0] && !s->matrix[1][4]
+       && !s->matrix[0][7] && !s->matrix[1][6]
+    )
+        return RENAME(mix8to2);
 
     return NULL;
 }
