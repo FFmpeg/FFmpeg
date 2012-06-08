FFmpeg/FFmpeg

commit d5a7229ba4aabc2b6407c731d9175879ae54c5ea

Add a float DSP framework to libavutil
Move vector_fmul() from DSPContext to AVFloatDSPContext.

libavutil/arm/float_dsp_arm.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008 Siarhei Siamashka <ssvb@users.sourceforge.net>
+ * Copyright (c) 2009 Mans Rullgard <mans@mansr.com>
  *
  * This file is part of Libav.
  *
@@ -18,13 +18,12 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
-#include "libavcodec/dsputil.h"
-#include "dsputil_arm.h"
+#ifndef AVUTIL_ARM_FLOAT_DSP_ARM_H
+#define AVUTIL_ARM_FLOAT_DSP_ARM_H
 
-void ff_vector_fmul_reverse_vfp(float *dst, const float *src0,
-                                const float *src1, int len);
+#include "libavutil/float_dsp.h"
 
-void ff_dsputil_init_vfp(DSPContext* c, AVCodecContext *avctx)
-{
-    c->vector_fmul_reverse = ff_vector_fmul_reverse_vfp;
-}
+void ff_dsputil_init_vfp (AVFloatDSPContext *fdsp);
+void ff_dsputil_init_neon(AVFloatDSPContext *fdsp);
+
+#endif /* AVUTIL_ARM_FLOAT_DSP_ARM_H */

libavutil/arm/float_dsp_init_arm.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008 Siarhei Siamashka <ssvb@users.sourceforge.net>
+ * ARM optimized DSP utils
  *
  * This file is part of Libav.
  *
@@ -18,13 +18,16 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
-#include "libavcodec/dsputil.h"
-#include "dsputil_arm.h"
+#include "libavutil/arm/cpu.h"
+#include "libavutil/float_dsp.h"
+#include "float_dsp_arm.h"
 
-void ff_vector_fmul_reverse_vfp(float *dst, const float *src0,
-                                const float *src1, int len);
-
-void ff_dsputil_init_vfp(DSPContext* c, AVCodecContext *avctx)
+void ff_float_dsp_init_arm(AVFloatDSPContext *fdsp)
 {
-    c->vector_fmul_reverse = ff_vector_fmul_reverse_vfp;
+    int cpu_flags = av_get_cpu_flags();
+
+    if (have_vfp(cpu_flags))
+        ff_dsputil_init_vfp(fdsp);
+    if (have_neon(cpu_flags))
+        ff_dsputil_init_neon(fdsp);
 }

libavutil/arm/float_dsp_init_neon.c
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 2008 Siarhei Siamashka <ssvb@users.sourceforge.net>
+ * ARM NEON optimised Float DSP functions
+ * Copyright (c) 2008 Mans Rullgard <mans@mansr.com>
  *
  * This file is part of Libav.
  *
@@ -18,13 +19,14 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
-#include "libavcodec/dsputil.h"
-#include "dsputil_arm.h"
+#include <stdint.h>
 
-void ff_vector_fmul_reverse_vfp(float *dst, const float *src0,
-                                const float *src1, int len);
+#include "libavutil/float_dsp.h"
+#include "float_dsp_arm.h"
 
-void ff_dsputil_init_vfp(DSPContext* c, AVCodecContext *avctx)
+void ff_vector_fmul_neon(float *dst, const float *src0, const float *src1, int len);
+
+void ff_dsputil_init_neon(AVFloatDSPContext *fdsp)
 {
-    c->vector_fmul_reverse = ff_vector_fmul_reverse_vfp;
+    c->vector_fmul = ff_vector_fmul_neon;
 }

libavutil/arm/float_dsp_init_vfp.c
@@ -18,13 +18,17 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
-#include "libavcodec/dsputil.h"
-#include "dsputil_arm.h"
+#include "libavutil/arm/cpu.h"
+#include "libavutil/float_dsp.h"
+#include "float_dsp_arm.h"
 
-void ff_vector_fmul_reverse_vfp(float *dst, const float *src0,
-                                const float *src1, int len);
+void ff_vector_fmul_vfp(float *dst, const float *src0, const float *src1,
+                        int len);
 
 void ff_dsputil_init_vfp(DSPContext* c, AVCodecContext *avctx)
 {
-    c->vector_fmul_reverse = ff_vector_fmul_reverse_vfp;
+    int cpu_flags = av_get_cpu_flags();
+
+    if (!have_vfpv3(cpu_flags))
+        c->vector_fmul = ff_vector_fmul_vfp;
 }

libavcodec/aac.h
@@ -30,6 +30,7 @@
 #ifndef AVCODEC_AAC_H
 #define AVCODEC_AAC_H
 
+#include "libavutil/float_dsp.h"
 #include "avcodec.h"
 #include "dsputil.h"
 #include "fft.h"
@@ -292,6 +293,7 @@ typedef struct {
     FFTContext mdct_ltp;
     DSPContext dsp;
     FmtConvertContext fmt_conv;
+    AVFloatDSPContext fdsp;
     int random_state;
     /** @} */
 

libavcodec/aacdec.c
@@ -79,7 +79,7 @@
            Parametric Stereo.
  */
 
-
+#include "libavutil/float_dsp.h"
 #include "avcodec.h"
 #include "internal.h"
 #include "get_bits.h"
@@ -867,6 +867,7 @@ static av_cold int aac_decode_init(AVCodecContext *avctx)
 
     ff_dsputil_init(&ac->dsp, avctx);
     ff_fmt_convert_init(&ac->fmt_conv, avctx);
+    avpriv_float_dsp_init(&ac->fdsp, avctx->flags & CODEC_FLAG_BITEXACT);
 
     ac->random_state = 0x1f2e3d4c;
 
@@ -2032,10 +2033,10 @@ static void windowing_and_mdct_ltp(AACContext *ac, float *out,
     const float *swindow_prev = ics->use_kb_window[1] ? ff_aac_kbd_short_128 : ff_sine_128;
 
     if (ics->window_sequence[0] != LONG_STOP_SEQUENCE) {
-        ac->dsp.vector_fmul(in, in, lwindow_prev, 1024);
+        ac->fdsp.vector_fmul(in, in, lwindow_prev, 1024);
     } else {
         memset(in, 0, 448 * sizeof(float));
-        ac->dsp.vector_fmul(in + 448, in + 448, swindow_prev, 128);
+        ac->fdsp.vector_fmul(in + 448, in + 448, swindow_prev, 128);
     }
     if (ics->window_sequence[0] != LONG_START_SEQUENCE) {
         ac->dsp.vector_fmul_reverse(in + 1024, in + 1024, lwindow, 1024);

libavcodec/aacenc.c
@@ -30,6 +30,7 @@
  * add temporal noise shaping
  ***********************************/
 
+#include "libavutil/float_dsp.h"
 #include "libavutil/opt.h"
 #include "avcodec.h"
 #include "put_bits.h"
@@ -182,7 +183,9 @@ static void put_audio_specific_config(AVCodecContext *avctx)
 }
 
 #define WINDOW_FUNC(type) \
-static void apply_ ##type ##_window(DSPContext *dsp, SingleChannelElement *sce, const float *audio)
+static void apply_ ##type ##_window(DSPContext *dsp, AVFloatDSPContext *fdsp, \
+                                    SingleChannelElement *sce, \
+                                    const float *audio)
 
 WINDOW_FUNC(only_long)
 {
@@ -190,7 +193,7 @@ WINDOW_FUNC(only_long)
     const float *pwindow = sce->ics.use_kb_window[1] ? ff_aac_kbd_long_1024 : ff_sine_1024;
     float *out = sce->ret;
 
-    dsp->vector_fmul        (out,        audio,        lwindow, 1024);
+    fdsp->vector_fmul       (out,        audio,        lwindow, 1024);
     dsp->vector_fmul_reverse(out + 1024, audio + 1024, pwindow, 1024);
 }
 
@@ -200,7 +203,7 @@ WINDOW_FUNC(long_start)
     const float *swindow = sce->ics.use_kb_window[0] ? ff_aac_kbd_short_128 : ff_sine_128;
     float *out = sce->ret;
 
-    dsp->vector_fmul(out, audio, lwindow, 1024);
+    fdsp->vector_fmul(out, audio, lwindow, 1024);
     memcpy(out + 1024, audio + 1024, sizeof(out[0]) * 448);
     dsp->vector_fmul_reverse(out + 1024 + 448, audio + 1024 + 448, swindow, 128);
     memset(out + 1024 + 576, 0, sizeof(out[0]) * 448);
@@ -213,7 +216,7 @@ WINDOW_FUNC(long_stop)
     float *out = sce->ret;
 
     memset(out, 0, sizeof(out[0]) * 448);
-    dsp->vector_fmul(out + 448, audio + 448, swindow, 128);
+    fdsp->vector_fmul(out + 448, audio + 448, swindow, 128);
     memcpy(out + 576, audio + 576, sizeof(out[0]) * 448);
     dsp->vector_fmul_reverse(out + 1024, audio + 1024, lwindow, 1024);
 }
@@ -227,7 +230,7 @@ WINDOW_FUNC(eight_short)
     int w;
 
     for (w = 0; w < 8; w++) {
-        dsp->vector_fmul        (out, in, w ? pwindow : swindow, 128);
+        fdsp->vector_fmul       (out, in, w ? pwindow : swindow, 128);
         out += 128;
         in  += 128;
         dsp->vector_fmul_reverse(out, in, swindow, 128);
@@ -235,7 +238,9 @@ WINDOW_FUNC(eight_short)
     }
 }
 
-static void (*const apply_window[4])(DSPContext *dsp, SingleChannelElement *sce, const float *audio) = {
+static void (*const apply_window[4])(DSPContext *dsp, AVFloatDSPContext *fdsp,
+                                     SingleChannelElement *sce,
+                                     const float *audio) = {
     [ONLY_LONG_SEQUENCE]   = apply_only_long_window,
     [LONG_START_SEQUENCE]  = apply_long_start_window,
     [EIGHT_SHORT_SEQUENCE] = apply_eight_short_window,
@@ -248,7 +253,7 @@ static void apply_window_and_mdct(AACEncContext *s, SingleChannelElement *sce,
     int i;
     float *output = sce->ret;
 
-    apply_window[sce->ics.window_sequence[0]](&s->dsp, sce, audio);
+    apply_window[sce->ics.window_sequence[0]](&s->dsp, &s->fdsp, sce, audio);
 
     if (sce->ics.window_sequence[0] != EIGHT_SHORT_SEQUENCE)
         s->mdct1024.mdct_calc(&s->mdct1024, sce->coeffs, output);
@@ -694,6 +699,7 @@ static av_cold int dsp_init(AVCodecContext *avctx, AACEncContext *s)
     int ret = 0;
 
     ff_dsputil_init(&s->dsp, avctx);
+    avpriv_float_dsp_init(&s->fdsp, avctx->flags & CODEC_FLAG_BITEXACT);
 
     // window init
     ff_kbd_window_init(ff_aac_kbd_long_1024, 4.0, 1024);

libavcodec/aacenc.h
@@ -22,6 +22,7 @@
 #ifndef AVCODEC_AACENC_H
 #define AVCODEC_AACENC_H
 
+#include "libavutil/float_dsp.h"
 #include "avcodec.h"
 #include "put_bits.h"
 #include "dsputil.h"
@@ -58,6 +59,7 @@ typedef struct AACEncContext {
     FFTContext mdct1024;                         ///< long (1024 samples) frame transform context
     FFTContext mdct128;                          ///< short (128 samples) frame transform context
     DSPContext  dsp;
+    AVFloatDSPContext fdsp;
     float *planar_samples[6];                    ///< saved preprocessed input
 
     int samplerate_index;                        ///< MPEG-4 samplerate index

commit a839d6abf870af82bdde8afdef40377eb31c82f8

ARM: fix float_dsp breakage from d5a7229
Signed-off-by: Mans Rullgard <mans@mansr.com>

libavutil/arm/float_dsp_arm.h
@@ -23,7 +23,7 @@
 
 #include "libavutil/float_dsp.h"
 
-void ff_dsputil_init_vfp (AVFloatDSPContext *fdsp);
-void ff_dsputil_init_neon(AVFloatDSPContext *fdsp);
+void ff_float_dsp_init_vfp (AVFloatDSPContext *fdsp);
+void ff_float_dsp_init_neon(AVFloatDSPContext *fdsp);
 
 #endif /* AVUTIL_ARM_FLOAT_DSP_ARM_H */

libavutil/arm/float_dsp_init_arm.c
@@ -27,7 +27,7 @@ void ff_float_dsp_init_arm(AVFloatDSPContext *fdsp)
     int cpu_flags = av_get_cpu_flags();
 
     if (have_vfp(cpu_flags))
-        ff_dsputil_init_vfp(fdsp);
+        ff_float_dsp_init_vfp(fdsp);
     if (have_neon(cpu_flags))
-        ff_dsputil_init_neon(fdsp);
+        ff_float_dsp_init_neon(fdsp);
 }

libavutil/arm/float_dsp_init_neon.c
@@ -26,7 +26,7 @@
 
 void ff_vector_fmul_neon(float *dst, const float *src0, const float *src1, int len);
 
-void ff_dsputil_init_neon(AVFloatDSPContext *fdsp)
+void ff_float_dsp_init_neon(AVFloatDSPContext *fdsp)
 {
-    c->vector_fmul = ff_vector_fmul_neon;
+    fdsp->vector_fmul = ff_vector_fmul_neon;
 }

libavutil/arm/float_dsp_init_vfp.c
@@ -25,10 +25,10 @@
 void ff_vector_fmul_vfp(float *dst, const float *src0, const float *src1,
                         int len);
 
-void ff_dsputil_init_vfp(DSPContext* c, AVCodecContext *avctx)
+void ff_float_dsp_init_vfp(AVFloatDSPContext *fdsp)
 {
     int cpu_flags = av_get_cpu_flags();
 
     if (!have_vfpv3(cpu_flags))
-        c->vector_fmul = ff_vector_fmul_vfp;
+        fdsp->vector_fmul = ff_vector_fmul_vfp;
 }