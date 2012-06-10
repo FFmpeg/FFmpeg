0d249316043fb69a3972029bff3a2969689ba8b6
Nicolas George <nicolas.george@normalesup.org>
2012-06-10 10:53:03 +0200

    vf_fps: set frame_rate.

--- libavfilter/vf_fps.c
+++ libavfilter/vf_fps.c
@@ -120,6 +120,7 @@ static int config_props(AVFilterLink* link)
     FPSContext   *s = link->src->priv;
 
     link->time_base = (AVRational){ s->framerate.den, s->framerate.num };
+    link->frame_rate= s->framerate;
     link->w         = link->src->inputs[0]->w;
     link->h         = link->src->inputs[0]->h;
     s->pts          = AV_NOPTS_VALUE;
