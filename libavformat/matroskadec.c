59c122b3b0a00808e3c4f534927755d89e7baa62
Michael Niedermayer <michaelni@gmx.at>
2012-06-15 01:35:52 +0200

    matroskadec: add assert on lack of overflow in pkt_size+offset
    currently a overflow there should be impossible but future changes to
    the code could easily introduce a bug that no longer limits the 2
    values sufficiently so better protect it via av_assert.
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/matroskadec.c
+++ libavformat/matroskadec.c
@@ -2053,6 +2053,7 @@ static int matroska_parse_block(MatroskaDemuxContext *matroska, uint8_t *data,
                     offset = matroska_decode_buffer(&pkt_data,&pkt_size, track);
                     if (offset < 0)
                         continue;
+                    av_assert0(offset + pkt_size >= pkt_size);
                 }
 
                 pkt = av_mallocz(sizeof(AVPacket));
