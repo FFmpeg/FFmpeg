0b2ecf8224cf882033737541cf3bd615dd414625
Stefano Sabatini <stefasab@gmail.com>
2012-06-16 11:35:38 +0200

    lavc/imgconvert: remove pointless switch block from avpicture_fill()

--- libavcodec/imgconvert.c
+++ libavcodec/imgconvert.c
@@ -334,16 +334,6 @@ int avpicture_layout(const AVPicture* src, enum PixelFormat pix_fmt, int width, 
         }
     }
 
-    switch (pix_fmt) {
-    case PIX_FMT_RGB8:
-    case PIX_FMT_BGR8:
-    case PIX_FMT_RGB4_BYTE:
-    case PIX_FMT_BGR4_BYTE:
-    case PIX_FMT_GRAY8:
-        // do not include palette for these pseudo-paletted formats
-        return size;
-    }
-
     if (desc->flags & PIX_FMT_PAL) {
         uint32_t *d32 = (unsigned char *)(((size_t)dest + 3) & ~3);
         for (i = 0; i<256; i++)
