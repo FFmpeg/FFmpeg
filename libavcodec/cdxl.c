b2e2287c0461031e3a68ceb856394109d2a9e60b
Paul B Mahol <onemda@gmail.com>
2012-06-17 11:22:42 +0000

    cdxl: disable checked bitstream reader
    Overreads should not be possible.
    
    Signed-off-by: Paul B Mahol <onemda@gmail.com>

--- libavcodec/cdxl.c
+++ libavcodec/cdxl.c
@@ -19,6 +19,8 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
+#define UNCHECKED_BITSTREAM_READER 1
+
 #include "libavutil/intreadwrite.h"
 #include "libavutil/imgutils.h"
 #include "avcodec.h"
