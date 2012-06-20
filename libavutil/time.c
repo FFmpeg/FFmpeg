61183b5ab4c203363ee0ee696bdf4819be307230
Mans Rullgard <mans@mansr.com>
2012-06-20 13:46:52 +0100

    av_gettime: support Win32 without gettimeofday()
    Based on patch by Ronald S. Bultje.
    
    Signed-off-by: Mans Rullgard <mans@mansr.com>

--- libavutil/time.c
+++ libavutil/time.c
@@ -16,15 +16,31 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
+#include "config.h"
+
 #include <stddef.h>
 #include <stdint.h>
+#if HAVE_GETTIMEOFDAY
 #include <sys/time.h>
+#elif HAVE_GETSYSTEMTIMEASFILETIME
+#include <windows.h>
+#endif
 
 #include "libavutil/time.h"
 
 int64_t av_gettime(void)
 {
+#if HAVE_GETTIMEOFDAY
     struct timeval tv;
     gettimeofday(&tv, NULL);
     return (int64_t)tv.tv_sec * 1000000 + tv.tv_usec;
+#elif HAVE_GETSYSTEMTIMEASFILETIME
+    FILETIME ft;
+    int64_t t;
+    GetSystemTimeAsFileTime(&ft);
+    t = (int64_t)ft.dwHighDateTime << 32 | ft.dwLowDateTime;
+    return t / 10 - 11644473600000000; /* Jan 1, 1601 */
+#else
+    return -1;
+#endif
 }
