43886eaebc8e803a2f90051afd03068be47e398f
Alex Converse <alex.converse@gmail.com>
2012-06-19 19:06:34 -0700

    aacdec: Fix popping channel layouts.
    'channel_layout' not 'channels' from the stored configuration should go
    to AVCodecContext's 'channel_layout'.

--- libavcodec/aacdec.c
+++ libavcodec/aacdec.c
@@ -368,7 +368,7 @@ static void pop_output_configuration(AACContext *ac) {
     if (ac->oc[1].status != OC_LOCKED) {
         ac->oc[1] = ac->oc[0];
         ac->avctx->channels = ac->oc[1].channels;
-        ac->avctx->channel_layout = ac->oc[1].channels;
+        ac->avctx->channel_layout = ac->oc[1].channel_layout;
     }
 }
 
