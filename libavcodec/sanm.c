fd0b8c6ad53ff2a01868ecf1bbf0c936876a0c61
Paul B Mahol <onemda@gmail.com>
2012-06-21 14:48:05 +0000

    sanm: silence warnings
    Signed-off-by: Paul B Mahol <onemda@gmail.com>

--- libavcodec/sanm.c
+++ libavcodec/sanm.c
@@ -996,16 +996,18 @@ static int decode_4(SANMVideoContext *ctx)
 
 static int decode_5(SANMVideoContext *ctx)
 {
+#if HAVE_BIGENDIAN
     uint16_t *frm;
     int npixels;
+#endif
     uint8_t *dst = (uint8_t*)ctx->frm0;
 
     if (rle_decode(ctx, dst, ctx->buf_size))
         return AVERROR_INVALIDDATA;
 
+#if HAVE_BIGENDIAN
     npixels = ctx->npixels;
     frm = ctx->frm0;
-#if HAVE_BIGENDIAN
     while (npixels--)
         *frm++ = av_bswap16(*frm);
 #endif
