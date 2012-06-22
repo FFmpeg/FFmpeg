15bde060844b7d32af80b352834ba5f33cab55d1
Carl Eugen Hoyos <cehoyos@ag.or.at>
2012-06-22 16:10:42 +0200

    Revert "mathematics.h: remove a couple of math defines"
    This reverts commit a2b186a188c126dd0402414d9e8198ec4df63ebe.
    
    The defines were present for several years, removing
    them without a version bump may break API.
    (Fixes MPlayer compilation.)

--- libavutil/mathematics.h
+++ libavutil/mathematics.h
@@ -26,11 +26,29 @@
 #include "attributes.h"
 #include "rational.h"
 
+#ifndef M_E
+#define M_E            2.7182818284590452354   /* e */
+#endif
+#ifndef M_LN2
+#define M_LN2          0.69314718055994530942  /* log_e 2 */
+#endif
+#ifndef M_LN10
+#define M_LN10         2.30258509299404568402  /* log_e 10 */
+#endif
 #ifndef M_LOG2_10
 #define M_LOG2_10      3.32192809488736234787  /* log_2 10 */
 #endif
 #ifndef M_PHI
 #define M_PHI          1.61803398874989484820   /* phi / golden ratio */
+#endif
+#ifndef M_PI
+#define M_PI           3.14159265358979323846  /* pi */
+#endif
+#ifndef M_SQRT1_2
+#define M_SQRT1_2      0.70710678118654752440  /* 1/sqrt(2) */
+#endif
+#ifndef M_SQRT2
+#define M_SQRT2        1.41421356237309504880  /* sqrt(2) */
 #endif
 #ifndef NAN
 #define NAN            (0.0/0.0)
