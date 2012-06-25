dc30c27eb162f383a0eddb2a7e5e911687255c8e
Michael Niedermayer <michaelni@gmx.at>
2012-06-25 21:12:22 +0200

    h264_parser: use av_assert
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/h264_parser.c
+++ libavcodec/h264_parser.c
@@ -31,8 +31,6 @@
 #include "h264data.h"
 #include "golomb.h"
 
-#include <assert.h>
-
 
 static int ff_h264_find_frame_end(H264Context *h, const uint8_t *buf, int buf_size)
 {
@@ -324,7 +322,7 @@ static int h264_parse(AVCodecParserContext *s,
         }
 
         if(next<0 && next != END_NOT_FOUND){
-            assert(pc->last_index + next >= 0 );
+            av_assert1(pc->last_index + next >= 0 );
             ff_h264_find_frame_end(h, &pc->buffer[pc->last_index + next], -next); //update state
         }
     }
