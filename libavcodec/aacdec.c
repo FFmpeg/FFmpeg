3c928477d2f42c456f4c560db032a4380a3b6953
Mans Rullgard <mans@mansr.com>
2012-07-01 13:50:09 +0100

    aacdec: remove dead assignment
    Signed-off-by: Mans Rullgard <mans@mansr.com>

--- libavcodec/aacdec.c
+++ libavcodec/aacdec.c
@@ -1271,7 +1271,7 @@ static inline float *VMUL4S(float *dst, const float *v, unsigned idx,
     t.i = s.i ^ (sign & 1U<<31);
     *dst++ = v[idx>>4 & 3] * t.f;
 
-    sign <<= nz & 1; nz >>= 1;
+    sign <<= nz & 1;
     t.i = s.i ^ (sign & 1U<<31);
     *dst++ = v[idx>>6 & 3] * t.f;
 
