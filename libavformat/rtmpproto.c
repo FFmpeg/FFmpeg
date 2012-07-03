08e087ccf7f7ef9b4e8c55b571bc779c0bc815b0
Jordi Ortiz <nenjordi@gmail.com>
2012-07-03 18:20:02 +0000

    rtmp: rtmp_parse_result() add case for video and audio packets to avoid undesired debug output.
    Signed-off-by: Kostya Shishkov <kostya.shishkov@gmail.com>

--- libavformat/rtmpproto.c
+++ libavformat/rtmpproto.c
@@ -959,6 +959,10 @@ static int rtmp_parse_result(URLContext *s, RTMPContext *rt, RTMPPacket *pkt)
                 return ret;
         }
         break;
+    case RTMP_PT_VIDEO:
+    case RTMP_PT_AUDIO:
+        /* Audio and Video packets are parsed in get_packet() */
+        break;
     default:
         av_log(s, AV_LOG_VERBOSE, "Unknown packet type received 0x%02X\n", pkt->type);
         break;
