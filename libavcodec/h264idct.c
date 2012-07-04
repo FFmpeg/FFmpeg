69d5e40e5afe24439a5b820ca135e1609c8d58a0
Michael Niedermayer <michaelni@gmx.at>
2012-07-05 01:33:18 +0200

    h264idct: 12 and 14 bit support
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/h264idct.c
+++ libavcodec/h264idct.c
@@ -36,3 +36,11 @@
 #define BIT_DEPTH 10
 #include "h264idct_template.c"
 #undef BIT_DEPTH
+
+#define BIT_DEPTH 12
+#include "h264idct_template.c"
+#undef BIT_DEPTH
+
+#define BIT_DEPTH 14
+#include "h264idct_template.c"
+#undef BIT_DEPTH
