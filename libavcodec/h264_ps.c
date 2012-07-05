acb52d322818694da1c1e03c82bd887cddac8f3e
Michael Niedermayer <michaelni@gmx.at>
2012-07-05 19:12:09 +0200

    h264: print error on unsupported seperate color planes
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/h264_ps.c
+++ libavcodec/h264_ps.c
@@ -359,6 +359,10 @@ int ff_h264_decode_seq_parameter_set(H264Context *h){
             goto fail;
         } else if(sps->chroma_format_idc == 3) {
             sps->residual_color_transform_flag = get_bits1(&s->gb);
+            if(sps->residual_color_transform_flag) {
+                av_log(h->s.avctx, AV_LOG_ERROR, "separate color planes are not supported\n");
+                goto fail;
+            }
         }
         sps->bit_depth_luma   = get_ue_golomb(&s->gb) + 8;
         sps->bit_depth_chroma = get_ue_golomb(&s->gb) + 8;
