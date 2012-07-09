5467742232c312b7d61dca7ac57447f728d8d6c9
Kostya Shishkov <kostya.shishkov@gmail.com>
2012-07-09 18:15:27 +0200

    mss3: use standard zigzag table

--- libavcodec/mss3.c
+++ libavcodec/mss3.c
@@ -26,6 +26,7 @@
 
 #include "avcodec.h"
 #include "bytestream.h"
+#include "dsputil.h"
 #include "mss34dsp.h"
 
 #define HEADER_SIZE 27
@@ -119,17 +120,6 @@ typedef struct MSS3Context {
     int              dctblock[64];
     int              hblock[16 * 16];
 } MSS3Context;
-
-static const uint8_t zigzag_scan[64] = {
-    0,   1,  8, 16,  9,  2,  3, 10,
-    17, 24, 32, 25, 18, 11,  4,  5,
-    12, 19, 26, 33, 40, 48, 41, 34,
-    27, 20, 13,  6,  7, 14, 21, 28,
-    35, 42, 49, 56, 57, 50, 43, 36,
-    29, 22, 15, 23, 30, 37, 44, 51,
-    58, 59, 52, 45, 38, 31, 39, 46,
-    53, 60, 61, 54, 47, 55, 62, 63
-};
 
 
 static void model2_reset(Model2 *m)
@@ -557,7 +547,7 @@ static int decode_dct(RangeCoder *c, DCTBlockCoder *bc, int *block,
         if (!sign)
             val = -val;
 
-        zz_pos = zigzag_scan[pos];
+        zz_pos = ff_zigzag_direct[pos];
         block[zz_pos] = val * bc->qmat[zz_pos];
         pos++;
     }
