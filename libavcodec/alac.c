a06fdadd97778fe50713c2762714cbd8b0eb81b6
Justin Ruggles <justin.ruggles@gmail.com>
2012-07-09 10:34:11 -0400

    alac: cosmetics: reindent after last commit

--- libavcodec/alac.c
+++ libavcodec/alac.c
@@ -85,16 +85,16 @@ static inline int decode_scalar(GetBitContext *gb, int k, int readsamplesize)
         /* use alternative encoding */
         x = get_bits(gb, readsamplesize);
     } else if (k != 1) {
-            int extrabits = show_bits(gb, k);
+        int extrabits = show_bits(gb, k);
 
-            /* multiply x by 2^k - 1, as part of their strange algorithm */
-            x = (x << k) - x;
+        /* multiply x by 2^k - 1, as part of their strange algorithm */
+        x = (x << k) - x;
 
-            if (extrabits > 1) {
-                x += extrabits - 1;
-                skip_bits(gb, k);
-            } else
-                skip_bits(gb, k - 1);
+        if (extrabits > 1) {
+            x += extrabits - 1;
+            skip_bits(gb, k);
+        } else
+            skip_bits(gb, k - 1);
     }
     return x;
 }
