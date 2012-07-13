c3a4c1bd5abf96cd4dc3486004e0fd70d02b6d28
Compn <tempn@twmi.rr.com>
2012-07-13 15:10:53 -0400

    add PLV1 fourcc to mpeg4 works on http://samples.ffmpeg.org/V-codecs/PLV1/york-water-meteor.avi

--- libavformat/riff.c
+++ libavformat/riff.c
@@ -91,6 +91,7 @@ const AVCodecTag ff_codec_bmp_tags[] = {
     { CODEC_ID_MPEG4,        MKTAG('X', 'V', 'I', 'X') },
     { CODEC_ID_MPEG4,        MKTAG('D', 'r', 'e', 'X') },
     { CODEC_ID_MPEG4,        MKTAG('Q', 'M', 'P', '4') }, /* QNAP Systems */
+    { CODEC_ID_MPEG4,        MKTAG('P', 'L', 'V', '1') }, /* Pelco DVR MPEG-4 */
     { CODEC_ID_MSMPEG4V3,    MKTAG('M', 'P', '4', '3') },
     { CODEC_ID_MSMPEG4V3,    MKTAG('D', 'I', 'V', '3') },
     { CODEC_ID_MSMPEG4V3,    MKTAG('M', 'P', 'G', '3') },
