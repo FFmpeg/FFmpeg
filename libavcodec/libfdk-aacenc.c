7ff318e2dd31e8fd10b593111ee64aea11ceb9a4
Derek Buitenhuis <derek.buitenhuis@gmail.com>
2012-07-12 21:24:38 -0400

    libfdk-aac: Port to ff_alloc_packet2
    Signed-off-by: Derek Buitenhuis <derek.buitenhuis@gmail.com>

--- libavcodec/libfdk-aacenc.c
+++ libavcodec/libfdk-aacenc.c
@@ -306,7 +306,7 @@ static int aac_encode_frame(AVCodecContext *avctx, AVPacket *avpkt,
     }
 
     /* The maximum packet size is 6144 bits aka 768 bytes per channel. */
-    if ((ret = ff_alloc_packet(avpkt, FFMAX(8192, 768 * avctx->channels)))) {
+    if ((ret = ff_alloc_packet2(avctx, avpkt, FFMAX(8192, 768 * avctx->channels)))) {
         av_log(avctx, AV_LOG_ERROR, "Error getting output packet\n");
         return ret;
     }
