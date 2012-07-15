9a1963fbb872f08222a5b73fe96931708ca32732
Stefano Sabatini <stefasab@gmail.com>
2012-07-15 16:27:22 +0200

    ffprobe: reindent after the previous commit

--- ffprobe.c
+++ ffprobe.c
@@ -1662,15 +1662,15 @@ static av_always_inline int get_decoded_frame(AVFormatContext *fmt_ctx,
 
     *got_frame = 0;
     if (dec_ctx->codec) {
-    switch (dec_ctx->codec_type) {
-    case AVMEDIA_TYPE_VIDEO:
-        ret = avcodec_decode_video2(dec_ctx, frame, got_frame, pkt);
-        break;
+        switch (dec_ctx->codec_type) {
+        case AVMEDIA_TYPE_VIDEO:
+            ret = avcodec_decode_video2(dec_ctx, frame, got_frame, pkt);
+            break;
 
-    case AVMEDIA_TYPE_AUDIO:
-        ret = avcodec_decode_audio4(dec_ctx, frame, got_frame, pkt);
-        break;
-    }
+        case AVMEDIA_TYPE_AUDIO:
+            ret = avcodec_decode_audio4(dec_ctx, frame, got_frame, pkt);
+            break;
+        }
     }
 
     return ret;
