d515e9c2259dd825b167a94266efd5336a711702
Anton Khirnov <anton@khirnov.net>
2012-07-15 09:50:49 +0200

    vf_fps: fix a memleak on malloc failure.

--- libavfilter/vf_fps.c
+++ libavfilter/vf_fps.c
@@ -161,8 +161,10 @@ static int write_to_fifo(AVFifoBuffer *fifo, AVFilterBufferRef *buf)
     int ret;
 
     if (!av_fifo_space(fifo) &&
-        (ret = av_fifo_realloc2(fifo, 2*av_fifo_size(fifo))))
+        (ret = av_fifo_realloc2(fifo, 2*av_fifo_size(fifo)))) {
+        avfilter_unref_bufferp(&buf);
         return ret;
+    }
 
     av_fifo_generic_write(fifo, &buf, sizeof(buf), NULL);
     return 0;
