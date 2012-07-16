0b40153d20ba80e842d288038a62c4dfdc5384a3
Kostya Shishkov <kostya.shishkov@gmail.com>
2012-07-16 20:14:37 +0200

    tscc2: DCT output should not be clipped
    This fixes decoding some TSCC2 files with large quantisers.

--- libavcodec/tscc2.c
+++ libavcodec/tscc2.c
@@ -95,7 +95,7 @@ static av_cold int init_vlcs(TSCC2Context *c)
     OP(d3, 5 * ((s0) - (s1) + (s2)) - 2 * (s3));  \
 
 #define COL_OP(a, b)  a = b
-#define ROW_OP(a, b)  a = av_clip_uint8((((b) + 0x20) >> 6) + 0x80)
+#define ROW_OP(a, b)  a = (((b) + 0x20) >> 6) + 0x80
 
 static void tscc2_idct4_put(int *in, int q[3], uint8_t *dst, int stride)
 {
