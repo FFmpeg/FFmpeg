d06bfda07eab95318e6586ea37110ee99f02c97a
Nicolas George <nicolas.george@normalesup.org>
2012-07-17 17:43:39 +0200

    lavfi: rescale link->current_pts.
    The doxy says, and the heap implementations assumes,
    it is in AV_TIME_BASE units.

--- libavfilter/avfilter.c
+++ libavfilter/avfilter.c
@@ -359,7 +359,8 @@ void ff_update_link_current_pts(AVFilterLink *link, int64_t pts)
 {
     if (pts == AV_NOPTS_VALUE)
         return;
-    link->current_pts =  pts; /* TODO use duration */
+    link->current_pts = av_rescale_q(pts, link->time_base, AV_TIME_BASE_Q);
+    /* TODO use duration */
     if (link->graph && link->age_index >= 0)
         ff_avfilter_graph_update_heap(link->graph, link);
 }
