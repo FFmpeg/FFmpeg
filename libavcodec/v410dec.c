d147aedb6b2160477a19cc46bb7cda9d425d3e47
Carl Eugen Hoyos <cehoyos@ag.or.at>
2012-07-23 23:44:13 +0200

    Fix typo in v410 decoder.

--- libavcodec/v410dec.c
+++ libavcodec/v410dec.c
@@ -30,10 +30,10 @@ static av_cold int v410_decode_init(AVCodecContext *avctx)
 
     if (avctx->width & 1) {
         if (avctx->err_recognition & AV_EF_EXPLODE) {
-            av_log(avctx, AV_LOG_ERROR, "v410 requires width to be even, continuing anyway.\n");
+            av_log(avctx, AV_LOG_ERROR, "v410 requires width to be even.\n");
             return AVERROR_INVALIDDATA;
         } else {
-            av_log(avctx, AV_LOG_WARNING, "v410 requires width to be even.\n");
+            av_log(avctx, AV_LOG_WARNING, "v410 requires width to be even, continuing anyway.\n");
         }
     }
 
