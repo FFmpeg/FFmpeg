08fc1ad151fcaac67f4550c224254352c14e4e10
Michael Niedermayer <michaelni@gmx.at>
2012-07-25 08:42:25 +0200

    vf_overlay: prevent premature freeing of cur_buf
    Signed-off-by: Anton Khirnov <anton@khirnov.net>
    Signed-off-by: Luca Barbato <lu_zero@gentoo.org>

--- libavfilter/vf_overlay.c
+++ libavfilter/vf_overlay.c
@@ -238,6 +238,7 @@ static int start_frame_overlay(AVFilterLink *inlink, AVFilterBufferRef *inpicref
     AVFilterContext *ctx = inlink->dst;
     OverlayContext *over = ctx->priv;
 
+    inlink->cur_buf  = NULL;
     over->overpicref = inpicref;
     over->overpicref->pts = av_rescale_q(inpicref->pts, ctx->inputs[OVERLAY]->time_base,
                                          ctx->outputs[0]->time_base);
