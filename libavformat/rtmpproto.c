2357f606876173a25acf3130868e374cc44c5f47
Samuel Pitoiset <samuel.pitoiset@gmail.com>
2012-07-26 14:05:18 +0200

    rtmp: Prevent reading outside of an allocate buffer when receiving server bandwidth packets
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- libavformat/rtmpproto.c
+++ libavformat/rtmpproto.c
@@ -950,6 +950,13 @@ static int handle_server_bw(URLContext *s, RTMPPacket *pkt)
 {
     RTMPContext *rt = s->priv_data;
 
+    if (pkt->data_size < 4) {
+        av_log(s, AV_LOG_ERROR,
+               "Too short server bandwidth report packet (%d)\n",
+               pkt->data_size);
+        return AVERROR_INVALIDDATA;
+    }
+
     rt->server_bw = AV_RB32(pkt->data);
     if (rt->server_bw <= 0) {
         av_log(s, AV_LOG_ERROR, "Incorrect server bandwidth %d\n",
