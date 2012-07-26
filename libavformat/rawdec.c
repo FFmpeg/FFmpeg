fdc371114faf8535efbde67729bf5a3c56fbcf04
Michael Niedermayer <michaelni@gmx.at>
2012-07-26 02:11:06 +0200

    ff_raw_audio_read_header: mark as AVSTREAM_PARSE_FULL_RAW
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/rawdec.c
+++ libavformat/rawdec.c
@@ -134,7 +134,7 @@ int ff_raw_audio_read_header(AVFormatContext *s)
         return AVERROR(ENOMEM);
     st->codec->codec_type = AVMEDIA_TYPE_AUDIO;
     st->codec->codec_id = s->iformat->raw_codec_id;
-    st->need_parsing = AVSTREAM_PARSE_FULL;
+    st->need_parsing = AVSTREAM_PARSE_FULL_RAW;
     st->start_time = 0;
     /* the parameters will be extracted from the compressed bitstream */
 
