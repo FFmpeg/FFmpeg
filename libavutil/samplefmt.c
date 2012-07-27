753a1b09882b921c0874fa11feeaac5684e4ef16
jamal <jamrial@gmail.com>
2012-07-27 13:11:02 -0300

    samplefmt: Fix warning about discarded qualifier
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavutil/samplefmt.c
+++ libavutil/samplefmt.c
@@ -163,7 +163,7 @@ int av_samples_fill_arrays(uint8_t **audio_data, int *linesize,
     if (buf_size < 0)
         return buf_size;
 
-    audio_data[0] = buf;
+    audio_data[0] = (uint8_t *)buf;
     for (ch = 1; planar && ch < nb_channels; ch++)
         audio_data[ch] = audio_data[ch-1] + line_size;
 
