70d71b58018e640a09a4d919993766a2050cef8d
Justin Ruggles <justin.ruggles@gmail.com>
2012-07-28 12:57:05 -0400

    af_asyncts: add first_pts option
    This allows for padding/trimming at the start of stream. By default, no
    assumption is made about the first frame's expected pts, so no padding or
    trimming is done.

--- libavfilter/af_asyncts.c
+++ libavfilter/af_asyncts.c
@@ -49,6 +49,7 @@ static const AVOption options[] = {
     { "min_delta",  "Minimum difference between timestamps and audio data "
                     "(in seconds) to trigger padding/trimmin the data.",        OFFSET(min_delta_sec), AV_OPT_TYPE_FLOAT, { 0.1 }, 0, INT_MAX, A },
     { "max_comp",   "Maximum compensation in samples per second.",              OFFSET(max_comp),      AV_OPT_TYPE_INT,   { 500 }, 0, INT_MAX, A },
+    { "first_pts",  "Assume the first pts should be this value.",               OFFSET(pts),           AV_OPT_TYPE_INT64, { AV_NOPTS_VALUE }, INT64_MIN, INT64_MAX, A },
     { NULL },
 };
 
@@ -72,8 +73,6 @@ static int init(AVFilterContext *ctx, const char *args)
         return ret;
     }
     av_opt_free(s);
-
-    s->pts = AV_NOPTS_VALUE;
 
     return 0;
 }
