31619584556466e4beab98e9b04ed4c5ba0db178
Michael Niedermayer <michaelni@gmx.at>
2012-07-30 05:08:19 +0200

    yadif: update frame rate
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavfilter/vf_yadif.c
+++ libavfilter/vf_yadif.c
@@ -405,10 +405,15 @@ static int null_draw_slice(AVFilterLink *link, int y, int h, int slice_dir)
 
 static int config_props(AVFilterLink *link)
 {
+    YADIFContext *yadif = link->src->priv;
+
     link->time_base.num = link->src->inputs[0]->time_base.num;
     link->time_base.den = link->src->inputs[0]->time_base.den * 2;
     link->w             = link->src->inputs[0]->w;
     link->h             = link->src->inputs[0]->h;
+
+    if(yadif->mode&1)
+        link->frame_rate = av_mul_q(link->src->inputs[0]->frame_rate, (AVRational){2,1});
 
     return 0;
 }
