74f82f92a4683a431db84b0a4d4197f705ec54ad
Mans Rullgard <mans@mansr.com>
2012-07-31 14:53:57 +0100

    motion_est: remove useless no_motion_search() function
    At both places this function is called, mb_[xy] == s->mb_[xy]
    making the call together with following code equivalent to
    simply assigning zeros.
    
    Signed-off-by: Mans Rullgard <mans@mansr.com>

--- libavcodec/motion_est.c
+++ libavcodec/motion_est.c
@@ -374,13 +374,6 @@ int ff_init_me(MpegEncContext *s){
     return 0;
 }
 
-static inline void no_motion_search(MpegEncContext * s,
-                                    int *mx_ptr, int *my_ptr)
-{
-    *mx_ptr = 16 * s->mb_x;
-    *my_ptr = 16 * s->mb_y;
-}
-
 #define Z_THRESHOLD 256
 
 #define CHECK_SAD_HALF_MV(suffix, x, y) \
@@ -1040,9 +1033,8 @@ void ff_estimate_p_frame_motion(MpegEncContext * s,
     switch(s->me_method) {
     case ME_ZERO:
     default:
-        no_motion_search(s, &mx, &my);
-        mx-= mb_x*16;
-        my-= mb_y*16;
+        mx   = 0;
+        my   = 0;
         dmin = 0;
         break;
     case ME_X1:
@@ -1283,10 +1275,9 @@ static int ff_estimate_motion_b(MpegEncContext * s,
     switch(s->me_method) {
     case ME_ZERO:
     default:
-        no_motion_search(s, &mx, &my);
+        mx   = 0;
+        my   = 0;
         dmin = 0;
-        mx-= mb_x*16;
-        my-= mb_y*16;
         break;
     case ME_X1:
     case ME_EPZS:
