752344dada4eb3004ea3b0e37ba11abee7e4e89a
Nicolas George <nicolas.george@normalesup.org>
2012-08-02 19:28:55 +0200

    amerge/astreamsync/pan: fix license boilerplate.
    The boilerplate states that the files are under LGPL,
    but refer the user to the GPL at one place.
    These files were (re)implemented specifically for FFmpeg.

--- libavfilter/af_pan.c
+++ libavfilter/af_pan.c
@@ -13,7 +13,7 @@
  * FFmpeg is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with FFmpeg; if not, write to the Free Software
