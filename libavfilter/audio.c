800750417ffea64cde827de08e31e6523205f8d1
Reinhard Tartler <siretart@tauware.de>
2012-08-04 15:24:15 +0200

    lavfi: properly signal out-of-memory error in ff_filter_samples
    Found with a clang-scan report on http://fate.libav.org/csa/

--- libavfilter/audio.c
+++ libavfilter/audio.c
@@ -172,6 +172,10 @@ int ff_filter_samples(AVFilterLink *link, AVFilterBufferRef *samplesref)
 
         buf_out = ff_default_get_audio_buffer(link, dst->min_perms,
                                               samplesref->audio->nb_samples);
+        if (!buf_out) {
+            avfilter_unref_buffer(samplesref);
+            return AVERROR(ENOMEM);
+        }
         buf_out->pts                = samplesref->pts;
         buf_out->audio->sample_rate = samplesref->audio->sample_rate;
 
