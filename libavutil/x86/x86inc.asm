7238265052d3b7ec1aa61d6ddc72aa1910c22725
Mans Rullgard <mans@mansr.com>
2012-08-06 00:21:36 +0100

    x86: fix rNmp macros with nasm
    For some reason, nasm requires this.  No harm done to yasm.
    
    Signed-off-by: Mans Rullgard <mans@mansr.com>

--- libavutil/x86/x86inc.asm
+++ libavutil/x86/x86inc.asm
@@ -139,10 +139,10 @@ CPU amdnop
         %define r%1mp %2
     %elif ARCH_X86_64 ; memory
         %define r%1m [rsp + stack_offset + %3]
-        %define r%1mp qword r %+ %1m
+        %define r%1mp qword r %+ %1 %+ m
     %else
         %define r%1m [esp + stack_offset + %3]
-        %define r%1mp dword r %+ %1m
+        %define r%1mp dword r %+ %1 %+ m
     %endif
     %define r%1  %2
 %endmacro
