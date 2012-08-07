29d1df66adb3723d1e7f2d363984b50792fb7c11
Michael Niedermayer <michaelni@gmx.at>
2012-08-07 04:38:44 +0200

    mpegaudiodec: replace assert() by check under #ifdef DEBUG
    The assert can be false with some invalid inputs, the check is
    too expensive to always do though for just a warning message.
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/mpegaudiodec.c
+++ libavcodec/mpegaudiodec.c
@@ -263,7 +263,10 @@ static inline int l3_unscale(int value, int exponent)
     e  = table_4_3_exp  [4 * value + (exponent & 3)];
     m  = table_4_3_value[4 * value + (exponent & 3)];
     e -= exponent >> 2;
-    assert(e >= 1);
+#ifdef DEBUG
+    if(e < 1)
+        av_log(0, AV_LOG_WARNING, "l3_unscale: e is %d\n", e);
+#endif
     if (e > 31)
         return 0;
     m = (m + (1 << (e - 1))) >> e;
