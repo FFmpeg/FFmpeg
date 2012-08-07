e78e6c37efc98189c4c3d89acd3265d7a7ba6d05
Kostya Shishkov <kostya.shishkov@gmail.com>
2012-08-07 19:32:20 +0200

    g723_1: clip argument for 15-bit version of normalize_bits()
    It expects maximum value to be 32767 but calculations in scale_vector()
    which uses this function can give it ABS(-32768) which leads to wrong
    result and thus clipping is needed.

--- libavcodec/g723_1.c
+++ libavcodec/g723_1.c
@@ -290,6 +290,7 @@ static int scale_vector(int16_t *vector, int length)
     for (i = 0; i < length; i++)
         max = FFMAX(max, FFABS(vector[i]));
 
+    max   = FFMIN(max, 0x7FFF);
     bits  = normalize_bits(max, 15);
     scale = (bits == 15) ? 0x7FFF : (1 << bits);
 
