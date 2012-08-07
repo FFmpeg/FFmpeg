122d5c526a43122b1f9ac9bce79e3938c8354e43
Alex Converse <alex.converse@gmail.com>
2012-08-07 12:19:58 -0700

    aacdec: Don't fall back to the old output configuration when no old configuration is present.
    Fixes MP4 files where the first frame is broken.

--- libavcodec/aacdec.c
+++ libavcodec/aacdec.c
@@ -365,7 +365,7 @@ static void push_output_configuration(AACContext *ac) {
  * configuration is unlocked.
  */
 static void pop_output_configuration(AACContext *ac) {
-    if (ac->oc[1].status != OC_LOCKED) {
+    if (ac->oc[1].status != OC_LOCKED && ac->oc[0].status != OC_NONE) {
         ac->oc[1] = ac->oc[0];
         ac->avctx->channels = ac->oc[1].channels;
         ac->avctx->channel_layout = ac->oc[1].channel_layout;
