cb5bb521aa01afd43a6d6b5dfcebb14ab73615c6
Clément Bœsch <ubitux@gmail.com>
2012-08-08 23:10:17 +0200

    lavf/movenc: use two explicit bytes instead of a short.
    This also avoids a pointless cast.
    
    Idea-by: Reimar Döffinger

--- libavformat/movenc.c
+++ libavformat/movenc.c
@@ -3145,12 +3145,12 @@ static int mov_write_subtitle_end_packet(AVFormatContext *s,
                                          int stream_index,
                                          int64_t dts) {
     AVPacket end;
-    short data = 0;
+    uint8_t data[2] = {0};
     int ret;
 
     av_init_packet(&end);
-    end.size = sizeof (short);
-    end.data = (char *)&data;
+    end.size = sizeof(data);
+    end.data = data;
     end.pts = dts;
     end.dts = dts;
     end.duration = 0;
