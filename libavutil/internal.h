d913fd1f00adbbba50c58384906ef40f5ec4850d
Mans Rullgard <mans@mansr.com>
2012-08-08 18:18:14 +0100

    libavutil: drop fallback definitions of INTxx_MIN/MAX
    This list is incomplete (we also use UINT16_MAX), so there does
    not appear to be any system we care about that needs these.
    
    Signed-off-by: Mans Rullgard <mans@mansr.com>

--- libavutil/internal.h
+++ libavutil/internal.h
@@ -52,38 +52,6 @@ struct AVDictionary {
 #endif
 #endif
 
-#ifndef INT16_MIN
-#define INT16_MIN       (-0x7fff - 1)
-#endif
-
-#ifndef INT16_MAX
-#define INT16_MAX       0x7fff
-#endif
-
-#ifndef INT32_MIN
-#define INT32_MIN       (-0x7fffffff - 1)
-#endif
-
-#ifndef INT32_MAX
-#define INT32_MAX       0x7fffffff
-#endif
-
-#ifndef UINT32_MAX
-#define UINT32_MAX      0xffffffff
-#endif
-
-#ifndef INT64_MIN
-#define INT64_MIN       (-0x7fffffffffffffffLL - 1)
-#endif
-
-#ifndef INT64_MAX
-#define INT64_MAX INT64_C(9223372036854775807)
-#endif
-
-#ifndef UINT64_MAX
-#define UINT64_MAX UINT64_C(0xFFFFFFFFFFFFFFFF)
-#endif
-
 #ifndef INT_BIT
 #    define INT_BIT (CHAR_BIT * sizeof(int))
 #endif
