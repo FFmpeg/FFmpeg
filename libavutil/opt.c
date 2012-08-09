a6d6b8a20072a5919d38258dd48cc612e2372f81
Stefano Sabatini <stefasab@gmail.com>
2012-08-09 11:50:58 +0200

    lavu/opt: fix crash in av_opt_next() in case the class has no options

--- libavutil/opt.c
+++ libavutil/opt.c
@@ -59,7 +59,8 @@ const AVOption *av_next_option(void *obj, const AVOption *last)
 const AVOption *av_opt_next(void *obj, const AVOption *last)
 {
     AVClass *class = *(AVClass**)obj;
-    if (!last && class->option[0].name) return class->option;
+    if (!last && class->option && class->option[0].name)
+        return class->option;
     if (last && last[1].name)           return ++last;
     return NULL;
 }
