ab0ea7cb41e698c1d8462441c72c204771418b55
Michael Niedermayer <michaelni@gmx.at>
2012-08-10 16:03:42 +0200

    ffplay: avoid SDL_atoi()
    It appears this function is not available everywhere
    Should fix Ticket1525
    
    Reviewed-by: Marton Balint <cus@passwd.hu>
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- ffplay.c
+++ ffplay.c
@@ -2098,7 +2098,7 @@ static int audio_open(void *opaque, int64_t wanted_channel_layout, int wanted_nb
 
     env = SDL_getenv("SDL_AUDIO_CHANNELS");
     if (env) {
-        wanted_nb_channels = SDL_atoi(env);
+        wanted_nb_channels = atoi(env);
         wanted_channel_layout = av_get_default_channel_layout(wanted_nb_channels);
     }
     if (!wanted_channel_layout || wanted_nb_channels != av_get_channel_layout_nb_channels(wanted_channel_layout)) {
