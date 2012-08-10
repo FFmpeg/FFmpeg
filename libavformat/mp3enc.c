bd6ece4609b49eb40f880252452b809bff8f13ec
Michael Niedermayer <michaelni@gmx.at>
2012-08-11 01:25:19 +0200

    mp3enc: fix CODEC_ID/CodecID that leaked in from older patch
    Found-by: jamal <jamrial@gmail.com>
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/mp3enc.c
+++ libavformat/mp3enc.c
@@ -370,10 +370,10 @@ static int mp2_write_trailer(struct AVFormatContext *s)
     return 0;
 }
 
-static int query_codec(enum CodecID id, int std_compliance)
+static int query_codec(enum AVCodecID id, int std_compliance)
 {
     CodecMime *cm= ff_id3v2_mime_tags;
-    while(cm->id != CODEC_ID_NONE) {
+    while(cm->id != AV_CODEC_ID_NONE) {
         if(id == cm->id)
             return MKTAG('A', 'P', 'I', 'C');
         cm++;
