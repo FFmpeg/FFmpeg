20035fa24103da9199de3515ca75ba1f6bb275aa
Michael Niedermayer <michaelni@gmx.at>
2012-08-13 15:02:21 +0200

    g723_1dec: remove dead code that leaked in from libav
    It appears someone thinks this special case can be reached
    Well, it cannot, thus not only do we not need to optimize it
    we dont need it at all
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/g723_1.c
+++ libavcodec/g723_1.c
@@ -259,12 +259,8 @@ static int scale_vector(int16_t *dst, const int16_t *vector, int length)
 
     bits  = normalize_bits(max, 15);
 
-    if (bits == 15)
-        for (i = 0; i < length; i++)
-            dst[i] = vector[i] * 0x7fff >> 3;
-    else
-        for (i = 0; i < length; i++)
-            dst[i] = vector[i] << bits >> 3;
+    for (i = 0; i < length; i++)
+        dst[i] = vector[i] << bits >> 3;
 
     return bits - 3;
 }
