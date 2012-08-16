64b23d7dec41e0e29604ab52c677ce8f0444070b
Michael Niedermayer <michaelni@gmx.at>
2012-08-16 17:28:37 +0200

    x86/motion_est_mmx: remove libavutil/internal.h include
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/x86/motion_est_mmx.c
+++ libavcodec/x86/motion_est_mmx.c
@@ -22,7 +22,6 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
-#include "libavutil/internal.h"
 #include "libavutil/mem.h"
 #include "libavutil/x86/asm.h"
 #include "libavcodec/dsputil.h"
