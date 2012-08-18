92b8c9d89e7ae86bffa05393bb230d5a165b0148
Nicolas George <nicolas.george@normalesup.org>
2012-08-18 14:12:41 +0200

    lavf/au: check bits per coded sample.
    Without that, it would repeatedly produce empty packets
    if the codec if forced to something invalid.
    
    Fix trac ticket #1652.

--- libavformat/au.c
+++ libavformat/au.c
@@ -181,10 +181,13 @@ static int au_read_packet(AVFormatContext *s,
                           AVPacket *pkt)
 {
     int ret;
+    int bpcs = av_get_bits_per_sample(s->streams[0]->codec->codec_id);
 
+    if (!bpcs)
+        return AVERROR(EINVAL);
     ret= av_get_packet(s->pb, pkt, BLOCK_SIZE *
                        s->streams[0]->codec->channels *
-                       av_get_bits_per_sample(s->streams[0]->codec->codec_id) >> 3);
+                       bpcs >> 3);
     if (ret < 0)
         return ret;
     pkt->flags &= ~AV_PKT_FLAG_CORRUPT;
