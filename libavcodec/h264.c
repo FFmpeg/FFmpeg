FFmpeg/FFmpeg

commit 2e8f3cbcda536970aee03f5543185d7fc01232a1

h264: vdpau: fix crash with unsupported colorspace
The h264_vdpau decoder crashed if output colorspace was not 8-bit 420.
Add a check to error out instead (current hardware does not support
other colorspaces, so successful decoding is not possible).

Signed-off-by: Martin Storsjö <martin@martin.st>

libavcodec/h264.c
@@ -3876,6 +3876,15 @@ again:
 
                 if (avctx->bits_per_raw_sample != h->sps.bit_depth_luma ||
                     h->cur_chroma_format_idc   != h->sps.chroma_format_idc) {
+                    if (s->avctx->codec->capabilities & CODEC_CAP_HWACCEL_VDPAU
+                        && (h->sps.bit_depth_luma != 8 ||
+                            h->sps.chroma_format_idc > 1)) {
+                        av_log(avctx, AV_LOG_ERROR,
+                               "VDPAU decoding does not support video "
+                               "colorspace\n");
+                        buf_index = -1;
+                        goto end;
+                    }
                     if (h->sps.bit_depth_luma >= 8 && h->sps.bit_depth_luma <= 10) {
                         avctx->bits_per_raw_sample = h->sps.bit_depth_luma;
                         h->cur_chroma_format_idc   = h->sps.chroma_format_idc;

commit 6f5b1a2ba4cc568b3b8ae11b7dfd4a70fb891680

h264: Check that the codec isn't null before accessing it
This fixes crashes introduced by 2e8f3cbcda5, the codec can be null
when called from parsers.

Signed-off-by: Martin Storsjö <martin@martin.st>

libavcodec/h264.c
@@ -3876,7 +3876,8 @@ again:
 
                 if (avctx->bits_per_raw_sample != h->sps.bit_depth_luma ||
                     h->cur_chroma_format_idc   != h->sps.chroma_format_idc) {
-                    if (s->avctx->codec->capabilities & CODEC_CAP_HWACCEL_VDPAU
+                    if (s->avctx->codec &&
+                        s->avctx->codec->capabilities & CODEC_CAP_HWACCEL_VDPAU
                         && (h->sps.bit_depth_luma != 8 ||
                             h->sps.chroma_format_idc > 1)) {
                         av_log(avctx, AV_LOG_ERROR,