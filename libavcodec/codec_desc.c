7816c7e7728d5094d52c34652a504c38ebfc4628
Philip Langdale <philipl@overt.org>
2012-08-28 21:02:25 -0700

    Add missing codec descriptor for timingless subrip.
    Signed-off-by: Philip Langdale <philipl@overt.org>

--- libavcodec/codec_desc.c
+++ libavcodec/codec_desc.c
@@ -2052,7 +2052,13 @@ static const AVCodecDescriptor codec_descriptors[] = {
         .id        = AV_CODEC_ID_SRT,
         .type      = AVMEDIA_TYPE_SUBTITLE,
         .name      = "srt",
-        .long_name = NULL_IF_CONFIG_SMALL("SubRip Text"),
+        .long_name = NULL_IF_CONFIG_SMALL("SubRip subtitle with embedded timing"),
+    },
+    {
+        .id        = AV_CODEC_ID_SUBRIP,
+        .type      = AVMEDIA_TYPE_SUBTITLE,
+        .name      = "subrip",
+        .long_name = NULL_IF_CONFIG_SMALL("SubRip subtitle"),
     },
     {
         .id        = AV_CODEC_ID_MICRODVD,
