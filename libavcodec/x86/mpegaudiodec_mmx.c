976f2e0a542e47aaf68ddbe001fb70a00bf96d99
Diego Biurrun <diego@biurrun.de>
2012-08-29 11:14:17 +0200

    x86: Fix linking with some or all of yasm, mmx, optimizations disabled
    Some optimized template functions reference optimized symbols, so they
    must be explicitly disabled when those symbols are unavailable.
    
    (cherry picked from commit ec36aa69448f20a78d8c4588265022e0b2272ab5)
    Signed-off-by: Diego Biurrun <diego@biurrun.de>

--- libavcodec/x86/mpegaudiodec_mmx.c
+++ libavcodec/x86/mpegaudiodec_mmx.c
@@ -160,6 +160,7 @@ static void apply_window_mp3(float *in, float *win, int *unused, float *out,
 }
 
 
+#if HAVE_YASM
 #define DECL_IMDCT_BLOCKS(CPU1, CPU2)                                       \
 static void imdct36_blocks_ ## CPU1(float *out, float *buf, float *in,      \
                                int count, int switch_point, int block_type) \
@@ -197,6 +198,7 @@ DECL_IMDCT_BLOCKS(sse2,sse)
 DECL_IMDCT_BLOCKS(sse3,sse)
 DECL_IMDCT_BLOCKS(ssse3,sse)
 DECL_IMDCT_BLOCKS(avx,avx)
+#endif /* HAVE_YASM */
 
 void ff_mpadsp_init_mmx(MPADSPContext *s)
 {
