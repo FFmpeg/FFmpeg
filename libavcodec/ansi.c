368a768cbcbf9320fcd8cf6c2ecb7f6904837a95
Peter Ross <pross@xvid.org>
2012-09-01 15:15:13 +1000

    ansi: remove misplaced return
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/ansi.c
+++ libavcodec/ansi.c
@@ -372,7 +372,6 @@ static int decode_frame(AVCodecContext *avctx,
             } else {
                 s->state = STATE_NORMAL;
                 draw_char(avctx, 0x1B);
-                    return -1;
                 continue;
             }
             break;
