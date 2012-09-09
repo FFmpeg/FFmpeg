17f9626b5dd86085a155922221f41338bc8bba1f
Michael Niedermayer <michaelni@gmx.at>
2012-09-09 14:41:18 +0200

    tiff/doubles2str: check for truncation
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/tiff.c
+++ libavcodec/tiff.c
@@ -215,7 +215,9 @@ static char *doubles2str(double *dp, int count, const char *sep)
     ap0   = ap;
     ap[0] = '\0';
     for (i = 0; i < count; i++) {
-        int l = snprintf(ap, component_len, "%f%s", dp[i], sep);
+        unsigned l = snprintf(ap, component_len, "%f%s", dp[i], sep);
+        if(l >= component_len)
+            return NULL;
         ap += l;
     }
     ap0[strlen(ap0) - strlen(sep)] = '\0';
