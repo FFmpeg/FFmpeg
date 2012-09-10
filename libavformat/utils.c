b422e2c58a9606c670a0d2709b14fc8e0cb9a028
Michael Niedermayer <michaelni@gmx.at>
2012-09-10 16:15:07 +0200

    compute_pkt_fields: fix harmless integer overflow
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/utils.c
+++ libavformat/utils.c
@@ -1026,7 +1026,7 @@ static void compute_pkt_fields(AVFormatContext *s, AVStream *st,
         pc && pc->pict_type != AV_PICTURE_TYPE_B)
         presentation_delayed = 1;
 
-    if(pkt->pts != AV_NOPTS_VALUE && pkt->dts != AV_NOPTS_VALUE && pkt->dts - (1LL<<(st->pts_wrap_bits-1)) > pkt->pts && st->pts_wrap_bits<63){
+    if(pkt->pts != AV_NOPTS_VALUE && pkt->dts != AV_NOPTS_VALUE && st->pts_wrap_bits<63 && pkt->dts - (1LL<<(st->pts_wrap_bits-1)) > pkt->pts){
         if(is_relative(st->cur_dts) || pkt->dts - (1LL<<(st->pts_wrap_bits-1)) > st->cur_dts) {
             pkt->dts -= 1LL<<st->pts_wrap_bits;
         } else
