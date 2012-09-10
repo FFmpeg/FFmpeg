8ff78578612f5d8b28164445e94e8805a9899daf
Mans Rullgard <mans@mansr.com>
2012-09-10 15:47:00 +0100

    ARM: libavresample: NEON optimised generic fltp to s16 conversion
    Signed-off-by: Mans Rullgard <mans@mansr.com>

--- libavresample/arm/audio_convert_init.c
+++ libavresample/arm/audio_convert_init.c
@@ -26,6 +26,8 @@
 #include "libavresample/audio_convert.h"
 
 void ff_conv_flt_to_s16_neon(int16_t *dst, const float *src, int len);
+void ff_conv_fltp_to_s16_neon(int16_t *dst, float *const *src,
+                              int len, int channels);
 void ff_conv_fltp_to_s16_2ch_neon(int16_t *dst, float *const *src,
                                   int len, int channels);
 
@@ -37,6 +39,9 @@ av_cold void ff_audio_convert_init_arm(AudioConvert *ac)
         ff_audio_convert_set_func(ac, AV_SAMPLE_FMT_S16, AV_SAMPLE_FMT_FLT,
                                   0, 16, 8, "NEON",
                                   ff_conv_flt_to_s16_neon);
+        ff_audio_convert_set_func(ac, AV_SAMPLE_FMT_S16, AV_SAMPLE_FMT_FLTP,
+                                  0, 16, 8, "NEON",
+                                  ff_conv_fltp_to_s16_neon);
         ff_audio_convert_set_func(ac, AV_SAMPLE_FMT_S16, AV_SAMPLE_FMT_FLTP,
                                   2, 16, 8, "NEON",
                                   ff_conv_fltp_to_s16_2ch_neon);
