386d60f9783ac094dae6c3c9210e0469f98c9147
Michael Niedermayer <michaelni@gmx.at>
2012-09-11 01:58:03 +0200

    aacdec/lcg_random: fix harmless integer overflow
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/aacdec.c
+++ libavcodec/aacdec.c
@@ -781,7 +781,7 @@ static int decode_audio_specific_config(AACContext *ac,
  *
  * @return  Returns a 32-bit pseudorandom integer
  */
-static av_always_inline int lcg_random(int previous_val)
+static av_always_inline int lcg_random(unsigned previous_val)
 {
     return previous_val * 1664525 + 1013904223;
 }
