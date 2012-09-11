5a627c14b53c520cda410d2cb1b64dc3158bf024
Derek Buitenhuis <derek.buitenhuis@gmail.com>
2012-09-11 11:50:23 -0400

    Remove mt-work directory
    It is all dead and rotting code.
    
    Also, move yuvcmp.c to tools. Its license is non-existent, however,
    and should be clarified.
    
    Signed-off-by: Derek Buitenhuis <derek.buitenhuis@gmail.com>

--- mt-work/email.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh -v
-
-# args [where to put patches] [smtp server] [destination]
-
-git format-patch -o "$1" --inline --subject-prefix=soc --thread origin
-git send-email --no-chain-reply-to --smtp-server $2 --to $3 --dry-run "$1"
--- mt-work/valgrind-check.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-valgrind --track-origins=yes --leak-check=full ./ffmpeg_g -threads 1 -vsync 0 -y -t 30 -i "$1" -an -f null /dev/null
-
-valgrind --track-origins=yes --leak-check=full ./ffmpeg_g -threads 3 -vsync 0 -y -t 30 -i "$1" -an -f null /dev/null
