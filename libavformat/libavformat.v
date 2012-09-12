bc3d5173cc8f8be338c627fd5800fc042789c2d3
Michael Niedermayer <michaelni@gmx.at>
2012-09-12 17:45:33 +0200

    libavformat.v: export ffurl_protocol_next
    This is used by avidemux, and is likely usefull to others too.
    
    Patch by: gruntster (Avidemux Rev 7990 — 2012-05-30 13:02:27)
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/libavformat.v
+++ libavformat/libavformat.v
@@ -20,6 +20,7 @@ LIBAVFORMAT_$MAJOR {
                 ffurl_seek;
                 ffurl_size;
                 ffurl_write;
+                ffurl_protocol_next;
                 url_open;
                 url_close;
                 url_write;
