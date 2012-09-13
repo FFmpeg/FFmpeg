51fa6538297f26f27a8ea1d9c0c3016f42ee7ecf
Stefano Sabatini <stefasab@gmail.com>
2012-09-13 10:16:38 +0200

    examples/decoding_encoding: clarify operations in the usage message

--- doc/examples/decoding_encoding.c
+++ doc/examples/decoding_encoding.c
@@ -608,6 +608,9 @@ int main(int argc, char **argv)
     if (argc < 2) {
         printf("usage: %s output_type\n"
                "API example program to decode/encode a media stream with libavcodec.\n"
+               "This program generates a synthetic stream and encodes it to a file\n"
+               "named test.h264, test.mp2 or test.mpg depending on output_type.\n"
+               "The encoded stream is then decoded and written to a raw data output\n."
                "output_type must be choosen between 'h264', 'mp2', 'mpg'\n",
                argv[0]);
         return 1;
