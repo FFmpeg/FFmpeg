e3329474a366de066b25e86f35f5abf9c5a4b7b2
Ben Jackson <ben@ben.com>
2012-09-13 21:26:43 -0700

    pthread: Avoid crashes/odd behavior caused by spurious wakeups
    pthread_wait_cond can wake up for no reason (Wikipedia: Spurious_wakeup).
    The FF_THREAD_SLICE thread mechanism could spontaneously execute jobs or
    allow the caller of avctx->execute to return before all jobs were complete.
    This adds tests to both cases to ensure the wakeup is real.
    
    Signed-off-by: Ben Jackson <ben@ben.com>
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/pthread.c
+++ libavcodec/pthread.c
@@ -80,6 +80,7 @@ typedef struct ThreadContext {
     pthread_cond_t current_job_cond;
     pthread_mutex_t current_job_lock;
     int current_job;
+    unsigned int current_execute;
     int done;
 } ThreadContext;
 
@@ -204,6 +205,7 @@ static void* attribute_align_arg worker(void *v)
     AVCodecContext *avctx = v;
     ThreadContext *c = avctx->thread_opaque;
     int our_job = c->job_count;
+    int last_execute = 0;
     int thread_count = avctx->thread_count;
     int self_id;
 
@@ -214,7 +216,9 @@ static void* attribute_align_arg worker(void *v)
             if (c->current_job == thread_count + c->job_count)
                 pthread_cond_signal(&c->last_job_cond);
 
-            pthread_cond_wait(&c->current_job_cond, &c->current_job_lock);
+            while (last_execute == c->current_execute && !c->done)
+                pthread_cond_wait(&c->current_job_cond, &c->current_job_lock);
+            last_execute = c->current_execute;
             our_job = self_id;
 
             if (c->done) {
@@ -234,7 +238,8 @@ static void* attribute_align_arg worker(void *v)
 
 static av_always_inline void avcodec_thread_park_workers(ThreadContext *c, int thread_count)
 {
-    pthread_cond_wait(&c->last_job_cond, &c->current_job_lock);
+    while (c->current_job != thread_count + c->job_count)
+        pthread_cond_wait(&c->last_job_cond, &c->current_job_lock);
     pthread_mutex_unlock(&c->current_job_lock);
 }
 
@@ -283,6 +288,7 @@ static int avcodec_thread_execute(AVCodecContext *avctx, action_func* func, void
         c->rets = &dummy_ret;
         c->rets_count = 1;
     }
+    c->current_execute++;
     pthread_cond_broadcast(&c->current_job_cond);
 
     avcodec_thread_park_workers(c, avctx->thread_count);
