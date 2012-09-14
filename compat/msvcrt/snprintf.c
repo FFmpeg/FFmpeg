663c21d4ac1a011f71fe48a6a8b6934cd2ef1a4b
Ronald S. Bultje <rsbultje@gmail.com>
2012-09-14 16:03:25 -0400

    compat/vsnprintf: return number of bytes required on truncation.
    This conforms to C99, but requires Windows >= XP.

--- compat/msvcrt/snprintf.c
+++ compat/msvcrt/snprintf.c
@@ -43,9 +43,10 @@ int avpriv_vsnprintf(char *s, size_t n, const char *fmt,
                      va_list ap)
 {
     int ret;
+    va_list ap_copy;
 
     if (n == 0)
-        return 0;
+        return _vscprintf(fmt, ap);
     else if (n > INT_MAX)
         return AVERROR(EOVERFLOW);
 
@@ -56,9 +57,11 @@ int avpriv_vsnprintf(char *s, size_t n, const char *fmt,
      * _snprintf/_vsnprintf() to workaround this problem.
      * See http://msdn.microsoft.com/en-us/library/1kt27hek(v=vs.80).aspx */
     memset(s, 0, n);
-    ret = _vsnprintf(s, n - 1, fmt, ap);
+    va_copy(ap_copy, ap);
+    ret = _vsnprintf(s, n - 1, fmt, ap_copy);
+    va_end(ap_copy);
     if (ret == -1)
-        ret = n;
+        ret = _vscprintf(fmt, ap);
 
     return ret;
 }
