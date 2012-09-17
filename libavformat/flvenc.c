6a08955c104450dfc8f56aa9f552a4c08f8d8f7f
Benjamin Larsson <benjamin@southpole.se>
2012-09-17 21:14:58 +0200

    flvenc: silence bogus warning
    The compiler fails to figure out that enc->codec_type can only
    have 3 different values.
    Thus when an if/else is encountered it triggers on the possibility
    of the else case has not initialized the flags variable.
    
    Signed-off-by: Luca Barbato <lu_zero@gentoo.org>

--- libavformat/flvenc.c
+++ libavformat/flvenc.c
@@ -421,7 +421,7 @@ static int flv_write_packet(AVFormatContext *s, AVPacket *pkt)
     unsigned ts;
     int size = pkt->size;
     uint8_t *data = NULL;
-    int flags, flags_size;
+    int flags = 0, flags_size;
 
     // av_log(s, AV_LOG_DEBUG, "type:%d pts: %"PRId64" size:%d\n",
     //        enc->codec_type, timestamp, size);
