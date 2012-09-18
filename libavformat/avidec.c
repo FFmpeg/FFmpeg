8d07742cb1cda0d3aad3908fa57780fe8a659ea9
Michael Niedermayer <michaelni@gmx.at>
2012-09-18 21:47:22 +0200

    avidec: workaround aac with broken block align
    Fixes Ticket1742
    
    Based on patch by: Nikola Vranic <nikola.vranic@rt-rk.com>
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/avidec.c
+++ libavformat/avidec.c
@@ -676,6 +676,10 @@ static int avi_read_header(AVFormatContext *s)
                         st->codec->codec_id  = AV_CODEC_ID_ADPCM_IMA_AMV;
                         ast->dshow_block_align = 0;
                     }
+                    if(st->codec->codec_id == AV_CODEC_ID_AAC && ast->dshow_block_align <= 4 && ast->dshow_block_align) {
+                        av_log(s, AV_LOG_DEBUG, "overriding invalid dshow_block_align of %d\n", ast->dshow_block_align);
+                        ast->dshow_block_align = 0;
+                    }
                     break;
                 case AVMEDIA_TYPE_SUBTITLE:
                     st->codec->codec_type = AVMEDIA_TYPE_SUBTITLE;
