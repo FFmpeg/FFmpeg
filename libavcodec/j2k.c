d1974e05d1d6d1d28049537968bd159b39d609ec
Michael Niedermayer <michaelni@gmx.at>
2012-09-20 02:13:52 +0200

    j2k: fix "static is not at beginning of declaration" warning
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/j2k.c
+++ libavcodec/j2k.c
@@ -252,7 +252,7 @@ int ff_j2k_init_component(J2kComponent *comp, J2kCodingStyle *codsty, J2kQuantSt
             int cblkperprecw, cblkperprech;
 
             if (qntsty->quantsty != J2K_QSTY_NONE){
-                const static uint8_t lut_gain[2][4] = {{0, 0, 0, 0}, {0, 1, 1, 2}};
+                static const uint8_t lut_gain[2][4] = {{0, 0, 0, 0}, {0, 1, 1, 2}};
                 int numbps;
 
                 numbps = cbps + lut_gain[codsty->transform][bandno + reslevelno>0];
