58e90259930312f7b0e9671a0a3820c2c622e291
Stefano Sabatini <stefasab@gmail.com>
2012-09-24 18:21:21 +0200

    ffprobe: reindent after previous commit

--- ffprobe.c
+++ ffprobe.c
@@ -353,16 +353,16 @@ static void writer_print_time(WriterContext *wctx, const char *key,
 {
     char buf[128];
 
-        if ((!is_duration && ts == AV_NOPTS_VALUE) || (is_duration && ts == 0)) {
-            writer_print_string(wctx, key, "N/A", 1);
-        } else {
-            double d = ts * av_q2d(*time_base);
-            struct unit_value uv;
-            uv.val.d = d;
-            uv.unit = unit_second_str;
-            value_string(buf, sizeof(buf), uv);
-            writer_print_string(wctx, key, buf, 0);
-        }
+    if ((!is_duration && ts == AV_NOPTS_VALUE) || (is_duration && ts == 0)) {
+        writer_print_string(wctx, key, "N/A", 1);
+    } else {
+        double d = ts * av_q2d(*time_base);
+        struct unit_value uv;
+        uv.val.d = d;
+        uv.unit = unit_second_str;
+        value_string(buf, sizeof(buf), uv);
+        writer_print_string(wctx, key, buf, 0);
+    }
 }
 
 static void writer_print_ts(WriterContext *wctx, const char *key, int64_t ts, int is_duration)
