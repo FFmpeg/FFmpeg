00e1afd83f7585f4acb65911100d79e32c1f6fc4
Clément Bœsch <clement.boesch@smartjog.com>
2012-09-24 09:36:09 +0200

    fate: add faststart regression test.
    Also factorize the common options for the different mov-based tests.
    
    Since the header is now on top in the last generated file, the data
    offset in the seek test needed some updates as well.

--- tests/lavf-regression.sh
+++ tests/lavf-regression.sh
@@ -107,8 +107,9 @@ do_lavf flv "" "-an"
 fi
 
 if [ -n "$do_mov" ] ; then
-do_lavf mov "" "-movflags +rtphint -acodec pcm_alaw -vcodec mpeg4"
-do_lavf_timecode mov "-acodec pcm_alaw -vcodec mpeg4"
+mov_common_opt="-acodec pcm_alaw -vcodec mpeg4"
+do_lavf mov "" "-movflags +rtphint $mov_common_opt"
+do_lavf_timecode mov "-movflags +faststart $mov_common_opt"
 fi
 
 if [ -n "$do_ismv" ] ; then
