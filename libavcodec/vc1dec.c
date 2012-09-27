aa4121276777b20eaaa83bf9bd544b00748c865c
Kostya Shishkov <kostya.shishkov@gmail.com>
2012-09-27 19:25:06 +0200

    vc1dec: add flush function for WMV9 and VC-1 decoders
    CC: libav-stable@libav.org
    (cherry picked from commit 4dc8c8386eef942dba35c4f2fb3210e22b511a5b)
    
    Signed-off-by: Anton Khirnov <anton@khirnov.net>
    (cherry picked from commit 02b72394627933dc8ce26445231a69f00dba491b)
    
    Conflicts:
    	libavcodec/vc1dec.c
    
    Signed-off-by: Anton Khirnov <anton@khirnov.net>
    (cherry picked from commit 0173a7966b331105158a88f96b9afcc431d2fef8)
    
    Signed-off-by: Anton Khirnov <anton@khirnov.net>

--- libavcodec/vc1dec.c
+++ libavcodec/vc1dec.c
@@ -3328,6 +3328,7 @@ AVCodec vc1_decoder = {
     vc1_decode_frame,
     CODEC_CAP_DR1 | CODEC_CAP_DELAY,
     NULL,
+    .flush          = ff_mpeg_flush,
     .long_name = NULL_IF_CONFIG_SMALL("SMPTE VC-1"),
     .pix_fmts = ff_hwaccel_pixfmt_list_420
 };
@@ -3344,6 +3345,7 @@ AVCodec wmv3_decoder = {
     vc1_decode_frame,
     CODEC_CAP_DR1 | CODEC_CAP_DELAY,
     NULL,
+    .flush          = ff_mpeg_flush,
     .long_name = NULL_IF_CONFIG_SMALL("Windows Media Video 9"),
     .pix_fmts = ff_hwaccel_pixfmt_list_420
 };
