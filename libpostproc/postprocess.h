ec87389183bc4c55327200d5a93779d1db7a1e24
jamal <jamrial@gmail.com>
2012-09-27 11:12:18 -0300

    postproc: Create version.h header
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libpostproc/postprocess.h
+++ libpostproc/postprocess.h
@@ -27,23 +27,7 @@
  *     external postprocessing API
  */
 
-#include "libavutil/avutil.h"
-
-#ifndef LIBPOSTPROC_VERSION_MAJOR
-#define LIBPOSTPROC_VERSION_MAJOR 52
-#define LIBPOSTPROC_VERSION_MINOR  0
-#define LIBPOSTPROC_VERSION_MICRO 100
-#endif
-
-#define LIBPOSTPROC_VERSION_INT AV_VERSION_INT(LIBPOSTPROC_VERSION_MAJOR, \
-                                               LIBPOSTPROC_VERSION_MINOR, \
-                                               LIBPOSTPROC_VERSION_MICRO)
-#define LIBPOSTPROC_VERSION     AV_VERSION(LIBPOSTPROC_VERSION_MAJOR, \
-                                           LIBPOSTPROC_VERSION_MINOR, \
-                                           LIBPOSTPROC_VERSION_MICRO)
-#define LIBPOSTPROC_BUILD       LIBPOSTPROC_VERSION_INT
-
-#define LIBPOSTPROC_IDENT       "postproc" AV_STRINGIFY(LIBPOSTPROC_VERSION)
+#include "libpostproc/version.h"
 
 /**
  * Return the LIBPOSTPROC_VERSION_INT constant.
