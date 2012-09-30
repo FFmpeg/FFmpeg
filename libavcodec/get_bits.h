5f1c3c785c786f3c78c350d4503fcfd794fa3c64
Michael Niedermayer <michaelni@gmx.at>
2012-09-30 02:37:40 +0200

    get_bits_long: fix variable type
    This fixes a theoretical signed overflow
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/get_bits.h
+++ libavcodec/get_bits.h
@@ -306,10 +306,10 @@ static inline unsigned int get_bits_long(GetBitContext *s, int n)
         return get_bits(s, n);
     else {
 #ifdef BITSTREAM_READER_LE
-        int ret = get_bits(s, 16);
+        unsigned ret = get_bits(s, 16);
         return ret | (get_bits(s, n-16) << 16);
 #else
-        int ret = get_bits(s, 16) << (n-16);
+        unsigned ret = get_bits(s, 16) << (n-16);
         return ret | get_bits(s, n-16);
 #endif
     }
