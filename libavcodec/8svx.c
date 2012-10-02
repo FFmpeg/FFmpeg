6eed92a2b7977b7aa91554e9911106462681d242
Michael Niedermayer <michaelni@gmx.at>
2012-10-03 01:52:29 +0200

    8svx: fix delta_decode cliping limits
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/8svx.c
+++ libavcodec/8svx.c
@@ -74,9 +74,9 @@ static int delta_decode(int8_t *dst, const uint8_t *src, int src_size,
 
     while (n--) {
         uint8_t d = *src++;
-        val = av_clip(val + table[d & 0x0f], -127, 128);
+        val = av_clip(val + table[d & 0x0f], -128, 127);
         *dst++ = val;
-        val = av_clip(val + table[d >> 4]  , -127, 128);
+        val = av_clip(val + table[d >> 4]  , -128, 127);
         *dst++ = val;
     }
 
