a854362b40f0e458db5a1fb0d2612a5702ee0ace
Martin Storsjö <martin@martin.st>
2012-10-02 23:56:40 +0200

    segment: Flush buffered data before finishing a segment
    This makes sure any buffered data is written to the segment, for
    muxers that buffer up data internally (e.g. fragmented mp4).
    
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- libavformat/segment.c
+++ libavformat/segment.c
@@ -111,6 +111,7 @@ static int segment_end(AVFormatContext *oc, int write_trailer)
 {
     int ret = 0;
 
+    av_write_frame(oc, NULL); /* Flush any buffered data */
     if (write_trailer)
         av_write_trailer(oc);
     avio_close(oc->pb);
