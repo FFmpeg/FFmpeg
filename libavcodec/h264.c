1e2e2c8095de2d9ea3259305cfeff28f40e4ca12
Clément Bœsch <clement.boesch@smartjog.com>
2012-10-03 15:14:40 +0200

    lavc/h264: move ff_init_cabac_states() from decode_slice() to ff_h264_decode_init().
    This fixes one of the potential races spotted by Helgrind.

--- libavcodec/h264.c
+++ libavcodec/h264.c
@@ -1112,6 +1112,8 @@ av_cold int ff_h264_decode_init(AVCodecContext *avctx)
         s->low_delay           = 0;
     }
 
+    ff_init_cabac_states();
+
     return 0;
 }
 
@@ -3533,7 +3535,6 @@ static int decode_slice(struct AVCodecContext *avctx, void *arg)
         align_get_bits(&s->gb);
 
         /* init cabac */
-        ff_init_cabac_states();
         ff_init_cabac_decoder(&h->cabac,
                               s->gb.buffer + get_bits_count(&s->gb) / 8,
                               (get_bits_left(&s->gb) + 7) / 8);
