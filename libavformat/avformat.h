27a341518e91ca18cbee292725a74a3c23a0d272
Paul B Mahol <onemda@gmail.com>
2012-10-07 17:57:13 +0000

    avformat: fix typo in comment
    Signed-off-by: Paul B Mahol <onemda@gmail.com>

--- libavformat/avformat.h
+++ libavformat/avformat.h
@@ -581,7 +581,7 @@ enum AVStreamParseType {
     AVSTREAM_PARSE_FULL_ONCE,  /**< full parsing and repack of the first frame only, only implemented for H.264 currently */
     AVSTREAM_PARSE_FULL_RAW=MKTAG(0,'R','A','W'),       /**< full parsing and repack with timestamp and position generation by parser for raw
                                                              this assumes that each packet in the file contains no demuxer level headers and
-                                                             just codec level data, otherwise position generaion would fail */
+                                                             just codec level data, otherwise position generation would fail */
 };
 
 typedef struct AVIndexEntry {
