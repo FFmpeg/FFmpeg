c5fdd0696ab5cd4dfe82d83b38a355d24dfa540c
Michael Niedermayer <michaelni@gmx.at>
2012-10-08 03:09:16 +0200

    tiff: fix "assignment discards qualifiers from pointer target type" warning
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/tiff.c
+++ libavcodec/tiff.c
@@ -332,7 +332,7 @@ static int tiff_uncompress(uint8_t *dst, unsigned long *len, const uint8_t *src,
     z_stream zstream = { 0 };
     int zret;
 
-    zstream.next_in = src;
+    zstream.next_in = (uint8_t *)src;
     zstream.avail_in = size;
     zstream.next_out = dst;
     zstream.avail_out = *len;
