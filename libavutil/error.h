1a2c7880aa1ca5893b860e81dd3f0a4c2696de18
Janne Grunau <janne-libav@jannau.net>
2012-10-10 22:25:51 +0200

    averror: make error values proper negative values
    Fixes stupid mistake in 25227c3a78fefd74bc6ea34032cd49ec8c8d9d85.

--- libavutil/error.h
+++ libavutil/error.h
@@ -45,21 +45,21 @@
 #define AVUNERROR(e) (e)
 #endif
 
-#define AVERROR_BSF_NOT_FOUND      0xb9acbd08 ///< Bitstream filter not found
-#define AVERROR_DECODER_NOT_FOUND  0xbcbabb08 ///< Decoder not found
-#define AVERROR_DEMUXER_NOT_FOUND  0xb2babb08 ///< Demuxer not found
-#define AVERROR_ENCODER_NOT_FOUND  0xbcb1ba08 ///< Encoder not found
-#define AVERROR_EOF                0xdfb9b0bb ///< End of file
-#define AVERROR_EXIT               0xabb6a7bb ///< Immediate exit was requested; the called function should not be restarted
-#define AVERROR_FILTER_NOT_FOUND   0xb3b6b908 ///< Filter not found
-#define AVERROR_INVALIDDATA        0xbebbb1b7 ///< Invalid data found when processing input
-#define AVERROR_MUXER_NOT_FOUND    0xa7aab208 ///< Muxer not found
-#define AVERROR_OPTION_NOT_FOUND   0xabafb008 ///< Option not found
-#define AVERROR_PATCHWELCOME       0xbaa8beb0 ///< Not yet implemented in Libav, patches welcome
-#define AVERROR_PROTOCOL_NOT_FOUND 0xb0adaf08 ///< Protocol not found
-#define AVERROR_STREAM_NOT_FOUND   0xadabac08 ///< Stream not found
-#define AVERROR_BUG                0xdfb8aabe ///< Bug detected, please report the issue
-#define AVERROR_UNKNOWN            0xb1b4b1ab ///< Unknown error, typically from an external library
+#define AVERROR_BSF_NOT_FOUND      (-0x39acbd08) ///< Bitstream filter not found
+#define AVERROR_DECODER_NOT_FOUND  (-0x3cbabb08) ///< Decoder not found
+#define AVERROR_DEMUXER_NOT_FOUND  (-0x32babb08) ///< Demuxer not found
+#define AVERROR_ENCODER_NOT_FOUND  (-0x3cb1ba08) ///< Encoder not found
+#define AVERROR_EOF                (-0x5fb9b0bb) ///< End of file
+#define AVERROR_EXIT               (-0x2bb6a7bb) ///< Immediate exit was requested; the called function should not be restarted
+#define AVERROR_FILTER_NOT_FOUND   (-0x33b6b908) ///< Filter not found
+#define AVERROR_INVALIDDATA        (-0x3ebbb1b7) ///< Invalid data found when processing input
+#define AVERROR_MUXER_NOT_FOUND    (-0x27aab208) ///< Muxer not found
+#define AVERROR_OPTION_NOT_FOUND   (-0x2bafb008) ///< Option not found
+#define AVERROR_PATCHWELCOME       (-0x3aa8beb0) ///< Not yet implemented in Libav, patches welcome
+#define AVERROR_PROTOCOL_NOT_FOUND (-0x30adaf08) ///< Protocol not found
+#define AVERROR_STREAM_NOT_FOUND   (-0x2dabac08) ///< Stream not found
+#define AVERROR_BUG                (-0x5fb8aabe) ///< Bug detected, please report the issue
+#define AVERROR_UNKNOWN            (-0x31b4b1ab) ///< Unknown error, typically from an external library
 
 /**
  * Put a description of the AVERROR code errnum in errbuf.
