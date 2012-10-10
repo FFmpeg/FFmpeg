1a104bf641b1f5e278dd1cbf29853921785aa797
Carl Eugen Hoyos <cehoyos@ag.or.at>
2012-10-10 23:59:43 +0200

    Fix broken timestamps for some mp3 in avi samples.
    Mostly based on 73ad355 by Michael Niedermayer.
    Fixes ticket #606.

--- libavformat/avidec.c
+++ libavformat/avidec.c
@@ -681,7 +681,8 @@ static int avi_read_header(AVFormatContext *s)
                         av_log(s, AV_LOG_DEBUG, "overriding invalid dshow_block_align of %d\n", ast->dshow_block_align);
                         ast->dshow_block_align = 0;
                     }
-                    if(st->codec->codec_id == AV_CODEC_ID_AAC && ast->dshow_block_align == 1024 && ast->sample_size == 1024) {
+                    if(st->codec->codec_id == AV_CODEC_ID_AAC && ast->dshow_block_align == 1024 && ast->sample_size == 1024 ||
+                       st->codec->codec_id == AV_CODEC_ID_MP3 && ast->dshow_block_align == 1152 && ast->sample_size == 1152) {
                         av_log(s, AV_LOG_DEBUG, "overriding sample_size\n");
                         ast->sample_size = 0;
                     }
