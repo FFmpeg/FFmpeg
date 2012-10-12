46c1e5de58ac8cbe0f222e2649cf6bd6dd768012
Paul B Mahol <onemda@gmail.com>
2012-10-12 14:23:01 +0000

    yop: check return value of avformat_new_stream()
    Fixes null pointer dereference, fixes CID703729.
    
    Signed-off-by: Paul B Mahol <onemda@gmail.com>
    (cherry picked from commit 3d179edf6d2a987e7eb134eea541954338a19add)
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/yop.c
+++ libavformat/yop.c
@@ -59,6 +59,8 @@ static int yop_read_header(AVFormatContext *s, AVFormatParameters *ap)
 
     audio_stream = avformat_new_stream(s, NULL);
     video_stream = avformat_new_stream(s, NULL);
+    if (!audio_stream || !video_stream)
+        return AVERROR(ENOMEM);
 
     // Extra data that will be passed to the decoder
     video_stream->codec->extradata_size = 8;
