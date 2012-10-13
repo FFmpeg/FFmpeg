1037e484f0f1c45ab0a398c78985d3b91daa410c
Michael Niedermayer <michaelni@gmx.at>
2012-10-13 22:36:15 +0200

    dnxhddata_ Fix mixup of sizeof() and array elements in ff_dnxhd_find_cid()
    Fixes CID717910
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/dnxhddata.c
+++ libavcodec/dnxhddata.c
@@ -1038,7 +1038,7 @@ int ff_dnxhd_find_cid(AVCodecContext *avctx, int bit_depth)
         if (cid->width == avctx->width && cid->height == avctx->height &&
             cid->interlaced == !!(avctx->flags & CODEC_FLAG_INTERLACED_DCT) &&
             cid->bit_depth == bit_depth) {
-            for (j = 0; j < sizeof(cid->bit_rates); j++) {
+            for (j = 0; j < FF_ARRAY_ELEMS(cid->bit_rates); j++) {
                 if (cid->bit_rates[j] == mbs)
                     return cid->cid;
             }
