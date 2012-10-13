80db07adfe8a1ff716af51ca9aaf357280c8b363
Michael Niedermayer <michaelni@gmx.at>
2012-10-13 19:58:44 +0200

    probetest: check command line arguments
    Fixes CID733835
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- tools/probetest.c
+++ tools/probetest.c
@@ -63,6 +63,16 @@ int main(int argc, char **argv)
     if(argc >= 3)
         max_size = atoi(argv[2]);
 
+    if (max_size > 1000000000U/8) {
+        fprintf(stderr, "max_size out of bounds\n");
+        return 1;
+    }
+
+    if (retry_count > 1000000000U) {
+        fprintf(stderr, "retry_count out of bounds\n");
+        return 1;
+    }
+
     avcodec_register_all();
     av_register_all();
 
