a726ac9a11b44782bddba23e179984b738fcc6b7
Stefano Sabatini <stefasab@gmail.com>
2012-10-15 15:42:16 +0200

    examples/muxing: extend usage notice

--- doc/examples/muxing.c
+++ doc/examples/muxing.c
@@ -431,8 +431,10 @@ int main(int argc, char **argv)
     if (argc != 2) {
         printf("usage: %s output_file\n"
                "API example program to output a media file with libavformat.\n"
+               "This program generates a synthetic audio and video stream, encodes and\n"
+               "muxes them into a file named output_file.\n"
                "The output format is automatically guessed according to the file extension.\n"
-               "Raw images can also be output by using '%%d' in the filename\n"
+               "Raw images can also be output by using '%%d' in the filename.\n"
                "\n", argv[0]);
         return 1;
     }
