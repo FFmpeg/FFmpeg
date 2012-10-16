5d2b8850746b4513a43938f60930b060cad36ee5
Michael Niedermayer <michaelni@gmx.at>
2012-10-16 23:58:26 +0200

    lavfi: limit matching w/h/fmt asserts to non scale filters
    This fixes a regression with the scale filters input changing.
    In the long run filters should get a flag to indicate support of this
    and then this flag be used here.
    But the regression should not be left standing until thats done.
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavfilter/video.c
+++ libavfilter/video.c
@@ -249,9 +249,11 @@ int ff_start_frame(AVFilterLink *link, AVFilterBufferRef *picref)
 
     FF_TPRINTF_START(NULL, start_frame); ff_tlog_link(NULL, link, 0); ff_tlog(NULL, " "); ff_tlog_ref(NULL, picref, 1);
 
-    av_assert1(picref->format                     == link->format);
-    av_assert1(picref->video->w                   == link->w);
-    av_assert1(picref->video->h                   == link->h);
+    if (strcmp(link->dst->filter->name, "scale")) {
+        av_assert1(picref->format                     == link->format);
+        av_assert1(picref->video->w                   == link->w);
+        av_assert1(picref->video->h                   == link->h);
+    }
 
     if (link->closed) {
         avfilter_unref_buffer(picref);
