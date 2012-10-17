71bc8c95d7cac552d3a2cb3120e41207dfb48e50
Michael Niedermayer <michaelni@gmx.at>
2012-10-17 20:24:47 +0200

    ffm_read_write_index: check lseek return code
    Fixes CID732184
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- ffserver.c
+++ ffserver.c
@@ -333,7 +333,8 @@ static int64_t ffm_read_write_index(int fd)
 {
     uint8_t buf[8];
 
-    lseek(fd, 8, SEEK_SET);
+    if (lseek(fd, 8, SEEK_SET) < 0)
+        return AVERROR(EIO);
     if (read(fd, buf, 8) != 8)
         return AVERROR(EIO);
     return AV_RB64(buf);
