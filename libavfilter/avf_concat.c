dbe57f7e9f08a45cbdb51327fdf3cd892febdac2
Nicolas George <nicolas.george@normalesup.org>
2012-10-17 19:54:22 +0200

    lavfi/avf_concat: fix invalid exclusive test.
    The invalid test did not cause any actual problem since
    the first branch is only possible with bogus filters.
    
    Fix coverity issue CID 733850.
    (cherry picked from commit 709628aa71f24520553eb10b0cf6d56784e6c3ec)
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavfilter/avf_concat.c
+++ libavfilter/avf_concat.c
@@ -195,7 +195,7 @@ static void process_frame(AVFilterLink *inlink, AVFilterBufferRef *buf)
         av_log(ctx, AV_LOG_ERROR, "Frame after EOF on input %s\n",
                ctx->input_pads[in_no].name);
         avfilter_unref_buffer(buf);
-    } if (in_no >= cat->cur_idx + ctx->nb_outputs) {
+    } else if (in_no >= cat->cur_idx + ctx->nb_outputs) {
         ff_bufqueue_add(ctx, &cat->in[in_no].queue, buf);
     } else {
         push_frame(ctx, in_no, buf);
