935ecfb002385697d57573975f95c401eba68d64
Stefano Sabatini <stefasab@gmail.com>
2012-10-20 11:53:22 +0200

    examples/scaling_video: remove unnecessary intermediary variable in fill_yuv_frame()

--- doc/examples/scaling_video.c
+++ doc/examples/scaling_video.c
@@ -32,20 +32,18 @@
 static void fill_yuv_image(uint8_t *data[4], int linesize[4],
                            int width, int height, int frame_index)
 {
-    int x, y, i;
-
-    i = frame_index;
+    int x, y;
 
     /* Y */
     for (y = 0; y < height; y++)
         for (x = 0; x < width; x++)
-            data[0][y * linesize[0] + x] = x + y + i * 3;
+            data[0][y * linesize[0] + x] = x + y + frame_index * 3;
 
     /* Cb and Cr */
     for (y = 0; y < height / 2; y++) {
         for (x = 0; x < width / 2; x++) {
-            data[1][y * linesize[1] + x] = 128 + y + i * 2;
-            data[2][y * linesize[2] + x] = 64 + x + i * 5;
+            data[1][y * linesize[1] + x] = 128 + y + frame_index * 2;
+            data[2][y * linesize[2] + x] = 64 + x + frame_index * 5;
         }
     }
 }
