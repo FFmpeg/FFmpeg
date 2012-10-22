b6e36a424447413d959753f6e703f35bcfb8cd7b
Stefano Sabatini <stefasab@gmail.com>
2012-10-22 10:24:04 +0200

    lavu/parseutils: remove unused gcd variable in av_parse_ratio()
    Fix warning.

--- libavutil/parseutils.c
+++ libavutil/parseutils.c
@@ -47,7 +47,6 @@ int av_parse_ratio(AVRational *q, const char *str, int max,
 {
     char c;
     int ret;
-    int64_t gcd;
 
     if (sscanf(str, "%d:%d%c", &q->num, &q->den, &c) != 2) {
         double d;
