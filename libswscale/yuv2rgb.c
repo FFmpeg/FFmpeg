cccb45751e93142d71be78f6bb90bbfb50ee13be
Michael Niedermayer <michaelni@gmx.at>
2012-10-24 01:24:26 +0200

    yuv2rgb: fix integer overflow in fill_gv_table()
    Fixes CID703747
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libswscale/yuv2rgb.c
+++ libswscale/yuv2rgb.c
@@ -680,7 +680,7 @@ static void fill_table(uint8_t* table[256 + 2*YUVRGB_TABLE_HEADROOM], const int 
     }
 }
 
-static void fill_gv_table(int table[256 + 2*YUVRGB_TABLE_HEADROOM], const int elemsize, const int inc)
+static void fill_gv_table(int table[256 + 2*YUVRGB_TABLE_HEADROOM], const int elemsize, const int64_t inc)
 {
     int i;
     int off    = -(inc >> 9);
