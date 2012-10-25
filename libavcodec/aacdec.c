da4e4d65f456f170e75865807d21a8b5ed5ea4d7
Michael Niedermayer <michaelni@gmx.at>
2012-10-25 13:42:02 +0200

    aacdec: reorder multiuplications to make code safer against too large input values.
    Fixes CID700697
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/aacdec.c
+++ libavcodec/aacdec.c
@@ -1242,7 +1242,7 @@ static void decode_mid_side_stereo(ChannelElement *cpe, GetBitContext *gb,
         for (idx = 0; idx < cpe->ch[0].ics.num_window_groups * cpe->ch[0].ics.max_sfb; idx++)
             cpe->ms_mask[idx] = get_bits1(gb);
     } else if (ms_present == 2) {
-        memset(cpe->ms_mask, 1, cpe->ch[0].ics.num_window_groups * cpe->ch[0].ics.max_sfb * sizeof(cpe->ms_mask[0]));
+        memset(cpe->ms_mask, 1,  sizeof(cpe->ms_mask[0]) * cpe->ch[0].ics.num_window_groups * cpe->ch[0].ics.max_sfb);
     }
 }
 
