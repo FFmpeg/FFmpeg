cc72d52dc1edc2224ba1c1db777f8efa614f0cbb
Martin Ettl <ettl.martin78@googlemail.com>
2012-10-25 16:34:32 +0000

    ffserver: fix printf argument type

--- ffserver.c
+++ ffserver.c
@@ -801,7 +801,7 @@ static void http_send_too_busy_reply(int fd)
                        "\r\n"
                        "<html><head><title>Too busy</title></head><body>\r\n"
                        "<p>The server is too busy to serve your request at this time.</p>\r\n"
-                       "<p>The number of current connections is %d, and this exceeds the limit of %d.</p>\r\n"
+                       "<p>The number of current connections is %u, and this exceeds the limit of %u.</p>\r\n"
                        "</body></html>\r\n",
                        nb_connections, nb_max_connections);
     av_assert0(len < sizeof(buffer));
