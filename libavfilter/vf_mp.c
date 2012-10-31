29a9e4d5c97e50114436b93339a93440fb3e2122
Stefano Sabatini <stefasab@gmail.com>
2012-10-31 14:39:55 +0100

    lavfi/mp: drop the mp=rectangle filter
    All the rectangle features have been ported to the native drawbox filter,
    which has an equivalent syntax.

--- libavfilter/vf_mp.c
+++ libavfilter/vf_mp.c
@@ -163,7 +163,6 @@ extern const vf_info_t vf_info_pp7;
 extern const vf_info_t vf_info_pp;
 extern const vf_info_t vf_info_pullup;
 extern const vf_info_t vf_info_qp;
-extern const vf_info_t vf_info_rectangle;
 extern const vf_info_t vf_info_sab;
 extern const vf_info_t vf_info_scale;
 extern const vf_info_t vf_info_softpulldown;
@@ -211,7 +210,6 @@ static const vf_info_t* const filters[]={
     &vf_info_pp7,
     &vf_info_pullup,
     &vf_info_qp,
-    &vf_info_rectangle,
     &vf_info_sab,
     &vf_info_softpulldown,
     &vf_info_softskip,
