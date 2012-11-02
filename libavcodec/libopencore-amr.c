0aaf0a07d490fba6e0262959d5b0520aeb5cb4c3
Michael Niedermayer <michaelni@gmx.at>
2012-11-02 23:46:47 +0100

    libopencore-amrdec: allow sampling rates other than 8khz
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/libopencore-amr.c
+++ libavcodec/libopencore-amr.c
@@ -31,7 +31,8 @@ static void amr_decode_fix_avctx(AVCodecContext *avctx)
 {
     const int is_amr_wb = 1 + (avctx->codec_id == AV_CODEC_ID_AMR_WB);
 
-    avctx->sample_rate = 8000 * is_amr_wb;
+    if (!avctx->sample_rate)
+        avctx->sample_rate = 8000 * is_amr_wb;
 
     if (avctx->channels > 1) {
         av_log_missing_feature(avctx, "multi-channel AMR", 0);
