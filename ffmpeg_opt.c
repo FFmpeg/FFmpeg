a62242678ff96eade59960d1bbf65e4f3f03344f
Stefano Sabatini <stefasab@gmail.com>
2012-11-02 12:57:15 +0100

    ffmpeg: complain loudly and exit in case removed -sameq option is specified
    This is better than silently ignoring the option, which results in
    unexpected quality results.

--- ffmpeg_opt.c
+++ ffmpeg_opt.c
@@ -159,8 +159,11 @@ static int opt_pad(void *optctx, const char *opt, const char *arg)
 
 static int opt_sameq(void *optctx, const char *opt, const char *arg)
 {
-    av_log(NULL, AV_LOG_WARNING, "Ignoring option '%s'\n", opt);
-    return 0;
+    av_log(NULL, AV_LOG_ERROR, "Option '%s' was removed. "
+           "If you are looking for an option to preserve the quality (which is not "
+           "what -%s was for), use -qscale 0 or an equivalent quality factor option.\n",
+           opt, opt);
+    return AVERROR(EINVAL);
 }
 
 static int opt_video_channel(void *optctx, const char *opt, const char *arg)
