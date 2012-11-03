5f531386210e844f0a6819404cde58da54c46a00
Michael Niedermayer <michaelni@gmx.at>
2012-11-04 00:56:57 +0100

    movenc: remove unneeded check
    Fixes CID741417
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/movenc.c
+++ libavformat/movenc.c
@@ -2037,7 +2037,7 @@ static int mov_write_int8_metadata(AVFormatContext *s, AVIOContext *pb,
 
     if (!(t = av_dict_get(s->metadata, tag, NULL, 0)))
         return 0;
-    num = t ? atoi(t->value) : 0;
+    num = atoi(t->value);
 
     avio_wb32(pb, len+8);
     ffio_wfourcc(pb, name);
