a0348d0966a81a66f3a1bf061576b24d5296b933
Paul B Mahol <onemda@gmail.com>
2012-11-04 23:17:12 +0000

    pnm: remove nonsense code
    If maxval is more than 255 than each value takes 2 bytes with most
    significant byte first.
    
    Signed-off-by: Paul B Mahol <onemda@gmail.com>

--- libavcodec/pnm.c
+++ libavcodec/pnm.c
@@ -163,8 +163,6 @@ int ff_pnm_decode_header(AVCodecContext *avctx, PNMContext * const s)
         if (s->maxval >= 256) {
             if (avctx->pix_fmt == AV_PIX_FMT_GRAY8) {
                 avctx->pix_fmt = AV_PIX_FMT_GRAY16BE;
-                if (s->maxval != 65535)
-                    avctx->pix_fmt = AV_PIX_FMT_GRAY16;
             } else if (avctx->pix_fmt == AV_PIX_FMT_RGB24) {
                 if (s->maxval > 255)
                     avctx->pix_fmt = AV_PIX_FMT_RGB48BE;
