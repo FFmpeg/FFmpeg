29d46d7bce1c67852e4c6e22605144eb32b21072
Stefano Sabatini <stefasab@gmail.com>
2012-11-06 22:59:24 +0100

    ffprobe: fix potential NULL pointer dereference
    Found by Coverity, should fix CID 733741.

--- ffprobe.c
+++ ffprobe.c
@@ -1095,13 +1095,13 @@ static void json_print_section_header(WriterContext *wctx)
         json->indent_level++;
         if (section->flags & SECTION_FLAG_IS_ARRAY) {
             printf("\"%s\": [\n", buf.str);
-        } else if (!(parent_section->flags & SECTION_FLAG_IS_ARRAY)) {
+        } else if (parent_section && !(parent_section->flags & SECTION_FLAG_IS_ARRAY)) {
             printf("\"%s\": {%s", buf.str, json->item_start_end);
         } else {
             printf("{%s", json->item_start_end);
 
             /* this is required so the parser can distinguish between packets and frames */
-            if (parent_section->id == SECTION_ID_PACKETS_AND_FRAMES) {
+            if (parent_section && parent_section->id == SECTION_ID_PACKETS_AND_FRAMES) {
                 if (!json->compact)
                     JSON_INDENT();
                 printf("\"type\": \"%s\"%s", section->name, json->item_sep);
