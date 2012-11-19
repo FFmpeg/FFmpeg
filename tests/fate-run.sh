4a753fc5f77300450b3f0706c3abdc5d9f7fe315
Stefano Sabatini <stefasab@gmail.com>
2012-11-20 00:21:23 +0100

    tests: prefer ffprobe -show_entries to -show_format_entry
    The latter option was deprecated.

--- tests/fate-run.sh
+++ tests/fate-run.sh
@@ -71,7 +71,7 @@ run(){
 }
 
 probefmt(){
-    run ffprobe -show_format_entry format_name -print_format default=nw=1:nk=1 -v 0 "$@"
+    run ffprobe -show_entries format=format_name -print_format default=nw=1:nk=1 -v 0 "$@"
 }
 
 ffmpeg(){
