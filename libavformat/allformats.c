9a31997938c94a6809519d82bfa74cf32dce99ae
Paul B Mahol <onemda@gmail.com>
2012-11-23 11:20:11 +0000

    BRSTM demuxer
    Signed-off-by: Paul B Mahol <onemda@gmail.com>

--- libavformat/allformats.c
+++ libavformat/allformats.c
@@ -79,6 +79,7 @@ void av_register_all(void)
     REGISTER_DEMUXER  (BINK, bink);
     REGISTER_MUXDEMUX (BIT, bit);
     REGISTER_DEMUXER  (BMV, bmv);
+    REGISTER_DEMUXER  (BRSTM, brstm);
     REGISTER_DEMUXER  (C93, c93);
     REGISTER_MUXDEMUX (CAF, caf);
     REGISTER_MUXDEMUX (CAVSVIDEO, cavsvideo);
