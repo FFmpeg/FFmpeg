51e9f58e1c9c7e012ed7d10ade6a609221d346ef
Stefano Sabatini <stefasab@gmail.com>
2012-11-25 17:34:18 +0100

    lavu/opt: add support for reading pixel and sample format through av_get_int()
    Simplify backward compatibility, when switching from AV_OPT_TYPE_INT to
    AV_OPT_TYPE_SAMPLE/PIXEL_FMT.

--- libavutil/opt.c
+++ libavutil/opt.c
@@ -71,6 +71,8 @@ static int read_number(const AVOption *o, void *dst, double *num, int *den, int6
 {
     switch (o->type) {
     case AV_OPT_TYPE_FLAGS:     *intnum = *(unsigned int*)dst;return 0;
+    case AV_OPT_TYPE_PIXEL_FMT:
+    case AV_OPT_TYPE_SAMPLE_FMT:
     case AV_OPT_TYPE_INT:       *intnum = *(int         *)dst;return 0;
     case AV_OPT_TYPE_INT64:     *intnum = *(int64_t     *)dst;return 0;
     case AV_OPT_TYPE_FLOAT:     *num    = *(float       *)dst;return 0;
