4f50646697606df39317b93c2a427603b77636ee
Christophe Gisquet <christophe.gisquet@gmail.com>
2012-11-25 07:55:28 +0100

    x86: sbrdsp: Implement SSE qmf_post_shuffle
    255 to 174 cycles on Arrandale / Win64. Unrolling yields no gain.
    
    Signed-off-by: Diego Biurrun <diego@biurrun.de>

--- libavcodec/x86/sbrdsp_init.c
+++ libavcodec/x86/sbrdsp_init.c
@@ -31,6 +31,7 @@ void ff_sbr_hf_g_filt_sse(float (*Y)[2], const float (*X_high)[40][2],
 void ff_sbr_hf_gen_sse(float (*X_high)[2], const float (*X_low)[2],
                        const float alpha0[2], const float alpha1[2],
                        float bw, int start, int end);
+void ff_sbr_qmf_post_shuffle_sse(float W[32][2], const float *z);
 
 void ff_sbrdsp_init_x86(SBRDSPContext *s)
 {
@@ -41,5 +42,6 @@ void ff_sbrdsp_init_x86(SBRDSPContext *s)
         s->sum64x5    = ff_sbr_sum64x5_sse;
         s->hf_g_filt  = ff_sbr_hf_g_filt_sse;
         s->hf_gen     = ff_sbr_hf_gen_sse;
+        s->qmf_post_shuffle = ff_sbr_qmf_post_shuffle_sse;
     }
 }
