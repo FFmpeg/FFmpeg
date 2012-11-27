10c8f913410e7e13dc055da826c6928af8f2987f
Paul B Mahol <onemda@gmail.com>
2012-11-27 10:12:28 +0000

    astdec: improve probing
    Signed-off-by: Paul B Mahol <onemda@gmail.com>

--- libavformat/ast.c
+++ libavformat/ast.c
@@ -26,8 +26,11 @@
 
 static int ast_probe(AVProbeData *p)
 {
-    if (AV_RL32(p->buf) == MKTAG('S', 'T', 'R', 'M'))
-        return AVPROBE_SCORE_MAX / 2;
+    if (AV_RL32(p->buf) == MKTAG('S','T','R','M') &&
+        AV_RB16(p->buf + 10) &&
+        AV_RB16(p->buf + 12) &&
+        AV_RB32(p->buf + 16))
+        return AVPROBE_SCORE_MAX / 3 * 2;
     return 0;
 }
 
