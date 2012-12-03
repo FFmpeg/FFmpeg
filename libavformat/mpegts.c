03290ffb57b25734120024e6943c7feca7bf2060
Michael Niedermayer <michaelni@gmx.at>
2012-12-03 17:34:38 +0100

    mpegts: check that codec is not open in mpegts_find_stream_type
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/mpegts.c
+++ libavformat/mpegts.c
@@ -598,6 +598,11 @@ static const StreamType DESC_types[] = {
 static void mpegts_find_stream_type(AVStream *st,
                                     uint32_t stream_type, const StreamType *types)
 {
+    if (avcodec_is_open(st->codec)) {
+        av_log(NULL, AV_LOG_DEBUG, "cannot set stream info, codec is open\n");
+        return;
+    }
+
     for (; types->stream_type; types++) {
         if (stream_type == types->stream_type) {
             st->codec->codec_type = types->codec_type;
