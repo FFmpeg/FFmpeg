dd8a76ab080d6862ec8e182e805794ebb27be8b0
Clément Bœsch <ubitux@gmail.com>
2012-12-05 04:58:03 +0100

    lavfi/gradfun: do not increment DC pointer for odd values.
    First DC is only used once otherwise. This also makes the code
    consistent with ASM versions.

--- libavfilter/vf_gradfun.c
+++ libavfilter/vf_gradfun.c
@@ -56,7 +56,7 @@ DECLARE_ALIGNED(16, static const uint16_t, dither)[8][8] = {
 void ff_gradfun_filter_line_c(uint8_t *dst, const uint8_t *src, const uint16_t *dc, int width, int thresh, const uint16_t *dithers)
 {
     int x;
-    for (x = 0; x < width; x++, dc += x & 1) {
+    for (x = 0; x < width; dc += x & 1, x++) {
         int pix = src[x] << 7;
         int delta = dc[0] - pix;
         int m = abs(delta) * thresh >> 16;
