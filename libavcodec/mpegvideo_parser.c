928346091a2eaa3cda72bf348d29712afee0f73a
Wolfram Gloger <wmglo@dent.med.uni-muenchen.de>
2012-12-05 19:26:12 +0100

    mpegvideo_parser: fix buffer access beyond end
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/mpegvideo_parser.c
+++ libavcodec/mpegvideo_parser.c
@@ -56,6 +56,7 @@ static void mpegvideo_extract_headers(AVCodecParserContext *s,
         case PICTURE_START_CODE:
             if (bytes_left >= 2) {
                 s->pict_type = (buf[1] >> 3) & 7;
+                if (bytes_left >= 4)
                 vbv_delay = ((buf[1] & 0x07) << 13) | (buf[2] << 5) | (buf[3]  >> 3);
             }
             break;
