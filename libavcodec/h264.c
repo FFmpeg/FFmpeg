FFmpeg/FFmpeg

commit 0b300daad2f5cb59a7c06dde5ac701685e6edf16

h264: error out on unset current_picture_ptr for h->current_slice > 0
Fixes a segfault with fuzzed sample sample_varPAR_s11622_r001-02.avi.

CC: libav-stable@libav.org

libavcodec/h264.c
@@ -2624,6 +2624,11 @@ static int decode_slice_header(H264Context *h, H264Context *h0)
             s->picture_structure = last_pic_structure;
             s->dropable          = last_pic_dropable;
             return AVERROR_INVALIDDATA;
+        } else if (!s->current_picture_ptr) {
+            av_log(s->avctx, AV_LOG_ERROR,
+                   "unset current_picture_ptr on %d. slice\n",
+                   h0->current_slice + 1);
+            return AVERROR_INVALIDDATA;
         }
     } else {
         /* Shorten frame num gaps so we don't have to allocate reference

commit 5945c7b35d9169caf9ecef1c419eebdebb909e60

h264: slice-mt: check master context for valid current_picture_ptr
Fixes errors in slice based multithreading introduced in 0b300daad2f5.

CC: libav-stable@libav.org

libavcodec/h264.c
@@ -2624,7 +2624,7 @@ static int decode_slice_header(H264Context *h, H264Context *h0)
             s->picture_structure = last_pic_structure;
             s->dropable          = last_pic_dropable;
             return AVERROR_INVALIDDATA;
-        } else if (!s->current_picture_ptr) {
+        } else if (!s0->current_picture_ptr) {
             av_log(s->avctx, AV_LOG_ERROR,
                    "unset current_picture_ptr on %d. slice\n",
                    h0->current_slice + 1);