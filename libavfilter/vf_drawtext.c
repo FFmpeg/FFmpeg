3942294fd595942dbc04b912ea6e2bbbd07464b8
Stefano Sabatini <stefasab@gmail.com>
2012-12-06 13:48:00 +0100

    lavfi/drawtext: make use of outlink variable in filter_frame()
    Fix warning.

--- libavfilter/vf_drawtext.c
+++ libavfilter/vf_drawtext.c
@@ -1001,7 +1001,7 @@ static int filter_frame(AVFilterLink *inlink, AVFilterBufferRef *frame)
 
     dtext->var_values[VAR_N] += 1.0;
 
-    return ff_filter_frame(inlink->dst->outputs[0], frame);
+    return ff_filter_frame(outlink, frame);
 }
 
 static const AVFilterPad avfilter_vf_drawtext_inputs[] = {
