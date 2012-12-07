8ca8b43d7108b9d7e6887fd553d13a71afa670c6
Paul B Mahol <onemda@gmail.com>
2012-12-07 13:05:43 +0000

    lavf/pcm: check size, do not produce invalid packets
    Inspired by 92b8c9d89e7ae86bffa05393bb230d5a165b0148.
    
    Signed-off-by: Paul B Mahol <onemda@gmail.com>

--- libavformat/pcm.c
+++ libavformat/pcm.c
@@ -30,6 +30,8 @@ int ff_pcm_read_packet(AVFormatContext *s, AVPacket *pkt)
     int ret, size;
 
     size= RAW_SAMPLES*s->streams[0]->codec->block_align;
+    if (size <= 0)
+        return AVERROR(EINVAL);
 
     ret= av_get_packet(s->pb, pkt, size);
 
