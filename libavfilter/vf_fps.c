4b4f16cdc00029dbb380f91a05de80fb1fcd57d7
Clément Bœsch <ubitux@gmail.com>
2012-12-11 18:14:08 +0100

    lavfi/fps: remove parse opt error message.
    See cc650cf0 for more info.

--- libavfilter/vf_fps.c
+++ libavfilter/vf_fps.c
@@ -80,11 +80,8 @@ static av_cold int init(AVFilterContext *ctx, const char *args)
     s->class = &fps_class;
     av_opt_set_defaults(s);
 
-    if ((ret = av_opt_set_from_string(s, args, shorthand, "=", ":")) < 0) {
-        av_log(ctx, AV_LOG_ERROR, "Error parsing the options string %s.\n",
-               args);
+    if ((ret = av_opt_set_from_string(s, args, shorthand, "=", ":")) < 0)
         return ret;
-    }
 
     if ((ret = av_parse_video_rate(&s->framerate, s->fps)) < 0) {
         av_log(ctx, AV_LOG_ERROR, "Error parsing framerate %s.\n", s->fps);
