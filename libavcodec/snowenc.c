5cb080fa5e198303a14a537c9388855ccd0a669b
Michael Niedermayer <michaelni@gmx.at>
2012-12-13 14:26:03 +0100

    snowenc: check ff_get_buffer() return value
    Fixes CID747722
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/snowenc.c
+++ libavcodec/snowenc.c
@@ -239,7 +239,8 @@ static av_cold int encode_init(AVCodecContext *avctx)
     ff_set_cmp(&s->dsp, s->dsp.me_cmp, s->avctx->me_cmp);
     ff_set_cmp(&s->dsp, s->dsp.me_sub_cmp, s->avctx->me_sub_cmp);
 
-    ff_get_buffer(s->avctx, &s->input_picture);
+    if ((ret = ff_get_buffer(s->avctx, &s->input_picture)) < 0)
+        return ret;
 
     if(s->avctx->me_method == ME_ITER){
         int i;
