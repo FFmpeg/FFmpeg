0213d5ad86d7ec26363b10895a96a3701c4e7fa8
Michael Niedermayer <michaelni@gmx.at>
2012-12-13 03:04:49 +0100

    alsdec: make return checking for read_*_block_data() consistent
    Reviewed-by: Thilo Borgmann <thilo.borgmann@googlemail.com>
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/alsdec.c
+++ libavcodec/alsdec.c
@@ -974,7 +974,7 @@ static int read_block(ALSDecContext *ctx, ALSBlockData *bd)
     *bd->shift_lsbs = 0;
     // read block type flag and read the samples accordingly
     if (get_bits1(gb)) {
-        if (read_var_block_data(ctx, bd))
+        if (read_var_block_data(ctx, bd) < 0)
             return -1;
     } else {
         if (read_const_block_data(ctx, bd) < 0)
