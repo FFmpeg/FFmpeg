1e901ffc619459944ae7102428f48972cd899caa
Michael Niedermayer <michaelni@gmx.at>
2012-12-16 01:07:53 +0100

    wrap_timestamp: remove unneeded check
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/utils.c
+++ libavformat/utils.c
@@ -87,7 +87,7 @@ static int is_relative(int64_t ts) {
  */
 static int64_t wrap_timestamp(AVStream *st, int64_t timestamp)
 {
-    if (st->pts_wrap_behavior != AV_PTS_WRAP_IGNORE && st->pts_wrap_bits < 64 &&
+    if (st->pts_wrap_behavior != AV_PTS_WRAP_IGNORE &&
         st->pts_wrap_reference != AV_NOPTS_VALUE && timestamp != AV_NOPTS_VALUE) {
         if (st->pts_wrap_behavior == AV_PTS_WRAP_ADD_OFFSET &&
             timestamp < st->pts_wrap_reference)
