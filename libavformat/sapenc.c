2f7465b5bf6ca6aff5a3f0dd671695e86972538c
Jernej Virag <jernej@virag.si>
2012-12-17 10:04:15 +0100

    SAP muxer set session name from metadata
    Pass "title" metadata field to av_sdp_create (as in RTP muxer) in SAP
    muxer for correct
    session name in SAP SDP announcements.
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/sapenc.c
+++ libavformat/sapenc.c
@@ -25,6 +25,7 @@
 #include "libavutil/avstring.h"
 #include "libavutil/intreadwrite.h"
 #include "libavutil/time.h"
+#include "libavutil/dict.h"
 #include "internal.h"
 #include "network.h"
 #include "os_support.h"
@@ -76,6 +77,7 @@ static int sap_write_header(AVFormatContext *s)
     struct sockaddr_storage localaddr;
     socklen_t addrlen = sizeof(localaddr);
     int udp_fd;
+    AVDictionaryEntry* title = av_dict_get(s->metadata, "title", NULL, 0);
 
     if (!ff_network_init())
         return AVERROR(EIO);
@@ -156,6 +158,10 @@ static int sap_write_header(AVFormatContext *s)
             goto fail;
         s->streams[i]->priv_data = contexts[i];
         av_strlcpy(contexts[i]->filename, url, sizeof(contexts[i]->filename));
+    }
+
+    if (s->nb_streams > 0 && title) {
+        av_dict_set(&contexts[0]->metadata, "title", title->value, 0);
     }
 
     ff_url_join(url, sizeof(url), "udp", NULL, announce_addr, port,
