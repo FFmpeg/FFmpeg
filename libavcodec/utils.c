ec4a084581f78fa758803c6e128376d49b66a735
Carl Eugen Hoyos <cehoyos@ag.or.at>
2012-12-19 10:01:07 +0100

    Show underscores in av_get_codec_tag_string().

--- libavcodec/utils.c
+++ libavcodec/utils.c
@@ -2014,7 +2014,7 @@ size_t av_get_codec_tag_string(char *buf, size_t buf_size, unsigned int codec_ta
 #define IS_PRINT(x)                                               \
     (((x) >= '0' && (x) <= '9') ||                                \
      ((x) >= 'a' && (x) <= 'z') || ((x) >= 'A' && (x) <= 'Z') ||  \
-     ((x) == '.' || (x) == ' ' || (x) == '-'))
+     ((x) == '.' || (x) == ' ' || (x) == '-' || (x) == '_'))
 
     for (i = 0; i < 4; i++) {
         len = snprintf(buf, buf_size,
