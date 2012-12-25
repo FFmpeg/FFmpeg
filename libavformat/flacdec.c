4fcf6aa7a3a204af3abcdc2aea2bd6247e6baf6f
Michael Niedermayer <michaelni@gmx.at>
2012-12-25 01:43:14 +0100

    flac_probe: make buffer pointers const
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/flacdec.c
+++ libavformat/flacdec.c
@@ -277,8 +277,8 @@ fail:
 
 static int flac_probe(AVProbeData *p)
 {
-    uint8_t *bufptr = p->buf;
-    uint8_t *end    = p->buf + p->buf_size;
+    const uint8_t *bufptr = p->buf;
+    const uint8_t *end    = p->buf + p->buf_size;
 
     if(bufptr > end-4 || memcmp(bufptr, "fLaC", 4)) return 0;
     else                                            return AVPROBE_SCORE_MAX/2;
