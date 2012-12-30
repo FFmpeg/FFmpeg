33f28a3be3092f642778253d9529dd66fe2a014a
Martin Storsjö <martin@martin.st>
2012-12-30 22:38:23 +0200

    rtmp: Add a function for writing AMF strings based on two substrings
    This avoids having to concatenate them into one buffer before writing
    them as AMF.
    
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- libavformat/rtmppkt.c
+++ libavformat/rtmppkt.c
@@ -47,6 +47,19 @@ void ff_amf_write_string(uint8_t **dst, const char *str)
     bytestream_put_buffer(dst, str, strlen(str));
 }
 
+void ff_amf_write_string2(uint8_t **dst, const char *str1, const char *str2)
+{
+    int len1 = 0, len2 = 0;
+    if (str1)
+        len1 = strlen(str1);
+    if (str2)
+        len2 = strlen(str2);
+    bytestream_put_byte(dst, AMF_DATA_TYPE_STRING);
+    bytestream_put_be16(dst, len1 + len2);
+    bytestream_put_buffer(dst, str1, len1);
+    bytestream_put_buffer(dst, str2, len2);
+}
+
 void ff_amf_write_null(uint8_t **dst)
 {
     bytestream_put_byte(dst, AMF_DATA_TYPE_NULL);
