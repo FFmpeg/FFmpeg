2d8d63a9a07c8dca3587bd2a7b3ac56b2cbbeb24
Michael Niedermayer <michaelni@gmx.at>
2012-12-31 18:30:03 +0100

    ffplay: fix sws_flags
    Fixes Ticket45
    
    Reviewed-by: Marton Balint <cus@passwd.hu>
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- ffplay.c
+++ ffplay.c
@@ -1753,6 +1753,7 @@ static int configure_video_filters(AVFilterGraph *graph, VideoState *is, const c
     if (!buffersink_params)
         return AVERROR(ENOMEM);
 
+    av_opt_get_int(sws_opts, "sws_flags", 0, &sws_flags);
     snprintf(sws_flags_str, sizeof(sws_flags_str), "flags=%"PRId64, sws_flags);
     graph->scale_sws_opts = av_strdup(sws_flags_str);
 
