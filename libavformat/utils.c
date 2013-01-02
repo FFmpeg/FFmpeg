ff08767817c2af07321ffc4fa35274da3d163d81
Michael Niedermayer <michaelni@gmx.at>
2013-01-02 01:15:58 +0100

    lavf: Fix codec id forcing with probed codecs
    Fixes Ticket2088
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>
    (cherry picked from commit 341e40f1e1eeba1a2952c26363630596193e887b)

--- libavformat/utils.c
+++ libavformat/utils.c
@@ -731,6 +731,7 @@ no_packet:
                 }else
                     av_log(s, AV_LOG_WARNING, "probed stream %d failed\n", st->index);
             }
+            force_codec_ids(s, st);
         }
     }
 }
