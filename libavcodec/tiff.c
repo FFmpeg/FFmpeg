52334f5be2ef31a5c836f689f3ab95b25bad026a
Michael Niedermayer <michaelni@gmx.at>
2013-01-03 04:25:55 +0100

    tiff: print error for old style LZW
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/tiff.c
+++ libavcodec/tiff.c
@@ -462,6 +462,9 @@ static int tiff_unpack_strip(TiffContext *s, uint8_t *dst, int stride,
             src = s->deinvert_buf;
             ssrc = src;
         }
+        if (size > 1 && !src[0] && (src[1]&1)) {
+            av_log(s->avctx, AV_LOG_ERROR, "Old style LZW is unsupported\n");
+        }
         if (ff_lzw_decode_init(s->lzw, 8, src, size, FF_LZW_TIFF) < 0) {
             av_log(s->avctx, AV_LOG_ERROR, "Error initializing LZW decoder\n");
             return -1;
