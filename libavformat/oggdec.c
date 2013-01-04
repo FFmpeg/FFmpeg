ee6b868ac835266ebdc10291f5485ace7769bb2e
Luca Barbato <lu_zero@gentoo.org>
2013-01-04 16:05:51 +0100

    oggdec: make sure the private parse data is cleaned up
    Related to CVE-2012-2882
    
    (cherry picked from commit d894f74762bc95310ba23f804b7ba8dffc8f6646)
    
    Conflicts:
    
    	libavformat/oggdec.h
    	libavformat/oggparsevorbis.c
    (cherry picked from commit b0240165d93d4a08d15d244953219a4d4e725d3f)
    
    Signed-off-by: Reinhard Tartler <siretart@tauware.de>

--- libavformat/oggdec.c
+++ libavformat/oggdec.c
@@ -499,6 +499,10 @@ static int ogg_read_close(AVFormatContext *s)
 
     for (i = 0; i < ogg->nstreams; i++) {
         av_free(ogg->streams[i].buf);
+        if (ogg->streams[i].codec &&
+            ogg->streams[i].codec->cleanup) {
+            ogg->streams[i].codec->cleanup(s, i);
+        }
         av_free(ogg->streams[i].private);
     }
     av_free(ogg->streams);
