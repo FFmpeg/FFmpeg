8b48e2c654cd75ef960d71fedecdce1e1175b8ed
Piotr Bandurski <ami_stuff@o2.pl>
2013-01-06 01:56:23 +0100

    tiffdec: Use the correct height field.
    Fixes Ticket913
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>
    (cherry picked from commit 4784a135b2b0fe4d1b4c6256bd37265fc45aed3d)
    
    Conflicts:
    	libavcodec/tiff.c

--- libavcodec/tiff.c
+++ libavcodec/tiff.c
@@ -436,7 +436,7 @@ static int tiff_decode_tag(TiffContext *s, const uint8_t *start, const uint8_t *
         break;
     case TIFF_ROWSPERSTRIP:
         if(type == TIFF_LONG && value == -1)
-            value = s->avctx->height;
+            value = s->height;
         if(value < 1){
             av_log(s->avctx, AV_LOG_ERROR, "Incorrect value of rows per strip\n");
             return -1;
