9c80ed836a511293f4cc3a858060969d32f2b1ce
Martin Storsjö <martin@martin.st>
2013-01-07 22:02:07 +0200

    rtpdec_vp8: Avoid a warning about a possibly unused variable
    The warning is a false positive, but I prefer actually initializing
    it over masking it with av_uninit, since the code is not performance
    critical.
    
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- libavformat/rtpdec_vp8.c
+++ libavformat/rtpdec_vp8.c
@@ -70,7 +70,7 @@ static int vp8_handle_packet(AVFormatContext *ctx, PayloadContext *vp8,
     int extended_bits, part_id;
     int pictureid_present = 0, tl0picidx_present = 0, tid_present = 0,
         keyidx_present = 0;
-    int pictureid = -1, pictureid_mask;
+    int pictureid = -1, pictureid_mask = 0;
     int returned_old_frame = 0;
     uint32_t old_timestamp;
 
