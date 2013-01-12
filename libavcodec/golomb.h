fd165ace7b2791af75c74652364d47957d4ea932
Michael Niedermayer <michaelni@gmx.at>
2013-01-12 22:14:25 +0100

    golomb: check log validity before shifting
    Fixes invalid right shift in fate-cavs
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/golomb.h
+++ libavcodec/golomb.h
@@ -66,10 +66,14 @@ static inline int get_ue_golomb(GetBitContext *gb){
         return ff_ue_golomb_vlc_code[buf];
     }else{
         log= 2*av_log2(buf) - 31;
-        buf>>= log;
-        buf--;
         LAST_SKIP_BITS(re, gb, 32 - log);
         CLOSE_READER(re, gb);
+        if (CONFIG_FTRAPV && log < 0) {
+            av_log(0, AV_LOG_ERROR, "Invalid UE golomb code\n");
+            return AVERROR_INVALIDDATA;
+        }
+        buf>>= log;
+        buf--;
 
         return buf;
     }
