68ff7d265f17d9475d8e648d55d04ea9d1d41cdb
Michael Niedermayer <michaelni@gmx.at>
2013-01-12 17:27:35 +0100

    swr: use local variable for ns_errors
    71 -> 63 kcycles
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libswresample/dither_template.c
+++ libswresample/dither_template.c
@@ -33,14 +33,15 @@ void RENAME(swri_noise_shaping)(SwrContext *s, AudioData *dsts, const AudioData 
         const float *noise = ((const float *)noises->ch[ch]) + s->dither.noise_pos;
         const DELEM *src = (const DELEM*)srcs->ch[ch];
         DELEM *dst = (DELEM*)dsts->ch[ch];
+        float *ns_errors = s->dither.ns_errors[ch];
         pos  = s->dither.ns_pos;
         for (i=0; i<count; i++) {
             double d1, d = src[i]*S_1;
             for(j=0; j<taps; j++)
-                d -= s->dither.ns_coeffs[j] * s->dither.ns_errors[ch][pos + j];
+                d -= s->dither.ns_coeffs[j] * ns_errors[pos + j];
             pos = pos ? pos - 1 : taps - 1;
             d1 = rint(d + noise[i]);
-            s->dither.ns_errors[ch][pos + taps] = s->dither.ns_errors[ch][pos] = d1 - d;
+            ns_errors[pos + taps] = ns_errors[pos] = d1 - d;
             d1 *= S;
             CLIP(d1);
             dst[i] = d1;
