6f3bc92c294b4ce903047e1fd43d71e59c86337b
Michael Niedermayer <michaelni@gmx.at>
2013-01-14 16:23:39 +0100

    init_get_bits8: zero pointers & struct on error
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>
    (cherry picked from commit 153fad14e5a2f85637aa6c254ced0fc1c68974e2)
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/get_bits.h
+++ libavcodec/get_bits.h
@@ -407,7 +407,7 @@ static inline int init_get_bits8(GetBitContext *s, const uint8_t *buffer,
                                  int byte_size)
 {
     if (byte_size > INT_MAX / 8 || byte_size < 0)
-        return AVERROR_INVALIDDATA;
+        byte_size = -1;
     return init_get_bits(s, buffer, byte_size * 8);
 }
 
