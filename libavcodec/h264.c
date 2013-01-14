ea382767ad2191acbe97e90624059723e15f0e4b
Anton Khirnov <anton@khirnov.net>
2013-01-14 20:07:53 +0100

    h264: fix ff_generate_sliding_window_mmcos() prototype.
    It's been returning an error value since
    bad446e251405dc250c3cbee199072e083a1e4b9
    
    Also check for the errors it returns.

--- libavcodec/h264.c
+++ libavcodec/h264.c
@@ -2904,7 +2904,9 @@ static int decode_slice_header(H264Context *h, H264Context *h0)
             s->current_picture_ptr->frame_num = h->prev_frame_num;
             ff_thread_report_progress(&s->current_picture_ptr->f, INT_MAX, 0);
             ff_thread_report_progress(&s->current_picture_ptr->f, INT_MAX, 1);
-            ff_generate_sliding_window_mmcos(h, 1);
+            if ((ret = ff_generate_sliding_window_mmcos(h, 1)) < 0 &&
+                s->avctx->err_recognition & AV_EF_EXPLODE)
+                return ret;
             if (ff_h264_execute_ref_pic_marking(h, h->mmco, h->mmco_index) < 0 &&
                 (s->avctx->err_recognition & AV_EF_EXPLODE))
                 return AVERROR_INVALIDDATA;
