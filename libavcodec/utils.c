b75ac7c71cc89bd162b330ab95f5009ab6af46d8
Michael Niedermayer <michaelni@gmx.at>
2013-01-17 18:51:04 +0100

    lavc: include timebase in avcodec string at debug level.
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/utils.c
+++ libavcodec/utils.c
@@ -2120,6 +2120,15 @@ void avcodec_string(char *buf, int buf_size, AVCodecContext *enc, int encode)
                      ", %s", av_get_sample_fmt_name(enc->sample_fmt));
         }
         break;
+    case AVMEDIA_TYPE_DATA:
+        if (av_log_get_level() >= AV_LOG_DEBUG) {
+            int g = av_gcd(enc->time_base.num, enc->time_base.den);
+            if (g)
+                snprintf(buf + strlen(buf), buf_size - strlen(buf),
+                         ", %d/%d",
+                         enc->time_base.num / g, enc->time_base.den / g);
+        }
+        break;
     default:
         return;
     }
