a717fa84ede90b7323db2a64f9309da31b0d378f
Jason <jason@cpcweb.com>
2013-01-22 12:31:46 -0500

    lavu/timecode: Allow drop frame mode for 60000/1001 fps
    Reviewed-by: Matthieu Bouron <matthieu.bouron@gmail.com>
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavutil/timecode.c
+++ libavutil/timecode.c
@@ -152,8 +152,8 @@ static int check_timecode(void *log_ctx, AVTimecode *tc)
         av_log(log_ctx, AV_LOG_ERROR, "Timecode frame rate must be specified\n");
         return AVERROR(EINVAL);
     }
-    if ((tc->flags & AV_TIMECODE_FLAG_DROPFRAME) && tc->fps != 30) {
-        av_log(log_ctx, AV_LOG_ERROR, "Drop frame is only allowed with 30000/1001 FPS\n");
+    if ((tc->flags & AV_TIMECODE_FLAG_DROPFRAME) && tc->fps != 30 && tc->fps != 60) {
+        av_log(log_ctx, AV_LOG_ERROR, "Drop frame is only allowed with 30000/1001 or 60000/1001 FPS\n");
         return AVERROR(EINVAL);
     }
     if (check_fps(tc->fps) < 0) {
