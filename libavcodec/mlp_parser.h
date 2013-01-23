e67491a2a49aba7675e87713a3c9751ece14386e
Michael Niedermayer <michaelni@gmx.at>
2013-01-23 14:54:40 +0100

    Merge commit '99ccd2ba10eac2b282c272ad9e75f082123c765a'
    * commit '99ccd2ba10eac2b282c272ad9e75f082123c765a':
      mlp: store the channel layout for each substream.
    
    Conflicts:
    	libavcodec/mlp_parser.c
    	libavcodec/mlpdec.c
    
    (cherry picked from commit fa36270c4c68589882cfeae68a037f1d191231c2)
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/mlp_parser.h
+++ libavcodec/mlp_parser.h
@@ -39,9 +39,12 @@ typedef struct MLPHeaderInfo
     int group1_samplerate;      ///< Sample rate of first substream
     int group2_samplerate;      ///< Sample rate of second substream (MLP only)
 
-    int channels_mlp;           ///< Channel arrangement for MLP streams
-    int channels_thd_stream1;   ///< Channel arrangement for substream 1 of TrueHD streams (5.1)
-    int channels_thd_stream2;   ///< Channel arrangement for substream 2 of TrueHD streams (7.1)
+    int channels_mlp;                       ///< Channel count for MLP streams
+    int channels_thd_stream1;               ///< Channel count for substream 1 of TrueHD streams ("6-channel presentation")
+    int channels_thd_stream2;               ///< Channel count for substream 2 of TrueHD streams ("8-channel presentation")
+    uint64_t channel_layout_mlp;            ///< Channel layout for MLP streams
+    uint64_t channel_layout_thd_stream1;    ///< Channel layout for substream 1 of TrueHD streams ("6-channel presentation")
+    uint64_t channel_layout_thd_stream2;    ///< Channel layout for substream 2 of TrueHD streams ("8-channel presentation")
 
     int access_unit_size;       ///< Number of samples per coded frame
     int access_unit_size_pow2;  ///< Next power of two above number of samples per frame
