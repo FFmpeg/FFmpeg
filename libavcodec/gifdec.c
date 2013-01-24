9547034f9120187e23ad76424dd4d70247e62212
Michael Niedermayer <michaelni@gmx.at>
2013-01-24 23:11:10 +0100

    gifdec: gif_copy_img_rect: Fix end pointer
    Fixes out of array accesses
    
    Found-by: Mateusz "j00ru" Jurczyk and Gynvael Coldwind
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>
    (cherry picked from commit c10350358da58600884292c08a8690289b81de29)
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/gifdec.c
+++ libavcodec/gifdec.c
@@ -118,7 +118,7 @@ static void gif_copy_img_rect(const uint32_t *src, uint32_t *dst,
     const uint32_t *src_px, *src_pr,
                    *src_py = src + y_start,
                    *dst_py = dst + y_start;
-    const uint32_t *src_pb = src_py + t * linesize;
+    const uint32_t *src_pb = src_py + h * linesize;
     uint32_t *dst_px;
 
     for (; src_py < src_pb; src_py += linesize, dst_py += linesize) {
