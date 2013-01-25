f1412c799732e0a9bcba191f8eb23e3d14b7cece
Paul B Mahol <onemda@gmail.com>
2013-01-25 11:51:16 +0000

    lavc/gifdec: use memcpy()
    Signed-off-by: Paul B Mahol <onemda@gmail.com>

--- libavcodec/gifdec.c
+++ libavcodec/gifdec.c
@@ -116,7 +116,7 @@ static void gif_copy_img_rect(const uint32_t *src, uint32_t *dst,
                               int linesize, int l, int t, int w, int h)
 {
     const int y_start = t * linesize;
-    const uint32_t *src_px, *src_pr,
+    const uint32_t *src_px,
                    *src_py = src + y_start,
                    *dst_py = dst + y_start;
     const uint32_t *src_pb = src_py + h * linesize;
@@ -125,10 +125,8 @@ static void gif_copy_img_rect(const uint32_t *src, uint32_t *dst,
     for (; src_py < src_pb; src_py += linesize, dst_py += linesize) {
         src_px = src_py + l;
         dst_px = (uint32_t *)dst_py + l;
-        src_pr = src_px + w;
 
-        for (; src_px < src_pr; src_px++, dst_px++)
-            *dst_px = *src_px;
+        memcpy(dst_px, src_px, w * sizeof(uint32_t));
     }
 }
 
