6ed9fc44badb256bdf235e700702bee46a9f6527
Anton Khirnov <anton@khirnov.net>
2013-01-26 21:10:54 +0100

    svq1: replace struct svq1_frame_size with an array.
    It is used as an array in svq1enc, so this is more correct.

--- libavcodec/svq1.c
+++ libavcodec/svq1.c
@@ -37,7 +37,7 @@
 #include "svq1_vlc.h"
 
 /* standard video sizes */
-const struct svq1_frame_size ff_svq1_frame_size_table[7] = {
+const uint16_t ff_svq1_frame_size_table[7][2] = {
     { 160, 120 }, { 128,  96 }, { 176, 144 }, { 352, 288 },
     { 704, 576 }, { 240, 180 }, { 320, 240 }
 };
