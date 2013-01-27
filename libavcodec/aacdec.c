e6ac11e41734cbb24ec5e6c73264d030e5f07a64
Michael Niedermayer <michaelni@gmx.at>
2013-01-27 20:37:27 +0100

    aacdec: check channel count
    Prevent out of array accesses
    
    Found-by: Mateusz "j00ru" Jurczyk and Gynvael Coldwind
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>
    (cherry picked from commit 96f452ac647dae33c53c242ef3266b65a9beafb6)
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/aacdec.c
+++ libavcodec/aacdec.c
@@ -568,6 +568,11 @@ static av_cold int aac_decode_init(AVCodecContext *avctx)
         output_scale_factor = 1.0;
     }
 
+    if (avctx->channels > MAX_CHANNELS) {
+        av_log(avctx, AV_LOG_ERROR, "Too many channels\n");
+        return AVERROR_INVALIDDATA;
+    }
+
     AAC_INIT_VLC_STATIC( 0, 304);
     AAC_INIT_VLC_STATIC( 1, 270);
     AAC_INIT_VLC_STATIC( 2, 550);
