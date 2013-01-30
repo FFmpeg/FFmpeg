32de2831036ae0ecdfa7bfdb3d59c1ae2095bbc8
Michael Niedermayer <michaelni@gmx.at>
2013-01-30 16:54:15 +0100

    avstring: fix "warning: return discards const qualifier from pointer target type"
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavutil/avstring.c
+++ libavutil/avstring.c
@@ -69,11 +69,11 @@ char *av_strnstr(const char *haystack, const char *needle, size_t hay_length)
 {
     size_t needle_len = strlen(needle);
     if (!needle_len)
-        return haystack;
+        return (char*)haystack;
     while (hay_length >= needle_len) {
         hay_length--;
         if (!memcmp(haystack, needle, needle_len))
-            return haystack;
+            return (char*)haystack;
         haystack++;
     }
     return NULL;
