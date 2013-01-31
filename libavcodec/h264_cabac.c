07db7a0dd8239e255c08800162eb45b82c2c49fe
Michael Niedermayer <michaelni@gmx.at>
2013-01-31 04:20:24 +0100

    h264_cabac: Break infinite loops
    This fixes out of array reads and/or infinite loops.
    
    30 is the maximum number of bits that can be read into
    coeff_abs below.
    
    CC: libav-stable@libav.org
    Found-by: Mateusz "j00ru" Jurczyk and Gynvael Coldwind
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- libavcodec/h264_cabac.c
+++ libavcodec/h264_cabac.c
@@ -1711,7 +1711,7 @@ decode_cabac_residual_internal(H264Context *h, int16_t *block,
 \
             if( coeff_abs >= 15 ) { \
                 int j = 0; \
-                while( get_cabac_bypass( CC ) ) { \
+                while (get_cabac_bypass(CC) && j < 30) { \
                     j++; \
                 } \
 \
