48870853b2b771c5e0a3b57ae7c8d91d7b0867f0
Michael Niedermayer <michaelni@gmx.at>
2013-02-07 15:00:29 +0100

    x86/dsputil: Fix author attribution after code has been moved/splited around
    Reference:
    commit 3615e2be846f6382aa42360d02d16b25f443af9f
    Author: Michael Niedermayer <michaelni@gmx.at>
    Date:   Tue Dec 2 22:02:57 2003 +0000
    
        h263_h_loop_filter_mmx
    
        Originally committed as revision 2553 to svn://svn.ffmpeg.org/ffmpeg/trunk
    
    commit 359f98ded95ec65f7109b25f3cfaa9189c4fab9c
    Author: Michael Niedermayer <michaelni@gmx.at>
    Date:   Tue Dec 2 20:28:10 2003 +0000
    
        h263_v_loop_filter_mmx
    
        Originally committed as revision 2552 to svn://svn.ffmpeg.org/ffmpeg/trunk
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/x86/dsputil.asm
+++ libavcodec/x86/dsputil.asm
@@ -1,6 +1,8 @@
 ;******************************************************************************
 ;* MMX optimized DSP utils
 ;* Copyright (c) 2008 Loren Merritt
+;* Copyright (c) 2003-2013 Michael Niedermayer
+;* Copyright (c) 2013 Daniel Kang
 ;*
 ;* This file is part of FFmpeg.
 ;*
