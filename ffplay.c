4be7d578a3cf03a77a277c78d6cf1420f87a7593
Marton Balint <cus@passwd.hu>
2013-02-16 20:21:13 +0100

    ffplay: signal seek event to read thread
    Improves seek delay.
    
    Signed-off-by: Marton Balint <cus@passwd.hu>

--- ffplay.c
+++ ffplay.c
@@ -1184,6 +1184,7 @@ static void stream_seek(VideoState *is, int64_t pos, int64_t rel, int seek_by_by
         if (seek_by_bytes)
             is->seek_flags |= AVSEEK_FLAG_BYTE;
         is->seek_req = 1;
+        SDL_CondSignal(is->continue_read_thread);
     }
 }
 
