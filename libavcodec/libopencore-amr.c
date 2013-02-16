e6bda9a9fd86505927a2e095e495eae104860701
Diego Biurrun <diego@biurrun.de>
2013-02-16 23:05:03 +0100

    libopencore-amr: Conditionally compile decoder and encoder bits
    CC: libav-stable@libav.org
    Signed-off-by: Luca Barbato <lu_zero@gentoo.org>

--- libavcodec/libopencore-amr.c
+++ libavcodec/libopencore-amr.c
@@ -60,6 +60,7 @@ typedef struct AMRContext {
     AudioFrameQueue afq;
 } AMRContext;
 
+#if CONFIG_LIBOPENCORE_AMRNB_DECODER
 static av_cold int amr_nb_decode_init(AVCodecContext *avctx)
 {
     AMRContext *s  = avctx->priv_data;
@@ -137,7 +138,9 @@ AVCodec ff_libopencore_amrnb_decoder = {
     .capabilities   = CODEC_CAP_DR1,
     .long_name      = NULL_IF_CONFIG_SMALL("OpenCORE AMR-NB (Adaptive Multi-Rate Narrow-Band)"),
 };
+#endif /* CONFIG_LIBOPENCORE_AMRNB_DECODER */
 
+#if CONFIG_LIBOPENCORE_AMRNB_ENCODER
 /* Common code for fixed and float version*/
 typedef struct AMR_bitrates {
     int       rate;
@@ -301,8 +304,9 @@ AVCodec ff_libopencore_amrnb_encoder = {
     .long_name      = NULL_IF_CONFIG_SMALL("OpenCORE AMR-NB (Adaptive Multi-Rate Narrow-Band)"),
     .priv_class     = &class,
 };
+#endif /* CONFIG_LIBOPENCORE_AMRNB_ENCODER */
 
-#endif
+#endif /* CONFIG_LIBOPENCORE_AMRNB */
 
 /* -----------AMR wideband ------------*/
 #if CONFIG_LIBOPENCORE_AMRWB
