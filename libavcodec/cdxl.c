1f4ab61b746b0a51624466fac5e7dc60eea19aa0
Paul B Mahol <onemda@gmail.com>
2013-02-18 16:19:27 +0000

    lavc/cdxl: clear palette before reading it
    Signed-off-by: Paul B Mahol <onemda@gmail.com>

--- libavcodec/cdxl.c
+++ libavcodec/cdxl.c
@@ -119,6 +119,7 @@ static void cdxl_decode_rgb(CDXLVideoContext *c)
 {
     uint32_t *new_palette = (uint32_t *)c->frame.data[1];
 
+    memset(c->frame.data[1], 0, AVPALETTE_SIZE);
     import_palette(c, new_palette);
     import_format(c, c->frame.linesize[0], c->frame.data[0]);
 }
