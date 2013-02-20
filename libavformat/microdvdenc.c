7346354b5597cc2739af1d911b3e47e0eeba16a2
Clément Bœsch <ubitux@gmail.com>
2013-02-20 23:43:25 +0100

    lavf/microdvd: fix muxing.
    This was broken since 1f265f52.

--- libavformat/microdvdenc.c
+++ libavformat/microdvdenc.c
@@ -19,12 +19,14 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
+#include <inttypes.h>
 #include "avformat.h"
-#include "rawenc.h"
+#include "internal.h"
 
 static int microdvd_write_header(struct AVFormatContext *s)
 {
     AVCodecContext *avctx = s->streams[0]->codec;
+    AVRational tb = avctx->time_base;
 
     if (s->nb_streams != 1 || avctx->codec_id != AV_CODEC_ID_MICRODVD) {
         av_log(s, AV_LOG_ERROR, "Exactly one MicroDVD stream is needed.\n");
@@ -36,6 +38,21 @@ static int microdvd_write_header(struct AVFormatContext *s)
         avio_write(s->pb, avctx->extradata, avctx->extradata_size);
         avio_flush(s->pb);
     }
+
+    avpriv_set_pts_info(s->streams[0], 64, tb.num, tb.den);
+    return 0;
+}
+
+static int microdvd_write_packet(AVFormatContext *avf, AVPacket *pkt)
+{
+    avio_printf(avf->pb, "{%"PRId64"}", pkt->pts);
+    if (pkt->duration < 0)
+        avio_write(avf->pb, "{}", 2);
+    else
+        avio_printf(avf->pb, "{%"PRId64"}", pkt->pts + pkt->duration);
+    avio_write(avf->pb, pkt->data, pkt->size);
+    avio_write(avf->pb, "\n", 1);
+    avio_flush(avf->pb);
     return 0;
 }
 
@@ -45,7 +62,7 @@ AVOutputFormat ff_microdvd_muxer = {
     .mime_type      = "text/x-microdvd",
     .extensions     = "sub",
     .write_header   = microdvd_write_header,
-    .write_packet   = ff_raw_write_packet,
+    .write_packet   = microdvd_write_packet,
     .flags          = AVFMT_NOTIMESTAMPS,
     .subtitle_codec = AV_CODEC_ID_MICRODVD,
 };
