8fe59240a15d38bd0e5911ba8167afd328e064d1
Michael Niedermayer <michaelni@gmx.at>
2013-03-02 20:29:47 +0100

    libxvid: use av_freep() for saftey
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/libxvid.c
+++ libavcodec/libxvid.c
@@ -739,8 +739,8 @@ static av_cold int xvid_encode_close(AVCodecContext *avctx) {
 
     av_freep(&avctx->extradata);
     if( x->twopassbuffer != NULL ) {
-        av_free(x->twopassbuffer);
-        av_free(x->old_twopassbuffer);
+        av_freep(&x->twopassbuffer);
+        av_freep(&x->old_twopassbuffer);
         avctx->stats_out = NULL;
     }
     if (x->twopassfd>=0) {
@@ -748,9 +748,9 @@ static av_cold int xvid_encode_close(AVCodecContext *avctx) {
         close(x->twopassfd);
         x->twopassfd = -1;
     }
-    av_free(x->twopassfile);
-    av_free(x->intra_matrix);
-    av_free(x->inter_matrix);
+    av_freep(&x->twopassfile);
+    av_freep(&x->intra_matrix);
+    av_freep(&x->inter_matrix);
 
     return 0;
 }
