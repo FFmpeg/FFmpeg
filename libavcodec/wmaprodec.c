20373a66ec68d958c266f643a7d0e5ec254c0fcc
Anton Khirnov <anton@khirnov.net>
2013-03-06 09:58:00 +0100

    wmaprodec: require block_align to be set.
    Avoids an infinite loop in the calling programs with decoder not
    consuming any input and not returning output.
    
    CC:libav-stable@libav.org
    (cherry picked from commit cacad1c058f66558ec727faac3b277d2dee264d4)
    
    Signed-off-by: Reinhard Tartler <siretart@tauware.de>

--- libavcodec/wmaprodec.c
+++ libavcodec/wmaprodec.c
@@ -280,6 +280,11 @@ static av_cold int decode_init(AVCodecContext *avctx)
     int log2_max_num_subframes;
     int num_possible_block_sizes;
 
+    if (!avctx->block_align) {
+        av_log(avctx, AV_LOG_ERROR, "block_align is not set\n");
+        return AVERROR(EINVAL);
+    }
+
     s->avctx = avctx;
     ff_dsputil_init(&s->dsp, avctx);
     avpriv_float_dsp_init(&s->fdsp, avctx->flags & CODEC_FLAG_BITEXACT);
