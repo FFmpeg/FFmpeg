9628e5a4acb02467d6bcc07989527664bf2334e7
Ronald S. Bultje <rsbultje@gmail.com>
2013-03-09 15:14:35 -0800

    hpeldsp: add half-pel functions (currently copies of dsputil).

--- libavcodec/rnd_avg.h
+++ libavcodec/rnd_avg.h
@@ -22,7 +22,14 @@
 #ifndef AVCODEC_RND_AVG_H
 #define AVCODEC_RND_AVG_H
 
+#include <stddef.h>
 #include <stdint.h>
+
+#define CALL_2X_PIXELS(a, b, n)\
+static void a(uint8_t *block, const uint8_t *pixels, ptrdiff_t line_size, int h){\
+    b(block  , pixels  , line_size, h);\
+    b(block+n, pixels+n, line_size, h);\
+}
 
 #define         BYTE_VEC32(c)   ((c)*0x01010101UL)
 #define         BYTE_VEC64(c)   ((c)*0x0001000100010001UL)
