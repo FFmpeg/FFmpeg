475c090af2486870610abea93478a926a57b6a91
Stefano Sabatini <stefasab@gmail.com>
2013-03-10 11:48:11 +0100

    cmdutils: make print_all_libs_info() show libavresample version
    Even if not directly supported by FFmpeg, it is still useful to see the
    linked version of libavresample.

--- cmdutils.c
+++ cmdutils.c
@@ -991,7 +991,7 @@ static void print_all_libs_info(int flags, int level)
     PRINT_LIB_INFO(avformat, AVFORMAT, flags, level);
     PRINT_LIB_INFO(avdevice, AVDEVICE, flags, level);
     PRINT_LIB_INFO(avfilter, AVFILTER, flags, level);
-//    PRINT_LIB_INFO(avresample, AVRESAMPLE, flags, level);
+    PRINT_LIB_INFO(avresample, AVRESAMPLE, flags, level);
     PRINT_LIB_INFO(swscale,  SWSCALE,  flags, level);
     PRINT_LIB_INFO(swresample,SWRESAMPLE,  flags, level);
 #if CONFIG_POSTPROC
