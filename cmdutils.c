63edd2f98994bb6d4ca3a70c14fb93d177feea2c
Stefano Sabatini <stefasab@gmail.com>
2013-03-10 12:02:24 +0100

    cmdutils: remove duplicated check on CONFIG_POSTPROC
    The check is already performed by the PRINT_LIB_INFO macro.

--- cmdutils.c
+++ cmdutils.c
@@ -994,9 +994,7 @@ static void print_all_libs_info(int flags, int level)
     PRINT_LIB_INFO(avresample, AVRESAMPLE, flags, level);
     PRINT_LIB_INFO(swscale,  SWSCALE,  flags, level);
     PRINT_LIB_INFO(swresample,SWRESAMPLE,  flags, level);
-#if CONFIG_POSTPROC
     PRINT_LIB_INFO(postproc, POSTPROC, flags, level);
-#endif
 }
 
 static void print_program_info(int flags, int level)
