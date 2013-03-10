e820e3a2591a0d544925ff1522d2a688a647f1b0
Fabrizio Gennari <fabrizio.ge@tiscali.it>
2013-03-10 10:22:45 +0100

    mem: Fix usage of memalign() on DJGPP.
    Credits to Khusraw of bttr-software.de forum.
    Signed-off-by: Carl Eugen Hoyos <cehoyos@ag.or.at>
    (cherry picked from commit 91b747ac78218805a79435f50587f87167569dad)
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavutil/mem.c
+++ libavutil/mem.c
@@ -95,7 +95,11 @@ void *av_malloc(size_t size)
 #elif HAVE_ALIGNED_MALLOC
     ptr = _aligned_malloc(size, ALIGN);
 #elif HAVE_MEMALIGN
+#ifndef __DJGPP__
     ptr = memalign(ALIGN, size);
+#else
+    ptr = memalign(size, ALIGN);
+#endif
     /* Why 64?
      * Indeed, we should align it:
      *   on  4 for 386
