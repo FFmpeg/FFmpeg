19688e87e5f87421146eda62797644d5b25dfacc
Michael Niedermayer <michaelni@gmx.at>
2013-03-12 18:43:17 +0100

    h264: show recovery SEIs in debug output
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/h264_sei.c
+++ libavcodec/h264_sei.c
@@ -161,6 +161,9 @@ static int decode_recovery_point(H264Context *h){
     h->sei_recovery_frame_cnt = get_ue_golomb(&h->gb);
     skip_bits(&h->gb, 4);       /* 1b exact_match_flag, 1b broken_link_flag, 2b changing_slice_group_idc */
 
+    if(h->avctx->debug & FF_DEBUG_PICT_INFO)
+        av_log(h->avctx, AV_LOG_DEBUG, "sei_recovery_frame_cnt: %d\n", h->sei_recovery_frame_cnt);
+
     return 0;
 }
 
