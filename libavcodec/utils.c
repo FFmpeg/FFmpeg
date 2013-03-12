cfc1efc77c7a996b3da7c125c43f7c759cb6d144
Michael Niedermayer <michaelni@gmx.at>
2013-03-13 00:39:05 +0100

    avcodec: assert that old codec ids match new
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/utils.c
+++ libavcodec/utils.c
@@ -2538,6 +2538,11 @@ unsigned avcodec_version(void)
     av_assert0(AV_CODEC_ID_SRT==94216);
     av_assert0(LIBAVCODEC_VERSION_MICRO >= 100);
 
+    av_assert0(CODEC_ID_CLLC == AV_CODEC_ID_CLLC);
+    av_assert0(CODEC_ID_PCM_S8_PLANAR == AV_CODEC_ID_PCM_S8_PLANAR);
+    av_assert0(CODEC_ID_ADPCM_IMA_APC == AV_CODEC_ID_ADPCM_IMA_APC);
+    av_assert0(CODEC_ID_ILBC == AV_CODEC_ID_ILBC);
+    av_assert0(CODEC_ID_SRT == AV_CODEC_ID_SRT);
     return LIBAVCODEC_VERSION_INT;
 }
 
