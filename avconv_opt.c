3e175a2a550f5c2f788e8cd969052e10a5025a27
Anton Khirnov <anton@khirnov.net>
2013-03-14 09:44:07 +0100

    avconv: mark attachment streams as immediately finished
    There are never any packets for those streams.
    Fixes an infinite loop with -attach.

--- avconv_opt.c
+++ avconv_opt.c
@@ -1096,6 +1096,7 @@ static OutputStream *new_attachment_stream(OptionsContext *o, AVFormatContext *o
 {
     OutputStream *ost = new_output_stream(o, oc, AVMEDIA_TYPE_ATTACHMENT);
     ost->stream_copy = 1;
+    ost->finished    = 1;
     return ost;
 }
 
