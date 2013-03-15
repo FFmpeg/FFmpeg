12d8ae297911c3020f4d3c1c34967a47b30fb8aa
Xi Wang <xi.wang@gmail.com>
2013-03-15 06:31:21 -0400

    atrac3: avoid oversized shifting in decode_bytes()
    When `off' is 0, `0x537F6103 << 32' in the following expression invokes
    undefined behavior, the result of which is not necessarily 0.
    
        (0x537F6103 >> (off * 8)) | (0x537F6103 << (32 - (off * 8)))
    
    Avoid oversized shifting.
    
    CC: libav-stable@libav.org
    
    Signed-off-by: Xi Wang <xi.wang@gmail.com>
    Signed-off-by: Luca Barbato <lu_zero@gentoo.org>
    
    (cherry picked from commit eba1ff31304e407db3cefd7532108408f364367b)
    
    Conflicts:
    	libavcodec/atrac3.c

--- libavcodec/atrac3.c
+++ libavcodec/atrac3.c
@@ -184,8 +184,11 @@ static int decode_bytes(const uint8_t* inbuffer, uint8_t* out, int bytes){
     uint32_t* obuf = (uint32_t*) out;
 
     off = (intptr_t)inbuffer & 3;
-    buf = (const uint32_t*) (inbuffer - off);
-    c = av_be2ne32((0x537F6103 >> (off*8)) | (0x537F6103 << (32-(off*8))));
+    buf = (const uint32_t *)(inbuffer - off);
+    if (off)
+        c = av_be2ne32((0x537F6103U >> (off * 8)) | (0x537F6103U << (32 - (off * 8))));
+    else
+        c = av_be2ne32(0x537F6103U);
     bytes += 3 + off;
     for (i = 0; i < bytes/4; i++)
         obuf[i] = c ^ buf[i];
