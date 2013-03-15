3d751b1ef6619b20631b17c880aa96f2dd3a11dd
James Darnley <james.darnley@gmail.com>
2013-03-15 16:37:33 +0100

    yadif: correct strides in filter_edges_16bit
    The C code treats the data as arrays of uint16_t so strides must not
    be in bytes but in pixels.
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavfilter/vf_yadif.c
+++ libavfilter/vf_yadif.c
@@ -157,6 +157,8 @@ static void filter_edges_16bit(void *dst1, void *prev1, void *cur1, void *next1,
     int x;
     uint16_t *prev2 = parity ? prev : cur ;
     uint16_t *next2 = parity ? cur  : next;
+    mrefs /= 2;
+    prefs /= 2;
 
     FILTER(0, 3, 0)
 
