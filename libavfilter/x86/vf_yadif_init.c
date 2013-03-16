0a5814c9ba23f510fd8218c6677cc9b878d542c6
James Darnley <james.darnley@gmail.com>
2013-03-16 21:42:24 +0100

    yadif: x86 assembly for 9 to 14-bit samples
    These smaller samples do not need to be unpacked to double words
    allowing the code to process more pixels every iteration (still 2 in MMX
    but 6 in SSE2).  It also avoids emulating the missing double word
    instructions on older instruction sets.
    
    Like with the previous code for 16-bit samples this has been tested on
    an Athlon64 and a Core2Quad.
    
    Athlon64:
    1809275 decicycles in C,    32718 runs, 50 skips
     911675 decicycles in mmx,  32727 runs, 41 skips, 2.0x faster
     495284 decicycles in sse2, 32747 runs, 21 skips, 3.7x faster
    
    Core2Quad:
     921363 decicycles in C,     32756 runs, 12 skips
     486537 decicycles in mmx,   32764 runs,  4 skips, 1.9x faster
     293296 decicycles in sse2,  32759 runs,  9 skips, 3.1x faster
     284910 decicycles in ssse3, 32759 runs,  9 skips, 3.2x faster
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavfilter/x86/vf_yadif_init.c
+++ libavfilter/x86/vf_yadif_init.c
@@ -49,6 +49,16 @@ void ff_yadif_filter_line_16bit_sse4(void *dst, void *prev, void *cur,
                                      void *next, int w, int prefs,
                                      int mrefs, int parity, int mode);
 
+void ff_yadif_filter_line_10bit_mmxext(void *dst, void *prev, void *cur,
+                                       void *next, int w, int prefs,
+                                       int mrefs, int parity, int mode);
+void ff_yadif_filter_line_10bit_sse2(void *dst, void *prev, void *cur,
+                                     void *next, int w, int prefs,
+                                     int mrefs, int parity, int mode);
+void ff_yadif_filter_line_10bit_ssse3(void *dst, void *prev, void *cur,
+                                      void *next, int w, int prefs,
+                                      int mrefs, int parity, int mode);
+
 av_cold void ff_yadif_init_x86(YADIFContext *yadif)
 {
     int cpu_flags = av_get_cpu_flags();
@@ -56,7 +66,7 @@ av_cold void ff_yadif_init_x86(YADIFContext *yadif)
                                   : yadif->csp->comp[0].depth_minus1 + 1;
 
 #if HAVE_YASM
-    if (bit_depth > 8) {
+    if (bit_depth >= 15) {
 #if ARCH_X86_32
         if (EXTERNAL_MMXEXT(cpu_flags))
             yadif->filter_line = ff_yadif_filter_line_16bit_mmxext;
@@ -67,6 +77,15 @@ av_cold void ff_yadif_init_x86(YADIFContext *yadif)
             yadif->filter_line = ff_yadif_filter_line_16bit_ssse3;
         if (EXTERNAL_SSE4(cpu_flags))
             yadif->filter_line = ff_yadif_filter_line_16bit_sse4;
+    } else if ( bit_depth >= 9 && bit_depth <= 14) {
+#if ARCH_X86_32
+        if (EXTERNAL_MMXEXT(cpu_flags))
+            yadif->filter_line = ff_yadif_filter_line_10bit_mmxext;
+#endif /* ARCH_X86_32 */
+        if (EXTERNAL_SSE2(cpu_flags))
+            yadif->filter_line = ff_yadif_filter_line_10bit_sse2;
+        if (EXTERNAL_SSSE3(cpu_flags))
+            yadif->filter_line = ff_yadif_filter_line_10bit_ssse3;
     } else {
 #if ARCH_X86_32
     if (EXTERNAL_MMXEXT(cpu_flags))
