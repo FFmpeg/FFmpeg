5787a71637357e0f5862360a0e3fb6bef480b37c
Stefano Sabatini <stefasab@gmail.com>
2013-03-17 01:08:51 +0100

    lavfi/swapuv: remove deprecated base field use

--- libavfilter/vf_swapuv.c
+++ libavfilter/vf_swapuv.c
@@ -33,7 +33,6 @@ static void do_swap(AVFrame *frame)
 {
     FFSWAP(uint8_t*,     frame->data[1],     frame->data[2]);
     FFSWAP(int,          frame->linesize[1], frame->linesize[2]);
-    FFSWAP(uint8_t*,     frame->base[1],     frame->base[2]);
     FFSWAP(uint64_t,     frame->error[1],    frame->error[2]);
     FFSWAP(AVBufferRef*, frame->buf[1],      frame->buf[2]);
 }
