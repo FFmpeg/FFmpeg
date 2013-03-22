a42d15a3a0efddf693218bc1c94f98f8c48a7469
Stefano Sabatini <stefasab@gmail.com>
2013-03-22 01:43:43 +0100

    lavfi/anullsrc: add missing checks

--- libavfilter/asrc_anullsrc.c
+++ libavfilter/asrc_anullsrc.c
@@ -114,12 +114,17 @@ static int request_frame(AVFilterLink *outlink)
     AVFrame *samplesref;
 
     samplesref = ff_get_audio_buffer(outlink, null->nb_samples);
+    if (!samplesref)
+        return AVERROR(ENOMEM);
+
     samplesref->pts = null->pts;
     samplesref->channel_layout = null->channel_layout;
     samplesref->sample_rate = outlink->sample_rate;
 
     ret = ff_filter_frame(outlink, av_frame_clone(samplesref));
     av_frame_free(&samplesref);
+    if (ret < 0)
+        return ret;
 
     null->pts += null->nb_samples;
     return ret;
