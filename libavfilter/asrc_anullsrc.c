fe2cfd50f041c1fa589fb459a623d1b371db12f1
Stefano Sabatini <stefasab@gmail.com>
2013-03-22 01:39:48 +0100

    lavfi/anullsrc: simplify logging code in config_props

--- libavfilter/asrc_anullsrc.c
+++ libavfilter/asrc_anullsrc.c
@@ -98,10 +98,8 @@ static int config_props(AVFilterLink *outlink)
 {
     ANullContext *null = outlink->src->priv;
     char buf[128];
-    int chans_nb;
 
-    chans_nb = av_get_channel_layout_nb_channels(null->channel_layout);
-    av_get_channel_layout_string(buf, sizeof(buf), chans_nb, null->channel_layout);
+    av_get_channel_layout_string(buf, sizeof(buf), 0, null->channel_layout);
     av_log(outlink->src, AV_LOG_VERBOSE,
            "sample_rate:%d channel_layout:'%s' nb_samples:%d\n",
            null->sample_rate, buf, null->nb_samples);
