a2f7314ba231cde459c4f33f1a7602ae9d9d2d28
Michael Niedermayer <michaelni@gmx.at>
2013-03-25 21:48:45 +0100

    libpostproc: silence valgrind/fate warning about using uninitialized data
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libpostproc/postprocess_template.c
+++ libpostproc/postprocess_template.c
@@ -3216,17 +3216,19 @@ static inline void RENAME(duplicate)(uint8_t src[], int stride)
 #if TEMPLATE_PP_MMX
     __asm__ volatile(
         "movq (%0), %%mm0               \n\t"
+        "movq %%mm0, (%0, %1, 4)        \n\t"
         "add %1, %0                     \n\t"
         "movq %%mm0, (%0)               \n\t"
         "movq %%mm0, (%0, %1)           \n\t"
         "movq %%mm0, (%0, %1, 2)        \n\t"
+        "movq %%mm0, (%0, %1, 4)        \n\t"
         : "+r" (src)
         : "r" ((x86_reg)-stride)
     );
 #else
     int i;
     uint8_t *p=src;
-    for(i=0; i<3; i++){
+    for(i=0; i<5; i++){
         p-= stride;
         memcpy(p, src, 8);
     }
