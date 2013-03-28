65340c976c664b94427ac50f5d03b0e77883c108
Matt Wolenetz <wolenetz@chromium.org>
2013-03-27 17:29:57 -0700

    Fix pthread_cond and pthread_mutex leaks in vp8
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/vp8.c
+++ libavcodec/vp8.c
@@ -40,6 +40,10 @@ static void free_buffers(VP8Context *s)
     int i;
     if (s->thread_data)
         for (i = 0; i < MAX_THREADS; i++) {
+#if HAVE_THREADS
+            pthread_cond_destroy(&s->thread_data[i].cond);
+            pthread_mutex_destroy(&s->thread_data[i].lock);
+#endif
             av_freep(&s->thread_data[i].filter_strength);
             av_freep(&s->thread_data[i].edge_emu_buffer);
         }
