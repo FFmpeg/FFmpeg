27b7bfc7b5be601be0dd5a2a472be80fc5263bdd
Michael Niedermayer <michaelni@gmx.at>
2013-03-28 17:22:35 +0100

    avidec: Fix demuxing of non seekable avis with multiple RIFFs
    This solution uses the size of the first RIFF if its valid
    to check frame size validity when the filesize could not be determined.
    Feedback is welcome, other ideas and improvments as well!
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/avidec.c
+++ libavformat/avidec.c
@@ -944,7 +944,7 @@ start_sync:
         n= get_stream_idx(d+2);
         av_dlog(s, "%X %X %X %X %X %X %X %X %"PRId64" %u %d\n",
                 d[0], d[1], d[2], d[3], d[4], d[5], d[6], d[7], i, size, n);
-        if(i + (uint64_t)size > avi->fsize || d[0] > 127)
+        if(i*(avi->io_fsize>0) + (uint64_t)size > avi->fsize || d[0] > 127)
             continue;
 
         //parse ix##
