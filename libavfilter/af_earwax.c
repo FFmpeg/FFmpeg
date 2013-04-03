0ab10f69efd3ab0e66ec9d611e2d41bdccea83cd
Paul B Mahol <onemda@gmail.com>
2013-04-03 10:52:01 +0000

    lavfi/earwax: clip sample, this is what sox does
    Fixes #1884.
    
    Signed-off-by: Paul B Mahol <onemda@gmail.com>

--- libavfilter/af_earwax.c
+++ libavfilter/af_earwax.c
@@ -98,10 +98,10 @@ static inline int16_t *scalarproduct(const int16_t *in, const int16_t *endin, in
     int16_t j;
 
     while (in < endin) {
-        sample = 32;
+        sample = 0;
         for (j = 0; j < NUMTAPS; j++)
             sample += in[j] * filt[j];
-        *out = sample >> 6;
+        *out = av_clip_int16(sample >> 6);
         out++;
         in++;
     }
