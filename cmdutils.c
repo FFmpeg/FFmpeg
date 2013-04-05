FFmpeg/FFmpeg

commit 8bb5680d904008b1a851f14078a2e9a9e9489fee

cmdutils: improve feedback in case of not found option

cmdutils.c
@@ -294,7 +294,8 @@ static int write_option(void *optctx, const OptionDef *po, const char *opt,
         int ret = po->u.func_arg(optctx, opt, arg);
         if (ret < 0) {
             av_log(NULL, AV_LOG_ERROR,
-                   "Failed to set value '%s' for option '%s'\n", arg, opt);
+                   "Failed to set value '%s' for option '%s': %s\n",
+                   arg, opt, av_err2str(ret));
             return ret;
         }
     }
@@ -546,6 +547,8 @@ int opt_default(void *optctx, const char *opt, const char *arg)
 
     if (consumed)
         return 0;
+    av_log(NULL, AV_LOG_ERROR, "Could not find option '%s' in any of the FFmpeg subsystems "
+           "(codec, format, scaler, resampler contexts)\n", opt);
     return AVERROR_OPTION_NOT_FOUND;
 }
 

commit 495ed19b5bb0b3facb1833001dce4a4419e82800

cmdutils: remove error message from opt_default() in case of missing option
A similar error message is generated by the calling function, avoid
duplication and spurious error message occurring with negated boolean
options introduced in 8bb5680, which can be reproduced for example with
the command:
ffmpeg -f lavfi -i testsrc=d=0.1 -nostdin -f null -

cmdutils.c
@@ -547,8 +547,6 @@ int opt_default(void *optctx, const char *opt, const char *arg)
 
     if (consumed)
         return 0;
-    av_log(NULL, AV_LOG_ERROR, "Could not find option '%s' in any of the FFmpeg subsystems "
-           "(codec, format, scaler, resampler contexts)\n", opt);
     return AVERROR_OPTION_NOT_FOUND;
 }
 