a5ce5ac9fb49f156c30524c466cd8a990d3830f1
Matthieu Bouron <matthieu.bouron@gmail.com>
2013-04-11 01:31:08 +0200

    lavfi/apad: switch to AVOption-based system

--- libavfilter/avfilter.c
+++ libavfilter/avfilter.c
@@ -658,6 +658,7 @@ int avfilter_init_filter(AVFilterContext *filter, const char *args, void *opaque
     int anton_options =
         !strcmp(filter->filter->name,  "afade"     ) ||
         !strcmp(filter->filter->name,  "aformat") ||
+        !strcmp(filter->filter->name,  "apad"      ) ||
         !strcmp(filter->filter->name,  "ass")     ||
         !strcmp(filter->filter->name,  "blackframe") ||
         !strcmp(filter->filter->name,  "blend"     ) ||
