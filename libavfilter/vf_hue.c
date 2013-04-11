160ed967f459966191de34f4fc8fc1664d1ff2e6
Stefano Sabatini <stefasab@gmail.com>
2013-04-11 20:56:51 +0200

    lavfi/hue: extend debugging message
    In particular, show h value in degrees.

--- libavfilter/vf_hue.c
+++ libavfilter/vf_hue.c
@@ -270,11 +270,12 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *inpic)
         hue->hue = hue->hue_deg * M_PI / 180;
     } else if (hue->hue_expr) {
         hue->hue = av_expr_eval(hue->hue_pexpr, hue->var_values, NULL);
+        hue->hue_deg = hue->hue * 180 / M_PI;
     }
 
     av_log(inlink->dst, AV_LOG_DEBUG,
-           "H:%0.1f s:%0.f t:%0.1f n:%d\n",
-           hue->hue, hue->saturation,
+           "H:%0.1f*PI h:%0.1f s:%0.f t:%0.1f n:%d\n",
+           hue->hue/M_PI, hue->hue_deg, hue->saturation,
            hue->var_values[VAR_T], (int)hue->var_values[VAR_N]);
 
     compute_sin_and_cos(hue);
