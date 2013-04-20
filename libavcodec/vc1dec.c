6c9d28a2294de86519c7c613c76394c66dcfd83c
Michael Niedermayer <michaelni@gmx.at>
2013-04-20 02:15:13 +0200

    vc1dec: Fix tff == 0 handling in init_block_index()
    This fixes several files from VLC ticket5887
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/vc1dec.c
+++ libavcodec/vc1dec.c
@@ -78,7 +78,7 @@ static void init_block_index(VC1Context *v)
 {
     MpegEncContext *s = &v->s;
     ff_init_block_index(s);
-    if (v->field_mode && v->second_field) {
+    if (v->field_mode && !(v->second_field ^ v->tff)) {
         s->dest[0] += s->current_picture_ptr->f.linesize[0];
         s->dest[1] += s->current_picture_ptr->f.linesize[1];
         s->dest[2] += s->current_picture_ptr->f.linesize[2];
