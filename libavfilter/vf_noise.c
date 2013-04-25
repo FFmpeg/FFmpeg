89b5ed5f1569b5a12cddbe2829397a30f03d3d12
Paul B Mahol <onemda@gmail.com>
2013-04-25 17:03:37 +0000

    lavfi/noise: remove get_video_buffer, its redundant now
    Signed-off-by: Paul B Mahol <onemda@gmail.com>

--- libavfilter/vf_noise.c
+++ libavfilter/vf_noise.c
@@ -447,7 +447,6 @@ static const AVFilterPad noise_inputs[] = {
     {
         .name             = "default",
         .type             = AVMEDIA_TYPE_VIDEO,
-        .get_video_buffer = ff_null_get_video_buffer,
         .filter_frame     = filter_frame,
         .config_props     = config_input,
     },
