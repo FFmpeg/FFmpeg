7ed51ee37eb550dca8cfdfe6758b86e3e050f860
Cedric Fung <cedric@vec.io>
2013-04-26 14:54:06 +0800

    Add cache callback and dump segments index for every read

--- libavformat/cache.c
+++ libavformat/cache.c
@@ -30,6 +30,7 @@
 #include "libavutil/avassert.h"
 #include "libavutil/avstring.h"
 #include "libavutil/file.h"
+#include "libavutil/opt.h"
 #include "os_support.h"
 #include "avformat.h"
 #include "url.h"
@@ -55,10 +56,13 @@ typedef struct Segment {
   struct Segment *next;
 } Segment;
 
-typedef struct Context {
+typedef struct CacheContext {
+  const AVClass *class;
   URLContext *inner;
   Segment *segs;
   Segment *seg;
+  int64_t *segs_flat;
+  int num_segs;
   int8_t cache_fill;
   char *cache_path;
   int fdw, fdr, fdi;
@@ -66,15 +70,18 @@ typedef struct Context {
   int64_t length;
   pthread_t thread;
   pthread_mutex_t mutex;
-} Context;
+  void (*callback)(int64_t *segs, int len);
+} CacheContext;
 
-static Segment *segments_select(Segment **segs, int64_t position)
+static Segment *segments_select(CacheContext *c, Segment **segs, int64_t position)
 {
   Segment *next = *segs;
 
   if (next == NULL) {
     *segs = av_mallocz(sizeof(Segment));
     (*segs)->begin = (*segs)->end = position;
+    c->num_segs = 1;
+    c->segs_flat = av_mallocz(sizeof(int64_t) * 2 * c->num_segs);
     return *segs;
   }
 
@@ -84,6 +91,8 @@ static Segment *segments_select(Segment **segs, int64_t position)
       seg->begin = seg->end = position;
       seg->next = next->next;
       *segs = seg;
+      c->num_segs++;
+      c->segs_flat = av_realloc(c->segs_flat, sizeof(int64_t) * 2 * c->num_segs);
     } else if (next->begin <= position && position <= next->end) {
       return next;
     } else if (next->end < position && (!next->next || position < next->next->begin)) {
@@ -91,6 +100,8 @@ static Segment *segments_select(Segment **segs, int64_t position)
       seg->begin = seg->end = position;
       seg->next = next->next;
       next->next = seg;
+      c->num_segs++;
+      c->segs_flat = av_realloc(c->segs_flat, sizeof(int64_t) * 2 * c->num_segs);
     }
     next = next->next;
   }
@@ -98,7 +109,7 @@ static Segment *segments_select(Segment **segs, int64_t position)
   return NULL;
 }
 
-static void segments_balance(Context *c, Segment *seg)
+static void segments_balance(CacheContext *c, Segment *seg)
 {
   Segment *next = seg->next;
   int64_t pos = -1;
@@ -112,31 +123,38 @@ static void segments_balance(Context *c, Segment *seg)
     }
     seg->next = next->next;
     av_free(next);
+    c->num_segs--;
+    c->segs_flat = av_realloc(c->segs_flat, sizeof(int64_t) * 2 * c->num_segs);
   }
 }
 
-static void segments_dump(Segment *segs, int fd)
+static void segments_dump(CacheContext *c, Segment *segs, int fd)
 {
   lseek(fd, 0, SEEK_SET);
   ftruncate64(fd, 0);
-  while (segs) {
+  for (int i = 0; segs; i += 2) {
     write(fd, &segs->begin, sizeof(int64_t));
     write(fd, &segs->end, sizeof(int64_t));
+    c->segs_flat[i] = segs->begin;
+    c->segs_flat[i+1] = segs->end;
     segs = segs->next;
   }
 }
 
-static Segment *segments_load(int fd)
+static Segment *segments_load(CacheContext *c, int fd)
 {
   Segment *seg = NULL, **next = &seg;
   int64_t begin = 0, end = 0;
   lseek(fd, 0, SEEK_SET);
+  c->num_segs = 0;
   while (sizeof(int64_t) == read(fd, &begin, sizeof(int64_t)) &&
       sizeof(int64_t) == read(fd, &end, sizeof(int64_t))) {
     *next = av_mallocz(sizeof(Segment));
     (*next)->begin = begin;
     (*next)->end = end;
     av_log(NULL, AV_LOG_INFO, "[%"PRId64"-%"PRId64"]\t", begin, end);
+    c->num_segs++;
+    c->segs_flat = av_realloc(c->segs_flat, sizeof(int64_t) * 2 * c->num_segs);
     next = &((*next)->next);
   }
   av_log(NULL, AV_LOG_INFO, "\n");
@@ -156,7 +174,7 @@ static void* cache_fill_thread(void* arg)
 {
   uint8_t buf[1024];
   int r, r_;
-  Context *c = (Context *)arg;
+  CacheContext *c = (CacheContext *)arg;
 
   while (c->cache_fill && c->seg->end - c->seg->begin < c->length) {
     pthread_mutex_lock(&c->mutex);
@@ -167,6 +185,10 @@ static void* cache_fill_thread(void* arg)
         av_assert0(r_ == r);
         c->seg->end += r_;
         segments_balance(c, c->seg);
+        if (c->segs_flat && c->num_segs > 0 && c->callback) {
+          segments_dump(c, c->segs, c->fdi);
+          c->callback(c->segs_flat, c->num_segs);
+        }
       } else {
         usleep(200);
       }
@@ -183,7 +205,7 @@ static int cache_open(URLContext *h, const char *arg, int flags)
 {
   char *url;
   int dlen, opened;
-  Context *c= h->priv_data;
+  CacheContext *c= h->priv_data;
 
   arg = strchr(arg, ':') + 1;
   url = strchr(arg, ':') + 1;
@@ -198,18 +220,22 @@ static int cache_open(URLContext *h, const char *arg, int flags)
   }
 
   c->length = ffurl_size(c->inner);
-  if (c->length > 1048576) {
+  if (!c->inner->is_streamed && c->length > 1048576) {
     c->fdw = open(c->cache_path, O_RDWR | O_BINARY | O_CREAT, 0600);
     c->fdr = open(c->cache_path, O_RDWR | O_BINARY, 0600);
-    if (!c->inner->is_streamed && ftruncate64(c->fdw, c->length) == 0) {
+    if (c->fdw != -1 && c->fdr != -1 && ftruncate64(c->fdw, c->length) == 0) {
       char index_path[dlen + 4];
       snprintf(index_path, dlen + 4, "%s.ssi", c->cache_path);
       c->fdi = open(index_path, O_RDWR | O_BINARY | O_CREAT, 0600);
-      c->segs = segments_load(c->fdi);
+      c->segs = segments_load(c, c->fdi);
       if (c->segs) {
         c->seg = c->segs;
+        if (c->segs_flat && c->num_segs > 0 && c->callback) {
+          segments_dump(c, c->segs, c->fdi);
+          c->callback(c->segs_flat, c->num_segs);
+        }
       } else {
-        c->seg = segments_select(&c->segs, 0);
+        c->seg = segments_select(c, &c->segs, 0);
       }
       pthread_mutex_init(&c->mutex, NULL);
       pthread_create(&c->thread, NULL, cache_fill_thread, c);
@@ -226,7 +252,7 @@ static int cache_open(URLContext *h, const char *arg, int flags)
 
 static int cache_read(URLContext *h, unsigned char *buf, int size)
 {
-  Context *c= h->priv_data;
+  CacheContext *c= h->priv_data;
   Segment* seg = c->seg;
   int len = 0;
 
@@ -250,7 +276,7 @@ static int cache_read(URLContext *h, unsigned char *buf, int size)
 
 static int64_t cache_seek(URLContext *h, int64_t position, int whence)
 {
-  Context *c= h->priv_data;
+  CacheContext *c= h->priv_data;
   Segment *candi = NULL;
 
   if (!c->cache_fill || whence == AVSEEK_SIZE) {
@@ -258,7 +284,7 @@ static int64_t cache_seek(URLContext *h, int64_t position, int whence)
   }
 
   pthread_mutex_lock(&c->mutex);
-  candi = segments_select(&c->segs, position);
+  candi = segments_select(c, &c->segs, position);
   if (candi == c->seg) {
     c->position = lseek(c->fdr, position, whence);
   } else {
@@ -278,14 +304,15 @@ static int64_t cache_seek(URLContext *h, int64_t position, int whence)
 
 static int cache_close(URLContext *h)
 {
-  Context *c= h->priv_data;
+  CacheContext *c= h->priv_data;
 
   if (c->cache_fill) {
     c->cache_fill = 0;
     pthread_join(c->thread, NULL);
-    segments_dump(c->segs, c->fdi);
+    segments_dump(c, c->segs, c->fdi);
     pthread_mutex_destroy(&c->mutex);
     segments_free(c->segs);
+    av_free(c->segs_flat);
     close(c->fdw);
     close(c->fdr);
     close(c->fdi);
@@ -297,11 +324,24 @@ static int cache_close(URLContext *h)
   return 0;
 }
 
+static const AVOption options[] = {
+  {"cache_clk", "callback when cache updated", offsetof(CacheContext, callback), AV_OPT_TYPE_INT, {0}, 0, INT_MAX},
+  {NULL}
+};
+
+static const AVClass cache_context_class = {
+  .class_name     = "cache",
+  .item_name      = av_default_item_name,
+  .option         = options,
+  .version        = LIBAVUTIL_VERSION_INT,
+};
+
 URLProtocol ff_cache_protocol = {
   .name                = "cache",
   .url_open            = cache_open,
   .url_read            = cache_read,
   .url_seek            = cache_seek,
   .url_close           = cache_close,
-  .priv_data_size      = sizeof(Context),
+  .priv_data_size      = sizeof(CacheContext),
+  .priv_data_class     = &cache_context_class,
 };
