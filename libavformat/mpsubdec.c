a82cc54fb5ce3774ac7f4a646e6c3a2388d528bd
Clément Bœsch <ubitux@gmail.com>
2013-05-06 22:48:36 +0200

    lavf/mpsub: remove sscanf from probing.
    This should make probing faster.

--- libavformat/mpsubdec.c
+++ libavformat/mpsubdec.c
@@ -37,11 +37,10 @@ static int mpsub_probe(AVProbeData *p)
     const char *ptr_end = p->buf + p->buf_size;
 
     while (ptr < ptr_end) {
-        int n;
-
-        if (!memcmp(ptr, "FORMAT=TIME", 11) ||
-            sscanf(ptr, "FORMAT=%d", &n) == 1)
+        if (!memcmp(ptr, "FORMAT=TIME", 11))
             return AVPROBE_SCORE_EXTENSION;
+        if (!memcmp(ptr, "FORMAT=", 7))
+            return AVPROBE_SCORE_EXTENSION / 3;
         ptr += strcspn(ptr, "\n") + 1;
     }
     return 0;
