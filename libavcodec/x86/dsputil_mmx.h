1b343cedd7cd68e7865aa5280d1568c7e5d79917
Diego Biurrun <diego@biurrun.de>
2013-05-07 01:37:03 +0200

    x86: dsputil: Remove unused ff_pb_3F constant

--- libavcodec/x86/dsputil_mmx.h
+++ libavcodec/x86/dsputil_mmx.h
@@ -48,7 +48,6 @@ extern const uint64_t ff_pw_255;
 
 extern const xmm_reg  ff_pb_1;
 extern const xmm_reg  ff_pb_3;
-extern const uint64_t ff_pb_3F;
 extern const xmm_reg  ff_pb_F8;
 extern const uint64_t ff_pb_FC;
 
