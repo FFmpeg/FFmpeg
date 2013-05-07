a694b2b15865f6fd42217c82146e1be43f3b3f9b
Reinhard Tartler <siretart@tauware.de>
2013-05-07 07:26:19 +0200

    shorten: set invalid channels count to 0
    Prevent the loop shorten_decode_close from writing and freeing out of
    the array boundary.
    
    Found-by: Mateusz "j00ru" Jurczyk and Gynvael Coldwind
    Signed-off-by: Luca Barbato <lu_zero@gentoo.org>
    
    CC: libav-stable@libav.org
    
    (cherry picked from commit c10da30d8426a1f681d99a780b6e311f7fb4e5c5)
    (cherry picked from commit 21d568be179c54a1596d1377b4da7fbe755bfe7f)
    
    Signed-off-by: Luca Barbato <lu_zero@gentoo.org>
    
    Conflicts:
    	libavcodec/shorten.c

--- libavcodec/shorten.c
+++ libavcodec/shorten.c
@@ -344,6 +344,7 @@ static int shorten_decode_frame(AVCodecContext *avctx,
         s->channels = get_uint(s, CHANSIZE);
         if (s->channels <= 0 || s->channels > MAX_CHANNELS) {
             av_log(s->avctx, AV_LOG_ERROR, "too many channels: %d\n", s->channels);
+            s->channels = 0;
             return -1;
         }
 
