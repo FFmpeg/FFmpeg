3928c4ae45757687217ea5f66b225a9cc9a851d4
Anton Khirnov <anton@khirnov.net>
2013-05-07 07:20:32 +0200

    gsmdec: convert to new channel layout API
    Signed-off-by: Vittorio Giovara <vittorio.giovara@gmail.com>
    Signed-off-by: James Almer <jamrial@gmail.com>

--- libavcodec/gsmdec.c
+++ libavcodec/gsmdec.c
@@ -34,8 +34,8 @@
 
 static av_cold int gsm_init(AVCodecContext *avctx)
 {
-    avctx->channels       = 1;
-    avctx->channel_layout = AV_CH_LAYOUT_MONO;
+    av_channel_layout_uninit(&avctx->ch_layout);
+    avctx->ch_layout      = (AVChannelLayout)AV_CHANNEL_LAYOUT_MONO;
     if (!avctx->sample_rate)
         avctx->sample_rate = 8000;
     avctx->sample_fmt     = AV_SAMPLE_FMT_S16;
