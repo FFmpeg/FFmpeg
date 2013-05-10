5c4c929878f46ac53382fc7e00cd01e7b0a5b3c8
Stefano Sabatini <stefasab@gmail.com>
2013-05-10 10:16:47 +0200

    lavfi/avfiltergraph: fix style

--- libavfilter/avfiltergraph.c
+++ libavfilter/avfiltergraph.c
@@ -1081,24 +1081,24 @@ int avfilter_graph_send_command(AVFilterGraph *graph, const char *target, const 
 {
     int i, r = AVERROR(ENOSYS);
 
-    if(!graph)
+    if (!graph)
         return r;
 
-    if((flags & AVFILTER_CMD_FLAG_ONE) && !(flags & AVFILTER_CMD_FLAG_FAST)) {
-        r=avfilter_graph_send_command(graph, target, cmd, arg, res, res_len, flags | AVFILTER_CMD_FLAG_FAST);
-        if(r != AVERROR(ENOSYS))
+    if ((flags & AVFILTER_CMD_FLAG_ONE) && !(flags & AVFILTER_CMD_FLAG_FAST)) {
+        r = avfilter_graph_send_command(graph, target, cmd, arg, res, res_len, flags | AVFILTER_CMD_FLAG_FAST);
+        if (r != AVERROR(ENOSYS))
             return r;
     }
 
-    if(res_len && res)
-        res[0]= 0;
+    if (res_len && res)
+        res[0] = 0;
 
     for (i = 0; i < graph->nb_filters; i++) {
         AVFilterContext *filter = graph->filters[i];
-        if(!strcmp(target, "all") || (filter->name && !strcmp(target, filter->name)) || !strcmp(target, filter->filter->name)){
+        if (!strcmp(target, "all") || (filter->name && !strcmp(target, filter->name)) || !strcmp(target, filter->filter->name)) {
             r = avfilter_process_command(filter, cmd, arg, res, res_len, flags);
-            if(r != AVERROR(ENOSYS)) {
-                if((flags & AVFILTER_CMD_FLAG_ONE) || r<0)
+            if (r != AVERROR(ENOSYS)) {
+                if ((flags & AVFILTER_CMD_FLAG_ONE) || r < 0)
                     return r;
             }
         }
