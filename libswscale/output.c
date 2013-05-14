e751481cd82d610cf93c6104a41396bd6338d073
Michael Niedermayer <michaelni@gmx.at>
2013-05-14 18:48:54 +0200

    swscale/output: silence compiler warnings about uninitialized variables
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libswscale/output.c
+++ libswscale/output.c
@@ -762,13 +762,13 @@ yuv2rgba64_2_c_template(SwsContext *c, const int32_t *buf[2],
     int  yalpha1 = 4096 - yalpha;
     int uvalpha1 = 4096 - uvalpha;
     int i;
+    int A1 = 0, A2 = 0; // Init to avoid compiler warnings
 
     for (i = 0; i < ((dstW + 1) >> 1); i++) {
         int Y1 = (buf0[i * 2]     * yalpha1  + buf1[i * 2]     * yalpha) >> 14;
         int Y2 = (buf0[i * 2 + 1] * yalpha1  + buf1[i * 2 + 1] * yalpha) >> 14;
         int U  = (ubuf0[i]        * uvalpha1 + ubuf1[i]        * uvalpha + (-128 << 23)) >> 14;
         int V  = (vbuf0[i]        * uvalpha1 + vbuf1[i]        * uvalpha + (-128 << 23)) >> 14;
-        int A1, A2;
         int R, G, B;
 
         Y1 -= c->yuv2rgb_y_offset;
