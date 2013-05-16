09b3bbe6057c9d03dff2467b1e6748a617afea15
Michael Niedermayer <michaelni@gmx.at>
2013-05-16 22:54:16 +0200

    libvpxenc: Fix "passing argument 3 of av_image_copy from incompatible pointer type" warning
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/libvpxdec.c
+++ libavcodec/libvpxdec.c
@@ -96,7 +96,7 @@ static int vp8_decode(AVCodecContext *avctx,
         }
         if ((ret = ff_get_buffer(avctx, picture, 0)) < 0)
             return ret;
-        av_image_copy(picture->data, picture->linesize, img->planes,
+        av_image_copy(picture->data, picture->linesize, (const uint8_t **)img->planes,
                       img->stride, avctx->pix_fmt, img->d_w, img->d_h);
         *got_frame           = 1;
     }
