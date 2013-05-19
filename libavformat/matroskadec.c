729fa55b8f4250b2fee7f4a511372d0b9f696389
Michael Niedermayer <michaelni@gmx.at>
2013-05-19 23:38:01 +0200

    avformat/matroskadec: fix integer overflow
    Fixes fate-mkv with ekopath 4
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/matroskadec.c
+++ libavformat/matroskadec.c
@@ -2284,7 +2284,7 @@ static int matroska_parse_block(MatroskaDemuxContext *matroska, uint8_t *data,
         return res;
     av_assert1(block_duration != AV_NOPTS_VALUE);
 
-    block_time = AV_RB16(data);
+    block_time = sign_extend(AV_RB16(data), 16);
     data += 2;
     flags = *data++;
     size -= 3;
