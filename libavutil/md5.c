7e03886bbb46bf150ac8b457da66255b6d935eab
Giorgio Vazzana <mywing81@gmail.com>
2013-05-21 10:36:20 +0200

    avutil/md5: reindent after previous commits
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavutil/md5.c
+++ libavutil/md5.c
@@ -83,11 +83,11 @@ static const uint32_t T[64] = { // T[i]= fabs(sin(i+1)<<32)
         a += T[i];                                                      \
                                                                         \
         if (i < 32) {                                                   \
-            if (i < 16) a += (d ^ (b & (c ^ d))) + X[       i  & 15];   \
-            else        a += ((d & b) | (~d & c))+ X[(1 + 5*i) & 15];   \
+            if (i < 16) a += (d ^ (b & (c ^ d)))  + X[       i  & 15];  \
+            else        a += ((d & b) | (~d & c)) + X[(1 + 5*i) & 15];  \
         } else {                                                        \
-            if (i < 48) a += (b ^ c ^ d)         + X[(5 + 3*i) & 15];   \
-            else        a += (c ^ (b | ~d))      + X[(    7*i) & 15];   \
+            if (i < 48) a += (b ^ c ^ d)          + X[(5 + 3*i) & 15];  \
+            else        a += (c ^ (b | ~d))       + X[(    7*i) & 15];  \
         }                                                               \
         a = b + (a << t | a >> (32 - t));                               \
     } while (0)
@@ -107,31 +107,31 @@ static void body(uint32_t ABCD[4], uint32_t *src, int nblocks)
         X = src + n * 16;
 
 #if HAVE_BIGENDIAN
-    for (i = 0; i < 16; i++)
-        X[i] = av_bswap32(X[i]);
+        for (i = 0; i < 16; i++)
+            X[i] = av_bswap32(X[i]);
 #endif
 
 #if CONFIG_SMALL
-    for (i = 0; i < 64; i++) {
-        CORE(i, a, b, c, d);
-        t = d;
-        d = c;
-        c = b;
-        b = a;
-        a = t;
-    }
+        for (i = 0; i < 64; i++) {
+            CORE(i, a, b, c, d);
+            t = d;
+            d = c;
+            c = b;
+            b = a;
+            a = t;
+        }
 #else
 #define CORE2(i)                                                        \
-    CORE( i,   a,b,c,d); CORE((i+1),d,a,b,c);                           \
-    CORE((i+2),c,d,a,b); CORE((i+3),b,c,d,a)
+        CORE( i,   a,b,c,d); CORE((i+1),d,a,b,c);                       \
+        CORE((i+2),c,d,a,b); CORE((i+3),b,c,d,a)
 #define CORE4(i) CORE2(i); CORE2((i+4)); CORE2((i+8)); CORE2((i+12))
-    CORE4(0); CORE4(16); CORE4(32); CORE4(48);
+        CORE4(0); CORE4(16); CORE4(32); CORE4(48);
 #endif
 
-    ABCD[0] += d;
-    ABCD[1] += c;
-    ABCD[2] += b;
-    ABCD[3] += a;
+        ABCD[0] += d;
+        ABCD[1] += c;
+        ABCD[2] += b;
+        ABCD[3] += a;
     }
 }
 
