87a2bf2459537b81ddeb64ae2ba7b4e8bae29250
Michael Niedermayer <michaelni@gmx.at>
2013-05-23 15:47:32 +0200

    avutil/bprint: Fix doxygen comments
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavutil/bprint.h
+++ libavutil/bprint.h
@@ -74,10 +74,10 @@
  */
 typedef struct AVBPrint {
     FF_PAD_STRUCTURE(1024,
-    char *str;         /** string so far */
-    unsigned len;      /** length so far */
-    unsigned size;     /** allocated memory */
-    unsigned size_max; /** maximum allocated memory */
+    char *str;         /**< string so far */
+    unsigned len;      /**< length so far */
+    unsigned size;     /**< allocated memory */
+    unsigned size_max; /**< maximum allocated memory */
     char reserved_internal_buffer[1];
     )
 } AVBPrint;
