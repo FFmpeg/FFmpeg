438c45c08a1f9f0e476d9e3edb1995da61192bce
Michael Niedermayer <michaelni@gmx.at>
2013-05-26 14:53:23 +0200

    j2kdec: fix used bytestream function
    Found-by: durandal_1707
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/j2kdec.c
+++ libavcodec/j2kdec.c
@@ -331,7 +331,7 @@ static int get_cod(Jpeg2000DecoderContext *s, Jpeg2000CodingStyle *c, uint8_t *p
     tmp.csty = bytestream2_get_byteu(&s->g);
 
     // get progression order
-    tmp.prog_order = bytestream_get_byte(&s->g);
+    tmp.prog_order = bytestream2_get_byteu(&s->g);
     if (tmp.prog_order){
         av_log(s->avctx, AV_LOG_ERROR, "only LRCP progression supported\n");
     }
