02923b8c781c4f52b3f6b4f3e541d7cec187035c
Michael Niedermayer <michaelni@gmx.at>
2013-05-30 18:30:42 +0200

    h264_cavlc: fix reading skip run
    Fixes Ticket2606
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>
    (cherry picked from commit 826b3a75cd295c03720e00d3de83e1abcbedd4b9)
    
    Conflicts:
    	libavcodec/h264_cavlc.c

--- libavcodec/h264_cavlc.c
+++ libavcodec/h264_cavlc.c
@@ -708,7 +708,7 @@ int ff_h264_decode_mb_cavlc(H264Context *h){
                 down the code */
     if(h->slice_type_nos != AV_PICTURE_TYPE_I){
         if(s->mb_skip_run==-1)
-            s->mb_skip_run= get_ue_golomb(&s->gb);
+            s->mb_skip_run= get_ue_golomb_long(&s->gb);
 
         if (s->mb_skip_run--) {
             if(FRAME_MBAFF && (s->mb_y&1) == 0){
