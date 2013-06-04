ff63ba55d23ac30ed15182a81d6129f86f51dc26
nuoerlz <nuoerlz@gmail.com>
2013-06-04 17:47:22 +0800

    Enable SSL support

--- iOS.sh
+++ iOS.sh
@@ -1,5 +1,8 @@
 DEST=`pwd`/build/ios
 SOURCE=`pwd`
+SSL=${SOURCE}/../ios-openssl
+SSLINCLUDE=${SSL}/include
+SSLLIBS=${SSL}/lib
 
 export FFMPEG_DIR=${SOURCE}
 
@@ -22,70 +25,67 @@ export LD=$DEVRoot/usr/bin/ld
 
 function doConfigure()
 {
-    #  *NEED* gas-preprocessor.pl file in PATH:
-    # wget https://github.com/jacobson/gas-preprocessor/blob/master/gas-preprocessor.pl
-    ./configure \
-        --prefix=${DIST} \
-        --cc=${CC} \
-        --as="gas-preprocessor.pl ${CC}" \
-        --sysroot=${SDKRoot} \
-        --enable-cross-compile \
-        --target-os=darwin \
-        --arch=arm \
-        --cpu=cortex-a8 \
-        --extra-cflags="-DVPLAYER_IOS -arch ${ARCH}" \
-        --extra-ldflags="-arch ${ARCH} -isysroot ${SDKRoot}" \
-				--disable-runtime-cpudetect \
-        --disable-symver \
-        --disable-doc \
-        --disable-ffplay \
-        --disable-ffmpeg \
-        --disable-ffprobe \
-        --disable-ffserver \
-				--disable-avdevice \
-				--disable-postproc \
-        --disable-encoders \
-        --disable-muxers \
-        --disable-devices \
-        --disable-demuxer=sbg \
-        --disable-demuxer=dts \
-        --disable-parser=dca \
-        --disable-decoder=dca \
-				--disable-decoder=svq3 \
-        --enable-muxer=mp4 \
-        --enable-protocols  \
-        --enable-parsers \
-        --enable-demuxers \
-        --enable-decoders \
-        --enable-network \
-        --enable-swscale  \
-        --enable-version3 \
-        \
-        --enable-avdevice \
-        --disable-asm \
-        --disable-shared
+	#  *NEED* gas-preprocessor.pl file in PATH:
+	# wget https://github.com/jacobson/gas-preprocessor/blob/master/gas-preprocessor.pl
+	./configure \
+		--prefix=${DIST} \
+		--cc=${CC} \
+		--as="gas-preprocessor.pl ${CC}" \
+		--sysroot=${SDKRoot} \
+		--enable-cross-compile \
+		--target-os=darwin \
+		--arch=arm \
+		--cpu=cortex-a8 \
+		--extra-cflags="-DVPLAYER_IOS -arch ${ARCH} -I${SSLINCLUDE}" \
+		--extra-ldflags="-arch ${ARCH} -isysroot ${SDKRoot} -L${SSLLIBS}" \
+		--disable-runtime-cpudetect \
+		--disable-symver \
+		--disable-doc \
+		--disable-ffplay \
+		--disable-ffmpeg \
+		--disable-ffprobe \
+		--disable-ffserver \
+		--disable-avdevice \
+		--disable-postproc \
+		--disable-encoders \
+		--disable-muxers \
+		--disable-devices \
+		--disable-demuxer=sbg \
+		--disable-demuxer=dts \
+		--disable-parser=dca \
+		--disable-decoder=dca \
+		--disable-decoder=svq3 \
+		--enable-muxer=mp4 \
+		--enable-protocols  \
+		--enable-parsers \
+		--enable-demuxers \
+		--enable-decoders \
+		--enable-network \
+		--enable-openssl \
+		--enable-swscale  \
+		--enable-version3 \
+		\
+		--enable-avdevice \
+		--disable-asm \
+		--disable-shared
 
-				#--enable-openssl \
-
-    [[ $? != 0 ]] && kill $$
+	[[ $? != 0 ]] && kill $$
 }
 
 
 for iarch in armv7; do
 #for iarch in armv7 armv7s; do
-    export ARCH=$iarch
-    export DIST=${DEST}/build-dist-$iarch
-    confInfo=${DIST}/configure-info.out
-    makeInfo=${DIST}/make-info.out
+	export ARCH=$iarch
+	export DIST=${DEST}/build-dist-$iarch
+	confInfo=${DIST}/configure-info.out
+	makeInfo=${DIST}/make-info.out
 
-    rm -rf $DIST
-    mkdir -p $DIST
-    cd $FFMPEG_DIR
+	rm -rf $DIST
+	mkdir -p $DIST
+	cd $FFMPEG_DIR
 
-    doConfigure | tee $confInfo
+	doConfigure | tee $confInfo
 
-    make clean && make && make install | tee $makeInfo
+	make clean && make && make install | tee $makeInfo
 done
 
-
-exit 0
