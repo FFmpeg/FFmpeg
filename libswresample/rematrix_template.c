d62030ffcaf14dd801918fe26e670d68fdc21984
Michael Niedermayer <michaelni@gmx.at>
2013-06-10 02:46:03 +0200

    swresample/rematrix_template: Fix integer overflow in mix8to2
    Fixes: CID1030352
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libswresample/rematrix_template.c
+++ libswresample/rematrix_template.c
@@ -76,7 +76,7 @@ static void RENAME(mix8to2)(SAMPLE **out, const SAMPLE **in, COEFF *coeffp, inte
     int i;
 
     for(i=0; i<len; i++) {
-        INTER t = in[2][i]*coeffp[0*8+2] + in[3][i]*coeffp[0*8+3];
+        INTER t = in[2][i]*(INTER)coeffp[0*8+2] + in[3][i]*(INTER)coeffp[0*8+3];
         out[0][i] = R(t + in[0][i]*(INTER)coeffp[0*8+0] + in[4][i]*(INTER)coeffp[0*8+4] + in[6][i]*(INTER)coeffp[0*8+6]);
         out[1][i] = R(t + in[1][i]*(INTER)coeffp[1*8+1] + in[5][i]*(INTER)coeffp[1*8+5] + in[7][i]*(INTER)coeffp[1*8+7]);
     }
