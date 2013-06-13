c4db4b17569fa4230f41fb758c147e50b3f1fa3f
Reuben Martin <reuben.m@gmail.com>
2013-06-12 22:21:35 -0500

    Added GXF format code to identify AVC Intra video streams. This was an extension to the origional GXF spec implemented by Grass Valley.
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/gxf.c
+++ libavformat/gxf.c
@@ -163,6 +163,11 @@ static int get_sindex(AVFormatContext *s, int id, int format) {
             st->codec->channel_layout = AV_CH_LAYOUT_STEREO;
             st->codec->sample_rate = 48000;
             break;
+        case 26: /* AVCi50 / AVCi100 (AVC Intra) */
+            st->codec->codec_type = AVMEDIA_TYPE_VIDEO;
+            st->codec->codec_id = CODEC_ID_H264;
+            st->need_parsing = AVSTREAM_PARSE_HEADERS;
+            break;
         // timecode tracks:
         case 7:
         case 8:
