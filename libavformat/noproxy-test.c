ec7c51c7868d3ccc66b5cc38bf126258b94f086c
Luca Barbato <lu_zero@gentoo.org>
2013-06-15 11:41:36 +0200

    avf: move ff_http_match_no_proxy to network
    It is only used by network protocols.

--- libavformat/noproxy-test.c
+++ libavformat/noproxy-test.c
@@ -18,7 +18,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
-#include "internal.h"
+#include "network.h"
 
 static void test(const char *pattern, const char *host)
 {
