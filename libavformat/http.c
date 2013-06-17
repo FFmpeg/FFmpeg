de12b454f21675e18127cd8b549c53deec7cd048
Clément Bœsch <ubitux@gmail.com>
2013-06-17 17:03:15 +0200

    lavf/http: remove Mozilla/5.0 from user agent.
    It is notably known to break playback on http streaming servers who use
    the user agent to guess if it's a browser (to display a summary) or a
    player (to stream the audio data).
    
    This reverts 1fabd95.
    
    Fixes Ticket #2663.

--- libavformat/http.c
+++ libavformat/http.c
@@ -70,7 +70,7 @@ typedef struct {
 #define OFFSET(x) offsetof(HTTPContext, x)
 #define D AV_OPT_FLAG_DECODING_PARAM
 #define E AV_OPT_FLAG_ENCODING_PARAM
-#define DEFAULT_USER_AGENT "Mozilla/5.0 Lavf/" AV_STRINGIFY(LIBAVFORMAT_VERSION)
+#define DEFAULT_USER_AGENT "Lavf/" AV_STRINGIFY(LIBAVFORMAT_VERSION)
 static const AVOption options[] = {
 {"seekable", "control seekability of connection", OFFSET(seekable), AV_OPT_TYPE_INT, {.i64 = -1}, -1, 1, D },
 {"chunked_post", "use chunked transfer-encoding for posts", OFFSET(chunked_post), AV_OPT_TYPE_INT, {.i64 = 1}, 0, 1, E },
