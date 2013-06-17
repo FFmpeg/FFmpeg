054779625f76bcc3dea9ff1175f4eedc8847c187
Janne Grunau <janne-libav@jannau.net>
2013-06-18 01:56:06 +0200

    vaapi: return early from ff_vaapi_render_picture() without picture
    Fixes an assertion when called on uninitialized frame. Spotted after
    seeking in vlc.
    Fixes ticket #2461.
    
    Tested-by: Timo
    Reviewed-by: Joakim Plate
    Signed-off-by: Carl Eugen Hoyos <cehoyos@ag.or.at>
    (cherry picked from commit 2ee380aefbb92c89e8e9c7aa5846aa404919ec7c)

--- libavcodec/vaapi.c
+++ libavcodec/vaapi.c
@@ -46,6 +46,9 @@ int ff_vaapi_render_picture(struct vaapi_context *vactx, VASurfaceID surface)
     VABufferID va_buffers[3];
     unsigned int n_va_buffers = 0;
 
+    if (!vactx->pic_param_buf_id)
+        return 0;
+
     vaUnmapBuffer(vactx->display, vactx->pic_param_buf_id);
     va_buffers[n_va_buffers++] = vactx->pic_param_buf_id;
 
