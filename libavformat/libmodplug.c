e4bb67bc506924e1eb115b04202aebd2a31374bc
Michael Niedermayer <michaelni@gmx.at>
2013-06-21 01:11:45 +0200

    avformat/libmodplug: Reduce the probe score for small input
    This ensures that theres enough data for mpeg_probe() to recognize mpeg-ps
    Fixes Ticket2583
    
    Based on code by: Paul B Mahol <onemda@gmail.com>
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>
    (cherry picked from commit c605adbf562fbf535e83ae427bb681bc45e440c8)

--- libavformat/libmodplug.c
+++ libavformat/libmodplug.c
@@ -347,6 +347,19 @@ static int modplug_read_seek(AVFormatContext *s, int stream_idx, int64_t ts, int
     return 0;
 }
 
+static const char modplug_extensions[] = "669,abc,amf,ams,dbm,dmf,dsm,far,it,mdl,med,mid,mod,mt2,mtm,okt,psm,ptm,s3m,stm,ult,umx,xm,itgz,itr,itz,mdgz,mdr,mdz,s3gz,s3r,s3z,xmgz,xmr,xmz";
+
+static int modplug_probe(AVProbeData *p)
+{
+    if (av_match_ext(p->filename, modplug_extensions)) {
+        if (p->buf_size < 16384)
+            return AVPROBE_SCORE_MAX/4-1;
+        else
+            return AVPROBE_SCORE_MAX/2;
+    }
+    return 0;
+}
+
 static const AVClass modplug_class = {
     .class_name = "ModPlug demuxer",
     .item_name  = av_default_item_name,
@@ -358,11 +371,11 @@ AVInputFormat ff_libmodplug_demuxer = {
     .name           = "libmodplug",
     .long_name      = NULL_IF_CONFIG_SMALL("ModPlug demuxer"),
     .priv_data_size = sizeof(ModPlugContext),
+    .read_probe     = modplug_probe,
     .read_header    = modplug_read_header,
     .read_packet    = modplug_read_packet,
     .read_close     = modplug_read_close,
     .read_seek      = modplug_read_seek,
-    .extensions     = "669,abc,amf,ams,dbm,dmf,dsm,far,it,mdl,med,mid,mod,mt2,mtm,okt,psm,ptm,s3m,stm,ult,umx,xm"
-                      ",itgz,itr,itz,mdgz,mdr,mdz,s3gz,s3r,s3z,xmgz,xmr,xmz", // compressed mods
+    .extensions     = modplug_extensions,
     .priv_class     = &modplug_class,
 };
