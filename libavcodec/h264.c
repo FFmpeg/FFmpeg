2005fddcbb4e18e8f7c34326e40609e4a2d83c31
Michael Niedermayer <michaelni@gmx.at>
2013-06-21 19:51:30 +0200

    h264/ff_h264_check_intra_pred_mode: fix input value check
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/h264.c
+++ libavcodec/h264.c
@@ -481,7 +481,7 @@ int ff_h264_check_intra_pred_mode(H264Context *h, int mode, int is_chroma)
     static const int8_t top[7]  = { LEFT_DC_PRED8x8, 1, -1, -1 };
     static const int8_t left[7] = { TOP_DC_PRED8x8, -1, 2, -1, DC_128_PRED8x8 };
 
-    if (mode > 6U) {
+    if (mode > 3U) {
         av_log(h->avctx, AV_LOG_ERROR,
                "out of range intra chroma pred mode at %d %d\n",
                h->mb_x, h->mb_y);
