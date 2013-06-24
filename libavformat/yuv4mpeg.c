cea8a0077fc597a11838f2f7242c01722d577b44
Ronald S. Bultje <rsbultje@gmail.com>
2013-06-24 19:13:58 -0400

    yuv4mpeg: correctly handle chroma for odd luma sizes.
    Signed-off-by: Derek Buitenhuis <derek.buitenhuis@gmail.com>

--- libavformat/yuv4mpeg.c
+++ libavformat/yuv4mpeg.c
@@ -217,8 +217,8 @@ static int yuv4_write_packet(AVFormatContext *s, AVPacket *pkt)
         // Adjust for smaller Cb and Cr planes
         av_pix_fmt_get_chroma_sub_sample(st->codec->pix_fmt, &h_chroma_shift,
                                          &v_chroma_shift);
-        width  >>= h_chroma_shift;
-        height >>= v_chroma_shift;
+        width  = FF_CEIL_RSHIFT(width,  h_chroma_shift);
+        height = FF_CEIL_RSHIFT(height, v_chroma_shift);
 
         ptr1 = picture->data[1];
         ptr2 = picture->data[2];
