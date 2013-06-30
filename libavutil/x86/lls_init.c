247425241cb3b2b76df1c2aced5ce0d56126b82d
Michael Niedermayer <michaelni@gmx.at>
2013-06-30 18:58:06 +0200

    avutil/x86: disable ff_evaluate_lls_sse2() for 32bit
    It just segfaults on 32bit, thus its disabled until someone fixes it.
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavutil/x86/lls_init.c
+++ libavutil/x86/lls_init.c
@@ -32,7 +32,7 @@ av_cold void ff_init_lls_x86(LLSModel *m)
     int cpu_flags = av_get_cpu_flags();
     if (EXTERNAL_SSE2(cpu_flags)) {
         m->update_lls = ff_update_lls_sse2;
-        if (m->indep_count >= 4)
+        if (m->indep_count >= 4 && ARCH_X86_64)
             m->evaluate_lls = ff_evaluate_lls_sse2;
     }
     if (EXTERNAL_AVX(cpu_flags) && 0) {
