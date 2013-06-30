186ec1784336df11df48e646595e2ec646f82329
Michael Niedermayer <michaelni@gmx.at>
2013-06-30 18:18:13 +0200

    avformat/aviobuf: Add ffio_ensure_seekback()
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/aviobuf.c
+++ libavformat/aviobuf.c
@@ -725,6 +725,31 @@ int ffio_fdopen(AVIOContext **s, URLContext *h)
     return 0;
 }
 
+int ffio_ensure_seekback(AVIOContext *s, int buf_size)
+{
+    uint8_t *buffer;
+    int max_buffer_size = s->max_packet_size ?
+                          s->max_packet_size : IO_BUFFER_SIZE;
+
+    buf_size += s->buf_ptr - s->buffer + max_buffer_size;
+
+    if (buf_size < s->buffer_size || s->seekable)
+        return 0;
+    av_assert0(!s->write_flag);
+
+    buffer = av_malloc(buf_size);
+    if (!buffer)
+        return AVERROR(ENOMEM);
+
+    memcpy(buffer, s->buffer, s->buffer_size);
+    av_free(s->buffer);
+    s->buf_ptr = buffer + (s->buf_ptr - s->buffer);
+    s->buf_end = buffer + (s->buf_end - s->buffer);
+    s->buffer = buffer;
+    s->buffer_size = buf_size;
+    return 0;
+}
+
 int ffio_set_buf_size(AVIOContext *s, int buf_size)
 {
     uint8_t *buffer;
