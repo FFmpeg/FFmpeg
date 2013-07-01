78962d3df49afe5011b572656ecfe940bd5fbf2e
Michael Niedermayer <michaelni@gmx.at>
2013-07-01 10:01:11 +0200

    jpeg2000: Check that nreslevels2decode has been initialized before use
    Avoid buffer overread.
    
    Found-by: Mateusz "j00ru" Jurczyk and Gynvael Coldwind
    Signed-off-by: Luca Barbato <lu_zero@gentoo.org>

--- libavcodec/jpeg2000.c
+++ libavcodec/jpeg2000.c
@@ -206,6 +206,11 @@ int ff_jpeg2000_init_component(Jpeg2000Component *comp,
     int reslevelno, bandno, gbandno = 0, ret, i, j;
     uint32_t csize = 1;
 
+    if (!codsty->nreslevels2decode) {
+        av_log(avctx, AV_LOG_ERROR, "nreslevels2decode uninitialized\n");
+        return AVERROR_INVALIDDATA;
+    }
+
     if (ret = ff_jpeg2000_dwt_init(&comp->dwt, comp->coord,
                                    codsty->nreslevels2decode - 1,
                                    codsty->transform))
