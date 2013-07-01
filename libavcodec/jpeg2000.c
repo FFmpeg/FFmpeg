690c8af97bb1c9a57d5d233a5ef76409ff9b15b2
Michael Niedermayer <michaelni@gmx.at>
2013-07-01 10:01:23 +0200

    jpeg2000: Remove unneeded variable initialization
    Signed-off-by: Luca Barbato <lu_zero@gentoo.org>

--- libavcodec/jpeg2000.c
+++ libavcodec/jpeg2000.c
@@ -202,7 +202,7 @@ int ff_jpeg2000_init_component(Jpeg2000Component *comp,
 {
     uint8_t log2_band_prec_width, log2_band_prec_height;
     int reslevelno, bandno, gbandno = 0, ret, i, j;
-    uint32_t csize = 1;
+    uint32_t csize;
 
     if (!codsty->nreslevels2decode) {
         av_log(avctx, AV_LOG_ERROR, "nreslevels2decode uninitialized\n");
