a478e99a603f2abf3b65bc96176af7b278555035
Michael Niedermayer <michaelni@gmx.at>
2013-07-02 11:58:08 +0200

    avutil/x86: reenable ff_update_lls_avx()
    The bug has been fixed in c8b920a9b7fa534a6141695ace4e8c2dfcd56cee by Loren Merritt
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavutil/x86/lls.asm
+++ libavutil/x86/lls.asm
@@ -125,7 +125,7 @@ cglobal update_lls, 2,5,8, ctx, var, i, j, covar2
 .ret:
     REP_RET
 
-%if HAVE_AVX_EXTERNAL && 0
+%if HAVE_AVX_EXTERNAL
 INIT_YMM avx
 cglobal update_lls, 3,6,8, ctx, var, count, i, j, count2
     %define covarq ctxq
