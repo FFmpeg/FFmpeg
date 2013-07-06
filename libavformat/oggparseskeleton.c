63d7684fefcfa9de1a04b43471f876e882ba7aac
James Almer <jamrial@gmail.com>
2013-07-06 04:34:13 -0300

    oggparseskeleton: Replace avpriv_report_missing_feature() with a normal av_log() call
    since there should not be more than one fisbone for a given stream.
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/oggparseskeleton.c
+++ libavformat/oggparseskeleton.c
@@ -80,8 +80,7 @@ static int skeleton_header(AVFormatContext *s, int idx)
         }
         os = ogg->streams + target_idx;
         if (os->start_granule != OGG_NOGRANULE_VALUE) {
-            avpriv_report_missing_feature(s,
-                                          "Multiple fisbone for the same stream");
+            av_log(s, AV_LOG_WARNING, "Multiple fisbone for the same stream\n");
             return 1;
         }
         if (start_granule != OGG_NOGRANULE_VALUE) {
