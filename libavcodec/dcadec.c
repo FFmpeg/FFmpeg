c03533ace27c9553cf716bb7a8336d258b4c2018
Luca Barbato <lu_zero@gentoo.org>
2013-07-10 19:00:15 +0200

    dca: Respect the current limits in the downmixing capabilities
    Reported-by: Mateusz "j00ru" Jurczyk and Gynvael Coldwind
    CC: libav-stable@libav.org
    (cherry picked from commit 3802833bc1f79775a1547c5e427fed6e92b77e53)
    
    Signed-off-by: Luca Barbato <lu_zero@gentoo.org>

--- libavcodec/dcadec.c
+++ libavcodec/dcadec.c
@@ -809,6 +809,12 @@ static int dca_subframe_header(DCAContext *s, int base_channel, int block_index)
                        "Invalid channel mode %d\n", am);
                 return AVERROR_INVALIDDATA;
             }
+            if (s->prim_channels > FF_ARRAY_ELEMS(dca_default_coeffs[0])) {
+                av_log_ask_for_sample(s->avctx, "Downmixing %d channels",
+                                      s->prim_channels);
+                return AVERROR_PATCHWELCOME;
+            }
+
             for (j = base_channel; j < s->prim_channels; j++) {
                 s->downmix_coef[j][0] = dca_default_coeffs[am][j][0];
                 s->downmix_coef[j][1] = dca_default_coeffs[am][j][1];
