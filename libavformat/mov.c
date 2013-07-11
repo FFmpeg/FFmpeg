38ce775eead70b84272ee4ba908b6db3516e7cbd
Thierry Foucu <tfoucu@gmail.com>
2013-07-11 14:37:59 -0700

    libavformat/mov.c: Skipped duplicated MOOV atom
     This should fix ticket 1378
     If we have parsed a moov atom, and found another one, just skip it.
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/mov.c
+++ libavformat/mov.c
@@ -785,6 +785,12 @@ static int mov_read_moov(MOVContext *c, AVIOContext *pb, MOVAtom atom)
 {
     int ret;
 
+    if (c->found_moov) {
+        av_log(c->fc, AV_LOG_WARNING, "Found duplicated MOOV Atom. Skipped it\n");
+        avio_skip(pb, atom.size);
+        return 0;
+    }
+
     if ((ret = mov_read_default(c, pb, atom)) < 0)
         return ret;
     /* we parsed the 'moov' atom, we can terminate the parsing as soon as we find the 'mdat' */
