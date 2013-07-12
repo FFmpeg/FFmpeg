f56fe04de3cc4cdc6b618dfe7a3506fc2ea79795
Nicolas Bertrand <nicoinattendu@gmail.com>
2013-07-12 23:15:43 +0200

    jpeg2000: Initialize only once mqc arrays
    Improves decoding speed.
    
    Signed-off-by: Luca Barbato <lu_zero@gentoo.org>

--- libavcodec/mqc.c
+++ libavcodec/mqc.c
@@ -92,14 +92,9 @@ uint16_t ff_mqc_qe [2 * 47];
 uint8_t ff_mqc_nlps[2 * 47];
 uint8_t ff_mqc_nmps[2 * 47];
 
-void ff_mqc_init_contexts(MqcState *mqc)
+void ff_mqc_init_context_tables(void)
 {
     int i;
-    memset(mqc->cx_states, 0, sizeof(mqc->cx_states));
-    mqc->cx_states[MQC_CX_UNI] = 2 * 46;
-    mqc->cx_states[MQC_CX_RL]  = 2 * 3;
-    mqc->cx_states[0]          = 2 * 4;
-
     for (i = 0; i < 47; i++) {
         ff_mqc_qe[2 * i]     =
         ff_mqc_qe[2 * i + 1] = cx_states[i].qe;
@@ -109,4 +104,12 @@ void ff_mqc_init_contexts(MqcState *mqc)
         ff_mqc_nmps[2 * i]     = 2 * cx_states[i].nmps;
         ff_mqc_nmps[2 * i + 1] = 2 * cx_states[i].nmps + 1;
     }
+}
+
+void ff_mqc_init_contexts(MqcState *mqc)
+{
+    memset(mqc->cx_states, 0, sizeof(mqc->cx_states));
+    mqc->cx_states[MQC_CX_UNI] = 2 * 46;
+    mqc->cx_states[MQC_CX_RL]  = 2 * 3;
+    mqc->cx_states[0]          = 2 * 4;
 }
