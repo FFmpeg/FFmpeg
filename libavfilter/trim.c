e9678631f16bd3a599a01853ce827dc3d7b63516
Paul B Mahol <onemda@gmail.com>
2013-07-13 21:53:34 +0000

    lavfi/trim: fix sample copy for >8 channels
    Signed-off-by: Paul B Mahol <onemda@gmail.com>

--- libavfilter/trim.c
+++ libavfilter/trim.c
@@ -333,7 +333,7 @@ static int atrim_filter_frame(AVFilterLink *inlink, AVFrame *frame)
 
         av_frame_copy_props(out, frame);
         av_samples_copy(out->extended_data, frame->extended_data, 0, start_sample,
-                        out->nb_samples, av_get_channel_layout_nb_channels(frame->channel_layout),
+                        out->nb_samples, inlink->channels,
                         frame->format);
         if (out->pts != AV_NOPTS_VALUE)
             out->pts += av_rescale_q(start_sample, (AVRational){ 1, out->sample_rate },
