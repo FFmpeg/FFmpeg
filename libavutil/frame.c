0a06e6e49b9b50d74dee2506139076bfe3606834
Michael Niedermayer <michaelni@gmx.at>
2013-07-13 20:58:18 +0200

    av_frame_get_buffer: Do not fail when the channel layout is unknown
    Fixes handling of cases with unknown layouts but known channel counts
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavutil/frame.c
+++ libavutil/frame.c
@@ -223,7 +223,7 @@ int av_frame_get_buffer(AVFrame *frame, int align)
 
     if (frame->width > 0 && frame->height > 0)
         return get_video_buffer(frame, align);
-    else if (frame->nb_samples > 0 && frame->channel_layout)
+    else if (frame->nb_samples > 0 && (frame->channel_layout || frame->channels > 0))
         return get_audio_buffer(frame, align);
 
     return AVERROR(EINVAL);
