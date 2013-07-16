6b68e2a43b3407522080be50a2a19cff2f9715ef
Paul B Mahol <onemda@gmail.com>
2013-07-16 21:39:06 +0000

    lavfi: add compand filter
    Signed-off-by: Paul B Mahol <onemda@gmail.com>

--- libavfilter/allfilters.c
+++ libavfilter/allfilters.c
@@ -80,6 +80,7 @@ void avfilter_register_all(void)
     REGISTER_FILTER(BIQUAD,         biquad,         af);
     REGISTER_FILTER(CHANNELMAP,     channelmap,     af);
     REGISTER_FILTER(CHANNELSPLIT,   channelsplit,   af);
+    REGISTER_FILTER(COMPAND,        compand,        af);
     REGISTER_FILTER(EARWAX,         earwax,         af);
     REGISTER_FILTER(EBUR128,        ebur128,        af);
     REGISTER_FILTER(EQUALIZER,      equalizer,      af);
