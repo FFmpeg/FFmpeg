9a6d3eee599508ba70d711df6927daa9b8c3cc5a
Michael Niedermayer <michaelni@gmx.at>
2013-07-19 17:54:57 +0200

    jpeg2000dec: silence unused variable warning
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>
    (cherry picked from commit db33010483a024f1343bfb516415fe9ef77e82d9)

--- libavcodec/jpeg2000dec.c
+++ libavcodec/jpeg2000dec.c
@@ -1557,7 +1557,7 @@ static int jp2_find_codestream(Jpeg2000DecoderContext *s)
                     atom2_size -= 2;
                     for (; n>0; n--) {
                         int cn   = bytestream2_get_be16(&s->g);
-                        int typ  = bytestream2_get_be16(&s->g);
+                        int av_unused typ  = bytestream2_get_be16(&s->g);
                         int asoc = bytestream2_get_be16(&s->g);
                         if (cn < 4 || asoc < 4)
                             s->cdef[cn] = asoc;
