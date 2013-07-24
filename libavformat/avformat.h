a553cb4683a2da078166cbfd7b839c3b3ef5dbe2
Anshul Maheshwari <er.anshul.maheshwari@gmail.com>
2013-07-24 12:20:02 +0530

    documented that av_codec_close is required to call
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/avformat.h
+++ libavformat/avformat.h
@@ -1405,6 +1405,9 @@ const AVClass *avformat_get_class(void);
  *
  * When muxing, should be called by the user before avformat_write_header().
  *
+ * User is required to call avcodec_close() and avformat_free_context() to
+ * clean up the allocation by avformat_new_stream().
+ *
  * @param c If non-NULL, the AVCodecContext corresponding to the new stream
  * will be initialized to use this codec. This is needed for e.g. codec-specific
  * defaults to be set, so codec should be provided if it is known.
