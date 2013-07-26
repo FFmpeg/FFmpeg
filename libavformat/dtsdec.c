18043e3d2282ec19ff541335b2bb9f2c75a3e270
Michael Niedermayer <michaelni@gmx.at>
2013-07-26 04:09:30 +0200

    avformat/dtsdec: Improve probe, reject things looking like analoge signals
    Fixes Ticket2810
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>
    (cherry picked from commit 6663205338f57eedb2392263dde48e2717c6e980)

--- libavformat/dtsdec.c
+++ libavformat/dtsdec.c
@@ -34,6 +34,7 @@ static int dts_probe(AVProbeData *p)
     uint32_t state = -1;
     int markers[3] = {0};
     int sum, max;
+    int64_t diff = 0;
 
     buf = p->buf;
 
@@ -54,12 +55,16 @@ static int dts_probe(AVProbeData *p)
         if (state == DCA_MARKER_14B_LE)
             if ((bytestream_get_be16(&bufp) & 0xF0FF) == 0xF007)
                 markers[2]++;
+
+        if (buf - p->buf >= 4)
+            diff += FFABS(AV_RL16(buf) - AV_RL16(buf-4));
     }
     sum = markers[0] + markers[1] + markers[2];
     max = markers[1] > markers[0];
     max = markers[2] > markers[max] ? 2 : max;
     if (markers[max] > 3 && p->buf_size / markers[max] < 32*1024 &&
-        markers[max] * 4 > sum * 3)
+        markers[max] * 4 > sum * 3 &&
+        diff / p->buf_size > 200)
         return AVPROBE_SCORE_EXTENSION + 1;
 
     return 0;
