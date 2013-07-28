c5b46a064d9991f2cd045c90179fccf35ecffc34
Michael Niedermayer <michaelni@gmx.at>
2013-07-28 20:04:05 +0300

    lavf: Don't interpret just slightly broken timestamps as wraparound
    This avoids breaking some slightly incorrect (dts > pts) timestamps
    in sample HLS streams from Apple.
    
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- libavformat/utils.c
+++ libavformat/utils.c
@@ -727,8 +727,9 @@ static void compute_pkt_fields(AVFormatContext *s, AVStream *st,
         pc && pc->pict_type != AV_PICTURE_TYPE_B)
         presentation_delayed = 1;
 
-    if(pkt->pts != AV_NOPTS_VALUE && pkt->dts != AV_NOPTS_VALUE && pkt->dts > pkt->pts && st->pts_wrap_bits<63
-       /*&& pkt->dts-(1LL<<st->pts_wrap_bits) < pkt->pts*/){
+    if (pkt->pts != AV_NOPTS_VALUE && pkt->dts != AV_NOPTS_VALUE &&
+        st->pts_wrap_bits < 63 &&
+        pkt->dts - (1LL << (st->pts_wrap_bits - 1)) > pkt->pts) {
         pkt->dts -= 1LL<<st->pts_wrap_bits;
     }
 
