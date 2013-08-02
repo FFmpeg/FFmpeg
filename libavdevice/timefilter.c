2b9590ebab8baef557d5d0f5daeedd386983bbc0
Michael Niedermayer <michaelni@gmx.at>
2013-08-02 16:59:03 +0200

    avdevice/timefilter-test: dont try to optimize par1 for n0=0 case
    for the n0=0 case there are multiple solutions and different
    platforms pick different ones
    This should reduce the issues with fate and the timefilter test
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavdevice/timefilter.c
+++ libavdevice/timefilter.c
@@ -118,7 +118,8 @@ int main(void)
                 double par0, par1;
                 better = 0;
                 for (par0 = bestpar0 * 0.8; par0 <= bestpar0 * 1.21; par0 += bestpar0 * 0.05) {
-                    for (par1 = bestpar1 * 0.8; par1 <= bestpar1 * 1.21; par1 += bestpar1 * 0.05) {
+                    double range = n0 ? 0.8 : 1.0;
+                    for (par1 = bestpar1 * range; par1 <= bestpar1 * (2.01 - range); par1 += bestpar1 * 0.05) {
                         double error   = 0;
                         TimeFilter *tf = ff_timefilter_new(1, par0, par1);
                         for (i = 0; i < SAMPLES; i++) {
