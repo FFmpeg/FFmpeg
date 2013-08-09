a76d0cdf21c3d9e464623cc0ad1c005abf952afa
Martin Storsjö <martin@martin.st>
2013-08-09 10:58:15 +0300

    libavutil: Move avpriv_open to a new file, file_open.c
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- libavutil/file.c
+++ libavutil/file.c
@@ -21,7 +21,6 @@
 #include "internal.h"
 #include "log.h"
 #include "mem.h"
-#include <stdarg.h>
 #include <fcntl.h>
 #include <sys/stat.h>
 #if HAVE_UNISTD_H
@@ -35,69 +34,6 @@
 #elif HAVE_MAPVIEWOFFILE
 #include <windows.h>
 #endif
-
-#if defined(_WIN32) && !defined(__MINGW32CE__)
-#undef open
-#undef lseek
-#undef stat
-#undef fstat
-#include <windows.h>
-#include <share.h>
-#include <errno.h>
-
-static int win32_open(const char *filename_utf8, int oflag, int pmode)
-{
-    int fd;
-    int num_chars;
-    wchar_t *filename_w;
-
-    /* convert UTF-8 to wide chars */
-    num_chars = MultiByteToWideChar(CP_UTF8, MB_ERR_INVALID_CHARS, filename_utf8, -1, NULL, 0);
-    if (num_chars <= 0)
-        goto fallback;
-    filename_w = av_mallocz(sizeof(wchar_t) * num_chars);
-    if (!filename_w) {
-        errno = ENOMEM;
-        return -1;
-    }
-    MultiByteToWideChar(CP_UTF8, 0, filename_utf8, -1, filename_w, num_chars);
-
-    fd = _wsopen(filename_w, oflag, SH_DENYNO, pmode);
-    av_freep(&filename_w);
-
-    if (fd != -1 || (oflag & O_CREAT))
-        return fd;
-
-fallback:
-    /* filename may be be in CP_ACP */
-    return _sopen(filename_utf8, oflag, SH_DENYNO, pmode);
-}
-#define open win32_open
-#endif
-
-int avpriv_open(const char *filename, int flags, ...)
-{
-    int fd;
-    unsigned int mode = 0;
-    va_list ap;
-
-    va_start(ap, flags);
-    if (flags & O_CREAT)
-        mode = va_arg(ap, unsigned int);
-    va_end(ap);
-
-#ifdef O_CLOEXEC
-    flags |= O_CLOEXEC;
-#endif
-
-    fd = open(filename, flags, mode);
-#if HAVE_FCNTL
-    if (fd != -1)
-        fcntl(fd, F_SETFD, FD_CLOEXEC);
-#endif
-
-    return fd;
-}
 
 typedef struct {
     const AVClass *class;
