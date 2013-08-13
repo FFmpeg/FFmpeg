8e0b6d82b3dd515ac8b371c2eec549036b9d0bae
Compn <tempn@twmi.rr.com>
2013-08-13 09:41:28 -0400

    riff: add msn audio comment

--- libavformat/riff.c
+++ libavformat/riff.c
@@ -372,7 +372,7 @@ const AVCodecTag ff_codec_wav_tags[] = {
     { AV_CODEC_ID_ADPCM_YAMAHA,    0x0020 },
     { AV_CODEC_ID_TRUESPEECH,      0x0022 },
     { AV_CODEC_ID_GSM_MS,          0x0031 },
-    { AV_CODEC_ID_GSM_MS,          0x0032 },
+    { AV_CODEC_ID_GSM_MS,          0x0032 },  /* msn audio */
     { AV_CODEC_ID_AMR_NB,          0x0038 },  /* rogue format number */
     { AV_CODEC_ID_G723_1,          0x0042 },
     { AV_CODEC_ID_ADPCM_G726,      0x0045 },
