68b63a3432016756a4a47396a8c522ad0c217e29
Michael Niedermayer <michaelni@gmx.at>
2013-08-20 00:14:24 +0200

    mpegvideo: Use picture_ptr instead of picture in ff_mpeg_draw_horiz_band()
    Fixes strides and issue with mplayer and mpeg2 field pictures
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/mpegvideo.c
+++ libavcodec/mpegvideo.c
@@ -2989,8 +2989,8 @@ void ff_draw_horiz_band(AVCodecContext *avctx, DSPContext *dsp, Picture *cur,
 void ff_mpeg_draw_horiz_band(MpegEncContext *s, int y, int h)
 {
     int draw_edges = s->unrestricted_mv && !s->intra_only;
-    ff_draw_horiz_band(s->avctx, &s->dsp, &s->current_picture,
-                       &s->last_picture, y, h, s->picture_structure,
+    ff_draw_horiz_band(s->avctx, &s->dsp, s->current_picture_ptr,
+                       s->last_picture_ptr, y, h, s->picture_structure,
                        s->first_field, draw_edges, s->low_delay,
                        s->v_edge_pos, s->h_edge_pos);
 }
