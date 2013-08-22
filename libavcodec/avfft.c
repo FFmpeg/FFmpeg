04dcdc464087eece349f30db42bab903cd077778
Nicolas George <nicolas.george@normalesup.org>
2013-08-22 20:25:09 +0200

    lavc/avfft: init context to 0.
    Prevent an invalid free in case of init failure.

--- libavcodec/avfft.c
+++ libavcodec/avfft.c
@@ -27,7 +27,7 @@
 
 FFTContext *av_fft_init(int nbits, int inverse)
 {
-    FFTContext *s = av_malloc(sizeof(*s));
+    FFTContext *s = av_mallocz(sizeof(*s));
 
     if (s && ff_fft_init(s, nbits, inverse))
         av_freep(&s);
