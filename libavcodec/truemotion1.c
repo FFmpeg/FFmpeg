83b915d495eab386c07a4ec7859acaf64fcf58c9
Paul B Mahol <onemda@gmail.com>
2013-08-23 23:12:42 +0000

    truemotion1: use av_freep()
    Signed-off-by: Paul B Mahol <onemda@gmail.com>

--- libavcodec/truemotion1.c
+++ libavcodec/truemotion1.c
@@ -891,7 +891,7 @@ static av_cold int truemotion1_decode_end(AVCodecContext *avctx)
     TrueMotion1Context *s = avctx->priv_data;
 
     av_frame_unref(&s->frame);
-    av_free(s->vert_pred);
+    av_freep(&s->vert_pred);
 
     return 0;
 }
