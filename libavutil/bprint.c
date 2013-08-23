0b46d6f3efa7c647a42d3e77d09c274f611b752b
Lukasz Marek <lukasz.m.luki@gmail.com>
2013-08-23 23:21:50 +0200

    lavu/bprint: add append buffer function
    Signed-off-by: Lukasz Marek <lukasz.m.luki@gmail.com>
    Reveiwed-by: Nicolas George <nicolas.george@normalesup.org>
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavutil/bprint.c
+++ libavutil/bprint.c
@@ -155,6 +155,24 @@ void av_bprint_chars(AVBPrint *buf, char c, unsigned n)
     av_bprint_grow(buf, n);
 }
 
+void av_bprint_append_data(AVBPrint *buf, const char *data, unsigned size)
+{
+    unsigned room, real_n;
+
+    while (1) {
+        room = av_bprint_room(buf);
+        if (size < room)
+            break;
+        if (av_bprint_alloc(buf, size))
+            break;
+    }
+    if (room) {
+        real_n = FFMIN(size, room - 1);
+        memcpy(buf->str + buf->len, data, real_n);
+    }
+    av_bprint_grow(buf, size);
+}
+
 void av_bprint_strftime(AVBPrint *buf, const char *fmt, const struct tm *tm)
 {
     unsigned room;
