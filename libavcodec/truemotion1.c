b8ff4f5ea3d321b7922e6862aab521ec18830d88
Paul B Mahol <onemda@gmail.com>
2013-08-23 23:16:13 +0000

    truemotion1: check av_fast_malloc() return value
    Signed-off-by: Paul B Mahol <onemda@gmail.com>

--- libavcodec/truemotion1.c
+++ libavcodec/truemotion1.c
@@ -407,6 +407,8 @@ static int truemotion1_decode_header(TrueMotion1Context *s)
         s->avctx->pix_fmt = new_pix_fmt;
         avcodec_set_dimensions(s->avctx, s->w, s->h);
         av_fast_malloc(&s->vert_pred, &s->vert_pred_size, s->avctx->width * sizeof(unsigned int));
+        if (!s->vert_pred)
+            return AVERROR(ENOMEM);
     }
 
     /* There is 1 change bit per 4 pixels, so each change byte represents
@@ -473,6 +475,8 @@ static av_cold int truemotion1_decode_init(AVCodecContext *avctx)
     /* there is a vertical predictor for each pixel in a line; each vertical
      * predictor is 0 to start with */
     av_fast_malloc(&s->vert_pred, &s->vert_pred_size, s->avctx->width * sizeof(unsigned int));
+    if (!s->vert_pred)
+        return AVERROR(ENOMEM);
 
     return 0;
 }
