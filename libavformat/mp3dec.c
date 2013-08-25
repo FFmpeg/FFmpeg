8d679bc7e665d23ffabee93554ee90212b7a7c20
Michael Niedermayer <michaelni@gmx.at>
2013-08-26 00:57:35 +0200

    avformat/mp3dec: improve detection of mp3s with huge id3 tags
    When a id3 tag is larger than the amount of space we will probe
    give it a score similar to what a .mp3 extension would have given it
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/mp3dec.c
+++ libavformat/mp3dec.c
@@ -85,7 +85,7 @@ static int mp3_read_probe(AVProbeData *p)
     else if(max_frames>200)return AVPROBE_SCORE_EXTENSION;
     else if(max_frames>=4) return AVPROBE_SCORE_EXTENSION / 2;
     else if(ff_id3v2_match(buf0, ID3v2_DEFAULT_MAGIC) && 2*ff_id3v2_tag_len(buf0) >= p->buf_size)
-                           return AVPROBE_SCORE_EXTENSION / 4;
+                           return p->buf_size < PROBE_BUF_MAX ? AVPROBE_SCORE_EXTENSION / 4 : AVPROBE_SCORE_EXTENSION - 2;
     else if(max_frames>=1) return 1;
     else                   return 0;
 //mpegps_mp3_unrecognized_format.mpg has max_frames=3
