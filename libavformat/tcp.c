22fbc7f8becdee1fd6ab7cac93ae23d6022e9a56
Michael Niedermayer <michaelni@gmx.at>
2013-08-25 19:44:40 +0200

    avformat/tcp: fix pointer to int warning
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/tcp.c
+++ libavformat/tcp.c
@@ -127,7 +127,7 @@ static int tcp_open(URLContext *h, const char *uri, int flags)
         }
     } else {
         if ((ret = ff_listen_connect(fd, cur_ai->ai_addr, cur_ai->ai_addrlen,
-                                     s->open_timeout / 1000, h, cur_ai->ai_next)) < 0) {
+                                     s->open_timeout / 1000, h, !!cur_ai->ai_next)) < 0) {
 
             if (ret == AVERROR_EXIT)
                 goto fail1;
