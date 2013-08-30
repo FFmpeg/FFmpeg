48cd1037f661f8fc566c5aae1ea174b77b031704
Paul B Mahol <onemda@gmail.com>
2013-08-30 10:39:27 +0000

    cmdutils: silence warning about incompatible pointer types
    Signed-off-by: Paul B Mahol <onemda@gmail.com>

--- cmdutils.c
+++ cmdutils.c
@@ -500,7 +500,7 @@ void parse_loglevel(int argc, char **argv, const OptionDef *options)
 static const AVOption *opt_find(void *obj, const char *name, const char *unit,
                             int opt_flags, int search_flags)
 {
-    AVOption *o = av_opt_find(obj, name, unit, opt_flags, search_flags);
+    const AVOption *o = av_opt_find(obj, name, unit, opt_flags, search_flags);
     if(o && !o->flags)
         return NULL;
     return o;
