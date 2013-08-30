5c504e4df7b4abe6e54d0be3e327b65348267906
Michael Niedermayer <michaelni@gmx.at>
2013-08-30 20:57:04 +0200

    vformat/subtitles: check av_copy_packets return code
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/subtitles.c
+++ libavformat/subtitles.c
@@ -85,7 +85,9 @@ int ff_subtitles_queue_read_packet(FFDemuxSubtitlesQueue *q, AVPacket *pkt)
 
     if (q->current_sub_idx == q->nb_subs)
         return AVERROR_EOF;
-    av_copy_packet(pkt, sub);
+    if (av_copy_packet(pkt, sub) < 0) {
+        return AVERROR(ENOMEM);
+    }
 
     pkt->dts = pkt->pts;
     q->current_sub_idx++;
