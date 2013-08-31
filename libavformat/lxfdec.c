237ef710a17792b0ae404dbe610f0e6fbf0fe24d
Michael Niedermayer <michaelni@gmx.at>
2013-09-01 00:21:37 +0200

    avformat/lxfdec: use a parser to parse video frame headers
    lxf needs a parser (or would need to set a few fields explicitly).
    Fixes Ticket2917
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>
    (cherry picked from commit 8349be852be7f68fe0590584fd46c4d5f1c16b3d)

--- libavformat/lxfdec.c
+++ libavformat/lxfdec.c
@@ -257,6 +257,7 @@ static int lxf_read_header(AVFormatContext *s)
     st->codec->bit_rate   = 1000000 * ((video_params >> 14) & 0xFF);
     st->codec->codec_tag  = video_params & 0xF;
     st->codec->codec_id   = ff_codec_get_id(lxf_tags, st->codec->codec_tag);
+    st->need_parsing      = AVSTREAM_PARSE_HEADERS;
 
     av_log(s, AV_LOG_DEBUG, "record: %x = %i-%02i-%02i\n",
            record_date, 1900 + (record_date & 0x7F), (record_date >> 7) & 0xF,
