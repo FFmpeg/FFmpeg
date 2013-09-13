108b91c3fe61f5296585a1e326e85f0f18576ad6
Michael Niedermayer <michaelni@gmx.at>
2013-09-14 00:31:40 +0200

    avcodec/mpeg12dec: clear mpeg_enc_ctx_allocated when contexts are deallocated
    Fixes Ticket2950
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/mpeg12dec.c
+++ libavcodec/mpeg12dec.c
@@ -1211,6 +1211,7 @@ static int mpeg_decode_postinit(AVCodecContext *avctx)
             s->parse_context.buffer = 0;
             ff_MPV_common_end(s);
             s->parse_context = pc;
+            s1->mpeg_enc_ctx_allocated = 0;
         }
 
         if ((s->width == 0) || (s->height == 0))
@@ -2035,6 +2036,7 @@ static int vcr2_init_sequence(AVCodecContext *avctx)
     s->out_format = FMT_MPEG1;
     if (s1->mpeg_enc_ctx_allocated) {
         ff_MPV_common_end(s);
+        s1->mpeg_enc_ctx_allocated = 0;
     }
     s->width  = avctx->coded_width;
     s->height = avctx->coded_height;
