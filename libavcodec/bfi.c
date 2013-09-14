1a3d142f1f1e65f554cba84d8a515684ecb390d0
Piotr Bandurski <ami_stuff@o2.pl>
2013-09-14 13:54:10 +0200

    avcodec/bfi: check for malloc failure
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/bfi.c
+++ libavcodec/bfi.c
@@ -42,6 +42,8 @@ static av_cold int bfi_decode_init(AVCodecContext *avctx)
     BFIContext *bfi = avctx->priv_data;
     avctx->pix_fmt  = AV_PIX_FMT_PAL8;
     bfi->dst        = av_mallocz(avctx->width * avctx->height);
+    if (!bfi->dst)
+        return AVERROR(ENOMEM);
     return 0;
 }
 
