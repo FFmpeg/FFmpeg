2d28950da9b2733e2630d3c5aa8a85fd4793f7f5
Michael Niedermayer <michaelni@gmx.at>
2013-09-17 21:48:54 +0200

    swscale/utils: remove redundant NULL checks before sws_freeVec()
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libswscale/utils.c
+++ libswscale/utils.c
@@ -1953,14 +1953,10 @@ void sws_freeFilter(SwsFilter *filter)
     if (!filter)
         return;
 
-    if (filter->lumH)
-        sws_freeVec(filter->lumH);
-    if (filter->lumV)
-        sws_freeVec(filter->lumV);
-    if (filter->chrH)
-        sws_freeVec(filter->chrH);
-    if (filter->chrV)
-        sws_freeVec(filter->chrV);
+    sws_freeVec(filter->lumH);
+    sws_freeVec(filter->lumV);
+    sws_freeVec(filter->chrH);
+    sws_freeVec(filter->chrV);
     av_free(filter);
 }
 
