184c79729d4011f33027bcdc61a63d521017ebc1
Michael Niedermayer <michaelni@gmx.at>
2013-09-19 16:26:25 +0200

    h264_sei: check SEI size
    Signed-off-by: Anton Khirnov <anton@khirnov.net>
    Signed-off-by: Vittorio Giovara <vittorio.giovara@gmail.com>

--- libavcodec/h264_sei.c
+++ libavcodec/h264_sei.c
@@ -177,6 +177,12 @@ int ff_h264_decode_sei(H264Context *h){
             size+= show_bits(&s->gb, 8);
         }while(get_bits(&s->gb, 8) == 255);
 
+        if (size > get_bits_left(&s->gb) / 8) {
+            av_log(s->avctx, AV_LOG_ERROR, "SEI type %d truncated at %d\n",
+                   type, get_bits_left(&s->gb));
+            return AVERROR_INVALIDDATA;
+        }
+
         switch(type){
         case SEI_TYPE_PIC_TIMING: // Picture timing SEI
             if(decode_picture_timing(h) < 0)
