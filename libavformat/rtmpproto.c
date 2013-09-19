b97b1adb3f807e1acd00d56319ee6cb41cc727e4
Martin Storsjö <martin@martin.st>
2013-09-19 12:48:04 +0300

    rtmpproto: Add a comment explaining the logic in handle_notify
    This explains why the cleanup in 5626f994f was wrong and why
    ae0f316a was needed.
    
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- libavformat/rtmpproto.c
+++ libavformat/rtmpproto.c
@@ -2095,9 +2095,11 @@ static int handle_notify(URLContext *s, RTMPPacket *pkt) {
 
     // generate packet header and put data into buffer for FLV demuxer
     if (rt->flv_off < rt->flv_size) {
+        // There is old unread data in the buffer, thus append at the end
         old_flv_size  = rt->flv_size;
         rt->flv_size += datatowritelength + 15;
     } else {
+        // All data has been read, write the new data at the start of the buffer
         old_flv_size = 0;
         rt->flv_size = datatowritelength + 15;
         rt->flv_off  = 0;
