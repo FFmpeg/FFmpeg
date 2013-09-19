09f2581dc5edb3642858d69d9a70b67e249167e9
Alex Smith <alex.smith@warpsharp.info>
2013-09-19 16:58:21 -0400

    msvc/icl: Use __declspec(deprecated)
    Prior to this on msvc/icl there was no handling of deprecated functions
    and the deprecated warning was disabled.
    
    After enabling there are a number of warnings relating to the CRT and
    the use of the non-secure versions of several functions.  Defining
    _CRT_SECURE_NO_WARNINGS silences these warnings.
    
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- libavutil/attributes.h
+++ libavutil/attributes.h
@@ -72,6 +72,8 @@
 
 #if AV_GCC_VERSION_AT_LEAST(3,1)
 #    define attribute_deprecated __attribute__((deprecated))
+#elif defined(_MSC_VER)
+#    define attribute_deprecated __declspec(deprecated)
 #else
 #    define attribute_deprecated
 #endif
