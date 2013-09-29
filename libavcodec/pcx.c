ff07ec143ebd3833fd5a3f4b6c00474ac523a31f
Martin Storsjö <martin@martin.st>
2013-09-29 18:39:52 +0300

    pcx: Return an error on broken palette if err_detect is set to 'explode'
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- libavcodec/pcx.c
+++ libavcodec/pcx.c
@@ -171,7 +171,8 @@ static int pcx_decode_frame(AVCodecContext *avctx, void *data, int *got_frame,
 
         if (buf_size < 769) {
             av_log(avctx, AV_LOG_ERROR, "File is too short\n");
-            ret = buf_size;
+            ret = avctx->err_recognition & AV_EF_EXPLODE ?
+                  AVERROR_INVALIDDATA : buf_size;
             goto end;
         }
 
@@ -187,7 +188,8 @@ static int pcx_decode_frame(AVCodecContext *avctx, void *data, int *got_frame,
         }
         if (*buf++ != 12) {
             av_log(avctx, AV_LOG_ERROR, "expected palette after image data\n");
-            ret = buf_size;
+            ret = avctx->err_recognition & AV_EF_EXPLODE ?
+                  AVERROR_INVALIDDATA : buf_size;
             goto end;
         }
     } else if (nplanes == 1) {   /* all packed formats, max. 16 colors */
