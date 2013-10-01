0425fd7eb2186e40e372f9b92c6456b293b8ae4a
Michael Niedermayer <michaelni@gmx.at>
2013-10-01 18:28:11 +0200

    ffmpeg: dont return reserved values
    Found-by: Daemon404
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- ffmpeg.c
+++ ffmpeg.c
@@ -3421,7 +3421,7 @@ int main(int argc, char **argv)
     av_log(NULL, AV_LOG_DEBUG, "%"PRIu64" frames successfully decoded, %"PRIu64" decoding errors\n",
            decode_error_stat[0], decode_error_stat[1]);
     if (2*decode_error_stat[0] < decode_error_stat[1])
-        exit_program(254);
+        exit_program(69);
 
     exit_program(received_nb_signals ? 255 : 0);
     return 0;
