4916088a93027fa0a4097c6f2b9086a2861856ac
Michael Niedermayer <michaelni@gmx.at>
2013-10-06 15:22:18 +0200

    fate: fix dpx on big endian
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- tests/lavf-regression.sh
+++ tests/lavf-regression.sh
@@ -247,7 +247,7 @@ fi
 if [ -n "$do_dpx" ] ; then
 do_image_formats dpx
 do_image_formats dpx "-pix_fmt gbrp10le" "-pix_fmt gbrp10le"
-do_image_formats dpx "-pix_fmt gbrp12le"
+do_image_formats dpx "-pix_fmt gbrp12le" "-pix_fmt gbrp12le"
 do_image_formats dpx "-pix_fmt rgb48le"
 do_image_formats dpx "-pix_fmt rgb48le -bits_per_raw_sample 10" "-pix_fmt rgb48le"
 do_image_formats dpx "-pix_fmt rgba64le"
