54bb30bae2ec6d721dd4dd89fff762b14fcf68e4
Maxim Poliakovski <max_pole@gmx.de>
2013-10-10 20:46:00 +0200

    omadec: Disable "Unsupported codec ATRAC3+" warning
    Also add a list of supported decoders.
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/omadec.c
+++ libavformat/omadec.c
@@ -37,6 +37,7 @@
  * - Sound data organized in packets follow the EA3 header
  *   (can be encrypted using the Sony DRM!).
  *
+ * Supported decoders: ATRAC3, ATRAC3+, MP3, LPCM
  */
 
 #include "libavutil/channel_layout.h"
@@ -379,7 +380,6 @@ static int oma_read_header(AVFormatContext *s)
         st->codec->sample_rate = samplerate;
         st->codec->bit_rate    = samplerate * framesize * 8 / 2048;
         avpriv_set_pts_info(st, 64, 1, samplerate);
-        av_log(s, AV_LOG_ERROR, "Unsupported codec ATRAC3+!\n");
         break;
     case OMA_CODECID_MP3:
         st->need_parsing = AVSTREAM_PARSE_FULL_RAW;
