904c89ac1bdc311b8da2f722abb0e882c7d5d6a7
Stefano Sabatini <stefasab@gmail.com>
2013-10-15 01:20:35 +0200

    lswr/swresample: convert ocl and icl options to AV_OPT_TYPE_CHANNEL_LAYOUT
    Improve interface flexibility. In particular makes it possible to set
    channel layout through symbolic names using the av_opt_set() interface.

--- libswresample/swresample.h
+++ libswresample/swresample.h
@@ -44,8 +44,8 @@
  * matrix):
  * @code
  * SwrContext *swr = swr_alloc();
- * av_opt_set_int(swr, "in_channel_layout",  AV_CH_LAYOUT_5POINT1, 0);
- * av_opt_set_int(swr, "out_channel_layout", AV_CH_LAYOUT_STEREO,  0);
+ * av_opt_set_channel_layout(swr, "in_channel_layout",  AV_CH_LAYOUT_5POINT1, 0);
+ * av_opt_set_channel_layout(swr, "out_channel_layout", AV_CH_LAYOUT_STEREO,  0);
  * av_opt_set_int(swr, "in_sample_rate",     48000,                0);
  * av_opt_set_int(swr, "out_sample_rate",    44100,                0);
  * av_opt_set_sample_fmt(swr, "in_sample_fmt",  AV_SAMPLE_FMT_FLTP, 0);
