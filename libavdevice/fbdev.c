1421ee263742b73bc6ff1ed32dd732360297b257
Lukasz Marek <lukasz.m.luki@gmail.com>
2013-10-18 20:45:13 +0200

    lavd/fbdev: pass proper memory map length
    fbdev->data was allocated with size fbdev->fixinfo.smem_len, release data
    in fbdev_read_close() using the same size.
    
    Signed-off-by: Lukasz Marek <lukasz.m.luki@gmail.com>
    Signed-off-by: Stefano Sabatini <stefasab@gmail.com>

--- libavdevice/fbdev.c
+++ libavdevice/fbdev.c
@@ -231,7 +231,7 @@ static av_cold int fbdev_read_close(AVFormatContext *avctx)
 {
     FBDevContext *fbdev = avctx->priv_data;
 
-    munmap(fbdev->data, fbdev->frame_size);
+    munmap(fbdev->data, fbdev->fixinfo.smem_len);
     close(fbdev->fd);
 
     return 0;
