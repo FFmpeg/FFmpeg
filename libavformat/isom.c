5b9f39860d83380c4af3039710b4fcf3ea1f4c99
Carl Eugen Hoyos <cehoyos@ag.or.at>
2013-10-18 07:50:50 +0200

    Support 4k AVC-Intra in mov.

--- libavformat/isom.c
+++ libavformat/isom.c
@@ -167,6 +167,7 @@ const AVCodecTag ff_codec_movvideo_tags[] = {
     { AV_CODEC_ID_H264, MKTAG('a', 'i', '1', '3') }, /* AVC-Intra 100M 1080p24/30/60 */
     { AV_CODEC_ID_H264, MKTAG('a', 'i', '1', '5') }, /* AVC-Intra 100M 1080i50 */
     { AV_CODEC_ID_H264, MKTAG('a', 'i', '1', '6') }, /* AVC-Intra 100M 1080i60 */
+    { AV_CODEC_ID_H264, MKTAG('a', 'i', 'v', 'x') }, /* AVC-Intra 200M 4kp24 */
     { AV_CODEC_ID_H264, MKTAG('A', 'V', 'i', 'n') }, /* AVC-Intra with implicit SPS/PPS */
 
     { AV_CODEC_ID_MPEG1VIDEO, MKTAG('m', '1', 'v', '1') }, /* Apple MPEG-1 Camcorder */
