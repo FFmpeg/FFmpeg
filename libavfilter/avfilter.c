45dd7df83b39b350ac2f6fa5e09db90c6f5f900c
Michael Niedermayer <michaelni@gmx.at>
2013-10-21 16:21:14 +0200

    avfilter/ff_insert_pad: fix order of operations
    Fixes out of bounds access
    Fixes CID732170
    Fixes CID732169
    
    No filter is known to use this function in a way so the issue can be reproduced.
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>
    (cherry picked from commit ab2bfb85d49b2f8aa505816f93e75fd18ad0a361)
    
    Conflicts:
    
    	libavfilter/avfilter.c
    (cherry picked from commit 86591b244f3a27293153896813f5569b49b2f5c0)
    
    Conflicts:
    
    	libavfilter/avfilter.c

--- libavfilter/avfilter.c
+++ libavfilter/avfilter.c
@@ -117,9 +117,9 @@ void ff_insert_pad(unsigned idx, unsigned *count, size_t padidx_off,
     (*links)[idx] = NULL;
 
     (*count)++;
-    for (i = idx+1; i < *count; i++)
-        if (*links[i])
-            (*(unsigned *)((uint8_t *) *links[i] + padidx_off))++;
+    for (i = idx + 1; i < *count; i++)
+        if ((*links)[i])
+            (*(unsigned *)((uint8_t *) (*links)[i] + padidx_off))++;
 }
 
 int avfilter_link(AVFilterContext *src, unsigned srcpad,
