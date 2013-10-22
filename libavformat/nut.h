55ae13e3deffa21027a44f31b4ae2f53a4fa7426
Derek Buitenhuis <derek.buitenhuis@gmail.com>
2013-10-22 16:11:11 +0100

    nut: Fix unchecked allocations
    Signed-off-by: Derek Buitenhuis <derek.buitenhuis@gmail.com>

--- libavformat/nut.h
+++ libavformat/nut.h
@@ -122,7 +122,7 @@ void ff_nut_reset_ts(NUTContext *nut, AVRational time_base, int64_t val);
 int64_t ff_lsb2full(StreamContext *stream, int64_t lsb);
 int ff_nut_sp_pos_cmp(const Syncpoint *a, const Syncpoint *b);
 int ff_nut_sp_pts_cmp(const Syncpoint *a, const Syncpoint *b);
-void ff_nut_add_sp(NUTContext *nut, int64_t pos, int64_t back_ptr, int64_t ts);
+int ff_nut_add_sp(NUTContext *nut, int64_t pos, int64_t back_ptr, int64_t ts);
 void ff_nut_free_sp(NUTContext *nut);
 
 extern const Dispositions ff_nut_dispositions[];
