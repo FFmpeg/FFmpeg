cc0e47b55096361723b364afa43b79a3f5619cdc
Michael Niedermayer <michaelni@gmx.at>
2013-10-30 23:27:28 +0100

    avcodec/jpeglsdec: check err value for ls_get_code_runterm()
    Fixes infinite loop
    Fixes Ticket3086
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/jpeglsdec.c
+++ libavcodec/jpeglsdec.c
@@ -148,6 +148,8 @@ static inline int ls_get_code_runterm(GetBitContext *gb, JLSState *state,
         ret = ret >> 1;
     }
 
+    if(FFABS(ret) > 0xFFFF)
+        return -0x10000;
     /* update state */
     state->A[Q] += FFABS(ret) - RItype;
     ret         *= state->twonear;
