9bcf647867cf0c8ddad0b728264bcf77c36299ee
Michael Niedermayer <michaelni@gmx.at>
2013-11-01 18:53:01 +0100

    avcodec: document that FF_DEBUG_VIS_QP / FF_DEBUG_VIS_MB_TYPE should only be used through avoptions
    This preempts ABI issues if their values get reused by the fork for something unrelated
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/avcodec.h
+++ libavcodec/avcodec.h
@@ -2462,8 +2462,8 @@ typedef struct AVCodecContext {
 #define FF_DEBUG_MMCO        0x00000800
 #define FF_DEBUG_BUGS        0x00001000
 #if FF_API_DEBUG_MV
-#define FF_DEBUG_VIS_QP      0x00002000
-#define FF_DEBUG_VIS_MB_TYPE 0x00004000
+#define FF_DEBUG_VIS_QP      0x00002000 ///< only access through AVOptions from outside libavcodec
+#define FF_DEBUG_VIS_MB_TYPE 0x00004000 ///< only access through AVOptions from outside libavcodec
 #endif
 #define FF_DEBUG_BUFFERS     0x00008000
 #define FF_DEBUG_THREADS     0x00010000
