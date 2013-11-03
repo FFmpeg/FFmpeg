3ef9b7ab95cc703b67a8b658dca45c80df0aaa66
Yusuke Nakamura <muken.the.vfrmaniac@gmail.com>
2013-11-03 18:47:31 +0000

    hevc_ps: Use AV_PIX_FMT_YUVJ420P if YUV 4:2:0 8-bit full scale
    Signed-off-by: Vittorio Giovara <vittorio.giovara@gmail.com>

--- libavcodec/hevc_ps.c
+++ libavcodec/hevc_ps.c
@@ -449,6 +449,8 @@ static void decode_vui(HEVCContext *s, HEVCSPS *sps)
         vui->video_format                    = get_bits(gb, 3);
         vui->video_full_range_flag           = get_bits1(gb);
         vui->colour_description_present_flag = get_bits1(gb);
+        if (vui->video_full_range_flag && sps->pix_fmt == AV_PIX_FMT_YUV420P)
+            sps->pix_fmt = AV_PIX_FMT_YUVJ420P;
         if (vui->colour_description_present_flag) {
             vui->colour_primaries        = get_bits(gb, 8);
             vui->transfer_characteristic = get_bits(gb, 8);
