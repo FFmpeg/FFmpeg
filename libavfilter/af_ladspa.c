988e2e7c1c18cc31aebae1a6b21c9305f715e6a8
Stefano Sabatini <stefasab@gmail.com>
2013-11-04 13:03:56 +0100

    lavfi/ladspa: cast return value of av_x_if_null to char*, fix warnings

--- libavfilter/af_ladspa.c
+++ libavfilter/af_ladspa.c
@@ -450,9 +450,11 @@ static av_cold int init(AVFilterContext *ctx)
 
             count_ports(desc, &inputs, &outputs);
             av_log(ctx, AV_LOG_INFO, "%lu:%lu %-25s %s\n", inputs, outputs, desc->Label,
-                                     av_x_if_null(desc->Name, "?"));
-            av_log(ctx, AV_LOG_VERBOSE, "Maker: %s\n", av_x_if_null(desc->Maker, "?"));
-            av_log(ctx, AV_LOG_VERBOSE, "Copyright: %s\n", av_x_if_null(desc->Copyright, "?"));
+                   (char *)av_x_if_null(desc->Name, "?"));
+            av_log(ctx, AV_LOG_VERBOSE, "Maker: %s\n",
+                   (char *)av_x_if_null(desc->Maker, "?"));
+            av_log(ctx, AV_LOG_VERBOSE, "Copyright: %s\n",
+                   (char *)av_x_if_null(desc->Copyright, "?"));
         }
         return AVERROR_EXIT;
     } else {
