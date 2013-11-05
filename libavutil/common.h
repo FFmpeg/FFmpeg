66872161cbb7187b0c20267653d415ef700aca35
Michael Niedermayer <michaelni@gmx.at>
2013-11-05 11:49:09 +0100

    avutil/common: error out with clear message if __STDC_CONSTANT_MACROS is not defined with c++
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavutil/common.h
+++ libavutil/common.h
@@ -26,6 +26,10 @@
 #ifndef AVUTIL_COMMON_H
 #define AVUTIL_COMMON_H
 
+#if defined(__cplusplus) && !defined(__STDC_CONSTANT_MACROS) && !defined(UINT64_C)
+#error missing -D__STDC_CONSTANT_MACROS / #define __STDC_CONSTANT_MACROS
+#endif
+
 #include <errno.h>
 #include <inttypes.h>
 #include <limits.h>
