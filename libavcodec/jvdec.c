b7462a3904d71ff799584faf5b875cad59ca2f31
Anton Khirnov <anton@khirnov.net>
2013-11-09 10:14:46 +0100

    jvdec: use the AVFrame API properly.

--- libavcodec/jvdec.c
+++ libavcodec/jvdec.c
@@ -33,7 +33,7 @@
 
 typedef struct JvContext {
     DSPContext dsp;
-    AVFrame    frame;
+    AVFrame   *frame;
     uint32_t   palette[AVPALETTE_COUNT];
     int        palette_has_changed;
 } JvContext;
@@ -41,6 +41,11 @@ typedef struct JvContext {
 static av_cold int decode_init(AVCodecContext *avctx)
 {
     JvContext *s = avctx->priv_data;
+
+    s->frame = av_frame_alloc();
+    if (!s->frame)
+        return AVERROR(ENOMEM);
+
     avctx->pix_fmt = AV_PIX_FMT_PAL8;
     ff_dsputil_init(&s->dsp, avctx);
     return 0;
@@ -144,7 +149,7 @@ static int decode_frame(AVCodecContext *avctx,
     buf += 5;
 
     if (video_size) {
-        if ((ret = ff_reget_buffer(avctx, &s->frame)) < 0) {
+        if ((ret = ff_reget_buffer(avctx, s->frame)) < 0) {
             av_log(avctx, AV_LOG_ERROR, "get_buffer() failed\n");
             return ret;
         }
@@ -155,15 +160,15 @@ static int decode_frame(AVCodecContext *avctx,
 
             for (j = 0; j < avctx->height; j += 8)
                 for (i = 0; i < avctx->width; i += 8)
-                    decode8x8(&gb, s->frame.data[0] + j*s->frame.linesize[0] + i,
-                              s->frame.linesize[0], &s->dsp);
+                    decode8x8(&gb, s->frame->data[0] + j*s->frame->linesize[0] + i,
+                              s->frame->linesize[0], &s->dsp);
 
             buf += video_size;
         } else if (video_type == 2) {
             if (buf + 1 <= buf_end) {
                 int v = *buf++;
                 for (j = 0; j < avctx->height; j++)
-                    memset(s->frame.data[0] + j*s->frame.linesize[0], v, avctx->width);
+                    memset(s->frame->data[0] + j*s->frame->linesize[0], v, avctx->width);
             }
         } else {
             av_log(avctx, AV_LOG_WARNING, "unsupported frame type %i\n", video_type);
@@ -180,13 +185,13 @@ static int decode_frame(AVCodecContext *avctx,
     }
 
     if (video_size) {
-        s->frame.key_frame           = 1;
-        s->frame.pict_type           = AV_PICTURE_TYPE_I;
-        s->frame.palette_has_changed = s->palette_has_changed;
+        s->frame->key_frame           = 1;
+        s->frame->pict_type           = AV_PICTURE_TYPE_I;
+        s->frame->palette_has_changed = s->palette_has_changed;
         s->palette_has_changed       = 0;
-        memcpy(s->frame.data[1], s->palette, AVPALETTE_SIZE);
+        memcpy(s->frame->data[1], s->palette, AVPALETTE_SIZE);
 
-        if ((ret = av_frame_ref(data, &s->frame)) < 0)
+        if ((ret = av_frame_ref(data, s->frame)) < 0)
             return ret;
         *got_frame = 1;
     }
@@ -198,7 +203,7 @@ static av_cold int decode_close(AVCodecContext *avctx)
 {
     JvContext *s = avctx->priv_data;
 
-    av_frame_unref(&s->frame);
+    av_frame_free(&s->frame);
 
     return 0;
 }
