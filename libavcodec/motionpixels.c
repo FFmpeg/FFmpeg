ca41c72c6d9515d9045bd3b68104525dee81b8d0
Anton Khirnov <anton@khirnov.net>
2013-11-15 15:33:20 +0100

    motionpixels: clip VLC codes.
    Fixes invalid reads.
    Found-by: Mateusz "j00ru" Jurczyk and Gynvael Coldwind
    CC:libav-stable@libav.org

--- libavcodec/motionpixels.c
+++ libavcodec/motionpixels.c
@@ -180,6 +180,7 @@ static int mp_get_vlc(MotionPixelsContext *mp, GetBitContext *gb)
     int i;
 
     i = (mp->codes_count == 1) ? 0 : get_vlc2(gb, mp->vlc.table, mp->max_codes_bits, 1);
+    i = FFMIN(i, FF_ARRAY_ELEMS(mp->codes) - 1);
     return mp->codes[i].delta;
 }
 
