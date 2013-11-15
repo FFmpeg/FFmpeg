f0259a587ee3419dd894873ea617b4c98eeaca1c
Anton Khirnov <anton@khirnov.net>
2013-11-15 10:15:24 +0100

    h264: check buffer size before accessing it
    Fixes invalid reads.
    Found-by: Mateusz "j00ru" Jurczyk and Gynvael Coldwind
    CC:libav-stable@libav.org

--- libavcodec/h264.c
+++ libavcodec/h264.c
@@ -4554,7 +4554,7 @@ static int decode_nal_units(H264Context *h, const uint8_t *buf, int buf_size,
                 h->workaround_bugs |= FF_BUG_TRUNCATED;
 
             if (!(h->workaround_bugs & FF_BUG_TRUNCATED))
-                while (ptr[dst_length - 1] == 0 && dst_length > 0)
+                while (dst_length > 0 && ptr[dst_length - 1] == 0)
                     dst_length--;
             bit_length = !dst_length ? 0
                                      : (8 * dst_length -
