4e70eeef3a5e1277175891c7cfd19f8a3d323495
Clément Bœsch <clement@stupeflix.com>
2013-11-21 12:52:17 +0100

    cmdutils: randomize spaces after 69cf626f9.

--- cmdutils.c
+++ cmdutils.c
@@ -1081,24 +1081,24 @@ static void print_program_info(int flags, int level)
 
 static void print_buildconf(int flags, int level)
 {
-    const char *indent = flags & INDENT? "  " : "";
+    const char *indent = flags & INDENT ? "  " : "";
     char str[] = { FFMPEG_CONFIGURATION };
     char *conflist, *remove_tilde, *splitconf;
 
     // Change all the ' --' strings to '~--' so that
     // they can be identified as tokens.
-    while ( (conflist = strstr(str, " --")) != NULL ) {
+    while ((conflist = strstr(str, " --")) != NULL) {
         strncpy(conflist, "~--", 3);
     }
 
     // Compensate for the weirdness this would cause
     // when passing 'pkg-config --static'.
-    while ( (remove_tilde = strstr(str, "pkg-config~")) != NULL ) {
-        strncpy(remove_tilde, "pkg-config ",11);
+    while ((remove_tilde = strstr(str, "pkg-config~")) != NULL) {
+        strncpy(remove_tilde, "pkg-config ", 11);
     }
 
     splitconf = strtok(str, "~");
-    av_log(NULL, level, "\n%sconfiguration:\n",indent);
+    av_log(NULL, level, "\n%sconfiguration:\n", indent);
     while (splitconf != NULL) {
         av_log(NULL, level, "%s%s%s\n", indent, indent, splitconf);
         splitconf = strtok(NULL, "~");
