dd5f925927d6aaf01716a1a802a428340eeea077
Michael Niedermayer <michaelni@gmx.at>
2013-12-04 01:49:14 +0100

    avformat/utils: Fix pts_wrap_behavior calculation with negative first_dts
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/utils.c
+++ libavformat/utils.c
@@ -874,11 +874,12 @@ static int update_wrap_reference(AVFormatContext *s, AVStream *st, int stream_in
         st->pts_wrap_reference == AV_NOPTS_VALUE && st->first_dts != AV_NOPTS_VALUE) {
         int i;
 
+        int64_t ref = st->first_dts & ((1LL<<st->pts_wrap_bits)-1);
         // reference time stamp should be 60 s before first time stamp
-        int64_t pts_wrap_reference = st->first_dts - av_rescale(60, st->time_base.den, st->time_base.num);
+        int64_t pts_wrap_reference = ref - av_rescale(60, st->time_base.den, st->time_base.num);
         // if first time stamp is not more than 1/8 and 60s before the wrap point, subtract rather than add wrap offset
-        int pts_wrap_behavior = (st->first_dts < (1LL<<st->pts_wrap_bits) - (1LL<<st->pts_wrap_bits-3)) ||
-            (st->first_dts < (1LL<<st->pts_wrap_bits) - av_rescale(60, st->time_base.den, st->time_base.num)) ?
+        int pts_wrap_behavior = (ref < (1LL<<st->pts_wrap_bits) - (1LL<<st->pts_wrap_bits-3)) ||
+            (ref < (1LL<<st->pts_wrap_bits) - av_rescale(60, st->time_base.den, st->time_base.num)) ?
             AV_PTS_WRAP_ADD_OFFSET : AV_PTS_WRAP_SUB_OFFSET;
 
         AVProgram *first_program = av_find_program_from_stream(s, NULL, stream_index);
