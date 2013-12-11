b6eee405ffd9b212d5093e00bfb401df4f4b4a2f
Michael Niedermayer <michaelni@gmx.at>
2013-12-11 15:28:27 +0100

    avcodec/utils: Print warning if avcodec_set_dimensions() failed
    Fixes CID1135744
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/utils.c
+++ libavcodec/utils.c
@@ -222,7 +222,10 @@ unsigned avcodec_get_edge_width(void)
 #if FF_API_SET_DIMENSIONS
 void avcodec_set_dimensions(AVCodecContext *s, int width, int height)
 {
-    ff_set_dimensions(s, width, height);
+    int ret = ff_set_dimensions(s, width, height);
+    if (ret < 0) {
+        av_log(s, AV_LOG_WARNING, "Failed to set dimensions %d %d\n", width, height);
+    }
 }
 #endif
 
