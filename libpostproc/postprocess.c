f6173fed608b81e70f40664f356d127618851427
Michael Niedermayer <michaelni@gmx.at>
2013-12-11 22:24:51 +0100

    postproc: fix null pointer dereference with invalid option strings
    Fixes CID1135760
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libpostproc/postprocess.c
+++ libpostproc/postprocess.c
@@ -719,6 +719,10 @@ pp_mode *pp_get_mode_by_name_and_quality(const char *name, int quality)
         if(filterToken == NULL) break;
         p+= strlen(filterToken) + 1; // p points to next filterToken
         filterName= strtok(filterToken, optionDelimiters);
+        if (filterName == NULL) {
+            ppMode->error++;
+            break;
+        }
         av_log(NULL, AV_LOG_DEBUG, "pp: %s::%s\n", filterToken, filterName);
 
         if(*filterName == '-'){
