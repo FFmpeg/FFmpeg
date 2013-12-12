d780fdb904a4a3f555252fad6b898534c9af128b
Michael Niedermayer <michaelni@gmx.at>
2013-12-12 18:41:39 +0100

    avformat/hlsenc: copy metadata
    Fixes Ticket2296
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/hlsenc.c
+++ libavformat/hlsenc.c
@@ -70,6 +70,7 @@ static int hls_mux_init(AVFormatContext *s)
 
     oc->oformat            = hls->oformat;
     oc->interrupt_callback = s->interrupt_callback;
+    av_dict_copy(&oc->metadata, s->metadata, 0);
 
     for (i = 0; i < s->nb_streams; i++) {
         AVStream *st;
