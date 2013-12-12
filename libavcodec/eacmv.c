f194f2be418a9a9bbb74cb0b54e3bc6fea0da0ba
Anton Khirnov <anton@khirnov.net>
2013-12-12 07:31:26 +0100

    eacmv: check the framerate before setting it.
    Found-by: Mateusz "j00ru" Jurczyk and Gynvael Coldwind
    CC:libav-stable@libav.org
    (cherry picked from commit 24057c83207d6ea8bfd824155ac37be8a33dfd0c)
    Signed-off-by: Reinhard Tartler <siretart@tauware.de>
    
    Conflicts:
    	libavcodec/eacmv.c

--- libavcodec/eacmv.c
+++ libavcodec/eacmv.c
@@ -119,7 +119,7 @@ static void cmv_decode_inter(CmvContext * s, const uint8_t *buf, const uint8_t *
 
 static void cmv_process_header(CmvContext *s, const uint8_t *buf, const uint8_t *buf_end)
 {
-    int pal_start, pal_count, i;
+    int pal_start, pal_count, i, fps;
 
     if(buf_end - buf < 16) {
         av_log(s->avctx, AV_LOG_WARNING, "truncated header\n");
@@ -131,8 +131,9 @@ static void cmv_process_header(CmvContext *s, const uint8_t *buf, const uint8_t 
     if (s->avctx->width!=s->width || s->avctx->height!=s->height)
         avcodec_set_dimensions(s->avctx, s->width, s->height);
 
-    s->avctx->time_base.num = 1;
-    s->avctx->time_base.den = AV_RL16(&buf[10]);
+    fps = AV_RL16(&buf[10]);
+    if (fps > 0)
+        s->avctx->time_base = (AVRational){ 1, fps };
 
     pal_start = AV_RL16(&buf[12]);
     pal_count = AV_RL16(&buf[14]);
