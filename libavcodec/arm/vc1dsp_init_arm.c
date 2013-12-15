832e19063209a5f355af733d1a45f5051f49ce33
Mason Carter <void.main.argc.argv@gmail.com>
2013-12-14 17:32:56 -0800

    vc1: arm: Add NEON assembly
    For:
    
    ff_vc1_inv_trans_{8,4}x{8,4}_{dc_,}neon
    ff_put_pixels8x8_neon
    ff_put_vc1_mspel_mc{0,1,2,3}{0,1,2,3}_neon (except for 00)
    
    Based on ARM assembly code in libavcodec/arm by Rob Clark and Mans
    Rullgard.
    
    Signed-off-by: Martin Storsjö <martin@martin.st>

---
+++ libavcodec/arm/vc1dsp_init_arm.c
@@ -0,0 +1,32 @@
+/*
+ * This file is part of Libav.
+ *
+ * Libav is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * Libav is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Libav; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#include <stdint.h>
+
+#include "libavutil/attributes.h"
+#include "libavutil/arm/cpu.h"
+#include "libavcodec/vc1dsp.h"
+#include "vc1dsp.h"
+
+av_cold void ff_vc1dsp_init_arm(VC1DSPContext *dsp)
+{
+    int cpu_flags = av_get_cpu_flags();
+
+    if (have_neon(cpu_flags))
+        ff_vc1dsp_init_neon(dsp);
+}
---
+++ libavcodec/arm/vc1dsp_init_neon.c
@@ -0,0 +1,100 @@
+/*
+ * This file is part of Libav.
+ *
+ * Libav is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * Libav is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Libav; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#include <stdint.h>
+
+#include "libavutil/attributes.h"
+#include "libavcodec/vc1dsp.h"
+#include "vc1dsp.h"
+
+void ff_vc1_inv_trans_8x8_neon(int16_t *block);
+void ff_vc1_inv_trans_4x8_neon(uint8_t *dest, int linesize, int16_t *block);
+void ff_vc1_inv_trans_8x4_neon(uint8_t *dest, int linesize, int16_t *block);
+void ff_vc1_inv_trans_4x4_neon(uint8_t *dest, int linesize, int16_t *block);
+
+void ff_vc1_inv_trans_8x8_dc_neon(uint8_t *dest, int linesize, int16_t *block);
+void ff_vc1_inv_trans_4x8_dc_neon(uint8_t *dest, int linesize, int16_t *block);
+void ff_vc1_inv_trans_8x4_dc_neon(uint8_t *dest, int linesize, int16_t *block);
+void ff_vc1_inv_trans_4x4_dc_neon(uint8_t *dest, int linesize, int16_t *block);
+
+void ff_put_pixels8x8_neon(uint8_t *block, const uint8_t *pixels,
+                           ptrdiff_t line_size, int rnd);
+
+void ff_put_vc1_mspel_mc10_neon(uint8_t *dst, const uint8_t *src,
+                                ptrdiff_t stride, int rnd);
+void ff_put_vc1_mspel_mc20_neon(uint8_t *dst, const uint8_t *src,
+                                ptrdiff_t stride, int rnd);
+void ff_put_vc1_mspel_mc30_neon(uint8_t *dst, const uint8_t *src,
+                                ptrdiff_t stride, int rnd);
+
+void ff_put_vc1_mspel_mc01_neon(uint8_t *dst, const uint8_t *src,
+                                ptrdiff_t stride, int rnd);
+void ff_put_vc1_mspel_mc02_neon(uint8_t *dst, const uint8_t *src,
+                                ptrdiff_t stride, int rnd);
+void ff_put_vc1_mspel_mc03_neon(uint8_t *dst, const uint8_t *src,
+                                ptrdiff_t stride, int rnd);
+
+void ff_put_vc1_mspel_mc11_neon(uint8_t *dst, const uint8_t *src,
+                                ptrdiff_t stride, int rnd);
+void ff_put_vc1_mspel_mc12_neon(uint8_t *dst, const uint8_t *src,
+                                ptrdiff_t stride, int rnd);
+void ff_put_vc1_mspel_mc13_neon(uint8_t *dst, const uint8_t *src,
+                                ptrdiff_t stride, int rnd);
+
+void ff_put_vc1_mspel_mc21_neon(uint8_t *dst, const uint8_t *src,
+                                ptrdiff_t stride, int rnd);
+void ff_put_vc1_mspel_mc22_neon(uint8_t *dst, const uint8_t *src,
+                                ptrdiff_t stride, int rnd);
+void ff_put_vc1_mspel_mc23_neon(uint8_t *dst, const uint8_t *src,
+                                ptrdiff_t stride, int rnd);
+
+void ff_put_vc1_mspel_mc31_neon(uint8_t *dst, const uint8_t *src,
+                                ptrdiff_t stride, int rnd);
+void ff_put_vc1_mspel_mc32_neon(uint8_t *dst, const uint8_t *src,
+                                ptrdiff_t stride, int rnd);
+void ff_put_vc1_mspel_mc33_neon(uint8_t *dst, const uint8_t *src,
+                                ptrdiff_t stride, int rnd);
+
+av_cold void ff_vc1dsp_init_neon(VC1DSPContext *dsp)
+{
+    dsp->vc1_inv_trans_8x8 = ff_vc1_inv_trans_8x8_neon;
+    dsp->vc1_inv_trans_4x8 = ff_vc1_inv_trans_4x8_neon;
+    dsp->vc1_inv_trans_8x4 = ff_vc1_inv_trans_8x4_neon;
+    dsp->vc1_inv_trans_4x4 = ff_vc1_inv_trans_4x4_neon;
+    dsp->vc1_inv_trans_8x8_dc = ff_vc1_inv_trans_8x8_dc_neon;
+    dsp->vc1_inv_trans_4x8_dc = ff_vc1_inv_trans_4x8_dc_neon;
+    dsp->vc1_inv_trans_8x4_dc = ff_vc1_inv_trans_8x4_dc_neon;
+    dsp->vc1_inv_trans_4x4_dc = ff_vc1_inv_trans_4x4_dc_neon;
+
+    dsp->put_vc1_mspel_pixels_tab[ 0] = ff_put_pixels8x8_neon;
+    dsp->put_vc1_mspel_pixels_tab[ 1] = ff_put_vc1_mspel_mc10_neon;
+    dsp->put_vc1_mspel_pixels_tab[ 2] = ff_put_vc1_mspel_mc20_neon;
+    dsp->put_vc1_mspel_pixels_tab[ 3] = ff_put_vc1_mspel_mc30_neon;
+    dsp->put_vc1_mspel_pixels_tab[ 4] = ff_put_vc1_mspel_mc01_neon;
+    dsp->put_vc1_mspel_pixels_tab[ 5] = ff_put_vc1_mspel_mc11_neon;
+    dsp->put_vc1_mspel_pixels_tab[ 6] = ff_put_vc1_mspel_mc21_neon;
+    dsp->put_vc1_mspel_pixels_tab[ 7] = ff_put_vc1_mspel_mc31_neon;
+    dsp->put_vc1_mspel_pixels_tab[ 8] = ff_put_vc1_mspel_mc02_neon;
+    dsp->put_vc1_mspel_pixels_tab[ 9] = ff_put_vc1_mspel_mc12_neon;
+    dsp->put_vc1_mspel_pixels_tab[10] = ff_put_vc1_mspel_mc22_neon;
+    dsp->put_vc1_mspel_pixels_tab[11] = ff_put_vc1_mspel_mc32_neon;
+    dsp->put_vc1_mspel_pixels_tab[12] = ff_put_vc1_mspel_mc03_neon;
+    dsp->put_vc1_mspel_pixels_tab[13] = ff_put_vc1_mspel_mc13_neon;
+    dsp->put_vc1_mspel_pixels_tab[14] = ff_put_vc1_mspel_mc23_neon;
+    dsp->put_vc1_mspel_pixels_tab[15] = ff_put_vc1_mspel_mc33_neon;
+}
