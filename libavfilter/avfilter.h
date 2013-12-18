d44bd7fb27d4121512eae0ffce5e48bac25e82dc
Vittorio Giovara <vittorio.giovara@gmail.com>
2013-12-18 11:40:38 +0100

    avfilter: add documentation for needs_writable

--- libavfilter/avfilter.h
+++ libavfilter/avfilter.h
@@ -358,6 +358,12 @@ struct AVFilterPad {
      */
     int needs_fifo;
 
+    /**
+     * The filter expects writable frames from its input link,
+     * duplicating data buffers if needed.
+     *
+     * input pads only.
+     */
     int needs_writable;
 };
 #endif
