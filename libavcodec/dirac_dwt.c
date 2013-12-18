1935173fd1decb494300bf96b507419aae2b116a
Michael Niedermayer <michaelni@gmx.at>
2013-12-18 15:49:20 +0100

    avcodec/dirac_dwt: initialize support value for fidelity wavelet
    Avoids use of uninitialized memory
    Fixes: msan_uninit-mem_7f15e1988a6e_2748_RL_420p_ffdirac.drc
    Found-by: Mateusz "j00ru" Jurczyk and Gynvael Coldwind
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/dirac_dwt.c
+++ libavcodec/dirac_dwt.c
@@ -535,6 +535,7 @@ int ff_spatial_idwt_init2(DWTContext *d, IDWTELEM *buffer, int width, int height
         d->vertical_compose_l0 = (void*)vertical_compose_fidelityiL0;
         d->vertical_compose_h0 = (void*)vertical_compose_fidelityiH0;
         d->horizontal_compose = horizontal_compose_fidelityi;
+        d->support = 0; // not really used
         break;
     case DWT_DIRAC_DAUB9_7:
         d->spatial_compose = spatial_compose_daub97i_dy;
