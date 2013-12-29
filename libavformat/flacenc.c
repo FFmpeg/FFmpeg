c14b011a97f370c5cb0b8af123e916fa6932a72b
James Darnley <james.darnley@gmail.com>
2013-12-30 00:41:50 +0100

    lavf/flacenc: fix comment after previous change
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/flacenc.c
+++ libavformat/flacenc.c
@@ -105,7 +105,7 @@ static int flac_write_header(struct AVFormatContext *s)
     /* The command line flac encoder defaults to placing a seekpoint
      * every 10s.  So one might add padding to allow that later
      * but there seems to be no simple way to get the duration here.
-     * So let's try the flac default of 8192 bytes */
+     * So just add the amount requested by the user. */
     if (padding)
         flac_write_block_padding(s->pb, padding, 1);
 
