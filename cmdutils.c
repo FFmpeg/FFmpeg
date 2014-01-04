7ce88e5ec41484c452da56853a6897803da9c2a5
Johan Andersson <j@i19.se>
2014-01-04 20:47:32 +0100

    cmdutils: update copyright year to 2014.
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- cmdutils.c
+++ cmdutils.c
@@ -58,7 +58,7 @@
 struct SwsContext *sws_opts;
 AVDictionary *format_opts, *codec_opts, *resample_opts;
 
-static const int this_year = 2013;
+static const int this_year = 2014;
 
 void init_opts(void)
 {
