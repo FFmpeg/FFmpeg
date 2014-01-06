908b951b4e945eb0ba373548ff5921c547cec0fd
Michael Niedermayer <michaelni@gmx.at>
2014-01-06 02:51:20 +0100

    avcodec/hevc: Fix modulo operations
    Fixes qp fields becoming out of range
    Fixes: asan_static-oob_e393a3_6998_WPP_A_ericsson_MAIN10_2.bit
    Found-by: Mateusz "j00ru" Jurczyk and Gynvael Coldwind
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>
    (cherry picked from commit 4ced5d7780fea2ea49444d6686d26f26b3a2160f)
    
    Conflicts:
    
    	libavcodec/hevc_filter.c

--- libavcodec/hevc_filter.c
+++ libavcodec/hevc_filter.c
@@ -155,7 +155,8 @@ void ff_hevc_set_qPy(HEVCContext *s, int xC, int yC, int xBase, int yBase, int l
 
     if (s->HEVClc->tu.cu_qp_delta != 0) {
         int off = s->sps->qp_bd_offset;
-        s->HEVClc->qp_y = ((qp_y + s->HEVClc->tu.cu_qp_delta + 52 + 2 * off) % (52 + off)) - off;
+        s->HEVClc->qp_y = FFUMOD(qp_y + s->HEVClc->tu.cu_qp_delta + 52 + 2 * off,
+                                 52 + off) - off;
     } else
         s->HEVClc->qp_y = qp_y;
 }
