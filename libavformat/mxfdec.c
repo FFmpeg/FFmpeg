42f9132218ca11a8e9a3c82a175b46bca092113e
Luca Barbato <lu_zero@gentoo.org>
2014-01-07 21:26:14 +0100

    mxf: Do not use int to check the seek position
    Overly large files are to be expected.
    
    Reported-by: Jean-Baptiste Kempf <jb@videolan.org>

--- libavformat/mxfdec.c
+++ libavformat/mxfdec.c
@@ -2238,8 +2238,10 @@ static int mxf_read_seek(AVFormatContext *s, int stream_index, int64_t sample_ti
         sample_time = 0;
     seconds = av_rescale(sample_time, st->time_base.num, st->time_base.den);
 
-    if ((ret = avio_seek(s->pb, (s->bit_rate * seconds) >> 3, SEEK_SET)) < 0)
-        return ret;
+    seekpos = avio_seek(s->pb, (s->bit_rate * seconds) >> 3, SEEK_SET);
+    if (seekpos < 0)
+        return seekpos;
+
     ff_update_cur_dts(s, st, sample_time);
     mxf->current_edit_unit = sample_time;
     } else {
