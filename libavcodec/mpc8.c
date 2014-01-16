88b240646f57a62299c6c4c4d7554f0a80c3a29a
Diego Biurrun <diego@biurrun.de>
2014-01-17 00:53:40 +0100

    build: Drop stray mpc8 decoder dependency on dsputil

--- libavcodec/mpc8.c
+++ libavcodec/mpc8.c
@@ -29,7 +29,6 @@
 #include "libavutil/lfg.h"
 #include "avcodec.h"
 #include "get_bits.h"
-#include "dsputil.h"
 #include "internal.h"
 #include "mpegaudiodsp.h"
 
@@ -119,7 +118,6 @@ static av_cold int mpc8_decode_init(AVCodecContext * avctx)
     }
     memset(c->oldDSCF, 0, sizeof(c->oldDSCF));
     av_lfg_init(&c->rnd, 0xDEADBEEF);
-    ff_dsputil_init(&c->dsp, avctx);
     ff_mpadsp_init(&c->mpadsp);
 
     ff_mpc_init();
