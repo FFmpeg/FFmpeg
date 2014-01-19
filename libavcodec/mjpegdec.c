b902ba478c81909422668fa7da7878848d792e52
Michael Niedermayer <michaelni@gmx.at>
2014-01-19 16:44:58 +0100

    avcodec/mjpegdec: Dont skip picture allocation if theres no picture allocated
    Fixes Ticket 3245
    (cherry picked from commit ad8d063f230c05f8b5efbd05cc5a9f51a2549dcf)

--- libavcodec/mjpegdec.c
+++ libavcodec/mjpegdec.c
@@ -329,7 +329,7 @@ int ff_mjpeg_decode_sof(MJpegDecodeContext *s)
         s->first_picture = 0;
     }
 
-    if (s->interlaced && (s->bottom_field == !s->interlace_polarity)) {
+    if (s->got_picture && s->interlaced && (s->bottom_field == !s->interlace_polarity)) {
         if (s->progressive) {
             av_log_ask_for_sample(s->avctx, "progressively coded interlaced pictures not supported\n");
             return AVERROR_INVALIDDATA;
