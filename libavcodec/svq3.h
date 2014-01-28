61e7c7f27b0a2652bf5cd282b97762ee99d025ef
Diego Biurrun <diego@biurrun.de>
2014-01-28 08:23:02 +0100

    svq3: Adjust #endif comment

--- libavcodec/svq3.h
+++ libavcodec/svq3.h
@@ -24,4 +24,4 @@
 void ff_svq3_luma_dc_dequant_idct_c(int16_t *output, int16_t *input, int qp);
 void ff_svq3_add_idct_c(uint8_t *dst, int16_t *block, int stride, int qp, int dc);
 
-#endif /* AVCODEC_DSPUTIL_H */
+#endif /* AVCODEC_SVQ3_H */
