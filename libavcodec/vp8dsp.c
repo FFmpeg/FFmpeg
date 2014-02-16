a707d18a48898342046014f41a030b82bf1be0b3
Peter Ross <pross@xvid.org>
2014-02-16 15:32:59 +1100

    avcodec/vp8dsp: zeroise input coefficient array after use in vp7 idct functions
    Signed-off-by: Peter Ross <pross@xvid.org>
    Reviewed-by: "Ronald S. Bultje" <rsbultje@gmail.com>
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/vp8dsp.c
+++ libavcodec/vp8dsp.c
@@ -50,6 +50,10 @@ static void vp7_luma_dc_wht_c(int16_t block[4][4][16], int16_t dc[16])
         b1 = (tmp[i + 0] - tmp[i + 8]) * 23170;
         c1 = tmp[i + 4] * 12540 - tmp[i + 12] * 30274;
         d1 = tmp[i + 4] * 30274 + tmp[i + 12] * 12540;
+        dc[i*4+0] = 0;
+        dc[i*4+1] = 0;
+        dc[i*4+2] = 0;
+        dc[i*4+3] = 0;
         block[0][i][0] = (a1 + d1 + 0x20000) >> 18;
         block[3][i][0] = (a1 - d1 + 0x20000) >> 18;
         block[1][i][0] = (b1 + c1 + 0x20000) >> 18;
@@ -80,6 +84,10 @@ static void vp7_idct_add_c(uint8_t *dst, int16_t block[16], ptrdiff_t stride)
         b1 = (block[i*4+0] - block[i*4+2]) * 23170;
         c1 = block[i*4+1] * 12540 - block[i*4+3] * 30274;
         d1 = block[i*4+1] * 30274 + block[i*4+3] * 12540;
+        block[i*4+0] = 0;
+        block[i*4+1] = 0;
+        block[i*4+2] = 0;
+        block[i*4+3] = 0;
         tmp[i*4+0] = (a1 + d1) >> 14;
         tmp[i*4+3] = (a1 - d1) >> 14;
         tmp[i*4+1] = (b1 + c1) >> 14;
