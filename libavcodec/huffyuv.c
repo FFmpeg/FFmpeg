4332b01c30a446e9c177fe44761a612daa0334c7
Michael Niedermayer <michaelni@gmx.at>
2014-02-19 20:19:38 +0100

    avcodec/huffyuv: simplify allocation of temporaries
    This also fixes a null pointer dereference with rgb and plane prediction
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/huffyuv.c
+++ libavcodec/huffyuv.c
@@ -58,17 +58,11 @@ av_cold int ff_huffyuv_alloc_temp(HYuvContext *s)
 {
     int i;
 
-    if (s->bitstream_bpp<24 || s->version > 2) {
-        for (i=0; i<3; i++) {
-            s->temp[i]= av_malloc(2*s->width + 16);
-            if (!s->temp[i])
-                return AVERROR(ENOMEM);
-            s->temp16[i] = (uint16_t*)s->temp[i];
-        }
-    } else {
-        s->temp[0]= av_mallocz(4*s->width + 16);
-        if (!s->temp[0])
+    for (i=0; i<3; i++) {
+        s->temp[i]= av_malloc(4*s->width + 16);
+        if (!s->temp[i])
             return AVERROR(ENOMEM);
+        s->temp16[i] = (uint16_t*)s->temp[i];
     }
     return 0;
 }
