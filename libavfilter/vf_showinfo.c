b4679ecdbd80ae050094733f137ccd7ebf1316ab
Anton Khirnov <anton@khirnov.net>
2014-02-19 18:48:15 +0100

    {v,a}f_showinfo: print frame side data

--- libavfilter/vf_showinfo.c
+++ libavfilter/vf_showinfo.c
@@ -28,6 +28,8 @@
 #include "libavutil/imgutils.h"
 #include "libavutil/internal.h"
 #include "libavutil/pixdesc.h"
+#include "libavutil/stereo3d.h"
+
 #include "avfilter.h"
 #include "internal.h"
 #include "video.h"
@@ -35,6 +37,36 @@
 typedef struct {
     unsigned int frame;
 } ShowInfoContext;
+
+static void dump_stereo3d(AVFilterContext *ctx, AVFrameSideData *sd)
+{
+    AVStereo3D *stereo;
+
+    av_log(ctx, AV_LOG_INFO, "stereoscopic information: ");
+    if (sd->size < sizeof(*stereo)) {
+        av_log(ctx, AV_LOG_INFO, "invalid data");
+        return;
+    }
+
+    stereo = (AVStereo3D *)sd->data;
+
+    av_log(ctx, AV_LOG_INFO, "type - ");
+    switch (stereo->type) {
+    case AV_STEREO3D_2D:                  av_log(ctx, AV_LOG_INFO, "2D");                     break;
+    case AV_STEREO3D_SIDEBYSIDE:          av_log(ctx, AV_LOG_INFO, "side by side");           break;
+    case AV_STEREO3D_TOPBOTTOM:           av_log(ctx, AV_LOG_INFO, "top and bottom");         break;
+    case AV_STEREO3D_FRAMESEQUENCE:       av_log(ctx, AV_LOG_INFO, "frame alternate");        break;
+    case AV_STEREO3D_CHECKERBOARD:        av_log(ctx, AV_LOG_INFO, "checkerboard");           break;
+    case AV_STEREO3D_LINES:               av_log(ctx, AV_LOG_INFO, "interleaved lines");      break;
+    case AV_STEREO3D_COLUMNS:             av_log(ctx, AV_LOG_INFO, "interleaved columns");    break;
+    case AV_STEREO3D_SIDEBYSIDE_QUINCUNX: av_log(ctx, AV_LOG_INFO, "side by side "
+                                                                   "(quincunx subsampling)"); break;
+    default:                              av_log(ctx, AV_LOG_WARNING, "unknown");             break;
+    }
+
+    if (stereo->flags & AV_STEREO3D_FLAG_INVERT)
+        av_log(ctx, AV_LOG_INFO, " (inverted)");
+}
 
 static int filter_frame(AVFilterLink *inlink, AVFrame *frame)
 {
@@ -70,6 +102,29 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *frame)
            frame->key_frame,
            av_get_picture_type_char(frame->pict_type),
            checksum, plane_checksum[0], plane_checksum[1], plane_checksum[2], plane_checksum[3]);
+
+    for (i = 0; i < frame->nb_side_data; i++) {
+        AVFrameSideData *sd = frame->side_data[i];
+
+        av_log(ctx, AV_LOG_INFO, "  side data - ");
+        switch (sd->type) {
+        case AV_FRAME_DATA_PANSCAN:
+            av_log(ctx, AV_LOG_INFO, "pan/scan");
+            break;
+        case AV_FRAME_DATA_A53_CC:
+            av_log(ctx, AV_LOG_INFO, "A/53 closed captions (%d bytes)", sd->size);
+            break;
+        case AV_FRAME_DATA_STEREO3D:
+            dump_stereo3d(ctx, sd);
+            break;
+        default:
+            av_log(ctx, AV_LOG_WARNING, "unknown side data type %d (%d bytes)",
+                   sd->type, sd->size);
+            break;
+        }
+
+        av_log(ctx, AV_LOG_INFO, "\n");
+    }
 
     showinfo->frame++;
     return ff_filter_frame(inlink->dst->outputs[0], frame);
