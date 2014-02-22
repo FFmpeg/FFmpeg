b89596a432504fb9919089fe9c8fc11f60733b54
Carl Eugen Hoyos <cehoyos@ag.or.at>
2014-02-22 11:17:56 +0100

    Fix FSF address in colormatrix and libzvbi license headers.
    As reported by irc user aca.
    The files in compat/avisynth are maintained elsewhere, no changes.

--- libavfilter/vf_colormatrix.c
+++ libavfilter/vf_colormatrix.c
@@ -17,7 +17,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software Foundation,
- * Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
 /**
