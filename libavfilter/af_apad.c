9f2a3bcee7bc3a9c3619a5baabaf44afac2b276a
Paul B Mahol <onemda@gmail.com>
2014-02-26 16:34:58 +0000

    avfilter: remove superflous lines
    Signed-off-by: Paul B Mahol <onemda@gmail.com>

--- libavfilter/af_apad.c
+++ libavfilter/af_apad.c
@@ -16,7 +16,6 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with FFmpeg; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
- *
  */
 
 /**
