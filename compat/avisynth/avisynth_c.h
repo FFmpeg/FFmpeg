d773d7775a632785f99ee1a69f4a776f956844b3
Andreas Cadhalpun <Andreas.Cadhalpun@googlemail.com>
2014-03-06 18:58:04 +0100

    Correct the FSF address for two avisynth files to '51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA'
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>
    (cherry picked from commit 64b6164b721c77cbb28b381af31bd8ea753818c5)

--- compat/avisynth/avisynth_c.h
+++ compat/avisynth/avisynth_c.h
@@ -13,7 +13,8 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with this program; if not, write to the Free Software
-// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA, or visit
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+// MA 02110-1301 USA, or visit
 // http://www.gnu.org/copyleft/gpl.html .
 //
 // As a special exception, I give you permission to link to the
