45acc228a6d5f1e7d6c5ce6da63b293bd5eda57d
Janne Grunau <janne-libav@jannau.net>
2014-03-08 11:52:14 +0100

    doc: fix a couple of typos in frame.h
    (cherry picked from commit a18ef7a76c735bcf78ed4825e33ad7f9f6f77a54)

--- libavutil/frame.h
+++ libavutil/frame.h
@@ -428,7 +428,7 @@ AVFrame *av_frame_alloc(void);
 void av_frame_free(AVFrame **frame);
 
 /**
- * Setup a new reference to the data described by an given frame.
+ * Set up a new reference to the data described by the source frame.
  *
  * Copy frame properties from src to dst and create a new reference for each
  * AVBufferRef from src.
