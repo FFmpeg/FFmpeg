774668763a4c1acccb605e8926759207c0c8ccef
Michael Niedermayer <michaelni@gmx.at>
2014-03-11 01:20:18 +0100

    ffplay: display information on stream cycling
    Reviewed-by: Marton Balint <cus@passwd.hu>
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- ffplay.c
+++ ffplay.c
@@ -3120,6 +3120,11 @@ static void stream_cycle_channel(VideoState *is, int codec_type)
  the_end:
     if (p && stream_index != -1)
         stream_index = p->stream_index[stream_index];
+    av_log(NULL, AV_LOG_INFO, "Switch %s stream from #%d to #%d\n",
+           av_get_media_type_string(codec_type),
+           old_index,
+           stream_index);
+
     stream_component_close(is, old_index);
     stream_component_open(is, stream_index);
 }
