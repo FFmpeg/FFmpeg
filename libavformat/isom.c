6d7b22fb8f247a51e668c83b2f2460a9b3e948a8
Michael Niedermayer <michaelni@gmx.at>
2014-03-16 12:22:26 +0000

    isom: fix C99-style declaration

--- libavformat/isom.c
+++ libavformat/isom.c
@@ -442,11 +442,12 @@ static int mov_rewrite_dvd_sub_extradata(AVStream *st)
     char buf[256];
     int pal_s_pos = 0;
     uint8_t *src = st->codec->extradata;
+    int i;
 
     if (st->codec->extradata_size != 64)
         return 0;
 
-    for (int i = 0; i < 16; i++) {
+    for (i = 0; i < 16; i++) {
         uint32_t yuv = AV_RB32(src + i * 4);
         uint32_t rgba = yuv_to_rgba(yuv);
 
