9dca02ee541120de2a96c387faed9a4e033a60fd
Michael Niedermayer <michaelni@gmx.at>
2014-03-21 15:45:03 +0100

    ffmpeg: dont call exit_program() from a signal hander
    This is unsafe and can deadlock amongth other things
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- ffmpeg.c
+++ ffmpeg.c
@@ -328,7 +328,7 @@ sigterm_handler(int sig)
     received_nb_signals++;
     term_exit_sigsafe();
     if(received_nb_signals > 3)
-        exit_program(123);
+        exit(123);
 }
 
 void term_init(void)
