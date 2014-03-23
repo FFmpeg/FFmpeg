FFmpeg/FFmpeg

commit 322a1dda973e802db7b57f2007fad3efcd5bab81

dsputil: Refactor duplicated CALL_2X_PIXELS / PIXELS16 macros

libavcodec/x86/h264_qpel.c
@@ -25,6 +25,7 @@
 #include "libavutil/x86/cpu.h"
 #include "libavcodec/h264qpel.h"
 #include "libavcodec/mpegvideo.h"
+#include "libavcodec/pixels.h"
 #include "dsputil_x86.h"
 
 #if HAVE_YASM
@@ -49,8 +50,8 @@ void ff_avg_pixels16_l2_mmxext(uint8_t *dst, uint8_t *src1, uint8_t *src2,
 #define ff_put_pixels16_l2_sse2 ff_put_pixels16_l2_mmxext
 #define ff_avg_pixels16_l2_sse2 ff_avg_pixels16_l2_mmxext
 
-PIXELS16(static, ff_avg, , , _mmxext)
-PIXELS16(static, ff_put, , , _mmxext)
+CALL_2X_PIXELS(ff_avg_pixels16_mmxext, ff_avg_pixels8_mmxext, 8)
+CALL_2X_PIXELS(ff_put_pixels16_mmxext, ff_avg_pixels8_mmxext, 8)
 
 #define DEF_QPEL(OPNAME)\
 void ff_ ## OPNAME ## _h264_qpel4_h_lowpass_mmxext(uint8_t *dst, uint8_t *src, int dstStride, int srcStride);\

libavcodec/arm/hpeldsp_init_arm.c
@@ -21,7 +21,7 @@
 
 #include "libavutil/arm/cpu.h"
 #include "libavutil/attributes.h"
-#include "libavcodec/rnd_avg.h"
+#include "libavcodec/pixels.h"
 #include "hpeldsp_arm.h"
 
 void ff_put_pixels8_arm(uint8_t *block, const uint8_t *pixels, ptrdiff_t line_size, int h);

libavcodec/dsputil.h
@@ -31,7 +31,6 @@
 #define AVCODEC_DSPUTIL_H
 
 #include "avcodec.h"
-#include "rnd_avg.h"
 
 /* encoding scans */
 extern const uint8_t ff_alternate_horizontal_scan[64];
@@ -40,7 +39,6 @@ extern const uint8_t ff_zigzag248_direct[64];
 
 extern uint32_t ff_square_tab[512];
 
-/* pixel operations */
 void ff_put_pixels8x8_c(uint8_t *dst, uint8_t *src, ptrdiff_t stride);
 void ff_avg_pixels8x8_c(uint8_t *dst, uint8_t *src, ptrdiff_t stride);
 void ff_put_pixels16x16_c(uint8_t *dst, uint8_t *src, ptrdiff_t stride);

libavcodec/dsputil_template.c
@@ -27,6 +27,8 @@
  * DSP utils
  */
 
+#include "pixels.h"
+
 #include "bit_depth_template.c"
 
 #if BIT_DEPTH == 8

libavcodec/hpel_template.c
@@ -19,6 +19,8 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
+#include "pixels.h"
+
 #define DEF_HPEL(OPNAME, OP)                                            \
 static inline void FUNCC(OPNAME ## _pixels2)(uint8_t *block,            \
                                              const uint8_t *pixels,     \

libavcodec/hpeldsp_template.c
@@ -28,6 +28,8 @@
  * Half-pel DSP functions
  */
 
+#include "pixels.h"
+
 #include "bit_depth_template.c"
 
 #include "hpel_template.c"

libavcodec/pixels.h
@@ -16,33 +16,22 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
-#ifndef AVCODEC_RND_AVG_H
-#define AVCODEC_RND_AVG_H
+#ifndef AVCODEC_PIXELS_H
+#define AVCODEC_PIXELS_H
 
 #include <stddef.h>
 #include <stdint.h>
 
-#define BYTE_VEC32(c) ((c) * 0x01010101UL)
-#define BYTE_VEC64(c) ((c) * 0x0001000100010001UL)
-
-static inline uint32_t rnd_avg32(uint32_t a, uint32_t b)
-{
-    return (a | b) - (((a ^ b) & ~BYTE_VEC32(0x01)) >> 1);
+/* pixel operations */
+#define CALL_2X_PIXELS_MACRO(STATIC, a, b, n)        \
+STATIC void a(uint8_t *block, const uint8_t *pixels, \
+              ptrdiff_t line_size, int h)            \
+{                                                    \
+    b(block, pixels, line_size, h);                  \
+    b(block + n, pixels + n, line_size, h);          \
 }
 
-static inline uint32_t no_rnd_avg32(uint32_t a, uint32_t b)
-{
-    return (a & b) + (((a ^ b) & ~BYTE_VEC32(0x01)) >> 1);
-}
+#define CALL_2X_PIXELS(a, b, n) CALL_2X_PIXELS_MACRO(static, a, b, n)
+#define CALL_2X_PIXELS_EXPORT(a, b, n) CALL_2X_PIXELS_MACRO(, a, b, n)
 
-static inline uint64_t rnd_avg64(uint64_t a, uint64_t b)
-{
-    return (a | b) - (((a ^ b) & ~BYTE_VEC64(0x01)) >> 1);
-}
-
-static inline uint64_t no_rnd_avg64(uint64_t a, uint64_t b)
-{
-    return (a & b) + (((a ^ b) & ~BYTE_VEC64(0x01)) >> 1);
-}
-
-#endif /* AVCODEC_RND_AVG_H */
+#endif /* AVCODEC_PIXELS_H */

libavcodec/x86/dsputil_init.c
@@ -24,6 +24,7 @@
 #include "libavutil/x86/cpu.h"
 #include "libavcodec/avcodec.h"
 #include "libavcodec/dsputil.h"
+#include "libavcodec/pixels.h"
 #include "libavcodec/simple_idct.h"
 #include "libavcodec/version.h"
 #include "dsputil_x86.h"
@@ -107,8 +108,8 @@ void ff_vector_clip_int32_sse4(int32_t *dst, const int32_t *src,
 
 #if HAVE_YASM
 
-PIXELS16(static, ff_avg, , , _mmxext)
-PIXELS16(static, ff_put, , , _mmxext)
+CALL_2X_PIXELS(ff_avg_pixels16_mmxext, ff_avg_pixels8_mmxext, 8)
+CALL_2X_PIXELS(ff_put_pixels16_mmxext, ff_put_pixels8_mmxext, 8)
 
 #define QPEL_OP(OPNAME, RND, MMX)                                       \
 static void OPNAME ## qpel8_mc00_ ## MMX(uint8_t *dst, uint8_t *src,    \

commit c8246d37664af9feb23edede944846687aea787a

avcodec/x86/h264_qpel: Fix typo introduced by 322a1dda973e802db7b57f2007fad3efcd5bab81
Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

libavcodec/x86/h264_qpel.c
@@ -51,7 +51,7 @@ void ff_avg_pixels16_l2_mmxext(uint8_t *dst, uint8_t *src1, uint8_t *src2,
 #define ff_avg_pixels16_l2_sse2 ff_avg_pixels16_l2_mmxext
 
 CALL_2X_PIXELS(ff_avg_pixels16_mmxext, ff_avg_pixels8_mmxext, 8)
-CALL_2X_PIXELS(ff_put_pixels16_mmxext, ff_avg_pixels8_mmxext, 8)
+CALL_2X_PIXELS(ff_put_pixels16_mmxext, ff_put_pixels8_mmxext, 8)
 
 #define DEF_QPEL(OPNAME)\
 void ff_ ## OPNAME ## _h264_qpel4_h_lowpass_mmxext(uint8_t *dst, uint8_t *src, int dstStride, int srcStride);\