a80a7131d11af69d2519377b35b58d8c8b3430af
Michael Niedermayer <michaelni@gmx.at>
2014-03-24 05:01:32 +0100

    swscale/swscale: fix integer overflow
    Should fix fate failure with clang ftrapv
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>
    (cherry picked from commit c9c0451224fd7bc38b4e135e99f114f80c1ae67f)
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libswscale/swscale.c
+++ libswscale/swscale.c
@@ -208,8 +208,9 @@ static void lumRangeToJpeg16_c(int16_t *_dst, int width)
 {
     int i;
     int32_t *dst = (int32_t *) _dst;
-    for (i = 0; i < width; i++)
-        dst[i] = (FFMIN(dst[i], 30189 << 4) * 4769 - (39057361 << 2)) >> 12;
+    for (i = 0; i < width; i++) {
+        dst[i] = ((int)(FFMIN(dst[i], 30189 << 4) * 4769U - (39057361 << 2))) >> 12;
+    }
 }
 
 static void lumRangeFromJpeg16_c(int16_t *_dst, int width)
