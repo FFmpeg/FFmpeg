00ccdcd88c9315e750ffebf364e4209176a1b8cb
Piotr Bandurski <ami_stuff@o2.pl>
2014-04-03 16:29:32 +0200

    riff: add 0x594a TwoCC for Xan DPCM
    Sample-Id: crusader-no-regret.avi

--- libavformat/riff.c
+++ libavformat/riff.c
@@ -402,6 +402,7 @@ const AVCodecTag ff_codec_wav_tags[] = {
     { AV_CODEC_ID_PCM_MULAW,       0x6c75 },
     { AV_CODEC_ID_AAC,             0x706d },
     { AV_CODEC_ID_AAC,             0x4143 },
+    { AV_CODEC_ID_XAN_DPCM,        0x594a },
     { AV_CODEC_ID_SPEEX,           0xA109 },
     { AV_CODEC_ID_FLAC,            0xF1AC },
     { AV_CODEC_ID_ADPCM_SWF,       ('S' << 8) + 'F' },
