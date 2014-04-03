8c272923736861cccfae0befebf09760baf54bf1
Lukasz Marek <lukasz.m.luki2@gmail.com>
2014-04-03 14:44:43 +0200

    lavu/opt: dont crash on freeing NULL AVOptionRanges
    Signed-off-by: Lukasz Marek <lukasz.m.luki2@gmail.com>
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavutil/opt.c
+++ libavutil/opt.c
@@ -1608,6 +1608,9 @@ void av_opt_freep_ranges(AVOptionRanges **rangesp)
     int i;
     AVOptionRanges *ranges = *rangesp;
 
+    if (!ranges)
+        return;
+
     for (i = 0; i < ranges->nb_ranges * ranges->nb_components; i++) {
         AVOptionRange *range = ranges->range[i];
         av_freep(&range->str);
