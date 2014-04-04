25b6837f7cacd691b19cbc12b9dad1ce84a318a1
Alessandro Ghedini <alessandro@ghedini.me>
2014-04-04 11:50:43 +0200

    replaygain: fix gain sign decoding
    The gain sign was incorrectly decoded: since the FFSIGN() macro treats 0 as
    negative, gain values starting with "0." were always decoded as negative.
    
    Signed-off-by: Anton Khirnov <anton@khirnov.net>

--- libavformat/replaygain.c
+++ libavformat/replaygain.c
@@ -40,12 +40,16 @@ static int32_t parse_gain(const char *gain)
     char *fraction;
     int  scale = 10000;
     int32_t mb = 0;
+    int sign   = 1;
     int db;
 
     if (!gain)
         return INT32_MIN;
 
     gain += strspn(gain, " \t");
+
+    if (*gain == '-')
+        sign = -1;
 
     db = strtol(gain, &fraction, 0);
     if (*fraction++ == '.') {
@@ -59,7 +63,7 @@ static int32_t parse_gain(const char *gain)
     if (abs(db) > (INT32_MAX - mb) / 100000)
         return INT32_MIN;
 
-    return db * 100000 + FFSIGN(db) * mb;
+    return db * 100000 + sign * mb;
 }
 
 static uint32_t parse_peak(const uint8_t *peak)
