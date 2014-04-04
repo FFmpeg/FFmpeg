3aac5fcfa9d3748659d78ab2a66d0ccce22cfd4f
Lukasz Marek <lukasz.m.luki@gmail.com>
2014-04-04 19:28:45 +0200

    lavu/opt: validate range before dereference
    This change make error handling simplier.
    av_opt_freep_ranges may be called when some ranges are NULL,
    for example after memory allocation fail.
    
    Signed-off-by: Lukasz Marek <lukasz.m.luki@gmail.com>
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavutil/opt.c
+++ libavutil/opt.c
@@ -1613,8 +1613,10 @@ void av_opt_freep_ranges(AVOptionRanges **rangesp)
 
     for (i = 0; i < ranges->nb_ranges * ranges->nb_components; i++) {
         AVOptionRange *range = ranges->range[i];
-        av_freep(&range->str);
-        av_freep(&ranges->range[i]);
+        if (range) {
+            av_freep(&range->str);
+            av_freep(&ranges->range[i]);
+        }
     }
     av_freep(&ranges->range);
     av_freep(rangesp);
