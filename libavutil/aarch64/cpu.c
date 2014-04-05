8675bcb0addb1c7fb0b04682d1f3f95d5b8dae14
Janne Grunau <j@jannau.net>
2014-04-05 13:46:51 +0200

    aarch64: add armv8 CPU flag

--- libavutil/aarch64/cpu.c
+++ libavutil/aarch64/cpu.c
@@ -22,6 +22,7 @@
 
 int ff_get_cpu_flags_aarch64(void)
 {
-    return AV_CPU_FLAG_NEON * HAVE_NEON |
-           AV_CPU_FLAG_VFP  * HAVE_VFP;
+    return AV_CPU_FLAG_ARMV8 * HAVE_ARMV8 |
+           AV_CPU_FLAG_NEON  * HAVE_NEON  |
+           AV_CPU_FLAG_VFP   * HAVE_VFP;
 }
