861732246ff26186cbaa12edfcdb40abd2fd81b1
Michael Niedermayer <michaelni@gmx.at>
2014-04-05 21:34:03 +0200

    avcodec/wma: use av_freep(), do not leave stale pointers in memory
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>
    (cherry picked from commit d167faafe9dfa0b82bebb267c3c4e5fa5286bd67)
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/wma.c
+++ libavcodec/wma.c
@@ -386,9 +386,9 @@ int ff_wma_end(AVCodecContext *avctx)
     }
     for (i = 0; i < 2; i++) {
         ff_free_vlc(&s->coef_vlc[i]);
-        av_free(s->run_table[i]);
-        av_free(s->level_table[i]);
-        av_free(s->int_table[i]);
+        av_freep(&s->run_table[i]);
+        av_freep(&s->level_table[i]);
+        av_freep(&s->int_table[i]);
     }
 
     return 0;
