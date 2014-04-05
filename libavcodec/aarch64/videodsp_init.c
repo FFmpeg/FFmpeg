d3789eeeed3423bd1ca9dc40030a2f7a21ea5332
Janne Grunau <j@jannau.net>
2014-04-05 11:47:18 +0200

    aarch64: implement videodsp.prefetch
    8% faster h264 decoding on Apple A7.

--- libavcodec/aarch64/videodsp_init.c
+++ libavcodec/aarch64/videodsp_init.c
@@ -1,6 +1,4 @@
 /*
- * Copyright (C) 2012 Ronald S. Bultje
- *
  * This file is part of Libav.
  *
  * Libav is free software; you can redistribute it and/or
@@ -19,36 +17,16 @@
  */
 
 #include "libavutil/attributes.h"
-#include "libavutil/common.h"
-#include "videodsp.h"
+#include "libavutil/cpu.h"
+#include "libavutil/aarch64/cpu.h"
+#include "libavcodec/videodsp.h"
 
-#define BIT_DEPTH 8
-#include "videodsp_template.c"
-#undef BIT_DEPTH
+void ff_prefetch_aarch64(uint8_t *mem, ptrdiff_t stride, int h);
 
-#define BIT_DEPTH 16
-#include "videodsp_template.c"
-#undef BIT_DEPTH
-
-static void just_return(uint8_t *buf, ptrdiff_t stride, int h)
+av_cold void ff_videodsp_init_aarch64(VideoDSPContext *ctx, int bpc)
 {
-}
+    int cpu_flags = av_get_cpu_flags();
 
-av_cold void ff_videodsp_init(VideoDSPContext *ctx, int bpc)
-{
-    ctx->prefetch = just_return;
-    if (bpc <= 8) {
-        ctx->emulated_edge_mc = ff_emulated_edge_mc_8;
-    } else {
-        ctx->emulated_edge_mc = ff_emulated_edge_mc_16;
-    }
-
-    if (ARCH_AARCH64)
-        ff_videodsp_init_aarch64(ctx, bpc);
-    if (ARCH_ARM)
-        ff_videodsp_init_arm(ctx, bpc);
-    if (ARCH_PPC)
-        ff_videodsp_init_ppc(ctx, bpc);
-    if (ARCH_X86)
-        ff_videodsp_init_x86(ctx, bpc);
+    if (have_armv8(cpu_flags))
+        ctx->prefetch = ff_prefetch_aarch64;
 }
