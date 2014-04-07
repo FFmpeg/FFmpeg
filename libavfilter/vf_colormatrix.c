e36aaeca3a3043b95a02ced3f0e36643132e2e60
Michael Niedermayer <michaelni@gmx.at>
2014-04-08 01:25:00 +0200

    avfilter/vf_colormatrix: fix memleak on error
    Fixes CID1197065
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavfilter/vf_colormatrix.c
+++ libavfilter/vf_colormatrix.c
@@ -353,6 +353,7 @@ static int filter_frame(AVFilterLink *link, AVFrame *in)
         case AVCOL_SPC_BT470BG   : source = COLOR_MODE_BT601     ; break;
         default :
             av_log(ctx, AV_LOG_ERROR, "Input frame does not specify a supported colorspace, and none has been specified as source either\n");
+            av_frame_free(&out);
             return AVERROR(EINVAL);
         }
         color->mode = source * 4 + color->dest;
