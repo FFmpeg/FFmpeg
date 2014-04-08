1752b1459d09d822e2765497416c7dd66e6a875b
Michael Niedermayer <michaelni@gmx.at>
2014-04-09 00:54:54 +0200

    swscale/utils: scale filter_size in warning so that it can be used as max for the compile time define
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libswscale/utils.c
+++ libswscale/utils.c
@@ -581,7 +581,8 @@ static av_cold int initFilter(int16_t **outFilter, int32_t **filterPos,
         goto fail;
     if (filterSize >= MAX_FILTER_SIZE * 16 /
                       ((flags & SWS_ACCURATE_RND) ? APCK_SIZE : 16)) {
-        av_log(NULL, AV_LOG_ERROR, "sws: filterSize %d is too large, try less extreme scaling or increase MAX_FILTER_SIZE and recompile\n", filterSize);
+        av_log(NULL, AV_LOG_ERROR, "sws: filterSize %d is too large, try less extreme scaling or increase MAX_FILTER_SIZE and recompile\n",
+               FF_CEIL_RSHIFT((filterSize+1) * ((flags & SWS_ACCURATE_RND) ? APCK_SIZE : 16), 4));
         goto fail;
     }
     *outFilterSize = filterSize;
