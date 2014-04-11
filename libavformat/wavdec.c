549bbdfb4ba44ef69841a7b6f7a9785a2d670299
csheng <csheng@marvell.com>
2014-04-11 03:33:50 -0400

    avformat/wavdec: enlarge probe_packets for wav
    fix issue https://trac.ffmpeg.org/ticket/3550
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/wavdec.c
+++ libavformat/wavdec.c
@@ -114,7 +114,7 @@ static void handle_stream_probing(AVStream *st)
 {
     if (st->codec->codec_id == AV_CODEC_ID_PCM_S16LE) {
         st->request_probe = AVPROBE_SCORE_EXTENSION;
-        st->probe_packets = FFMIN(st->probe_packets, 4);
+        st->probe_packets = FFMIN(st->probe_packets, 14);
     }
 }
 
