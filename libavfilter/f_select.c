98805cbac5a45538b5b3fe2473bfe74f22f2d08c
Michael Niedermayer <michaelni@gmx.at>
2014-04-16 02:06:37 +0200

    avfilter/f_select: fix loss of precission in SAD calculation
    Fixes part of Ticket3466
    Found-by: Andrey_Karpov / PVS-Studio
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>
    (cherry picked from commit 5a8ef3c66b762f265b05aa096105555f1d26879c)
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavfilter/f_select.c
+++ libavfilter/f_select.c
@@ -279,7 +279,7 @@ static double get_scene_score(AVFilterContext *ctx, AVFrame *frame)
             p2 += 8 * linesize;
         }
         emms_c();
-        mafd = nb_sad ? sad / nb_sad : 0;
+        mafd = nb_sad ? (double)sad / nb_sad : 0;
         diff = fabs(mafd - select->prev_mafd);
         ret  = av_clipf(FFMIN(mafd, diff) / 100., 0, 1);
         select->prev_mafd = mafd;
