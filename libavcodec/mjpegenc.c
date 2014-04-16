7b38ea4003ed164f1f131ced270fac81ff555c88
Michael Niedermayer <michaelni@gmx.at>
2014-04-16 02:06:37 +0200

    avcodec/mjpegen: Fix declared argument size
    Fixes part of Ticket3466
    Found-by: Andrey_Karpov / PVS-Studio
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>
    (cherry picked from commit 256f530603ef3838a712a4fcd737b46b7bce455e)
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/mjpegenc.c
+++ libavcodec/mjpegenc.c
@@ -454,7 +454,7 @@ static void encode_block(MpegEncContext *s, int16_t *block, int n)
         put_bits(&s->pb, huff_size_ac[0], huff_code_ac[0]);
 }
 
-void ff_mjpeg_encode_mb(MpegEncContext *s, int16_t block[6][64])
+void ff_mjpeg_encode_mb(MpegEncContext *s, int16_t block[12][64])
 {
     int i;
     if (s->chroma_format == CHROMA_444) {
