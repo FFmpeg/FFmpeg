27998451d86d2da50a20317d0442fa0d6e77100b
Peter Ross <pross@xvid.org>
2014-04-17 21:01:08 +1000

    DSD Stream File (DSF) demuxer
    Signed-off-by: Peter Ross <pross@xvid.org>
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/allformats.c
+++ libavformat/allformats.c
@@ -106,6 +106,7 @@ void av_register_all(void)
     REGISTER_DEMUXER (DFA,              dfa);
     REGISTER_MUXDEMUX(DIRAC,            dirac);
     REGISTER_MUXDEMUX(DNXHD,            dnxhd);
+    REGISTER_DEMUXER (DSF,              dsf);
     REGISTER_DEMUXER (DSICIN,           dsicin);
     REGISTER_MUXDEMUX(DTS,              dts);
     REGISTER_DEMUXER (DTSHD,            dtshd);
