30cf47c6f0168355c3ddd806cfeb563c6a2f7978
Peter Ross <pross@xvid.org>
2014-04-18 14:49:40 +1000

    ff_id3v2_read: add option to limit ID3 magic number search
    Several chunked formats (AIFF, IFF,DSF) store ID3 metadata within an 'ID3 '
    chunk tag. If such chunks are stored sequentially, it is possible for the
    ID3v2 parser to confuse the chunk tag for the ID3 magic number. e.g.
    
    [1st chunk tag ('ID3 ') | chunk size] [ID3 magic number | metadata ...]
    [2nd chunk tag ('ID3 ') | chunk size] [ID3 magic number | metadata ...]
    
    Fixes ticket #3530.
    
    Signed-off-by: Peter Ross <pross@xvid.org>
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>
    (cherry picked from commit 5331773cc33ba26b9e26ace643d926219e46a17b)
    
    Conflicts:
    	libavformat/dsfdec.c

--- libavformat/aiffdec.c
+++ libavformat/aiffdec.c
@@ -237,7 +237,7 @@ static int aiff_read_header(AVFormatContext *s)
             break;
         case MKTAG('I', 'D', '3', ' '):
             position = avio_tell(pb);
-            ff_id3v2_read(s, ID3v2_DEFAULT_MAGIC, &id3v2_extra_meta);
+            ff_id3v2_read(s, ID3v2_DEFAULT_MAGIC, &id3v2_extra_meta, size);
             if (id3v2_extra_meta)
                 if ((ret = ff_id3v2_parse_apic(s, &id3v2_extra_meta)) < 0) {
                     ff_id3v2_free_extra_meta(&id3v2_extra_meta);
