8c88ea76df9b692cc40c4a4826534a02d38a088a
Michael Niedermayer <michaelni@gmx.at>
2014-04-20 17:32:17 +0200

    avcodec/tiff: use av_malloc(z)_array()
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/tiff.c
+++ libavcodec/tiff.c
@@ -838,7 +838,7 @@ static int tiff_decode_tag(TiffContext *s, AVFrame *frame)
             s->geotag_count = 0;
             return -1;
         }
-        s->geotags = av_mallocz(sizeof(TiffGeoTag) * s->geotag_count);
+        s->geotags = av_mallocz_array(s->geotag_count, sizeof(TiffGeoTag));
         if (!s->geotags) {
             av_log(s->avctx, AV_LOG_ERROR, "Error allocating temporary buffer\n");
             s->geotag_count = 0;
@@ -860,7 +860,7 @@ static int tiff_decode_tag(TiffContext *s, AVFrame *frame)
             return AVERROR_INVALIDDATA;
         if (bytestream2_get_bytes_left(&s->gb) < count * sizeof(int64_t))
             return AVERROR_INVALIDDATA;
-        dp = av_malloc(count * sizeof(double));
+        dp = av_malloc_array(count, sizeof(double));
         if (!dp) {
             av_log(s->avctx, AV_LOG_ERROR, "Error allocating temporary buffer\n");
             goto end;
