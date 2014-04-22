e79b15f5f1aeddcd0aeac7f34642752893f06705
Michael Niedermayer <michaelni@gmx.at>
2014-04-23 00:49:34 +0200

    avutil/avutil.h: remove duplicate rational.h include
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavutil/avutil.h
+++ libavutil/avutil.h
@@ -286,7 +286,6 @@ char av_get_picture_type_char(enum AVPictureType pict_type);
 #include "version.h"
 #include "macros.h"
 #include "mathematics.h"
-#include "rational.h"
 #include "log.h"
 #include "pixfmt.h"
 
