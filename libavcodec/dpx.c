e0e60c9211332e3b41512e0a65ba02d613b6fd3f
Michael Niedermayer <michaelni@gmx.at>
2014-04-25 23:13:17 +0200

    avcodec/dpx: extract frame rate
    Fixes part of Ticket3511
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/dpx.c
+++ libavcodec/dpx.c
@@ -20,6 +20,7 @@
  */
 
 #include "libavutil/intreadwrite.h"
+#include "libavutil/intfloat_readwrite.h"
 #include "libavutil/imgutils.h"
 #include "bytestream.h"
 #include "avcodec.h"
@@ -145,6 +146,16 @@ static int decode_frame(AVCodecContext *avctx,
                   0x10000);
     else
         avctx->sample_aspect_ratio = (AVRational){ 0, 1 };
+
+    if (offset >= 1724 + 4) {
+        buf = avpkt->data + 1724;
+        i = read32(&buf, endian);
+        if(i) {
+            AVRational q = av_d2q(av_int2flt(i), 4096);
+            if (q.num > 0 && q.den > 0)
+                avctx->time_base = av_inv_q(q);
+        }
+    }
 
     switch (descriptor) {
     case 6:  // Y
