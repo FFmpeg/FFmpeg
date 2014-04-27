279ff8d2f6dc9b769dc31d6490ef4244c416cafa
Lukasz Marek <lukasz.m.luki2@gmail.com>
2014-04-28 01:34:18 +0200

    lavd/fbdev_enc: move list device code to fbdev_common
    Signed-off-by: Lukasz Marek <lukasz.m.luki2@gmail.com>

--- libavdevice/fbdev_common.c
+++ libavdevice/fbdev_common.c
@@ -20,9 +20,13 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
 #include <stdlib.h>
 #include "fbdev_common.h"
 #include "libavutil/common.h"
+#include "avdevice.h"
 
 struct rgb_pixfmt_map_entry {
     int bits_per_pixel;
@@ -65,3 +69,61 @@ const char* ff_fbdev_default_device()
     return dev;
 }
 
+int ff_fbdev_get_device_list(AVDeviceInfoList *device_list)
+{
+    struct fb_var_screeninfo varinfo;
+    struct fb_fix_screeninfo fixinfo;
+    char device_file[12];
+    AVDeviceInfo *device = NULL;
+    int i, fd, ret = 0;
+    const char *default_device = ff_fbdev_default_device();
+
+    if (!device_list)
+        return AVERROR(EINVAL);
+
+    for (i = 0; i <= 31; i++) {
+        snprintf(device_file, sizeof(device_file), "/dev/fb%d", i);
+
+        if ((fd = avpriv_open(device_file, O_RDWR)) < 0)
+            continue;
+        if (ioctl(fd, FBIOGET_VSCREENINFO, &varinfo) == -1)
+            goto fail_device;
+        if (ioctl(fd, FBIOGET_FSCREENINFO, &fixinfo) == -1)
+            goto fail_device;
+
+        device = av_mallocz(sizeof(AVDeviceInfo));
+        if (!device) {
+            ret = AVERROR(ENOMEM);
+            goto fail_device;
+        }
+        device->device_name = av_strdup(device_file);
+        device->device_description = av_strdup(fixinfo.id);
+        if (!device->device_name || !device->device_description) {
+            ret = AVERROR(ENOMEM);
+            goto fail_device;
+        }
+
+        if ((ret = av_dynarray_add_nofree(&device_list->devices,
+                                          &device_list->nb_devices, device)) < 0)
+            goto fail_device;
+
+        if (default_device && !strcmp(device->device_name, default_device)) {
+            device_list->default_device = device_list->nb_devices - 1;
+            default_device = NULL;
+        }
+        close(fd);
+        continue;
+
+      fail_device:
+        if (device) {
+            av_free(device->device_name);
+            av_free(device->device_description);
+            av_freep(&device);
+        }
+        if (fd >= 0)
+            close(fd);
+        if (ret < 0)
+            return ret;
+    }
+    return 0;
+}
