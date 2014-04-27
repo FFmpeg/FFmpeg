e31727bd53fc69ace0373deabf48360ac6af94ec
Michael Niedermayer <michaelni@gmx.at>
2014-04-28 00:16:42 +0200

    avcodec/mjpegdec: make type of shift unsigned to avoid undefined behavior
    Found-by: Reimar Döffinger <Reimar.Doeffinger@gmx.de>
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/mjpegdec.c
+++ libavcodec/mjpegdec.c
@@ -1272,7 +1272,7 @@ static int mjpeg_decode_scan_progressive_ac(MJpegDecodeContext *s, int ss,
     }
 
     if (!Al) {
-        s->coefs_finished[c] |= (2LL << se) - (1LL << ss);
+        s->coefs_finished[c] |= (2ULL << se) - (1ULL << ss);
         last_scan = !~s->coefs_finished[c];
     }
 
