a1e5be5c1a0c98206a1ae034d278702f5c8ef2a3
Lukasz Marek <lukasz.m.luki2@gmail.com>
2014-04-30 00:23:14 +0200

    lavd/pulse_audio_enc: add pointer checks
    Signed-off-by: Lukasz Marek <lukasz.m.luki2@gmail.com>

--- libavdevice/pulse_audio_enc.c
+++ libavdevice/pulse_audio_enc.c
@@ -692,8 +692,10 @@ static void pulse_get_output_timestamp(AVFormatContext *h, int stream, int64_t *
     pa_threaded_mainloop_lock(s->mainloop);
     pa_stream_get_latency(s->stream, &latency, &neg);
     pa_threaded_mainloop_unlock(s->mainloop);
-    *wall = av_gettime();
-    *dts = s->timestamp - (neg ? -latency : latency);
+    if (wall)
+        *wall = av_gettime();
+    if (dts)
+        *dts = s->timestamp - (neg ? -latency : latency);
 }
 
 static int pulse_get_device_list(AVFormatContext *h, AVDeviceInfoList *device_list)
