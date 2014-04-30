8532566e8022b85a3601b5cfb077bcd4d303ae73
Michael Niedermayer <michaelni@gmx.at>
2014-04-30 18:26:28 +0200

    avcodec/mjpegdec: use av_mallocz_array()
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/mjpegdec.c
+++ libavcodec/mjpegdec.c
@@ -562,8 +562,8 @@ unk_pixfmt:
             int size = bw * bh * s->h_count[i] * s->v_count[i];
             av_freep(&s->blocks[i]);
             av_freep(&s->last_nnz[i]);
-            s->blocks[i]       = av_mallocz(size * sizeof(**s->blocks));
-            s->last_nnz[i]     = av_mallocz(size * sizeof(**s->last_nnz));
+            s->blocks[i]       = av_mallocz_array(size, sizeof(**s->blocks));
+            s->last_nnz[i]     = av_mallocz_array(size, sizeof(**s->last_nnz));
             if (!s->blocks[i] || !s->last_nnz[i])
                 return AVERROR(ENOMEM);
             s->block_stride[i] = bw * s->h_count[i];
