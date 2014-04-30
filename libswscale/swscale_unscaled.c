3d79041f85498eee309bc3c2adc985a9686a5057
Michael Niedermayer <michaelni@gmx.at>
2014-04-30 06:21:58 +0200

    sws: dont use the optimized 410->420 unscaled conversion when height%4
    Fixes Ticket3594
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>
    (cherry picked from commit 421b21ca8a02a346ba03cea3bb2ecc33f791fc30)

--- libswscale/swscale_unscaled.c
+++ libswscale/swscale_unscaled.c
@@ -1017,7 +1017,7 @@ void ff_get_unscaled_swscale(SwsContext *c)
         c->swScale = ff_yuv2rgb_get_func_ptr(c);
     }
 
-    if (srcFormat == AV_PIX_FMT_YUV410P &&
+    if (srcFormat == AV_PIX_FMT_YUV410P && !(dstH & 3) &&
         (dstFormat == AV_PIX_FMT_YUV420P || dstFormat == AV_PIX_FMT_YUVA420P) &&
         !(flags & SWS_BITEXACT)) {
         c->swScale = yvu9ToYv12Wrapper;
