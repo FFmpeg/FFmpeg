c9281a01b78cc3f09e36300a0ca3f5824d1c74cf
Anton Khirnov <anton@khirnov.net>
2014-05-01 13:11:57 +0200

    lavf: drop the zero-sized packets hack
    There should not be any valid reason anymore for passing completely
    empty packets to lavf.
    OTOH side data-only packets can be useful.

--- libavformat/mux.c
+++ libavformat/mux.c
@@ -629,12 +629,6 @@ int av_interleaved_write_frame(AVFormatContext *s, AVPacket *pkt)
     if (pkt) {
         AVStream *st = s->streams[pkt->stream_index];
 
-        //FIXME/XXX/HACK drop zero sized packets
-        if (st->codec->codec_type == AVMEDIA_TYPE_AUDIO && pkt->size == 0) {
-            ret = 0;
-            goto fail;
-        }
-
         av_dlog(s, "av_interleaved_write_frame size:%d dts:%" PRId64 " pts:%" PRId64 "\n",
                 pkt->size, pkt->dts, pkt->pts);
         if ((ret = compute_pkt_fields2(s, st, pkt)) < 0 && !(s->oformat->flags & AVFMT_NOTIMESTAMPS))
