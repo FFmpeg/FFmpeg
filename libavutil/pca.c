beecfa81ac0b539bab3791b868392b625f599043
Michael Niedermayer <michaelni@gmx.at>
2014-05-04 19:00:16 +0200

    avutil/pca: Use av_malloc_array()
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavutil/pca.c
+++ libavutil/pca.c
@@ -42,7 +42,7 @@ PCA *ff_pca_init(int n){
 
     pca= av_mallocz(sizeof(*pca));
     pca->n= n;
-    pca->z = av_malloc(sizeof(*pca->z) * n);
+    pca->z = av_malloc_array(n, sizeof(*pca->z));
     pca->count=0;
     pca->covariance= av_calloc(n*n, sizeof(double));
     pca->mean= av_calloc(n, sizeof(double));
