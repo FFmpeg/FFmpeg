0be95996d0a07a2f92105da1ed8c13d239c46ad8
Michael Niedermayer <michaelni@gmx.at>
2014-05-09 12:45:17 +0200

    avcodec/mpegaudiodec_template: make shift unsigned to avoid undefined behavior
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/mpegaudiodec_template.c
+++ libavcodec/mpegaudiodec_template.c
@@ -216,7 +216,7 @@ static inline int l1_unscale(int n, int mant, int scale_factor)
     shift   = scale_factor_modshift[scale_factor];
     mod     = shift & 3;
     shift >>= 2;
-    val     = MUL64(mant + (-1 << n) + 1, scale_factor_mult[n-1][mod]);
+    val     = MUL64((int)(mant + (-1U << n) + 1), scale_factor_mult[n-1][mod]);
     shift  += n;
     /* NOTE: at this point, 1 <= shift >= 21 + 15 */
     return (int)((val + (1LL << (shift - 1))) >> shift);
