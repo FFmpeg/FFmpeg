c3417ed7fd9239b349921ea15228682586daba88
Michael Niedermayer <michaelni@gmx.at>
2014-05-14 01:31:18 +0200

    swscale/utils: Add check that ensures that the hardcoded struct offsets are valid
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libswscale/utils.c
+++ libswscale/utils.c
@@ -1107,6 +1107,8 @@ SwsContext *sws_alloc_context(void)
 {
     SwsContext *c = av_mallocz(sizeof(SwsContext));
 
+    av_assert0(offsetof(SwsContext, redDither) + DITHER32_INT == offsetof(SwsContext, dither32));
+
     if (c) {
         c->av_class = &sws_context_class;
         av_opt_set_defaults(c);
