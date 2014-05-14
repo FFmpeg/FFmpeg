3af7e7b501d960d6dc07ebfebffc766ebd73fb1a
Michael Niedermayer <michaelni@gmx.at>
2014-05-15 01:54:36 +0200

    avfilter/vsrc_mandelbrot: use av_malloc_array()
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavfilter/vsrc_mandelbrot.c
+++ libavfilter/vsrc_mandelbrot.c
@@ -130,9 +130,9 @@ static av_cold int init(AVFilterContext *ctx)
 
     mb->cache_allocated = mb->w * mb->h * 3;
     mb->cache_used = 0;
-    mb->point_cache= av_malloc(sizeof(*mb->point_cache)*mb->cache_allocated);
-    mb-> next_cache= av_malloc(sizeof(*mb-> next_cache)*mb->cache_allocated);
-    mb-> zyklus    = av_malloc(sizeof(*mb->zyklus) * (mb->maxiter+16));
+    mb->point_cache= av_malloc_array(mb->cache_allocated, sizeof(*mb->point_cache));
+    mb-> next_cache= av_malloc_array(mb->cache_allocated, sizeof(*mb-> next_cache));
+    mb-> zyklus    = av_malloc_array(mb->maxiter + 16, sizeof(*mb->zyklus));
 
     return 0;
 }
