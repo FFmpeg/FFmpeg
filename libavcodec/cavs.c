1fb46858c2498c67ae2d6775f7da29732e88fb8a
Michael Niedermayer <michaelni@gmx.at>
2014-05-16 03:02:57 +0200

    avcodec/cavs: prevent out of array read
    The used value should not matter as long as its within the array
    as it is multiplied by 0
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/cavs.c
+++ libavcodec/cavs.c
@@ -525,7 +525,7 @@ void ff_cavs_inter(AVSContext *h, enum cavs_mb mb_type)
 static inline void scale_mv(AVSContext *h, int *d_x, int *d_y,
                             cavs_vector *src, int distp)
 {
-    int den = h->scale_den[src->ref];
+    int den = h->scale_den[FFMAX(src->ref, 0)];
 
     *d_x = (src->x * distp * den + 256 + (src->x >> 31)) >> 9;
     *d_y = (src->y * distp * den + 256 + (src->y >> 31)) >> 9;
