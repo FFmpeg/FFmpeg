17c8a069167e055b33813aa9a29220c3563573b7
Michael Niedermayer <michaelni@gmx.at>
2014-05-18 04:02:44 +0200

    avformat/framehash: switch to AVFMT_FLAG_BITEXACT
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/framehash.c
+++ libavformat/framehash.c
@@ -24,7 +24,7 @@ int ff_framehash_write_header(AVFormatContext *s)
 {
     int i;
 
-    if (s->nb_streams && !(s->streams[0]->codec->flags & CODEC_FLAG_BITEXACT))
+    if (s->nb_streams && !(s->flags & AVFMT_FLAG_BITEXACT))
         avio_printf(s->pb, "#software: %s\n", LIBAVFORMAT_IDENT);
     for (i = 0; i < s->nb_streams; i++) {
         AVStream *st = s->streams[i];
