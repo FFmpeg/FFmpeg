64fb19cc995f4bf13085ae1f871334a8ff336b46
Michael Niedermayer <michaelni@gmx.at>
2014-05-27 17:43:03 +0200

    avfilter/formats: Avoid using non public AV_PIX_FMT_NB
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavfilter/formats.c
+++ libavfilter/formats.c
@@ -572,7 +572,7 @@ int ff_parse_pixel_format(enum AVPixelFormat *ret, const char *arg, void *log_ct
     int pix_fmt = av_get_pix_fmt(arg);
     if (pix_fmt == AV_PIX_FMT_NONE) {
         pix_fmt = strtol(arg, &tail, 0);
-        if (*tail || (unsigned)pix_fmt >= AV_PIX_FMT_NB) {
+        if (*tail || !av_pix_fmt_desc_get(pix_fmt)) {
             av_log(log_ctx, AV_LOG_ERROR, "Invalid pixel format '%s'\n", arg);
             return AVERROR(EINVAL);
         }
