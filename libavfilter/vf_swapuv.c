e10ac3a12ea82154c36cf7467718968b40b09842
Michael Niedermayer <michaelni@gmx.at>
2014-05-27 17:43:03 +0200

    avfilter/vf_swapuv: Avoid using non public AV_PIX_FMT_NB
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavfilter/vf_swapuv.c
+++ libavfilter/vf_swapuv.c
@@ -73,7 +73,7 @@ static int query_formats(AVFilterContext *ctx)
     AVFilterFormats *formats = NULL;
     int fmt;
 
-    for (fmt = 0; fmt < AV_PIX_FMT_NB; fmt++) {
+    for (fmt = 0; av_pix_fmt_desc_get(fmt); fmt++) {
         const AVPixFmtDescriptor *desc = av_pix_fmt_desc_get(fmt);
         if (is_planar_yuv(desc))
             ff_add_format(&formats, fmt);
