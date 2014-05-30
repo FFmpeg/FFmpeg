8c891d90cac96e1131a82209f8c6399651fe3b8b
Michael Niedermayer <michaelni@gmx.at>
2014-05-30 03:09:23 +0200

    avcodec/x86/qpeldsp_init: Restore author attribution
    See: 368f50359eb328b0b9d67451f56fda20b3255f9a
    See: 44eb49512888143905860af2de2932ab002cdbf7, and many others
    See:
    similarity index 83%
    copy from libavcodec/x86/dsputil_init.c
    copy to libavcodec/x86/qpeldsp_init.c
    index ebbf97f..8f296a1 100644
    --- a/libavcodec/x86/dsputil_init.c
    +++ b/libavcodec/x86/qpeldsp_init.c
    @@ -1,6 +1,5 @@
     /*
    - * Copyright (c) 2000, 2001 Fabrice Bellard
    - * Copyright (c) 2002-2004 Michael Niedermayer <michaelni@gmx.at>
    + * quarterpel DSP functions
      *
      * This file is part of FFmpeg.
      *
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/x86/qpeldsp_init.c
+++ libavcodec/x86/qpeldsp_init.c
@@ -1,5 +1,7 @@
 /*
  * quarterpel DSP functions
+ * Copyright (c) 2000, 2001 Fabrice Bellard
+ * Copyright (c) 2002-2004 Michael Niedermayer <michaelni@gmx.at>
  *
  * This file is part of FFmpeg.
  *
