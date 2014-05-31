879aa062f973bf0f357bde8e5722522bc85d1884
Michael Niedermayer <michaelni@gmx.at>
2014-05-31 17:06:26 +0200

    avutil/frame: frame_copy_video: support copying from a smaller to a larger frame
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavutil/frame.c
+++ libavutil/frame.c
@@ -589,8 +589,8 @@ static int frame_copy_video(AVFrame *dst, const AVFrame *src)
     const uint8_t *src_data[4];
     int i, planes;
 
-    if (dst->width  != src->width ||
-        dst->height != src->height)
+    if (dst->width  < src->width ||
+        dst->height < src->height)
         return AVERROR(EINVAL);
 
     planes = av_pix_fmt_count_planes(dst->format);
@@ -601,7 +601,7 @@ static int frame_copy_video(AVFrame *dst, const AVFrame *src)
     memcpy(src_data, src->data, sizeof(src_data));
     av_image_copy(dst->data, dst->linesize,
                   src_data, src->linesize,
-                  dst->format, dst->width, dst->height);
+                  dst->format, src->width, src->height);
 
     return 0;
 }
