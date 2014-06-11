a4bd4733c0af333f2907f6be276b5094891903a0
Derek Buitenhuis <derek.buitenhuis@gmail.com>
2014-06-11 17:51:49 +0100

    swscale: Allow the max filter size to be set at compile time
    This can help "extreme" resizes, e.g with some 4k stuff.
    
    Signed-off-by: Derek Buitenhuis <derek.buitenhuis@gmail.com>

--- libswscale/utils.c
+++ libswscale/utils.c
@@ -582,7 +582,7 @@ static av_cold int initFilter(int16_t **outFilter, int32_t **filterPos,
         goto fail;
     if (filterSize >= MAX_FILTER_SIZE * 16 /
                       ((flags & SWS_ACCURATE_RND) ? APCK_SIZE : 16)) {
-        av_log(NULL, AV_LOG_ERROR, "sws: filterSize %d is too large, try less extreme scaling or increase MAX_FILTER_SIZE and recompile\n",
+        av_log(NULL, AV_LOG_ERROR, "sws: filterSize %d is too large, try less extreme scaling or set --sws-max-filter-size and recompile\n",
                FF_CEIL_RSHIFT((filterSize+1) * ((flags & SWS_ACCURATE_RND) ? APCK_SIZE : 16), 4));
         goto fail;
     }
