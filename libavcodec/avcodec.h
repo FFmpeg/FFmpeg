67d29da4bd23057a1f646568442a77b844cb2d1b
Michael Niedermayer <michaelni@gmx.at>
2014-06-11 21:18:14 +0200

    avcodec: increase FF_INPUT_BUFFER_PADDING_SIZE to 32
    Sometimes the input buffers get directly used as raw images and
    SIMD optimized video/image filters can sometimes read more than 16 bytes
    over the end.
    a specific example is the AVX 24bpp to yuv code
    
    This also fixes fate-vsynth3-rgb
    
    Reviewed-by: Christophe Gisquet <christophe.gisquet@gmail.com>
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/avcodec.h
+++ libavcodec/avcodec.h
@@ -614,7 +614,7 @@ typedef struct AVCodecDescriptor {
  * Note: If the first 23 bits of the additional bytes are not 0, then damaged
  * MPEG bitstreams could cause overread and segfault.
  */
-#define FF_INPUT_BUFFER_PADDING_SIZE 16
+#define FF_INPUT_BUFFER_PADDING_SIZE 32
 
 /**
  * @ingroup lavc_encoding
