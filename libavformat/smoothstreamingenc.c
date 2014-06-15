c1b15c16ef748777336222f57d753f3e6b095d7f
Michael Niedermayer <michaelni@gmx.at>
2014-06-15 23:20:56 +0200

    avformat/smoothstreamingenc: Use av_mallocz_array()
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/smoothstreamingenc.c
+++ libavformat/smoothstreamingenc.c
@@ -303,7 +303,7 @@ static int ism_write_header(AVFormatContext *s)
         goto fail;
     }
 
-    c->streams = av_mallocz(sizeof(*c->streams) * s->nb_streams);
+    c->streams = av_mallocz_array(s->nb_streams, sizeof(*c->streams));
     if (!c->streams) {
         ret = AVERROR(ENOMEM);
         goto fail;
