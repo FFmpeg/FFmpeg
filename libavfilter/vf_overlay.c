d5f817793e297e5ed0277df7813897d87ba51e30
Clément Bœsch <u@pkh.me>
2014-06-29 13:26:12 +0200

    avfilter/overlay: reindent

--- libavfilter/vf_overlay.c
+++ libavfilter/vf_overlay.c
@@ -554,21 +554,20 @@ static AVFrame *do_blend(AVFilterContext *ctx, AVFrame *mainpic,
     OverlayContext *s = ctx->priv;
     AVFilterLink *inlink = ctx->inputs[0];
 
-        /* TODO: reindent */
-        if (s->eval_mode == EVAL_MODE_FRAME) {
-            int64_t pos = av_frame_get_pkt_pos(mainpic);
+    if (s->eval_mode == EVAL_MODE_FRAME) {
+        int64_t pos = av_frame_get_pkt_pos(mainpic);
 
-            s->var_values[VAR_N] = inlink->frame_count;
-            s->var_values[VAR_T] = mainpic->pts == AV_NOPTS_VALUE ?
-                NAN : mainpic->pts * av_q2d(inlink->time_base);
-            s->var_values[VAR_POS] = pos == -1 ? NAN : pos;
+        s->var_values[VAR_N] = inlink->frame_count;
+        s->var_values[VAR_T] = mainpic->pts == AV_NOPTS_VALUE ?
+            NAN : mainpic->pts * av_q2d(inlink->time_base);
+        s->var_values[VAR_POS] = pos == -1 ? NAN : pos;
 
-            eval_expr(ctx);
-            av_log(ctx, AV_LOG_DEBUG, "n:%f t:%f pos:%f x:%f xi:%d y:%f yi:%d\n",
-                   s->var_values[VAR_N], s->var_values[VAR_T], s->var_values[VAR_POS],
-                   s->var_values[VAR_X], s->x,
-                   s->var_values[VAR_Y], s->y);
-        }
+        eval_expr(ctx);
+        av_log(ctx, AV_LOG_DEBUG, "n:%f t:%f pos:%f x:%f xi:%d y:%f yi:%d\n",
+               s->var_values[VAR_N], s->var_values[VAR_T], s->var_values[VAR_POS],
+               s->var_values[VAR_X], s->x,
+               s->var_values[VAR_Y], s->y);
+    }
 
     blend_image(ctx, mainpic, second, s->x, s->y);
     return mainpic;
