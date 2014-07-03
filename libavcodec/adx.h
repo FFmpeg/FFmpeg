d5cf5afabbf43f00283e70b12afbe1da030d85b6
Anton Khirnov <anton@khirnov.net>
2014-07-03 19:01:59 +0000

    adxdec: get rid of an avpriv function
    The only thing the demuxer needs is the sample rate to set the timebase,
    which can be simply read with AV_RB32.

--- libavcodec/adx.h
+++ libavcodec/adx.h
@@ -74,7 +74,12 @@ void ff_adx_calculate_coeffs(int cutoff, int sample_rate, int bits, int *coeff);
  * @param[out] coeff        2 LPC coefficients, can be NULL
  * @return data offset or negative error code if header is invalid
  */
+int ff_adx_decode_header(AVCodecContext *avctx, const uint8_t *buf,
+                         int bufsize, int *header_size, int *coeff);
+
+#if LIBAVCODEC_VERSION_MAJOR < 56
 int avpriv_adx_decode_header(AVCodecContext *avctx, const uint8_t *buf,
                              int bufsize, int *header_size, int *coeff);
+#endif
 
 #endif /* AVCODEC_ADX_H */
