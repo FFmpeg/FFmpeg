dea377fcc0cbb2ffe4b1be860d2e207da28b098a
James Zern <jzern@google.com>
2014-07-02 20:28:39 -0700

    ivfenc: support VP9
    libvpx tools accept vp9 ivf files
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/ivfenc.c
+++ libavformat/ivfenc.c
@@ -30,8 +30,9 @@ static int ivf_write_header(AVFormatContext *s)
         return AVERROR(EINVAL);
     }
     ctx = s->streams[0]->codec;
-    if (ctx->codec_type != AVMEDIA_TYPE_VIDEO || ctx->codec_id != AV_CODEC_ID_VP8) {
-        av_log(s, AV_LOG_ERROR, "Currently only VP8 is supported!\n");
+    if (ctx->codec_type != AVMEDIA_TYPE_VIDEO ||
+        !(ctx->codec_id == AV_CODEC_ID_VP8 || ctx->codec_id == AV_CODEC_ID_VP9)) {
+        av_log(s, AV_LOG_ERROR, "Currently only VP8 and VP9 are supported!\n");
         return AVERROR(EINVAL);
     }
     avio_write(pb, "DKIF", 4);
