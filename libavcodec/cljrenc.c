373a6dda5422186bc057297342a9e559a564595e
Nidhi Makhijani <nidhimj22@gmail.com>
2014-07-05 21:04:05 +0530

    cljr: split decoder and encoder
    Signed-off-by: Diego Biurrun <diego@biurrun.de>

--- libavcodec/cljrenc.c
+++ libavcodec/cljrenc.c
@@ -1,5 +1,5 @@
 /*
- * Cirrus Logic AccuPak (CLJR) codec
+ * Cirrus Logic AccuPak (CLJR) encoder
  * Copyright (c) 2003 Alex Beregszaszi
  *
  * This file is part of Libav.
@@ -21,83 +21,15 @@
 
 /**
  * @file
- * Cirrus Logic AccuPak codec.
+ * Cirrus Logic AccuPak encoder.
  */
 
+#include "libavutil/common.h"
+
 #include "avcodec.h"
-#include "get_bits.h"
 #include "internal.h"
 #include "put_bits.h"
 
-#if CONFIG_CLJR_DECODER
-static int decode_frame(AVCodecContext *avctx,
-                        void *data, int *got_frame,
-                        AVPacket *avpkt)
-{
-    const uint8_t *buf = avpkt->data;
-    int buf_size       = avpkt->size;
-    GetBitContext gb;
-    AVFrame * const p = data;
-    int x, y, ret;
-
-    if (avctx->height <= 0 || avctx->width <= 0) {
-        av_log(avctx, AV_LOG_ERROR, "Invalid width or height\n");
-        return AVERROR_INVALIDDATA;
-    }
-
-    if (buf_size < avctx->height * avctx->width) {
-        av_log(avctx, AV_LOG_ERROR,
-               "Resolution larger than buffer size. Invalid header?\n");
-        return AVERROR_INVALIDDATA;
-    }
-
-    if ((ret = ff_get_buffer(avctx, p, 0)) < 0) {
-        av_log(avctx, AV_LOG_ERROR, "get_buffer() failed\n");
-        return ret;
-    }
-    p->pict_type = AV_PICTURE_TYPE_I;
-    p->key_frame = 1;
-
-    init_get_bits(&gb, buf, buf_size * 8);
-
-    for (y = 0; y < avctx->height; y++) {
-        uint8_t *luma = &p->data[0][y * p->linesize[0]];
-        uint8_t *cb   = &p->data[1][y * p->linesize[1]];
-        uint8_t *cr   = &p->data[2][y * p->linesize[2]];
-        for (x = 0; x < avctx->width; x += 4) {
-            luma[3] = get_bits(&gb, 5) << 3;
-            luma[2] = get_bits(&gb, 5) << 3;
-            luma[1] = get_bits(&gb, 5) << 3;
-            luma[0] = get_bits(&gb, 5) << 3;
-            luma += 4;
-            *(cb++) = get_bits(&gb, 6) << 2;
-            *(cr++) = get_bits(&gb, 6) << 2;
-        }
-    }
-
-    *got_frame = 1;
-
-    return buf_size;
-}
-
-static av_cold int decode_init(AVCodecContext *avctx)
-{
-    avctx->pix_fmt = AV_PIX_FMT_YUV411P;
-    return 0;
-}
-
-AVCodec ff_cljr_decoder = {
-    .name           = "cljr",
-    .long_name      = NULL_IF_CONFIG_SMALL("Cirrus Logic AccuPak"),
-    .type           = AVMEDIA_TYPE_VIDEO,
-    .id             = AV_CODEC_ID_CLJR,
-    .init           = decode_init,
-    .decode         = decode_frame,
-    .capabilities   = CODEC_CAP_DR1,
-};
-#endif
-
-#if CONFIG_CLJR_ENCODER
 static av_cold int encode_init(AVCodecContext *avctx)
 {
     avctx->coded_frame = av_frame_alloc();
@@ -163,4 +95,3 @@ AVCodec ff_cljr_encoder = {
     .pix_fmts       = (const enum AVPixelFormat[]) { AV_PIX_FMT_YUV411P,
                                                    AV_PIX_FMT_NONE },
 };
-#endif
