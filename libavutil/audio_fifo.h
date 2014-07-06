a7985cfd4c51b7fe2b870fc4ecd109707ee035d6
Timothy Gu <timothygu99@gmail.com>
2014-07-06 10:54:17 -0700

    audio_fifo: Split into a separate doxygen module
    Signed-off-by: Timothy Gu <timothygu99@gmail.com>
    Signed-off-by: Diego Biurrun <diego@biurrun.de>

--- libavutil/audio_fifo.h
+++ libavutil/audio_fifo.h
@@ -34,6 +34,9 @@
 /**
  * @addtogroup lavu_audio
  * @{
+ *
+ * @defgroup lavu_audiofifo Audio FIFO Buffer
+ * @{
  */
 
 /**
@@ -140,6 +143,7 @@ int av_audio_fifo_size(AVAudioFifo *af);
 int av_audio_fifo_space(AVAudioFifo *af);
 
 /**
+ * @}
  * @}
  */
 
