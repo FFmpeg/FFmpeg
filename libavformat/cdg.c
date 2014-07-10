0089fb79cc11eef199c0c63da472aefbeb9fed2c
Michael Niedermayer <michaelni@gmx.at>
2014-07-11 01:04:04 +0200

    avformat/cdg: Do not fail if filesize cannot be determined
    This fixes cdg with piped input which was broken by the previous commit
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/cdg.c
+++ libavformat/cdg.c
@@ -46,10 +46,11 @@ static int read_header(AVFormatContext *s)
     avpriv_set_pts_info(vst, 32, 1, 300);
 
     ret = avio_size(s->pb);
-    if (ret < 0)
-        return ret;
+    if (ret < 0) {
+        av_log(s, AV_LOG_WARNING, "Cannot calculate duration as file size cannot be determined\n");
+    } else
+        vst->duration = (ret * vst->time_base.den) / (CDG_PACKET_SIZE * 300);
 
-    vst->duration = (ret * vst->time_base.den) / (CDG_PACKET_SIZE * 300);
     return 0;
 }
 
