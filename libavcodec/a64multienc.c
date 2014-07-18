d5ba19e611b0b25ec249f7db2b99594519db99da
Michael Niedermayer <michaelni@gmx.at>
2014-07-18 04:57:00 +0200

    avcodec/a64multienc use av_freep()
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/a64multienc.c
+++ libavcodec/a64multienc.c
@@ -187,11 +187,11 @@ static av_cold int a64multi_close_encoder(AVCodecContext *avctx)
 {
     A64Context *c = avctx->priv_data;
     av_frame_free(&avctx->coded_frame);
-    av_free(c->mc_meta_charset);
-    av_free(c->mc_best_cb);
-    av_free(c->mc_charset);
-    av_free(c->mc_charmap);
-    av_free(c->mc_colram);
+    av_freep(&c->mc_meta_charset);
+    av_freep(&c->mc_best_cb);
+    av_freep(&c->mc_charset);
+    av_freep(&c->mc_charmap);
+    av_freep(&c->mc_colram);
     return 0;
 }
 
