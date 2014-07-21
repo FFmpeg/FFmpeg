eba2233b58c2c4b468c58287d6537b2f1188a8cd
Diego Biurrun <diego@biurrun.de>
2014-07-21 13:58:55 -0700

    build: Add define for SIMD extensions requiring 16-byte aligned buffers

--- libavcodec/utils.c
+++ libavcodec/utils.c
@@ -189,7 +189,7 @@ int ff_side_data_update_matrix_encoding(AVFrame *frame,
     return 0;
 }
 
-#if HAVE_NEON || ARCH_PPC || HAVE_MMX
+#if HAVE_SIMD_ALIGN_16
 #   define STRIDE_ALIGN 16
 #else
 #   define STRIDE_ALIGN 8
