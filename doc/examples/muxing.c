5d2cf1ae8642608dcddf6a237f34f4c14123cfcc
Michael Niedermayer <michaelni@gmx.at>
2014-07-26 22:19:10 +0200

    doc/examples/muxing: select a supported sample rate for the encoder, favor 44100
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- doc/examples/muxing.c
+++ doc/examples/muxing.c
@@ -117,6 +117,14 @@ static void add_stream(OutputStream *ost, AVFormatContext *oc,
             (*codec)->sample_fmts[0] : AV_SAMPLE_FMT_FLTP;
         c->bit_rate    = 64000;
         c->sample_rate = 44100;
+        if ((*codec)->supported_samplerates) {
+            c->sample_rate = (*codec)->supported_samplerates[0];
+            for (i = 0; (*codec)->supported_samplerates[i]; i++) {
+                if ((*codec)->supported_samplerates[i] == 44100)
+                    c->sample_rate = 44100;
+            }
+        }
+        c->channels        = av_get_channel_layout_nb_channels(c->channel_layout);
         c->channel_layout = AV_CH_LAYOUT_STEREO;
         if ((*codec)->channel_layouts) {
             c->channel_layout = (*codec)->channel_layouts[0];
