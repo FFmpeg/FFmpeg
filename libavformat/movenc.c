f2855eb4d7128e53c027bc38b0e9627138fed607
gerion.entrup@t-online.de <gerion.entrup@t-online.de>
2014-07-30 04:19:15 +0200

    avformat/movenc: add m4b to list of ipod playable files
    m4b is the extension used by iDevices to detect audiobooks.

--- libavformat/movenc.c
+++ libavformat/movenc.c
@@ -895,8 +895,10 @@ static int ipod_get_codec_tag(AVFormatContext *s, MOVTrack *track)
            tag == MKTAG('t', 'e', 'x', 't'))))
         tag = ff_codec_get_tag(codec_ipod_tags, track->enc->codec_id);
 
-    if (!av_match_ext(s->filename, "m4a") && !av_match_ext(s->filename, "m4v"))
-        av_log(s, AV_LOG_WARNING, "Warning, extension is not .m4a nor .m4v "
+    if (!av_match_ext(s->filename, "m4a") &&
+        !av_match_ext(s->filename, "m4b") &&
+        !av_match_ext(s->filename, "m4v"))
+        av_log(s, AV_LOG_WARNING, "Warning, extension is not .m4a, .m4v nor  .m4b "
                "Quicktime/Ipod might not play the file\n");
 
     return tag;
