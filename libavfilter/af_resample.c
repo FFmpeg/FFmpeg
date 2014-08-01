c060d046aa2f89c0e601a2dcfbce53f0e36cf498
Luca Barbato <lu_zero@gentoo.org>
2014-08-02 01:58:07 +0200

    af_resample: Set the number of samples in the last frame
    Otherwise trailing zeroes would appear.
    
    Signed-off-by: Luca Barbato <lu_zero@gentoo.org>

--- libavfilter/af_resample.c
+++ libavfilter/af_resample.c
@@ -202,6 +202,7 @@ static int request_frame(AVFilterLink *outlink)
             return (ret == 0) ? AVERROR_EOF : ret;
         }
 
+        frame->nb_samples = ret;
         frame->pts = s->next_pts;
         return ff_filter_frame(outlink, frame);
     }
