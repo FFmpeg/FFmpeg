7096e8738b8890cadaeaa7f80f9031a49965ded8
Matthew Oliver <protogonoi@gmail.com>
2014-08-02 11:56:51 +1000

    Update VS projects to support libcdio by default.

--- SMC/unistd.h
+++ SMC/unistd.h
@@ -11,14 +11,37 @@
 #include <stdlib.h>
 //#include <io.h>
 #include <process.h>
+#include <direct.h>
+#include <fcntl.h>
+
+#define __UNISTD_GETOPT__
+//#include <getopt.h>
+#undef __UNISTD_GETOPT__
 
 #define R_OK    4       /* Test for read permission.  */
 #define W_OK    2       /* Test for write permission.  */
 //#define   X_OK    1       /* execute permission - unsupported in windows*/
 #define F_OK    0       /* Test for existence.  */
 
+#ifndef STDIN_FILENO
+#define STDIN_FILENO 0
+#endif
+
+#ifndef STDOUT_FILENO
+#define STDOUT_FILENO 1
+#endif
+
+#ifndef STDERR_FILENO
+#define STDERR_FILENO 2
+#endif
+
+#define srandom srand
+#define random rand
+
 #define inline __inline
 typedef unsigned short mode_t;
+#include <BaseTsd.h>
+typedef SSIZE_T ssize_t;
 
 #endif
 
