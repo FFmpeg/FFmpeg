aa943bd31fada23db5cb9611215656ab9ebe5b94
Michael Niedermayer <michaelni@gmx.at>
2014-08-03 00:54:33 +0100

    huffyuvdec: check width size for yuv422p
    Avoid out of array accesses.
    
    CC: libav-stable@libav.org
    Bug-Id: CVE-2013-0848
    Signed-off-by: Vittorio Giovara <vittorio.giovara@gmail.com>
    Signed-off-by: Anton Khirnov <anton@khirnov.net>
    (cherry picked from commit a7153444df9040bf6ae103e0bbf6104b66f974cb)
    Signed-off-by: Anton Khirnov <anton@khirnov.net>

--- libavcodec/huffyuvdec.c
+++ libavcodec/huffyuvdec.c
@@ -339,6 +339,13 @@ static av_cold int decode_init(AVCodecContext *avctx)
         return AVERROR_INVALIDDATA;
     }
 
+    if (s->predictor == MEDIAN && avctx->pix_fmt == AV_PIX_FMT_YUV422P &&
+        avctx->width % 4) {
+        av_log(avctx, AV_LOG_ERROR, "width must be multiple of 4 "
+               "for this combination of colorspace and predictor type.\n");
+        return AVERROR_INVALIDDATA;
+    }
+
     if ((ret = ff_huffyuv_alloc_temp(s)) < 0)
         return ret;
 
