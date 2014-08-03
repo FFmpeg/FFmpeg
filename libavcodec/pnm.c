b4d525eb634666e39745585b17c35faed54352b6
Michael Niedermayer <michaelni@gmx.at>
2014-08-03 14:01:03 +0200

    avcodec/pnm: check buf[0] before using buf[1]
    Fixes CSA warning
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/pnm.c
+++ libavcodec/pnm.c
@@ -63,9 +63,9 @@ int ff_pnm_decode_header(AVCodecContext *avctx, PNMContext * const s)
     int h, w, depth, maxval;
 
     pnm_get(s, buf1, sizeof(buf1));
-    s->type= buf1[1]-'0';
     if(buf1[0] != 'P')
         return AVERROR_INVALIDDATA;
+    s->type= buf1[1]-'0';
 
     if (s->type==1 || s->type==4) {
         avctx->pix_fmt = AV_PIX_FMT_MONOWHITE;
