592a854f8c8bf402efad5d651363bdb17f85786a
Michael Niedermayer <michaelni@gmx.at>
2014-08-05 18:16:46 +0200

    avformat/iff: Fix "source comment"
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/iff.c
+++ libavformat/iff.c
@@ -584,7 +584,7 @@ static int iff_read_header(AVFormatContext *s)
                     }
                     break;
                 case 2:
-                    tag = ref < FF_ARRAY_ELEMS(dsd_source_comment) ? dsd_history_comment[ref] : "source_comment";
+                    tag = ref < FF_ARRAY_ELEMS(dsd_source_comment) ? dsd_source_comment[ref] : "source_comment";
                     break;
                 case 3:
                     tag = ref < FF_ARRAY_ELEMS(dsd_history_comment) ? dsd_history_comment[ref] : "file_history";
