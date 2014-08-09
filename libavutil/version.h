c89afc9acfc739dabe93f109f0635e242a767355
Michael Niedermayer <michaelni@gmx.at>
2014-08-09 22:35:23 +0200

    delay removial of FF_API_CODEC_ID, FF_API_OLD_AVOPTIONS and FF_API_OLD_ENCODE_VIDEO
    This prevents a build failure when bumping.
    the uses could easily be updated / removed, if people prefer.
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavutil/version.h
+++ libavutil/version.h
@@ -87,7 +87,7 @@
 #define FF_API_FIND_OPT                 (LIBAVUTIL_VERSION_MAJOR < 54)
 #endif
 #ifndef FF_API_OLD_AVOPTIONS
-#define FF_API_OLD_AVOPTIONS            (LIBAVUTIL_VERSION_MAJOR < 54)
+#define FF_API_OLD_AVOPTIONS            (LIBAVUTIL_VERSION_MAJOR < 55)
 #endif
 #ifndef FF_API_PIX_FMT
 #define FF_API_PIX_FMT                  (LIBAVUTIL_VERSION_MAJOR < 55)
