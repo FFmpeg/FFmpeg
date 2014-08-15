e7f3b507a3437db1f4157abffdda2218afd739e0
Michael Niedermayer <michaelni@gmx.at>
2014-08-16 00:49:12 +0200

    avcodec/mips/compute_antialias_float: remove unused variable
    Reviewed-by: James Darnley <james.darnley@gmail.com>
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/mips/compute_antialias_float.h
+++ libavcodec/mips/compute_antialias_float.h
@@ -61,7 +61,6 @@ static void compute_antialias_mips_float(MPADecodeContext *s,
 {
     float *ptr, *ptr_end;
     float *csa = &csa_table[0][0];
-    int n;
     /* temporary variables */
     float in1, in2, in3, in4, in5, in6, in7, in8;
     float out1, out2, out3, out4;
@@ -72,10 +71,8 @@ static void compute_antialias_mips_float(MPADecodeContext *s,
         if (!g->switch_point)
             return;
         /* XXX: check this for 8000Hz case */
-        n = 1;
         ptr_end = ptr + 18;
     } else {
-        n = 31;
         ptr_end = ptr + 558;
     }
 
