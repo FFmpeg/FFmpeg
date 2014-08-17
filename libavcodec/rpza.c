b6c9266722d7732784930bb5728986e022257958
Michael Niedermayer <michaelni@gmx.at>
2014-08-17 14:30:32 +0200

    avcodec/rpza: fix +- error
    Fixes Ticket3471
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/rpza.c
+++ libavcodec/rpza.c
@@ -94,11 +94,11 @@ static void rpza_decode_stream(RpzaContext *s)
     chunk_size = bytestream2_get_be32(&s->gb) & 0x00FFFFFF;
 
     /* If length mismatch use size from MOV file and try to decode anyway */
-    if (chunk_size != bytestream2_get_bytes_left(&s->gb) - 4)
+    if (chunk_size != bytestream2_get_bytes_left(&s->gb) + 4)
         av_log(s->avctx, AV_LOG_WARNING,
                "MOV chunk size %d != encoded chunk size %d\n",
                chunk_size,
-               bytestream2_get_bytes_left(&s->gb) - 4
+               bytestream2_get_bytes_left(&s->gb) + 4
               );
 
     /* Number of 4x4 blocks in frame. */
