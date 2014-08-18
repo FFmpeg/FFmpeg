90b2f3136778311fb5e097b8ee1f527518231c23
Paul B Mahol <onemda@gmail.com>
2014-08-18 10:32:57 +0000

    cafdec: check for eof in read_info_chunk()
    Fixes Ticket #3864
    Found-by: Piotr Bandurski <ami_stuff@o2.pl>
    Signed-off-by: Paul B Mahol <onemda@gmail.com>

--- libavformat/cafdec.c
+++ libavformat/cafdec.c
@@ -205,7 +205,7 @@ static void read_info_chunk(AVFormatContext *s, int64_t size)
     AVIOContext *pb = s->pb;
     unsigned int i;
     unsigned int nb_entries = avio_rb32(pb);
-    for (i = 0; i < nb_entries; i++) {
+    for (i = 0; i < nb_entries && !avio_feof(pb); i++) {
         char key[32];
         char value[1024];
         avio_get_str(pb, INT_MAX, key, sizeof(key));
