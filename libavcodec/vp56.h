2143948381c8118bdc2f50bd4079520b9885bd54
Diego Biurrun <diego@biurrun.de>
2014-09-02 23:25:07 +0200

    Drop unnecessary av_unused attributes.

--- libavcodec/vp56.h
+++ libavcodec/vp56.h
@@ -299,7 +299,7 @@ static av_always_inline int vp8_rac_get(VP56RangeCoder *c)
     return vp56_rac_get_prob(c, 128);
 }
 
-static av_unused int vp56_rac_gets(VP56RangeCoder *c, int bits)
+static int vp56_rac_gets(VP56RangeCoder *c, int bits)
 {
     int value = 0;
 
@@ -310,7 +310,7 @@ static av_unused int vp56_rac_gets(VP56RangeCoder *c, int bits)
     return value;
 }
 
-static av_unused int vp8_rac_get_uint(VP56RangeCoder *c, int bits)
+static int vp8_rac_get_uint(VP56RangeCoder *c, int bits)
 {
     int value = 0;
 
