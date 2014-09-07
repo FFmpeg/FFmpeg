ced4e9fdbba79f752cdace41be8627fe798cde7c
Michael Niedermayer <michaelni@gmx.at>
2014-09-07 16:39:39 +0200

    avformat/m4vdec: Check for non startcode 00 00 00 sequences in probe
    Fixes miss detection of PCM as m4v
    Fixes Ticket 3928
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>
    (cherry picked from commit 7c1835c52a4be2e4e996f83c91a8d5a147b01100)
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/m4vdec.c
+++ libavformat/m4vdec.c
@@ -33,13 +33,15 @@ static int mpeg4video_probe(AVProbeData *probe_packet)
 
     for(i=0; i<probe_packet->buf_size; i++){
         temp_buffer = (temp_buffer<<8) + probe_packet->buf[i];
-        if ((temp_buffer & 0xffffff00) != 0x100)
+        if (temp_buffer & 0xfffffe00)
+            continue;
+        if (temp_buffer < 2)
             continue;
 
         if (temp_buffer == VOP_START_CODE)                         VOP++;
         else if (temp_buffer == VISUAL_OBJECT_START_CODE)          VISO++;
-        else if (temp_buffer < 0x120)                              VO++;
-        else if (temp_buffer < 0x130)                              VOL++;
+        else if (temp_buffer >= 0x100 && temp_buffer < 0x120)      VO++;
+        else if (temp_buffer >= 0x120 && temp_buffer < 0x130)      VOL++;
         else if (   !(0x1AF < temp_buffer && temp_buffer < 0x1B7)
                  && !(0x1B9 < temp_buffer && temp_buffer < 0x1C4)) res++;
     }
