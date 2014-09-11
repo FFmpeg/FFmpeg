a87527ad67377d502fc0731021239797cb8e9481
Clément Bœsch <u@pkh.me>
2014-09-11 21:09:39 +0200

    avfilter/ass: make sure the log level are in available range

--- libavfilter/vf_subtitles.c
+++ libavfilter/vf_subtitles.c
@@ -80,7 +80,9 @@ static const int ass_libavfilter_log_level_map[] = {
 
 static void ass_log(int ass_level, const char *fmt, va_list args, void *ctx)
 {
-    int level = ass_libavfilter_log_level_map[ass_level];
+    const int ass_level_clip = av_clip(ass_level, 0,
+        FF_ARRAY_ELEMS(ass_libavfilter_log_level_map) - 1);
+    const int level = ass_libavfilter_log_level_map[ass_level_clip];
 
     av_vlog(ctx, level, fmt, args);
     av_log(ctx, level, "\n");
