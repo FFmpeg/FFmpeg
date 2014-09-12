883e98bc25755327565d8ae0f565a6c886f24816
Andreas Cadhalpun <Andreas.Cadhalpun@googlemail.com>
2014-09-13 01:25:22 +0200

    probe: Bump the score for mime type matching
    It should be more trustworthy than extension matching.
    
    Signed-off-by: Luca Barbato <lu_zero@gentoo.org>

--- libavformat/format.c
+++ libavformat/format.c
@@ -196,7 +196,7 @@ AVInputFormat *av_probe_input_format2(AVProbeData *pd, int is_opened,
                 score = AVPROBE_SCORE_EXTENSION;
         }
         if (av_match_name(lpd.mime_type, fmt1->mime_type))
-            score = FFMAX(score, AVPROBE_SCORE_EXTENSION);
+            score = FFMAX(score, AVPROBE_SCORE_MIME);
         if (score > *score_max) {
             *score_max = score;
             fmt        = fmt1;
