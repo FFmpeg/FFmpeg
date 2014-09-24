4f4f08e6f00f9949c27e2f76bb7f732e6d16871e
James Almer <jamrial@gmail.com>
2014-09-24 19:53:07 -0300

    x86/idctdsp: port {put,add}_pixels_clamped to yasm
    Also add sse2 versions for both.
    put_pixels_clamped port and sse2 version originally written by Timothy Gu.
    
    Reviewed-by: Michael Niedermayer <michaelni@gmx.at>
    Signed-off-by: James Almer <jamrial@gmail.com>

--- libavcodec/x86/idctdsp.h
+++ libavcodec/x86/idctdsp.h
@@ -23,8 +23,12 @@
 
 void ff_add_pixels_clamped_mmx(const int16_t *block, uint8_t *pixels,
                                ptrdiff_t line_size);
+void ff_add_pixels_clamped_sse2(const int16_t *block, uint8_t *pixels,
+                                ptrdiff_t line_size);
 void ff_put_pixels_clamped_mmx(const int16_t *block, uint8_t *pixels,
                                ptrdiff_t line_size);
+void ff_put_pixels_clamped_sse2(const int16_t *block, uint8_t *pixels,
+                                ptrdiff_t line_size);
 void ff_put_signed_pixels_clamped_mmx(const int16_t *block, uint8_t *pixels,
                                       ptrdiff_t line_size);
 void ff_put_signed_pixels_clamped_sse2(const int16_t *block, uint8_t *pixels,
