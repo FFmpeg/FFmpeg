a2efbecc4ed12d287cf29856418c4da4a7648d95
Martin Storsjö <martin@martin.st>
2014-09-24 11:17:04 +0300

    libavformat: Move avc mp4 startcode parsing to a shared file
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- libavformat/rtpenc_h264.c
+++ libavformat/rtpenc_h264.c
@@ -29,21 +29,6 @@
 #include "avc.h"
 #include "rtpenc.h"
 
-static const uint8_t *avc_mp4_find_startcode(const uint8_t *start, const uint8_t *end, int nal_length_size)
-{
-    unsigned int res = 0;
-
-    if (end - start < nal_length_size)
-        return NULL;
-    while (nal_length_size--)
-        res = (res << 8) | *start++;
-
-    if (res > end - start)
-        return NULL;
-
-    return start + res;
-}
-
 static void nal_send(AVFormatContext *s1, const uint8_t *buf, int size, int last)
 {
     RTPMuxContext *s = s1->priv_data;
@@ -88,14 +73,14 @@ void ff_rtp_send_h264(AVFormatContext *s1, const uint8_t *buf1, int size)
 
     s->timestamp = s->cur_timestamp;
     if (s->nal_length_size)
-        r = avc_mp4_find_startcode(buf1, end, s->nal_length_size) ? buf1 : end;
+        r = ff_avc_mp4_find_startcode(buf1, end, s->nal_length_size) ? buf1 : end;
     else
         r = ff_avc_find_startcode(buf1, end);
     while (r < end) {
         const uint8_t *r1;
 
         if (s->nal_length_size) {
-            r1 = avc_mp4_find_startcode(r, end, s->nal_length_size);
+            r1 = ff_avc_mp4_find_startcode(r, end, s->nal_length_size);
             if (!r1)
                 r1 = end;
             r += s->nal_length_size;
