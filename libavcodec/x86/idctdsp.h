c0f9df30dd025acf7c9855cb2c742e185a2ec8db
Carl Eugen Hoyos <cehoyos@ag.or.at>
2014-09-25 22:18:25 +0200

    lavc/x86/idctdsp.h: Fix make checkheaders.

--- libavcodec/x86/idctdsp.h
+++ libavcodec/x86/idctdsp.h
@@ -20,6 +20,7 @@
 #define AVCODEC_X86_IDCTDSP_H
 
 #include <stdint.h>
+#include <stddef.h>
 
 void ff_add_pixels_clamped_mmx(const int16_t *block, uint8_t *pixels,
                                ptrdiff_t line_size);
