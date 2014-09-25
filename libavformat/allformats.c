88c937fdc8a8c887ca482aa26a9d6862e72d0dd3
Carl Eugen Hoyos <cehoyos@ag.or.at>
2014-09-25 23:02:06 +0200

    Autodetect jpg images.
    Based on 2d3842f5 by Michael Niedermayer.
    
    Fixes ticket #2541.

--- libavformat/allformats.c
+++ libavformat/allformats.c
@@ -325,6 +325,7 @@ void av_register_all(void)
     REGISTER_DEMUXER (IMAGE_DPX_PIPE,        image_dpx_pipe);
     REGISTER_DEMUXER (IMAGE_EXR_PIPE,        image_exr_pipe);
     REGISTER_DEMUXER (IMAGE_J2K_PIPE,        image_j2k_pipe);
+    REGISTER_DEMUXER (IMAGE_JPEG_PIPE,       image_jpeg_pipe);
     REGISTER_DEMUXER (IMAGE_JPEGLS_PIPE,     image_jpegls_pipe);
     REGISTER_DEMUXER (IMAGE_PICTOR_PIPE,     image_pictor_pipe);
     REGISTER_DEMUXER (IMAGE_PNG_PIPE,        image_png_pipe);
