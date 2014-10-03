4b4ed88e892007626fb969611ef926d30ccc8b46
Michael Niedermayer <michaelni@gmx.at>
2014-10-03 17:54:21 +0200

    avcodec/pngdec: Calculate MPNG bytewidth more defensively
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>
    (cherry picked from commit e830902934a29df05c7af65aef2a480b15f572c4)
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/pngdec.c
+++ libavcodec/pngdec.c
@@ -860,10 +860,11 @@ exit_loop:
             int i, j;
             uint8_t *pd      = p->data[0];
             uint8_t *pd_last = s->last_picture.f->data[0];
+            int ls = FFMIN(av_image_get_linesize(p->format, s->width, 0), s->width * s->bpp);
 
             ff_thread_await_progress(&s->last_picture, INT_MAX, 0);
             for (j = 0; j < s->height; j++) {
-                for (i = 0; i < s->width * s->bpp; i++)
+                for (i = 0; i < ls; i++)
                     pd[i] += pd_last[i];
                 pd      += s->image_linesize;
                 pd_last += s->image_linesize;
