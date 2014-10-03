97cb9b6cbb0ea1e8127a4a52790a8e35c20988ed
Michael Niedermayer <michaelni@gmx.at>
2014-10-03 13:54:06 +0200

    Revert "avformat/img2dec: reduce bmppipe probe score"
    This seems not needed anymore
    
    This reverts commit 321c3cd1a97b9307760c3ebb175296590c382eca.
    
    Found-by: carl

--- libavformat/img2dec.c
+++ libavformat/img2dec.c
@@ -585,7 +585,7 @@ static int bmp_probe(AVProbeData *p)
         return 0;
 
     if (!AV_RN32(b + 6)) {
-        return AVPROBE_SCORE_EXTENSION - 1; // lower than extension as bmp pipe has bugs
+        return AVPROBE_SCORE_EXTENSION + 1;
     } else {
         return AVPROBE_SCORE_EXTENSION / 4;
     }
