68211c70afcfd4d675029cc85fba7dee8de53bdf
Luca Barbato <lu_zero@gentoo.org>
2014-10-04 12:40:35 +0200

    vc1: Do not assume seek happens after decoding
    If a seek is requested before the decoding start there is no
    current picture.
    
    CC: libav-stable@libav.org

--- libavcodec/vc1dec.c
+++ libavcodec/vc1dec.c
@@ -5503,7 +5503,7 @@ static void vc1_sprite_flush(AVCodecContext *avctx)
        Since we can't enforce it, clear to black the missing sprite. This is
        wrong but it looks better than doing nothing. */
 
-    if (f->data[0])
+    if (f && f->data[0])
         for (plane = 0; plane < (s->flags&CODEC_FLAG_GRAY ? 1 : 3); plane++)
             for (i = 0; i < v->sprite_height>>!!plane; i++)
                 memset(f->data[plane] + i * f->linesize[plane],
