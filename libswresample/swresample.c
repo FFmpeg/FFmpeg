11313263ab37c433286189de4ac51d29d2e5e18e
Michael Niedermayer <michaelni@gmx.at>
2014-10-06 01:08:20 +0200

    swresample/swresample: fix sample drop loop end condition
    Fixes Ticket3985
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>
    (cherry picked from commit f9fefa499f0af48f47ea73c8ce0b25df0976c315)
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libswresample/swresample.c
+++ libswresample/swresample.c
@@ -753,6 +753,8 @@ int swr_convert(struct SwrContext *s, uint8_t *out_arg[SWR_CH_MAX], int out_coun
         in_count = 0;
         if(ret>0) {
             s->drop_output -= ret;
+            if (!s->drop_output && !out_arg)
+                return 0;
             continue;
         }
 
