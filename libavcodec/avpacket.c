c9eac8062e6c68f854837f644aa6045f2a97c151
Michael Niedermayer <michaelni@gmx.at>
2014-10-07 03:40:57 +0200

    avcodec/avpacket: use av_freep(), do not leave stale pointers in memory
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/avpacket.c
+++ libavcodec/avpacket.c
@@ -272,7 +272,7 @@ void av_packet_free_side_data(AVPacket *pkt)
 {
     int i;
     for (i = 0; i < pkt->side_data_elems; i++)
-        av_free(pkt->side_data[i].data);
+        av_freep(&pkt->side_data[i].data);
     av_freep(&pkt->side_data);
     pkt->side_data_elems = 0;
 }
