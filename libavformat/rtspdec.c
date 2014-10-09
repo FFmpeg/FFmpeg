3df8d52fcdc9036b4074fdc612d487ece8bb5b7f
Luca Barbato <lu_zero@gentoo.org>
2014-10-09 22:07:35 +0200

    rtsp: Add rtsps to the probe

--- libavformat/rtspdec.c
+++ libavformat/rtspdec.c
@@ -680,7 +680,11 @@ static int rtsp_listen(AVFormatContext *s)
 
 static int rtsp_probe(AVProbeData *p)
 {
-    if (av_strstart(p->filename, "rtsp:", NULL))
+    if (
+#if CONFIG_TLS_PROTOCOL
+        av_strstart(p->filename, "rtsps:", NULL) ||
+#endif
+        av_strstart(p->filename, "rtsp:", NULL))
         return AVPROBE_SCORE_MAX;
     return 0;
 }
