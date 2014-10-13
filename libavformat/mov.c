57884172e0c797a8268fba6e9672ce22c398d9ac
Michael Niedermayer <michaelni@gmx.at>
2014-10-13 13:36:06 +0200

    avformat/mov: fix mix of declaration and statement
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/mov.c
+++ libavformat/mov.c
@@ -780,9 +780,9 @@ static int mov_read_moof(MOVContext *c, AVIOContext *pb, MOVAtom atom)
     if (!c->has_looked_for_mfra && c->use_mfra_for > 0) {
         c->has_looked_for_mfra = 1;
         if (pb->seekable) {
+            int ret;
             av_log(c->fc, AV_LOG_VERBOSE, "stream has moof boxes, will look "
                     "for a mfra\n");
-            int ret;
             if ((ret = mov_read_mfra(c, pb)) < 0) {
                 av_log(c->fc, AV_LOG_VERBOSE, "found a moof box but failed to "
                         "read the mfra (may be a live ismv)\n");
