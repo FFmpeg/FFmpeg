23b3fe00bb0d44a073f7a8e0a51f5c42d05facb4
Michael Niedermayer <michaelni@gmx.at>
2014-10-13 16:02:42 +0200

    postproc: fix qp count
    Found-by: ubitux
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>
    (cherry picked from commit 0b7e5d0d75e7d8762dd04d35f8c0821736164372)
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libpostproc/postprocess.c
+++ libpostproc/postprocess.c
@@ -975,7 +975,7 @@ void  pp_postprocess(const uint8_t * src[3], const int srcStride[3],
 
     if(pict_type & PP_PICT_TYPE_QP2){
         int i;
-        const int count= mbHeight * absQPStride;
+        const int count= FFMAX(mbHeight * absQPStride, mbWidth);
         for(i=0; i<(count>>2); i++){
             ((uint32_t*)c->stdQPTable)[i] = (((const uint32_t*)QP_store)[i]>>1) & 0x7F7F7F7F;
         }
@@ -1000,7 +1000,7 @@ void  pp_postprocess(const uint8_t * src[3], const int srcStride[3],
     if((pict_type&7)!=3){
         if (QPStride >= 0){
             int i;
-            const int count= mbHeight * FFMAX(QPStride, mbWidth);
+            const int count= FFMAX(mbHeight * QPStride, mbWidth);
             for(i=0; i<(count>>2); i++){
                 ((uint32_t*)c->nonBQPTable)[i] = ((const uint32_t*)QP_store)[i] & 0x3F3F3F3F;
             }
