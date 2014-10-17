1aed82846cbcdd19cab9d2eea0f7aaaa45fb674a
Michael Niedermayer <michaelni@gmx.at>
2014-10-17 12:37:40 +0200

    avformat/m4vdec: mark as possibly containing discontinuities
    This will be needed for the resolution change mpeg4 test
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/m4vdec.c
+++ libavformat/m4vdec.c
@@ -56,5 +56,5 @@ static int mpeg4video_probe(AVProbeData *probe_packet)
     return 0;
 }
 
-FF_DEF_RAWVIDEO_DEMUXER(m4v, "raw MPEG-4 video", mpeg4video_probe, "m4v",
-                        AV_CODEC_ID_MPEG4)
+FF_DEF_RAWVIDEO_DEMUXER2(m4v, "raw MPEG-4 video", mpeg4video_probe, "m4v",
+                         AV_CODEC_ID_MPEG4, AVFMT_GENERIC_INDEX | AVFMT_TS_DISCONT)
