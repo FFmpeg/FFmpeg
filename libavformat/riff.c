a71a5c911eb0bd46bfafc439550e0184a883a3a7
Michael Niedermayer <michaelni@gmx.at>
2014-10-19 14:34:50 +0200

    avformat/riff: Add UGY2
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/riff.c
+++ libavformat/riff.c
@@ -341,6 +341,7 @@ const AVCodecTag ff_codec_bmp_tags[] = {
     /* Ut Video version 13.0.1 BT.709 codecs */
     { AV_CODEC_ID_UTVIDEO,      MKTAG('U', 'L', 'H', '0') },
     { AV_CODEC_ID_UTVIDEO,      MKTAG('U', 'L', 'H', '2') },
+    { AV_CODEC_ID_UTVIDEO,      MKTAG('U', 'Q', 'Y', '2') },
     { AV_CODEC_ID_VBLE,         MKTAG('V', 'B', 'L', 'E') },
     { AV_CODEC_ID_ESCAPE130,    MKTAG('E', '1', '3', '0') },
     { AV_CODEC_ID_DXTORY,       MKTAG('x', 't', 'o', 'r') },
