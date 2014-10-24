b46b233baffc2076a1a17a264ba9553ae0d4878f
Vittorio Giovara <vittorio.giovara@gmail.com>
2014-10-24 13:15:39 +0100

    filmstripdec: avoid integer overflow
    CC: libav-stable@libav.org
    Bug-Id: CID 732246

--- libavformat/filmstripdec.c
+++ libavformat/filmstripdec.c
@@ -84,7 +84,7 @@ static int read_packet(AVFormatContext *s,
         return AVERROR(EIO);
     pkt->dts = avio_tell(s->pb) / (st->codec->width * (st->codec->height + film->leading) * 4);
     pkt->size = av_get_packet(s->pb, pkt, st->codec->width * st->codec->height * 4);
-    avio_skip(s->pb, st->codec->width * film->leading * 4);
+    avio_skip(s->pb, st->codec->width * (int64_t) film->leading * 4);
     if (pkt->size < 0)
         return pkt->size;
     pkt->flags |= AV_PKT_FLAG_KEY;
