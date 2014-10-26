c0002ddb019d7f2f22da301b2855f86d697d37bd
Michael Niedermayer <michaelni@gmx.at>
2014-10-26 15:52:12 +0100

    ffmpeg_opt: Check the return code from strftime()
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- ffmpeg_opt.c
+++ ffmpeg_opt.c
@@ -509,7 +509,8 @@ static int opt_recording_timestamp(void *optctx, const char *opt, const char *ar
     char buf[128];
     int64_t recording_timestamp = parse_time_or_die(opt, arg, 0) / 1E6;
     struct tm time = *gmtime((time_t*)&recording_timestamp);
-    strftime(buf, sizeof(buf), "creation_time=%FT%T%z", &time);
+    if (!strftime(buf, sizeof(buf), "creation_time=%FT%T%z", &time))
+        return -1;
     parse_option(o, "metadata", buf, options);
 
     av_log(NULL, AV_LOG_WARNING, "%s is deprecated, set the 'creation_time' metadata "
