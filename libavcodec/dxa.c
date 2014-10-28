f3cdeeb070d4793d98b16a7ad66049e537ab7ba7
Michael Niedermayer <michaelni@gmx.at>
2014-10-28 15:26:42 +0100

    avcodec/dxa: check dimensions
    Fixes out of array access
    Fixes: asan_heap-oob_11222fb_21_020.dxa
    Found-by: Mateusz "j00ru" Jurczyk and Gynvael Coldwind
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>
    (cherry picked from commit e70312dfc22c4e54d5716f28f28db8f99c74cc90)
    
    Conflicts:
    
    	libavcodec/dxa.c
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/dxa.c
+++ libavcodec/dxa.c
@@ -295,6 +295,11 @@ static av_cold int decode_init(AVCodecContext *avctx)
     c->avctx = avctx;
     avctx->pix_fmt = PIX_FMT_PAL8;
 
+    if (avctx->width%4 || avctx->height%4) {
+        av_log(avctx, AV_LOG_ERROR, "dimensions are not a multiple of 4");
+        return AVERROR_INVALIDDATA;
+    }
+
     avcodec_get_frame_defaults(&c->pic);
     avcodec_get_frame_defaults(&c->prev);
 
