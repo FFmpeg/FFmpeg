d1b5ad39677669b6a829ce34b5e4d2aa79527e89
Thomas Mundt <loudmax-at-yahoo.de@ffmpeg.org>
2014-10-29 20:26:25 +0000

    mxfenc: fix indentation after last commit
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/mxfenc.c
+++ libavformat/mxfenc.c
@@ -998,17 +998,17 @@ static void mxf_write_mpegvideo_desc(AVFormatContext *s, AVStream *st)
     int profile_and_level = (st->codec->profile<<4) | st->codec->level;
 
     if (st->codec->codec_id != AV_CODEC_ID_H264) {
-    mxf_write_cdci_common(s, st, mxf_mpegvideo_descriptor_key, 8+5);
+        mxf_write_cdci_common(s, st, mxf_mpegvideo_descriptor_key, 8+5);
 
-    // bit rate
-    mxf_write_local_tag(pb, 4, 0x8000);
-    avio_wb32(pb, sc->video_bit_rate);
+        // bit rate
+        mxf_write_local_tag(pb, 4, 0x8000);
+        avio_wb32(pb, sc->video_bit_rate);
 
-    // profile and level
-    mxf_write_local_tag(pb, 1, 0x8007);
-    if (!st->codec->profile)
-        profile_and_level |= 0x80; // escape bit
-    avio_w8(pb, profile_and_level);
+        // profile and level
+        mxf_write_local_tag(pb, 1, 0x8007);
+        if (!st->codec->profile)
+            profile_and_level |= 0x80; // escape bit
+        avio_w8(pb, profile_and_level);
     } else {
         mxf_write_cdci_common(s, st, mxf_mpegvideo_descriptor_key, 0);
     }
