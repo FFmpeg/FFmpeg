19d0949d31b0d2db4d01a667d515ce5099cb0530
Arwa Arif <arwaarif1994@gmail.com>
2014-10-30 22:06:20 +0530

    lavfi: add xbr filter xBR
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavfilter/allfilters.c
+++ libavfilter/allfilters.c
@@ -213,6 +213,7 @@ void avfilter_register_all(void)
     REGISTER_FILTER(VIDSTABTRANSFORM, vidstabtransform, vf);
     REGISTER_FILTER(VIGNETTE,       vignette,       vf);
     REGISTER_FILTER(W3FDIF,         w3fdif,         vf);
+    REGISTER_FILTER(XBR,            xbr,            vf);
     REGISTER_FILTER(YADIF,          yadif,          vf);
     REGISTER_FILTER(ZMQ,            zmq,            vf);
     REGISTER_FILTER(ZOOMPAN,        zoompan,        vf);
