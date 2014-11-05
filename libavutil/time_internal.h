9485cce6d55baf547e92ef1f54cad117f2a38287
Michael Niedermayer <michaelni@gmx.at>
2014-11-05 18:44:15 +0100

    time_internal: Do not attempt to override *time_r() macros
    In case these already are defined as macros, we shouldn't try to
    redefine them.
    
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- libavutil/time_internal.h
+++ libavutil/time_internal.h
@@ -22,7 +22,7 @@
 #include <time.h>
 #include "config.h"
 
-#if !HAVE_GMTIME_R
+#if !HAVE_GMTIME_R && !defined(gmtime_r)
 static inline struct tm *gmtime_r(const time_t* clock, struct tm *result)
 {
     struct tm *ptr = gmtime(clock);
@@ -33,7 +33,7 @@ static inline struct tm *gmtime_r(const time_t* clock, struct tm *result)
 }
 #endif
 
-#if !HAVE_LOCALTIME_R
+#if !HAVE_LOCALTIME_R && !defined(localtime_r)
 static inline struct tm *localtime_r(const time_t* clock, struct tm *result)
 {
     struct tm *ptr = localtime(clock);
