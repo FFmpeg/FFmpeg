2b14593148e0aef6f3ca76dcc3f9f2133a066b3d
Lukasz Marek <lukasz.m.luki2@gmail.com>
2014-11-07 09:05:41 +0100

    ffmpeg_opt: free incorrect priv_data for feed stream
    new_output_stream creates a codec context with arbitraty picked codec.
    Later data is updated, but priv_data are left alone.
    There is a bit chance there is a mismatch between codecs.
    
    Signed-off-by: Lukasz Marek <lukasz.m.luki2@gmail.com>
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- ffmpeg_opt.c
+++ ffmpeg_opt.c
@@ -1649,6 +1649,10 @@ static int read_ffserver_streams(OptionsContext *o, AVFormatContext *s, const ch
         else if (st->codec->codec_type == AVMEDIA_TYPE_VIDEO && !ost->stream_copy)
             choose_pixel_fmt(st, st->codec, codec, st->codec->pix_fmt);
         avcodec_copy_context(ost->enc_ctx, st->codec);
+        if (ost->enc_ctx->priv_data) {
+            av_opt_free(ost->enc_ctx->priv_data);
+            av_freep(&ost->enc_ctx->priv_data);
+        }
     }
 
     avformat_close_input(&ic);
