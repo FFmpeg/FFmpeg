bdcb5794f0c2d74371152303bffe4172671af264
Vittorio Giovara <vittorio.giovara@gmail.com>
2014-11-11 13:26:57 +0100

    nellymoserenc: fix array index
    CC: libav-stable@libav.org
    Bug-Id: CID 732258

--- libavcodec/nellymoserenc.c
+++ libavcodec/nellymoserenc.c
@@ -231,7 +231,7 @@ static void get_exponent_dynamic(NellyMoserEncodeContext *s, float *cand, int *i
     float  (*opt )[OPT_SIZE] = s->opt ;
     uint8_t(*path)[OPT_SIZE] = s->path;
 
-    for (i = 0; i < NELLY_BANDS * OPT_SIZE; i++) {
+    for (i = 0; i < OPT_SIZE; i++) {
         opt[0][i] = INFINITY;
     }
 
