4dda5e9b0829b119c17d950906c61d3ebffc494f
Vittorio Giovara <vittorio.giovara@gmail.com>
2014-11-11 17:40:01 +0100

    sunrastenc: mention missing break
    Bug-Id: CID 732231

--- libavcodec/sunrastenc.c
+++ libavcodec/sunrastenc.c
@@ -169,6 +169,7 @@ static av_cold int sunrast_encode_init(AVCodecContext *avctx)
     case AV_PIX_FMT_PAL8 :
         s->maptype   = RMT_EQUAL_RGB;
         s->maplength = 3 * 256;
+        /* fall-through */
     case AV_PIX_FMT_GRAY8:
         s->depth = 8;
         break;
