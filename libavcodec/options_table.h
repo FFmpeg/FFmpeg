c544ffd2aeab766293eeab326c604a11bbd4ecee
Lukasz Marek <lukasz.m.luki2@gmail.com>
2014-11-11 08:17:26 +0100

    lavc/options_table: add pixel_format and video_size options
    Adding these options simplify ffm format implementation based on AVOption API.
    
    Signed-off-by: Lukasz Marek <lukasz.m.luki2@gmail.com>

--- libavcodec/options_table.h
+++ libavcodec/options_table.h
@@ -488,6 +488,8 @@ static const AVOption avcodec_options[] = {
 {"bt", NULL, 0, AV_OPT_TYPE_CONST, {.i64 = AV_FIELD_BT }, 0, 0, V|D|E, "field_order" },
 {"dump_separator", "set information dump field separator", OFFSET(dump_separator), AV_OPT_TYPE_STRING, {.str = NULL}, CHAR_MIN, CHAR_MAX, A|V|S|D|E},
 {"codec_whitelist", "List of decoders that are allowed to be used", OFFSET(codec_whitelist), AV_OPT_TYPE_STRING, { .str = NULL },  CHAR_MIN, CHAR_MAX, A|V|S|D },
+{"pixel_format", "set pixel format", OFFSET(pix_fmt), AV_OPT_TYPE_PIXEL_FMT, {.i64=AV_PIX_FMT_NONE}, -1, INT_MAX, 0 },
+{"video_size", "set video size", OFFSET(width), AV_OPT_TYPE_IMAGE_SIZE, {.str=NULL}, 0, INT_MAX, 0 },
 {NULL},
 };
 
