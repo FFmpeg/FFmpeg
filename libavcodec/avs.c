9e0a38d32b36fac7fd73bdb93e820ae0b9e03616
Vittorio Giovara <vittorio.giovara@gmail.com>
2014-11-12 11:13:05 +0100

    avs: check ff_set_dimensions return value
    CC: libav-stable@libav.org
    Bug-Id: CID 1135738
    (cherry picked from commit c7384664ba0cbb12d882effafbc6d321ae706cff)
    Signed-off-by: Luca Barbato <lu_zero@gentoo.org>

--- libavcodec/avs.c
+++ libavcodec/avs.c
@@ -165,9 +165,8 @@ static av_cold int avs_decode_init(AVCodecContext * avctx)
         return AVERROR(ENOMEM);
 
     avctx->pix_fmt = AV_PIX_FMT_PAL8;
-    ff_set_dimensions(avctx, 318, 198);
 
-    return 0;
+    return ff_set_dimensions(avctx, 318, 198);
 }
 
 static av_cold int avs_decode_end(AVCodecContext *avctx)
