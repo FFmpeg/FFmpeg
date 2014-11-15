a2f05d33373eb56a87f9581b33d50e44bb5f6a0b
Arwa Arif <arwaarif1994@gmail.com>
2014-11-15 13:29:31 +0530

    lavfi : change xBR filter to LGPL
    Hyllian's message : "Hi, Put lgpl on this and use it as you wish. It's free!"
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavfilter/vf_xbr.c
+++ libavfilter/vf_xbr.c
@@ -5,19 +5,23 @@
  *
  * Copyright (c) 2014 Arwa Arif <arwaarif1994@gmail.com>
  *
- * FFmpeg is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * Changed from GPL to LGPL, in accordance with Hyllian's message:
+ *     On Tue, Nov 11, 2014 at 11:52 PM, Sérgio <sergiogdb@gmail.com> wrote:
+ *         > Hi, Put lgpl on this and use it as you wish. It's free!
+ *
+ * FFmpeg is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
  *
  * FFmpeg is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along
- * with FFmpeg; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FFmpeg; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
 /**
