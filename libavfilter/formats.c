863ee8a855b8ce27ffef41479eb66da58763faed
Vittorio Giovara <vittorio.giovara@gmail.com>
2014-11-17 00:22:20 +0100

    lavfi: clean memory on error in ADD_FORMAT()
    CC: libav-stable@libav.org
    Bug-Id: CID 1250334

--- libavfilter/formats.c
+++ libavfilter/formats.c
@@ -188,8 +188,10 @@ do {                                                        \
                                                             \
     fmts = av_realloc((*f)->list,                           \
                       sizeof(*(*f)->list) * ((*f)->nb + 1));\
-    if (!fmts)                                              \
+    if (!fmts) {                                            \
+        av_freep(&f);                                       \
         return AVERROR(ENOMEM);                             \
+    }                                                       \
                                                             \
     (*f)->list = fmts;                                      \
     (*f)->list[(*f)->nb++] = fmt;                           \
