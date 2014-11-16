530eb6acf8ee867bf00728bf7efaf505da107e17
Michael Niedermayer <michaelni@gmx.at>
2014-11-16 04:02:56 +0100

    avformat/hlsenc: Free context after hls_append_segment
    Fixes reading uninitialized memory
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/hlsenc.c
+++ libavformat/hlsenc.c
@@ -389,9 +389,10 @@ static int hls_write_trailer(struct AVFormatContext *s)
     av_write_trailer(oc);
     hls->size = avio_tell(hls->avf->pb) - hls->start_pos;
     avio_closep(&oc->pb);
-    avformat_free_context(oc);
     av_free(hls->basename);
     hls_append_segment(hls, hls->duration, hls->start_pos, hls->size);
+    avformat_free_context(oc);
+    hls->avf = NULL;
     hls_window(s, 1);
 
     hls_free_segments(hls);
