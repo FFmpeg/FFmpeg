ade140eb733197b3c4cdc63485652169ed757d89
Michael Niedermayer <michaelni@gmx.at>
2014-11-18 11:33:00 +0100

    libavcodec/libx264: Use av_freep() avoid leaving stale pointers in memory
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/libx264.c
+++ libavcodec/libx264.c
@@ -305,7 +305,7 @@ static av_cold int X264_close(AVCodecContext *avctx)
     X264Context *x4 = avctx->priv_data;
 
     av_freep(&avctx->extradata);
-    av_free(x4->sei);
+    av_freep(&x4->sei);
 
     if (x4->enc)
         x264_encoder_close(x4->enc);
