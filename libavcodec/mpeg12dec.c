0f8908aa1b66fbc8d62939ce8ee1ee04b856528f
Michael Niedermayer <michaelni@gmx.at>
2014-11-20 15:09:42 +0100

    avcodec/mpeg12dec: do not fail on invalid frame_rate_index
    Fixes Ticket4073
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/mpeg12dec.c
+++ libavcodec/mpeg12dec.c
@@ -2123,7 +2123,7 @@ static int mpeg1_decode_sequence(AVCodecContext *avctx,
     s->frame_rate_index = get_bits(&s->gb, 4);
     if (s->frame_rate_index == 0 || s->frame_rate_index > 13) {
         av_log(avctx, AV_LOG_WARNING, "frame_rate_index %d is invalid\n", s->frame_rate_index);
-        return -1;
+        s->frame_rate_index = 1;
     }
     s->bit_rate = get_bits(&s->gb, 18) * 400;
     if (get_bits1(&s->gb) == 0) { /* marker */
