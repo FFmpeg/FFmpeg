7d75a399a4d216ea3d924d6e8c18868731d93132
Lukasz Marek <lukasz.m.luki2@gmail.com>
2014-11-21 00:17:46 +0100

    lavc/options: fix rc_eq leak
    rc_eq is an option, so it is copied by av_opt_copy(dest, src); above.

--- libavcodec/options.c
+++ libavcodec/options.c
@@ -209,16 +209,6 @@ int avcodec_copy_context(AVCodecContext *dest, const AVCodecContext *src)
     dest->inter_matrix    = NULL;
     dest->rc_override     = NULL;
     dest->subtitle_header = NULL;
-#if FF_API_MPV_OPT
-    FF_DISABLE_DEPRECATION_WARNINGS
-    dest->rc_eq           = NULL;
-    if (src->rc_eq) {
-        dest->rc_eq = av_strdup(src->rc_eq);
-        if (!dest->rc_eq)
-            return AVERROR(ENOMEM);
-    }
-    FF_ENABLE_DEPRECATION_WARNINGS
-#endif
 
 #define alloc_and_copy_or_fail(obj, size, pad) \
     if (src->obj && size > 0) { \
