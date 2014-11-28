63547db8822a227f97191526912f3f6c460f4cc4
Matthew Oliver <protogonoi@gmail.com>
2014-11-28 15:09:18 +1100

    project_generator: Add licensing info to code headers.

--- SMP/project_generate/source/configGenerator.cpp
+++ SMP/project_generate/source/configGenerator.cpp
@@ -1,4 +1,23 @@
-
+/*
+ * copyright (c) 2014 Matthew Oliver
+ *
+ * This file is part of ShiftMediaProject.
+ *
+ * ShiftMediaProject is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * ShiftMediaProject is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with ShiftMediaProject; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+ 
 #include "configGenerator.h"
 
 #include <algorithm>
