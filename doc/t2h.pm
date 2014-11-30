928322c15f985eb965f0379fbd971d06143763aa
Andreas Cadhalpun <Andreas.Cadhalpun@googlemail.com>
2014-11-30 23:47:36 +0100

    doc: correct license template for t2h.pm
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- doc/t2h.pm
+++ doc/t2h.pm
@@ -14,9 +14,9 @@
 # FFmpeg is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# Lesser General Public License for more details.
+# General Public License for more details.
 #
-# You should have received a copy of the GNU Lesser General Public
+# You should have received a copy of the GNU General Public
 # License along with FFmpeg; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
