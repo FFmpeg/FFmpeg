7ad742b2247a0d6f742a656892b4963fa77744dd
Michael Niedermayer <michaelni@gmx.at>
2014-12-06 16:57:23 +0100

    avfilter/vf_signalstats: fix avframe leak on error
    Fixes CID1257014
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavfilter/vf_signalstats.c
+++ libavfilter/vf_signalstats.c
@@ -132,8 +132,10 @@ static AVFrame *alloc_frame(enum AVPixelFormat pixfmt, int w, int h)
     frame->width  = w;
     frame->height = h;
 
-    if (av_frame_get_buffer(frame, 32) < 0)
+    if (av_frame_get_buffer(frame, 32) < 0) {
+        av_frame_free(&frame);
         return NULL;
+    }
 
     return frame;
 }
