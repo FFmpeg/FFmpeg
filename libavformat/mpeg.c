74080ded9630f9492d9a382815502c141c146900
Michael Niedermayer <michaelni@gmx.at>
2014-12-06 22:38:38 +0100

    avformat/mpeg: add commented out av_log() to probe
    This is commonly needed for debugging mis-detections
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/mpeg.c
+++ libavformat/mpeg.c
@@ -102,6 +102,9 @@ static int mpegps_probe(AVProbeData *p)
     if (vid + audio > invalid + 1) /* invalid VDR files nd short PES streams */
         score = AVPROBE_SCORE_EXTENSION / 2;
 
+//     av_log(NULL, AV_LOG_ERROR, "vid:%d aud:%d sys:%d pspack:%d invalid:%d size:%d \n",
+//            vid, audio, sys, pspack, invalid, p->buf_size);
+
     if (sys > invalid && sys * 9 <= pspack * 10)
         return (audio > 12 || vid > 3 || pspack > 2) ? AVPROBE_SCORE_EXTENSION + 2
                                                      : AVPROBE_SCORE_EXTENSION / 2 + 1; // 1 more than mp3
