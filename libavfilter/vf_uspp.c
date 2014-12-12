13c3a97bf11834fc4b4d05f6b26371c4c7f47c0d
Michael Niedermayer <michaelni@gmx.at>
2014-12-12 01:50:03 +0100

    avfilter/vf_uspp: remove YUV 411/422/440
    snow doesnt support 422/411/440 currently, so these do not work yet
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavfilter/vf_uspp.c
+++ libavfilter/vf_uspp.c
@@ -288,11 +288,11 @@ static void filter(USPPContext *p, uint8_t *dst[3], uint8_t *src[3],
 static int query_formats(AVFilterContext *ctx)
 {
     static const enum PixelFormat pix_fmts[] = {
-        AV_PIX_FMT_YUV444P,  AV_PIX_FMT_YUV422P,
-        AV_PIX_FMT_YUV420P,  AV_PIX_FMT_YUV411P,
-        AV_PIX_FMT_YUV410P,  AV_PIX_FMT_YUV440P,
-        AV_PIX_FMT_YUVJ444P, AV_PIX_FMT_YUVJ422P,
-        AV_PIX_FMT_YUVJ420P, AV_PIX_FMT_YUVJ440P,
+        AV_PIX_FMT_YUV444P,
+        AV_PIX_FMT_YUV420P,
+        AV_PIX_FMT_YUV410P,
+        AV_PIX_FMT_YUVJ444P,
+        AV_PIX_FMT_YUVJ420P,
         AV_PIX_FMT_NONE
     };
     ff_set_common_formats(ctx, ff_make_format_list(pix_fmts));
