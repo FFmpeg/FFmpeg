9bafd6a8f6a013c5f45141f596574a9a3fbfb561
Anton Khirnov <anton@khirnov.net>
2014-12-14 21:01:59 +0100

    mmvideo: check frame dimensions
    The frame size must be set by the caller and each dimension must be a
    multiple of 2.
    
    Found-by: Mateusz "j00ru" Jurczyk and Gynvael Coldwind
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>
    See: 8b0e96e1f21b761ca15dbb470cd619a1ebf86c3e
    These should be redundant, but are backported for saftey anyway
    (cherry picked from commit b0273232d8fffdc8a977ccdad460b8071a0e353c)
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/mmvideo.c
+++ libavcodec/mmvideo.c
@@ -61,6 +61,13 @@ static av_cold int mm_decode_init(AVCodecContext *avctx)
 
     avctx->pix_fmt = AV_PIX_FMT_PAL8;
 
+    if (!avctx->width || !avctx->height ||
+        (avctx->width & 1) || (avctx->height & 1)) {
+        av_log(avctx, AV_LOG_ERROR, "Invalid video dimensions: %dx%d\n",
+               avctx->width, avctx->height);
+        return AVERROR(EINVAL);
+    }
+
     s->frame = av_frame_alloc();
     if (!s->frame)
         return AVERROR(ENOMEM);
