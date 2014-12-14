844f7f78aa523fb452c0271cb7dc6742039b9d3c
Michael Niedermayer <michaelni@gmx.at>
2014-12-14 17:26:11 +0100

    avformat/aviobuf: Check that avio_seek() target is non negative
    Fixes out of array access
    
    Suggested-by: Andrew Scherkus <scherkus@google.com>
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>
    (cherry picked from commit ed86dbd05d61363dc1c0d33f3267e2177c985fdd)
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/aviobuf.c
+++ libavformat/aviobuf.c
@@ -217,6 +217,9 @@ int64_t avio_seek(AVIOContext *s, int64_t offset, int whence)
             return offset1;
         offset += offset1;
     }
+    if (offset < 0)
+        return AVERROR(EINVAL);
+
     offset1 = offset - pos;
     if (!s->must_flush && (!s->direct || !s->seek) &&
         offset1 >= 0 && offset1 <= (s->buf_end - s->buffer)) {
