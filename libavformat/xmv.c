ae658efef86b932484a88c52dc9b803bb5d99e3d
Michael Niedermayer <michaelni@gmx.at>
2014-12-15 15:51:04 +0100

    avformat/xmv: Check return code of ff_alloc_extradata()
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/xmv.c
+++ libavformat/xmv.c
@@ -294,6 +294,7 @@ static int xmv_process_packet_header(AVFormatContext *s)
 {
     XMVDemuxContext *xmv = s->priv_data;
     AVIOContext     *pb  = s->pb;
+    int ret;
 
     uint8_t  data[8];
     uint16_t audio_track;
@@ -383,7 +384,8 @@ static int xmv_process_packet_header(AVFormatContext *s)
                 if (vst->codec->extradata_size < 4) {
                     av_freep(&vst->codec->extradata);
 
-                    ff_alloc_extradata(vst->codec, 4);
+                    if ((ret = ff_alloc_extradata(vst->codec, 4)) < 0)
+                        return ret;
                 }
 
                 memcpy(vst->codec->extradata, xmv->video.extradata, 4);
