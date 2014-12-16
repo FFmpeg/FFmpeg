7ef11e82213fb7732e746c02664e061f66664318
Michael Niedermayer <michaelni@gmx.at>
2014-12-16 20:45:31 +0100

    avcodec/utvideodec: Fix handling of slice_height=0
    Fixes out of array accesses
    Fixes: asan_heap-oob_25bcd7e_3783_cov_3553517262_utvideo_rgba_median.avi
    Found-by: Mateusz "j00ru" Jurczyk and Gynvael Coldwind
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>
    (cherry picked from commit 3881606240953b9275a247a1c98a567f3c44890f)
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/utvideodec.c
+++ libavcodec/utvideodec.c
@@ -214,6 +214,8 @@ static void restore_median(uint8_t *src, int step, int stride,
         slice_height = ((((slice + 1) * height) / slices) & cmask) -
                        slice_start;
 
+        if (!slice_height)
+            continue;
         bsrc = src + slice_start * stride;
 
         // first line - left neighbour prediction
@@ -269,6 +271,8 @@ static void restore_median_il(uint8_t *src, int step, int stride,
         slice_height   = ((((slice + 1) * height) / slices) & cmask) -
                          slice_start;
         slice_height >>= 1;
+        if (!slice_height)
+            continue;
 
         bsrc = src + slice_start * stride;
 
