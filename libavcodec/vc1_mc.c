b51cc701bc4ac34959944f30ca8a7e01e1338d43
Michael Niedermayer <michaelni@gmx.at>
2014-12-24 22:47:38 +0100

    avcodec/vc1_mc: use the same reference as luma does in ff_vc1_mc_4mv_chroma()
    Fixes Ticket3230
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/vc1_mc.c
+++ libavcodec/vc1_mc.c
@@ -529,7 +529,7 @@ void ff_vc1_mc_4mv_chroma(VC1Context *v, int dir)
     /* calculate chroma MV vector from four luma MVs */
     if (!v->field_mode || (v->field_mode && !v->numref)) {
         valid_count = get_chroma_mv(mvx, mvy, intra, 0, &tx, &ty);
-        chroma_ref_type = v->reffield;
+        chroma_ref_type = v->ref_field_type[dir];
         if (!valid_count) {
             s->current_picture.motion_val[1][s->block_index[0] + v->blocks_off][0] = 0;
             s->current_picture.motion_val[1][s->block_index[0] + v->blocks_off][1] = 0;
