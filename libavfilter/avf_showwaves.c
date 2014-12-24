ecafde6606a51c285ed7ca4d27697392b493919e
Clément Bœsch <u@pkh.me>
2014-12-24 15:11:44 +0100

    avfilter/showwaves: fix height range for cline mode

--- libavfilter/avf_showwaves.c
+++ libavfilter/avf_showwaves.c
@@ -222,7 +222,7 @@ static void draw_sample_cline(uint8_t *buf, int height, int linesize,
                               int16_t sample, int16_t *prev_y, int intensity)
 {
     int k;
-    const int h     = av_rescale(abs(sample), height, UINT16_MAX);
+    const int h     = av_rescale(abs(sample), height, INT16_MAX);
     const int start = (height - h) / 2;
     const int end   = start + h;
     for (k = start; k < end; k++)
