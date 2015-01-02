88d19d240aeead6ae00fc2dcab0f60362578bce8
Giorgio Vazzana <mywing81@gmail.com>
2015-01-02 16:33:17 +0100

    avutil/camellia: fix documentation for av_camellia_crypt()
    Signed-off-by: Giorgio Vazzana <mywing81@gmail.com>
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavutil/camellia.h
+++ libavutil/camellia.h
@@ -58,7 +58,7 @@ int av_camellia_init(struct AVCAMELLIA *ctx, const uint8_t *key, int key_bits);
   * @param ctx an AVCAMELLIA context
   * @param dst destination array, can be equal to src
   * @param src source array, can be equal to dst
-  * @param count number of 8 byte blocks
+  * @param count number of 16 byte blocks
   * @paran iv initialization vector for CBC mode, NULL for ECB mode
   * @param decrypt 0 for encryption, 1 for decryption
  */
