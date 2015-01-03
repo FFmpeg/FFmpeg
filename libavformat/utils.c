932caa50a5dec7f2a69da8a417ccae84d19ae5e5
Martin Storsjö <martin@martin.st>
2015-01-03 21:27:21 +0200

    libavformat: Check for malloc failures in avformat_new_stream
    CC: libav-stable@libav.org
    Signed-off-by: Martin Storsjö <martin@martin.st>
    (cherry picked from commit 9f810a9b374e0ff8e2a19fd8a7347afe2933e229)
    Signed-off-by: Luca Barbato <lu_zero@gentoo.org>

--- libavformat/utils.c
+++ libavformat/utils.c
@@ -2535,6 +2535,11 @@ AVStream *avformat_new_stream(AVFormatContext *s, const AVCodec *c)
     }
 
     st->codec = avcodec_alloc_context3(c);
+    if (!st->codec) {
+        av_free(st->info);
+        av_free(st);
+        return NULL;
+    }
     if (s->iformat) {
         /* no default bitrate if decoding */
         st->codec->bit_rate = 0;
