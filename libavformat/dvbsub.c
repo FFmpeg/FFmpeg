b0826b832465084b7aec4b1bfac6c689557343e0
Michael Niedermayer <michaelni@gmx.at>
2015-01-05 16:22:26 +0100

    avformat/dvbsub: fix indent after previous commit
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/dvbsub.c
+++ libavformat/dvbsub.c
@@ -34,28 +34,28 @@ static int dvbsub_probe(AVProbeData *p)
 
     for(i=0; i<p->buf_size; i++){
         if (p->buf[i] == 0x0f) {
-        const uint8_t *ptr = p->buf + i;
-        uint8_t histogram[6] = {0};
-        int min = 255;
-        for(j=0; ptr + 6 < end; j++) {
-            if (*ptr != 0x0f)
-                break;
-            type    = ptr[1];
-            page_id = AV_RB16(ptr + 2);
-            len     = AV_RB16(ptr + 4);
-            if (type == 0x80) {
-                ;
-            } else if (type >= 0x10 && type <= 0x14) {
-                histogram[type - 0x10] ++;
-            } else
-                break;
-            ptr += 6 + len;
-        }
-        for (k=0; k < 4; k++) {
-            min = FFMIN(min, histogram[k]);
-        }
-        if (min && j > max_score)
-            max_score = j;
+            const uint8_t *ptr = p->buf + i;
+            uint8_t histogram[6] = {0};
+            int min = 255;
+            for(j=0; ptr + 6 < end; j++) {
+                if (*ptr != 0x0f)
+                    break;
+                type    = ptr[1];
+                page_id = AV_RB16(ptr + 2);
+                len     = AV_RB16(ptr + 4);
+                if (type == 0x80) {
+                    ;
+                } else if (type >= 0x10 && type <= 0x14) {
+                    histogram[type - 0x10] ++;
+                } else
+                    break;
+                ptr += 6 + len;
+            }
+            for (k=0; k < 4; k++) {
+                min = FFMIN(min, histogram[k]);
+            }
+            if (min && j > max_score)
+                max_score = j;
         }
     }
 
