89df3fd49e9992441f680326902b4912d79f514f
Vittorio Giovara <vittorio.giovara@gmail.com>
2015-01-15 15:15:21 +0100

    swscale: fix warning about incompatible function pointer type

--- libswscale/input.c
+++ libswscale/input.c
@@ -596,7 +596,7 @@ static void planar_rgb_to_y(uint8_t *dst, const uint8_t *src[4], int width)
     }
 }
 
-static void planar_rgb_to_a(uint8_t *_dst, const uint8_t *src[4], int width, int32_t *unused)
+static void planar_rgb_to_a(uint8_t *_dst, const uint8_t *src[4], int width)
 {
     uint16_t *dst = (uint16_t *)_dst;
     int i;
