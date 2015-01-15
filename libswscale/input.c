2df7277711cbe3ca5415ff8e31228dcffa309211
Vittorio Giovara <vittorio.giovara@gmail.com>
2015-01-15 15:15:22 +0100

    swscale: fix gbrap to gbrap alpha scaling

--- libswscale/input.c
+++ libswscale/input.c
@@ -596,12 +596,11 @@ static void planar_rgb_to_y(uint8_t *dst, const uint8_t *src[4], int width)
     }
 }
 
-static void planar_rgb_to_a(uint8_t *_dst, const uint8_t *src[4], int width)
+static void planar_rgb_to_a(uint8_t *dst, const uint8_t *src[4], int width)
 {
-    uint16_t *dst = (uint16_t *)_dst;
     int i;
     for (i = 0; i < width; i++)
-        dst[i] = src[3][i] << 6;
+        dst[i] = src[3][i];
 }
 
 static void planar_rgb_to_uv(uint8_t *dstU, uint8_t *dstV, const uint8_t *src[4], int width)
