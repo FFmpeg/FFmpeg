46b0fce627d6ac2a79e8327ca21b476a90b486bc
Michael Niedermayer <michaelni@gmx.at>
2015-01-15 16:41:15 +0100

    avutil/pixfmt: Document the duplicated AV_PIX_FMT_GBRAP* formats
    This is similar to the other duplicated cases
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavutil/pixfmt.h
+++ libavutil/pixfmt.h
@@ -239,6 +239,11 @@ enum AVPixelFormat {
     AV_PIX_FMT_YA16BE,       ///< 16bit gray, 16bit alpha (big-endian)
     AV_PIX_FMT_YA16LE,       ///< 16bit gray, 16bit alpha (little-endian)
 
+    /**
+     * duplicated pixel formats for compatibility with libav.
+     * FFmpeg supports these formats since May 3 2013 (commit e6d4e687558d08187e7a415a7725e4b1a416f782)
+     * Libav added them Jan 14 2015 with incompatible values (commit 0e6c7dfa650e8b0497bfa7a06394b7a462ddc33a)
+     */
     AV_PIX_FMT_GBRAP_LIBAV,        ///< planar GBRA 4:4:4:4 32bpp
     AV_PIX_FMT_GBRAP16BE_LIBAV,    ///< planar GBRA 4:4:4:4 64bpp, big-endian
     AV_PIX_FMT_GBRAP16LE_LIBAV,    ///< planar GBRA 4:4:4:4 64bpp, little-endian
