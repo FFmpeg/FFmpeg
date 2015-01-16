855ae45c5a6417b535641fcd162915dab436b521
Michael Niedermayer <michaelni@gmx.at>
2015-01-16 02:10:35 +0100

    avdevice: Use av_format_get_control_message_cb()
    This is required as the location of this field could change and is
    specified in libavformat not avdevice
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>
    (cherry picked from commit ba97cf2c4562b60fbef89103b61516891e31845e)
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavdevice/avdevice.c
+++ libavdevice/avdevice.c
@@ -48,9 +48,9 @@ int avdevice_app_to_dev_control_message(struct AVFormatContext *s, enum AVAppToD
 int avdevice_dev_to_app_control_message(struct AVFormatContext *s, enum AVDevToAppMessageType type,
                                         void *data, size_t data_size)
 {
-    if (!s->control_message_cb)
+    if (!av_format_get_control_message_cb(s))
         return AVERROR(ENOSYS);
-    return s->control_message_cb(s, type, data, data_size);
+    return av_format_get_control_message_cb(s)(s, type, data, data_size);
 }
 
 int avdevice_list_devices(AVFormatContext *s, AVDeviceInfoList **device_list)
