e6c90ce94f1b07f50cea2babf7471af455cca0ff
Anton Khirnov <anton@khirnov.net>
2015-01-17 22:28:46 +0100

    h264: move loopfilter parameters into the per-slice context

--- libavcodec/h264_mb_template.c
+++ libavcodec/h264_mb_template.c
@@ -158,7 +158,7 @@ static av_noinline void FUNC(hl_decode_mb)(H264Context *h, H264SliceContext *sl)
         }
     } else {
         if (IS_INTRA(mb_type)) {
-            if (h->deblocking_filter)
+            if (sl->deblocking_filter)
                 xchg_mb_border(h, sl, dest_y, dest_cb, dest_cr, linesize,
                                uvlinesize, 1, 0, SIMPLE, PIXEL_SHIFT);
 
@@ -171,7 +171,7 @@ static av_noinline void FUNC(hl_decode_mb)(H264Context *h, H264SliceContext *sl)
                                       transform_bypass, PIXEL_SHIFT,
                                       block_offset, linesize, dest_y, 0);
 
-            if (h->deblocking_filter)
+            if (sl->deblocking_filter)
                 xchg_mb_border(h, sl, dest_y, dest_cb, dest_cr, linesize,
                                uvlinesize, 0, 0, SIMPLE, PIXEL_SHIFT);
         } else if (is_h264) {
@@ -342,7 +342,7 @@ static av_noinline void FUNC(hl_decode_mb_444)(H264Context *h, H264SliceContext 
         }
     } else {
         if (IS_INTRA(mb_type)) {
-            if (h->deblocking_filter)
+            if (sl->deblocking_filter)
                 xchg_mb_border(h, sl, dest[0], dest[1], dest[2], linesize,
                                linesize, 1, 1, SIMPLE, PIXEL_SHIFT);
 
@@ -351,7 +351,7 @@ static av_noinline void FUNC(hl_decode_mb_444)(H264Context *h, H264SliceContext 
                                           transform_bypass, PIXEL_SHIFT,
                                           block_offset, linesize, dest[p], p);
 
-            if (h->deblocking_filter)
+            if (sl->deblocking_filter)
                 xchg_mb_border(h, sl, dest[0], dest[1], dest[2], linesize,
                                linesize, 0, 1, SIMPLE, PIXEL_SHIFT);
         } else {
