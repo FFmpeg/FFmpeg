ecab21ac47d0d4ca604bebf494017ae5090853a8
Anton Khirnov <anton@khirnov.net>
2015-01-18 12:31:11 +0100

    h264: do not reset the ref lists in flush_change()
    They are always constructed anew when needed, so there is no need to
    reset them explicitly.

--- libavcodec/h264.c
+++ libavcodec/h264.c
@@ -1057,10 +1057,6 @@ void ff_h264_flush_change(H264Context *h)
     if (h->cur_pic_ptr)
         h->cur_pic_ptr->reference = 0;
     h->first_field = 0;
-    memset(h->ref_list[0], 0, sizeof(h->ref_list[0]));
-    memset(h->ref_list[1], 0, sizeof(h->ref_list[1]));
-    memset(h->default_ref_list[0], 0, sizeof(h->default_ref_list[0]));
-    memset(h->default_ref_list[1], 0, sizeof(h->default_ref_list[1]));
     ff_h264_reset_sei(h);
     h->recovery_frame = -1;
     h->frame_recovered = 0;
