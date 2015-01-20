ba6888c0e3b5b0d6785b36d0ec7e51de4628b787
Christophe Gisquet <christophe.gisquet@gmail.com>
2015-01-20 22:33:00 +0100

    ffmpeg_opt: expand format for strftime
    While the shortened format specifiers are valid C99 ones, they are
    not supported by e.g. some Visual Studio runtimes (and thus mingw).
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- ffmpeg_opt.c
+++ ffmpeg_opt.c
@@ -519,7 +519,7 @@ static int opt_recording_timestamp(void *optctx, const char *opt, const char *ar
     char buf[128];
     int64_t recording_timestamp = parse_time_or_die(opt, arg, 0) / 1E6;
     struct tm time = *gmtime((time_t*)&recording_timestamp);
-    if (!strftime(buf, sizeof(buf), "creation_time=%FT%T%z", &time))
+    if (!strftime(buf, sizeof(buf), "creation_time=%Y-%m-%dT%H:%M:%S%z", &time))
         return -1;
     parse_option(o, "metadata", buf, options);
 
