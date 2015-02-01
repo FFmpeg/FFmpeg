1a8f5c9e2dbd276d9106a8116e08df40b9e1a538
Michael Niedermayer <michaelni@gmx.at>
2015-02-01 19:19:25 +0100

    avformat/utils: Fix number suffixes in tb_unreliable()
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>
    (cherry picked from commit 4b15bba2aec93776bfdc69a1bca42a4795a7d191)
    
    Conflicts:
    
    	libavformat/utils.c
    (cherry picked from commit e651a2f88c219e74c9851563e74100f7652a6005)

--- libavformat/utils.c
+++ libavformat/utils.c
@@ -2623,8 +2623,8 @@ static int get_std_framerate(int i){
  * And there are "variable" fps files this needs to detect as well.
  */
 static int tb_unreliable(AVCodecContext *c){
-    if(   c->time_base.den >= 101L*c->time_base.num
-       || c->time_base.den <    5L*c->time_base.num
+    if(   c->time_base.den >= 101LL*c->time_base.num
+       || c->time_base.den <    5LL*c->time_base.num
 /*       || c->codec_tag == AV_RL32("DIVX")
        || c->codec_tag == AV_RL32("XVID")*/
        || c->codec_tag == AV_RL32("mp4v")
