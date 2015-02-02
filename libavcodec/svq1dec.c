9b8c8a9395c849639aea0f6b5300e991e93c3a73
Luca Barbato <lu_zero@gentoo.org>
2015-02-02 02:22:34 +0100

    svq1dec: Validate the stages value strictly
    It can be less than -1.
    
    Bug-Id: CID 1194397 / CID 1194398
    CC: libav-stable@libav.org

--- libavcodec/svq1dec.c
+++ libavcodec/svq1dec.c
@@ -191,7 +191,7 @@ static int svq1_decode_block_intra(GetBitContext *bitbuf, uint8_t *pixels,
             continue;   /* skip vector */
         }
 
-        if (stages > 0 && level >= 4) {
+        if ((stages > 0 && level >= 4) || stages < 0) {
             av_dlog(NULL,
                     "Error (svq1_decode_block_intra): invalid vector: stages=%i level=%i\n",
                     stages, level);
@@ -253,7 +253,7 @@ static int svq1_decode_block_non_intra(GetBitContext *bitbuf, uint8_t *pixels,
         if (stages == -1)
             continue;           /* skip vector */
 
-        if ((stages > 0) && (level >= 4)) {
+        if ((stages > 0 && level >= 4) || stages < 0) {
             av_dlog(NULL,
                     "Error (svq1_decode_block_non_intra): invalid vector: stages=%i level=%i\n",
                     stages, level);
