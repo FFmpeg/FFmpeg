c93a7b264f76a0c92e25a561848448333731fbce
Paul B Mahol <onemda@gmail.com>
2015-02-03 15:15:11 +0000

    avcodec/ac3dec: use init_get_bits8()
    Signed-off-by: Paul B Mahol <onemda@gmail.com>

--- libavcodec/ac3dec.c
+++ libavcodec/ac3dec.c
@@ -1429,7 +1429,8 @@ static int ac3_decode_frame(AVCodecContext * avctx, void *data,
         memcpy(s->input_buffer, buf, FFMIN(buf_size, AC3_FRAME_BUFFER_SIZE));
     buf = s->input_buffer;
     /* initialize the GetBitContext with the start of valid AC-3 Frame */
-    init_get_bits(&s->gbc, buf, buf_size * 8);
+    if ((ret = init_get_bits8(&s->gbc, buf, buf_size)) < 0)
+        return ret;
 
     /* parse the syncinfo */
     err = parse_frame_header(s);
