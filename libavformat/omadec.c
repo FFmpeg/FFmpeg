29fd3032cc961c02acd45ef63aa3e687f5cd15af
Michael Niedermayer <michaelni@gmx.at>
2015-02-07 21:16:20 +0100

    avformat/omadec: Subtract headersize in timestamp calculation
    Fixes pts/dts
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/omadec.c
+++ libavformat/omadec.c
@@ -430,9 +430,9 @@ static int oma_read_packet(AVFormatContext *s, AVPacket *pkt)
 
     pkt->stream_index = 0;
 
-    if (pos > 0 && byte_rate > 0) {
+    if (pos >= oc->content_start && byte_rate > 0) {
         pkt->pts =
-        pkt->dts = av_rescale(pos, st->time_base.den,
+        pkt->dts = av_rescale(pos - oc->content_start, st->time_base.den,
                               byte_rate * (int64_t)st->time_base.num);
     }
 
