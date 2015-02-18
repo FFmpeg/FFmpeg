2684ff3573a68037aa298bedd93b00a126c48a30
Federico Tomassetti <ftomassetti@groupon.com>
2015-02-18 12:11:44 +0000

    eamad: check for out of bounds read
    Bug-Id: CID 1257500
    CC: libav-stable@libav.org
    
    Signed-off-by: Luca Barbato <lu_zero@gentoo.org>
    (cherry picked from commit 061c489895d29049a88dc6118e4b639a273b31d6)
    
    Conflicts:
    
    	libavcodec/eamad.c

--- libavcodec/eamad.c
+++ libavcodec/eamad.c
@@ -151,6 +151,11 @@ static inline int decode_block_intra(MadContext *s, int16_t * block)
                 break;
             } else if (level != 0) {
                 i += run;
+                if (i > 63) {
+                    av_log(s->avctx, AV_LOG_ERROR,
+                           "ac-tex damaged at %d %d\n", s->mb_x, s->mb_y);
+                    return -1;
+                }
                 j = scantable[i];
                 level = (level*quant_matrix[j]) >> 4;
                 level = (level-1)|1;
@@ -165,6 +170,11 @@ static inline int decode_block_intra(MadContext *s, int16_t * block)
                 run = SHOW_UBITS(re, &s->gb, 6)+1; LAST_SKIP_BITS(re, &s->gb, 6);
 
                 i += run;
+                if (i > 63) {
+                    av_log(s->avctx, AV_LOG_ERROR,
+                           "ac-tex damaged at %d %d\n", s->mb_x, s->mb_y);
+                    return -1;
+                }
                 j = scantable[i];
                 if (level < 0) {
                     level = -level;
@@ -175,10 +185,6 @@ static inline int decode_block_intra(MadContext *s, int16_t * block)
                     level = (level*quant_matrix[j]) >> 4;
                     level = (level-1)|1;
                 }
-            }
-            if (i > 63) {
-                av_log(s->avctx, AV_LOG_ERROR, "ac-tex damaged at %d %d\n", s->mb_x, s->mb_y);
-                return -1;
             }
 
             block[j] = level;
