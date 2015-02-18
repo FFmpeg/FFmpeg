b0f5227558999df36d9980f23dbf811c980a13c4
Clément Bœsch <clement@stupeflix.com>
2015-02-18 11:17:10 +0100

    avfilter/paletteuse: fix error dithering accuracy

--- libavfilter/vf_paletteuse.c
+++ libavfilter/vf_paletteuse.c
@@ -133,9 +133,9 @@ static int query_formats(AVFilterContext *ctx)
 
 static av_always_inline int dither_color(uint32_t px, int er, int eg, int eb, int scale, int shift)
 {
-    return av_clip_uint8((px >> 16 & 0xff) + ((er * scale) >> shift)) << 16
-         | av_clip_uint8((px >>  8 & 0xff) + ((eg * scale) >> shift)) <<  8
-         | av_clip_uint8((px       & 0xff) + ((eb * scale) >> shift));
+    return av_clip_uint8((px >> 16 & 0xff) + ((er * scale) / (1<<shift))) << 16
+         | av_clip_uint8((px >>  8 & 0xff) + ((eg * scale) / (1<<shift))) <<  8
+         | av_clip_uint8((px       & 0xff) + ((eb * scale) / (1<<shift)));
 }
 
 static av_always_inline int diff(const uint8_t *c1, const uint8_t *c2)
