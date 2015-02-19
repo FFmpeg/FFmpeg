03f39fbb2a558153a3c464edec1378d637a755fe
Michael Niedermayer <michaelni@gmx.at>
2015-02-19 16:25:29 +0100

    avcodec/x86/mlpdsp_init: Simplify mlp_filter_channel_x86()
    Based on patch by Francisco Blas Izquierdo Riera
    Commit message partly taken from carl
    
    fixes a compilation
    error in mlpdsp_init.c with -fstack-check and some gcc compilers (I
    reproduced the issue with gcc 4.7.3) by simplifying the code.
    
    See also https://bugs.gentoo.org/show_bug.cgi?id=471756
    
    $ make libavcodec/x86/mlpdsp_init.o
    libavcodec/x86/mlpdsp_init.c: In function ‘mlp_filter_channel_x86’:
    libavcodec/x86/mlpdsp_init.c:142:5: error: can’t find a register in
    class ‘GENERAL_REGS’ while reloading ‘asm’
    libavcodec/x86/mlpdsp_init.c:142:5: error: ‘asm’ operand has impossible
    constraints
    
    4551 -> 4509 dezicycles
    
    Reviewed-by: Ramiro Polla <ramiro.polla@gmail.com>
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/x86/mlpdsp_init.c
+++ libavcodec/x86/mlpdsp_init.c
@@ -148,8 +148,8 @@ static void mlp_filter_channel_x86(int32_t *state, const int32_t *coeff,
         FIRMUL   (ff_mlp_firorder_6, 0x14   )
         FIRMUL   (ff_mlp_firorder_5, 0x10   )
         FIRMUL   (ff_mlp_firorder_4, 0x0c   )
-        FIRMULREG(ff_mlp_firorder_3, 0x08,10)
-        FIRMULREG(ff_mlp_firorder_2, 0x04, 9)
+        FIRMUL   (ff_mlp_firorder_3, 0x08   )
+        FIRMUL   (ff_mlp_firorder_2, 0x04   )
         FIRMULREG(ff_mlp_firorder_1, 0x00, 8)
         LABEL_MANGLE(ff_mlp_firorder_0)":\n\t"
         "jmp  *%6                     \n\t"
@@ -178,8 +178,6 @@ static void mlp_filter_channel_x86(int32_t *state, const int32_t *coeff,
         : /* 4*/"r"((x86_reg)mask), /* 5*/"r"(firjump),
           /* 6*/"r"(iirjump)      , /* 7*/"c"(filter_shift)
         , /* 8*/"r"((int64_t)coeff[0])
-        , /* 9*/"r"((int64_t)coeff[1])
-        , /*10*/"r"((int64_t)coeff[2])
         : "rax", "rdx", "rsi"
 #else /* ARCH_X86_32 */
           /* 3*/"+m"(blocksize)
