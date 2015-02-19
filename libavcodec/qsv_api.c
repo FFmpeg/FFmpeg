0fbb271318899a0fb1fbcbb3db8292e909b91e23
Anton Khirnov <anton@khirnov.net>
2015-02-19 17:35:34 +0100

    qsv_api: fix building with libmfx disabled

--- libavcodec/qsv_api.c
+++ libavcodec/qsv_api.c
@@ -18,11 +18,25 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
+#include "config.h"
+
+#include <stddef.h>
+
 #include "libavutil/mem.h"
 
+#if CONFIG_QSV
 #include "qsv.h"
 
 AVQSVContext *av_qsv_alloc_context(void)
 {
     return av_mallocz(sizeof(AVQSVContext));
 }
+#else
+
+struct AVQSVContext *av_qsv_alloc_context(void);
+
+struct AVQSVContext *av_qsv_alloc_context(void)
+{
+    return NULL;
+}
+#endif
