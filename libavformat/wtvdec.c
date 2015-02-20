8f65bedbaff42b046061b1874a4980f4d65b589e
Michael Niedermayer <michaelni@gmx.at>
2015-02-20 20:57:31 +0100

    avformat/wtvdec: Use 64bit for ret to avoid overflow
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>
    (cherry picked from commit d44e0d8b930732a4a247b4884d75cf62b4ad3664)
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/wtvdec.c
+++ libavformat/wtvdec.c
@@ -760,7 +760,7 @@ static int recover(WtvContext *wtv, uint64_t broken_pos)
     int i;
     for (i = 0; i < wtv->nb_index_entries; i++) {
         if (wtv->index_entries[i].pos > broken_pos) {
-            int ret = avio_seek(pb, wtv->index_entries[i].pos, SEEK_SET);
+            int64_t ret = avio_seek(pb, wtv->index_entries[i].pos, SEEK_SET);
             if (ret < 0)
                 return ret;
             wtv->pts = wtv->index_entries[i].timestamp;
