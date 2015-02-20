107738051393b1f91d4624909113e156ea910b08
Michael Niedermayer <michaelni@gmx.at>
2015-02-20 20:13:06 +0100

    avformat/idcin: Use 64bit for ret to avoid overflow
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>
    (cherry picked from commit d1923d15a3544cbb94563a59e7169291db76b312)
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/idcin.c
+++ libavformat/idcin.c
@@ -359,7 +359,7 @@ static int idcin_read_seek(AVFormatContext *s, int stream_index,
     IdcinDemuxContext *idcin = s->priv_data;
 
     if (idcin->first_pkt_pos > 0) {
-        int ret = avio_seek(s->pb, idcin->first_pkt_pos, SEEK_SET);
+        int64_t ret = avio_seek(s->pb, idcin->first_pkt_pos, SEEK_SET);
         if (ret < 0)
             return ret;
         ff_update_cur_dts(s, s->streams[idcin->video_stream_index], 0);
