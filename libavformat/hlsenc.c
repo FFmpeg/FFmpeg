e78a729163c70af61856689f71c0ad9913fbc5d2
Michael Niedermayer <michaelni@gmx.at>
2015-02-21 01:21:53 +0100

    avformat/hlsenc: Use ff_rename() only for local files
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/hlsenc.c
+++ libavformat/hlsenc.c
@@ -245,8 +245,10 @@ static int hls_window(AVFormatContext *s, int last)
     char temp_filename[1024];
     int64_t sequence = FFMAX(hls->start_sequence, hls->sequence - hls->nb_entries);
     int version = hls->flags & HLS_SINGLE_FILE ? 4 : 3;
+    const char *proto = avio_find_protocol_name(s->filename);
+    int use_rename = proto && !strcmp(proto, "file");
 
-    snprintf(temp_filename, sizeof(temp_filename), "%s.tmp", s->filename);
+    snprintf(temp_filename, sizeof(temp_filename), use_rename ? "%s.tmp" : "%s", s->filename);
     if ((ret = avio_open2(&out, temp_filename, AVIO_FLAG_WRITE,
                           &s->interrupt_callback, NULL)) < 0)
         goto fail;
@@ -282,7 +284,7 @@ static int hls_window(AVFormatContext *s, int last)
 
 fail:
     avio_closep(&out);
-    if (ret >= 0)
+    if (ret >= 0 && use_rename)
         ff_rename(temp_filename, s->filename, s);
     return ret;
 }
