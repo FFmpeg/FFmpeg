e6b18f5700f447ba16857ca707f0400a094333cb
Andreas Cadhalpun <Andreas.Cadhalpun@googlemail.com>
2015-02-28 20:11:36 +0100

    avcodec/utils: use correct printf specifier in ff_set_sar
    Signed-off-by: Andreas Cadhalpun <Andreas.Cadhalpun@googlemail.com>
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>
    (cherry picked from commit 732c3ebffaff5005367d7f947fa903f3b6e92f68)
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/utils.c
+++ libavcodec/utils.c
@@ -246,7 +246,7 @@ int ff_set_sar(AVCodecContext *avctx, AVRational sar)
     int ret = av_image_check_sar(avctx->width, avctx->height, sar);
 
     if (ret < 0) {
-        av_log(avctx, AV_LOG_WARNING, "ignoring invalid SAR: %u/%u\n",
+        av_log(avctx, AV_LOG_WARNING, "ignoring invalid SAR: %d/%d\n",
                sar.num, sar.den);
         avctx->sample_aspect_ratio = (AVRational){ 0, 1 };
         return ret;
