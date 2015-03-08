6e43bb2dc29c1257256501c0c85668391a4a651c
Michael Niedermayer <michaelni@gmx.at>
2015-03-08 23:27:43 +0100

    avcodec/tiff: move bpp check to after "end:"
    This ensures that all current and future code-pathes get bpp checked
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>
    (cherry picked from commit d5e9fc782150d4596c72440a0aa02b7f4f1254b1)
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/tiff.c
+++ libavcodec/tiff.c
@@ -628,13 +628,6 @@ static int tiff_decode_tag(TiffContext *s, AVFrame *frame)
                 s->bpp = -1;
             }
         }
-        if (s->bpp > 64U) {
-            av_log(s->avctx, AV_LOG_ERROR,
-                   "This format is not supported (bpp=%d, %d components)\n",
-                   s->bpp, count);
-            s->bpp = 0;
-            return AVERROR_INVALIDDATA;
-        }
         break;
     case TIFF_SAMPLES_PER_PIXEL:
         if (count != 1) {
@@ -926,6 +919,13 @@ static int tiff_decode_tag(TiffContext *s, AVFrame *frame)
         }
     }
 end:
+    if (s->bpp > 64U) {
+        av_log(s->avctx, AV_LOG_ERROR,
+                "This format is not supported (bpp=%d, %d components)\n",
+                s->bpp, count);
+        s->bpp = 0;
+        return AVERROR_INVALIDDATA;
+    }
     bytestream2_seek(&s->gb, start, SEEK_SET);
     return 0;
 }
