d81150929d35efc423b488e548d4dc881cde5dbd
Andreas Cadhalpun <Andreas.Cadhalpun@googlemail.com>
2015-03-08 23:31:48 +0100

    ffmdec: fix infinite loop at EOF
    If EOF is reached, while skipping bytes, avio_tell(pb) won't change
    anymore, resulting in an infinite loop.
    
    Signed-off-by: Andreas Cadhalpun <Andreas.Cadhalpun@googlemail.com>
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>
    (cherry picked from commit 6fa98822eba501a4898fdec5b75acd3026201005)
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/ffmdec.c
+++ libavformat/ffmdec.c
@@ -354,7 +354,7 @@ static int ffm2_read_header(AVFormatContext *s)
     }
 
     /* get until end of block reached */
-    while ((avio_tell(pb) % ffm->packet_size) != 0)
+    while ((avio_tell(pb) % ffm->packet_size) != 0 && !pb->eof_reached)
         avio_r8(pb);
 
     /* init packet demux */
@@ -481,7 +481,7 @@ static int ffm_read_header(AVFormatContext *s)
     }
 
     /* get until end of block reached */
-    while ((avio_tell(pb) % ffm->packet_size) != 0)
+    while ((avio_tell(pb) % ffm->packet_size) != 0 && !pb->eof_reached)
         avio_r8(pb);
 
     /* init packet demux */
