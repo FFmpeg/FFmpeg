88f2d31d4e929f5592ed03d5f167ce3addccbe5f
Michael Niedermayer <michaelni@gmx.at>
2015-03-14 21:23:32 +0100

    avformat/mov: Use sizeof(filename) instead of a literal number
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>
    (cherry picked from commit 21a53dd08dce7cc5b3fdf9c4826b4b74d8300ea0)
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/mov.c
+++ libavformat/mov.c
@@ -2312,9 +2312,9 @@ static int mov_open_dref(AVIOContext **pb, const char *src, MOVDref *ref,
             filename[src_path - src] = 0;
 
             for (i = 1; i < ref->nlvl_from; i++)
-                av_strlcat(filename, "../", 1024);
+                av_strlcat(filename, "../", sizeof(filename));
 
-            av_strlcat(filename, ref->path + l + 1, 1024);
+            av_strlcat(filename, ref->path + l + 1, sizeof(filename));
 
             if (!avio_open2(pb, filename, AVIO_FLAG_READ, int_cb, NULL))
                 return 0;
