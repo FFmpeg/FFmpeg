61dbd3f3d09a6b569c41946a4c07dac0f21d413a
Michael Niedermayer <michaelni@gmx.at>
2015-03-19 23:28:39 +0100

    avcodec/dnxhddec: Check that the frame is interlaced before using cur_field
    Fixes Ticket4227
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>
    (cherry picked from commit 2c660e34cf3c2b77cd2bef6f292920334dfd9192)
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/dnxhddec.c
+++ libavcodec/dnxhddec.c
@@ -363,7 +363,7 @@ static int dnxhd_decode_macroblock(DNXHDContext *ctx, AVFrame *frame,
     dest_u = frame->data[1] + ((y * dct_linesize_chroma) << 4) + (x << (3 + shift1 + ctx->is_444));
     dest_v = frame->data[2] + ((y * dct_linesize_chroma) << 4) + (x << (3 + shift1 + ctx->is_444));
 
-    if (ctx->cur_field) {
+    if (frame->interlaced_frame && ctx->cur_field) {
         dest_y += frame->linesize[0];
         dest_u += frame->linesize[1];
         dest_v += frame->linesize[2];
