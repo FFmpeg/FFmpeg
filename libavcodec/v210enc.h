f8fd0c29ce5dc7d4be13e7e89d0f1e8a035ae71b
Vittorio Giovara <vittorio.giovara@gmail.com>
2015-03-23 20:13:43 +0000

    v210: Fix typo in header guards

--- libavcodec/v210enc.h
+++ libavcodec/v210enc.h
@@ -16,8 +16,8 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
-#ifndef AVCOENC_V210ENC_H
-#define AVCOENC_V210ENC_H
+#ifndef AVCODEC_V210ENC_H
+#define AVCODEC_V210ENC_H
 
 #include "libavutil/log.h"
 #include "libavutil/opt.h"
@@ -32,4 +32,4 @@ typedef struct V210EncContext {
 
 void ff_v210enc_init_x86(V210EncContext *s);
 
-#endif /* AVCOENC_V210ENC_H */
+#endif /* AVCODEC_V210ENC_H */
