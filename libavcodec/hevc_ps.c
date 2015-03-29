ce0bc09ee2580d49fec90a6eb0de2ba1b580c854
Anton Khirnov <anton@khirnov.net>
2015-03-29 15:20:30 +0200

    hevc: do not change the VPS if its contents are the same
    This is the same as is done for SPS.

--- libavcodec/hevc_ps.c
+++ libavcodec/hevc_ps.c
@@ -407,8 +407,14 @@ int ff_hevc_decode_nal_vps(HEVCContext *s)
     }
     get_bits1(gb); /* vps_extension_flag */
 
-    av_buffer_unref(&s->vps_list[vps_id]);
-    s->vps_list[vps_id] = vps_buf;
+    if (s->vps_list[vps_id] &&
+        !memcmp(s->vps_list[vps_id]->data, vps_buf->data, vps_buf->size)) {
+        av_buffer_unref(&vps_buf);
+    } else {
+        av_buffer_unref(&s->vps_list[vps_id]);
+        s->vps_list[vps_id] = vps_buf;
+    }
+
     return 0;
 
 err:
