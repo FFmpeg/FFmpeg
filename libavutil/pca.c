3fd66f7f4589dbc31d144454546506a0bb939647
Michael Niedermayer <michaelni@gmx.at>
2015-03-30 04:37:42 +0200

    avutil/pca: Check for av_malloc* failures
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>
    (cherry picked from commit dadc43eee4d9036aa532665a04720238cc15e922)
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavutil/pca.c
+++ libavutil/pca.c
@@ -41,11 +41,19 @@ PCA *ff_pca_init(int n){
         return NULL;
 
     pca= av_mallocz(sizeof(*pca));
+    if (!pca)
+        return NULL;
+
     pca->n= n;
     pca->z = av_malloc_array(n, sizeof(*pca->z));
     pca->count=0;
     pca->covariance= av_calloc(n*n, sizeof(double));
     pca->mean= av_calloc(n, sizeof(double));
+
+    if (!pca->z || !pca->covariance || !pca->mean) {
+        ff_pca_free(pca);
+        return NULL;
+    }
 
     return pca;
 }
