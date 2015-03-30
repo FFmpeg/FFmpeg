1a10134e20efcda2666fb6f3488b2cda8c7756dc
Michael Niedermayer <michaelni@gmx.at>
2015-03-30 22:59:09 +0200

    swresample/swresample: Use av_mallocz_array()
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libswresample/swresample.c
+++ libswresample/swresample.c
@@ -352,7 +352,7 @@ int swri_realloc_audio(AudioData *a, int count){
     av_assert0(a->bps);
     av_assert0(a->ch_count);
 
-    a->data= av_mallocz(countb*a->ch_count);
+    a->data= av_mallocz_array(countb, a->ch_count);
     if(!a->data)
         return AVERROR(ENOMEM);
     for(i=0; i<a->ch_count; i++){
