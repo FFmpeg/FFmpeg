08d0dbf2aecbbe387227dfae4314e0a989ca4556
Lukasz Marek <lukasz.m.luki2@gmail.com>
2015-04-02 00:36:02 +0200

    lavu/dict: don't accept AV_DICT_DONT_STRDUP_VAL for av_dict_set_int
    Signed-off-by: Lukasz Marek <lukasz.m.luki2@gmail.com>
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavutil/dict.c
+++ libavutil/dict.c
@@ -141,6 +141,7 @@ int av_dict_set_int(AVDictionary **pm, const char *key, int64_t value,
 {
     char valuestr[22];
     snprintf(valuestr, sizeof(valuestr), "%"PRId64, value);
+    flags &= ~AV_DICT_DONT_STRDUP_VAL;
     return av_dict_set(pm, key, valuestr, flags);
 }
 
