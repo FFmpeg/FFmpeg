a6a45e5a9a9dfb725456f62e053532e505aadcfc
Stephen Hutchinson <qyot27@gmail.com>
2015-04-02 15:39:25 -0400

    avisynth: Fix detection of AviSynth 2.5
    In order to safely exit when the user tries to use AviSynth 2.5, the
    continue_on_fail value for 2.6's functions need to be set to 1.
    Otherwise, the library loader fails before the 'upgrade to 2.6'
    log message appears.
    
    Signed-off-by: Diego Biurrun <diego@biurrun.de>

--- libavformat/avisynth.c
+++ libavformat/avisynth.c
@@ -140,15 +140,15 @@ static av_cold int avisynth_load_library(void)
     LOAD_AVS_FUNC(avs_release_video_frame, 0);
     LOAD_AVS_FUNC(avs_take_clip, 0);
 #ifdef USING_AVISYNTH
-    LOAD_AVS_FUNC(avs_bits_per_pixel, 0);
-    LOAD_AVS_FUNC(avs_get_height_p, 0);
-    LOAD_AVS_FUNC(avs_get_pitch_p, 0);
-    LOAD_AVS_FUNC(avs_get_read_ptr_p, 0);
-    LOAD_AVS_FUNC(avs_get_row_size_p, 0);
-    LOAD_AVS_FUNC(avs_is_yv24, 0);
-    LOAD_AVS_FUNC(avs_is_yv16, 0);
-    LOAD_AVS_FUNC(avs_is_yv411, 0);
-    LOAD_AVS_FUNC(avs_is_y8, 0);
+    LOAD_AVS_FUNC(avs_bits_per_pixel, 1);
+    LOAD_AVS_FUNC(avs_get_height_p, 1);
+    LOAD_AVS_FUNC(avs_get_pitch_p, 1);
+    LOAD_AVS_FUNC(avs_get_read_ptr_p, 1);
+    LOAD_AVS_FUNC(avs_get_row_size_p, 1);
+    LOAD_AVS_FUNC(avs_is_yv24, 1);
+    LOAD_AVS_FUNC(avs_is_yv16, 1);
+    LOAD_AVS_FUNC(avs_is_yv411, 1);
+    LOAD_AVS_FUNC(avs_is_y8, 1);
 #endif
 #undef LOAD_AVS_FUNC
 
