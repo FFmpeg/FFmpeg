5637ff01218a44fcc1fff9b4382a15a6470abcc9
Martin Storsjö <martin@martin.st>
2015-04-02 15:17:01 +0300

    libavcodec: Clarify the documentation of the internal codec capability flags
    The previous documentation was very vague and almost misleading.
    
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- libavcodec/internal.h
+++ libavcodec/internal.h
@@ -34,11 +34,16 @@
 #include "config.h"
 
 /**
- * Codec is thread safe.
+ * The codec does not modify any global variables in the init function,
+ * allowing to call the init function without locking any global mutexes.
  */
 #define FF_CODEC_CAP_INIT_THREADSAFE        (1 << 0)
 /**
- * Codec cleans up memory on init failure.
+ * The codec allows calling the close function for deallocation even if
+ * the init function returned a failure. Without this capability flag, a
+ * codec does such cleanup internally when returning failures from the
+ * init function and does not expect the close function to be called at
+ * all.
  */
 #define FF_CODEC_CAP_INIT_CLEANUP           (1 << 1)
 
