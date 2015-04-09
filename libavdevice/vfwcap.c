259fd4c7cfb8afbb022921b44fe6611fcefff3b1
Michael Niedermayer <michaelni@gmx.at>
2015-04-09 21:18:51 +0200

    avdevice/vfwcap: put the headers back in the order in which they need to be
    This avoids build failures
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavdevice/vfwcap.c
+++ libavdevice/vfwcap.c
@@ -19,8 +19,8 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
-#include <vfw.h>
 #include <windows.h>
+#include <vfw.h>
 
 #include "libavutil/internal.h"
 #include "libavutil/log.h"
