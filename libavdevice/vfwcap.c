4d0f6d3fb421b3ab774ccd3c299f0a242f359a75
Michael Niedermayer <michaelni@gmx.at>
2015-04-10 18:08:52 +0200

    avdevice/vfwcap: revert header reordering from c201069fac9a76e6604f9d84d76a172434d62200
    This should fix build on vs2013
    
    Found-by: Ferdinand Oeinck <ferdo@bigroses.demon.nl>
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavdevice/vfwcap.c
+++ libavdevice/vfwcap.c
@@ -19,15 +19,15 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
-#include <windows.h>
-#include <vfw.h>
-
 #include "libavutil/internal.h"
 #include "libavutil/log.h"
 #include "libavutil/opt.h"
 #include "libavutil/parseutils.h"
 
 #include "libavformat/internal.h"
+
+#include <windows.h>
+#include <vfw.h>
 
 #include "avdevice.h"
 
