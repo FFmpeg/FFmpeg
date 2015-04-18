baa58c19c48fa34a3782cd93264b7b0b1c7091e7
Michael Niedermayer <michaelni@gmx.at>
2015-04-18 20:50:23 +0200

    avcodec/alsdec: Use av_mallocz_array() for chan_data to ensure the arrays never contain random data
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>
    (cherry picked from commit 7e104647a3556fc61a139483cee1cb7dfa2dc5bd)
    
    Conflicts:
    
    	libavcodec/alsdec.c

--- libavcodec/alsdec.c
+++ libavcodec/alsdec.c
@@ -1703,9 +1703,9 @@ static av_cold int decode_init(AVCodecContext *avctx)
 
     // allocate and assign channel data buffer for mcc mode
     if (sconf->mc_coding) {
-        ctx->chan_data_buffer  = av_malloc(sizeof(*ctx->chan_data_buffer) *
+        ctx->chan_data_buffer  = av_mallocz(sizeof(*ctx->chan_data_buffer) *
                                            num_buffers * num_buffers);
-        ctx->chan_data         = av_malloc(sizeof(*ctx->chan_data) *
+        ctx->chan_data         = av_mallocz(sizeof(*ctx->chan_data) *
                                            num_buffers);
         ctx->reverted_channels = av_malloc(sizeof(*ctx->reverted_channels) *
                                            num_buffers);
