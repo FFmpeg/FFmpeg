338ed3ed33c412c2828446c4e2a76949161fec6a
Diego Biurrun <diego@biurrun.de>
2015-04-20 13:58:45 +0200

    ffv1: Pass correct pointers to av_free()
    Bug-Id: CID 1295124

--- libavcodec/ffv1.c
+++ libavcodec/ffv1.c
@@ -226,8 +226,8 @@ av_cold int ffv1_init_slice_contexts(FFV1Context *f)
 
 memfail:
     for (j = 0; j < i; j++) {
-        av_free(&f->slice_context[j]->sample_buffer);
-        av_free(&f->slice_context[j]);
+        av_free(f->slice_context[j]->sample_buffer);
+        av_free(f->slice_context[j]);
     }
     return AVERROR(ENOMEM);
 }
