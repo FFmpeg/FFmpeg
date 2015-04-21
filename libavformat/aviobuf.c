be5b679a6eb1252d20ea5e33ba2f35c127515652
Zhang Rui <bbcallen@gmail.com>
2015-04-21 17:47:26 +0800

    Revert "Fix url_feof() for file appendings."
    This reverts commit 3bd624b4ee355328382295af411f5343b1329f43.
    
    avio_feof() could reset eof_reached which is not expected.

--- libavformat/aviobuf.c
+++ libavformat/aviobuf.c
@@ -359,10 +359,6 @@ int avio_feof(AVIOContext *s)
 {
     if(!s)
         return 0;
-    if(s->eof_reached){
-        s->eof_reached=0;
-        fill_buffer(s);
-    }
     return s->eof_reached;
 }
 
