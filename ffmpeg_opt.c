a1acae0409700e1abe62cbc900321fcffc9d971d
Michael Niedermayer <michaelni@gmx.at>
2015-05-02 23:04:00 +0200

    ffmpeg_opt: remove rotate metadata in case of autorotate
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- ffmpeg_opt.c
+++ ffmpeg_opt.c
@@ -2184,8 +2184,11 @@ loop_end:
                 continue;
             ist = input_streams[output_streams[i]->source_index];
             av_dict_copy(&output_streams[i]->st->metadata, ist->st->metadata, AV_DICT_DONT_OVERWRITE);
-            if (!output_streams[i]->stream_copy)
+            if (!output_streams[i]->stream_copy) {
                 av_dict_set(&output_streams[i]->st->metadata, "encoder", NULL, 0);
+                if (ist->autorotate)
+                    av_dict_set(&output_streams[i]->st->metadata, "rotate", NULL, 0);
+            }
         }
 
     /* process manually set metadata */
