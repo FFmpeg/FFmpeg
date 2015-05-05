e31cdb5cc8080e164c66838a69f37f60f2d0ea82
Carl Eugen Hoyos <cehoyos@ag.or.at>
2015-05-05 23:51:20 +0200

    lavf/riff: Add MultiScope II fourcc MSC2 as MJPEG.

--- libavformat/riff.c
+++ libavformat/riff.c
@@ -174,6 +174,7 @@ const AVCodecTag ff_codec_bmp_tags[] = {
     { AV_CODEC_ID_MJPEG,        MKTAG('L', 'J', 'P', 'G') },
     { AV_CODEC_ID_MJPEG,        MKTAG('d', 'm', 'b', '1') },
     { AV_CODEC_ID_MJPEG,        MKTAG('m', 'j', 'p', 'a') },
+    { AV_CODEC_ID_MJPEG,        MKTAG('M', 'S', 'C', '2') }, /* Multiscope II */
     { AV_CODEC_ID_LJPEG,        MKTAG('L', 'J', 'P', 'G') },
     /* Pegasus lossless JPEG */
     { AV_CODEC_ID_MJPEG,        MKTAG('J', 'P', 'G', 'L') },
