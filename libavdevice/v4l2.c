a563a1468f5c5dcdccd5a9ed0009cb3edbde6813
Giorgio Vazzana <mywing81@gmail.com>
2015-05-08 17:42:34 +0200

    lavd/v4l2: fix typo
    This was introduced in cde6e328de214ffe8387641cdc1e433a2c2150f3
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>
    (cherry picked from commit 23e6cf832ff6e7260f7d5190a2d284b64d29e316)
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavdevice/v4l2.c
+++ libavdevice/v4l2.c
@@ -286,7 +286,7 @@ static void list_formats(AVFormatContext *ctx, int type)
         } else if (vfd.flags & V4L2_FMT_FLAG_COMPRESSED &&
                    type & V4L_COMPFORMATS) {
             const AVCodecDescriptor *desc = avcodec_descriptor_get(codec_id);
-            av_log(ctx, AV_LOG_INFO, "Compressedll      : %9s : %20s :",
+            av_log(ctx, AV_LOG_INFO, "Compressed: %9s : %20s :",
                    desc ? desc->name : "Unsupported",
                    vfd.description);
         } else {
