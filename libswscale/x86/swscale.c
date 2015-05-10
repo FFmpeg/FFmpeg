54e64eaf68e08aa69a441b783dd51304e64ca479
Michael Niedermayer <michaelni@gmx.at>
2015-05-10 15:09:04 +0200

    swscale/x86/swscale: Fix warning about loosing significant bits in cast
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libswscale/x86/swscale.c
+++ libswscale/x86/swscale.c
@@ -201,7 +201,7 @@ static void yuv2yuvX_sse3(const int16_t *filter, int filterSize,
                            const int16_t **src, uint8_t *dest, int dstW,
                            const uint8_t *dither, int offset)
 {
-    if(((int)dest) & 15){
+    if(((uintptr_t)dest) & 15){
         yuv2yuvX_mmxext(filter, filterSize, src, dest, dstW, dither, offset);
         return;
     }
