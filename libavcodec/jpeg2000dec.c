bf2254bffd50d4bfcd5bb1373662d72fedc2f54c
Michael Niedermayer <michaelni@gmx.at>
2015-05-13 15:15:55 +0200

    avcodec/jpeg2000dec: fix boolean operator
    Fixes CID1271791 #7-6
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>
    (cherry picked from commit f8f155a18ac454e7ff3312e0e0c3a70eb4359143)
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/jpeg2000dec.c
+++ libavcodec/jpeg2000dec.c
@@ -1559,7 +1559,7 @@ static int jp2_find_codestream(Jpeg2000DecoderContext *s)
                         int cn   = bytestream2_get_be16(&s->g);
                         int av_unused typ  = bytestream2_get_be16(&s->g);
                         int asoc = bytestream2_get_be16(&s->g);
-                        if (cn < 4 || asoc < 4)
+                        if (cn < 4 && asoc < 4)
                             s->cdef[cn] = asoc;
                     }
                 }
