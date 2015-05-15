e6ec65d2d3295be905be29fc22d4bcebf1c697f6
Michael Niedermayer <michaelni@gmx.at>
2015-05-15 20:19:21 +0200

    avformat/rtsp: print a debug level note if time parsing fails
    Fixes CID733718 again
    
    This partly reverts commit eb7ddb5066843ab16caf33b05eafb2576648b797.

--- libavformat/rtsp.c
+++ libavformat/rtsp.c
@@ -179,7 +179,8 @@ static void rtsp_parse_range_npt(const char *p, int64_t *start, int64_t *end)
     if (*p == '-') {
         p++;
         get_word_sep(buf, sizeof(buf), "-", &p);
-        av_parse_time(end, buf, 1);
+        if (av_parse_time(end, buf, 1) < 0)
+            av_log(NULL, AV_LOG_DEBUG, "Failed to parse interval end specification '%s'\n", buf);
     }
 }
 
