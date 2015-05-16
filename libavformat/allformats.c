9b56ac74b170d12027fbc81f581a451a709f1105
Luca Barbato <lu_zero@gentoo.org>
2015-05-16 17:03:28 +0200

    mpjpeg: Initial implementation
    Support only streams with Content-Length.
    
    Signed-off-by: Luca Barbato <lu_zero@gentoo.org>

--- libavformat/allformats.c
+++ libavformat/allformats.c
@@ -163,7 +163,7 @@ void av_register_all(void)
     REGISTER_MUXDEMUX(MPEGTS,           mpegts);
     REGISTER_DEMUXER (MPEGTSRAW,        mpegtsraw);
     REGISTER_DEMUXER (MPEGVIDEO,        mpegvideo);
-    REGISTER_MUXER   (MPJPEG,           mpjpeg);
+    REGISTER_MUXDEMUX(MPJPEG,           mpjpeg);
     REGISTER_DEMUXER (MSNWC_TCP,        msnwc_tcp);
     REGISTER_DEMUXER (MTV,              mtv);
     REGISTER_DEMUXER (MV,               mv);
