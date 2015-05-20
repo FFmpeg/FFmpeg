9dd9090b89d57451740fe8d2d49082b1152d2928
Michael Niedermayer <michaelni@gmx.at>
2015-05-20 17:13:15 +0200

    avformat/nutdec: Return error on EOF from get_str()
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>
    (cherry picked from commit 6bbb2f8f4da67af374d62403742482cc5962aa21)
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/nutdec.c
+++ libavformat/nutdec.c
@@ -49,6 +49,8 @@ static int get_str(AVIOContext *bc, char *string, unsigned int maxlen)
     if (maxlen)
         string[FFMIN(len, maxlen - 1)] = 0;
 
+    if (bc->eof_reached)
+        return AVERROR_EOF;
     if (maxlen == len)
         return -1;
     else
