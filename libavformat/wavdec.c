40a3e1e9c54997e4dfc7802b5a758b68ceb64982
Rodger Combs <rodger.combs@gmail.com>
2015-05-23 14:07:14 +0200

    avformat/wavdec: Increase dts packet threshold to fix more misdetections
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/wavdec.c
+++ libavformat/wavdec.c
@@ -119,7 +119,7 @@ static void handle_stream_probing(AVStream *st)
 {
     if (st->codec->codec_id == AV_CODEC_ID_PCM_S16LE) {
         st->request_probe = AVPROBE_SCORE_EXTENSION;
-        st->probe_packets = FFMIN(st->probe_packets, 20);
+        st->probe_packets = FFMIN(st->probe_packets, 32);
     }
 }
 
