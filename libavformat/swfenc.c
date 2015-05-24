28134d6a0e1462c7bc6807284738ed552ef6d2a5
Michael Niedermayer <michaelni@gmx.at>
2015-05-24 12:38:54 +0200

    avformat/swfenc: Remove another unneeded float computation
    fewer chances for rounding differences between platforms is better
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/swfenc.c
+++ libavformat/swfenc.c
@@ -236,7 +236,7 @@ static int swf_write_header(AVFormatContext *s)
     }
 
     if (!swf->audio_enc)
-        swf->samples_per_frame = (44100.0 * rate_base) / rate;
+        swf->samples_per_frame = (44100LL * rate_base) / rate;
     else
         swf->samples_per_frame = (swf->audio_enc->sample_rate * rate_base) / rate;
 
