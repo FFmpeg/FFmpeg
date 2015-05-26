1b236541a6b55da6ddc73fadd9a252c7e4bfb55f
Michael Niedermayer <michaelni@gmx.at>
2015-05-26 05:08:32 +0200

    avcodec/h264: Fix HWACCEL_MAX for D3D11
    Found-by: philipl
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/h264_slice.c
+++ libavcodec/h264_slice.c
@@ -863,6 +863,7 @@ static void init_scan_tables(H264Context *h)
 static enum AVPixelFormat get_pixel_format(H264Context *h, int force_callback)
 {
 #define HWACCEL_MAX (CONFIG_H264_DXVA2_HWACCEL + \
+                     CONFIG_H264_D3D11VA_HWACCEL + \
                      CONFIG_H264_VAAPI_HWACCEL + \
                      (CONFIG_H264_VDA_HWACCEL * 2) + \
                      CONFIG_H264_VDPAU_HWACCEL)
