57cde2b180fcec0eaf60aad65f436ab6420546f5
wm4 <nfxjfg@googlemail.com>
2015-05-26 19:25:42 +0200

    lavf: move TLS-related ifdeffery to library specific files
    There is no need to have this mess in network.c.
    
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- libavformat/tls.h
+++ libavformat/tls.h
@@ -51,4 +51,10 @@ typedef struct TLSShared {
 
 int ff_tls_open_underlying(TLSShared *c, URLContext *parent, const char *uri, AVDictionary **options);
 
+void ff_gnutls_init(void);
+void ff_gnutls_deinit(void);
+
+void ff_openssl_init(void);
+void ff_openssl_deinit(void);
+
 #endif /* AVFORMAT_TLS_H */
