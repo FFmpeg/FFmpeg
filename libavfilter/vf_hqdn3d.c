4240e6a92117811509be3e5f360a44dde8618040
Michael Niedermayer <michaelni@gmx.at>
2015-05-27 02:46:45 +0200

    avfilter/vf_hqdn3d: Initialize the whole LUT
    With bps > 8 more than 255..255 are used
    The initialized table content is left unchanged,
    But it could also be adjusted for the slight difference of
    the maximum
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavfilter/vf_hqdn3d.c
+++ libavfilter/vf_hqdn3d.c
@@ -180,9 +180,9 @@ static int16_t *precalc_coefs(double dist25, int depth)
 
     gamma = log(0.25) / log(1.0 - FFMIN(dist25,252.0)/255.0 - 0.00001);
 
-    for (i = -255<<LUT_BITS; i <= 255<<LUT_BITS; i++) {
+    for (i = -256<<LUT_BITS; i < 256<<LUT_BITS; i++) {
         double f = ((i<<(9-LUT_BITS)) + (1<<(8-LUT_BITS)) - 1) / 512.0; // midpoint of the bin
-        simil = 1.0 - FFABS(f) / 255.0;
+        simil = FFMAX(0, 1.0 - FFABS(f) / 255.0);
         C = pow(simil, gamma) * 256.0 * f;
         ct[(256<<LUT_BITS)+i] = lrint(C);
     }
