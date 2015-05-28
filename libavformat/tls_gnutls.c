FFmpeg/FFmpeg

commit 57cde2b180fcec0eaf60aad65f436ab6420546f5

lavf: move TLS-related ifdeffery to library specific files
There is no need to have this mess in network.c.

Signed-off-by: Martin Storsjö <martin@martin.st>

libavformat/tls_gnutls.c
@@ -19,6 +19,8 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
+#include <errno.h>
+
 #include <gnutls/gnutls.h>
 #include <gnutls/x509.h>
 
@@ -28,9 +30,15 @@
 #include "os_support.h"
 #include "url.h"
 #include "tls.h"
+#include "libavcodec/internal.h"
 #include "libavutil/avstring.h"
 #include "libavutil/opt.h"
 #include "libavutil/parseutils.h"
+
+#if HAVE_THREADS && GNUTLS_VERSION_NUMBER <= 0x020b00
+#include <gcrypt.h>
+GCRY_THREAD_OPTION_PTHREAD_IMPL;
+#endif
 
 typedef struct TLSContext {
     const AVClass *class;
@@ -39,6 +47,24 @@ typedef struct TLSContext {
     gnutls_certificate_credentials_t cred;
     int need_shutdown;
 } TLSContext;
+
+void ff_gnutls_init(void)
+{
+    avpriv_lock_avformat();
+#if HAVE_THREADS && GNUTLS_VERSION_NUMBER < 0x020b00
+    if (gcry_control(GCRYCTL_ANY_INITIALIZATION_P) == 0)
+        gcry_control(GCRYCTL_SET_THREAD_CBS, &gcry_threads_pthread);
+#endif
+    gnutls_global_init();
+    avpriv_unlock_avformat();
+}
+
+void ff_gnutls_deinit(void)
+{
+    avpriv_lock_avformat();
+    gnutls_global_deinit();
+    avpriv_unlock_avformat();
+}
 
 static int print_tls_error(URLContext *h, int ret)
 {
@@ -67,7 +93,7 @@ static int tls_close(URLContext *h)
         gnutls_certificate_free_credentials(c->cred);
     if (c->tls_shared.tcp)
         ffurl_close(c->tls_shared.tcp);
-    ff_tls_deinit();
+    ff_gnutls_deinit();
     return 0;
 }
 
@@ -103,7 +129,7 @@ static int tls_open(URLContext *h, const char *uri, int flags, AVDictionary **op
     TLSShared *c = &p->tls_shared;
     int ret;
 
-    ff_tls_init();
+    ff_gnutls_init();
 
     if ((ret = ff_tls_open_underlying(c, h, uri, options)) < 0)
         goto fail;

libavformat/network.c
@@ -20,102 +20,29 @@
 
 #include <fcntl.h>
 #include "network.h"
+#include "tls.h"
 #include "url.h"
 #include "libavcodec/internal.h"
 #include "libavutil/mem.h"
 
-#if HAVE_THREADS
-#if HAVE_PTHREADS
-#include <pthread.h>
-#else
-#include "compat/w32pthreads.h"
-#endif
-#endif
-
-#if CONFIG_OPENSSL
-#include <openssl/ssl.h>
-static int openssl_init;
-#if HAVE_THREADS
-#include <openssl/crypto.h>
-#include "libavutil/avutil.h"
-pthread_mutex_t *openssl_mutexes;
-static void openssl_lock(int mode, int type, const char *file, int line)
-{
-    if (mode & CRYPTO_LOCK)
-        pthread_mutex_lock(&openssl_mutexes[type]);
-    else
-        pthread_mutex_unlock(&openssl_mutexes[type]);
-}
-#if !defined(WIN32) && OPENSSL_VERSION_NUMBER < 0x10000000
-static unsigned long openssl_thread_id(void)
-{
-    return (intptr_t) pthread_self();
-}
-#endif
-#endif
-#endif
-#if CONFIG_GNUTLS
-#include <gnutls/gnutls.h>
-#if HAVE_THREADS && GNUTLS_VERSION_NUMBER <= 0x020b00
-#include <gcrypt.h>
-#include <errno.h>
-GCRY_THREAD_OPTION_PTHREAD_IMPL;
-#endif
-#endif
-
 void ff_tls_init(void)
 {
-    avpriv_lock_avformat();
-#if CONFIG_OPENSSL
-    if (!openssl_init) {
-        SSL_library_init();
-        SSL_load_error_strings();
-#if HAVE_THREADS
-        if (!CRYPTO_get_locking_callback()) {
-            int i;
-            openssl_mutexes = av_malloc(sizeof(pthread_mutex_t) * CRYPTO_num_locks());
-            for (i = 0; i < CRYPTO_num_locks(); i++)
-                pthread_mutex_init(&openssl_mutexes[i], NULL);
-            CRYPTO_set_locking_callback(openssl_lock);
-#if !defined(WIN32) && OPENSSL_VERSION_NUMBER < 0x10000000
-            CRYPTO_set_id_callback(openssl_thread_id);
+#if CONFIG_TLS_OPENSSL_PROTOCOL
+    ff_openssl_init();
 #endif
-        }
+#if CONFIG_TLS_GNUTLS_PROTOCOL
+    ff_gnutls_init();
 #endif
-    }
-    openssl_init++;
-#endif
-#if CONFIG_GNUTLS
-#if HAVE_THREADS && GNUTLS_VERSION_NUMBER < 0x020b00
-    if (gcry_control(GCRYCTL_ANY_INITIALIZATION_P) == 0)
-        gcry_control(GCRYCTL_SET_THREAD_CBS, &gcry_threads_pthread);
-#endif
-    gnutls_global_init();
-#endif
-    avpriv_unlock_avformat();
 }
 
 void ff_tls_deinit(void)
 {
-    avpriv_lock_avformat();
-#if CONFIG_OPENSSL
-    openssl_init--;
-    if (!openssl_init) {
-#if HAVE_THREADS
-        if (CRYPTO_get_locking_callback() == openssl_lock) {
-            int i;
-            CRYPTO_set_locking_callback(NULL);
-            for (i = 0; i < CRYPTO_num_locks(); i++)
-                pthread_mutex_destroy(&openssl_mutexes[i]);
-            av_free(openssl_mutexes);
-        }
+#if CONFIG_TLS_OPENSSL_PROTOCOL
+    ff_openssl_deinit();
 #endif
-    }
+#if CONFIG_TLS_GNUTLS_PROTOCOL
+    ff_gnutls_deinit();
 #endif
-#if CONFIG_GNUTLS
-    gnutls_global_deinit();
-#endif
-    avpriv_unlock_avformat();
 }
 
 int ff_network_inited_globally;

libavformat/tls.h
@@ -51,4 +51,10 @@ typedef struct TLSShared {
 
 int ff_tls_open_underlying(TLSShared *c, URLContext *parent, const char *uri, AVDictionary **options);
 
+void ff_gnutls_init(void);
+void ff_gnutls_deinit(void);
+
+void ff_openssl_init(void);
+void ff_openssl_deinit(void);
+
 #endif /* AVFORMAT_TLS_H */

libavformat/tls_openssl.c
@@ -25,13 +25,18 @@
 #include "os_support.h"
 #include "url.h"
 #include "tls.h"
+#include "libavcodec/internal.h"
 #include "libavutil/avstring.h"
+#include "libavutil/avutil.h"
 #include "libavutil/opt.h"
 #include "libavutil/parseutils.h"
+#include "libavutil/thread.h"
 
 #include <openssl/bio.h>
 #include <openssl/ssl.h>
 #include <openssl/err.h>
+
+static int openssl_init;
 
 typedef struct TLSContext {
     const AVClass *class;
@@ -39,6 +44,65 @@ typedef struct TLSContext {
     SSL_CTX *ctx;
     SSL *ssl;
 } TLSContext;
+
+#if HAVE_THREADS
+#include <openssl/crypto.h>
+pthread_mutex_t *openssl_mutexes;
+static void openssl_lock(int mode, int type, const char *file, int line)
+{
+    if (mode & CRYPTO_LOCK)
+        pthread_mutex_lock(&openssl_mutexes[type]);
+    else
+        pthread_mutex_unlock(&openssl_mutexes[type]);
+}
+#if !defined(WIN32) && OPENSSL_VERSION_NUMBER < 0x10000000
+static unsigned long openssl_thread_id(void)
+{
+    return (intptr_t) pthread_self();
+}
+#endif
+#endif
+
+void ff_openssl_init(void)
+{
+    avpriv_lock_avformat();
+    if (!openssl_init) {
+        SSL_library_init();
+        SSL_load_error_strings();
+#if HAVE_THREADS
+        if (!CRYPTO_get_locking_callback()) {
+            int i;
+            openssl_mutexes = av_malloc(sizeof(pthread_mutex_t) * CRYPTO_num_locks());
+            for (i = 0; i < CRYPTO_num_locks(); i++)
+                pthread_mutex_init(&openssl_mutexes[i], NULL);
+            CRYPTO_set_locking_callback(openssl_lock);
+#if !defined(WIN32) && OPENSSL_VERSION_NUMBER < 0x10000000
+            CRYPTO_set_id_callback(openssl_thread_id);
+#endif
+        }
+#endif
+    }
+    openssl_init++;
+    avpriv_unlock_avformat();
+}
+
+void ff_openssl_deinit(void)
+{
+    avpriv_lock_avformat();
+    openssl_init--;
+    if (!openssl_init) {
+#if HAVE_THREADS
+        if (CRYPTO_get_locking_callback() == openssl_lock) {
+            int i;
+            CRYPTO_set_locking_callback(NULL);
+            for (i = 0; i < CRYPTO_num_locks(); i++)
+                pthread_mutex_destroy(&openssl_mutexes[i]);
+            av_free(openssl_mutexes);
+        }
+#endif
+    }
+    avpriv_unlock_avformat();
+}
 
 static int print_tls_error(URLContext *h, int ret)
 {
@@ -57,7 +121,7 @@ static int tls_close(URLContext *h)
         SSL_CTX_free(c->ctx);
     if (c->tls_shared.tcp)
         ffurl_close(c->tls_shared.tcp);
-    ff_tls_deinit();
+    ff_openssl_deinit();
     return 0;
 }
 
@@ -131,7 +195,7 @@ static int tls_open(URLContext *h, const char *uri, int flags, AVDictionary **op
     BIO *bio;
     int ret;
 
-    ff_tls_init();
+    ff_openssl_init();
 
     if ((ret = ff_tls_open_underlying(c, h, uri, options)) < 0)
         goto fail;

commit d4d90504a687d2c0ef77ccf11d831f24dcff9cf1

tls_gnutls: Add missing includes for the gcrypt thread safety callbacks
This fixes building with gcrypt-backed gnutls versions, broken
in 57cde2b180.

Signed-off-by: Martin Storsjö <martin@martin.st>

libavformat/tls_gnutls.c
@@ -37,6 +37,7 @@
 
 #if HAVE_THREADS && GNUTLS_VERSION_NUMBER <= 0x020b00
 #include <gcrypt.h>
+#include "libavutil/thread.h"
 GCRY_THREAD_OPTION_PTHREAD_IMPL;
 #endif
 