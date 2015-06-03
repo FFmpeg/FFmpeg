3dc303a05192b2a7863b4ce4df10646b501ebe4e
Ganesh Ajjanagadde <gajjanagadde@gmail.com>
2015-06-02 23:17:48 -0400

    swresample/dither: check memory allocation
    check memory allocation in swri_get_dither()
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>
    (cherry picked from commit 196b885a5f0aa3ca022c1fa99509f47341239784)
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libswresample/swresample_internal.h
+++ libswresample/swresample_internal.h
@@ -191,7 +191,7 @@ void swri_rematrix_free(SwrContext *s);
 int swri_rematrix(SwrContext *s, AudioData *out, AudioData *in, int len, int mustcopy);
 void swri_rematrix_init_x86(struct SwrContext *s);
 
-void swri_get_dither(SwrContext *s, void *dst, int len, unsigned seed, enum AVSampleFormat noise_fmt);
+int swri_get_dither(SwrContext *s, void *dst, int len, unsigned seed, enum AVSampleFormat noise_fmt);
 int swri_dither_init(SwrContext *s, enum AVSampleFormat out_fmt, enum AVSampleFormat in_fmt);
 
 void swri_audio_convert_init_aarch64(struct AudioConvert *ac,
