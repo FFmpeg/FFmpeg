8606e881b02bec2ac24943d22c8afe11d641fac8
Luca Barbato <lu_zero@gentoo.org>
2015-06-10 14:29:08 +0100

    mpeg12: Move the vlc bits to a stand alone file

--- libavcodec/mpeg12.h
+++ libavcodec/mpeg12.h
@@ -22,29 +22,12 @@
 #ifndef AVCODEC_MPEG12_H
 #define AVCODEC_MPEG12_H
 
+#include "mpeg12vlc.h"
 #include "mpegvideo.h"
-
-#define DC_VLC_BITS 9
-#define MV_VLC_BITS 9
-#define TEX_VLC_BITS 9
-
-#define MBINCR_VLC_BITS 9
-#define MB_PAT_VLC_BITS 9
-#define MB_PTYPE_VLC_BITS 6
-#define MB_BTYPE_VLC_BITS 6
-
-extern VLC ff_dc_lum_vlc;
-extern VLC ff_dc_chroma_vlc;
-extern VLC ff_mbincr_vlc;
-extern VLC ff_mb_ptype_vlc;
-extern VLC ff_mb_btype_vlc;
-extern VLC ff_mb_pat_vlc;
-extern VLC ff_mv_vlc;
 
 extern uint8_t ff_mpeg12_static_rl_table_store[2][2][2*MAX_RUN + MAX_LEVEL + 3];
 
 void ff_mpeg12_common_init(MpegEncContext *s);
-void ff_mpeg12_init_vlcs(void);
 
 static inline int decode_dc(GetBitContext *gb, int component)
 {
