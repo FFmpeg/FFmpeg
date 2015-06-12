a9b2a51178ea446909015f061ab5df65e3b66bf6
Vittorio Giovara <vittorio.giovara@gmail.com>
2015-06-12 13:39:17 +0100

    avconv_opt: Check localtime() return value
    Reported-By: infer

--- avconv_opt.c
+++ avconv_opt.c
@@ -1899,6 +1899,11 @@ static int opt_vstats(void *optctx, const char *opt, const char *arg)
     time_t today2 = time(NULL);
     struct tm *today = localtime(&today2);
 
+    if (!today) { // maybe tomorrow
+        av_log(NULL, AV_LOG_FATAL, "Unable to get current time.\n");
+        exit_program(1);
+    }
+
     snprintf(filename, sizeof(filename), "vstats_%02d%02d%02d.log", today->tm_hour, today->tm_min,
              today->tm_sec);
     return opt_vstats_file(NULL, opt, filename);
