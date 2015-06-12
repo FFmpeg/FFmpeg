a75d22445ecb7adbe3fb8f705cb4fd9aa0d6b5ee
Michael Niedermayer <michaelni@gmx.at>
2015-06-13 01:08:16 +0200

    ffprobe: check av_asprintf() for failure
    Found-by: Daemon404
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- ffprobe.c
+++ ffprobe.c
@@ -2831,6 +2831,9 @@ static int opt_show_format_entry(void *optctx, const char *opt, const char *arg)
     char *buf = av_asprintf("format=%s", arg);
     int ret;
 
+    if (!buf)
+        return AVERROR(ENOMEM);
+
     av_log(NULL, AV_LOG_WARNING,
            "Option '%s' is deprecated, use '-show_entries format=%s' instead\n",
            opt, arg);
