22349b9f7d4ff813a08b50fa79113cf1e36f4936
Michael Niedermayer <michaelni@gmx.at>
2015-06-12 15:36:20 +0200

    ffmpeg_opt: Check for localtime() failure
    Found-by: Daemon404
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>
    (cherry picked from commit 8e91d9652ea5048d9014e7636e12c6ed4732d7b7)
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- ffmpeg_opt.c
+++ ffmpeg_opt.c
@@ -2321,6 +2321,9 @@ static int opt_vstats(void *optctx, const char *opt, const char *arg)
     time_t today2 = time(NULL);
     struct tm *today = localtime(&today2);
 
+    if (!today)
+        return AVERROR(errno);
+
     snprintf(filename, sizeof(filename), "vstats_%02d%02d%02d.log", today->tm_hour, today->tm_min,
              today->tm_sec);
     return opt_vstats_file(NULL, opt, filename);
