93d076b4fde12f11c67dcea86a27cc9770450fd4
wm4 <nfxjfg@googlemail.com>
2015-06-18 20:36:19 +0200

    avformat: clarify what package needs to be compiled with SSL support
    Try to reduce user confusion.
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>
    (cherry picked from commit f6c3f1ed6048ebc55f652ed59f7af9acebbf57e5)
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/avio.c
+++ libavformat/avio.c
@@ -263,7 +263,7 @@ int ffurl_alloc(URLContext **puc, const char *filename, int flags,
 
     *puc = NULL;
     if (av_strstart(filename, "https:", NULL))
-        av_log(NULL, AV_LOG_WARNING, "https protocol not found, recompile with "
+        av_log(NULL, AV_LOG_WARNING, "https protocol not found, recompile FFmpeg with "
                                      "openssl, gnutls,\n"
                                      "or securetransport enabled.\n");
     return AVERROR_PROTOCOL_NOT_FOUND;
