041aa800f22090e0b7ff2a79098d632c9e3a7e92
Michael Niedermayer <michaelni@gmx.at>
2015-06-24 02:46:05 +0200

    avformat/mp3dec: Remove the ID3v1 tag removial code
    The code is simply broken, the read packets are not aligned to
    the mp3 frames, the file end or the id3 tag thus this simply
    cannot reliably find the ID3v1 tag to remove it
    
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/mp3dec.c
+++ libavformat/mp3dec.c
@@ -415,13 +415,6 @@ static int mp3_read_packet(AVFormatContext *s, AVPacket *pkt)
     pkt->flags &= ~AV_PKT_FLAG_CORRUPT;
     pkt->stream_index = 0;
 
-    if (ret >= ID3v1_TAG_SIZE &&
-        memcmp(&pkt->data[ret - ID3v1_TAG_SIZE], "TAG", 3) == 0)
-        ret -= ID3v1_TAG_SIZE;
-
-    /* note: we need to modify the packet size here to handle the last
-       packet */
-    pkt->size = ret;
     return ret;
 }
 
