6f4cd33efb5a9ec75db1677d5f7846c60337129f
Luca Barbato <lu_zero@gentoo.org>
2015-06-26 15:57:15 +0200

    cosmetic: Reformat ff_h263_decode_mba

--- libavcodec/ituh263dec.c
+++ libavcodec/ituh263dec.c
@@ -139,12 +139,12 @@ int ff_h263_decode_mba(MpegEncContext *s)
 {
     int i, mb_pos;
 
-    for(i=0; i<6; i++){
-        if(s->mb_num-1 <= ff_mba_max[i]) break;
-    }
-    mb_pos= get_bits(&s->gb, ff_mba_length[i]);
-    s->mb_x= mb_pos % s->mb_width;
-    s->mb_y= mb_pos / s->mb_width;
+    for (i = 0; i < 6; i++)
+        if (s->mb_num - 1 <= ff_mba_max[i])
+            break;
+    mb_pos  = get_bits(&s->gb, ff_mba_length[i]);
+    s->mb_x = mb_pos % s->mb_width;
+    s->mb_y = mb_pos / s->mb_width;
 
     return mb_pos;
 }
