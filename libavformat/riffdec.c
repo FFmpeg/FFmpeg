1b4c468477f3b8d372da8ef4e5405539ad9c1501
Luca Barbato <lu_zero@gentoo.org>
2015-06-28 17:32:40 +0200

    riff: Validate the wav header size before trying to parse it

--- libavformat/riffdec.c
+++ libavformat/riffdec.c
@@ -79,6 +79,9 @@ int ff_get_wav_header(AVIOContext *pb, AVCodecContext *codec, int size)
 {
     int id;
 
+    if (size < 14)
+        return AVERROR_INVALIDDATA;
+
     id                 = avio_rl16(pb);
     codec->codec_type  = AVMEDIA_TYPE_AUDIO;
     codec->channels    = avio_rl16(pb);
