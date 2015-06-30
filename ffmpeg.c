70b97a89d2d7ba837c830b1da5924314d68fc3e7
Andreas Cadhalpun <Andreas.Cadhalpun@googlemail.com>
2015-06-30 21:09:50 +0200

    ffmpeg: only count got_output/errors in decode_error_stat
    If threading is used, the first (thread_count - 1) packets are read
    before any frame/error is returned. Counting this as successful decoding
    is wrong, because it also happens when no single frame could be decoded.
    
    Reviewed-by: Michael Niedermayer <michaelni@gmx.at>
    Signed-off-by: Andreas Cadhalpun <Andreas.Cadhalpun@googlemail.com>
    (cherry picked from commit bd0f14123fd8c7ff1c27b726d143f84c67f3a522)
    Signed-off-by: Andreas Cadhalpun <Andreas.Cadhalpun@googlemail.com>

--- ffmpeg.c
+++ ffmpeg.c
@@ -1874,7 +1874,7 @@ static int decode_audio(InputStream *ist, AVPacket *pkt, int *got_output)
         ret = AVERROR_INVALIDDATA;
     }
 
-    if (*got_output || ret<0 || pkt->size)
+    if (*got_output || ret<0)
         decode_error_stat[ret<0] ++;
 
     if (ret < 0 && exit_on_error)
@@ -2022,7 +2022,7 @@ static int decode_video(InputStream *ist, AVPacket *pkt, int *got_output)
             );
     }
 
-    if (*got_output || ret<0 || pkt->size)
+    if (*got_output || ret<0)
         decode_error_stat[ret<0] ++;
 
     if (ret < 0 && exit_on_error)
@@ -2143,7 +2143,7 @@ static int transcode_subtitles(InputStream *ist, AVPacket *pkt, int *got_output)
     int i, ret = avcodec_decode_subtitle2(ist->dec_ctx,
                                           &subtitle, got_output, pkt);
 
-    if (*got_output || ret<0 || pkt->size)
+    if (*got_output || ret<0)
         decode_error_stat[ret<0] ++;
 
     if (ret < 0 && exit_on_error)
