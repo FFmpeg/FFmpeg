d09b4cce21cdad5ef2855698395ffd6e37445212
Luca Barbato <lu_zero@gentoo.org>
2015-07-04 00:46:44 +0200

    mpjpeg: Simplify using avio_printf

--- libavformat/mpjpeg.c
+++ libavformat/mpjpeg.c
@@ -26,24 +26,17 @@
 
 static int mpjpeg_write_header(AVFormatContext *s)
 {
-    uint8_t buf1[256];
-
-    snprintf(buf1, sizeof(buf1), "--%s\n", BOUNDARY_TAG);
-    avio_write(s->pb, buf1, strlen(buf1));
+    avio_printf(s->pb, "--%s\n", BOUNDARY_TAG);
     avio_flush(s->pb);
     return 0;
 }
 
 static int mpjpeg_write_packet(AVFormatContext *s, AVPacket *pkt)
 {
-    uint8_t buf1[256];
-
-    snprintf(buf1, sizeof(buf1), "Content-type: image/jpeg\n\n");
-    avio_write(s->pb, buf1, strlen(buf1));
+    avio_printf(s->pb, "Content-type: image/jpeg\n\n");
     avio_write(s->pb, pkt->data, pkt->size);
 
-    snprintf(buf1, sizeof(buf1), "\n--%s\n", BOUNDARY_TAG);
-    avio_write(s->pb, buf1, strlen(buf1));
+    avio_printf(s->pb, "\n--%s\n", BOUNDARY_TAG);
     return 0;
 }
 
