3bb58c377b4544b6025a2314209bc5efd6bda061
Ronald S. Bultje <rsbultje@gmail.com>
2015-07-11 11:21:17 -0400

    vf_psnr: fix rgb channel order mixup in final log message.
    Reviewed-by: Paul B Mahol <onemda@gmail.com>
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavfilter/vf_psnr.c
+++ libavfilter/vf_psnr.c
@@ -327,9 +327,11 @@ static av_cold void uninit(AVFilterContext *ctx)
         char buf[256];
 
         buf[0] = 0;
-        for (j = 0; j < s->nb_components; j++)
+        for (j = 0; j < s->nb_components; j++) {
+            int c = s->is_rgb ? s->rgba_map[j] : j;
             av_strlcatf(buf, sizeof(buf), " %c:%0.2f", s->comps[j],
-                        get_psnr(s->mse_comp[j], s->nb_frames, s->max[j]));
+                        get_psnr(s->mse_comp[c], s->nb_frames, s->max[c]));
+        }
         av_log(ctx, AV_LOG_INFO, "PSNR%s average:%0.2f min:%0.2f max:%0.2f\n",
                buf,
                get_psnr(s->mse, s->nb_frames, s->average_max),
