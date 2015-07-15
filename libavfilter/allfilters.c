a8fbb7b62597d346161b02c822bd851274d83527
Paul B Mahol <onemda@gmail.com>
2015-07-15 01:48:37 +0000

    avfilter: add random video filter
    Signed-off-by: Paul B Mahol <onemda@gmail.com>

--- libavfilter/allfilters.c
+++ libavfilter/allfilters.c
@@ -198,6 +198,7 @@ void avfilter_register_all(void)
     REGISTER_FILTER(PSNR,           psnr,           vf);
     REGISTER_FILTER(PULLUP,         pullup,         vf);
     REGISTER_FILTER(QP,             qp,             vf);
+    REGISTER_FILTER(RANDOM,         random,         vf);
     REGISTER_FILTER(REMOVEGRAIN,    removegrain,    vf);
     REGISTER_FILTER(REMOVELOGO,     removelogo,     vf);
     REGISTER_FILTER(REPEATFIELDS,   repeatfields,   vf);
