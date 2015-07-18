8fe79605fdcec35f2b277126f5de2d0338468a90
Michael Niedermayer <michael@niedermayer.cc>
2015-07-18 19:02:26 +0200

    avcodec/vp8: Fix null pointer dereference in ff_vp8_decode_free()
    Fixes: signal_sigsegv_d5de23_967_vp80_00_comprehensive_010.ivf with memlimit 524288
    
    Found-by: Samuel Groß, Mateusz "j00ru" Jurczyk and Gynvael Coldwind
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit a84f0e8d8f293df3c535f9b893730a835bed6520)
    
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/vp8.c
+++ libavcodec/vp8.c
@@ -2675,6 +2675,9 @@ av_cold int ff_vp8_decode_free(AVCodecContext *avctx)
     VP8Context *s = avctx->priv_data;
     int i;
 
+    if (!s)
+        return 0;
+
     vp8_decode_flush_impl(avctx, 1);
     for (i = 0; i < FF_ARRAY_ELEMS(s->frames); i++)
         av_frame_free(&s->frames[i].tf.f);
