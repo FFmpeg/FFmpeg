e3851169eedce6c90534ca41edf3e05e2576453e
James Almer <jamrial@gmail.com>
2015-07-20 00:50:52 -0300

    x86/vf_ssim: add ff_ssim_4x4_line_xop
    ~20% faster than ssse3. Also enabled for x86_32
    
    Reviewed-by: Ronald S. Bultje <rsbultje@gmail.com>
    Signed-off-by: James Almer <jamrial@gmail.com>

--- libavfilter/x86/vf_ssim_init.c
+++ libavfilter/x86/vf_ssim_init.c
@@ -25,6 +25,9 @@
 void ff_ssim_4x4_line_ssse3(const uint8_t *buf, ptrdiff_t buf_stride,
                             const uint8_t *ref, ptrdiff_t ref_stride,
                             int (*sums)[4], int w);
+void ff_ssim_4x4_line_xop  (const uint8_t *buf, ptrdiff_t buf_stride,
+                            const uint8_t *ref, ptrdiff_t ref_stride,
+                            int (*sums)[4], int w);
 float ff_ssim_end_line_sse4(const int (*sum0)[4], const int (*sum1)[4], int w);
 
 void ff_ssim_init_x86(SSIMDSPContext *dsp)
@@ -35,4 +38,6 @@ void ff_ssim_init_x86(SSIMDSPContext *dsp)
         dsp->ssim_4x4_line = ff_ssim_4x4_line_ssse3;
     if (EXTERNAL_SSE4(cpu_flags))
         dsp->ssim_end_line = ff_ssim_end_line_sse4;
+    if (EXTERNAL_XOP(cpu_flags))
+        dsp->ssim_4x4_line = ff_ssim_4x4_line_xop;
 }
