4ab56667594842283dc5ae07f0daba2a2cb4d3af
Rodger Combs <rodger.combs@gmail.com>
2015-07-20 15:00:35 -0500

    lavf/mov: add support for sidx fragment indexes
    Fixes trac #3842

--- libavformat/isom.h
+++ libavformat/isom.h
@@ -103,6 +103,7 @@ typedef struct MOVSbgp {
 typedef struct MOVFragmentIndexItem {
     int64_t moof_offset;
     int64_t time;
+    int headers_read;
 } MOVFragmentIndexItem;
 
 typedef struct MOVFragmentIndex {
@@ -197,6 +198,7 @@ typedef struct MOVContext {
     int has_looked_for_mfra;
     MOVFragmentIndex** fragment_index_data;
     unsigned fragment_index_count;
+    int fragment_index_complete;
     int atom_depth;
     unsigned int aax_mode;  ///< 'aax' file has been detected
     uint8_t file_key[20];
