625bf6a55c3dd25613ba328f93d0fd23b5bb7c0f
Paul B Mahol <onemda@gmail.com>
2015-07-21 14:40:00 +0000

    avfilter/vf_reverse: check also pts_size when reallocating
    Fixes crash of x32.
    
    Signed-off-by: Paul B Mahol <onemda@gmail.com>

--- libavfilter/vf_reverse.c
+++ libavfilter/vf_reverse.c
@@ -72,15 +72,16 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *in)
 {
     AVFilterContext *ctx = inlink->dst;
     ReverseContext *s    = ctx->priv;
+    void *ptr;
 
-    if (s->nb_frames + 1 > s->frames_size / sizeof(*(s->frames))) {
-        void *ptr;
-
+    if (s->nb_frames + 1 > s->pts_size / sizeof(*(s->pts))) {
         ptr = av_fast_realloc(s->pts, &s->pts_size, s->pts_size * 2);
         if (!ptr)
             return AVERROR(ENOMEM);
         s->pts = ptr;
+    }
 
+    if (s->nb_frames + 1 > s->frames_size / sizeof(*(s->frames))) {
         ptr = av_fast_realloc(s->frames, &s->frames_size, s->frames_size * 2);
         if (!ptr)
             return AVERROR(ENOMEM);
