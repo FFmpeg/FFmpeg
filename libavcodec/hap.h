FFmpeg/FFmpeg

commit 977105407cae55876041dddbf4ce0934cdd4cd6c

hap: Decode using optimal slices sizes
Enjoy some cache locality and use less threads.
About 5x speedup (from 60ms to 12ms to decode a 4k frame).

Signed-off-by: Luca Barbato <lu_zero@gentoo.org>

libavcodec/hap.h
@@ -46,6 +46,8 @@ typedef struct HapContext {
     uint8_t *snappied;       /* Buffer interacting with snappy */
     size_t max_snappy;       /* Maximum compressed size for snappy buffer */
 
+    int slice_size;          /* Optimal slice size */
+
     /* Pointer to the selected compress or decompress function */
     int (*tex_fun)(uint8_t *dst, ptrdiff_t stride, const uint8_t *block);
 } HapContext;

libavcodec/hapdec.c
@@ -137,16 +137,30 @@ static int setup_texture(AVCodecContext *avctx, size_t length)
 }
 
 static int decompress_texture_thread(AVCodecContext *avctx, void *arg,
-                                     int block_nb, int thread_nb)
+                                     int slice, int thread_nb)
 {
     HapContext *ctx = avctx->priv_data;
     AVFrame *frame = arg;
-    int x = (TEXTURE_BLOCK_W * block_nb) % avctx->coded_width;
-    int y = TEXTURE_BLOCK_H * (TEXTURE_BLOCK_W * block_nb / avctx->coded_width);
-    uint8_t *p = frame->data[0] + x * 4 + y * frame->linesize[0];
-    const uint8_t *d = ctx->tex_data + block_nb * ctx->tex_rat;
+    const uint8_t *d = ctx->tex_data;
+    int w_block = avctx->coded_width / TEXTURE_BLOCK_W;
+    int x, y;
+    int start_slice, end_slice;
 
-    ctx->tex_fun(p, frame->linesize[0], d);
+    start_slice = slice * ctx->slice_size;
+    end_slice   = FFMIN(start_slice + ctx->slice_size, avctx->coded_height);
+
+    start_slice /= TEXTURE_BLOCK_H;
+    end_slice   /= TEXTURE_BLOCK_H;
+
+    for (y = start_slice; y < end_slice; y++) {
+        uint8_t *p = frame->data[0] + y * frame->linesize[0] * TEXTURE_BLOCK_H;
+        int off  = y * w_block;
+        for (x = 0; x < w_block; x++) {
+            ctx->tex_fun(p + x * 16, frame->linesize[0],
+                         d + (off + x) * ctx->tex_rat);
+        }
+    }
+
     return 0;
 }
 
@@ -156,7 +170,10 @@ static int hap_decode(AVCodecContext *avctx, void *data,
     HapContext *ctx = avctx->priv_data;
     ThreadFrame tframe;
     int ret, length;
-    int blocks = avctx->coded_width * avctx->coded_height / (TEXTURE_BLOCK_W * TEXTURE_BLOCK_H);
+    int slices = FFMIN(avctx->thread_count,
+                       avctx->coded_height / TEXTURE_BLOCK_H);
+
+    ctx->slice_size = avctx->coded_height / slices;
 
     bytestream2_init(&ctx->gbc, avpkt->data, avpkt->size);
 
@@ -180,7 +197,7 @@ static int hap_decode(AVCodecContext *avctx, void *data,
     ff_thread_finish_setup(avctx);
 
     /* Use the decompress function on the texture, one block per thread */
-    avctx->execute2(avctx, decompress_texture_thread, tframe.f, NULL, blocks);
+    avctx->execute2(avctx, decompress_texture_thread, tframe.f, NULL, slices);
 
     /* Frame is ready to be output */
     tframe.f->pict_type = AV_PICTURE_TYPE_I;

commit ae5a8dca675ee544178225256893e679b750cb63

hap: Fix slice size computation
A bug was introduced in 977105407cae55876041dddbf4ce0934cdd4cd6c whereby when
frame height wasn't divisible by the number of threads, pixels would be omitted
from the bottom rows during decode.

Signed-off-by: Luca Barbato <lu_zero@gentoo.org>

libavcodec/hap.h
@@ -46,7 +46,7 @@ typedef struct HapContext {
     uint8_t *snappied;       /* Buffer interacting with snappy */
     size_t max_snappy;       /* Maximum compressed size for snappy buffer */
 
-    int slice_size;          /* Optimal slice size */
+    int slice_count;         /* Number of slices for threaded operations */
 
     /* Pointer to the selected compress or decompress function */
     int (*tex_fun)(uint8_t *dst, ptrdiff_t stride, const uint8_t *block);

libavcodec/hapdec.c
@@ -143,14 +143,23 @@ static int decompress_texture_thread(AVCodecContext *avctx, void *arg,
     AVFrame *frame = arg;
     const uint8_t *d = ctx->tex_data;
     int w_block = avctx->coded_width / TEXTURE_BLOCK_W;
+    int h_block = avctx->coded_height / TEXTURE_BLOCK_H;
     int x, y;
     int start_slice, end_slice;
+    int base_blocks_per_slice = h_block / ctx->slice_count;
+    int remainder_blocks = h_block % ctx->slice_count;
 
-    start_slice = slice * ctx->slice_size;
-    end_slice   = FFMIN(start_slice + ctx->slice_size, avctx->coded_height);
+    /* When the frame height (in blocks) doesn't divide evenly between the
+     * number of slices, spread the remaining blocks evenly between the first
+     * operations */
+    start_slice = slice * base_blocks_per_slice;
+    /* Add any extra blocks (one per slice) that have been added before this slice */
+    start_slice += FFMIN(slice, remainder_blocks);
 
-    start_slice /= TEXTURE_BLOCK_H;
-    end_slice   /= TEXTURE_BLOCK_H;
+    end_slice = start_slice + base_blocks_per_slice;
+    /* Add an extra block if there are still remainder blocks to be accounted for */
+    if (slice < remainder_blocks)
+        end_slice++;
 
     for (y = start_slice; y < end_slice; y++) {
         uint8_t *p = frame->data[0] + y * frame->linesize[0] * TEXTURE_BLOCK_H;
@@ -170,10 +179,6 @@ static int hap_decode(AVCodecContext *avctx, void *data,
     HapContext *ctx = avctx->priv_data;
     ThreadFrame tframe;
     int ret, length;
-    int slices = FFMIN(avctx->thread_count,
-                       avctx->coded_height / TEXTURE_BLOCK_H);
-
-    ctx->slice_size = avctx->coded_height / slices;
 
     bytestream2_init(&ctx->gbc, avpkt->data, avpkt->size);
 
@@ -197,7 +202,7 @@ static int hap_decode(AVCodecContext *avctx, void *data,
     ff_thread_finish_setup(avctx);
 
     /* Use the decompress function on the texture, one block per thread */
-    avctx->execute2(avctx, decompress_texture_thread, tframe.f, NULL, slices);
+    avctx->execute2(avctx, decompress_texture_thread, tframe.f, NULL, ctx->slice_count);
 
     /* Frame is ready to be output */
     tframe.f->pict_type = AV_PICTURE_TYPE_I;
@@ -226,6 +231,9 @@ static av_cold int hap_init(AVCodecContext *avctx)
     avctx->pix_fmt = AV_PIX_FMT_RGBA;
 
     ff_texturedsp_init(&ctx->dxtc);
+
+    ctx->slice_count = av_clip(avctx->thread_count, 1,
+                               avctx->coded_height / TEXTURE_BLOCK_H);
 
     return 0;
 }