5b69fb783e9da81d9f4fec8abb05f02041fd7a7e
Paul B Mahol <onemda@gmail.com>
2015-07-29 19:04:28 +0200

    avfilter: add atadenoise

--- libavfilter/allfilters.c
+++ libavfilter/allfilters.c
@@ -113,6 +113,7 @@ void avfilter_register_all(void)
 
     REGISTER_FILTER(ALPHAEXTRACT,   alphaextract,   vf);
     REGISTER_FILTER(ALPHAMERGE,     alphamerge,     vf);
+    REGISTER_FILTER(ATADENOISE,     atadenoise,     vf);
     REGISTER_FILTER(ASS,            ass,            vf);
     REGISTER_FILTER(BBOX,           bbox,           vf);
     REGISTER_FILTER(BLACKDETECT,    blackdetect,    vf);
