cc167f7e55dafdeeca7ac9622331db8d8f6cb463
Ivan Uskov <ivan.uskov@nablet.com>
2015-08-04 06:40:06 -0400

    libavcodec/qsvdec.c: correct handling of dynamic frame size changing has been implemented
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/qsvdec.h
+++ libavcodec/qsvdec.h
@@ -51,9 +51,20 @@ typedef struct QSVContext {
     AVFifoBuffer *async_fifo;
     AVFifoBuffer *input_fifo;
 
+    // we should to buffer input packets at some cases
+    // else it is not possible to handle dynamic stream changes correctly
+    // this fifo uses for input packets buffering
+    AVFifoBuffer *pkt_fifo;
+
     // this flag indicates that header parsed,
     // decoder instance created and ready to general decoding
     int engine_ready;
+
+    // we can not just re-init decoder if different sequence header arrived
+    // we should to deliver all buffered frames but we can not decode new packets
+    // this time. So when reinit_pending is non-zero we flushing decoder and
+    // accumulate new arrived packets into pkt_fifo
+    int reinit_pending;
 
     // options set by the caller
     int async_depth;
