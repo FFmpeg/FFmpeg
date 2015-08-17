0f091f808af7d536dc3edd99e196801a871f49d6
Michael Niedermayer <michael@niedermayer.cc>
2015-08-17 03:08:10 +0200

    swscale/swscale-test: Fix slice height in random reference data creation.
    Found-by: Pedro Arthur <bygrandao@gmail.com>
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libswscale/swscale-test.c
+++ libswscale/swscale-test.c
@@ -399,7 +399,7 @@ bad_option:
     for (y = 0; y < H; y++)
         for (x = 0; x < W * 4; x++)
             rgb_data[ x + y * 4 * W] = av_lfg_get(&rand);
-    sws_scale(sws, rgb_src, rgb_stride, 0, H, src, stride);
+    sws_scale(sws, rgb_src, rgb_stride, 0, H / 12, src, stride);
     sws_freeContext(sws);
     av_free(rgb_data);
 
