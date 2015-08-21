FFmpeg/FFmpeg

commit f9ab4fe1f7c1e9d410ca5ee2c9ff8d2892aad068

h264: Discard currently unsupported registered sei
Signed-off-by: Luca Barbato <lu_zero@gentoo.org>

libavcodec/h264_sei.c
@@ -169,6 +169,12 @@ static int decode_registered_user_data_closed_caption(H264Context *h, int size)
                 skip_bits(&h->gb, 8);   // marker_bits
             }
         }
+    } else {
+        int i;
+        avpriv_request_sample("Subtitles with data type 0x%02x",
+                              user_data_type_code);
+        for (i = 0; i < size - 1; i++)
+            skip_bits(&h->gb, 8);
     }
 
     return 0;

commit 61d8fa2a1ab76f0f3ac1442faa104ace4b29decc

h264: Fix faulty call to avpriv_request_sample
Broken in f9ab4fe1f7c1e9d410ca5ee2c9ff8d2892aad068

libavcodec/h264_sei.c
@@ -171,7 +171,7 @@ static int decode_registered_user_data_closed_caption(H264Context *h, int size)
         }
     } else {
         int i;
-        avpriv_request_sample("Subtitles with data type 0x%02x",
+        avpriv_request_sample(h->avctx, "Subtitles with data type 0x%02x",
                               user_data_type_code);
         for (i = 0; i < size - 1; i++)
             skip_bits(&h->gb, 8);