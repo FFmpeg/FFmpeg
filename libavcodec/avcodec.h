03e71cccba288e8f3c8ad57ed77b0b267caf8dd1
Andreas Cadhalpun <Andreas.Cadhalpun@googlemail.com>
2015-08-21 23:58:56 +0200

    avcodec: remove FF_API_LOWRES around max_lowres
    Reviewed-by: wm4 <nfxjfg@googlemail.com>
    Reviewed-by: Michael Niedermayer <michael@niedermayer.cc>
    Signed-off-by: Andreas Cadhalpun <Andreas.Cadhalpun@googlemail.com>

--- libavcodec/avcodec.h
+++ libavcodec/avcodec.h
@@ -3493,9 +3493,7 @@ typedef struct AVCodec {
     const int *supported_samplerates;       ///< array of supported audio samplerates, or NULL if unknown, array is terminated by 0
     const enum AVSampleFormat *sample_fmts; ///< array of supported sample formats, or NULL if unknown, array is terminated by -1
     const uint64_t *channel_layouts;         ///< array of support channel layouts, or NULL if unknown. array is terminated by 0
-#if FF_API_LOWRES
     uint8_t max_lowres;                     ///< maximum value for lowres supported by the decoder, no direct access, use av_codec_get_max_lowres()
-#endif
     const AVClass *priv_class;              ///< AVClass for the private context
     const AVProfile *profiles;              ///< array of recognized profiles, or NULL if unknown, array is terminated by {FF_PROFILE_UNKNOWN}
 
