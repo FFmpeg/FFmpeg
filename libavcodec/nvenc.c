413d4e54a9bffe2d0afdc6d8a80f516e5df6a421
Luca Barbato <lu_zero@gentoo.org>
2015-08-24 01:29:59 +0200

    nvenc: Properly free the fifos
    Signed-off-by: Luca Barbato <lu_zero@gentoo.org>

--- libavcodec/nvenc.c
+++ libavcodec/nvenc.c
@@ -771,6 +771,10 @@ av_cold int ff_nvenc_encode_close(AVCodecContext *avctx)
     NV_ENCODE_API_FUNCTION_LIST *nv = &ctx->nvel.nvenc_funcs;
     int i;
 
+    av_fifo_free(ctx->timestamps);
+    av_fifo_free(ctx->pending);
+    av_fifo_free(ctx->ready);
+
     if (ctx->in) {
         for (i = 0; i < ctx->nb_surfaces; ++i) {
             nv->nvEncDestroyInputBuffer(ctx->nvenc_ctx, ctx->in[i].in);
