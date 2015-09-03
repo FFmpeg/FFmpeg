fddcd9e5702ebd2166d8ac73c4b6df32beda23e8
Michael Niedermayer <michael@niedermayer.cc>
2015-09-03 12:40:12 +0200

    avformat/file: Fix copy and paste error
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavformat/file.c
+++ libavformat/file.c
@@ -180,7 +180,7 @@ static int file_move(URLContext *h_src, URLContext *h_dst)
     const char *filename_src = h_src->filename;
     const char *filename_dst = h_dst->filename;
     av_strstart(filename_src, "file:", &filename_src);
-    av_strstart(filename_dst, "file:", &filename_src);
+    av_strstart(filename_dst, "file:", &filename_dst);
 
     if (rename(filename_src, filename_dst) < 0)
         return AVERROR(errno);
