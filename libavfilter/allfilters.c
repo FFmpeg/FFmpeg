39c61d84594780fc3f329806ab864a3734c84527
Paul B Mahol <onemda@gmail.com>
2015-09-05 19:12:58 +0000

    avfilter: add audio limiter filter
    Signed-off-by: Paul B Mahol <onemda@gmail.com>

--- libavfilter/allfilters.c
+++ libavfilter/allfilters.c
@@ -52,6 +52,7 @@ void avfilter_register_all(void)
     REGISTER_FILTER(AFADE,          afade,          af);
     REGISTER_FILTER(AFORMAT,        aformat,        af);
     REGISTER_FILTER(AINTERLEAVE,    ainterleave,    af);
+    REGISTER_FILTER(ALIMITER,       alimiter,       af);
     REGISTER_FILTER(ALLPASS,        allpass,        af);
     REGISTER_FILTER(AMERGE,         amerge,         af);
     REGISTER_FILTER(AMIX,           amix,           af);
