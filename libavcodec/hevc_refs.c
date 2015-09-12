9e1b4939160c2a41ea41cc8b817991ef69221bc6
Hendrik Leppkes <h.leppkes@gmail.com>
2015-09-12 21:50:24 +0200

    hevc: properly handle no_rasl_output_flag when removing pictures from the DPB
    Fixes ticket #4185.
    
    Reviewed-By: Mickael Raulet <Mickael.Raulet@insa-rennes.fr>
    Signed-off-by: Hendrik Leppkes <h.leppkes@gmail.com>
    (cherry picked from commit 0118158efa8e45761f9f65a3bb74f33907bd2aec)

--- libavcodec/hevc_refs.c
+++ libavcodec/hevc_refs.c
@@ -163,7 +163,7 @@ int ff_hevc_output_frame(HEVCContext *s, AVFrame *out, int flush)
         int min_poc   = INT_MAX;
         int i, min_idx, ret;
 
-        if (s->sh.no_output_of_prior_pics_flag == 1) {
+        if (s->sh.no_output_of_prior_pics_flag == 1 && s->no_rasl_output_flag == 1) {
             for (i = 0; i < FF_ARRAY_ELEMS(s->DPB); i++) {
                 HEVCFrame *frame = &s->DPB[i];
                 if ((frame->flags & HEVC_FRAME_FLAG_OUTPUT) && frame->poc != s->poc &&
