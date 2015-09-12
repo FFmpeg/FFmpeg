3381d92671092779d40b2b7b55a595ac2fe5d474
Rostislav Pehlivanov <atomnuker@gmail.com>
2015-09-12 13:15:02 +0100

    aacenc_tns: readjust values for new TNS decision making
    Since TNS was fixed with the recent commits retweak the values
    so it's more frequently used.
    Still not enabled by default yet, though it's possible that it
    will be made enabled by default in the near future.
    
    Signed-off-by: Rostislav Pehlivanov <atomnuker@gmail.com>

--- libavcodec/aacenc_tns.h
+++ libavcodec/aacenc_tns.h
@@ -37,8 +37,8 @@
 #define TNS_Q_BITS_SHORT 3
 
 /* TNS will only be used if the LPC gain is within these margins */
-#define TNS_GAIN_THRESHOLD_LOW  1.395f
-#define TNS_GAIN_THRESHOLD_HIGH 11.19f
+#define TNS_GAIN_THRESHOLD_LOW  1.437f
+#define TNS_GAIN_THRESHOLD_HIGH 21.19f
 
 /* If the energy ratio between the low SFBs vs the high SFBs is not between
  * those two values, use 2 filters instead */
@@ -46,7 +46,7 @@
 #define TNS_E_RATIO_HIGH 1.23
 
 /* Do not use TNS if the psy band spread is below this value */
-#define TNS_SPREAD_THRESHOLD 37.081512f
+#define TNS_SPREAD_THRESHOLD 0.5f
 
 void ff_aac_encode_tns_info(AACEncContext *s, SingleChannelElement *sce);
 void ff_aac_apply_tns(AACEncContext *s, SingleChannelElement *sce);
