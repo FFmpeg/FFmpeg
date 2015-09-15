aaf4e703f6d9583e1a910191f39e738cf5595e24
Paul B Mahol <onemda@gmail.com>
2015-09-15 16:27:52 +0000

    avfilter/af_stereowiden: fix 2nd argument for av_calloc
    Signed-off-by: Paul B Mahol <onemda@gmail.com>

--- libavfilter/af_stereowiden.c
+++ libavfilter/af_stereowiden.c
@@ -73,7 +73,7 @@ static int config_input(AVFilterLink *inlink)
     StereoWidenContext *s = ctx->priv;
 
     s->length = 2 * s->delay * inlink->sample_rate / 1000;
-    s->buffer = av_calloc(s->length, sizeof(s->buffer));
+    s->buffer = av_calloc(s->length, sizeof(*s->buffer));
     if (!s->buffer)
         return AVERROR(ENOMEM);
     s->write = s->buffer;
