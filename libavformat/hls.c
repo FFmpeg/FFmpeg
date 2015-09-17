db9de9b954a7dd453e928d17d7521915afe86764
Ganesh Ajjanagadde <gajjanagadde@gmail.com>
2015-09-16 20:26:42 -0400

    avformat/hls: remove unused function
    Fixes -Wunused-function from
    http://fate.ffmpeg.org/report.cgi?time=20150820031140&slot=arm64-darwin-clang-apple-5.1
    
    Signed-off-by: Ganesh Ajjanagadde <gajjanagadde@gmail.com>

--- libavformat/hls.c
+++ libavformat/hls.c
@@ -495,19 +495,6 @@ static int ensure_playlist(HLSContext *c, struct playlist **pls, const char *url
     return 0;
 }
 
-static int open_in(HLSContext *c, AVIOContext **in, const char *url)
-{
-    AVDictionary *tmp = NULL;
-    int ret;
-
-    av_dict_copy(&tmp, c->avio_opts, 0);
-
-    ret = avio_open2(in, url, AVIO_FLAG_READ, c->interrupt_callback, &tmp);
-
-    av_dict_free(&tmp);
-    return ret;
-}
-
 static int url_connect(struct playlist *pls, AVDictionary *opts, AVDictionary *opts2)
 {
     AVDictionary *tmp = NULL;
