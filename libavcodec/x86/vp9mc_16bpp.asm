2f9ab159607fd088f8ced1e603da14d203fbfffe
James Almer <jamrial@gmail.com>
2015-09-17 22:49:58 -0300

    x86/vp9: add avx2 subpel MC SIMD for 10/12bpp
    Reviewed-by: Ronald S. Bultje <rsbultje@gmail.com>
    Signed-off-by: James Almer <jamrial@gmail.com>

--- libavcodec/x86/vp9mc_16bpp.asm
+++ libavcodec/x86/vp9mc_16bpp.asm
@@ -201,6 +201,9 @@ cglobal vp9_%1_8tap_1d_h_ %+ %%px %+ _12, 6, 6, %2, dst, dstride, src, sstride, 
 INIT_XMM sse2
 filter_h_fn put
 filter_h_fn avg
+INIT_YMM avx2
+filter_h_fn put
+filter_h_fn avg
 
 %macro filter_v4_fn 1-2 12
 %if ARCH_X86_64
@@ -417,5 +420,8 @@ cglobal vp9_%1_8tap_1d_v_ %+ %%px %+ _12, 4, 7, %2, dst, dstride, src, sstride, 
 %endmacro
 
 INIT_XMM sse2
+filter_v_fn put
+filter_v_fn avg
+INIT_YMM avx2
 filter_v_fn put
 filter_v_fn avg
