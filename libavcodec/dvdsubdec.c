9aab22223908180cbfc3c5fa1b19b58d806b5097
wm4 <nfxjfg@googlemail.com>
2015-09-21 18:22:35 +0200

    avcodec/dvdsubdec: reject some broken packets
    If cmd_pos is broken, this would just keep accumulating packets in the
    reassembly buffer, until it fails and flushes the buffer on overflow.
    Since packets are usually rather small, this will take a lot of subtitle
    packets. The perceived effect is that subtitles are not displayed
    anymore after the faulty packet was passed to the decoder.
    
    I'm not terribly sure about this, but on the other hand this code is
    active only when fragmented packets need to be reassembled.
    
    Fixes sample file in trac issue #4872.

--- libavcodec/dvdsubdec.c
+++ libavcodec/dvdsubdec.c
@@ -227,6 +227,7 @@ static int decode_dvd_subtitles(DVDSubContext *ctx, AVSubtitle *sub_header,
     int date;
     int i;
     int is_menu = 0;
+    uint32_t size;
 
     if (buf_size < 10)
         return -1;
@@ -241,10 +242,16 @@ static int decode_dvd_subtitles(DVDSubContext *ctx, AVSubtitle *sub_header,
         cmd_pos = 2;
     }
 
+    size = READ_OFFSET(buf + (big_offsets ? 2 : 0));
     cmd_pos = READ_OFFSET(buf + cmd_pos);
 
-    if (cmd_pos < 0 || cmd_pos > buf_size - 2 - offset_size)
+    if (cmd_pos < 0 || cmd_pos > buf_size - 2 - offset_size) {
+        if (cmd_pos > size) {
+            av_log(ctx, AV_LOG_ERROR, "Discarding invalid packet\n");
+            return 0;
+        }
         return AVERROR(EAGAIN);
+    }
 
     while (cmd_pos > 0 && cmd_pos < buf_size - 2 - offset_size) {
         date = AV_RB16(buf + cmd_pos);
