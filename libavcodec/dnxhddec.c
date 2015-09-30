7e4070d2e70bbd0e70dd83c06089e465a0eda704
Christophe Gisquet <christophe.gisquet@gmail.com>
2015-09-30 10:14:59 +0200

    dnxhddec: initialize with mb-aligned dimensions
    The coded size is a multiple of the macroblock size, which is 16.
    
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/dnxhddec.c
+++ libavcodec/dnxhddec.c
@@ -87,6 +87,9 @@ static av_cold int dnxhd_decode_init(AVCodecContext *avctx)
     ctx->cid = -1;
     avctx->colorspace = AVCOL_SPC_BT709;
 
+    avctx->coded_width  = FFALIGN(avctx->width,  16);
+    avctx->coded_height = FFALIGN(avctx->height, 16);
+
     ctx->rows = av_mallocz_array(avctx->thread_count, sizeof(RowContext));
     if (!ctx->rows)
         return AVERROR(ENOMEM);
