ca540fbdb448950b2cc2be82bd1b75cee1bc94f3
Nicolas George <george@nsup.org>
2015-10-02 16:29:08 +0200

    lavfi/vf_select: remove looping on request_frame().

--- libavfilter/f_select.c
+++ libavfilter/f_select.c
@@ -379,18 +379,9 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *frame)
 
 static int request_frame(AVFilterLink *outlink)
 {
-    AVFilterContext *ctx = outlink->src;
-    SelectContext *select = ctx->priv;
     AVFilterLink *inlink = outlink->src->inputs[0];
-    int out_no = FF_OUTLINK_IDX(outlink);
-
-    do {
-        int ret = ff_request_frame(inlink);
-        if (ret < 0)
-            return ret;
-    } while (select->select_out != out_no);
-
-    return 0;
+    int ret = ff_request_frame(inlink);
+    return ret;
 }
 
 static av_cold void uninit(AVFilterContext *ctx)
