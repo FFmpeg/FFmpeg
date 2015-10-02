35c3043ea4e67f07ada1e45aaf7b5af2c88c352c
Nicolas George <george@nsup.org>
2015-10-02 16:03:16 +0200

    lavfi/avf_showspectrum: reindent after last commit.

--- libavfilter/avf_showspectrum.c
+++ libavfilter/avf_showspectrum.c
@@ -267,18 +267,17 @@ static int request_frame(AVFilterLink *outlink)
     unsigned i;
     int ret;
 
-    /* TODO reindent */
-        ret = ff_request_frame(inlink);
-        if (ret == AVERROR_EOF && s->sliding == FULLFRAME && s->xpos > 0 &&
-            s->outpicref) {
-            for (i = 0; i < outlink->h; i++) {
-                memset(s->outpicref->data[0] + i * s->outpicref->linesize[0] + s->xpos,   0, outlink->w - s->xpos);
-                memset(s->outpicref->data[1] + i * s->outpicref->linesize[1] + s->xpos, 128, outlink->w - s->xpos);
-                memset(s->outpicref->data[2] + i * s->outpicref->linesize[2] + s->xpos, 128, outlink->w - s->xpos);
-            }
-            ret = ff_filter_frame(outlink, s->outpicref);
-            s->outpicref = NULL;
+    ret = ff_request_frame(inlink);
+    if (ret == AVERROR_EOF && s->sliding == FULLFRAME && s->xpos > 0 &&
+        s->outpicref) {
+        for (i = 0; i < outlink->h; i++) {
+            memset(s->outpicref->data[0] + i * s->outpicref->linesize[0] + s->xpos,   0, outlink->w - s->xpos);
+            memset(s->outpicref->data[1] + i * s->outpicref->linesize[1] + s->xpos, 128, outlink->w - s->xpos);
+            memset(s->outpicref->data[2] + i * s->outpicref->linesize[2] + s->xpos, 128, outlink->w - s->xpos);
         }
+        ret = ff_filter_frame(outlink, s->outpicref);
+        s->outpicref = NULL;
+    }
 
     return ret;
 }
