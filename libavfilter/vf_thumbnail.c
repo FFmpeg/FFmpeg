ea2fd42f9df495d248aefb4dadc5c9c8131b76b8
Nicolas George <george@nsup.org>
2015-10-02 16:20:17 +0200

    lavfi/vf_thumbnail: reindent after last commit.

--- libavfilter/vf_thumbnail.c
+++ libavfilter/vf_thumbnail.c
@@ -171,17 +171,16 @@ static int request_frame(AVFilterLink *link)
 {
     AVFilterContext *ctx = link->src;
     ThumbContext *s = ctx->priv;
+    int ret = ff_request_frame(ctx->inputs[0]);
 
-    /* TODO reindent */
-        int ret = ff_request_frame(ctx->inputs[0]);
-        if (ret == AVERROR_EOF && s->n) {
-            ret = ff_filter_frame(link, get_best_frame(ctx));
-            if (ret < 0)
-                return ret;
-            ret = AVERROR_EOF;
-        }
+    if (ret == AVERROR_EOF && s->n) {
+        ret = ff_filter_frame(link, get_best_frame(ctx));
         if (ret < 0)
             return ret;
+        ret = AVERROR_EOF;
+    }
+    if (ret < 0)
+        return ret;
     return 0;
 }
 
