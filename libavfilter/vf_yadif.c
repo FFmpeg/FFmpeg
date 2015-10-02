8a9fa46e87daf09fca14c903e6ee0c13fd453998
Nicolas George <george@nsup.org>
2015-10-02 15:58:53 +0200

    lavfi/vf_yadif: reindent after last commit.

--- libavfilter/vf_yadif.c
+++ libavfilter/vf_yadif.c
@@ -386,26 +386,24 @@ static int request_frame(AVFilterLink *link)
         return 0;
     }
 
+    if (yadif->eof)
+        return AVERROR_EOF;
 
-    /* TODO reindent */
-        if (yadif->eof)
-            return AVERROR_EOF;
+    ret  = ff_request_frame(link->src->inputs[0]);
 
-        ret  = ff_request_frame(link->src->inputs[0]);
+    if (ret == AVERROR_EOF && yadif->cur) {
+        AVFrame *next = av_frame_clone(yadif->next);
 
-        if (ret == AVERROR_EOF && yadif->cur) {
-            AVFrame *next = av_frame_clone(yadif->next);
+        if (!next)
+            return AVERROR(ENOMEM);
 
-            if (!next)
-                return AVERROR(ENOMEM);
+        next->pts = yadif->next->pts * 2 - yadif->cur->pts;
 
-            next->pts = yadif->next->pts * 2 - yadif->cur->pts;
-
-            filter_frame(link->src->inputs[0], next);
-            yadif->eof = 1;
-        } else if (ret < 0) {
-            return ret;
-        }
+        filter_frame(link->src->inputs[0], next);
+        yadif->eof = 1;
+    } else if (ret < 0) {
+        return ret;
+    }
 
     return 0;
 }
