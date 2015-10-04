46f3015f35258ac53d7103cbf6c4f7df589ecaf8
Michael Niedermayer <michael@niedermayer.cc>
2015-10-04 22:57:55 +0200

    avcodec/tta: Un-break build without threads
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/tta.c
+++ libavcodec/tta.c
@@ -387,7 +387,6 @@ error:
     return ret;
 }
 
-#if HAVE_THREADS
 static int init_thread_copy(AVCodecContext *avctx)
 {
     TTAContext *s = avctx->priv_data;
@@ -405,7 +404,6 @@ static av_cold int tta_decode_close(AVCodecContext *avctx) {
 
     return 0;
 }
-#endif
 
 #define OFFSET(x) offsetof(TTAContext, x)
 #define DEC (AV_OPT_FLAG_DECODING_PARAM | AV_OPT_FLAG_AUDIO_PARAM)
