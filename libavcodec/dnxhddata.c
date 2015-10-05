5911eeb033c384ba0842f7e2e11d737f1d479dd9
Christophe Gisquet <christophe.gisquet@gmail.com>
2015-10-05 20:44:46 +0200

    dnxhdenc: mark CID 1260 encoder experimental
    The MBAFF handling recently introduced on the decoder side shows that
    the encoder does not support it correctly. Therefore, make the related
    profile experimental.
    
    Furthermore, current encoder logic treats it as unable to encode as
    progressive, which isn't the case.
    
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/dnxhddata.c
+++ libavcodec/dnxhddata.c
@@ -1158,6 +1158,11 @@ int ff_dnxhd_find_cid(AVCodecContext *avctx, int bit_depth)
         if (cid->width == avctx->width && cid->height == avctx->height &&
             interlaced == !!(avctx->flags & AV_CODEC_FLAG_INTERLACED_DCT) &&
             !(cid->flags & DNXHD_444) && cid->bit_depth == bit_depth) {
+            if (avctx->strict_std_compliance > FF_COMPLIANCE_EXPERIMENTAL &&
+                cid->flags & DNXHD_MBAFF) {
+                av_log(avctx, AV_LOG_WARNING, "Profile selected is experimental\n");
+                continue;
+            }
             for (j = 0; j < FF_ARRAY_ELEMS(cid->bit_rates); j++) {
                 if (cid->bit_rates[j] == mbs)
                     return cid->cid;
