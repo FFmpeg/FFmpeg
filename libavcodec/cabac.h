8a73b8c5b42d02c0056f751e290c5cd60c6b802e
Anton Khirnov <anton@khirnov.net>
2015-10-07 11:39:46 -0400

    cabac: Make cabac starts hardcoded
    There's not much reason to generate such a small table at runtime.
    
    Signed-off-by: Derek Buitenhuis <derek.buitenhuis@gmail.com>

--- libavcodec/cabac.h
+++ libavcodec/cabac.h
@@ -31,7 +31,7 @@
 
 #include "put_bits.h"
 
-extern uint8_t ff_h264_cabac_tables[512 + 4*2*64 + 4*64 + 63];
+extern const uint8_t ff_h264_cabac_tables[512 + 4*2*64 + 4*64 + 63];
 #define H264_NORM_SHIFT_OFFSET 0
 #define H264_LPS_RANGE_OFFSET 512
 #define H264_MLPS_STATE_OFFSET 1024
@@ -52,6 +52,5 @@ typedef struct CABACContext{
 
 void ff_init_cabac_encoder(CABACContext *c, uint8_t *buf, int buf_size);
 void ff_init_cabac_decoder(CABACContext *c, const uint8_t *buf, int buf_size);
-void ff_init_cabac_states(void);
 
 #endif /* AVCODEC_CABAC_H */
