cd847839f33e58a2c2259595335ab483ef32667d
赵宇龙 <zylthinking@gmail.com>
2015-10-09 13:59:56 +0800

    h264_mp4toannexb: fix the pps offset when there are more than one sps in avcc
    the pps offset is used to locate pps in the spspps_buf; however, the
    current calc method is wrong because it is the offset of the original
    avctx->extradata;
    when there is only one sps in the avcc; the value is correct by
    coincidence, however, it will fail in avcc with multi sps
    
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/h264_mp4toannexb_bsf.c
+++ libavcodec/h264_mp4toannexb_bsf.c
@@ -122,7 +122,7 @@ pps:
         if (!unit_nb && !sps_done++) {
             unit_nb = *extradata++; /* number of pps unit(s) */
             if (unit_nb) {
-                ctx->pps_offset = (extradata - 1) - (avctx->extradata + 4);
+                ctx->pps_offset = total_size;
                 pps_seen = 1;
             }
         }
