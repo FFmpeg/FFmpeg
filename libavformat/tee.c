1450a39ad495ad086b1110d15f999c0eeca92ef8
Nicolas George <george@nsup.org>
2015-10-10 15:19:43 +0200

    lavf/tee: fix side data double free.
    Similar to 33fefdb44.
    Fix trac ticket #4921.
    
    Signed-off-by: Nicolas George <george@nsup.org>
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit 1acc90eaa54ad82a21474ed759b8ed3a0f3d482d)
    
    Conflicts:
    
    	libavformat/tee.c
    
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavformat/tee.c
+++ libavformat/tee.c
@@ -409,6 +409,8 @@ FF_ENABLE_DEPRECATION_WARNINGS
         }
 
         if (ret > 0) {
+            pkt->side_data = NULL;
+            pkt->side_data_elems = 0;
             av_free_packet(pkt);
             new_pkt.buf = av_buffer_create(new_pkt.data, new_pkt.size,
                                            av_buffer_default_free, NULL, 0);
