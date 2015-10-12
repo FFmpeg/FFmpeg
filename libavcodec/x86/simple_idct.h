7ece8b50b19e140ace13eda6f1a9f45f868c2528
Christophe Gisquet <christophe.gisquet@gmail.com>
2015-10-12 19:37:49 +0200

    x86: simple_idct: 12bits versions
    On 12 frames of a 444p 12 bits DNxHR sequence, _put function:
    C:         78902 decicycles in idct,  262071 runs,     73 skips
    avx:       32478 decicycles in idct,  262045 runs,     99 skips
    
    Difference between the 2:
    stddev:    0.39 PSNR:104.47 MAXDIFF:    2
    
    This is unavoidable and due to the scale factors used in the x86
    version, which cannot match the C ones.
    
    In addition, the trick of adding an initial bias to the input of a
    pass can overflow, as the input coefficients are already 15bits,
    which is the maximum this function can handle.
    
    Overall, however, the omse on 12 bits samples goes from 0.16916 to
    0.16883. Reducing rowshift by 1 improves to 0.0908, but causes
    overflows.
    
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/x86/simple_idct.h
+++ libavcodec/x86/simple_idct.h
@@ -31,4 +31,10 @@ void ff_simple_idct10_avx(int16_t *block);
 void ff_simple_idct10_put_sse2(uint8_t *dest, int line_size, int16_t *block);
 void ff_simple_idct10_put_avx(uint8_t *dest, int line_size, int16_t *block);
 
+void ff_simple_idct12_sse2(int16_t *block);
+void ff_simple_idct12_avx(int16_t *block);
+
+void ff_simple_idct12_put_sse2(uint8_t *dest, int line_size, int16_t *block);
+void ff_simple_idct12_put_avx(uint8_t *dest, int line_size, int16_t *block);
+
 #endif /* AVCODEC_X86_SIMPLE_IDCT_H */
--- libavcodec/x86/simple_idct10.asm
+++ libavcodec/x86/simple_idct10.asm
@@ -29,9 +29,12 @@
 
 SECTION_RODATA
 
+cextern pw_2
 cextern pw_16
 cextern pw_1023
+cextern pw_4095
 pd_round_12: times 4 dd 1<<(12-1)
+pd_round_15: times 4 dd 1<<(15-1)
 pd_round_19: times 4 dd 1<<(19-1)
 
 %include "libavcodec/x86/simple_idct10_template.asm"
@@ -45,6 +48,19 @@ cglobal simple_idct10, 1, 1, 16
 
 cglobal simple_idct10_put, 3, 3, 16
     IDCT_FN    "", 12, "", 19, 0, pw_1023
+    RET
+
+cglobal simple_idct12, 1, 1, 16
+    ; coeffs are already 15bits, adding the offset would cause
+    ; overflow in the input
+    IDCT_FN    "", 15, pw_2, 16
+    RET
+
+cglobal simple_idct12_put, 3, 3, 16
+    ; range isn't known, so the C simple_idct range is used
+    ; Also, using a bias on input overflows, so use the bias
+    ; on output of the first butterfly instead
+    IDCT_FN    "", 15, pw_2, 16, 0, pw_4095
     RET
 %endmacro
 
