54cd1ab55513e0a12f90dba8036b0a54b16d8ff7
Rodger Combs <rodger.combs@gmail.com>
2015-10-11 22:15:08 -0500

    lavu/aes: align AVAES struct members

--- libavutil/aes_internal.h
+++ libavutil/aes_internal.h
@@ -21,6 +21,7 @@
 #ifndef AVUTIL_AES_INTERNAL_H
 #define AVUTIL_AES_INTERNAL_H
 
+#include "mem.h"
 #include <stdint.h>
 
 typedef union {
@@ -33,8 +34,8 @@ typedef union {
 typedef struct AVAES {
     // Note: round_key[16] is accessed in the init code, but this only
     // overwrites state, which does not matter (see also commit ba554c0).
-    av_aes_block round_key[15];
-    av_aes_block state[2];
+    DECLARE_ALIGNED(16, av_aes_block, round_key)[15];
+    DECLARE_ALIGNED(16, av_aes_block, state)[2];
     int rounds;
     void (*crypt)(struct AVAES *a, uint8_t *dst, const uint8_t *src, int count, uint8_t *iv, int rounds);
 } AVAES;
