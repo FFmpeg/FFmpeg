ec588db56fdc21606a8c8b9b32f4a54aacbe7aca
Rodger Combs <rodger.combs@gmail.com>
2015-10-11 22:12:16 -0500

    lavu/aes: move AVAES to separate internal header

--- libavutil/aes.c
+++ libavutil/aes.c
@@ -22,23 +22,9 @@
 
 #include "common.h"
 #include "aes.h"
+#include "aes_internal.h"
 #include "intreadwrite.h"
 #include "timer.h"
-
-typedef union {
-    uint64_t u64[2];
-    uint32_t u32[4];
-    uint8_t u8x4[4][4];
-    uint8_t u8[16];
-} av_aes_block;
-
-typedef struct AVAES {
-    // Note: round_key[16] is accessed in the init code, but this only
-    // overwrites state, which does not matter (see also commit ba554c0).
-    av_aes_block round_key[15];
-    av_aes_block state[2];
-    int rounds;
-} AVAES;
 
 const int av_aes_size= sizeof(AVAES);
 
