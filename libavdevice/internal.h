6d0c8cb79ac0977099e1f6afb28465bd587c5706
Ganesh Ajjanagadde <gajjanagadde@gmail.com>
2015-10-15 21:49:30 -0400

    avdevice/internal: add av_warn_unused_result
    This does not trigger any warnings but adds robustness.
    
    Reviewed-by: Michael Niedermayer <michael@niedermayer.cc>
    Signed-off-by: Ganesh Ajjanagadde <gajjanagadde@gmail.com>

--- libavdevice/internal.h
+++ libavdevice/internal.h
@@ -21,6 +21,7 @@
 
 #include "libavformat/avformat.h"
 
+av_warn_unused_result
 int ff_alloc_input_device_context(struct AVFormatContext **avctx, struct AVInputFormat *iformat,
                                   const char *format);
 
