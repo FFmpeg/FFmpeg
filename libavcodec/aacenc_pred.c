148cf4690c81e64a3078caeb0344325686b66b16
Rostislav Pehlivanov <atomnuker@gmail.com>
2015-10-17 02:15:44 +0100

    aacenc_pred: only print predictor information if profile is aac_main
    Needed because LTP uses predictor_present as well.

--- libavcodec/aacenc_pred.c
+++ libavcodec/aacenc_pred.c
@@ -333,7 +333,8 @@ void ff_aac_encode_main_pred(AACEncContext *s, SingleChannelElement *sce)
     IndividualChannelStream *ics = &sce->ics;
     const int pmax = FFMIN(ics->max_sfb, ff_aac_pred_sfb_max[s->samplerate_index]);
 
-    if (!ics->predictor_present)
+    if (s->profile != FF_PROFILE_AAC_MAIN ||
+        !ics->predictor_present)
         return;
 
     put_bits(&s->pb, 1, !!ics->predictor_reset_group);
