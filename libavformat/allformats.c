0e08d6ca140a3849a05372c71dfe669fdcf250a7
Paul B Mahol <onemda@gmail.com>
2015-10-17 21:41:18 +0200

    avformat: add msf demuxer
    Signed-off-by: Paul B Mahol <onemda@gmail.com>

--- libavformat/allformats.c
+++ libavformat/allformats.c
@@ -206,6 +206,7 @@ void av_register_all(void)
     REGISTER_MUXDEMUX(MPJPEG,           mpjpeg);
     REGISTER_DEMUXER (MPL2,             mpl2);
     REGISTER_DEMUXER (MPSUB,            mpsub);
+    REGISTER_DEMUXER (MSF,              msf);
     REGISTER_DEMUXER (MSNWC_TCP,        msnwc_tcp);
     REGISTER_DEMUXER (MTV,              mtv);
     REGISTER_DEMUXER (MV,               mv);
