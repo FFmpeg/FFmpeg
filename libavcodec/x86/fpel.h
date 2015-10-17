607f820ec7c99c86f0ec25057bd6ca68179ed9b8
Timothy Gu <timothygu99@gmail.com>
2015-10-17 16:34:07 -0700

    x86: fpel: Remove erroneous ff_put_pixels8_mmxext prototype
    This function does not exist.

--- libavcodec/x86/fpel.h
+++ libavcodec/x86/fpel.h
@@ -40,8 +40,6 @@ void ff_put_pixels4_mmx(uint8_t *block, const uint8_t *pixels,
                         ptrdiff_t line_size, int h);
 void ff_put_pixels8_mmx(uint8_t *block, const uint8_t *pixels,
                         ptrdiff_t line_size, int h);
-void ff_put_pixels8_mmxext(uint8_t *block, const uint8_t *pixels,
-                           ptrdiff_t line_size, int h);
 void ff_put_pixels16_mmx(uint8_t *block, const uint8_t *pixels,
                          ptrdiff_t line_size, int h);
 void ff_put_pixels16_sse2(uint8_t *block, const uint8_t *pixels,
