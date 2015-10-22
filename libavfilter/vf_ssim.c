e7d9061d4f932b382d1b7776e2e11729d4ff5b52
Ronald S. Bultje <rsbultje@gmail.com>
2015-10-22 14:51:11 -0400

    vf_ssim: print per-channel dB values.

--- libavfilter/vf_ssim.c
+++ libavfilter/vf_ssim.c
@@ -348,7 +348,8 @@ static av_cold void uninit(AVFilterContext *ctx)
         buf[0] = 0;
         for (i = 0; i < s->nb_components; i++) {
             int c = s->is_rgb ? s->rgba_map[i] : i;
-            av_strlcatf(buf, sizeof(buf), " %c:%f", s->comps[i], s->ssim[c] / s->nb_frames);
+            av_strlcatf(buf, sizeof(buf), " %c:%f (%f)", s->comps[i], s->ssim[c] / s->nb_frames,
+                        ssim_db(s->ssim[c], s->nb_frames));
         }
         av_log(ctx, AV_LOG_INFO, "SSIM%s All:%f (%f)\n", buf,
                s->ssim_total / s->nb_frames, ssim_db(s->ssim_total, s->nb_frames));
