81d7f118e20c3fec05e484699faa5d58ca381271
Ronald S. Bultje <rsbultje@gmail.com>
2015-10-22 15:25:38 -0400

    vf_psnr: remove %0.2f format specifiers for stream summary line.
    This makes output equally precise as vf_ssim.

--- libavfilter/vf_psnr.c
+++ libavfilter/vf_psnr.c
@@ -324,10 +324,10 @@ static av_cold void uninit(AVFilterContext *ctx)
         buf[0] = 0;
         for (j = 0; j < s->nb_components; j++) {
             int c = s->is_rgb ? s->rgba_map[j] : j;
-            av_strlcatf(buf, sizeof(buf), " %c:%0.2f", s->comps[j],
+            av_strlcatf(buf, sizeof(buf), " %c:%f", s->comps[j],
                         get_psnr(s->mse_comp[c], s->nb_frames, s->max[c]));
         }
-        av_log(ctx, AV_LOG_INFO, "PSNR%s average:%0.2f min:%0.2f max:%0.2f\n",
+        av_log(ctx, AV_LOG_INFO, "PSNR%s average:%f min:%f max:%f\n",
                buf,
                get_psnr(s->mse, s->nb_frames, s->average_max),
                get_psnr(s->max_mse, 1, s->average_max),
