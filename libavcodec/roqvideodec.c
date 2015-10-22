ef4fbee79df6b3835bef36bbe59bdc7c834676f2
Hendrik Leppkes <h.leppkes@gmail.com>
2015-10-22 20:18:00 +0200

    roqvideodec: use av_frame_copy

--- libavcodec/roqvideodec.c
+++ libavcodec/roqvideodec.c
@@ -204,16 +204,16 @@ static int roq_decode_frame(AVCodecContext *avctx,
     const uint8_t *buf = avpkt->data;
     int buf_size = avpkt->size;
     RoqContext *s = avctx->priv_data;
-    int copy= !s->current_frame->data[0];
+    int copy= !s->current_frame->data[0] && s->last_frame->data[0];
     int ret;
 
     if ((ret = ff_reget_buffer(avctx, s->current_frame)) < 0)
         return ret;
 
-    if(copy)
-        av_image_copy(s->current_frame->data, s->current_frame->linesize,
-                      s->last_frame->data, s->last_frame->linesize,
-                      avctx->pix_fmt, avctx->width, avctx->height);
+    if(copy) {
+        if ((ret = av_frame_copy(s->current_frame, s->last_frame)) < 0)
+            return ret;
+    }
 
     bytestream2_init(&s->gb, buf, buf_size);
     roqvideo_decode_frame(s);
