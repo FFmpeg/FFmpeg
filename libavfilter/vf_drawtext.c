85baef4ff1512bcc2544928bfa5f42072903a691
Diego Biurrun <diego@biurrun.de>
2015-10-30 15:02:04 +0100

    vf_drawtext: Move static keyword to beginning of variable declaration
    libavfilter/vf_drawtext.c:226:1: warning: ‘static’ is not at beginning of declaration [-Wold-style-declaration]

--- libavfilter/vf_drawtext.c
+++ libavfilter/vf_drawtext.c
@@ -219,11 +219,10 @@ static const AVClass drawtext_class = {
 #define FT_ERRORDEF(e, v, s) { (e), (s) },
 #define FT_ERROR_END_LIST { 0, NULL } };
 
-struct ft_error
-{
+static struct ft_error {
     int err;
     const char *err_msg;
-} static ft_errors[] =
+} ft_errors[] =
 #include FT_ERRORS_H
 
 #define FT_ERRMSG(e) ft_errors[e].err_msg
