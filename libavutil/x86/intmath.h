91053990602de8da4df911692710f0e8362daac9
Matt Oliver <protogonoi@gmail.com>
2015-11-11 00:13:42 +1100

    avutil/x86/intmath: Disable use of tzcnt on older intel compilers.
    ICC versions older than atleast 12.1.6 dont have the tzcnt intrinsics.
    
    Reviewed-by: Michael Niedermayer <michael@niedermayer.cc>
    Signed-off-by: Matt Oliver <protogonoi@gmail.com>

--- libavutil/x86/intmath.h
+++ libavutil/x86/intmath.h
@@ -32,7 +32,7 @@
 #include "config.h"
 
 #if HAVE_FAST_CLZ
-#if defined(__INTEL_COMPILER) || defined(_MSC_VER)
+#if (defined(__INTEL_COMPILER) && (__INTEL_COMPILER>=1216)) || defined(_MSC_VER)
 #   if defined(__INTEL_COMPILER)
 #       define ff_log2(x) (_bit_scan_reverse((x)|1))
 #   else
