a9c73b13c1170a43bc8b3e0e85c21f97145b3d2f
Michael Niedermayer <michael@niedermayer.cc>
2015-11-15 20:49:17 +0100

    avcodec/jpeg2000: Check comp coords to be within the supported size
    Fixes assertion failure
    Fixes: 03e0abe721b1174856d41a1eb5d6a896/signal_sigabrt_7ffff6ae7cc9_3813_e71bf3541abed3ccba031cd5ba0269a4.avi
    
    This fix is choosen to be simple to backport, better solution
    for master is planed
    
    Found-by: Mateusz "j00ru" Jurczyk and Gynvael Coldwind
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit a1a8cbcb35ef2759a66b4f0875785e4b3f277057)
    
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/jpeg2000.c
+++ libavcodec/jpeg2000.c
@@ -217,6 +217,11 @@ int ff_jpeg2000_init_component(Jpeg2000Component *comp,
         return AVERROR_INVALIDDATA;
     csize = (comp->coord[0][1] - comp->coord[0][0]) *
             (comp->coord[1][1] - comp->coord[1][0]);
+    if (comp->coord[0][1] > 32768 ||
+        comp->coord[1][1] > 32768) {
+        av_log(avctx, AV_LOG_ERROR, "component size too large\n");
+        return AVERROR_PATCHWELCOME;
+    }
 
     if (codsty->transform == FF_DWT97) {
         comp->i_data = NULL;
