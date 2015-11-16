8adff79b6d30d1ae2cb9cf906e1d7fcd759cd638
Ganesh Ajjanagadde <gajjanagadde@gmail.com>
2015-11-15 20:58:46 -0500

    avformat/mov: remove redundant assignment
    This is possibly undefined behavior based on sequence point rules, but I
    have not studied the spec at that level of detail.
    
    Fixes: CID 1338321.
    
    Reviewed-by: Michael Niedermayer <michael@niedermayer.cc>
    Signed-off-by: Ganesh Ajjanagadde <gajjanagadde@gmail.com>

--- libavformat/mov.c
+++ libavformat/mov.c
@@ -4936,7 +4936,7 @@ static int mov_seek_fragment(AVFormatContext *s, AVStream *st, int64_t timestamp
 
     for (i = 0; i < mov->fragment_index_count; i++) {
         if (mov->fragment_index_data[i]->track_id == st->id) {
-            MOVFragmentIndex *index = index = mov->fragment_index_data[i];
+            MOVFragmentIndex *index = mov->fragment_index_data[i];
             for (j = index->item_count - 1; j >= 0; j--) {
                 if (index->items[j].time <= timestamp) {
                     if (index->items[j].headers_read)
