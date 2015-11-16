fafb10c30cdb5735b0685bdeb9f0609104b8ac32
Ganesh Ajjanagadde <gajjanagadde@gmail.com>
2015-11-16 16:42:40 -0500

    avcodec/lpc: replace #define by typedef
    See e.g https://stackoverflow.com/questions/1666353/are-typedef-and-define-the-same-in-c
    for rationale.
    
    Reviewed-by: Michael Niedermayer <michael@niedermayer.cc>
    Signed-off-by: Ganesh Ajjanagadde <gajjanagadde@gmail.com>

--- libavcodec/lpc.h
+++ libavcodec/lpc.h
@@ -116,12 +116,12 @@ void ff_lpc_init_x86(LPCContext *s);
 void ff_lpc_end(LPCContext *s);
 
 #if USE_FIXED
-#define LPC_TYPE int
+typedef int LPC_TYPE;
 #else
 #ifdef LPC_USE_DOUBLE
-#define LPC_TYPE double
+typedef double LPC_TYPE;
 #else
-#define LPC_TYPE float
+typedef float LPC_TYPE;
 #endif
 #endif // USE_FIXED
 
