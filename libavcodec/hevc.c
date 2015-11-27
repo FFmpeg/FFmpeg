1cbc2cb5084e732d89a56792133576149040f693
Michael Niedermayer <michael@niedermayer.cc>
2015-11-27 23:33:03 +0100

    avcodec/hevc: allocate entries unconditionally
    Fixes out of array access
    Fixes: 08664a2a7921ef48172f26495c7455be/asan_heap-oob_23036c6_3301_523388ef84285a0270caf67a43247b59.bit
    
    Found-by: Mateusz "j00ru" Jurczyk and Gynvael Coldwind
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit d85aa76115214183e7e3b7d65e950da61474959a)
    
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/hevc.c
+++ libavcodec/hevc.c
@@ -2450,11 +2450,9 @@ static int hls_slice_data_wpp(HEVCContext *s, const HEVCNAL *nal)
         return AVERROR(ENOMEM);
     }
 
+    ff_alloc_entries(s->avctx, s->sh.num_entry_point_offsets + 1);
 
     if (!s->sList[1]) {
-        ff_alloc_entries(s->avctx, s->sh.num_entry_point_offsets + 1);
-
-
         for (i = 1; i < s->threads_number; i++) {
             s->sList[i] = av_malloc(sizeof(HEVCContext));
             memcpy(s->sList[i], s, sizeof(HEVCContext));
