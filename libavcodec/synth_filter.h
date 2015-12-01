705f5e5e155f6f280a360af220fc5b30cfcee702
Janne Grunau <janne-libav@jannau.net>
2015-12-01 13:37:41 +0100

    arm64: port synth_filter_float_neon from arm
    ~25% faster dts decoding overall. The checkasm CPU cycles numbers are
    not that useful since synth_filter_float() calls FFTContext.imdct_half().
    
                             cortex-a57   cortex-a53
    synth_filter_float_c:    1866.2       3490.9
    synth_filter_float_neon:  915.0       1531.5
    
    With fftc.imdct_half forced to imdct_half_neon:
                             cortex-a57   cortex-a53
    synth_filter_float_c:    1718.4       3025.3
    synth_filter_float_neon:  926.2       1530.1

--- libavcodec/synth_filter.h
+++ libavcodec/synth_filter.h
@@ -32,6 +32,7 @@ typedef struct SynthFilterContext {
 } SynthFilterContext;
 
 void ff_synth_filter_init(SynthFilterContext *c);
+void ff_synth_filter_init_aarch64(SynthFilterContext *c);
 void ff_synth_filter_init_arm(SynthFilterContext *c);
 void ff_synth_filter_init_x86(SynthFilterContext *c);
 
