76af12f5429b72820afd3b448cb38e9709993b03
Andreas Cadhalpun <Andreas.Cadhalpun@googlemail.com>
2015-12-02 22:47:12 +0100

    ffmdec: reject zero-sized chunks
    If size is zero, avio_get_str fails, leaving the buffer uninitialized.
    This causes invalid reads in av_set_options_string.
    
    Reviewed-by: Michael Niedermayer <michael@niedermayer.cc>
    Signed-off-by: Andreas Cadhalpun <Andreas.Cadhalpun@googlemail.com>
    (cherry picked from commit a611375db532c3d5363d97b10fadd0211811a4fd)
    Signed-off-by: Andreas Cadhalpun <Andreas.Cadhalpun@googlemail.com>

--- libavformat/ffmdec.c
+++ libavformat/ffmdec.c
@@ -415,7 +415,7 @@ static int ffm2_read_header(AVFormatContext *s)
             }
             break;
         case MKBETAG('S', '2', 'V', 'I'):
-            if (f_stvi++) {
+            if (f_stvi++ || !size) {
                 ret = AVERROR(EINVAL);
                 goto fail;
             }
@@ -430,7 +430,7 @@ static int ffm2_read_header(AVFormatContext *s)
                 goto fail;
             break;
         case MKBETAG('S', '2', 'A', 'U'):
-            if (f_stau++) {
+            if (f_stau++ || !size) {
                 ret = AVERROR(EINVAL);
                 goto fail;
             }
