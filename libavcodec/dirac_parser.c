142f8308a121051c6e2d3531ea4892018ba67ef3
Michael Niedermayer <michael@niedermayer.cc>
2015-12-05 17:11:54 +0100

    avcodec/dirac_parser: Fix potential overflows in pointer checks
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit 79798f7c57b098c78e0bbc6becd64b9888b013d1)
    
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/dirac_parser.c
+++ libavcodec/dirac_parser.c
@@ -100,10 +100,12 @@ typedef struct DiracParseUnit {
 static int unpack_parse_unit(DiracParseUnit *pu, DiracParseContext *pc,
                              int offset)
 {
-    uint8_t *start = pc->buffer + offset;
-    uint8_t *end   = pc->buffer + pc->index;
-    if (start < pc->buffer || (start + 13 > end))
+    int8_t *start;
+
+    if (offset < 0 || pc->index - 13 < offset)
         return 0;
+
+    start = pc->buffer + offset;
     pu->pu_type = start[4];
 
     pu->next_pu_offset = AV_RB32(start + 5);
