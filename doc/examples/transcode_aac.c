bb265b764a055f2dc576b9aec62460d9580868f4
Diego Biurrun <diego@biurrun.de>
2015-12-08 16:54:07 +0100

    examples/transcode_aac: Drop pointless return value const qualifier
    doc/examples/transcode_aac.c:52:20: warning: type qualifiers ignored on function return type [-Wignored-qualifiers]

--- doc/examples/transcode_aac.c
+++ doc/examples/transcode_aac.c
@@ -49,7 +49,7 @@
  * @param error Error code to be converted
  * @return Corresponding error text (not thread-safe)
  */
-static char *const get_error_text(const int error)
+static char *get_error_text(const int error)
 {
     static char error_buffer[255];
     av_strerror(error, error_buffer, sizeof(error_buffer));
