753930bc7300dd595c4bab51c5a70d1da9083da4
Andreas Cadhalpun <Andreas.Cadhalpun@googlemail.com>
2015-12-20 22:20:57 +0100

    doc: make apidoc output independent of SRC_PATH
    Previously it included the SRC_PATH in every title.
    
    Reviewed-by: Michael Niedermayer <michael@niedermayer.cc>
    Signed-off-by: Andreas Cadhalpun <Andreas.Cadhalpun@googlemail.com>

--- doc/doxy-wrapper.sh
+++ doc/doxy-wrapper.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
-SRC_PATH="${1}"
+OUT_DIR="${1}"
 DOXYFILE="${2}"
 DOXYGEN="${3}"
 
 shift 3
 
-if [ -e "$SRC_PATH/VERSION" ]; then
-    VERSION=`cat "$SRC_PATH/VERSION"`
+if [ -e "VERSION" ]; then
+    VERSION=`cat "VERSION"`
 else
-    VERSION=`cd "$SRC_PATH"; git describe`
+    VERSION=`git describe`
 fi
 
 $DOXYGEN - <<EOF
 @INCLUDE        = ${DOXYFILE}
 INPUT           = $@
-EXAMPLE_PATH    = ${SRC_PATH}/doc/examples
 HTML_TIMESTAMP  = NO
 PROJECT_NUMBER  = $VERSION
+OUTPUT_DIRECTORY = $OUT_DIR
 EOF
