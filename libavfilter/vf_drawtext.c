1d6308dbc664d6108a0acc35e525c93c7ee4defa
Clément Bœsch <clement@stupeflix.com>
2015-12-21 16:07:22 +0100

    lavfi/drawtext: hide first font load warning when fontconfig is present

--- libavfilter/vf_drawtext.c
+++ libavfilter/vf_drawtext.c
@@ -372,8 +372,10 @@ static int load_font_file(AVFilterContext *ctx, const char *path, int index)
 
     err = FT_New_Face(s->library, path, index, &s->face);
     if (err) {
+#if !CONFIG_LIBFONTCONFIG
         av_log(ctx, AV_LOG_ERROR, "Could not load font \"%s\": %s\n",
                s->fontfile, FT_ERRMSG(err));
+#endif
         return AVERROR(EINVAL);
     }
     return 0;
