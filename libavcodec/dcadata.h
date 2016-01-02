b95cba7b3c829e4efd9621f7425af0e0f8ad70d4
Hendrik Leppkes <h.leppkes@gmail.com>
2016-01-02 19:06:40 +0100

    avcodec/dca: remove unused float quant table

--- libavcodec/dcadata.h
+++ libavcodec/dcadata.h
@@ -37,7 +37,6 @@ extern const uint32_t ff_dca_scale_factor_quant7[128];
 extern const uint32_t ff_dca_lossy_quant[32];
 
 extern const uint32_t ff_dca_lossless_quant[32];
-extern const float ff_dca_lossless_quant_d[32];
 
 extern const int8_t ff_dca_high_freq_vq[1024][32];
 
