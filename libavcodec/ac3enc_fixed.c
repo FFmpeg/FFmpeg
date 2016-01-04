b0f67d03c50fcfc5abf08bc538484af60b147f78
Diego Biurrun <diego@biurrun.de>
2016-01-04 11:59:00 +0100

    ac3enc: Avoid unnecessary macro indirections
    Signed-off-by: James Almer <jamrial@gmail.com>

--- libavcodec/ac3enc_fixed.c
+++ libavcodec/ac3enc_fixed.c
@@ -115,7 +115,7 @@ static CoefType calc_cpl_coord(CoefSumType energy_ch, CoefSumType energy_cpl)
  *
  * @param s  AC-3 encoder private context
  */
-av_cold void AC3_NAME(mdct_end)(AC3EncodeContext *s)
+av_cold void ff_ac3_fixed_mdct_end(AC3EncodeContext *s)
 {
     ff_mdct_end(&s->mdct);
 }
@@ -127,7 +127,7 @@ av_cold void AC3_NAME(mdct_end)(AC3EncodeContext *s)
  * @param s  AC-3 encoder private context
  * @return   0 on success, negative error code on failure
  */
-av_cold int AC3_NAME(mdct_init)(AC3EncodeContext *s)
+av_cold int ff_ac3_fixed_mdct_init(AC3EncodeContext *s)
 {
     int ret = ff_mdct_init(&s->mdct, 9, 0, -1.0);
     s->mdct_window = ff_ac3_window;
