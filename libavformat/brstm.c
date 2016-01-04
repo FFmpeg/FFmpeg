1cb2331eca0dbde1bc63bc715a0e98771dda8b80
Andreas Cadhalpun <Andreas.Cadhalpun@googlemail.com>
2016-01-04 13:44:16 +0100

    brstm: fix missing closing brace
    Signed-off-by: Andreas Cadhalpun <Andreas.Cadhalpun@googlemail.com>

--- libavformat/brstm.c
+++ libavformat/brstm.c
@@ -392,6 +392,7 @@ static int read_packet(AVFormatContext *s, AVPacket *pkt)
         if (!b->adpc) {
             av_log(s, AV_LOG_ERROR, "adpcm_thp requires ADPC chunk, but none was found.\n");
             return AVERROR_INVALIDDATA;
+        }
         if (!b->table) {
             b->table = av_mallocz(32 * codec->channels);
             if (!b->table)
