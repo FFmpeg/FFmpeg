2316c3586591f858ad14f586bb30a54700dd266e
Michael Niedermayer <michael@niedermayer.cc>
2016-01-09 10:49:23 +0100

    avformat: Add integer fps from 31 to 60 to get_std_framerate()
    Fixes Ticket 5106
    
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit 2039b3e7511ef183dae206575114e15b6d99c134)
    
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavformat/avformat.h
+++ libavformat/avformat.h
@@ -968,7 +968,7 @@ typedef struct AVStream {
     /**
      * Stream information used internally by av_find_stream_info()
      */
-#define MAX_STD_TIMEBASES (30*12+7+6)
+#define MAX_STD_TIMEBASES (30*12+30+3+6)
     struct {
         int64_t last_dts;
         int64_t duration_gcd;
