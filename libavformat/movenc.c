6e249466cc6bd5b17d6e8cbd9a84a636cc92fd60
Michael Niedermayer <michael@niedermayer.cc>
2016-01-09 03:36:19 +0100

    avformat/movenc: Check that pkt duration is within 32bit range
    Durations outside are not supported
    
    Fixes Ticket5114
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavformat/movenc.c
+++ libavformat/movenc.c
@@ -4362,10 +4362,10 @@ int ff_mov_write_packet(AVFormatContext *s, AVPacket *pkt)
             pkt->dts = trk->cluster[trk->entry - 1].dts + 1;
             pkt->pts = AV_NOPTS_VALUE;
         }
-        if (pkt->duration < 0) {
-            av_log(s, AV_LOG_ERROR, "Application provided duration: %"PRId64" is invalid\n", pkt->duration);
-            return AVERROR(EINVAL);
-        }
+    }
+    if (pkt->duration < 0 || pkt->duration > INT_MAX) {
+        av_log(s, AV_LOG_ERROR, "Application provided duration: %"PRId64" is invalid\n", pkt->duration);
+        return AVERROR(EINVAL);
     }
     if (mov->flags & FF_MOV_FLAG_FRAGMENT) {
         int ret;
