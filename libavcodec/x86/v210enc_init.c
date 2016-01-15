d29237e5578a187c5a8d91338cd70ce0fd6f6003
James Darnley <james.darnley@gmail.com>
2016-01-15 20:35:05 +0100

    v210: Add avx2 version of the 8-bit line encoder
    Around 35% faster than the avx version.
    
    Signed-off-by: Henrik Gramner <henrik@gramner.com>
    Signed-off-by: Luca Barbato <lu_zero@gentoo.org>

--- libavcodec/x86/v210enc_init.c
+++ libavcodec/x86/v210enc_init.c
@@ -24,6 +24,8 @@ void ff_v210_planar_pack_8_ssse3(const uint8_t *y, const uint8_t *u,
                                  ptrdiff_t width);
 void ff_v210_planar_pack_8_avx(const uint8_t *y, const uint8_t *u,
                                const uint8_t *v, uint8_t *dst, ptrdiff_t width);
+void ff_v210_planar_pack_8_avx2(const uint8_t *y, const uint8_t *u,
+                                const uint8_t *v, uint8_t *dst, ptrdiff_t width);
 void ff_v210_planar_pack_10_ssse3(const uint16_t *y, const uint16_t *u,
                                   const uint16_t *v, uint8_t *dst,
                                   ptrdiff_t width);
@@ -39,4 +41,9 @@ av_cold void ff_v210enc_init_x86(V210EncContext *s)
 
     if (EXTERNAL_AVX(cpu_flags))
         s->pack_line_8 = ff_v210_planar_pack_8_avx;
+
+    if (EXTERNAL_AVX2(cpu_flags)) {
+        s->sample_factor = 2;
+        s->pack_line_8   = ff_v210_planar_pack_8_avx2;
+    }
 }
