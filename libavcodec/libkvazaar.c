472d488ebcc53bea4cdb124edb94558e72d8f23f
Arttu Ylä-Outinen <arttu.yla-outinen@tut.fi>
2016-01-15 15:53:46 +0200

    libkvazaar: Set frame rate as a rational number
    Updates libkvazaar to pass the exact frame rate to Kvazaar by setting
    the numerator and denominator separately instead of a single floating
    point number. The exact frame rate is needed for writing timing info to
    the bitstream.
    
    Requires Kvazaar version 0.8.1.
    
    Signed-off-by: Arttu Ylä-Outinen <arttu.yla-outinen@tut.fi>
    Signed-off-by: Luca Barbato <lu_zero@gentoo.org>

--- libavcodec/libkvazaar.c
+++ libavcodec/libkvazaar.c
@@ -80,8 +80,8 @@ static av_cold int libkvazaar_init(AVCodecContext *avctx)
     cfg->width  = avctx->width;
     cfg->height = avctx->height;
 
-    cfg->framerate =
-      avctx->time_base.den / (double)(avctx->time_base.num * avctx->ticks_per_frame);
+    cfg->framerate_num   = avctx->time_base.den;
+    cfg->framerate_denom = avctx->time_base.num * avctx->ticks_per_frame;
     cfg->target_bitrate = avctx->bit_rate;
     cfg->vui.sar_width  = avctx->sample_aspect_ratio.num;
     cfg->vui.sar_height = avctx->sample_aspect_ratio.den;
