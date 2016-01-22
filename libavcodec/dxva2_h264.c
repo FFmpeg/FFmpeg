406f30072e4ecc51c101a27a790bc3cf1a22bad4
Hendrik Leppkes <h.leppkes@gmail.com>
2016-01-22 13:50:42 +0100

    dxva2_h264: fix reference field marking in long slice struct
    Fixes artifacts in interlaced decoding on old Intel GPUs.

--- libavcodec/dxva2_h264.c
+++ libavcodec/dxva2_h264.c
@@ -252,7 +252,7 @@ static void fill_slice_long(AVCodecContext *avctx, DXVA_Slice_H264_Long *slice,
                 else
                     index = get_refpic_index(pp, ff_dxva2_get_surface_index(avctx, ctx, r->f));
                 fill_picture_entry(&slice->RefPicList[list][i], index,
-                                   r->reference == PICT_BOTTOM_FIELD);
+                                   sl->ref_list[list][i].reference == PICT_BOTTOM_FIELD);
                 for (plane = 0; plane < 3; plane++) {
                     int w, o;
                     if (plane == 0 && sl->luma_weight_flag[list]) {
