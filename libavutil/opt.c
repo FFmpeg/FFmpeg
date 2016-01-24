f3ace85d8869c3dddd2d28d064002d0d912e3624
Michael Niedermayer <michael@niedermayer.cc>
2016-01-24 03:42:46 +0100

    avutil/opt: check for and handle errors in av_opt_set_dict2()
    Previously errors could result in random entries to be lost.
    
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavutil/opt.c
+++ libavutil/opt.c
@@ -1476,10 +1476,11 @@ int av_opt_set_dict2(void *obj, AVDictionary **options, int search_flags)
     while ((t = av_dict_get(*options, "", t, AV_DICT_IGNORE_SUFFIX))) {
         ret = av_opt_set(obj, t->key, t->value, search_flags);
         if (ret == AVERROR_OPTION_NOT_FOUND)
-            av_dict_set(&tmp, t->key, t->value, 0);
-        else if (ret < 0) {
+            ret = av_dict_set(&tmp, t->key, t->value, 0);
+        if (ret < 0) {
             av_log(obj, AV_LOG_ERROR, "Error setting option %s to value %s.\n", t->key, t->value);
-            break;
+            av_dict_free(&tmp);
+            return ret;
         }
         ret = 0;
     }
