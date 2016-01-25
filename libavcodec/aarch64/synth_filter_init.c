209f50e16b5e66424d593ba4f9d4d8be5feff947
James Almer <jamrial@gmail.com>
2016-01-24 23:52:19 -0300

    avcodec/synth_filter: split off remaining code from dcadec files
    Signed-off-by: James Almer <jamrial@gmail.com>

--- libavcodec/aarch64/synth_filter_init.c
+++ libavcodec/aarch64/synth_filter_init.c
@@ -23,17 +23,25 @@
 #include "libavutil/aarch64/cpu.h"
 #include "libavutil/attributes.h"
 #include "libavutil/internal.h"
-#include "libavcodec/dcadsp.h"
+#include "libavcodec/fft.h"
+#include "libavcodec/synth_filter.h"
 
-void ff_dca_lfe_fir0_neon(float *out, const float *in, const float *coefs);
-void ff_dca_lfe_fir1_neon(float *out, const float *in, const float *coefs);
+#include "asm-offsets.h"
 
-av_cold void ff_dcadsp_init_aarch64(DCADSPContext *s)
+#if HAVE_NEON || HAVE_VFP
+AV_CHECK_OFFSET(FFTContext, imdct_half, IMDCT_HALF);
+#endif
+
+void ff_synth_filter_float_neon(FFTContext *imdct,
+                                float *synth_buf_ptr, int *synth_buf_offset,
+                                float synth_buf2[32], const float window[512],
+                                float out[32], const float in[32],
+                                float scale);
+
+av_cold void ff_synth_filter_init_aarch64(SynthFilterContext *s)
 {
     int cpu_flags = av_get_cpu_flags();
 
-    if (have_neon(cpu_flags)) {
-        s->lfe_fir[0] = ff_dca_lfe_fir0_neon;
-        s->lfe_fir[1] = ff_dca_lfe_fir1_neon;
-    }
+    if (have_neon(cpu_flags))
+        s->synth_filter_float = ff_synth_filter_float_neon;
 }
