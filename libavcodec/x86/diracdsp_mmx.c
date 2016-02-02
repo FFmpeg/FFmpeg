dd57b316c1094ac5da6b1ede8d3d763a8d64857f
Timothy Gu <timothygu99@gmail.com>
2016-02-01 20:47:56 -0800

    diracdsp_mmx: Fix some more indentations

--- libavcodec/x86/diracdsp_mmx.c
+++ libavcodec/x86/diracdsp_mmx.c
@@ -91,22 +91,22 @@ void ff_put_dirac_pixels16_sse2(uint8_t *dst, const uint8_t *src[5], int stride,
     if (h&3)
         ff_put_dirac_pixels16_c(dst, src, stride, h);
     else
-    ff_put_pixels16_sse2(dst, src[0], stride, h);
+        ff_put_pixels16_sse2(dst, src[0], stride, h);
 }
 void ff_avg_dirac_pixels16_sse2(uint8_t *dst, const uint8_t *src[5], int stride, int h)
 {
     if (h&3)
         ff_avg_dirac_pixels16_c(dst, src, stride, h);
     else
-    ff_avg_pixels16_sse2(dst, src[0], stride, h);
+        ff_avg_pixels16_sse2(dst, src[0], stride, h);
 }
 void ff_put_dirac_pixels32_sse2(uint8_t *dst, const uint8_t *src[5], int stride, int h)
 {
     if (h&3) {
         ff_put_dirac_pixels32_c(dst, src, stride, h);
     } else {
-    ff_put_pixels16_sse2(dst   , src[0]   , stride, h);
-    ff_put_pixels16_sse2(dst+16, src[0]+16, stride, h);
+        ff_put_pixels16_sse2(dst   , src[0]   , stride, h);
+        ff_put_pixels16_sse2(dst+16, src[0]+16, stride, h);
     }
 }
 void ff_avg_dirac_pixels32_sse2(uint8_t *dst, const uint8_t *src[5], int stride, int h)
@@ -114,8 +114,8 @@ void ff_avg_dirac_pixels32_sse2(uint8_t *dst, const uint8_t *src[5], int stride,
     if (h&3) {
         ff_avg_dirac_pixels32_c(dst, src, stride, h);
     } else {
-    ff_avg_pixels16_sse2(dst   , src[0]   , stride, h);
-    ff_avg_pixels16_sse2(dst+16, src[0]+16, stride, h);
+        ff_avg_pixels16_sse2(dst   , src[0]   , stride, h);
+        ff_avg_pixels16_sse2(dst+16, src[0]+16, stride, h);
     }
 }
 
