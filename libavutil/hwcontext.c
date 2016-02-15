551c6775abb5e0ad34c26d7e23bc6fbbe8ccc9d4
Mark Thompson <mrt@jkqxz.net>
2016-02-15 19:32:45 +0000

    lavu: VAAPI hwcontext implementation
    Signed-off-by: Anton Khirnov <anton@khirnov.net>

--- libavutil/hwcontext.c
+++ libavutil/hwcontext.c
@@ -32,6 +32,9 @@ static const HWContextType *hw_table[] = {
 #if CONFIG_CUDA
     &ff_hwcontext_type_cuda,
 #endif
+#if CONFIG_VAAPI
+    &ff_hwcontext_type_vaapi,
+#endif
 #if CONFIG_VDPAU
     &ff_hwcontext_type_vdpau,
 #endif
