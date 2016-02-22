76377d66b7b8dcd2e3dee2bf2b3f70257f6694f8
Muhammad Faiz <mfcc64@gmail.com>
2016-02-23 01:05:08 +0700

    avfilter/avf_showcqt: remove unneeded headers

--- libavfilter/avf_showcqt.c
+++ libavfilter/avf_showcqt.c
@@ -18,9 +18,6 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
-#include <math.h>
-#include <stdlib.h>
-
 #include "config.h"
 #include "libavcodec/avfft.h"
 #include "libavutil/avassert.h"
