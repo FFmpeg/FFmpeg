2ade1cdafb96bf47e77f7ed74731d78a30aae950
Vittorio Giovara <vittorio.giovara@gmail.com>
2016-02-24 17:45:11 -0500

    intrax8: K&R formatting cosmetics

--- libavcodec/intrax8.h
+++ libavcodec/intrax8.h
@@ -24,25 +24,29 @@
 #include "intrax8dsp.h"
 
 typedef struct IntraX8Context {
-    VLC * j_ac_vlc[4];//they point to the static j_mb_vlc
-    VLC * j_orient_vlc;
-    VLC * j_dc_vlc[3];
+    VLC *j_ac_vlc[4]; // they point to the static j_mb_vlc
+    VLC *j_orient_vlc;
+    VLC *j_dc_vlc[3];
 
     int use_quant_matrix;
-//set by ff_intrax8_common_init
-    uint8_t * prediction_table;//2*(mb_w*2)
+
+    // set by ff_intrax8_common_init
+    uint8_t *prediction_table; // 2 * (mb_w * 2)
     ScanTable scantable[3];
-//set by the caller codec
-    MpegEncContext * s;
+
+    // set by the caller codec
+    MpegEncContext *s;
     IntraX8DSPContext dsp;
     int quant;
     int dquant;
     int qsum;
-//calculated per frame
+
+    // calculated per frame
     int quant_dc_chroma;
     int divide_quant_dc_luma;
     int divide_quant_dc_chroma;
-//changed per block
+
+    // changed per block
     int edges;
     int flat_dc;
     int predicted_dc;
@@ -52,8 +56,8 @@ typedef struct IntraX8Context {
     int est_run;
 } IntraX8Context;
 
-void ff_intrax8_common_init(IntraX8Context * w, MpegEncContext * const s);
-void ff_intrax8_common_end(IntraX8Context * w);
-int  ff_intrax8_decode_picture(IntraX8Context * w, int quant, int halfpq);
+void ff_intrax8_common_init(IntraX8Context *w, MpegEncContext *const s);
+void ff_intrax8_common_end(IntraX8Context *w);
+int ff_intrax8_decode_picture(IntraX8Context *w, int quant, int halfpq);
 
 #endif /* AVCODEC_INTRAX8_H */
