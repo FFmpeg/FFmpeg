76019f5fed066f350fd8c9e55ec2c47ddb47f2a8
Michael Niedermayer <michael@niedermayer.cc>
2016-03-05 17:39:48 +0100

    fate: Add pal8 copy test for avi
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- tests/lavf-regression.sh
+++ tests/lavf-regression.sh
@@ -170,6 +170,10 @@ DEC_OPTS="$DEC_OPTS -idct auto"
 do_lavf_fate mov "qtrle/Animation-16Greys.mov"
 fi
 
+if [ -n "$do_avi_cram" ] ; then
+DEC_OPTS="$DEC_OPTS -idct auto"
+do_lavf_fate avi "cram/toon.avi"
+fi
 
 if [ -n "$do_wtv" ] ; then
 do_lavf wtv "" "-acodec mp2 -threads 1"
