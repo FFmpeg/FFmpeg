66edd8656b851a0c85ba25ec293cc66192c363ae
Ganesh Ajjanagadde <gajjanag@gmail.com>
2016-03-08 22:08:29 -0500

    lavc/lpc: exploit even symmetry of window function
    Yields 2x improvement in function performance, and boosts aac encoding
    speed by ~ 4% overall. Sample benchmark (Haswell+GCC under -march=native):
    after:
    ffmpeg -i sin.flac -acodec aac -y sin_new.aac  5.22s user 0.03s system 105% cpu 4.970 total
    
    before:
    ffmpeg -i sin.flac -acodec aac -y sin_new.aac  5.40s user 0.05s system 105% cpu 5.162 total
    
    Reviewed-by: Rostislav Pehlivanov <atomnuker@gmail.com>
    Signed-off-by: Ganesh Ajjanagadde <gajjanag@gmail.com>

--- libavcodec/lpc.c
+++ libavcodec/lpc.c
@@ -176,9 +176,10 @@ double ff_lpc_calc_ref_coefs_f(LPCContext *s, const float *samples, int len,
     const double a = 0.5f, b = 1.0f - a;
 
     /* Apply windowing */
-    for (i = 0; i < len; i++) {
+    for (i = 0; i <= len / 2; i++) {
         double weight = a - b*cos((2*M_PI*i)/(len - 1));
         s->windowed_samples[i] = weight*samples[i];
+        s->windowed_samples[len-1-i] = weight*samples[len-1-i];
     }
 
     s->lpc_compute_autocorr(s->windowed_samples, len, order, autoc);
