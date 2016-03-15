8ff0f6ae8253d394f096b39d69b66e5247ed1066
Marton Balint <cus@passwd.hu>
2016-03-15 19:19:20 +0100

    avformat/segment: set format options for all segments
    Fixes ticket #5318.
    
    Reviewed-by: Stefano Sabatini <stefasab@gmail.com>
    Signed-off-by: Marton Balint <cus@passwd.hu>

--- libavformat/segment.c
+++ libavformat/segment.c
@@ -256,7 +256,11 @@ static int segment_start(AVFormatContext *s, int write_header)
         av_opt_set(oc->priv_data, "mpegts_flags", "+resend_headers", 0);
 
     if (write_header) {
-        if ((err = avformat_write_header(oc, NULL)) < 0)
+        AVDictionary *options = NULL;
+        av_dict_copy(&options, seg->format_options, 0);
+        err = avformat_write_header(oc, &options);
+        av_dict_free(&options);
+        if (err < 0)
             return err;
     }
 
