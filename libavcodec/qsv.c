92c1a83ee9394b39d68f6affd9104752a03714f8
Luca Barbato <lu_zero@gentoo.org>
2016-03-15 14:10:05 +0100

    qsv: Fix loading multiple plugins
    av_get_token does not strip the trailing separator.

--- libavcodec/qsv.c
+++ libavcodec/qsv.c
@@ -133,6 +133,8 @@ int ff_qsv_init_internal_session(AVCodecContext *avctx, mfxSession *session,
                 goto load_plugin_fail;
             }
 
+            if (*load_plugins)
+                load_plugins++;
 load_plugin_fail:
             av_freep(&plugin);
             if (err < 0)
