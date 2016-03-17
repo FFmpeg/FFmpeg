20d89a3a32716d0e485e74d43fc202c5a9f18ec8
James Almer <jamrial@gmail.com>
2016-03-17 00:50:08 -0300

    libwebpenc_animencoder: zero initialize the WebPAnimEncoderOptions struct
    This zeroes the WebPAnimEncoderOptions.verbose field, silencing library info messages
    printed to stderr.
    
    Reviewed-by: James Zern <jzern@google.com>
    Signed-off-by: James Almer <jamrial@gmail.com>
    (cherry picked from commit 626b6b769ced6d3e55d2661985ab2a1cb89f481e)

--- libavcodec/libwebpenc_animencoder.c
+++ libavcodec/libwebpenc_animencoder.c
@@ -41,7 +41,7 @@ static av_cold int libwebp_anim_encode_init(AVCodecContext *avctx)
     int ret = ff_libwebp_encode_init_common(avctx);
     if (!ret) {
         LibWebPAnimContext *s = avctx->priv_data;
-        WebPAnimEncoderOptions enc_options;
+        WebPAnimEncoderOptions enc_options = { 0 };
         WebPAnimEncoderOptionsInit(&enc_options);
         // TODO(urvang): Expose some options on command-line perhaps.
         s->enc = WebPAnimEncoderNew(avctx->width, avctx->height, &enc_options);
