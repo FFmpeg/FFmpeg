8284a4ba11e2d8ca55e3222e5730bfa8d6cca940
Clément Bœsch <u@pkh.me>
2016-03-20 16:14:22 +0100

    lavf/dv: use c->sys->frame_size in dv_frame_offset()
    dv_frame_offset() is static and called only from dv_read_seek(), where
    c->sys->frame_size is already used.
    
    This simplifies the incoming codecpar merge where
    avctx->{coded_width,coded_height,time_base} are not accessible anymore.

--- libavformat/dv.c
+++ libavformat/dv.c
@@ -418,13 +418,12 @@ static int64_t dv_frame_offset(AVFormatContext *s, DVDemuxContext *c,
                                int64_t timestamp, int flags)
 {
     // FIXME: sys may be wrong if last dv_read_packet() failed (buffer is junk)
-    const AVDVProfile *sys = av_dv_codec_profile2(c->vst->codec->coded_width, c->vst->codec->coded_height,
-                                                  c->vst->codec->pix_fmt, c->vst->codec->time_base);
+    const int frame_size = c->sys->frame_size;
     int64_t offset;
     int64_t size       = avio_size(s->pb) - s->internal->data_offset;
-    int64_t max_offset = ((size - 1) / sys->frame_size) * sys->frame_size;
+    int64_t max_offset = ((size - 1) / frame_size) * frame_size;
 
-    offset = sys->frame_size * timestamp;
+    offset = frame_size * timestamp;
 
     if (size >= 0 && offset > max_offset)
         offset = max_offset;
