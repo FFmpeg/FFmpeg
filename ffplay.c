de8d77ed1c217d3c1d1b9c66a049d588cc5b6c1c
Lawrence Neal <nealla@lwneal.com>
2016-04-01 15:25:49 -0700

    Add example colored labels to video timeline

--- ffplay.c
+++ ffplay.c
@@ -364,29 +364,68 @@ static AVPacket flush_pkt;
 static SDL_Surface *screen;
 
 
+////////////////////////
+// TODO: Move the following into its own module
+typedef struct {
+    int label;
+    double start_time;
+    double duration;
+} TimePeriod;
+TimePeriod positive_labels[4] = {
+    {1, 1.0, 4.0},
+    {2, 8.0, 2.0},
+    {3, 11.0, 3.0},
+    {2, 16.0, 10.0}
+};
+
+static void index_to_yuv(int index, uint8_t *y, uint8_t *u, uint8_t *v) {
+    if (index < 0) {
+        *y = 64;
+        *u = *v = 128;
+    } else if (index == 0) {
+        *y = 192;
+        *u = *v = 128;
+    } else {
+        *y = (128 + index * 79) % 256;
+        *u = (index * 71) % 256;
+        *v = (255 - index * 193) % 256;
+    }
+}
+
 static int draw_larry_stuff(AVFrame *frame, double pts, double duration) {
     // Draw a progress bar at the bottom of the video
     int progress_bar_height = frame->width/2 > 32 ? 32 : frame->width/2;
     double progress = pts / duration;
     fprintf(stderr, "PTS %f duration %f progress %f\n", pts, duration, progress);
-    uint8_t **data = frame->data;
-    for (int x = 0; x < progress * frame->width; x++) {
-        for (int y = frame->height - progress_bar_height; y < frame->height; y++) {
-            int luma = data[0][x + y * frame->linesize[0]];
-            int ux = x/2, uy = y/2;
-            int u = data[1][ux + uy * frame->linesize[1]];
-            int v = data[2][ux + uy * frame->linesize[2]];
 
-            luma = 255 - luma;
-            u = 255 - u;
-            v = 255 - v;
-            data[0][x + y * frame->linesize[0]] = luma;
-            data[1][ux + uy * frame->linesize[1]] = u;
-            data[2][ux + uy * frame->linesize[2]] = v;
+    int *progress_bar_color = calloc(frame->width, sizeof(int));
+    for (int x = 0; x < progress * frame->width; x++) {
+        progress_bar_color[x] = -1;
+    }
+    for (int x = progress * frame->width; x < frame->width; x++) {
+        progress_bar_color[x] = 0;
+    }
+    for (int i = 0; i < 4; i++) {
+        int left = frame->width * (positive_labels[i].start_time / duration);
+        int right = left + frame->width * (positive_labels[i].duration / duration);
+        for (int x = fmax(0, left); x < fmin(right, frame->width); x++) {
+            progress_bar_color[x] = positive_labels[i].label;
         }
     }
+
+    for (int x = 0; x < frame->width; x++) {
+        for (int y = frame->height - progress_bar_height; y < frame->height; y++) {
+            int ux = x/2, uy = y/2;
+            index_to_yuv(progress_bar_color[x],
+                &frame->data[0][x + y * frame->linesize[0]],
+                &frame->data[1][ux + uy * frame->linesize[1]], 
+                &frame->data[2][ux + uy * frame->linesize[2]]);
+        }
+    }
+    free(progress_bar_color);
     return 0;
 }
+////////////////////////
 
 #if CONFIG_AVFILTER
 static int opt_add_vfilter(void *optctx, const char *opt, const char *arg)
