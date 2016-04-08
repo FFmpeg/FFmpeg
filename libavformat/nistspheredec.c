259879d32d124c60ef868e17e419c7c7c287674a
Paul B Mahol <onemda@gmail.com>
2016-04-08 13:19:00 +0200

    avformat/nistspheredec: fix silly bug
    Signed-off-by: Paul B Mahol <onemda@gmail.com>

--- libavformat/nistspheredec.c
+++ libavformat/nistspheredec.c
@@ -112,7 +112,7 @@ static int nist_read_header(AVFormatContext *s)
             sscanf(buffer, "%*s %*s %"SCNd32, &st->codec->bits_per_coded_sample);
         } else {
             char key[32], value[32];
-            if (sscanf(buffer, "%31s %*s %31s", key, value) == 3) {
+            if (sscanf(buffer, "%31s %*s %31s", key, value) == 2) {
                 av_dict_set(&s->metadata, key, value, AV_DICT_APPEND);
             } else {
                 av_log(s, AV_LOG_ERROR, "Failed to parse '%s' as metadata\n", buffer);
