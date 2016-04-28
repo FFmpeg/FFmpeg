76fd8145a1e24e95eea9af70a3a25e518e2d0be1
Michael Niedermayer <michael@niedermayer.cc>
2016-04-28 20:49:13 +0200

    avfilter/af_amix: dont fail if there are no samples in output_frame()
    Fixes Ticket5326
    
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit abc957e896beb3ce33c5691b9b3701993a381852)
    
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavfilter/af_amix.c
+++ libavfilter/af_amix.c
@@ -276,6 +276,9 @@ static int output_frame(AVFilterLink *outlink, int nb_samples)
 
     calculate_scales(s, nb_samples);
 
+    if (nb_samples == 0)
+        return 0;
+
     out_buf = ff_get_audio_buffer(outlink, nb_samples);
     if (!out_buf)
         return AVERROR(ENOMEM);
