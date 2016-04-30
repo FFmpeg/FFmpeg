9a499e0ae7dd271e7e718bfe787e248bf3b4011f
Michael Niedermayer <michael@niedermayer.cc>
2016-05-01 01:26:27 +0200

    avformat/riff: add m704
    (Ticket 2616)
    
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavformat/riff.c
+++ libavformat/riff.c
@@ -170,6 +170,7 @@ const AVCodecTag ff_codec_bmp_tags[] = {
     /* Matrox MPEG-2 intra-only */
     { AV_CODEC_ID_MPEG2VIDEO,   MKTAG('M', '7', '0', '1') },
     { AV_CODEC_ID_MPEG2VIDEO,   MKTAG('M', '7', '0', '3') },
+    { AV_CODEC_ID_MPEG2VIDEO,   MKTAG('M', '7', '0', '4') },
     { AV_CODEC_ID_MPEG2VIDEO,   MKTAG('M', '7', '0', '5') },
     { AV_CODEC_ID_MPEG2VIDEO,   MKTAG('m', 'p', 'g', 'v') },
     { AV_CODEC_ID_MPEG1VIDEO,   MKTAG('B', 'W', '1', '0') },
