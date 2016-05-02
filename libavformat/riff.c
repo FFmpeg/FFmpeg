dc34fa6a9e643131e04b067aa33d8609ac419ad3
Michael Niedermayer <michael@niedermayer.cc>
2016-05-03 00:39:55 +0200

    avformat/riff: add M702
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavformat/riff.c
+++ libavformat/riff.c
@@ -169,6 +169,7 @@ const AVCodecTag ff_codec_bmp_tags[] = {
     { AV_CODEC_ID_MPEG2VIDEO,   MKTAG('E', 'M', '2', 'V') },
     /* Matrox MPEG-2 intra-only */
     { AV_CODEC_ID_MPEG2VIDEO,   MKTAG('M', '7', '0', '1') },
+    { AV_CODEC_ID_MPEG2VIDEO,   MKTAG('M', '7', '0', '2') },
     { AV_CODEC_ID_MPEG2VIDEO,   MKTAG('M', '7', '0', '3') },
     { AV_CODEC_ID_MPEG2VIDEO,   MKTAG('M', '7', '0', '4') },
     { AV_CODEC_ID_MPEG2VIDEO,   MKTAG('M', '7', '0', '5') },
