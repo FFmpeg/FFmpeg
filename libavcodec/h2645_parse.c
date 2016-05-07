d46e85635070fd2c4cd7cd4b97720e5868c1bfc1
Hendrik Leppkes <h.leppkes@gmail.com>
2016-05-07 16:28:39 +0200

    h265_parse: skip zero sized NAL units
    Avoids extra error checks later on and/or invalid reads.
    
    Signed-off-by: Derek Buitenhuis <derek.buitenhuis@gmail.com>

--- libavcodec/h2645_parse.c
+++ libavcodec/h2645_parse.c
@@ -333,7 +333,7 @@ int ff_h2645_packet_split(H2645Packet *pkt, const uint8_t *buf, int length,
             ret = hevc_parse_nal_header(nal, logctx);
         else
             ret = h264_parse_nal_header(nal, logctx);
-        if (ret <= 0) {
+        if (ret <= 0 || nal->size <= 0) {
             if (ret < 0) {
                 av_log(logctx, AV_LOG_ERROR, "Invalid NAL unit %d, skipping.\n",
                        nal->type);
