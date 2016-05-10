3bbef6082aee8effda8e3ed9c8083739e752b7e4
Michael Niedermayer <michael@niedermayer.cc>
2016-05-10 23:12:58 +0200

    avformat/oggparseopus: Check that granule pos is within the supported range
    Larger values would imply file durations of astronomic proportions and cause
    overflows
    
    Fixes integer overflow
    Fixes: usan_int64_overflow
    
    Found-by: Thomas Guilbert <tguilbert@google.com>
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit 8efaee3710baa87af40556a622bf2d96a27c6425)
    
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavformat/oggparseopus.c
+++ libavformat/oggparseopus.c
@@ -117,6 +117,10 @@ static int opus_packet(AVFormatContext *avf, int idx)
 
     if (!os->psize)
         return AVERROR_INVALIDDATA;
+    if (os->granule > INT64_MAX - UINT32_MAX) {
+        av_log(avf, AV_LOG_ERROR, "Unsupported huge granule pos %"PRId64 "\n", os->granule);
+        return AVERROR_INVALIDDATA;
+    }
 
     if ((!os->lastpts || os->lastpts == AV_NOPTS_VALUE) && !(os->flags & OGG_FLAG_EOS)) {
         int seg, d;
