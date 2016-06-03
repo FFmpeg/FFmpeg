a2e6c785cec33d15d1bfde375447b1915cb30ca9
Michael Niedermayer <michael@niedermayer.cc>
2016-06-03 23:59:36 +0200

    avformat/rawdec: Fix avg_framerate for h264
    The framerate is 25 which is a fixed default and is wrong undo the 1 line
    change which caused this regression
    Only the avg_frame rate setting is removed
    The timebase update is not done as there was a objection (see ML)
    Fixes Ticket 5444
    
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavformat/rawdec.c
+++ libavformat/rawdec.c
@@ -84,7 +84,6 @@ int ff_raw_video_read_header(AVFormatContext *s)
     st->codecpar->codec_id = s->iformat->raw_codec_id;
     st->need_parsing = AVSTREAM_PARSE_FULL_RAW;
 
-    st->avg_frame_rate = s1->framerate;
     st->internal->avctx->framerate = s1->framerate;
     avpriv_set_pts_info(st, 64, 1, 1200000);
 
