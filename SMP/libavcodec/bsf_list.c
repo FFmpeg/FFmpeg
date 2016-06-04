d0f5651c2ea17ae53df84c5ca08ad30aae3c181c
Matt Oliver <protogonoi@gmail.com>
2016-06-05 06:40:28 +1000

    projects: Update for recent upstream merge.

---
+++ SMP/libavcodec/bsf_list.c
@@ -0,0 +1,18 @@
+static const AVBitStreamFilter *bitstream_filters[] = {
+    &ff_aac_adtstoasc_bsf,
+    &ff_chomp_bsf,
+    &ff_dump_extradata_bsf,
+    &ff_dca_core_bsf,
+    &ff_h264_mp4toannexb_bsf,
+    &ff_hevc_mp4toannexb_bsf,
+    &ff_imx_dump_header_bsf,
+    &ff_mjpeg2jpeg_bsf,
+    &ff_mjpega_dump_header_bsf,
+    &ff_mp3_header_decompress_bsf,
+    &ff_mpeg4_unpack_bframes_bsf,
+    &ff_mov2textsub_bsf,
+    &ff_noise_bsf,
+    &ff_remove_extradata_bsf,
+    &ff_text2movsub_bsf,
+    &ff_vp9_superframe_bsf,
+    NULL };
