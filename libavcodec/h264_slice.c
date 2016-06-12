4a8ed314fb388b8e6bc31c373474c05cfb09061c
Michael Niedermayer <michael@niedermayer.cc>
2016-06-12 21:28:10 +0200

    avcodec/h264_slice: Add casts to suppress warnings
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/h264_slice.c
+++ libavcodec/h264_slice.c
@@ -354,13 +354,13 @@ int ff_h264_update_thread_context(AVCodecContext *dst,
         h->ps.pps_ref = av_buffer_ref(h1->ps.pps_ref);
         if (!h->ps.pps_ref)
             return AVERROR(ENOMEM);
-        h->ps.pps = h->ps.pps_ref->data;
+        h->ps.pps = (const PPS*)h->ps.pps_ref->data;
     }
     if (h1->ps.sps_ref) {
         h->ps.sps_ref = av_buffer_ref(h1->ps.sps_ref);
         if (!h->ps.sps_ref)
             return AVERROR(ENOMEM);
-        h->ps.sps = h->ps.sps_ref->data;
+        h->ps.sps = (SPS*)h->ps.sps_ref->data;
     }
 
     if (need_reinit || !inited) {
