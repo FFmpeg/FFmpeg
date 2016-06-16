afd04058bc910e4a3b1017e03ea543d8be395818
James Almer <jamrial@gmail.com>
2016-06-16 19:17:26 -0300

    avformat/oggparsevorbis: free base64 encoded data immediately after decoding it
    It has no use afterwards and freeing it before calling ff_flac_parse_picture()
    may help prevent OOM issues on memory constrained scenarios.
    
    Signed-off-by: James Almer <jamrial@gmail.com>

--- libavformat/oggparsevorbis.c
+++ libavformat/oggparsevorbis.c
@@ -161,10 +161,11 @@ int ff_vorbis_comment(AVFormatContext *as, AVDictionary **m,
                     av_freep(&ct);
                     continue;
                 }
-                if ((ret = av_base64_decode(pict, ct, len)) > 0)
-                    ret = ff_flac_parse_picture(as, pict, ret);
+                ret = av_base64_decode(pict, ct, len);
                 av_freep(&tt);
                 av_freep(&ct);
+                if (ret > 0)
+                    ret = ff_flac_parse_picture(as, pict, ret);
                 av_freep(&pict);
                 if (ret < 0) {
                     av_log(as, AV_LOG_WARNING, "Failed to parse cover art block.\n");
