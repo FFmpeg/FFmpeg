bfbffbd7d8fd014a3693c51d7cde9926315a9fa0
rogerdpack <rogerpack2005@gmail.com>
2016-06-17 14:04:34 -0600

    dshow: avoid duplicate NULL check before free
    Signed-off-by: rogerdpack <rogerpack2005@gmail.com>
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavdevice/dshow.c
+++ libavdevice/dshow.c
@@ -104,14 +104,10 @@ dshow_read_close(AVFormatContext *s)
     if (ctx->device_filter[AudioDevice])
         IBaseFilter_Release(ctx->device_filter[AudioDevice]);
 
-    if (ctx->device_name[0])
-        av_freep(&ctx->device_name[0]);
-    if (ctx->device_name[1])
-        av_freep(&ctx->device_name[1]);
-    if (ctx->device_unique_name[0])
-        av_freep(&ctx->device_unique_name[0]);
-    if (ctx->device_unique_name[1])
-        av_freep(&ctx->device_unique_name[1]);
+    av_freep(&ctx->device_name[0]);
+    av_freep(&ctx->device_name[1]);
+    av_freep(&ctx->device_unique_name[0]);
+    av_freep(&ctx->device_unique_name[1]);
 
     if(ctx->mutex)
         CloseHandle(ctx->mutex);
