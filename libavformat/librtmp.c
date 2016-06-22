4dbfcd07570a9e45e9597561023adb6da26f27f6
Luca Barbato <lu_zero@gentoo.org>
2016-06-22 06:36:31 +0200

    librtmp: Avoid an infiniloop setting connection arguments
    The exit condition was missing.
    
    CC: libav-stable@libav.org

--- libavformat/librtmp.c
+++ libavformat/librtmp.c
@@ -189,6 +189,8 @@ static int rtmp_open(URLContext *s, const char *uri, int flags)
 
             if (sep)
                 p = sep + 1;
+            else
+                break;
         }
     }
     if (ctx->playpath) {
