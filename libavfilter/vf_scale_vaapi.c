582d4211e00015b68626f77ce4af53161e2b1713
Mark Thompson <sw@jkqxz.net>
2016-06-26 22:35:49 +0100

    vf_scale_vaapi: Respect driver quirks around buffer destruction

--- libavfilter/vf_scale_vaapi.c
+++ libavfilter/vf_scale_vaapi.c
@@ -342,13 +342,14 @@ static int scale_vaapi_filter_frame(AVFilterLink *inlink, AVFrame *input_frame)
         goto fail_after_render;
     }
 
-    // This doesn't get freed automatically for some reason.
-    vas = vaDestroyBuffer(ctx->hwctx->display, params_id);
-    if (vas != VA_STATUS_SUCCESS) {
-        av_log(ctx, AV_LOG_ERROR, "Failed to free parameter buffer: "
-               "%d (%s).\n", vas, vaErrorStr(vas));
-        err = AVERROR(EIO);
-        goto fail;
+    if (ctx->hwctx->driver_quirks &
+        AV_VAAPI_DRIVER_QUIRK_RENDER_PARAM_BUFFERS) {
+        vas = vaDestroyBuffer(ctx->hwctx->display, params_id);
+        if (vas != VA_STATUS_SUCCESS) {
+            av_log(ctx, AV_LOG_ERROR, "Failed to free parameter buffer: "
+                   "%d (%s).\n", vas, vaErrorStr(vas));
+            // And ignore.
+        }
     }
 
     av_frame_copy_props(output_frame, input_frame);
