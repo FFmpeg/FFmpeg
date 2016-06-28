9632ccc7d82c9a295530fac73616c66fdb149320
Matt Oliver <protogonoi@gmail.com>
2016-06-28 16:48:49 +1000

    project: Allow get dependency script to optionally download prebuilt releases.

--- SMP/project_get_dependencies.bat
+++ SMP/project_get_dependencies.bat
@@ -4,7 +4,6 @@ SETLOCAL EnableDelayedExpansion
 SET UPSTREAMURL=https://github.com/ShiftMediaProject
 SET DEPENDENCIES=( ^
 bzip2, ^
-fdk-aac, ^
 fontconfig, ^
 freetype2, ^
 fribidi, ^
@@ -35,22 +34,32 @@ xvid, ^
 zlib ^
 )
 
-REM Get passed in list of dependencies to skip
+RREM Get passed in list of dependencies to skip
 SET PASSDEPENDENCIES=%~1
 
 REM Check if git is installed and available
-git status >NUL 2>&1
-IF %ERRORLEVEL% NEQ 0 (
-  ECHO A working copy of git was not found. To use this script you must first install git for windows.
-  EXIT /B 1
+IF "%MSVC_VER%"=="" (
+    git status >NUL 2>&1
+    IF %ERRORLEVEL% NEQ 0 (
+      ECHO A working copy of git was not found. To use this script you must first install git for windows.
+      EXIT /B 1
+    )
 )
 
-SET CURRDIR=%~dp1
+REM Store current directory and ensure working directory is the location of current .bat
+SET CURRDIR=%CD%
+cd %~dp0
 
 cd ..\..
 FOR %%I IN %DEPENDENCIES% DO (
     ECHO !PASSDEPENDENCIES! | FINDSTR /C:"%%I" >NUL 2>&1 || (
-        CALL :cloneOrUpdateRepo "%%I" )
+        REM Check if MSVC_VER environment variable is set
+        IF "%MSVC_VER%"=="" (
+            CALL :cloneOrUpdateRepo "%%I"
+        ) ELSE (
+            CALL :downloadLibs "%%I"
+        )
+    )
 )
 cd %CURRDIR% >NUL
 GOTO exit
@@ -104,19 +113,59 @@ IF EXIST "%REPONAME%\SMP\project_get_dependencies.bat" (
 ECHO.
 EXIT /B %ERRORLEVEL%
 
+REM Function to download existing prebuilt libraries
+REM  downloadLibs: RepoName
+REM    RepoName = Name of the repository
+:downloadLibs
+SET REPONAME=%~1
+REM Get latest release
+ECHO %REPONAME%: Getting latest release...
+SET UPSTREAMAPIURL=%UPSTREAMURL:github.com=api.github.com/repos%
+powershell -nologo -noprofile -command "try { Invoke-RestMethod -Uri %UPSTREAMAPIURL%/%REPONAME%/releases/latest > latest.json } catch {exit 1}"
+IF NOT %ERRORLEVEL% == 0 ( ECHO Failed getting latest %REPONAME% release & GOTO exitOnError )
+REM Get tag for latest release
+FOR /F "tokens=* USEBACKQ" %%F IN (`TYPE latest.json ^| FINDSTR /B "tag_name"`) DO SET TAG=%%F
+FOR /F "tokens=2 delims=: " %%F in ("%TAG%") DO SET TAG=%%F
+IF "%TAG%"=="" ( ECHO Failed getting latest %REPONAME% release tag information & GOTO exitOnError )
+REM Get download name of latest release
+FOR /F "tokens=* USEBACKQ" %%F IN (`TYPE latest.json ^| FINDSTR "name="`) DO SET LIBNAME=%%F
+SET LIBNAME=%LIBNAME:*name=%
+FOR /F "tokens=1 delims=_" %%F in ("%LIBNAME:~1%") DO SET LIBNAME=%%F
+IF "%LIBNAME%"=="" ( ECHO Failed getting latest %REPONAME% release name information & GOTO exitOnError )
+DEL /F /Q latest.json
+REM Get the download location for the required tag
+SET TAG2=%TAG:+=.%
+SET DLURL=%UPSTREAMURL%/%REPONAME%/releases/download/%TAG%/%LIBNAME%_%TAG2%_msvc%MSVC_VER%.zip
+REM Download a pre-built archive and extract
+ECHO %REPONAME%: Downloading %LIBNAME%_%TAG%_msvc%MSVC_VER%.zip...
+SET PREBUILTDIR=prebuilt
+MKDIR %PREBUILTDIR% >NUL 2>&1
+powershell -nologo -noprofile -command "try { (New-Object Net.WebClient).DownloadFile('%DLURL%', '%PREBUILTDIR%\temp.zip') } catch {exit 1}"
+IF NOT %ERRORLEVEL% == 0 ( ECHO Failed downloading %DLURL% & GOTO exitOnError )
+powershell -nologo -noprofile -command "try { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('%PREBUILTDIR%\temp.zip', '%PREBUILTDIR%'); } catch [System.IO.IOException] {exit 0} catch {exit 1}"
+IF NOT %ERRORLEVEL% == 0 ( ECHO Failed extracting downloaded archive & GOTO exitOnError )
+DEL /F /Q %PREBUILTDIR%\\temp.zip
+ECHO.
+EXIT /B %ERRORLEVEL%
+
 :exitOnError
 cd %CURRDIR%
 
 :exit
+REM Directly exit if an AppVeyor build
+IF NOT "%APPVEYOR%"=="" (
+    GOTO return
+)
 REM Return the passed dependency list
 (
     ENDLOCAL
     SET PASSDEPENDENCIES=%PASSDEPENDENCIES%
 )
     
-REM Check if this was laucnhed from an existing terminal or directly from .bat
+REM Check if this was launched from an existing terminal or directly from .bat
 REM  If launched by executing the .bat then pause on completion
 ECHO %CMDCMDLINE% | FINDSTR /L %COMSPEC% >NUL 2>&1
 IF %ERRORLEVEL% == 0 IF "%~1"=="" PAUSE
 
+:return
 EXIT /B 0
