f755aa5ebdb8cbc036dd7841d2574906b7ce4195
Clément Bœsch <clement@stupeflix.com>
2016-06-29 11:41:07 +0200

    lavc: move 2 more BITSTREAM_READER_LE definitions
    This was mentioned to be in ae753dbd0de448b51f0e0b980c26b94747ec44f5 but
    actually wasn't.

--- libavcodec/truemotion2rt.c
+++ libavcodec/truemotion2rt.c
@@ -22,8 +22,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "avcodec.h"
 #define BITSTREAM_READER_LE
+#include "avcodec.h"
 #include "get_bits.h"
 #include "internal.h"
 #include "libavutil/imgutils.h"
