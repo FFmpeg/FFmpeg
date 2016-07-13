726501a34ea4bf2222ca8e856ba6cf107b48465f
Ronald S. Bultje <rsbultje@gmail.com>
2016-07-13 12:37:21 -0400

    vp9: add 32x32 idct AVX2 implementation.
    About 1.8x speedup compared to AVX version for full IDCT. Other
    sub-IDCT scenarios also see speedups. Full --bench output for
    idct_32x32_add_{bpp}_${subidct}_${opt} (50k cycles):
    
    nop: 16.5
    vp9_inv_dct_dct_32x32_add_8_1_c: 2284.4
    vp9_inv_dct_dct_32x32_add_8_1_sse2: 145.0
    vp9_inv_dct_dct_32x32_add_8_1_ssse3: 137.4
    vp9_inv_dct_dct_32x32_add_8_1_avx: 137.1
    vp9_inv_dct_dct_32x32_add_8_1_avx2: 73.2
    vp9_inv_dct_dct_32x32_add_8_2_c: 14680.8
    vp9_inv_dct_dct_32x32_add_8_2_sse2: 2617.2
    vp9_inv_dct_dct_32x32_add_8_2_ssse3: 982.9
    vp9_inv_dct_dct_32x32_add_8_2_avx: 958.5
    vp9_inv_dct_dct_32x32_add_8_2_avx2: 704.2
    vp9_inv_dct_dct_32x32_add_8_4_c: 14443.1
    vp9_inv_dct_dct_32x32_add_8_4_sse2: 2717.1
    vp9_inv_dct_dct_32x32_add_8_4_ssse3: 965.7
    vp9_inv_dct_dct_32x32_add_8_4_avx: 1000.7
    vp9_inv_dct_dct_32x32_add_8_4_avx2: 717.1
    vp9_inv_dct_dct_32x32_add_8_8_c: 14436.4
    vp9_inv_dct_dct_32x32_add_8_8_sse2: 2671.8
    vp9_inv_dct_dct_32x32_add_8_8_ssse3: 1038.5
    vp9_inv_dct_dct_32x32_add_8_8_avx: 983.0
    vp9_inv_dct_dct_32x32_add_8_8_avx2: 729.4
    vp9_inv_dct_dct_32x32_add_8_16_c: 14614.7
    vp9_inv_dct_dct_32x32_add_8_16_sse2: 2701.7
    vp9_inv_dct_dct_32x32_add_8_16_ssse3: 1334.4
    vp9_inv_dct_dct_32x32_add_8_16_avx: 1276.7
    vp9_inv_dct_dct_32x32_add_8_16_avx2: 719.5
    vp9_inv_dct_dct_32x32_add_8_32_c: 14363.6
    vp9_inv_dct_dct_32x32_add_8_32_sse2: 2575.6
    vp9_inv_dct_dct_32x32_add_8_32_ssse3: 2633.9
    vp9_inv_dct_dct_32x32_add_8_32_avx: 2539.6
    vp9_inv_dct_dct_32x32_add_8_32_avx2: 1395.0

--- libavcodec/x86/vp9dsp_init.c
+++ libavcodec/x86/vp9dsp_init.c
@@ -115,6 +115,7 @@ itxfm_func(idct, idct, 32, ssse3);
 itxfm_func(idct, idct, 32, avx);
 itxfm_func(iwht, iwht, 4, mmx);
 itxfm_func(idct, idct, 16, avx2);
+itxfm_func(idct, idct, 32, avx2);
 
 #undef itxfm_func
 #undef itxfm_funcs
@@ -384,6 +385,7 @@ av_cold void ff_vp9dsp_init_x86(VP9DSPContext *dsp, int bpp, int bitexact)
         if (ARCH_X86_64) {
 #if ARCH_X86_64 && HAVE_AVX2_EXTERNAL
             dsp->itxfm_add[TX_16X16][DCT_DCT] = ff_vp9_idct_idct_16x16_add_avx2;
+            dsp->itxfm_add[TX_32X32][DCT_DCT] = ff_vp9_idct_idct_32x32_add_avx2;
             init_subpel3_32_64(0, put, 8, avx2);
             init_subpel3_32_64(1, avg, 8, avx2);
 #endif
