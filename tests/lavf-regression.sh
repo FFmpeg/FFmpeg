120f34b6ac062568a395ebc9490c7d213c264087
James Almer <jamrial@gmail.com>
2016-07-20 22:29:54 -0300

    avformat/oggenc: add vp8 muxing support
    Addresses ticket #5687
    
    Signed-off-by: James Almer <jamrial@gmail.com>

--- tests/lavf-regression.sh
+++ tests/lavf-regression.sh
@@ -165,6 +165,10 @@ DEC_OPTS="$DEC_OPTS -idct auto"
 do_lavf_fate ogg "vp3/coeff_level64.mkv"
 fi
 
+if [ -n "$do_ogg_vp8" ] ; then
+do_lavf_fate ogv "vp8/RRSF49-short.webm" "-acodec copy"
+fi
+
 if [ -n "$do_mov_qtrle_mace6" ] ; then
 DEC_OPTS="$DEC_OPTS -idct auto"
 do_lavf_fate mov "qtrle/Animation-16Greys.mov"
