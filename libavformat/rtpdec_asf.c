73e09e371bda2c7ee738092f4d4dd252b34de904
Kacper Michajłow <kasper93@gmail.com>
2016-07-23 23:47:39 +0200

    libavformat/rtpdec_asf: zero initialize the AVIOContext struct
    This fixes crash in avformat_open_input() when accessing
    protocol_whitelist field.
    
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit e947b75b1c76ef6793209c2c445b8c224a28717a)
    
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavformat/rtpdec_asf.c
+++ libavformat/rtpdec_asf.c
@@ -101,7 +101,7 @@ int ff_wms_parse_sdp_a_line(AVFormatContext *s, const char *p)
 {
     int ret = 0;
     if (av_strstart(p, "pgmpu:data:application/vnd.ms.wms-hdr.asfv1;base64,", &p)) {
-        AVIOContext pb;
+        AVIOContext pb = { 0 };
         RTSPState *rt = s->priv_data;
         AVDictionary *opts = NULL;
         int len = strlen(p) * 6 / 8;
