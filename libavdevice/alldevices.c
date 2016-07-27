4fef648d10bf3bcfd4b8fa5755c1128966a2427c
Diego Biurrun <diego@biurrun.de>
2016-07-27 11:25:19 +0200

    Remove the legacy X11 screen grabber
    The XCB screen grabber is a drop-in replacement and not under GPL.

--- libavdevice/alldevices.c
+++ libavdevice/alldevices.c
@@ -58,7 +58,6 @@ void avdevice_register_all(void)
     REGISTER_INOUTDEV(SNDIO,            sndio);
     REGISTER_INDEV   (V4L2,             v4l2);
     REGISTER_INDEV   (VFWCAP,           vfwcap);
-    REGISTER_INDEV   (X11GRAB,          x11grab);
     REGISTER_INDEV   (X11GRAB_XCB,      x11grab_xcb);
 
     /* external libraries */
