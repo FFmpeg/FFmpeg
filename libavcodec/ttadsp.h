d950279cbfede7270c6d55d0dd6eed35a962fd86
James Almer <jamrial@gmail.com>
2016-08-06 18:27:01 -0300

    avcodec/ttadsp: cosmetics
    Clean some header includes and use the same naming scheme as
    in ttaencdsp
    
    Signed-off-by: James Almer <jamrial@gmail.com>

--- libavcodec/ttadsp.h
+++ libavcodec/ttadsp.h
@@ -20,12 +20,11 @@
 #define AVCODEC_TTADSP_H
 
 #include <stdint.h>
-#include "ttadata.h"
 
 typedef struct TTADSPContext {
-    void (*ttafilter_process_dec)(int32_t *qm, int32_t *dx, int32_t *dl,
-                                  int32_t *error, int32_t *in, int32_t shift,
-                                  int32_t round);
+    void (*filter_process)(int32_t *qm, int32_t *dx, int32_t *dl,
+                           int32_t *error, int32_t *in, int32_t shift,
+                           int32_t round);
 } TTADSPContext;
 
 void ff_ttadsp_init(TTADSPContext *c);
