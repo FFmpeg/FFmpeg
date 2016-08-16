b438c2025c3e277bb506a3a7b8ec8a0d52ed3b07
Paul B Mahol <onemda@gmail.com>
2016-08-16 18:03:56 +0200

    avfilter/window_func: add cauchy, parzen and poisson window function

--- libavfilter/window_func.c
+++ libavfilter/window_func.c
@@ -128,6 +128,50 @@ void ff_generate_window_func(float *lut, int N, int win_func, float *overlap)
         }
         *overlap = 0.5;}
         break;
+    case WFUNC_CAUCHY:
+        for (n = 0; n < N; n++) {
+            double x = 2 * ((n / (double)(N - 1)) - .5);
+
+            if (x <= -.5 || x >= .5) {
+                lut[n] = 0;
+            } else {
+                lut[n] = FFMIN(1, fabs(1/(1+4*16*x*x)));
+            }
+        }
+        *overlap = 0.75;
+        break;
+    case WFUNC_PARZEN:
+        for (n = 0; n < N; n++) {
+            double x = 2 * ((n / (double)(N - 1)) - .5);
+
+            if (x > 0.25 && x <= 0.5) {
+                lut[n] = -2 * powf(-1 + 2 * x, 3);
+            } else if (x >= -.5 && x < -.25) {
+                lut[n] = 2 * powf(1 + 2 * x, 3);
+            } else if (x >= -.25 && x < 0) {
+                lut[n] = 1 - 24 * x * x - 48 * x * x * x;
+            } else if (x >= 0 && x <= .25) {
+                lut[n] = 1 - 24 * x * x + 48 * x * x * x;
+            } else {
+                lut[n] = 0;
+            }
+        }
+        *overlap = 0.75;
+        break;
+    case WFUNC_POISSON:
+        for (n = 0; n < N; n++) {
+            double x = 2 * ((n / (double)(N - 1)) - .5);
+
+            if (x >= 0 && x <= .5) {
+                lut[n] = exp(-6*x);
+            } else if (x < 0 && x >= -.5) {
+                lut[n] = exp(6*x);
+            } else {
+                lut[n] = 0;
+            }
+        }
+        *overlap = 0.75;
+        break;
     default:
         av_assert0(0);
     }
