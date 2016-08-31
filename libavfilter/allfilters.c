ee605aa730dd230cd67cff5233318634ac42a1cc
Paul B Mahol <onemda@gmail.com>
2016-09-01 01:18:45 +0200

    avfilter: add gblur filter
    Signed-off-by: Paul B Mahol <onemda@gmail.com>

--- libavfilter/allfilters.c
+++ libavfilter/allfilters.c
@@ -199,6 +199,7 @@ void avfilter_register_all(void)
     REGISTER_FILTER(FRAMESTEP,      framestep,      vf);
     REGISTER_FILTER(FREI0R,         frei0r,         vf);
     REGISTER_FILTER(FSPP,           fspp,           vf);
+    REGISTER_FILTER(GBLUR,          gblur,          vf);
     REGISTER_FILTER(GEQ,            geq,            vf);
     REGISTER_FILTER(GRADFUN,        gradfun,        vf);
     REGISTER_FILTER(HALDCLUT,       haldclut,       vf);
