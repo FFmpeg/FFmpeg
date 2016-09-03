8f0bd1d9bad5b8aa42b5b4cec103cc4afed5eab6
Carl Eugen Hoyos <cehoyos@ag.or.at>
2016-09-03 13:56:40 +0200

    lavf/httpauth: Do case-insensitive http header checks.
    Tested by trac user NTAuthority.
    Fixes ticket #5786.

--- libavformat/version.h
+++ libavformat/version.h
@@ -33,7 +33,7 @@
 // Also please add any ticket numbers that you believe might be affected here
 #define LIBAVFORMAT_VERSION_MAJOR  57
 #define LIBAVFORMAT_VERSION_MINOR  48
-#define LIBAVFORMAT_VERSION_MICRO 101
+#define LIBAVFORMAT_VERSION_MICRO 102
 
 #define LIBAVFORMAT_VERSION_INT AV_VERSION_INT(LIBAVFORMAT_VERSION_MAJOR, \
                                                LIBAVFORMAT_VERSION_MINOR, \
