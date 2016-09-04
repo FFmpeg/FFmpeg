f077ad69c682c13ab75a72aec11a61cac53f0c91
Carl Eugen Hoyos <cehoyos@ag.or.at>
2016-09-04 21:11:02 +0200

    lavc/avpacket: Fix undefined behaviour, do not pass a null pointer to memcpy().
    Fixes ticket #5128.

--- libavcodec/avpacket.c
+++ libavcodec/avpacket.c
@@ -581,7 +581,8 @@ int av_packet_ref(AVPacket *dst, const AVPacket *src)
         ret = packet_alloc(&dst->buf, src->size);
         if (ret < 0)
             goto fail;
-        memcpy(dst->buf->data, src->data, src->size);
+        if (src->size)
+            memcpy(dst->buf->data, src->data, src->size);
 
         dst->data = dst->buf->data;
     } else {
