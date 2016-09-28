788544ff0ed6fe67fda80ad6d3a0796ace035584
Diego Biurrun <diego@biurrun.de>
2016-09-28 19:14:28 +0200

    audiodsp: x86: Remove pointless header file
    Its single forward declaration can be moved to the only place
    it is used, like is done for all other dsp init files.

--- libavcodec/x86/audiodsp_init.c
+++ libavcodec/x86/audiodsp_init.c
@@ -23,7 +23,6 @@
 #include "libavutil/cpu.h"
 #include "libavutil/x86/cpu.h"
 #include "libavcodec/audiodsp.h"
-#include "audiodsp.h"
 
 int32_t ff_scalarproduct_int16_mmxext(const int16_t *v1, const int16_t *v2,
                                       int order);
@@ -38,6 +37,9 @@ void ff_vector_clip_int32_int_sse2(int32_t *dst, const int32_t *src,
                                    int32_t min, int32_t max, unsigned int len);
 void ff_vector_clip_int32_sse4(int32_t *dst, const int32_t *src,
                                int32_t min, int32_t max, unsigned int len);
+
+void ff_vector_clipf_sse(float *dst, const float *src,
+                         int len, float min, float max);
 
 av_cold void ff_audiodsp_init_x86(AudioDSPContext *c)
 {
