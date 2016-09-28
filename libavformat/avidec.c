c8c5f66b42edc37474baa5cb51460cbf6f33075b
Michael Niedermayer <michael@niedermayer.cc>
2016-09-28 15:47:12 +0200

    avformat/avidec: Remove ancient assert
    This assert can with crafted files fail, a warning is already printed
    for this case.
    
    Fixes assertion failure
    Fixes:1/assert.avi
    
    Found-by: 连一汉 <lianyihan@360.cn>
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit 14bac7e00d72eac687612d9b125e585011a56d4f)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavformat/avidec.c
+++ libavformat/avidec.c
@@ -1853,7 +1853,6 @@ static int avi_read_seek(AVFormatContext *s, int stream_index,
             continue;
 
 //        av_assert1(st2->codecpar->block_align);
-        av_assert0(fabs(av_q2d(st2->time_base) - ast2->scale / (double)ast2->rate) < av_q2d(st2->time_base) * 0.00000001);
         index = av_index_search_timestamp(st2,
                                           av_rescale_q(timestamp,
                                                        st->time_base,
