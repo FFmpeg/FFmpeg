7911186ed616ae81dd8617d6d0e8b08c818db9d8
Diego Biurrun <diego@biurrun.de>
2016-10-08 17:58:02 +0200

    emms: Give apriv_emms_yasm() a more general name

--- libavutil/x86/emms.h
+++ libavutil/x86/emms.h
@@ -22,7 +22,7 @@
 #include "config.h"
 #include "libavutil/attributes.h"
 
-void avpriv_emms_yasm(void);
+void avpriv_emms_asm(void);
 
 #if HAVE_MMX_INLINE
 #   define emms_c emms_c
@@ -39,7 +39,7 @@ static av_always_inline void emms_c(void)
 #   include <mmintrin.h>
 #   define emms_c _mm_empty
 #elif HAVE_MMX_EXTERNAL
-#   define emms_c avpriv_emms_yasm
+#   define emms_c avpriv_emms_asm
 #endif /* HAVE_MMX_INLINE */
 
 #endif /* AVUTIL_X86_EMMS_H */
