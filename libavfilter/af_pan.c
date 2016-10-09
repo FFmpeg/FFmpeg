8baf2d8fadc16d7f2c7d4e686f19e495ae6cc07e
Moritz Barsnick <barsnick@gmx.net>
2016-10-09 12:57:00 +0200

    lavfi: fix typos
    Signed-off-by: Moritz Barsnick <barsnick@gmx.net>
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit f4e4bde1f4cff99d4ec59ed361ff9228b2050e6b)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavfilter/af_pan.c
+++ libavfilter/af_pan.c
@@ -109,7 +109,7 @@ static av_cold int init(AVFilterContext *ctx)
     if (!pan->args) {
         av_log(ctx, AV_LOG_ERROR,
                "pan filter needs a channel layout and a set "
-               "of channels definitions as parameter\n");
+               "of channel definitions as parameter\n");
         return AVERROR(EINVAL);
     }
     if (!args)
@@ -276,7 +276,7 @@ static int config_props(AVFilterLink *link)
     if (link->channels > MAX_CHANNELS ||
         pan->nb_output_channels > MAX_CHANNELS) {
         av_log(ctx, AV_LOG_ERROR,
-               "af_pan support a maximum of %d channels. "
+               "af_pan supports a maximum of %d channels. "
                "Feel free to ask for a higher limit.\n", MAX_CHANNELS);
         return AVERROR_PATCHWELCOME;
     }
