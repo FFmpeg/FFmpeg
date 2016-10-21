4f96f9d1118e073d346d16be157fa5075434e7f2
Michael Niedermayer <michael@niedermayer.cc>
2016-10-21 13:40:18 +0200

    avcodec/utils: Clear MMX state before returning from avcodec_default_execute*()
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/utils.c
+++ libavcodec/utils.c
@@ -1010,6 +1010,7 @@ int avcodec_default_execute(AVCodecContext *c, int (*func)(AVCodecContext *c2, v
         if (ret)
             ret[i] = r;
     }
+    emms_c();
     return 0;
 }
 
@@ -1022,6 +1023,7 @@ int avcodec_default_execute2(AVCodecContext *c, int (*func)(AVCodecContext *c2, 
         if (ret)
             ret[i] = r;
     }
+    emms_c();
     return 0;
 }
 
