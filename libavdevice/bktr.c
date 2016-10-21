2f806622e1270d3ed1d41a53049a19673dafbe70
Diego Biurrun <diego@biurrun.de>
2016-10-21 14:17:07 +0200

    bktr: Use memset(0) instead of zero initialization for struct sigaction
    sigaction is not defined in standards as a struct starting with another
    struct. Some *BSD variants do however, resulting in a warning from the
    zero initialization, which this change eliminates.
    
    This partially reverts a92be9b856bd11b081041c43c25d442028fe9a63.

--- libavdevice/bktr.c
+++ libavdevice/bktr.c
@@ -104,7 +104,7 @@ static av_cold int bktr_init(const char *video_device, int width, int height,
     long ioctl_frequency;
     char *arg;
     int c;
-    struct sigaction act = { 0 }, old;
+    struct sigaction act, old;
     int ret;
     char errbuf[128];
 
@@ -135,6 +135,7 @@ static av_cold int bktr_init(const char *video_device, int width, int height,
             frequency = 0.0;
     }
 
+    memset(&act, 0, sizeof(act));
     sigemptyset(&act.sa_mask);
     act.sa_handler = catchsignal;
     sigaction(SIGUSR1, &act, &old);
