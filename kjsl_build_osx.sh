daa09d4e99b850343f82d91f65c5aa9b140defa1
Kevin Staunton-Lambert <kevleyski@gmail.com>
2016-10-31 16:23:07 +1100

    Adding ffplay

--- kjsl_build_osx.sh
+++ kjsl_build_osx.sh
@@ -1,7 +1,13 @@
+#!/bin/bash
+
+# KJSL: script to build FFmpeg on OSX
+
+brew install automake fdk-aac git lame libass libtool libvorbis libvpx opus sdl shtool texi2html theora wget x264 xvid yasm
+
 ./configure  --prefix=/usr/local --enable-gpl --enable-nonfree \
 --enable-libass --enable-libfdk-aac --enable-libfreetype \
 --enable-libopus --enable-libtheora --enable-libvorbis \
---enable-libvpx --enable-libx264 --enable-libxvid \
+--enable-libvpx --enable-libx264 --enable-libxvid --enable-ffplay \
 --extra-ldflags="-L/usr/local/Cellar/lame/3.99.5/lib \
 -L/usr/local/Cellar/libogg/1.3.2/lib \
 -L/usr/local/Cellar/theora/1.1.1/lib \
@@ -12,3 +18,6 @@
 -I/usr/local/Cellar/theora/1.1.1/include \
 -I/usr/local/Cellar/libvorbis/1.3.5/include \
 -I/usr/local/Cellar/xvid/1.3.4/include"
+
+make 
+sudo make install
