e5a17dc80368d425753447b782a2d36b74eb10a9
Kevin Staunton-Lambert <kevleyski@gmail.com>
2016-10-31 16:48:41 +1100

    SDL dependency

--- kjsl_build_osx.sh
+++ kjsl_build_osx.sh
@@ -17,7 +17,8 @@ brew install automake fdk-aac git lame libass libtool libvorbis libvpx opus sdl 
 -I/usr/local/Cellar/libogg/1.3.2/include \
 -I/usr/local/Cellar/theora/1.1.1/include \
 -I/usr/local/Cellar/libvorbis/1.3.5/include \
--I/usr/local/Cellar/xvid/1.3.4/include"
+-I/usr/local/Cellar/xvid/1.3.4/include \
+-I/usr/local//Cellar/sdl/1.2.15/include"
 
 make 
 sudo make install
