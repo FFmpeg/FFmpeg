fd4de527dec12a3d9abc0aaa87882c1863b22f01
Kevin Staunton-Lambert <kevleyski@gmail.com>
2016-10-31 15:43:46 +1100

    OSX build script

---
+++ kjsl_build_osx.sh
@@ -0,0 +1,14 @@
+./configure  --prefix=/usr/local --enable-gpl --enable-nonfree \
+--enable-libass --enable-libfdk-aac --enable-libfreetype \
+--enable-libopus --enable-libtheora --enable-libvorbis \
+--enable-libvpx --enable-libx264 --enable-libxvid \
+--extra-ldflags="-L/usr/local/Cellar/lame/3.99.5/lib \
+-L/usr/local/Cellar/libogg/1.3.2/lib \
+-L/usr/local/Cellar/theora/1.1.1/lib \
+-L/usr/local/Cellar/libvorbis/1.3.5/lib \
+-L/usr/local/Cellar/xvid/1.3.4/lib" \
+--extra-cflags="-I/usr/local/Cellar/lame/3.99.5/include \
+-I/usr/local/Cellar/libogg/1.3.2/include \
+-I/usr/local/Cellar/theora/1.1.1/include \
+-I/usr/local/Cellar/libvorbis/1.3.5/include \
+-I/usr/local/Cellar/xvid/1.3.4/include"
