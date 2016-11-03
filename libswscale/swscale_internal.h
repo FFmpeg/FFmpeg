19be5fb7286b02fbe384f12fe4aa5c376d43d2e1
Luca Barbato <lu_zero@gentoo.org>
2016-11-03 16:10:03 +0100

    swscale: Add GRAY12
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libswscale/swscale_internal.h
+++ libswscale/swscale_internal.h
@@ -695,6 +695,8 @@ static av_always_inline int isRGB(enum AVPixelFormat pix_fmt)
 #define isGray(x)                      \
     ((x) == AV_PIX_FMT_GRAY8       ||  \
      (x) == AV_PIX_FMT_YA8         ||  \
+     (x) == AV_PIX_FMT_GRAY12BE    ||  \
+     (x) == AV_PIX_FMT_GRAY12LE    ||  \
      (x) == AV_PIX_FMT_GRAY16BE    ||  \
      (x) == AV_PIX_FMT_GRAY16LE    ||  \
      (x) == AV_PIX_FMT_YA16BE      ||  \
