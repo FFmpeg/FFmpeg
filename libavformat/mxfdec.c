c11fd9de7653cd95005b5f7432a61a2e4c1145f1
Andreas Cadhalpun <Andreas.Cadhalpun@googlemail.com>
2016-11-05 00:17:53 +0100

    mxfdec: fix NULL pointer dereference
    Metadata streams have priv_data set to NULL.
    
    Reviewed-by: Michael Niedermayer <michael@niedermayer.cc>
    Signed-off-by: Andreas Cadhalpun <Andreas.Cadhalpun@googlemail.com>
    (cherry picked from commit 0efb6106118c17308b3fdc3190f5e5bf84b01d5c)
    Signed-off-by: Andreas Cadhalpun <Andreas.Cadhalpun@googlemail.com>

--- libavformat/mxfdec.c
+++ libavformat/mxfdec.c
@@ -391,7 +391,7 @@ static int mxf_get_stream_index(AVFormatContext *s, KLVPacket *klv)
     for (i = 0; i < s->nb_streams; i++) {
         MXFTrack *track = s->streams[i]->priv_data;
         /* SMPTE 379M 7.3 */
-        if (!memcmp(klv->key + sizeof(mxf_essence_element_key), track->track_number, sizeof(track->track_number)))
+        if (track && !memcmp(klv->key + sizeof(mxf_essence_element_key), track->track_number, sizeof(track->track_number)))
             return i;
     }
     /* return 0 if only one stream, for OP Atom files with 0 as track number */
