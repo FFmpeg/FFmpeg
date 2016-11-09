905cdcaa9d081d3d945ce555b27b43a75c3af57b
Diego Biurrun <diego@biurrun.de>
2016-11-09 06:07:06 +0100

    examples/decode_audio: Add missing header for av_free()

--- doc/examples/decode_audio.c
+++ doc/examples/decode_audio.c
@@ -29,9 +29,10 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "libavcodec/avcodec.h"
-
 #include "libavutil/frame.h"
+#include "libavutil/mem.h"
+
+#include "libavcodec/avcodec.h"
 
 #define AUDIO_INBUF_SIZE 20480
 #define AUDIO_REFILL_THRESH 4096
