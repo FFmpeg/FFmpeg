52d8c1e474c3e11fed06cf17e22c1e59406a0ce3
Andreas Cadhalpun <Andreas.Cadhalpun@googlemail.com>
2016-11-13 18:22:12 +0100

    filmstripdec: correctly check image dimensions
    This prevents a division by zero in read_packet.
    
    Reviewed-by: Paul B Mahol <onemda@gmail.com>
    Signed-off-by: Andreas Cadhalpun <Andreas.Cadhalpun@googlemail.com>
    (cherry picked from commit 25012c56448a48487cdc9699465e640871dbcd60)
    Signed-off-by: Andreas Cadhalpun <Andreas.Cadhalpun@googlemail.com>

--- libavformat/filmstripdec.c
+++ libavformat/filmstripdec.c
@@ -25,6 +25,7 @@
  */
 
 #include "libavutil/intreadwrite.h"
+#include "libavutil/imgutils.h"
 #include "avformat.h"
 #include "internal.h"
 
@@ -68,10 +69,8 @@ static int read_header(AVFormatContext *s)
     st->codecpar->height     = avio_rb16(pb);
     film->leading         = avio_rb16(pb);
 
-    if (st->codecpar->width * 4LL * st->codecpar->height >= INT_MAX) {
-        av_log(s, AV_LOG_ERROR, "dimensions too large\n");
-        return AVERROR_PATCHWELCOME;
-    }
+    if (av_image_check_size(st->codecpar->width, st->codecpar->height, 0, s) < 0)
+        return AVERROR_INVALIDDATA;
 
     avpriv_set_pts_info(st, 64, 1, avio_rb16(pb));
 
