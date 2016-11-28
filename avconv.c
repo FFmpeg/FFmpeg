d0c84c41d33ffd270d5f9fe0290e08341397fdee
Luca Barbato <lu_zero@gentoo.org>
2016-11-28 20:52:47 +0100

    avconv: Fix the audio next dts computation
    Use the correct timebase.
    
    CC: libav-stable@libav.org

--- avconv.c
+++ avconv.c
@@ -1350,7 +1350,7 @@ static int decode_audio(InputStream *ist, AVPacket *pkt, int *got_output,
     /* if the decoder provides a pts, use it instead of the last packet pts.
        the decoder could be delaying output by a packet or more. */
     if (decoded_frame->pts != AV_NOPTS_VALUE)
-        ist->next_dts = decoded_frame->pts;
+        ist->next_dts = av_rescale_q(decoded_frame->pts, ist->st->time_base, AV_TIME_BASE_Q);
     else if (pkt && pkt->pts != AV_NOPTS_VALUE) {
         decoded_frame->pts = pkt->pts;
     }
