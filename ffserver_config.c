29ef35abffa00dd3975ab96314f1aafabc140f88
Michael Niedermayer <michael@niedermayer.cc>
2016-11-29 18:48:40 +0100

    ffserver_config: Setup codecpar in add_codec()
    fixes segfault in the status page code
    
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit 472fee91bcf9b7bae81fd4c00bbe5151ca458a7c)

--- ffserver_config.c
+++ ffserver_config.c
@@ -323,6 +323,8 @@ done:
     av_dict_free(&recommended);
     av_stream_set_recommended_encoder_configuration(st, enc_config);
     st->codec = av;
+    st->codecpar = avcodec_parameters_alloc();
+    avcodec_parameters_from_context(st->codecpar, av);
     stream->streams[stream->nb_streams++] = st;
 }
 
