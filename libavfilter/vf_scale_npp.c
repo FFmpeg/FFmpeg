88281a5256f0034451c09acab3aff44acb43c2a3
Timo Rothenpieler <timo@rothenpieler.org>
2016-11-29 17:28:15 +0100

    avfilter/vf_scale_npp: check ff_set_common_formats return value

--- libavfilter/vf_scale_npp.c
+++ libavfilter/vf_scale_npp.c
@@ -169,11 +169,9 @@ static int nppscale_query_formats(AVFilterContext *ctx)
     static const enum AVPixelFormat pixel_formats[] = {
         AV_PIX_FMT_CUDA, AV_PIX_FMT_NONE,
     };
-    AVFilterFormats *pix_fmts  = ff_make_format_list(pixel_formats);
+    AVFilterFormats *pix_fmts = ff_make_format_list(pixel_formats);
 
-    ff_set_common_formats(ctx, pix_fmts);
-
-    return 0;
+    return ff_set_common_formats(ctx, pix_fmts);
 }
 
 static int init_stage(NPPScaleStageContext *stage, AVBufferRef *device_ctx)
