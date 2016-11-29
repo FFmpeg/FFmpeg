7dafb3a25a580a5f8f1a5083835c67be9ed17043
Timo Rothenpieler <timo@rothenpieler.org>
2016-11-29 17:40:18 +0100

    avfilter/vf_hwupload_cuda: fix potential leak

--- libavfilter/vf_hwupload_cuda.c
+++ libavfilter/vf_hwupload_cuda.c
@@ -64,11 +64,13 @@ static int cudaupload_query_formats(AVFilterContext *ctx)
         AV_PIX_FMT_CUDA, AV_PIX_FMT_NONE,
     };
     AVFilterFormats *in_fmts  = ff_make_format_list(input_pix_fmts);
-    AVFilterFormats *out_fmts = ff_make_format_list(output_pix_fmts);
+    AVFilterFormats *out_fmts;
 
     ret = ff_formats_ref(in_fmts, &ctx->inputs[0]->out_formats);
     if (ret < 0)
         return ret;
+
+    out_fmts = ff_make_format_list(output_pix_fmts);
 
     ret = ff_formats_ref(out_fmts, &ctx->outputs[0]->in_formats);
     if (ret < 0)
