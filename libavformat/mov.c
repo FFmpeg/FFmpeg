d4b731e271ba944ade8f6a128271479529507de9
Matt Wolenetz <wolenetz@google.com>
2016-12-14 15:27:49 -0800

    lavf/mov.c: Avoid heap allocation wraps in mov_read_{senc,saiz}()
    Core of patch is from paul@paulmehta.com
    Reference https://crbug.com/643952 (senc,saiz portions)
    
    Signed-off-by: Matt Wolenetz <wolenetz@chromium.org>
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit 36aba43bd5fae8595dd9a566fbcfbbea63f0fca3)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavformat/mov.c
+++ libavformat/mov.c
@@ -4599,8 +4599,8 @@ static int mov_read_senc(MOVContext *c, AVIOContext *pb, MOVAtom atom)
 
     avio_rb32(pb);        /* entries */
 
-    if (atom.size < 8) {
-        av_log(c->fc, AV_LOG_ERROR, "senc atom size %"PRId64" too small\n", atom.size);
+    if (atom.size < 8 || atom.size > FFMIN(INT_MAX, SIZE_MAX)) {
+        av_log(c->fc, AV_LOG_ERROR, "senc atom size %"PRId64" invalid\n", atom.size);
         return AVERROR_INVALIDDATA;
     }
 
@@ -4666,6 +4666,11 @@ static int mov_read_saiz(MOVContext *c, AVIOContext *pb, MOVAtom atom)
 
     if (atom.size <= atom_header_size) {
         return 0;
+    }
+
+    if (atom.size > FFMIN(INT_MAX, SIZE_MAX)) {
+        av_log(c->fc, AV_LOG_ERROR, "saiz atom auxiliary_info_sizes size %"PRId64" invalid\n", atom.size);
+        return AVERROR_INVALIDDATA;
     }
 
     /* save the auxiliary info sizes as is */
