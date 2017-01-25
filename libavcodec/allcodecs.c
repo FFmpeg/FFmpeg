280a40dd2739e9a23bf6372752dcd133ff36e149
Paul B Mahol <onemda@gmail.com>
2017-01-25 13:33:52 +0100

    avcodec: add ATRAC Advanced Lossless decoders
    Only lossy part is decoded for now.
    
    Signed-off-by: Paul B Mahol <onemda@gmail.com>

--- libavcodec/allcodecs.c
+++ libavcodec/allcodecs.c
@@ -403,7 +403,9 @@ void avcodec_register_all(void)
     REGISTER_DECODER(APE,               ape);
     REGISTER_DECODER(ATRAC1,            atrac1);
     REGISTER_DECODER(ATRAC3,            atrac3);
+    REGISTER_DECODER(ATRAC3AL,          atrac3al);
     REGISTER_DECODER(ATRAC3P,           atrac3p);
+    REGISTER_DECODER(ATRAC3PAL,         atrac3pal);
     REGISTER_DECODER(BINKAUDIO_DCT,     binkaudio_dct);
     REGISTER_DECODER(BINKAUDIO_RDFT,    binkaudio_rdft);
     REGISTER_DECODER(BMV_AUDIO,         bmv_audio);
