e6bff23f1e11aefb16a2b5d6ee72bf7469c5a66e
Anton Khirnov <anton@khirnov.net>
2017-02-08 09:32:17 +0100

    cpu: add a function for querying maximum required data alignment

--- libavutil/cpu.c
+++ libavutil/cpu.c
@@ -16,6 +16,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
+#include <stddef.h>
 #include <stdint.h>
 #include <stdatomic.h>
 
@@ -179,4 +180,16 @@ int av_cpu_count(void)
 #endif
 
     return nb_cpus;
+}
+
+size_t av_cpu_max_align(void)
+{
+    int flags = av_get_cpu_flags();
+
+    if (flags & AV_CPU_FLAG_AVX)
+        return 32;
+    if (flags & (AV_CPU_FLAG_ALTIVEC | AV_CPU_FLAG_SSE | AV_CPU_FLAG_NEON))
+        return 16;
+
+    return 8;
 }
