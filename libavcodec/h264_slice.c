c03029a835949fc0e68b4c6558ebcdc3ae137087
Michael Niedermayer <michael@niedermayer.cc>
2017-02-08 17:55:41 +0100

    avcodec/h264_slice: Clear ref_counts on redundant slices
    Fixes reading freed memory
    Fixes: 568/clusterfuzz-testcase-6107186067406848
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/targets/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/h264_slice.c
+++ libavcodec/h264_slice.c
@@ -1999,8 +1999,10 @@ int ff_h264_queue_decode_slice(H264Context *h, const H2645NAL *nal)
         return ret;
 
     // discard redundant pictures
-    if (sl->redundant_pic_count > 0)
+    if (sl->redundant_pic_count > 0) {
+        sl->ref_count[0] = sl->ref_count[1] = 0;
         return 0;
+    }
 
     if (sl->first_mb_addr == 0 || !h->current_slice) {
         if (h->setup_finished) {
