248dc5c1646dcdd96fe79761105c4ae889e711fd
John Stebbins <stebbins@jetheaddev.com>
2017-02-23 14:22:56 -0700

    h264dec: fix dropped initial SEI recovery point

--- libavcodec/h264dec.c
+++ libavcodec/h264dec.c
@@ -452,7 +452,6 @@ void ff_h264_flush_change(H264Context *h)
     if (h->cur_pic_ptr)
         h->cur_pic_ptr->reference = 0;
     h->first_field = 0;
-    ff_h264_sei_uninit(&h->sei);
     h->recovery_frame = -1;
     h->frame_recovered = 0;
 }
@@ -466,6 +465,7 @@ static void flush_dpb(AVCodecContext *avctx)
     memset(h->delayed_pic, 0, sizeof(h->delayed_pic));
 
     ff_h264_flush_change(h);
+    ff_h264_sei_uninit(&h->sei);
 
     for (i = 0; i < H264_MAX_PICTURE_COUNT; i++)
         ff_h264_unref_picture(h, &h->DPB[i]);
