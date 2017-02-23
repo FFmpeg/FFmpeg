7e4f32f4e4b93c95dcc872cb844c5548e69f352e
Michael Niedermayer <michael@niedermayer.cc>
2017-02-23 15:19:32 +0100

    avutil/frame: Disallow zero sized frame side data
    There should be no case that needs this and its a potential for
    creating corner cases
    
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavutil/frame.c
+++ libavutil/frame.c
@@ -668,6 +668,8 @@ AVFrameSideData *av_frame_new_side_data(AVFrame *frame,
                                         enum AVFrameSideDataType type,
                                         int size)
 {
+    if (size <= 0)
+        return NULL;
 
     return frame_new_side_data(frame, type, av_buffer_alloc(size));
 }
