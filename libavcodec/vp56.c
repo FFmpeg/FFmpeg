0db93a9d403de9c53b3b431a06e089a66c600989
Michael Niedermayer <michael@niedermayer.cc>
2017-02-25 21:07:22 +0100

    avcodec/vp56: Factorize vp56_render_mb() out
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit 4c0139463c8f0a6f28e7b193c2a85608a7635bbd)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/vp56.c
+++ libavcodec/vp56.c
@@ -400,30 +400,18 @@ static void vp56_mc(VP56Context *s, int b, int plane, uint8_t *src,
     }
 }
 
-static int vp56_decode_mb(VP56Context *s, int row, int col, int is_alpha)
+static av_always_inline void vp56_render_mb(VP56Context *s, int row, int col, int is_alpha, VP56mb mb_type)
 {
-    AVFrame *frame_current, *frame_ref;
-    VP56mb mb_type;
-    VP56Frame ref_frame;
     int b, ab, b_max, plane, off;
-    int ret;
-
-    if (s->frames[VP56_FRAME_CURRENT]->key_frame)
-        mb_type = VP56_MB_INTRA;
-    else
-        mb_type = vp56_decode_mv(s, row, col);
-    ref_frame = ff_vp56_reference_frame[mb_type];
-
-    ret = s->parse_coeff(s);
-    if (ret < 0)
-        return ret;
+    AVFrame *frame_current, *frame_ref;
+    VP56Frame ref_frame = ff_vp56_reference_frame[mb_type];
 
     vp56_add_predictors_dc(s, ref_frame);
 
     frame_current = s->frames[VP56_FRAME_CURRENT];
     frame_ref = s->frames[ref_frame];
     if (mb_type != VP56_MB_INTRA && !frame_ref->data[0])
-        return 0;
+        return;
 
     ab = 6*is_alpha;
     b_max = 6 - 2*is_alpha;
@@ -473,57 +461,38 @@ static int vp56_decode_mb(VP56Context *s, int row, int col, int is_alpha)
         s->block_coeff[4][0] = 0;
         s->block_coeff[5][0] = 0;
     }
+}
+
+static int vp56_decode_mb(VP56Context *s, int row, int col, int is_alpha)
+{
+    VP56mb mb_type;
+    int ret;
+
+    if (s->frames[VP56_FRAME_CURRENT]->key_frame)
+        mb_type = VP56_MB_INTRA;
+    else
+        mb_type = vp56_decode_mv(s, row, col);
+
+    ret = s->parse_coeff(s);
+    if (ret < 0)
+        return ret;
+
+    vp56_render_mb(s, row, col, is_alpha, mb_type);
+
     return 0;
 }
 
 static int vp56_conceal_mb(VP56Context *s, int row, int col, int is_alpha)
 {
-    AVFrame *frame_current, *frame_ref;
     VP56mb mb_type;
-    VP56Frame ref_frame;
-    int b, ab, b_max, plane, off;
 
     if (s->frames[VP56_FRAME_CURRENT]->key_frame)
         mb_type = VP56_MB_INTRA;
     else
         mb_type = vp56_conceal_mv(s, row, col);
-    ref_frame = ff_vp56_reference_frame[mb_type];
 
-    frame_current = s->frames[VP56_FRAME_CURRENT];
-    frame_ref = s->frames[ref_frame];
-    if (mb_type != VP56_MB_INTRA && !frame_ref->data[0])
-        return 0;
+    vp56_render_mb(s, row, col, is_alpha, mb_type);
 
-    ab = 6*is_alpha;
-    b_max = 6 - 2*is_alpha;
-
-    switch (mb_type) {
-        case VP56_MB_INTRA:
-            for (b=0; b<b_max; b++) {
-                plane = ff_vp56_b2p[b+ab];
-                s->vp3dsp.idct_put(frame_current->data[plane] + s->block_offset[b],
-                                s->stride[plane], s->block_coeff[b]);
-            }
-            break;
-
-        case VP56_MB_INTER_NOVEC_PF:
-        case VP56_MB_INTER_NOVEC_GF:
-            for (b=0; b<b_max; b++) {
-                plane = ff_vp56_b2p[b+ab];
-                off = s->block_offset[b];
-                s->hdsp.put_pixels_tab[1][0](frame_current->data[plane] + off,
-                                             frame_ref->data[plane] + off,
-                                             s->stride[plane], 8);
-                s->vp3dsp.idct_add(frame_current->data[plane] + off,
-                                s->stride[plane], s->block_coeff[b]);
-            }
-            break;
-    }
-
-    if (is_alpha) {
-        s->block_coeff[4][0] = 0;
-        s->block_coeff[5][0] = 0;
-    }
     return 0;
 }
 
