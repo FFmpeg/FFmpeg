bbc8f3d20e09ab238e345a02ed983434d2efe633
Alexis Ballier <aballier@gentoo.org>
2017-03-11 20:18:54 +0100

    lavf/vf_framerate: Fix frame leak when increasing framerate.
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavfilter/vf_framerate.c
+++ libavfilter/vf_framerate.c
@@ -526,7 +526,7 @@ static av_cold void uninit(AVFilterContext *ctx)
     FrameRateContext *s = ctx->priv;
     int i;
 
-    for (i = s->frst + 1; i < s->last; i++) {
+    for (i = s->frst; i < s->last; i++) {
         if (s->srce[i] && (s->srce[i] != s->srce[i + 1]))
             av_frame_free(&s->srce[i]);
     }
