7cebc5a9ccba0de7bddf7900ae85652ebc66141c
Michael Niedermayer <michael@niedermayer.cc>
2017-03-13 02:51:15 +0100

    avcodec/wavpack: Fix runtime error: shift exponent 32 is too large for 32-bit type 'int'
    Fixes: 822/clusterfuzz-testcase-4873433189974016
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/targets/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/wavpack.c
+++ libavcodec/wavpack.c
@@ -846,7 +846,7 @@ static int wavpack_decode_block(AVCodecContext *avctx, int block_no,
                 continue;
             }
             bytestream2_get_buffer(&gb, val, 4);
-            if (val[0] > 32) {
+            if (val[0] > 31) {
                 av_log(avctx, AV_LOG_ERROR,
                        "Invalid INT32INFO, extra_bits = %d (> 32)\n", val[0]);
                 continue;
