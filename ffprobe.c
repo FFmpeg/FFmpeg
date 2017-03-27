b613245c9715c34358522737bf0cf6a4f9392ca3
James Almer <jamrial@gmail.com>
2017-03-27 20:37:29 -0300

    ffprobe: free log buffer's parent_name during cleanup
    Fixes memleak.

--- ffprobe.c
+++ ffprobe.c
@@ -1911,6 +1911,7 @@ static void clear_log(int need_lock)
         pthread_mutex_lock(&log_mutex);
     for (i=0; i<log_buffer_size; i++) {
         av_freep(&log_buffer[i].context_name);
+        av_freep(&log_buffer[i].parent_name);
         av_freep(&log_buffer[i].log_message);
     }
     log_buffer_size = 0;
