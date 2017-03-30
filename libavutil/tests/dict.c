afebf470ca73c17cc8393bfd7eeebfdf6809c2b8
Michael Niedermayer <michael@niedermayer.cc>
2017-03-30 23:07:52 +0200

    avutil/tests/dict: Check return of av_dict_parse_string()
    Fixes: CID1396402
    
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavutil/tests/dict.c
+++ libavutil/tests/dict.c
@@ -35,12 +35,15 @@ static void test_separators(const AVDictionary *m, const char pair, const char v
     char vals[]  = {val, '\0'};
 
     char *buffer = NULL;
+    int ret;
+
     av_dict_copy(&dict, m, 0);
     print_dict(dict);
     av_dict_get_string(dict, &buffer, val, pair);
     printf("%s\n", buffer);
     av_dict_free(&dict);
-    av_dict_parse_string(&dict, buffer, vals, pairs, 0);
+    ret = av_dict_parse_string(&dict, buffer, vals, pairs, 0);
+    printf("ret %d\n", ret);
     av_freep(&buffer);
     print_dict(dict);
     av_dict_free(&dict);
