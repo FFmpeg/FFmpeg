92f3fc9f2f3948dbfddf8f749d2595427cfe906b
Vittorio Giovara <vittorio.giovara@gmail.com>
2017-03-31 16:36:10 +0200

    lxfdec: convert to new channel layout API
    Signed-off-by: Vittorio Giovara <vittorio.giovara@gmail.com>
    Signed-off-by: James Almer <jamrial@gmail.com>

--- libavformat/lxfdec.c
+++ libavformat/lxfdec.c
@@ -281,7 +281,7 @@ static int lxf_read_header(AVFormatContext *s)
 
         st->codecpar->codec_type  = AVMEDIA_TYPE_AUDIO;
         st->codecpar->sample_rate = LXF_SAMPLERATE;
-        st->codecpar->channels    = lxf->channels;
+        st->codecpar->ch_layout.nb_channels = lxf->channels;
 
         avpriv_set_pts_info(st, 64, 1, st->codecpar->sample_rate);
     }
