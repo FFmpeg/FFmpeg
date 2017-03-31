e7f9f548e94aad2d3d33f7ce441cf01e0e8c4889
Vittorio Giovara <vittorio.giovara@gmail.com>
2017-03-31 13:35:10 +0200

    bethsoftvid: convert to new channel layout API
    Signed-off-by: Vittorio Giovara <vittorio.giovara@gmail.com>
    Signed-off-by: James Almer <jamrial@gmail.com>

--- libavformat/bethsoftvid.c
+++ libavformat/bethsoftvid.c
@@ -254,8 +254,7 @@ static int vid_read_packet(AVFormatContext *s,
                 vid->audio_index                 = st->index;
                 st->codecpar->codec_type            = AVMEDIA_TYPE_AUDIO;
                 st->codecpar->codec_id              = AV_CODEC_ID_PCM_U8;
-                st->codecpar->channels              = 1;
-                st->codecpar->channel_layout        = AV_CH_LAYOUT_MONO;
+                st->codecpar->ch_layout             = (AVChannelLayout)AV_CHANNEL_LAYOUT_MONO;
                 st->codecpar->bits_per_coded_sample = 8;
                 st->codecpar->sample_rate           = vid->sample_rate;
                 st->codecpar->bit_rate              = 8 * st->codecpar->sample_rate;
