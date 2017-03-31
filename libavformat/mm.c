7baa1807b8204a1cae3c657d203f1a05ee14b79f
Vittorio Giovara <vittorio.giovara@gmail.com>
2017-03-31 16:57:52 +0200

    mm: convert to new channel layout API
    Signed-off-by: Vittorio Giovara <vittorio.giovara@gmail.com>
    Signed-off-by: James Almer <jamrial@gmail.com>

--- libavformat/mm.c
+++ libavformat/mm.c
@@ -124,8 +124,7 @@ static int read_header(AVFormatContext *s)
         st->codecpar->codec_type = AVMEDIA_TYPE_AUDIO;
         st->codecpar->codec_tag = 0; /* no fourcc */
         st->codecpar->codec_id = AV_CODEC_ID_PCM_U8;
-        st->codecpar->channels = 1;
-        st->codecpar->channel_layout = AV_CH_LAYOUT_MONO;
+        st->codecpar->ch_layout = (AVChannelLayout)AV_CHANNEL_LAYOUT_MONO;
         st->codecpar->sample_rate = 8000;
         avpriv_set_pts_info(st, 64, 1, 8000); /* 8000 hz */
     }
