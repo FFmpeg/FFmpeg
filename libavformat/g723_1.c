f17932faf5e599ddfa84beafe21128d271b4f8fb
Vittorio Giovara <vittorio.giovara@gmail.com>
2017-03-31 14:13:28 +0200

    g723_1: convert to new channel layout API
    Signed-off-by: Vittorio Giovara <vittorio.giovara@gmail.com>
    Signed-off-by: James Almer <jamrial@gmail.com>

--- libavformat/g723_1.c
+++ libavformat/g723_1.c
@@ -41,8 +41,7 @@ static av_cold int g723_1_init(AVFormatContext *s)
 
     st->codecpar->codec_type     = AVMEDIA_TYPE_AUDIO;
     st->codecpar->codec_id       = AV_CODEC_ID_G723_1;
-    st->codecpar->channel_layout = AV_CH_LAYOUT_MONO;
-    st->codecpar->channels       = 1;
+    st->codecpar->ch_layout      = (AVChannelLayout)AV_CHANNEL_LAYOUT_MONO;
     st->codecpar->sample_rate    = 8000;
 
     avpriv_set_pts_info(st, 64, 1, st->codecpar->sample_rate);
