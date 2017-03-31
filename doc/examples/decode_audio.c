b27dd80255a0a5f22b02b512ba218ffb44f5902a
James Almer <jamrial@gmail.com>
2017-03-31 16:50:39 -0300

    doc/decode_audio: use <> to include libav* headers
    Found-by: ubitux

--- doc/examples/decode_audio.c
+++ doc/examples/decode_audio.c
@@ -31,10 +31,10 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "libavutil/frame.h"
-#include "libavutil/mem.h"
+#include <libavutil/frame.h>
+#include <libavutil/mem.h>
 
-#include "libavcodec/avcodec.h"
+#include <libavcodec/avcodec.h>
 
 #define AUDIO_INBUF_SIZE 20480
 #define AUDIO_REFILL_THRESH 4096
