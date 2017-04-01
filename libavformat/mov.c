f4400a92f58fbe19b14eb675c241f7df310758fd
Michael Niedermayer <michael@niedermayer.cc>
2017-04-01 19:18:36 +0200

    avformat/mov: Check creation_time for overflow
    Fixes integer overflow
    Fixes: 701640
    
    Found-by: Found-by: Thomas Guilbert <tguilbert@google.com>
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit 39ee3ddff87a12e108fc4e0d36f756d0ca080472)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavformat/mov.c
+++ libavformat/mov.c
@@ -1186,6 +1186,12 @@ static void mov_metadata_creation_time(AVDictionary **metadata, int64_t time)
     if (time) {
         if(time >= 2082844800)
             time -= 2082844800;  /* seconds between 1904-01-01 and Epoch */
+
+        if ((int64_t)(time * 1000000ULL) / 1000000 != time) {
+            av_log(NULL, AV_LOG_DEBUG, "creation_time is not representable\n");
+            return;
+        }
+
         avpriv_dict_set_timestamp(metadata, "creation_time", time * 1000000);
     }
 }
