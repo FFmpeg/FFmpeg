279e3aaa14daba6b7a37d75f3fb6e29c732d123f
Luca Barbato <lu_zero@gentoo.org>
2017-04-17 14:10:52 +0000

    flv: Validate the packet size
    Size can be negative at that point.
    
    Bug-Id: 1041
    CC: libav-stable@libav.org

--- libavformat/flvdec.c
+++ libavformat/flvdec.c
@@ -912,6 +912,12 @@ skip:
         st->codecpar->codec_id == AV_CODEC_ID_H264) {
         int type = avio_r8(s->pb);
         size--;
+
+        if (size < 0) {
+            ret = AVERROR_INVALIDDATA;
+            goto leave;
+        }
+
         if (st->codecpar->codec_id == AV_CODEC_ID_H264) {
             // sign extension
             int32_t cts = (avio_rb24(s->pb) + 0xff800000) ^ 0xff800000;
