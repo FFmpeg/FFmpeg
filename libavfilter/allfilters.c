74acc1eec58288c9f3e35924a88807ab7dd33569
Paul B Mahol <onemda@gmail.com>
2017-04-18 15:18:40 +0200

    avfilter: add deflicker filter
    Signed-off-by: Paul B Mahol <onemda@gmail.com>

--- libavfilter/allfilters.c
+++ libavfilter/allfilters.c
@@ -166,6 +166,7 @@ static void register_all(void)
     REGISTER_FILTER(DEBAND,         deband,         vf);
     REGISTER_FILTER(DECIMATE,       decimate,       vf);
     REGISTER_FILTER(DEFLATE,        deflate,        vf);
+    REGISTER_FILTER(DEFLICKER,      deflicker,      vf);
     REGISTER_FILTER(DEINTERLACE_QSV,deinterlace_qsv,vf);
     REGISTER_FILTER(DEINTERLACE_VAAPI, deinterlace_vaapi, vf);
     REGISTER_FILTER(DEJUDDER,       dejudder,       vf);
