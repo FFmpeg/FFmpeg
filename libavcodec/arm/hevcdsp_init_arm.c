edbf0fffb15dde7a1de70b05855529d5fc769f14
Alexandra Hájková <alexandra.khirnova@gmail.com>
2017-05-01 20:16:02 +0200

    hevc: Add NEON add_residual for bitdepth 10
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- libavcodec/arm/hevcdsp_init_arm.c
+++ libavcodec/arm/hevcdsp_init_arm.c
@@ -28,12 +28,20 @@
 
 void ff_hevc_add_residual_4x4_8_neon(uint8_t *_dst, int16_t *coeffs,
                                      ptrdiff_t stride);
+void ff_hevc_add_residual_4x4_10_neon(uint8_t *_dst, int16_t *coeffs,
+                                      ptrdiff_t stride);
 void ff_hevc_add_residual_8x8_8_neon(uint8_t *_dst, int16_t *coeffs,
                                      ptrdiff_t stride);
+void ff_hevc_add_residual_8x8_10_neon(uint8_t *_dst, int16_t *coeffs,
+                                      ptrdiff_t stride);
 void ff_hevc_add_residual_16x16_8_neon(uint8_t *_dst, int16_t *coeffs,
                                        ptrdiff_t stride);
+void ff_hevc_add_residual_16x16_10_neon(uint8_t *_dst, int16_t *coeffs,
+                                        ptrdiff_t stride);
 void ff_hevc_add_residual_32x32_8_neon(uint8_t *_dst, int16_t *coeffs,
                                        ptrdiff_t stride);
+void ff_hevc_add_residual_32x32_10_neon(uint8_t *_dst, int16_t *coeffs,
+                                        ptrdiff_t stride);
 
 void ff_hevc_idct_4x4_dc_8_neon(int16_t *coeffs);
 void ff_hevc_idct_8x8_dc_8_neon(int16_t *coeffs);
@@ -72,6 +80,11 @@ av_cold void ff_hevc_dsp_init_arm(HEVCDSPContext *c, int bit_depth)
             c->idct[2] = ff_hevc_idct_16x16_8_neon;
         }
         if (bit_depth == 10) {
+            c->add_residual[0] = ff_hevc_add_residual_4x4_10_neon;
+            c->add_residual[1] = ff_hevc_add_residual_8x8_10_neon;
+            c->add_residual[2] = ff_hevc_add_residual_16x16_10_neon;
+            c->add_residual[3] = ff_hevc_add_residual_32x32_10_neon;
+
             c->idct_dc[0] = ff_hevc_idct_4x4_dc_10_neon;
             c->idct_dc[1] = ff_hevc_idct_8x8_dc_10_neon;
             c->idct_dc[2] = ff_hevc_idct_16x16_dc_10_neon;
