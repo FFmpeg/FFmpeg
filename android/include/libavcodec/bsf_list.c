a4326d2226380537d7d4d505f29c39d504e0ca82
Chih-Wei Huang <cwhuang@linux.org.tw>
2017-05-02 16:35:42 +0800

    android: update generated files

--- android/include/libavcodec/bsf_list.c
+++ android/include/libavcodec/bsf_list.c
@@ -3,6 +3,7 @@ static const AVBitStreamFilter *bitstream_filters[] = {
     &ff_chomp_bsf,
     &ff_dump_extradata_bsf,
     &ff_dca_core_bsf,
+    &ff_extract_extradata_bsf,
     &ff_h264_mp4toannexb_bsf,
     &ff_hevc_mp4toannexb_bsf,
     &ff_imx_dump_header_bsf,
@@ -12,7 +13,9 @@ static const AVBitStreamFilter *bitstream_filters[] = {
     &ff_mpeg4_unpack_bframes_bsf,
     &ff_mov2textsub_bsf,
     &ff_noise_bsf,
+    &ff_null_bsf,
     &ff_remove_extradata_bsf,
     &ff_text2movsub_bsf,
     &ff_vp9_superframe_bsf,
+    &ff_vp9_superframe_split_bsf,
     NULL };
