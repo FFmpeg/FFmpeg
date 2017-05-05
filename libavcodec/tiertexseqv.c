1e8212798c823c312d9c433c9cf00a633fcd79a2
Michael Niedermayer <michael@niedermayer.cc>
2017-05-05 12:48:12 +0200

    avcodec/tiertexseqv: set the fixed dimenasions, do not depend on the demuxer doing so
    Fixes: out of array access
    Fixes: 1348/clusterfuzz-testcase-minimized-6195673642827776
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/targets/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit ce551a3925a1cf9c7824e26a246b99b6773bda4b)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/tiertexseqv.c
+++ libavcodec/tiertexseqv.c
@@ -213,9 +213,14 @@ static int seqvideo_decode(SeqVideoContext *seq, const unsigned char *data, int 
 static av_cold int seqvideo_decode_init(AVCodecContext *avctx)
 {
     SeqVideoContext *seq = avctx->priv_data;
+    int ret;
 
     seq->avctx = avctx;
     avctx->pix_fmt = AV_PIX_FMT_PAL8;
+
+    ret = ff_set_dimensions(avctx, 256, 128);
+    if (ret < 0)
+        return ret;
 
     seq->frame = av_frame_alloc();
     if (!seq->frame)
