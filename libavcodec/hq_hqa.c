f20c485e4e4185a849e6d15b240459f4385966b3
Michael Niedermayer <michael@niedermayer.cc>
2017-05-06 19:11:46 +0200

    avcodec/hq_hqa: Fix runtime error: left shift of negative value -207
    Fixes: 1375/clusterfuzz-testcase-minimized-6070134701555712
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/targets/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit 1283c4244767bd19918f355c31d702a94ee0cc1b)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/hq_hqa.c
+++ libavcodec/hq_hqa.c
@@ -68,11 +68,11 @@ static int hq_decode_block(HQContext *c, GetBitContext *gb, int16_t block[64],
     memset(block, 0, 64 * sizeof(*block));
 
     if (!is_hqa) {
-        block[0] = get_sbits(gb, 9) << 6;
+        block[0] = get_sbits(gb, 9) * 64;
         q = ff_hq_quants[qsel][is_chroma][get_bits(gb, 2)];
     } else {
         q = ff_hq_quants[qsel][is_chroma][get_bits(gb, 2)];
-        block[0] = get_sbits(gb, 9) << 6;
+        block[0] = get_sbits(gb, 9) * 64;
     }
 
     for (;;) {
