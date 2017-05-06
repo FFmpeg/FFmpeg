6f590bf05fe5694639fd2f0bb6840c2a74c8f31d
Michael Niedermayer <michael@niedermayer.cc>
2017-05-06 12:17:01 +0200

    avcodec/clearvideo: Fix multiple runtime error: left shift of negative value -1024
    Fixes: 1360/clusterfuzz-testcase-minimized-5606472043986944
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/targets/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit c0ffcb34c7e94817be934a1ee4a0b4c054723549)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/clearvideo.c
+++ libavcodec/clearvideo.c
@@ -185,8 +185,8 @@ static inline int decode_block(CLVContext *ctx, int16_t *blk, int has_ac,
     const int t3 = OP( 2408 * blk[5 * step] - 1609 * blk[3 * step]);    \
     const int t4 = OP( 1108 * blk[2 * step] - 2676 * blk[6 * step]);    \
     const int t5 = OP( 2676 * blk[2 * step] + 1108 * blk[6 * step]);    \
-    const int t6 = ((blk[0 * step] + blk[4 * step]) << dshift) + bias;  \
-    const int t7 = ((blk[0 * step] - blk[4 * step]) << dshift) + bias;  \
+    const int t6 = ((blk[0 * step] + blk[4 * step]) * (1 << dshift)) + bias;  \
+    const int t7 = ((blk[0 * step] - blk[4 * step]) * (1 << dshift)) + bias;  \
     const int t8 = t0 + t2;                                             \
     const int t9 = t0 - t2;                                             \
     const int tA = 181 * (t9 + (t1 - t3)) + 0x80 >> 8;                  \
