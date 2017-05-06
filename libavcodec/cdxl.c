7a69c1b2abfa96f0578cbd3ff82126b883ba6ef0
Michael Niedermayer <michael@niedermayer.cc>
2017-05-06 22:24:52 +0200

    avcodec/cdxl: Check format parameter
    Fixes out of array access
    Fixes: 1378/clusterfuzz-testcase-minimized-5715088008806400
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/targets/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit e1b60aad77c27ed5d4dfc11e5e6a05a38c70489d)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/cdxl.c
+++ libavcodec/cdxl.c
@@ -277,7 +277,7 @@ static int cdxl_decode_frame(AVCodecContext *avctx, void *data,
     c->padded_bits  = aligned_width - c->avctx->width;
     if (c->video_size < aligned_width * avctx->height * (int64_t)c->bpp / 8)
         return AVERROR_INVALIDDATA;
-    if (!encoding && c->palette_size && c->bpp <= 8) {
+    if (!encoding && c->palette_size && c->bpp <= 8 && c->format != CHUNKY) {
         avctx->pix_fmt = AV_PIX_FMT_PAL8;
     } else if (encoding == 1 && (c->bpp == 6 || c->bpp == 8)) {
         if (c->palette_size != (1 << (c->bpp - 1)))
