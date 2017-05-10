7735b8e565f16ece7ad46a59a7bb35ad60998c13
Michael Niedermayer <michael@niedermayer.cc>
2017-05-10 18:37:50 +0200

    avcodec/webp: Fixes null pointer dereference
    Fixes: 1470/clusterfuzz-testcase-minimized-5404421666111488
    Fixes: 1472/clusterfuzz-testcase-minimized-5677426430443520
    Fixes: 1875/clusterfuzz-testcase-minimized-5536474562822144
    
    Approved-by: BBB
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit 67020711b7d45afa073ef671f755765035a64373)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/webp.c
+++ libavcodec/webp.c
@@ -1334,6 +1334,9 @@ static int vp8_lossy_decode_frame(AVCodecContext *avctx, AVFrame *p,
     if (ret < 0)
         return ret;
 
+    if (!*got_frame)
+        return AVERROR_INVALIDDATA;
+
     update_canvas_size(avctx, avctx->width, avctx->height);
 
     if (s->has_alpha) {
