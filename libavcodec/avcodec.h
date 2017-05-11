d5c3132d6fbd7528b07c6492e2728baf97a577a9
Michael Niedermayer <michael@niedermayer.cc>
2017-05-11 13:01:36 +0200

    avcodec/avcodec: Limit the number of side data elements per packet
    Fixes: 1293/clusterfuzz-testcase-minimized-6054752074858496
    
    See: [FFmpeg-devel] [PATCH] avcodec/avcodec: Limit the number of side data elements per packet
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/targets/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit d5711cb89121268e8d78ebe8563a68e67a236cbb)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/avcodec.h
+++ libavcodec/avcodec.h
@@ -1515,7 +1515,17 @@ enum AVPacketSideDataType {
      * should be associated with a video stream and containts data in the form
      * of the AVMasteringDisplayMetadata struct.
      */
-    AV_PKT_DATA_MASTERING_DISPLAY_METADATA
+    AV_PKT_DATA_MASTERING_DISPLAY_METADATA,
+
+    /**
+     * The number of side data elements (in fact a bit more than it).
+     * This is not part of the public API/ABI in the sense that it may
+     * change when new side data types are added.
+     * This must stay the last enum value.
+     * If its value becomes huge, some code using it
+     * needs to be updated as it assumes it to be smaller than other limits.
+     */
+    AV_PKT_DATA_NB
 };
 
 #define AV_PKT_DATA_QUALITY_FACTOR AV_PKT_DATA_QUALITY_STATS //DEPRECATED
