dc46625d34c3572880d0939652f9085e7a21ac5d
raymondzheng <raymondzheng1412@gmail.com>
2017-05-12 12:21:37 +0800

    avformat/pipe: enable seekable

--- libavformat/file.c
+++ libavformat/file.c
@@ -386,7 +386,7 @@ static int pipe_open(URLContext *h, const char *filename, int flags)
     setmode(fd, O_BINARY);
 #endif
     c->fd = fd;
-    h->is_streamed = 1;
+    h->is_streamed = 0;
     return 0;
 }
 
@@ -395,6 +395,7 @@ const URLProtocol ff_pipe_protocol = {
     .url_open            = pipe_open,
     .url_read            = file_read,
     .url_write           = file_write,
+    .url_seek            = file_seek,
     .url_get_file_handle = file_get_handle,
     .url_check           = file_check,
     .priv_data_size      = sizeof(FileContext),
