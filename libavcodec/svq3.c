8e6d9d48a0b4369d71815d104df32b067fe9bc60
Michael Niedermayer <michael@niedermayer.cc>
2017-05-15 21:21:20 +0200

    avcodec/svq3: Fix runtime error: left shift of negative value -6
    Fixes: 1604/clusterfuzz-testcase-minimized-5312060206350336
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/targets/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit a6eb006ad47beb6d5e5cc2c99f8185965209ec6b)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/svq3.c
+++ libavcodec/svq3.c
@@ -524,8 +524,8 @@ static inline int svq3_mc_dir(SVQ3Context *s, int size, int mode,
             if (mode != PREDICT_MODE) {
                 svq3_pred_motion(s, k, part_width >> 2, dir, 1, &mx, &my);
             } else {
-                mx = s->next_pic->motion_val[0][b_xy][0] << 1;
-                my = s->next_pic->motion_val[0][b_xy][1] << 1;
+                mx = s->next_pic->motion_val[0][b_xy][0] * 2;
+                my = s->next_pic->motion_val[0][b_xy][1] * 2;
 
                 if (dir == 0) {
                     mx = mx * s->frame_num_offset /
