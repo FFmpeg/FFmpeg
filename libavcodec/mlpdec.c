63cc52ed97f57ae47f47118ee84ac3877c591b82
Michael Niedermayer <michael@niedermayer.cc>
2017-05-17 00:53:32 +0200

    avcodec/mlpdec: Fix runtime error: left shift of negative value -1
    Fixes: 1636/clusterfuzz-testcase-minimized-5310494757879808
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/targets/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit 552adf1dd3a38fb7a1a6109dd2b517d63290f20e)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/mlpdec.c
+++ libavcodec/mlpdec.c
@@ -701,7 +701,7 @@ static int read_filter_params(MLPDecodeContext *m, GetBitContext *gbp,
             /* TODO: Check validity of state data. */
 
             for (i = 0; i < order; i++)
-                fp->state[i] = state_bits ? get_sbits(gbp, state_bits) << state_shift : 0;
+                fp->state[i] = state_bits ? get_sbits(gbp, state_bits) * (1 << state_shift) : 0;
         }
     }
 
