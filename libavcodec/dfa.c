cd5f0c32bab1b4d07372ba48bf910e634029f25d
Michael Niedermayer <michael@niedermayer.cc>
2017-05-18 01:54:43 +0200

    avcodec/dfa: Fix: runtime error: signed integer overflow: -14202 * 196877 cannot be represented in type 'int'
    Fixes: 1657/clusterfuzz-testcase-minimized-4710000079405056
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/targets/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit 58ac7fb9c395ab91cb321fa4c8c9e127ce8147c3)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/dfa.c
+++ libavcodec/dfa.c
@@ -250,7 +250,7 @@ static int decode_wdlt(GetByteContext *gb, uint8_t *frame, int width, int height
         segments = bytestream2_get_le16u(gb);
         while ((segments & 0xC000) == 0xC000) {
             unsigned skip_lines = -(int16_t)segments;
-            unsigned delta = -((int16_t)segments * width);
+            int64_t delta = -((int16_t)segments * (int64_t)width);
             if (frame_end - frame <= delta || y + lines + skip_lines > height)
                 return AVERROR_INVALIDDATA;
             frame    += delta;
