wiseplay-app/ijkplayer-ffmpeg

avformat/concatdec: fix the h264 annexb extradata check
The start code can be either in the first three or four bytes.

libavformat/concatdec.c
@@ -199,8 +199,11 @@ static int detect_stream_specific(AVFormatContext *avf, int idx)
     AVBitStreamFilterContext *bsf;
     int ret;
 
-    if (cat->auto_convert && st->codecpar->codec_id == AV_CODEC_ID_H264 &&
-        (st->codecpar->extradata_size < 4 || AV_RB32(st->codecpar->extradata) != 1)) {
+    if (cat->auto_convert && st->codecpar->codec_id == AV_CODEC_ID_H264) {
+        if (!st->codecpar->extradata_size                                                ||
+            (st->codecpar->extradata_size >= 3 && AV_RB24(st->codecpar->extradata) == 1) ||
+            (st->codecpar->extradata_size >= 4 && AV_RB32(st->codecpar->extradata) == 1))
+            return 0;
         av_log(cat->avf, AV_LOG_INFO,
                "Auto-inserting h264_mp4toannexb bitstream filter\n");
         if (!(bsf = av_bitstream_filter_init("h264_mp4toannexb"))) {

FFmpeg/FFmpeg

(cherry picked from commit b4330a0e02fcbef61d630a369abe5f4421ced659)

libavformat/concatdec.c
@@ -193,8 +193,11 @@ static int detect_stream_specific(AVFormatContext *avf, int idx)
     ConcatStream *cs = &cat->cur_file->streams[idx];
     AVBitStreamFilterContext *bsf;
 
-    if (cat->auto_convert && st->codec->codec_id == AV_CODEC_ID_H264 &&
-        (st->codec->extradata_size < 4 || AV_RB32(st->codec->extradata) != 1)) {
+    if (cat->auto_convert && st->codec->codec_id == AV_CODEC_ID_H264) {
+        if (!st->codec->extradata_size                                             ||
+            (st->codec->extradata_size >= 3 && AV_RB24(st->codec->extradata) == 1) ||
+            (st->codec->extradata_size >= 4 && AV_RB32(st->codec->extradata) == 1))
+            return 0;
         av_log(cat->avf, AV_LOG_INFO,
                "Auto-inserting h264_mp4toannexb bitstream filter\n");
         if (!(bsf = av_bitstream_filter_init("h264_mp4toannexb"))) {