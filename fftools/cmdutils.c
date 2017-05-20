8cfab9fa8c1529f3d4db3e7d5b317acdbc08d37d
Anton Khirnov <anton@khirnov.net>
2017-05-20 12:55:00 +0200

    cmdutils: drop libavformat/network.h include
    It is not a public header and has not been used since
    10173c0e58e557582dbd659f42c6aa164a8682db

--- fftools/cmdutils.c
+++ fftools/cmdutils.c
@@ -55,9 +55,6 @@
 #include "libavutil/ffversion.h"
 #include "libavutil/version.h"
 #include "cmdutils.h"
-#if CONFIG_NETWORK
-#include "libavformat/network.h"
-#endif
 #if HAVE_SYS_RESOURCE_H
 #include <sys/time.h>
 #include <sys/resource.h>
