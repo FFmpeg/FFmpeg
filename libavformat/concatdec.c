FFmpeg/FFmpeg

commit f5da453b068f55d335ca403d2e2b4dd2ac3d4331

concatdec: pass AVFormatContext flags to sub demuxer

libavformat/concatdec.c
@@ -322,6 +322,7 @@ static int open_file(AVFormatContext *avf, unsigned fileno)
     if (!cat->avf)
         return AVERROR(ENOMEM);
 
+    cat->avf->flags |= avf->flags;
     cat->avf->interrupt_callback = avf->interrupt_callback;
 
     if ((ret = ff_copy_whiteblacklists(cat->avf, avf)) < 0)

tests/ref/fate/concat-demuxer-simple2-lavf-ts
@@ -1,12 +1,21 @@
-video|1|982|0.010911|-2618|-0.029089|3600|0.040000|N/A|N/A|24815|564|K_
-video|1|4582|0.050911|982|0.010911|3600|0.040000|N/A|N/A|16443|27072|__
-video|1|8182|0.090911|4582|0.050911|3600|0.040000|N/A|N/A|14522|44932|__
-video|1|11782|0.130911|8182|0.090911|3600|0.040000|N/A|N/A|12636|60536|__
-video|1|15382|0.170911|11782|0.130911|3600|0.040000|N/A|N/A|13407|74260|__
-video|1|18982|0.210911|15382|0.170911|3600|0.040000|N/A|N/A|13106|88924|__
-video|1|22582|0.250911|18982|0.210911|3600|0.040000|N/A|N/A|12769|102836|__
-video|1|26182|0.290911|22582|0.250911|3600|0.040000|N/A|N/A|12037|116748|__
-audio|0|0|0.000000|0|0.000000|2351|0.026122|N/A|N/A|222|159988|K_
+video|1|982|0.010911|-2618|-0.029089|3600|0.040000|N/A|N/A|24801|564|K_MPEGTS Stream ID|1
+
+video|1|4582|0.050911|982|0.010911|3600|0.040000|N/A|N/A|16429|27072|__MPEGTS Stream ID|1
+
+video|1|8182|0.090911|4582|0.050911|3600|0.040000|N/A|N/A|14508|44932|__MPEGTS Stream ID|1
+
+video|1|11782|0.130911|8182|0.090911|3600|0.040000|N/A|N/A|12622|60536|__MPEGTS Stream ID|1
+
+video|1|15382|0.170911|11782|0.130911|3600|0.040000|N/A|N/A|13393|74260|__MPEGTS Stream ID|1
+
+video|1|18982|0.210911|15382|0.170911|3600|0.040000|N/A|N/A|13092|88924|__MPEGTS Stream ID|1
+
+video|1|22582|0.250911|18982|0.210911|3600|0.040000|N/A|N/A|12755|102836|__MPEGTS Stream ID|1
+
+video|1|26182|0.290911|22582|0.250911|3600|0.040000|N/A|N/A|12023|116748|__MPEGTS Stream ID|1
+
+audio|0|0|0.000000|0|0.000000|2351|0.026122|N/A|N/A|208|159988|K_MPEGTS Stream ID|1
+
 audio|0|2351|0.026122|2351|0.026122|2351|0.026122|N/A|N/A|209|N/A|K_
 audio|0|4702|0.052244|4702|0.052244|2351|0.026122|N/A|N/A|209|N/A|K_
 audio|0|7053|0.078367|7053|0.078367|2351|0.026122|N/A|N/A|209|N/A|K_
@@ -20,17 +29,28 @@ audio|0|23510|0.261222|23510|0.261222|2351|0.026122|N/A|N/A|209|N/A|K_
 audio|0|25861|0.287344|25861|0.287344|2351|0.026122|N/A|N/A|209|N/A|K_
 audio|0|28212|0.313467|28212|0.313467|2351|0.026122|N/A|N/A|209|N/A|K_
 audio|0|30563|0.339589|30563|0.339589|2351|0.026122|N/A|N/A|209|N/A|K_
-video|1|29782|0.330911|26182|0.290911|3600|0.040000|N/A|N/A|14112|130096|__
-video|1|33382|0.370911|29782|0.330911|3600|0.040000|N/A|N/A|13343|145324|__
-video|1|36982|0.410911|33382|0.370911|3600|0.040000|N/A|N/A|12149|162996|__
-video|1|40582|0.450911|36982|0.410911|3600|0.040000|N/A|N/A|12296|176344|__
-video|1|44182|0.490911|40582|0.450911|3600|0.040000|N/A|N/A|24800|189692|K_
-video|1|47782|0.530911|44182|0.490911|3600|0.040000|N/A|N/A|17454|216388|__
-video|1|51382|0.570911|47782|0.530911|3600|0.040000|N/A|N/A|15033|235000|__
-video|1|54982|0.610911|51382|0.570911|3600|0.040000|N/A|N/A|13463|251356|__
-video|1|58582|0.650911|54982|0.610911|3600|0.040000|N/A|N/A|12412|266020|__
-video|1|62182|0.690911|58582|0.650911|3600|0.040000|N/A|N/A|13469|279744|__
-audio|0|32915|0.365722|32915|0.365722|2351|0.026122|N/A|N/A|223|322608|K_
+video|1|29782|0.330911|26182|0.290911|3600|0.040000|N/A|N/A|14098|130096|__MPEGTS Stream ID|1
+
+video|1|33382|0.370911|29782|0.330911|3600|0.040000|N/A|N/A|13329|145324|__MPEGTS Stream ID|1
+
+video|1|36982|0.410911|33382|0.370911|3600|0.040000|N/A|N/A|12135|162996|__MPEGTS Stream ID|1
+
+video|1|40582|0.450911|36982|0.410911|3600|0.040000|N/A|N/A|12282|176344|__MPEGTS Stream ID|1
+
+video|1|44182|0.490911|40582|0.450911|3600|0.040000|N/A|N/A|24786|189692|K_MPEGTS Stream ID|1
+
+video|1|47782|0.530911|44182|0.490911|3600|0.040000|N/A|N/A|17440|216388|__MPEGTS Stream ID|1
+
+video|1|51382|0.570911|47782|0.530911|3600|0.040000|N/A|N/A|15019|235000|__MPEGTS Stream ID|1
+
+video|1|54982|0.610911|51382|0.570911|3600|0.040000|N/A|N/A|13449|251356|__MPEGTS Stream ID|1
+
+video|1|58582|0.650911|54982|0.610911|3600|0.040000|N/A|N/A|12398|266020|__MPEGTS Stream ID|1
+
+video|1|62182|0.690911|58582|0.650911|3600|0.040000|N/A|N/A|13455|279744|__MPEGTS Stream ID|1
+
+audio|0|32915|0.365722|32915|0.365722|2351|0.026122|N/A|N/A|209|322608|K_MPEGTS Stream ID|1
+
 audio|0|35266|0.391844|35266|0.391844|2351|0.026122|N/A|N/A|209|N/A|K_
 audio|0|37617|0.417967|37617|0.417967|2351|0.026122|N/A|N/A|209|N/A|K_
 audio|0|39968|0.444089|39968|0.444089|2351|0.026122|N/A|N/A|209|N/A|K_
@@ -44,12 +64,18 @@ audio|0|56425|0.626944|56425|0.626944|2351|0.026122|N/A|N/A|209|N/A|K_
 audio|0|58776|0.653067|58776|0.653067|2351|0.026122|N/A|N/A|209|N/A|K_
 audio|0|61127|0.679189|61127|0.679189|2351|0.026122|N/A|N/A|209|N/A|K_
 audio|0|63478|0.705311|63478|0.705311|2351|0.026122|N/A|N/A|209|N/A|K_
-video|1|65782|0.730911|62182|0.690911|3600|0.040000|N/A|N/A|13850|294408|__
-video|1|69382|0.770911|65782|0.730911|3600|0.040000|N/A|N/A|12177|309448|__
-video|1|72982|0.810911|69382|0.770911|3600|0.040000|N/A|N/A|12706|325992|__
-video|1|76582|0.850911|72982|0.810911|3600|0.040000|N/A|N/A|10838|339528|__
-video|1|80182|0.890911|76582|0.850911|3600|0.040000|N/A|N/A|11300|351372|__
-audio|0|65829|0.731433|65829|0.731433|2351|0.026122|N/A|N/A|223|404576|K_
+video|1|65782|0.730911|62182|0.690911|3600|0.040000|N/A|N/A|13836|294408|__MPEGTS Stream ID|1
+
+video|1|69382|0.770911|65782|0.730911|3600|0.040000|N/A|N/A|12163|309448|__MPEGTS Stream ID|1
+
+video|1|72982|0.810911|69382|0.770911|3600|0.040000|N/A|N/A|12692|325992|__MPEGTS Stream ID|1
+
+video|1|76582|0.850911|72982|0.810911|3600|0.040000|N/A|N/A|10824|339528|__MPEGTS Stream ID|1
+
+video|1|80182|0.890911|76582|0.850911|3600|0.040000|N/A|N/A|11286|351372|__MPEGTS Stream ID|1
+
+audio|0|65829|0.731433|65829|0.731433|2351|0.026122|N/A|N/A|209|404576|K_MPEGTS Stream ID|1
+
 audio|0|68180|0.757556|68180|0.757556|2351|0.026122|N/A|N/A|209|N/A|K_
 audio|0|70531|0.783678|70531|0.783678|2351|0.026122|N/A|N/A|209|N/A|K_
 audio|0|72882|0.809800|72882|0.809800|2351|0.026122|N/A|N/A|209|N/A|K_
@@ -60,17 +86,27 @@ audio|0|82286|0.914289|82286|0.914289|2351|0.026122|N/A|N/A|209|N/A|K_
 audio|0|84637|0.940411|84637|0.940411|2351|0.026122|N/A|N/A|209|N/A|K_
 audio|0|86988|0.966533|86988|0.966533|2351|0.026122|N/A|N/A|209|N/A|K_
 audio|0|89339|0.992656|89339|0.992656|2351|0.026122|N/A|N/A|209|N/A|K_
-video|1|83782|0.930911|80182|0.890911|3600|0.040000|N/A|N/A|12692|363592|__
+video|1|83782|0.930911|80182|0.890911|3600|0.040000|N/A|N/A|12678|363592|__MPEGTS Stream ID|1
+
 video|1|87382|0.970911|83782|0.930911|3600|0.040000|N/A|N/A|24711|377880|K_
-video|1|91964|1.021822|88364|0.981822|3600|0.040000|N/A|N/A|24815|564|K_
-video|1|95564|1.061822|91964|1.021822|3600|0.040000|N/A|N/A|16443|27072|__
-video|1|99164|1.101822|95564|1.061822|3600|0.040000|N/A|N/A|14522|44932|__
-video|1|102764|1.141822|99164|1.101822|3600|0.040000|N/A|N/A|12636|60536|__
-video|1|106364|1.181822|102764|1.141822|3600|0.040000|N/A|N/A|13407|74260|__
-video|1|109964|1.221822|106364|1.181822|3600|0.040000|N/A|N/A|13106|88924|__
-video|1|113564|1.261822|109964|1.221822|3600|0.040000|N/A|N/A|12769|102836|__
-video|1|117164|1.301822|113564|1.261822|3600|0.040000|N/A|N/A|12037|116748|__
-audio|0|90982|1.010911|90982|1.010911|2351|0.026122|N/A|N/A|222|159988|K_
+video|1|91964|1.021822|88364|0.981822|3600|0.040000|N/A|N/A|24801|564|K_MPEGTS Stream ID|1
+
+video|1|95564|1.061822|91964|1.021822|3600|0.040000|N/A|N/A|16429|27072|__MPEGTS Stream ID|1
+
+video|1|99164|1.101822|95564|1.061822|3600|0.040000|N/A|N/A|14508|44932|__MPEGTS Stream ID|1
+
+video|1|102764|1.141822|99164|1.101822|3600|0.040000|N/A|N/A|12622|60536|__MPEGTS Stream ID|1
+
+video|1|106364|1.181822|102764|1.141822|3600|0.040000|N/A|N/A|13393|74260|__MPEGTS Stream ID|1
+
+video|1|109964|1.221822|106364|1.181822|3600|0.040000|N/A|N/A|13092|88924|__MPEGTS Stream ID|1
+
+video|1|113564|1.261822|109964|1.221822|3600|0.040000|N/A|N/A|12755|102836|__MPEGTS Stream ID|1
+
+video|1|117164|1.301822|113564|1.261822|3600|0.040000|N/A|N/A|12023|116748|__MPEGTS Stream ID|1
+
+audio|0|90982|1.010911|90982|1.010911|2351|0.026122|N/A|N/A|208|159988|K_MPEGTS Stream ID|1
+
 audio|0|93333|1.037033|93333|1.037033|2351|0.026122|N/A|N/A|209|N/A|K_
 audio|0|95684|1.063156|95684|1.063156|2351|0.026122|N/A|N/A|209|N/A|K_
 audio|0|98035|1.089278|98035|1.089278|2351|0.026122|N/A|N/A|209|N/A|K_
@@ -84,17 +120,28 @@ audio|0|114492|1.272133|114492|1.272133|2351|0.026122|N/A|N/A|209|N/A|K_
 audio|0|116843|1.298256|116843|1.298256|2351|0.026122|N/A|N/A|209|N/A|K_
 audio|0|119194|1.324378|119194|1.324378|2351|0.026122|N/A|N/A|209|N/A|K_
 audio|0|121545|1.350500|121545|1.350500|2351|0.026122|N/A|N/A|209|N/A|K_
-video|1|120764|1.341822|117164|1.301822|3600|0.040000|N/A|N/A|14112|130096|__
-video|1|124364|1.381822|120764|1.341822|3600|0.040000|N/A|N/A|13343|145324|__
-video|1|127964|1.421822|124364|1.381822|3600|0.040000|N/A|N/A|12149|162996|__
-video|1|131564|1.461822|127964|1.421822|3600|0.040000|N/A|N/A|12296|176344|__
-video|1|135164|1.501822|131564|1.461822|3600|0.040000|N/A|N/A|24800|189692|K_
-video|1|138764|1.541822|135164|1.501822|3600|0.040000|N/A|N/A|17454|216388|__
-video|1|142364|1.581822|138764|1.541822|3600|0.040000|N/A|N/A|15033|235000|__
-video|1|145964|1.621822|142364|1.581822|3600|0.040000|N/A|N/A|13463|251356|__
-video|1|149564|1.661822|145964|1.621822|3600|0.040000|N/A|N/A|12412|266020|__
-video|1|153164|1.701822|149564|1.661822|3600|0.040000|N/A|N/A|13469|279744|__
-audio|0|123897|1.376633|123897|1.376633|2351|0.026122|N/A|N/A|223|322608|K_
+video|1|120764|1.341822|117164|1.301822|3600|0.040000|N/A|N/A|14098|130096|__MPEGTS Stream ID|1
+
+video|1|124364|1.381822|120764|1.341822|3600|0.040000|N/A|N/A|13329|145324|__MPEGTS Stream ID|1
+
+video|1|127964|1.421822|124364|1.381822|3600|0.040000|N/A|N/A|12135|162996|__MPEGTS Stream ID|1
+
+video|1|131564|1.461822|127964|1.421822|3600|0.040000|N/A|N/A|12282|176344|__MPEGTS Stream ID|1
+
+video|1|135164|1.501822|131564|1.461822|3600|0.040000|N/A|N/A|24786|189692|K_MPEGTS Stream ID|1
+
+video|1|138764|1.541822|135164|1.501822|3600|0.040000|N/A|N/A|17440|216388|__MPEGTS Stream ID|1
+
+video|1|142364|1.581822|138764|1.541822|3600|0.040000|N/A|N/A|15019|235000|__MPEGTS Stream ID|1
+
+video|1|145964|1.621822|142364|1.581822|3600|0.040000|N/A|N/A|13449|251356|__MPEGTS Stream ID|1
+
+video|1|149564|1.661822|145964|1.621822|3600|0.040000|N/A|N/A|12398|266020|__MPEGTS Stream ID|1
+
+video|1|153164|1.701822|149564|1.661822|3600|0.040000|N/A|N/A|13455|279744|__MPEGTS Stream ID|1
+
+audio|0|123897|1.376633|123897|1.376633|2351|0.026122|N/A|N/A|209|322608|K_MPEGTS Stream ID|1
+
 audio|0|126248|1.402756|126248|1.402756|2351|0.026122|N/A|N/A|209|N/A|K_
 audio|0|128599|1.428878|128599|1.428878|2351|0.026122|N/A|N/A|209|N/A|K_
 audio|0|130950|1.455000|130950|1.455000|2351|0.026122|N/A|N/A|209|N/A|K_
@@ -108,12 +155,18 @@ audio|0|147407|1.637856|147407|1.637856|2351|0.026122|N/A|N/A|209|N/A|K_
 audio|0|149758|1.663978|149758|1.663978|2351|0.026122|N/A|N/A|209|N/A|K_
 audio|0|152109|1.690100|152109|1.690100|2351|0.026122|N/A|N/A|209|N/A|K_
 audio|0|154460|1.716222|154460|1.716222|2351|0.026122|N/A|N/A|209|N/A|K_
-video|1|156764|1.741822|153164|1.701822|3600|0.040000|N/A|N/A|13850|294408|__
-video|1|160364|1.781822|156764|1.741822|3600|0.040000|N/A|N/A|12177|309448|__
-video|1|163964|1.821822|160364|1.781822|3600|0.040000|N/A|N/A|12706|325992|__
-video|1|167564|1.861822|163964|1.821822|3600|0.040000|N/A|N/A|10838|339528|__
-video|1|171164|1.901822|167564|1.861822|3600|0.040000|N/A|N/A|11300|351372|__
-audio|0|156811|1.742344|156811|1.742344|2351|0.026122|N/A|N/A|223|404576|K_
+video|1|156764|1.741822|153164|1.701822|3600|0.040000|N/A|N/A|13836|294408|__MPEGTS Stream ID|1
+
+video|1|160364|1.781822|156764|1.741822|3600|0.040000|N/A|N/A|12163|309448|__MPEGTS Stream ID|1
+
+video|1|163964|1.821822|160364|1.781822|3600|0.040000|N/A|N/A|12692|325992|__MPEGTS Stream ID|1
+
+video|1|167564|1.861822|163964|1.821822|3600|0.040000|N/A|N/A|10824|339528|__MPEGTS Stream ID|1
+
+video|1|171164|1.901822|167564|1.861822|3600|0.040000|N/A|N/A|11286|351372|__MPEGTS Stream ID|1
+
+audio|0|156811|1.742344|156811|1.742344|2351|0.026122|N/A|N/A|209|404576|K_MPEGTS Stream ID|1
+
 audio|0|159162|1.768467|159162|1.768467|2351|0.026122|N/A|N/A|209|N/A|K_
 audio|0|161513|1.794589|161513|1.794589|2351|0.026122|N/A|N/A|209|N/A|K_
 audio|0|163864|1.820711|163864|1.820711|2351|0.026122|N/A|N/A|209|N/A|K_
@@ -124,12 +177,17 @@ audio|0|173268|1.925200|173268|1.925200|2351|0.026122|N/A|N/A|209|N/A|K_
 audio|0|175619|1.951322|175619|1.951322|2351|0.026122|N/A|N/A|209|N/A|K_
 audio|0|177970|1.977444|177970|1.977444|2351|0.026122|N/A|N/A|209|N/A|K_
 audio|0|180321|2.003567|180321|2.003567|2351|0.026122|N/A|N/A|209|N/A|K_
-video|1|174764|1.941822|171164|1.901822|3600|0.040000|N/A|N/A|12692|363592|__
+video|1|174764|1.941822|171164|1.901822|3600|0.040000|N/A|N/A|12678|363592|__MPEGTS Stream ID|1
+
 video|1|178364|1.981822|174764|1.941822|3600|0.040000|N/A|N/A|24711|377880|K_
-video|1|139582|1.550911|135982|1.510911|3600|0.040000|N/A|N/A|12706|325992|__
-video|1|143182|1.590911|139582|1.550911|3600|0.040000|N/A|N/A|10838|339528|__
-video|1|146782|1.630911|143182|1.590911|3600|0.040000|N/A|N/A|11300|351372|__
-audio|0|132429|1.471433|132429|1.471433|2351|0.026122|N/A|N/A|223|404576|K_
+video|1|139582|1.550911|135982|1.510911|3600|0.040000|N/A|N/A|12692|325992|__MPEGTS Stream ID|1
+
+video|1|143182|1.590911|139582|1.550911|3600|0.040000|N/A|N/A|10824|339528|__MPEGTS Stream ID|1
+
+video|1|146782|1.630911|143182|1.590911|3600|0.040000|N/A|N/A|11286|351372|__MPEGTS Stream ID|1
+
+audio|0|132429|1.471433|132429|1.471433|2351|0.026122|N/A|N/A|209|404576|K_MPEGTS Stream ID|1
+
 audio|0|134780|1.497556|134780|1.497556|2351|0.026122|N/A|N/A|209|N/A|K_
 audio|0|137131|1.523678|137131|1.523678|2351|0.026122|N/A|N/A|209|N/A|K_
 audio|0|139482|1.549800|139482|1.549800|2351|0.026122|N/A|N/A|209|N/A|K_
@@ -140,12 +198,18 @@ audio|0|148886|1.654289|148886|1.654289|2351|0.026122|N/A|N/A|209|N/A|K_
 audio|0|151237|1.680411|151237|1.680411|2351|0.026122|N/A|N/A|209|N/A|K_
 audio|0|153588|1.706533|153588|1.706533|2351|0.026122|N/A|N/A|209|N/A|K_
 audio|0|155939|1.732656|155939|1.732656|2351|0.026122|N/A|N/A|209|N/A|K_
-video|1|150382|1.670911|146782|1.630911|3600|0.040000|N/A|N/A|12692|363592|__
+video|1|150382|1.670911|146782|1.630911|3600|0.040000|N/A|N/A|12678|363592|__MPEGTS Stream ID|1
+
 video|1|153982|1.710911|150382|1.670911|3600|0.040000|N/A|N/A|24711|377880|K_
-video|1|161182|1.790911|157582|1.750911|3600|0.040000|N/A|N/A|12149|162996|__
-video|1|164782|1.830911|161182|1.790911|3600|0.040000|N/A|N/A|12296|176344|__
-video|1|168382|1.870911|164782|1.830911|3600|0.040000|N/A|N/A|24800|189692|K_
-video|1|171982|1.910911|168382|1.870911|3600|0.040000|N/A|N/A|17454|216388|__
-video|1|175582|1.950911|171982|1.910911|3600|0.040000|N/A|N/A|15033|235000|__
+video|1|161182|1.790911|157582|1.750911|3600|0.040000|N/A|N/A|12135|162996|__MPEGTS Stream ID|1
+
+video|1|164782|1.830911|161182|1.790911|3600|0.040000|N/A|N/A|12282|176344|__MPEGTS Stream ID|1
+
+video|1|168382|1.870911|164782|1.830911|3600|0.040000|N/A|N/A|24786|189692|K_MPEGTS Stream ID|1
+
+video|1|171982|1.910911|168382|1.870911|3600|0.040000|N/A|N/A|17440|216388|__MPEGTS Stream ID|1
+
+video|1|175582|1.950911|171982|1.910911|3600|0.040000|N/A|N/A|15019|235000|__MPEGTS Stream ID|1
+
 0|mp2|unknown|audio|1/44100|[3][0][0][0]|0x0003|s16p|44100|1|mono|0|N/A|0/0|0/0|1/90000|0|0.000000|N/A|N/A|64000|N/A|N/A|N/A|N/A|89|0|0|0|0|0|0|0|0|0|0|0|0
 1|mpeg2video|4|video|1/25|[2][0][0][0]|0x0002|352|288|0|0|1|1:1|11:9|yuv420p|8|tv|unknown|unknown|unknown|left|progressive|N/A|1|N/A|25/1|25/1|1/90000|N/A|N/A|N/A|N/A|N/A|N/A|N/A|N/A|N/A|60|0|0|0|0|0|0|0|0|0|0|0|0

commit 0dcac9c3f0f8f32009098edb704fac4b08bac951

lavf/concatdec: do not transfer custom IO flag
If the source is using a custom IO, setting this flag causes heavy leaks
since the segments will not have their avio context closed.

Regression since f5da453b068f55d335ca403d2e2b4dd2ac3d4331.

libavformat/concatdec.c
@@ -324,7 +324,7 @@ static int open_file(AVFormatContext *avf, unsigned fileno)
     if (!cat->avf)
         return AVERROR(ENOMEM);
 
-    cat->avf->flags |= avf->flags;
+    cat->avf->flags |= avf->flags & ~AVFMT_FLAG_CUSTOM_IO;
     cat->avf->interrupt_callback = avf->interrupt_callback;
 
     if ((ret = ff_copy_whiteblacklists(cat->avf, avf)) < 0)