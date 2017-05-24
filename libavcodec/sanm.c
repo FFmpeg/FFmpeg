861c05b286f2f4cf6c5a621977e3957bb6c30ad9
Max Justicz <maxj@mit.edu>
2017-05-24 15:25:50 +0200

    avcodec/sanm: Fix uninitialized reference frames
    Fixes: poc.snm
    
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit ca616b0f72c65b0ef5f9e1e6125698b15f50a26e)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/sanm.c
+++ libavcodec/sanm.c
@@ -462,11 +462,11 @@ static void destroy_buffers(SANMVideoContext *ctx)
 
 static av_cold int init_buffers(SANMVideoContext *ctx)
 {
-    av_fast_padded_malloc(&ctx->frm0, &ctx->frm0_size, ctx->buf_size);
-    av_fast_padded_malloc(&ctx->frm1, &ctx->frm1_size, ctx->buf_size);
-    av_fast_padded_malloc(&ctx->frm2, &ctx->frm2_size, ctx->buf_size);
+    av_fast_padded_mallocz(&ctx->frm0, &ctx->frm0_size, ctx->buf_size);
+    av_fast_padded_mallocz(&ctx->frm1, &ctx->frm1_size, ctx->buf_size);
+    av_fast_padded_mallocz(&ctx->frm2, &ctx->frm2_size, ctx->buf_size);
     if (!ctx->version)
-        av_fast_padded_malloc(&ctx->stored_frame,
+        av_fast_padded_mallocz(&ctx->stored_frame,
                               &ctx->stored_frame_size, ctx->buf_size);
 
     if (!ctx->frm0 || !ctx->frm1 || !ctx->frm2 ||
