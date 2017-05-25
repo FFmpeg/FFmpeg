fdba18c0683b8eb194eb26356efc5cbaf0dc406c
Michael Niedermayer <michael@niedermayer.cc>
2017-05-25 11:11:33 +0200

    avcodec/jpeg2000dec: Fix copy and paste error
    Found-by: jamrial
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit 5782e0ba8cc30bb08a806cdeda1adfb89a0556b4)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/jpeg2000dec.c
+++ libavcodec/jpeg2000dec.c
@@ -301,8 +301,7 @@ static int get_siz(Jpeg2000DecoderContext *s)
     if (s->tile_offset_x < 0 || s->tile_offset_y < 0 ||
         s->image_offset_x < s->tile_offset_x ||
         s->image_offset_y < s->tile_offset_y) {
-        av_log(s->avctx, AV_LOG_ERROR, "Tile offsets are invalid\n",
-               s->ncomponents);
+        av_log(s->avctx, AV_LOG_ERROR, "Tile offsets are invalid\n");
         return AVERROR_INVALIDDATA;
     }
 
