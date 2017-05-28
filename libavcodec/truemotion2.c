362a98eea9b85324a733e653dbe06bea124014a9
Michael Niedermayer <michael@niedermayer.cc>
2017-05-28 21:54:02 +0200

    avcodec/truemotion2: Fix runtime error: left shift of 1 by 31 places cannot be represented in type 'int'
    Fixes part of: 1888/clusterfuzz-testcase-minimized-5237704826552320
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit c9e884f3d98df85bf7f2cf30d71877b22929fdcb)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/truemotion2.c
+++ libavcodec/truemotion2.c
@@ -272,7 +272,7 @@ static int tm2_read_deltas(TM2Context *ctx, int stream_id)
     for (i = 0; i < d; i++) {
         v = get_bits_long(&ctx->gb, mb);
         if (v & (1 << (mb - 1)))
-            ctx->deltas[stream_id][i] = v - (1 << mb);
+            ctx->deltas[stream_id][i] = v - (1U << mb);
         else
             ctx->deltas[stream_id][i] = v;
     }
