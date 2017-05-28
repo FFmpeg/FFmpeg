ed1a66821382586c80799c1fc625419a567b5c82
Michael Niedermayer <michael@niedermayer.cc>
2017-05-28 14:00:30 +0200

    avcodec/jpeg2000: Fix runtime error: signed integer overflow: 4185 + 2147483394 cannot be represented in type 'int'
    Fixes: 1870/clusterfuzz-testcase-minimized-4686788029317120
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit 781f88bb26534ececc76eaa972f02536ba2f0f55)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/jpeg2000.h
+++ libavcodec/jpeg2000.h
@@ -220,7 +220,7 @@ static inline int ff_jpeg2000_ceildivpow2(int a, int b)
 
 static inline int ff_jpeg2000_ceildiv(int a, int b)
 {
-    return (a + b - 1) / b;
+    return (a + (int64_t)b - 1) / b;
 }
 
 /* TIER-1 routines */
