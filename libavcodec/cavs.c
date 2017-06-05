0d0418adb59261b41e2f850ef2adcc3507931c21
Michael Niedermayer <michael@niedermayer.cc>
2017-06-05 19:33:56 +0200

    avcodec/cavs: Fix runtime error: signed integer overflow: -12648062 * 256 cannot be represented in type 'int'
    Fixes: 2067/clusterfuzz-testcase-minimized-5578430902960128
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit 1e6ee86d9254e8fd2158cc9a31d3be96b0809411)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/cavs.c
+++ libavcodec/cavs.c
@@ -538,8 +538,7 @@ void ff_cavs_inter(AVSContext *h, enum cavs_mb mb_type)
 static inline void scale_mv(AVSContext *h, int *d_x, int *d_y,
                             cavs_vector *src, int distp)
 {
-    int den = h->scale_den[FFMAX(src->ref, 0)];
-
+    int64_t den = h->scale_den[FFMAX(src->ref, 0)];
     *d_x = (src->x * distp * den + 256 + FF_SIGNBIT(src->x)) >> 9;
     *d_y = (src->y * distp * den + 256 + FF_SIGNBIT(src->y)) >> 9;
 }
