fef11b7c38bd1142e817228d82f6e798210ea985
Michael Niedermayer <michael@niedermayer.cc>
2017-06-11 20:19:59 +0200

    avcodec/wavpack: Fix runtime error: signed integer overflow: 1886191616 + 277872640 cannot be represented in type 'int'
    Fixes: 2181/clusterfuzz-testcase-minimized-6314784322486272
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit c996374d4d86e0efbef71812448b4c65656bc667)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/wavpack.h
+++ libavcodec/wavpack.h
@@ -94,7 +94,7 @@ typedef struct Decorr {
 typedef struct WvChannel {
     int median[3];
     int slow_level, error_limit;
-    int bitrate_acc, bitrate_delta;
+    unsigned bitrate_acc, bitrate_delta;
 } WvChannel;
 
 // macros for manipulating median values
