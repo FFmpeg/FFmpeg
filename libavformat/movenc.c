974d508e571085dce43e688595da058804169088
John Stebbins <stebbins@jetheaddev.com>
2017-06-15 11:41:15 -0700

    movenc: write correct format hvcc when tag is hvc1
    (cherry picked from commit 1ea9b7fdf99b60c0db95a2ce97b2108469bbb245)
    Signed-off-by: Derek Buitenhuis <derek.buitenhuis@gmail.com>

--- libavformat/movenc.c
+++ libavformat/movenc.c
@@ -1126,7 +1126,10 @@ static int mov_write_hvcc_tag(AVIOContext *pb, MOVTrack *track)
 
     avio_wb32(pb, 0);
     ffio_wfourcc(pb, "hvcC");
-    ff_isom_write_hvcc(pb, track->vos_data, track->vos_len, 0);
+    if (track->tag == MKTAG('h','v','c','1'))
+        ff_isom_write_hvcc(pb, track->vos_data, track->vos_len, 1);
+    else
+        ff_isom_write_hvcc(pb, track->vos_data, track->vos_len, 0);
     return update_size(pb, pos);
 }
 
