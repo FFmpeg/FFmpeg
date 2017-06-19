2820c9dfaa1f4093fea471665fdbef9ca7080bcd
Paul B Mahol <onemda@gmail.com>
2017-06-19 18:12:29 +0200

    avfilter/af_superequalizer: fix out of array access
    Signed-off-by: Paul B Mahol <onemda@gmail.com>

--- libavfilter/af_superequalizer.c
+++ libavfilter/af_superequalizer.c
@@ -126,7 +126,7 @@ static void process_param(float *bc, EqParameter *param, float fs)
 
     for (i = 0; i <= NBANDS; i++) {
         param[i].lower = i == 0 ? 0 : bands[i - 1];
-        param[i].upper = i == NBANDS - 1 ? fs : bands[i];
+        param[i].upper = i == NBANDS ? fs : bands[i];
         param[i].gain  = bc[i];
     }
 }
