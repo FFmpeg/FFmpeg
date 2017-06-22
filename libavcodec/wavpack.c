24e95f9d4de012f51fdd5767dff0b3142e13ec3a
Michael Niedermayer <michael@niedermayer.cc>
2017-06-23 01:58:48 +0200

    avcodec/wavpack: Fix integer overflow
    Fixes: runtime error: signed integer overflow: 227511904 + 1964113935 cannot be represented in type 'int'
    Fixes: 2331/clusterfuzz-testcase-minimized-6182185830711296
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/wavpack.c
+++ libavcodec/wavpack.c
@@ -225,7 +225,7 @@ static int wv_get_value(WavpackFrameContext *ctx, GetBitContext *gb,
         INC_MED(1);
         DEC_MED(2);
     } else {
-        base = GET_MED(0) + GET_MED(1) + GET_MED(2) * (t - 2);
+        base = GET_MED(0) + GET_MED(1) + GET_MED(2) * (t - 2U);
         add  = GET_MED(2) - 1;
         INC_MED(0);
         INC_MED(1);
