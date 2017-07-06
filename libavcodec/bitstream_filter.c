51db262312558fc62b8b89a138b280e612f37509
Derek Buitenhuis <derek.buitenhuis@gmail.com>
2017-07-06 14:11:28 -0400

    bitstream_filter: Add missing error check
    Signed-off-by: Derek Buitenhuis <derek.buitenhuis@gmail.com>

--- libavcodec/bitstream_filter.c
+++ libavcodec/bitstream_filter.c
@@ -122,6 +122,8 @@ int av_bitstream_filter_filter(AVBitStreamFilterContext *bsfc,
                 shorthand[0] = opt->name;
 
             ret = av_opt_set_from_string(priv->ctx->priv_data, bsfc->args, shorthand, "=", ":");
+            if (ret < 0)
+                return ret;
         }
 
         ret = av_bsf_init(priv->ctx);
