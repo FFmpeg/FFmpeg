aa01a35dd507e4e692ea37e015fa7a45d7566851
Michael Niedermayer <michael@niedermayer.cc>
2017-07-12 17:25:16 +0200

    avcodec/magicyuv: Check that vlc len is not too large
    Fixes: runtime error: shift exponent -95 is negative
    Fixes: 2568/clusterfuzz-testcase-minimized-4926115716005888
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit 341f01290c2353669ed2263f56e1a9f4c67cc597)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/magicyuv.c
+++ libavcodec/magicyuv.c
@@ -84,6 +84,8 @@ static int huff_build(VLC *vlc, uint8_t *len)
     for (i = 0; i < 256; i++) {
         he[i].sym = 255 - i;
         he[i].len = len[i];
+        if (len[i] == 0 || len[i] > 32)
+            return AVERROR_INVALIDDATA;
     }
     AV_QSORT(he, 256, HuffEntry, huff_cmp_len);
 
