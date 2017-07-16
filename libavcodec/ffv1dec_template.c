064d0c64628216b06ff96a0ddea494dfb09b0535
Michael Niedermayer <michael@niedermayer.cc>
2017-07-16 02:08:50 +0200

    avcodec/ffv1dec_template: Fix signed integer overflow
    Fixes: runtime error: negation of -2147483648 cannot be represented in type 'int'; cast to an unsigned type to negate this value to itself
    Fixes: 2634/clusterfuzz-testcase-minimized-4540890636877824
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit 4de4308d2aa3bfaa286ab566caf087d523cf9a85)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/ffv1dec_template.c
+++ libavcodec/ffv1dec_template.c
@@ -96,7 +96,7 @@ static av_always_inline void RENAME(decode_line)(FFV1Context *s, int w,
         }
 
         if (sign)
-            diff = -diff;
+            diff = -(unsigned)diff;
 
         sample[1][x] = av_mod_uintp2(RENAME(predict)(sample[1] + x, sample[0] + x) + diff, bits);
     }
