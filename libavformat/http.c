e2f8f14052d99d7710cda0a821e236765b315d0b
Jacek Jendrzej <satbaby@kawaii.com>
2017-07-20 13:46:46 +0200

    lavf/http: Reset compressed header flag, fix http 302 request
    Fixes ticket #6404.
    Fixes ticket #6666.
    
    Signed-off-by: Jacek Jendrzej <satbaby@kawaii.com>

--- libavformat/http.c
+++ libavformat/http.c
@@ -1248,6 +1248,7 @@ static int http_connect(URLContext *h, const char *path, const char *local_path,
     s->willclose        = 0;
     s->end_chunked_post = 0;
     s->end_header       = 0;
+    s->compressed       = 0;
     if (post && !s->post_data && !send_expect_100) {
         /* Pretend that it did work. We didn't read any header yet, since
          * we've still to send the POST data, but the code calling this
