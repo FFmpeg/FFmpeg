0689cc2475557339a00240d05b7fc4fcb70e21cd
foo86 <foobaz86@gmail.com>
2017-07-22 17:18:47 +0300

    avcodec/dolby_e: add AV_CODEC_CAP_CHANNEL_CONF capability

--- libavcodec/dolby_e.c
+++ libavcodec/dolby_e.c
@@ -701,7 +701,7 @@ AVCodec ff_dolby_e_decoder = {
     .decode         = dolby_e_decode_frame,
     .close          = dolby_e_close,
     .flush          = dolby_e_flush,
-    .capabilities   = AV_CODEC_CAP_DR1,
+    .capabilities   = AV_CODEC_CAP_DR1 | AV_CODEC_CAP_CHANNEL_CONF,
     .sample_fmts    = (const enum AVSampleFormat[]) { AV_SAMPLE_FMT_FLTP, AV_SAMPLE_FMT_NONE },
     .caps_internal  = FF_CODEC_CAP_INIT_THREADSAFE | FF_CODEC_CAP_INIT_CLEANUP,
 };
