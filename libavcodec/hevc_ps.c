58ce199abac4f990d6c80b2be7192965e0d20ef6
Michael Niedermayer <michael@niedermayer.cc>
2017-07-24 15:48:37 +0200

    avcodec/hevc_ps: fix integer overflow in log2_parallel_merge_level_minus2
    Fixes: runtime error: signed integer overflow: -2147483647 - 2 cannot be represented in type 'int'
    Fixes: 2702/clusterfuzz-testcase-minimized-4511932591636480
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit 74c1c22d7f0d25f527ed2ebf62493be5ad52c972)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/hevc_ps.c
+++ libavcodec/hevc_ps.c
@@ -1394,6 +1394,7 @@ int ff_hevc_decode_nal_pps(GetBitContext *gb, AVCodecContext *avctx,
     HEVCSPS      *sps = NULL;
     int i, ret = 0;
     unsigned int pps_id = 0;
+    unsigned log2_parallel_merge_level_minus2;
 
     AVBufferRef *pps_buf;
     HEVCPPS *pps = av_mallocz(sizeof(*pps));
@@ -1580,13 +1581,14 @@ int ff_hevc_decode_nal_pps(GetBitContext *gb, AVCodecContext *avctx,
             goto err;
     }
     pps->lists_modification_present_flag = get_bits1(gb);
-    pps->log2_parallel_merge_level       = get_ue_golomb_long(gb) + 2;
-    if (pps->log2_parallel_merge_level > sps->log2_ctb_size) {
+    log2_parallel_merge_level_minus2     = get_ue_golomb_long(gb);
+    if (log2_parallel_merge_level_minus2 > sps->log2_ctb_size) {
         av_log(avctx, AV_LOG_ERROR, "log2_parallel_merge_level_minus2 out of range: %d\n",
-               pps->log2_parallel_merge_level - 2);
+               log2_parallel_merge_level_minus2);
         ret = AVERROR_INVALIDDATA;
         goto err;
     }
+    pps->log2_parallel_merge_level       = log2_parallel_merge_level_minus2 + 2;
 
     pps->slice_header_extension_present_flag = get_bits1(gb);
 
