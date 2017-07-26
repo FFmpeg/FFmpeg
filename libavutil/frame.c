4cebf0fc456ceb4f8c4f328b2fe849bd7e8a1c4a
James Almer <jamrial@gmail.com>
2017-07-26 00:38:22 -0300

    avutil/frame: add ICC Profile to av_frame_side_data_name
    Reviwed-by: Rostislav Pehlivanov <atomnuker@gmail.com>
    Signed-off-by: James Almer <jamrial@gmail.com>

--- libavutil/frame.c
+++ libavutil/frame.c
@@ -778,6 +778,7 @@ const char *av_frame_side_data_name(enum AVFrameSideDataType type)
     case AV_FRAME_DATA_MASTERING_DISPLAY_METADATA:  return "Mastering display metadata";
     case AV_FRAME_DATA_CONTENT_LIGHT_LEVEL:         return "Content light level metadata";
     case AV_FRAME_DATA_GOP_TIMECODE:                return "GOP timecode";
+    case AV_FRAME_DATA_ICC_PROFILE:                 return "ICC profile";
     }
     return NULL;
 }
