4f714352488dad2fa8da6cdf3fe0365869ab8c6e
Michael Niedermayer <michael@niedermayer.cc>
2017-07-28 03:22:40 +0200

    avcodec/dirac_dwt: Fix multiple integer overflows in COMPOSE_DD97iH0()
    Fixes: runtime error: signed integer overflow: 9 * 335544320 cannot be represented in type 'int'
    Fixes: 2739/clusterfuzz-testcase-minimized-6737297955356672
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit bf8ab72ae95bb11f2c281d464594c2f6ba70326b)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/dirac_dwt.h
+++ libavcodec/dirac_dwt.h
@@ -99,7 +99,7 @@ void ff_spatial_idwt_slice2(DWTContext *d, int y);
     (b1 + ((b0 + b2 + 1) >> 1))
 
 #define COMPOSE_DD97iH0(b0, b1, b2, b3, b4)\
-    (b2 + ((-b0 + 9*b1 + 9*b3 - b4 + 8) >> 4))
+    (b2 + ((int)(-b0 + 9U*b1 + 9U*b3 - b4 + 8) >> 4))
 
 #define COMPOSE_DD137iL0(b0, b1, b2, b3, b4)\
     (b2 - ((-b0 + 9*b1 + 9*b3 - b4 + 16) >> 5))
