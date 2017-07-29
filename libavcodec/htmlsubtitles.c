479ab8c3f842145fcdd87a574b3fd7b4022034a3
Clément Bœsch <u@pkh.me>
2017-07-29 21:22:17 +0200

    lavc/htmlsubtitles: handle colors starting with many '#'

--- libavcodec/htmlsubtitles.c
+++ libavcodec/htmlsubtitles.c
@@ -28,6 +28,10 @@
 static int html_color_parse(void *log_ctx, const char *str)
 {
     uint8_t rgba[4];
+    int nb_sharps = 0;
+    while (str[nb_sharps] == '#')
+        nb_sharps++;
+    str += FFMAX(0, nb_sharps - 1);
     if (av_parse_color(rgba, str, strcspn(str, "\" >"), log_ctx) < 0)
         return -1;
     return rgba[0] | rgba[1] << 8 | rgba[2] << 16;
