02d224406f40f45803efd69b8cfe1602649821bc
Michael Niedermayer <michael@niedermayer.cc>
2017-08-15 03:32:44 +0200

    avcodec/diracdec: Fixes integer overflow
    Fixes: runtime error: signed integer overflow: 340018243 * 27 cannot be represented in type 'int'
    Fixes: 2861/clusterfuzz-testcase-minimized-5361070510178304
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit 92da23093c784b1d9f0db4db51d28ea80a59e759)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/diracdec.c
+++ libavcodec/diracdec.c
@@ -442,7 +442,7 @@ static av_cold int dirac_decode_end(AVCodecContext *avctx)
 static inline int coeff_unpack_golomb(GetBitContext *gb, int qfactor, int qoffset)
 {
     int coeff = dirac_get_se_golomb(gb);
-    const int sign = FFSIGN(coeff);
+    const unsigned sign = FFSIGN(coeff);
     if (coeff)
         coeff = sign*((sign * coeff * qfactor + qoffset) >> 2);
     return coeff;
