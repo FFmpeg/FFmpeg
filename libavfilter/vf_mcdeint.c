f7d4c60ac47547f69ddc96e7bed682f54436cd1a
James Almer <jamrial@gmail.com>
2017-08-30 14:34:04 -0300

    avfilter/vf_mcdeint: free the AVCodecContext struct properly
    Signed-off-by: James Almer <jamrial@gmail.com>

--- libavfilter/vf_mcdeint.c
+++ libavfilter/vf_mcdeint.c
@@ -154,10 +154,7 @@ static av_cold void uninit(AVFilterContext *ctx)
 {
     MCDeintContext *mcdeint = ctx->priv;
 
-    if (mcdeint->enc_ctx) {
-        avcodec_close(mcdeint->enc_ctx);
-        av_freep(&mcdeint->enc_ctx);
-    }
+    avcodec_free_context(&mcdeint->enc_ctx);
 }
 
 static int query_formats(AVFilterContext *ctx)
