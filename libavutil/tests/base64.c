e0b9b3e60ea3b970c5fcdbccb401cd9d93b9a63f
Clément Bœsch <cboesch@gopro.com>
2017-09-07 15:52:47 +0200

    lavu/tests: move timer.h include earlier
    In the next commit, timer.h will require a _GNU_SOURCE to be set before
    including system headers. This commit prevents compilation failures.

--- libavutil/tests/base64.c
+++ libavutil/tests/base64.c
@@ -17,12 +17,14 @@
  */
 
 // LCOV_EXCL_START
+
+#include "libavutil/timer.h"
+
 #include <stdint.h>
 #include <stdio.h>
 
 #include "libavutil/common.h"
 #include "libavutil/base64.h"
-#include "libavutil/timer.h"
 
 #define MAX_DATA_SIZE    1024
 #define MAX_ENCODED_SIZE 2048
--- libavutil/tests/des.c
+++ libavutil/tests/des.c
@@ -16,6 +16,8 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
+#include "libavutil/timer.h"
+
 #include "libavutil/des.c"
 
 #include <stdint.h>
