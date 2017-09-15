148c8e88c43cfbabd6aee9f01ef30942cee9d359
Ashish Singh <ashk43712@gmail.com>
2017-09-16 02:17:58 +0530

    avfilter: add vmafmotion filter
    Signed-off-by: Ashish Singh <ashk43712@gmail.com>
    Signed-off-by: Ronald S. Bultje <rsbultje@gmail.com>

--- libavfilter/allfilters.c
+++ libavfilter/allfilters.c
@@ -342,6 +342,7 @@ static void register_all(void)
     REGISTER_FILTER(VIDSTABDETECT,  vidstabdetect,  vf);
     REGISTER_FILTER(VIDSTABTRANSFORM, vidstabtransform, vf);
     REGISTER_FILTER(VIGNETTE,       vignette,       vf);
+    REGISTER_FILTER(VMAFMOTION,     vmafmotion,     vf);
     REGISTER_FILTER(VSTACK,         vstack,         vf);
     REGISTER_FILTER(W3FDIF,         w3fdif,         vf);
     REGISTER_FILTER(WAVEFORM,       waveform,       vf);
