624d4739dbfc950ee756e83ae187f97dc82d10c9
Mark Thompson <sw@jkqxz.net>
2017-09-23 16:46:11 +0100

    cbs_h264: Fix writing streams with auxiliary pictures
    Tested with the alphaconformanceG sample.
    
    Fixes CID 1419836.
    
    (cherry picked from commit 9ed18f302b09e444f5b1be01979cce62c4b2c04a)

--- libavcodec/cbs_h2645.c
+++ libavcodec/cbs_h2645.c
@@ -1008,7 +1008,7 @@ static int cbs_h264_write_nal_unit(CodedBitstreamContext *ctx,
 
     case H264_NAL_SPS_EXT:
         {
-            H264RawSPSExtension *sps_ext;
+            H264RawSPSExtension *sps_ext = unit->content;
 
             err = cbs_h264_write_sps_extension(ctx, pbc, sps_ext);
             if (err < 0)
@@ -1032,6 +1032,7 @@ static int cbs_h264_write_nal_unit(CodedBitstreamContext *ctx,
 
     case H264_NAL_SLICE:
     case H264_NAL_IDR_SLICE:
+    case H264_NAL_AUXILIARY_SLICE:
         {
             H264RawSlice *slice = unit->content;
             GetBitContext gbc;
