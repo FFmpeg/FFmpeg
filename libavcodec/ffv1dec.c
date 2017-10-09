48933f28c2a8dce024bef8110e907811b3d4c9f6
Michael Niedermayer <michael@niedermayer.cc>
2017-10-09 11:49:28 +0200

    avcodec/ffv1dec: Fix out of array read in slice counting
    Fixes: test-201710.mp4
    
    Found-by: 连一汉 <lianyihan@360.cn> and Zhibin Hu
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit c20f4fcb74da2d0432c7b54499bb98f48236b904)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/ffv1dec.c
+++ libavcodec/ffv1dec.c
@@ -699,7 +699,7 @@ static int read_header(FFV1Context *f)
     } else {
         const uint8_t *p = c->bytestream_end;
         for (f->slice_count = 0;
-             f->slice_count < MAX_SLICES && 3 < p - c->bytestream_start;
+             f->slice_count < MAX_SLICES && 3 + 5*!!f->ec < p - c->bytestream_start;
              f->slice_count++) {
             int trailer = 3 + 5*!!f->ec;
             int size = AV_RB24(p-trailer);
