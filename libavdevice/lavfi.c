535117d1f6deba266654b8a43cd5bd7630045fd3
Carl Eugen Hoyos <ceffmpeg@gmail.com>
2017-10-21 20:22:52 +0200

    lavd/lavfi: Constify two variables.
    Fixes the following warnings:
    libavdevice/lavfi.c:136:16: warning: assignment discards 'const' qualifier from pointer target type
    libavdevice/lavfi.c:137:17: warning: assignment discards 'const' qualifier from pointer target type

--- libavdevice/lavfi.c
+++ libavdevice/lavfi.c
@@ -121,7 +121,7 @@ av_cold static int lavfi_read_header(AVFormatContext *avctx)
 {
     LavfiContext *lavfi = avctx->priv_data;
     AVFilterInOut *input_links = NULL, *output_links = NULL, *inout;
-    AVFilter *buffersink, *abuffersink;
+    const AVFilter *buffersink, *abuffersink;
     int *pix_fmts = create_all_formats(AV_PIX_FMT_NB);
     enum AVMediaType type;
     int ret = 0, i, n;
