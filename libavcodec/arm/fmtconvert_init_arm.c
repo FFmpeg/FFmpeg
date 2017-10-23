b487add7ecf78efda36d49815f8f8757bd24d4cb
Martin Storsjö <martin@martin.st>
2017-10-23 10:44:53 +0300

    arm: Remove a redundant check in fmtconvert_init_arm.c
    This was missed in e2710e790c0, where have_vfp && !have_vfpv3 were
    converted into have_vfp_vm.
    
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- libavcodec/arm/fmtconvert_init_arm.c
+++ libavcodec/arm/fmtconvert_init_arm.c
@@ -42,10 +42,8 @@ av_cold void ff_fmt_convert_init_arm(FmtConvertContext *c, AVCodecContext *avctx
     int cpu_flags = av_get_cpu_flags();
 
     if (have_vfp_vm(cpu_flags)) {
-        if (!have_vfpv3(cpu_flags)) {
-            c->int32_to_float_fmul_scalar = ff_int32_to_float_fmul_scalar_vfp;
-            c->int32_to_float_fmul_array8 = ff_int32_to_float_fmul_array8_vfp;
-        }
+        c->int32_to_float_fmul_scalar = ff_int32_to_float_fmul_scalar_vfp;
+        c->int32_to_float_fmul_array8 = ff_int32_to_float_fmul_array8_vfp;
     }
 
     if (have_neon(cpu_flags)) {
