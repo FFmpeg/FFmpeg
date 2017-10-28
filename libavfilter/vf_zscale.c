ff763351e74550df3b9a0465634d1ec48b15b043
dxfhgwet <noreply@example.com>
2017-10-27 21:54:13 -0700

    avfilter/zscale: fix memory leak

--- libavfilter/vf_zscale.c
+++ libavfilter/vf_zscale.c
@@ -673,6 +673,7 @@ static void uninit(AVFilterContext *ctx)
     ZScaleContext *s = ctx->priv;
 
     zimg_filter_graph_free(s->graph);
+    zimg_filter_graph_free(s->alpha_graph);
     av_freep(&s->tmp);
     s->tmp_size = 0;
 }
