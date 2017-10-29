ddd0b19169dc3c0dfcdbaf246147a64c87b22c24
James Almer <jamrial@gmail.com>
2017-10-29 14:10:56 -0300

    ffprobe: remove usage of deprecated getter functions
    Signed-off-by: James Almer <jamrial@gmail.com>

--- fftools/ffprobe.c
+++ fftools/ffprobe.c
@@ -2792,7 +2792,7 @@ static int show_format(WriterContext *w, InputFile *ifile)
     else           print_str_opt("size", "N/A");
     if (fmt_ctx->bit_rate > 0) print_val    ("bit_rate", fmt_ctx->bit_rate, unit_bit_per_second_str);
     else                       print_str_opt("bit_rate", "N/A");
-    print_int("probe_score", av_format_get_probe_score(fmt_ctx));
+    print_int("probe_score", fmt_ctx->probe_score);
     if (do_show_format_tags)
         ret = show_tags(w, fmt_ctx->metadata, SECTION_ID_FORMAT_TAGS);
 
