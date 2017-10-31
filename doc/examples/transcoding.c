fdfc51766d28087c44307ddb5a4b087d47dbe8ef
Jun Zhao <jun.zhao@intel.com>
2017-10-31 16:09:45 +0800

    examples/transcoding: suppress build warning.
    suppress the "warning: assignment discards ‘const’ qualifier from
    pointer target type" build warning.
    
    Signed-off-by: Jun Zhao <jun.zhao@intel.com>
    Reviewed-by: Steven Liu <lingjiujianke@gmail.com>
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- doc/examples/transcoding.c
+++ doc/examples/transcoding.c
@@ -227,8 +227,8 @@ static int init_filter(FilteringContext* fctx, AVCodecContext *dec_ctx,
 {
     char args[512];
     int ret = 0;
-    AVFilter *buffersrc = NULL;
-    AVFilter *buffersink = NULL;
+    const AVFilter *buffersrc = NULL;
+    const AVFilter *buffersink = NULL;
     AVFilterContext *buffersrc_ctx = NULL;
     AVFilterContext *buffersink_ctx = NULL;
     AVFilterInOut *outputs = avfilter_inout_alloc();
