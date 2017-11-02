fbca6e7d162b9a7105d1059826dfe96616fa803a
Sean McGovern <gseanmcg@gmail.com>
2017-11-01 20:45:20 -0400

    matroskadec: allow RealAudio/Cook/Sipro streams of flavor 0
    Regression since 569d18aa9dc989c37bb4d4b968026fe5afa6fff9.
    
    Bug-Id: 1055
    Cc: libav-stable@libav.org

--- libavformat/matroskadec.c
+++ libavformat/matroskadec.c
@@ -1916,7 +1916,7 @@ static int matroska_parse_tracks(AVFormatContext *s)
             track->audio.sub_packet_h    = avio_rb16(&b);
             track->audio.frame_size      = avio_rb16(&b);
             track->audio.sub_packet_size = avio_rb16(&b);
-            if (flavor                       <= 0 ||
+            if (flavor                        < 0 ||
                 track->audio.coded_framesize <= 0 ||
                 track->audio.sub_packet_h    <= 0 ||
                 track->audio.frame_size      <= 0 ||
