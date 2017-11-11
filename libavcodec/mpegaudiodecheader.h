006546c635f05444cd7dd241f9e65bef346771bd
James Almer <jamrial@gmail.com>
2017-11-11 01:39:47 -0300

    avcodec/mpegaudiodecheader: remove dead code
    Signed-off-by: James Almer <jamrial@gmail.com>

--- libavcodec/mpegaudiodecheader.h
+++ libavcodec/mpegaudiodecheader.h
@@ -57,11 +57,6 @@ int avpriv_mpegaudio_decode_header(MPADecodeHeader *s, uint32_t header);
 int ff_mpa_decode_header(uint32_t head, int *sample_rate,
                          int *channels, int *frame_size, int *bitrate, enum AVCodecID *codec_id);
 
-#if LIBAVCODEC_VERSION_MAJOR < 58
-int avpriv_mpa_decode_header(AVCodecContext *avctx, uint32_t head, int *sample_rate, int *channels, int *frame_size, int *bitrate);
-int avpriv_mpa_decode_header2(uint32_t head, int *sample_rate, int *channels, int *frame_size, int *bitrate, enum AVCodecID *codec_id);
-#endif
-
 /* fast header check for resync */
 static inline int ff_mpa_check_header(uint32_t header){
     /* header */
