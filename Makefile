FFmpeg/FFmpeg

commit 7ebe7e8e7a76c0ce302f4f583ef0d14220031214

build: Remove pkg-config files on clean instead of on distclean
The files are no longer generated by configure, so they should not
be removed by the distclean target any longer.

Makefile
@@ -172,7 +172,6 @@ clean::
 	$(RM) -rf coverage.info lcov
 
 distclean::
-	$(RM) $(DISTCLEANSUFFIXES)
 	$(RM) .version avversion.h config.asm config.h mapfile \
             avbuild/.config avbuild/config.* libavutil/avconfig.h \
             libavcodec/bsf_list.c libavformat/protocol_list.c

avbuild/common.mak
@@ -49,8 +49,7 @@ $(TOOLOBJS): | tools
 
 OBJDIRS := $(OBJDIRS) $(dir $(OBJS) $(HOBJS) $(HOSTOBJS) $(TESTOBJS))
 
-CLEANSUFFIXES     = *.d *.o *~ *.h.c *.gcda *.gcno *.map *.ver *.version
-DISTCLEANSUFFIXES = *.pc
+CLEANSUFFIXES     = *.d *.gcda *.gcno *.h.c *.map *.o *.pc *.ver *.version *~
 LIBSUFFIXES       = *.a *.lib *.so *.so.* *.dylib *.dll *.def *.dll.a
 
 define RULES

avbuild/library.mak
@@ -57,10 +57,6 @@ clean::
 	$(RM) $(addprefix $(SUBDIR),$(CLEANFILES) $(CLEANSUFFIXES) $(LIBSUFFIXES)) \
 	    $(CLEANSUFFIXES:%=$(SUBDIR)$(ARCH)/%) $(CLEANSUFFIXES:%=$(SUBDIR)tests/%)
 
-distclean:: clean
-	$(RM) $(DISTCLEANSUFFIXES:%=$(SUBDIR)%) $(DISTCLEANSUFFIXES:%=$(SUBDIR)$(ARCH)/%) \
-            $(DISTCLEANSUFFIXES:%=$(SUBDIR)tests/%)
-
 install-lib$(NAME)-shared: $(SUBDIR)$(SLIBNAME)
 	$(Q)mkdir -p "$(SHLIBDIR)"
 	$$(INSTALL) -m 755 $$< "$(SHLIBDIR)/$(SLIB_INSTALL_NAME)"

commit 5c2a01f064d5ab2b309d25c7f46c6c4471838d90

Makefile: fix distclean target
It must imply clean. Regression since 7ebe7e8e7a76c0ce302f4f583ef0d14220031214.

Signed-off-by: James Almer <jamrial@gmail.com>

Makefile
@@ -171,7 +171,7 @@ clean::
 	$(RM) $(addprefix compat/,$(CLEANSUFFIXES)) $(addprefix compat/*/,$(CLEANSUFFIXES))
 	$(RM) -rf coverage.info lcov
 
-distclean::
+distclean: clean
 	$(RM) .version avversion.h config.asm config.h mapfile \
             avbuild/.config avbuild/config.* libavutil/avconfig.h \
             libavcodec/bsf_list.c libavformat/protocol_list.c