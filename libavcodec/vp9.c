a5679933c1b8b6bef5c5c3eb7c70d06c695066cf
wm4 <nfxjfg@googlemail.com>
2017-11-17 14:15:22 +0100

    vp9: use superframe split BSF
    webm usually has invisible superframes merged with normal frames.
    (vpxenc muxes them in this form, which is evidence enough that this is
    the standard webm packet format. It's rather unclear whether ffmpeg is
    even allowed to remux them with split packets.)
    
    The vp9 decoder needs them to be in separate packets for multithreading
    to work. Add the BSF to the decoder, so the conversion happens
    automatically.
    
    This contains the important part of fa1749dd34c55fb9, which
    was apparently skipped in commit d417e95af76. This restores Libav API
    compatibility.

--- libavcodec/vp9.c
+++ libavcodec/vp9.c
@@ -1795,6 +1795,7 @@ AVCodec ff_vp9_decoder = {
     .init_thread_copy      = ONLY_IF_THREADS_ENABLED(vp9_decode_init_thread_copy),
     .update_thread_context = ONLY_IF_THREADS_ENABLED(vp9_decode_update_thread_context),
     .profiles              = NULL_IF_CONFIG_SMALL(ff_vp9_profiles),
+    .bsfs                  = "vp9_superframe_split",
     .hw_configs            = (const AVCodecHWConfigInternal*[]) {
 #if CONFIG_VP9_DXVA2_HWACCEL
                                HWACCEL_DXVA2(vp9),
