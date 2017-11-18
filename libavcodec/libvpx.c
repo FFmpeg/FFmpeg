e54061ae6a5e22bad5c66ef4411acc8f778a9f90
James Zern <jzern@google.com>
2017-11-17 17:25:21 -0800

    libvpx: remove pre-1.4.0 checks
    Reviewed-by: James Almer <jamrial@gmail.com>
    Signed-off-by: James Zern <jzern@google.com>

--- libavcodec/libvpx.c
+++ libavcodec/libvpx.c
@@ -40,9 +40,7 @@ static const enum AVPixelFormat vp9_pix_fmts_highcol[] = {
     AV_PIX_FMT_YUV422P,
     AV_PIX_FMT_YUV440P,
     AV_PIX_FMT_YUV444P,
-#if VPX_IMAGE_ABI_VERSION >= 3
     AV_PIX_FMT_GBRP,
-#endif
     AV_PIX_FMT_NONE
 };
 
@@ -60,30 +58,22 @@ static const enum AVPixelFormat vp9_pix_fmts_highbd[] = {
     AV_PIX_FMT_YUV422P12,
     AV_PIX_FMT_YUV440P12,
     AV_PIX_FMT_YUV444P12,
-#if VPX_IMAGE_ABI_VERSION >= 3
     AV_PIX_FMT_GBRP,
     AV_PIX_FMT_GBRP10,
     AV_PIX_FMT_GBRP12,
-#endif
     AV_PIX_FMT_NONE
 };
 #endif
 
 av_cold void ff_vp9_init_static(AVCodec *codec)
 {
-    if (    vpx_codec_version_major() < 1
-        || (vpx_codec_version_major() == 1 && vpx_codec_version_minor() < 3))
-        codec->capabilities |= AV_CODEC_CAP_EXPERIMENTAL;
     codec->pix_fmts = vp9_pix_fmts_def;
 #if CONFIG_LIBVPX_VP9_ENCODER
-    if (    vpx_codec_version_major() > 1
-        || (vpx_codec_version_major() == 1 && vpx_codec_version_minor() >= 4)) {
-#ifdef VPX_CODEC_CAP_HIGHBITDEPTH
+    {
         vpx_codec_caps_t codec_caps = vpx_codec_get_caps(vpx_codec_vp9_cx());
         if (codec_caps & VPX_CODEC_CAP_HIGHBITDEPTH)
             codec->pix_fmts = vp9_pix_fmts_highbd;
         else
-#endif
             codec->pix_fmts = vp9_pix_fmts_highcol;
     }
 #endif
