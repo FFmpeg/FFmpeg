aea0f06db74c1f924429d3cf9ff9f803d00feaf5
James Almer <jamrial@gmail.com>
2017-11-19 00:32:25 -0300

    x86/jpeg2000dsp: add ff_ict_float_{fma3,fma4}
    jpeg2000_ict_float_c: 2296.0
    jpeg2000_ict_float_sse: 628.0
    jpeg2000_ict_float_avx: 317.0
    jpeg2000_ict_float_fma3: 262.0
    
    Signed-off-by: James Almer <jamrial@gmail.com>

--- libavcodec/x86/jpeg2000dsp_init.c
+++ libavcodec/x86/jpeg2000dsp_init.c
@@ -26,6 +26,8 @@
 
 void ff_ict_float_sse(void *src0, void *src1, void *src2, int csize);
 void ff_ict_float_avx(void *src0, void *src1, void *src2, int csize);
+void ff_ict_float_fma3(void *src0, void *src1, void *src2, int csize);
+void ff_ict_float_fma4(void *src0, void *src1, void *src2, int csize);
 void ff_rct_int_sse2 (void *src0, void *src1, void *src2, int csize);
 void ff_rct_int_avx2 (void *src0, void *src1, void *src2, int csize);
 
@@ -42,6 +44,14 @@ av_cold void ff_jpeg2000dsp_init_x86(Jpeg2000DSPContext *c)
 
     if (EXTERNAL_AVX_FAST(cpu_flags)) {
         c->mct_decode[FF_DWT97] = ff_ict_float_avx;
+    }
+
+    if (EXTERNAL_FMA4(cpu_flags)) {
+        c->mct_decode[FF_DWT97] = ff_ict_float_fma4;
+    }
+
+    if (EXTERNAL_FMA3_FAST(cpu_flags)) {
+        c->mct_decode[FF_DWT97] = ff_ict_float_fma3;
     }
 
     if (EXTERNAL_AVX2_FAST(cpu_flags)) {
