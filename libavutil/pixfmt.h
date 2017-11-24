e1d192442780a6b60f75b78092a862bd40141099
Mark Thompson <sw@jkqxz.net>
2017-11-24 12:36:09 +0000

    lavu/pixfmt: Remove gap in the middle of enum AVPixelFormat
    This was added for compatibility with libav, by leaving a space for
    formats added in libav to be merged.  Since that feature has been
    removed, we don't need a gap here.
    
    Reviewed-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavutil/pixfmt.h
+++ libavutil/pixfmt.h
@@ -230,7 +230,7 @@ enum AVPixelFormat {
      */
     AV_PIX_FMT_CUDA,
 
-    AV_PIX_FMT_0RGB=0x123+4,///< packed RGB 8:8:8, 32bpp, XRGBXRGB...   X=unused/undefined
+    AV_PIX_FMT_0RGB,        ///< packed RGB 8:8:8, 32bpp, XRGBXRGB...   X=unused/undefined
     AV_PIX_FMT_RGB0,        ///< packed RGB 8:8:8, 32bpp, RGBXRGBX...   X=unused/undefined
     AV_PIX_FMT_0BGR,        ///< packed BGR 8:8:8, 32bpp, XBGRXBGR...   X=unused/undefined
     AV_PIX_FMT_BGR0,        ///< packed BGR 8:8:8, 32bpp, BGRXBGRX...   X=unused/undefined
