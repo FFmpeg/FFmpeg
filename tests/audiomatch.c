7a5c73896394c3f53371b7c91f0da99dbf0c2ae2
Michael Niedermayer <michael@niedermayer.cc>
2017-12-19 21:05:40 +0100

    tests/audiomatch: Add missing return code at the end of main()
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit 65da5c56e661a839e017db4c51c73d6f3d8a8fcb)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- tests/audiomatch.c
+++ tests/audiomatch.c
@@ -107,4 +107,6 @@ int main(int argc, char **argv){
         }
     }
     printf("presig: %d postsig:%d c:%7.4f lenerr:%d\n", bestpos, datlen - siglen - bestpos, bestc / sigamp, datlen - siglen);
+
+    return 0;
 }
