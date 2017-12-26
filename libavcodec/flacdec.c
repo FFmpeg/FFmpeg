3d23f7a0969bf76ad6dcdc2c4a5cd3ae884745a8
Michael Niedermayer <michael@niedermayer.cc>
2017-12-26 23:24:44 +0100

    avcodec/flacdec: Fix overflow in multiplication in decode_subframe_fixed()
    Fixes: signed integer overflow: 2 * 1629495328 cannot be represented in type 'int'
    Fixes: 4716/clusterfuzz-testcase-minimized-5835915940331520
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/flacdec.c
+++ libavcodec/flacdec.c
@@ -302,7 +302,7 @@ static int decode_subframe_fixed(FLACContext *s, int32_t *decoded,
     if (pred_order > 2)
         c = b - decoded[pred_order-2] + decoded[pred_order-3];
     if (pred_order > 3)
-        d = c - decoded[pred_order-2] + 2*decoded[pred_order-3] - decoded[pred_order-4];
+        d = c - decoded[pred_order-2] + 2U*decoded[pred_order-3] - decoded[pred_order-4];
 
     switch (pred_order) {
     case 0:
