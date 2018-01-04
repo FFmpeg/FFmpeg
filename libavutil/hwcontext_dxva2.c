18fbfd7bf86effb49c0341b37b9c1d73940d688e
wm4 <nfxjfg@googlemail.com>
2018-01-04 15:52:46 +0100

    hwcontext_dxva2: initialize D3DDISPLAYMODEEX correctly

--- libavutil/hwcontext_dxva2.c
+++ libavutil/hwcontext_dxva2.c
@@ -485,6 +485,7 @@ static int dxva2_device_create9ex(AVHWDeviceContext *ctx, UINT adapter)
     if (FAILED(hr))
         return AVERROR_UNKNOWN;
 
+    modeex.Size = sizeof(D3DDISPLAYMODEEX);
     hr = IDirect3D9Ex_GetAdapterDisplayModeEx(d3d9ex, adapter, &modeex, NULL);
     if (FAILED(hr)) {
         IDirect3D9Ex_Release(d3d9ex);
