205046420d5a4d389adb705538df3d6158be1fdb
Paul B Mahol <onemda@gmail.com>
2018-01-08 08:43:42 +0100

    avfilter/af_aiir: fix typo which may cause overread
    Signed-off-by: Paul B Mahol <onemda@gmail.com>

--- libavfilter/af_aiir.c
+++ libavfilter/af_aiir.c
@@ -306,7 +306,7 @@ static int expand(AVFilterContext *ctx, double *pz, int nb, double *coeffs)
     for (i = 0; i < nb + 1; i++) {
         if (fabs(coeffs[2 * i + 1]) > DBL_EPSILON) {
             av_log(ctx, AV_LOG_ERROR, "coeff: %lf of z^%d is not real; poles/zeros are not complex conjugates.\n",
-                   coeffs[2 * i + i], i);
+                   coeffs[2 * i + 1], i);
             return AVERROR(EINVAL);
         }
     }
