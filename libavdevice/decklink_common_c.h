fc93eca126aa4d68dd37c3e1b9d15bf05565ef04
Vishwanath Dixit <vdixit@akamai.com>
2018-01-15 13:52:26 +0530

    avdevice/decklink: addition of absolute wallclock option for pts source
    Signed-off-by: Marton Balint <cus@passwd.hu>

--- libavdevice/decklink_common_c.h
+++ libavdevice/decklink_common_c.h
@@ -28,6 +28,7 @@ typedef enum DecklinkPtsSource {
     PTS_SRC_VIDEO     = 2,
     PTS_SRC_REFERENCE = 3,
     PTS_SRC_WALLCLOCK = 4,
+    PTS_SRC_ABS_WALLCLOCK = 5,
     PTS_SRC_NB
 } DecklinkPtsSource;
 
