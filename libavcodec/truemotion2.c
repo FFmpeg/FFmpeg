d39b17f2375a4bf7c8a477b1881d9e8b1d007670
Michael Niedermayer <michael@niedermayer.cc>
2018-01-20 04:10:50 +0100

    avcodec/truemotion2: Fix integer overflow in TM2_RECALC_BLOCK()
    Fixes: signed integer overflow: 1477974040 - -1877995504 cannot be represented in type 'int'
    Fixes: 4861/clusterfuzz-testcase-minimized-4570316383715328
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit 56a53340ed4cc55898e49c07081311ebb2816630)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/truemotion2.c
+++ libavcodec/truemotion2.c
@@ -441,8 +441,8 @@ static inline int GET_TOK(TM2Context *ctx,int type)
 
 /* recalculate last and delta values for next blocks */
 #define TM2_RECALC_BLOCK(CHR, stride, last, CD) {\
-    CD[0] = CHR[1] - last[1];\
-    CD[1] = (int)CHR[stride + 1] - (int)CHR[1];\
+    CD[0] = (unsigned)CHR[         1] - (unsigned)last[1];\
+    CD[1] = (unsigned)CHR[stride + 1] - (unsigned) CHR[1];\
     last[0] = (int)CHR[stride + 0];\
     last[1] = (int)CHR[stride + 1];}
 
