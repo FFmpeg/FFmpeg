c4153d40d5d8a38a9762fbc9626728beba5887e4
Michael Niedermayer <michael@niedermayer.cc>
2018-02-03 18:49:07 +0100

    avcodec/scpr: Fix reading a pixel before the first
    Fixes: 5540/clusterfuzz-testcase-minimized-6122458273808384
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit 0fb33a82890753233225c61863fff1fcc9d970d4)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/scpr.c
+++ libavcodec/scpr.c
@@ -681,6 +681,8 @@ static int decompress_p(AVCodecContext *avctx,
                                 return AVERROR_INVALIDDATA;
 
                             if (bx == 0) {
+                                if (by < 2)
+                                    return AVERROR_INVALIDDATA;
                                 z = backstep;
                             } else {
                                 z = 0;
@@ -710,6 +712,8 @@ static int decompress_p(AVCodecContext *avctx,
                                 return AVERROR_INVALIDDATA;
 
                             if (bx == 0) {
+                                if (by < 2)
+                                    return AVERROR_INVALIDDATA;
                                 z = backstep;
                             } else {
                                 z = 0;
