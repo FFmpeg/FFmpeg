1c3144751a8235ce5abd463d31f20dac4e73c6b7
Tobias Rapp <t.rapp@noa-archive.com>
2018-02-14 17:01:08 +0100

    swresample/rematrix: fix update of channel matrix if input or output layout is undefined
    Prefer direct in/out channel count values over channel layout, when
    available. Fixes a pan filter bug (ticket #6790).
    
    Signed-off-by: Tobias Rapp <t.rapp@noa-archive.com>
    (cherry picked from commit 6325bd3717348615adafb52e4da2fd01a3007d0a)

--- libswresample/rematrix.c
+++ libswresample/rematrix.c
@@ -69,8 +69,10 @@ int swr_set_matrix(struct SwrContext *s, const double *matrix, int stride)
         return AVERROR(EINVAL);
     memset(s->matrix, 0, sizeof(s->matrix));
     memset(s->matrix_flt, 0, sizeof(s->matrix_flt));
-    nb_in  = av_get_channel_layout_nb_channels(s->user_in_ch_layout);
-    nb_out = av_get_channel_layout_nb_channels(s->user_out_ch_layout);
+    nb_in = (s->user_in_ch_count > 0) ? s->user_in_ch_count :
+        av_get_channel_layout_nb_channels(s->user_in_ch_layout);
+    nb_out = (s->user_out_ch_count > 0) ? s->user_out_ch_count :
+        av_get_channel_layout_nb_channels(s->user_out_ch_layout);
     for (out = 0; out < nb_out; out++) {
         for (in = 0; in < nb_in; in++)
             s->matrix_flt[out][in] = s->matrix[out][in] = matrix[in];
