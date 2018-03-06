5ffd13098d4bcf0bb47c80056b8cae066da5be8b
Avaer Kazmer <a@modules.io>
2018-03-06 14:12:16 -0800

    Modernize index.js

--- index.js
+++ index.js
@@ -3,30 +3,30 @@ const ffmpeg = require('./build/Release/ffmpeg');
 const {Video} = ffmpeg
 
 const v = new Video();
+
+console.log(v.width, v.height, v.currentTime, v.duration);
+
 const encodedData = fs.readFileSync('./sample.mpeg');
-if (v.load(encodedData)) {
-  console.log(v.width, v.height);
-  console.log(v.currentTime, v.duration);
+v.load(encodedData);
 
-  v.currentTime = 10;
+console.log(v.width, v.height, v.currentTime, v.duration);
 
-  console.log(v.currentTime, v.duration);
+v.currentTime = 10;
+
+console.log(v.currentTime, v.duration);
+console.log(v.data.slice(v.data.length / 2, v.data.length / 2 + 4));
+
+v.play();
+setTimeout(() => {
   console.log(v.data.slice(v.data.length / 2, v.data.length / 2 + 4));
 
-  v.play();
-  setTimeout(() => {
-    console.log(v.data.slice(v.data.length / 2, v.data.length / 2 + 4));
+  v.pause();
 
-    v.pause();
+  clearInterval(interval);
+}, 1000);
 
-    clearInterval(interval);
-  }, 1000);
-
-  const interval = setInterval(() => {
-    Video.updateAll();
-  }, 1000 / 90);
-} else {
-  throw new Error('failed to load');
-}
+const interval = setInterval(() => {
+  Video.updateAll();
+}, 1000 / 90);
 
 module.exports = ffmpeg;
