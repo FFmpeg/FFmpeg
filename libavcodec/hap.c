f869e54d228d43adb3b1e0026a48273befe443eb
Martin Vignali <martin.vignali@gmail.com>
2018-03-11 19:15:56 +0100

    avcodec/hap : move parse_section_header to hap.c in order to be use by new bsf filter

--- libavcodec/hap.c
+++ libavcodec/hap.c
@@ -53,3 +53,25 @@ av_cold void ff_hap_free_context(HapContext *ctx)
     av_freep(&ctx->chunks);
     av_freep(&ctx->chunk_results);
 }
+
+int ff_hap_parse_section_header(GetByteContext *gbc, int *section_size,
+                                enum HapSectionType *section_type)
+{
+    if (bytestream2_get_bytes_left(gbc) < 4)
+        return AVERROR_INVALIDDATA;
+
+    *section_size = bytestream2_get_le24(gbc);
+    *section_type = bytestream2_get_byte(gbc);
+
+    if (*section_size == 0) {
+        if (bytestream2_get_bytes_left(gbc) < 4)
+            return AVERROR_INVALIDDATA;
+
+        *section_size = bytestream2_get_le32(gbc);
+    }
+
+    if (*section_size > bytestream2_get_bytes_left(gbc) || *section_size < 0)
+        return AVERROR_INVALIDDATA;
+    else
+        return 0;
+}
