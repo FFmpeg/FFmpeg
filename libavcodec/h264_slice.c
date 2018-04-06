fffc77b472800a75eed147f8510ab52b87631256
Michael Niedermayer <michael@niedermayer.cc>
2018-04-07 00:34:25 +0200

    avcodec/h264_slice: Fix integer overflow with last_poc
    Fixes: signed integer overflow: 2147483646 - -2816 cannot be represented in type 'int'
    Fixes: crbug 823145
    
    Reported-by: Matt Wolenetz <wolenetz@google.com>
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit 8c02cd8ca097871dcd00cf8e08ce51660873f405)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/h264_slice.c
+++ libavcodec/h264_slice.c
@@ -1322,7 +1322,7 @@ static int h264_select_output_frame(H264Context *h)
     }
     out_of_order = MAX_DELAYED_PIC_COUNT - i;
     if(   cur->f->pict_type == AV_PICTURE_TYPE_B
-       || (h->last_pocs[MAX_DELAYED_PIC_COUNT-2] > INT_MIN && h->last_pocs[MAX_DELAYED_PIC_COUNT-1] - h->last_pocs[MAX_DELAYED_PIC_COUNT-2] > 2))
+       || (h->last_pocs[MAX_DELAYED_PIC_COUNT-2] > INT_MIN && h->last_pocs[MAX_DELAYED_PIC_COUNT-1] - (int64_t)h->last_pocs[MAX_DELAYED_PIC_COUNT-2] > 2))
         out_of_order = FFMAX(out_of_order, 1);
     if (out_of_order == MAX_DELAYED_PIC_COUNT) {
         av_log(h->avctx, AV_LOG_VERBOSE, "Invalid POC %d<%d\n", cur->poc, h->last_pocs[0]);
