9479955c626529550d337067af85760e256eabb3
Sergey Lavrushkin <dualfal@gmail.com>
2018-04-10 19:41:40 +0300

    Adds SRCNN filter.
    Signed-off-by: Pedro Arthur <bygrandao@gmail.com>

--- libavfilter/allfilters.c
+++ libavfilter/allfilters.c
@@ -322,6 +322,7 @@ extern AVFilter ff_vf_smartblur;
 extern AVFilter ff_vf_sobel;
 extern AVFilter ff_vf_split;
 extern AVFilter ff_vf_spp;
+extern AVFilter ff_vf_srcnn;
 extern AVFilter ff_vf_ssim;
 extern AVFilter ff_vf_stereo3d;
 extern AVFilter ff_vf_streamselect;
