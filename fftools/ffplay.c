6be690685a8876a61f87b2b8bf30547e09030a96
Matt Oliver <protogonoi@gmail.com>
2018-04-10 23:01:18 +1000

    ffplay: Fix realloc_texture when input texture is NULL.
    SDL_QueryTexture and SDL_DestroyTexture require that the input texture
    pointer be non-null. Debug builds of SDL will correctly check for this
    and break program execution. This patch fixes this by checking the
    status of the texture pointer.
    
    Signed-off-by: Matt Oliver <protogonoi@gmail.com>

--- fftools/ffplay.c
+++ fftools/ffplay.c
@@ -834,10 +834,11 @@ static int realloc_texture(SDL_Texture **texture, Uint32 new_format, int new_wid
 {
     Uint32 format;
     int access, w, h;
-    if (SDL_QueryTexture(*texture, &format, &access, &w, &h) < 0 || new_width != w || new_height != h || new_format != format) {
+    if (!*texture || SDL_QueryTexture(*texture, &format, &access, &w, &h) < 0 || new_width != w || new_height != h || new_format != format) {
         void *pixels;
         int pitch;
-        SDL_DestroyTexture(*texture);
+        if (*texture)
+            SDL_DestroyTexture(*texture);
         if (!(*texture = SDL_CreateTexture(renderer, new_format, SDL_TEXTUREACCESS_STREAMING, new_width, new_height)))
             return -1;
         if (SDL_SetTextureBlendMode(*texture, blendmode) < 0)
