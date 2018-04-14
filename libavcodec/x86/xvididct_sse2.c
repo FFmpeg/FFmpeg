347aa8f72356124ec6b95bf8ebd1faf72db03f8d
Martin Storsjö <martin@martin.st>
2018-04-14 13:37:03 +0300

    x86: Don't declare a non-static function as inline
    This fixes building with clang in msvc mode, which does support
    gcc style inline assembly.

--- libavcodec/x86/xvididct_sse2.c
+++ libavcodec/x86/xvididct_sse2.c
@@ -342,7 +342,7 @@ DECLARE_ASM_CONST(16, int32_t, walkenIdctRounders)[] = {
     "movdqa   %%xmm6, 4*16("dct")     \n\t" \
     "movdqa   "SREG2", 7*16("dct")    \n\t"
 
-inline void ff_xvid_idct_sse2(short *block)
+void ff_xvid_idct_sse2(short *block)
 {
     __asm__ volatile (
         "movq     "MANGLE (m127) ", %%mm0                              \n\t"
