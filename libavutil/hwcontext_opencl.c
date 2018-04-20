a2613647c4f40b9e802cd21f37545ef1fdf370d1
Rostislav Pehlivanov <atomnuker@gmail.com>
2018-04-20 05:30:04 +0100

    hwcontext_opencl: use ff_hwframe_map_replace()
    Signed-off-by: Rostislav Pehlivanov <atomnuker@gmail.com>

--- libavutil/hwcontext_opencl.c
+++ libavutil/hwcontext_opencl.c
@@ -2172,10 +2172,7 @@ static int opencl_map_from_vaapi(AVHWFramesContext *dst_fc,
     if (err < 0)
         goto fail;
 
-    // Adjust the map descriptor so that unmap works correctly.
-    hwmap = (HWMapDescriptor*)dst->buf[0]->data;
-    av_frame_unref(hwmap->source);
-    err = av_frame_ref(hwmap->source, src);
+    err = ff_hwframe_map_replace(dst, src);
 
 fail:
     av_frame_free(&tmp);
