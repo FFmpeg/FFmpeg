356a33b20aec0e6b3ca8c85da60e4c95b7559b35
Paul B Mahol <onemda@gmail.com>
2018-04-26 17:57:20 +0200

    avfilter/vf_atadenoise: do not abort if user specified invalid size
    Update documentation with new size default.
    
    Signed-off-by: Paul B Mahol <onemda@gmail.com>

--- libavfilter/vf_atadenoise.c
+++ libavfilter/vf_atadenoise.c
@@ -112,8 +112,8 @@ static av_cold int init(AVFilterContext *ctx)
     ATADenoiseContext *s = ctx->priv;
 
     if (!(s->size & 1)) {
-        av_log(ctx, AV_LOG_ERROR, "size %d is invalid. Must be an odd value.\n", s->size);
-        return AVERROR(EINVAL);
+        av_log(ctx, AV_LOG_WARNING, "size %d is invalid. Must be an odd value, setting it to %d.\n", s->size, s->size|1);
+        s->size |= 1;
     }
     s->mid = s->size / 2 + 1;
 
