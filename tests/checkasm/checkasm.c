f679711c1b516786a39f9e582622a200502fff74
Clément Bœsch <u@pkh.me>
2018-05-06 10:57:23 +0200

    checkasm: add vf_nlmeans test for ssd_integral_image

--- tests/checkasm/checkasm.c
+++ tests/checkasm/checkasm.c
@@ -159,6 +159,9 @@ static const struct {
     #if CONFIG_HFLIP_FILTER
         { "vf_hflip", checkasm_check_vf_hflip },
     #endif
+    #if CONFIG_NLMEANS_FILTER
+        { "vf_nlmeans", checkasm_check_nlmeans },
+    #endif
     #if CONFIG_THRESHOLD_FILTER
         { "vf_threshold", checkasm_check_vf_threshold },
     #endif
