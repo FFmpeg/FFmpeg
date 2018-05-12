81b3e7c9c3ad0cb81c49ad7557041e4cd04bab58
Michael Niedermayer <michael@niedermayer.cc>
2018-05-12 18:33:28 +0200

    avformat/mp3dec: Require 50% of the file to be mp3 frames in the maxframes>200 probing test
    This corrects several misdetections of large files
    
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavformat/mp3dec.c
+++ libavformat/mp3dec.c
@@ -107,7 +107,7 @@ static int mp3_read_probe(AVProbeData *p)
     // keep this in sync with ac3 probe, both need to avoid
     // issues with MPEG-files!
     if   (first_frames>=7) return AVPROBE_SCORE_EXTENSION + 1;
-    else if(max_frames>200)return AVPROBE_SCORE_EXTENSION;
+    else if(max_frames>200 && p->buf_size < 2*max_framesizes)return AVPROBE_SCORE_EXTENSION;
     else if(max_frames>=4 && p->buf_size < 2*max_framesizes) return AVPROBE_SCORE_EXTENSION / 2;
     else if(ff_id3v2_match(buf0, ID3v2_DEFAULT_MAGIC) && 2*ff_id3v2_tag_len(buf0) >= p->buf_size)
                            return p->buf_size < PROBE_BUF_MAX ? AVPROBE_SCORE_EXTENSION / 4 : AVPROBE_SCORE_EXTENSION - 2;
