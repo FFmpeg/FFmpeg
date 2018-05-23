37ca9610df8c605a9d4e9fda53f6417a31280f16
Kevin Lambert <klambert@switch.tv>
2018-05-24 08:43:46 +1000

    AV1

--- kjsl_build_osx.sh
+++ kjsl_build_osx.sh
@@ -4,6 +4,14 @@
 
 brew install automake fdk-aac git lame libass libtool libvorbis libvpx opus sdl shtool texi2html theora wget x264 xvid yasm
 
+curl -O -L https://github.com/kevleyski/aom/archive/master.zip
+unzip master.zip
+cd aom-master
+./configure --prefix="$HOME/ffmpeg_build" --cpu=sse3 --enable-av1 --disable-shared
+make
+make install
+cd ..
+
 ./configure  --prefix=/usr/local --enable-gpl --enable-nonfree \
 --enable-libass --enable-libfdk-aac --enable-libfreetype \
 --enable-libopus --enable-libtheora --enable-libvorbis \
@@ -13,13 +21,15 @@ brew install automake fdk-aac git lame libass libtool libvorbis libvpx opus sdl 
 -L/usr/local/Cellar/libogg/1.3.2/lib \
 -L/usr/local/Cellar/theora/1.1.1/lib \
 -L/usr/local/Cellar/libvorbis/1.3.5/lib \
--L/usr/local/Cellar/xvid/1.3.4/lib" \
+-L/usr/local/Cellar/xvid/1.3.4/lib \
+-L$HOME/ffmpeg_build/lib" \
 --extra-cflags="-I/usr/local/Cellar/lame/3.99.5/include \
 -I/usr/local/Cellar/libogg/1.3.2/include \
 -I/usr/local/Cellar/theora/1.1.1/include \
 -I/usr/local/Cellar/libvorbis/1.3.5/include \
 -I/usr/local/Cellar/xvid/1.3.4/include \
--I/usr/local//Cellar/sdl/1.2.15/include"
+-I/usr/local/Cellar/sdl/1.2.15/include \
+-I$HOME/ffmpeg_build/include"
 
 make
 sudo make install
