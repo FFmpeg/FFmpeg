c8b57d4333b1bc5c4125fdac8fbf4625c399fc9c
Jan Ekström <jeebjp@gmail.com>
2018-05-28 03:21:35 +0300

    lavf/libssh: translate a read of 0 to EOF
    Yet another case of forgotten 0 =! EOF translation.
    
    While the documentation for this specific synchronous read
    function does not mention it, the documentation for
    `sftp_async_read` documents it, as well as looking at the
    implementation of this function leads one to find
    `if (handle->eof) { return 0; }`.
    
    Reported by stnutt on IRC.
    
    (cherry picked from commit 26892c7615395f331f6143535f03a2957973e2e0)

--- libavformat/libssh.c
+++ libavformat/libssh.c
@@ -295,7 +295,7 @@ static int libssh_read(URLContext *h, unsigned char *buf, int size)
         av_log(libssh, AV_LOG_ERROR, "Read error.\n");
         return AVERROR(EIO);
     }
-    return bytes_read;
+    return bytes_read ? bytes_read : AVERROR_EOF;
 }
 
 static int libssh_write(URLContext *h, const unsigned char *buf, int size)
