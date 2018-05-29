4f0efed0a56cf99fb8849162f9b105d449c35257
Kevin Lambert <klambert@switch.tv>
2018-05-29 10:42:06 +1000

    Allow AV1 WebM

--- kjsl_build_osx.sh
+++ kjsl_build_osx.sh
@@ -12,24 +12,25 @@ make
 make install
 cd ..
 
-./configure  --prefix=/usr/local --enable-gpl --enable-nonfree \
---enable-libass --enable-libfdk-aac --enable-libfreetype \
---enable-libopus --enable-libtheora --enable-libvorbis \
---enable-libvpx --enable-libx264 --enable-libxvid --enable-ffplay \
---enable-libaom \
---extra-ldflags="-L/usr/local/Cellar/lame/3.99.5/lib \
--L/usr/local/Cellar/libogg/1.3.2/lib \
--L/usr/local/Cellar/theora/1.1.1/lib \
--L/usr/local/Cellar/libvorbis/1.3.5/lib \
--L/usr/local/Cellar/xvid/1.3.4/lib \
--L$HOME/ffmpeg_build/lib" \
---extra-cflags="-I/usr/local/Cellar/lame/3.99.5/include \
--I/usr/local/Cellar/libogg/1.3.2/include \
--I/usr/local/Cellar/theora/1.1.1/include \
--I/usr/local/Cellar/libvorbis/1.3.5/include \
--I/usr/local/Cellar/xvid/1.3.4/include \
--I/usr/local/Cellar/sdl/1.2.15/include \
--I$HOME/ffmpeg_build/include"
+# build FFmpeg 4 (with AV1)
+ ./configure  --prefix=/usr/local --enable-gpl --enable-nonfree \
+ --enable-libass --enable-libfdk-aac --enable-libfreetype \
+ --enable-libopus --enable-libtheora --enable-libvorbis \
+ --enable-libvpx --enable-libx264 --enable-libxvid --enable-ffplay \
+ --enable-libaom \
+ --extra-ldflags="-L/usr/local/Cellar/lame/3.99.5/lib \
+ -L/usr/local/Cellar/libogg/1.3.2/lib \
+ -L/usr/local/Cellar/theora/1.1.1/lib \
+ -L/usr/local/Cellar/libvorbis/1.3.5/lib \
+ -L/usr/local/Cellar/xvid/1.3.4/lib \
+ -L/usr/local/Cellar/aom/HEAD-285d941/lib" \
+ --extra-cflags="-I/usr/local/Cellar/lame/3.99.5/include \
+ -I/usr/local/Cellar/libogg/1.3.2/include \
+ -I/usr/local/Cellar/theora/1.1.1/include \
+ -I/usr/local/Cellar/libvorbis/1.3.5/include \
+ -I/usr/local/Cellar/xvid/1.3.4/include \
+ -I/usr/local/Cellar/sdl/1.2.15/include \
+ -I/usr/local/Cellar/aom/HEAD-285d941/include"
 
 make
 sudo make install
