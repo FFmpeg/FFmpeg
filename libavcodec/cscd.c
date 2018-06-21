35c365f6ef44c37bfc17af45b6e909fff7fa3f0f
Michael Niedermayer <michael@niedermayer.cc>
2018-06-22 01:18:20 +0200

    avcodec/cscd: Check output buffer size for lzo.
    Fixes: Timeout
    Fixes: 8665/clusterfuzz-testcase-minimized-ffmpeg_AV_CODEC_ID_CSCD_fuzzer-5768442610188288
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    (cherry picked from commit 78167b498f53c36c31105a2bf11e90b03637598f)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/cscd.c
+++ libavcodec/cscd.c
@@ -81,7 +81,7 @@ static int decode_frame(AVCodecContext *avctx, void *data, int *got_frame,
     switch ((buf[0] >> 1) & 7) {
         case 0: { // lzo compression
             int outlen = c->decomp_size, inlen = buf_size - 2;
-            if (av_lzo1x_decode(c->decomp_buf, &outlen, &buf[2], &inlen)) {
+            if (av_lzo1x_decode(c->decomp_buf, &outlen, &buf[2], &inlen) || outlen) {
                 av_log(avctx, AV_LOG_ERROR, "error during lzo decompression\n");
                 return AVERROR_INVALIDDATA;
             }
