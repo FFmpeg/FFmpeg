bd27a9364ca274ca97f1df6d984e88a0700fb235
Michael Niedermayer <michael@niedermayer.cc>
2018-07-03 00:27:04 +0200

    avcodec/mpeg4videodec: Remove use of FF_PROFILE_MPEG4_SIMPLE_STUDIO as indicator of studio profile
    The profile field is changed by code inside and outside the decoder,
    its not a reliable indicator of the internal codec state.
    Maintaining it consistency with studio_profile is messy.
    Its easier to just avoid it and use only studio_profile
    
    Fixes: assertion failure
    Fixes: ffmpeg_crash_9.avi
    
    Found-by: Thuan Pham, Marcel Böhme, Andrew Santosa and Alexandru Razvan Caciulescu with AFLSmart
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/error_resilience.c
+++ libavcodec/error_resilience.c
@@ -814,8 +814,7 @@ static int er_supported(ERContext *s)
 {
     if(s->avctx->hwaccel && s->avctx->hwaccel->decode_slice           ||
        !s->cur_pic.f                                                  ||
-       s->cur_pic.field_picture                                       ||
-       s->avctx->profile == FF_PROFILE_MPEG4_SIMPLE_STUDIO
+       s->cur_pic.field_picture
     )
         return 0;
     return 1;
