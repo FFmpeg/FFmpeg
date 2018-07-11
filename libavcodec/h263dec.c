5545a6df879f0c94e7e74ddc6606e300716f390b
Carl Eugen Hoyos <ceffmpeg@gmail.com>
2018-07-12 00:07:55 +0200

    lavc/h263dec: Remove a variable declaration that can lead to a warning.

--- libavcodec/h263dec.c
+++ libavcodec/h263dec.c
@@ -47,10 +47,9 @@
 
 static enum AVPixelFormat h263_get_format(AVCodecContext *avctx)
 {
-    MpegEncContext *s = avctx->priv_data;
     /* MPEG-4 Studio Profile only, not supported by hardware */
     if (avctx->bits_per_raw_sample > 8) {
-        av_assert1(s->studio_profile);
+        av_assert1(((MpegEncContext *)avctx->priv_data)->studio_profile);
         return avctx->pix_fmt;
     }
 
