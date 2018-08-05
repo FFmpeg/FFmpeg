0f4c3b0b8e5435d13fd3b64c91969b31c3c018dc
Michael Niedermayer <michael@niedermayer.cc>
2018-08-05 14:51:36 +0200

    avcodec/mpegaudio_parser: Initialize poutbuf*
    Possibly fixes: null pointer dereference
    Possibly fixes: 9352/clusterfuzz-testcase-minimized-ffmpeg_AV_CODEC_ID_MP3ADUFLOAT_fuzzer-5146068961460224
    Fixes: Heap-use-after-free
    Fixes: 9453/clusterfuzz-testcase-minimized-ffmpeg_AV_CODEC_ID_MP3ADUFLOAT_fuzzer-5137954375729152
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/mpegaudio_parser.c
+++ libavcodec/mpegaudio_parser.c
@@ -99,6 +99,8 @@ static int mpegaudio_parse(AVCodecParserContext *s1,
                     } else if (codec_id == AV_CODEC_ID_MP3ADU) {
                         avpriv_report_missing_feature(avctx,
                             "MP3ADU full parser");
+                        *poutbuf = NULL;
+                        *poutbuf_size = 0;
                         return 0; /* parsers must not return error codes */
                     }
 
