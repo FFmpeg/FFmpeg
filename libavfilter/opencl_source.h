5423fe29da6bbf28f5253800cace133ac8343a7e
Danil Iashchenko <danyaschenko@gmail.com>
2018-08-12 23:12:08 +0300

    lavfi: add erosion_opencl, dilation_opencl filters
    Add erosion_opencl, dilation_opencl filters. Behave like existing erosion and dilation filters.

--- libavfilter/opencl_source.h
+++ libavfilter/opencl_source.h
@@ -22,6 +22,7 @@
 extern const char *ff_opencl_source_avgblur;
 extern const char *ff_opencl_source_colorspace_common;
 extern const char *ff_opencl_source_convolution;
+extern const char *ff_opencl_source_neighbor;
 extern const char *ff_opencl_source_overlay;
 extern const char *ff_opencl_source_tonemap;
 extern const char *ff_opencl_source_unsharp;
