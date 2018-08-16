c9ed7f002492c7901f8638d8765bf4532dc2900a
Jun Zhao <mypopydev@gmail.com>
2018-08-16 14:33:23 +0800

    lavc/encode: remove redundant av_init_packet after av_packet_unref.
    remove redundant av_init_packet after av_packet_unref.
    av_packet_unref have call av_init_packet and reset the packet size.
    
    Signed-off-by: Jun Zhao <mypopydev@gmail.com>

--- libavcodec/encode.c
+++ libavcodec/encode.c
@@ -135,7 +135,6 @@ int attribute_align_arg avcodec_encode_audio2(AVCodecContext *avctx,
 
     if (!(avctx->codec->capabilities & AV_CODEC_CAP_DELAY) && !frame) {
         av_packet_unref(avpkt);
-        av_init_packet(avpkt);
         return 0;
     }
 
@@ -241,7 +240,6 @@ int attribute_align_arg avcodec_encode_audio2(AVCodecContext *avctx,
 
     if (ret < 0 || !*got_packet_ptr) {
         av_packet_unref(avpkt);
-        av_init_packet(avpkt);
         goto end;
     }
 
@@ -282,8 +280,6 @@ int attribute_align_arg avcodec_encode_video2(AVCodecContext *avctx,
 
     if (!(avctx->codec->capabilities & AV_CODEC_CAP_DELAY) && !frame) {
         av_packet_unref(avpkt);
-        av_init_packet(avpkt);
-        avpkt->size = 0;
         return 0;
     }
 
