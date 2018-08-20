5ecd580953bbed48e144c364d3cdf7ea1f3e7ddf
Fredrik Hubinette <hubbe@google.com>
2018-08-20 12:59:32 -0700

    avformat/utils: avoid undefined integer overflow behavior in update_stream_timings()
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavformat/utils.c
+++ libavformat/utils.c
@@ -2666,7 +2666,7 @@ static void update_stream_timings(AVFormatContext *ic)
                 duration = FFMAX(duration, duration1);
         }
     }
-    if (start_time == INT64_MAX || (start_time > start_time_text && start_time - start_time_text < AV_TIME_BASE))
+    if (start_time == INT64_MAX || (start_time > start_time_text && start_time - (uint64_t)start_time_text < AV_TIME_BASE))
         start_time = start_time_text;
     else if (start_time > start_time_text)
         av_log(ic, AV_LOG_VERBOSE, "Ignoring outlier non primary stream starttime %f\n", start_time_text / (float)AV_TIME_BASE);
