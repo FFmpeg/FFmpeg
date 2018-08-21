e7080d1fe29b0fc2ef3ac99234e037e6a49f5c97
Dale Curtis <dalecurtis@chromium.org>
2018-08-21 15:42:31 -0700

    avformat/mov: Correct opus-in-mp4 pre-skip to be uint16_t versus int16_t.
    This field is a uint16_t, see docs:
    http://opus-codec.org/docs/opus_in_isobmff.html#4.3.2
    
    Signed-off-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: James Almer <jamrial@gmail.com>
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavformat/mov.c
+++ libavformat/mov.c
@@ -6614,7 +6614,7 @@ static int mov_read_dops(MOVContext *c, AVIOContext *pb, MOVAtom atom)
     const int OPUS_SEEK_PREROLL_MS = 80;
     AVStream *st;
     size_t size;
-    int16_t pre_skip;
+    uint16_t pre_skip;
 
     if (c->fc->nb_streams < 1)
         return 0;
