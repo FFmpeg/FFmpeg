90ba17652aa1f5773bb121c2ec6a092acf66a5e1
Roman Arzumanyan <rarzumanyan@nvidia.com>
2018-08-29 12:53:02 +0300

    avfilter: add nvidia NPP based transpose filter
    Signed-off-by: Timo Rothenpieler <timo@rothenpieler.org>

--- libavfilter/allfilters.c
+++ libavfilter/allfilters.c
@@ -358,6 +358,7 @@ extern AVFilter ff_vf_tmix;
 extern AVFilter ff_vf_tonemap;
 extern AVFilter ff_vf_tonemap_opencl;
 extern AVFilter ff_vf_transpose;
+extern AVFilter ff_vf_transpose_npp;
 extern AVFilter ff_vf_trim;
 extern AVFilter ff_vf_unpremultiply;
 extern AVFilter ff_vf_unsharp;
