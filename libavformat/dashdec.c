ad9b4ecc26bef7babb31b58f19c869f6464c2db9
Steven Liu <lq@chinaffmpeg.org>
2018-09-05 14:33:53 +0800

    avformat/dashdec: refine compute current fragment for presentation_timeoffset mode
    fix ticket id: #7369 #7382.
    use ((wallclock - availability_start_time) * timescale -presentation_timeoffset) / duration
    
    Signed-off-by: Steven Liu <lq@onvideo.cn>

--- libavformat/dashdec.c
+++ libavformat/dashdec.c
@@ -1341,7 +1341,7 @@ static int64_t calc_cur_seg_no(AVFormatContext *s, struct representation *pls)
         } else if (pls->fragment_duration){
             av_log(s, AV_LOG_TRACE, "in fragment_duration mode fragment_timescale = %"PRId64", presentation_timeoffset = %"PRId64"\n", pls->fragment_timescale, pls->presentation_timeoffset);
             if (pls->presentation_timeoffset) {
-                num = pls->presentation_timeoffset * pls->fragment_timescale / pls->fragment_duration;
+                num = pls->first_seq_no + (((get_current_time_in_sec() - c->availability_start_time) * pls->fragment_timescale)-pls->presentation_timeoffset) / pls->fragment_duration;
             } else if (c->publish_time > 0 && !c->availability_start_time) {
                 if (c->min_buffer_time) {
                     num = pls->first_seq_no + (((c->publish_time + pls->fragment_duration) - c->suggested_presentation_delay) * pls->fragment_timescale) / pls->fragment_duration - c->min_buffer_time;
