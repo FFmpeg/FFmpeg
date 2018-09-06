b9d1f5bf68111772e552421d21a32b0d61718899
Zhao Zhili <quinkblack@foxmail.com>
2018-09-06 17:15:11 +0800

    avcodec/h264dec: check number of SPS in is_extra
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/h264dec.c
+++ libavcodec/h264dec.c
@@ -855,6 +855,8 @@ static int is_extra(const uint8_t *buf, int buf_size)
 {
     int cnt= buf[5]&0x1f;
     const uint8_t *p= buf+6;
+    if (!cnt)
+        return 0;
     while(cnt--){
         int nalsize= AV_RB16(p) + 2;
         if(nalsize > buf_size - (p-buf) || (p[2] & 0x9F) != 7)
