6f23c327c5a0efe61a466d2c62e404f6b935d400
hwren <hwrenx@126.com>
2018-09-12 13:53:07 +0800

    lavc/libdavs2: remove unused header files
    Signed-off-by: hwren <hwrenx@126.com>

--- libavcodec/libdavs2.c
+++ libavcodec/libdavs2.c
@@ -22,13 +22,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
-#include "libavutil/avassert.h"
-#include "libavutil/common.h"
-#include "libavutil/avutil.h"
 #include "avcodec.h"
-#include "libavutil/imgutils.h"
-#include "internal.h"
-
 #include "davs2.h"
 
 typedef struct DAVS2Context {
