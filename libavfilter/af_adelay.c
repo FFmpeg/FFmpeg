bf5bdbdb926742235e2037389115e9308223e153
Paul B Mahol <onemda@gmail.com>
2018-10-18 10:21:09 +0200

    avfilter/af_adelay: do not crash if allocation of chandelay failed

--- libavfilter/af_adelay.c
+++ libavfilter/af_adelay.c
@@ -249,10 +249,11 @@ static int request_frame(AVFilterLink *outlink)
 static av_cold void uninit(AVFilterContext *ctx)
 {
     AudioDelayContext *s = ctx->priv;
-    int i;
 
-    for (i = 0; i < s->nb_delays; i++)
-        av_freep(&s->chandelay[i].samples);
+    if (s->chandelay) {
+        for (int i = 0; i < s->nb_delays; i++)
+            av_freep(&s->chandelay[i].samples);
+    }
     av_freep(&s->chandelay);
 }
 
