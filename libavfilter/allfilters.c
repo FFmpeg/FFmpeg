97e9e3504d36ef11efac60e300259689aaeb29fc
Paul B Mahol <onemda@gmail.com>
2018-10-30 09:33:12 +0100

    avfilter: add tpad filter

--- libavfilter/allfilters.c
+++ libavfilter/allfilters.c
@@ -365,6 +365,7 @@ extern AVFilter ff_vf_tlut2;
 extern AVFilter ff_vf_tmix;
 extern AVFilter ff_vf_tonemap;
 extern AVFilter ff_vf_tonemap_opencl;
+extern AVFilter ff_vf_tpad;
 extern AVFilter ff_vf_transpose;
 extern AVFilter ff_vf_transpose_npp;
 extern AVFilter ff_vf_trim;
