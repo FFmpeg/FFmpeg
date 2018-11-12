7992c2867efc79eb8e9983e15a7ba998d6c48ffd
Tobias Rapp <t.rapp@noa-archive.com>
2018-11-12 17:04:43 +0100

    avcodec/libopenh264enc: fix class_name
    Reverts some accidental change in commit e621b1ca646a2f268797adc3194b694a852548d2.
    
    Reviewed-by: Jan Ekström <jeebjp@gmail.com>
    Signed-off-by: Tobias Rapp <t.rapp@noa-archive.com>

--- libavcodec/libopenh264enc.c
+++ libavcodec/libopenh264enc.c
@@ -75,7 +75,7 @@ static const AVOption options[] = {
 };
 
 static const AVClass class = {
-    .class_name = "libvo_amrwbenc",
+    .class_name = "libopenh264enc",
     .item_name  = av_default_item_name,
     .option     = options,
     .version    = LIBAVUTIL_VERSION_INT,
