b3dbd651718bccd295bbe822bb4614e6afbf0a44
Evgeny <kea@obninsk.ru>
2018-11-16 16:33:21 +0300

    ALSA out: finalization time is too long not need wait for playing buffered data

--- libavdevice/alsa.c
+++ libavdevice/alsa.c
@@ -301,8 +301,9 @@ av_cold int ff_alsa_close(AVFormatContext *s1)
     AlsaData *s = s1->priv_data;
 
     snd_pcm_nonblock(s->h, 0);
-    snd_pcm_drain(s->h);
-    av_freep(&s->reorder_buf);
+//    snd_pcm_drain(s->h);
+	snd_pcm_drop(s->h);
+	av_freep(&s->reorder_buf);
     if (CONFIG_ALSA_INDEV)
         ff_timefilter_destroy(s->timefilter);
     snd_pcm_close(s->h);
