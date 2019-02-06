d1afa7284c3feba4debfebf1b9cf8ad67640e34a
Michael Niedermayer <michael@niedermayer.cc>
2019-02-06 15:29:38 +0100

    avformat/matroskadec: Do not leak queued packets on sync errors
    Fixes: memleak
    Fixes: clusterfuzz-testcase-minimized-audio_decoder_fuzzer-5649187601121280
    
    Reported-by: Chris Cunningham <chcunningham@google.com>
    Tested-by: Chris Cunningham <chcunningham@google.com>
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavformat/matroskadec.c
+++ libavformat/matroskadec.c
@@ -3541,7 +3541,7 @@ static int matroska_read_packet(AVFormatContext *s, AVPacket *pkt)
             ret = matroska_resync(matroska, pos);
     }
 
-    return ret;
+    return 0;
 }
 
 static int matroska_read_seek(AVFormatContext *s, int stream_index,
