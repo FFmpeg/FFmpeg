02518ba07fe6ec7295ac9f786965de72a02b6a4e
Wenxiang Qian <leonwxqian@qq.com>
2019-02-13 08:47:20 +0100

    avformat/ftp: Fix Out-of-Bounds Access and Information Leak in ftp.c:393
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit a142ffdcaec06fcbf7d4b00dbb0e5ddfb9e3344d)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavformat/ftp.c
+++ libavformat/ftp.c
@@ -389,7 +389,7 @@ static int ftp_file_size(FTPContext *s)
     static const int size_codes[] = {213, 0};
 
     snprintf(command, sizeof(command), "SIZE %s\r\n", s->path);
-    if (ftp_send_command(s, command, size_codes, &res) == 213 && res) {
+    if (ftp_send_command(s, command, size_codes, &res) == 213 && res && strlen(res) > 4) {
         s->filesize = strtoll(&res[4], NULL, 10);
     } else {
         s->filesize = -1;
