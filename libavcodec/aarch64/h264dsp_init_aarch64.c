7e4d3dbe18cec6f4ce0e9dfeb570d47af70af12c
Carl Eugen Hoyos <ceffmpeg@gmail.com>
2019-02-20 23:56:21 +0100

    lavc/aarch64/h264dsp_init: Only use neon horizontal intra loopfilter for 4:2:0.

--- libavcodec/aarch64/h264dsp_init_aarch64.c
+++ libavcodec/aarch64/h264dsp_init_aarch64.c
@@ -91,11 +91,12 @@ av_cold void ff_h264dsp_init_aarch64(H264DSPContext *c, const int bit_depth,
         c->h264_h_loop_filter_luma_intra= ff_h264_h_loop_filter_luma_intra_neon;
 
         c->h264_v_loop_filter_chroma = ff_h264_v_loop_filter_chroma_neon;
-        if (chroma_format_idc <= 1)
-        c->h264_h_loop_filter_chroma = ff_h264_h_loop_filter_chroma_neon;
+        if (chroma_format_idc <= 1) {
+            c->h264_h_loop_filter_chroma = ff_h264_h_loop_filter_chroma_neon;
+            c->h264_h_loop_filter_chroma_intra = ff_h264_h_loop_filter_chroma_intra_neon;
+            c->h264_h_loop_filter_chroma_mbaff_intra = ff_h264_h_loop_filter_chroma_mbaff_intra_neon;
+        }
         c->h264_v_loop_filter_chroma_intra = ff_h264_v_loop_filter_chroma_intra_neon;
-        c->h264_h_loop_filter_chroma_intra = ff_h264_h_loop_filter_chroma_intra_neon;
-        c->h264_h_loop_filter_chroma_mbaff_intra = ff_h264_h_loop_filter_chroma_mbaff_intra_neon;
 
         c->weight_h264_pixels_tab[0] = ff_weight_h264_pixels_16_neon;
         c->weight_h264_pixels_tab[1] = ff_weight_h264_pixels_8_neon;
