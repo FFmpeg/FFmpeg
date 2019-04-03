7c1875143db4573bad2d39f13aadbef7b13628ad
Jun Zhao <barryjzhao@tencent.com>
2019-04-03 09:13:52 +0800

    doc/examples/metadata: fix the example can't dump FLV metadata
    fix the example can't dump FLV metadata.
    
    Signed-off-by: Jun Zhao <barryjzhao@tencent.com>

--- doc/examples/metadata.c
+++ doc/examples/metadata.c
@@ -47,6 +47,11 @@ int main (int argc, char **argv)
     if ((ret = avformat_open_input(&fmt_ctx, argv[1], NULL, NULL)))
         return ret;
 
+    if ((ret = avformat_find_stream_info(fmt_ctx, NULL)) < 0) {
+        av_log(NULL, AV_LOG_ERROR, "Cannot find stream information\n");
+        return ret;
+    }
+
     while ((tag = av_dict_get(fmt_ctx->metadata, "", tag, AV_DICT_IGNORE_SUFFIX)))
         printf("%s=%s\n", tag->key, tag->value);
 
