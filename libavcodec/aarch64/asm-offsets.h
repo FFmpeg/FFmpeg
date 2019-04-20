f62ee527cbdafcd51a8037b21b565924d579d022
Lynne <dev@lynne.ee>
2019-04-20 02:04:51 +0100

    aarch64/asm-offsets: remove old CELT offsets
    They're not used and they're incorrect.

--- libavcodec/aarch64/asm-offsets.h
+++ libavcodec/aarch64/asm-offsets.h
@@ -19,14 +19,6 @@
 #ifndef AVCODEC_AARCH64_ASM_OFFSETS_H
 #define AVCODEC_AARCH64_ASM_OFFSETS_H
 
-/* CeltIMDCTContext */
-#define CELT_EXPTAB                     0x20
-#define CELT_FFT_N                      0x00
-#define CELT_LEN2                       0x04
-#define CELT_LEN4                       (CELT_LEN2 + 0x4)   // loaded as pair
-#define CELT_TMP                        0x10
-#define CELT_TWIDDLE                    (CELT_TMP + 0x8)    // loaded as pair
-
 /* FFTContext */
 #define IMDCT_HALF                      0x48
 
