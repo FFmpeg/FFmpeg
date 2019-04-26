d7d82cfcd4394fde5f9b53ab0251ce1b55b674a7
ManojGuptaBonda <mbonda@nvidia.com>
2019-04-26 09:43:36 +0530

    avcodec/hevcdec: Declare that VDPAU can handle HEVC 4:4:4 content
    The latest generation video decoder on the Turing chips supports
    decoding HEVC 4:4:4. This change adds AV_PIX_FMT_VDPAU as a valid format
    for HEVC 4:4:4 8 bit.

--- libavcodec/hevcdec.c
+++ libavcodec/hevcdec.c
@@ -408,8 +408,15 @@ static enum AVPixelFormat get_format(HEVCContext *s, const HEVCSPS *sps)
         *fmt++ = AV_PIX_FMT_CUDA;
 #endif
         break;
-    case AV_PIX_FMT_YUV420P12:
     case AV_PIX_FMT_YUV444P:
+#if CONFIG_HEVC_VDPAU_HWACCEL
+        *fmt++ = AV_PIX_FMT_VDPAU;
+#endif
+#if CONFIG_HEVC_NVDEC_HWACCEL
+        *fmt++ = AV_PIX_FMT_CUDA;
+#endif
+        break;
+    case AV_PIX_FMT_YUV420P12:
     case AV_PIX_FMT_YUV444P10:
     case AV_PIX_FMT_YUV444P12:
 #if CONFIG_HEVC_NVDEC_HWACCEL
