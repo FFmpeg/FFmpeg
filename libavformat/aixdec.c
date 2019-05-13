00c5526fc8bbfcd210f685c49bcd345fa2160b60
Anton Khirnov <anton@khirnov.net>
2019-05-13 11:11:26 +0200

    aixdec: convert to new channel layout API
    Signed-off-by: James Almer <jamrial@gmail.com>

--- libavformat/aixdec.c
+++ libavformat/aixdec.c
@@ -64,7 +64,7 @@ static int aix_read_header(AVFormatContext *s)
         st->codecpar->codec_type  = AVMEDIA_TYPE_AUDIO;
         st->codecpar->codec_id    = AV_CODEC_ID_ADPCM_ADX;
         st->codecpar->sample_rate = avio_rb32(s->pb);
-        st->codecpar->channels    = avio_r8(s->pb);
+        st->codecpar->ch_layout.nb_channels = avio_r8(s->pb);
         avpriv_set_pts_info(st, 64, 1, st->codecpar->sample_rate);
         avio_skip(s->pb, 3);
     }
