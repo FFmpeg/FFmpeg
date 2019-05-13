81b3ebbae9b42425b524b8ea413845a09e99f578
Anton Khirnov <anton@khirnov.net>
2019-05-13 11:11:26 +0200

    sdx: convert to new channel layout API
    Signed-off-by: James Almer <jamrial@gmail.com>

--- libavformat/sdxdec.c
+++ libavformat/sdxdec.c
@@ -54,7 +54,7 @@ static int sdx_read_header(AVFormatContext *s)
         return AVERROR(ENOMEM);
 
     st->codecpar->codec_type = AVMEDIA_TYPE_AUDIO;
-    st->codecpar->channels = 1;
+    st->codecpar->ch_layout.nb_channels = 1;
     st->codecpar->sample_rate = avio_rl32(s->pb);
     switch (depth) {
     case 8:
