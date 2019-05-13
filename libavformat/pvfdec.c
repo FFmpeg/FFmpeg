02732974056c56e57d494912a7b8c2ffc22d518b
Anton Khirnov <anton@khirnov.net>
2019-05-13 11:11:26 +0200

    pvf: convert to new channel layout API
    Signed-off-by: James Almer <jamrial@gmail.com>

--- libavformat/pvfdec.c
+++ libavformat/pvfdec.c
@@ -54,11 +54,11 @@ static int pvf_read_header(AVFormatContext *s)
         return AVERROR(ENOMEM);
 
     st->codecpar->codec_type  = AVMEDIA_TYPE_AUDIO;
-    st->codecpar->channels    = channels;
+    st->codecpar->ch_layout.nb_channels = channels;
     st->codecpar->sample_rate = sample_rate;
     st->codecpar->codec_id    = ff_get_pcm_codec_id(bps, 0, 1, 0xFFFF);
     st->codecpar->bits_per_coded_sample = bps;
-    st->codecpar->block_align = bps * st->codecpar->channels / 8;
+    st->codecpar->block_align = bps * st->codecpar->ch_layout.nb_channels / 8;
 
     avpriv_set_pts_info(st, 64, 1, st->codecpar->sample_rate);
 
