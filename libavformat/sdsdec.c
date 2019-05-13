e968ae81f2dfd035436eabc79f21958824f2a290
Anton Khirnov <anton@khirnov.net>
2019-05-13 11:11:26 +0200

    sds: convert to new channel layout API
    Signed-off-by: James Almer <jamrial@gmail.com>

--- libavformat/sdsdec.c
+++ libavformat/sdsdec.c
@@ -110,7 +110,7 @@ static int sds_read_header(AVFormatContext *ctx)
     avio_skip(pb, 11);
 
     st->codecpar->codec_type = AVMEDIA_TYPE_AUDIO;
-    st->codecpar->channels = 1;
+    st->codecpar->ch_layout.nb_channels = 1;
     st->codecpar->sample_rate = sample_period ? 1000000000 / sample_period : 16000;
     st->duration = (avio_size(pb) - 21) / (127) * s->size / 4;
 
