3cd1ad5c9e205e352a4ff526050927c5ce87d092
Anton Khirnov <anton@khirnov.net>
2019-05-13 11:11:26 +0200

    avr: convert to new channel layout API
    Signed-off-by: James Almer <jamrial@gmail.com>

--- libavformat/avr.c
+++ libavformat/avr.c
@@ -53,9 +53,9 @@ static int avr_read_header(AVFormatContext *s)
 
     chan = avio_rb16(s->pb);
     if (!chan) {
-        st->codecpar->channels = 1;
+        st->codecpar->ch_layout.nb_channels = 1;
     } else if (chan == 0xFFFFu) {
-        st->codecpar->channels = 2;
+        st->codecpar->ch_layout.nb_channels = 2;
     } else {
         avpriv_request_sample(s, "chan %d", chan);
         return AVERROR_PATCHWELCOME;
@@ -81,7 +81,7 @@ static int avr_read_header(AVFormatContext *s)
         return AVERROR_PATCHWELCOME;
     }
 
-    st->codecpar->block_align = bps * st->codecpar->channels / 8;
+    st->codecpar->block_align = bps * st->codecpar->ch_layout.nb_channels / 8;
 
     avpriv_set_pts_info(st, 64, 1, st->codecpar->sample_rate);
     return 0;
