206f72d0f2a9a5423343ce2df6e03143f5a6cc48
Jun Zhao <barryjzhao@tencent.com>
2019-05-17 15:35:28 +0800

    libavdevice/gdigrab: fix ffmpeg -devices doesn't show gdigrab
    missed the category AV_CLASS_CATEGORY_DEVICE_VIDEO_INPUT lead to
    ffmpeg -devices doesn't show gdigrab as a input device
    
    FIx #7848
    
    Found-by: dangibson
    Reviewed-by: Paul B Mahol <onemda@gmail.com>
    Signed-off-by: Jun Zhao <barryjzhao@tencent.com>

--- libavdevice/gdigrab.c
+++ libavdevice/gdigrab.c
@@ -647,6 +647,7 @@ static const AVClass gdigrab_class = {
     .item_name  = av_default_item_name,
     .option     = options,
     .version    = LIBAVUTIL_VERSION_INT,
+    .category   = AV_CLASS_CATEGORY_DEVICE_VIDEO_INPUT,
 };
 
 /** gdi grabber device demuxer declaration */
