5a6fc87443e29fceff632c6abe5fb34ee905012e
Michael Niedermayer <michael@niedermayer.cc>
2019-05-29 01:12:39 +0200

    avcodec/gdv: Check remaining output after decode 5/6/8/
    Improves: Timeout (355sec -> 97sec)
    Improves: 14709/clusterfuzz-testcase-minimized-ffmpeg_AV_CODEC_ID_GDV_fuzzer-5704215281795072
    
    Reviewed-by: Paul B Mahol <onemda@gmail.com>
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg

--- libavcodec/gdv.c
+++ libavcodec/gdv.c
@@ -308,7 +308,7 @@ static int decompress_5(AVCodecContext *avctx, unsigned skip)
             int len;
             int b = bytestream2_get_byte(gb);
             if (b == 0) {
-                break;
+                return 0;
             }
             if (b != 0xFF) {
                 len = b;
@@ -323,6 +323,8 @@ static int decompress_5(AVCodecContext *avctx, unsigned skip)
             lz_copy(pb, g2, off, len);
         }
     }
+    if (bytestream2_get_bytes_left_p(pb) > 0)
+        return AVERROR_INVALIDDATA;
     return 0;
 }
 
@@ -444,6 +446,9 @@ static int decompress_68(AVCodecContext *avctx, unsigned skip, unsigned use8)
             lz_copy(pb, g2, off, len);
         }
     }
+
+    if (bytestream2_get_bytes_left_p(pb) > 0)
+        return AVERROR_INVALIDDATA;
 
     return 0;
 }
