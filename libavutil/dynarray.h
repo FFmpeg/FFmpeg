76ef18fd3910a5b479a3d9e584a0adc13f0d84ee
Steven Liu <lq@chinaffmpeg.org>
2019-05-30 22:27:28 +0800

    avutil/dynarry.h: fix comment grammar mistakes of FF_DYNARRAY_ADD
    Reviewed-by: James Almer <jamrial@gmail.com>
    Signed-off-by: Steven Liu <lq@chinaffmpeg.org>

--- libavutil/dynarray.h
+++ libavutil/dynarray.h
@@ -23,7 +23,7 @@
 #include "mem.h"
 
 /**
- * Add an element of to a dynamic array.
+ * Add an element to a dynamic array.
  *
  * The array is reallocated when its number of elements reaches powers of 2.
  * Therefore, the amortized cost of adding an element is constant.
