c2507e1946d345ca6e8cb63624c8509f7b63bd31
Michael Niedermayer <michael@niedermayer.cc>
2019-07-02 19:57:08 +0200

    avcodec/ivi: Ask for samples with odd tiles
    Fixes: Assertion failure
    Fixes: 15422/clusterfuzz-testcase-minimized-ffmpeg_AV_CODEC_ID_INDEO5_fuzzer-5676625481433088
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit a7e02cf3ad6f6eaae07fa68ecb93014e1dfd224e)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/ivi.c
+++ libavcodec/ivi.c
@@ -429,6 +429,10 @@ av_cold int ff_ivi_init_tiles(IVIPlaneDesc *planes,
         t_height = !p ? tile_height : (tile_height + 3) >> 2;
 
         if (!p && planes[0].num_bands == 4) {
+            if (t_width % 2 || t_height % 2) {
+                avpriv_request_sample(NULL, "Odd tiles");
+                return AVERROR_PATCHWELCOME;
+            }
             t_width  >>= 1;
             t_height >>= 1;
         }
