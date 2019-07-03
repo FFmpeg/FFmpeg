ec806c3da8fe7ff4fce7f0fc3c369dd974af3e79
Michael Niedermayer <michael@niedermayer.cc>
2019-07-03 23:25:07 +0200

    avcodec/huffyuv: remove gray8a (the format is listed but not supported by the implementation)
    Fixes: null pointer dereference
    Fixes: 15464/clusterfuzz-testcase-minimized-ffmpeg_AV_CODEC_ID_HYMT_fuzzer-5681391150301184
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    Reviewed-by: Paul B Mahol <onemda@gmail.com>
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit 6aaa01afe4fb774d0767684aa00f075b0ee5fca6)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/huffyuvdec.c
+++ libavcodec/huffyuvdec.c
@@ -418,9 +418,6 @@ static av_cold int decode_init(AVCodecContext *avctx)
         case 0x0F0:
             avctx->pix_fmt = AV_PIX_FMT_GRAY16;
             break;
-        case 0x170:
-            avctx->pix_fmt = AV_PIX_FMT_GRAY8A;
-            break;
         case 0x470:
             avctx->pix_fmt = AV_PIX_FMT_GBRP;
             break;
