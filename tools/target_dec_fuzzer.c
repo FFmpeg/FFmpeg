42a2edcc1d772d9785f58d49ad29dd0846f923cd
Michael Niedermayer <michael@niedermayer.cc>
2019-07-10 22:32:49 +0200

    tools/target_dec_fuzzer: fix memleak of extradata
    Fixes: memleak
    Fixes: 15535/clusterfuzz-testcase-minimized-ffmpeg_AV_CODEC_ID_SMACKER_fuzzer-5692162424963072
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- tools/target_dec_fuzzer.c
+++ tools/target_dec_fuzzer.c
@@ -201,7 +201,7 @@ int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
 
     int res = avcodec_open2(ctx, c, NULL);
     if (res < 0) {
-        av_free(ctx);
+        avcodec_free_context(&ctx);
         av_free(parser_avctx);
         av_parser_close(parser);
         return 0; // Failure of avcodec_open2() does not imply that a issue was found
