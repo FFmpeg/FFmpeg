077bf9058f162414340b427dd467b7b8739874cd
Paul B Mahol <onemda@gmail.com>
2019-07-14 17:50:37 +0200

    avfilter/vf_displace: use time_base from framesync
    Fixes non-monotonous timestamps.

--- libavfilter/vf_displace.c
+++ libavfilter/vf_displace.c
@@ -265,7 +265,7 @@ static int process_frame(FFFrameSync *fs)
 
         s->displace(s, in, xpic, ypic, out);
     }
-    out->pts = av_rescale_q(in->pts, s->fs.time_base, outlink->time_base);
+    out->pts = av_rescale_q(s->fs.pts, s->fs.time_base, outlink->time_base);
 
     return ff_filter_frame(outlink, out);
 }
@@ -332,7 +332,6 @@ static int config_output(AVFilterLink *outlink)
 
     outlink->w = srclink->w;
     outlink->h = srclink->h;
-    outlink->time_base = srclink->time_base;
     outlink->sample_aspect_ratio = srclink->sample_aspect_ratio;
     outlink->frame_rate = srclink->frame_rate;
 
@@ -356,7 +355,10 @@ static int config_output(AVFilterLink *outlink)
     s->fs.opaque   = s;
     s->fs.on_event = process_frame;
 
-    return ff_framesync_configure(&s->fs);
+    ret = ff_framesync_configure(&s->fs);
+    outlink->time_base = s->fs.time_base;
+
+    return ret;
 }
 
 static int activate(AVFilterContext *ctx)
