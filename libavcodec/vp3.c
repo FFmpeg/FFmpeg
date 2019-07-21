d1c2e5a36254b2f52d06572035fbf166019b85a2
Michael Niedermayer <michael@niedermayer.cc>
2019-07-22 00:41:06 +0200

    avcodec/vp3: Check that theora is theora
    Theora is forced to be non zero if it is zero and a sample
    is asked for, as suggested by reimar
    
    Fixes: Timeout (2min -> 600ms)
    Fixes: 15366/clusterfuzz-testcase-minimized-ffmpeg_AV_CODEC_ID_THEORA_fuzzer-5737849938247680
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit b4bf7226aff28e9ca379c5a3dedf745a2d316739)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/vp3.c
+++ libavcodec/vp3.c
@@ -2270,6 +2270,10 @@ static int theora_decode_header(AVCodecContext *avctx, GetBitContext *gb)
     s->theora_header = 0;
     s->theora = get_bits_long(gb, 24);
     av_log(avctx, AV_LOG_DEBUG, "Theora bitstream version %X\n", s->theora);
+    if (!s->theora) {
+        s->theora = 1;
+        avpriv_request_sample(s->avctx, "theora 0");
+    }
 
     /* 3.2.0 aka alpha3 has the same frame orientation as original vp3
      * but previous versions have the image flipped relative to vp3 */
