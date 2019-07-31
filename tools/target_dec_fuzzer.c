1e2e47e348111636ff1b06709985294fe0dff1af
Michael Niedermayer <michael@niedermayer.cc>
2019-07-31 10:34:21 +0200

    tools/target_dec_fuzzer: Print max_pixels and iterations at the end
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- tools/target_dec_fuzzer.c
+++ tools/target_dec_fuzzer.c
@@ -296,6 +296,8 @@ maximums_reached:
         decode_handler(ctx, frame, &got_frame, &avpkt);
     } while (got_frame == 1 && it++ < maxiteration);
 
+    fprintf(stderr, "pixels decoded: %"PRId64", iterations: %d\n", ec_pixels, it);
+
     av_frame_free(&frame);
     avcodec_free_context(&ctx);
     avcodec_free_context(&parser_avctx);
