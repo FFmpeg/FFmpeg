36292283b2dfaa45ebc99d66dca350e615ce920e
Michael Niedermayer <michael@niedermayer.cc>
2019-08-03 00:29:48 +0200

    avcodec/mss1: check for overread and forward errors
    Fixes: Timeout (106sec -> 14ms)
    Fixes: 15576/clusterfuzz-testcase-minimized-ffmpeg_AV_CODEC_ID_MSS1_fuzzer-5688080461201408
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    Reviewed-by: Paul B Mahol <onemda@gmail.com>
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit 43015afd7ce9055f1fa2d7648c3fcd9b7cfd7721)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/mss2.c
+++ libavcodec/mss2.c
@@ -152,6 +152,7 @@ static void arith2_init(ArithCoder *c, GetByteContext *gB)
     c->low           = 0;
     c->high          = 0xFFFFFF;
     c->value         = bytestream2_get_be24(gB);
+    c->overread      = 0;
     c->gbc.gB        = gB;
     c->get_model_sym = arith2_get_model_sym;
     c->get_number    = arith2_get_number;
