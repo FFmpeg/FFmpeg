90e965be6d90fcf729b020dba675b062b59ee17d
Andriy Gelman <andriy.gelman@gmail.com>
2019-08-08 10:37:10 -0400

    tools/zmqsend: Avoid mem copy past the end of input buffer
    This patch avoids a read past the end of the input buffer in memcpy since the size
    of the received zmq message is recv_buf_size - 1.
    
    Reviewed-by: Paul B Mahol <onemda@gmail.com>
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- tools/zmqsend.c
+++ tools/zmqsend.c
@@ -155,7 +155,7 @@ int main(int argc, char **argv)
         ret = 1;
         goto end;
     }
-    memcpy(recv_buf, zmq_msg_data(&msg), recv_buf_size);
+    memcpy(recv_buf, zmq_msg_data(&msg), recv_buf_size - 1);
     recv_buf[recv_buf_size-1] = 0;
     printf("%s\n", recv_buf);
     zmq_msg_close(&msg);
