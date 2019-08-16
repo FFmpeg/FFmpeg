561ba15c973120c9565a8f75a7439f3e8a10e04d
Błażej Szczygieł <spaz16@wp.pl>
2019-08-16 10:38:46 +0200

    lavf/tls_gnutls: check for interrupt inside handshake loop
    fixes #8080
    
    Signed-off-by: Błażej Szczygieł <spaz16@wp.pl>

--- libavformat/tls_gnutls.c
+++ libavformat/tls_gnutls.c
@@ -183,6 +183,11 @@ static int tls_open(URLContext *h, const char *uri, int flags, AVDictionary **op
     gnutls_transport_set_ptr(p->session, c->tcp);
     gnutls_priority_set_direct(p->session, "NORMAL", NULL);
     do {
+        if (ff_check_interrupt(&h->interrupt_callback)) {
+            ret = AVERROR_EXIT;
+            goto fail;
+        }
+
         ret = gnutls_handshake(p->session);
         if (gnutls_error_is_fatal(ret)) {
             ret = print_tls_error(h, ret);
