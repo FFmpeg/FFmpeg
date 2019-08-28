85993ceae3738c672b95e4414b202dbc34c19023
Michael Niedermayer <michael@niedermayer.cc>
2019-08-28 19:07:23 +0200

    avformat/hcom: Tell the compiler about set but not read variables
    This avoids 3 warnings
    
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavformat/hcom.c
+++ libavformat/hcom.c
@@ -38,7 +38,7 @@ static int hcom_probe(const AVProbeData *p)
 static int hcom_read_header(AVFormatContext *s)
 {
     AVStream *st;
-    unsigned data_size, rsrc_size, huffcount;
+    av_unused unsigned data_size, rsrc_size, huffcount;
     unsigned compresstype, divisor;
     unsigned dict_entries;
     int ret;
