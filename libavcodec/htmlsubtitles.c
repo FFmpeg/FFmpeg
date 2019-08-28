eeb50e42ba39672df2b098e073bbf9d4c0ea5818
Michael Niedermayer <michael@niedermayer.cc>
2019-08-28 21:56:35 +0200

    avcodec/htmlsubtitles: Avoid locale dependant isdigit()
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit b94cf549e2d9e456d77f8539baca0fffa805ba69)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/htmlsubtitles.c
+++ libavcodec/htmlsubtitles.c
@@ -55,7 +55,7 @@ static int scanbraces(const char* in) {
     if (strncmp(in, "{\\an", 4) != 0) {
         return 0;
     }
-    if (!isdigit(in[4])) {
+    if (!av_isdigit(in[4])) {
         return 0;
     }
     if (in[5] != '}') {
