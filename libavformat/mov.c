75cd59ec213c9282acd5271d0cbacc4505ae7a3b
Michael Niedermayer <michael@niedermayer.cc>
2019-08-31 01:25:03 +0200

    avformat/mov: Check for EOF in mov_read_meta()
    Fixes: Timeout (195sec -> 2ms)
    Fixes: 16735/clusterfuzz-testcase-minimized-ffmpeg_DEMUXER_fuzzer-5090676403863552
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit 093d1f42507e07d9acb43a8a3135e4ebe3530fe2)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavformat/mov.c
+++ libavformat/mov.c
@@ -4418,7 +4418,10 @@ static int mov_read_custom(MOVContext *c, AVIOContext *pb, MOVAtom atom)
 static int mov_read_meta(MOVContext *c, AVIOContext *pb, MOVAtom atom)
 {
     while (atom.size > 8) {
-        uint32_t tag = avio_rl32(pb);
+        uint32_t tag;
+        if (avio_feof(pb))
+            return AVERROR_EOF;
+        tag = avio_rl32(pb);
         atom.size -= 4;
         if (tag == MKTAG('h','d','l','r')) {
             avio_seek(pb, -8, SEEK_CUR);
