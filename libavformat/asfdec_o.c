33870a6aa7cd7c1a2308d678d97116c6285bfa96
Andreas Rheinhardt <andreas.rheinhardt@gmail.com>
2019-09-17 19:49:51 +0200

    avformat/asfdec_o: Don't reset twice
    A variable has been assigned a value twice consecutively; essentially
    the same happens when one performs av_init_packet on an AVPacket after
    a call to av_packet_unref.
    
    Found via PVS-Studio (see ticket #8156).
    
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@gmail.com>

--- libavformat/asfdec_o.c
+++ libavformat/asfdec_o.c
@@ -1135,9 +1135,7 @@ static void reset_packet(ASFPacket *asf_pkt)
     asf_pkt->duration  = 0;
     asf_pkt->flags     = 0;
     asf_pkt->dts       = 0;
-    asf_pkt->duration  = 0;
     av_packet_unref(&asf_pkt->avpkt);
-    av_init_packet(&asf_pkt->avpkt);
 }
 
 static int asf_read_replicated_data(AVFormatContext *s, ASFPacket *asf_pkt)
