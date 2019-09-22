05a2ce93265442285b2838530ba5bb5702cd18b1
Paul B Mahol <onemda@gmail.com>
2019-09-22 19:24:17 +0200

    avformat/y4m: do not try to seek if pts is less than 0
    Fixes #8193.

--- libavformat/yuv4mpegdec.c
+++ libavformat/yuv4mpegdec.c
@@ -326,6 +326,8 @@ static int yuv4_read_seek(AVFormatContext *s, int stream_index,
 
     if (flags & AVSEEK_FLAG_BACKWARD)
         pts = FFMAX(0, pts - 1);
+    if (pts < 0)
+        return -1;
     pos = pts * s->packet_size;
 
     if (avio_seek(s->pb, pos + s->internal->data_offset, SEEK_SET) < 0)
