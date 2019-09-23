648f5c93820ed204bdc477562b61055068c412fe
Timo Rothenpieler <timo@rothenpieler.org>
2019-09-23 15:47:27 +0200

    avcodec/nvenc: add driver version info for SDK 9.1
    Signed-off-by: Timo Rothenpieler <timo@rothenpieler.org>

--- libavcodec/nvenc.c
+++ libavcodec/nvenc.c
@@ -122,7 +122,15 @@ static int nvenc_print_error(void *log_ctx, NVENCSTATUS err,
 
 static void nvenc_print_driver_requirement(AVCodecContext *avctx, int level)
 {
-#if NVENCAPI_CHECK_VERSION(9, 0)
+#if NVENCAPI_CHECK_VERSION(9, 2)
+    const char *minver = "(unknown)";
+#elif NVENCAPI_CHECK_VERSION(9, 1)
+# if defined(_WIN32) || defined(__CYGWIN__)
+    const char *minver = "435.15";
+# else
+    const char *minver = "435.21";
+# endif
+#elif NVENCAPI_CHECK_VERSION(9, 0)
 # if defined(_WIN32) || defined(__CYGWIN__)
     const char *minver = "418.81";
 # else
