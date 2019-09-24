a77fb510c2684a43980c79ff85ff36e094cae63c
Limin Wang <lance.lmwang@gmail.com>
2019-09-24 19:23:58 +0800

    avutil/avstring: support input path as a null pointer or empty string
    Linux and OSX systems support basename and dirname via <libgen.h>, I plan to
    make the wrapper interface conform to the standard interface first.
    If it is feasible, I will continue to modify it to call the system interface
    if there is already a system call interface.
    
    You can get more description about the system interface by below command:
     "man 3 basename"
    
    Reviewed-by: Marton Balint <cus@passwd.hu>
    Reviewed-by: Tomas Härdin <tjoppen@acc.umu.se>
    Reviewed-by: Steven Liu <lq@chinaffmpeg.org>
    Signed-off-by: Limin Wang <lance.lmwang@gmail.com>

--- libavutil/avstring.c
+++ libavutil/avstring.c
@@ -257,8 +257,12 @@ char *av_strireplace(const char *str, const char *from, const char *to)
 
 const char *av_basename(const char *path)
 {
-    char *p = strrchr(path, '/');
+    char *p;
 
+    if (!path || *path == '\0')
+        return ".";
+
+    p = strrchr(path, '/');
 #if HAVE_DOS_PATHS
     char *q = strrchr(path, '\\');
     char *d = strchr(path, ':');
@@ -274,11 +278,11 @@ const char *av_basename(const char *path)
 
 const char *av_dirname(char *path)
 {
-    char *p = strrchr(path, '/');
+    char *p = path ? strrchr(path, '/') : NULL;
 
 #if HAVE_DOS_PATHS
-    char *q = strrchr(path, '\\');
-    char *d = strchr(path, ':');
+    char *q = path ? strrchr(path, '\\') : NULL;
+    char *d = path ? strchr(path, ':')  : NULL;
 
     d = d ? d + 1 : d;
 
