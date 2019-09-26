9e32c47102257b41d6585bf7ddac789eb1e640a2
Michael Niedermayer <michael@niedermayer.cc>
2019-09-26 22:00:37 +0200

    tools/target_dec_fuzzer: Adjust threshold for SCPR
    Fixes: Timeout (46sec -> 7sec)
    Fixes: 17644/clusterfuzz-testcase-minimized-ffmpeg_AV_CODEC_ID_SCPR_fuzzer-5715704283660288
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- tools/target_dec_fuzzer.c
+++ tools/target_dec_fuzzer.c
@@ -153,6 +153,7 @@ int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
     case AV_CODEC_ID_MOTIONPIXELS:maxpixels /= 256; break;
     case AV_CODEC_ID_MSS2:        maxpixels /= 16384; break;
     case AV_CODEC_ID_MSZH:        maxpixels /= 128; break;
+    case AV_CODEC_ID_SCPR:        maxpixels /= 32;    break;
     case AV_CODEC_ID_SNOW:        maxpixels /= 128; break;
     case AV_CODEC_ID_TRUEMOTION2: maxpixels /= 1024; break;
     case AV_CODEC_ID_VP7:         maxpixels /= 256;  break;
