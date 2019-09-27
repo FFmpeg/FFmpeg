04858650b1787c132c865530084855b36027b8a7
Lou Logan <lou@lrcd.com>
2019-09-27 15:17:15 -0800

    ffmpeg_opt: remove errant space
    Signed-off-by: Lou Logan <lou@lrcd.com>

--- fftools/ffmpeg_opt.c
+++ fftools/ffmpeg_opt.c
@@ -932,7 +932,7 @@ static void assert_file_overwrite(const char *filename)
     if (!file_overwrite) {
         if (proto_name && !strcmp(proto_name, "file") && avio_check(filename, 0) == 0) {
             if (stdin_interaction && !no_file_overwrite) {
-                fprintf(stderr,"File '%s' already exists. Overwrite ? [y/N] ", filename);
+                fprintf(stderr,"File '%s' already exists. Overwrite? [y/N] ", filename);
                 fflush(stderr);
                 term_exit();
                 signal(SIGINT, SIG_DFL);
