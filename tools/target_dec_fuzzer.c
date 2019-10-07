8caa7166676eaab89dd5cab5ab4ec506b00db70d
Michael Niedermayer <michael@niedermayer.cc>
2019-10-07 15:32:06 +0200

    tools/target_dec_fuzzer: Adjust threshold for CFHD
    Fixes: Timeout (18sec -> 5sec)
    Fixes: 17977/clusterfuzz-testcase-minimized-ffmpeg_AV_CODEC_ID_CFHD_fuzzer-5186112762413056
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- tools/target_dec_fuzzer.c
+++ tools/target_dec_fuzzer.c
@@ -138,6 +138,7 @@ int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
     switch (c->id) {
         // Allows a small input to generate gigantic output
     case AV_CODEC_ID_BINKVIDEO: maxpixels /= 32; break;
+    case AV_CODEC_ID_CFHD:      maxpixels /= 128; break;
     case AV_CODEC_ID_DIRAC:     maxpixels /= 8192; break;
     case AV_CODEC_ID_DXV:       maxpixels /= 32;  break;
     case AV_CODEC_ID_FFWAVESYNTH: maxsamples /= 16384; break;
