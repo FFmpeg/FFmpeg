223a2c2a6079a8a5a8bbaf4a7671f959a8dd8bc2
Andreas Rheinhardt <andreas.rheinhardt@gmail.com>
2019-10-11 06:06:58 +0200

    fftools/ffmpeg: Improve streamcopy
    do_streamcopy() has a packet that gets zero-initialized first, then gets
    initialized via av_init_packet() after which some of its fields are
    oerwritten again with the actually desired values (unless it's EOF): The
    side data is copied into the packet with av_copy_packet_side_data() and
    if the source packet is refcounted, the packet will get a new reference
    to the source packet's data. Furthermore, the flags are copied and the
    timestamp related fields are overwritten with new values.
    
    This commit replaces this by using av_packet_ref() to both initialize
    the packet as well as populate its fields with the right values (unless
    it's EOF again in which case the packet will still be initialized). The
    differences to the current approach are as follows:
    a) There is no call to a deprecated function (av_copy_packet_side_data())
    any more.
    b) Several fields that weren't copied before are now copied from the source
    packet to the new packet (e.g. pos). Some of them (the timestamp related
    fields) may be immediately overwritten again and some don't seem to be
    used at all (e.g. pos), but in return using av_packet_ref() allows to forgo
    the initializations.
    c) There was no check for whether copying side data fails or not. This
    has been changed: Now the program is exited in this case.
    
    Using av_packet_ref() does not lead to unnecessary copying of data,
    because the source packets are already always refcounted (they originate
    from av_read_frame()).
    
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@gmail.com>
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- fftools/ffmpeg.c
+++ fftools/ffmpeg.c
@@ -1995,12 +1995,13 @@ static void do_streamcopy(InputStream *ist, OutputStream *ost, const AVPacket *p
     InputFile   *f = input_files [ist->file_index];
     int64_t start_time = (of->start_time == AV_NOPTS_VALUE) ? 0 : of->start_time;
     int64_t ost_tb_start_time = av_rescale_q(start_time, AV_TIME_BASE_Q, ost->mux_timebase);
-    AVPacket opkt = { 0 };
-
-    av_init_packet(&opkt);
+    AVPacket opkt;
 
     // EOF: flush output bitstream filters.
     if (!pkt) {
+        av_init_packet(&opkt);
+        opkt.data = NULL;
+        opkt.size = 0;
         output_packet(of, &opkt, ost, 1);
         return;
     }
@@ -2039,10 +2040,11 @@ static void do_streamcopy(InputStream *ist, OutputStream *ost, const AVPacket *p
     if (ost->enc_ctx->codec_type == AVMEDIA_TYPE_VIDEO)
         ost->sync_opts++;
 
+    if (av_packet_ref(&opkt, pkt) < 0)
+        exit_program(1);
+
     if (pkt->pts != AV_NOPTS_VALUE)
         opkt.pts = av_rescale_q(pkt->pts, ist->st->time_base, ost->mux_timebase) - ost_tb_start_time;
-    else
-        opkt.pts = AV_NOPTS_VALUE;
 
     if (pkt->dts == AV_NOPTS_VALUE)
         opkt.dts = av_rescale_q(ist->dts, AV_TIME_BASE_Q, ost->mux_timebase);
@@ -2060,18 +2062,6 @@ static void do_streamcopy(InputStream *ist, OutputStream *ost, const AVPacket *p
     }
 
     opkt.duration = av_rescale_q(pkt->duration, ist->st->time_base, ost->mux_timebase);
-
-    opkt.flags    = pkt->flags;
-
-    if (pkt->buf) {
-        opkt.buf = av_buffer_ref(pkt->buf);
-        if (!opkt.buf)
-            exit_program(1);
-    }
-    opkt.data = pkt->data;
-    opkt.size = pkt->size;
-
-    av_copy_packet_side_data(&opkt, pkt);
 
     output_packet(of, &opkt, ost, 0);
 }
