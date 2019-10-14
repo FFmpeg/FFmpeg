996ff3fe86b0a7a0d2b8bc5a2570cb1271eb9474
Paul B Mahol <onemda@gmail.com>
2019-10-14 10:52:27 +0200

    avfilter/vf_edgedetect: fix coverity issue

--- libavfilter/vf_edgedetect.c
+++ libavfilter/vf_edgedetect.c
@@ -150,8 +150,9 @@ static void gaussian_blur(AVFilterContext *ctx, int w, int h,
     int i, j;
 
     memcpy(dst, src, w); dst += dst_linesize; src += src_linesize;
-    if (h > 1)
+    if (h > 1) {
         memcpy(dst, src, w); dst += dst_linesize; src += src_linesize;
+    }
     for (j = 2; j < h - 2; j++) {
         dst[0] = src[0];
         dst[1] = src[1];
@@ -181,8 +182,9 @@ static void gaussian_blur(AVFilterContext *ctx, int w, int h,
         dst += dst_linesize;
         src += src_linesize;
     }
-    if (h > 2)
+    if (h > 2) {
         memcpy(dst, src, w); dst += dst_linesize; src += src_linesize;
+    }
     if (h > 3)
         memcpy(dst, src, w);
 }
