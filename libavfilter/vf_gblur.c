1e35519fe0b8bbad84641e83d49138152720b544
Paul B Mahol <onemda@gmail.com>
2019-10-16 19:21:00 +0200

    avfilter/vf_gblur: fix undefined behaviour
    Fixes #8292

--- libavfilter/vf_gblur.c
+++ libavfilter/vf_gblur.c
@@ -157,6 +157,7 @@ static int filter_postscale(AVFilterContext *ctx, void *arg, int jobnr, int nb_j
 {
     GBlurContext *s = ctx->priv;
     ThreadData *td = arg;
+    const float max = (1 << s->depth) - 1;
     const int height = td->height;
     const int width = td->width;
     const int64_t numpixels = width * (int64_t)height;
@@ -166,8 +167,10 @@ static int filter_postscale(AVFilterContext *ctx, void *arg, int jobnr, int nb_j
     float *buffer = s->buffer;
     unsigned i;
 
-    for (i = slice_start; i < slice_end; i++)
+    for (i = slice_start; i < slice_end; i++) {
         buffer[i] *= postscale;
+        buffer[i] = av_clipf(buffer[i], 0.f, max);
+    }
 
     return 0;
 }
