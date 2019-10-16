932913f0b406814d9c374c5e1a5ab6aa45a53168
Paul B Mahol <onemda@gmail.com>
2019-10-16 18:06:47 +0200

    avfilter/vf_edgedetect: fix undefined behaviour
    Fixes #8287

--- libavfilter/vf_edgedetect.c
+++ libavfilter/vf_edgedetect.c
@@ -216,7 +216,7 @@ static int get_rounded_direction(int gx, int gy)
 
         if (gx < 0)
             gx = -gx, gy = -gy;
-        gy <<= 16;
+        gy *= (1 << 16);
         tanpi8gx  =  27146 * gx;
         tan3pi8gx = 158218 * gx;
         if (gy > -tan3pi8gx && gy < -tanpi8gx)  return DIRECTION_45UP;
