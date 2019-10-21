aa26f83bdd7ea1f5850520fc25aae38315f6338b
Paul B Mahol <onemda@gmail.com>
2019-10-21 16:03:22 +0200

    avfilter/af_join: fix possible memory leaks
    Allocation of input frames is independent from
    allocation of new input pads.

--- libavfilter/af_join.c
+++ libavfilter/af_join.c
@@ -207,9 +207,12 @@ static av_cold void join_uninit(AVFilterContext *ctx)
     JoinContext *s = ctx->priv;
     int i;
 
+    for (i = 0; i < s->inputs && s->input_frames; i++) {
+        av_frame_free(&s->input_frames[i]);
+    }
+
     for (i = 0; i < ctx->nb_inputs; i++) {
         av_freep(&ctx->input_pads[i].name);
-        av_frame_free(&s->input_frames[i]);
     }
 
     av_freep(&s->channels);
