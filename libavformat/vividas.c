faea5b4462c4325b4ec7c150c3c31929429773cc
Michael Niedermayer <michael@niedermayer.cc>
2019-10-29 22:17:45 +0100

    avformat/vividas: Add EOF check in val_1 loop in track_header()
    Fixes: Timeout (148sec -> 0.1sec)
    Fixes: 18427/clusterfuzz-testcase-minimized-ffmpeg_DEMUXER_fuzzer-5682124627116032
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavformat/vividas.c
+++ libavformat/vividas.c
@@ -295,6 +295,8 @@ static int track_header(VividasDemuxContext *viv, AVFormatContext *s,  uint8_t *
     for (i=0;i<val_1;i++) {
         int c = avio_r8(pb);
         for (j=0;j<c;j++) {
+            if (avio_feof(pb))
+                return AVERROR_EOF;
             avio_r8(pb); // val_3
             avio_r8(pb); // val_4
         }
