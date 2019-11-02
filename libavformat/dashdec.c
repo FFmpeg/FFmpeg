a23c8d75ec34ea6dbc3c88e88f0ec6b6d6cbe91d
Jun Zhao <barryjzhao@tencent.com>
2019-11-02 22:46:00 +0800

    lavf/dashenc: enable probesize/max_analyze_duration setting in sub-demuxer
    Enable probesize/max_analyze_duration setting when open the sub-demuxer,
    it's will be used to minimizing the initial delay.
    
    Signed-off-by: Jun Zhao <barryjzhao@tencent.com>

--- libavformat/dashdec.c
+++ libavformat/dashdec.c
@@ -1935,8 +1935,8 @@ static int reopen_demux_for_component(AVFormatContext *s, struct representation 
         goto fail;
 
     pls->ctx->flags = AVFMT_FLAG_CUSTOM_IO;
-    pls->ctx->probesize = 1024 * 4;
-    pls->ctx->max_analyze_duration = 4 * AV_TIME_BASE;
+    pls->ctx->probesize = s->probesize > 0 ? s->probesize : 1024 * 4;;
+    pls->ctx->max_analyze_duration = s->max_analyze_duration > 0 ? s->max_analyze_duration : 4 * AV_TIME_BASE;
     ret = av_probe_input_buffer(&pls->pb, &in_fmt, "", NULL, 0, 0);
     if (ret < 0) {
         av_log(s, AV_LOG_ERROR, "Error when loading first fragment, playlist %d\n", (int)pls->rep_idx);
