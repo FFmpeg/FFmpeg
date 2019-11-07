b1af78f5269d36226ecba3591e3abe95efcfdbdf
Michael Niedermayer <michael@niedermayer.cc>
2019-11-07 21:16:32 +0100

    avformat/mp3dec: Check that the frame fits within the probe buffer
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit e9a335150a62bb377a26ce096187b4476145d02b)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavformat/mp3dec.c
+++ libavformat/mp3dec.c
@@ -89,7 +89,7 @@ static int mp3_read_probe(AVProbeData *p)
 
             header = AV_RB32(buf2);
             ret = avpriv_mpegaudio_decode_header(&h, header);
-            if (ret != 0)
+            if (ret != 0 || end - buf2 < h.frame_size)
                 break;
             buf2 += h.frame_size;
         }
