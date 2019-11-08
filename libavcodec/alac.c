7686ba1f149a94c3bac235589de8aa8db92be4e5
Michael Niedermayer <michael@niedermayer.cc>
2019-11-08 20:40:46 +0100

    avcodec/alac: Fix integer overflow in lpc_prediction() with sign
    Fixes: signed integer overflow: -2147483648 * -1 cannot be represented in type 'int'
    Fixes: 18643/clusterfuzz-testcase-minimized-ffmpeg_AV_CODEC_ID_ALAC_fuzzer-5672182449700864
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/alac.c
+++ libavcodec/alac.c
@@ -227,7 +227,7 @@ static void lpc_prediction(int32_t *error_buffer, uint32_t *buffer_out,
                 val  = d - pred[j];
                 sign = sign_only(val) * error_sign;
                 lpc_coefs[j] -= sign;
-                val *= sign;
+                val *= (unsigned)sign;
                 error_val -= (val >> lpc_quant) * (j + 1);
             }
         }
