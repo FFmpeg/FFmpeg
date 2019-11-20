b6571777d28d59308e622883c9fe7d0765e406a8
Alex Mogurenko <alex@mogurenko.com>
2019-11-20 13:22:46 +0200

    avcodec/mjpegbdec: Fix yuv444 pix_fmt detection
    by default adobe_transform set to 0 and because of that mjpegb decoder detects yuv444 pix fmt as bgrp

--- libavcodec/mjpegbdec.c
+++ libavcodec/mjpegbdec.c
@@ -56,6 +56,7 @@ static int mjpegb_decode_frame(AVCodecContext *avctx,
     buf_ptr = buf;
     buf_end = buf + buf_size;
     s->got_picture = 0;
+    s->adobe_transform = -1;
 
 read_header:
     /* reset on every SOI */
