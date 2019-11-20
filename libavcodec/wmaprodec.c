b37e5eb31f93d827f266db4d4c2dcbed9a1ba3b5
Michael Niedermayer <michael@niedermayer.cc>
2019-11-20 20:34:55 +0100

    avcodec/wmaprodec: Fixes integer overflow with 32bit samples
    Fixes: left shift of 1 by 31 places cannot be represented in type 'int'
    Fixes: 18860/clusterfuzz-testcase-minimized-ffmpeg_AV_CODEC_ID_WMAPRO_fuzzer-5755223125786624
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit a9cc69c0d59057ea172a107e0308fdf5fd8fc04e)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/wmaprodec.c
+++ libavcodec/wmaprodec.c
@@ -523,7 +523,7 @@ static av_cold int decode_init(AVCodecContext *avctx)
     for (i = 0; i < WMAPRO_BLOCK_SIZES; i++)
         ff_mdct_init(&s->mdct_ctx[i], WMAPRO_BLOCK_MIN_BITS+1+i, 1,
                      1.0 / (1 << (WMAPRO_BLOCK_MIN_BITS + i - 1))
-                     / (1 << (s->bits_per_sample - 1)));
+                     / (1ll << (s->bits_per_sample - 1)));
 
     /** init MDCT windows: simple sine window */
     for (i = 0; i < WMAPRO_BLOCK_SIZES; i++) {
