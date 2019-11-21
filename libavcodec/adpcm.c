d8e5d0264eea4c9b9e3d7e36e0053ab14e33af4f
Michael Niedermayer <michael@niedermayer.cc>
2019-11-21 23:02:56 +0100

    avcodec/adpcm: Clip predictor for APC
    Fixes: signed integer overflow: -2147483648 - 13 cannot be represented in type 'int'
    Fixes: 18893/clusterfuzz-testcase-minimized-ffmpeg_AV_CODEC_ID_ADPCM_IMA_APC_fuzzer-5630760442920960
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit 9fe07908c3f67d59cf4db5668d61b34506189590)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/adpcm.c
+++ libavcodec/adpcm.c
@@ -139,8 +139,8 @@ static av_cold int adpcm_decode_init(AVCodecContext * avctx)
         break;
     case AV_CODEC_ID_ADPCM_IMA_APC:
         if (avctx->extradata && avctx->extradata_size >= 8) {
-            c->status[0].predictor = AV_RL32(avctx->extradata);
-            c->status[1].predictor = AV_RL32(avctx->extradata + 4);
+            c->status[0].predictor = av_clip_intp2(AV_RL32(avctx->extradata    ), 18);
+            c->status[1].predictor = av_clip_intp2(AV_RL32(avctx->extradata + 4), 18);
         }
         break;
     case AV_CODEC_ID_ADPCM_IMA_WS:
