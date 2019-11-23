5a8877da35bc8abcb1aa6ce702bb6c4437e2460c
Michael Niedermayer <michael@niedermayer.cc>
2019-11-23 16:36:02 +0100

    tools/target_dec_fuzzer: Adjust threshold for dst
    Fixes: Timeout (400sec -> 14sec)
    Fixes: 18989/clusterfuzz-testcase-minimized-ffmpeg_AV_CODEC_ID_DST_fuzzer-5175008116867072
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- tools/target_dec_fuzzer.c
+++ tools/target_dec_fuzzer.c
@@ -140,6 +140,7 @@ int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
     case AV_CODEC_ID_BINKVIDEO: maxpixels /= 32; break;
     case AV_CODEC_ID_CFHD:      maxpixels /= 128; break;
     case AV_CODEC_ID_DIRAC:     maxpixels /= 8192; break;
+    case AV_CODEC_ID_DST:       maxsamples /= 8192; break;
     case AV_CODEC_ID_DXV:       maxpixels /= 32;  break;
     case AV_CODEC_ID_FFWAVESYNTH: maxsamples /= 16384; break;
     case AV_CODEC_ID_MSRLE:     maxpixels /= 16;  break;
