df912bbc5f9cffa56341382a92aff739755f6d60
Andreas Rheinhardt <andreas.rheinhardt@gmail.com>
2019-11-27 08:48:56 +0100

    avformat/flacenc: Add const to ff_flac_write_header() parameter
    The extradata is not changed at all.
    
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@gmail.com>
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavformat/flacenc.h
+++ libavformat/flacenc.h
@@ -26,7 +26,7 @@
 #include "libavcodec/bytestream.h"
 #include "avformat.h"
 
-int ff_flac_write_header(AVIOContext *pb, uint8_t *extradata,
+int ff_flac_write_header(AVIOContext *pb, const uint8_t *extradata,
                          int extradata_size, int last_block);
 
 int ff_flac_is_native_layout(uint64_t channel_layout);
