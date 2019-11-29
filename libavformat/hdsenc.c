e880f4fb387b00a29985dd0041b94e82358df219
Steven Liu <lq@chinaffmpeg.org>
2019-11-29 13:15:58 +0800

    avformat/hdsenc: removed unused check of avformat_free_context
    Reviewed-by: Michael Niedermayer <michael@niedermayer.cc>
    Reviewed-by: Jun Zhao <barryjzhao@tencent.com>
    Signed-off-by: Steven Liu <lq@chinaffmpeg.org>

--- libavformat/hdsenc.c
+++ libavformat/hdsenc.c
@@ -146,8 +146,7 @@ static void hds_free(AVFormatContext *s)
             av_write_trailer(os->ctx);
         if (os->ctx)
             avio_context_free(&os->ctx->pb);
-        if (os->ctx)
-            avformat_free_context(os->ctx);
+        avformat_free_context(os->ctx);
         av_freep(&os->metadata);
         for (j = 0; j < os->nb_extra_packets; j++)
             av_freep(&os->extra_packets[j]);
