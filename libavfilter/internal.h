02daafb45c5e638629a4104be6c09b824d55786a
Nicolas George <george@nsup.org>
2019-12-02 16:46:11 +0100

    lavfi: remove AVFilterPad.poll_frame().
    This design is no longer used and was replaced a long time ago.

--- libavfilter/internal.h
+++ libavfilter/internal.h
@@ -93,17 +93,6 @@ struct AVFilterPad {
     int (*filter_frame)(AVFilterLink *link, AVFrame *frame);
 
     /**
-     * Frame poll callback. This returns the number of immediately available
-     * samples. It should return a positive value if the next request_frame()
-     * is guaranteed to return one frame (with no delay).
-     *
-     * Defaults to just calling the source poll_frame() method.
-     *
-     * Output pads only.
-     */
-    int (*poll_frame)(AVFilterLink *link);
-
-    /**
      * Frame request callback. A call to this should result in some progress
      * towards producing output over the given link. This should return zero
      * on success, and another value on error.
