e1d836d2375c93cbc44a2b0d34e404682c1e8436
Michael Niedermayer <michael@niedermayer.cc>
2019-12-03 21:33:18 +0100

    avcodec/atrac9dec: Check q_unit_cnt more completely before using it to access at9_tab_band_ext_group
    Fixes: index 8 out of bounds for type 'const uint8_t [8][3]'
    Fixes: 19127/clusterfuzz-testcase-minimized-ffmpeg_AV_CODEC_ID_ATRAC9_fuzzer-5709394985091072
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    Reviewed-by: Lynne <dev@lynne.ee>
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/atrac9dec.c
+++ libavcodec/atrac9dec.c
@@ -199,7 +199,7 @@ static inline int parse_band_ext(ATRAC9Context *s, ATRAC9BlockData *b,
     int ext_band = 0;
 
     if (b->has_band_ext) {
-        if (b->q_unit_cnt < 13)
+        if (b->q_unit_cnt < 13 || b->q_unit_cnt > 20)
             return AVERROR_INVALIDDATA;
         ext_band = at9_tab_band_ext_group[b->q_unit_cnt - 13][2];
         if (stereo) {
