4110029e562c3a1f9b1e8d1d2c890d21981cab03
Steven Liu <lq@chinaffmpeg.org>
2019-12-04 17:17:18 +0800

    avformat/cache: rename the class name fro Cache to cache
    liuqideMacBook-Pro:build liuqi$ ffmpeg --help full | grep cache
    cannot find cache protocol options.
    
    after patch:
    bogon:dash liuqi$ ./ffmpeg --help full | grep cache
    cache AVOptions:
    can find the cache AVOptions after patch.
    
    Signed-off-by: Steven Liu <lq@chinaffmpeg.org>

--- libavformat/cache.c
+++ libavformat/cache.c
@@ -326,7 +326,7 @@ static const AVOption options[] = {
 };
 
 static const AVClass cache_context_class = {
-    .class_name = "Cache",
+    .class_name = "cache",
     .item_name  = av_default_item_name,
     .option     = options,
     .version    = LIBAVUTIL_VERSION_INT,
