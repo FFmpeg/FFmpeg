3b4eae4cae3123ddefad6899ec01425fa65ec802
Andreas Rheinhardt <andreas.rheinhardt@gmail.com>
2019-12-09 23:26:03 +0100

    avcodec/cbs_h2645: Treat slices without data as invalid
    Slices that end after their header (meaning slices after the header
    without any data before the rbsp_stop_one_bit or possibly without any
    rbsp_stop_one_bit at all) are invalid and are now dropped. This ensures
    that one doesn't run into two asserts in cbs_h2645_write_slice_data().
    
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@gmail.com>
    Fixes: 19629/clusterfuzz-testcase-minimized-ffmpeg_BSF_H264_METADATA_fuzzer-5676822528524288
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit 66fac1ff7ccbc4fb6a3aa6b9a95de7b6bc31d14e)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/cbs_h2645.c
+++ libavcodec/cbs_h2645.c
@@ -768,6 +768,9 @@ static int cbs_h264_read_nal_unit(CodedBitstreamContext *ctx,
             if (err < 0)
                 return err;
 
+            if (!cbs_h2645_read_more_rbsp_data(&gbc))
+                return AVERROR_INVALIDDATA;
+
             pos = get_bits_count(&gbc);
             len = unit->data_size;
 
@@ -930,6 +933,9 @@ static int cbs_h265_read_nal_unit(CodedBitstreamContext *ctx,
             err = cbs_h265_read_slice_segment_header(ctx, &gbc, &slice->header);
             if (err < 0)
                 return err;
+
+            if (!cbs_h2645_read_more_rbsp_data(&gbc))
+                return AVERROR_INVALIDDATA;
 
             pos = get_bits_count(&gbc);
             len = unit->data_size;
