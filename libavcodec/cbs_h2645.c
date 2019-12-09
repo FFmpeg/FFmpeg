e9b438371232cd127717530e67421d86fc50f249
Andreas Rheinhardt <andreas.rheinhardt@gmail.com>
2019-12-09 23:26:02 +0100

    avcodec/cbs_h2645: Remove dead code to delete trailing zeroes
    Trailing zeroes are already discarded when splitting a fragment, which
    makes the code to remove them when decomposing slices dead code.
    
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@gmail.com>
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit 8f701932b39a6345f2a8bab85f48d555a98802e3)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/cbs_h2645.c
+++ libavcodec/cbs_h2645.c
@@ -770,13 +770,6 @@ static int cbs_h264_read_nal_unit(CodedBitstreamContext *ctx,
 
             pos = get_bits_count(&gbc);
             len = unit->data_size;
-            if (!unit->data[len - 1]) {
-                int z;
-                for (z = 0; z < len && !unit->data[len - z - 1]; z++);
-                av_log(ctx->log_ctx, AV_LOG_DEBUG, "Deleted %d trailing zeroes "
-                       "from slice data.\n", z);
-                len -= z;
-            }
 
             slice->data_size = len - pos / 8;
             slice->data_ref  = av_buffer_alloc(slice->data_size +
@@ -940,13 +933,6 @@ static int cbs_h265_read_nal_unit(CodedBitstreamContext *ctx,
 
             pos = get_bits_count(&gbc);
             len = unit->data_size;
-            if (!unit->data[len - 1]) {
-                int z;
-                for (z = 0; z < len && !unit->data[len - z - 1]; z++);
-                av_log(ctx->log_ctx, AV_LOG_DEBUG, "Deleted %d trailing zeroes "
-                       "from slice data.\n", z);
-                len -= z;
-            }
 
             slice->data_size = len - pos / 8;
             slice->data_ref  = av_buffer_alloc(slice->data_size +
