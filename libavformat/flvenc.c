a6d292b954cb45b512251b31efbe58e44dcf159c
Andreas Rheinhardt <andreas.rheinhardt@gmail.com>
2019-12-10 02:41:44 +0100

    avformat/flvenc: Don't reimplement ff_alloc_extradata
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@gmail.com>
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavformat/flvenc.c
+++ libavformat/flvenc.c
@@ -908,14 +908,10 @@ static int flv_write_packet(AVFormatContext *s, AVPacket *pkt)
         int side_size = 0;
         uint8_t *side = av_packet_get_side_data(pkt, AV_PKT_DATA_NEW_EXTRADATA, &side_size);
         if (side && side_size > 0 && (side_size != par->extradata_size || memcmp(side, par->extradata, side_size))) {
-            av_free(par->extradata);
-            par->extradata = av_mallocz(side_size + AV_INPUT_BUFFER_PADDING_SIZE);
-            if (!par->extradata) {
-                par->extradata_size = 0;
-                return AVERROR(ENOMEM);
-            }
+            ret = ff_alloc_extradata(par, side_size);
+            if (ret < 0)
+                return ret;
             memcpy(par->extradata, side, side_size);
-            par->extradata_size = side_size;
             flv_write_codec_header(s, par, pkt->dts);
         }
     }
