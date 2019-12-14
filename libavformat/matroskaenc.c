7039045c56e8747e5827dc32236d8c88691ecc46
Andreas Rheinhardt <andreas.rheinhardt@gmail.com>
2019-12-14 02:47:13 +0100

    avformat/matroskaenc: Update the default version of WavPack
    The Matroska muxer currently assumed WavPack version 4.03 in case it was
    not explicitly signalled via extradata; but following a recommendation
    from David Bryant, the WavPack creator, this is changed to 4.10.
    
    Reviewed-by: David Bryant <david@wavpack.com>
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@gmail.com>

--- libavformat/matroskaenc.c
+++ libavformat/matroskaenc.c
@@ -589,7 +589,7 @@ static int put_wv_codecpriv(AVIOContext *pb, AVCodecParameters *par)
     if (par->extradata && par->extradata_size == 2)
         avio_write(pb, par->extradata, 2);
     else
-        avio_wl16(pb, 0x403); // fallback to the version mentioned in matroska specs
+        avio_wl16(pb, 0x410); // fallback to the most recent version
     return 0;
 }
 
