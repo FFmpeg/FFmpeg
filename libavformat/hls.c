c3bb6efef285adb8ca06278df15473f3c4d5aec5
Marvin Scholz <epirat07@gmail.com>
2019-12-17 01:17:42 +0100

    avformat/hls: propagate icy to child AVIOContexts
    When the user decides they do not want to to send the Icy-MetaData
    header, this should be respected for all requests, not just the first
    one.
    
    Fix #5578
    
    Reviewed-by: Liu Steven <lq@chinaffmpeg.org>
    Signed-off-by: James Almer <jamrial@gmail.com>

--- libavformat/hls.c
+++ libavformat/hls.c
@@ -1663,7 +1663,7 @@ static int save_avio_options(AVFormatContext *s)
 {
     HLSContext *c = s->priv_data;
     static const char * const opts[] = {
-        "headers", "http_proxy", "user_agent", "cookies", "referer", "rw_timeout", NULL };
+        "headers", "http_proxy", "user_agent", "cookies", "referer", "rw_timeout", "icy", NULL };
     const char * const * opt = opts;
     uint8_t *buf;
     int ret = 0;
