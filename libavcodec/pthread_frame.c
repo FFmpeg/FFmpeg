091341f2ab5bd35ca1a2aae90503adc74f8d3523
Linjie Fu <linjie.fu@intel.com>
2019-12-27 16:47:35 +0800

    lavc/pthread_frame: Update user context in ff_frame_thread_free
    Resolution/format changes lead to re-initialization of hardware
    accelerations(vaapi/dxva2/..) with new hwaccel_priv_data in
    the worker-thread. But hwaccel_priv_data in user context won't
    be updated until the resolution changing frame is output.
    
    A termination with "-vframes" just after the reinit will lead to:
        1. memory leak in worker-thread.
        2. double free in user-thread.
    
    Update user context in ff_frame_thread_free with the last thread
    submit_packet() was called on.
    
    To reproduce:
    ffmpeg -hwaccel vaapi(dxva2) -v verbose -i
        fate-suite/h264/reinit-large_420_8-to-small_420_8.h264 -pix_fmt nv12
        -f rawvideo -vsync passthrough -vframes 47 -y out.yuv
    
    Signed-off-by: Linjie Fu <linjie.fu@intel.com>
    Signed-off-by: Anton Khirnov <anton@khirnov.net>

--- libavcodec/pthread_frame.c
+++ libavcodec/pthread_frame.c
@@ -658,6 +658,13 @@ void ff_frame_thread_free(AVCodecContext *avctx, int thread_count)
 
     park_frame_worker_threads(fctx, thread_count);
 
+    if (fctx->prev_thread && avctx->internal->hwaccel_priv_data !=
+                             fctx->prev_thread->avctx->internal->hwaccel_priv_data) {
+        if (update_context_from_thread(avctx, fctx->prev_thread->avctx, 1) < 0) {
+            av_log(avctx, AV_LOG_ERROR, "Failed to update user thread.\n");
+        }
+    }
+
     if (fctx->prev_thread && fctx->prev_thread != fctx->threads)
         if (update_context_from_thread(fctx->threads->avctx, fctx->prev_thread->avctx, 0) < 0) {
             av_log(avctx, AV_LOG_ERROR, "Final thread update failed\n");
