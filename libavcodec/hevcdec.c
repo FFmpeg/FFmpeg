669428ac5d9b39f718e7d46f52410722dd20b94a
Linjie Fu <linjie.fu@intel.com>
2020-01-15 15:02:21 +0800

    lavc/hevcdec: add 4:2:2 8-bit/10-bit VAAPI decode support
    Add decode support for 4:2:2 8-bt and 10-bit HEVC Range Extension clips.
    
    Signed-off-by: Linjie Fu <linjie.fu@intel.com>

--- libavcodec/hevcdec.c
+++ libavcodec/hevcdec.c
@@ -427,6 +427,12 @@ static enum AVPixelFormat get_format(HEVCContext *s, const HEVCSPS *sps)
         *fmt++ = AV_PIX_FMT_CUDA;
 #endif
         break;
+    case AV_PIX_FMT_YUV422P:
+    case AV_PIX_FMT_YUV422P10LE:
+#if CONFIG_HEVC_VAAPI_HWACCEL
+       *fmt++ = AV_PIX_FMT_VAAPI;
+#endif
+        break;
     case AV_PIX_FMT_YUV420P12:
     case AV_PIX_FMT_YUV444P10:
     case AV_PIX_FMT_YUV444P12:
