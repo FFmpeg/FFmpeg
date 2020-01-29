86eef55878ffab43606613ffc789129c9478ea3e
Anton Khirnov <anton@khirnov.net>
2020-01-29 17:25:51 +0100

    nspdec: convert to new channel layout API
    Signed-off-by: James Almer <jamrial@gmail.com>

--- libavformat/nspdec.c
+++ libavformat/nspdec.c
@@ -88,7 +88,7 @@ static int nsp_read_header(AVFormatContext *s)
     }
 
     st->codecpar->codec_type  = AVMEDIA_TYPE_AUDIO;
-    st->codecpar->channels    = channels;
+    st->codecpar->ch_layout.nb_channels = channels;
     st->codecpar->sample_rate = rate;
     st->codecpar->codec_id    = AV_CODEC_ID_PCM_S16LE;
     st->codecpar->block_align = 2 * channels;
