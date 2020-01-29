8950839aaca748ccbc463615a3515ca83c0ada65
Anton Khirnov <anton@khirnov.net>
2020-01-29 17:17:24 +0100

    ifv: convert to new channel layout API
    Signed-off-by: James Almer <jamrial@gmail.com>

--- libavformat/ifv.c
+++ libavformat/ifv.c
@@ -155,8 +155,7 @@ static int ifv_read_header(AVFormatContext *s)
 
         st->codecpar->codec_type = AVMEDIA_TYPE_AUDIO;
         st->codecpar->codec_id = AV_CODEC_ID_PCM_S16LE;
-        st->codecpar->channels = 1;
-        st->codecpar->channel_layout = AV_CH_LAYOUT_MONO;
+        st->codecpar->ch_layout = (AVChannelLayout)AV_CHANNEL_LAYOUT_MONO;
         st->codecpar->sample_rate = ifv->sample_rate;
         ifv->audio_stream_index = st->index;
 
