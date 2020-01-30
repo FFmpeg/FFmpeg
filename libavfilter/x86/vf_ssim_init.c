fcc0424c933742c8fc852371e985d16b6eb4bfe9
Paul B Mahol <onemda@gmail.com>
2020-01-30 22:01:23 +0100

    avfilter/vf_ssim: improve precision
    Use doubles for accumulating floats.

--- libavfilter/x86/vf_ssim_init.c
+++ libavfilter/x86/vf_ssim_init.c
@@ -28,7 +28,7 @@ void ff_ssim_4x4_line_ssse3(const uint8_t *buf, ptrdiff_t buf_stride,
 void ff_ssim_4x4_line_xop  (const uint8_t *buf, ptrdiff_t buf_stride,
                             const uint8_t *ref, ptrdiff_t ref_stride,
                             int (*sums)[4], int w);
-float ff_ssim_end_line_sse4(const int (*sum0)[4], const int (*sum1)[4], int w);
+double ff_ssim_end_line_sse4(const int (*sum0)[4], const int (*sum1)[4], int w);
 
 void ff_ssim_init_x86(SSIMDSPContext *dsp)
 {
