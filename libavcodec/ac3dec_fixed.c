27c42d33bce9fa10fc91c54be3751565d5a4eae3
Michael Niedermayer <michael@niedermayer.cc>
2020-02-02 00:07:19 +0100

    avcodec/ac3dec_fixed: Remove some temporary variables from scale_coefs()
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/ac3dec_fixed.c
+++ libavcodec/ac3dec_fixed.c
@@ -110,28 +110,14 @@ static void scale_coefs (
       mul <<= shift;
       for (i=0; i<len; i+=8) {
 
-          temp = src[i] * mul;
-          temp1 = src[i+1] * mul;
-          temp2 = src[i+2] * mul;
-
-          dst[i] = temp;
-          temp3 = src[i+3] * mul;
-
-          dst[i+1] = temp1;
-          temp4 = src[i + 4] * mul;
-          dst[i+2] = temp2;
-
-          temp5 = src[i+5] * mul;
-          dst[i+3] = temp3;
-          temp6 = src[i+6] * mul;
-
-          dst[i+4] = temp4;
-          temp7 = src[i+7] * mul;
-
-          dst[i+5] = temp5;
-          dst[i+6] = temp6;
-          dst[i+7] = temp7;
-
+          dst[i]   = src[i  ] * mul;
+          dst[i+1] = src[i+1] * mul;
+          dst[i+2] = src[i+2] * mul;
+          dst[i+3] = src[i+3] * mul;
+          dst[i+4] = src[i+4] * mul;
+          dst[i+5] = src[i+5] * mul;
+          dst[i+6] = src[i+6] * mul;
+          dst[i+7] = src[i+7] * mul;
       }
     }
 }
