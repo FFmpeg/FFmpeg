dc46b3c34e0651215a9357bcc0aba04e2bad9721
Andreas Rheinhardt <andreas.rheinhardt@gmail.com>
2020-02-14 12:24:03 +0100

    avcodec/dvdsub: Fix warning about incompatible pointer type
    Fixes "passing argument 2 of ‘strtoul’ from incompatible pointer
    type [-Wincompatible-pointer-types]" ("expected ‘char ** restrict’ but
    argument is of type ‘const char **’") for GCC and "passing 'const char
    **' to parameter of type 'char **' discards qualifiers in nested pointer
    types [-Wincompatible-pointer-types-discards-qualifiers]" for Clang.
    
    The cast itself is safe; it is only needed because strtoul itself is not
    const-correct.
    
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@gmail.com>
    Reviewed-by: Paul B Mahol <onemda@gmail.com>
    Signed-off-by: James Almer <jamrial@gmail.com>

--- libavcodec/dvdsub.c
+++ libavcodec/dvdsub.c
@@ -26,7 +26,7 @@
 void ff_dvdsub_parse_palette(uint32_t *palette, const char *p)
 {
     for (int i = 0; i < 16; i++) {
-        palette[i] = strtoul(p, &p, 16);
+        palette[i] = strtoul(p, (char **)&p, 16);
         while (*p == ',' || av_isspace(*p))
             p++;
     }
