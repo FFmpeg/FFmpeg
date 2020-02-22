2db37bf4cd4e7e2f5e0602691df10b65934feb60
Michael Niedermayer <michael@niedermayer.cc>
2020-02-23 00:08:19 +0100

    tools/target_dec_fuzzer: Adjust threshold for zerocodec
    Fixes: Timeout (147sec -> 1sec)
    Fixes: 20764/clusterfuzz-testcase-minimized-ffmpeg_AV_CODEC_ID_ZEROCODEC_fuzzer-5068274603917312
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- tools/target_dec_fuzzer.c
+++ tools/target_dec_fuzzer.c
@@ -168,6 +168,7 @@ int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
     case AV_CODEC_ID_TRUEMOTION2: maxpixels  /= 1024;  break;
     case AV_CODEC_ID_VP7:         maxpixels  /= 256;   break;
     case AV_CODEC_ID_VP9:         maxpixels  /= 4096;  break;
+    case AV_CODEC_ID_ZEROCODEC:   maxpixels  /= 128;   break;
     }
 
 
