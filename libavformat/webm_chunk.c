d8e63ed70e5f2202ad790c1cc58d5d6bc353a551
Andreas Rheinhardt <andreas.rheinhardt@gmail.com>
2020-04-07 21:31:27 +0200

    avformat/webm_chunk: Remove superfluous headers
    libavutil/avstring.h is unnecessary since 8a632b3e. The other
    unnecessary headers were never used.
    
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@gmail.com>

--- libavformat/webm_chunk.c
+++ libavformat/webm_chunk.c
@@ -24,23 +24,14 @@
  * chunk, followed by data chunks where each Cluster is written out as a Chunk.
  */
 
-#include <float.h>
-#include <time.h>
-
 #include "avformat.h"
 #include "avio.h"
 #include "avio_internal.h"
 #include "internal.h"
 
-#include "libavutil/avassert.h"
 #include "libavutil/log.h"
 #include "libavutil/opt.h"
-#include "libavutil/avstring.h"
-#include "libavutil/parseutils.h"
 #include "libavutil/mathematics.h"
-#include "libavutil/time.h"
-#include "libavutil/time_internal.h"
-#include "libavutil/timestamp.h"
 
 #define MAX_FILENAME_SIZE 1024
 
