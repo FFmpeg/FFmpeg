39962072a83eb846d8f2aa7757a0ff33cc3dad51
Josh de Kock <josh@itanimul.li>
2020-04-14 13:34:53 +0100

    tools: stop using deprecated av_codec_next()
    Signed-off-by: Josh de Kock <josh@itanimul.li>

--- tools/enum_options.c
+++ tools/enum_options.c
@@ -113,13 +113,14 @@ static void show_format_opts(void)
 
 static void show_codec_opts(void)
 {
+    void *iter = NULL;
     AVCodec *c = NULL;
 
     printf("@section Generic codec AVOptions\n");
     show_opts(avcodec_get_class());
 
     printf("@section Codec-specific AVOptions\n");
-    while ((c = av_codec_next(c))) {
+    while ((c = av_codec_iterate(&iter))) {
         if (!c->priv_class)
             continue;
         printf("@subsection %s AVOptions\n", c->priv_class->class_name);
