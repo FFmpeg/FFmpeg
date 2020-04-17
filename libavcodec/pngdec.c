55d830f69a2ff3ca191d97862200d4cc480d25b7
Peter Ross <pross@xvid.org>
2020-04-17 22:35:43 +1000

    avcodec/pngdec: set return value on av_stereo3d_create_side_data() failure
    Signed-off-by: Peter Ross <pross@xvid.org>
    Reviewed-by: Vittorio Giovara <vittorio.giovara@gmail.com>

--- libavcodec/pngdec.c
+++ libavcodec/pngdec.c
@@ -1286,8 +1286,10 @@ static int decode_frame_common(AVCodecContext *avctx, PNGDecContext *s,
         case MKTAG('s', 'T', 'E', 'R'): {
             int mode = bytestream2_get_byte(&s->gb);
             AVStereo3D *stereo3d = av_stereo3d_create_side_data(p);
-            if (!stereo3d)
+            if (!stereo3d) {
+                ret = AVERROR(ENOMEM);
                 goto fail;
+            }
 
             if (mode == 0 || mode == 1) {
                 stereo3d->type  = AV_STEREO3D_SIDEBYSIDE;
