4077647a8f9fd9c0a6622bf13bc35138c7dfcd4a
Michael Niedermayer <michael@niedermayer.cc>
2020-04-19 17:05:53 +0200

    avcodec/binkaudio: Fix 2Ghz sample_rate
    Fixes: signed integer overflow: 2147483647 + 1 cannot be represented in type 'int'
    Fixes: 19950/clusterfuzz-testcase-minimized-ffmpeg_AV_CODEC_ID_BINKAUDIO_DCT_fuzzer-5765514337189888
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    Suggested-by: Paul
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit f603d10b1e6bb2fbf4dcccc43d3ea2fb911b36ba)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/binkaudio.c
+++ libavcodec/binkaudio.c
@@ -109,7 +109,7 @@ static av_cold int decode_init(AVCodecContext *avctx)
     s->frame_len     = 1 << frame_len_bits;
     s->overlap_len   = s->frame_len / 16;
     s->block_size    = (s->frame_len - s->overlap_len) * s->channels;
-    sample_rate_half = (sample_rate + 1) / 2;
+    sample_rate_half = (sample_rate + 1LL) / 2;
     if (avctx->codec->id == AV_CODEC_ID_BINKAUDIO_RDFT)
         s->root = 2.0 / (sqrt(s->frame_len) * 32768.0);
     else
