68d9c0be898d76132c569a84c24eca2d9270eb13
Gyan Doshi <ffmpeg@gyani.pro>
2020-04-20 22:44:59 +0530

    avformat/mpegtsenc: don't print warning for DVB text streams
    They can be demuxed by ffmpeg.

--- libavformat/mpegtsenc.c
+++ libavformat/mpegtsenc.c
@@ -349,6 +349,10 @@ static int get_dvb_stream_type(AVFormatContext *s, AVStream *st)
     case AV_CODEC_ID_TIMED_ID3:
         stream_type = STREAM_TYPE_METADATA;
         break;
+    case AV_CODEC_ID_DVB_SUBTITLE:
+    case AV_CODEC_ID_DVB_TELETEXT:
+        stream_type = STREAM_TYPE_PRIVATE_DATA;
+        break;
     default:
         av_log(s, AV_LOG_WARNING, "Stream %d, codec %s, is muxed as a private data stream "
                "and may not be recognized upon reading.\n", st->index, avcodec_get_name(st->codecpar->codec_id));
