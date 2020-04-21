9f3d3ff2ecebd8882627ca4c9cb58481252ee9db
Andreas Rheinhardt <andreas.rheinhardt@gmail.com>
2020-04-21 02:57:25 +0200

    avformat/mux: Remove unnecessary headers
    mux.c was split from utils.c in 55f9037f38bc3beacb2f5a17408c1d24c077d7fd
    and during this split all headers were simply copied without checking if
    they were only needed in the part that stayed in utils.c (or whether
    these haeders were needed at all). As a result quite a lot of headers
    in mux.c are unnecessary. This commit removes them.
    
    Reviewed-by: Anton Khirnov <anton@khirnov.net>
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@gmail.com>

--- libavformat/mux.c
+++ libavformat/mux.c
@@ -20,29 +20,16 @@
  */
 
 #include "avformat.h"
-#include "avio_internal.h"
 #include "internal.h"
 #include "libavcodec/internal.h"
-#include "libavcodec/bytestream.h"
 #include "libavutil/opt.h"
 #include "libavutil/dict.h"
 #include "libavutil/pixdesc.h"
 #include "libavutil/timestamp.h"
-#include "metadata.h"
-#include "id3v2.h"
 #include "libavutil/avassert.h"
 #include "libavutil/avstring.h"
 #include "libavutil/internal.h"
 #include "libavutil/mathematics.h"
-#include "libavutil/parseutils.h"
-#include "libavutil/time.h"
-#include "riff.h"
-#include "audiointerleave.h"
-#include "url.h"
-#include <stdarg.h>
-#if CONFIG_NETWORK
-#include "network.h"
-#endif
 
 /**
  * @file
