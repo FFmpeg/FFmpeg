c7d8082357965bfd02bc3a8ba6867b36119d2895
Paul B Mahol <onemda@gmail.com>
2020-04-22 15:29:16 +0200

    avfilter: add asubboost filter

--- libavfilter/allfilters.c
+++ libavfilter/allfilters.c
@@ -79,6 +79,7 @@ extern AVFilter ff_af_asplit;
 extern AVFilter ff_af_asr;
 extern AVFilter ff_af_astats;
 extern AVFilter ff_af_astreamselect;
+extern AVFilter ff_af_asubboost;
 extern AVFilter ff_af_atempo;
 extern AVFilter ff_af_atrim;
 extern AVFilter ff_af_axcorrelate;
--- libavfilter/version.h
+++ libavfilter/version.h
@@ -30,7 +30,7 @@
 #include "libavutil/version.h"
 
 #define LIBAVFILTER_VERSION_MAJOR   7
-#define LIBAVFILTER_VERSION_MINOR  79
+#define LIBAVFILTER_VERSION_MINOR  80
 #define LIBAVFILTER_VERSION_MICRO 100
 
 
