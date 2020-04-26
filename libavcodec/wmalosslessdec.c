d8a1e0a79efcb8faf26350060bb57dcd449cd427
Michael Niedermayer <michael@niedermayer.cc>
2020-04-26 21:19:13 +0200

    avcodec/wmalosslessdec: Fix integer overflows in revert_inter_ch_decorr()
    Fixes: signed integer overflow: -717241856 + -1434459904 cannot be represented in type 'int'
    Fixes: 21405/clusterfuzz-testcase-minimized-ffmpeg_AV_CODEC_ID_WMALOSSLESS_fuzzer-5677143666458624
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit e9a4c4fe9918220be492a4a9d74c2293fd706be3)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/wmalosslessdec.c
+++ libavcodec/wmalosslessdec.c
@@ -784,8 +784,8 @@ static void revert_inter_ch_decorr(WmallDecodeCtx *s, int tile_size)
     else if (s->is_channel_coded[0] || s->is_channel_coded[1]) {
         int icoef;
         for (icoef = 0; icoef < tile_size; icoef++) {
-            s->channel_residues[0][icoef] -= s->channel_residues[1][icoef] >> 1;
-            s->channel_residues[1][icoef] += s->channel_residues[0][icoef];
+            s->channel_residues[0][icoef] -= (unsigned)(s->channel_residues[1][icoef] >> 1);
+            s->channel_residues[1][icoef] += (unsigned) s->channel_residues[0][icoef];
         }
     }
 }
