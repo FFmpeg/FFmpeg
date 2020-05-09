06fdc823371d295a6f29993e5c2e9ed7df3610d2
Andreas Rheinhardt <andreas.rheinhardt@gmail.com>
2020-05-09 21:26:30 +0200

    avformat/mux: Remove unnecessary unreferencing of AVPacket
    Since commit c5324d92c5f206dcdc2cf93ae237eaa7c1ad0a40 all custom
    interleave_packet() functions always return clean packets (even on
    error), so that unreferencing manually can be removed.
    
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@gmail.com>

--- libavformat/mux.c
+++ libavformat/mux.c
@@ -1067,10 +1067,7 @@ int ff_interleaved_peek(AVFormatContext *s, int stream,
 static int interleave_packet(AVFormatContext *s, AVPacket *out, AVPacket *in, int flush)
 {
     if (s->oformat->interleave_packet) {
-        int ret = s->oformat->interleave_packet(s, out, in, flush);
-        if (in)
-            av_packet_unref(in);
-        return ret;
+        return s->oformat->interleave_packet(s, out, in, flush);
     } else
         return ff_interleave_packet_per_dts(s, out, in, flush);
 }
