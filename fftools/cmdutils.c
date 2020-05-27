e0fbb6cf2bea2427c5b794a5a9fbd39fd0b4f888
Anton Khirnov <anton@khirnov.net>
2020-05-27 15:25:38 +0200

    fftools: switch to the new child class iteration API

--- fftools/cmdutils.c
+++ fftools/cmdutils.c
@@ -202,13 +202,14 @@ void show_help_options(const OptionDef *options, const char *msg, int req_flags,
 
 void show_help_children(const AVClass *class, int flags)
 {
-    const AVClass *child = NULL;
+    void *iter = NULL;
+    const AVClass *child;
     if (class->option) {
         av_opt_show2(&class, NULL, flags, 0);
         printf("\n");
     }
 
-    while (child = av_opt_child_class_next(class, child))
+    while (child = av_opt_child_class_iterate(class, &iter))
         show_help_children(child, flags);
 }
 
