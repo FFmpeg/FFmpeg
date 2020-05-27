851e0dfe296af1cb96dc5315547ef459cd85c598
Anton Khirnov <anton@khirnov.net>
2020-05-27 15:34:07 +0200

    AVCodecContext: switch to child_class_iterate()

--- libavcodec/options.c
+++ libavcodec/options.c
@@ -53,6 +53,7 @@ static void *codec_child_next(void *obj, void *prev)
     return NULL;
 }
 
+#if FF_API_CHILD_CLASS_NEXT
 static const AVClass *codec_child_class_next(const AVClass *prev)
 {
     void *iter = NULL;
@@ -65,6 +66,17 @@ static const AVClass *codec_child_class_next(const AVClass *prev)
 
     /* find next codec with priv options */
     while (c = av_codec_iterate(&iter))
+        if (c->priv_class)
+            return c->priv_class;
+    return NULL;
+}
+#endif
+
+static const AVClass *codec_child_class_iterate(void **iter)
+{
+    const AVCodec *c;
+    /* find next codec with priv options */
+    while (c = av_codec_iterate(iter))
         if (c->priv_class)
             return c->priv_class;
     return NULL;
@@ -84,7 +96,10 @@ static const AVClass av_codec_context_class = {
     .version                 = LIBAVUTIL_VERSION_INT,
     .log_level_offset_offset = offsetof(AVCodecContext, log_level_offset),
     .child_next              = codec_child_next,
+#if FF_API_CHILD_CLASS_NEXT
     .child_class_next        = codec_child_class_next,
+#endif
+    .child_class_iterate     = codec_child_class_iterate,
     .category                = AV_CLASS_CATEGORY_ENCODER,
     .get_category            = get_category,
 };
