e149b24c63859a40000d45aafb56abb70f334655
Michael Niedermayer <michael@niedermayer.cc>
2020-06-07 19:24:10 +0200

    avcodec/lossless_audiodsp: Fix undefined overflows in scalarproduct_and_madd_int16_c()
    Fixes: signed integer overflow: 2142077091 + 6881070 cannot be represented in type 'int'
    Fixes: 22737/clusterfuzz-testcase-minimized-ffmpeg_AV_CODEC_ID_WMALOSSLESS_fuzzer-5958388889681920
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit c0dfe134beefde4070d43910518b1f4a58f01794)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/lossless_audiodsp.c
+++ libavcodec/lossless_audiodsp.c
@@ -27,7 +27,7 @@ static int32_t scalarproduct_and_madd_int16_c(int16_t *v1, const int16_t *v2,
                                               const int16_t *v3,
                                               int order, int mul)
 {
-    int res = 0;
+    unsigned res = 0;
 
     do {
         res   += *v1 * *v2++;
