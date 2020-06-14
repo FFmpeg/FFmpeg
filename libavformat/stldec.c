e13874b9eae4e156ca1c478e6d59d3461bbdc09f
Andreas Rheinhardt <andreas.rheinhardt@gmail.com>
2020-06-14 03:24:23 +0200

    avformat/stldec: Fix memleak upon read header failure
    The already parsed subtitles (contained in an FFDemuxSubtitlesQueue)
    would leak if an error happened upon reading a subsequent subtitle.
    
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@gmail.com>

--- libavformat/stldec.c
+++ libavformat/stldec.c
@@ -97,8 +97,10 @@ static int stl_read_header(AVFormatContext *s)
         if (pts_start != AV_NOPTS_VALUE) {
             AVPacket *sub;
             sub = ff_subtitles_queue_insert(&stl->q, p, strlen(p), 0);
-            if (!sub)
+            if (!sub) {
+                ff_subtitles_queue_clean(&stl->q);
                 return AVERROR(ENOMEM);
+            }
             sub->pos = pos;
             sub->pts = pts_start;
             sub->duration = duration;
