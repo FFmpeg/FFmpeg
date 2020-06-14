5c9f00b47ea49425a3df3f0de4bea4c02d97e739
Andreas Rheinhardt <andreas.rheinhardt@gmail.com>
2020-06-14 03:21:46 +0200

    avformat/samidec: Fix memleak upon read header failure
    The already parsed subtitles (contained in an FFDemuxSubtitlesQueue)
    would leak if an error happened upon reading a subsequent subtitle
    or when creating extradata.
    
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@gmail.com>
    (cherry picked from commit f161f8e4ad10c8ae5b2e97870e09bc6a421408eb)
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@gmail.com>

--- libavformat/samidec.c
+++ libavformat/samidec.c
@@ -108,6 +108,8 @@ static int sami_read_header(AVFormatContext *s)
     ff_subtitles_queue_finalize(s, &sami->q);
 
 end:
+    if (res < 0)
+        ff_subtitles_queue_clean(&sami->q);
     av_bprint_finalize(&buf, NULL);
     return res;
 }
