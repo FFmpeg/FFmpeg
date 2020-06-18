42b28565aa852b98d95d8d02f7b0781999f9d533
Michael Niedermayer <michael@niedermayer.cc>
2020-06-18 11:56:53 +0200

    avutil/avsscanf: Add () to avoid integer overflow in scanexp()
    Fixes: signed integer overflow: 2147483610 + 52 cannot be represented in type 'int'
    Fixes: 23260/clusterfuzz-testcase-minimized-ffmpeg_AV_CODEC_ID_PBM_fuzzer-5187871274434560
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavutil/avsscanf.c
+++ libavutil/avsscanf.c
@@ -229,9 +229,9 @@ static long long scanexp(FFFILE *f, int pok)
         return LLONG_MIN;
     }
     for (x=0; c-'0'<10U && x<INT_MAX/10; c = shgetc(f))
-        x = 10*x + c-'0';
+        x = 10*x + (c-'0');
     for (y=x; c-'0'<10U && y<LLONG_MAX/100; c = shgetc(f))
-        y = 10*y + c-'0';
+        y = 10*y + (c-'0');
     for (; c-'0'<10U; c = shgetc(f));
     shunget(f);
     return neg ? -y : y;
