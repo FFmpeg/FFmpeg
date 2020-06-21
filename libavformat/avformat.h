b8c6aae575f09a1f2eef45e5b9d95b28790bc087
Andreas Rheinhardt <andreas.rheinhardt@gmail.com>
2020-06-21 06:47:23 +0200

    avformat/avformat: Improve documentation of av_stream_get_side_data
    Document that it also sets the size in case the desired side data is
    absent (if the pointer has been supplied).
    
    Reviewed-by: Anton Khirnov <anton@khirnov.net>
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@gmail.com>

--- libavformat/avformat.h
+++ libavformat/avformat.h
@@ -2189,7 +2189,8 @@ uint8_t *av_stream_new_side_data(AVStream *stream,
  *
  * @param stream stream
  * @param type desired side information type
- * @param size pointer for side information size to store (optional)
+ * @param size If supplied, *size will be set to the size of the side data
+ *             or to zero if the desired side data is not present.
  * @return pointer to data if present or NULL otherwise
  */
 uint8_t *av_stream_get_side_data(const AVStream *stream,
