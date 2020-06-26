1dee8bf9099ce9460486c566aa1fb0612890d939
Limin Wang <lance.lmwang@gmail.com>
2020-06-26 08:47:26 +0800

    avformat/gifdec: -1 -> AV_BPRINT_SIZE_UNLIMITED
    Signed-off-by: Limin Wang <lance.lmwang@gmail.com>

--- libavformat/gifdec.c
+++ libavformat/gifdec.c
@@ -144,7 +144,7 @@ static int gif_read_header(AVFormatContext *s)
                 AVBPrint bp;
                 int block_size;
 
-                av_bprint_init(&bp, 0, -1);
+                av_bprint_init(&bp, 0, AV_BPRINT_SIZE_UNLIMITED);
                 while ((block_size = avio_r8(pb)) != 0) {
                     avio_read_to_bprint(pb, &bp, block_size);
                 }
