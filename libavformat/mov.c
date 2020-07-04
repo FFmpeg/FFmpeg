a06587b01af49822f79d0b0a174759d53b360649
Zhao Zhili <quinkblack@foxmail.com>
2020-07-05 00:51:53 +0800

    avformat/mov: Fix unaligned read of uint32_t and endian-dependance in mov_read_default
    Reviewed-by: Andreas Rheinhardt <andreas.rheinhardt@gmail.com>
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit 806a4d5187aeb82b97898683242886ed1e84f894)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavformat/mov.c
+++ libavformat/mov.c
@@ -6824,13 +6824,12 @@ static int mov_read_default(MOVContext *c, AVIOContext *pb, MOVAtom atom)
                 a.size >= 8 &&
                 c->fc->strict_std_compliance < FF_COMPLIANCE_STRICT &&
                 c->moov_retry) {
-                uint8_t buf[8];
-                uint32_t *type = (uint32_t *)buf + 1;
-                if (avio_read(pb, buf, 8) != 8)
-                    return AVERROR_INVALIDDATA;
+                uint32_t type;
+                avio_skip(pb, 4);
+                type = avio_rl32(pb);
                 avio_seek(pb, -8, SEEK_CUR);
-                if (*type == MKTAG('m','v','h','d') ||
-                    *type == MKTAG('c','m','o','v')) {
+                if (type == MKTAG('m','v','h','d') ||
+                    type == MKTAG('c','m','o','v')) {
                     av_log(c->fc, AV_LOG_ERROR, "Detected moov in a free atom.\n");
                     a.type = MKTAG('m','o','o','v');
                 }
