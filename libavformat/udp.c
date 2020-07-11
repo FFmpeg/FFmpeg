3205ed31a7756ae563301e2f5a5dd2c853b20349
Jun Zhao <barryjzhao@tencent.com>
2020-07-11 19:09:36 +0800

    lavf/udp: fix the comments for default UDP socket recvbuf size
    15d160cc0b2 increased the UDP socket receiving buffer size
    (64K ->384K), but missed to update this comments.
    
    Reviewed-by: Marton Balint <cus@passwd.hu>
    Signed-off-by: Jun Zhao <barryjzhao@tencent.com>

--- libavformat/udp.c
+++ libavformat/udp.c
@@ -852,7 +852,7 @@ static int udp_open(URLContext *h, const char *uri, int flags)
             goto fail;
         }
     } else {
-        /* set udp recv buffer size to the requested value (default 64K) */
+        /* set udp recv buffer size to the requested value (default UDP_RX_BUF_SIZE) */
         tmp = s->buffer_size;
         if (setsockopt(udp_fd, SOL_SOCKET, SO_RCVBUF, &tmp, sizeof(tmp)) < 0) {
             ff_log_net_error(h, AV_LOG_WARNING, "setsockopt(SO_RECVBUF)");
