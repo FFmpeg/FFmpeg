466c14d10728cd315c60e602ffe3797e7dd0c3d3
Timo Rothenpieler <timo@rothenpieler.org>
2020-07-28 21:44:11 +0200

    avcodec/nvenc: fix B-Ref-Mode support check for recent HEVC

--- libavcodec/nvenc.c
+++ libavcodec/nvenc.c
@@ -424,7 +424,7 @@ static int nvenc_check_capabilities(AVCodecContext *avctx)
 
 #ifdef NVENC_HAVE_BFRAME_REF_MODE
     ret = nvenc_check_cap(avctx, NV_ENC_CAPS_SUPPORT_BFRAME_REF_MODE);
-    if (ctx->b_ref_mode == NV_ENC_BFRAME_REF_MODE_EACH && ret != 1) {
+    if (ctx->b_ref_mode == NV_ENC_BFRAME_REF_MODE_EACH && ret != 1 && ret != 3) {
         av_log(avctx, AV_LOG_WARNING, "Each B frame as reference is not supported\n");
         return AVERROR(ENOSYS);
     } else if (ctx->b_ref_mode != NV_ENC_BFRAME_REF_MODE_DISABLED && ret == 0) {
