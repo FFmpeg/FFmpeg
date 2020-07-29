fabbb680d42aa81d1a7b3138b1f66d8ddd7ce81d
Paul B Mahol <onemda@gmail.com>
2020-07-29 10:01:35 +0200

    avcodec/cfhd: move if/else out of loop in inverse transform

--- libavcodec/cfhd.c
+++ libavcodec/cfhd.c
@@ -211,39 +211,37 @@ static inline void filter(int16_t *output, ptrdiff_t out_stride,
     int16_t tmp;
     int i;
 
-    for (i = 0; i < len; i++) {
-        if (i == 0) {
-            tmp = (11*low[0*low_stride] - 4*low[1*low_stride] + low[2*low_stride] + 4) >> 3;
-            output[(2*i+0)*out_stride] = (tmp + high[0*high_stride]) >> 1;
-            if (clip)
-                output[(2*i+0)*out_stride] = av_clip_uintp2_c(output[(2*i+0)*out_stride], clip);
+    tmp = (11*low[0*low_stride] - 4*low[1*low_stride] + low[2*low_stride] + 4) >> 3;
+    output[(2*0+0)*out_stride] = (tmp + high[0*high_stride]) >> 1;
+    if (clip)
+        output[(2*0+0)*out_stride] = av_clip_uintp2_c(output[(2*0+0)*out_stride], clip);
 
-            tmp = ( 5*low[0*low_stride] + 4*low[1*low_stride] - low[2*low_stride] + 4) >> 3;
-            output[(2*i+1)*out_stride] = (tmp - high[0*high_stride]) >> 1;
-            if (clip)
-                output[(2*i+1)*out_stride] = av_clip_uintp2_c(output[(2*i+1)*out_stride], clip);
-        } else if (i == len-1) {
-            tmp = ( 5*low[i*low_stride] + 4*low[(i-1)*low_stride] - low[(i-2)*low_stride] + 4) >> 3;
-            output[(2*i+0)*out_stride] = (tmp + high[i*high_stride]) >> 1;
-            if (clip)
-                output[(2*i+0)*out_stride] = av_clip_uintp2_c(output[(2*i+0)*out_stride], clip);
+    tmp = ( 5*low[0*low_stride] + 4*low[1*low_stride] - low[2*low_stride] + 4) >> 3;
+    output[(2*0+1)*out_stride] = (tmp - high[0*high_stride]) >> 1;
+    if (clip)
+        output[(2*0+1)*out_stride] = av_clip_uintp2_c(output[(2*0+1)*out_stride], clip);
 
-            tmp = (11*low[i*low_stride] - 4*low[(i-1)*low_stride] + low[(i-2)*low_stride] + 4) >> 3;
-            output[(2*i+1)*out_stride] = (tmp - high[i*high_stride]) >> 1;
-            if (clip)
-                output[(2*i+1)*out_stride] = av_clip_uintp2_c(output[(2*i+1)*out_stride], clip);
-        } else {
-            tmp = (low[(i-1)*low_stride] - low[(i+1)*low_stride] + 4) >> 3;
-            output[(2*i+0)*out_stride] = (tmp + low[i*low_stride] + high[i*high_stride]) >> 1;
-            if (clip)
-                output[(2*i+0)*out_stride] = av_clip_uintp2_c(output[(2*i+0)*out_stride], clip);
+    for (i = 1; i < len - 1; i++) {
+        tmp = (low[(i-1)*low_stride] - low[(i+1)*low_stride] + 4) >> 3;
+        output[(2*i+0)*out_stride] = (tmp + low[i*low_stride] + high[i*high_stride]) >> 1;
+        if (clip)
+            output[(2*i+0)*out_stride] = av_clip_uintp2_c(output[(2*i+0)*out_stride], clip);
 
-            tmp = (low[(i+1)*low_stride] - low[(i-1)*low_stride] + 4) >> 3;
-            output[(2*i+1)*out_stride] = (tmp + low[i*low_stride] - high[i*high_stride]) >> 1;
-            if (clip)
-                output[(2*i+1)*out_stride] = av_clip_uintp2_c(output[(2*i+1)*out_stride], clip);
-        }
+        tmp = (low[(i+1)*low_stride] - low[(i-1)*low_stride] + 4) >> 3;
+        output[(2*i+1)*out_stride] = (tmp + low[i*low_stride] - high[i*high_stride]) >> 1;
+        if (clip)
+            output[(2*i+1)*out_stride] = av_clip_uintp2_c(output[(2*i+1)*out_stride], clip);
     }
+
+    tmp = ( 5*low[i*low_stride] + 4*low[(i-1)*low_stride] - low[(i-2)*low_stride] + 4) >> 3;
+    output[(2*i+0)*out_stride] = (tmp + high[i*high_stride]) >> 1;
+    if (clip)
+        output[(2*i+0)*out_stride] = av_clip_uintp2_c(output[(2*i+0)*out_stride], clip);
+
+    tmp = (11*low[i*low_stride] - 4*low[(i-1)*low_stride] + low[(i-2)*low_stride] + 4) >> 3;
+    output[(2*i+1)*out_stride] = (tmp - high[i*high_stride]) >> 1;
+    if (clip)
+        output[(2*i+1)*out_stride] = av_clip_uintp2_c(output[(2*i+1)*out_stride], clip);
 }
 
 static inline void interlaced_vertical_filter(int16_t *output, int16_t *low, int16_t *high,
