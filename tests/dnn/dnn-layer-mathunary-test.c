9fbdd5454bba660a2ec3aa4da31183b2f7fd3bc8
Mingyu Yin <mingyu.yin@intel.com>
2020-07-31 15:41:24 +0800

    dnn_backend_native_layer_mathunary: add ceil support
    It can be tested with the model generated with below python script:
    
    import tensorflow as tf
    import os
    import numpy as np
    import imageio
    from tensorflow.python.framework import graph_util
    name = 'ceil'
    
    pb_file_path = os.getcwd()
    if not os.path.exists(pb_file_path+'/{}_savemodel/'.format(name)):
        os.mkdir(pb_file_path+'/{}_savemodel/'.format(name))
    
    with tf.Session(graph=tf.Graph()) as sess:
        in_img = imageio.imread('detection.jpg')
        in_img = in_img.astype(np.float32)
        in_data = in_img[np.newaxis, :]
        input_x = tf.placeholder(tf.float32, shape=[1, None, None, 3], name='dnn_in')
        y = tf.math.ceil( input_x, name='dnn_out')
        sess.run(tf.global_variables_initializer())
        constant_graph = graph_util.convert_variables_to_constants(sess, sess.graph_def, ['dnn_out'])
    
        with tf.gfile.FastGFile(pb_file_path+'/{}_savemodel/model.pb'.format(name), mode='wb') as f:
            f.write(constant_graph.SerializeToString())
    
        print("model.pb generated, please in ffmpeg path use\n \n \
        python tools/python/convert.py ceil_savemodel/model.pb --outdir=ceil_savemodel/ \n \n \
        to generate model.model\n")
    
        output = sess.run(y, feed_dict={ input_x: in_data})
        imageio.imsave("out.jpg", np.squeeze(output))
    
        print("To verify, please ffmpeg path use\n \n \
        ./ffmpeg -i detection.jpg -vf format=rgb24,dnn_processing=model=ceil_savemodel/model.pb:input=dnn_in:output=dnn_out:dnn_backend=tensorflow -f framemd5 ceil_savemodel/tensorflow_out.md5\n \n \
        to generate output result of tensorflow model\n")
    
        print("To verify, please ffmpeg path use\n \n \
        ./ffmpeg -i detection.jpg -vf format=rgb24,dnn_processing=model=ceil_savemodel/model.model:input=dnn_in:output=dnn_out:dnn_backend=native -f framemd5 ceil_savemodel/native_out.md5\n \n \
        to generate output result of native model\n")
    
    Signed-off-by: Mingyu Yin <mingyu.yin@intel.com>
    Reviewed-by: Guo, Yejun <yejun.guo@intel.com>

--- tests/dnn/dnn-layer-mathunary-test.c
+++ tests/dnn/dnn-layer-mathunary-test.c
@@ -56,6 +56,8 @@ static float get_expected(float f, DNNMathUnaryOperation op)
         return acosh(f);
     case DMUO_ATANH:
         return atanh(f);
+    case DMUO_CEIL:
+        return ceil(f);
     default:
         av_assert0(!"not supported yet");
         return 0.f;
@@ -127,6 +129,8 @@ int main(int agrc, char **argv)
     if (test(DMUO_ACOSH))
         return 1;
     if (test(DMUO_ATANH))
+        return 1;
+    if (test(DMUO_CEIL))
         return 1;
     return 0;
 }
