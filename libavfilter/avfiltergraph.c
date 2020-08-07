07b52a4b70a3a0593f8fa327187447efae68c03c
Andreas Rheinhardt <andreas.rheinhardt@gmail.com>
2020-08-07 04:28:07 +0200

    Revert "lavfi/avfiltergraph: add check before free the format"
    This reverts commit f156f4ab2317f22bfef33c7eaead0d5d5f162903.
    
    The checks added by said commit are nonsense because they did not help
    in case ff_merge_samplerates() or ff_merge_formats() returned NULL
    while freeing one of its arguments: Said freeing does not change
    the local variables of can_merge_formats().
    
    Reviewed-by: Nicolas George <george@nsup.org>
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@gmail.com>
    (cherry picked from commit c4c10feaa8c3604abfec4283715b37358f73b6e7)

--- libavfilter/avfiltergraph.c
+++ libavfilter/avfiltergraph.c
@@ -419,10 +419,8 @@ static int can_merge_formats(AVFilterFormats *a_arg,
         av_freep(&ret);
         return 1;
     } else {
-        if (a)
-            av_freep(&a->formats);
-        if (b)
-            av_freep(&b->formats);
+        av_freep(&a->formats);
+        av_freep(&b->formats);
         av_freep(&a);
         av_freep(&b);
         return 0;
