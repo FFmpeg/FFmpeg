67c5f747d16bdb8147e7e092108509233c018e9c
Michael Niedermayer <michael@niedermayer.cc>
2020-09-02 23:13:00 +0200

    avformat/dxa: Use av_rescale() for duration computation
    Fixes: signed integer overflow: 8224000000 * 1629552639 cannot be represented in type 'long'
    Fixes: 24908/clusterfuzz-testcase-minimized-ffmpeg_DEMUXER_fuzzer-4658478506049536
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit c313089fbe1df71b5406dd9d7e4d36361051c620)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavformat/dxa.c
+++ libavformat/dxa.c
@@ -143,7 +143,7 @@ static int dxa_read_header(AVFormatContext *s)
     c->readvid = !c->has_sound;
     c->vidpos  = avio_tell(pb);
     s->start_time = 0;
-    s->duration = (int64_t)c->frames * AV_TIME_BASE * num / den;
+    s->duration = av_rescale(c->frames, AV_TIME_BASE * (int64_t)num, den);
     av_log(s, AV_LOG_DEBUG, "%d frame(s)\n",c->frames);
 
     return 0;
