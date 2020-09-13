acda9ff6ce2b94a199a6967118fa19d58a09c146
Andreas Rheinhardt <andreas.rheinhardt@gmail.com>
2020-09-13 20:16:30 +0200

    avcodec/alacenc: Remove redundant code to free extradata
    It is already freed generically for encoders.
    
    Reviewed-by: Paul B Mahol <onemda@gmail.com>
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@gmail.com>

--- libavcodec/alacenc.c
+++ libavcodec/alacenc.c
@@ -498,8 +498,6 @@ static av_cold int alac_encode_close(AVCodecContext *avctx)
 {
     AlacEncodeContext *s = avctx->priv_data;
     ff_lpc_end(&s->lpc_ctx);
-    av_freep(&avctx->extradata);
-    avctx->extradata_size = 0;
     return 0;
 }
 
