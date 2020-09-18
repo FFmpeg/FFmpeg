345d04e8704cadf64d1b232b0d1be91566d1af06
Zhao Zhili <zhilizhao@tencent.com>
2020-09-18 22:33:38 +0800

    avformat/mov: remove an always true condition

--- libavformat/mov.c
+++ libavformat/mov.c
@@ -1511,7 +1511,7 @@ static int mov_read_mvhd(MOVContext *c, AVIOContext *pb, MOVAtom atom)
     c->duration = (version == 1) ? avio_rb64(pb) : avio_rb32(pb); /* duration */
     // set the AVFormatContext duration because the duration of individual tracks
     // may be inaccurate
-    if (c->time_scale > 0 && !c->trex_data)
+    if (!c->trex_data)
         c->fc->duration = av_rescale(c->duration, AV_TIME_BASE, c->time_scale);
     avio_rb32(pb); /* preferred scale */
 
