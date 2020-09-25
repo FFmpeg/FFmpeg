92a47b7a82ac6c0869e7e072fad1dc9bfd322357
Michael Niedermayer <michael@niedermayer.cc>
2020-09-25 20:08:37 +0200

    avcodec/dxtory: Fix negative shift in dx2_decode_slice_410()
    Fixes: left shift of negative value -768
    Fixes: 25574/clusterfuzz-testcase-minimized-ffmpeg_AV_CODEC_ID_DXTORY_fuzzer-6012596027916288
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    Reviewed-by: Paul B Mahol <onemda@gmail.com>
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit abebd87764992dc22c82802bdc75d40aac14ab86)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/dxtory.c
+++ libavcodec/dxtory.c
@@ -399,7 +399,7 @@ static int dx2_decode_slice_410(GetBitContext *gb, int width, int height,
             V[x >> 2] = decode_sym(gb, lru[2]) ^ 0x80;
         }
 
-        Y += ystride << 2;
+        Y += ystride * 4;
         U += ustride;
         V += vstride;
     }
