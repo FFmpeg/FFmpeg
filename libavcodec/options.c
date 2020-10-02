0191f2d29c2a62b8699ac75f09708187e943814c
James Almer <jamrial@gmail.com>
2020-10-01 22:43:03 -0300

    avcodec/options: add missing FF_API_COPY_CONTEXT checks
    Signed-off-by: James Almer <jamrial@gmail.com>

--- libavcodec/options.c
+++ libavcodec/options.c
@@ -327,6 +327,7 @@ static const AVOption frame_options[]={
 {NULL},
 };
 
+#if FF_API_COPY_CONTEXT
 static const AVClass av_frame_class = {
     .class_name              = "AVFrame",
     .item_name               = NULL,
@@ -338,6 +339,7 @@ const AVClass *avcodec_get_frame_class(void)
 {
     return &av_frame_class;
 }
+#endif
 
 #define SROFFSET(x) offsetof(AVSubtitleRect,x)
 
@@ -352,6 +354,7 @@ static const AVOption subtitle_rect_options[]={
 {NULL},
 };
 
+#if FF_API_COPY_CONTEXT
 static const AVClass av_subtitle_rect_class = {
     .class_name             = "AVSubtitleRect",
     .item_name              = NULL,
@@ -363,3 +366,4 @@ const AVClass *avcodec_get_subtitle_rect_class(void)
 {
     return &av_subtitle_rect_class;
 }
+#endif
