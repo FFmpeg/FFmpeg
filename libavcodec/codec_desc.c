8e74d98a29a31c47836248d35cdb198e75a6fc37
hwren <hwrenx@126.com>
2020-10-05 20:07:01 +0800

    lavc: add AVS3 codec id and desc
    Signed-off-by: hbj <hanbj@pku.edu.cn>
    Signed-off-by: hwren <hwrenx@126.com>

--- libavcodec/codec_desc.c
+++ libavcodec/codec_desc.c
@@ -1413,6 +1413,13 @@ static const AVCodecDescriptor codec_descriptors[] = {
         .props     = AV_CODEC_PROP_INTRA_ONLY | AV_CODEC_PROP_LOSSLESS,
     },
     {
+        .id        = AV_CODEC_ID_AVS3,
+        .type      = AVMEDIA_TYPE_VIDEO,
+        .name      = "avs3",
+        .long_name = NULL_IF_CONFIG_SMALL("AVS3-P2/IEEE1857.10"),
+        .props     = AV_CODEC_PROP_LOSSY,
+    },
+    {
         .id        = AV_CODEC_ID_Y41P,
         .type      = AVMEDIA_TYPE_VIDEO,
         .name      = "y41p",
