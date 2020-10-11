FFmpeg/FFmpeg

commit ca49476ace90ddebc5f92d9d82297f77e528c21e

avcodec/adpcm: take into account block_align when decoding ADPCM_PSX
Should reduce decoding overhead.

libavcodec/adpcm.c
@@ -1966,11 +1966,13 @@ static int adpcm_decode_frame(AVCodecContext *avctx, void *data,
         }
         break;
     case AV_CODEC_ID_ADPCM_PSX:
+        for (int block = 0; block < avpkt->size / FFMAX(avctx->block_align, 16 * avctx->channels); block++) {
+            int nb_samples_per_block = 28 * FFMAX(avctx->block_align, 16 * avctx->channels) / (16 * avctx->channels);
         for (channel = 0; channel < avctx->channels; channel++) {
-            samples = samples_p[channel];
+            samples = samples_p[channel] + block * nb_samples_per_block;
 
             /* Read in every sample for this channel.  */
-            for (i = 0; i < nb_samples / 28; i++) {
+            for (i = 0; i < nb_samples_per_block / 28; i++) {
                 int filter, shift, flag, byte;
 
                 filter = bytestream2_get_byteu(&gb);
@@ -2000,6 +2002,7 @@ static int adpcm_decode_frame(AVCodecContext *avctx, void *data,
                     c->status[channel].sample1 = sample;
                 }
             }
+        }
         }
         break;
     case AV_CODEC_ID_ADPCM_ARGO:

commit 991246657b436d54d8f72c57fcc4888b835bb7c9

avcodec/adpcm: Check block align for AV_CODEC_ID_ADPCM_PSX
Regression since: ca49476ace90ddebc5f92d9d82297f77e528c21e
Fixes: out of array write
Fixes: 25786/clusterfuzz-testcase-minimized-ffmpeg_AV_CODEC_ID_ADPCM_PSX_fuzzer-5704869380620288

Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

libavcodec/adpcm.c
@@ -135,6 +135,8 @@ static av_cold int adpcm_decode_init(AVCodecContext * avctx)
         break;
     case AV_CODEC_ID_ADPCM_PSX:
         max_channels = 8;
+        if (avctx->block_align % (16 * avctx->channels))
+            return AVERROR_INVALIDDATA;
         break;
     case AV_CODEC_ID_ADPCM_IMA_DAT4:
     case AV_CODEC_ID_ADPCM_THP:
@@ -1968,6 +1970,7 @@ static int adpcm_decode_frame(AVCodecContext *avctx, void *data,
             int nb_samples_per_block = 28 * FFMAX(avctx->block_align, 16 * avctx->channels) / (16 * avctx->channels);
             for (channel = 0; channel < avctx->channels; channel++) {
                 samples = samples_p[channel] + block * nb_samples_per_block;
+                av_assert0((block + 1) * nb_samples_per_block <= nb_samples);
 
                 /* Read in every sample for this channel.  */
                 for (i = 0; i < nb_samples_per_block / 28; i++) {