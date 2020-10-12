5b115c2cbe39e198b35730a9df67a48d43a96efe
Michael Niedermayer <michael@niedermayer.cc>
2020-10-12 17:11:27 +0200

    avcodec/aacdec_fixed: Limit index in vector_pow43()
    Fixes: out of array access
    Fixes: 26087/clusterfuzz-testcase-minimized-ffmpeg_AV_CODEC_ID_AAC_FIXED_fuzzer-5724825462767616
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit 4f83a536384afda45acb6d7cdd22017c8c314f9e)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/aacdec_fixed.c
+++ libavcodec/aacdec_fixed.c
@@ -155,9 +155,9 @@ static void vector_pow43(int *coefs, int len)
     for (i=0; i<len; i++) {
         coef = coefs[i];
         if (coef < 0)
-            coef = -(int)ff_cbrt_tab_fixed[-coef];
+            coef = -(int)ff_cbrt_tab_fixed[(-coef) & 8191];
         else
-            coef = (int)ff_cbrt_tab_fixed[coef];
+            coef =  (int)ff_cbrt_tab_fixed[  coef  & 8191];
         coefs[i] = coef;
     }
 }
