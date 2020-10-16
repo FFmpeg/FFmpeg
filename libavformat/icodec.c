04b4479da8a2a730c3cb75d0704c4e0f3ed7916a
Michael Niedermayer <michael@niedermayer.cc>
2020-10-16 19:05:23 +0200

    avformat/icodec: Change order of operations to avoid NULL dereference
    Fixes: SEGV on unknown address 0x000000000000
    Fixes: 26379/clusterfuzz-testcase-minimized-ffmpeg_dem_ICO_fuzzer-5709011753893888
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    Reviewed-by: Peter Ross
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit 3300f5c133650ba25f94531d40ecc94c79b84457)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavformat/icodec.c
+++ libavformat/icodec.c
@@ -156,11 +156,13 @@ static int read_packet(AVFormatContext *s, AVPacket *pkt)
     IcoDemuxContext *ico = s->priv_data;
     IcoImage *image;
     AVIOContext *pb = s->pb;
-    AVStream *st = s->streams[0];
+    AVStream *st;
     int ret;
 
     if (ico->current_image >= ico->nb_images)
         return AVERROR_EOF;
+
+    st = s->streams[0];
 
     image = &ico->images[ico->current_image];
 
