87f0c8280c7556b52b72b9379547eed77e9810d7
Anton Khirnov <anton@khirnov.net>
2020-10-25 15:50:55 +0100

    ffmpeg.c: stop accessing private AVStream.codec_info_nb_frames
    Use AVSTREAM_EVENT_FLAG_NEW_PACKETS instead, which should provide the
    same information in this case.

--- fftools/ffmpeg_opt.c
+++ fftools/ffmpeg_opt.c
@@ -2215,7 +2215,8 @@ static int open_output_file(OptionsContext *o, const char *filename)
             for (i = 0; i < nb_input_streams; i++) {
                 int score;
                 ist = input_streams[i];
-                score = ist->st->codecpar->width * ist->st->codecpar->height + 100000000*!!ist->st->codec_info_nb_frames
+                score = ist->st->codecpar->width * ist->st->codecpar->height
+                           + 100000000 * !!(ist->st->event_flags & AVSTREAM_EVENT_FLAG_NEW_PACKETS)
                            + 5000000*!!(ist->st->disposition & AV_DISPOSITION_DEFAULT);
                 if (ist->user_set_discard == AVDISCARD_ALL)
                     continue;
