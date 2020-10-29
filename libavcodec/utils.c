2433b62f7980b8f98d129f5b998103ad736a2b59
Zane van Iperen <zane@zanevaniperen.com>
2020-10-29 20:55:41 +1000

    avcodec/utils: add adpcm_ima_alp to av_get_exact_bits_per_sample()
    Signed-off-by: Zane van Iperen <zane@zanevaniperen.com>

--- libavcodec/utils.c
+++ libavcodec/utils.c
@@ -1490,6 +1490,7 @@ int av_get_exact_bits_per_sample(enum AVCodecID codec_id)
     case AV_CODEC_ID_8SVX_FIB:
     case AV_CODEC_ID_ADPCM_ARGO:
     case AV_CODEC_ID_ADPCM_CT:
+    case AV_CODEC_ID_ADPCM_IMA_ALP:
     case AV_CODEC_ID_ADPCM_IMA_APC:
     case AV_CODEC_ID_ADPCM_IMA_APM:
     case AV_CODEC_ID_ADPCM_IMA_EA_SEAD:
