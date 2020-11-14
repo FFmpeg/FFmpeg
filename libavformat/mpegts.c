ca55240b8c1fd4cfdb61f88fd2cb378d475d910a
Marton Balint <cus@passwd.hu>
2020-11-15 00:31:11 +0100

    avformat/mpegts: make sure mpegts_read_header always stops at the first pmt
    mpegts_read_header stops parsing the file at the first PMT. However the check
    that ensured this was wrong because streams can also be added before the first
    PMT is received (e.g. EIT).
    
    So let's make sure we are in the header reading phase by checking if ts->pkt is
    unset instead of checking if the number of streams found so far is 0.
    
    Signed-off-by: Marton Balint <cus@passwd.hu>
    (cherry picked from commit bf19833ae26b054a111de79b5ab1681c00cd8d0a)

--- libavformat/mpegts.c
+++ libavformat/mpegts.c
@@ -2352,7 +2352,7 @@ static void pmt_cb(MpegTSFilter *filter, const uint8_t *section, int section_len
         goto out;
 
     // stop parsing after pmt, we found header
-    if (!ts->stream->nb_streams)
+    if (!ts->pkt)
         ts->stop_parse = 2;
 
     set_pmt_found(ts, h->id);
