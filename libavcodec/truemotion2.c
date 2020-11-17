0339af05a4eaa1adbf153b7fa2213eea7e531573
Andreas Rheinhardt <andreas.rheinhardt@gmail.com>
2020-11-17 11:22:09 +0100

    avcodec/truemotion2: Mark decoder as init-threadsafe
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@gmail.com>

--- libavcodec/truemotion2.c
+++ libavcodec/truemotion2.c
@@ -1022,5 +1022,5 @@ AVCodec ff_truemotion2_decoder = {
     .close          = decode_end,
     .decode         = decode_frame,
     .capabilities   = AV_CODEC_CAP_DR1,
-    .caps_internal  = FF_CODEC_CAP_INIT_CLEANUP,
+    .caps_internal  = FF_CODEC_CAP_INIT_CLEANUP | FF_CODEC_CAP_INIT_THREADSAFE,
 };
