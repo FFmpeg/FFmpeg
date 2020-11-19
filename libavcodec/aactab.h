c4b4cd775af0f4b83c34fd595c05d2fa99018323
Andreas Rheinhardt <andreas.rheinhardt@gmail.com>
2020-11-19 16:50:44 +0100

    avcodec/aactab: Fix conflicting types for ff_aac_codebook*
    The header missed that these are arrays of const pointers (to const
    data).
    
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@gmail.com>

--- libavcodec/aactab.h
+++ libavcodec/aactab.h
@@ -167,9 +167,9 @@ extern const uint16_t * const ff_aac_spectral_codes[11];
 extern const uint8_t  * const ff_aac_spectral_bits [11];
 extern const uint16_t  ff_aac_spectral_sizes[11];
 
-extern const float *ff_aac_codebook_vectors[];
-extern const float *ff_aac_codebook_vector_vals[];
-extern const uint16_t *ff_aac_codebook_vector_idx[];
+extern const float *const ff_aac_codebook_vectors[];
+extern const float *const ff_aac_codebook_vector_vals[];
+extern const uint16_t *const ff_aac_codebook_vector_idx[];
 
 extern const uint16_t * const ff_swb_offset_1024[13];
 extern const uint16_t * const ff_swb_offset_960 [13];
