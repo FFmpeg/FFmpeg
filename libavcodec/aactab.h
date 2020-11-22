8c9404b8d27b8f2c187709d5c4fa51e75c918a0b
Andreas Rheinhardt <andreas.rheinhardt@gmail.com>
2020-11-22 20:00:30 +0100

    avcodec/aactab: Remove declaration of inexistent array
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@gmail.com>

--- libavcodec/aactab.h
+++ libavcodec/aactab.h
@@ -95,7 +95,6 @@ DECLARE_ALIGNED(32, extern float,  ff_aac_kbd_short_128)[128];
 DECLARE_ALIGNED(32, extern float,  ff_aac_kbd_long_960)[960];
 DECLARE_ALIGNED(32, extern float,  ff_aac_kbd_short_120)[120];
 DECLARE_ALIGNED(32, extern int,    ff_aac_kbd_long_1024_fixed)[1024];
-DECLARE_ALIGNED(32, extern int,    ff_aac_kbd_long_512_fixed)[512];
 DECLARE_ALIGNED(32, extern int,    ff_aac_kbd_short_128_fixed)[128];
 DECLARE_ALIGNED(32, extern const float, ff_aac_eld_window_512)[1920];
 DECLARE_ALIGNED(32, extern const int,   ff_aac_eld_window_512_fixed)[1920];
