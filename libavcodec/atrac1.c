7fd865834748b4d7edf47133c9d9887349cb6161
Andreas Rheinhardt <andreas.rheinhardt@gmail.com>
2020-11-22 17:32:26 +0100

    avcodec/atrac1: Mark decoder as init-threadsafe
    Possible because ff_init_ff_sine_windows() is now threadsafe.
    
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@gmail.com>

--- libavcodec/atrac1.c
+++ libavcodec/atrac1.c
@@ -392,5 +392,5 @@ AVCodec ff_atrac1_decoder = {
     .capabilities   = AV_CODEC_CAP_DR1,
     .sample_fmts    = (const enum AVSampleFormat[]) { AV_SAMPLE_FMT_FLTP,
                                                       AV_SAMPLE_FMT_NONE },
-    .caps_internal  = FF_CODEC_CAP_INIT_CLEANUP,
+    .caps_internal  = FF_CODEC_CAP_INIT_THREADSAFE | FF_CODEC_CAP_INIT_CLEANUP,
 };
