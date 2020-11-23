f0f28626c0ce64f3c7e9e59df0c139e9cffbdbfa
Andreas Rheinhardt <andreas.rheinhardt@gmail.com>
2020-11-23 03:49:35 +0100

    avcodec/asvdec: Reindentation
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@gmail.com>

--- libavcodec/asvdec.c
+++ libavcodec/asvdec.c
@@ -48,21 +48,21 @@ static VLC asv2_level_vlc;
 
 static av_cold void init_vlcs(void)
 {
-        INIT_VLC_STATIC(&ccp_vlc, CCP_VLC_BITS, 17,
-                        &ff_asv_ccp_tab[0][1], 2, 1,
-                        &ff_asv_ccp_tab[0][0], 2, 1, 32);
-        INIT_LE_VLC_STATIC(&dc_ccp_vlc, DC_CCP_VLC_BITS, 8,
-                           &ff_asv_dc_ccp_tab[0][1], 2, 1,
-                           &ff_asv_dc_ccp_tab[0][0], 2, 1, 16);
-        INIT_LE_VLC_STATIC(&ac_ccp_vlc, AC_CCP_VLC_BITS, 16,
-                           &ff_asv_ac_ccp_tab[0][1], 2, 1,
-                           &ff_asv_ac_ccp_tab[0][0], 2, 1, 64);
-        INIT_VLC_STATIC(&level_vlc,      ASV1_LEVEL_VLC_BITS, 7,
-                        &ff_asv_level_tab[0][1], 2, 1,
-                        &ff_asv_level_tab[0][0], 2, 1, 16);
-        INIT_LE_VLC_STATIC(&asv2_level_vlc, ASV2_LEVEL_VLC_BITS, 63,
-                           &ff_asv2_level_tab[0][1], 4, 2,
-                           &ff_asv2_level_tab[0][0], 4, 2, 1024);
+    INIT_VLC_STATIC(&ccp_vlc, CCP_VLC_BITS, 17,
+                    &ff_asv_ccp_tab[0][1], 2, 1,
+                    &ff_asv_ccp_tab[0][0], 2, 1, 32);
+    INIT_LE_VLC_STATIC(&dc_ccp_vlc, DC_CCP_VLC_BITS, 8,
+                       &ff_asv_dc_ccp_tab[0][1], 2, 1,
+                       &ff_asv_dc_ccp_tab[0][0], 2, 1, 16);
+    INIT_LE_VLC_STATIC(&ac_ccp_vlc, AC_CCP_VLC_BITS, 16,
+                       &ff_asv_ac_ccp_tab[0][1], 2, 1,
+                       &ff_asv_ac_ccp_tab[0][0], 2, 1, 64);
+    INIT_VLC_STATIC(&level_vlc, ASV1_LEVEL_VLC_BITS, 7,
+                    &ff_asv_level_tab[0][1], 2, 1,
+                    &ff_asv_level_tab[0][0], 2, 1, 16);
+    INIT_LE_VLC_STATIC(&asv2_level_vlc, ASV2_LEVEL_VLC_BITS, 63,
+                       &ff_asv2_level_tab[0][1], 4, 2,
+                       &ff_asv2_level_tab[0][0], 4, 2, 1024);
 }
 
 static inline int asv1_get_level(GetBitContext *gb)
