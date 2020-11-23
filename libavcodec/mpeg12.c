0e3272d9a47bb672f2e320c778d4849be471703b
Andreas Rheinhardt <andreas.rheinhardt@gmail.com>
2020-11-23 03:12:23 +0100

    avcodec/mpeg12: Reindentation
    Reviewed-by: Anton Khirnov <anton@khirnov.net>
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@gmail.com>

--- libavcodec/mpeg12.c
+++ libavcodec/mpeg12.c
@@ -135,31 +135,31 @@ VLC ff_mb_pat_vlc;
 
 static av_cold void mpeg12_init_vlcs(void)
 {
-        INIT_VLC_STATIC(&ff_dc_lum_vlc, DC_VLC_BITS, 12,
-                        ff_mpeg12_vlc_dc_lum_bits, 1, 1,
-                        ff_mpeg12_vlc_dc_lum_code, 2, 2, 512);
-        INIT_VLC_STATIC(&ff_dc_chroma_vlc,  DC_VLC_BITS, 12,
-                        ff_mpeg12_vlc_dc_chroma_bits, 1, 1,
-                        ff_mpeg12_vlc_dc_chroma_code, 2, 2, 514);
-        INIT_VLC_STATIC(&ff_mv_vlc, MV_VLC_BITS, 17,
-                        &ff_mpeg12_mbMotionVectorTable[0][1], 2, 1,
-                        &ff_mpeg12_mbMotionVectorTable[0][0], 2, 1, 266);
-        INIT_VLC_STATIC(&ff_mbincr_vlc, MBINCR_VLC_BITS, 36,
-                        &ff_mpeg12_mbAddrIncrTable[0][1], 2, 1,
-                        &ff_mpeg12_mbAddrIncrTable[0][0], 2, 1, 538);
-        INIT_VLC_STATIC(&ff_mb_pat_vlc, MB_PAT_VLC_BITS, 64,
-                        &ff_mpeg12_mbPatTable[0][1], 2, 1,
-                        &ff_mpeg12_mbPatTable[0][0], 2, 1, 512);
+    INIT_VLC_STATIC(&ff_dc_lum_vlc, DC_VLC_BITS, 12,
+                    ff_mpeg12_vlc_dc_lum_bits, 1, 1,
+                    ff_mpeg12_vlc_dc_lum_code, 2, 2, 512);
+    INIT_VLC_STATIC(&ff_dc_chroma_vlc,  DC_VLC_BITS, 12,
+                    ff_mpeg12_vlc_dc_chroma_bits, 1, 1,
+                    ff_mpeg12_vlc_dc_chroma_code, 2, 2, 514);
+    INIT_VLC_STATIC(&ff_mv_vlc, MV_VLC_BITS, 17,
+                    &ff_mpeg12_mbMotionVectorTable[0][1], 2, 1,
+                    &ff_mpeg12_mbMotionVectorTable[0][0], 2, 1, 266);
+    INIT_VLC_STATIC(&ff_mbincr_vlc, MBINCR_VLC_BITS, 36,
+                    &ff_mpeg12_mbAddrIncrTable[0][1], 2, 1,
+                    &ff_mpeg12_mbAddrIncrTable[0][0], 2, 1, 538);
+    INIT_VLC_STATIC(&ff_mb_pat_vlc, MB_PAT_VLC_BITS, 64,
+                    &ff_mpeg12_mbPatTable[0][1], 2, 1,
+                    &ff_mpeg12_mbPatTable[0][0], 2, 1, 512);
 
-        INIT_VLC_STATIC(&ff_mb_ptype_vlc, MB_PTYPE_VLC_BITS, 7,
-                        &table_mb_ptype[0][1], 2, 1,
-                        &table_mb_ptype[0][0], 2, 1, 64);
-        INIT_VLC_STATIC(&ff_mb_btype_vlc, MB_BTYPE_VLC_BITS, 11,
-                        &table_mb_btype[0][1], 2, 1,
-                        &table_mb_btype[0][0], 2, 1, 64);
+    INIT_VLC_STATIC(&ff_mb_ptype_vlc, MB_PTYPE_VLC_BITS, 7,
+                    &table_mb_ptype[0][1], 2, 1,
+                    &table_mb_ptype[0][0], 2, 1, 64);
+    INIT_VLC_STATIC(&ff_mb_btype_vlc, MB_BTYPE_VLC_BITS, 11,
+                    &table_mb_btype[0][1], 2, 1,
+                    &table_mb_btype[0][0], 2, 1, 64);
 
-        INIT_2D_VLC_RL(ff_rl_mpeg1, 680, 0);
-        INIT_2D_VLC_RL(ff_rl_mpeg2, 674, 0);
+    INIT_2D_VLC_RL(ff_rl_mpeg1, 680, 0);
+    INIT_2D_VLC_RL(ff_rl_mpeg2, 674, 0);
 }
 
 av_cold void ff_mpeg12_init_vlcs(void)
