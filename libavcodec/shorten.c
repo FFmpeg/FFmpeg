7b9f0b80593d9b33e08106947bd432303848ce12
Andreas Rheinhardt <andreas.rheinhardt@gmail.com>
2020-11-26 19:18:04 +0100

    avcodec/shorten: Mark decoder as init-threadsafe
    Reviewed-by: Anton Khirnov <anton@khirnov.net>
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@gmail.com>

--- libavcodec/shorten.c
+++ libavcodec/shorten.c
@@ -815,4 +815,5 @@ AVCodec ff_shorten_decoder = {
     .sample_fmts    = (const enum AVSampleFormat[]) { AV_SAMPLE_FMT_S16P,
                                                       AV_SAMPLE_FMT_U8P,
                                                       AV_SAMPLE_FMT_NONE },
+    .caps_internal  = FF_CODEC_CAP_INIT_THREADSAFE,
 };
