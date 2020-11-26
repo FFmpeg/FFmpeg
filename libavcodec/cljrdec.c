b143f8c26ac4da3a08ca78d7c586c9cc1ba408aa
Andreas Rheinhardt <andreas.rheinhardt@gmail.com>
2020-11-26 17:11:03 +0100

    avcodec/cljrdec: Mark decoder as init-threadsafe
    Reviewed-by: Anton Khirnov <anton@khirnov.net>
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@gmail.com>

--- libavcodec/cljrdec.c
+++ libavcodec/cljrdec.c
@@ -90,5 +90,6 @@ AVCodec ff_cljr_decoder = {
     .init           = decode_init,
     .decode         = decode_frame,
     .capabilities   = AV_CODEC_CAP_DR1,
+    .caps_internal  = FF_CODEC_CAP_INIT_THREADSAFE,
 };
 
