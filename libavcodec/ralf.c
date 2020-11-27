543a2c3784450f1fddd70e7591ff503e06b45d1e
Andreas Rheinhardt <andreas.rheinhardt@gmail.com>
2020-11-27 13:42:40 +0100

    avcodec/ralf: Mark decoder as init-threadsafe
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@gmail.com>

--- libavcodec/ralf.c
+++ libavcodec/ralf.c
@@ -539,4 +539,5 @@ const AVCodec ff_ralf_decoder = {
                       AV_CODEC_CAP_DR1,
     .sample_fmts    = (const enum AVSampleFormat[]) { AV_SAMPLE_FMT_S16P,
                                                       AV_SAMPLE_FMT_NONE },
+    .caps_internal  = FF_CODEC_CAP_INIT_THREADSAFE,
 };
