db09d3ed17f19a1cbbd951a1c70105c2297c6768
Andreas Rheinhardt <andreas.rheinhardt@gmail.com>
2020-11-27 13:54:05 +0100

    avcodec/roqaudioenc: Mark encoder as init-threadsafe
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@gmail.com>

--- libavcodec/roqaudioenc.c
+++ libavcodec/roqaudioenc.c
@@ -195,4 +195,5 @@ const AVCodec ff_roq_dpcm_encoder = {
     .capabilities   = AV_CODEC_CAP_DELAY,
     .sample_fmts    = (const enum AVSampleFormat[]){ AV_SAMPLE_FMT_S16,
                                                      AV_SAMPLE_FMT_NONE },
+    .caps_internal  = FF_CODEC_CAP_INIT_THREADSAFE,
 };
