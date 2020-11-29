50734d7676f785caaaf4e09fa951f5766398bee5
Andreas Rheinhardt <andreas.rheinhardt@gmail.com>
2020-11-30 00:13:03 +0100

    avcodec/dsicinaudio: Mark decoder as init-threadsafe
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@gmail.com>

--- libavcodec/dsicinaudio.c
+++ libavcodec/dsicinaudio.c
@@ -130,4 +130,5 @@ const AVCodec ff_dsicinaudio_decoder = {
     .init           = cinaudio_decode_init,
     .decode         = cinaudio_decode_frame,
     .capabilities   = AV_CODEC_CAP_DR1 | AV_CODEC_CAP_CHANNEL_CONF,
+    .caps_internal  = FF_CODEC_CAP_INIT_THREADSAFE,
 };
