abe61c349d7368c38dde37da1d6aee091129d461
Andreas Rheinhardt <andreas.rheinhardt@gmail.com>
2020-11-29 18:08:09 +0100

    avcodec/iff: Mark decoder as init-threadsafe
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@gmail.com>

--- libavcodec/iff.c
+++ libavcodec/iff.c
@@ -1911,7 +1911,7 @@ const AVCodec ff_iff_ilbm_decoder = {
     .init           = decode_init,
     .close          = decode_end,
     .decode         = decode_frame,
-    .caps_internal  = FF_CODEC_CAP_INIT_CLEANUP,
     .capabilities   = AV_CODEC_CAP_DR1,
+    .caps_internal  = FF_CODEC_CAP_INIT_THREADSAFE | FF_CODEC_CAP_INIT_CLEANUP,
 };
 #endif
