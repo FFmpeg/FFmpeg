af815d4bea1898b96cf1da0a8738fbae73a34a02
Andreas Rheinhardt <andreas.rheinhardt@gmail.com>
2020-11-30 20:21:04 +0100

    avcodec/vmnc: Mark decoder as init-threadsafe
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@gmail.com>

--- libavcodec/vmnc.c
+++ libavcodec/vmnc.c
@@ -582,4 +582,5 @@ const AVCodec ff_vmnc_decoder = {
     .close          = decode_end,
     .decode         = decode_frame,
     .capabilities   = AV_CODEC_CAP_DR1,
+    .caps_internal  = FF_CODEC_CAP_INIT_THREADSAFE,
 };
