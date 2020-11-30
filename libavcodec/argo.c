c549a94b9d2e8501d0ed809d0014483c05a1f8d5
Andreas Rheinhardt <andreas.rheinhardt@gmail.com>
2020-11-30 17:23:40 +0100

    avcodec/argo: Mark decoder as init-threadsafe
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@gmail.com>

--- libavcodec/argo.c
+++ libavcodec/argo.c
@@ -736,5 +736,5 @@ const AVCodec ff_argo_decoder = {
     .flush          = decode_flush,
     .close          = decode_close,
     .capabilities   = AV_CODEC_CAP_DR1,
-    .caps_internal  = FF_CODEC_CAP_INIT_CLEANUP,
+    .caps_internal  = FF_CODEC_CAP_INIT_THREADSAFE | FF_CODEC_CAP_INIT_CLEANUP,
 };
