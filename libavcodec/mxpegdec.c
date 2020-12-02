506406b8034b782d0ef93ba50dfe1b6386471290
Michael Niedermayer <michael@niedermayer.cc>
2020-12-03 00:31:07 +0100

    avcodec/mxpegdec: fix SOF counting
    Fixes: Timeout (>10sec -> 15ms)
    Fixes: 27652/clusterfuzz-testcase-minimized-ffmpeg_AV_CODEC_ID_MXPEG_fuzzer-5125920868007936
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit 401495def62638a205569cac0f7861c7faba4d18)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/mxpegdec.c
+++ libavcodec/mxpegdec.c
@@ -247,16 +247,17 @@ static int mxpeg_decode_frame(AVCodecContext *avctx,
                            "Multiple SOF in a frame\n");
                     return AVERROR_INVALIDDATA;
                 }
-                s->got_sof_data = 0;
                 ret = ff_mjpeg_decode_sof(jpg);
                 if (ret < 0) {
                     av_log(avctx, AV_LOG_ERROR,
                            "SOF data decode error\n");
+                    s->got_sof_data = 0;
                     return ret;
                 }
                 if (jpg->interlaced) {
                     av_log(avctx, AV_LOG_ERROR,
                            "Interlaced mode not supported in MxPEG\n");
+                    s->got_sof_data = 0;
                     return AVERROR(EINVAL);
                 }
                 s->got_sof_data ++;
