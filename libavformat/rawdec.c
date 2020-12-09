7ab5192260c387a10998481fe1468f1304f6ebd2
Carl Eugen Hoyos <ceffmpeg@gmail.com>
2020-12-09 23:42:33 +0100

    lavf/rawdec: Return a low score if the mjpeg probe function detected one frame.
    Fixes ticket #9026.

--- libavformat/rawdec.c
+++ libavformat/rawdec.c
@@ -208,6 +208,8 @@ static int mjpeg_probe(const AVProbeData *p)
             return AVPROBE_SCORE_EXTENSION / 2;
         return AVPROBE_SCORE_EXTENSION / 4;
     }
+    if (!nb_invalid && nb_frames)
+        return AVPROBE_SCORE_EXTENSION / 4;
 
     return 0;
 }
