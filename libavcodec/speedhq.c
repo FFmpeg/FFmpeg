eea2638e9af9e65198d7002db5c3bd5a14b04bfc
Andreas Rheinhardt <andreas.rheinhardt@gmail.com>
2020-12-10 09:41:07 +0100

    configure, libavcodec/speedhq: Fix compiling SpeedHQ encoder
    Reviewed-by: Paul B Mahol <onemda@gmail.com>
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@gmail.com>

--- libavcodec/speedhq.c
+++ libavcodec/speedhq.c
@@ -26,6 +26,7 @@
 
 #define BITSTREAM_READER_LE
 
+#include "config.h"
 #include "libavutil/attributes.h"
 
 #include "avcodec.h"
@@ -140,6 +141,7 @@ RLTable ff_rl_speedhq = {
     speedhq_level,
 };
 
+#if CONFIG_SPEEDHQ_DECODER
 /* NOTE: The first element is always 16, unscaled. */
 static const uint8_t unscaled_quant_matrix[64] = {
     16, 16, 19, 22, 26, 27, 29, 34,
@@ -656,3 +658,4 @@ AVCodec ff_speedhq_decoder = {
     .decode         = speedhq_decode_frame,
     .capabilities   = AV_CODEC_CAP_DR1,
 };
+#endif /* CONFIG_SPEEDHQ_DECODER */
