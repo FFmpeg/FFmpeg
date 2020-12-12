ec231f9662235405fe6d19e12b4d2d4d983d1623
Michael Niedermayer <michael@niedermayer.cc>
2020-12-12 22:19:22 +0100

    avformat/aadec: Check for EOF while reading chapters
    Fixes: timeout
    Fixes: 28199/clusterfuzz-testcase-minimized-ffmpeg_dem_AA_fuzzer-4896162657861632
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit bcc7d14453ea2bafa6569a07002943808f2a396a)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavformat/aadec.c
+++ libavformat/aadec.c
@@ -223,7 +223,8 @@ static int aa_read_header(AVFormatContext *s)
     while ((chapter_pos = avio_tell(pb)) >= 0 && chapter_pos < c->content_end) {
         int chapter_idx = s->nb_chapters;
         uint32_t chapter_size = avio_rb32(pb);
-        if (chapter_size == 0) break;
+        if (chapter_size == 0 || avio_feof(pb))
+            break;
         chapter_pos -= start + CHAPTER_HEADER_SIZE * chapter_idx;
         avio_skip(pb, 4 + chapter_size);
         if (!avpriv_new_chapter(s, chapter_idx, st->time_base,
