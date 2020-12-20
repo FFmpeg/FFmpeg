6d3b70c27ef1639784fdb3382e5a06b1afa3fe3e
Marton Balint <cus@passwd.hu>
2020-12-20 19:32:56 +0100

    avfilter/vf_framerate: fix infinite loop with 1-frame input
    Fixes infinite loop in:
    ffmpeg -f lavfi -i testsrc=d=0.04 -vf framerate=50 -f null none
    
    Signed-off-by: Marton Balint <cus@passwd.hu>

--- libavfilter/vf_framerate.c
+++ libavfilter/vf_framerate.c
@@ -170,7 +170,9 @@ static int process_work_frame(AVFilterContext *ctx)
         return 0;
 
     if (!s->f0) {
-        s->work = av_frame_clone(s->f1);
+        av_assert1(s->flush);
+        s->work = s->f1;
+        s->f1 = NULL;
     } else {
         if (work_pts >= s->pts1 + s->delta && s->flush)
             return 0;
