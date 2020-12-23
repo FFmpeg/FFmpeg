f975cf7cc061b814bc029bb2fc86f656494c4d5c
Paul B Mahol <onemda@gmail.com>
2020-12-23 11:59:51 +0100

    avcodec/cfhd: only increase s->level if transform is supported

--- libavcodec/cfhd.c
+++ libavcodec/cfhd.c
@@ -436,7 +436,7 @@ static int cfhd_decode(AVCodecContext *avctx, void *data, int *got_frame,
             }
             init_plane_defaults(s);
         } else if (tag == SubbandNumber) {
-            if (s->subband_num != 0 && data == 1)  // hack
+            if (s->subband_num != 0 && data == 1 && (s->transform_type == 0 || s->transform_type == 2))  // hack
                 s->level++;
             av_log(avctx, AV_LOG_DEBUG, "Subband number %"PRIu16"\n", data);
             s->subband_num = data;
