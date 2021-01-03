c51887c56ad3dc1eb0ea23196f9a3db6335a945e
Andreas Rheinhardt <andreas.rheinhardt@gmail.com>
2021-01-03 16:49:36 +0100

    avfilter/af_asupercut: Deduplicate options
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@gmail.com>

--- libavfilter/af_asupercut.c
+++ libavfilter/af_asupercut.c
@@ -405,7 +405,7 @@ AVFilter ff_af_asubcut = {
                        AVFILTER_FLAG_SLICE_THREADS,
 };
 
-static const AVOption asuperpass_options[] = {
+static const AVOption asuperpass_asuperstop_options[] = {
     { "centerf","set center frequency", OFFSET(cutoff), AV_OPT_TYPE_DOUBLE, {.dbl=1000}, 2, 999999, FLAGS },
     { "order",  "set filter order",     OFFSET(order),  AV_OPT_TYPE_INT,    {.i64=4},    4,     20, FLAGS },
     { "qfactor","set Q-factor",         OFFSET(qfactor),AV_OPT_TYPE_DOUBLE, {.dbl=1.},0.01,   100., FLAGS },
@@ -413,6 +413,7 @@ static const AVOption asuperpass_options[] = {
     { NULL }
 };
 
+#define asuperpass_options asuperpass_asuperstop_options
 AVFILTER_DEFINE_CLASS(asuperpass);
 
 AVFilter ff_af_asuperpass = {
@@ -429,14 +430,7 @@ AVFilter ff_af_asuperpass = {
                        AVFILTER_FLAG_SLICE_THREADS,
 };
 
-static const AVOption asuperstop_options[] = {
-    { "centerf","set center frequency", OFFSET(cutoff), AV_OPT_TYPE_DOUBLE, {.dbl=1000}, 2, 999999, FLAGS },
-    { "order",  "set filter order",     OFFSET(order),  AV_OPT_TYPE_INT,    {.i64=4},    4,     20, FLAGS },
-    { "qfactor","set Q-factor",         OFFSET(qfactor),AV_OPT_TYPE_DOUBLE, {.dbl=1.},0.01,   100., FLAGS },
-    { "level",  "set input level",      OFFSET(level),  AV_OPT_TYPE_DOUBLE, {.dbl=1.},   0.,    2., FLAGS },
-    { NULL }
-};
-
+#define asuperstop_options asuperpass_asuperstop_options
 AVFILTER_DEFINE_CLASS(asuperstop);
 
 AVFilter ff_af_asuperstop = {
