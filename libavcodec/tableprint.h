da607832b57607fe9221bc2ecd25ea25ef6dd3aa
Andreas Rheinhardt <andreas.rheinhardt@gmail.com>
2021-01-06 22:40:28 +0100

    avcodec/tableprint: Don't include mem_internal.h
    tableprint.h does not declare anything as aligned; it just prints
    DECLARE_ALIGNED. So it can be removed; in fact, it needs to be removed,
    because mem_internal.h includes config.h which leads to warnings when
    building with hardcoded tables enabled because of redefinitions of
    CONFIG_HARDCODED_TABLES.
    
    (Furthermore, config.h is only valid for the target, not the host,
    so HAVE_LOCAL_ALIGNED might even be wrong here.)
    
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@gmail.com>

--- libavcodec/tableprint.h
+++ libavcodec/tableprint.h
@@ -27,7 +27,6 @@
 #include <stdio.h>
 
 #include "libavutil/common.h"
-#include "libavutil/mem_internal.h"
 
 #define WRITE_1D_FUNC_ARGV(type, linebrk, fmtstr, ...)\
 void write_##type##_array(const type *data, int len)\
