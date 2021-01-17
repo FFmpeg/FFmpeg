7ec328d53ce65cdc3d35f27a2ebb4b3e3c277e09
Michael Niedermayer <michael@niedermayer.cc>
2021-01-17 22:57:28 +0100

    avformat/tta: Use 64bit intermediate for index
    Fixes: signed integer overflow: 42032 * 51092 cannot be represented in type 'int'
    Fixes: 26910/clusterfuzz-testcase-minimized-ffmpeg_dem_TTA_fuzzer-6679539648430080
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit fd61b42b4c8709a7888fa5c9cce0c19d754e39fc)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavformat/tta.c
+++ libavformat/tta.c
@@ -119,7 +119,7 @@ static int tta_read_header(AVFormatContext *s)
     for (i = 0; i < c->totalframes; i++) {
         uint32_t size = avio_rl32(s->pb);
         int r;
-        if ((r = av_add_index_entry(st, framepos, i * c->frame_size, size, 0,
+        if ((r = av_add_index_entry(st, framepos, i * (int64_t)c->frame_size, size, 0,
                                     AVINDEX_KEYFRAME)) < 0)
             return r;
         framepos += size;
