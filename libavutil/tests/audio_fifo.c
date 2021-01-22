163559ed6206155bb997ff8f283e343e778e4118
Matthieu Patou <mpatou@fb.com>
2021-01-22 15:55:11 -0800

    avutil/tests/audio_ffio: add missing header
    Needed for HAVE_BIGENDIAN
    
    Suggested-by: ffmpeg@fb.com
    Signed-off-by: James Almer <jamrial@gmail.com>

--- libavutil/tests/audio_fifo.c
+++ libavutil/tests/audio_fifo.c
@@ -15,6 +15,7 @@
  * License along with FFmpeg; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
+#include "config.h"
 
 #include <stdlib.h>
 #include <stdio.h>
