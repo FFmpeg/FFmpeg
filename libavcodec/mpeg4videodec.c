2490ac635c8d5ddb5f0ca762caa8a5c22df22920
Andreas Rheinhardt <andreas.rheinhardt@gmail.com>
2021-01-23 17:24:33 +0100

    avcodec/mpeg4videodec: Fix indentation
    It was wrong since e03bf251d8784f4d1df2c22381c902087e151e31.
    
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@gmail.com>

--- libavcodec/mpeg4videodec.c
+++ libavcodec/mpeg4videodec.c
@@ -3201,15 +3201,15 @@ static int decode_studiovisualobject(Mpeg4DecContext *ctx, GetBitContext *gb)
     MpegEncContext *s = &ctx->m;
     int visual_object_type;
 
-        skip_bits(gb, 4); /* visual_object_verid */
-        visual_object_type = get_bits(gb, 4);
-        if (visual_object_type != VOT_VIDEO_ID) {
-            avpriv_request_sample(s->avctx, "VO type %u", visual_object_type);
-            return AVERROR_PATCHWELCOME;
-        }
+    skip_bits(gb, 4); /* visual_object_verid */
+    visual_object_type = get_bits(gb, 4);
+    if (visual_object_type != VOT_VIDEO_ID) {
+        avpriv_request_sample(s->avctx, "VO type %u", visual_object_type);
+        return AVERROR_PATCHWELCOME;
+    }
 
-        next_start_code_studio(gb);
-        extension_and_user_data(s, gb, 1);
+    next_start_code_studio(gb);
+    extension_and_user_data(s, gb, 1);
 
     return 0;
 }
