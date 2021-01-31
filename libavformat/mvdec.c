ff6a6b94174ecda019d6cbeb07cdce4891db38dd
Michael Niedermayer <michael@niedermayer.cc>
2021-01-31 16:19:42 +0100

    avformat/mvdec: Check for EOF in read_index()
    Fixes: Timeout
    Fixes: 29550/clusterfuzz-testcase-minimized-ffmpeg_dem_MV_fuzzer-5094307193290752
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit 6c64351bb1f4dc148069a37754b746fcd4c784cf)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavformat/mvdec.c
+++ libavformat/mvdec.c
@@ -269,6 +269,8 @@ static void read_index(AVIOContext *pb, AVStream *st)
         uint32_t pos  = avio_rb32(pb);
         uint32_t size = avio_rb32(pb);
         avio_skip(pb, 8);
+        if (avio_feof(pb))
+            return ;
         av_add_index_entry(st, pos, timestamp, size, 0, AVINDEX_KEYFRAME);
         if (st->codecpar->codec_type == AVMEDIA_TYPE_AUDIO) {
             timestamp += size / (st->codecpar->channels * 2LL);
