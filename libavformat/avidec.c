752575d95329d08edb85ab6a5e069e498febaa53
Michael Niedermayer <michael@niedermayer.cc>
2021-01-31 16:30:59 +0100

    avformat/avidec: Use 64bit in get_duration()
    Fixes: signed integer overflow: 2147483424 + 8224 cannot be represented in type 'int'
    Fixes: 29619/clusterfuzz-testcase-minimized-ffmpeg_dem_AVI_fuzzer-5191424373030912
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit a0ceb0cdd41b56241697cd8f83e22cdb4822d2d9)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavformat/avidec.c
+++ libavformat/avidec.c
@@ -130,7 +130,7 @@ static inline int get_duration(AVIStream *ast, int len)
     if (ast->sample_size)
         return len;
     else if (ast->dshow_block_align)
-        return (len + ast->dshow_block_align - 1) / ast->dshow_block_align;
+        return (len + (int64_t)ast->dshow_block_align - 1) / ast->dshow_block_align;
     else
         return 1;
 }
