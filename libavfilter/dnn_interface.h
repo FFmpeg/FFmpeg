0884063f88a9b726b296722cabe397b12337b16a
Guo, Yejun <yejun.guo@intel.com>
2021-02-08 09:53:39 +0800

    dnn_interface.h: add enum DNNColorOrder
    the data type and order together decide the color format, we could
    not use AVPixelFormat directly because not all the possible formats
    are covered by it.
    
    Signed-off-by: Guo, Yejun <yejun.guo@intel.com>

--- libavfilter/dnn_interface.h
+++ libavfilter/dnn_interface.h
@@ -37,6 +37,11 @@ typedef enum {DNN_NATIVE, DNN_TF, DNN_OV} DNNBackendType;
 typedef enum {DNN_FLOAT = 1, DNN_UINT8 = 4} DNNDataType;
 
 typedef enum {
+    DCO_NONE,
+    DCO_BGR,
+} DNNColorOrder;
+
+typedef enum {
     DAST_FAIL,              // something wrong
     DAST_EMPTY_QUEUE,       // no more inference result to get
     DAST_NOT_READY,         // all queued inferences are not finished
@@ -52,8 +57,10 @@ typedef enum {
 
 typedef struct DNNData{
     void *data;
-    DNNDataType dt;
     int width, height, channels;
+    // dt and order together decide the color format
+    DNNDataType dt;
+    DNNColorOrder order;
 } DNNData;
 
 typedef struct DNNModel{
