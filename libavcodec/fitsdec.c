4aef642cfdd0cb9a351709a5f57fd48a4d587c9f
Paul B Mahol <onemda@gmail.com>
2021-02-08 17:45:57 +0100

    avcodec/fitsdec: properly initialize header->data_max

--- libavcodec/fitsdec.c
+++ libavcodec/fitsdec.c
@@ -63,7 +63,7 @@ static int fill_data_min_max(const uint8_t *ptr8, FITSHeader *header, const uint
     int i, j;
 
     header->data_min = DBL_MAX;
-    header->data_max = DBL_MIN;
+    header->data_max = -DBL_MAX;
     switch (header->bitpix) {
 #define CASE_N(a, t, rd) \
     case a: \
