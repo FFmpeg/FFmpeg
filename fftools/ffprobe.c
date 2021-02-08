04f49645a519233f3638104e0df5215758652fcb
Anton Khirnov <anton@khirnov.net>
2021-02-08 12:05:55 +0100

    ffprobe: drop code accessing deprecated AVStream.codec

--- fftools/ffprobe.c
+++ fftools/ffprobe.c
@@ -3050,11 +3050,6 @@ static int open_input_file(InputFile *ifile, const char *filename,
 
             ist->dec_ctx->pkt_timebase = stream->time_base;
             ist->dec_ctx->framerate = stream->avg_frame_rate;
-#if FF_API_LAVF_AVCTX
-            ist->dec_ctx->properties = stream->codec->properties;
-            ist->dec_ctx->coded_width = stream->codec->coded_width;
-            ist->dec_ctx->coded_height = stream->codec->coded_height;
-#endif
 
             if (avcodec_open2(ist->dec_ctx, codec, &opts) < 0) {
                 av_log(NULL, AV_LOG_WARNING, "Could not open codec for input stream %d\n",
