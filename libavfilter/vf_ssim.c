3300d718f7fa9788d7eee66917f382dece398af5
Paul B Mahol <onemda@gmail.com>
2021-02-12 23:15:08 +0100

    avfilter/vf_ssim: remove precision limits for metadata values

--- libavfilter/vf_ssim.c
+++ libavfilter/vf_ssim.c
@@ -83,7 +83,7 @@ FRAMESYNC_DEFINE_CLASS(ssim, SSIMContext, fs);
 static void set_meta(AVDictionary **metadata, const char *key, char comp, float d)
 {
     char value[128];
-    snprintf(value, sizeof(value), "%0.2f", d);
+    snprintf(value, sizeof(value), "%f", d);
     if (comp) {
         char key2[128];
         snprintf(key2, sizeof(key2), "%s%c", key, comp);
