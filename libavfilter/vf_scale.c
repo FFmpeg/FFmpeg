1ac57ed11219a954576ba04a8e2927e5ff8d8db3
Michael Niedermayer <michael@niedermayer.cc>
2021-02-19 21:43:45 +0100

    avfilter/vf_scale: Fix adding 0 to NULL (which is UB) in scale_slice()
    Found-by: Jeremy Leconte <jleconte@google.com>
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit 1cf96ce269364e3c2b4ec2097f121ad42b336839)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavfilter/vf_scale.c
+++ libavfilter/vf_scale.c
@@ -635,8 +635,8 @@ static int scale_slice(AVFilterLink *link, AVFrame *out_buf, AVFrame *cur_pic, s
         int vsub= ((i+1)&2) ? scale->vsub : 0;
          in_stride[i] = cur_pic->linesize[i] * mul;
         out_stride[i] = out_buf->linesize[i] * mul;
-         in[i] = cur_pic->data[i] + ((y>>vsub)+field) * cur_pic->linesize[i];
-        out[i] = out_buf->data[i] +            field  * out_buf->linesize[i];
+         in[i] = FF_PTR_ADD(cur_pic->data[i], ((y>>vsub)+field) * cur_pic->linesize[i]);
+        out[i] = FF_PTR_ADD(out_buf->data[i],            field  * out_buf->linesize[i]);
     }
     if (scale->input_is_pal)
          in[1] = cur_pic->data[1];
