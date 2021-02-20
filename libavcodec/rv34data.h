b913a4e18d3278017fcaaecd90a1c53e916b4479
Andreas Rheinhardt <andreas.rheinhardt@gmail.com>
2021-02-20 15:22:38 +0100

    avcodec/rv34data: Remove rv34_dquant_tab
    It is unused and coincides with ff_modified_quant_tab.
    
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@gmail.com>

--- libavcodec/rv34data.h
+++ libavcodec/rv34data.h
@@ -100,19 +100,6 @@ static const uint8_t rv34_quant_to_vlc_set[2][32] = {
 };
 
 /**
- * table for obtaining the quantizer difference
- * @todo Use with ff_modified_quant_tab from h263data.h.
- */
-static const uint8_t rv34_dquant_tab[2][32]={
-//  0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31
-{
-    0, 3, 1, 2, 3, 4, 5, 6, 7, 8, 9, 9,10,11,12,13,14,15,16,17,18,18,19,20,21,22,23,24,25,26,27,28
-},{
-    0, 2, 3, 4, 5, 6, 7, 8, 9,10,11,13,14,15,16,17,18,19,20,21,22,24,25,26,27,28,29,30,31,31,31,26
-}
-};
-
-/**
  * maximum number of macroblocks for each of the possible slice offset sizes
  * @todo This is the same as ff_mba_max, maybe use it instead.
  */
