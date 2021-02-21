aa34e99f889af37e5c719737de1347e4985e0196
Lynne <dev@lynne.ee>
2021-02-21 17:03:57 +0100

    lavu/tx: space out enum AVTXType values with newlines
    Makes separation clearer.

--- libavutil/tx.h
+++ libavutil/tx.h
@@ -43,6 +43,7 @@ enum AVTXType {
      * The stride parameter is ignored.
      */
     AV_TX_FLOAT_FFT = 0,
+
     /**
      * Standard MDCT with sample data type of float and a scale type of
      * float. Length is the frame size, not the window size (which is 2x frame)
@@ -55,19 +56,23 @@ enum AVTXType {
      * contain redundant data. This is what most codecs work with.
      */
     AV_TX_FLOAT_MDCT = 1,
+
     /**
      * Same as AV_TX_FLOAT_FFT with a data type of AVComplexDouble.
      */
     AV_TX_DOUBLE_FFT = 2,
+
     /**
      * Same as AV_TX_FLOAT_MDCT with data and scale type of double.
      * Stride must be a non-zero multiple of sizeof(double).
      */
     AV_TX_DOUBLE_MDCT = 3,
+
     /**
      * Same as AV_TX_FLOAT_FFT with a data type of AVComplexInt32.
      */
     AV_TX_INT32_FFT = 4,
+
     /**
      * Same as AV_TX_FLOAT_MDCT with data type of int32_t and scale type of float.
      * Only scale values less than or equal to 1.0 are supported.
