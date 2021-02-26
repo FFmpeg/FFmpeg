f1a894f9d3767eca9a4fbd10ac4312262d3f2968
James Almer <jamrial@gmail.com>
2021-02-26 19:22:42 -0300

    avcodec: add missing FF_API_OLD_ENCDEC wrappers to xmm clobber functions
    Signed-off-by: James Almer <jamrial@gmail.com>

--- libavcodec/aarch64/neontest.c
+++ libavcodec/aarch64/neontest.c
@@ -29,6 +29,7 @@ wrap(avcodec_open2(AVCodecContext *avctx,
     testneonclobbers(avcodec_open2, avctx, codec, options);
 }
 
+#if FF_API_OLD_ENCDEC
 wrap(avcodec_decode_audio4(AVCodecContext *avctx,
                            AVFrame *frame,
                            int *got_frame_ptr,
@@ -47,15 +48,6 @@ wrap(avcodec_decode_video2(AVCodecContext *avctx,
                      got_picture_ptr, avpkt);
 }
 
-wrap(avcodec_decode_subtitle2(AVCodecContext *avctx,
-                              AVSubtitle *sub,
-                              int *got_sub_ptr,
-                              AVPacket *avpkt))
-{
-    testneonclobbers(avcodec_decode_subtitle2, avctx, sub,
-                     got_sub_ptr, avpkt);
-}
-
 wrap(avcodec_encode_audio2(AVCodecContext *avctx,
                            AVPacket *avpkt,
                            const AVFrame *frame,
@@ -65,17 +57,27 @@ wrap(avcodec_encode_audio2(AVCodecContext *avctx,
                      got_packet_ptr);
 }
 
+wrap(avcodec_encode_video2(AVCodecContext *avctx, AVPacket *avpkt,
+                           const AVFrame *frame, int *got_packet_ptr))
+{
+    testneonclobbers(avcodec_encode_video2, avctx, avpkt, frame, got_packet_ptr);
+}
+#endif
+
+wrap(avcodec_decode_subtitle2(AVCodecContext *avctx,
+                              AVSubtitle *sub,
+                              int *got_sub_ptr,
+                              AVPacket *avpkt))
+{
+    testneonclobbers(avcodec_decode_subtitle2, avctx, sub,
+                     got_sub_ptr, avpkt);
+}
+
 wrap(avcodec_encode_subtitle(AVCodecContext *avctx,
                              uint8_t *buf, int buf_size,
                              const AVSubtitle *sub))
 {
     testneonclobbers(avcodec_encode_subtitle, avctx, buf, buf_size, sub);
-}
-
-wrap(avcodec_encode_video2(AVCodecContext *avctx, AVPacket *avpkt,
-                           const AVFrame *frame, int *got_packet_ptr))
-{
-    testneonclobbers(avcodec_encode_video2, avctx, avpkt, frame, got_packet_ptr);
 }
 
 wrap(avcodec_send_packet(AVCodecContext *avctx, const AVPacket *avpkt))
