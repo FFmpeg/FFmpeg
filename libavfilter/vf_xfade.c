2aece147d594f78afc969100c0fc637f4b1ebe86
Paul B Mahol <onemda@gmail.com>
2021-03-14 13:06:50 +0100

    avfilter/vf_xfade: drop unused frames from inactive inputs

--- libavfilter/vf_xfade.c
+++ libavfilter/vf_xfade.c
@@ -1851,6 +1851,11 @@ static int xfade_activate(AVFilterContext *ctx)
     FF_FILTER_FORWARD_STATUS_BACK_ALL(outlink, ctx);
 
     if (s->xfade_is_over) {
+        if (!s->eof[0]) {
+            ret = ff_inlink_consume_frame(ctx->inputs[0], &in);
+            if (ret > 0)
+                av_frame_free(&in);
+        }
         ret = ff_inlink_consume_frame(ctx->inputs[1], &in);
         if (ret < 0) {
             return ret;
