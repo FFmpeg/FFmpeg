28859d6e7cdbf0cc230cbf782f3895a9fa22616c
Michael Niedermayer <michael@niedermayer.cc>
2021-03-18 22:20:03 +0100

    avcodec/aacpsy: Check model_priv_data before dereferencing in psy_3gpp_end()
    Fixes: null pointer dereference
    Fixes: av_freep.mp4
    
    Found-by: Rafael Dutra <rafael.dutra@cispa.de>
    Tested-by: Rafael Dutra <rafael.dutra@cispa.de>
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/aacpsy.c
+++ libavcodec/aacpsy.c
@@ -855,7 +855,8 @@ static void psy_3gpp_analyze(FFPsyContext *ctx, int channel,
 static av_cold void psy_3gpp_end(FFPsyContext *apc)
 {
     AacPsyContext *pctx = (AacPsyContext*) apc->model_priv_data;
-    av_freep(&pctx->ch);
+    if (pctx)
+        av_freep(&pctx->ch);
     av_freep(&apc->model_priv_data);
 }
 
