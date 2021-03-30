5213143514001604562590e6cf2e8c030948e9ef
Michael Niedermayer <michael@niedermayer.cc>
2021-03-30 13:22:14 +0200

    avcodec/pnm_parser: Check image size addition for overflow
    Fixes: assertion failure
    Fixes: out of array access
    Fixes: 32664/clusterfuzz-testcase-minimized-ffmpeg_AV_CODEC_ID_PGMYUV_fuzzer-6533642202513408.fuzz
    Fixes: 32669/clusterfuzz-testcase-minimized-ffmpeg_AV_CODEC_ID_PGMYUV_fuzzer-6001928875147264
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit 79ac8d55468adc9cb9a0908e671807a2a789b7d0)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/pnm_parser.c
+++ libavcodec/pnm_parser.c
@@ -64,7 +64,7 @@ retry:
     } else {
         int ret = av_image_get_buffer_size(avctx->pix_fmt, avctx->width, avctx->height, 1);
         next = pnmctx.bytestream - pnmctx.bytestream_start + skip;
-        if (ret >= 0)
+        if (ret >= 0 && next + (uint64_t)ret <= INT_MAX)
             next += ret;
         if (pnmctx.bytestream_start != buf + skip)
             next -= pc->index;
