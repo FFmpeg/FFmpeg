cb59c8cec49a74490d927ba4f4d2fcc81bde22bd
Andreas Rheinhardt <andreas.rheinhardt@outlook.com>
2021-05-07 06:13:29 +0200

    avcodec/nuv: Mark decoder as init-threadsafe
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

--- libavcodec/nuv.c
+++ libavcodec/nuv.c
@@ -372,5 +372,5 @@ const AVCodec ff_nuv_decoder = {
     .close          = decode_end,
     .decode         = decode_frame,
     .capabilities   = AV_CODEC_CAP_DR1,
-    .caps_internal  = FF_CODEC_CAP_INIT_CLEANUP,
+    .caps_internal  = FF_CODEC_CAP_INIT_THREADSAFE | FF_CODEC_CAP_INIT_CLEANUP,
 };
