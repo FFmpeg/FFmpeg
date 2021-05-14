a61b7654a223d9e232c4a7a699f02cc045e79d19
Shubhanshu Saxena <shubhanshu.e01@gmail.com>
2021-05-14 12:41:02 +0530

    lavfi/dnn_backend_native_layer_conv2d.h: Documentation
    Add documentation for 2D Convolution Layer
    
    Signed-off-by: Shubhanshu Saxena <shubhanshu.e01@gmail.com>

--- libavfilter/dnn/dnn_backend_native_layer_conv2d.h
+++ libavfilter/dnn/dnn_backend_native_layer_conv2d.h
@@ -34,7 +34,34 @@ typedef struct ConvolutionalParams{
     float *biases;
 } ConvolutionalParams;
 
+/**
+ * @brief Load the 2D Convolution Layer.
+ *
+ * It assigns the 2D convolution layer with ConvolutionalParams
+ * after parsing from the model file context.
+ *
+ * @param layer pointer to the DNN layer instance
+ * @param model_file_context pointer to model file context
+ * @param file_size model file size to check if data is read
+ * correctly from the model file
+ * @param operands_num operand count of the whole model to
+ * check if data is read correctly from the model file
+ * @return number of bytes read from the model file
+ * @retval 0 if out of memory or an error occurs
+ */
 int ff_dnn_load_layer_conv2d(Layer *layer, AVIOContext *model_file_context, int file_size, int operands_num);
+
+/**
+ * @brief Execute the 2D Convolution Layer.
+ *
+ * @param operands all operands for the model
+ * @param input_operand_indexes input operand indexes for this layer
+ * @param output_operand_index output operand index for this layer
+ * @param parameters convolution parameters
+ * @param ctx pointer to Native model context for logging
+ * @retval 0 if the execution succeeds
+ * @retval DNN_ERROR if the execution fails
+ */
 int ff_dnn_execute_layer_conv2d(DnnOperand *operands, const int32_t *input_operand_indexes,
                                 int32_t output_operand_index, const void *parameters, NativeContext *ctx);
 #endif
