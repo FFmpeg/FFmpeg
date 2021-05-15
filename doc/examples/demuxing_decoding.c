933308a29063f849f7b71be08140f56f9dec1eca
Andreas Rheinhardt <andreas.rheinhardt@outlook.com>
2021-05-15 08:22:36 +0200

    doc/examples/demuxing_decoding: Remove unused options
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

--- doc/examples/demuxing_decoding.c
+++ doc/examples/demuxing_decoding.c
@@ -150,7 +150,6 @@ static int open_codec_context(int *stream_idx,
     int ret, stream_index;
     AVStream *st;
     const AVCodec *dec = NULL;
-    AVDictionary *opts = NULL;
 
     ret = av_find_best_stream(fmt_ctx, type, -1, -1, NULL, 0);
     if (ret < 0) {
@@ -185,7 +184,7 @@ static int open_codec_context(int *stream_idx,
         }
 
         /* Init the decoders */
-        if ((ret = avcodec_open2(*dec_ctx, dec, &opts)) < 0) {
+        if ((ret = avcodec_open2(*dec_ctx, dec, NULL)) < 0) {
             fprintf(stderr, "Failed to open %s codec\n",
                     av_get_media_type_string(type));
             return ret;
