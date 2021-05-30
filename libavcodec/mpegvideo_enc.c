552430993d2dbf482643d3e9f811fe46ed3cb0b1
Michael Niedermayer <michael@niedermayer.cc>
2021-05-30 18:38:23 +0200

    avcodec/mpegvideo_enc: Limit bitrate tolerance to the representable
    Fixes: error: 1.66789e+11 is outside the range of representable values of type 'int'
    Fixes: Ticket8201
    
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit 245017ec8a87d6e4c764d06afeca37100b980d85)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/mpegvideo_enc.c
+++ libavcodec/mpegvideo_enc.c
@@ -492,9 +492,13 @@ FF_ENABLE_DEPRECATION_WARNINGS
     if (!s->fixed_qscale &&
         avctx->bit_rate * av_q2d(avctx->time_base) >
             avctx->bit_rate_tolerance) {
+        double nbt = avctx->bit_rate * av_q2d(avctx->time_base) * 5;
         av_log(avctx, AV_LOG_WARNING,
                "bitrate tolerance %d too small for bitrate %"PRId64", overriding\n", avctx->bit_rate_tolerance, avctx->bit_rate);
-        avctx->bit_rate_tolerance = 5 * avctx->bit_rate * av_q2d(avctx->time_base);
+        if (nbt <= INT_MAX) {
+            avctx->bit_rate_tolerance = nbt;
+        } else
+            avctx->bit_rate_tolerance = INT_MAX;
     }
 
     if (s->avctx->rc_max_rate &&
