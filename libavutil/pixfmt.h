6b1268f8c371c2e6efa5c8465b85ae9025254184
Valerii Zapodovnikov <val.zapod.vz@gmail.com>
2021-05-31 16:00:16 +0300

    pixfmt: fixed wrong fix of comment
    This mostly reverts 785bfb1d7bb8de567c3aac1d9cc369b55ac9fb7b.
    But I also added some clarifications so that nobody mixes primaries
    with matrix again. SMPTE 240 and 170 primaires are the same, while
    matrix coeff. are different, because 240 is derived from 170's new
    primaries and white point while 170 uses BT.601 derived from BT.470
    System M (yes, with Illuminant C) a.k.a. NTSC 1953. Some nits too.
    
    Reviewed-by: Reto Kromer <lists@reto.ch>
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavutil/pixfmt.h
+++ libavutil/pixfmt.h
@@ -443,32 +443,32 @@ enum AVPixelFormat {
 
 /**
   * Chromaticity coordinates of the source primaries.
-  * These values match the ones defined by ISO/IEC 23001-8_2013 § 7.1.
+  * These values match the ones defined by ISO/IEC 23091-2_2019 subclause 8.1 and ITU-T H.273.
   */
 enum AVColorPrimaries {
     AVCOL_PRI_RESERVED0   = 0,
-    AVCOL_PRI_BT709       = 1,  ///< also ITU-R BT1361 / IEC 61966-2-4 / SMPTE RP177 Annex B
+    AVCOL_PRI_BT709       = 1,  ///< also ITU-R BT1361 / IEC 61966-2-4 / SMPTE RP 177 Annex B
     AVCOL_PRI_UNSPECIFIED = 2,
     AVCOL_PRI_RESERVED    = 3,
     AVCOL_PRI_BT470M      = 4,  ///< also FCC Title 47 Code of Federal Regulations 73.682 (a)(20)
 
     AVCOL_PRI_BT470BG     = 5,  ///< also ITU-R BT601-6 625 / ITU-R BT1358 625 / ITU-R BT1700 625 PAL & SECAM
     AVCOL_PRI_SMPTE170M   = 6,  ///< also ITU-R BT601-6 525 / ITU-R BT1358 525 / ITU-R BT1700 NTSC
-    AVCOL_PRI_SMPTE240M   = 7,  ///< functionally identical to above
+    AVCOL_PRI_SMPTE240M   = 7,  ///< identical to above, also called "SMPTE C" even though it uses D65
     AVCOL_PRI_FILM        = 8,  ///< colour filters using Illuminant C
     AVCOL_PRI_BT2020      = 9,  ///< ITU-R BT2020
     AVCOL_PRI_SMPTE428    = 10, ///< SMPTE ST 428-1 (CIE 1931 XYZ)
     AVCOL_PRI_SMPTEST428_1 = AVCOL_PRI_SMPTE428,
     AVCOL_PRI_SMPTE431    = 11, ///< SMPTE ST 431-2 (2011) / DCI P3
     AVCOL_PRI_SMPTE432    = 12, ///< SMPTE ST 432-1 (2010) / P3 D65 / Display P3
-    AVCOL_PRI_EBU3213     = 22, ///< EBU Tech. 3213-E / JEDEC P22 phosphors
+    AVCOL_PRI_EBU3213     = 22, ///< EBU Tech. 3213-E (nothing there) / one of JEDEC P22 group phosphors
     AVCOL_PRI_JEDEC_P22   = AVCOL_PRI_EBU3213,
     AVCOL_PRI_NB                ///< Not part of ABI
 };
 
 /**
  * Color Transfer Characteristic.
- * These values match the ones defined by ISO/IEC 23001-8_2013 § 7.2.
+ * These values match the ones defined by ISO/IEC 23091-2_2019 subclause 8.2.
  */
 enum AVColorTransferCharacteristic {
     AVCOL_TRC_RESERVED0    = 0,
@@ -497,18 +497,18 @@ enum AVColorTransferCharacteristic {
 
 /**
  * YUV colorspace type.
- * These values match the ones defined by ISO/IEC 23001-8_2013 § 7.3.
+ * These values match the ones defined by ISO/IEC 23091-2_2019 subclause 8.3.
  */
 enum AVColorSpace {
-    AVCOL_SPC_RGB         = 0,  ///< order of coefficients is actually GBR, also IEC 61966-2-1 (sRGB)
-    AVCOL_SPC_BT709       = 1,  ///< also ITU-R BT1361 / IEC 61966-2-4 xvYCC709 / SMPTE RP177 Annex B
+    AVCOL_SPC_RGB         = 0,  ///< order of coefficients is actually GBR, also IEC 61966-2-1 (sRGB), YZX and ST 428-1
+    AVCOL_SPC_BT709       = 1,  ///< also ITU-R BT1361 / IEC 61966-2-4 xvYCC709 / derived in SMPTE RP 177 Annex B
     AVCOL_SPC_UNSPECIFIED = 2,
-    AVCOL_SPC_RESERVED    = 3,
+    AVCOL_SPC_RESERVED    = 3,  ///< reserved for future use by ITU-T and ISO/IEC just like 15-255 are
     AVCOL_SPC_FCC         = 4,  ///< FCC Title 47 Code of Federal Regulations 73.682 (a)(20)
     AVCOL_SPC_BT470BG     = 5,  ///< also ITU-R BT601-6 625 / ITU-R BT1358 625 / ITU-R BT1700 625 PAL & SECAM / IEC 61966-2-4 xvYCC601
-    AVCOL_SPC_SMPTE170M   = 6,  ///< also ITU-R BT601-6 525 / ITU-R BT1358 525 / ITU-R BT1700 NTSC
-    AVCOL_SPC_SMPTE240M   = 7,  ///< functionally identical to above
-    AVCOL_SPC_YCGCO       = 8,  ///< Used by Dirac / VC-2 and H.264 FRext, see ITU-T SG16
+    AVCOL_SPC_SMPTE170M   = 6,  ///< also ITU-R BT601-6 525 / ITU-R BT1358 525 / ITU-R BT1700 NTSC / functionally identical to above
+    AVCOL_SPC_SMPTE240M   = 7,  ///< derived from 170M primaries and D65 white point, 170M is derived from BT470 System M's primaries
+    AVCOL_SPC_YCGCO       = 8,  ///< used by Dirac / VC-2 and H.264 FRext, see ITU-T SG16
     AVCOL_SPC_YCOCG       = AVCOL_SPC_YCGCO,
     AVCOL_SPC_BT2020_NCL  = 9,  ///< ITU-R BT2020 non-constant luminance system
     AVCOL_SPC_BT2020_CL   = 10, ///< ITU-R BT2020 constant luminance system
@@ -530,9 +530,9 @@ enum AVColorSpace {
  * recommended, as it also defines the full range representation.
  *
  * Common definitions:
- *   - For RGB and luminance planes such as Y in YCbCr and I in ICtCp,
+ *   - For RGB and luma planes such as Y in YCbCr and I in ICtCp,
  *     'E' is the original value in range of 0.0 to 1.0.
- *   - For chrominance planes such as Cb,Cr and Ct,Cp, 'E' is the original
+ *   - For chroma planes such as Cb,Cr and Ct,Cp, 'E' is the original
  *     value in range of -0.5 to 0.5.
  *   - 'n' is the output bit depth.
  *   - For additional definitions such as rounding and clipping to valid n
@@ -544,13 +544,13 @@ enum AVColorRange {
     /**
      * Narrow or limited range content.
      *
-     * - For luminance planes:
+     * - For luma planes:
      *
      *       (219 * E + 16) * 2^(n-8)
      *
      *   F.ex. the range of 16-235 for 8 bits
      *
-     * - For chrominance planes:
+     * - For chroma planes:
      *
      *       (224 * E + 128) * 2^(n-8)
      *
@@ -561,13 +561,13 @@ enum AVColorRange {
     /**
      * Full range content.
      *
-     * - For RGB and luminance planes:
+     * - For RGB and luma planes:
      *
      *       (2^n - 1) * E
      *
      *   F.ex. the range of 0-255 for 8 bits
      *
-     * - For chrominance planes:
+     * - For chroma planes:
      *
      *       (2^n - 1) * E + 2^(n - 1)
      *
