8a6103326e165b30510378cbd0e7953910f2b55c
James Almer <jamrial@gmail.com>
2021-06-07 00:04:21 -0300

    avutil/samplefmt: don't add offsets to NULL pointers
    Signed-off-by: James Almer <jamrial@gmail.com>

--- libavutil/samplefmt.c
+++ libavutil/samplefmt.c
@@ -160,12 +160,19 @@ int av_samples_fill_arrays(uint8_t **audio_data, int *linesize,
     if (buf_size < 0)
         return buf_size;
 
+    if (linesize)
+        *linesize = line_size;
+
+    memset(audio_data, 0, planar
+                          ? sizeof(*audio_data) * nb_channels
+                          : sizeof(*audio_data));
+
+    if (!buf)
+        return buf_size;
+
     audio_data[0] = (uint8_t *)buf;
     for (ch = 1; planar && ch < nb_channels; ch++)
         audio_data[ch] = audio_data[ch-1] + line_size;
-
-    if (linesize)
-        *linesize = line_size;
 
     return buf_size;
 }
