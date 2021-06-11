ae0863b91ddf60795967273b913d93825b04183c
James Almer <jamrial@gmail.com>
2021-06-10 21:19:39 -0300

    examples/muxing: add missing header
    Fixed compilation broken by e67e02d15672.
    
    Signed-off-by: James Almer <jamrial@gmail.com>

--- doc/examples/muxing.c
+++ doc/examples/muxing.c
@@ -39,6 +39,7 @@
 #include <libavutil/opt.h>
 #include <libavutil/mathematics.h>
 #include <libavutil/timestamp.h>
+#include <libavcodec/avcodec.h>
 #include <libavformat/avformat.h>
 #include <libswscale/swscale.h>
 #include <libswresample/swresample.h>
