60515a6d610491f030aaa4cf230d0367a85e2d4b
Anton Khirnov <anton@khirnov.net>
2021-07-07 18:42:15 +0200

    lavfi/vf_scale: pass the thread count to the scaler

--- libavfilter/vf_scale.c
+++ libavfilter/vf_scale.c
@@ -542,6 +542,7 @@ static int config_props(AVFilterLink *outlink)
             av_opt_set_int(s, "sws_flags", scale->flags, 0);
             av_opt_set_int(s, "param0", scale->param[0], 0);
             av_opt_set_int(s, "param1", scale->param[1], 0);
+            av_opt_set_int(s, "threads", ff_filter_get_nb_threads(ctx), 0);
             if (scale->in_range != AVCOL_RANGE_UNSPECIFIED)
                 av_opt_set_int(s, "src_range",
                                scale->in_range == AVCOL_RANGE_JPEG, 0);
