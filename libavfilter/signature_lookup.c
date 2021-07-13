012804d2e9087a65f557e18ef8171a5c6b779f6a
Jai Luthra <me@jailuthra.in>
2021-07-13 13:25:25 +0530

    avfilter/vf_signature: Initialize all houghspace elements
    Co-authored-by: Oscar <oscar_davids@outlook.com>

--- libavfilter/signature_lookup.c
+++ libavfilter/signature_lookup.c
@@ -200,7 +200,7 @@ static MatchingInfo* get_matching_parameters(AVFilterContext *ctx, SignatureCont
     /* initialize houghspace */
     for (i = 0; i < MAX_FRAMERATE; i++) {
         hspace[i] = av_malloc_array(2 * HOUGH_MAX_OFFSET + 1, sizeof(hspace_elem));
-        for (j = 0; j < HOUGH_MAX_OFFSET; j++) {
+        for (j = 0; j < 2 * HOUGH_MAX_OFFSET + 1; j++) {
             hspace[i][j].score = 0;
             hspace[i][j].dist = 99999;
         }
