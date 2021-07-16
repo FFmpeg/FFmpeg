c4e51af5e79db28b315ec2b3bfffe3f4b2bb7640
Paul B Mahol <onemda@gmail.com>
2021-07-16 09:01:47 +0200

    avfilter/vf_w3fdif: fix time base for large denominators

--- libavfilter/vf_w3fdif.c
+++ libavfilter/vf_w3fdif.c
@@ -334,10 +334,8 @@ static int config_output(AVFilterLink *outlink)
 {
     AVFilterLink *inlink = outlink->src->inputs[0];
 
-    outlink->time_base.num = inlink->time_base.num;
-    outlink->time_base.den = inlink->time_base.den * 2;
-    outlink->frame_rate.num = inlink->frame_rate.num * 2;
-    outlink->frame_rate.den = inlink->frame_rate.den;
+    outlink->time_base = av_mul_q(inlink->time_base, (AVRational){1, 2});
+    outlink->frame_rate = av_mul_q(inlink->frame_rate, (AVRational){2, 1});
 
     return 0;
 }
