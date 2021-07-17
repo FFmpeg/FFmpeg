f687f8e0d3a5a4b2f0159a6b83e57f9df9a15f62
Zane van Iperen <zane@zanevaniperen.com>
2021-07-18 09:18:47 +1000

    avcodec/argo: use bits_per_coded_sample instead of bits_per_raw_sample
    Signed-off-by: Zane van Iperen <zane@zanevaniperen.com>

--- libavcodec/argo.c
+++ libavcodec/argo.c
@@ -676,12 +676,12 @@ static av_cold int decode_init(AVCodecContext *avctx)
 {
     ArgoContext *s = avctx->priv_data;
 
-    switch (avctx->bits_per_raw_sample) {
+    switch (avctx->bits_per_coded_sample) {
     case  8: s->bpp = 1;
              avctx->pix_fmt = AV_PIX_FMT_PAL8; break;
     case 24: s->bpp = 4;
              avctx->pix_fmt = AV_PIX_FMT_BGR0; break;
-    default: avpriv_request_sample(s, "depth == %u", avctx->bits_per_raw_sample);
+    default: avpriv_request_sample(s, "depth == %u", avctx->bits_per_coded_sample);
              return AVERROR_PATCHWELCOME;
     }
 
