9ffa49496d1aae4cbbb387aac28a9e061a6ab0a6
maryam ebrahimzadeh <me22bee@outlook.com>
2021-08-04 16:15:18 -0400

    avformat/adtsenc: return value check for init_get_bits in adts_decode_extradata
    As the second argument for init_get_bits (buf) can be crafted, a return value check for this function call is necessary.
    'buf' is  part of  'AVPacket pkt'.
    replace init_get_bits with init_get_bits8.
    
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavformat/adtsenc.c
+++ libavformat/adtsenc.c
@@ -53,9 +53,11 @@ static int adts_decode_extradata(AVFormatContext *s, ADTSContext *adts, const ui
     GetBitContext gb;
     PutBitContext pb;
     MPEG4AudioConfig m4ac;
-    int off;
+    int off, ret;
 
-    init_get_bits(&gb, buf, size * 8);
+    ret = init_get_bits8(&gb, buf, size);
+    if (ret < 0)
+        return ret;
     off = avpriv_mpeg4audio_get_config2(&m4ac, buf, size, 1, s);
     if (off < 0)
         return off;
