43ca887bc2edaaa7dbcf40798f5345dda871cbe2
Mikhail Nitenko <mnitenko@gmail.com>
2021-08-20 00:08:00 +0300

    lavc/aarch64: h264, add chroma loop filters for 10bit
    Benchmarks:                                             A53     A72
    h264_h_loop_filter_chroma422_10bpp_c:                  282.7   114.2
    h264_h_loop_filter_chroma422_10bpp_neon:               109.5    78.5
    h264_h_loop_filter_chroma_10bpp_c:                     165.0    81.5
    h264_h_loop_filter_chroma_10bpp_neon:                  120.0    76.7
    h264_h_loop_filter_chroma_intra422_10bpp_c:            323.7   124.2
    h264_h_loop_filter_chroma_intra422_10bpp_neon:         155.0   102.7
    h264_h_loop_filter_chroma_intra_10bpp_c:               121.0    49.5
    h264_h_loop_filter_chroma_intra_10bpp_neon:             79.7    53.7
    h264_h_loop_filter_chroma_mbaff422_10bpp_c:            188.5    75.0
    h264_h_loop_filter_chroma_mbaff422_10bpp_neon:         120.0    75.5
    h264_h_loop_filter_chroma_mbaff_intra422_10bpp_c:      116.7    46.0
    h264_h_loop_filter_chroma_mbaff_intra422_10bpp_neon:    79.7    53.7
    h264_h_loop_filter_chroma_mbaff_intra_10bpp_c:          63.0    27.2
    h264_h_loop_filter_chroma_mbaff_intra_10bpp_neon:       48.5    34.0
    h264_v_loop_filter_chroma_10bpp_c:                     258.7   135.5
    h264_v_loop_filter_chroma_10bpp_neon:                   71.2    51.0
    h264_v_loop_filter_chroma_intra_10bpp_c:               158.0    70.7
    h264_v_loop_filter_chroma_intra_10bpp_neon:             48.7    31.5
    
    Signed-off-by: Mikhail Nitenko <mnitenko@gmail.com>
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- libavcodec/aarch64/h264dsp_init_aarch64.c
+++ libavcodec/aarch64/h264dsp_init_aarch64.c
@@ -83,6 +83,29 @@ void ff_h264_idct8_add4_neon(uint8_t *dst, const int *block_offset,
                              int16_t *block, int stride,
                              const uint8_t nnzc[5 * 8]);
 
+void ff_h264_v_loop_filter_luma_neon_10(uint8_t *pix, ptrdiff_t stride, int alpha,
+                                        int beta, int8_t *tc0);
+void ff_h264_h_loop_filter_luma_neon_10(uint8_t *pix, ptrdiff_t stride, int alpha,
+                                        int beta, int8_t *tc0);
+void ff_h264_v_loop_filter_luma_intra_neon_10(uint8_t *pix, ptrdiff_t stride, int alpha,
+                                              int beta);
+void ff_h264_h_loop_filter_luma_intra_neon_10(uint8_t *pix, ptrdiff_t stride, int alpha,
+                                              int beta);
+void ff_h264_v_loop_filter_chroma_neon_10(uint8_t *pix, ptrdiff_t stride, int alpha,
+                                          int beta, int8_t *tc0);
+void ff_h264_h_loop_filter_chroma_neon_10(uint8_t *pix, ptrdiff_t stride, int alpha,
+                                          int beta, int8_t *tc0);
+void ff_h264_h_loop_filter_chroma422_neon_10(uint8_t *pix, ptrdiff_t stride, int alpha,
+                                             int beta, int8_t *tc0);
+void ff_h264_v_loop_filter_chroma_intra_neon_10(uint8_t *pix, ptrdiff_t stride,
+                                                int alpha, int beta);
+void ff_h264_h_loop_filter_chroma_intra_neon_10(uint8_t *pix, ptrdiff_t stride,
+                                                int alpha, int beta);
+void ff_h264_h_loop_filter_chroma422_intra_neon_10(uint8_t *pix, ptrdiff_t stride,
+                                                   int alpha, int beta);
+void ff_h264_h_loop_filter_chroma_mbaff_intra_neon_10(uint8_t *pix, ptrdiff_t stride,
+                                                      int alpha, int beta);
+
 av_cold void ff_h264dsp_init_aarch64(H264DSPContext *c, const int bit_depth,
                                      const int chroma_format_idc)
 {
@@ -125,5 +148,19 @@ av_cold void ff_h264dsp_init_aarch64(H264DSPContext *c, const int bit_depth,
         c->h264_idct8_add       = ff_h264_idct8_add_neon;
         c->h264_idct8_dc_add    = ff_h264_idct8_dc_add_neon;
         c->h264_idct8_add4      = ff_h264_idct8_add4_neon;
+    } else if (have_neon(cpu_flags) && bit_depth == 10) {
+        c->h264_v_loop_filter_chroma = ff_h264_v_loop_filter_chroma_neon_10;
+        c->h264_v_loop_filter_chroma_intra = ff_h264_v_loop_filter_chroma_intra_neon_10;
+
+        if (chroma_format_idc <= 1) {
+            c->h264_h_loop_filter_chroma = ff_h264_h_loop_filter_chroma_neon_10;
+            c->h264_h_loop_filter_chroma_intra = ff_h264_h_loop_filter_chroma_intra_neon_10;
+            c->h264_h_loop_filter_chroma_mbaff_intra = ff_h264_h_loop_filter_chroma_mbaff_intra_neon_10;
+        } else {
+            c->h264_h_loop_filter_chroma = ff_h264_h_loop_filter_chroma422_neon_10;
+            c->h264_h_loop_filter_chroma_mbaff = ff_h264_h_loop_filter_chroma_neon_10;
+            c->h264_h_loop_filter_chroma_intra = ff_h264_h_loop_filter_chroma422_intra_neon_10;
+            c->h264_h_loop_filter_chroma_mbaff_intra = ff_h264_h_loop_filter_chroma_intra_neon_10;
+        }
     }
 }
