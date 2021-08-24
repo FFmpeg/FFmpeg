ff7ed0b006dfe35d02ef14034e9ee514f8d5963d
Limin Wang <lance.lmwang@gmail.com>
2021-08-24 19:10:19 +0800

    avcodec/extract_extradata_bsf: add support for AVS3
    Signed-off-by: Limin Wang <lance.lmwang@gmail.com>

--- libavcodec/extract_extradata_bsf.c
+++ libavcodec/extract_extradata_bsf.c
@@ -328,6 +328,7 @@ static const struct {
 } extract_tab[] = {
     { AV_CODEC_ID_AV1,        extract_extradata_av1     },
     { AV_CODEC_ID_AVS2,       extract_extradata_mpeg4   },
+    { AV_CODEC_ID_AVS3,       extract_extradata_mpeg4   },
     { AV_CODEC_ID_CAVS,       extract_extradata_mpeg4   },
     { AV_CODEC_ID_H264,       extract_extradata_h2645   },
     { AV_CODEC_ID_HEVC,       extract_extradata_h2645   },
@@ -396,6 +397,7 @@ static void extract_extradata_close(AVBSFContext *ctx)
 static const enum AVCodecID codec_ids[] = {
     AV_CODEC_ID_AV1,
     AV_CODEC_ID_AVS2,
+    AV_CODEC_ID_AVS3,
     AV_CODEC_ID_CAVS,
     AV_CODEC_ID_H264,
     AV_CODEC_ID_HEVC,
