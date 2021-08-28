fdb4c035551f177f550d69ed62e8d8bca1c6f32c
Paul B Mahol <onemda@gmail.com>
2021-08-29 01:35:16 +0200

    avfilter/vf_tpad: rescale EOF pts for case outlink time_base differs

--- libavfilter/vf_tpad.c
+++ libavfilter/vf_tpad.c
@@ -125,6 +125,7 @@ static int activate(AVFilterContext *ctx)
 
     if (!s->eof && ff_inlink_acknowledge_status(inlink, &status, &pts)) {
         if (status == AVERROR_EOF) {
+            pts = av_rescale_q(pts, inlink->time_base, outlink->time_base);
             if (!s->pad_stop) {
                 ff_outlink_set_status(outlink, status, pts);
                 return 0;
