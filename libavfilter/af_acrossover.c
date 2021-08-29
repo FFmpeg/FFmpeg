02fcd485f91e468638198ba05847af514ae58df9
Paul B Mahol <onemda@gmail.com>
2021-08-29 10:29:15 +0200

    avfilter/af_acrossover: check for rest of sample formats

--- libavfilter/af_acrossover.c
+++ libavfilter/af_acrossover.c
@@ -483,6 +483,7 @@ static int config_input(AVFilterLink *inlink)
     switch (inlink->format) {
     case AV_SAMPLE_FMT_FLTP: s->filter_channels = filter_channels_fltp; break;
     case AV_SAMPLE_FMT_DBLP: s->filter_channels = filter_channels_dblp; break;
+    default: return AVERROR_BUG;
     }
 
     s->xover = ff_get_audio_buffer(inlink, 2 * (ctx->nb_outputs * 10 + ctx->nb_outputs * 10 +
