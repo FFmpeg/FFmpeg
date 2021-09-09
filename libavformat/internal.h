6af21de373c979bc2087717acb61e834768ebe4b
Andreas Rheinhardt <andreas.rheinhardt@outlook.com>
2021-09-09 17:08:49 +0200

    avformat/utils: Use st for AVStream variable in avpriv_set_pts_info
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

--- libavformat/internal.h
+++ libavformat/internal.h
@@ -696,13 +696,13 @@ int64_t ff_gen_search(AVFormatContext *s, int stream_index,
  * (numerator or denominator are non-positive), it leaves the stream
  * unchanged.
  *
- * @param s stream
+ * @param st stream
  * @param pts_wrap_bits number of bits effectively used by the pts
  *        (used for wrap control)
  * @param pts_num time base numerator
  * @param pts_den time base denominator
  */
-void avpriv_set_pts_info(AVStream *s, int pts_wrap_bits,
+void avpriv_set_pts_info(AVStream *st, int pts_wrap_bits,
                          unsigned int pts_num, unsigned int pts_den);
 
 /**
