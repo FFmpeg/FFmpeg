6b08ed9f956408e9d5ef0339a51258cb2a648042
Andreas Rheinhardt <andreas.rheinhardt@outlook.com>
2021-09-09 17:49:43 +0200

    fftools/ffprobe: Don't access AVProgram.(start|end)_time
    These are internal fields.
    
    Reviewed-by: Tobias Rapp <t.rapp@noa-archive.com>
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

--- fftools/ffprobe.c
+++ fftools/ffprobe.c
@@ -2844,10 +2844,6 @@ static int show_program(WriterContext *w, InputFile *ifile, AVProgram *program)
     print_int("nb_streams", program->nb_stream_indexes);
     print_int("pmt_pid", program->pmt_pid);
     print_int("pcr_pid", program->pcr_pid);
-    print_ts("start_pts", program->start_time);
-    print_time("start_time", program->start_time, &AV_TIME_BASE_Q);
-    print_ts("end_pts", program->end_time);
-    print_time("end_time", program->end_time, &AV_TIME_BASE_Q);
     if (do_show_program_tags)
         ret = show_tags(w, program->metadata, SECTION_ID_PROGRAM_TAGS);
     if (ret < 0)
