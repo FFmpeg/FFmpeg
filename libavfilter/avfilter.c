c256491f9669893917f4db5c0cf56b17530f10b6
Andreas Rheinhardt <andreas.rheinhardt@outlook.com>
2021-09-15 05:13:11 +0200

    avfilter/avfilter: Actually error out on init error
    Currently an error from init could be overwritten by successfully
    setting the enable expression.
    
    Reviewed-by: Nicolas George <george@nsup.org>
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>
    (cherry picked from commit 10ad3cd7982e5a5d922cabd176c97571a7684c60)

--- libavfilter/avfilter.c
+++ libavfilter/avfilter.c
@@ -925,6 +925,8 @@ int avfilter_init_dict(AVFilterContext *ctx, AVDictionary **options)
         ret = ctx->filter->init(ctx);
     else if (ctx->filter->init_dict)
         ret = ctx->filter->init_dict(ctx, options);
+    if (ret < 0)
+        return ret;
 
     if (ctx->enable_str) {
         ret = set_enable_expr(ctx, ctx->enable_str);
@@ -932,7 +934,7 @@ int avfilter_init_dict(AVFilterContext *ctx, AVDictionary **options)
             return ret;
     }
 
-    return ret;
+    return 0;
 }
 
 int avfilter_init_str(AVFilterContext *filter, const char *args)
