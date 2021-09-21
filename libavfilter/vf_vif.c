c87d7c8a8167f09f1ec6996f52a10bbc8c5b4f90
Andreas Rheinhardt <andreas.rheinhardt@outlook.com>
2021-09-21 06:09:26 +0200

    avfilter/vf_vif: Remove always-false format check
    This filter uses ff_set_common_formats_from_list().
    
    Reviewed-by: Paul B Mahol <onemda@gmail.com>
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

--- libavfilter/vf_vif.c
+++ libavfilter/vf_vif.c
@@ -496,10 +496,6 @@ static int config_input_ref(AVFilterLink *inlink)
         av_log(ctx, AV_LOG_ERROR, "Width and height of input videos must be same.\n");
         return AVERROR(EINVAL);
     }
-    if (ctx->inputs[0]->format != ctx->inputs[1]->format) {
-        av_log(ctx, AV_LOG_ERROR, "Inputs must be of same pixel format.\n");
-        return AVERROR(EINVAL);
-    }
 
     s->desc = av_pix_fmt_desc_get(inlink->format);
     s->width = ctx->inputs[0]->w;
