14488c26bb0e7d03ba86ec39392a57035094ca09
Andreas Rheinhardt <andreas.rheinhardt@outlook.com>
2021-09-21 06:09:26 +0200

    avfilter/vf_identity: Remove always-false format check
    These filters use ff_set_common_formats_from_list().
    
    Reviewed-by: Paul B Mahol <onemda@gmail.com>
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

--- libavfilter/vf_identity.c
+++ libavfilter/vf_identity.c
@@ -274,10 +274,6 @@ static int config_input_ref(AVFilterLink *inlink)
         av_log(ctx, AV_LOG_ERROR, "Width and height of input videos must be same.\n");
         return AVERROR(EINVAL);
     }
-    if (ctx->inputs[0]->format != ctx->inputs[1]->format) {
-        av_log(ctx, AV_LOG_ERROR, "Inputs must be of same pixel format.\n");
-        return AVERROR(EINVAL);
-    }
 
     s->is_rgb = ff_fill_rgba_map(s->rgba_map, inlink->format) >= 0;
     s->comps[0] = s->is_rgb ? 'R' : 'Y' ;
