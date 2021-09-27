b10b1947616ba65022416e617adf35aa2c901b39
Andreas Rheinhardt <andreas.rheinhardt@outlook.com>
2021-09-27 12:28:23 +0200

    avfilter/af_adenorm: Use formats list instead of query function
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

--- libavfilter/af_adenorm.c
+++ libavfilter/af_adenorm.c
@@ -43,23 +43,6 @@ typedef struct ADenormContext {
                    const void *src, int nb_samples);
 } ADenormContext;
 
-static int query_formats(AVFilterContext *ctx)
-{
-    static const enum AVSampleFormat sample_fmts[] = {
-        AV_SAMPLE_FMT_FLTP, AV_SAMPLE_FMT_DBLP,
-        AV_SAMPLE_FMT_NONE
-    };
-    int ret = ff_set_common_formats_from_list(ctx, sample_fmts);
-    if (ret < 0)
-        return ret;
-
-    ret = ff_set_common_all_channel_counts(ctx);
-    if (ret < 0)
-        return ret;
-
-    return ff_set_common_all_samplerates(ctx);
-}
-
 static void dc_denorm_fltp(AVFilterContext *ctx, void *dstp,
                            const void *srcp, int nb_samples)
 {
@@ -310,7 +293,7 @@ const AVFilter ff_af_adenorm = {
     .priv_size       = sizeof(ADenormContext),
     FILTER_INPUTS(adenorm_inputs),
     FILTER_OUTPUTS(adenorm_outputs),
-    FILTER_QUERY_FUNC(query_formats),
+    FILTER_SAMPLEFMTS(AV_SAMPLE_FMT_FLTP, AV_SAMPLE_FMT_DBLP),
     .priv_class      = &adenorm_class,
     .process_command = process_command,
     .flags           = AVFILTER_FLAG_SUPPORT_TIMELINE_GENERIC |
