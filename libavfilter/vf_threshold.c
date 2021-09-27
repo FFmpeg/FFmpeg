edc18ddaa02f3ddd9a9bbec060878920263da4b7
Andreas Rheinhardt <andreas.rheinhardt@outlook.com>
2021-09-27 17:33:35 +0200

    avfilter/vf_threshold: Use formats list instead of query function
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

--- libavfilter/vf_threshold.c
+++ libavfilter/vf_threshold.c
@@ -43,8 +43,6 @@ static const AVOption threshold_options[] = {
 
 AVFILTER_DEFINE_CLASS(threshold);
 
-static int query_formats(AVFilterContext *ctx)
-{
     static const enum AVPixelFormat pix_fmts[] = {
         AV_PIX_FMT_YUVA444P, AV_PIX_FMT_YUV444P, AV_PIX_FMT_YUV440P,
         AV_PIX_FMT_YUVJ444P, AV_PIX_FMT_YUVJ440P,
@@ -66,9 +64,6 @@ static int query_formats(AVFilterContext *ctx)
         AV_PIX_FMT_GRAY12, AV_PIX_FMT_GRAY14, AV_PIX_FMT_GRAY16,
         AV_PIX_FMT_NONE
     };
-
-    return ff_set_common_formats_from_list(ctx, pix_fmts);
-}
 
 typedef struct ThreadData {
     AVFrame *in;
@@ -355,7 +350,7 @@ const AVFilter ff_vf_threshold = {
     .activate      = activate,
     FILTER_INPUTS(inputs),
     FILTER_OUTPUTS(outputs),
-    FILTER_QUERY_FUNC(query_formats),
+    FILTER_PIXFMTS_ARRAY(pix_fmts),
     .flags         = AVFILTER_FLAG_SUPPORT_TIMELINE_INTERNAL | AVFILTER_FLAG_SLICE_THREADS,
     .process_command = ff_filter_process_command,
 };
