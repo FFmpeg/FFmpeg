af15b00e2e70793c54fb6e25c7f7cc6a00a6fc82
Andreas Rheinhardt <andreas.rheinhardt@outlook.com>
2021-09-27 21:57:19 +0200

    avfilter/vf_fillborders: Use formats list instead of query function
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

--- libavfilter/vf_fillborders.c
+++ libavfilter/vf_fillborders.c
@@ -55,8 +55,6 @@ typedef struct FillBordersContext {
     void (*fillborders)(struct FillBordersContext *s, AVFrame *frame);
 } FillBordersContext;
 
-static int query_formats(AVFilterContext *ctx)
-{
     static const enum AVPixelFormat pix_fmts[] = {
         AV_PIX_FMT_YUVA444P, AV_PIX_FMT_YUV444P, AV_PIX_FMT_YUV440P,
         AV_PIX_FMT_YUVJ444P, AV_PIX_FMT_YUVJ440P,
@@ -78,8 +76,6 @@ static int query_formats(AVFilterContext *ctx)
         AV_PIX_FMT_GRAY8, AV_PIX_FMT_GRAY9, AV_PIX_FMT_GRAY10, AV_PIX_FMT_GRAY12, AV_PIX_FMT_GRAY14, AV_PIX_FMT_GRAY16,
         AV_PIX_FMT_NONE
     };
-    return ff_set_common_formats_from_list(ctx, pix_fmts);
-}
 
 static void smear_borders8(FillBordersContext *s, AVFrame *frame)
 {
@@ -725,7 +721,7 @@ const AVFilter ff_vf_fillborders = {
     .priv_class    = &fillborders_class,
     FILTER_INPUTS(fillborders_inputs),
     FILTER_OUTPUTS(fillborders_outputs),
-    FILTER_QUERY_FUNC(query_formats),
+    FILTER_PIXFMTS_ARRAY(pix_fmts),
     .flags         = AVFILTER_FLAG_SUPPORT_TIMELINE_GENERIC,
     .process_command = process_command,
 };
