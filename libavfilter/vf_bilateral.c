32777acb362c6d4e6cbdbd4d732af164c6254c0b
Andreas Rheinhardt <andreas.rheinhardt@outlook.com>
2021-09-27 23:36:09 +0200

    avfilter/vf_bilateral: Use formats list instead of query func
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

--- libavfilter/vf_bilateral.c
+++ libavfilter/vf_bilateral.c
@@ -66,8 +66,6 @@ static const AVOption bilateral_options[] = {
 
 AVFILTER_DEFINE_CLASS(bilateral);
 
-static int query_formats(AVFilterContext *ctx)
-{
     static const enum AVPixelFormat pix_fmts[] = {
         AV_PIX_FMT_YUVA444P, AV_PIX_FMT_YUV444P, AV_PIX_FMT_YUV440P,
         AV_PIX_FMT_YUVJ444P, AV_PIX_FMT_YUVJ440P,
@@ -88,9 +86,6 @@ static int query_formats(AVFilterContext *ctx)
         AV_PIX_FMT_GRAY8, AV_PIX_FMT_GRAY9, AV_PIX_FMT_GRAY10, AV_PIX_FMT_GRAY12, AV_PIX_FMT_GRAY14, AV_PIX_FMT_GRAY16,
         AV_PIX_FMT_NONE
     };
-
-    return ff_set_common_formats_from_list(ctx, pix_fmts);
-}
 
 static int config_params(AVFilterContext *ctx)
 {
@@ -385,7 +380,7 @@ const AVFilter ff_vf_bilateral = {
     .uninit        = uninit,
     FILTER_INPUTS(bilateral_inputs),
     FILTER_OUTPUTS(bilateral_outputs),
-    FILTER_QUERY_FUNC(query_formats),
+    FILTER_PIXFMTS_ARRAY(pix_fmts),
     .flags         = AVFILTER_FLAG_SUPPORT_TIMELINE_GENERIC,
     .process_command = process_command,
 };
