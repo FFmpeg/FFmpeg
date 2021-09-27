fa816dc0a559b14ae81c7d5afe969d25cad7e570
Andreas Rheinhardt <andreas.rheinhardt@outlook.com>
2021-09-27 21:51:04 +0200

    avfilter/vf_fspp: Use formats list instead of query function
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

--- libavfilter/vf_fspp.c
+++ libavfilter/vf_fspp.c
@@ -491,8 +491,6 @@ static void row_fdct_c(int16_t *data, const uint8_t *pixels, ptrdiff_t line_size
     }
 }
 
-static int query_formats(AVFilterContext *ctx)
-{
     static const enum AVPixelFormat pix_fmts[] = {
         AV_PIX_FMT_YUV444P,  AV_PIX_FMT_YUV422P,
         AV_PIX_FMT_YUV420P,  AV_PIX_FMT_YUV411P,
@@ -502,9 +500,6 @@ static int query_formats(AVFilterContext *ctx)
         AV_PIX_FMT_GBRP, AV_PIX_FMT_GRAY8,
         AV_PIX_FMT_NONE
     };
-
-    return ff_set_common_formats_from_list(ctx, pix_fmts);
-}
 
 static int config_input(AVFilterLink *inlink)
 {
@@ -669,7 +664,7 @@ const AVFilter ff_vf_fspp = {
     .uninit          = uninit,
     FILTER_INPUTS(fspp_inputs),
     FILTER_OUTPUTS(fspp_outputs),
-    FILTER_QUERY_FUNC(query_formats),
+    FILTER_PIXFMTS_ARRAY(pix_fmts),
     .priv_class      = &fspp_class,
     .flags           = AVFILTER_FLAG_SUPPORT_TIMELINE_INTERNAL,
 };
