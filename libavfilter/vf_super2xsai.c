6ee0ec11a37850d3f1f30b632b88392b9a69d18e
Andreas Rheinhardt <andreas.rheinhardt@outlook.com>
2021-09-27 17:35:13 +0200

    avfilter/vf_super2xsai: Use formats list instead of query function
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

--- libavfilter/vf_super2xsai.c
+++ libavfilter/vf_super2xsai.c
@@ -245,8 +245,6 @@ static int super2xsai(AVFilterContext *ctx, void *arg, int jobnr, int nb_jobs)
     return 0;
 }
 
-static int query_formats(AVFilterContext *ctx)
-{
     static const enum AVPixelFormat pix_fmts[] = {
         AV_PIX_FMT_RGBA, AV_PIX_FMT_BGRA, AV_PIX_FMT_ARGB, AV_PIX_FMT_ABGR,
         AV_PIX_FMT_RGB24, AV_PIX_FMT_BGR24,
@@ -254,9 +252,6 @@ static int query_formats(AVFilterContext *ctx)
         AV_PIX_FMT_RGB565LE, AV_PIX_FMT_BGR565LE, AV_PIX_FMT_RGB555LE, AV_PIX_FMT_BGR555LE,
         AV_PIX_FMT_NONE
     };
-
-    return ff_set_common_formats_from_list(ctx, pix_fmts);
-}
 
 static int config_input(AVFilterLink *inlink)
 {
@@ -361,6 +356,6 @@ const AVFilter ff_vf_super2xsai = {
     .priv_size     = sizeof(Super2xSaIContext),
     FILTER_INPUTS(super2xsai_inputs),
     FILTER_OUTPUTS(super2xsai_outputs),
-    FILTER_QUERY_FUNC(query_formats),
+    FILTER_PIXFMTS_ARRAY(pix_fmts),
     .flags         = AVFILTER_FLAG_SLICE_THREADS,
 };
