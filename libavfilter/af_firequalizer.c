efc9a5b4f9957241f64573b40383f7550d62219c
Andreas Rheinhardt <andreas.rheinhardt@outlook.com>
2021-09-27 13:10:11 +0200

    avfilter/af_firequalizer: Store format in filter, remove query func
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

--- libavfilter/af_firequalizer.c
+++ libavfilter/af_firequalizer.c
@@ -180,23 +180,6 @@ static av_cold void uninit(AVFilterContext *ctx)
     av_freep(&s->gain_entry_cmd);
 }
 
-static int query_formats(AVFilterContext *ctx)
-{
-    static const enum AVSampleFormat sample_fmts[] = {
-        AV_SAMPLE_FMT_FLTP,
-        AV_SAMPLE_FMT_NONE
-    };
-    int ret = ff_set_common_all_channel_counts(ctx);
-    if (ret < 0)
-        return ret;
-
-    ret = ff_set_common_formats_from_list(ctx, sample_fmts);
-    if (ret < 0)
-        return ret;
-
-    return ff_set_common_all_samplerates(ctx);
-}
-
 static void fast_convolute(FIREqualizerContext *av_restrict s, const float *av_restrict kernel_buf, float *av_restrict conv_buf,
                            OverlapIndex *av_restrict idx, float *av_restrict data, int nsamples)
 {
@@ -962,6 +945,6 @@ const AVFilter ff_af_firequalizer = {
     .priv_size          = sizeof(FIREqualizerContext),
     FILTER_INPUTS(firequalizer_inputs),
     FILTER_OUTPUTS(firequalizer_outputs),
-    FILTER_QUERY_FUNC(query_formats),
+    FILTER_SINGLE_SAMPLEFMT(AV_SAMPLE_FMT_FLTP),
     .priv_class         = &firequalizer_class,
 };
