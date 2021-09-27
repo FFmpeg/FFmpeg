19cdebc6acdf6d373472d3ca4660093f49c15ec2
Andreas Rheinhardt <andreas.rheinhardt@outlook.com>
2021-09-27 23:07:43 +0200

    avfilter/vf_deshake: Use formats list instead of query function
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

--- libavfilter/vf_deshake.c
+++ libavfilter/vf_deshake.c
@@ -372,15 +372,11 @@ static av_cold int init(AVFilterContext *ctx)
     return 0;
 }
 
-static int query_formats(AVFilterContext *ctx)
-{
     static const enum AVPixelFormat pix_fmts[] = {
         AV_PIX_FMT_YUV420P,  AV_PIX_FMT_YUV422P,  AV_PIX_FMT_YUV444P,  AV_PIX_FMT_YUV410P,
         AV_PIX_FMT_YUV411P,  AV_PIX_FMT_YUV440P,  AV_PIX_FMT_YUVJ420P, AV_PIX_FMT_YUVJ422P,
         AV_PIX_FMT_YUVJ444P, AV_PIX_FMT_YUVJ440P, AV_PIX_FMT_NONE
     };
-    return ff_set_common_formats_from_list(ctx, pix_fmts);
-}
 
 static int config_props(AVFilterLink *link)
 {
@@ -554,6 +550,6 @@ const AVFilter ff_vf_deshake = {
     .uninit        = uninit,
     FILTER_INPUTS(deshake_inputs),
     FILTER_OUTPUTS(deshake_outputs),
-    FILTER_QUERY_FUNC(query_formats),
+    FILTER_PIXFMTS_ARRAY(pix_fmts),
     .priv_class    = &deshake_class,
 };
