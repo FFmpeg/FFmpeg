617cf44c8c56066e87f80c85218add82f1714ae0
Limin Wang <lance.lmwang@gmail.com>
2021-10-02 22:23:19 +0800

    avcodec/bitpacked: check av_buffer_ref result
    Signed-off-by: Limin Wang <lance.lmwang@gmail.com>

--- libavcodec/bitpacked.c
+++ libavcodec/bitpacked.c
@@ -45,6 +45,10 @@ static int bitpacked_decode_uyvy422(AVCodecContext *avctx, AVFrame *frame,
     /* there is no need to copy as the data already match
      * a known pixel format */
     frame->buf[0] = av_buffer_ref(avpkt->buf);
+    if (!frame->buf[0]) {
+        return AVERROR(ENOMEM);
+    }
+
     ret = av_image_fill_arrays(frame->data, frame->linesize, avpkt->data,
                                avctx->pix_fmt, avctx->width, avctx->height, 1);
     if (ret < 0) {
