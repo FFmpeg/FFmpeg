c9b44a79d1bc87251f36926b288f79adefec397f
Andreas Rheinhardt <andreas.rheinhardt@outlook.com>
2021-10-03 14:53:31 +0200

    avcodec/rasc: Fix potential use of uninitialized value
    Fixes Coverity issue #1439566.
    
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

--- libavcodec/rasc.c
+++ libavcodec/rasc.c
@@ -721,6 +721,7 @@ static int decode_frame(AVCodecContext *avctx,
             break;
         default:
             bytestream2_skip(gb, size);
+            ret = 0;
         }
 
         if (ret < 0)
