66c7329c6e1e25e125fb250f4cddd829ad5ada57
Andreas Rheinhardt <andreas.rheinhardt@outlook.com>
2021-10-07 12:53:49 +0200

    Revert "avfilter/vf_idet: reduce noisyness if the filter has been auto inserted"
    This reverts commit 723c37d3b7b5555f23bfdfe3e5c3599543c06332.
    Said commit was in preparation for auto-inserting the idet filter.
    This has never happened; even if it did, the code is wrong, because
    it segfaults if the filter instance doesn't have a name (having one
    is not mandatory). Furthermore, it is documented for libavfilter to
    not assign any semantics to the name, which this check violates.
    
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>
    (cherry picked from commit f626a3d0e0e6c59af4e844c7e2d308a93ea8c3e2)

--- libavfilter/vf_idet.c
+++ libavfilter/vf_idet.c
@@ -336,20 +336,19 @@ static int request_frame(AVFilterLink *link)
 static av_cold void uninit(AVFilterContext *ctx)
 {
     IDETContext *idet = ctx->priv;
-    int level = strncmp(ctx->name, "auto-inserted", 13) ? AV_LOG_INFO : AV_LOG_DEBUG;
 
-    av_log(ctx, level, "Repeated Fields: Neither:%6"PRId64" Top:%6"PRId64" Bottom:%6"PRId64"\n",
+    av_log(ctx, AV_LOG_INFO, "Repeated Fields: Neither:%6"PRId64" Top:%6"PRId64" Bottom:%6"PRId64"\n",
            idet->total_repeats[REPEAT_NONE],
            idet->total_repeats[REPEAT_TOP],
            idet->total_repeats[REPEAT_BOTTOM]
         );
-    av_log(ctx, level, "Single frame detection: TFF:%6"PRId64" BFF:%6"PRId64" Progressive:%6"PRId64" Undetermined:%6"PRId64"\n",
+    av_log(ctx, AV_LOG_INFO, "Single frame detection: TFF:%6"PRId64" BFF:%6"PRId64" Progressive:%6"PRId64" Undetermined:%6"PRId64"\n",
            idet->total_prestat[TFF],
            idet->total_prestat[BFF],
            idet->total_prestat[PROGRESSIVE],
            idet->total_prestat[UNDETERMINED]
         );
-    av_log(ctx, level, "Multi frame detection: TFF:%6"PRId64" BFF:%6"PRId64" Progressive:%6"PRId64" Undetermined:%6"PRId64"\n",
+    av_log(ctx, AV_LOG_INFO, "Multi frame detection: TFF:%6"PRId64" BFF:%6"PRId64" Progressive:%6"PRId64" Undetermined:%6"PRId64"\n",
            idet->total_poststat[TFF],
            idet->total_poststat[BFF],
            idet->total_poststat[PROGRESSIVE],
