7f3878828d88c6a8287d54818659e6f70293dabb
Lynne <dev@lynne.ee>
2021-10-11 00:20:15 +0200

    hwcontext_vulkan: bump required Vulkan loader version to 1.2

--- libavutil/hwcontext_vulkan.h
+++ libavutil/hwcontext_vulkan.h
@@ -50,7 +50,7 @@ typedef struct AVVulkanDeviceContext {
     PFN_vkGetInstanceProcAddr get_proc_addr;
 
     /**
-     * Vulkan instance. Must be at least version 1.1.
+     * Vulkan instance. Must be at least version 1.2.
      */
     VkInstance inst;
 
