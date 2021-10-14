c0edfb514bbadd9ce525ed7e1b7ae1cdd65e0df8
Limin Wang <lance.lmwang@gmail.com>
2021-10-14 19:47:32 +0800

    avformat/movenc: support dvwC box for Dolby Vision
    By <<Dolby Vision Streams Within the ISO Base Media File Format Version 2.2>>
    
    Signed-off-by: Limin Wang <lance.lmwang@gmail.com>

--- libavformat/movenc.c
+++ libavformat/movenc.c
@@ -1912,7 +1912,9 @@ static int mov_write_sv3d_tag(AVFormatContext *s, AVIOContext *pb, AVSphericalMa
 static int mov_write_dvcc_dvvc_tag(AVFormatContext *s, AVIOContext *pb, AVDOVIDecoderConfigurationRecord *dovi)
 {
     avio_wb32(pb, 32); /* size = 8 + 24 */
-    if (dovi->dv_profile > 7)
+    if (dovi->dv_profile > 10)
+        ffio_wfourcc(pb, "dvwC");
+    else if (dovi->dv_profile > 7)
         ffio_wfourcc(pb, "dvvC");
     else
         ffio_wfourcc(pb, "dvcC");
@@ -1926,7 +1928,7 @@ static int mov_write_dvcc_dvvc_tag(AVFormatContext *s, AVIOContext *pb, AVDOVIDe
     ffio_fill(pb, 0, 4 * 4); /* reserved */
     av_log(s, AV_LOG_DEBUG, "DOVI in %s box, version: %d.%d, profile: %d, level: %d, "
            "rpu flag: %d, el flag: %d, bl flag: %d, compatibility id: %d\n",
-           dovi->dv_profile > 7 ? "dvvC" : "dvcC",
+           dovi->dv_profile > 10 ? "dvwC" : (dovi->dv_profile > 7 ? "dvvC" : "dvcC"),
            dovi->dv_version_major, dovi->dv_version_minor,
            dovi->dv_profile, dovi->dv_level,
            dovi->rpu_present_flag,
