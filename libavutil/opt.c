9fd2b39428165c2319106e82a3c1ca1bba11aa54
Zhao Zhili <quinkblack@foxmail.com>
2021-11-10 23:36:15 +0800

    avutil/opt: handle whole range of int64_t in av_opt_get_int
    Make get_int/set_int symetric. The int64_t to double to int64_t
    conversion is unprecise for large value.
    
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavutil/opt.c
+++ libavutil/opt.c
@@ -920,7 +920,10 @@ int av_opt_get_int(void *obj, const char *name, int search_flags, int64_t *out_v
 
     if ((ret = get_number(obj, name, NULL, &num, &den, &intnum, search_flags)) < 0)
         return ret;
-    *out_val = num * intnum / den;
+    if (num == den)
+        *out_val = intnum;
+    else
+        *out_val = num * intnum / den;
     return 0;
 }
 
