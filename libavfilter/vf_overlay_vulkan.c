14f78d6234f3c0370a7960c43792551b9533d314
Wu Jianhua <jianhua.wu@intel.com>
2021-11-19 21:52:42 +0800

    avfilter/overlay_vulkan: use FF_ARRAY_ELEMS instead of magic number
    Signed-off-by: Wu Jianhua <jianhua.wu@intel.com>

--- libavfilter/vf_overlay_vulkan.c
+++ libavfilter/vf_overlay_vulkan.c
@@ -146,7 +146,7 @@ static av_cold int init_filter(AVFilterContext *ctx)
 
         ff_vk_set_compute_shader_sizes(shd, CGROUPS);
 
-        RET(ff_vk_add_descriptor_set(vkctx, s->pl, shd,  desc_i, 3, 0)); /* set 0 */
+        RET(ff_vk_add_descriptor_set(vkctx, s->pl, shd,  desc_i, FF_ARRAY_ELEMS(desc_i), 0)); /* set 0 */
         RET(ff_vk_add_descriptor_set(vkctx, s->pl, shd, &desc_b, 1, 0)); /* set 1 */
 
         GLSLD(   overlay_noalpha                                              );
