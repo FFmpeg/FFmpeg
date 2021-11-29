722d28db129b2e52d19c1a5051ea4ab0a9b84062
Marton Balint <cus@passwd.hu>
2021-11-30 00:50:16 +0100

    avformat/file: use proper return value in file_close
    Signed-off-by: Marton Balint <cus@passwd.hu>

--- libavformat/file.c
+++ libavformat/file.c
@@ -266,7 +266,8 @@ static int64_t file_seek(URLContext *h, int64_t pos, int whence)
 static int file_close(URLContext *h)
 {
     FileContext *c = h->priv_data;
-    return close(c->fd);
+    int ret = close(c->fd);
+    return (ret == -1) ? AVERROR(errno) : 0;
 }
 
 static int file_open_dir(URLContext *h)
