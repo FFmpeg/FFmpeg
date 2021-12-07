dd949124793c722ed55dead9da245574ace81968
Michael Niedermayer <michael@niedermayer.cc>
2021-12-07 09:14:09 +0100

    avformat/4xm: Check for duplicate track ids
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavformat/4xm.c
+++ libavformat/4xm.c
@@ -149,6 +149,9 @@ static int parse_strk(AVFormatContext *s,
         memset(&fourxm->tracks[fourxm->track_count], 0,
                sizeof(AudioTrack) * (track + 1 - fourxm->track_count));
         fourxm->track_count = track + 1;
+    } else {
+        if (fourxm->tracks[track].bits)
+            return AVERROR_INVALIDDATA;
     }
     fourxm->tracks[track].adpcm       = AV_RL32(buf + 12);
     fourxm->tracks[track].channels    = AV_RL32(buf + 36);
