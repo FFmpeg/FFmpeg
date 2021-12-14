d82c91ba2f889bc5e0ee7c976219f91e62dc9f8b
Andreas Rheinhardt <andreas.rheinhardt@outlook.com>
2021-12-14 11:19:08 +0100

    configure, avcodec/Makefile: Add new mpeg4audio CONFIG_EXTRA group
    This group is mainly for the users of *_mpeg4audio_get_config2();
    it is not for those who only use avpriv_mpeg4audio_sample_rates.
    This is in preparation for splitting the latter into a file of its own;
    if there were no CONFIG_EXTRA group for *_mpeg4audio_get_config2()
    users, one would have to add a dependency to the new file for all
    these users on top of the existing dependency on mpeg4audio.o.
    
    Adding a new CONFIG_EXTRA group only takes effect after a reconfigure;
    so in order to force a reconfigure some unnecessary headers from
    libavdevice/alldevices.c have been removed.
    
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

--- libavdevice/alldevices.c
+++ libavdevice/alldevices.c
@@ -18,8 +18,6 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
-#include "config.h"
-#include "libavutil/thread.h"
 #include "libavformat/internal.h"
 #include "avdevice.h"
 
