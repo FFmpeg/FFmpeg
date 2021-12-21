0f77ee9d97a9a9ed56da59a192acc616f574b932
rcombs <rcombs@rcombs.me>
2021-12-21 02:37:10 -0600

    lavfi/metal: fix build on pre-10.15 SDKs

--- libavfilter/metal/utils.m
+++ libavfilter/metal/utils.m
@@ -29,6 +29,8 @@ void ff_metal_compute_encoder_dispatch(id<MTLDevice> device,
     NSUInteger h = pipeline.maxTotalThreadsPerThreadgroup / w;
     MTLSize threadsPerThreadgroup = MTLSizeMake(w, h, 1);
     BOOL fallback = YES;
+    // MAC_OS_X_VERSION_10_15 is only defined on SDKs new enough to include its functionality (including iOS, tvOS, etc)
+#ifdef MAC_OS_X_VERSION_10_15
     if (@available(macOS 10.15, iOS 11, tvOS 14.5, *)) {
         if ([device supportsFamily:MTLGPUFamilyCommon3]) {
             MTLSize threadsPerGrid = MTLSizeMake(width, height, 1);
@@ -36,6 +38,7 @@ void ff_metal_compute_encoder_dispatch(id<MTLDevice> device,
             fallback = NO;
         }
     }
+#endif
     if (fallback) {
         MTLSize threadgroups = MTLSizeMake((width + w - 1) / w,
                                            (height + h - 1) / h,
