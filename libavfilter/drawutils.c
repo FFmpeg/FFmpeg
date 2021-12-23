9b236b95f8f210b4a60336449f39b374849eb7b9
rcombs <rcombs@rcombs.me>
2021-12-23 04:13:37 -0600

    lavfi/drawutils: remove redundant BE format checks
    We already explicitly don't support big-endian in general

--- libavfilter/drawutils.c
+++ libavfilter/drawutils.c
@@ -93,7 +93,7 @@ int ff_draw_init(FFDrawContext *draw, enum AVPixelFormat format, unsigned flags)
         return AVERROR(ENOSYS);
     if (desc->flags & ~(AV_PIX_FMT_FLAG_PLANAR | AV_PIX_FMT_FLAG_RGB | AV_PIX_FMT_FLAG_ALPHA))
         return AVERROR(ENOSYS);
-    if (format == AV_PIX_FMT_P010LE || format == AV_PIX_FMT_P010BE || format == AV_PIX_FMT_P016LE || format == AV_PIX_FMT_P016BE)
+    if (format == AV_PIX_FMT_P010LE || format == AV_PIX_FMT_P016LE)
         return AVERROR(ENOSYS);
     if (format == AV_PIX_FMT_YUVJ420P || format == AV_PIX_FMT_YUVJ422P || format == AV_PIX_FMT_YUVJ444P ||
         format == AV_PIX_FMT_YUVJ411P || format == AV_PIX_FMT_YUVJ440P)
