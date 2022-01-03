4be85c9331ca7c3360dffbbd4ce5055e8c478d2e
Michael Niedermayer <michael@niedermayer.cc>
2022-01-03 22:10:46 +0100

    lib*/version.h: Bump Versions after release/5.0 branch
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavformat/version.h
+++ libavformat/version.h
@@ -32,7 +32,7 @@
 // Major bumping may affect Ticket5467, 5421, 5451(compatibility with Chromium)
 // Also please add any ticket numbers that you believe might be affected here
 #define LIBAVFORMAT_VERSION_MAJOR  59
-#define LIBAVFORMAT_VERSION_MINOR  14
+#define LIBAVFORMAT_VERSION_MINOR  15
 #define LIBAVFORMAT_VERSION_MICRO 100
 
 #define LIBAVFORMAT_VERSION_INT AV_VERSION_INT(LIBAVFORMAT_VERSION_MAJOR, \
