c32f6b7f8abcfd9f671f80e423e92ee0408eaa08
Andreas Rheinhardt <andreas.rheinhardt@outlook.com>
2022-01-10 17:24:44 +0100

    avcodec/h264pred: Remove dead > 8 pixels checks for 8bit codecs
    RV40, SVQ3 and VP7/VP8 are eight-bit only, so it makes no sense
    to check for them in the codepath initializing > eight bit contexts.
    Move the codec-specific code to a switch located after the eight-bit
    init code where this is easily possible; and add checks to the macro
    to enable the compiler to remove the remaining checks when initializing
    bitdepths > 8 at compile-time.
    
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

--- libavcodec/h264pred.c
+++ libavcodec/h264pred.c
@@ -445,56 +445,19 @@ av_cold void ff_h264_pred_init(H264PredContext *h, int codec_id,
 #define FUNCD(a) a ## _c
 
 #define H264_PRED(depth) \
-    if(codec_id != AV_CODEC_ID_RV40){\
-        if (codec_id == AV_CODEC_ID_VP7 || codec_id == AV_CODEC_ID_VP8) {\
-            h->pred4x4[VERT_PRED       ]= FUNCD(pred4x4_vertical_vp8);\
-            h->pred4x4[HOR_PRED        ]= FUNCD(pred4x4_horizontal_vp8);\
-        } else {\
             h->pred4x4[VERT_PRED       ]= FUNCC(pred4x4_vertical          , depth);\
             h->pred4x4[HOR_PRED        ]= FUNCC(pred4x4_horizontal        , depth);\
-        }\
         h->pred4x4[DC_PRED             ]= FUNCC(pred4x4_dc                , depth);\
-        if(codec_id == AV_CODEC_ID_SVQ3)\
-            h->pred4x4[DIAG_DOWN_LEFT_PRED ]= FUNCD(pred4x4_down_left_svq3);\
-        else\
             h->pred4x4[DIAG_DOWN_LEFT_PRED ]= FUNCC(pred4x4_down_left     , depth);\
         h->pred4x4[DIAG_DOWN_RIGHT_PRED]= FUNCC(pred4x4_down_right        , depth);\
         h->pred4x4[VERT_RIGHT_PRED     ]= FUNCC(pred4x4_vertical_right    , depth);\
         h->pred4x4[HOR_DOWN_PRED       ]= FUNCC(pred4x4_horizontal_down   , depth);\
-        if (codec_id == AV_CODEC_ID_VP7 || codec_id == AV_CODEC_ID_VP8) {\
-            h->pred4x4[VERT_LEFT_PRED  ]= FUNCD(pred4x4_vertical_left_vp8);\
-        } else\
             h->pred4x4[VERT_LEFT_PRED  ]= FUNCC(pred4x4_vertical_left     , depth);\
         h->pred4x4[HOR_UP_PRED         ]= FUNCC(pred4x4_horizontal_up     , depth);\
-        if (codec_id != AV_CODEC_ID_VP7 && codec_id != AV_CODEC_ID_VP8) {\
             h->pred4x4[LEFT_DC_PRED    ]= FUNCC(pred4x4_left_dc           , depth);\
             h->pred4x4[TOP_DC_PRED     ]= FUNCC(pred4x4_top_dc            , depth);\
-        } else {\
-            h->pred4x4[TM_VP8_PRED     ]= FUNCD(pred4x4_tm_vp8);\
-            h->pred4x4[DC_127_PRED     ]= FUNCD(pred4x4_127_dc);\
-            h->pred4x4[DC_129_PRED     ]= FUNCD(pred4x4_129_dc);\
-            h->pred4x4[VERT_VP8_PRED   ]= FUNCC(pred4x4_vertical          , depth);\
-            h->pred4x4[HOR_VP8_PRED    ]= FUNCC(pred4x4_horizontal        , depth);\
-        }\
-        if (codec_id != AV_CODEC_ID_VP8)\
+    if (depth > 8 || codec_id != AV_CODEC_ID_VP8)\
             h->pred4x4[DC_128_PRED     ]= FUNCC(pred4x4_128_dc            , depth);\
-    }else{\
-        h->pred4x4[VERT_PRED           ]= FUNCC(pred4x4_vertical          , depth);\
-        h->pred4x4[HOR_PRED            ]= FUNCC(pred4x4_horizontal        , depth);\
-        h->pred4x4[DC_PRED             ]= FUNCC(pred4x4_dc                , depth);\
-        h->pred4x4[DIAG_DOWN_LEFT_PRED ]= FUNCD(pred4x4_down_left_rv40);\
-        h->pred4x4[DIAG_DOWN_RIGHT_PRED]= FUNCC(pred4x4_down_right        , depth);\
-        h->pred4x4[VERT_RIGHT_PRED     ]= FUNCC(pred4x4_vertical_right    , depth);\
-        h->pred4x4[HOR_DOWN_PRED       ]= FUNCC(pred4x4_horizontal_down   , depth);\
-        h->pred4x4[VERT_LEFT_PRED      ]= FUNCD(pred4x4_vertical_left_rv40);\
-        h->pred4x4[HOR_UP_PRED         ]= FUNCD(pred4x4_horizontal_up_rv40);\
-        h->pred4x4[LEFT_DC_PRED        ]= FUNCC(pred4x4_left_dc           , depth);\
-        h->pred4x4[TOP_DC_PRED         ]= FUNCC(pred4x4_top_dc            , depth);\
-        h->pred4x4[DC_128_PRED         ]= FUNCC(pred4x4_128_dc            , depth);\
-        h->pred4x4[DIAG_DOWN_LEFT_PRED_RV40_NODOWN]= FUNCD(pred4x4_down_left_rv40_nodown);\
-        h->pred4x4[HOR_UP_PRED_RV40_NODOWN]= FUNCD(pred4x4_horizontal_up_rv40_nodown);\
-        h->pred4x4[VERT_LEFT_PRED_RV40_NODOWN]= FUNCD(pred4x4_vertical_left_rv40_nodown);\
-    }\
 \
     h->pred8x8l[VERT_PRED           ]= FUNCC(pred8x8l_vertical            , depth);\
     h->pred8x8l[HOR_PRED            ]= FUNCC(pred8x8l_horizontal          , depth);\
@@ -516,16 +479,14 @@ av_cold void ff_h264_pred_init(H264PredContext *h, int codec_id,
         h->pred8x8[VERT_PRED8x8   ]= FUNCC(pred8x16_vertical              , depth);\
         h->pred8x8[HOR_PRED8x8    ]= FUNCC(pred8x16_horizontal            , depth);\
     }\
-    if (codec_id != AV_CODEC_ID_VP7 && codec_id != AV_CODEC_ID_VP8) {\
         if (chroma_format_idc <= 1) {\
             h->pred8x8[PLANE_PRED8x8]= FUNCC(pred8x8_plane                , depth);\
         } else {\
             h->pred8x8[PLANE_PRED8x8]= FUNCC(pred8x16_plane               , depth);\
         }\
-    } else\
-        h->pred8x8[PLANE_PRED8x8]= FUNCD(pred8x8_tm_vp8);\
-    if (codec_id != AV_CODEC_ID_RV40 && codec_id != AV_CODEC_ID_VP7 && \
-        codec_id != AV_CODEC_ID_VP8) {\
+    if (depth > 8 || (codec_id != AV_CODEC_ID_RV40 && \
+                      codec_id != AV_CODEC_ID_VP7  && \
+                      codec_id != AV_CODEC_ID_VP8)) { \
         if (chroma_format_idc <= 1) {\
             h->pred8x8[DC_PRED8x8     ]= FUNCC(pred8x8_dc                     , depth);\
             h->pred8x8[LEFT_DC_PRED8x8]= FUNCC(pred8x8_left_dc                , depth);\
@@ -547,10 +508,6 @@ av_cold void ff_h264_pred_init(H264PredContext *h, int codec_id,
         h->pred8x8[DC_PRED8x8     ]= FUNCD(pred8x8_dc_rv40);\
         h->pred8x8[LEFT_DC_PRED8x8]= FUNCD(pred8x8_left_dc_rv40);\
         h->pred8x8[TOP_DC_PRED8x8 ]= FUNCD(pred8x8_top_dc_rv40);\
-        if (codec_id == AV_CODEC_ID_VP7 || codec_id == AV_CODEC_ID_VP8) {\
-            h->pred8x8[DC_127_PRED8x8]= FUNCC(pred8x8_127_dc, 8);\
-            h->pred8x8[DC_129_PRED8x8]= FUNCC(pred8x8_129_dc, 8);\
-        }\
     }\
     if (chroma_format_idc <= 1) {\
         h->pred8x8[DC_128_PRED8x8 ]= FUNCC(pred8x8_128_dc                 , depth);\
@@ -561,23 +518,7 @@ av_cold void ff_h264_pred_init(H264PredContext *h, int codec_id,
     h->pred16x16[DC_PRED8x8     ]= FUNCC(pred16x16_dc                     , depth);\
     h->pred16x16[VERT_PRED8x8   ]= FUNCC(pred16x16_vertical               , depth);\
     h->pred16x16[HOR_PRED8x8    ]= FUNCC(pred16x16_horizontal             , depth);\
-    switch(codec_id){\
-    case AV_CODEC_ID_SVQ3:\
-       h->pred16x16[PLANE_PRED8x8  ]= FUNCD(pred16x16_plane_svq3);\
-       break;\
-    case AV_CODEC_ID_RV40:\
-       h->pred16x16[PLANE_PRED8x8  ]= FUNCD(pred16x16_plane_rv40);\
-       break;\
-    case AV_CODEC_ID_VP7:\
-    case AV_CODEC_ID_VP8:\
-       h->pred16x16[PLANE_PRED8x8  ]= FUNCD(pred16x16_tm_vp8);\
-       h->pred16x16[DC_127_PRED8x8]= FUNCC(pred16x16_127_dc, 8);\
-       h->pred16x16[DC_129_PRED8x8]= FUNCC(pred16x16_129_dc, 8);\
-       break;\
-    default:\
-       h->pred16x16[PLANE_PRED8x8  ]= FUNCC(pred16x16_plane               , depth);\
-       break;\
-    }\
+    h->pred16x16[PLANE_PRED8x8  ]= FUNCC(pred16x16_plane                  , depth);\
     h->pred16x16[LEFT_DC_PRED8x8]= FUNCC(pred16x16_left_dc                , depth);\
     h->pred16x16[TOP_DC_PRED8x8 ]= FUNCC(pred16x16_top_dc                 , depth);\
     h->pred16x16[DC_128_PRED8x8 ]= FUNCC(pred16x16_128_dc                 , depth);\
@@ -615,6 +556,38 @@ av_cold void ff_h264_pred_init(H264PredContext *h, int codec_id,
         default:
             av_assert0(bit_depth<=8);
             H264_PRED(8)
+            switch (codec_id) {
+            case AV_CODEC_ID_SVQ3:
+                h->pred4x4[DIAG_DOWN_LEFT_PRED] = FUNCD(pred4x4_down_left_svq3);
+                h->pred16x16[PLANE_PRED8x8    ] = FUNCD(pred16x16_plane_svq3);
+                break;
+            case AV_CODEC_ID_RV40:
+                h->pred4x4[DIAG_DOWN_LEFT_PRED] = FUNCD(pred4x4_down_left_rv40);
+                h->pred4x4[VERT_LEFT_PRED     ] = FUNCD(pred4x4_vertical_left_rv40);
+                h->pred4x4[HOR_UP_PRED        ] = FUNCD(pred4x4_horizontal_up_rv40);
+                h->pred4x4[DIAG_DOWN_LEFT_PRED_RV40_NODOWN] = FUNCD(pred4x4_down_left_rv40_nodown);
+                h->pred4x4[HOR_UP_PRED_RV40_NODOWN] = FUNCD(pred4x4_horizontal_up_rv40_nodown);
+                h->pred4x4[VERT_LEFT_PRED_RV40_NODOWN] = FUNCD(pred4x4_vertical_left_rv40_nodown);
+                h->pred16x16[PLANE_PRED8x8    ] = FUNCD(pred16x16_plane_rv40);
+                break;
+            case AV_CODEC_ID_VP7:
+            case AV_CODEC_ID_VP8:
+                h->pred4x4[VERT_PRED       ] = FUNCD(pred4x4_vertical_vp8);
+                h->pred4x4[HOR_PRED        ] = FUNCD(pred4x4_horizontal_vp8);
+                h->pred4x4[VERT_LEFT_PRED  ] = FUNCD(pred4x4_vertical_left_vp8);
+                h->pred4x4[TM_VP8_PRED     ] = FUNCD(pred4x4_tm_vp8);
+                h->pred4x4[VERT_VP8_PRED   ] = FUNCC(pred4x4_vertical, 8);
+                h->pred4x4[DC_127_PRED     ] = FUNCD(pred4x4_127_dc);
+                h->pred4x4[DC_129_PRED     ] = FUNCD(pred4x4_129_dc);
+                h->pred4x4[HOR_VP8_PRED    ] = FUNCC(pred4x4_horizontal, 8);
+                h->pred8x8[PLANE_PRED8x8   ] = FUNCD(pred8x8_tm_vp8);
+                h->pred8x8[DC_127_PRED8x8  ] = FUNCC(pred8x8_127_dc, 8);
+                h->pred8x8[DC_129_PRED8x8  ] = FUNCC(pred8x8_129_dc, 8);
+                h->pred16x16[PLANE_PRED8x8 ] = FUNCD(pred16x16_tm_vp8);
+                h->pred16x16[DC_127_PRED8x8] = FUNCC(pred16x16_127_dc, 8);
+                h->pred16x16[DC_129_PRED8x8] = FUNCC(pred16x16_129_dc, 8);
+                break;
+            }
             break;
     }
 
