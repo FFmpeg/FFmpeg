5d5b62e595593f0fc79c301260aacfe6bc8ee4f4
Andreas Rheinhardt <andreas.rheinhardt@outlook.com>
2022-01-18 13:13:11 +0100

    avformat/mux: Remove assert based on faulty assumptions
    This assert is based upon the wrong assumption that
    the noninterleaved codepath is never used; if it is used,
    max_interleave_delta is irrelevant. It furthermore
    ignores audio_preload.
    
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

--- libavformat/mux.c
+++ libavformat/mux.c
@@ -697,7 +697,6 @@ static int write_packet(AVFormatContext *s, AVPacket *pkt)
                 );
             }
         } else {
-            av_assert2(pkt->dts == AV_NOPTS_VALUE || pkt->dts >= 0 || s->max_interleave_delta > 0);
             if (pkt->dts != AV_NOPTS_VALUE && pkt->dts < 0) {
                 av_log(s, AV_LOG_WARNING,
                     "Packets poorly interleaved, failed to avoid negative "
