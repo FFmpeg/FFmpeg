c14976be045f3fe658c12d7e30946cdb380452ec
Lynne <dev@lynne.ee>
2022-01-25 04:45:34 +0100

    lavu/tx: improve documentation for existing transforms

--- libavutil/tx.h
+++ libavutil/tx.h
@@ -38,50 +38,35 @@ typedef struct AVComplexInt32 {
 
 enum AVTXType {
     /**
-     * Standard complex to complex FFT with sample data type AVComplexFloat.
+     * Standard complex to complex FFT with sample data type of AVComplexFloat,
+     * AVComplexDouble or AVComplexInt32, for each respective variant.
+     *
      * Output is not 1/len normalized. Scaling currently unsupported.
-     * The stride parameter is ignored.
+     * The stride parameter must be set to the size of a single sample in bytes.
      */
-    AV_TX_FLOAT_FFT = 0,
+    AV_TX_FLOAT_FFT  = 0,
+    AV_TX_DOUBLE_FFT = 2,
+    AV_TX_INT32_FFT  = 4,
 
     /**
-     * Standard MDCT with sample data type of float and a scale type of
-     * float. Length is the frame size, not the window size (which is 2x frame)
+     * Standard MDCT with a sample data type of float, double or int32_t,
+     * respecively. For the float and int32 variants, the scale type is
+     * 'float', while for the double variant, it's 'double'.
+     *
+     * Length is the frame size, not the window size (which is 2x frame).
      * For forward transforms, the stride specifies the spacing between each
      * sample in the output array in bytes. The input must be a flat array.
      *
      * For inverse transforms, the stride specifies the spacing between each
-     * sample in the input array in bytes. The output will be a flat array.
-     * Stride must be a non-zero multiple of sizeof(float).
+     * sample in the input array in bytes. The output must be a flat array.
      *
      * NOTE: the inverse transform is half-length, meaning the output will not
      * contain redundant data. This is what most codecs work with. To do a full
      * inverse transform, set the AV_TX_FULL_IMDCT flag on init.
      */
-    AV_TX_FLOAT_MDCT = 1,
-
-    /**
-     * Same as AV_TX_FLOAT_FFT with a data type of AVComplexDouble.
-     */
-    AV_TX_DOUBLE_FFT = 2,
-
-    /**
-     * Same as AV_TX_FLOAT_MDCT with data and scale type of double.
-     * Stride must be a non-zero multiple of sizeof(double).
-     */
+    AV_TX_FLOAT_MDCT  = 1,
     AV_TX_DOUBLE_MDCT = 3,
-
-    /**
-     * Same as AV_TX_FLOAT_FFT with a data type of AVComplexInt32.
-     */
-    AV_TX_INT32_FFT = 4,
-
-    /**
-     * Same as AV_TX_FLOAT_MDCT with data type of int32_t and scale type of float.
-     * Only scale values less than or equal to 1.0 are supported.
-     * Stride must be a non-zero multiple of sizeof(int32_t).
-     */
-    AV_TX_INT32_MDCT = 5,
+    AV_TX_INT32_MDCT  = 5,
 };
 
 /**
