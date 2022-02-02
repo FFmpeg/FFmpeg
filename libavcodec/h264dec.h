32ce3b71688aa797d9795e7aa0427df02a0da189
Andreas Rheinhardt <andreas.rheinhardt@outlook.com>
2022-02-02 05:01:51 +0100

    avcodec/mpegutils.h: Move DELAYED_PIC_REF macro to h264dec.h
    It is only used by the H.264 decoder (as well as the dirac decoder,
    which already uses a local copy).
    
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

--- libavcodec/h264dec.h
+++ libavcodec/h264dec.h
@@ -138,6 +138,11 @@ typedef struct H264Picture {
     int mbaff;              ///< 1 -> MBAFF frame 0-> not MBAFF
     int field_picture;      ///< whether or not picture was encoded in separate fields
 
+/**
+ * H264Picture.reference has this flag set,
+ * when the picture is held for delayed output.
+ */
+#define DELAYED_PIC_REF  (1 << 2)
     int reference;
     int recovered;          ///< picture at IDR or recovery point + recovery count
     int invalid_gap;
--- libavcodec/mpegutils.h
+++ libavcodec/mpegutils.h
@@ -37,12 +37,6 @@
 #define PICT_BOTTOM_FIELD  2
 #define PICT_FRAME         3
 
-/**
- * Value of Picture.reference when Picture is not a reference picture, but
- * is held for delayed output.
- */
-#define DELAYED_PIC_REF 4
-
 #define MAX_MB_BYTES    (30 * 16 * 16 * 3 / 8 + 120)
 #define MAX_FCODE        7
 
