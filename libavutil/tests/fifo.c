d51211526722196345029b7e45ee59f01ed3baf6
Anton Khirnov <anton@khirnov.net>
2022-02-08 19:24:22 +0100

    lavu/fifo: add a test for _cb functions
    Makes an auto-growing FIFO and performs a sequence of randomly-sized
    writes/peeks/reads.

--- libavutil/tests/fifo.c
+++ libavutil/tests/fifo.c
@@ -18,7 +18,53 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "libavutil/common.h"
 #include "libavutil/fifo.h"
+#include "libavutil/lfg.h"
+#include "libavutil/random_seed.h"
+
+typedef struct CBState {
+    unsigned int read_idx;
+    unsigned int write_idx;
+    unsigned int to_process;
+    unsigned int offset;
+} CBState;
+
+static int read_cb(void *opaque, void *buf, size_t *nb_elems)
+{
+    CBState  *s = opaque;
+    unsigned *b = buf;
+
+    *nb_elems = FFMIN(*nb_elems, s->to_process);
+
+    for (unsigned i = 0; i < *nb_elems; i++)
+        if (b[i] != s->read_idx + s->offset + i) {
+            printf("Mismatch at idx %u offset %u i %u\n",
+                   s->read_idx, s->offset, i);
+            return AVERROR_BUG;
+        }
+
+    s->offset     += *nb_elems;
+    s->to_process -= *nb_elems;
+
+    return 0;
+}
+
+static int write_cb(void *opaque, void *buf, size_t *nb_elems)
+{
+    CBState  *s = opaque;
+    unsigned *b = buf;
+
+    *nb_elems = FFMIN(*nb_elems, s->to_process);
+
+    for (unsigned i = 0; i < *nb_elems; i++)
+        b[i] = s->write_idx + i;
+
+    s->write_idx  += *nb_elems;
+    s->to_process -= *nb_elems;
+
+    return 0;
+}
 
 int main(void)
 {
@@ -87,6 +133,61 @@ int main(void)
     for (i = 0; i < n; i++) {
         av_fifo_peek(fifo, &j, 1, i);
         printf("%d: %d\n", i, j);
+    }
+
+    av_fifo_freep2(&fifo);
+
+    /* test randomly-sized write/read/peek with a callback */
+    {
+        CBState        s = { 0 };
+        uint32_t    seed = av_get_random_seed();
+
+        AVLFG lfg;
+        int ret;
+
+        av_lfg_init(&lfg, seed);
+
+        fifo = av_fifo_alloc2(1, sizeof(unsigned), AV_FIFO_FLAG_AUTO_GROW);
+
+        for (i = 0; i < 32; i++) {
+            size_t       nb_elems = 16;
+            unsigned   to_process = av_lfg_get(&lfg) % nb_elems;
+
+            s.to_process = to_process;
+
+            ret = av_fifo_write_from_cb(fifo, write_cb, &s, &nb_elems);
+            if (ret < 0 || s.to_process || nb_elems != to_process) {
+                printf("FIFO write fail; seed %"PRIu32"\n", seed);
+                return 1;
+            }
+
+            nb_elems = av_fifo_can_read(fifo);
+            if (nb_elems > 1) {
+                s.offset     = av_lfg_get(&lfg) % (nb_elems - 1);
+                nb_elems    -= s.offset;
+
+                s.to_process = av_lfg_get(&lfg) % nb_elems;
+                to_process   = s.to_process;
+
+                ret = av_fifo_peek_to_cb(fifo, read_cb, &s, &nb_elems, s.offset);
+                if (ret < 0 || s.to_process || nb_elems != to_process) {
+                    printf("FIFO peek fail; seed %"PRIu32"\n", seed);
+                    return 1;
+                }
+            }
+
+            nb_elems     = av_fifo_can_read(fifo);
+            to_process   = nb_elems ? av_lfg_get(&lfg) % nb_elems : 0;
+            s.to_process = to_process;
+            s.offset     = 0;
+
+            ret = av_fifo_read_to_cb(fifo, read_cb, &s, &nb_elems);
+            if (ret < 0 || s.to_process || to_process != nb_elems) {
+                printf("FIFO read fail; seed %"PRIu32"\n", seed);
+                return 1;
+            }
+            s.read_idx += s.offset;
+        }
     }
 
     av_fifo_freep2(&fifo);
