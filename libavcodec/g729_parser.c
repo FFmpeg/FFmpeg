e9e2ddbc6c78cc18b76093617f82c920e58a8d1f
Michael Niedermayer <michael@niedermayer.cc>
2022-02-27 14:43:04 +0100

    avcodec/g729_parser: Check channels
    Fixes: signed integer overflow: 10 * 808464428 cannot be represented in type 'int'
    Fixes: assertion failure
    Fixes: ticket9651
    
    Reviewed-by: Paul B Mahol <onemda@gmail.com>
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit 757da974b21833529cc41bdcc9684c29660cdfa8)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/g729_parser.c
+++ libavcodec/g729_parser.c
@@ -49,6 +49,9 @@ static int g729_parse(AVCodecParserContext *s1, AVCodecContext *avctx,
         s->block_size = (avctx->bit_rate < 8000) ? G729D_6K4_BLOCK_SIZE : G729_8K_BLOCK_SIZE;
         if (avctx->codec_id == AV_CODEC_ID_ACELP_KELVIN)
             s->block_size++;
+        // channels > 2 is invalid, we pass the packet on unchanged
+        if (avctx->channels > 2)
+            s->block_size = 0;
         s->block_size *= avctx->channels;
         s->duration   = avctx->frame_size;
     }
