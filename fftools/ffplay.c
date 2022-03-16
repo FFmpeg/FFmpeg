117672c8144b55452dbbecca5bbad88a292430e4
Zhao Zhili <quinkblack@foxmail.com>
2022-03-16 10:14:11 +0800

    ffplay: don't shadow global variable
    Signed-off-by: Marton Balint <cus@passwd.hu>

--- fftools/ffplay.c
+++ fftools/ffplay.c
@@ -1455,13 +1455,13 @@ static void check_external_clock_speed(VideoState *is) {
 }
 
 /* seek in the stream */
-static void stream_seek(VideoState *is, int64_t pos, int64_t rel, int seek_by_bytes)
+static void stream_seek(VideoState *is, int64_t pos, int64_t rel, int by_bytes)
 {
     if (!is->seek_req) {
         is->seek_pos = pos;
         is->seek_rel = rel;
         is->seek_flags &= ~AVSEEK_FLAG_BYTE;
-        if (seek_by_bytes)
+        if (by_bytes)
             is->seek_flags |= AVSEEK_FLAG_BYTE;
         is->seek_req = 1;
         SDL_CondSignal(is->continue_read_thread);
