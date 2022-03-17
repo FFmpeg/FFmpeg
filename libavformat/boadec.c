FFmpeg/FFmpeg

commit 8269fbcb7ab3b1df3c186126d7f67be7cb7c8f39

boa: convert to new channel layout API
Signed-off-by: James Almer <jamrial@gmail.com>

libavformat/boadec.c
@@ -55,15 +55,16 @@ static int read_header(AVFormatContext *s)
     avio_rl32(s->pb);
     avio_rl32(s->pb);
     st->codecpar->sample_rate = avio_rl32(s->pb);
-    st->codecpar->channels    = avio_rl32(s->pb);
-    if (st->codecpar->channels > FF_SANE_NB_CHANNELS || st->codecpar->channels <= 0)
+    st->codecpar->ch_layout.nb_channels    = avio_rl32(s->pb);
+    if (st->codecpar->ch_layout.nb_channels > FF_SANE_NB_CHANNELS ||
+        st->codecpar->ch_layout.order <= 0)
         return AVERROR(ENOSYS);
     ffformatcontext(s)->data_offset = data_offset = avio_rl32(s->pb);
     avio_r8(s->pb);
     st->codecpar->block_align = avio_rl32(s->pb);
     if (st->codecpar->block_align > INT_MAX / FF_SANE_NB_CHANNELS || st->codecpar->block_align <= 0)
         return AVERROR_INVALIDDATA;
-    st->codecpar->block_align *= st->codecpar->channels;
+    st->codecpar->block_align *= st->codecpar->ch_layout.nb_channels;
 
     avio_seek(s->pb, data_offset, SEEK_SET);
 

commit 0af06344cd35eb97d6c1e01470c5307530f3cdbe

avformat/boadec: fix check for invalid channel count
Regression since 8269fbcb7ab3b1df3c186126d7f67be7cb7c8f39.

Signed-off-by: James Almer <jamrial@gmail.com>

libavformat/boadec.c
@@ -57,7 +57,7 @@ static int read_header(AVFormatContext *s)
     st->codecpar->sample_rate = avio_rl32(s->pb);
     st->codecpar->ch_layout.nb_channels    = avio_rl32(s->pb);
     if (st->codecpar->ch_layout.nb_channels > FF_SANE_NB_CHANNELS ||
-        st->codecpar->ch_layout.order <= 0)
+        st->codecpar->ch_layout.nb_channels <= 0)
         return AVERROR(ENOSYS);
     ffformatcontext(s)->data_offset = data_offset = avio_rl32(s->pb);
     avio_r8(s->pb);