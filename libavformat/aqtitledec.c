b90c01a501382dce57b5579687e860d428b324dc
Michael Niedermayer <michael@niedermayer.cc>
2022-03-20 00:07:50 +0100

    avformat/aqtitledec: Skip unrepresentable durations
    Fixes: signed integer overflow: -5 - 9223372036854775807 cannot be represented in type 'long'
    Fixes: 45665/clusterfuzz-testcase-minimized-ffmpeg_DEMUXER_fuzzer-475618463934054
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit c2d1597a8a6470045a8da241d4f65c81f26c3107)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavformat/aqtitledec.c
+++ libavformat/aqtitledec.c
@@ -74,7 +74,8 @@ static int aqt_read_header(AVFormatContext *s)
             new_event = 1;
             pos = avio_tell(s->pb);
             if (sub) {
-                sub->duration = frame - sub->pts;
+                if (frame >= sub->pts && (uint64_t)frame - sub->pts < INT64_MAX)
+                    sub->duration = frame - sub->pts;
                 sub = NULL;
             }
         } else if (*line) {
