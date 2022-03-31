c62bbd4d2015ffa717369e687601fb2d481af6b0
Ben Avison <bavison@riscosopen.org>
2022-03-31 18:23:46 +0100

    avcodec/vc1: Arm 64-bit NEON deblocking filter fast paths
    checkasm benchmarks on 1.5 GHz Cortex-A72 are as follows. Note that the C
    version can still outperform the NEON version in specific cases. The balance
    between different code paths is stream-dependent, but in practice the best
    case happens about 5% of the time, the worst case happens about 40% of the
    time, and the complexity of the remaining cases fall somewhere in between.
    Therefore, taking the average of the best and worst case timings is
    probably a conservative estimate of the degree by which the NEON code
    improves performance.
    
    vc1dsp.vc1_h_loop_filter4_bestcase_c: 10.7
    vc1dsp.vc1_h_loop_filter4_bestcase_neon: 43.5
    vc1dsp.vc1_h_loop_filter4_worstcase_c: 184.5
    vc1dsp.vc1_h_loop_filter4_worstcase_neon: 73.7
    vc1dsp.vc1_h_loop_filter8_bestcase_c: 31.2
    vc1dsp.vc1_h_loop_filter8_bestcase_neon: 62.2
    vc1dsp.vc1_h_loop_filter8_worstcase_c: 358.2
    vc1dsp.vc1_h_loop_filter8_worstcase_neon: 88.2
    vc1dsp.vc1_h_loop_filter16_bestcase_c: 51.0
    vc1dsp.vc1_h_loop_filter16_bestcase_neon: 107.7
    vc1dsp.vc1_h_loop_filter16_worstcase_c: 722.7
    vc1dsp.vc1_h_loop_filter16_worstcase_neon: 140.5
    vc1dsp.vc1_v_loop_filter4_bestcase_c: 9.7
    vc1dsp.vc1_v_loop_filter4_bestcase_neon: 43.0
    vc1dsp.vc1_v_loop_filter4_worstcase_c: 178.7
    vc1dsp.vc1_v_loop_filter4_worstcase_neon: 69.0
    vc1dsp.vc1_v_loop_filter8_bestcase_c: 30.2
    vc1dsp.vc1_v_loop_filter8_bestcase_neon: 50.7
    vc1dsp.vc1_v_loop_filter8_worstcase_c: 353.0
    vc1dsp.vc1_v_loop_filter8_worstcase_neon: 69.2
    vc1dsp.vc1_v_loop_filter16_bestcase_c: 60.0
    vc1dsp.vc1_v_loop_filter16_bestcase_neon: 90.0
    vc1dsp.vc1_v_loop_filter16_worstcase_c: 714.2
    vc1dsp.vc1_v_loop_filter16_worstcase_neon: 97.2
    
    Signed-off-by: Ben Avison <bavison@riscosopen.org>
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- libavcodec/aarch64/vc1dsp_init_aarch64.c
+++ libavcodec/aarch64/vc1dsp_init_aarch64.c
@@ -25,6 +25,13 @@
 
 #include "config.h"
 
+void ff_vc1_v_loop_filter4_neon(uint8_t *src, ptrdiff_t stride, int pq);
+void ff_vc1_h_loop_filter4_neon(uint8_t *src, ptrdiff_t stride, int pq);
+void ff_vc1_v_loop_filter8_neon(uint8_t *src, ptrdiff_t stride, int pq);
+void ff_vc1_h_loop_filter8_neon(uint8_t *src, ptrdiff_t stride, int pq);
+void ff_vc1_v_loop_filter16_neon(uint8_t *src, ptrdiff_t stride, int pq);
+void ff_vc1_h_loop_filter16_neon(uint8_t *src, ptrdiff_t stride, int pq);
+
 void ff_put_vc1_chroma_mc8_neon(uint8_t *dst, uint8_t *src, ptrdiff_t stride,
                                 int h, int x, int y);
 void ff_avg_vc1_chroma_mc8_neon(uint8_t *dst, uint8_t *src, ptrdiff_t stride,
@@ -39,6 +46,13 @@ av_cold void ff_vc1dsp_init_aarch64(VC1DSPContext *dsp)
     int cpu_flags = av_get_cpu_flags();
 
     if (have_neon(cpu_flags)) {
+        dsp->vc1_v_loop_filter4  = ff_vc1_v_loop_filter4_neon;
+        dsp->vc1_h_loop_filter4  = ff_vc1_h_loop_filter4_neon;
+        dsp->vc1_v_loop_filter8  = ff_vc1_v_loop_filter8_neon;
+        dsp->vc1_h_loop_filter8  = ff_vc1_h_loop_filter8_neon;
+        dsp->vc1_v_loop_filter16 = ff_vc1_v_loop_filter16_neon;
+        dsp->vc1_h_loop_filter16 = ff_vc1_h_loop_filter16_neon;
+
         dsp->put_no_rnd_vc1_chroma_pixels_tab[0] = ff_put_vc1_chroma_mc8_neon;
         dsp->avg_no_rnd_vc1_chroma_pixels_tab[0] = ff_avg_vc1_chroma_mc8_neon;
         dsp->put_no_rnd_vc1_chroma_pixels_tab[1] = ff_put_vc1_chroma_mc4_neon;
