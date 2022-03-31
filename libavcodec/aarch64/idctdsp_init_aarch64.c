5379412ed0c587d82788c6fc46b7787cfe10f72d
Ben Avison <bavison@riscosopen.org>
2022-03-31 18:23:49 +0100

    avcodec/idctdsp: Arm 64-bit NEON block add and clamp fast paths
    checkasm benchmarks on 1.5 GHz Cortex-A72 are as follows.
    
    idctdsp.add_pixels_clamped_c: 313.3
    idctdsp.add_pixels_clamped_neon: 24.3
    idctdsp.put_pixels_clamped_c: 220.3
    idctdsp.put_pixels_clamped_neon: 15.5
    idctdsp.put_signed_pixels_clamped_c: 210.5
    idctdsp.put_signed_pixels_clamped_neon: 19.5
    
    Signed-off-by: Ben Avison <bavison@riscosopen.org>
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- libavcodec/aarch64/idctdsp_init_aarch64.c
+++ libavcodec/aarch64/idctdsp_init_aarch64.c
@@ -27,19 +27,29 @@
 #include "libavcodec/idctdsp.h"
 #include "idct.h"
 
+void ff_put_pixels_clamped_neon(const int16_t *, uint8_t *, ptrdiff_t);
+void ff_put_signed_pixels_clamped_neon(const int16_t *, uint8_t *, ptrdiff_t);
+void ff_add_pixels_clamped_neon(const int16_t *, uint8_t *, ptrdiff_t);
+
 av_cold void ff_idctdsp_init_aarch64(IDCTDSPContext *c, AVCodecContext *avctx,
                                      unsigned high_bit_depth)
 {
     int cpu_flags = av_get_cpu_flags();
 
-    if (have_neon(cpu_flags) && !avctx->lowres && !high_bit_depth) {
-        if (avctx->idct_algo == FF_IDCT_AUTO ||
-            avctx->idct_algo == FF_IDCT_SIMPLEAUTO ||
-            avctx->idct_algo == FF_IDCT_SIMPLENEON) {
-            c->idct_put  = ff_simple_idct_put_neon;
-            c->idct_add  = ff_simple_idct_add_neon;
-            c->idct      = ff_simple_idct_neon;
-            c->perm_type = FF_IDCT_PERM_PARTTRANS;
+    if (have_neon(cpu_flags)) {
+        if (!avctx->lowres && !high_bit_depth) {
+            if (avctx->idct_algo == FF_IDCT_AUTO ||
+                avctx->idct_algo == FF_IDCT_SIMPLEAUTO ||
+                avctx->idct_algo == FF_IDCT_SIMPLENEON) {
+                c->idct_put  = ff_simple_idct_put_neon;
+                c->idct_add  = ff_simple_idct_add_neon;
+                c->idct      = ff_simple_idct_neon;
+                c->perm_type = FF_IDCT_PERM_PARTTRANS;
+            }
         }
+
+        c->add_pixels_clamped        = ff_add_pixels_clamped_neon;
+        c->put_pixels_clamped        = ff_put_pixels_clamped_neon;
+        c->put_signed_pixels_clamped = ff_put_signed_pixels_clamped_neon;
     }
 }
