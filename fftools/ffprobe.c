83e1a1de8833845224948e5d002355c03dd117d5
Stefano Sabatini <stefasab@gmail.com>
2022-04-20 23:43:46 +0200

    doc/ffprobe: clarify that the input file is not optional
    Fix trac issue http://trac.ffmpeg.org/ticket/9562

--- fftools/ffprobe.c
+++ fftools/ffprobe.c
@@ -3417,7 +3417,7 @@ end:
 static void show_usage(void)
 {
     av_log(NULL, AV_LOG_INFO, "Simple multimedia streams analyzer\n");
-    av_log(NULL, AV_LOG_INFO, "usage: %s [OPTIONS] [INPUT_FILE]\n", program_name);
+    av_log(NULL, AV_LOG_INFO, "usage: %s [OPTIONS] INPUT_FILE\n", program_name);
     av_log(NULL, AV_LOG_INFO, "\n");
 }
 
