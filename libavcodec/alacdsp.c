8618061a4f589276d2f533de7108fa561a6bd524
Michael Niedermayer <michael@niedermayer.cc>
2022-04-28 23:34:53 +0200

    avcodec/alacdsp: Make intermediates unsigned
    Fixes: signed integer overflow: -14914387 + -2147418648 cannot be represented in type 'int'
    Fixes: 46464/clusterfuzz-testcase-minimized-ffmpeg_AV_CODEC_ID_ALAC_fuzzer-474307197311385
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit 8709f4c10a216cb3e11564bc392841e832f8e3b1)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/alacdsp.c
+++ libavcodec/alacdsp.c
@@ -29,12 +29,12 @@ static void decorrelate_stereo(int32_t *buffer[2], int nb_samples,
     int i;
 
     for (i = 0; i < nb_samples; i++) {
-        int32_t a, b;
+        uint32_t a, b;
 
         a = buffer[0][i];
         b = buffer[1][i];
 
-        a -= (int)(b * (unsigned)decorr_left_weight) >> decorr_shift;
+        a -= (int)(b * decorr_left_weight) >> decorr_shift;
         b += a;
 
         buffer[0][i] = b;
