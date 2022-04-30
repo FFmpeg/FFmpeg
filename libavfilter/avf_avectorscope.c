3ce4e0808924c55cbb387a6249bcb1e82ea44f63
Paul B Mahol <onemda@gmail.com>
2022-04-30 16:15:43 +0200

    avfilter/avf_avectorscope: request new frames only when needed

--- libavfilter/avf_avectorscope.c
+++ libavfilter/avf_avectorscope.c
@@ -414,6 +414,11 @@ static int activate(AVFilterContext *ctx)
     if (ret > 0)
         return filter_frame(inlink, in);
 
+    if (ff_inlink_queued_samples(inlink) >= s->nb_samples) {
+        ff_filter_set_ready(ctx, 10);
+        return 0;
+    }
+
     FF_FILTER_FORWARD_STATUS(inlink, outlink);
     FF_FILTER_FORWARD_WANTED(outlink, inlink);
 
