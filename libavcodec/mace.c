afc7679c89fc5bd017b910cfc0a45f8ed080c1d4
Paul B Mahol <onemda@gmail.com>
2022-06-06 17:41:19 +0200

    avcodec/mace: fix some style issues

--- libavcodec/mace.c
+++ libavcodec/mace.c
@@ -183,14 +183,13 @@ static int16_t read_table(ChannelData *chd, uint8_t val, int tab_idx)
         current = - 1 - tabs[tab_idx].tab2[((chd->index & 0x7f0) >> 4)*tabs[tab_idx].stride + 2*tabs[tab_idx].stride-val-1];
 
     if (( chd->index += tabs[tab_idx].tab1[val]-(chd->index >> 5) ) < 0)
-      chd->index = 0;
+        chd->index = 0;
 
     return current;
 }
 
 static void chomp3(ChannelData *chd, int16_t *output, uint8_t val, int tab_idx)
 {
-
     int16_t current = read_table(chd, val, tab_idx);
 
     current = mace_broken_clip_int16(current + chd->level);
