6b5e3590c761aaf8e1ccceee8eadd8d49cf9c0af
Andreas Rheinhardt <andreas.rheinhardt@outlook.com>
2022-06-14 00:30:06 +0200

    avformat/mxfdec: Use ff_data_to_hex() for data->hex conversion
    In this case it also stops pretending that the length of
    the output string is somehow checked (which is currently
    being done by using snprintf that is called with the amount
    of space needed instead of the amount of space actually available).
    
    Reviewed-by: Tomas Härdin <tjoppen@acc.umu.se>
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

--- libavformat/mxfdec.c
+++ libavformat/mxfdec.c
@@ -2120,22 +2120,13 @@ static int mxf_is_intra_only(MXFDescriptor *descriptor)
 
 static int mxf_umid_to_str(UID ul, UID uid, char **str)
 {
-    int i;
     char *p;
     p = *str = av_mallocz(sizeof(UID) * 4 + 2 + 1);
     if (!p)
         return AVERROR(ENOMEM);
     snprintf(p, 2 + 1, "0x");
-    p += 2;
-    for (i = 0; i < sizeof(UID); i++) {
-        snprintf(p, 2 + 1, "%.2X", ul[i]);
-        p += 2;
-
-    }
-    for (i = 0; i < sizeof(UID); i++) {
-        snprintf(p, 2 + 1, "%.2X", uid[i]);
-        p += 2;
-    }
+    ff_data_to_hex(p + 2, ul, sizeof(UID), 0);
+    ff_data_to_hex(p + 2 + 2 * sizeof(UID), uid, sizeof(UID), 0);
     return 0;
 }
 
