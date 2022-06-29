03b2ed9a50dc6cbf332a2d93e4c8811b88e6df7f
Andreas Rheinhardt <andreas.rheinhardt@outlook.com>
2022-06-29 13:29:56 +0200

    avcodec/hevcdec: Return immediately upon hevc_init_context() failure
    This function is only called from the decoder's init function
    and given that this decoder has FF_CODEC_CAP_INIT_CLEANUP set,
    hevc_decode_free() is called automatically (currently it would
    be called twice with the second call being redundant).
    
    Reviewed-by: Anton Khirnov <anton@khirnov.net>
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

--- libavcodec/hevcdec.c
+++ libavcodec/hevcdec.c
@@ -3647,34 +3647,34 @@ static av_cold int hevc_init_context(AVCodecContext *avctx)
     s->HEVClcList = av_mallocz(sizeof(HEVCLocalContext*) * s->threads_number);
     s->sList = av_mallocz(sizeof(HEVCContext*) * s->threads_number);
     if (!s->HEVClc || !s->HEVClcList || !s->sList)
-        goto fail;
+        return AVERROR(ENOMEM);
     s->HEVClcList[0] = s->HEVClc;
     s->sList[0] = s;
 
     s->cabac_state = av_malloc(HEVC_CONTEXTS);
     if (!s->cabac_state)
-        goto fail;
+        return AVERROR(ENOMEM);
 
     s->output_frame = av_frame_alloc();
     if (!s->output_frame)
-        goto fail;
+        return AVERROR(ENOMEM);
 
     for (i = 0; i < FF_ARRAY_ELEMS(s->DPB); i++) {
         s->DPB[i].frame = av_frame_alloc();
         if (!s->DPB[i].frame)
-            goto fail;
+            return AVERROR(ENOMEM);
         s->DPB[i].tf.f = s->DPB[i].frame;
 
         s->DPB[i].frame_grain = av_frame_alloc();
         if (!s->DPB[i].frame_grain)
-            goto fail;
+            return AVERROR(ENOMEM);
     }
 
     s->max_ra = INT_MAX;
 
     s->md5_ctx = av_md5_alloc();
     if (!s->md5_ctx)
-        goto fail;
+        return AVERROR(ENOMEM);
 
     ff_bswapdsp_init(&s->bdsp);
 
@@ -3684,10 +3684,6 @@ static av_cold int hevc_init_context(AVCodecContext *avctx)
     ff_hevc_reset_sei(&s->sei);
 
     return 0;
-
-fail:
-    hevc_decode_free(avctx);
-    return AVERROR(ENOMEM);
 }
 
 #if HAVE_THREADS
