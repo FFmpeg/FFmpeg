d79de91f1ea8262a1f6c0ef103bf4433da000eff
Michael Niedermayer <michael@niedermayer.cc>
2022-07-03 00:43:21 +0200

    avcodec/qdrw: adjust max colors to array size
    Fixes: out of array access
    Fixes: 48429/clusterfuzz-testcase-minimized-ffmpeg_AV_CODEC_ID_QDRAW_fuzzer-4608329791438848
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    Reviewed-by: Paul B Mahol <onemda@gmail.com>
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit cd847f86d31f87f0f7733ca6ab7a2c022a1398bd)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/qdrw.c
+++ libavcodec/qdrw.c
@@ -369,7 +369,7 @@ static int decode_frame(AVCodecContext *avctx,
             bytestream2_skip(&gbc, 18);
             colors = bytestream2_get_be16(&gbc);
 
-            if (colors < 0 || colors > 256) {
+            if (colors < 0 || colors > 255) {
                 av_log(avctx, AV_LOG_ERROR,
                        "Error color count - %i(0x%X)\n", colors, colors);
                 return AVERROR_INVALIDDATA;
