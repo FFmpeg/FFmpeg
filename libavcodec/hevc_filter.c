3a41f58c94e2595204f70e0c9e7526e282574958
Michael Niedermayer <michael@niedermayer.cc>
2022-07-22 00:51:32 +0200

    avcodec/hevc_filter: copy_CTB() only within width&height
    Fixes: out of array access
    Fixes: 49271/clusterfuzz-testcase-minimized-ffmpeg_AV_CODEC_ID_HEVC_fuzzer-5424984922652672
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit 009ef35d384c3df22d8a8be7416dc9d532e91c52)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/hevc_filter.c
+++ libavcodec/hevc_filter.c
@@ -145,10 +145,21 @@ int i, j;
 
     if (((intptr_t)dst | (intptr_t)src | stride_dst | stride_src) & 15) {
         for (i = 0; i < height; i++) {
-            for (j = 0; j < width; j+=8)
+            for (j = 0; j < width - 7; j+=8)
                 AV_COPY64U(dst+j, src+j);
             dst += stride_dst;
             src += stride_src;
+        }
+        if (width&7) {
+            dst += ((width>>3)<<3) - stride_dst * height;
+            src += ((width>>3)<<3) - stride_src * height;
+            width &= 7;
+            for (i = 0; i < height; i++) {
+                for (j = 0; j < width; j++)
+                    dst[j] = src[j];
+                dst += stride_dst;
+                src += stride_src;
+            }
         }
     } else {
         for (i = 0; i < height; i++) {
