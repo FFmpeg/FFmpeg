e081358e8d838b9d83c75fe58780b66cbbf3153d
Andreas Rheinhardt <andreas.rheinhardt@outlook.com>
2022-08-01 11:53:40 +0200

    avcodec/aacenc_tns: Remove unused header
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

--- libavcodec/aacenc_tns.c
+++ libavcodec/aacenc_tns.c
@@ -30,7 +30,6 @@
 #include "aacenc_tns.h"
 #include "aactab.h"
 #include "aacenc_utils.h"
-#include "aacenc_quantization.h"
 
 /* Could be set to 3 to save an additional bit at the cost of little quality */
 #define TNS_Q_BITS 4
