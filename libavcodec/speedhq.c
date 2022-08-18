c7f723ddb677d6a13e9d8b62851f9d2583100fdf
Michael Niedermayer <michael@niedermayer.cc>
2022-08-18 23:41:57 +0200

    avcodec/speedhq: Check width
    Fixes: out of array access
    Fixes: 50014/clusterfuzz-testcase-minimized-ffmpeg_AV_CODEC_ID_SPEEDHQ_fuzzer-4748914632294400
    
    Alternatively the buffer size can be increased
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit f0395f9ef6051315973f1fdded1804f81458566d)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/speedhq.c
+++ libavcodec/speedhq.c
@@ -498,7 +498,7 @@ static int speedhq_decode_frame(AVCodecContext *avctx,
     uint32_t second_field_offset;
     int ret;
 
-    if (buf_size < 4 || avctx->width < 8)
+    if (buf_size < 4 || avctx->width < 8 || avctx->width % 8 != 0)
         return AVERROR_INVALIDDATA;
 
     quality = buf[0];
