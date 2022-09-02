0b95a6a1d0b3d9c06aedd5fc929804661addd263
Andreas Rheinhardt <andreas.rheinhardt@outlook.com>
2022-09-02 22:54:04 +0200

    avcodec/mathops: Set hidden visibility where advantageous
    It is advantageous for ff_crop_tab, as the base pointer used to
    access this table is not the first element of it. But the real
    base pointer is still at a constant offset from the code/the GOT
    and can therefore be accessed relative to the instruction pointer
    (if supported by the arch) or relative to the GOT; without this,
    one has to first load address of ff_crop_tab (potentially via
    the GOT) and then offset manually (which is what the earlier code
    did).
    
    Reviewed-by: Martin Storsjö <martin@martin.st>
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

--- libavcodec/mathops.h
+++ libavcodec/mathops.h
@@ -24,6 +24,7 @@
 
 #include <stdint.h>
 
+#include "libavutil/attributes_internal.h"
 #include "libavutil/common.h"
 #include "config.h"
 
@@ -32,7 +33,7 @@
 extern const uint32_t ff_inverse[257];
 extern const uint8_t ff_log2_run[41];
 extern const uint8_t ff_sqrt_tab[256];
-extern const uint8_t ff_crop_tab[256 + 2 * MAX_NEG_CROP];
+extern const uint8_t attribute_visibility_hidden ff_crop_tab[256 + 2 * MAX_NEG_CROP];
 extern const uint8_t ff_zigzag_direct[64];
 extern const uint8_t ff_zigzag_scan[16+1];
 
