10ed73e933bbc9261c7637652c533ee508e60bca
Paul B Mahol <onemda@gmail.com>
2022-09-03 09:23:39 +0200

    avcodec/cfhddata: remove unused defines

--- libavcodec/cfhddata.c
+++ libavcodec/cfhddata.c
@@ -24,14 +24,6 @@
 
 #include "cfhd.h"
 
-/* some special codewords, not sure what they all mean */
-#define TABLE_9_BAND_END1 0x1C7859Eh
-#define TABLE_9_BAND_END_LEN1 25
-#define TABLE_9_BAND_END2 0x38F0B3Fh
-#define TABLE_9_BAND_END_LEN2 26
-#define TABLE_9_BAND_END3 0x38F0B3Eh
-#define TABLE_9_BAND_END_LEN3 26
-
 #define NB_VLC_TABLE_9   (71 + 3)
 #define NB_VLC_TABLE_18 (263 + 1)
 
