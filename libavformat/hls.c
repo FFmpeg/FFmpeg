47e510aa0c09bd02536c8359deadee8aa36e9794
Michael Niedermayer <michael@niedermayer.cc>
2022-09-09 00:32:23 +0200

    libavformat/hls: Free keys
    Fixes: memleak
    Fixes: 50703/clusterfuzz-testcase-minimized-ffmpeg_dem_HLS_fuzzer-6399058578636800
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    Reviewed-by: Steven Liu <lingjiujianke@gmail.com>
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit d32a9f3137c91de86547601a38fea0693c3497f1)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavformat/hls.c
+++ libavformat/hls.c
@@ -250,6 +250,7 @@ static void free_init_section_list(struct playlist *pls)
 {
     int i;
     for (i = 0; i < pls->n_init_sections; i++) {
+        av_freep(&pls->init_sections[i]->key);
         av_freep(&pls->init_sections[i]->url);
         av_freep(&pls->init_sections[i]);
     }
