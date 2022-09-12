859b31de5064535128f2ecd510c8e2d8b3be0214
Andreas Rheinhardt <andreas.rheinhardt@outlook.com>
2022-09-12 17:58:04 +0200

    avfilter/video: Fix newline in trace output
    Forgotten in 7e350379f87e7f74420b4813170fe808e2313911.
    
    Reviewed-by: Nicolas George <george@nsup.org>
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

--- libavfilter/video.c
+++ libavfilter/video.c
@@ -102,7 +102,7 @@ AVFrame *ff_get_video_buffer(AVFilterLink *link, int w, int h)
 {
     AVFrame *ret = NULL;
 
-    FF_TPRINTF_START(NULL, get_video_buffer); ff_tlog_link(NULL, link, 0);
+    FF_TPRINTF_START(NULL, get_video_buffer); ff_tlog_link(NULL, link, 1);
 
     if (link->dstpad->get_buffer.video)
         ret = link->dstpad->get_buffer.video(link, w, h);
