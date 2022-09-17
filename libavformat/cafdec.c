e7d1caf41f6e514f7b0a9c4c36b347e83b3468f4
Michael Niedermayer <michael@niedermayer.cc>
2022-09-17 21:48:43 +0200

    avformat/cafdec: Check that nb_frasmes fits within 64bit
    Fixes: signed integer overflow: 1099511693312 * 538976288 cannot be represented in type 'long'
    Fixes: 50993/clusterfuzz-testcase-minimized-ffmpeg_dem_CAF_fuzzer-6565048815845376
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit d4bb4e375975dc0d31d5309106cf6ee0ed75140f)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavformat/cafdec.c
+++ libavformat/cafdec.c
@@ -342,7 +342,7 @@ static int read_header(AVFormatContext *s)
 
 found_data:
     if (caf->bytes_per_packet > 0 && caf->frames_per_packet > 0) {
-        if (caf->data_size > 0)
+        if (caf->data_size > 0 && caf->data_size / caf->bytes_per_packet < INT64_MAX / caf->frames_per_packet)
             st->nb_frames = (caf->data_size / caf->bytes_per_packet) * caf->frames_per_packet;
     } else if (st->nb_index_entries && st->duration > 0) {
         if (st->codecpar->sample_rate && caf->data_size / st->duration > INT64_MAX / st->codecpar->sample_rate / 8) {
