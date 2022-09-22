dea944b838626b5576598b7f13cc34e6263ebbfe
Lynne <dev@lynne.ee>
2022-09-22 03:06:00 +0200

    x86/lpc: fix odd scalar loop overreads/writes

--- libavcodec/x86/lpc.asm
+++ libavcodec/x86/lpc.asm
@@ -107,7 +107,6 @@ cglobal lpc_apply_welch_window, 3, 5, 8, data, len, out, off1, off2
 
 .scalar_o:
     movapd xm7, [dec_tab_scalar]
-    subpd xm0, xm7
 
     ; Set offsets
     add off2q, (mmsize/4) + 4*cpuflag(avx2)
@@ -118,14 +117,14 @@ cglobal lpc_apply_welch_window, 3, 5, 8, data, len, out, off1, off2
     mulpd xm2, xm0, xm0
     subpd xm1, xm2
 
-    cvtdq2pd xm3, [dataq + off1q - (mmsize/4) + 4*cpuflag(avx2)]
-    cvtdq2pd xm4, [dataq + off2q - (mmsize/4) + 4*cpuflag(avx2)]
+    cvtdq2pd xm3, [dataq + off1q]
+    cvtdq2pd xm4, [dataq + off2q]
 
     mulpd xm3, xm1
     mulpd xm4, xm1
 
-    movhpd [outq + off1q*2], xm3
-    movhpd [outq + off2q*2], xm4
+    movlpd [outq + off1q*2], xm3
+    movlpd [outq + off2q*2], xm4
 
     addpd xm0, xm7
 
