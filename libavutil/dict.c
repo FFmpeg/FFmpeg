5f7c5a0bd72b3903919d08f7b6edb5a148b87d5b
Marvin Scholz <epirat07@gmail.com>
2022-09-24 16:36:56 +0200

    avutil/dict: Use av_dict_iterate in av_dict_get
    Signed-off-by: Anton Khirnov <anton@khirnov.net>

--- libavutil/dict.c
+++ libavutil/dict.c
@@ -60,18 +60,14 @@ const AVDictionaryEntry *av_dict_iterate(const AVDictionary *m,
 AVDictionaryEntry *av_dict_get(const AVDictionary *m, const char *key,
                                const AVDictionaryEntry *prev, int flags)
 {
-    unsigned int i, j;
+    const AVDictionaryEntry *entry = prev;
+    unsigned int j;
 
-    if (!m || !key)
+    if (!key)
         return NULL;
 
-    if (prev)
-        i = prev - m->elems + 1;
-    else
-        i = 0;
-
-    for (; i < m->count; i++) {
-        const char *s = m->elems[i].key;
+    while ((entry = av_dict_iterate(m, entry))) {
+        const char *s = entry->key;
         if (flags & AV_DICT_MATCH_CASE)
             for (j = 0; s[j] == key[j] && key[j]; j++)
                 ;
@@ -82,7 +78,7 @@ AVDictionaryEntry *av_dict_get(const AVDictionary *m, const char *key,
             continue;
         if (s[j] && !(flags & AV_DICT_IGNORE_SUFFIX))
             continue;
-        return &m->elems[i];
+        return (AVDictionaryEntry *)entry;
     }
     return NULL;
 }
