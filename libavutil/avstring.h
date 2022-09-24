023966d2f845b7887335adeab1dbafcfdfe12125
Marvin Scholz <epirat07@gmail.com>
2022-09-25 01:05:41 +0200

    avutil/avstring: Add doxy for missing argument

--- libavutil/avstring.h
+++ libavutil/avstring.h
@@ -135,6 +135,7 @@ size_t av_strlcatf(char *dst, size_t size, const char *fmt, ...) av_printf_forma
 /**
  * Get the count of continuous non zero chars starting from the beginning.
  *
+ * @param s   the string whose length to count
  * @param len maximum number of characters to check in the string, that
  *            is the maximum value which is returned by the function
  */
