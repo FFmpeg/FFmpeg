3c2050b749d7813ec302106ef56129a9117c091c
Marvin Scholz <epirat07@gmail.com>
2022-09-24 16:36:57 +0200

    avutil/dict: Use av_dict_iterate in av_dict_copy
    Signed-off-by: Anton Khirnov <anton@khirnov.net>

--- libavutil/dict.c
+++ libavutil/dict.c
@@ -236,9 +236,9 @@ void av_dict_free(AVDictionary **pm)
 
 int av_dict_copy(AVDictionary **dst, const AVDictionary *src, int flags)
 {
-    AVDictionaryEntry *t = NULL;
+    const AVDictionaryEntry *t = NULL;
 
-    while ((t = av_dict_get(src, "", t, AV_DICT_IGNORE_SUFFIX))) {
+    while ((t = av_dict_iterate(src, t))) {
         int ret = av_dict_set(dst, t->key, t->value, flags);
         if (ret < 0)
             return ret;
