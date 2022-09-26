6573e65ac00e22ba7c5add70f8f499a3d946f601
Andreas Rheinhardt <andreas.rheinhardt@outlook.com>
2022-09-26 02:14:45 +0200

    avcodec/aacdectab: Remove empty channel layouts
    They will be mistaken for the sentinel of the arrays
    they are in, thereby hiding the 6.1, 7.1 and 22.2 layouts.
    (This doesn't really matter, as these arrays are informational
    only for decoders.)
    
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

--- libavcodec/aacdectab.h
+++ libavcodec/aacdectab.h
@@ -73,7 +73,7 @@ static const uint8_t aac_channel_layout_map[16][16][3] = {
 };
 
 #if FF_API_OLD_CHANNEL_LAYOUT
-static const uint64_t aac_channel_layout[16] = {
+static const uint64_t aac_channel_layout[] = {
     AV_CH_LAYOUT_MONO,
     AV_CH_LAYOUT_STEREO,
     AV_CH_LAYOUT_SURROUND,
@@ -81,9 +81,6 @@ static const uint64_t aac_channel_layout[16] = {
     AV_CH_LAYOUT_5POINT0_BACK,
     AV_CH_LAYOUT_5POINT1_BACK,
     AV_CH_LAYOUT_7POINT1_WIDE_BACK,
-    0,
-    0,
-    0,
     AV_CH_LAYOUT_6POINT1,
     AV_CH_LAYOUT_7POINT1,
     AV_CH_LAYOUT_22POINT2,
@@ -92,7 +89,7 @@ static const uint64_t aac_channel_layout[16] = {
 };
 #endif
 
-static const AVChannelLayout aac_ch_layout[16] = {
+static const AVChannelLayout aac_ch_layout[] = {
     AV_CHANNEL_LAYOUT_MONO,
     AV_CHANNEL_LAYOUT_STEREO,
     AV_CHANNEL_LAYOUT_SURROUND,
@@ -100,9 +97,6 @@ static const AVChannelLayout aac_ch_layout[16] = {
     AV_CHANNEL_LAYOUT_5POINT0_BACK,
     AV_CHANNEL_LAYOUT_5POINT1_BACK,
     AV_CHANNEL_LAYOUT_7POINT1_WIDE_BACK,
-    { 0 },
-    { 0 },
-    { 0 },
     AV_CHANNEL_LAYOUT_6POINT1,
     AV_CHANNEL_LAYOUT_7POINT1,
     AV_CHANNEL_LAYOUT_22POINT2,
