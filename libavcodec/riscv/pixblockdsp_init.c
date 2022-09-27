d31013166ac3727ae7c7ebbb756e1e5800bc2b40
Rémi Denis-Courmont <remi@remlab.net>
2022-09-27 23:04:27 +0300

    lavc/pixblockdsp: RISC-V diff_pixels & diff_pixels_unaligned

--- libavcodec/riscv/pixblockdsp_init.c
+++ libavcodec/riscv/pixblockdsp_init.c
@@ -36,6 +36,8 @@ void ff_get_pixels_8_rvv(int16_t *block, const uint8_t *pixels,
                          ptrdiff_t stride);
 void ff_get_pixels_16_rvv(int16_t *block, const uint8_t *pixels,
                           ptrdiff_t stride);
+void ff_diff_pixels_rvv(int16_t *block, const uint8_t *s1, const uint8_t *s2,
+                        ptrdiff_t stride);
 
 av_cold void ff_pixblockdsp_init_riscv(PixblockDSPContext *c,
                                        AVCodecContext *avctx,
@@ -56,6 +58,8 @@ av_cold void ff_pixblockdsp_init_riscv(PixblockDSPContext *c,
             c->get_pixels_unaligned = c->get_pixels = ff_get_pixels_16_rvv;
         else
             c->get_pixels_unaligned = c->get_pixels = ff_get_pixels_8_rvv;
+
+        c->diff_pixels_unaligned = c->diff_pixels = ff_diff_pixels_rvv;
     }
 #endif
 }
