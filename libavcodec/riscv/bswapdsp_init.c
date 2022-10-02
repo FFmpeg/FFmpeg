2abafd7307d5ba31cc6492539caf0da15dd808a5
Rémi Denis-Courmont <remi@remlab.net>
2022-10-02 14:55:01 +0300

    lavc/bswapdsp: RISC-V V bswap16_buf

--- libavcodec/riscv/bswapdsp_init.c
+++ libavcodec/riscv/bswapdsp_init.c
@@ -27,6 +27,7 @@
 
 void ff_bswap32_buf_rvb(uint32_t *dst, const uint32_t *src, int len);
 void ff_bswap32_buf_rvv(uint32_t *dst, const uint32_t *src, int len);
+void ff_bswap16_buf_rvv(uint16_t *dst, const uint16_t *src, int len);
 
 av_cold void ff_bswapdsp_init_riscv(BswapDSPContext *c)
 {
@@ -37,7 +38,9 @@ av_cold void ff_bswapdsp_init_riscv(BswapDSPContext *c)
         c->bswap_buf = ff_bswap32_buf_rvb;
 #endif
 #if HAVE_RVV
-    if (cpu_flags & AV_CPU_FLAG_RVV_I32)
+    if (cpu_flags & AV_CPU_FLAG_RVV_I32) {
         c->bswap_buf = ff_bswap32_buf_rvv;
+        c->bswap16_buf = ff_bswap16_buf_rvv;
+    }
 #endif
 }
