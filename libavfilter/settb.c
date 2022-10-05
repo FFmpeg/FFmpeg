7c299c117823a766558e893ae4e9ac91b9dcdc7b
Anton Khirnov <anton@khirnov.net>
2022-10-05 10:29:21 +0200

    lavfi/settb: rescale input frame durations

--- libavfilter/settb.c
+++ libavfilter/settb.c
@@ -128,6 +128,7 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *frame)
     AVFilterLink *outlink = ctx->outputs[0];
 
     frame->pts = rescale_pts(inlink, outlink, frame->pts);
+    frame->duration = av_rescale_q(frame->duration, inlink->time_base, outlink->time_base);
 
     return ff_filter_frame(outlink, frame);
 }
