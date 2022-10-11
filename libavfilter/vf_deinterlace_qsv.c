d1526f4dacca42a5065b140d2e0058017e0e0049
Anton Khirnov <anton@khirnov.net>
2022-10-11 10:47:42 +0200

    lavfi/vf_deinterlace_qsv: set output frame durations

--- libavfilter/vf_deinterlace_qsv.c
+++ libavfilter/vf_deinterlace_qsv.c
@@ -516,6 +516,11 @@ static int process_frame(AVFilterContext *ctx, const AVFrame *in,
         out->pts++;
     s->last_pts = out->pts;
 
+    if (outlink->frame_rate.num && outlink->frame_rate.den)
+        out->duration = av_rescale_q(1, av_inv_q(outlink->frame_rate), outlink->time_base);
+    else
+        out->duration = 0;
+
     ret = ff_filter_frame(outlink, out);
     if (ret < 0)
         return ret;
