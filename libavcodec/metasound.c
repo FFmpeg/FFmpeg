7894d4c9fa16204cf8bd266915e2da18a7775f72
Andreas Rheinhardt <andreas.rheinhardt@outlook.com>
2022-10-18 15:13:30 +0200

    avcodec/metasound: Remove unnecessary headers
    They are used in twinvq.c, not in metasound.c.
    
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

--- libavcodec/metasound.c
+++ libavcodec/metasound.c
@@ -26,15 +26,11 @@
 #include <stdint.h>
 
 #include "libavutil/channel_layout.h"
-#include "libavutil/float_dsp.h"
 
 #define BITSTREAM_READER_LE
 #include "avcodec.h"
 #include "codec_internal.h"
-#include "fft.h"
 #include "get_bits.h"
-#include "lsp.h"
-#include "sinewin.h"
 
 #include "twinvq.h"
 #include "metasound_data.h"
