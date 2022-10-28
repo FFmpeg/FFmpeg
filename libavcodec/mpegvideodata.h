920ce150c60b36b8039fd187a228c4e8df5dcfb5
Andreas Rheinhardt <andreas.rheinhardt@outlook.com>
2022-10-28 11:19:49 +0200

    avcodec/mpegvideodata: Make DC scale tables smaller
    These tables are only accessed in ff_set_qscale()
    which only accesses values 1..31 as well as in
    encode_picture() in mpegvideo_enc.c, accessing
    the value with index 8. So make these tables smaller.
    
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

--- libavcodec/mpegvideodata.h
+++ libavcodec/mpegvideodata.h
@@ -25,7 +25,7 @@
 extern const uint8_t ff_alternate_horizontal_scan[64];
 extern const uint8_t ff_alternate_vertical_scan[64];
 
-extern const uint8_t ff_mpeg1_dc_scale_table[128];
+extern const uint8_t ff_mpeg1_dc_scale_table[32];
 extern const uint8_t * const ff_mpeg2_dc_scale_table[4];
 
 extern const uint8_t ff_mpeg2_non_linear_qscale[32];
