3f4ec2d6afb9dbb39fec107541eacf0957c4d015
Michael Niedermayer <michael@niedermayer.cc>
2022-11-21 22:59:55 +0100

    avcodec/mlpdec: Check max matrix instead of max channel in noise check
    This is a regression since: adaa06581c5444c94eef72d61b8166f096e2687a
    Before this, max_channel and  max_matrix_channel where compared for equality
    
    Fixes: out of array access
    Fixes: 53340/clusterfuzz-testcase-minimized-ffmpeg_AV_CODEC_ID_TRUEHD_fuzzer-514959011885875
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit aa79560de5e9596ada0345e5d12aa00dbeddaaa6)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/mlpdec.c
+++ libavcodec/mlpdec.c
@@ -521,7 +521,7 @@ static int read_restart_header(MLPDecodeContext *m, GetBitContext *gbp,
 
     /* This should happen for TrueHD streams with >6 channels and MLP's noise
      * type. It is not yet known if this is allowed. */
-    if (max_channel > MAX_MATRIX_CHANNEL_MLP && !noise_type) {
+    if (max_matrix_channel > MAX_MATRIX_CHANNEL_MLP && !noise_type) {
         avpriv_request_sample(m->avctx,
                               "%d channels (more than the "
                               "maximum supported by the decoder)",
