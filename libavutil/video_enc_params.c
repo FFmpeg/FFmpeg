01f58f36465a13db069641cca47fa9777f03b875
Martin Storsjö <martin@martin.st>
2023-01-16 00:20:24 +0200

    lavu/video_enc_params: Avoid relying on an undefined C construct
    The construct of using offsetof on a (potentially anonymous) struct
    defined within the offsetof expression, while supported by all
    current compilers, has been declared explicitly undefined by the
    C standards committee [1].
    
    Clang recently got a change to identify this as an issue [2];
    initially it was treated as a hard error, but it was soon after
    softened into a warning under the -Wgnu-offsetof-extensions option
    (not enabled automatically as part of -Wall though).
    
    Nevertheless - in this particular case, it's trivial to fix the
    code not to rely on the construct that the standards committee has
    explicitly called out as undefined.
    
    [1] https://www.open-std.org/jtc1/sc22/wg14/www/docs/n2350.htm
    [2] https://reviews.llvm.org/D133574
    
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- libavutil/video_enc_params.c
+++ libavutil/video_enc_params.c
@@ -27,11 +27,11 @@
 AVVideoEncParams *av_video_enc_params_alloc(enum AVVideoEncParamsType type,
                                             unsigned int nb_blocks, size_t *out_size)
 {
-    const size_t blocks_offset = offsetof(
-        struct {
-            AVVideoEncParams   p;
-            AVVideoBlockParams b;
-        }, b);
+    struct TestStruct {
+        AVVideoEncParams   p;
+        AVVideoBlockParams b;
+    };
+    const size_t blocks_offset = offsetof(struct TestStruct, b);
     size_t size = blocks_offset;
     AVVideoEncParams *par;
 
