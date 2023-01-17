2cee62295fec51b3452148dc3806e9a59d777e86
Paul B Mahol <onemda@gmail.com>
2023-01-17 14:43:01 +0100

    avfilter/af_dialoguenhance: call av_frame_copy_props()

--- libavfilter/af_dialoguenhance.c
+++ libavfilter/af_dialoguenhance.c
@@ -320,7 +320,7 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *in)
     s->in = in;
     de_stereo(ctx, out);
 
-    out->pts = in->pts;
+    av_frame_copy_props(out, in);
     out->nb_samples = in->nb_samples;
     ret = ff_filter_frame(outlink, out);
 fail:
