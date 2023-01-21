7e4ad6e6a854c13f0c8b499a03cf50f21398874d
Marton Balint <cus@passwd.hu>
2023-01-22 00:58:22 +0100

    avcodec/mpeg4videoenc: use MpegEncContext->picture_number instead of encode function parameter
    They should be the same and that is what most encode functions use.
    
    Signed-off-by: Marton Balint <cus@passwd.hu>

--- libavcodec/mpeg4videoenc.c
+++ libavcodec/mpeg4videoenc.c
@@ -1065,7 +1065,7 @@ int ff_mpeg4_encode_picture_header(MpegEncContext *s, int picture_number)
         if (!(s->avctx->flags & AV_CODEC_FLAG_GLOBAL_HEADER)) {
             if (s->avctx->strict_std_compliance < FF_COMPLIANCE_VERY_STRICT)  // HACK, the reference sw is buggy
                 mpeg4_encode_visual_object_header(s);
-            if (s->avctx->strict_std_compliance < FF_COMPLIANCE_VERY_STRICT || picture_number == 0)  // HACK, the reference sw is buggy
+            if (s->avctx->strict_std_compliance < FF_COMPLIANCE_VERY_STRICT || s->picture_number == 0)  // HACK, the reference sw is buggy
                 mpeg4_encode_vol_header(s, 0, 0);
         }
         if (!(s->workaround_bugs & FF_BUG_MS))
