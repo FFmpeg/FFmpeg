14446f18315aada06808c89a521926c29ed07462
Michael Niedermayer <michael@niedermayer.cc>
2023-02-18 22:33:02 +0100

    avcodec/ffv1dec: Check that num h/v slices is supported
    Fixes: out of array access
    Fixes: 55597/clusterfuzz-testcase-minimized-ffmpeg_AV_CODEC_ID_FFV1_fuzzer-4898293416329216
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit 8ead0ae68eb64ad325efafd686c434727f3d666a)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/ffv1dec.c
+++ libavcodec/ffv1dec.c
@@ -477,6 +477,11 @@ static int read_extra_header(FFV1Context *f)
         return AVERROR_INVALIDDATA;
     }
 
+    if (f->num_h_slices > MAX_SLICES / f->num_v_slices) {
+        av_log(f->avctx, AV_LOG_ERROR, "slice count unsupported\n");
+        return AVERROR_PATCHWELCOME;
+    }
+
     f->quant_table_count = get_symbol(c, state, 0);
     if (f->quant_table_count > (unsigned)MAX_QUANT_TABLES || !f->quant_table_count) {
         av_log(f->avctx, AV_LOG_ERROR, "quant table count %d is invalid\n", f->quant_table_count);
