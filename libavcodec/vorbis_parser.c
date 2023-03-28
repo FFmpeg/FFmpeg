e0835b4878f4f866a540e739683411d72c91f431
rcombs <rcombs@rcombs.me>
2023-03-28 16:29:46 -0400

    lavc/vorbis_parser: ignore unrecognized packet types

--- libavcodec/vorbis_parser.c
+++ libavcodec/vorbis_parser.c
@@ -234,7 +234,8 @@ int av_vorbis_parse_frame_flags(AVVorbisParseContext *s, const uint8_t *buf,
             else if (buf[0] == 5)
                 *flags |= VORBIS_FLAG_SETUP;
             else
-                goto bad_packet;
+                av_log(s, AV_LOG_VERBOSE, "Ignoring packet with unknown type %u\n",
+                       buf[0]);
 
             /* Special packets have no duration. */
             return 0;
