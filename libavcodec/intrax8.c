cf7ed01938a4d8b2ccd28f1fadacd79103e54eed
Nicolas George <george@nsup.org>
2023-04-26 14:28:02 +0200

    lavc/intrax8: fix an assert

--- libavcodec/intrax8.c
+++ libavcodec/intrax8.c
@@ -109,7 +109,7 @@ static inline void x8_select_ac_table(IntraX8Context *const w, int mode)
     table_index       = get_bits(w->gb, 3);
     // 2 modes use same tables
     w->j_ac_vlc_table[mode] = j_ac_vlc[w->quant < 13][mode >> 1][table_index].table;
-    av_assert2(w->j_ac_vlc[mode]);
+    av_assert2(j_ac_vlc[mode]);
 }
 
 static inline int x8_get_orient_vlc(IntraX8Context *w)
