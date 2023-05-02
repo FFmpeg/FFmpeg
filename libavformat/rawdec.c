2850584876e52beaddf7a9f30e9914dad7115618
Anton Khirnov <anton@khirnov.net>
2023-05-02 10:51:08 +0200

    lavf/rawdec: stop setting codec context framerate
    Demuxers are not supposed to do this.

--- libavformat/rawdec.c
+++ libavformat/rawdec.c
@@ -87,7 +87,6 @@ int ff_raw_video_read_header(AVFormatContext *s)
     sti->need_parsing = AVSTREAM_PARSE_FULL_RAW;
 
     st->avg_frame_rate = s1->framerate;
-    sti->avctx->framerate = s1->framerate;
     avpriv_set_pts_info(st, 64, 1, 1200000);
 
 fail:
