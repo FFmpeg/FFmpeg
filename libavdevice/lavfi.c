5ce76506de1a7fbaf91af47a925d5ecfe13ae59c
Paul B Mahol <onemda@gmail.com>
2023-05-10 12:38:28 +0200

    avdevice/lavfi: check that pointer is set
    Fixes NULL pointer dereference later.

--- libavdevice/lavfi.c
+++ libavdevice/lavfi.c
@@ -174,6 +174,10 @@ av_cold static int lavfi_read_header(AVFormatContext *avctx)
      * create a mapping between them and the streams */
     for (i = 0, inout = output_links; inout; i++, inout = inout->next) {
         int stream_idx = 0, suffix = 0, use_subcc = 0;
+        if (!inout->name) {
+            av_log(avctx, AV_LOG_ERROR, "Missing %d outpad name\n", i);
+            FAIL(AVERROR(EINVAL));
+        }
         sscanf(inout->name, "out%n%d%n", &suffix, &stream_idx, &suffix);
         if (!suffix) {
             av_log(avctx,  AV_LOG_ERROR,
