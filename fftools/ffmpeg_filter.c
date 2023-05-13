0f501b2ad999a63669b6dd002544a447abd368d4
Anton Khirnov <anton@khirnov.net>
2023-05-13 10:26:16 +0200

    fftools/ffmpeg_filter: drop a redundant error message
    In case no decoder is available, dec_open() called from ist_use() will
    fail with 'Decoding requested, but no decoder found', so this check is
    redundant.

--- fftools/ffmpeg_filter.c
+++ fftools/ffmpeg_filter.c
@@ -1201,12 +1201,6 @@ static int configure_input_audio_filter(FilterGraph *fg, InputFilter *ifilter,
 static int configure_input_filter(FilterGraph *fg, InputFilter *ifilter,
                                   AVFilterInOut *in)
 {
-    if (!ifilter->ist->dec) {
-        av_log(NULL, AV_LOG_ERROR,
-               "No decoder for stream #%d:%d, filtering impossible\n",
-               ifilter->ist->file_index, ifilter->ist->st->index);
-        return AVERROR_DECODER_NOT_FOUND;
-    }
     switch (avfilter_pad_get_type(in->filter_ctx->input_pads, in->pad_idx)) {
     case AVMEDIA_TYPE_VIDEO: return configure_input_video_filter(fg, ifilter, in);
     case AVMEDIA_TYPE_AUDIO: return configure_input_audio_filter(fg, ifilter, in);
