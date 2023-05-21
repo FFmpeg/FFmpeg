8a4934166645e636ba1830e44a6223f8c243da8c
Paul B Mahol <onemda@gmail.com>
2023-05-21 20:40:57 +0200

    avformat/fitsdec: fix demuxer class category

--- libavformat/fitsdec.c
+++ libavformat/fitsdec.c
@@ -221,6 +221,7 @@ static const AVClass fits_demuxer_class = {
     .item_name  = av_default_item_name,
     .option     = fits_options,
     .version    = LIBAVUTIL_VERSION_INT,
+    .category   = AV_CLASS_CATEGORY_DEMUXER,
 };
 
 const AVInputFormat ff_fits_demuxer = {
