38bc713c6d9236a9c319dbba0c36db0fe38948cc
Michael Niedermayer <michael@niedermayer.cc>
2023-06-03 02:00:51 +0200

    avcodec/bonk: Fix integer overflow in predictor_calc_error()
    Fixes: signed integer overflow: -2147483300 - 12285 cannot be represented in type 'int'
    Fixes: 59462/clusterfuzz-testcase-minimized-ffmpeg_AV_CODEC_ID_BONK_fuzzer-5714298807386112
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit 2b25a5168e562676b299ef0f5a086e046833de4c)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/bonk.c
+++ libavcodec/bonk.c
@@ -277,7 +277,7 @@ static int predictor_calc_error(int *k, int *state, int order, int error)
     for (i = order-2; i >= 0; i--, k_ptr--, state_ptr--) {
         unsigned k_value = *k_ptr, state_value = *state_ptr;
 
-        x -= shift_down(k_value * (unsigned)state_value, LATTICE_SHIFT);
+        x -= (unsigned) shift_down(k_value * (unsigned)state_value, LATTICE_SHIFT);
         state_ptr[1] = state_value + shift_down(k_value * x, LATTICE_SHIFT);
     }
 
