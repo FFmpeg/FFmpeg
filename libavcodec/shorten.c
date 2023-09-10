64f538c356b22922663a6a6d3d0a062ae574dc44
Paul B Mahol <onemda@gmail.com>
2023-09-10 22:50:35 +0200

    avcodec/shorten: use uint16_t for wave_format

--- libavcodec/shorten.c
+++ libavcodec/shorten.c
@@ -280,7 +280,7 @@ static int decode_wave_header(AVCodecContext *avctx, const uint8_t *header,
                               int header_size)
 {
     int len, bps;
-    short wave_format;
+    uint16_t wave_format;
     GetByteContext gb;
 
     bytestream2_init(&gb, header, header_size);
