dd885ab2f5060eaedbfca6d590db44fa9547dd99
Anton Khirnov <anton@khirnov.net>
2023-09-14 16:46:47 +0200

    fftools/ffmpeg_enc: apply -top to individual encoded frames
    Fixes #9339.
    
    (Adapted from commit 43a0004b5c23dd8258cfe723a5c4e9fd9ee8f5ef)

--- fftools/ffmpeg.c
+++ fftools/ffmpeg.c
@@ -1278,6 +1278,9 @@ static void do_video_out(OutputFile *of,
             av_log(NULL, AV_LOG_DEBUG, "Forced keyframe at time %f\n", pts_time);
         }
 
+        if (ost->top_field_first >= 0)
+            in_picture->top_field_first = !!ost->top_field_first;
+
         ret = encode_frame(of, ost, in_picture);
         if (ret < 0)
             exit_program(1);
