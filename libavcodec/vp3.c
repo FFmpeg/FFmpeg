0e6fdebd0d5dafa07b4963f6b8ea7690ca124887
Andreas Rheinhardt <andreas.rheinhardt@outlook.com>
2023-09-15 15:08:57 +0200

    avcodec/vp3: Simplify shuffling frames, fix crash on alloc error
    When decoding non-keyframes, the decoding process expects
    there to be two reference frames, the last one and the golden
    one. The existence of the golden one is checked and in case
    it is there, it is presumed that the last one exists as well.
    
    This assumption is wrong in case of memory allocation failure,
    namely in case the call to ff_thread_ref_frame() that sets
    the last frame fails.
    
    Fix this by actually performing a shuffle without creating
    new references. This can't fail and has the advantage of
    fewer implicit allocations.
    
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

--- libavcodec/vp3.c
+++ libavcodec/vp3.c
@@ -2498,19 +2498,14 @@ static int update_frames(AVCodecContext *avctx)
     Vp3DecodeContext *s = avctx->priv_data;
     int ret = 0;
 
-    /* shuffle frames (last = current) */
-    ff_thread_release_ext_buffer(avctx, &s->last_frame);
-    ret = ff_thread_ref_frame(&s->last_frame, &s->current_frame);
-    if (ret < 0)
-        goto fail;
-
     if (s->keyframe) {
         ff_thread_release_ext_buffer(avctx, &s->golden_frame);
         ret = ff_thread_ref_frame(&s->golden_frame, &s->current_frame);
     }
+    /* shuffle frames */
+    ff_thread_release_ext_buffer(avctx, &s->last_frame);
+    FFSWAP(ThreadFrame, s->last_frame, s->current_frame);
 
-fail:
-    ff_thread_release_ext_buffer(avctx, &s->current_frame);
     return ret;
 }
 
