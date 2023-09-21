365529178dfd23ed79acf780effbaa8ed2a263f9
Paul B Mahol <onemda@gmail.com>
2023-09-21 22:18:12 +0200

    avformat/cafdec: improve probing

--- libavformat/cafdec.c
+++ libavformat/cafdec.c
@@ -52,9 +52,15 @@ typedef struct CafContext {
 
 static int probe(const AVProbeData *p)
 {
-    if (AV_RB32(p->buf) == MKBETAG('c','a','f','f') && AV_RB16(&p->buf[4]) == 1)
-        return AVPROBE_SCORE_MAX;
-    return 0;
+    if (AV_RB32(p->buf) != MKBETAG('c','a','f','f'))
+        return 0;
+    if (AV_RB16(&p->buf[4]) != 1)
+        return 0;
+    if (AV_RB32(p->buf + 8) != MKBETAG('d','e','s','c'))
+        return 0;
+    if (AV_RB64(p->buf + 12) != 32)
+        return 0;
+    return AVPROBE_SCORE_MAX;
 }
 
 /** Read audio description chunk */
