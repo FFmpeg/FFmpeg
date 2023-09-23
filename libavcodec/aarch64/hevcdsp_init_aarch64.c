b7a3150bc5b0fc268e9be474d3eb0461ebaf09c7
Logan Lyu <Logan.Lyu@myais.com.cn>
2023-09-23 10:38:36 +0800

    lavc/aarch64: new optimization for 8-bit hevc_epel_uni_hv
    checkasm bench:
    put_hevc_epel_uni_hv4_8_c: 204.7
    put_hevc_epel_uni_hv4_8_i8mm: 70.2
    put_hevc_epel_uni_hv6_8_c: 378.2
    put_hevc_epel_uni_hv6_8_i8mm: 131.9
    put_hevc_epel_uni_hv8_8_c: 637.7
    put_hevc_epel_uni_hv8_8_i8mm: 137.9
    put_hevc_epel_uni_hv12_8_c: 1301.9
    put_hevc_epel_uni_hv12_8_i8mm: 314.2
    put_hevc_epel_uni_hv16_8_c: 2203.4
    put_hevc_epel_uni_hv16_8_i8mm: 454.7
    put_hevc_epel_uni_hv24_8_c: 4848.2
    put_hevc_epel_uni_hv24_8_i8mm: 1065.2
    put_hevc_epel_uni_hv32_8_c: 8517.4
    put_hevc_epel_uni_hv32_8_i8mm: 1898.4
    put_hevc_epel_uni_hv48_8_c: 19591.7
    put_hevc_epel_uni_hv48_8_i8mm: 4107.2
    put_hevc_epel_uni_hv64_8_c: 33880.2
    put_hevc_epel_uni_hv64_8_i8mm: 6568.7
    
    Co-Authored-By: J. Dekker <jdek@itanimul.li>
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- libavcodec/aarch64/hevcdsp_init_aarch64.c
+++ libavcodec/aarch64/hevcdsp_init_aarch64.c
@@ -165,6 +165,10 @@ NEON8_FNPROTO(epel_uni_v, (uint8_t *dst,  ptrdiff_t dststride,
         const uint8_t *src, ptrdiff_t srcstride,
         int height, intptr_t mx, intptr_t my, int width),);
 
+NEON8_FNPROTO(epel_uni_hv, (uint8_t *dst, ptrdiff_t _dststride,
+        const uint8_t *src, ptrdiff_t srcstride,
+        int height, intptr_t mx, intptr_t my, int width), _i8mm);
+
 NEON8_FNPROTO(epel_uni_w_v, (uint8_t *_dst,  ptrdiff_t _dststride,
         const uint8_t *_src, ptrdiff_t _srcstride,
         int height, int denom, int wx, int ox,
@@ -298,6 +302,7 @@ av_cold void ff_hevc_dsp_init_aarch64(HEVCDSPContext *c, const int bit_depth)
 
         if (have_i8mm(cpu_flags)) {
             NEON8_FNASSIGN(c->put_hevc_epel, 0, 1, epel_h, _i8mm);
+            NEON8_FNASSIGN(c->put_hevc_epel_uni, 1, 1, epel_uni_hv, _i8mm);
             NEON8_FNASSIGN(c->put_hevc_epel_uni_w, 0, 1, epel_uni_w_h ,_i8mm);
             NEON8_FNASSIGN(c->put_hevc_qpel, 0, 1, qpel_h, _i8mm);
             NEON8_FNASSIGN(c->put_hevc_qpel_uni_w, 0, 1, qpel_uni_w_h, _i8mm);
