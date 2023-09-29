c4a144c29d48ac5c1ac336057e3d1f214b1d6bc9
Rémi Denis-Courmont <remi@remlab.net>
2023-09-29 20:10:15 +0300

    swscale/rgb2rgb: add R-V Zbb shuffle_bytes_3210

--- libswscale/riscv/rgb2rgb.c
+++ libswscale/riscv/rgb2rgb.c
@@ -29,6 +29,7 @@ void ff_shuffle_bytes_0321_rvv(const uint8_t *src, uint8_t *dst, int src_len);
 void ff_shuffle_bytes_2103_rvv(const uint8_t *src, uint8_t *dst, int src_len);
 void ff_shuffle_bytes_1230_rvv(const uint8_t *src, uint8_t *dst, int src_len);
 void ff_shuffle_bytes_3012_rvv(const uint8_t *src, uint8_t *dst, int src_len);
+void ff_shuffle_bytes_3210_rvb(const uint8_t *src, uint8_t *dst, int src_len);
 void ff_shuffle_bytes_3210_rvv(const uint8_t *src, uint8_t *dst, int src_len);
 void ff_interleave_bytes_rvv(const uint8_t *src1, const uint8_t *src2,
                              uint8_t *dst, int width, int height, int s1stride,
@@ -42,9 +43,13 @@ void ff_yuyvtoyuv422_rvv(uint8_t *ydst, uint8_t *udst, uint8_t *vdst,
 
 av_cold void rgb2rgb_init_riscv(void)
 {
-#if HAVE_RVV
     int flags = av_get_cpu_flags();
 
+#if (__riscv_xlen == 64)
+    if ((flags & AV_CPU_FLAG_RVB_BASIC) && (flags & AV_CPU_FLAG_RVB_ADDR))
+        shuffle_bytes_3210 = ff_shuffle_bytes_3210_rvb;
+#endif
+#if HAVE_RVV
     if ((flags & AV_CPU_FLAG_RVV_I32) && (flags & AV_CPU_FLAG_RVB_ADDR)) {
         shuffle_bytes_0321 = ff_shuffle_bytes_0321_rvv;
         shuffle_bytes_2103 = ff_shuffle_bytes_2103_rvv;
