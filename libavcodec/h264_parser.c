14e6423dfd3ede8b5978967be46f296eed10dcc8
Michael Niedermayer <michael@niedermayer.cc>
2023-10-01 00:07:20 +0200

    avcodec/h264_parser: saturate dts a bit
    Fixes: signed integer overflow: 0 - -9223372036854775808 cannot be represented in type 'long'
    Fixes: 51896/clusterfuzz-testcase-minimized-ffmpeg_IO_DEMUXER_fuzzer-6112289464123392
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit 7fedbc7606614cc1a6224effa8df762b6883bdc4)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/h264_parser.c
+++ libavcodec/h264_parser.c
@@ -625,10 +625,10 @@ static int h264_parse(AVCodecParserContext *s,
             int64_t num = avctx->time_base.num * (int64_t)avctx->pkt_timebase.den;
             if (s->dts != AV_NOPTS_VALUE) {
                 // got DTS from the stream, update reference timestamp
-                p->reference_dts = s->dts - av_rescale(s->dts_ref_dts_delta, num, den);
+                p->reference_dts = av_sat_sub64(s->dts, av_rescale(s->dts_ref_dts_delta, num, den));
             } else if (p->reference_dts != AV_NOPTS_VALUE) {
                 // compute DTS based on reference timestamp
-                s->dts = p->reference_dts + av_rescale(s->dts_ref_dts_delta, num, den);
+                s->dts = av_sat_add64(p->reference_dts, av_rescale(s->dts_ref_dts_delta, num, den));
             }
 
             if (p->reference_dts != AV_NOPTS_VALUE && s->pts == AV_NOPTS_VALUE)
