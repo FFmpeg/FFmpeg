a2175ca8615d09418564fcff63c85e18dd993ad1
John Mather <johnmather@sidefx.com>
2023-10-02 13:20:49 -0400

    avcodec/libkvazaar: Respect codec context color settings.
    This patch makes the libkvazaar encoder respect color settings that are
    present on the codec context, including color range, primaries, transfer
    function and colorspace.

--- libavcodec/libkvazaar.c
+++ libavcodec/libkvazaar.c
@@ -101,6 +101,13 @@ FF_ENABLE_DEPRECATION_WARNINGS
         cfg->rc_algorithm = KVZ_LAMBDA;
     }
 
+    cfg->vui.fullrange   = avctx->color_range == AVCOL_RANGE_JPEG;
+    cfg->vui.colorprim   = avctx->color_primaries;
+    cfg->vui.transfer    = avctx->color_trc;
+    cfg->vui.colormatrix = avctx->colorspace;
+    if (avctx->chroma_sample_location != AVCHROMA_LOC_UNSPECIFIED)
+        cfg->vui.chroma_loc = avctx->chroma_sample_location - 1;
+
     if (ctx->kvz_params) {
         AVDictionary *dict = NULL;
         if (!av_dict_parse_string(&dict, ctx->kvz_params, "=", ",", 0)) {
