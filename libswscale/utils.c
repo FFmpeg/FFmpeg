d043e5c54c3f8e98be1a4bc1bcb76407af0e8ac7
Niklas Haas <git@haasn.dev>
2023-10-28 13:19:52 +0200

    swscale: don't omit ff_sws_init_range_convert for high-bit
    This was a complete hack seemingly designed to work around a different
    bug, which was fixed in the previous commit. As such, there is no more
    reason not to do this, as it simply breaks changing color range in
    sws_setColorspaceDetails for no reason.

--- libswscale/utils.c
+++ libswscale/utils.c
@@ -1049,9 +1049,7 @@ int sws_setColorspaceDetails(struct SwsContext *c, const int inv_table[4],
     c->srcRange   = srcRange;
     c->dstRange   = dstRange;
 
-    //The srcBpc check is possibly wrong but we seem to lack a definitive reference to test this
-    //and what we have in ticket 2939 looks better with this check
-    if (need_reinit && (c->srcBpc == 8 || !isYUV(c->srcFormat)))
+    if (need_reinit)
         ff_sws_init_range_convert(c);
 
     c->dstFormatBpp = av_get_bits_per_pixel(desc_dst);
