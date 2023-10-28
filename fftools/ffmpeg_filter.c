9d0f361f7fb84392d8ea032e954f7323c48404b1
Niklas Haas <git@haasn.dev>
2023-10-28 14:45:18 +0200

    fftools/ffmpeg_filter: don't clear buffersrc params
    This memset is bogus, it accomplishes nothing in the best case and
    regresses future additions to AVBufferSrcParameters in the worst case.

--- fftools/ffmpeg_filter.c
+++ fftools/ffmpeg_filter.c
@@ -1465,11 +1465,8 @@ static int configure_input_video_filter(FilterGraph *fg, AVFilterGraph *graph,
     int ret, pad_idx = 0;
     int64_t tsoffset = 0;
     AVBufferSrcParameters *par = av_buffersrc_parameters_alloc();
-
     if (!par)
         return AVERROR(ENOMEM);
-    memset(par, 0, sizeof(*par));
-    par->format = AV_PIX_FMT_NONE;
 
     if (ist->dec_ctx->codec_type == AVMEDIA_TYPE_AUDIO) {
         av_log(fg, AV_LOG_ERROR, "Cannot connect video filter to audio input\n");
