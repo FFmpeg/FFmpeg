02594c8c011ca2cdc20334e9bb812ec8f6f37cf3
Rémi Denis-Courmont <remi@remlab.net>
2023-10-29 13:26:30 +0200

    lavc/pixblockdsp: rework R-V V get_pixels_unaligned
    As in the aligned case, we can use VLSE64.V, though the way of doing so
    gets more convoluted, so the performance gains are more modest:
    
    get_pixels_unaligned_c:       126.7
    get_pixels_unaligned_rvv_i32: 145.5 (before)
    get_pixels_unaligned_rvv_i64:  62.2 (after)
    
    For the reference, those are the aligned benchmarks (unchanged) on the
    same T-Head C908 hardware:
    
    get_pixels_c:                 126.7
    get_pixels_rvi:                85.7
    get_pixels_rvv_i64:            33.2

--- libavcodec/riscv/pixblockdsp_init.c
+++ libavcodec/riscv/pixblockdsp_init.c
@@ -56,20 +56,17 @@ av_cold void ff_pixblockdsp_init_riscv(PixblockDSPContext *c,
 
 #if HAVE_RVV
     if ((cpu_flags & AV_CPU_FLAG_RVV_I32) && ff_get_rv_vlenb() >= 16) {
+        c->diff_pixels = ff_diff_pixels_unaligned_rvv;
+        c->diff_pixels_unaligned = ff_diff_pixels_unaligned_rvv;
+    }
+
+    if ((cpu_flags & AV_CPU_FLAG_RVV_I64) && ff_get_rv_vlenb() >= 16) {
         if (!high_bit_depth) {
-            c->get_pixels = ff_get_pixels_unaligned_8_rvv;
+            c->get_pixels = ff_get_pixels_8_rvv;
             c->get_pixels_unaligned = ff_get_pixels_unaligned_8_rvv;
         }
 
-        c->diff_pixels = ff_diff_pixels_unaligned_rvv;
-        c->diff_pixels_unaligned = ff_diff_pixels_unaligned_rvv;
-
-        if (cpu_flags & AV_CPU_FLAG_RVV_I64) {
-            if (!high_bit_depth)
-                c->get_pixels = ff_get_pixels_8_rvv;
-
-            c->diff_pixels = ff_diff_pixels_rvv;
-        }
+        c->diff_pixels = ff_diff_pixels_rvv;
     }
 #endif
 }
