bb0a684d93f793457f7bff3940a1398eb9e91703
Michael Niedermayer <michael@niedermayer.cc>
2023-11-08 01:44:07 +0100

    avcodec/4xm: Check for cfrm exhaustion
    Fixes: index -1 out of bounds for type 'CFrameBuffer [100]'
    Fixes: 63877/clusterfuzz-testcase-minimized-ffmpeg_AV_CODEC_ID_FOURXM_fuzzer-5854263397711872
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/4xm.c
+++ libavcodec/4xm.c
@@ -885,6 +885,8 @@ static int decode_frame(AVCodecContext *avctx, AVFrame *picture,
         }
 
         if (i >= CFRAME_BUFFER_COUNT) {
+            if (free_index < 0)
+                return AVERROR_INVALIDDATA;
             i             = free_index;
             f->cfrm[i].id = id;
         }
