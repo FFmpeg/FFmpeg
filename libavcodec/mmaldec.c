e8a49b1424d405441fb23c2850274a84e95bd892
Zhao Zhili <zhilizhao@tencent.com>
2023-11-21 04:05:08 +0800

    avcodec/mmaldec: Fix build error
    Fix #10670.
    
    Signed-off-by: Zhao Zhili <zhilizhao@tencent.com>

--- libavcodec/mmaldec.c
+++ libavcodec/mmaldec.c
@@ -843,7 +843,7 @@ static const AVClass ffmmal_dec_class = {
         .flush          = ffmmal_flush, \
         .p.priv_class   = &ffmmal_dec_class, \
         .p.capabilities = AV_CODEC_CAP_DELAY | AV_CODEC_CAP_HARDWARE, \
-        .caps_internal  = FF_CODEC_CAP_NOT_INIT_THREADSAFE \
+        .caps_internal  = FF_CODEC_CAP_NOT_INIT_THREADSAFE, \
         .p.pix_fmts     = (const enum AVPixelFormat[]) { AV_PIX_FMT_MMAL, \
                                                          AV_PIX_FMT_YUV420P, \
                                                          AV_PIX_FMT_NONE}, \
