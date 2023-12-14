ca6f0192f2ef69ffcbfb366e13e48814bdfa0ef7
Anton Khirnov <anton@khirnov.net>
2023-12-14 18:04:16 +0100

    fftools/cmdutils: change option flags to (1 << N) style
    It is easier to read.

--- fftools/cmdutils.h
+++ fftools/cmdutils.h
@@ -106,28 +106,31 @@ typedef struct SpecifierOpt {
 typedef struct OptionDef {
     const char *name;
     int flags;
-#define HAS_ARG    0x0001
-#define OPT_BOOL   0x0002
-#define OPT_EXPERT 0x0004
-#define OPT_STRING 0x0008
-#define OPT_VIDEO  0x0010
-#define OPT_AUDIO  0x0020
-#define OPT_INT    0x0080
-#define OPT_FLOAT  0x0100
-#define OPT_SUBTITLE 0x0200
-#define OPT_INT64  0x0400
-#define OPT_EXIT   0x0800
-#define OPT_DATA   0x1000
-#define OPT_PERFILE  0x2000     /* the option is per-file (currently ffmpeg-only).
-                                   implied by OPT_OFFSET or OPT_SPEC */
-#define OPT_OFFSET 0x4000       /* option is specified as an offset in a passed optctx */
-#define OPT_SPEC   0x8000       /* option is to be stored in an array of SpecifierOpt.
-                                   Implies OPT_OFFSET. Next element after the offset is
-                                   an int containing element count in the array. */
-#define OPT_TIME  0x10000
-#define OPT_DOUBLE 0x20000
-#define OPT_INPUT  0x40000
-#define OPT_OUTPUT 0x80000
+#define HAS_ARG         (1 << 0)
+#define OPT_BOOL        (1 << 1)
+#define OPT_EXPERT      (1 << 2)
+#define OPT_STRING      (1 << 3)
+#define OPT_VIDEO       (1 << 4)
+#define OPT_AUDIO       (1 << 5)
+#define OPT_INT         (1 << 6)
+#define OPT_FLOAT       (1 << 7)
+#define OPT_SUBTITLE    (1 << 8)
+#define OPT_INT64       (1 << 9)
+#define OPT_EXIT        (1 << 10)
+#define OPT_DATA        (1 << 11)
+/* The option is per-file (currently ffmpeg-only).
+   implied by OPT_OFFSET or OPT_SPEC */
+#define OPT_PERFILE     (1 << 12)
+/* Option is specified as an offset in a passed optctx */
+#define OPT_OFFSET      (1 << 13)
+/* Option is to be stored in an array of SpecifierOpt. Implies OPT_OFFSET.
+   Next element after the offset is an int containing element count in the
+   array. */
+#define OPT_SPEC        (1 << 14)
+#define OPT_TIME        (1 << 15)
+#define OPT_DOUBLE      (1 << 16)
+#define OPT_INPUT       (1 << 17)
+#define OPT_OUTPUT      (1 << 18)
      union {
         void *dst_ptr;
         int (*func_arg)(void *, const char *, const char *);
