FFmpeg/FFmpeg

commit 45dc668aea0edac34969b5a1ff76cf9ad3a09be1

avfilter/f_reverse: readjust frame timestamps for areverse

libavfilter/f_reverse.c
@@ -33,6 +33,7 @@ typedef struct ReverseContext {
     unsigned int pts_size;
     int64_t *pts;
     int flush_idx;
+    int64_t nb_samples;
 } ReverseContext;
 
 static av_cold int init(AVFilterContext *ctx)
@@ -249,7 +250,8 @@ static int areverse_request_frame(AVFilterLink *outlink)
 
     if (ret == AVERROR_EOF && s->nb_frames > 0) {
         AVFrame *out = s->frames[s->nb_frames - 1];
-        out->pts     = s->pts[s->flush_idx++];
+        out->pts     = s->pts[s->flush_idx++] - s->nb_samples;
+        s->nb_samples += s->pts[s->flush_idx] - s->pts[s->flush_idx - 1] - out->nb_samples;
 
         if (av_sample_fmt_is_planar(out->format))
             reverse_samples_planar(out);

commit 61e73851a33f0b4cb7662f8578a4695e77bd3c19

avfilter/f_reverse: Apply PTS compensation only when pts is available
Fixes: out of array access
Fixes: tickets/10753/poc16ffmpeg

Regression since: 45dc668aea0edac34969b5a1ff76cf9ad3a09be1
Found-by: Zeng Yunxiang
Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

libavfilter/f_reverse.c
@@ -266,7 +266,8 @@ static int areverse_request_frame(AVFilterLink *outlink)
         AVFrame *out = s->frames[s->nb_frames - 1];
         out->duration = s->duration[s->flush_idx];
         out->pts     = s->pts[s->flush_idx++] - s->nb_samples;
-        s->nb_samples += s->pts[s->flush_idx] - s->pts[s->flush_idx - 1] - out->nb_samples;
+        if (s->nb_frames > 1)
+            s->nb_samples += s->pts[s->flush_idx] - s->pts[s->flush_idx - 1] - out->nb_samples;
 
         if (av_sample_fmt_is_planar(out->format))
             reverse_samples_planar(out);