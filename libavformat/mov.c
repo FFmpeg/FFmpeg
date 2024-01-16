be4fcf027b768161bf622e8fbeb326139334dd74
James Almer <jamrial@gmail.com>
2024-01-15 22:16:44 -0300

    avformat/mov: reset a log message as trace level
    Accidentally changed in d9fed9df2a
    
    Signed-off-by: James Almer <jamrial@gmail.com>

--- libavformat/mov.c
+++ libavformat/mov.c
@@ -8220,7 +8220,7 @@ static int mov_read_default(MOVContext *c, AVIOContext *pb, MOVAtom atom)
             a.size = avio_rb64(pb) - 8;
             total_size += 8;
         }
-        av_log(c->fc, AV_LOG_DEBUG, "type:'%s' parent:'%s' sz: %"PRId64" %"PRId64" %"PRId64"\n",
+        av_log(c->fc, AV_LOG_TRACE, "type:'%s' parent:'%s' sz: %"PRId64" %"PRId64" %"PRId64"\n",
                av_fourcc2str(a.type), av_fourcc2str(atom.type), a.size, total_size, atom.size);
         if (a.size == 0) {
             a.size = atom.size - total_size + 8;
