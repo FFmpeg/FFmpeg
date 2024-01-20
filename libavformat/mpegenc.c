02e4598f8f40d0ac43d73fa79f02caecf79cbac1
Stefano Sabatini <stefasab@gmail.com>
2024-01-20 17:54:37 +0100

    lavf/mpegenc: extend options definition

--- libavformat/mpegenc.c
+++ libavformat/mpegenc.c
@@ -1291,8 +1291,8 @@ static void mpeg_mux_deinit(AVFormatContext *ctx)
 #define OFFSET(x) offsetof(MpegMuxContext, x)
 #define E AV_OPT_FLAG_ENCODING_PARAM
 static const AVOption options[] = {
-    { "muxrate", NULL,                                          OFFSET(user_mux_rate), AV_OPT_TYPE_INT, { .i64 = 0 }, 0, ((1<<22) - 1) * (8 * 50), E },
-    { "preload", "Initial demux-decode delay in microseconds.", OFFSET(preload),  AV_OPT_TYPE_INT, { .i64 = 500000 }, 0, INT_MAX, E },
+    { "muxrate", "mux rate as bits/s", OFFSET(user_mux_rate), AV_OPT_TYPE_INT, { .i64 = 0 }, 0, ((1<<22) - 1) * (8 * 50), E },
+    { "preload", "initial demux-decode delay in microseconds", OFFSET(preload),  AV_OPT_TYPE_INT, { .i64 = 500000 }, 0, INT_MAX, E },
     { NULL },
 };
 
