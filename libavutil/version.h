4105899245d79976b4a1a230455066417f3f194d
James Almer <jamrial@gmail.com>
2024-01-20 10:58:23 -0300

    avutil: remove deprecated FF_API_FIFO_OLD_API
    Signed-off-by: James Almer <jamrial@gmail.com>

--- libavutil/version.h
+++ libavutil/version.h
@@ -105,7 +105,6 @@
  * @{
  */
 
-#define FF_API_FIFO_OLD_API             (LIBAVUTIL_VERSION_MAJOR < 59)
 #define FF_API_XVMC                     (LIBAVUTIL_VERSION_MAJOR < 59)
 #define FF_API_OLD_CHANNEL_LAYOUT       (LIBAVUTIL_VERSION_MAJOR < 59)
 #define FF_API_AV_FOPEN_UTF8            (LIBAVUTIL_VERSION_MAJOR < 59)
