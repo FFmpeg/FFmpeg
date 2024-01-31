82e8838165bba565912e6a00422f01329df32e18
Tong Wu <tong1.wu@intel.com>
2024-01-31 19:56:33 +0800

    avcodec/dxva2: fix different 'const' qualifiers warning
    Signed-off-by: Tong Wu <tong1.wu@intel.com>

--- libavcodec/dxva2.c
+++ libavcodec/dxva2.c
@@ -795,7 +795,7 @@ unsigned ff_dxva2_get_surface_index(const AVCodecContext *avctx,
     }
 #endif
 
-    av_log(avctx, AV_LOG_WARNING, "Could not get surface index. Using 0 instead.\n");
+    av_log((AVCodecContext *)avctx, AV_LOG_WARNING, "Could not get surface index. Using 0 instead.\n");
     return 0;
 }
 
