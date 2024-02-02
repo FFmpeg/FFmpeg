e74e18cae466b4799af381adb3da0db151fffa56
sunyuechi <sunyuechi@iscas.ac.cn>
2024-02-02 12:50:07 +0800

    lavc/vp8dsp: R-V V vp8_idct_dc_add4y
    c908:
    vp8_idct_dc_add4y_c: 368.5
    vp8_idct_dc_add4y_rvv_i32: 134.5
    
    Signed-off-by: Rémi Denis-Courmont <remi@remlab.net>

--- libavcodec/riscv/vp8dsp_init.c
+++ libavcodec/riscv/vp8dsp_init.c
@@ -26,6 +26,7 @@
 #include "libavcodec/vp8dsp.h"
 
 void ff_vp8_idct_dc_add_rvv(uint8_t *dst, int16_t block[16], ptrdiff_t stride);
+void ff_vp8_idct_dc_add4y_rvv(uint8_t *dst, int16_t block[4][16], ptrdiff_t stride);
 
 av_cold void ff_vp8dsp_init_riscv(VP8DSPContext *c)
 {
@@ -34,6 +35,7 @@ av_cold void ff_vp8dsp_init_riscv(VP8DSPContext *c)
 
     if (flags & AV_CPU_FLAG_RVV_I32 && ff_get_rv_vlenb() >= 16) {
         c->vp8_idct_dc_add = ff_vp8_idct_dc_add_rvv;
+        c->vp8_idct_dc_add4y = ff_vp8_idct_dc_add4y_rvv;
     }
 #endif
 }
