cb9f1f59a1db6f41494890ff23dec20325497c69
Andreas Rheinhardt <andreas.rheinhardt@outlook.com>
2024-02-18 02:46:33 +0100

    avutil/version: Remove outdated checks
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

--- libavutil/version.c
+++ libavutil/version.c
@@ -18,14 +18,9 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
-#include <stdlib.h>
-
 #include "config.h"
 #include "avassert.h"
 #include "avutil.h"
-#include "common.h"
-#include "libm.h"
-#include "log.h"
 #include "samplefmt.h"
 #include "version.h"
 
@@ -43,17 +38,6 @@ unsigned avutil_version(void)
     av_assert0(AVMEDIA_TYPE_ATTACHMENT == 4);
     av_assert0(AV_PICTURE_TYPE_BI == 7);
     av_assert0(LIBAVUTIL_VERSION_MICRO >= 100);
-
-    av_assert0(((size_t)-1) > 0); // C guarantees this but if false on a platform we care about revert at least b284e1ffe343d6697fb950d1ee517bafda8a9844
-
-    if (av_sat_dadd32(1, 2) != 5) {
-        av_log(NULL, AV_LOG_FATAL, "Libavutil has been built with a broken binutils, please upgrade binutils and rebuild\n");
-        abort();
-    }
-
-    if (llrint(1LL<<60) != 1LL<<60) {
-        av_log(NULL, AV_LOG_ERROR, "Libavutil has been linked to a broken llrint()\n");
-    }
 
     return LIBAVUTIL_VERSION_INT;
 }
