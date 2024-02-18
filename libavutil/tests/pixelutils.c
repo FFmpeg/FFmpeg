27cd7011bc2e1669a23a45b1ee90a58716cfb592
Andreas Rheinhardt <andreas.rheinhardt@outlook.com>
2024-02-18 14:37:06 +0100

    avutil/tests/pixelutils: Remove dead code
    Forgotten in e6b125e3be19fb341fd9a759ad0af3b39ba35e0c.
    
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

--- libavutil/tests/pixelutils.c
+++ libavutil/tests/pixelutils.c
@@ -109,14 +109,6 @@ static int run_test(const char *test,
     int i, a, ret = 0;
 
     for (a = 0; a < 3; a++) {
-        const uint8_t *block1 = b1;
-        const uint8_t *block2 = b2;
-
-        switch (a) {
-        case 0: block1++; block2++; break;
-        case 1:           block2++; break;
-        case 2:                     break;
-        }
         for (i = 1; i <= FF_ARRAY_ELEMS(sad_c); i++) {
             int r = run_single_test(test, b1, W1, b2, W2, a, i);
             if (r)
