8bc8b35a37960f6b391fb86c7431ba9a844241a3
Michael Niedermayer <michael@niedermayer.cc>
2024-02-20 03:32:38 +0100

    libswscale/utils: Fix bayer to yuvj
    Fixes: out of array access.
    
    Earlier code assumes that a unscaled bayer to yuvj420 converter exists
    but the later code then skips yuvj420
    
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit e9cc9e492f987ce23ce8c514258a17952dd20401)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libswscale/utils.c
+++ libswscale/utils.c
@@ -1789,7 +1789,7 @@ av_cold int sws_init_context(SwsContext *c, SwsFilter *srcFilter,
 
     /* unscaled special cases */
     if (unscaled && !usesHFilter && !usesVFilter &&
-        (c->srcRange == c->dstRange || isAnyRGB(dstFormat))) {
+        (c->srcRange == c->dstRange || isAnyRGB(dstFormat) || isBayer(srcFormat))) {
         ff_get_unscaled_swscale(c);
 
         if (c->swscale) {
