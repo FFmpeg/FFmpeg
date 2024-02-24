906fc2b2e77d9840d0738f717d4b693f2accc51c
Andreas Rheinhardt <andreas.rheinhardt@outlook.com>
2024-02-25 00:30:42 +0100

    fftools/ffmpeg_mux: Fix use of uninitialized variable
    Broken in a2fc86378a18b2c2966ce3438df8f27f646438e5.
    
    Reviewed-by: Anton Khirnov <anton@khirnov.net>
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

--- fftools/ffmpeg_mux.c
+++ fftools/ffmpeg_mux.c
@@ -506,14 +506,14 @@ int print_sdp(const char *filename);
 int print_sdp(const char *filename)
 {
     char sdp[16384];
-    int j, ret;
+    int j = 0, ret;
     AVIOContext *sdp_pb;
     AVFormatContext **avc;
 
     avc = av_malloc_array(nb_output_files, sizeof(*avc));
     if (!avc)
         return AVERROR(ENOMEM);
-    for (int i = 0, j = 0; i < nb_output_files; i++) {
+    for (int i = 0; i < nb_output_files; i++) {
         if (!strcmp(output_files[i]->format->name, "rtp")) {
             avc[j] = mux_from_of(output_files[i])->fc;
             j++;
