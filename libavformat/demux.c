af6a1dd0b2e9cd9b5fe41f1842995496fe083895
Nicolas Gaullier <nicolas.gaullier@cji.paris>
2024-03-04 17:27:02 +0100

    avformat/demux: duplicate side_data in parse_packet()
    If an input packet results in several output packets, the side data will be
    exported only by the first output packet, and be missing from the rest.
    
    Signed-off-by: Nicolas Gaullier <nicolas.gaullier@cji.paris>
    Signed-off-by: James Almer <jamrial@gmail.com>

--- libavformat/demux.c
+++ libavformat/demux.c
@@ -1174,7 +1174,6 @@ static int parse_packet(AVFormatContext *s, AVPacket *pkt,
                         int stream_index, int flush)
 {
     FormatContextInternal *const fci = ff_fc_internal(s);
-    FFFormatContext *const si = &fci->fc;
     AVStream *st = s->streams[stream_index];
     FFStream *const sti = ffstream(st);
     AVPacket *out_pkt = sti->parse_pkt;
@@ -1183,7 +1182,7 @@ static int parse_packet(AVFormatContext *s, AVPacket *pkt,
     uint8_t *extradata = sti->avctx->extradata;
     int extradata_size = sti->avctx->extradata_size;
     int size = pkt->size;
-    int ret = 0, got_output = flush;
+    int ret = 0, got_output = flush, pkt_side_data_consumed = 0;
 
     if (!size && !flush && sti->parser->flags & PARSER_FLAG_COMPLETE_FRAMES) {
         // preserve 0-size sync packets
@@ -1227,10 +1226,21 @@ static int parse_packet(AVFormatContext *s, AVPacket *pkt,
         got_output = !!out_pkt->size;
 
         if (pkt->side_data && !out_pkt->side_data) {
-            out_pkt->side_data       = pkt->side_data;
-            out_pkt->side_data_elems = pkt->side_data_elems;
-            pkt->side_data          = NULL;
-            pkt->side_data_elems    = 0;
+            /* for the first iteration, side_data are simply moved to output.
+             * in case of additional iterations, they are duplicated each time. */
+            if (!pkt_side_data_consumed) {
+                pkt_side_data_consumed = 1;
+                out_pkt->side_data       = pkt->side_data;
+                out_pkt->side_data_elems = pkt->side_data_elems;
+            } else for (int i = 0; i < pkt->side_data_elems; i++) {
+                const AVPacketSideData *const src_sd = &pkt->side_data[i];
+                uint8_t *dst_data = av_packet_new_side_data(out_pkt, src_sd->type, src_sd->size);
+                if (!dst_data) {
+                    ret = AVERROR(ENOMEM);
+                    goto fail;
+                }
+                memcpy(dst_data, src_sd->data, src_sd->size);
+            }
         }
 
         if (!out_pkt->size)
@@ -1304,6 +1314,10 @@ fail:
     if (sd) {
         sti->avctx->extradata      = extradata;
         sti->avctx->extradata_size = extradata_size;
+    }
+    if (pkt_side_data_consumed) {
+        pkt->side_data          = NULL;
+        pkt->side_data_elems    = 0;
     }
 
     if (ret < 0)
