b0e17e0f959b09fb228665a7eb7323af9b6ffeac
Zhao Zhili <zhilizhao@tencent.com>
2024-03-04 11:37:58 +0800

    avcodec/mediacodecenc: Add a hint message to use nv12 pix_fmt
    Signed-off-by: Zhao Zhili <zhilizhao@tencent.com>

--- libavcodec/mediacodecenc.c
+++ libavcodec/mediacodecenc.c
@@ -319,6 +319,9 @@ static av_cold int mediacodec_init(AVCodecContext *avctx)
     ret = ff_AMediaCodec_configure(s->codec, format, s->window, NULL, ret);
     if (ret) {
         av_log(avctx, AV_LOG_ERROR, "MediaCodec configure failed, %s\n", av_err2str(ret));
+        if (avctx->pix_fmt == AV_PIX_FMT_YUV420P)
+            av_log(avctx, AV_LOG_ERROR, "Please try -pix_fmt nv12, some devices don't "
+                                        "support yuv420p as encoder input format.\n");
         goto bailout;
     }
 
