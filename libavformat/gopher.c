384cc270d25ef1cc53f9cc0e047033b47cdaddd1
Christian Lee Seibold <christian.seibold32@outlook.com>
2024-03-05 00:39:18 -0600

    avformat/gopher: Add audio and video itemtypes
    The 's', ';', and '<' itemtypes are used for audio and video by
    Gophernicus and Gopher+.
    
    Signed-off-by: Christian Lee Seibold <christian.seibold32@outlook.com>
    Signed-off-by: Marton Balint <cus@passwd.hu>

--- libavformat/gopher.c
+++ libavformat/gopher.c
@@ -48,8 +48,11 @@ static int gopher_connect(URLContext *h, const char *path)
 
     if (!*path) return AVERROR(EINVAL);
     switch (*++path) {
+        case ';':
+        case '<':
         case '5':
         case '9':
+        case 's':
             path = strchr(path, '/');
             if (!path) return AVERROR(EINVAL);
             break;
