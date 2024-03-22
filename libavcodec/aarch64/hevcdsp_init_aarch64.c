5cbeefc79eea34aa856c6e4e728ae8a0fc247eed
Martin Storsjö <martin@martin.st>
2024-03-22 13:41:45 +0200

    aarch64: hevc: Produce plain neon versions of qpel_hv
    As the plain neon qpel_h functions process two rows at a time,
    we need to allocate storage for h+8 rows instead of h+7.
    
    By allocating storage for h+8 rows, incrementing the stack
    pointer won't end up at the right spot in the end. Store the
    intended final stack pointer value in a register x14 which we
    store on the stack.
    
    AWS Graviton 3:
    put_hevc_qpel_hv4_8_c: 386.0
    put_hevc_qpel_hv4_8_neon: 125.7
    put_hevc_qpel_hv4_8_i8mm: 83.2
    put_hevc_qpel_hv6_8_c: 749.0
    put_hevc_qpel_hv6_8_neon: 207.0
    put_hevc_qpel_hv6_8_i8mm: 166.0
    put_hevc_qpel_hv8_8_c: 1305.2
    put_hevc_qpel_hv8_8_neon: 216.5
    put_hevc_qpel_hv8_8_i8mm: 213.0
    put_hevc_qpel_hv12_8_c: 2570.5
    put_hevc_qpel_hv12_8_neon: 480.0
    put_hevc_qpel_hv12_8_i8mm: 398.2
    put_hevc_qpel_hv16_8_c: 4158.7
    put_hevc_qpel_hv16_8_neon: 659.7
    put_hevc_qpel_hv16_8_i8mm: 593.5
    put_hevc_qpel_hv24_8_c: 8626.7
    put_hevc_qpel_hv24_8_neon: 1653.5
    put_hevc_qpel_hv24_8_i8mm: 1398.7
    put_hevc_qpel_hv32_8_c: 14646.0
    put_hevc_qpel_hv32_8_neon: 2566.2
    put_hevc_qpel_hv32_8_i8mm: 2287.5
    put_hevc_qpel_hv48_8_c: 31072.5
    put_hevc_qpel_hv48_8_neon: 6228.5
    put_hevc_qpel_hv48_8_i8mm: 5291.0
    put_hevc_qpel_hv64_8_c: 53847.2
    put_hevc_qpel_hv64_8_neon: 9856.7
    put_hevc_qpel_hv64_8_i8mm: 8831.0
    
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- libavcodec/aarch64/hevcdsp_init_aarch64.c
+++ libavcodec/aarch64/hevcdsp_init_aarch64.c
@@ -267,6 +267,10 @@ NEON8_FNPROTO(qpel_v, (int16_t *dst,
 
 NEON8_FNPROTO(qpel_hv, (int16_t *dst,
         const uint8_t *src, ptrdiff_t srcstride,
+        int height, intptr_t mx, intptr_t my, int width),);
+
+NEON8_FNPROTO(qpel_hv, (int16_t *dst,
+        const uint8_t *src, ptrdiff_t srcstride,
         int height, intptr_t mx, intptr_t my, int width), _i8mm);
 
 NEON8_FNPROTO(qpel_uni_v, (uint8_t *dst,  ptrdiff_t dststride,
@@ -435,6 +439,8 @@ av_cold void ff_hevc_dsp_init_aarch64(HEVCDSPContext *c, const int bit_depth)
         NEON8_FNASSIGN(c->put_hevc_epel_bi, 1, 1, epel_bi_hv,);
 
         NEON8_FNASSIGN_SHARED_32(c->put_hevc_qpel_uni_w, 0, 1, qpel_uni_w_h,);
+
+        NEON8_FNASSIGN(c->put_hevc_qpel, 1, 1, qpel_hv,);
 
         if (have_i8mm(cpu_flags)) {
             NEON8_FNASSIGN(c->put_hevc_epel, 0, 1, epel_h, _i8mm);
