cd63dab55cde491afd7e7b24197c8745ce6bc71a
Andreas Rheinhardt <andreas.rheinhardt@outlook.com>
2024-03-28 05:31:59 +0100

    avcodec/mips/ac3dsp_mips: Add missing includes
    Likely broken in d7a75d21635eab4f4a1efea22945933059c2e36f.
    
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

--- libavcodec/mips/ac3dsp_mips.c
+++ libavcodec/mips/ac3dsp_mips.c
@@ -54,11 +54,13 @@
  */
 
 #include <stddef.h>
+#include <string.h>
 
 #include "config.h"
 #include "libavcodec/ac3dsp.h"
 #include "libavcodec/ac3.h"
 #include "libavcodec/ac3tab.h"
+#include "libavutil/macros.h"
 #include "libavutil/mips/asmdefs.h"
 
 #if HAVE_INLINE_ASM
