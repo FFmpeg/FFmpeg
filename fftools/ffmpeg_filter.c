114cbaa316d7d71d942229cd665e76a0a5e3c24b
Anton Khirnov <anton@khirnov.net>
2024-04-02 12:23:05 +0200

    fftools/ffmpeg_filter: drop a redundant check
    fg_finalise_bindings() already checks that all filtergraph outputs are
    connected.

--- fftools/ffmpeg_filter.c
+++ fftools/ffmpeg_filter.c
@@ -1472,11 +1472,6 @@ fail:
 static int configure_output_filter(FilterGraph *fg, AVFilterGraph *graph,
                                    OutputFilter *ofilter, AVFilterInOut *out)
 {
-    if (!ofilter->ost) {
-        av_log(fg, AV_LOG_FATAL, "Filter %s has an unconnected output\n", ofilter->name);
-        return AVERROR(EINVAL);
-    }
-
     switch (avfilter_pad_get_type(out->filter_ctx->output_pads, out->pad_idx)) {
     case AVMEDIA_TYPE_VIDEO: return configure_output_video_filter(fg, graph, ofilter, out);
     case AVMEDIA_TYPE_AUDIO: return configure_output_audio_filter(fg, graph, ofilter, out);
