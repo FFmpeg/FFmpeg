5e380bcdb13dd47ce9c358a4edb281f05fde3f24
LuMingYin <lumingyindetect@163.com>
2024-04-12 17:19:23 +0800

    avfilter/af_channelsplit: fix memory leak
    Signed-off-by: LuMingYin <lumingyindetect@163.com>
    Signed-off-by: Zhao Zhili <zhilizhao@tencent.com>

--- libavfilter/af_channelsplit.c
+++ libavfilter/af_channelsplit.c
@@ -163,8 +163,10 @@ static int filter_frame(AVFilterLink *outlink, AVFrame *buf)
 
     buf_out->data[0] = buf_out->extended_data[0] = buf_out->extended_data[s->map[i]];
     ret = av_channel_layout_from_mask(&buf_out->ch_layout, 1ULL << channel);
-    if (ret < 0)
+    if (ret < 0) {
+        av_frame_free(&buf_out);
         return ret;
+    }
 
     return ff_filter_frame(ctx->outputs[i], buf_out);
 }
