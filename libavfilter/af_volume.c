ebe78e3ef0b07dbefe90c8709aef9611183c08ed
Andreas Rheinhardt <andreas.rheinhardt@outlook.com>
2024-05-05 16:26:01 +0200

    avfilter/af_volume: Don't free options manually
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

--- libavfilter/af_volume.c
+++ libavfilter/af_volume.c
@@ -127,7 +127,6 @@ static av_cold void uninit(AVFilterContext *ctx)
 {
     VolumeContext *vol = ctx->priv;
     av_expr_free(vol->volume_pexpr);
-    av_opt_free(vol);
     av_freep(&vol->fdsp);
 }
 
