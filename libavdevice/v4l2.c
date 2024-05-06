008b0b4a3b0cbafa568d0dcbf78c798d56929802
Brad Smith <brad-at-comstyle.com@ffmpeg.org>
2024-05-05 23:59:47 -0400

    lavd/v4l2: Use proper field type for second parameter of ioctl() with BSD's
    The proper type was used until 73251678c83cbe24d08264da693411b166239bc7.
    
    This covers all of the OS's that currently have V4L2 support, permutations
    of Linux glibc/musl, Android bionic, FreeBSD, NetBSD, OpenBSD, Solaris.
    
    Copied from FreeBSD ports patch.
    
    Signed-off-by: Brad Smith <brad@comstyle.com>
    Signed-off-by: Marton Balint <cus@passwd.hu>
    (cherry picked from commit 9e674b31606c805dd31b4bb754364a72a5877238)
    Signed-off-by: Brad Smith <brad@comstyle.com>

--- libavdevice/v4l2.c
+++ libavdevice/v4l2.c
@@ -95,10 +95,10 @@ struct video_data {
     int (*open_f)(const char *file, int oflag, ...);
     int (*close_f)(int fd);
     int (*dup_f)(int fd);
-#ifdef __GLIBC__
-    int (*ioctl_f)(int fd, unsigned long int request, ...);
-#else
+#if defined(__sun) || defined(__BIONIC__) || defined(__musl__) /* POSIX-like */
     int (*ioctl_f)(int fd, int request, ...);
+#else
+    int (*ioctl_f)(int fd, unsigned long int request, ...);
 #endif
     ssize_t (*read_f)(int fd, void *buffer, size_t n);
     void *(*mmap_f)(void *start, size_t length, int prot, int flags, int fd, int64_t offset);
