6e77af1c220c4e0ae06431febad065c78eaa9772
sunyuechi <sunyuechi@iscas.ac.cn>
2024-05-08 00:54:08 +0800

    lavc/vp8dsp: R-V V put_epel v
    C908:
    vp8_put_epel4_v4_c: 11.0
    vp8_put_epel4_v4_rvv_i32: 5.0
    vp8_put_epel4_v6_c: 16.5
    vp8_put_epel4_v6_rvv_i32: 6.2
    vp8_put_epel8_v4_c: 43.7
    vp8_put_epel8_v4_rvv_i32: 11.2
    vp8_put_epel8_v6_c: 68.7
    vp8_put_epel8_v6_rvv_i32: 13.2
    vp8_put_epel16_v4_c: 92.5
    vp8_put_epel16_v4_rvv_i32: 13.7
    vp8_put_epel16_v6_c: 135.7
    vp8_put_epel16_v6_rvv_i32: 16.5
    
    Signed-off-by: Rémi Denis-Courmont <remi@remlab.net>

--- libavcodec/riscv/vp8dsp_init.c
+++ libavcodec/riscv/vp8dsp_init.c
@@ -90,6 +90,13 @@ av_cold void ff_vp78dsp_init_riscv(VP8DSPContext *c)
         c->put_vp8_epel_pixels_tab[0][0][1] = ff_put_vp8_epel16_h4_rvv;
         c->put_vp8_epel_pixels_tab[1][0][1] = ff_put_vp8_epel8_h4_rvv;
         c->put_vp8_epel_pixels_tab[2][0][1] = ff_put_vp8_epel4_h4_rvv;
+
+        c->put_vp8_epel_pixels_tab[0][2][0] = ff_put_vp8_epel16_v6_rvv;
+        c->put_vp8_epel_pixels_tab[1][2][0] = ff_put_vp8_epel8_v6_rvv;
+        c->put_vp8_epel_pixels_tab[2][2][0] = ff_put_vp8_epel4_v6_rvv;
+        c->put_vp8_epel_pixels_tab[0][1][0] = ff_put_vp8_epel16_v4_rvv;
+        c->put_vp8_epel_pixels_tab[1][1][0] = ff_put_vp8_epel8_v4_rvv;
+        c->put_vp8_epel_pixels_tab[2][1][0] = ff_put_vp8_epel4_v4_rvv;
     }
 #endif
 #endif
