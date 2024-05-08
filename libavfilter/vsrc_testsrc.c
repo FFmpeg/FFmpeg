77fc047bd91eaef615a2c36b93ee23d7d27e7d25
Marton Balint <cus@passwd.hu>
2024-05-08 09:40:57 +0200

    avfilter/vsrc_testsrc: do not round down width and height for color src
    ff_draw_rectangle handles subsampling since 2013.
    
    Fixes ticket #10989.
    
    Signed-off-by: Marton Balint <cus@passwd.hu>

--- libavfilter/vsrc_testsrc.c
+++ libavfilter/vsrc_testsrc.c
@@ -260,8 +260,6 @@ static int color_config_props(AVFilterLink *inlink)
                   inlink->color_range, 0);
     ff_draw_color(&test->draw, &test->color, test->color_rgba);
 
-    test->w = ff_draw_round_to_sub(&test->draw, 0, -1, test->w);
-    test->h = ff_draw_round_to_sub(&test->draw, 1, -1, test->h);
     if (av_image_check_size(test->w, test->h, 0, ctx) < 0)
         return AVERROR(EINVAL);
 
