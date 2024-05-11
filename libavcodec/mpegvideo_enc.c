b6b2b01025e016ce29e5add57305384a663edcfc
Michael Niedermayer <michael@niedermayer.cc>
2024-05-12 00:13:58 +0200

    avcodec/mpegvideo_enc: Fix potential overflow in RD
    Fixes: CID1500285 Unintentional integer overflow
    
    Sponsored-by: Sovereign Tech Fund
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/mpegvideo_enc.c
+++ libavcodec/mpegvideo_enc.c
@@ -1433,7 +1433,7 @@ static int estimate_best_b_count(MpegEncContext *s)
                 goto fail;
             }
 
-            rd += (out_size * lambda2) >> (FF_LAMBDA_SHIFT - 3);
+            rd += (out_size * (uint64_t)lambda2) >> (FF_LAMBDA_SHIFT - 3);
         }
 
         /* get the delayed frames */
@@ -1442,7 +1442,7 @@ static int estimate_best_b_count(MpegEncContext *s)
             ret = out_size;
             goto fail;
         }
-        rd += (out_size * lambda2) >> (FF_LAMBDA_SHIFT - 3);
+        rd += (out_size * (uint64_t)lambda2) >> (FF_LAMBDA_SHIFT - 3);
 
         rd += c->error[0] + c->error[1] + c->error[2];
 
