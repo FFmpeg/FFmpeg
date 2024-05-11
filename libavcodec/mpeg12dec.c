489f820a6e6161a431ef02e824ae59ebcbe7dea8
Michael Niedermayer <michael@niedermayer.cc>
2024-05-11 21:04:00 +0200

    avcodec/mpeg12dec: Use 64bit in bit computation
    I dont think this can actually overflow but 64bit seems reasonable to use
    
    Fixes: CID1521983 Unintentional integer overflow
    
    Sponsored-by: Sovereign Tech Fund
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit 4c725df059dd9a5f2071e204924105b3ceb74cbc)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/mpeg12dec.c
+++ libavcodec/mpeg12dec.c
@@ -3000,7 +3000,7 @@ static int ipu_decode_frame(AVCodecContext *avctx, void *data,
     int ret;
 
     // Check for minimal intra MB size (considering mb header, luma & chroma dc VLC, ac EOB VLC)
-    if (avpkt->size*8LL < (avctx->width+15)/16 * ((avctx->height+15)/16) * (2 + 3*4 + 2*2 + 2*6))
+    if (avpkt->size*8LL < (avctx->width+15)/16 * ((avctx->height+15)/16) * (2LL + 3*4 + 2*2 + 2*6))
         return AVERROR_INVALIDDATA;
 
     ret = ff_get_buffer(avctx, frame, 0);
