b2813ed083bf841f52105e5ba8fb5134e083d049
Michael Niedermayer <michael@niedermayer.cc>
2024-05-12 00:43:48 +0200

    avcodec/mscc & mwsc: Check loop counts before use
    This could cause timeouts
    
    Fixes: CID1439568 Untrusted loop bound
    
    Sponsored-by: Sovereign Tech Fund
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit e35fe3d8b9e345527a05b1ae958ac851fe09f1ed)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/mscc.c
+++ libavcodec/mscc.c
@@ -52,6 +52,9 @@ static int rle_uncompress(AVCodecContext *avctx, GetByteContext *gb, PutByteCont
         unsigned run = bytestream2_get_byte(gb);
 
         if (run) {
+            if (bytestream2_get_bytes_left_p(pb) < run * s->bpp)
+                return AVERROR_INVALIDDATA;
+
             switch (avctx->bits_per_coded_sample) {
             case 8:
                 fill = bytestream2_get_byte(gb);
@@ -100,6 +103,9 @@ static int rle_uncompress(AVCodecContext *avctx, GetByteContext *gb, PutByteCont
 
                 bytestream2_seek_p(pb, y * avctx->width * s->bpp + x * s->bpp, SEEK_SET);
             } else {
+                if (bytestream2_get_bytes_left_p(pb) < copy * s->bpp)
+                    return AVERROR_INVALIDDATA;
+
                 for (j = 0; j < copy; j++) {
                     switch (avctx->bits_per_coded_sample) {
                     case 8:
