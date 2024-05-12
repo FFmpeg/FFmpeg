d4083ecb7c872807aa24b6ec5865f8a2fba95cf2
sunyuechi <sunyuechi@iscas.ac.cn>
2024-05-12 20:57:07 +0800

    lavc/vc1dsp: R-V V mspel_pixels
                                                          C908 X60
    vc1dsp.avg_vc1_mspel_pixels_tab[0][0]_c            :  14.7 13.2
    vc1dsp.avg_vc1_mspel_pixels_tab[0][0]_rvv_i32      :   2.5  2.2
    vc1dsp.avg_vc1_mspel_pixels_tab[1][0]_c            :   3.7  3.5
    vc1dsp.avg_vc1_mspel_pixels_tab[1][0]_rvv_i64      :   1.0  1.2
    vc1dsp.put_vc1_mspel_pixels_tab[0][0]_c            :   9.0  8.0
    vc1dsp.put_vc1_mspel_pixels_tab[0][0]_rvi          :   1.0  1.0
    vc1dsp.put_vc1_mspel_pixels_tab[1][0]_c            :   2.5  2.2
    vc1dsp.put_vc1_mspel_pixels_tab[1][0]_rvi          :   0.5  0.5
    
    Signed-off-by: Rémi Denis-Courmont <remi@remlab.net>

--- libavcodec/riscv/vc1dsp_init.c
+++ libavcodec/riscv/vc1dsp_init.c
@@ -29,19 +29,33 @@ void ff_vc1_inv_trans_8x8_dc_rvv(uint8_t *dest, ptrdiff_t stride, int16_t *block
 void ff_vc1_inv_trans_4x8_dc_rvv(uint8_t *dest, ptrdiff_t stride, int16_t *block);
 void ff_vc1_inv_trans_8x4_dc_rvv(uint8_t *dest, ptrdiff_t stride, int16_t *block);
 void ff_vc1_inv_trans_4x4_dc_rvv(uint8_t *dest, ptrdiff_t stride, int16_t *block);
+void ff_put_pixels16x16_rvi(uint8_t *dst, const uint8_t *src, ptrdiff_t line_size, int rnd);
+void ff_put_pixels8x8_rvi(uint8_t *dst, const uint8_t *src, ptrdiff_t line_size, int rnd);
+void ff_avg_pixels16x16_rvv(uint8_t *dst, const uint8_t *src, ptrdiff_t line_size, int rnd);
+void ff_avg_pixels8x8_rvv(uint8_t *dst, const uint8_t *src, ptrdiff_t line_size, int rnd);
 
 av_cold void ff_vc1dsp_init_riscv(VC1DSPContext *dsp)
 {
-#if HAVE_RVV
+#if HAVE_RV
     int flags = av_get_cpu_flags();
 
+# if __riscv_xlen >= 64
+    if (flags & AV_CPU_FLAG_RVI) {
+        dsp->put_vc1_mspel_pixels_tab[1][0] = ff_put_pixels8x8_rvi;
+        dsp->put_vc1_mspel_pixels_tab[0][0] = ff_put_pixels16x16_rvi;
+    }
+# endif
+#if HAVE_RVV
     if (flags & AV_CPU_FLAG_RVV_I32 && ff_rv_vlen_least(128)) {
         dsp->vc1_inv_trans_4x8_dc = ff_vc1_inv_trans_4x8_dc_rvv;
         dsp->vc1_inv_trans_4x4_dc = ff_vc1_inv_trans_4x4_dc_rvv;
+        dsp->avg_vc1_mspel_pixels_tab[0][0] = ff_avg_pixels16x16_rvv;
         if (flags & AV_CPU_FLAG_RVV_I64) {
             dsp->vc1_inv_trans_8x8_dc = ff_vc1_inv_trans_8x8_dc_rvv;
             dsp->vc1_inv_trans_8x4_dc = ff_vc1_inv_trans_8x4_dc_rvv;
+            dsp->avg_vc1_mspel_pixels_tab[1][0] = ff_avg_pixels8x8_rvv;
         }
     }
+#endif
 #endif
 }
