d6bec7f83e639358f4ec0df6d573f6ea63b95c17
Brad Smith <brad@comstyle.com>
2024-05-18 07:38:40 -0400

    avutil/ppc/cpu: Also use the machdep.altivec sysctl on NetBSD
    Use the machdep.altivec sysctl on NetBSD for AltiVec detection
    as is done with OpenBSD.
    
    (cherry picked from commit 115c96b9bd53e775f425f23d5b73fa0a9dedbd08)
    Signed-off-by: Brad Smith <brad@comstyle.com>

--- libavutil/ppc/cpu.c
+++ libavutil/ppc/cpu.c
@@ -27,7 +27,7 @@
 #if HAVE_UNISTD_H
 #include <unistd.h>
 #endif
-#elif defined(__OpenBSD__)
+#elif defined(__NetBSD__) || defined(__OpenBSD__)
 #include <sys/types.h>
 #include <sys/sysctl.h>
 #include <machine/cpu.h>
@@ -56,8 +56,8 @@ int ff_get_cpu_flags_ppc(void)
     if (result == VECTORTYPE_ALTIVEC)
         return AV_CPU_FLAG_ALTIVEC;
     return 0;
-#elif defined(__APPLE__) || defined(__OpenBSD__)
-#ifdef __OpenBSD__
+#elif defined(__APPLE__) || defined(__NetBSD__) || defined(__OpenBSD__)
+#if defined(__NetBSD__) || defined(__OpenBSD__)
     int sels[2] = {CTL_MACHDEP, CPU_ALTIVEC};
 #else
     int sels[2] = {CTL_HW, HW_VECTORUNIT};
