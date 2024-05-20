3ab1855847eca3991d425e20235a4616cd943ab6
Michael Niedermayer <michael@niedermayer.cc>
2024-05-20 21:55:44 +0200

    avutil/tests/dict: Check av_dict_set() before get for failure
    Failure is possible due to strdup()
    
    Fixes: CID1516764 Dereference null return value
    
    Sponsored-by: Sovereign Tech Fund
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit e8a1e1899d9ededd78f8ec4722fe80c345bbf8f7)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavutil/tests/dict.c
+++ libavutil/tests/dict.c
@@ -122,12 +122,15 @@ int main(void)
 
     //valgrind sensible test
     printf("\nTesting av_dict_set() with existing AVDictionaryEntry.key as key\n");
-    av_dict_set(&dict, "key", "old", 0);
+    if (av_dict_set(&dict, "key", "old", 0) < 0)
+        return 1;
     e = av_dict_get(dict, "key", NULL, 0);
-    av_dict_set(&dict, e->key, "new val OK", 0);
+    if (av_dict_set(&dict, e->key, "new val OK", 0) < 0)
+        return 1;
     e = av_dict_get(dict, "key", NULL, 0);
     printf("%s\n", e->value);
-    av_dict_set(&dict, e->key, e->value, 0);
+    if (av_dict_set(&dict, e->key, e->value, 0) < 0)
+        return 1;
     e = av_dict_get(dict, "key", NULL, 0);
     printf("%s\n", e->value);
     av_dict_free(&dict);
