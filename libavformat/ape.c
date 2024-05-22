254b17eb13b42561be28cacf7ff6e513e13ab432
Michael Niedermayer <michael@niedermayer.cc>
2024-05-23 01:51:53 +0200

    avformat/ape: Use 64bit for final frame size
    Fixes: CID1505963 Unintentional integer overflow
    
    Sponsored-by: Sovereign Tech Fund
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit a2b8d03347930c051358fcbbdc557e57e157d9c9)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavformat/ape.c
+++ libavformat/ape.c
@@ -293,7 +293,7 @@ static int ape_read_header(AVFormatContext * s)
         final_size -= final_size & 3;
     }
     if (file_size <= 0 || final_size <= 0)
-        final_size = ape->finalframeblocks * 8;
+        final_size = ape->finalframeblocks * 8LL;
     ape->frames[ape->totalframes - 1].size = final_size;
 
     for (i = 0; i < ape->totalframes; i++) {
