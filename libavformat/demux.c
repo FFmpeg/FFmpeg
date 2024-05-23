84953f5c232e10d14ea85ef54779f7545195b8b8
Michael Niedermayer <michael@niedermayer.cc>
2024-05-23 23:25:42 +0200

    avformat/demux: resurrect dead stores
    Fixes: CID1473512 Unused value
    Fixes: CID1529228 Unused value
    
    Sponsored-by: Sovereign Tech Fund
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit 33da5f4e2717cc947cf44ad9a52668694ea4ee82)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavformat/demux.c
+++ libavformat/demux.c
@@ -2415,7 +2415,7 @@ static int add_coded_side_data(AVStream *st, AVCodecContext *avctx)
 int avformat_find_stream_info(AVFormatContext *ic, AVDictionary **options)
 {
     FFFormatContext *const si = ffformatcontext(ic);
-    int count = 0, ret = 0;
+    int count = 0, ret = 0, err;
     int64_t read_size;
     AVPacket *pkt1 = si->pkt;
     int64_t old_offset  = avio_tell(ic->pb);
@@ -2931,9 +2931,11 @@ int avformat_find_stream_info(AVFormatContext *ic, AVDictionary **options)
         }
     }
 
-    ret = compute_chapters_end(ic);
-    if (ret < 0)
+    err = compute_chapters_end(ic);
+    if (err < 0) {
+        ret = err;
         goto find_stream_info_err;
+    }
 
     /* update the stream parameters from the internal codec contexts */
     for (unsigned i = 0; i < ic->nb_streams; i++) {
