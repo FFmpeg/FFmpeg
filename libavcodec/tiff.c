bb3c50b46d50b8bf4f45d7ae8f24607aaf23acea
Andreas Rheinhardt <andreas.rheinhardt@outlook.com>
2024-06-02 06:11:46 +0200

    avcodec/tiff: Suppress unused variable warnings
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

--- libavcodec/tiff.c
+++ libavcodec/tiff.c
@@ -422,7 +422,7 @@ static void av_always_inline horizontal_fill(TiffContext *s,
             uint8_t shift = is_dng ? 0 : 16 - bpp;
             GetBitContext gb;
 
-            int ret = init_get_bits8(&gb, src, width);
+            av_unused int ret = init_get_bits8(&gb, src, width);
             av_assert1(ret >= 0);
             for (int i = 0; i < s->width; i++) {
                 dst16[i] = get_bits(&gb, bpp) << shift;
@@ -457,7 +457,7 @@ static void unpack_gray(TiffContext *s, AVFrame *p,
     GetBitContext gb;
     uint16_t *dst = (uint16_t *)(p->data[0] + lnum * p->linesize[0]);
 
-    int ret = init_get_bits8(&gb, src, width);
+    av_unused int ret = init_get_bits8(&gb, src, width);
     av_assert1(ret >= 0);
 
     for (int i = 0; i < s->width; i++) {
