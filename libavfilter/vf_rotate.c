c818250194c6f8d7773f668398786c4d731f2af6
Michael Niedermayer <michael@niedermayer.cc>
2024-06-03 19:51:49 +0200

    avfilter/vf_rotate: Check ff_draw_init2() return value
    Fixes: NULL pointer dereference
    Fixes: 3_343
    
    Found-by: De3mond
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit 9c9f095e30c196c0e3d510dc5300182ddb49a803)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavfilter/vf_rotate.c
+++ libavfilter/vf_rotate.c
@@ -288,7 +288,9 @@ static int config_props(AVFilterLink *outlink)
     double res;
     char *expr;
 
-    ff_draw_init2(&rot->draw, inlink->format, inlink->colorspace, inlink->color_range, 0);
+    ret = ff_draw_init2(&rot->draw, inlink->format, inlink->colorspace, inlink->color_range, 0);
+    if (ret < 0)
+        return ret;
     ff_draw_color(&rot->draw, &rot->color, rot->fillcolor);
 
     rot->hsub = pixdesc->log2_chroma_w;
