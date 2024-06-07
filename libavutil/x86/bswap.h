0231097d1bd6803a4724465797fcaddbe1fcc92d
Rémi Denis-Courmont <remi@remlab.net>
2024-06-07 19:43:09 +0300

    lavu/x86: remove GCC 4.4- stuff
    Since the C11 support is required, those GCC versions can no longer be
    supported anyhow. (Clang pretends to be GCC 4.4, but it looks like the
    code was intended for old GCC specifically.)

--- libavutil/x86/bswap.h
+++ libavutil/x86/bswap.h
@@ -57,16 +57,7 @@ static inline uint64_t av_const av_bswap64(uint64_t x)
 
 #elif HAVE_INLINE_ASM
 
-#if AV_GCC_VERSION_AT_MOST(4,0)
-#define av_bswap16 av_bswap16
-static av_always_inline av_const unsigned av_bswap16(unsigned x)
-{
-    __asm__("rorw $8, %w0" : "+r"(x));
-    return x;
-}
-#endif /* AV_GCC_VERSION_AT_MOST(4,0) */
-
-#if AV_GCC_VERSION_AT_MOST(4,4) || defined(__INTEL_COMPILER)
+#ifdef __INTEL_COMPILER
 #define av_bswap32 av_bswap32
 static av_always_inline av_const uint32_t av_bswap32(uint32_t x)
 {
@@ -82,7 +73,7 @@ static inline uint64_t av_const av_bswap64(uint64_t x)
     return x;
 }
 #endif
-#endif /* AV_GCC_VERSION_AT_MOST(4,4) */
+#endif /* __INTEL_COMPILER */
 
 #endif /* HAVE_INLINE_ASM */
 #endif /* AVUTIL_X86_BSWAP_H */
