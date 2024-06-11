96a63346eea3783d8e3bc04db33c4999e5d2dbeb
Michael Niedermayer <michael@niedermayer.cc>
2024-06-11 22:44:46 +0200

    avfilter/avf_showcwt: Check av_parse_video_rate() for failure
    Fixes: CID1539147 Unused value
    
    Sponsored-by: Sovereign Tech Fund
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit aab0c344c5d1d5b1020f87c62da3e523161a660f)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavfilter/avf_showcwt.c
+++ libavfilter/avf_showcwt.c
@@ -1000,6 +1000,8 @@ static int config_output(AVFilterLink *outlink)
     s->auto_frame_rate = av_make_q(inlink->sample_rate, s->hop_size);
     if (strcmp(s->rate_str, "auto")) {
         ret = av_parse_video_rate(&s->frame_rate, s->rate_str);
+        if (ret < 0)
+            return ret;
     } else {
         s->frame_rate = s->auto_frame_rate;
     }
