08383443ff695816297f94775ab793159ff38422
James Almer <jamrial@gmail.com>
2024-06-11 22:50:50 -0300

    avcodec/vorbisdec: don't use double intermediate in vorbisfloat2float
    Signed-off-by: James Almer <jamrial@gmail.com>

--- libavcodec/vorbisdec.c
+++ libavcodec/vorbisdec.c
@@ -181,11 +181,11 @@ static const char idx_err_str[] = "Index value %d out of range (0 - %d) for %s a
 
 static float vorbisfloat2float(unsigned val)
 {
-    double mant = val & 0x1fffff;
-    long exp    = (val & 0x7fe00000L) >> 21;
+    float mant = val & 0x1fffff;
+    int exp    = (val & 0x7fe00000) >> 21;
     if (val & 0x80000000)
         mant = -mant;
-    return ldexp(mant, exp - 20 - 768);
+    return ldexpf(mant, exp - 20 - 768);
 }
 
 
