47cdc9df8de3f84093f0beb50240d761d73ab44c
Michael Niedermayer <michael@niedermayer.cc>
2024-06-17 22:43:22 +0200

    avcodec/vc2enc: Fix overflows with storing large values
    Fixes: left shift of 1431634944 by 2 places cannot be represented in type 'int'
    Fixes: left shift of 1073741824 by 1 places cannot be represented in type 'int'
    Fixes: 69061/clusterfuzz-testcase-minimized-ffmpeg_AV_CODEC_ID_VC2_fuzzer-6325700826038272
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit af9935835335cae1ae5a4ec7fc14c1b5e25c1f2d)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/vc2enc.c
+++ libavcodec/vc2enc.c
@@ -183,7 +183,9 @@ typedef struct VC2EncContext {
 static av_always_inline void put_vc2_ue_uint(PutBitContext *pb, uint32_t val)
 {
     int i;
-    int pbits = 0, bits = 0, topbit = 1, maxval = 1;
+    int bits = 0;
+    unsigned topbit = 1, maxval = 1;
+    uint64_t pbits = 0;
 
     if (!val++) {
         put_bits(pb, 1, 1);
@@ -200,12 +202,13 @@ static av_always_inline void put_vc2_ue_uint(PutBitContext *pb, uint32_t val)
 
     for (i = 0; i < bits; i++) {
         topbit >>= 1;
+        av_assert2(pbits <= UINT64_MAX>>3);
         pbits <<= 2;
         if (val & topbit)
             pbits |= 0x1;
     }
 
-    put_bits(pb, bits*2 + 1, (pbits << 1) | 1);
+    put_bits64(pb, bits*2 + 1, (pbits << 1) | 1);
 }
 
 static av_always_inline int count_vc2_ue_uint(uint32_t val)
