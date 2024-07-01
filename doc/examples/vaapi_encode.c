16a0e979e732bbacc8b68ca87226afb27f824b51
Michael Niedermayer <michael@niedermayer.cc>
2024-07-02 01:47:33 +0200

    doc/examples/vaapi_encode: Try to check fwrite() for failure
    Fixes: CID1604548 Unused value
    
    Sponsored-by: Sovereign Tech Fund
    Reviewed-by: "Xiang, Haihao" <haihao.xiang-at-intel.com@ffmpeg.org>
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit 3e4bfff21192aed328c906c85424737128b108f1)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- doc/examples/vaapi_encode.c
+++ doc/examples/vaapi_encode.c
@@ -91,6 +91,10 @@ static int encode_write(AVCodecContext *avctx, AVFrame *frame, FILE *fout)
         enc_pkt->stream_index = 0;
         ret = fwrite(enc_pkt->data, enc_pkt->size, 1, fout);
         av_packet_unref(enc_pkt);
+        if (ret != enc_pkt->size) {
+            ret = AVERROR(errno);
+            break;
+        }
     }
 
 end:
