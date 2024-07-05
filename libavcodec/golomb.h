12c1072179bc936bd33cd63df0c7f9a55d9b8270
Michael Niedermayer <michael@niedermayer.cc>
2024-07-05 02:21:45 +0200

    avcodec/golomb: Document return for get_ur_golomb_jpegls() and get_sr_golomb_flac()
    Found while reviewing code related to CID1604409 Overflowed return value
    
    Sponsored-by: Sovereign Tech Fund
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit 7cf5b83f6fa367f99aefc1321bafc0a7e8db33cd)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/golomb.h
+++ libavcodec/golomb.h
@@ -301,6 +301,8 @@ static inline int get_ur_golomb(GetBitContext *gb, int k, int limit,
 
 /**
  * read unsigned golomb rice code (jpegls).
+ *
+ * @returns -1 on error
  */
 static inline int get_ur_golomb_jpegls(GetBitContext *gb, int k, int limit,
                                        int esc_len)
@@ -373,6 +375,8 @@ static inline int get_sr_golomb(GetBitContext *gb, int k, int limit,
 
 /**
  * read signed golomb rice code (flac).
+ *
+ * @returns INT_MIN on error
  */
 static inline int get_sr_golomb_flac(GetBitContext *gb, int k, int limit,
                                      int esc_len)
