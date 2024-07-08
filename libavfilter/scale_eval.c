ad9df8bcfebc1085cb8b42dae9ab688af824cdab
Michael Niedermayer <michael@niedermayer.cc>
2024-07-08 22:01:29 +0200

    avfilter/scale_eval: Use 64bit, check values in ff_scale_adjust_dimensions()
    Found by reviewing CID1513722 Operands don't affect result
    
    Sponsored-by: Sovereign Tech Fund
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavfilter/scale_eval.c
+++ libavfilter/scale_eval.c
@@ -114,7 +114,7 @@ int ff_scale_adjust_dimensions(AVFilterLink *inlink,
     int *ret_w, int *ret_h,
     int force_original_aspect_ratio, int force_divisible_by)
 {
-    int w, h;
+    int64_t w, h;
     int factor_w, factor_h;
 
     w = *ret_w;
@@ -149,9 +149,9 @@ int ff_scale_adjust_dimensions(AVFilterLink *inlink,
      * unless force_divisible_by is defined as well */
     if (force_original_aspect_ratio) {
         // Including force_divisible_by here rounds to the nearest multiple of it.
-        int tmp_w = av_rescale(h, inlink->w, inlink->h * (int64_t)force_divisible_by)
+        int64_t tmp_w = av_rescale(h, inlink->w, inlink->h * (int64_t)force_divisible_by)
                     * force_divisible_by;
-        int tmp_h = av_rescale(w, inlink->h, inlink->w * (int64_t)force_divisible_by)
+        int64_t tmp_h = av_rescale(w, inlink->h, inlink->w * (int64_t)force_divisible_by)
                     * force_divisible_by;
 
         if (force_original_aspect_ratio == 1) {
@@ -172,6 +172,9 @@ int ff_scale_adjust_dimensions(AVFilterLink *inlink,
              }
         }
     }
+
+    if ((int32_t)w != w || (int32_t)h != h)
+        return AVERROR(EINVAL);
 
     *ret_w = w;
     *ret_h = h;
