629f3f395f48abd9cdf60fd93ea0a6e020d6d2ac
Michael Niedermayer <michael@niedermayer.cc>
2024-07-13 09:16:48 +0200

    avformat/lmlm4: Eliminate some AVERROR(EIO)
    Found by code review related to CID732224 Overflowed constant
    
    Sponsored-by: Sovereign Tech Fund
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit 191a685010319cb0d248771574c7c61d76e4eb95)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavformat/lmlm4.c
+++ libavformat/lmlm4.c
@@ -94,15 +94,15 @@ static int lmlm4_read_packet(AVFormatContext *s, AVPacket *pkt)
 
     if (frame_type > LMLM4_MPEG1L2 || frame_type == LMLM4_INVALID) {
         av_log(s, AV_LOG_ERROR, "invalid or unsupported frame_type\n");
-        return AVERROR(EIO);
+        return AVERROR_INVALIDDATA;
     }
     if (packet_size > LMLM4_MAX_PACKET_SIZE || packet_size<=8) {
         av_log(s, AV_LOG_ERROR, "packet size %d is invalid\n", packet_size);
-        return AVERROR(EIO);
+        return AVERROR_INVALIDDATA;
     }
 
     if ((ret = av_get_packet(pb, pkt, frame_size)) <= 0)
-        return AVERROR(EIO);
+        return ret < 0 ? ret : AVERROR(EIO);
 
     avio_skip(pb, padding);
 
