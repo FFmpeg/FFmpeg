d8d288479d3431d65318d957aab710b13714fc05
Michael Niedermayer <michael@niedermayer.cc>
2024-07-16 21:31:21 +0200

    avformat/mxfdec: Reorder elements of expression in bisect loop
    Fixes: signed integer overflow: 9223372036854775807 - -1 cannot be represented in type 'long'
    Fixes: 68578/clusterfuzz-testcase-minimized-ffmpeg_dem_MXF_fuzzer-6032171648221184
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavformat/mxfdec.c
+++ libavformat/mxfdec.c
@@ -3821,8 +3821,7 @@ static int mxf_get_next_track_edit_unit(MXFContext *mxf, MXFTrack *track, int64_
 
     a = -1;
     b = track->original_duration;
-
-    while (b - a > 1) {
+    while (b - 1 > a) {
         m = (a + b) >> 1;
         if (mxf_edit_unit_absolute_offset(mxf, t, m, track->edit_rate, NULL, &offset, NULL, 0) < 0)
             return -1;
