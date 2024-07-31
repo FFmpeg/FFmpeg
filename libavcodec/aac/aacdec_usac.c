a518b5540de469ac5d4f96e820462e5ce519c2db
Michael Niedermayer <michael@niedermayer.cc>
2024-07-31 20:03:12 +0200

    avcodec/aac/aacdec_usac: Dont leave type at a invalid value
    Fixes: Assertion 0 failed at libavcodec/aac/aacdec_usac.c:1646
    Fixes: 70541/clusterfuzz-testcase-minimized-ffmpeg_AV_CODEC_ID_AAC_LATM_fuzzer-5190889543106560
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    Reviewed-by: Lynne <dev@lynne.ee>
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/aac/aacdec_usac.c
+++ libavcodec/aac/aacdec_usac.c
@@ -265,6 +265,7 @@ static int decode_usac_extension(AACDecContext *ac, AACUsacElemConfig *e,
         /* No configuration needed - fallthrough (len should be 0) */
     default:
         skip_bits(gb, 8*ext_config_len);
+        e->ext.type = ID_EXT_ELE_FILL;
         break;
     };
 
