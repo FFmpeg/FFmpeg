009d2a811339d485c15937c7b3a3353f130a125a
Michael Niedermayer <michael@niedermayer.cc>
2024-08-04 22:10:48 +0200

    avcodec/shorten: clear padding
    Fixes: use-of-uninitialized-value
    Fixes: 70854/clusterfuzz-testcase-minimized-ffmpeg_AV_CODEC_ID_SHORTEN_fuzzer-5533480570650624
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit e44349ee88418ac16051bbc9231c1bfdc25d3504)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/shorten.c
+++ libavcodec/shorten.c
@@ -562,6 +562,7 @@ static int shorten_decode_frame(AVCodecContext *avctx, AVFrame *frame,
     buf               = &s->bitstream[s->bitstream_index];
     buf_size         += s->bitstream_size;
     s->bitstream_size = buf_size;
+    memset(buf + buf_size, 0, AV_INPUT_BUFFER_PADDING_SIZE);
 
     /* do not decode until buffer has at least max_framesize bytes or
      * the end of the file has been reached */
