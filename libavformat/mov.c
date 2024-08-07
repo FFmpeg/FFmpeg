b534e402d8c45f22feafe70d6b7d1c5a48c4d411
Kacper Michajłow <kasper93@gmail.com>
2024-08-07 16:09:20 +0200

    avformat/mov: ensure required number of bytes is read
    Fixes: use-of-uninitialized-value
    
    Found by OSS-Fuzz.
    
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavformat/mov.c
+++ libavformat/mov.c
@@ -7096,7 +7096,7 @@ static int mov_read_free(MOVContext *c, AVIOContext *pb, MOVAtom atom)
     if (atom.size < 8)
         return 0;
 
-    ret = avio_read(pb, content, FFMIN(sizeof(content), atom.size));
+    ret = ffio_read_size(pb, content, FFMIN(sizeof(content), atom.size));
     if (ret < 0)
         return ret;
 
