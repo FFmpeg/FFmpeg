41307ff3e9384c51d646bff7e3dcf0d554098a8f
James Almer <jamrial@gmail.com>
2024-08-10 21:31:16 -0300

    avfilter/video: don't zero allocated buffers if memory poisoning is used
    Same as in avcodec/get_buffer.c
    Should help in debugging use of uninitialized memory.
    
    Signed-off-by: James Almer <jamrial@gmail.com>

--- libavfilter/video.c
+++ libavfilter/video.c
@@ -71,8 +71,10 @@ AVFrame *ff_default_get_video_buffer2(AVFilterLink *link, int w, int h, int alig
     }
 
     if (!li->frame_pool) {
-        li->frame_pool = ff_frame_pool_video_init(av_buffer_allocz, w, h,
-                                                  link->format, align);
+        li->frame_pool = ff_frame_pool_video_init(CONFIG_MEMORY_POISONING
+                                                     ? NULL
+                                                     : av_buffer_allocz,
+                                                  w, h, link->format, align);
         if (!li->frame_pool)
             return NULL;
     } else {
@@ -86,8 +88,10 @@ AVFrame *ff_default_get_video_buffer2(AVFilterLink *link, int w, int h, int alig
             pool_format != link->format || pool_align != align) {
 
             ff_frame_pool_uninit(&li->frame_pool);
-            li->frame_pool = ff_frame_pool_video_init(av_buffer_allocz, w, h,
-                                                      link->format, align);
+            li->frame_pool = ff_frame_pool_video_init(CONFIG_MEMORY_POISONING
+                                                         ? NULL
+                                                         : av_buffer_allocz,
+                                                      w, h, link->format, align);
             if (!li->frame_pool)
                 return NULL;
         }
