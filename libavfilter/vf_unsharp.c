2e370805da94ee5a8b3959b0635a3fbdb033125e
Zhao Zhili <zhilizhao@tencent.com>
2024-08-14 21:36:14 +0800

    avfilter/unsharp: Merge header into .c
    It was shared with opencl implementation.
    
    Signed-off-by: Zhao Zhili <zhilizhao@tencent.com>

--- libavfilter/vf_unsharp.c
+++ libavfilter/vf_unsharp.c
@@ -44,7 +44,39 @@
 #include "libavutil/mem.h"
 #include "libavutil/opt.h"
 #include "libavutil/pixdesc.h"
-#include "unsharp.h"
+
+#define MIN_MATRIX_SIZE 3
+#define MAX_MATRIX_SIZE 63
+
+typedef struct UnsharpFilterParam {
+    int msize_x;                             ///< matrix width
+    int msize_y;                             ///< matrix height
+    int amount;                              ///< effect amount
+    int steps_x;                             ///< horizontal step count
+    int steps_y;                             ///< vertical step count
+    int scalebits;                           ///< bits to shift pixel
+    int32_t halfscale;                       ///< amount to add to pixel
+    uint32_t *sr;        ///< finite state machine storage within a row
+    uint32_t **sc;       ///< finite state machine storage across rows
+} UnsharpFilterParam;
+
+typedef struct UnsharpContext {
+    const AVClass *class;
+    int lmsize_x, lmsize_y, cmsize_x, cmsize_y;
+    int amsize_x, amsize_y;
+    float lamount, camount;
+    float aamount;
+    UnsharpFilterParam luma;   ///< luma parameters (width, height, amount)
+    UnsharpFilterParam chroma; ///< chroma parameters (width, height, amount)
+    UnsharpFilterParam alpha;  ///< alpha parameters (width, height, amount)
+    int hsub, vsub;
+    int nb_planes;
+    int bitdepth;
+    int bps;
+    int nb_threads;
+    int (* apply_unsharp)(AVFilterContext *ctx, AVFrame *in, AVFrame *out);
+    int (* unsharp_slice)(AVFilterContext *ctx, void *arg, int jobnr, int nb_jobs);
+} UnsharpContext;
 
 typedef struct TheadData {
     UnsharpFilterParam *fp;
