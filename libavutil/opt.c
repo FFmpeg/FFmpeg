4a5bb84515bab769b7702277abc7d07ea4c4d855
Anton Khirnov <anton@khirnov.net>
2024-08-17 16:29:30 +0200

    lavu/opt: forward av_opt_get_video_rate() to av_opt_get_q()
    The two functions are exactly the same.

--- libavutil/opt.c
+++ libavutil/opt.c
@@ -1277,18 +1277,7 @@ int av_opt_get_image_size(void *obj, const char *name, int search_flags, int *w_
 
 int av_opt_get_video_rate(void *obj, const char *name, int search_flags, AVRational *out_val)
 {
-    int64_t intnum = 1;
-    double     num = 1;
-    int   ret, den = 1;
-
-    if ((ret = get_number(obj, name, &num, &den, &intnum, search_flags)) < 0)
-        return ret;
-
-    if (num == 1.0 && (int)intnum == intnum)
-        *out_val = (AVRational) { intnum, den };
-    else
-        *out_val = av_d2q(num * intnum / den, 1 << 24);
-    return 0;
+    return av_opt_get_q(obj, name, search_flags, out_val);
 }
 
 static int get_format(void *obj, const char *name, int search_flags, int *out_fmt,
