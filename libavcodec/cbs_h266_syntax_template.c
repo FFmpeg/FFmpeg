9e1682761f9c2478722ac0204253d2abdba41aed
Marvin Scholz <epirat07@gmail.com>
2024-08-31 23:12:13 +0200

    avcodec/cbs_h266: Fix copy paste mistake
    The us macro expect the range_max here, which seems should be
    MAX_UINT_BITS(hlen) here.
    
    Fix CID1618757 Copy-paste error

--- libavcodec/cbs_h266_syntax_template.c
+++ libavcodec/cbs_h266_syntax_template.c
@@ -1208,7 +1208,7 @@ static int FUNC(sps)(CodedBitstreamContext *ctx, RWContext *rw,
                            win_top_edge_ctus > current->sps_subpic_ctu_top_left_y[i]
                                ? win_top_edge_ctus - current->sps_subpic_ctu_top_left_y[i]
                                : 0,
-                           MAX_UINT_BITS(wlen), 1, i);
+                           MAX_UINT_BITS(hlen), 1, i);
                     } else {
                         infer(sps_subpic_height_minus1[i],
                               tmp_height_val -
