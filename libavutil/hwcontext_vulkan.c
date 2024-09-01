3415e0533f97e8c7f9df4b7c8564d00ead88b099
Lynne <dev@lynne.ee>
2024-09-01 03:35:32 +0000

    hwcontext_vulkan: disable more false positive validation checks
    Both of these are fundamentally incompatible with video decoding.
    
    The third one can be tracked in the following validation layer issue:
    https://github.com/KhronosGroup/Vulkan-ValidationLayers/issues/6627

--- libavutil/hwcontext_vulkan.c
+++ libavutil/hwcontext_vulkan.c
@@ -457,6 +457,9 @@ static VkBool32 VKAPI_CALL vk_dbg_callback(VkDebugUtilsMessageSeverityFlagBitsEX
 
     /* Ignore false positives */
     switch (data->messageIdNumber) {
+    case 0x086974c1: /* BestPractices-vkCreateCommandPool-command-buffer-reset */
+    case 0xfd92477a: /* BestPractices-vkAllocateMemory-small-allocation */
+    case 0x618ab1e7: /* VUID-VkImageViewCreateInfo-usage-02275 */
     case 0x30f4ac70: /* VUID-VkImageCreateInfo-pNext-06811 */
         return VK_FALSE;
     default:
