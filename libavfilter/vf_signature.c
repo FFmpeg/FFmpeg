e7126d96489bbfe4164550cb1253839ae63d724c
Marvin Scholz <epirat07@gmail.com>
2024-09-08 20:56:38 +0200

    lavfi/vf_signature: use av_err2str to simplify code
    No need to explicitly specify the buffer here as it is only
    ever passed to av_log, so av_err2str can be used.

--- libavfilter/vf_signature.c
+++ libavfilter/vf_signature.c
@@ -386,9 +386,7 @@ static int xml_export(AVFilterContext *ctx, StreamContext *sc, const char* filen
     f = avpriv_fopen_utf8(filename, "w");
     if (!f) {
         int err = AVERROR(EINVAL);
-        char buf[128];
-        av_strerror(err, buf, sizeof(buf));
-        av_log(ctx, AV_LOG_ERROR, "cannot open xml file %s: %s\n", filename, buf);
+        av_log(ctx, AV_LOG_ERROR, "cannot open xml file %s: %s\n", filename, av_err2str(err));
         return err;
     }
 
@@ -500,9 +498,7 @@ static int binary_export(AVFilterContext *ctx, StreamContext *sc, const char* fi
     f = avpriv_fopen_utf8(filename, "wb");
     if (!f) {
         int err = AVERROR(EINVAL);
-        char buf[128];
-        av_strerror(err, buf, sizeof(buf));
-        av_log(ctx, AV_LOG_ERROR, "cannot open file %s: %s\n", filename, buf);
+        av_log(ctx, AV_LOG_ERROR, "cannot open file %s: %s\n", filename, av_err2str(err));
         av_freep(&buffer);
         return err;
     }
