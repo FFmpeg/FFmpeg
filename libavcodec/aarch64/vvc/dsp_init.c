0dcf204e5d7df388e477fd40c6d4eb4609c7c671
Zhao Zhili <zhilizhao@tencent.com>
2024-09-11 16:42:54 +0800

    aarch64/vvc: Add put_epel_h i8mm
    put_chroma_h_8_4x4_c:                                    0.4 ( 1.00x)
    put_chroma_h_8_4x4_neon:                                 0.0 ( 0.00x)
    put_chroma_h_8_4x4_i8mm:                                 0.1 ( 2.67x)
    put_chroma_h_8_8x8_c:                                    1.6 ( 1.00x)
    put_chroma_h_8_8x8_neon:                                 0.1 (11.00x)
    put_chroma_h_8_8x8_i8mm:                                 0.1 (11.00x)
    put_chroma_h_8_16x16_c:                                  6.9 ( 1.00x)
    put_chroma_h_8_16x16_neon:                               1.1 ( 6.00x)
    put_chroma_h_8_16x16_i8mm:                               0.7 (10.62x)
    put_chroma_h_8_32x32_c:                                 27.6 ( 1.00x)
    put_chroma_h_8_32x32_neon:                               4.7 ( 5.95x)
    put_chroma_h_8_32x32_i8mm:                               4.4 ( 6.28x)
    put_chroma_h_8_64x64_c:                                116.2 ( 1.00x)
    put_chroma_h_8_64x64_neon:                              19.1 ( 6.07x)
    put_chroma_h_8_64x64_i8mm:                              17.1 ( 6.77x)
    put_chroma_h_8_128x128_c:                              466.6 ( 1.00x)
    put_chroma_h_8_128x128_neon:                            81.4 ( 5.73x)
    put_chroma_h_8_128x128_i8mm:                            71.7 ( 6.51x)

--- libavcodec/aarch64/vvc/dsp_init.c
+++ libavcodec/aarch64/vvc/dsp_init.c
@@ -127,6 +127,13 @@ void ff_vvc_dsp_init_aarch64(VVCDSPContext *const c, const int bd)
             c->inter.put[0][4][1][1] = ff_vvc_put_qpel_hv32_8_neon_i8mm;
             c->inter.put[0][5][1][1] = ff_vvc_put_qpel_hv64_8_neon_i8mm;
             c->inter.put[0][6][1][1] = ff_vvc_put_qpel_hv128_8_neon_i8mm;
+
+            c->inter.put[1][1][0][1] = ff_vvc_put_epel_h4_8_neon_i8mm;
+            c->inter.put[1][2][0][1] = ff_vvc_put_epel_h8_8_neon_i8mm;
+            c->inter.put[1][3][0][1] = ff_vvc_put_epel_h16_8_neon_i8mm;
+            c->inter.put[1][4][0][1] = ff_vvc_put_epel_h32_8_neon_i8mm;
+            c->inter.put[1][5][0][1] = ff_vvc_put_epel_h64_8_neon_i8mm;
+            c->inter.put[1][6][0][1] = ff_vvc_put_epel_h128_8_neon_i8mm;
         }
     } else if (bd == 10) {
         c->alf.filter[LUMA] = alf_filter_luma_10_neon;
