d05bbbd2965e0aacd0e498b5dc713effdf46ca21
Michael Niedermayer <michael@niedermayer.cc>
2024-09-12 20:08:42 +0200

    swscale/swscale: Use unsigned operation to avoid undefined behavior
    I have not checked that the constant is correct, this just fixes the undefined behavior
    
    Fixes: signed integer overflow: -646656 * 3517 cannot be represented in type 'int
    Fixes: 70559/clusterfuzz-testcase-minimized-ffmpeg_SWS_fuzzer-5209368631508992
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit 44c5641ae82387fcfce94820f5b53ce8e9dcd27f)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libswscale/swscale.c
+++ libswscale/swscale.c
@@ -222,7 +222,7 @@ static void lumRangeFromJpeg16_c(int16_t *_dst, int width)
     int i;
     int32_t *dst = (int32_t *) _dst;
     for (i = 0; i < width; i++)
-        dst[i] = (dst[i]*(14071/4) + (33561947<<4)/4)>>12;
+        dst[i] = ((int)(dst[i]*(14071U/4) + (33561947<<4)/4)) >> 12;
 }
 
 
