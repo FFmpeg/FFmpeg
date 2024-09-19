47b03916d2fdb6392ff6f1bccd48e4012eac6ed7
Michael Niedermayer <michael@niedermayer.cc>
2024-09-19 21:57:09 +0200

    avcodec/jfdctint_template: use unsigned z* in row_fdct()
    Fixes: signed integer overflow: 856827136 + 2123580416 cannot be represented in type 'int'
    Fixes: 70772/clusterfuzz-testcase-minimized-ffmpeg_AV_CODEC_ID_PRORES_KS_fuzzer-5180569961431040
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit f27c8b04d3059fa538db8f2db6503cbb586eb3ad)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/jfdctint_template.c
+++ libavcodec/jfdctint_template.c
@@ -188,7 +188,7 @@ static av_always_inline void FUNC(row_fdct)(int16_t *data)
 {
   int tmp0, tmp1, tmp2, tmp3, tmp4, tmp5, tmp6, tmp7;
   int tmp10, tmp11, tmp12, tmp13;
-  int z1, z2, z3, z4, z5;
+  unsigned z1, z2, z3, z4, z5;
   int16_t *dataptr;
   int ctr;
 
