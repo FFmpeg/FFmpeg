9a0d56f8f796f02800d20c39b07c6b00957553f9
Michael Niedermayer <michael@niedermayer.cc>
2024-10-01 22:04:58 +0200

    avcodec/ffv1enc: Correct error message about unsupported version
    Sponsored-by: Sovereign Tech Fund
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit 556c767786e9e3c072f7666d60a68a31a3400438)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/ffv1enc.c
+++ libavcodec/ffv1enc.c
@@ -549,7 +549,7 @@ static av_cold int encode_init(AVCodecContext *avctx)
         s->version = FFMAX(s->version, 3);
 
     if ((s->version == 2 || s->version>3) && avctx->strict_std_compliance > FF_COMPLIANCE_EXPERIMENTAL) {
-        av_log(avctx, AV_LOG_ERROR, "Version 2 needed for requested features but version 2 is experimental and not enabled\n");
+        av_log(avctx, AV_LOG_ERROR, "Version 2 or 4 needed for requested features but version 2 or 4 is experimental and not enabled\n");
         return AVERROR_INVALIDDATA;
     }
 
