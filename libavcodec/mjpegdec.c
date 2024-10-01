6696a9b8bd2eacf90d7bd199d37115ba12def793
Ramiro Polla <ramiro.polla@gmail.com>
2024-10-01 20:50:05 +0200

    avcodec/mjpegdec: ignore APPx stubs unless AV_EF_EXPLODE is set
    Consider APPx fields that are too short to contain an id field (32-bit)
    as stubs, and ignore them if AV_EF_EXPLODE is not set.
    
    This has been seen in the MJPEG output from some webcams (such as the
    Logitech C270 and C920) and the JPEG images embedded in DNG images
    from the Pentax K-1 camera.

--- libavcodec/mjpegdec.c
+++ libavcodec/mjpegdec.c
@@ -1859,20 +1859,22 @@ static int mjpeg_decode_app(MJpegDecodeContext *s)
     int len, id, i;
 
     len = get_bits(&s->gb, 16);
-    if (len < 6) {
-        if (s->bayer) {
-            // Pentax K-1 (digital camera) JPEG images embedded in DNG images contain unknown APP0 markers
-            av_log(s->avctx, AV_LOG_WARNING, "skipping APPx (len=%"PRId32") for bayer-encoded image\n", len);
-            skip_bits(&s->gb, len);
-            return 0;
-        } else
+    if (len < 2)
+        return AVERROR_INVALIDDATA;
+    len -= 2;
+
+    if (len < 4) {
+        if (s->avctx->err_recognition & AV_EF_EXPLODE)
             return AVERROR_INVALIDDATA;
+        av_log(s->avctx, AV_LOG_VERBOSE, "skipping APPx stub (len=%" PRId32 ")\n", len);
+        goto out;
     }
+
     if (8 * len > get_bits_left(&s->gb))
         return AVERROR_INVALIDDATA;
 
     id   = get_bits_long(&s->gb, 32);
-    len -= 6;
+    len -= 4;
 
     if (s->avctx->debug & FF_DEBUG_STARTCODE)
         av_log(s->avctx, AV_LOG_DEBUG, "APPx (%s / %8X) len=%d\n",
