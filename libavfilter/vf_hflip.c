6db8279325a473a8649a2c524cdd5177a5a76cee
Anton Khirnov <anton@khirnov.net>
2024-10-07 11:01:13 +0200

    lavfi/vf_hflip: switch to query_func2()

--- libavfilter/vf_hflip.c
+++ libavfilter/vf_hflip.c
@@ -37,7 +37,9 @@
 #include "libavutil/intreadwrite.h"
 #include "libavutil/imgutils.h"
 
-static int query_formats(AVFilterContext *ctx)
+static int query_formats(const AVFilterContext *ctx,
+                         AVFilterFormatsConfig **cfg_in,
+                         AVFilterFormatsConfig **cfg_out)
 {
     AVFilterFormats *pix_fmts = NULL;
     const AVPixFmtDescriptor *desc;
@@ -52,7 +54,7 @@ static int query_formats(AVFilterContext *ctx)
             return ret;
     }
 
-    return ff_set_common_formats(ctx, pix_fmts);
+    return ff_set_common_formats2(ctx, cfg_in, cfg_out, pix_fmts);
 }
 
 static int config_props(AVFilterLink *inlink)
@@ -150,6 +152,6 @@ const AVFilter ff_vf_hflip = {
     .priv_size     = sizeof(FlipContext),
     FILTER_INPUTS(avfilter_vf_hflip_inputs),
     FILTER_OUTPUTS(ff_video_default_filterpad),
-    FILTER_QUERY_FUNC(query_formats),
+    FILTER_QUERY_FUNC2(query_formats),
     .flags         = AVFILTER_FLAG_SLICE_THREADS | AVFILTER_FLAG_SUPPORT_TIMELINE_GENERIC,
 };
