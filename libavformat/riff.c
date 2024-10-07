f6878a9b0975b175f1f834156462ae7170186434
James Almer <jamrial@gmail.com>
2024-10-07 00:21:52 -0300

    avformat/riff: map v308 fourcc to RAWVIDEO decoder
    There's no need to keep using a custom decoder for this pixel format.
    md5 values change because the nut container now reports rawvideo as encoder.
    
    Signed-off-by: James Almer <jamrial@gmail.com>

--- libavformat/riff.c
+++ libavformat/riff.c
@@ -296,6 +296,7 @@ const AVCodecTag ff_codec_bmp_tags[] = {
     { AV_CODEC_ID_RAWVIDEO,     MKTAG('I', '4', 'C', 'B') },
     { AV_CODEC_ID_RAWVIDEO,     MKTAG('I', '0', 'F', 'L') },
     { AV_CODEC_ID_RAWVIDEO,     MKTAG('I', '0', 'F', 'B') },
+    { AV_CODEC_ID_RAWVIDEO,     MKTAG('v', '3', '0', '8') },
     { AV_CODEC_ID_RAWVIDEO,     MKTAG('v', '4', '0', '8') },
     { AV_CODEC_ID_RAWVIDEO,     MKTAG('y', '4', '0', '8') },
     { AV_CODEC_ID_FRWU,         MKTAG('F', 'R', 'W', 'U') },
