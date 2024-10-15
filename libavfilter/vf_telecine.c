14ece0bf589e60e05f8a07c7e42cd0b43724f139
Anton Khirnov <anton@khirnov.net>
2024-10-15 11:24:35 +0200

    lavfi/vf_telecine: convert to query_func2()

--- libavfilter/vf_telecine.c
+++ libavfilter/vf_telecine.c
@@ -99,13 +99,16 @@ static av_cold int init(AVFilterContext *ctx)
     return 0;
 }
 
-static int query_formats(AVFilterContext *ctx)
+static int query_formats(const AVFilterContext *ctx,
+                         AVFilterFormatsConfig **cfg_in,
+                         AVFilterFormatsConfig **cfg_out)
 {
     int reject_flags = AV_PIX_FMT_FLAG_BITSTREAM |
                        AV_PIX_FMT_FLAG_HWACCEL   |
                        AV_PIX_FMT_FLAG_PAL;
 
-    return ff_set_common_formats(ctx, ff_formats_pixdesc_filter(0, reject_flags));
+    return ff_set_common_formats2(ctx, cfg_in, cfg_out,
+                                  ff_formats_pixdesc_filter(0, reject_flags));
 }
 
 static int config_input(AVFilterLink *inlink)
@@ -326,5 +329,5 @@ const AVFilter ff_vf_telecine = {
     .uninit        = uninit,
     FILTER_INPUTS(telecine_inputs),
     FILTER_OUTPUTS(telecine_outputs),
-    FILTER_QUERY_FUNC(query_formats),
+    FILTER_QUERY_FUNC2(query_formats),
 };
