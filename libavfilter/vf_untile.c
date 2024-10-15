223327d51a3602963585981470644156431ea9a1
Anton Khirnov <anton@khirnov.net>
2024-10-15 11:24:35 +0200

    lavfi/vf_untile: convert to query_func2()

--- libavfilter/vf_untile.c
+++ libavfilter/vf_untile.c
@@ -61,13 +61,16 @@ static av_cold int init(AVFilterContext *ctx)
     return 0;
 }
 
-static int query_formats(AVFilterContext *ctx)
+static int query_formats(const AVFilterContext *ctx,
+                         AVFilterFormatsConfig **cfg_in,
+                         AVFilterFormatsConfig **cfg_out)
 {
     int reject_flags = AV_PIX_FMT_FLAG_HWACCEL   |
                        AV_PIX_FMT_FLAG_BITSTREAM |
                        FF_PIX_FMT_FLAG_SW_FLAT_SUB;
 
-    return ff_set_common_formats(ctx, ff_formats_pixdesc_filter(0, reject_flags));
+    return ff_set_common_formats2(ctx, cfg_in, cfg_out,
+                                  ff_formats_pixdesc_filter(0, reject_flags));
 }
 
 static int config_output(AVFilterLink *outlink)
@@ -182,6 +185,6 @@ const AVFilter ff_vf_untile = {
     .priv_size     = sizeof(UntileContext),
     FILTER_INPUTS(ff_video_default_filterpad),
     FILTER_OUTPUTS(untile_outputs),
-    FILTER_QUERY_FUNC(query_formats),
+    FILTER_QUERY_FUNC2(query_formats),
     .priv_class    = &untile_class,
 };
