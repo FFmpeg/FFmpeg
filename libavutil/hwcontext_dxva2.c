c76d92b2e068cf0a415b5bd19101657aff0ca730
James Almer <jamrial@gmail.com>
2024-10-25 23:56:27 -0300

    libavutil/hwcontext_dxva2: use the Y216 pixel format
    Signed-off-by: James Almer <jamrial@gmail.com>

--- libavutil/hwcontext_dxva2.c
+++ libavutil/hwcontext_dxva2.c
@@ -93,7 +93,7 @@ static const struct {
     { MKTAG('Y', '2', '1', '0'), AV_PIX_FMT_Y210 },
     { MKTAG('Y', '4', '1', '0'), AV_PIX_FMT_XV30 },
     { MKTAG('P', '0', '1', '6'), AV_PIX_FMT_P012 },
-    { MKTAG('Y', '2', '1', '6'), AV_PIX_FMT_Y212 },
+    { MKTAG('Y', '2', '1', '6'), AV_PIX_FMT_Y216 },
     { MKTAG('Y', '4', '1', '6'), AV_PIX_FMT_XV36 },
     { D3DFMT_P8,                 AV_PIX_FMT_PAL8 },
     { D3DFMT_A8R8G8B8,           AV_PIX_FMT_BGRA },
