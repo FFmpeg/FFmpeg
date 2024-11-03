c0b5b173a3c888745654444e2e3bb61d6fdbb2c5
Michael Niedermayer <michael@niedermayer.cc>
2024-11-03 20:18:36 +0100

    avformat/mccdec: Initialize and check rate.den
    Fixes: Assertion c > 0 failed at libavutil/mathematics.c:61
    Fixes: use-of-uninitialized-value
    Fixes: 42537627/clusterfuzz-testcase-minimized-fuzzer_protocol_memory-5939605805793280
    
    Found-by: ossfuzz
    Reported-by: Kacper Michajlow
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit bf8e43083f68c383b9d905d2c8c791ac33ecc7bc)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavformat/mccdec.c
+++ libavformat/mccdec.c
@@ -92,7 +92,7 @@ static int mcc_read_header(AVFormatContext *s)
 {
     MCCContext *mcc = s->priv_data;
     AVStream *st = avformat_new_stream(s, NULL);
-    AVRational rate;
+    AVRational rate = {0};
     int64_t ts, pos;
     uint8_t out[4096];
     char line[4096];
@@ -138,7 +138,7 @@ static int mcc_read_header(AVFormatContext *s)
             continue;
         }
 
-        if (av_sscanf(line, "%d:%d:%d:%d", &hh, &mm, &ss, &fs) != 4)
+        if (av_sscanf(line, "%d:%d:%d:%d", &hh, &mm, &ss, &fs) != 4 || rate.den <= 0)
             continue;
 
         ts = av_sat_add64(av_rescale(hh * 3600LL + mm * 60LL + ss, rate.num, rate.den), fs);
