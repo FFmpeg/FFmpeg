4ec767abcc1036a424e4b30ef6d0d3b180d4eed4
Nuo Mi <nuomi2021@gmail.com>
2024-11-23 17:32:36 +0800

    avcodec/vvcdec: misc, reformat inter_data()

--- libavcodec/vvc/ctu.c
+++ libavcodec/vvc/ctu.c
@@ -1779,7 +1779,7 @@ static int inter_data(VVCLocalContext *lc)
 
     if (pu->general_merge_flag) {
         ret = hls_merge_data(lc);
-    } else if (cu->pred_mode == MODE_IBC){
+    } else if (cu->pred_mode == MODE_IBC) {
         ret = mvp_data_ibc(lc);
     } else {
         ret = mvp_data(lc);
@@ -1788,8 +1788,7 @@ static int inter_data(VVCLocalContext *lc)
     if (ret)
         return ret;
 
-    if (cu->pred_mode == MODE_IBC)
-    {
+    if (cu->pred_mode == MODE_IBC) {
         ff_vvc_update_hmvp(lc, mi);
     } else if (!pu->merge_gpm_flag && !pu->inter_affine_flag && !pu->merge_subblock_flag) {
         refine_regular_subblock(lc);
