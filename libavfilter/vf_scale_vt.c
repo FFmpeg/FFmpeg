352333543c04596b86a035be2f4c738482ce353e
Koushik Dutta <koushd@gmail.com>
2024-11-24 20:48:54 -0800

    avfilter/scale_vt: implement negative width/height aspect ratio sizing
    Signed-off-by: Koushik Dutta <koushd@gmail.com>
    Signed-off-by: Zhao Zhili <zhilizhao@tencent.com>

--- libavfilter/vf_scale_vt.c
+++ libavfilter/vf_scale_vt.c
@@ -189,6 +189,8 @@ static int scale_vt_config_output(AVFilterLink *outlink)
     if (err < 0)
         return err;
 
+    ff_scale_adjust_dimensions(inlink, &s->output_width, &s->output_height, 0, 1);
+
     outlink->w = s->output_width;
     outlink->h = s->output_height;
 
