bfed5f6b7d9edf89b62b211ca738cc1dc87d9693
Zhao Zhili <zhilizhao@tencent.com>
2024-12-03 22:29:05 +0800

    aarch64/vvc: Reuse ff_vvc_put_pel_pixels for chroma

--- libavcodec/aarch64/vvc/dsp_init.c
+++ libavcodec/aarch64/vvc/dsp_init.c
@@ -131,6 +131,13 @@ void ff_vvc_dsp_init_aarch64(VVCDSPContext *const c, const int bd)
         c->inter.put[0][5][1][1] = ff_vvc_put_qpel_hv64_8_neon;
         c->inter.put[0][6][1][1] = ff_vvc_put_qpel_hv128_8_neon;
 
+        c->inter.put[1][1][0][0] = ff_vvc_put_pel_pixels4_8_neon;
+        c->inter.put[1][2][0][0] = ff_vvc_put_pel_pixels8_8_neon;
+        c->inter.put[1][3][0][0] = ff_vvc_put_pel_pixels16_8_neon;
+        c->inter.put[1][4][0][0] = ff_vvc_put_pel_pixels32_8_neon;
+        c->inter.put[1][5][0][0] = ff_vvc_put_pel_pixels64_8_neon;
+        c->inter.put[1][6][0][0] = ff_vvc_put_pel_pixels128_8_neon;
+
         c->inter.put[1][1][0][1] = ff_vvc_put_epel_h4_8_neon;
         c->inter.put[1][2][0][1] = ff_vvc_put_epel_h8_8_neon;
         c->inter.put[1][3][0][1] = ff_vvc_put_epel_h16_8_neon;
