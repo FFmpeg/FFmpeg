01084f3d16aeff2129fb1d2944921fc986cd4475
Niklas Haas <git@haasn.dev>
2024-12-04 12:14:46 +0100

    avutil/frame: add AVSideDataDescriptor for AV_FRAME_DATA_VIDEO_HINT
    This entry was seemingly missing.

--- libavutil/frame.c
+++ libavutil/frame.c
@@ -58,6 +58,7 @@ static const AVSideDataDescriptor sd_props[] = {
     [AV_FRAME_DATA_SPHERICAL]                   = { "Spherical Mapping",                            AV_SIDE_DATA_PROP_GLOBAL },
     [AV_FRAME_DATA_ICC_PROFILE]                 = { "ICC profile",                                  AV_SIDE_DATA_PROP_GLOBAL },
     [AV_FRAME_DATA_SEI_UNREGISTERED]            = { "H.26[45] User Data Unregistered SEI message",  AV_SIDE_DATA_PROP_MULTI },
+    [AV_FRAME_DATA_VIDEO_HINT]                  = { "Encoding video hint" },
 };
 
 static void get_frame_defaults(AVFrame *frame)
