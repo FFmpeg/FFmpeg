5a8c2b2b506cd4ad47aea424aa46c3c9cd6ca40b
Peter Ross <pross@xvid.org>
2024-12-21 12:16:08 +1100

    avformat/aiff: G.728 muxing and demuxing

--- libavformat/aiffdec.c
+++ libavformat/aiffdec.c
@@ -174,6 +174,9 @@ static int get_aiff_header(AVFormatContext *s, int64_t size,
         case AV_CODEC_ID_GSM:
             par->block_align = 33;
             break;
+        case AV_CODEC_ID_G728:
+            par->block_align = 5;
+            break;
         default:
             aiff->block_duration = 1;
             break;
