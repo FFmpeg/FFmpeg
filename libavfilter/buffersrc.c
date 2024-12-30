a01eaecf6325cefab5b26e0d905df6662db37be1
James Almer <jamrial@gmail.com>
2024-12-30 00:25:41 -0300

    avfilter/buffersrc: check for valid sample rate
    A sample rate <= 0 is invalid.
    
    Fixes an assert in ffmpeg_enc.c that assumed a valid sample rate would be set.
    Fixes ticket #11385.
    
    Signed-off-by: James Almer <jamrial@gmail.com>
    (cherry picked from commit 1446e37d3d032e1452844778b3e6ba2c20f0c322)

--- libavfilter/buffersrc.c
+++ libavfilter/buffersrc.c
@@ -382,6 +382,11 @@ FF_ENABLE_DEPRECATION_WARNINGS
         av_channel_layout_describe(&s->ch_layout, buf, sizeof(buf));
     }
 
+    if (s->sample_rate <= 0) {
+        av_log(ctx, AV_LOG_ERROR, "Sample rate not set\n");
+        return AVERROR(EINVAL);
+    }
+
     if (!s->time_base.num)
         s->time_base = (AVRational){1, s->sample_rate};
 
