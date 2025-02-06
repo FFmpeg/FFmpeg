FFmpeg/FFmpeg

commit 8a72775d5dba3253c4ffd9b7ec91f73beda42641

avutil/downmix_info: zero the allocated buffer
Signed-off-by: James Almer <jamrial@gmail.com>
(cherry picked from commit 0bed1b89530042f0a835b8d1178aa52b40a751f6)
(cherry picked from commit a649b2a8313571fcec101143e9479299eb8bc214)

libavutil/downmix_info.c
@@ -27,9 +27,12 @@ AVDownmixInfo *av_downmix_info_update_side_data(AVFrame *frame)
 
     side_data = av_frame_get_side_data(frame, AV_FRAME_DATA_DOWNMIX_INFO);
 
-    if (!side_data)
+    if (!side_data) {
         side_data = av_frame_new_side_data(frame, AV_FRAME_DATA_DOWNMIX_INFO,
                                            sizeof(AVDownmixInfo));
+        if (side_data)
+            memset(side_data->data, 0, sizeof(AVDownmixInfo))
+    }
 
     if (!side_data)
         return NULL;

commit e0f3c1018a0973abb18e240cbf0bc98daf15f6b6

avutil/downmix_info: add missing semicolon
Fixes compilation broken by 8a72775d5dba3253c4ffd9b7ec91f73beda42641.

Signed-off-by: James Almer <jamrial@gmail.com>

libavutil/downmix_info.c
@@ -31,7 +31,7 @@ AVDownmixInfo *av_downmix_info_update_side_data(AVFrame *frame)
         side_data = av_frame_new_side_data(frame, AV_FRAME_DATA_DOWNMIX_INFO,
                                            sizeof(AVDownmixInfo));
         if (side_data)
-            memset(side_data->data, 0, sizeof(AVDownmixInfo))
+            memset(side_data->data, 0, sizeof(AVDownmixInfo));
     }
 
     if (!side_data)