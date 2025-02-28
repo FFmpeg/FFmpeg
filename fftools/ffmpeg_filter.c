01f63ef0b44fa70d87edaf52f19fcdb2ae7446b3
James Almer <jamrial@gmail.com>
2025-02-27 23:38:09 -0300

    fftools/ffmpeg_filter: also remove display matrix side data from buffered frames
    Some frames may be buffered before a complex filtergraph can be configured.
    This change ensures the side data removal in the cases where autorotation is
    enabled also applies to them.
    
    Fixes ticket #11487
    
    Signed-off-by: James Almer <jamrial@gmail.com>

--- fftools/ffmpeg_filter.c
+++ fftools/ffmpeg_filter.c
@@ -2041,6 +2041,10 @@ static int configure_filtergraph(FilterGraph *fg, FilterGraphThread *fgt)
             if (ifp->type_src == AVMEDIA_TYPE_SUBTITLE) {
                 sub2video_frame(&ifp->ifilter, tmp, !fgt->graph);
             } else {
+                if (ifp->type_src == AVMEDIA_TYPE_VIDEO) {
+                    if (ifp->displaymatrix_applied)
+                        av_frame_remove_side_data(tmp, AV_FRAME_DATA_DISPLAYMATRIX);
+                }
                 ret = av_buffersrc_add_frame(ifp->filter, tmp);
             }
             av_frame_free(&tmp);
