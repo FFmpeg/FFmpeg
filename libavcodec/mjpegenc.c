78391f20c8822a490adb6359b9b2973730cb00ed
Andreas Rheinhardt <andreas.rheinhardt@outlook.com>
2025-03-04 19:26:50 +0100

    avcodec/mjpegenc: Reindent after the previous commit
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

--- libavcodec/mjpegenc.c
+++ libavcodec/mjpegenc.c
@@ -466,71 +466,67 @@ static void encode_block(MpegEncContext *s, int16_t *block, int n)
 static void mjpeg_record_mb(MpegEncContext *const s, int16_t block[][64],
                             int unused_x, int unused_y)
 {
-    int i;
-        if (s->chroma_format == CHROMA_444) {
-            record_block(s, block[0], 0);
-            record_block(s, block[2], 2);
-            record_block(s, block[4], 4);
-            record_block(s, block[8], 8);
-            record_block(s, block[5], 5);
-            record_block(s, block[9], 9);
+    if (s->chroma_format == CHROMA_444) {
+        record_block(s, block[0], 0);
+        record_block(s, block[2], 2);
+        record_block(s, block[4], 4);
+        record_block(s, block[8], 8);
+        record_block(s, block[5], 5);
+        record_block(s, block[9], 9);
 
-            if (16*s->mb_x+8 < s->width) {
-                record_block(s, block[1], 1);
-                record_block(s, block[3], 3);
-                record_block(s, block[6], 6);
-                record_block(s, block[10], 10);
-                record_block(s, block[7], 7);
-                record_block(s, block[11], 11);
-            }
-        } else {
-            for(i=0;i<5;i++) {
-                record_block(s, block[i], i);
-            }
-            if (s->chroma_format == CHROMA_420) {
-                record_block(s, block[5], 5);
-            } else {
-                record_block(s, block[6], 6);
-                record_block(s, block[5], 5);
-                record_block(s, block[7], 7);
-            }
+        if (16*s->mb_x+8 < s->width) {
+            record_block(s, block[1],   1);
+            record_block(s, block[3],   3);
+            record_block(s, block[6],   6);
+            record_block(s, block[10], 10);
+            record_block(s, block[7],   7);
+            record_block(s, block[11], 11);
         }
+    } else {
+        for (int i = 0; i < 5; i++)
+            record_block(s, block[i], i);
+        if (s->chroma_format == CHROMA_420) {
+            record_block(s, block[5], 5);
+        } else {
+            record_block(s, block[6], 6);
+            record_block(s, block[5], 5);
+            record_block(s, block[7], 7);
+        }
+    }
 }
 
 static void mjpeg_encode_mb(MpegEncContext *const s, int16_t block[][64],
                             int unused_x, int unused_y)
 {
-    int i;
-        if (s->chroma_format == CHROMA_444) {
-            encode_block(s, block[0], 0);
-            encode_block(s, block[2], 2);
-            encode_block(s, block[4], 4);
-            encode_block(s, block[8], 8);
-            encode_block(s, block[5], 5);
-            encode_block(s, block[9], 9);
+    if (s->chroma_format == CHROMA_444) {
+        encode_block(s, block[0], 0);
+        encode_block(s, block[2], 2);
+        encode_block(s, block[4], 4);
+        encode_block(s, block[8], 8);
+        encode_block(s, block[5], 5);
+        encode_block(s, block[9], 9);
 
-            if (16*s->mb_x+8 < s->width) {
-                encode_block(s, block[1], 1);
-                encode_block(s, block[3], 3);
-                encode_block(s, block[6], 6);
-                encode_block(s, block[10], 10);
-                encode_block(s, block[7], 7);
-                encode_block(s, block[11], 11);
-            }
-        } else {
-            for(i=0;i<5;i++) {
-                encode_block(s, block[i], i);
-            }
-            if (s->chroma_format == CHROMA_420) {
-                encode_block(s, block[5], 5);
-            } else {
-                encode_block(s, block[6], 6);
-                encode_block(s, block[5], 5);
-                encode_block(s, block[7], 7);
-            }
+        if (16 * s->mb_x + 8 < s->width) {
+            encode_block(s, block[1], 1);
+            encode_block(s, block[3], 3);
+            encode_block(s, block[6], 6);
+            encode_block(s, block[10], 10);
+            encode_block(s, block[7], 7);
+            encode_block(s, block[11], 11);
         }
+    } else {
+        for (int i = 0; i < 5; i++)
+            encode_block(s, block[i], i);
+        if (s->chroma_format == CHROMA_420) {
+            encode_block(s, block[5], 5);
+        } else {
+            encode_block(s, block[6], 6);
+            encode_block(s, block[5], 5);
+            encode_block(s, block[7], 7);
+        }
+    }
 
-        s->i_tex_bits += get_bits_diff(s);
+    s->i_tex_bits += get_bits_diff(s);
 }
 
 static av_cold int mjpeg_encode_init(AVCodecContext *avctx)
