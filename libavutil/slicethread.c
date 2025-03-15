e8a8a13128f54da16e2be088331562edf2bfc6d2
Andreas Rheinhardt <andreas.rheinhardt@outlook.com>
2025-03-15 05:34:29 +0100

    avutil/slicethread: Mark avpriv_slicethread_{create,free} as av_cold
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

--- libavutil/slicethread.c
+++ libavutil/slicethread.c
@@ -95,6 +95,7 @@ static void *attribute_align_arg thread_worker(void *v)
     }
 }
 
+av_cold
 int avpriv_slicethread_create(AVSliceThread **pctx, void *priv,
                               void (*worker_func)(void *priv, int jobnr, int threadnr, int nb_jobs, int nb_threads),
                               void (*main_func)(void *priv),
@@ -222,7 +223,7 @@ void avpriv_slicethread_execute(AVSliceThread *ctx, int nb_jobs, int execute_mai
     }
 }
 
-void avpriv_slicethread_free(AVSliceThread **pctx)
+av_cold void avpriv_slicethread_free(AVSliceThread **pctx)
 {
     AVSliceThread *ctx = *pctx;
     int nb_workers, i;
