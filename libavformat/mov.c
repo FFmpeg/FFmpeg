ca964ba139695fc0064d07ac6503626d549520c7
Zhao Zhili <zhilizhao@tencent.com>
2025-04-17 20:12:49 +0800

    avformat/mov: Reduce seek when interleaved_read is disabled
    Don't select sample with small dts when interleaved_read is disabled.
    
    Signed-off-by: Zhao Zhili <zhilizhao@tencent.com>

--- libavformat/mov.c
+++ libavformat/mov.c
@@ -10703,7 +10703,7 @@ static AVIndexEntry *mov_find_next_sample(AVFormatContext *s, AVStream **st)
                 ((s->pb->seekable & AVIO_SEEKABLE_NORMAL) &&
                  ((msc->pb != s->pb && dts < best_dts) || (msc->pb == s->pb && dts != AV_NOPTS_VALUE &&
                  ((dtsdiff <= AV_TIME_BASE && current_sample->pos < sample->pos) ||
-                  (dtsdiff > AV_TIME_BASE && dts < best_dts)))))) {
+                  (dtsdiff > AV_TIME_BASE && dts < best_dts && mov->interleaved_read)))))) {
                 sample = current_sample;
                 best_dts = dts;
                 *st = avst;
