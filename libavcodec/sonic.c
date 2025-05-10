534c87eb242bf3dfb221945289d60c729464802c
Michael Niedermayer <michael@niedermayer.cc>
2025-05-11 01:08:06 +0200

    avcodec/sonic: Check num_taps
    The encoder uses max 128 taps, which is quiet a lot already
    If work is done to improve sonic, it will be more radical than changing the taps
    
    Fixes: Timeout
    Fixes: 402539974/clusterfuzz-testcase-minimized-ffmpeg_AV_CODEC_ID_SONIC_fuzzer-6122944271286272
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit fd0a792766c8ae513dd849fc47fa9e899cc5664b)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/sonic.c
+++ libavcodec/sonic.c
@@ -924,6 +924,9 @@ static av_cold int sonic_decode_init(AVCodecContext *avctx)
     if (get_bits1(&gb)) // XXX FIXME
         av_log(avctx, AV_LOG_INFO, "Custom quant table\n");
 
+    if (s->num_taps > 128)
+        return AVERROR_INVALIDDATA;
+
     s->block_align = 2048LL*s->samplerate/(44100*s->downsampling);
     s->frame_size = s->channels*s->block_align*s->downsampling;
 //    avctx->frame_size = s->block_align;
