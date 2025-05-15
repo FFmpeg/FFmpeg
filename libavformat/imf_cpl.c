FFmpeg/FFmpeg

commit 61fa1e14e4178d3f2550c76f7a36484220f6dc0c

avformat/imf_cpl: Check the right variable
Fixes Coverity issue #1512407.

Reviewed-by: Pierre-Anthony Lemieux <pal@sandflow.com>
Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

libavformat/imf_cpl.c
@@ -625,7 +625,7 @@ static int fill_virtual_tracks(xmlNodePtr cpl_element, FFIMFCPL *cpl)
         av_log(NULL, AV_LOG_DEBUG, "Processing IMF CPL Segment\n");
 
         sequence_list_elem = ff_imf_xml_get_child_element_by_name(segment_elem, "SequenceList");
-        if (!segment_list_elem)
+        if (!sequence_list_elem)
             continue;
 
         sequence_elem = xmlFirstElementChild(sequence_list_elem);

commit 39800d78b07e65a6b29a69366d651f80105b95a1

avformat/imf_cpl: do not continue looping forever
Fixes: infinite loop
Fixes: 401658595/clusterfuzz-testcase-minimized-ffmpeg_DEMUXER_fuzzer-5756875014733824
Regression since: 61fa1e14e4178d3f2550c76f7a36484220f6dc0c

Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

libavformat/imf_cpl.c
@@ -709,8 +709,7 @@ static int fill_virtual_tracks(void *log_ctx, xmlNodePtr cpl_element, FFIMFCPL *
         av_log(log_ctx, AV_LOG_DEBUG, "Processing IMF CPL Segment\n");
 
         sequence_list_elem = ff_imf_xml_get_child_element_by_name(segment_elem, "SequenceList");
-        if (!sequence_list_elem)
-            continue;
+        if (sequence_list_elem) {
 
         sequence_elem = xmlFirstElementChild(sequence_list_elem);
         while (sequence_elem) {
@@ -734,6 +733,7 @@ static int fill_virtual_tracks(void *log_ctx, xmlNodePtr cpl_element, FFIMFCPL *
                 return ret;
 
             sequence_elem = xmlNextElementSibling(sequence_elem);
+        }
         }
 
         segment_elem = xmlNextElementSibling(segment_elem);