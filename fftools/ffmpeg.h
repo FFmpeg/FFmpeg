1f2b8d7238eff4ab8a4d8d6177e250b8180d51f4
softworkz <softworkz@hotmail.com>
2025-05-15 23:10:02 +0200

    fftools/graphprint: Now, make it a Killer-Feature!
    remember this: -sg   <= means Show Graph
    
    Signed-off-by: softworkz <softworkz@hotmail.com>

--- fftools/ffmpeg.h
+++ fftools/ffmpeg.h
@@ -721,6 +721,7 @@ extern int print_graphs;
 extern char *print_graphs_file;
 extern char *print_graphs_format;
 extern int auto_conversion_filters;
+extern int show_graph;
 
 extern const AVIOInterruptCB int_cb;
 
