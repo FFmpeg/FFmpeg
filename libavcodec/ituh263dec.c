d95041dffe611dc0d519df95168c205c368a9791
Andreas Rheinhardt <andreas.rheinhardt@outlook.com>
2025-05-25 20:56:43 +0200

    avcodec/ituh263dec: Remove redundant store
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

--- libavcodec/ituh263dec.c
+++ libavcodec/ituh263dec.c
@@ -434,15 +434,16 @@ end:
 
 static void h263_decode_dquant(MpegEncContext *s){
     static const int8_t quant_tab[4] = { -1, -2, 1, 2 };
+    int qscale;
 
     if(s->modified_quant){
         if(get_bits1(&s->gb))
-            s->qscale= ff_modified_quant_tab[get_bits1(&s->gb)][ s->qscale ];
+            qscale = ff_modified_quant_tab[get_bits1(&s->gb)][s->qscale];
         else
-            s->qscale= get_bits(&s->gb, 5);
+            qscale = get_bits(&s->gb, 5);
     }else
-        s->qscale += quant_tab[get_bits(&s->gb, 2)];
-    ff_set_qscale(s, s->qscale);
+        qscale = s->qscale + quant_tab[get_bits(&s->gb, 2)];
+    ff_set_qscale(s, qscale);
 }
 
 static void h263_pred_acdc(MpegEncContext * s, int16_t *block, int n)
