d811966ba6c276d1e942c63acfe0fa0e34a75506
Kacper Michajłow <kasper93@gmail.com>
2025-06-06 19:20:35 +0200

    tools/target_dem_fuzzer: remove unused fuzz_tag
    Signed-off-by: Kacper Michajłow <kasper93@gmail.com>
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- tools/target_dem_fuzzer.c
+++ tools/target_dem_fuzzer.c
@@ -98,10 +98,7 @@ static int64_t io_seek(void *opaque, int64_t offset, int whence)
 const uint32_t maxiteration = 8096;
 const int maxblocks= 50000;
 
-static const uint64_t FUZZ_TAG = 0x4741542D5A5A5546ULL;
-
 int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
-    const uint64_t fuzz_tag = FUZZ_TAG;
     uint32_t it = 0;
     AVFormatContext *avfmt = avformat_alloc_context();
     AVPacket *pkt;
