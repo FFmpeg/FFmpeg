38073187bd5b3df5b75957c2c75d6145e49e7532
Peter Ross <pross@xvid.org>
2025-06-10 07:57:18 +1000

    avcodec/tests/dct: add CONFIG_PRORES_DECODER guard
    Only enable the proresdsp check when the prores decoder is enabled.
    
    This fixes fate when configuring with --disable-everything

--- libavcodec/tests/dct.c
+++ libavcodec/tests/dct.c
@@ -52,7 +52,9 @@
 #include "libavcodec/faandct.h"
 #include "libavcodec/faanidct.h"
 #include "libavcodec/dctref.h"
+#if CONFIG_PRORES_DECODER
 #include "libavcodec/proresdsp.c"
+#endif
 
 struct algo {
     const char *name;
@@ -71,6 +73,7 @@ static const struct algo fdct_tab[] = {
 #endif /* CONFIG_FAANDCT */
 };
 
+#if CONFIG_PRORES_DECODER
 static void ff_prores_idct_wrap(int16_t *dst){
     LOCAL_ALIGNED(16, int16_t, qmat, [64]);
     int i;
@@ -83,6 +86,7 @@ static void ff_prores_idct_wrap(int16_t *dst){
          dst[i] -= 512;
     }
 }
+#endif
 
 static const struct algo idct_tab[] = {
     { "REF-DBL",     ff_ref_idct,          FF_IDCT_PERM_NONE },
@@ -90,7 +94,9 @@ static const struct algo idct_tab[] = {
     { "SIMPLE-C",    ff_simple_idct_int16_8bit,     FF_IDCT_PERM_NONE },
     { "SIMPLE-C10",  ff_simple_idct_int16_10bit,    FF_IDCT_PERM_NONE },
     { "SIMPLE-C12",  ff_simple_idct_int16_12bit,    FF_IDCT_PERM_NONE, 0, 1 },
+#if CONFIG_PRORES_DECODER
     { "PR-C",        ff_prores_idct_wrap,  FF_IDCT_PERM_NONE, 0, 1 },
+#endif
 #if CONFIG_FAANIDCT
     { "FAANI",       ff_faanidct,          FF_IDCT_PERM_NONE },
 #endif /* CONFIG_FAANIDCT */
