32cbadcd74a3f3cb1cf76cf1eb9dc5a2c8ae7e56
Andreas Rheinhardt <andreas.rheinhardt@outlook.com>
2025-06-13 02:37:42 +0200

    avcodec/mpegvideo: Avoid {a,d}c_val[{1,2}]
    These are already mostly unused; not using them here is in
    preparation for removing them altogether. Use block_index[4..5]
    instead for addressing.
    
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

--- libavcodec/mpegvideo.c
+++ libavcodec/mpegvideo.c
@@ -492,22 +492,23 @@ void ff_clean_intra_table_entries(MpegEncContext *s)
 {
     int wrap = s->b8_stride;
     int xy = s->block_index[0];
+    /* chroma */
+    unsigned uxy = s->block_index[4];
+    unsigned vxy = s->block_index[5];
+    int16_t *dc_val = s->dc_val[0];
 
     s->dc_val[0][xy           ] =
     s->dc_val[0][xy + 1       ] =
     s->dc_val[0][xy     + wrap] =
     s->dc_val[0][xy + 1 + wrap] = 1024;
+    dc_val[uxy] =
+    dc_val[vxy] = 1024;
     /* ac pred */
     memset(s->ac_val[0][xy       ], 0, 32 * sizeof(int16_t));
     memset(s->ac_val[0][xy + wrap], 0, 32 * sizeof(int16_t));
-    /* chroma */
-    wrap = s->mb_stride;
-    xy = s->mb_x + s->mb_y * wrap;
-    s->dc_val[1][xy] =
-    s->dc_val[2][xy] = 1024;
     /* ac pred */
-    memset(s->ac_val[1][xy], 0, 16 * sizeof(int16_t));
-    memset(s->ac_val[2][xy], 0, 16 * sizeof(int16_t));
+    memset(s->ac_val[0][uxy], 0, 16 * sizeof(int16_t));
+    memset(s->ac_val[0][vxy], 0, 16 * sizeof(int16_t));
 }
 
 void ff_init_block_index(MpegEncContext *s){ //FIXME maybe rename
