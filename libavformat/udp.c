23c70e3740b29c42db54f583fefaa2cae4fc360f
Timo Rothenpieler <timo@rothenpieler.org>
2025-06-26 02:37:15 +0200

    avformat/udp: don't override 0 localport

--- libavformat/udp.c
+++ libavformat/udp.c
@@ -780,7 +780,7 @@ static int udp_open(URLContext *h, const char *uri, int flags)
             goto fail;
     }
 
-    if ((s->is_multicast || s->local_port <= 0) && (h->flags & AVIO_FLAG_READ))
+    if ((s->is_multicast || s->local_port < 0) && (h->flags & AVIO_FLAG_READ))
         s->local_port = port;
 
     udp_fd = udp_socket_create(h, &my_addr, &len, s->localaddr);
