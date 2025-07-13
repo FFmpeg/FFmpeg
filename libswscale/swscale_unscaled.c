af9b43455a972841154b194057a79ee8b606e727
James Almer <jamrial@gmail.com>
2025-07-13 19:50:57 -0300

    swscale/swscale_unscaled: don't add offsets to NULL pointers
    Fixes: libswscale/swscale_unscaled.c:916:20: runtime error: applying zero offset to null pointer
    Signed-off-by: James Almer <jamrial@gmail.com>

--- libswscale/swscale_unscaled.c
+++ libswscale/swscale_unscaled.c
@@ -912,7 +912,7 @@ static int Rgb16ToPlanarRgb16Wrapper(SwsInternal *c, const uint8_t *const src[],
         return srcSliceH;
     }
 
-    for(i=0; i<4; i++) {
+    for (i = 0; i < 4 && dst[i]; i++) {
         dst2013[i] += stride2013[i] * srcSliceY / 2;
         dst1023[i] += stride1023[i] * srcSliceY / 2;
     }
