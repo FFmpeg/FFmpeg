ee4ff3f706f5b519bfc2bae37e7371aef2612eb4
James Almer <jamrial@gmail.com>
2025-07-18 13:01:29 -0300

    avfilter/x86/vf_blackdetect_init: don't enable the ASM functions on targets where it's known they will be slower
    Signed-off-by: James Almer <jamrial@gmail.com>

--- libavfilter/x86/vf_blackdetect_init.c
+++ libavfilter/x86/vf_blackdetect_init.c
@@ -28,7 +28,7 @@ unsigned ff_blackdetect_16_avx2(const uint8_t *, ptrdiff_t, ptrdiff_t, ptrdiff_t
 av_cold ff_blackdetect_fn ff_blackdetect_get_fn_x86(int depth)
 {
     int cpu_flags = av_get_cpu_flags();
-    if (EXTERNAL_AVX2(cpu_flags))
+    if (EXTERNAL_AVX2_FAST(cpu_flags))
         return depth == 8 ? ff_blackdetect_8_avx2 : ff_blackdetect_16_avx2;
     return NULL;
 }
