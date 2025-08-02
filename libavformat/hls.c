3d630b7479b8e0b9cae7390119d46a6becef5f2f
Michael Niedermayer <michael@niedermayer.cc>
2025-08-02 18:55:26 +0200

    avformat/hls: add cmfv/cmfa exceptions
    Fixes: Ticket11526
    
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit f3c3a6ecfb230c56a8ff9d219d79d5981b2aa4f3)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavformat/hls.c
+++ libavformat/hls.c
@@ -756,8 +756,8 @@ static int test_segment(AVFormatContext *s, const AVInputFormat *in_fmt, struct 
                      + 2*(ff_match_url_ext(seg->url, in_fmt->extensions) > 0);
             // Youtube uses aac files with .ts extension
             if(av_match_name("mp4", in_fmt->name) || av_match_name("aac", in_fmt->name)) {
-                matchF |=      av_match_ext(    seg->url, "ts,m2t,m2ts,mts,mpg,m4s,mpeg,mpegts")
-                          + 2*(ff_match_url_ext(seg->url, "ts,m2t,m2ts,mts,mpg,m4s,mpeg,mpegts") > 0);
+                matchF |=      av_match_ext(    seg->url, "ts,m2t,m2ts,mts,mpg,m4s,mpeg,mpegts,cmfv,cmfa")
+                          + 2*(ff_match_url_ext(seg->url, "ts,m2t,m2ts,mts,mpg,m4s,mpeg,mpegts,cmfv,cmfa") > 0);
             }
         } else if (!strcmp(in_fmt->name, "mpegts")) {
             const char *str = "ts,m2t,m2ts,mts,mpg,m4s,mpeg,mpegts"
