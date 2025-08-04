902c725b2cfce8126c8f397e8a7a794ab1cd932e
Michael Niedermayer <michael@niedermayer.cc>
2025-08-04 21:08:47 +0200

    avcodec/ffv1dec: Disable frame threading due to race condition
    Slice threading remains available!
    
    The race condition fix is in 8d5efc218245c3f0559f48837b3e63e2932525e0
    and bcf08c11710cab5db8eb3d0774e1a93e322fb821
    Backport of these is welcome
    
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/ffv1dec.c
+++ libavcodec/ffv1dec.c
@@ -1091,6 +1091,6 @@ AVCodec ff_ffv1_decoder = {
     .decode         = decode_frame,
     .update_thread_context = ONLY_IF_THREADS_ENABLED(update_thread_context),
     .capabilities   = AV_CODEC_CAP_DR1 /*| AV_CODEC_CAP_DRAW_HORIZ_BAND*/ |
-                      AV_CODEC_CAP_FRAME_THREADS | AV_CODEC_CAP_SLICE_THREADS,
-    .caps_internal  = FF_CODEC_CAP_INIT_CLEANUP | FF_CODEC_CAP_ALLOCATE_PROGRESS,
+                      AV_CODEC_CAP_SLICE_THREADS,
+    .caps_internal  = FF_CODEC_CAP_INIT_CLEANUP,
 };
