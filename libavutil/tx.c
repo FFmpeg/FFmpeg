56e0a883c1eb80a3a533616b70601e1a6a4ecb4b
Kacper Michajłow <kasper93@gmail.com>
2025-08-04 02:42:28 +0200

    avutil/tx: zero whole array, not only one element
    Signed-off-by: Kacper Michajłow <kasper93@gmail.com>

--- libavutil/tx.c
+++ libavutil/tx.c
@@ -284,7 +284,7 @@ static void reset_ctx(AVTXContext *s, int free_sub)
      * ff_tx_init_subtx() call is made. */
     s->nb_sub = 0;
     s->opaque = NULL;
-    memset(s->fn, 0, sizeof(*s->fn));
+    memset(s->fn, 0, sizeof(s->fn));
 }
 
 void ff_tx_clear_ctx(AVTXContext *s)
