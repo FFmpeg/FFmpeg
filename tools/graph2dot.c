082e2b26d4efd7cc7638a9d80a52861d4e4116db
Kacper Michajłow <kasper93@gmail.com>
2025-08-09 04:01:40 +0200

    tools/graph2dot: fix getopt return value type
    Signed-off-by: Kacper Michajłow <kasper93@gmail.com>

--- tools/graph2dot.c
+++ tools/graph2dot.c
@@ -113,7 +113,7 @@ int main(int argc, char **argv)
     FILE *infile            = NULL;
     char *graph_string      = NULL;
     AVFilterGraph *graph    = NULL;
-    char c;
+    int c;
 
     av_log_set_level(AV_LOG_DEBUG);
 
