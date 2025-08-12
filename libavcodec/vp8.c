d3375101fe2a6cf11dc763899d9eca1475f4be59
Kacper Michajłow <kasper93@gmail.com>
2025-08-12 05:35:09 +0200

    avcodec/vp8: remove set, but never used variable
    Signed-off-by: Kacper Michajłow <kasper93@gmail.com>

--- libavcodec/vp8.c
+++ libavcodec/vp8.c
@@ -2522,18 +2522,12 @@ static av_always_inline void filter_mb_row(AVCodecContext *avctx, void *tdata,
     VP8ThreadData *td = &s->thread_data[threadnr];
     int mb_x, mb_y = atomic_load(&td->thread_mb_pos) >> 16, num_jobs = s->num_jobs;
     AVFrame *curframe = s->curframe->tf.f;
-    VP8Macroblock *mb;
     VP8ThreadData *prev_td, *next_td;
     uint8_t *dst[3] = {
         curframe->data[0] + 16 * mb_y * s->linesize,
         curframe->data[1] +  8 * mb_y * s->uvlinesize,
         curframe->data[2] +  8 * mb_y * s->uvlinesize
     };
-
-    if (s->mb_layout == 1)
-        mb = s->macroblocks_base + ((s->mb_width + 1) * (mb_y + 1) + 1);
-    else
-        mb = s->macroblocks + (s->mb_height - mb_y - 1) * 2;
 
     if (mb_y == 0)
         prev_td = td;
@@ -2544,7 +2538,7 @@ static av_always_inline void filter_mb_row(AVCodecContext *avctx, void *tdata,
     else
         next_td = &s->thread_data[(jobnr + 1) % num_jobs];
 
-    for (mb_x = 0; mb_x < s->mb_width; mb_x++, mb++) {
+    for (mb_x = 0; mb_x < s->mb_width; mb_x++) {
         const VP8FilterStrength *f = &td->filter_strength[mb_x];
         if (prev_td != td)
             check_thread_pos(td, prev_td,
