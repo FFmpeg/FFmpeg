61b6877637041a1f817ad9811c839b0feae2b8af
Michael Niedermayer <michael@niedermayer.cc>
2025-09-11 20:12:55 +0200

    avcodec/mjpegdec: Explain buf_size/width/height check
    Suggested-by: Ramiro
    
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/mjpegdec.c
+++ libavcodec/mjpegdec.c
@@ -340,6 +340,8 @@ int ff_mjpeg_decode_sof(MJpegDecodeContext *s)
     av_log(s->avctx, AV_LOG_DEBUG, "sof0: picture: %dx%d\n", width, height);
     if (av_image_check_size(width, height, 0, s->avctx) < 0)
         return AVERROR_INVALIDDATA;
+
+    // A valid frame requires at least 1 bit for DC + 1 bit for AC for each 8x8 block.
     if (s->buf_size && (width + 7) / 8 * ((height + 7) / 8) > s->buf_size * 4LL)
         return AVERROR_INVALIDDATA;
 
