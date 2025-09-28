e340f31b898e51d12ec1ced52fcbf03fb4635533
Andreas Rheinhardt <andreas.rheinhardt@outlook.com>
2025-09-28 23:25:59 +0200

    avcodec/x86/fpel: Remove redundant repetition
    The repetition count is always one since
    2cf9e733c6a666600423a0967f23341d9f09e3c8.
    
    Reviewed-by: James Almer <jamrial@gmail.com>
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

--- libavcodec/x86/fpel.asm
+++ libavcodec/x86/fpel.asm
@@ -31,33 +31,27 @@ SECTION .text
 %if %2 == mmsize/2
 %define LOAD movh
 %define SAVE movh
-%define LEN  mmsize
 %else
 %define LOAD movu
 %define SAVE mova
-%define LEN  %2
 %endif
 cglobal %1_pixels%2, 4,5,4
     lea          r4, [r2*3]
 .loop:
-%assign %%i 0
-%rep LEN/mmsize
-    LOAD         m0, [r1 + %%i]
-    LOAD         m1, [r1+r2 + %%i]
-    LOAD         m2, [r1+r2*2 + %%i]
-    LOAD         m3, [r1+r4 + %%i]
+    LOAD         m0, [r1]
+    LOAD         m1, [r1+r2]
+    LOAD         m2, [r1+r2*2]
+    LOAD         m3, [r1+r4]
 %ifidn %1, avg
-    pavgb        m0, [r0 + %%i]
-    pavgb        m1, [r0+r2 + %%i]
-    pavgb        m2, [r0+r2*2 + %%i]
-    pavgb        m3, [r0+r4 + %%i]
+    pavgb        m0, [r0]
+    pavgb        m1, [r0+r2]
+    pavgb        m2, [r0+r2*2]
+    pavgb        m3, [r0+r4]
 %endif
-    SAVE       [r0 + %%i], m0
-    SAVE    [r0+r2 + %%i], m1
-    SAVE  [r0+r2*2 + %%i], m2
-    SAVE    [r0+r4 + %%i], m3
-%assign %%i %%i+mmsize
-%endrep
+    SAVE       [r0], m0
+    SAVE    [r0+r2], m1
+    SAVE  [r0+r2*2], m2
+    SAVE    [r0+r4], m3
     sub         r3d, 4
     lea          r1, [r1+r2*4]
     lea          r0, [r0+r2*4]
