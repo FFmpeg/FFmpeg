5511641365b83afa6449ae0b89d01b78556d941d
James Almer <jamrial@gmail.com>
2025-09-29 22:33:49 -0300

    avcodec/atrac9dec: use av_zero_extend()
    Signed-off-by: James Almer <jamrial@gmail.com>

--- libavcodec/atrac9dec.c
+++ libavcodec/atrac9dec.c
@@ -285,7 +285,7 @@ static inline int read_scalefactors(ATRAC9Context *s, ATRAC9BlockData *b,
         for (int i = 1; i < b->band_ext_q_unit; i++) {
             int val = c->scalefactors[i - 1] + get_vlc2(gb, tab,
                                                         ATRAC9_SF_VLC_BITS, 1);
-            c->scalefactors[i] = val & ((1 << len) - 1);
+            c->scalefactors[i] = av_zero_extend(val, len);
         }
 
         for (int i = 0; i < b->band_ext_q_unit; i++)
@@ -339,7 +339,7 @@ static inline int read_scalefactors(ATRAC9Context *s, ATRAC9BlockData *b,
         for (int i = 1; i < unit_cnt; i++) {
             int val = c->scalefactors[i - 1] + get_vlc2(gb, tab,
                                                         ATRAC9_SF_VLC_BITS, 1);
-            c->scalefactors[i] = val & ((1 << len) - 1);
+            c->scalefactors[i] = av_zero_extend(val, len);
         }
 
         for (int i = 0; i < unit_cnt; i++)
