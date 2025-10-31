e59d964a3c5a7b7dc2da511f0d51c65c5fba741d
lholliger <14064434+lholliger@users.noreply.github.com>
2025-10-31 10:21:38 -0400

    libavformat/mov: add support for name udta
    In the QTFF documentation for udta/"user data atoms", "name" is a
    valid entry for "name of object" however it is currently skipped
    leading to some missing metadata when copying for some files. This
    isn't classified the same as "(C)nam" which is "Title of content"
    so they aren't necessarily the same.

--- libavformat/mov.c
+++ libavformat/mov.c
@@ -392,6 +392,7 @@ static int mov_read_udta_string(MOVContext *c, AVIOContext *pb, MOVAtom atom)
         return mov_metadata_loci(c, pb, atom.size);
     case MKTAG( 'm','a','n','u'): key = "make"; break;
     case MKTAG( 'm','o','d','l'): key = "model"; break;
+    case MKTAG( 'n','a','m','e'): key = "name"; break;
     case MKTAG( 'p','c','s','t'): key = "podcast";
         parse = mov_metadata_int8_no_padding; break;
     case MKTAG( 'p','g','a','p'): key = "gapless_playback";
