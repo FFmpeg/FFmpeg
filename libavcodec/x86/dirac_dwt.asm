fe0d8cb3e48dff4322f31b5d793dc106095e761d
Andreas Rheinhardt <andreas.rheinhardt@outlook.com>
2025-11-15 10:09:18 +0100

    avcodec/x86/dirac_dwt: Remove MMX in comment
    Forgotten in 5e332fe35cd336a5c7718d5e9a5a93ece0e61a3a.
    
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

--- libavcodec/x86/dirac_dwt.asm
+++ libavcodec/x86/dirac_dwt.asm
@@ -252,7 +252,7 @@ cglobal horizontal_compose_dd97i_ssse3, 3,6,8, b, tmp, w, x, w2, b_w2
     jl      .lowpass_loop
 
     EDGE_EXTENSION 1, 2, xw
-    ; leave the last up to 7 (sse) or 3 (mmx) values for C
+    ; leave the last up to 7 (sse) values for C
     xor     xd, xd
     and    w2d, ~(mmsize/2 - 1)
     cmp    w2d, mmsize/2
