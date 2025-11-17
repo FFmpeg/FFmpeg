955277020fc0f846dd138ecdbc63f078f73baf01
Niklas Haas <git@haasn.dev>
2025-11-17 10:50:54 +0100

    avfilter/x86/f_ebur128: only use filter_channels_avx for >= 2 channels
    The approach of this ASM routine is to process two channels at a time using
    AVX instructions. Obviously, there is no point in doing this if there is only
    a single channel; in which case the scalar loop would be better.
    
    Fixes a performance regression when filtering mono audio on certain CPUs,
    notably e.g. the Intel N100.
    
    (cherry picked from commit f3346ca6f7d8249738ae0d9544b62be5c51ccfcd)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavfilter/x86/f_ebur128_init.c
+++ libavfilter/x86/f_ebur128_init.c
@@ -33,7 +33,8 @@ av_cold void ff_ebur128_init_x86(EBUR128DSPContext *dsp, int nb_channels)
     int cpu_flags = av_get_cpu_flags();
 
     if (ARCH_X86_64 && EXTERNAL_AVX(cpu_flags)) {
-        dsp->filter_channels = ff_ebur128_filter_channels_avx;
+        if (nb_channels >= 2)
+            dsp->filter_channels = ff_ebur128_filter_channels_avx;
         if (nb_channels == 2)
             dsp->find_peak = ff_ebur128_find_peak_2ch_avx;
     }
