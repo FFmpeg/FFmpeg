FFmpeg/FFmpeg

commit ba4b73c9779c32580f8a3ba08602a5d94e0bcd7c

lavf/rawdec: set avg_frame_rate
Timestamps in two FATE H.264 conformance tests now start at 1 instead
of 0, which also happens in some other H.264 tests before this commit
and so is not a big issue.
Conversely, timestamps in some HEVC conformance tests start from a
smaller value now.
Ideally this should be addressed later in a more general way.

h264-conformance-frext-frext2_panasonic_b no longer requires -vsync
passthrough.

libavformat/rawdec.c
@@ -86,6 +86,7 @@ int ff_raw_video_read_header(AVFormatContext *s)
     st->codecpar->codec_id = s->iformat->raw_codec_id;
     sti->need_parsing = AVSTREAM_PARSE_FULL_RAW;
 
+    st->avg_frame_rate = s1->framerate;
     sti->avctx->framerate = s1->framerate;
     avpriv_set_pts_info(st, 64, 1, 1200000);
 

tests/fate/h264.mak
@@ -346,7 +346,7 @@ fate-h264-conformance-frext-freh7_b:              CMD = framecrc -i $(TARGET_SAM
 fate-h264-conformance-frext-frext01_jvc_d:        CMD = framecrc -i $(TARGET_SAMPLES)/h264-conformance/FRext/FREXT01_JVC_D.264
 fate-h264-conformance-frext-frext02_jvc_c:        CMD = framecrc -i $(TARGET_SAMPLES)/h264-conformance/FRext/FREXT02_JVC_C.264
 fate-h264-conformance-frext-frext1_panasonic_c:   CMD = framecrc -i $(TARGET_SAMPLES)/h264-conformance/FRext/FRExt1_Panasonic.avc
-fate-h264-conformance-frext-frext2_panasonic_b:   CMD = framecrc -i $(TARGET_SAMPLES)/h264-conformance/FRext/FRExt2_Panasonic.avc -vsync passthrough
+fate-h264-conformance-frext-frext2_panasonic_b:   CMD = framecrc -i $(TARGET_SAMPLES)/h264-conformance/FRext/FRExt2_Panasonic.avc
 fate-h264-conformance-frext-frext3_panasonic_d:   CMD = framecrc -i $(TARGET_SAMPLES)/h264-conformance/FRext/FRExt3_Panasonic.avc
 fate-h264-conformance-frext-frext4_panasonic_a:   CMD = framecrc -i $(TARGET_SAMPLES)/h264-conformance/FRext/FRExt4_Panasonic.avc
 fate-h264-conformance-frext-frext_mmco4_sony_b:   CMD = framecrc -i $(TARGET_SAMPLES)/h264-conformance/FRext/FRExt_MMCO4_Sony_B.264

tests/ref/fate/h264-conformance-frext-frext2_panasonic_b
@@ -3,18 +3,18 @@
 #codec_id 0: rawvideo
 #dimensions 0: 352x288
 #sar 0: 0/1
-0,          0,          0,        1,   152064, 0x0147a2a9
-0,          1,          1,        1,   152064, 0xe2365351
-0,          2,          2,        1,   152064, 0xb5f9daee
-0,          3,          3,        1,   152064, 0xd60818b2
-0,          4,          4,        1,   152064, 0x21b528e5
-0,          5,          5,        1,   152064, 0x9c9dda18
-0,          6,          6,        1,   152064, 0x387fe7de
-0,          7,          7,        1,   152064, 0x8f0c5a1e
-0,          8,          8,        1,   152064, 0x02409694
-0,          9,          9,        1,   152064, 0x2b36a7a7
-0,         10,         10,        1,   152064, 0x3c6d3863
-0,         11,         11,        1,   152064, 0xaa7835e1
-0,         12,         12,        1,   152064, 0x6d105fe7
-0,         13,         13,        1,   152064, 0x9a348732
-0,         14,         14,        1,   152064, 0x96a3af13
+0,          1,          1,        1,   152064, 0x0147a2a9
+0,          2,          2,        1,   152064, 0xe2365351
+0,          3,          3,        1,   152064, 0xb5f9daee
+0,          4,          4,        1,   152064, 0xd60818b2
+0,          5,          5,        1,   152064, 0x21b528e5
+0,          6,          6,        1,   152064, 0x9c9dda18
+0,          7,          7,        1,   152064, 0x387fe7de
+0,          8,          8,        1,   152064, 0x8f0c5a1e
+0,          9,          9,        1,   152064, 0x02409694
+0,         10,         10,        1,   152064, 0x2b36a7a7
+0,         11,         11,        1,   152064, 0x3c6d3863
+0,         12,         12,        1,   152064, 0xaa7835e1
+0,         13,         13,        1,   152064, 0x6d105fe7
+0,         14,         14,        1,   152064, 0x9a348732
+0,         15,         15,        1,   152064, 0x96a3af13

tests/ref/fate/h264-conformance-frext-frext4_panasonic_a
@@ -3,13 +3,13 @@
 #codec_id 0: rawvideo
 #dimensions 0: 352x288
 #sar 0: 0/1
-0,          0,          0,        1,   152064, 0xd604d440
-0,          1,          1,        1,   152064, 0x08ef262c
-0,          2,          2,        1,   152064, 0x992fca8e
-0,          3,          3,        1,   152064, 0x5a02ee15
-0,          4,          4,        1,   152064, 0xf15d5c66
-0,          5,          5,        1,   152064, 0x360dda12
-0,          6,          6,        1,   152064, 0x18b54985
-0,          7,          7,        1,   152064, 0xf12b6cde
-0,          8,          8,        1,   152064, 0x3b2e63f7
-0,          9,          9,        1,   152064, 0x140abcfd
+0,          1,          1,        1,   152064, 0xd604d440
+0,          2,          2,        1,   152064, 0x08ef262c
+0,          3,          3,        1,   152064, 0x992fca8e
+0,          4,          4,        1,   152064, 0x5a02ee15
+0,          5,          5,        1,   152064, 0xf15d5c66
+0,          6,          6,        1,   152064, 0x360dda12
+0,          7,          7,        1,   152064, 0x18b54985
+0,          8,          8,        1,   152064, 0xf12b6cde
+0,          9,          9,        1,   152064, 0x3b2e63f7
+0,         10,         10,        1,   152064, 0x140abcfd

tests/ref/fate/hevc-conformance-CIP_A_Panasonic_3
@@ -3,5 +3,5 @@
 #codec_id 0: rawvideo
 #dimensions 0: 416x240
 #sar 0: 0/1
-0,          2,          2,        1,   149760, 0x6e1f16d0
-0,          3,          3,        1,   149760, 0x5be5bde9
+0,          0,          0,        1,   149760, 0x6e1f16d0
+0,          1,          1,        1,   149760, 0x5be5bde9

tests/ref/fate/hevc-conformance-CIP_C_Panasonic_2
@@ -3,5 +3,5 @@
 #codec_id 0: rawvideo
 #dimensions 0: 416x240
 #sar 0: 0/1
-0,          2,          2,        1,   149760, 0xcd1019b2
-0,          3,          3,        1,   149760, 0xeb39efeb
+0,          0,          0,        1,   149760, 0xcd1019b2
+0,          1,          1,        1,   149760, 0xeb39efeb

commit 3d96d83a0a009a3bf566b208b14a7d264f17263b

avformat/rawdec: set framerate in codec parameters
Commit ba4b73c9779c32580f8a3ba08602a5d94e0bcd7c caused a regression in
the usage of avg_frame_rate to detect the frame rate of raw h264/hevc
bitstreams: after the commit, avg_frame_rate is always the value of the
-framerate option (which is set to 25 by default) instead of the actual
frame rate derived from the bitstream SPS/VPS NALUs.

This commit fixes the regression by setting the framerate codec
parameter to the value of the framerate option instead. After this
change, bitstreams without timing information will derive avg_frame_rate
from the -framerate option, while bitstreams with timing information
will derive avg_frame_rate from the bitstream itself.

The h264-bsf-dts2pts test now returns the correct frame durations for a
bitstream with a mix of single-field and double-field frames.

Signed-off-by: Gavin Li <git@thegavinli.com>
Signed-off-by: James Almer <jamrial@gmail.com>

libavformat/rawdec.c
@@ -83,9 +83,9 @@ int ff_raw_video_read_header(AVFormatContext *s)
 
     st->codecpar->codec_type = AVMEDIA_TYPE_VIDEO;
     st->codecpar->codec_id = ffifmt(s->iformat)->raw_codec_id;
+    st->codecpar->framerate = s1->framerate;
     sti->need_parsing = AVSTREAM_PARSE_FULL_RAW;
 
-    st->avg_frame_rate = s1->framerate;
     avpriv_set_pts_info(st, 64, 1, 1200000);
 
 fail:

tests/ref/fate/cavs-demux
@@ -58,5 +58,5 @@ packet|codec_type=video|stream_index=0|pts=2240000|pts_time=1.866667|dts=2240000
 packet|codec_type=video|stream_index=0|pts=2280000|pts_time=1.900000|dts=2280000|dts_time=1.900000|duration=40000|duration_time=0.033333|size=67|pos=172185|flags=___|data_hash=CRC32:42484449
 packet|codec_type=video|stream_index=0|pts=2320000|pts_time=1.933333|dts=2320000|dts_time=1.933333|duration=40000|duration_time=0.033333|size=83|pos=172252|flags=___|data_hash=CRC32:a941bdf0
 packet|codec_type=video|stream_index=0|pts=2360000|pts_time=1.966667|dts=2360000|dts_time=1.966667|duration=40000|duration_time=0.033333|size=5417|pos=172335|flags=___|data_hash=CRC32:9d0d503b
-stream|index=0|codec_name=cavs|profile=unknown|codec_type=video|codec_tag_string=[0][0][0][0]|codec_tag=0x0000|width=1280|height=720|coded_width=1280|coded_height=720|has_b_frames=0|sample_aspect_ratio=N/A|display_aspect_ratio=N/A|pix_fmt=yuv420p|level=-99|color_range=unknown|color_space=unknown|color_transfer=unknown|color_primaries=unknown|chroma_location=unspecified|field_order=unknown|refs=1|id=N/A|r_frame_rate=30/1|avg_frame_rate=25/1|time_base=1/1200000|start_pts=N/A|start_time=N/A|duration_ts=N/A|duration=N/A|bit_rate=N/A|max_bit_rate=N/A|bits_per_raw_sample=N/A|nb_frames=N/A|nb_read_frames=N/A|nb_read_packets=60|extradata_size=18|extradata_hash=CRC32:1255d52e|disposition:default=0|disposition:dub=0|disposition:original=0|disposition:comment=0|disposition:lyrics=0|disposition:karaoke=0|disposition:forced=0|disposition:hearing_impaired=0|disposition:visual_impaired=0|disposition:clean_effects=0|disposition:attached_pic=0|disposition:timed_thumbnails=0|disposition:non_diegetic=0|disposition:captions=0|disposition:descriptions=0|disposition:metadata=0|disposition:dependent=0|disposition:still_image=0|disposition:multilayer=0
+stream|index=0|codec_name=cavs|profile=unknown|codec_type=video|codec_tag_string=[0][0][0][0]|codec_tag=0x0000|width=1280|height=720|coded_width=1280|coded_height=720|has_b_frames=0|sample_aspect_ratio=N/A|display_aspect_ratio=N/A|pix_fmt=yuv420p|level=-99|color_range=unknown|color_space=unknown|color_transfer=unknown|color_primaries=unknown|chroma_location=unspecified|field_order=unknown|refs=1|id=N/A|r_frame_rate=30/1|avg_frame_rate=30/1|time_base=1/1200000|start_pts=N/A|start_time=N/A|duration_ts=N/A|duration=N/A|bit_rate=N/A|max_bit_rate=N/A|bits_per_raw_sample=N/A|nb_frames=N/A|nb_read_frames=N/A|nb_read_packets=60|extradata_size=18|extradata_hash=CRC32:1255d52e|disposition:default=0|disposition:dub=0|disposition:original=0|disposition:comment=0|disposition:lyrics=0|disposition:karaoke=0|disposition:forced=0|disposition:hearing_impaired=0|disposition:visual_impaired=0|disposition:clean_effects=0|disposition:attached_pic=0|disposition:timed_thumbnails=0|disposition:non_diegetic=0|disposition:captions=0|disposition:descriptions=0|disposition:metadata=0|disposition:dependent=0|disposition:still_image=0|disposition:multilayer=0
 format|filename=bunny.mp4|nb_streams=1|nb_programs=0|nb_stream_groups=0|format_name=cavsvideo|start_time=N/A|duration=N/A|size=177752|bit_rate=N/A|probe_score=51

tests/ref/fate/enhanced-flv-hevc-hdr10
@@ -4,7 +4,7 @@
 #codec_id 0: hevc
 #dimensions 0: 1280x720
 #sar 0: 0/1
-0,          0,          0,       40,    77718, 0xb59c83a5
+0,          0,          0,        0,    77718, 0xb59c83a5
 [FRAME]
 media_type=video
 stream_index=0
@@ -17,7 +17,7 @@ best_effort_timestamp=0
 best_effort_timestamp_time=0.000000
 duration=N/A
 duration_time=N/A
-pkt_pos=459
+pkt_pos=439
 pkt_size=77718
 width=1280
 height=720

tests/ref/fate/h264-bsf-dts2pts
@@ -1,5 +1,5 @@
-219edd347ce3151f5b5579d300cd7179 *tests/data/fate/h264-bsf-dts2pts.mov
-243937 tests/data/fate/h264-bsf-dts2pts.mov
+451601038b9091014e45660bc98e09dc *tests/data/fate/h264-bsf-dts2pts.mov
+244033 tests/data/fate/h264-bsf-dts2pts.mov
 #extradata 0:       26, 0x75e2093d
 #tb 0: 1/1200000
 #media_type 0: video
@@ -7,52 +7,52 @@
 #dimensions 0: 352x288
 #sar 0: 0/1
 0,     -48000,          0,    48000,    13686, 0x5ee9bd4c
-0,          0,     240000,    48000,     9320, 0x17224db1, F=0x0
-0,      48000,     288000,    48000,     8903, 0xe394918b, F=0x0
-0,      96000,      96000,    48000,    10108, 0x98418e7e, F=0x0
-0,     144000,     144000,    48000,     2937, 0x49dccb76, F=0x0
-0,     192000,     192000,    48000,     2604, 0xfc8013cd, F=0x0
-0,     240000,     480000,    48000,     7420, 0xcb4155cd, F=0x0
-0,     288000,     528000,    48000,     5664, 0x060bc948, F=0x0
-0,     336000,     336000,    48000,     4859, 0x0a5a8368, F=0x0
-0,     384000,     384000,    48000,     2883, 0xb9639a19, F=0x0
-0,     432000,     432000,    48000,     2547, 0xba95e99d, F=0x0
-0,     480000,     672000,    48000,     4659, 0x19203a0d, F=0x0
-0,     528000,     696000,    48000,     9719, 0xb500c328, F=0x0
-0,     576000,     576000,    48000,     5078, 0x5359c6b8, F=0x0
-0,     624000,     624000,    48000,     5041, 0x88dfcdf1, F=0x0
-0,     672000,     864000,    48000,     9494, 0x29297319, F=0x0
-0,     720000,     720000,    48000,     4772, 0x80273a60, F=0x0
-0,     768000,     768000,    48000,     3237, 0xd99e742c, F=0x0
-0,     816000,     816000,    48000,     2650, 0xc7cc378a, F=0x0
-0,     864000,    1152000,    48000,     6519, 0x142aa357, F=0x0
-0,     912000,    1176000,    48000,     5878, 0xe70d7e21, F=0x0
-0,     960000,     960000,    48000,     2648, 0xe58b1c4b, F=0x0
-0,    1008000,    1008000,    48000,     4522, 0x33ad0882, F=0x0
-0,    1056000,    1056000,    48000,     3246, 0xdbfa539f, F=0x0
-0,    1104000,    1104000,    48000,     3027, 0xdb5bf675, F=0x0
-0,    1152000,    1392000,    48000,     9282, 0x07973603, F=0x0
-0,    1200000,    1200000,    48000,     2786, 0x14824d92, F=0x0
-0,    1248000,    1248000,    48000,     2719, 0x00614eef, F=0x0
-0,    1296000,    1296000,    48000,     2627, 0xe8e91216, F=0x0
-0,    1344000,    1344000,    48000,     2720, 0xbe974fcc, F=0x0
-0,    1392000,    1584000,    48000,     7687, 0x0de01895, F=0x0
-0,    1440000,    1440000,    48000,     5464, 0x113f954d, F=0x0
-0,    1488000,    1488000,    48000,     3482, 0x5c90cdae, F=0x0
-0,    1536000,    1536000,    48000,     2791, 0x4acb702a, F=0x0
-0,    1584000,    1872000,    48000,    11362, 0x13363bdb, F=0x0
-0,    1632000,    1920000,    48000,     2975, 0x99b1e813, F=0x0
-0,    1680000,    1680000,    48000,     2342, 0xe9587867, F=0x0
-0,    1728000,    1728000,    48000,     2634, 0x8d9814fc, F=0x0
-0,    1776000,    1776000,    48000,     2419, 0x033cbb5f, F=0x0
-0,    1824000,    1824000,    48000,     2498, 0x7dd9e476, F=0x0
-0,    1872000,    2112000,    48000,     2668, 0x358e2bd8, F=0x0
-0,    1920000,    2136000,    48000,     9068, 0x3a639927, F=0x0
-0,    1968000,    1968000,    48000,     4939, 0xa5309a8c, F=0x0
-0,    2016000,    2016000,    48000,     2650, 0x2ab82b97, F=0x0
-0,    2064000,    2064000,    48000,     2503, 0xfd97cd4c, F=0x0
-0,    2112000,    2352000,    48000,     5121, 0xaf88e5b8, F=0x0
-0,    2160000,    2160000,    48000,     2643, 0xa1791db0, F=0x0
-0,    2208000,    2208000,    48000,     2637, 0xe1a42510, F=0x0
-0,    2256000,    2256000,    48000,     2633, 0x08430f15, F=0x0
-0,    2304000,    2304000,    48000,     2721, 0xe6756990, F=0x0
+0,          0,     144000,    24000,     9320, 0x17224db1, F=0x0
+0,      24000,     168000,    24000,     8903, 0xe394918b, F=0x0
+0,      48000,      48000,    48000,    10108, 0x98418e7e, F=0x0
+0,      96000,      96000,    24000,     2937, 0x49dccb76, F=0x0
+0,     120000,     120000,    24000,     2604, 0xfc8013cd, F=0x0
+0,     144000,     288000,    24000,     7420, 0xcb4155cd, F=0x0
+0,     168000,     312000,    24000,     5664, 0x060bc948, F=0x0
+0,     192000,     192000,    48000,     4859, 0x0a5a8368, F=0x0
+0,     240000,     240000,    24000,     2883, 0xb9639a19, F=0x0
+0,     264000,     264000,    24000,     2547, 0xba95e99d, F=0x0
+0,     288000,     432000,    24000,     4659, 0x19203a0d, F=0x0
+0,     312000,     456000,    24000,     9719, 0xb500c328, F=0x0
+0,     336000,     336000,    48000,     5078, 0x5359c6b8, F=0x0
+0,     384000,     384000,    48000,     5041, 0x88dfcdf1, F=0x0
+0,     432000,     576000,    48000,     9494, 0x29297319, F=0x0
+0,     480000,     480000,    48000,     4772, 0x80273a60, F=0x0
+0,     528000,     528000,    24000,     3237, 0xd99e742c, F=0x0
+0,     552000,     552000,    24000,     2650, 0xc7cc378a, F=0x0
+0,     576000,     720000,    24000,     6519, 0x142aa357, F=0x0
+0,     600000,     744000,    24000,     5878, 0xe70d7e21, F=0x0
+0,     624000,     624000,    24000,     2648, 0xe58b1c4b, F=0x0
+0,     648000,     648000,    24000,     4522, 0x33ad0882, F=0x0
+0,     672000,     672000,    24000,     3246, 0xdbfa539f, F=0x0
+0,     696000,     696000,    24000,     3027, 0xdb5bf675, F=0x0
+0,     720000,     864000,    48000,     9282, 0x07973603, F=0x0
+0,     768000,     768000,    24000,     2786, 0x14824d92, F=0x0
+0,     792000,     792000,    24000,     2719, 0x00614eef, F=0x0
+0,     816000,     816000,    24000,     2627, 0xe8e91216, F=0x0
+0,     840000,     840000,    24000,     2720, 0xbe974fcc, F=0x0
+0,     864000,    1008000,    48000,     7687, 0x0de01895, F=0x0
+0,     912000,     912000,    48000,     5464, 0x113f954d, F=0x0
+0,     960000,     960000,    24000,     3482, 0x5c90cdae, F=0x0
+0,     984000,     984000,    24000,     2791, 0x4acb702a, F=0x0
+0,    1008000,    1152000,    24000,    11362, 0x13363bdb, F=0x0
+0,    1032000,    1176000,    24000,     2975, 0x99b1e813, F=0x0
+0,    1056000,    1056000,    24000,     2342, 0xe9587867, F=0x0
+0,    1080000,    1080000,    24000,     2634, 0x8d9814fc, F=0x0
+0,    1104000,    1104000,    24000,     2419, 0x033cbb5f, F=0x0
+0,    1128000,    1128000,    24000,     2498, 0x7dd9e476, F=0x0
+0,    1152000,    1296000,    24000,     2668, 0x358e2bd8, F=0x0
+0,    1176000,    1320000,    24000,     9068, 0x3a639927, F=0x0
+0,    1200000,    1200000,    48000,     4939, 0xa5309a8c, F=0x0
+0,    1248000,    1248000,    24000,     2650, 0x2ab82b97, F=0x0
+0,    1272000,    1272000,    24000,     2503, 0xfd97cd4c, F=0x0
+0,    1296000,    1440000,    48000,     5121, 0xaf88e5b8, F=0x0
+0,    1344000,    1344000,    24000,     2643, 0xa1791db0, F=0x0
+0,    1368000,    1368000,    24000,     2637, 0xe1a42510, F=0x0
+0,    1392000,    1392000,    24000,     2633, 0x08430f15, F=0x0
+0,    1416000,    1416000,    24000,     2721, 0xe6756990, F=0x0

tests/ref/fate/h264-conformance-cvpcmnl2_sva_c
@@ -1,7 +1,7 @@
-#tb 0: 1/25
+#tb 0: 1/50
 #media_type 0: video
 #codec_id 0: rawvideo
 #dimensions 0: 1280x720
 #sar 0: 0/1
-0,          0,          0,        1,  1382400, 0xccbe6bf8
-0,          1,          1,        1,  1382400, 0x49c0cfd7
+0,          0,          0,        2,  1382400, 0xccbe6bf8
+0,          2,          2,        2,  1382400, 0x49c0cfd7

tests/ref/fate/hevc-bsf-dts2pts-cra
@@ -1,5 +1,5 @@
-c3c00fdc637a19fa3d23d37d9974d28d *tests/data/fate/hevc-bsf-dts2pts-cra.mov
-103067 tests/data/fate/hevc-bsf-dts2pts-cra.mov
+f6102684fbf13aa624e6edece38c83f2 *tests/data/fate/hevc-bsf-dts2pts-cra.mov
+102971 tests/data/fate/hevc-bsf-dts2pts-cra.mov
 #extradata 0:      118, 0x25f51994
 #tb 0: 1/1200000
 #media_type 0: video
@@ -54,35 +54,35 @@ c3c00fdc637a19fa3d23d37d9974d28d *tests/data/fate/hevc-bsf-dts2pts-cra.mov
 0,    2016000,    2160000,    48000,      892, 0x0d2ab3bc, F=0x0
 0,    2064000,    2112000,    48000,      238, 0x45827561, F=0x0
 0,    2112000,    2208000,    48000,      281, 0x2a3a8e61, F=0x0
-0,    2160000,    2256010,    48000,     4629, 0xf2e0fb0f, F=0x0
-0,    2208000,    2256005,    48000,     1453, 0x6ae5dc98, F=0x0
-0,    2256000,    2256002,        1,      869, 0x3982ae69, F=0x0
-0,    2256001,    2256001,        1,      282, 0xd9e28960, F=0x0
-0,    2256002,    2256004,        2,      259, 0x253a809d, F=0x0
-0,    2256004,    2256007,        1,      835, 0x83499f30, F=0x0
-0,    2256005,    2256006,        1,      255, 0xa77b7690, F=0x0
-0,    2256006,    2256008,        1,      242, 0x83977ccf, F=0x0
-0,    2256007,    2256019,        1,     5082, 0xba55ee51, F=0x0
-0,    2256008,    2256014,        2,     1393, 0xc998b442, F=0x0
-0,    2256010,    2256012,        1,      742, 0x91ab75d2, F=0x0
-0,    2256011,    2256011,        1,      229, 0xfa326d98, F=0x0
-0,    2256012,    2256013,        1,      275, 0x49c38226, F=0x0
-0,    2256013,    2256017,        1,      869, 0xdd05acc4, F=0x0
-0,    2256014,    2256016,        2,      293, 0xcc9e904f, F=0x0
-0,    2256016,    2256018,        1,      334, 0x212aa4b1, F=0x0
-0,    2256017,    2256029,        1,     8539, 0xcccc9eb1
-0,    2256018,    2256024,        1,     1593, 0x5a351a68, F=0x0
-0,    2256019,    2256022,        1,     1042, 0xb77d00cc, F=0x0
-0,    2256020,    2256020,        2,      302, 0xbcdb9750, F=0x0
-0,    2256022,    2256023,        1,      336, 0xc7b0a55d, F=0x0
-0,    2256023,    2256026,        1,      875, 0x7e31b046, F=0x0
-0,    2256024,    2256025,        1,      401, 0xb473bca8, F=0x0
-0,    2256025,    2256028,        1,      246, 0x43357263, F=0x0
-0,    2256026,    2256038,        2,     3254, 0x8be44a2d, F=0x0
-0,    2256028,    2256034,        1,     1151, 0x29d52d14, F=0x0
-0,    2256029,    2256031,        1,      733, 0x33606982, F=0x0
-0,    2256030,    2256030,        1,      234, 0xb70a79ff, F=0x0
-0,    2256031,    2256032,        1,      228, 0x86916848, F=0x0
-0,    2256032,    2256036,        2,      689, 0xcca34b40, F=0x0
-0,    2256034,    2256035,        1,      223, 0xa96f6e31, F=0x0
-0,    2256035,    2256037,        1,      241, 0x7ac17531, F=0x0
+0,    2160000,    2640000,    48000,     4629, 0xf2e0fb0f, F=0x0
+0,    2208000,    2448000,    48000,     1453, 0x6ae5dc98, F=0x0
+0,    2256000,    2352000,    48000,      869, 0x3982ae69, F=0x0
+0,    2304000,    2304000,    48000,      282, 0xd9e28960, F=0x0
+0,    2352000,    2400000,    48000,      259, 0x253a809d, F=0x0
+0,    2400000,    2544000,    48000,      835, 0x83499f30, F=0x0
+0,    2448000,    2496000,    48000,      255, 0xa77b7690, F=0x0
+0,    2496000,    2592000,    48000,      242, 0x83977ccf, F=0x0
+0,    2544000,    3024000,    48000,     5082, 0xba55ee51, F=0x0
+0,    2592000,    2832000,    48000,     1393, 0xc998b442, F=0x0
+0,    2640000,    2736000,    48000,      742, 0x91ab75d2, F=0x0
+0,    2688000,    2688000,    48000,      229, 0xfa326d98, F=0x0
+0,    2736000,    2784000,    48000,      275, 0x49c38226, F=0x0
+0,    2784000,    2928000,    48000,      869, 0xdd05acc4, F=0x0
+0,    2832000,    2880000,    48000,      293, 0xcc9e904f, F=0x0
+0,    2880000,    2976000,    48000,      334, 0x212aa4b1, F=0x0
+0,    2928000,    3408000,    48000,     8539, 0xcccc9eb1
+0,    2976000,    3216000,    48000,     1593, 0x5a351a68, F=0x0
+0,    3024000,    3120000,    48000,     1042, 0xb77d00cc, F=0x0
+0,    3072000,    3072000,    48000,      302, 0xbcdb9750, F=0x0
+0,    3120000,    3168000,    48000,      336, 0xc7b0a55d, F=0x0
+0,    3168000,    3312000,    48000,      875, 0x7e31b046, F=0x0
+0,    3216000,    3264000,    48000,      401, 0xb473bca8, F=0x0
+0,    3264000,    3360000,    48000,      246, 0x43357263, F=0x0
+0,    3312000,    3792000,    48000,     3254, 0x8be44a2d, F=0x0
+0,    3360000,    3600000,    48000,     1151, 0x29d52d14, F=0x0
+0,    3408000,    3504000,    48000,      733, 0x33606982, F=0x0
+0,    3456000,    3456000,    48000,      234, 0xb70a79ff, F=0x0
+0,    3504000,    3552000,    48000,      228, 0x86916848, F=0x0
+0,    3552000,    3696000,    48000,      689, 0xcca34b40, F=0x0
+0,    3600000,    3648000,    48000,      223, 0xa96f6e31, F=0x0
+0,    3648000,    3744000,    48000,      241, 0x7ac17531, F=0x0

tests/ref/fate/hevc-bsf-dts2pts-idr
@@ -1,5 +1,5 @@
-368d177821450241820bf3507d74b35a *tests/data/fate/hevc-bsf-dts2pts-idr.mov
-346603 tests/data/fate/hevc-bsf-dts2pts-idr.mov
+8f02d31fece6a067a8488d8ca6ee3329 *tests/data/fate/hevc-bsf-dts2pts-idr.mov
+346579 tests/data/fate/hevc-bsf-dts2pts-idr.mov
 #extradata 0:      699, 0x9c810c10
 #tb 0: 1/1200000
 #media_type 0: video
@@ -47,7 +47,7 @@
 0,    1680000,    1824000,    48000,     2208, 0x8e2146e8, F=0x0
 0,    1728000,    1776000,    48000,      900, 0x669fb97b, F=0x0
 0,    1776000,    1872000,    48000,      817, 0x3eb689e1, F=0x0
-0,    1824000,    2256001,    48000,    17919, 0x4e83b301, F=0x0
+0,    1824000,    2304000,    48000,    17919, 0x4e83b301, F=0x0
 0,    1872000,    2112000,    48000,     4911, 0x2fc88e1f, F=0x0
 0,    1920000,    2016000,    48000,     2717, 0xcc1f551e, F=0x0
 0,    1968000,    1968000,    48000,      908, 0x43e1b421, F=0x0
@@ -55,19 +55,19 @@
 0,    2064000,    2208000,    48000,     2857, 0xdeee9614, F=0x0
 0,    2112000,    2160000,    48000,     1039, 0x9cf8f494, F=0x0
 0,    2160000,    2256000,    48000,      975, 0x0f4ec85b, F=0x0
-0,    2208000,    2256011,    48000,    18345, 0xf9dabd3b, F=0x0
-0,    2256000,    2256006,        1,     5280, 0xc5c33809, F=0x0
-0,    2256001,    2256004,        1,     2755, 0xda955c75, F=0x0
-0,    2256002,    2256002,        2,     1027, 0xf652eff3, F=0x0
-0,    2256004,    2256005,        1,      901, 0xe23cb716, F=0x0
-0,    2256005,    2256008,        1,     3010, 0x783de5cb, F=0x0
-0,    2256006,    2256007,        1,     1005, 0x91fcf92f, F=0x0
-0,    2256007,    2256010,        1,     1131, 0xd88b2920, F=0x0
-0,    2256008,    2256008,        2,    29018, 0xc9b25608
-0,    2256010,    2256016,        1,     7436, 0x18344e77, F=0x0
-0,    2256011,    2256013,        1,     4050, 0x54fcbee0, F=0x0
-0,    2256012,    2256012,        1,     1412, 0xe249bd86, F=0x0
-0,    2256013,    2256014,        1,      977, 0xa749dc21, F=0x0
-0,    2256014,    2256014,        2,     2953, 0xba90c008, F=0x0
-0,    2256016,    2256017,        1,     1004, 0xddebea9a, F=0x0
-0,    2256017,    2256017,        1,     1109, 0x7e081570, F=0x0
+0,    2208000,    2688000,    48000,    18345, 0xf9dabd3b, F=0x0
+0,    2256000,    2496000,    48000,     5280, 0xc5c33809, F=0x0
+0,    2304000,    2400000,    48000,     2755, 0xda955c75, F=0x0
+0,    2352000,    2352000,    48000,     1027, 0xf652eff3, F=0x0
+0,    2400000,    2448000,    48000,      901, 0xe23cb716, F=0x0
+0,    2448000,    2592000,    48000,     3010, 0x783de5cb, F=0x0
+0,    2496000,    2544000,    48000,     1005, 0x91fcf92f, F=0x0
+0,    2544000,    2640000,    48000,     1131, 0xd88b2920, F=0x0
+0,    2592000,    2592000,    48000,    29018, 0xc9b25608
+0,    2640000,    2880000,    48000,     7436, 0x18344e77, F=0x0
+0,    2688000,    2784000,    48000,     4050, 0x54fcbee0, F=0x0
+0,    2736000,    2736000,    48000,     1412, 0xe249bd86, F=0x0
+0,    2784000,    2832000,        1,      977, 0xa749dc21, F=0x0
+0,    2784001,    2784001,    95999,     2953, 0xba90c008, F=0x0
+0,    2880000,    2928000,        1,     1004, 0xddebea9a, F=0x0
+0,    2880001,    2880001,    48000,     1109, 0x7e081570, F=0x0

tests/ref/fate/hevc-bsf-dts2pts-idr-cra
@@ -1,5 +1,5 @@
-07a216d6537502705348fea392d5d73d *tests/data/fate/hevc-bsf-dts2pts-idr-cra.mov
-375266 tests/data/fate/hevc-bsf-dts2pts-idr-cra.mov
+ed8d735b9a2780ce8ada61fd31c68886 *tests/data/fate/hevc-bsf-dts2pts-idr-cra.mov
+375210 tests/data/fate/hevc-bsf-dts2pts-idr-cra.mov
 #extradata 0:      648, 0x30a7fa5c
 #tb 0: 1/1200000
 #media_type 0: video
@@ -47,7 +47,7 @@
 0,    1680000,    1824000,    48000,     2674, 0x296fd411, F=0x0
 0,    1728000,    1776000,    48000,     1420, 0x304d6168, F=0x0
 0,    1776000,    1872000,    48000,     1364, 0x48734084, F=0x0
-0,    1824000,    2256001,    48000,    19349, 0xda0944f0, F=0x0
+0,    1824000,    2304000,    48000,    19349, 0xda0944f0, F=0x0
 0,    1872000,    2112000,    48000,     5550, 0x9c346b7b, F=0x0
 0,    1920000,    2016000,    48000,     3180, 0x5368f72b, F=0x0
 0,    1968000,    1968000,    48000,     1475, 0x369b7aae, F=0x0
@@ -55,19 +55,19 @@
 0,    2064000,    2208000,    48000,     3365, 0x81d511a6, F=0x0
 0,    2112000,    2160000,    48000,     1575, 0xba81b0a5, F=0x0
 0,    2160000,    2256000,    48000,     1538, 0xf1199439, F=0x0
-0,    2208000,    2256011,    48000,    20138, 0x101fb220, F=0x0
-0,    2256000,    2256006,        1,     5806, 0xcc500158, F=0x0
-0,    2256001,    2256004,        1,     3267, 0xf8f0d6cd, F=0x0
-0,    2256002,    2256002,        2,     1557, 0x894faf3e, F=0x0
-0,    2256004,    2256005,        1,     1482, 0x6b1884b7, F=0x0
-0,    2256005,    2256008,        1,     3513, 0x81227a59, F=0x0
-0,    2256006,    2256007,        1,     1576, 0x855eabd8, F=0x0
-0,    2256007,    2256010,        1,     1668, 0x030ade2e, F=0x0
-0,    2256008,    2256008,        2,    32088, 0xfadbf5f6
-0,    2256010,    2256016,        1,     5921, 0x21fb4976, F=0x0
-0,    2256011,    2256013,        1,     3436, 0x92085cd6, F=0x0
-0,    2256012,    2256012,        1,     1613, 0x1e0cb7c6, F=0x0
-0,    2256013,    2256014,        1,     1483, 0x88d18a4e, F=0x0
-0,    2256014,    2256018,        2,     3540, 0xc57c8e7b, F=0x0
-0,    2256016,    2256017,        1,     1561, 0x9740a363, F=0x0
-0,    2256017,    2256019,        1,     1651, 0x18e3d52d, F=0x0
+0,    2208000,    2688000,    48000,    20138, 0x101fb220, F=0x0
+0,    2256000,    2496000,    48000,     5806, 0xcc500158, F=0x0
+0,    2304000,    2400000,    48000,     3267, 0xf8f0d6cd, F=0x0
+0,    2352000,    2352000,    48000,     1557, 0x894faf3e, F=0x0
+0,    2400000,    2448000,    48000,     1482, 0x6b1884b7, F=0x0
+0,    2448000,    2592000,    48000,     3513, 0x81227a59, F=0x0
+0,    2496000,    2544000,    48000,     1576, 0x855eabd8, F=0x0
+0,    2544000,    2640000,    48000,     1668, 0x030ade2e, F=0x0
+0,    2592000,    2592000,    48000,    32088, 0xfadbf5f6
+0,    2640000,    2880000,    48000,     5921, 0x21fb4976, F=0x0
+0,    2688000,    2784000,    48000,     3436, 0x92085cd6, F=0x0
+0,    2736000,    2736000,    48000,     1613, 0x1e0cb7c6, F=0x0
+0,    2784000,    2832000,    48000,     1483, 0x88d18a4e, F=0x0
+0,    2832000,    2976000,    48000,     3540, 0xc57c8e7b, F=0x0
+0,    2880000,    2928000,    48000,     1561, 0x9740a363, F=0x0
+0,    2928000,    3024000,    48000,     1651, 0x18e3d52d, F=0x0

tests/ref/lavf-fate/hevc.mp4
@@ -1,3 +1,3 @@
-37b3a3e84df2350380b05b2af4dc97f5 *tests/data/lavf-fate/lavf.hevc.mp4
+539b0f6daece6656609de0898a1f3d42 *tests/data/lavf-fate/lavf.hevc.mp4
 151340 tests/data/lavf-fate/lavf.hevc.mp4
 tests/data/lavf-fate/lavf.hevc.mp4 CRC=0xc0a771de