FFmpeg/FFmpeg

commit a2a67282859904283f1008e1ce0a7f6bc1e71757

avformat/iamf_parse: ensure each layout in an scalable channel representation has an increasing number of channels
Fixes issue #21013

Signed-off-by: James Almer <jamrial@gmail.com>
(cherry picked from commit 554ae5ada98cd44686ade995102d3aa2295d7b09)

libavformat/iamf_parse.c
@@ -398,6 +398,9 @@ static int scalable_channel_layout_config(void *s, AVIOContext *pb,
                                                           .nb_channels = substream_count +
                                                                          coupled_substream_count };
 
+        if (i && ch_layout.nb_channels <= audio_element->element->layers[i-1]->ch_layout.nb_channels)
+            return AVERROR_INVALIDDATA;
+
         for (int j = 0; j < substream_count; j++) {
             IAMFSubStream *substream = &audio_element->substreams[k++];
 

commit f65fc0b13754c94d6185b1b18e604b8129d4f774

avformat/iamf_parse: fix compilation error
Regression since a2a67282859904283f1008e1ce0a7f6bc1e71757.

Signed-off-by: James Almer <jamrial@gmail.com>

libavformat/iamf_parse.c
@@ -398,7 +398,7 @@ static int scalable_channel_layout_config(void *s, AVIOContext *pb,
                                                           .nb_channels = substream_count +
                                                                          coupled_substream_count };
 
-        if (i && ch_layout.nb_channels <= audio_element->element->layers[i-1]->ch_layout.nb_channels)
+        if (i && layer->ch_layout.nb_channels <= audio_element->element->layers[i-1]->ch_layout.nb_channels)
             return AVERROR_INVALIDDATA;
 
         for (int j = 0; j < substream_count; j++) {