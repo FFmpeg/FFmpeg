71db4f3cc1a682cab77343707d383d22aa4b426b
Rémi Denis-Courmont <remi@remlab.net>
2025-12-11 17:24:28 +0200

    lavc/llvidencdsp: R-V V sub_median_pred
    SpacemiT X60:
    sub_median_pred_c:                                  297862.8 ( 1.00x)
    sub_median_pred_rvb_b:                              101992.2 ( 2.92x)
    sub_median_pred_rvv_i32:                              4820.0 (61.80x)

--- libavcodec/riscv/llvidencdsp_init.c
+++ libavcodec/riscv/llvidencdsp_init.c
@@ -32,6 +32,9 @@ void ff_llvidenc_sub_median_pred_rvb(uint8_t *dst, const uint8_t *src1,
                                      int *left, int *left_top);
 void ff_llvidenc_diff_bytes_rvv(uint8_t *dst, const uint8_t *src1,
                                 const uint8_t *src2, intptr_t w);
+void ff_llvidenc_sub_median_pred_rvv(uint8_t *dst, const uint8_t *src1,
+                                     const uint8_t *src2, intptr_t width,
+                                     int *left, int *left_top);
 void ff_llvidenc_sub_left_predict_rvv(uint8_t *dst, const uint8_t *src,
                                       ptrdiff_t stride, ptrdiff_t width,
                                       int height);
@@ -47,6 +50,7 @@ av_cold void ff_llvidencdsp_init_riscv(LLVidEncDSPContext *c)
 #if HAVE_RVV
     if (flags & AV_CPU_FLAG_RVV_I32) {
         c->diff_bytes = ff_llvidenc_diff_bytes_rvv;
+        c->sub_median_pred = ff_llvidenc_sub_median_pred_rvv;
         c->sub_left_predict = ff_llvidenc_sub_left_predict_rvv;
     }
 #endif
