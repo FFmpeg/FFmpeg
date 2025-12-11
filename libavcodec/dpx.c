b6655e9594945c1a0666029a9206c066b71f73b5
James Almer <jamrial@gmail.com>
2025-12-11 16:21:23 -0300

    avcodec/dpx: make the lack of break in a switch case explicit
    Should fix CID 1676036
    
    Signed-off-by: James Almer <jamrial@gmail.com>

--- libavcodec/dpx.c
+++ libavcodec/dpx.c
@@ -237,6 +237,7 @@ static void unpack_frame(AVCodecContext *avctx, AVFrame *p, const uint8_t *buf,
         break;
     case 16:
         elements *= 2;
+    // fall-through
     case 8:
         if (   avctx->pix_fmt == AV_PIX_FMT_YUVA444P
             || avctx->pix_fmt == AV_PIX_FMT_YUV444P) {
