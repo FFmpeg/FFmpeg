ccd7e66f9e2cf41b06b5f44e82843ecc03dbb2f4
Rémi Denis-Courmont <remi@remlab.net>
2025-12-14 11:19:21 +0200

    lavc/mathops: remove bespoke Arm mid_pred()
    The C codegen is as good if not slightly better than the assembler at
    this point.

--- libavcodec/arm/mathops.h
+++ libavcodec/arm/mathops.h
@@ -81,28 +81,6 @@ static inline av_const int MUL16(int ra, int rb)
 
 #endif
 
-#define mid_pred mid_pred
-static inline av_const int mid_pred(int a, int b, int c)
-{
-    int m;
-    __asm__ (
-        "mov   %0, %2  \n\t"
-        "cmp   %1, %2  \n\t"
-        "itt   gt      \n\t"
-        "movgt %0, %1  \n\t"
-        "movgt %1, %2  \n\t"
-        "cmp   %1, %3  \n\t"
-        "it    le      \n\t"
-        "movle %1, %3  \n\t"
-        "cmp   %0, %1  \n\t"
-        "it    gt      \n\t"
-        "movgt %0, %1  \n\t"
-        : "=&r"(m), "+r"(a)
-        : "r"(b), "r"(c)
-        : "cc");
-    return m;
-}
-
 #endif /* HAVE_INLINE_ASM */
 
 #endif /* AVCODEC_ARM_MATHOPS_H */
