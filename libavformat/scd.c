34dbe2c487329551e5699f9795f6b99453eed651
Michael Niedermayer <michael@niedermayer.cc>
2026-02-08 00:19:12 +0100

    avformat/scd: Use ffio_read_size()
    Fixes: use of uninitialized memory
    Fixes: 471771529/clusterfuzz-testcase-minimized-ffmpeg_dem_SCD_fuzzer-5328203515494400
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit a5007428e853c5a61e4ac3c6e24fa0304c3d0c1e)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavformat/scd.c
+++ libavformat/scd.c
@@ -30,6 +30,7 @@
 #include "libavutil/mem.h"
 #include "libavformat/internal.h"
 #include "avformat.h"
+#include "avio_internal.h"
 #include "demux.h"
 
 #define SCD_MAGIC              ((uint64_t)MKBETAG('S', 'E', 'D', 'B') << 32 | \
@@ -121,7 +122,7 @@ static int scd_read_offsets(AVFormatContext *s)
     SCDDemuxContext  *ctx = s->priv_data;
     uint8_t buf[SCD_OFFSET_HEADER_SIZE];
 
-    if ((ret = avio_read(s->pb, buf, SCD_OFFSET_HEADER_SIZE)) < 0)
+    if ((ret = ffio_read_size(s->pb, buf, SCD_OFFSET_HEADER_SIZE)) < 0)
         return ret;
 
     ctx->hdr.table0.count  = AV_RB16(buf +  0);
