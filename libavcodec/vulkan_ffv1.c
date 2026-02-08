6971ba9075387fa5f8b81494fb91491a472b28de
Lynne <dev@lynne.ee>
2026-02-08 07:46:40 +0100

    vulkan_ffv1: improve decode report results printout

--- libavcodec/vulkan_ffv1.c
+++ libavcodec/vulkan_ffv1.c
@@ -885,15 +885,20 @@ static void vk_ffv1_free_frame_priv(AVRefStructOpaque _hwctx, void *data)
                                      1, &invalidate_data);
     }
 
+    int slice_error_cnt = 0;
+    int crc_mismatch_cnt = 0;
     for (int i = 0; i < fp->slice_num; i++) {
         uint32_t crc_res = 0;
         if (fp->crc_checked)
             crc_res = AV_RN32(slice_status->mapped_mem + 2*i*sizeof(uint32_t) + 0);
         uint32_t status = AV_RN32(slice_status->mapped_mem + 2*i*sizeof(uint32_t) + 4);
-        if (status || crc_res)
-            av_log(dev_ctx, AV_LOG_ERROR, "Slice %i status: 0x%x, CRC 0x%x\n",
-                   i, status, crc_res);
+        slice_error_cnt += !!status;
+        crc_mismatch_cnt += !!crc_res;
     }
+    if (slice_error_cnt || crc_mismatch_cnt)
+        av_log(dev_ctx, AV_LOG_ERROR, "Decode status: %i slices errored, "
+                                      "%i CRCs mismatched\n",
+               slice_error_cnt, crc_mismatch_cnt);
 
     av_buffer_unref(&fp->slice_state);
     av_buffer_unref(&fp->slice_offset_buf);
