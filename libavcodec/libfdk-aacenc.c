e3061be971395f126822a0381d1315a5d61c6846
James Almer <jamrial@gmail.com>
2026-02-13 13:01:28 -0300

    avcodec/libfdk-aacenc: rescale packet duration when calculating discarded samples
    Signed-off-by: James Almer <jamrial@gmail.com>

--- libavcodec/libfdk-aacenc.c
+++ libavcodec/libfdk-aacenc.c
@@ -530,7 +530,7 @@ static int aac_encode_frame(AVCodecContext *avctx, AVPacket *avpkt,
     ff_af_queue_remove(&s->afq, avctx->frame_size, &avpkt->pts,
                        &avpkt->duration);
 
-    discard_padding = avctx->frame_size - avpkt->duration;
+    discard_padding = avctx->frame_size - ff_samples_from_time_base(avctx, avpkt->duration);
     // Check if subtraction resulted in an overflow
     if ((discard_padding < avctx->frame_size) != (avpkt->duration > 0)) {
         av_log(avctx, AV_LOG_ERROR, "discard padding overflow\n");
