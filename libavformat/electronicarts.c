720d5c3c51521183fdaf0b0c6de52ff5ce00003e
Marvin Scholz <epirat07@gmail.com>
2026-02-18 00:15:43 +0100

    avformat/electronicarts: add fall-through annotations

--- libavformat/electronicarts.c
+++ libavformat/electronicarts.c
@@ -27,6 +27,7 @@
 
 #include <inttypes.h>
 
+#include "libavutil/attributes.h"
 #include "libavutil/intreadwrite.h"
 #include "libavutil/opt.h"
 #include "avformat.h"
@@ -614,6 +615,7 @@ static int ea_read_packet(AVFormatContext *s, AVPacket *pkt)
                 return AVERROR_INVALIDDATA;
             avio_skip(pb, 32);
             chunk_size -= 32;
+            av_fallthrough;
         case ISNd_TAG:
         case SCDl_TAG:
         case SNDC_TAG:
@@ -709,6 +711,7 @@ static int ea_read_packet(AVFormatContext *s, AVPacket *pkt)
         case TGQs_TAG:
         case MADk_TAG:
             key = AV_PKT_FLAG_KEY;
+            av_fallthrough;
         case MVIf_TAG:
         case fVGT_TAG:
         case MADm_TAG:
@@ -732,6 +735,7 @@ static int ea_read_packet(AVFormatContext *s, AVPacket *pkt)
         case MPCh_TAG:
         case pIQT_TAG:
             key = AV_PKT_FLAG_KEY;
+            av_fallthrough;
         case MV0F_TAG:
         case AV0F_TAG:
 get_video_packet:
