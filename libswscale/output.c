ebc1d9d63bc4478cbc65b4e5977ba513e8e46b15
Michael Niedermayer <michael@niedermayer.cc>
2026-03-03 18:23:39 +0100

    swscale/output: Fixes integer overflow in yuv2planeX_8_c
    Fixes: integer overflow (does not replicate, but looks like it should overflow with some craftet parameters)
    Fixes: #21584
    
    Found-by: HAORAN FANG
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit a59180022af5447d8b2847f539fb99c859175e95)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libswscale/output.c
+++ libswscale/output.c
@@ -473,8 +473,10 @@ static void yuv2planeX_8_c(const int16_t *filter, int filterSize,
     for (i=0; i<dstW; i++) {
         int val = dither[(i + offset) & 7] << 12;
         int j;
-        for (j=0; j<filterSize; j++)
-            val += src[j][i] * filter[j];
+        for (j=0; j<filterSize; j++) {
+            val += (unsigned)(src[j][i] * filter[j]);
+
+        }
 
         dest[i]= av_clip_uint8(val>>19);
     }
