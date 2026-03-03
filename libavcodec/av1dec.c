b05168310d51248065c57ff609af7628c1cd78a7
James Almer <jamrial@gmail.com>
2026-03-03 18:36:17 -0300

    avcodec/av1dec: sync frame header and tile group behavior with CBS
    A new Sequence Header or a Temporal Delimiter OBU invalidate any previous frame
    if not yet complete (As is the case of missing Tile Groups).
    Similarly, a new Frame Header invalidates any onging Tile Group parsing.
    
    Fixes: out of array access
    Fixes: av1dec_tile_desync.mp4
    Fixes: av1dec_tile_desync_bypass.mp4
    
    Signed-off-by: James Almer <jamrial@gmail.com>
    (cherry picked from commit a1496ced654e7dca9a426d263d35f9c39b7b4b88)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/av1dec.c
+++ libavcodec/av1dec.c
@@ -1300,6 +1300,8 @@ static int av1_receive_frame_internal(AVCodecContext *avctx, AVFrame *frame)
             ff_refstruct_replace(&s->seq_ref, unit->content_ref);
 
             s->raw_seq = &obu->obu.sequence_header;
+            s->raw_frame_header = NULL;
+            raw_tile_group      = NULL;
 
             ret = set_context_with_sequence(avctx, s->raw_seq);
             if (ret < 0) {
@@ -1326,6 +1328,8 @@ static int av1_receive_frame_internal(AVCodecContext *avctx, AVFrame *frame)
             }
 
             ff_refstruct_replace(&s->header_ref, unit->content_ref);
+
+            raw_tile_group      = NULL;
 
             if (unit->type == AV1_OBU_FRAME)
                 s->raw_frame_header = &obu->obu.frame.header;
@@ -1398,8 +1402,11 @@ static int av1_receive_frame_internal(AVCodecContext *avctx, AVFrame *frame)
                 }
             }
             break;
-        case AV1_OBU_TILE_LIST:
         case AV1_OBU_TEMPORAL_DELIMITER:
+            s->raw_frame_header = NULL;
+            raw_tile_group      = NULL;
+        // fall-through
+        case AV1_OBU_TILE_LIST:
         case AV1_OBU_PADDING:
             break;
         case AV1_OBU_METADATA:
