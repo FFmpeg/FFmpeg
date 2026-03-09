d09f5815d79b866d6c5932e1088c5c18ec835d7e
Andreas Rheinhardt <andreas.rheinhardt@outlook.com>
2026-03-09 13:18:01 +0100

    fftools/ffmpeg_opt: Remove disabled and deprecated adrift_threshold
    Deprecated in commit 5a04aae82193d75b8f8814dc7e35f4cc84b1beba
    on 2023-05-02.
    
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

--- fftools/ffmpeg_opt.c
+++ fftools/ffmpeg_opt.c
@@ -1597,14 +1597,6 @@ int opt_timelimit(void *optctx, const char *opt, const char *arg)
     return 0;
 }
 
-#if FFMPEG_OPT_ADRIFT_THRESHOLD
-static int opt_adrift_threshold(void *optctx, const char *opt, const char *arg)
-{
-    av_log(NULL, AV_LOG_WARNING, "Option -%s is deprecated and has no effect\n", opt);
-    return 0;
-}
-#endif
-
 static const char *const alt_channel_layout[] = { "ch_layout", NULL};
 static const char *const alt_codec[]          = { "c", "acodec", "vcodec", "scodec", "dcodec", NULL };
 static const char *const alt_filter[]         = { "af", "vf", NULL };
@@ -2174,11 +2166,6 @@ const OptionDef options[] = {
         "set hardware device used when filtering", "device" },
 
     // deprecated options
-#if FFMPEG_OPT_ADRIFT_THRESHOLD
-    { "adrift_threshold", OPT_TYPE_FUNC, OPT_FUNC_ARG | OPT_EXPERT,
-        { .func_arg = opt_adrift_threshold },
-        "deprecated, does nothing", "threshold" },
-#endif
 #if FFMPEG_OPT_TOP
     { "top", OPT_TYPE_INT,     OPT_VIDEO | OPT_EXPERT | OPT_PERSTREAM | OPT_INPUT | OPT_OUTPUT,
         { .off = OFFSET(top_field_first) },
