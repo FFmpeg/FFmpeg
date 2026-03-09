7e5eb2b65ce5e1b16a8b2bb406ed947c094214dd
zhanghongyuan <zhanghongyuan@uniontech.com>
2026-03-09 23:08:01 +0800

    fftools/opt_common: Use enum for encoder/decoder selection
    Replace magic numbers (0 and 1) with SHOW_DECODER and SHOW_ENCODER
    enum values throughout the opt_common.c file.

--- fftools/opt_common.c
+++ fftools/opt_common.c
@@ -68,6 +68,11 @@ enum show_muxdemuxers {
     SHOW_MUXERS,
 };
 
+enum show_codec {
+    SHOW_DECODER,
+    SHOW_ENCODER,
+};
+
 static FILE *report_file;
 static int report_file_level = AV_LOG_DEBUG;
 
@@ -591,9 +596,9 @@ int show_help(void *optctx, const char *opt, const char *arg)
     if (!*topic) {
         show_help_default(topic, par);
     } else if (!strcmp(topic, "decoder")) {
-        show_help_codec(par, 0);
+        show_help_codec(par, SHOW_DECODER);
     } else if (!strcmp(topic, "encoder")) {
-        show_help_codec(par, 1);
+        show_help_codec(par, SHOW_ENCODER);
     } else if (!strcmp(topic, "demuxer")) {
         show_help_demuxer(par);
     } else if (!strcmp(topic, "muxer")) {
@@ -708,16 +713,16 @@ int show_codecs(void *optctx, const char *opt, const char *arg)
 
         /* print decoders/encoders when there's more than one or their
          * names are different from codec name */
-        while ((codec = next_codec_for_id(desc->id, &iter, 0))) {
+        while ((codec = next_codec_for_id(desc->id, &iter, SHOW_DECODER))) {
             if (strcmp(codec->name, desc->name)) {
-                print_codecs_for_id(desc->id, 0);
+                print_codecs_for_id(desc->id, SHOW_DECODER);
                 break;
             }
         }
         iter = NULL;
-        while ((codec = next_codec_for_id(desc->id, &iter, 1))) {
+        while ((codec = next_codec_for_id(desc->id, &iter, SHOW_ENCODER))) {
             if (strcmp(codec->name, desc->name)) {
-                print_codecs_for_id(desc->id, 1);
+                print_codecs_for_id(desc->id, SHOW_ENCODER);
                 break;
             }
         }
@@ -774,12 +779,12 @@ static int print_codecs(int encoder)
 
 int show_decoders(void *optctx, const char *opt, const char *arg)
 {
-    return print_codecs(0);
+    return print_codecs(SHOW_DECODER);
 }
 
 int show_encoders(void *optctx, const char *opt, const char *arg)
 {
-    return print_codecs(1);
+    return print_codecs(SHOW_ENCODER);
 }
 
 int show_bsfs(void *optctx, const char *opt, const char *arg)
@@ -876,7 +881,7 @@ static int show_formats_devices(void *optctx, const char *opt, const char *arg, 
         const char *name      = NULL;
         const char *long_name = NULL;
 
-        if (muxdemuxers !=SHOW_DEMUXERS) {
+        if (muxdemuxers != SHOW_DEMUXERS) {
             ofmt_opaque = NULL;
             while ((ofmt = av_muxer_iterate(&ofmt_opaque))) {
                 is_dev = is_device(ofmt->priv_class);
