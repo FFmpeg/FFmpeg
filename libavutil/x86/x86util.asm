FFmpeg/FFmpeg

commit 80cd0677158b7ea4394d8e1594c7a1c984184f5b

avutil/x86util: don't produce empty object files on win{32,64}
In cases when preprocesor would remove all code, nasm would produce
empty object files. This is technically not wrong, but often cause
issues with various tooling:

* NASM fails to emit CodeView debug info when there is no code [1]
* Older VS2022 builds hangs on empty files [2]
* GNU binutils `strip` errors when there is no sections [3]
error: the input file '.o' has no sections

Workaround those issues by adding dummy byte in COMDAT section,
which is then dropped by linker, as the `__x86util_notref` symbol is not
referenced from C. [4] IMAGE_COMDAT_SELECT_ANY (2) is used to allow
multiple symbol definition.

This is limited to win{32,64} as this is the target where issues were
observed.

[1] https://github.com/netwide-assembler/nasm/issues/216
[2] https://developercommunity.visualstudio.com/t/MSVC-Hangs-when-compiling-ffmpeg-When-l/10233953
[3] https://trac.ffmpeg.org/ticket/6711
[4] https://www.nasm.us/docs/3.01/nasm09.html#section-9.6.1

Signed-off-by: Kacper Michajłow <kasper93@gmail.com>

libavutil/x86/x86util.asm
@@ -1022,3 +1022,15 @@
     pxor  %1, %2
 %endif
 %endmacro
+
+; NASM panics when emitting CodeView debug info for an empty translation unit.
+; GNU binutils `strip` and some other tools such as older MSVC linker also fail
+; on such files. Emit a dummy byte in a COMDAT section to work around this.
+; The linker will discard it since __x86util_notref is not referenced anywhere.
+%ifidn __OUTPUT_FORMAT__,win64
+    section .text$1 comdat=2:__x86util_notref
+        db 0
+%elifidn __OUTPUT_FORMAT__,win32
+    section .text$1 comdat=2:__x86util_notref
+        db 0
+%endif

commit e54e1179985145a2e0a5be9ee9000384ee58b7a5

avutil/x86/x86util: define .text section additionally to COMDAT one
This is needed to cover the case when assembled source doesn't have
.text section. NASM documentation suggest to add $ suffix to section
name for COMDAT in .text, but this actually requires the main .text
section to exist also. And use less generic suffix for our dummy
sub-section.

Third time's the charm.

Fixes: 80cd0677158b7ea4394d8e1594c7a1c984184f5b
Signed-off-by: Kacper Michajłow <kasper93@gmail.com>

libavutil/x86/x86util.asm
@@ -1028,9 +1028,11 @@
 ; on such files. Emit a dummy byte in a COMDAT section to work around this.
 ; The linker will discard it since __x86util_notref is not referenced anywhere.
 %ifidn __OUTPUT_FORMAT__,win64
-    section .text$1 comdat=2:__x86util_notref
+    section .text
+    section .text$__x86util_notref comdat=2:__x86util_notref
         db 0
 %elifidn __OUTPUT_FORMAT__,win32
-    section .text$1 comdat=2:__x86util_notref
+    section .text
+    section .text$__x86util_notref comdat=2:__x86util_notref
         db 0
 %endif