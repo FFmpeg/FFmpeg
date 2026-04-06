e97f52e55710b4510b22e551d79088c6fbf39072
Andreas Rheinhardt <andreas.rheinhardt@outlook.com>
2026-04-06 19:24:49 +0200

    avcodec/g726: Fix indentation
    Forgotten in e344c1ea36f228e2987e25327638771c4cedcd33.
    
    Reviewed-by: Lynne <dev@lynne.ee>
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

--- libavcodec/g726.c
+++ libavcodec/g726.c
@@ -59,12 +59,10 @@ static inline Float11* i2f(int i, Float11* f)
 
 static inline int16_t mult(Float11* f1, Float11* f2)
 {
-        int res, exp;
-
-        exp = f1->exp + f2->exp;
-        res = (((f1->mant * f2->mant) + 0x30) >> 4);
-        res = exp > 19 ? res << (exp - 19) : res >> (19 - exp);
-        return (f1->sign ^ f2->sign) ? -res : res;
+    int exp = f1->exp + f2->exp;
+    int res = (((f1->mant * f2->mant) + 0x30) >> 4);
+    res = exp > 19 ? res << (exp - 19) : res >> (19 - exp);
+    return (f1->sign ^ f2->sign) ? -res : res;
 }
 
 static inline int sgn(int value)
