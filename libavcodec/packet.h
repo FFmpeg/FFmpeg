5f6dff5e7dbd9b1f3221f7c37225365dbf1e1038
Romain Beauxis <romain.beauxis@gmail.com>
2026-04-22 09:54:18 -0500

    avcodec/packet: add AV_PKT_DATA_HEVC_CONF side data type
    Carries a raw HEVCDecoderConfigurationRecord for the Dolby Vision
    enhancement layer, parsed from the hvcE box (ISOM) or the corresponding
    BlockAdditionMapping (Matroska).

--- libavcodec/packet.h
+++ libavcodec/packet.h
@@ -376,6 +376,14 @@ enum AVPacketSideDataType {
     AV_PKT_DATA_DYNAMIC_HDR_SMPTE_2094_APP5,
 
     /**
+     * Dolby Vision enhancement-layer HEVC decoder configuration.
+     * Parsed from the @c hvcE box in ISOM-based containers or the
+     * corresponding BlockAdditionMapping in Matroska. The data is a raw
+     * HEVCDecoderConfigurationRecord as defined in ISO 14496-15.
+     */
+    AV_PKT_DATA_HEVC_CONF,
+
+    /**
      * The number of side data types.
      * This is not part of the public API/ABI in the sense that it may
      * change when new side data types are added.
