cc1459ed8559307bf94326343b7d4fbd64cc7d5e
Michael Niedermayer <michael@niedermayer.cc>
2026-05-18 23:37:12 +0200

    avformat/flvdec: Check size at the top of the main loop
    Fixes: signed integer overflow
    Fixes: ffmpeg_flv_read_packet_libavformat_flvdec.c_1900_26.poc
    
    Found-by: iceray-Li
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavformat/flvdec.c
+++ libavformat/flvdec.c
@@ -1565,6 +1565,10 @@ skip:
 
     for (;;) {
         int track_size = size;
+        if (size < 0) {
+            ret = FFERROR_REDO;
+            goto leave;
+        }
 
         if (multitrack_type != MultitrackTypeOneTrack) {
             track_size = avio_rb24(s->pb);
