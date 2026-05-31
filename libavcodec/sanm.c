874de02d87f68a1014c01461c29e52726bd7805e
Michael Niedermayer <michael@niedermayer.cc>
2026-05-31 16:45:47 +0200

    avcodec/sanm: reject codec37 frames taller than the allocated buffer
    No testcase
    
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit ab5043f055de39e34d21c9acfe0ec1e115af94bf)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/sanm.c
+++ libavcodec/sanm.c
@@ -1228,6 +1228,9 @@ static int old_codec37(SANMVideoContext *ctx, GetByteContext *gb, int top, int l
     if (width > ctx->aligned_width)
         return AVERROR_INVALIDDATA;
 
+    if (FFALIGN(height, 4) > ctx->aligned_height)
+        return AVERROR_INVALIDDATA;
+
     if (bytestream2_get_bytes_left(gb) < 16)
         return AVERROR_INVALIDDATA;
 
