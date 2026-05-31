cadf292e61f99b6746c83352c3571012bd0af8ce
Michael Niedermayer <michael@niedermayer.cc>
2026-05-31 19:50:37 +0200

    avcodec/aac/aacdec: reject decoded frame without a valid sample rate
    Later code will turn this into AVERROR_BUG
    
    When returning sample_rate == 0 samples is considered a bug, we have no
    nice choice but to error out cleanly
    
    Fixes: assertion failure
    Fixes: ffmpeg_AV_CODEC_ID_AAC_DEC_fuzzer crash-0a86d46fef2442b222ee34403c21f7f582ffccb0
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit e711e608273e0d7d195d2cb1912fb06d0772d96c)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/aac/aacdec.c
+++ libavcodec/aac/aacdec.c
@@ -2381,6 +2381,12 @@ static int decode_frame_ga(AVCodecContext *avctx, AACDecContext *ac,
         ac->oc[1].status = OC_LOCKED;
     }
 
+    if (samples && avctx->sample_rate <= 0) {
+        av_log(avctx, AV_LOG_ERROR,
+               "Cannot output a frame without a valid sample rate\n");
+        return AVERROR_INVALIDDATA;
+    }
+
     if (!ac->frame->data[0] && samples) {
         av_log(avctx, AV_LOG_ERROR, "no frame data found\n");
         return AVERROR_INVALIDDATA;
