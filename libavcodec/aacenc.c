55b510c702fc4857da96420fe1405817b6847ea5
James Almer <jamrial@gmail.com>
2026-06-02 12:08:44 -0300

    avcodec/aacenc: fix signaled channel pairing for 6.1 PCE layout
    Signed-off-by: James Almer <jamrial@gmail.com>

--- libavcodec/aacenc.c
+++ libavcodec/aacenc.c
@@ -235,7 +235,7 @@ static const AACPCEInfo aac_pce_configs[] = {
     {
         .layout = AV_CHANNEL_LAYOUT_6POINT1_BACK,
         .num_ele = { 2, 0, 2, 1 },
-        .pairing = { { 0, 1 },{ 1, 0 },{ 0 }, },
+        .pairing = { { 0, 1 },{ 0 },{ 1, 0 },{ 0 } },
         .index = { { 0, 0 },{ 0 },{ 1, 1 },{ 0 } },
         .config_map = { 5, TYPE_SCE, TYPE_CPE, TYPE_CPE, TYPE_SCE, TYPE_LFE },
         .reorder_map = { 2, 0, 1, 4, 5, 6, 3 },
