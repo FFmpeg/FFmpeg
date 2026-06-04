8c2c31f80c20d14eadd12dfde04b5ce2421d5223
Andreas Rheinhardt <andreas.rheinhardt@outlook.com>
2026-06-04 13:47:02 +0200

    avcodec/utils: Don't attempt to derive CodecID name from de/encoders
    The list of codec descriptors is supposed to encompass all codec IDs;
    it certainly encompasses all codec IDs used by de/encoders (this is
    checked in the avcodec test program which is run via FATE).
    So the avcodec_find_decoder()/avcodec_find_encoder() are pointless.
    
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

--- libavcodec/utils.c
+++ libavcodec/utils.c
@@ -406,20 +406,12 @@ int avpriv_codec_get_cap_skip_frame_fill_param(const AVCodec *codec){
 const char *avcodec_get_name(enum AVCodecID id)
 {
     const AVCodecDescriptor *cd;
-    const AVCodec *codec;
 
     if (id == AV_CODEC_ID_NONE)
         return "none";
     cd = avcodec_descriptor_get(id);
     if (cd)
         return cd->name;
-    av_log(NULL, AV_LOG_WARNING, "Codec 0x%x is not in the full list.\n", id);
-    codec = avcodec_find_decoder(id);
-    if (codec)
-        return codec->name;
-    codec = avcodec_find_encoder(id);
-    if (codec)
-        return codec->name;
     return "unknown_codec";
 }
 
