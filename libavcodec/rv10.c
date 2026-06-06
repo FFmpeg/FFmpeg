8fae91a6ab94adc43188f2e02b8721db706ae855
Michael Niedermayer <michael@niedermayer.cc>
2026-06-06 18:51:04 +0200

    avcodec/rv10, rv34: check init_get_bits8() before RealVideo bit access
    Found-by: Samarth Kumbla <samarthk@cantina.security>
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit 0c662529f66c289dc90b5c9e7b7c56a385ab92d8)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/rv10.c
+++ libavcodec/rv10.c
@@ -415,8 +415,9 @@ static int rv10_decode_packet(AVCodecContext *avctx, const uint8_t *buf,
     H263DecContext *const h = &rv->h;
     int mb_count, mb_pos, left, start_mb_x, active_bits_size, ret;
 
+    if ((ret = init_get_bits8(&h->gb, buf, FFMAX(buf_size, buf_size2))) < 0)
+        return ret;
     active_bits_size = buf_size * 8;
-    init_get_bits(&h->gb, buf, FFMAX(buf_size, buf_size2) * 8);
     if (h->c.codec_id == AV_CODEC_ID_RV10)
         mb_count = rv10_decode_picture_header(h);
     else
