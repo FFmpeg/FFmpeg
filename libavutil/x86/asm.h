93d57157ed625527fbd0beb855a5b19973a1c381
Kirill A. Korinsky <kirill@korins.ky>
2026-06-19 23:31:20 +0200

    configure/x86: split 7-register check from EBP availability
    LLVM commit
    https://github.com/llvm/llvm-project/commit/0d471b3f64d3116bd57c79d872f7384fff80daa5
    made Clang save and restore EBP around inline asm that only
    clobbers it. This can make the existing EBP crash probe pass even
    when the compiler still cannot allocate the 7-register i386 inline
    asm used behind HAVE_7REGS.
    
    Keep ebp_available for HAVE_6REGS and add a separate
    x86_32_7regs probe for HAVE_7REGS.
    
    Discussion:
    https://marc.info/?t=178015389900001&r=1&w=2
    (cherry picked from commit b57ff00bcf485ad06b1619d31499a6db27a636b0)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavutil/x86/asm.h
+++ libavutil/x86/asm.h
@@ -71,7 +71,7 @@ typedef int32_t x86_reg;
 typedef int x86_reg;
 #endif
 
-#define HAVE_7REGS (ARCH_X86_64 || (HAVE_EBX_AVAILABLE && HAVE_EBP_AVAILABLE))
+#define HAVE_7REGS (ARCH_X86_64 || HAVE_X86_32_7REGS)
 #define HAVE_6REGS (ARCH_X86_64 || (HAVE_EBX_AVAILABLE || HAVE_EBP_AVAILABLE))
 
 #if ARCH_X86_64 && defined(PIC)
