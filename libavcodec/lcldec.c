25867a16702756b261fee5d2c7b46f0d9aea94f0
Michael Niedermayer <michael@niedermayer.cc>
2026-06-28 19:04:07 +0200

    avcodec/lcldec: zero the not-decoded tail to avoid heap disclosure
    Fixes: use of uninitialized memory
    Fixes: CsNDKB1K1U0C
    Fixes: e2c3aa8e2b (avcodec/lcldec: More space for rgb24)
    Found-by: Adrian Junge (vurlo)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    (cherry picked from commit e7cbfd1c507b57a806a5825b87d609963e862c8c)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/lcldec.c
+++ libavcodec/lcldec.c
@@ -119,6 +119,9 @@ static unsigned int mszh_decomp(const unsigned char * srcptr, int srclen, unsign
         }
     }
 
+    if (destptr < destptr_end)
+        memset(destptr, 0, destptr_end - destptr);
+
     return destptr - destptr_bak;
 }
 
@@ -152,8 +155,11 @@ static int zlib_decomp(AVCodecContext *avctx, const uint8_t *src, int src_len, i
     if (expected != (unsigned int)zstream->total_out) {
         av_log(avctx, AV_LOG_ERROR, "Decoded size differs (%d != %lu)\n",
                expected, zstream->total_out);
-        if (expected > (unsigned int)zstream->total_out)
+        if (expected > (unsigned int)zstream->total_out) {
+            memset(c->decomp_buf + offset + zstream->total_out, 0,
+                   c->decomp_size - offset - zstream->total_out);
             return (unsigned int)zstream->total_out;
+        }
         return AVERROR_UNKNOWN;
     }
     return zstream->total_out;
