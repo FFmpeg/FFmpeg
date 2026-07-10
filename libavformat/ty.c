1d805749bab767296a91b139d9d5a00ac07b9d53
Michael Niedermayer <michael@niedermayer.cc>
2026-07-10 04:07:35 +0200

    avformat/ty: don't let the Series2 AC3 trim underflow the packet size
    Fixes: negative-size-param
    Fixes: ty-s2-ac3-negative-size-single-file.ffconcat / create_poc.py
    Fixes: g0qeE6KvrjZi
    Found-by: Adrian Junge (vurlo)
    (cherry picked from commit de771bd52774a52d45b0e2c82e56995a1ef40df7)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavformat/ty.c
+++ libavformat/ty.c
@@ -577,7 +577,7 @@ static int demux_audio(AVFormatContext *s, TyRecHdr *rec_hdr, AVPacket *pkt)
         if (ty->audio_type == TIVO_AUDIO_AC3 &&
                 ty->tivo_series == TIVO_SERIES2) {
             if (ty->ac3_pkt_size + pkt->size > AC3_PKT_LENGTH) {
-                pkt->size -= 2;
+                pkt->size -= FFMIN(pkt->size, 2);
                 ty->ac3_pkt_size = 0;
             } else {
                 ty->ac3_pkt_size += pkt->size;
