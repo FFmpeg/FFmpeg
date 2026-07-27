42970ac7e2a18334e1cc645f6a39194778c4e35a
Andreas Rheinhardt <andreas.rheinhardt@outlook.com>
2026-07-27 21:22:16 +0200

    tests/checkasm/checkasm: Map rnd to checkasm_rand_uint32
    Before the switch to libcheckasm, rnd returned an unsigned value;
    now it only returns a signed value in the range 0..INT_MAX,
    so that the sign bit is not random at all. This means that when
    rnd is used to initialize four bytes at a time, one bit is not random.
    Fix this by mapping rnd to checkasm_rand_uint32().
    
    Reviewed-by: Niklas Haas <ffmpeg@haasn.dev>
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

--- tests/checkasm/checkasm.h
+++ tests/checkasm/checkasm.h
@@ -133,7 +133,7 @@ void checkasm_check_vvc_alf(void);
 void checkasm_check_vvc_mc(void);
 void checkasm_check_vvc_sao(void);
 
-#define rnd checkasm_rand
+#define rnd checkasm_rand_uint32
 #define declare_func_float declare_func
 #define bench(...) checkasm_bench(__VA_ARGS__)
 
