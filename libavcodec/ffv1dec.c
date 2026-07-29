e479cc6e2411975769a5fed2874758d53f62e497
Michael Niedermayer <michael@niedermayer.cc>
2026-07-30 01:40:49 +0200

    avcodec/ffv1dec: mark the slice damaged when its remap fails
    (cherry picked from commit 8ab8b4fabe8440d32a966f355dc2102d86336673)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/ffv1dec.c
+++ libavcodec/ffv1dec.c
@@ -415,8 +415,10 @@ static int decode_slice(AVCodecContext *c, void *arg)
         }
 
         ret = decode_remap(f, sc);
-        if (ret < 0)
+        if (ret < 0) {
+            slice_set_damaged(f, sc);
             return ret;
+        }
     }
 
     if (ac == AC_GOLOMB_RICE) {
