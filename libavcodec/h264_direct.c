42b915e10aa3d12c67142fa15e36f93d7d13b526
Kacper Michajłow <kasper93@gmail.com>
2026-07-30 17:25:52 +0200

    avcodec/h264_direct: move check before loading things
    (cherry picked from commit 029c80900e2b92a8495e36aa19d7fac9c55b8482)

--- libavcodec/h264_direct.c
+++ libavcodec/h264_direct.c
@@ -184,12 +184,12 @@ void ff_h264_direct_ref_list_init(const H264Context *const h, H264SliceContext *
 static void await_reference_mb_row(const H264Context *const h, H264Ref *ref,
                                    int mb_y)
 {
+    if (!HAVE_THREADS || !(h->avctx->active_thread_type & FF_THREAD_FRAME))
+        return;
+
     int ref_field         = ref->reference - 1;
     int ref_field_picture = ref->parent->field_picture;
     int ref_height        = 16 * h->mb_height >> ref_field_picture;
-
-    if (!HAVE_THREADS || !(h->avctx->active_thread_type & FF_THREAD_FRAME))
-        return;
 
     /* FIXME: It can be safe to access mb stuff
      * even if pixels aren't deblocked yet. */
