1c82c684c1272b6b97c9143623e1a24b793f8a15
iSold Leo <s@qwqlog.com>
2026-08-04 14:31:44 +0800

    swscale: avoid overflow in fast bilinear edge handling
    The final edge clamp computes the source position using int
    multiplication before shifting. With sufficiently wide inputs this
    overflows, which may suppress the clamp and leave the last output
    pixels interpolated with the padding byte.
    
    Promote the multiplication to int64_t in the C, MMXEXT and VSX
    implementations.
    
    Add a regression test covering the rightmost pixel of a wide upscale,
    which is wrong before this change on both the C and the MMXEXT path.
    
    Fixes: signed integer overflow: 15 * 255918080 cannot be represented in type 'int'
    Fixes: #21591
    Signed-off-by: iSold Leo <s@qwqlog.com>
    (cherry picked from commit ec2a4105e2a5db5941e93326ee89897774a95462)
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libswscale/hscale_fast_bilinear.c
+++ libswscale/hscale_fast_bilinear.c
@@ -31,7 +31,7 @@ void ff_hyscale_fast_c(SwsInternal *c, int16_t *dst, int dstWidth,
         dst[i] = (src[xx] << 7) + (src[xx + 1] - src[xx]) * xalpha;
         xpos  += xInc;
     }
-    for (i=dstWidth-1; (i*xInc)>>16 >=srcW-1; i--)
+    for (i=dstWidth-1; (i*(int64_t)xInc)>>16 >=srcW-1; i--)
         dst[i] = src[srcW-1]*128;
 }
 
@@ -48,7 +48,7 @@ void ff_hcscale_fast_c(SwsInternal *c, int16_t *dst1, int16_t *dst2,
         dst2[i] = (src2[xx] * (xalpha ^ 127) + src2[xx + 1] * xalpha);
         xpos   += xInc;
     }
-    for (i=dstWidth-1; (i*xInc)>>16 >=srcW-1; i--) {
+    for (i=dstWidth-1; (i*(int64_t)xInc)>>16 >=srcW-1; i--) {
         dst1[i] = src1[srcW-1]*128;
         dst2[i] = src2[srcW-1]*128;
     }
