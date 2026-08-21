ecd49a58ece198e6fd538f6763e47d9f579cbc89
Kacper Michajłow <kasper93@gmail.com>
2026-08-21 02:11:43 +0200

    avcodec/vvc: remove workaround for broken win32 stdatomic emulation
    The compat header now takes a pointer to the corresponding non-atomic
    type in atomic_compare_exchange_strong() as the standard requires, so
    plain int is correct everywhere.
    
    Note that VVC is the only place that worked around this, all other
    users were simply broken with the old emulation.
    
    Signed-off-by: Kacper Michajłow <kasper93@gmail.com>

--- libavcodec/vvc/thread.c
+++ libavcodec/vvc/thread.c
@@ -646,11 +646,7 @@ static void task_run_stage(VVCTask *t, VVCContext *s, VVCLocalContext *lc)
 
     if (!atomic_load(&ft->ret)) {
         if ((ret = run[stage](s, lc, t)) < 0) {
-#ifdef COMPAT_ATOMICS_WIN32_STDATOMIC_H
-            intptr_t zero = 0;
-#else
             int zero = 0;
-#endif
             atomic_compare_exchange_strong(&ft->ret, &zero, ret);
             av_log(s->avctx, AV_LOG_ERROR,
                 "frame %5d, %s(%3d, %3d) failed with %d\r\n",
