a7fe7e163f6d451d7428137866658bf1860c40a2
Kacper Michajłow <kasper93@gmail.com>
2026-08-22 17:41:23 +0200

    avcodec: remove checks for compilers that cannot pass the C11 check
    gcc 4.4, clang 2.x and llvm-gcc are all older than the C11 requirement.
    Only the icl case of BROKEN_COMPILER remains.
    
    This also enables the existing ARMv6 decode_blockcodes() inline assembly
    for Clang, whose GCC compatibility version previously excluded it.
    
    Signed-off-by: Kacper Michajłow <kasper93@gmail.com>

--- libavcodec/arm/dca.h
+++ libavcodec/arm/dca.h
@@ -26,7 +26,7 @@
 #include "config.h"
 #include "libavcodec/mathops.h"
 
-#if HAVE_ARMV6_INLINE && AV_GCC_VERSION_AT_LEAST(4,4) && !CONFIG_THUMB
+#if HAVE_ARMV6_INLINE && !CONFIG_THUMB
 
 #define decode_blockcodes decode_blockcodes
 static inline int decode_blockcodes(int code1, int code2, int levels,
--- libavcodec/x86/cabac.h
+++ libavcodec/x86/cabac.h
@@ -29,9 +29,7 @@
 #include "libavutil/x86/asm.h"
 #include "config.h"
 
-#if   (defined(__i386) && defined(__clang__) && (__clang_major__<2 || (__clang_major__==2 && __clang_minor__<10)))\
-   || (                  !defined(__clang__) && defined(__llvm__) && __GNUC__==4 && __GNUC_MINOR__==2 && __GNUC_PATCHLEVEL__<=1)\
-   || (defined(__INTEL_COMPILER) && defined(_MSC_VER))
+#if defined(__INTEL_COMPILER) && defined(_MSC_VER)
 #       define BROKEN_COMPILER 1
 #else
 #       define BROKEN_COMPILER 0
