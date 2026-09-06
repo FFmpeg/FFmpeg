273141261699b660eed1b1c070fb8b1dd88c5218
Michael Niedermayer <michael@niedermayer.cc>
2026-09-07 00:37:01 +0200

    avcodec/nellymoserenc: initialize the trellis table row by row
    Fixes: index 35768 out of bounds for type 'float[35768]'
    Fixes: 9UQo8TUCJdYw
    Found-by: Zheng Yu <zheng@depthfirst.com>

--- libavcodec/nellymoserenc.c
+++ libavcodec/nellymoserenc.c
@@ -243,9 +243,9 @@ static void get_exponent_dynamic(NellyMoserEncodeContext *s, float *cand, int *i
     float  (*opt )[OPT_SIZE] = s->opt ;
     uint8_t(*path)[OPT_SIZE] = s->path;
 
-    for (i = 0; i < NELLY_BANDS * OPT_SIZE; i++) {
-        opt[0][i] = INFINITY;
-    }
+    for (band = 0; band < NELLY_BANDS; band++)
+        for (i = 0; i < OPT_SIZE; i++)
+            opt[band][i] = INFINITY;
 
     for (i = 0; i < 64; i++) {
         opt[0][ff_nelly_init_table[i]] = distance(cand[0], ff_nelly_init_table[i], 0);
