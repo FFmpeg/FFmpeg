fd7c73d01e976d2e332e85862ab63ab608710834
Zhao Zhili <zhilizhao@tencent.com>
2026-09-08 21:18:35 +0800

    avcodec/videotoolboxenc: drop h264 suffix from create_encoder_dict
    The helper also used by other codec.

--- libavcodec/videotoolboxenc.c
+++ libavcodec/videotoolboxenc.c
@@ -2459,8 +2459,8 @@ static int create_cv_pixel_buffer(AVCodecContext   *avctx,
     return 0;
 }
 
-static int create_encoder_dict_h264(const AVFrame *frame,
-                                    CFDictionaryRef* dict_out)
+static int create_encoder_dict(const AVFrame *frame,
+                               CFDictionaryRef* dict_out)
 {
     CFDictionaryRef dict = NULL;
     if (frame->pict_type == AV_PICTURE_TYPE_I) {
@@ -2493,7 +2493,7 @@ static int vtenc_send_frame(AVCodecContext *avctx,
     if (status)
         goto out;
 
-    status = create_encoder_dict_h264(frame, &frame_dict);
+    status = create_encoder_dict(frame, &frame_dict);
     if (status)
         goto out;
 
