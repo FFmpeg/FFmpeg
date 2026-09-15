FFmpeg/FFmpeg

commit b0159af6bc76553b1ab7546d821bb8c16a666cfd

avfilter/f_metadata: use the return value of vsnprintf() to write the argument list
Should fix use-of-uninitialized-value under MSAN.

Signed-off-by: James Almer <jamrial@gmail.com>

libavfilter/f_metadata.c
@@ -201,8 +201,8 @@ static void print_file(AVFilterContext *ctx, const char *msg, ...)
     va_start(argument_list, msg);
     if (msg) {
         char buf[128];
-        vsnprintf(buf, sizeof(buf), msg, argument_list);
-        avio_write(s->avio_context, buf, av_strnlen(buf, sizeof(buf)));
+        int ret = vsnprintf(buf, sizeof(buf), msg, argument_list);
+        avio_write(s->avio_context, buf, ret);
     }
     va_end(argument_list);
 }

commit fd625f452c0b59a61b81833de9f65a7b601a862c

avfilter/f_metadata: avoid overread on long formatted output
vsnprintf() returns the number of characters that would have been written
when its output is truncated. Passing that value to avio_write() makes it
read beyond the 128-byte stack buffer for long metadata values.

Use avio_vprintf() to write the complete formatted string without the fixed
intermediate buffer.

Fixes: b0159af6bc76553b1ab7546d821bb8c16a666cfd
Signed-off-by: Yongqiang Tian <yqtian668@gmail.com>

libavfilter/f_metadata.c
@@ -199,11 +199,8 @@ static void print_file(AVFilterContext *ctx, const char *msg, ...)
     va_list argument_list;
 
     va_start(argument_list, msg);
-    if (msg) {
-        char buf[128];
-        int ret = vsnprintf(buf, sizeof(buf), msg, argument_list);
-        avio_write(s->avio_context, buf, ret);
-    }
+    if (msg)
+        avio_vprintf(s->avio_context, msg, argument_list);
     va_end(argument_list);
 }
 